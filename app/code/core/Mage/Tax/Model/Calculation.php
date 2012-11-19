81d6ef6c49a083aec23d37bc67bcc8800bce73d0
Joseph Maxwell <maxwellquotes@gmail.com>
2012-11-19 16:00:58 -0600

    Fixing problem in Mage_Tax_Model_Calculation that effectively prevents and externally provided rates from being used in the system (externally, like modules providing streamlined sales tax lookups, etc). Before tax_rate_data_fetch, the rate_value section of data is unset, so hasRateValue() is ALWAYS null, as there is no reference to the calculation model passed into the event. Without this fix, one has to override this model to fix the event

--- app/code/core/Mage/Tax/Model/Calculation.php
+++ app/code/core/Mage/Tax/Model/Calculation.php
@@ -188,7 +188,7 @@ class Mage_Tax_Model_Calculation extends Mage_Core_Model_Abstract
             $this->unsRateValue();
             $this->unsCalculationProcess();
             $this->unsEventModuleId();
-            Mage::dispatchEvent('tax_rate_data_fetch', array('request'=>$request));
+            Mage::dispatchEvent('tax_rate_data_fetch', array('request'=>$request, 'sender'=>$this));
             if (!$this->hasRateValue()) {
                 $rateInfo = $this->_getResource()->getRateInfo($request);
                 $this->setCalculationProcess($rateInfo['process']);
