5f4562f82a2809dc15de1a331e45098c97c816dc
readmanr <robert_readman@hotmail.com>
2013-10-07 07:17:03 +0100

    Spelling correction for 'email' within NOTICE OF LICENSE Whilist accepted as correct, e-mail is a 1980s: abbreviation of electronic mail. As such should be correctly spelt as 'e-mail' and not email. This commit correct's the spelling within the NOTICE OF LICENSE header. Future commits can then be made to correct further misspellings with adequate testing.

--- app/code/Magento/Backend/Block/Template/Context.php
+++ app/code/Magento/Backend/Block/Template/Context.php
@@ -14,7 +14,7 @@ namespace Magento\Backend\Block\Template;
  * It is also available through the world-wide-web at this URL:
  * http://opensource.org/licenses/osl-3.0.php
  * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
+ * obtain it through the world-wide-web, please send an e-mail
  * to license@magentocommerce.com so we can send you a copy immediately.
  *
  * DISCLAIMER
--- dev/tests/integration/testsuite/Magento/Webapi/Model/Acl/RuleTest.php
+++ dev/tests/integration/testsuite/Magento/Webapi/Model/Acl/RuleTest.php
@@ -13,7 +13,7 @@ namespace Magento\Webapi\Model\Acl;
  * It is also available through the world-wide-web at this URL:
  * http://opensource.org/licenses/osl-3.0.php
  * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
+ * obtain it through the world-wide-web, please send an e-mail
  * to license@magentocommerce.com so we can send you a copy immediately.
  *
  * DISCLAIMER
