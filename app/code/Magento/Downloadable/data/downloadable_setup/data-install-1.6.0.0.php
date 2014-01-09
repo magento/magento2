359b0b48bf42874235b5feb9dd60778dfeec524f
Jason Neumann <jason.neumann@destxp.com>
2014-01-09 13:15:16 -0600

    apply_to downloadable fix
    if one of the fields apply_to is null here (which means it applies to all products) it was getting set to only apply to downloadable products. I saw this specifically with the cost attribute.

--- app/code/Magento/Downloadable/data/downloadable_setup/data-install-1.6.0.0.php
+++ app/code/Magento/Downloadable/data/downloadable_setup/data-install-1.6.0.0.php
@@ -38,8 +38,9 @@ $fieldList = array(
 
 // make these attributes applicable to downloadable products
 foreach ($fieldList as $field) {
-    $applyTo = explode(',', $installer->getAttribute(\Magento\Catalog\Model\Product::ENTITY, $field, 'apply_to'));
-    if (!in_array('downloadable', $applyTo)) {
+    $applyToStr = $installer->getAttribute(Mage_Catalog_Model_Product::ENTITY, $field, 'apply_to');
+    $applyTo = explode(',', $applyToStr);
+    if (!empty($applyToStr) && !in_array('downloadable', $applyTo)) {
         $applyTo[] = 'downloadable';
         $installer->updateAttribute(\Magento\Catalog\Model\Product::ENTITY, $field, 'apply_to', implode(',', $applyTo));
     }
