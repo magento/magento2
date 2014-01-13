482a4e1a4695ddb9a061a8c6ff392b8db7482deb
Jason Neumann <jason.neumann@destxp.com>
2014-01-13 12:50:11 -0600

    another place apply to gets broken

--- app/code/Magento/Bundle/data/bundle_setup/data-install-1.6.0.0.php
+++ app/code/Magento/Bundle/data/bundle_setup/data-install-1.6.0.0.php
@@ -32,8 +32,9 @@ $fieldList = array(
     'minimal_price','cost','tier_price','weight'
 );
 foreach ($fieldList as $field) {
-    $applyTo = explode(',', $installer->getAttribute(\Magento\Catalog\Model\Product::ENTITY, $field, 'apply_to'));
-    if (!in_array('bundle', $applyTo)) {
+    $applyToStr = $installer->getAttribute(Mage_Catalog_Model_Product::ENTITY, $field, 'apply_to');
+	$applyTo = explode(',', $applyToStr);
+	if (!empty($applyToStr) &&!in_array('bundle', $applyTo)) {
         $applyTo[] = 'bundle';
         $installer->updateAttribute(\Magento\Catalog\Model\Product::ENTITY, $field, 'apply_to', implode(',', $applyTo));
     }
