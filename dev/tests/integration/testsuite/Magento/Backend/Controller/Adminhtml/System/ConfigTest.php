d67d3c405f6cc7666c10fce70e74a702d90976a7
Arkadii Chyzhov <achyzhov@ebay.com>
2014-12-23 17:07:50 +0200

    MAGETWO-31863: Can't login to Admin from first attempt if change Base URL
    - added test

--- dev/tests/integration/testsuite/Magento/Backend/Controller/Adminhtml/System/ConfigTest.php
+++ dev/tests/integration/testsuite/Magento/Backend/Controller/Adminhtml/System/ConfigTest.php
@@ -14,4 +14,38 @@ class ConfigTest extends \Magento\Backend\Utility\Controller
         $this->dispatch('backend/admin/system_config/edit');
         $this->assertContains('<ul id="system_config_tabs"', $this->getResponse()->getBody());
     }
+
+    /**
+     * @magentoDbIsolation enabled
+     */
+    public function testChangeBaseUrl()
+    {
+        $newHost = 'm2test123.loc';
+        $request = $this->getRequest();
+        $request->setPost(
+            ['groups' =>
+                ['unsecure' =>
+                    ['fields' =>
+                        ['base_url' =>
+                            ['value' => 'http://' . $newHost . '/']
+                        ]
+                    ]
+                ],
+            'config_state' =>
+                ['web_unsecure' => 1]
+            ]
+        )->setParam(
+            'section',
+            'web'
+        );
+        $this->dispatch('backend/admin/system_config/save');
+
+        $this->assertTrue($this->getResponse()->isRedirect(), 'Redirect was expected, but none was performed.');
+
+        /** @var array|bool $url */
+        $url = parse_url($this->getResponse()->getHeader('Location')['value']);
+        $this->assertArrayNotHasKey('query', $url, 'No GET params were expected, but somewhat exists');
+        $this->assertEquals($newHost, $url['host'], 'A new host in the url expected, but there is old one');
+    }
 }
+
