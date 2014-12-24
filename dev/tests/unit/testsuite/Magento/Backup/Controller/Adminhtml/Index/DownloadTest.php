cc2e42410d53c327e033490781cb24feca8f2ad7
vpaladiychuk <vpaladiychuk@ebay.com>
2014-12-24 15:40:13 +0200

    MAGETWO-2204: "HEADERS ALREADY SENT" When Controller Action Outputs Directly

--- dev/tests/unit/testsuite/Magento/Backup/Controller/Adminhtml/Index/DownloadTest.php
+++ dev/tests/unit/testsuite/Magento/Backup/Controller/Adminhtml/Index/DownloadTest.php
@@ -76,4 +76,62 @@ class DownloadTest extends \PHPUnit_Framework_TestCase
         );
         $this->assertSame($resultRaw, $controller->execute());
     }
+
+    /**
+     * @dataProvider providerExecuteBackupNotFound
+     * @param string $time
+     * @param bool $exists
+     * @param \PHPUnit_Framework_MockObject_Matcher_InvokedCount $existsCount
+     */
+    public function testExecuteBackupNotFound($time, $exists, $existsCount)
+    {
+        $type = 'db';
+
+        $backup = $this->getMock('\Magento\Backup\Model\Backup', ['getTime', 'exists'], [], '', false);
+        $backup->expects($this->once())->method('getTime')->will($this->returnValue($time));
+        $backup->expects($existsCount)->method('exists')->will($this->returnValue($exists));
+
+        $response = $this->getMock('\Magento\Framework\App\ResponseInterface', [], [], '', false);
+
+        $request = $this->getMock('\Magento\Framework\App\RequestInterface', [], [], '', false);
+        $request->expects($this->at(0))->method('getParam')->with('time')->will($this->returnValue($time));
+        $request->expects($this->at(1))->method('getParam')->with('type')->will($this->returnValue($type));
+
+        $context = $this->getMock('\Magento\Backend\App\Action\Context', [], [], '', false);
+        $context->expects($this->once())->method('getRequest')->will($this->returnValue($request));
+        $context->expects($this->once())->method('getResponse')->will($this->returnValue($response));
+
+        $backupModelFactory = $this->getMock('\Magento\Backup\Model\BackupFactory', [], [], '', false);
+        $backupModelFactory->expects($this->once())->method('create')->with($time, $type)
+            ->will($this->returnValue($backup));
+
+        $resultRedirect = $this->getMock('Magento\Backend\Model\View\Result\Redirect', [], [], '', false);
+        $resultRedirect->expects($this->once())->method('setPath')->with('backup/*');
+
+        $resultRedirectFactory = $this->getMock('Magento\Backend\Model\View\Result\RedirectFactory', [], [], '', false);
+        $resultRedirectFactory->expects($this->once())->method('create')->will($this->returnValue($resultRedirect));
+
+        /** @var Download|\PHPUnit_Framework_MockObject_MockObject $controller */
+        $controller = (new \Magento\TestFramework\Helper\ObjectManager($this))->getObject(
+            'Magento\Backup\Controller\Adminhtml\Index\Download',
+            [
+                'context' => $context,
+                'backupModelFactory' => $backupModelFactory,
+                'resultRedirectFactory' => $resultRedirectFactory
+            ]
+        );
+        $this->assertSame($resultRedirect, $controller->execute());
+    }
+
+    /**
+     * @return array
+     */
+    public function providerExecuteBackupNotFound()
+    {
+        return [
+            [1, false, $this->once()],
+            [0, true, $this->never()],
+            [0, false, $this->never()]
+        ];
+    }
 } 
