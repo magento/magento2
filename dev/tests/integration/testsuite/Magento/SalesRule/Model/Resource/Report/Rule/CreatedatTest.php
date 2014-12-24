f1b21e05c9fcaaafd5753098ed0570979b8331b1
Sergey Ivashchenko <sivashchenko@ebay.com>
2014-12-24 14:29:15 +0200

    MAGETWO-7359: Incorrect totals shown on coupon usage report

--- dev/tests/integration/testsuite/Magento/SalesRule/Model/Resource/Report/Rule/CreatedatTest.php
+++ dev/tests/integration/testsuite/Magento/SalesRule/Model/Resource/Report/Rule/CreatedatTest.php
@@ -1,9 +1,6 @@
 <?php
 /**
- * {license_notice}
- *
- * @copyright   {copyright}
- * @license     {license_link}
+ * @copyright Copyright (c) 2014 X.commerce, Inc. (http://www.magentocommerce.com)
  */
 namespace Magento\SalesRule\Model\Resource\Report\Rule;
 
