226800f6be52f8d05db3ab17acfa110fe5a2d3db
Natalia Momotenko <nmomotenko@ebay.com>
2014-12-24 18:38:05 +0200

    MAGETWO-31855: UI improvements

--- dev/tests/functional/tests/app/Magento/Customer/Test/Block/Account/AddressesDefault.php
+++ dev/tests/functional/tests/app/Magento/Customer/Test/Block/Account/AddressesDefault.php
@@ -19,7 +19,7 @@ class AddressesDefault extends Block
      *
      * @var string
      */
-    protected $changeBillingAddressSelector = '.box-address-billing a';
+    protected $changeBillingAddressSelector = '.box-billing-address a';
 
     /**
      * Click on address book menu item
