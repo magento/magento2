08a812159508c42e8c65af95277a1f6bd4a2d35b
Iryna Lagno <ilagno@ebay.com>
2014-12-25 17:23:37 +0200

    MAGETWO-32116: Customer model and tables cleaning

--- app/code/Magento/Customer/sql/customer_setup/upgrade-2.0.0-2.0.0.1.php
+++ app/code/Magento/Customer/sql/customer_setup/upgrade-2.0.0-2.0.0.1.php
@@ -8,150 +8,36 @@ $installer = $this;
 $installer->startSetup();
 $connection = $installer->getConnection();
 
-//Drop entity_type_id and attribute_set_id from column for customer_address_entity
-$connection->dropForeignKey(
-    $installer->getTable('customer_address_entity_varchar'),
-    $installer->getFkName('customer_address_entity_varchar', 'entity_type_id', 'eav_entity_type', 'entity_type_id')
-);
-$connection->dropIndex(
-    'customer_address_entity_varchar',
-    $installer->getIdxName(
-        'customer_address_entity_varchar',
-        ['entity_type_id'],
-        \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_UNIQUE
-    )
-);
+$tableNames = [
+    'customer_address_entity_varchar', 'customer_address_entity_datetime',
+    'customer_address_entity_decimal', 'customer_address_entity_int', 'customer_address_entity_text',
+    'customer_entity_varchar', 'customer_entity_datetime',
+    'customer_entity_decimal', 'customer_entity_int', 'customer_entity_text'
+];
 
-$connection->dropForeignKey(
-    $installer->getTable('customer_address_entity_datetime'),
-    $installer->getFkName('customer_address_entity_datetime', 'entity_type_id', 'eav_entity_type', 'entity_type_id')
-);
-$connection->dropIndex(
-    'customer_address_entity_datetime',
-    $installer->getIdxName(
-        'customer_address_entity_datetime',
-        ['entity_type_id'],
-        \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_UNIQUE
-    )
-);
+foreach ($tableNames as $table) {
+    $connection->dropForeignKey(
+        $installer->getTable($table),
+        $installer->getFkName($table, 'entity_type_id', 'eav_entity_type', 'entity_type_id')
+    );
+    $connection->dropIndex(
+        $installer->getTable($table),
+        $installer->getIdxName(
+            $installer->getTable($table),
+            ['entity_type_id'],
+            \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_UNIQUE
+        )
+    );
+    $connection->dropColumn($installer->getTable($table), 'entity_type_id');
+}
 
-$connection->dropForeignKey(
-    $installer->getTable('customer_address_entity_decimal'),
-    $installer->getFkName('customer_address_entity_decimal', 'entity_type_id', 'eav_entity_type', 'entity_type_id')
-);
-$connection->dropIndex(
-    'customer_address_entity_decimal',
-    $installer->getIdxName(
-        'customer_address_entity_decimal',
-        ['entity_type_id'],
-        \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_UNIQUE
-    )
-);
-
-$connection->dropForeignKey(
-    $installer->getTable('customer_address_entity_int'),
-    $installer->getFkName('customer_address_entity_int', 'entity_type_id', 'eav_entity_type', 'entity_type_id')
-);
-$connection->dropIndex(
-    'customer_address_entity_int',
-    $installer->getIdxName(
-        'customer_address_entity_int',
-        ['entity_type_id'],
-        \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_UNIQUE
-    )
-);
-
-$connection->dropForeignKey(
-    $installer->getTable('customer_address_entity_text'),
-    $installer->getFkName('customer_address_entity_text', 'entity_type_id', 'eav_entity_type', 'entity_type_id')
-);
-$connection->dropIndex(
-    'customer_address_entity_text',
-    $installer->getIdxName(
-        'customer_address_entity_text',
-        ['entity_type_id'],
-        \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_UNIQUE
-    )
-);
 $connection->dropColumn($installer->getTable('customer_address_entity'), 'entity_type_id');
 $connection->dropColumn($installer->getTable('customer_address_entity'), 'attribute_set_id');
-$connection->dropColumn($installer->getTable('customer_address_entity_datetime'), 'entity_type_id');
-$connection->dropColumn($installer->getTable('customer_address_entity_decimal'), 'entity_type_id');
-$connection->dropColumn($installer->getTable('customer_address_entity_int'), 'entity_type_id');
-$connection->dropColumn($installer->getTable('customer_address_entity_text'), 'entity_type_id');
-$connection->dropColumn($installer->getTable('customer_address_entity_varchar'), 'entity_type_id');
 
-//Drop entity_type_id and attribute_set_id from column for customer_entity
 $connection->dropIndex(
-    'customer_entity',
+    $installer->getTable('customer_entity'),
     $installer->getIdxName('customer_entity', ['entity_type_id'])
-);
-
-$connection->dropForeignKey(
-    $installer->getTable('customer_entity_varchar'),
-    $installer->getFkName('customer_entity_varchar', 'entity_type_id', 'eav_entity_type', 'entity_type_id')
-);
-$connection->dropIndex(
-    'customer_entity_varchar',
-    $installer->getIdxName(
-        'customer_entity_varchar',
-        ['entity_type_id'],
-        \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_UNIQUE
-    )
-);
-
-$connection->dropForeignKey(
-    $installer->getTable('customer_entity_datetime'),
-    $installer->getFkName('customer_entity_datetime', 'entity_type_id', 'eav_entity_type', 'entity_type_id')
-);
-$connection->dropIndex(
-    'customer_entity_datetime',
-    $installer->getIdxName(
-        'customer_entity_datetime',
-        ['entity_type_id'],
-        \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_UNIQUE
-    )
-);
-
-$connection->dropForeignKey(
-    $installer->getTable('customer_entity_decimal'),
-    $installer->getFkName('customer_entity_decimal', 'entity_type_id', 'eav_entity_type', 'entity_type_id')
-);
-$connection->dropIndex(
-    'customer_entity_decimal',
-    $installer->getIdxName(
-        'customer_entity_decimal',
-        ['entity_type_id'],
-        \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_UNIQUE
-    )
-);
-
-$connection->dropForeignKey(
-    $installer->getTable('customer_entity_int'),
-    $installer->getFkName('customer_entity_int', 'entity_type_id', 'eav_entity_type', 'entity_type_id')
-);
-$connection->dropIndex(
-    'customer_entity_int',
-    $installer->getIdxName('customer_entity_int', ['entity_type_id'])
-);
-
-$connection->dropForeignKey(
-    $installer->getTable('customer_entity_text'),
-    $installer->getFkName('customer_entity_text', 'entity_type_id', 'eav_entity_type', 'entity_type_id')
-);
-$connection->dropIndex(
-    'customer_entity_text',
-    $installer->getIdxName(
-        'customer_entity_text',
-        ['entity_type_id'],
-        \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_UNIQUE
-    )
 );
 $connection->dropColumn($installer->getTable('customer_entity'), 'entity_type_id');
 $connection->dropColumn($installer->getTable('customer_entity'), 'attribute_set_id');
-$connection->dropColumn($installer->getTable('customer_entity_datetime'), 'entity_type_id');
-$connection->dropColumn($installer->getTable('customer_entity_decimal'), 'entity_type_id');
-$connection->dropColumn($installer->getTable('customer_entity_int'), 'entity_type_id');
-$connection->dropColumn($installer->getTable('customer_entity_text'), 'entity_type_id');
-$connection->dropColumn($installer->getTable('customer_entity_varchar'), 'entity_type_id');
 $installer->endSetup();
