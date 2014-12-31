69efac9254682c1ec364c2aa499b94d11b7b4128
Leonid Poluyanov <lpoluyanov@ebay.com>
2014-12-31 13:10:24 +0200

    MAGETWO-32058: Men and Women categories position is wrong

--- dev/tests/integration/testsuite/Magento/Catalog/Model/CategoryTest.php
+++ dev/tests/integration/testsuite/Magento/Catalog/Model/CategoryTest.php
@@ -114,6 +114,150 @@ class CategoryTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
+     * @magentoDataFixture Magento/Core/_files/store.php
+     * @magentoAppIsolation enabled
+     * @magentoConfigFixture current_store catalog/frontend/flat_catalog_product 1
+     */
+    public function testSetStoreIdWithNonNumericValue()
+    {
+        /** @var $store \Magento\Store\Model\Store */
+        $store = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create('Magento\Store\Model\Store');
+        $store->load('fixturestore');
+
+        $this->assertNotEquals($this->_model->getStoreId(), $store->getId());
+
+        $this->_model->setStoreId('fixturestore');
+
+        $this->assertEquals($this->_model->getStoreId(), $store->getId());
+    }
+
+    public function testGetUrl()
+    {
+        $this->assertStringEndsWith('catalog/category/view/', $this->_model->getUrl());
+
+        $this->_model->setUrl('test_url');
+        $this->assertEquals('test_url', $this->_model->getUrl());
+
+        $this->_model->setUrl(null);
+        $this->_model->setRequestPath('test_path');
+        $this->assertStringEndsWith('test_path', $this->_model->getUrl());
+
+        $this->_model->setUrl(null);
+        $this->_model->setRequestPath(null);
+        $this->_model->setId(1000);
+        $this->assertStringEndsWith('catalog/category/view/id/1000/', $this->_model->getUrl());
+    }
+
+    public function testGetCategoryIdUrl()
+    {
+        $this->assertStringEndsWith('catalog/category/view/', $this->_model->getCategoryIdUrl());
+        $this->_model->setUrlKey('test_key');
+        $this->assertStringEndsWith('catalog/category/view/s/test_key/', $this->_model->getCategoryIdUrl());
+    }
+
+    public function testFormatUrlKey()
+    {
+        $this->assertEquals('test', $this->_model->formatUrlKey('test'));
+        $this->assertEquals('test-some-chars-5', $this->_model->formatUrlKey('test-some#-chars^5'));
+        $this->assertEquals('test', $this->_model->formatUrlKey('test-????????'));
+    }
+
+    public function testGetImageUrl()
+    {
+        $this->assertFalse($this->_model->getImageUrl());
+        $this->_model->setImage('test.gif');
+        $this->assertStringEndsWith('media/catalog/category/test.gif', $this->_model->getImageUrl());
+    }
+
+    public function testGetCustomDesignDate()
+    {
+        $dates = $this->_model->getCustomDesignDate();
+        $this->assertArrayHasKey('from', $dates);
+        $this->assertArrayHasKey('to', $dates);
+    }
+
+    public function testGetDesignAttributes()
+    {
+        $attributes = $this->_model->getDesignAttributes();
+        $this->assertContains('custom_design_from', array_keys($attributes));
+        $this->assertContains('custom_design_to', array_keys($attributes));
+    }
+
+    public function testCheckId()
+    {
+        $this->assertEquals(4, $this->_model->checkId(4));
+        $this->assertFalse($this->_model->checkId(111));
+    }
+
+    public function testVerifyIds()
+    {
+        $ids = $this->_model->verifyIds([1, 2, 3, 4, 100]);
+        $this->assertContains(4, $ids);
+        $this->assertNotContains(100, $ids);
+    }
+
+    public function testHasChildren()
+    {
+        $this->_model->load(3);
+        $this->assertTrue($this->_model->hasChildren());
+        $this->_model->load(5);
+        $this->assertFalse($this->_model->hasChildren());
+    }
+
+    public function testGetRequestPath()
+    {
+        $this->assertNull($this->_model->getRequestPath());
+        $this->_model->setData('request_path', 'test');
+        $this->assertEquals('test', $this->_model->getRequestPath());
+    }
+
+    public function testGetName()
+    {
+        $this->assertNull($this->_model->getName());
+        $this->_model->setData('name', 'test');
+        $this->assertEquals('test', $this->_model->getName());
+    }
+
+    public function testGetProductCount()
+    {
+        $this->_model->load(6);
+        $this->assertEquals(0, $this->_model->getProductCount());
+        $this->_model->setData([]);
+        $this->_model->load(3);
+        $this->assertEquals(1, $this->_model->getProductCount());
+    }
+
+    public function testGetAvailableSortBy()
+    {
+        $this->assertEquals([], $this->_model->getAvailableSortBy());
+        $this->_model->setData('available_sort_by', 'test,and,test');
+        $this->assertEquals(['test', 'and', 'test'], $this->_model->getAvailableSortBy());
+    }
+
+    public function testGetAvailableSortByOptions()
+    {
+        $options = $this->_model->getAvailableSortByOptions();
+        $this->assertContains('price', array_keys($options));
+        $this->assertContains('position', array_keys($options));
+        $this->assertContains('name', array_keys($options));
+    }
+
+    public function testGetDefaultSortBy()
+    {
+        $this->assertEquals('position', $this->_model->getDefaultSortBy());
+    }
+
+    public function testValidate()
+    {
+        $this->_model->addData([
+            "include_in_menu" => false,
+            "is_active" => false,
+            'name' => 'test',
+        ]);
+        $this->assertNotEmpty($this->_model->validate());
+    }
+
+    /**
      * @magentoDataFixture Magento/Catalog/_files/category_with_position.php
      */
     public function testSaveCategoryWithPosition()
