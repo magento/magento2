9ef737c0c8209585ac238f271d284cb4f4f8171f
Maksym Iakusha <miakusha@ebay.com>
2015-01-05 12:28:13 +0200

    MAGETWO-29424: [Service Layer Refactoring]: Downloadable Product Module Basic Implementation

---
+++ app/code/Magento/Downloadable/Api/Data/LinkInterface.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ *
+ * @copyright Copyright (c) 2014 X.commerce, Inc. (http://www.magentocommerce.com)
+ */
+namespace Magento\Downloadable\Api\Data;
+
+/**
+ * @codeCoverageIgnore
+ */
+interface LinkInterface extends \Magento\Framework\Api\ExtensibleDataInterface
+{
+
+    /**
+     * @return int|null Sample(or link) id
+     */
+    public function getId();
+
+
+    /**
+     * @return string|null
+     */
+    public function getTitle();
+
+    /**
+     * @return int
+     */
+    public function getSortOrder();
+
+    /**
+     * Link shareable status
+     * 0 -- No
+     * 1 -- Yes
+     * 2 -- Use config default value
+     *
+     * @return int
+     */
+    public function getShareable();
+
+    /**
+     * Link price
+     *
+     * @return float
+     */
+    public function getPrice();
+
+    /**
+     * Number of downloads per user
+     * Null for unlimited downloads
+     *
+     * @return int|null
+     */
+    public function getNumberOfDownloads();
+
+    /**
+     * File or URL of sample if any
+     *
+     * @return \Magento\Downloadable\Service\V1\DownloadableLink\Data\DownloadableResourceInfo|null
+     */
+    public function getSampleResource();
+
+
+    /**
+     * File or URL of link
+     *
+     * @return \Magento\Downloadable\Service\V1\DownloadableLink\Data\DownloadableResourceInfo
+     */
+    public function getLinkResource();
+
+}
---
+++ app/code/Magento/Downloadable/Api/LinkManagementInterface.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ *
+ * @copyright Copyright (c) 2014 X.commerce, Inc. (http://www.magentocommerce.com)
+ */
+namespace Magento\Downloadable\Api;
+
+interface LinkManagementInterface
+{
+//    /**
+//     * List of samples for downloadable product
+//     *
+//     * @param string $productSku
+//     * @return \Magento\Downloadable\Service\V1\DownloadableLink\Data\DownloadableSampleInfo[]
+//     */
+//    public function getSamples($productSku);
+
+    /**
+     * List of links with associated samples
+     *
+     * @param string $productSku
+     * @return \Magento\Downloadable\Api\Data\LinkInterface[]
+     */
+    public function getLinks($productSku);
+
+
+}
