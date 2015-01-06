c97b5f118face8034021ff1a2200a0635e2eedd7
Olexandr Lysenko <olysenko@ebay.com>
2015-01-06 11:10:30 +0200

    MAGETWO-31593: Sales Quote as Standalone Magento Module

--- dev/tests/unit/testsuite/Magento/Quote/Model/Quote/Address/ToOrderTest.php
+++ dev/tests/unit/testsuite/Magento/Quote/Model/Quote/Address/ToOrderTest.php
@@ -1,9 +1,7 @@
 <?php
 /**
- * {license_notice}
  *
- * @copyright   {copyright}
- * @license     {license_link}
+ * @copyright Copyright (c) 2014 X.commerce, Inc. (http://www.magentocommerce.com)
  */
 namespace Magento\Quote\Model\Quote\Address;
 
