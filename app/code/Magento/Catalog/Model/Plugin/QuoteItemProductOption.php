42ca76dcf0a4ce6d597b6943ca994c6c29ec4487
Anton Kaplya <akaplya@ebay.com>
2015-01-06 13:58:40 +0200

    MAGETWO-32107: Stabilization and pull request

--- app/code/Magento/Catalog/Model/Plugin/QuoteItemProductOption.php
+++ app/code/Magento/Catalog/Model/Plugin/QuoteItemProductOption.php
@@ -10,16 +10,18 @@ class QuoteItemProductOption
      * @param \Magento\Quote\Model\Quote\Item\ToOrderItem $subject
      * @param callable $proceed
      * @param \Magento\Quote\Model\Quote\Item\AbstractItem $item
+     * @param array $additional
      * @return \Magento\Sales\Model\Order\Item
      * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
     public function aroundConvert(
         \Magento\Quote\Model\Quote\Item\ToOrderItem $subject,
         \Closure $proceed,
-        \Magento\Quote\Model\Quote\Item\AbstractItem $item
+        \Magento\Quote\Model\Quote\Item\AbstractItem $item,
+        $additional
     ) {
         /** @var $orderItem \Magento\Sales\Model\Order\Item */
-        $orderItem = $proceed($item);
+        $orderItem = $proceed($item, $additional);
 
         if (is_array($item->getOptions())) {
             foreach ($item->getOptions() as $itemOption) {
