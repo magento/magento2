d88354beb03294e2f86c5fc58ab4d447f8a4c287
Michal Malinowski <michal@ascensor.co.uk>
2015-01-06 14:22:13 +0000

    fixed php standards test failture

--- app/code/Magento/AdminNotification/Controller/Adminhtml/System/Message/ListAction.php
+++ app/code/Magento/AdminNotification/Controller/Adminhtml/System/Message/ListAction.php
@@ -1,5 +1,5 @@
 <?php
-/**
+/*
  *
  * @copyright Copyright (c) 2014 X.commerce, Inc. (http://www.magentocommerce.com)
  */
@@ -13,11 +13,11 @@ class ListAction extends \Magento\Backend\App\AbstractAction
     public function execute()
     {
         $severity = $this->getRequest()->getParam('severity');
-		$default = array(
-				'severity' => $severity,
-				'text' => 'All recent issues have been fixed. Please refresh the screen for an update.'
-			);
-		
+        $default = array(
+            'severity' => $severity,
+            'text' => 'All recent issues have been fixed. Please refresh the screen for an update.'
+            );
+
         $messageCollection = $this->_objectManager->get(
             'Magento\AdminNotification\Model\Resource\System\Message\Collection'
         );
@@ -28,11 +28,11 @@ class ListAction extends \Magento\Backend\App\AbstractAction
         foreach ($messageCollection->getItems() as $item) {
             $result[] = ['severity' => $item->getSeverity(), 'text' => $item->getText()];
         }
-		
-		if (empty($result)) {
-			$result[] = $default;
-		}
-		
+
+        if (empty($result)) {
+            $result[] = $default;
+        }
+
         $this->getResponse()->representJson(
             $this->_objectManager->get('Magento\Core\Helper\Data')->jsonEncode($result)
         );
