09c61765ae707a50af29699998ec4a4de02a7a90
Michal Malinowski <michal@ascensor.co.uk>
2015-01-07 10:32:15 +0000

    reviewed coding standards with newest PEAR documentation

--- app/code/Magento/AdminNotification/Controller/Adminhtml/System/Message/ListAction.php
+++ app/code/Magento/AdminNotification/Controller/Adminhtml/System/Message/ListAction.php
@@ -15,8 +15,8 @@ class ListAction extends \Magento\Backend\App\AbstractAction
         $severity = $this->getRequest()->getParam('severity');
         $default = array(
             'severity' => $severity,
-            'text' => 'All recent issues have been fixed. Please refresh the screen for an update.'
-            );
+            'text' => 'All recent issues have been fixed. Please refresh the screen for an update.',
+        );
         $messageCollection = $this->_objectManager->get(
             'Magento\AdminNotification\Model\Resource\System\Message\Collection'
         );
