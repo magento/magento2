d3339c77b8db8145db111703e9c44c0632149c77
Maksym Aposov <maposov@ebay.com>
2015-01-08 12:52:48 +0200

    MAGETWO-32357: Refactor and cover: Magento\CatalogSearch\Model\Resource\Advanced::addIndexableAttributeModifiedFilter

---
+++ dev/tests/unit/testsuite/Magento/CatalogSearch/Model/Resource/AdvancedTest.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * @copyright Copyright (c) 2014 X.commerce, Inc. (http://www.magentocommerce.com)
+ */
+namespace Magento\CatalogSearch\Model\Resource;
+
+use PHPUnit_Framework_TestCase;
+
+class AdvancedTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \Magento\CatalogSearch\Model\Resource\Advanced
+     */
+    private $model;
+
+    /**
+     * setUp method for AdvancedTest
+     */
+    protected function setUp()
+    {
+        $helper = new \Magento\TestFramework\Helper\ObjectManager($this);
+
+        $storeManager = $this->getStoreManager();
+
+        $this->model = $helper->getObject(
+            'Magento\CatalogSearch\Model\Resource\Advanced',
+            [
+                'storeManager' => $storeManager
+            ]
+        );
+    }
+
+    /**
+     * @dataProvider indexableAttributeDataProvider
+     */
+    public function testAddIndexableAttributeModifiedFilter($indexType, $value, $expected)
+    {
+        $selectMock = $this->getMock('Magento\Framework\DB\Select', [], [], '', false);
+        $collectionMock = $this->getMock('Magento\CatalogSearch\Model\Resource\Advanced\Collection', [], [], '', false);
+        $collectionMock->expects($this->once())->method('getSelect')->willReturn($selectMock);
+
+        $attributeMock = $this->getMock('Magento\Catalog\Model\Resource\Eav\Attribute', [], [], '', false);
+        $attributeMock->expects($this->once())->method('getIndexType')->willReturn($indexType);
+        $attributeMock->expects($this->any())->method('getAttributeId')->willReturn(1);
+
+        $this->assertEquals($expected,
+            $this->model->addIndexableAttributeModifiedFilter($collectionMock, $attributeMock, $value));
+    }
+
+    public function indexableAttributeDataProvider()
+    {
+        return array(
+            ['decimal', '', true],
+            ['source', ['from' => 0, 'to' => 0], false],
+            [false, ['from' => 0], true],
+            ['decimal', ['to' => 0], true],
+            ['source', ['from' => 1, 'to' => 1], true]
+        );
+    }
+
+    /**
+     * @return \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected function getStoreManager()
+    {
+        $store = $this->getMockBuilder('Magento\Store\Model\Store')
+            ->setMethods(['getId'])
+            ->disableOriginalConstructor()
+            ->getMock();
+        $store->expects($this->once())
+            ->method('getId')
+            ->willReturn(1);
+
+        $storeManager = $this->getMockBuilder('Magento\Store\Model\StoreManagerInterface')
+            ->setMethods(['getStore'])
+            ->disableOriginalConstructor()
+            ->getMockForAbstractClass();
+        $storeManager->expects($this->once())
+            ->method('getStore')
+            ->willReturn($store);
+
+        return $storeManager;
+    }
+}
