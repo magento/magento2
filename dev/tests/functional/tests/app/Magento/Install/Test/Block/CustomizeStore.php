8b47e7afde8223bef2f3194bbdbdc41af7e05f17
Sergey Ivashchenko <sivashchenko@ebay.com>
2015-01-08 16:56:49 +0200

    MAGETWO-32079: Alternative WebDriver support

--- dev/tests/functional/tests/app/Magento/Install/Test/Block/CustomizeStore.php
+++ dev/tests/functional/tests/app/Magento/Install/Test/Block/CustomizeStore.php
@@ -6,7 +6,9 @@
 namespace Magento\Install\Test\Block;
 
 use Mtf\Block\Form;
+use Mtf\Client\Element\SimpleElement;
 use Mtf\Client\Locator;
+use Mtf\Fixture\FixtureInterface;
 
 /**
  * Customize Your Store block.
@@ -21,6 +23,13 @@ class CustomizeStore extends Form
     protected $next = "[ng-click*='next']";
 
     /**
+     * First field selector
+     *
+     * @var string
+     */
+    protected $firstField = '[ng-model*="language"]';
+
+    /**
      * Click on 'Next' button.
      *
      * @return void
@@ -28,5 +37,18 @@ class CustomizeStore extends Form
     public function clickNext()
     {
         $this->_rootElement->find($this->next, Locator::SELECTOR_CSS)->click();
+    }
+
+    /**
+     * Ensure the form is loaded and fill the root form
+     *
+     * @param FixtureInterface $fixture
+     * @param SimpleElement|null $element
+     * @return $this
+     */
+    public function fill(FixtureInterface $fixture, SimpleElement $element = null)
+    {
+        $this->waitForElementVisible($this->firstField);
+        return parent::fill($fixture, $element);
     }
 }
