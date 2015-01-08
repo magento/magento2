ffe92e27545abbd1b262c0ea01e9b09ddbcf275e
Vinai Kopp <vinai@netzarbeiter.com>
2015-01-08 17:01:00 +0100

    Add integration test for View\Page\Config\Reader\Html

---
+++ dev/tests/integration/testsuite/Magento/Framework/View/Page/Config/Reader/HtmlTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Magento\Framework\View\Page\Config\Reader;
+
+
+class HtmlTest extends \PHPUnit_Framework_TestCase
+{
+    public function testInterpret()
+    {
+        /** @var \Magento\Framework\View\Layout\Reader\Context $readerContext */
+        $readerContext = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create(
+            'Magento\Framework\View\Layout\Reader\Context'
+        );
+        $pageXml = new \Magento\Framework\View\Layout\Element(__DIR__ . '/_files/_layout_update.xml', 0, true);
+        $parentElement = new \Magento\Framework\View\Layout\Element('<page></page>');
+
+        $html = new Html();
+        foreach ($pageXml->xpath('html') as $htmlElement) {
+            $html->interpret($readerContext, $htmlElement, $parentElement);
+        }
+        
+        $structure = $readerContext->getPageConfigStructure();
+        $this->assertEquals(['html' => ['test-name' => 'test-value']], $structure->getElementAttributes());
+    }
+} 
