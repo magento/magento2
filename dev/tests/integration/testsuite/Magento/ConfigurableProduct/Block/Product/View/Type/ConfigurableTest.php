124183a7ec64dc55b8948aa8a67e1e23eb36a0d2
Andrii Kasian <akasian@ebay.com>
2015-01-11 22:51:15 +0200

    MAGETWO-30873: Table catalogsearch_fulltext must setup with ENGINE=InnoDB

--- dev/tests/integration/testsuite/Magento/ConfigurableProduct/Block/Product/View/Type/ConfigurableTest.php
+++ dev/tests/integration/testsuite/Magento/ConfigurableProduct/Block/Product/View/Type/ConfigurableTest.php
@@ -7,6 +7,7 @@ namespace Magento\ConfigurableProduct\Block\Product\View\Type;
 /**
  * Test class for \Magento\ConfigurableProduct\Block\Product\View\Type\Configurable.
  *
+ * @magentoAppIsolation enabled
  * @magentoDataFixture Magento/ConfigurableProduct/_files/product_configurable.php
  */
 class ConfigurableTest extends \PHPUnit_Framework_TestCase
@@ -35,6 +36,9 @@ class ConfigurableTest extends \PHPUnit_Framework_TestCase
         $this->_block->setProduct($this->_product);
     }
 
+    /**
+     * @magentoAppIsolation enabled
+     */
     public function testGetAllowAttributes()
     {
         $attributes = $this->_block->getAllowAttributes();
@@ -45,11 +49,17 @@ class ConfigurableTest extends \PHPUnit_Framework_TestCase
         $this->assertGreaterThanOrEqual(1, $attributes->getSize());
     }
 
+    /**
+     * @magentoAppIsolation enabled
+     */
     public function testHasOptions()
     {
         $this->assertTrue($this->_block->hasOptions());
     }
 
+    /**
+     * @magentoAppIsolation enabled
+     */
     public function testGetAllowProducts()
     {
         $products = $this->_block->getAllowProducts();
@@ -58,7 +68,10 @@ class ConfigurableTest extends \PHPUnit_Framework_TestCase
             $this->assertInstanceOf('Magento\Catalog\Model\Product', $product);
         }
     }
-
+    
+    /**
+     * @magentoAppIsolation enabled
+     */
     public function testGetJsonConfig()
     {
         $config = json_decode($this->_block->getJsonConfig(), true);
