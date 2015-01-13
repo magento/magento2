aafcad78a48f37c5433193b4045c43e45307ea6b
Joan He <joan@x.com>
2015-01-13 09:21:34 -0600

    MAGETWO-32449: Move Emulation Tests class to Magento/Store/Model/App/

--- dev/tests/integration/testsuite/Magento/Store/Model/App/EmulationTest.php
+++ dev/tests/integration/testsuite/Magento/Store/Model/App/EmulationTest.php
@@ -3,7 +3,7 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\Core\Model\App;
+namespace Magento\Store\Model\App;
 
 class EmulationTest extends \PHPUnit_Framework_TestCase
 {
