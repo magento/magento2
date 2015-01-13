3b36481c45499e9e556a189f63443f4314da10c9
Arkadii Chyzhov <achyzhov@ebay.com>
2015-01-13 18:34:36 +0200

    MAGETWO-32352: Move Observer cleanCache and crontab.xml config to Magento_Backend
    - fixed namespace

--- dev/tests/unit/testsuite/Magento/Backend/Model/ObserverTest.php
+++ dev/tests/unit/testsuite/Magento/Backend/Model/ObserverTest.php
@@ -3,7 +3,7 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\Core\Model;
+namespace Magento\Backend\Model;
 
 class ObserverTest extends \PHPUnit_Framework_TestCase
 {
