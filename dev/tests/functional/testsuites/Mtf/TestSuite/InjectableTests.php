c9bd72749cd3bacce335d0c65a230c7cb74da73b
Dmytro Aponasenko <daponasenko@ebay.com>
2015-01-14 13:33:18 +0200

    MTA-1323: Create MTF Acceptance Functional Test Build Plan

--- dev/tests/functional/testsuites/Mtf/TestSuite/InjectableTests.php
+++ dev/tests/functional/testsuites/Mtf/TestSuite/InjectableTests.php
@@ -93,8 +93,8 @@ class InjectableTests extends \PHPUnit_Framework_TestSuite
     {
         if (!isset($this->objectManager)) {
             $objectManagerFactory = new ObjectManagerFactory();
-            $configurationFileName = isset($_ENV['configuration:Mtf/TestSuite/InjectableTests'])
-                ? $_ENV['configuration:Mtf/TestSuite/InjectableTests']
+            $configurationFileName = isset($_ENV['testsuite_rule'])
+                ? $_ENV['testsuite_rule']
                 : 'basic';
             $confFilePath = __DIR__ . '/InjectableTests/' . $configurationFileName . '.xml';
             /** @var \Mtf\TestRunner\Configuration $testRunnerConfiguration */
