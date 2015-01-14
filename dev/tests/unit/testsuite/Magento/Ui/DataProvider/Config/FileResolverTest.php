fd3a80e2b1236cd7a3d53776b21107239fd79a37
Vinai Kopp <vinai@netzarbeiter.com>
2015-01-14 11:57:56 +0100

    Add test to uncover glob pattern not being applied
    The Magento\Ui\DataProvider\Config\FileResolver does not apply the
    filename glob pattern passed to the method while matching files.
    This test triggers the error.

---
+++ dev/tests/unit/testsuite/Magento/Ui/DataProvider/Config/FileResolverTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Magento\Ui\DataProvider\Config;
+
+use Magento\Framework\Filesystem;
+use Magento\Ui\DataProvider\Config\FileResolver;
+use Magento\Framework\Config\FileIteratorFactory;
+
+class FileResolverTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \Magento\Framework\Filesystem\Directory\Read | \PHPUnit_Framework_MockObject_MockObject
+     */
+    private $mockDirectoryRead;
+
+    /**
+     * @var FileResolver
+     */
+    private $fileResolver;
+
+    public function setUp()
+    {
+        $this->mockDirectoryRead = $this->getMockBuilder('Magento\Framework\Filesystem\Directory\Read')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $stubFileIteratorFactory = $this->getMockBuilder('Magento\Framework\Config\FileIteratorFactory')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $stubFilesystem = $this->getMockBuilder('Magento\Framework\Filesystem')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $stubFilesystem->expects($this->any())
+            ->method('getDirectoryRead')
+            ->willReturn($this->mockDirectoryRead);
+        $this->fileResolver = new FileResolver($stubFilesystem, $stubFileIteratorFactory);
+    }
+
+    /**
+     * @test
+     */
+    public function itShouldApplyTheFilenamePattern()
+    {
+        $this->mockDirectoryRead->expects($this->once())
+            ->method('search')
+            ->with($this->matchesRegularExpression('#\*\.xml$#'))
+            ->willReturn([]);
+        
+        $this->fileResolver->get('*.xml', '');
+    }
+}
