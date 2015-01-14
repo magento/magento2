2d08d0ddb134d0138397e10c2539c359c93576b1
Kristof Ringleff <kristof@fooman.co.nz>
2015-01-14 19:59:43 +1300

    remove duplication

--- dev/tests/integration/testsuite/Magento/Framework/Interception/GeneralTest.php
+++ dev/tests/integration/testsuite/Magento/Framework/Interception/GeneralTest.php
@@ -25,6 +25,30 @@ class GeneralTest extends \PHPUnit_Framework_TestCase
 
     public function setUp()
     {
+        $this->setUpInterceptionConfig(
+            [
+                'Magento\Framework\Interception\Fixture\InterceptedInterface' => [
+                    'plugins' => [
+                        'first' => [
+                            'instance'  => 'Magento\Framework\Interception\Fixture\Intercepted\InterfacePlugin',
+                            'sortOrder' => 10,
+                        ],
+                    ],
+                ],
+                'Magento\Framework\Interception\Fixture\Intercepted'          => [
+                    'plugins' => [
+                        'second' => [
+                            'instance'  => 'Magento\Framework\Interception\Fixture\Intercepted\Plugin',
+                            'sortOrder' => 20,
+                        ],
+                    ],
+                ],
+            ]
+        );
+    }
+
+    public function setUpInterceptionConfig($pluginConfig)
+    {
         $config = new \Magento\Framework\Interception\ObjectManager\Config\Developer();
         $factory = new \Magento\Framework\ObjectManager\Factory\Dynamic\Developer($config, null);
 
@@ -34,26 +58,7 @@ class GeneralTest extends \PHPUnit_Framework_TestCase
         )->method(
             'read'
         )->will(
-            $this->returnValue(
-                [
-                    'Magento\Framework\Interception\Fixture\InterceptedInterface' => [
-                        'plugins' => [
-                            'first' => [
-                                'instance' => 'Magento\Framework\Interception\Fixture\Intercepted\InterfacePlugin',
-                                'sortOrder' => 10,
-                            ],
-                        ],
-                    ],
-                    'Magento\Framework\Interception\Fixture\Intercepted' => [
-                        'plugins' => [
-                            'second' => [
-                                'instance' => 'Magento\Framework\Interception\Fixture\Intercepted\Plugin',
-                                'sortOrder' => 20,
-                            ],
-                        ],
-                    ],
-                ]
-            )
+            $this->returnValue($pluginConfig)
         );
 
         $areaList = $this->getMock('Magento\Framework\App\AreaList', [], [], '', false);
@@ -76,14 +81,14 @@ class GeneralTest extends \PHPUnit_Framework_TestCase
             $factory,
             $config,
             [
-                'Magento\Framework\Config\CacheInterface' => $cache,
-                'Magento\Framework\Config\ScopeInterface' => $configScope,
-                'Magento\Framework\Config\ReaderInterface' => $this->_configReader,
-                'Magento\Framework\ObjectManager\RelationsInterface' => $relations,
-                'Magento\Framework\ObjectManager\ConfigInterface' => $config,
+                'Magento\Framework\Config\CacheInterface'                      => $cache,
+                'Magento\Framework\Config\ScopeInterface'                      => $configScope,
+                'Magento\Framework\Config\ReaderInterface'                     => $this->_configReader,
+                'Magento\Framework\ObjectManager\RelationsInterface'           => $relations,
+                'Magento\Framework\ObjectManager\ConfigInterface'              => $config,
                 'Magento\Framework\Interception\ObjectManager\ConfigInterface' => $config,
-                'Magento\Framework\ObjectManager\DefinitionInterface' => $definitions,
-                'Magento\Framework\Interception\DefinitionInterface' => $interceptionDefinitions
+                'Magento\Framework\ObjectManager\DefinitionInterface'          => $definitions,
+                'Magento\Framework\Interception\DefinitionInterface'           => $interceptionDefinitions
             ]
         );
         $factory->setObjectManager($this->_objectManager);
@@ -93,7 +98,8 @@ class GeneralTest extends \PHPUnit_Framework_TestCase
                 'preferences' => [
                     'Magento\Framework\Interception\PluginListInterface' =>
                         'Magento\Framework\Interception\PluginList\PluginList',
-                    'Magento\Framework\Interception\ChainInterface' => 'Magento\Framework\Interception\Chain\Chain',
+                    'Magento\Framework\Interception\ChainInterface'      =>
+                        'Magento\Framework\Interception\Chain\Chain',
                 ],
             ]
         );
--- dev/tests/integration/testsuite/Magento/Framework/Interception/TwoPluginTest.php
+++ dev/tests/integration/testsuite/Magento/Framework/Interception/TwoPluginTest.php
@@ -5,91 +5,26 @@
  */
 namespace Magento\Framework\Interception;
 
-use Magento\Framework\ObjectManager\Config\Config as ObjectManagerConfig;
-
 /**
- * Class GeneralTest
- * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+ * Class TwoPluginTest
  */
-class TwoPluginTest extends \PHPUnit_Framework_TestCase
+class TwoPluginTest extends GeneralTest
 {
-    /**
-     * @var \PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $_configReader;
-
-    /**
-     * @var \Magento\Framework\ObjectManagerInterface
-     */
-    protected $_objectManager;
-
     public function setUp()
     {
-        $config = new \Magento\Framework\Interception\ObjectManager\Config\Developer();
-        $factory = new \Magento\Framework\ObjectManager\Factory\Dynamic\Developer($config, null);
-
-        $this->_configReader = $this->getMock('Magento\Framework\Config\ReaderInterface');
-        $this->_configReader->expects(
-            $this->any()
-        )->method(
-            'read'
-        )->will(
-            $this->returnValue(
-                [
-                    'Magento\Framework\Interception\Fixture\Intercepted' => [
-                        'plugins' => [
-                            'first'     => [
-                                'instance'  => 'Magento\Framework\Interception\Fixture\Intercepted\FirstPlugin',
-                                'sortOrder' => 10,
-                            ], 'second' => [
-                                'instance'  => 'Magento\Framework\Interception\Fixture\Intercepted\Plugin',
-                                'sortOrder' => 20,
-                            ]
-                        ],
-                    ]
+        $this->setUpInterceptionConfig(
+            [
+                'Magento\Framework\Interception\Fixture\Intercepted' => [
+                    'plugins' => [
+                        'first'     => [
+                            'instance'  => 'Magento\Framework\Interception\Fixture\Intercepted\FirstPlugin',
+                            'sortOrder' => 10,
+                        ], 'second' => [
+                            'instance'  => 'Magento\Framework\Interception\Fixture\Intercepted\Plugin',
+                            'sortOrder' => 20,
+                        ]
+                    ],
                 ]
-            )
-        );
-
-        $areaList = $this->getMock('Magento\Framework\App\AreaList', [], [], '', false);
-        $areaList->expects($this->any())->method('getCodes')->will($this->returnValue([]));
-        $configScope = new \Magento\Framework\Config\Scope($areaList, 'global');
-        $cache = $this->getMock('Magento\Framework\Config\CacheInterface');
-        $cache->expects($this->any())->method('load')->will($this->returnValue(false));
-        $definitions = new \Magento\Framework\ObjectManager\Definition\Runtime();
-        $relations = new \Magento\Framework\ObjectManager\Relations\Runtime();
-        $interceptionConfig = new Config\Config(
-            $this->_configReader,
-            $configScope,
-            $cache,
-            $relations,
-            $config,
-            $definitions
-        );
-        $interceptionDefinitions = new Definition\Runtime();
-        $this->_objectManager = new \Magento\Framework\ObjectManager\ObjectManager(
-            $factory,
-            $config,
-            [
-                'Magento\Framework\Config\CacheInterface'                      => $cache,
-                'Magento\Framework\Config\ScopeInterface'                      => $configScope,
-                'Magento\Framework\Config\ReaderInterface'                     => $this->_configReader,
-                'Magento\Framework\ObjectManager\RelationsInterface'           => $relations,
-                'Magento\Framework\ObjectManager\ConfigInterface'              => $config,
-                'Magento\Framework\Interception\ObjectManager\ConfigInterface' => $config,
-                'Magento\Framework\ObjectManager\DefinitionInterface'          => $definitions,
-                'Magento\Framework\Interception\DefinitionInterface'           => $interceptionDefinitions
-            ]
-        );
-        $factory->setObjectManager($this->_objectManager);
-        $config->setInterceptionConfig($interceptionConfig);
-        $config->extend(
-            [
-                'preferences' => [
-                    'Magento\Framework\Interception\PluginListInterface' =>
-                        'Magento\Framework\Interception\PluginList\PluginList',
-                    'Magento\Framework\Interception\ChainInterface'      => 'Magento\Framework\Interception\Chain\Chain',
-                ],
             ]
         );
     }
@@ -117,6 +52,7 @@ class TwoPluginTest extends \PHPUnit_Framework_TestCase
         $subject = $this->_objectManager->create('Magento\Framework\Interception\Fixture\Intercepted');
         $this->assertEquals('<F:V><F:bV/><F:V/>', $subject->V('test'));
     }
+
     public function testPluginBeforeAroundAfterWins()
     {
         $subject = $this->_objectManager->create('Magento\Framework\Interception\Fixture\Intercepted');
