41e570ee14f1477d389eb932ae8ad8ced98dab9d
Yuxing Zheng <yuxzheng@ebay.com>
2015-01-14 11:02:16 -0600

    MAGETWO-32639: Move Core Design classes to Theme Module
     - Moved related tests under Theme module

--- dev/tests/unit/testsuite/Magento/Theme/Model/View/DesignTest.php
+++ dev/tests/unit/testsuite/Magento/Theme/Model/View/DesignTest.php
@@ -3,7 +3,7 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\Core\Model\View;
+namespace Magento\Theme\Model\View;
 
 class DesignTest extends \PHPUnit_Framework_TestCase
 {
