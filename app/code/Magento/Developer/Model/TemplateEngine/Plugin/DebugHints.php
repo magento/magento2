f1f6ecf6f07b3a386884a22170eeba45e73f7c54
Bryant Luk <bluk@ebay.com>
2015-01-15 14:38:13 -0600

    MAGETWO-32642: Move Magento\Core\Model\TemplateEngine\Plugin\DebugHints.php to the Magento_Developer module
    - Move code and configuration

--- app/code/Magento/Developer/Model/TemplateEngine/Plugin/DebugHints.php
+++ app/code/Magento/Developer/Model/TemplateEngine/Plugin/DebugHints.php
@@ -5,7 +5,7 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\Core\Model\TemplateEngine\Plugin;
+namespace Magento\Developer\Model\TemplateEngine\Plugin;
 
 class DebugHints
 {
