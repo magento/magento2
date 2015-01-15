1baff77d3708ce0cd886a70fe07cf338e0ef4f3c
Cari Spruiell <cspruiell@ebay.com>
2015-01-15 16:32:31 -0600

    MAGETWO-32636: Move Core\Model\Theme and Core\Helper classes to Theme Module
    - added integration test files to fix Bamboo failures

---
+++ dev/tests/integration/testsuite/Magento/Theme/Model/_files/design/frontend/Vendor/custom_theme/Fixture_Module/web/fixture_script.js
@@ -0,0 +1,5 @@
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+/* modular fixture view file located inside the nested view of the custom theme */
