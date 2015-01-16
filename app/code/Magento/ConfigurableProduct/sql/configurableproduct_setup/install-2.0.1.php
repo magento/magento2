b61e1025124948ca997f6cf68fb3f9070306e865
Illia Grybkov <igrybkov@ebay.com>
2015-01-16 18:45:22 +0200

    MAGETWO-32341: [GITHUB] Configurable product attribute scope #866
    - Fix PhpDoc comment

--- app/code/Magento/ConfigurableProduct/sql/configurableproduct_setup/install-2.0.1.php
+++ app/code/Magento/ConfigurableProduct/sql/configurableproduct_setup/install-2.0.1.php
@@ -4,9 +4,8 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-
-$installer = $this;
 /* @var $installer \Magento\Catalog\Model\Resource\Setup */
+$installer = $this;
 
 $installer->startSetup();
 
