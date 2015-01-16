1de6a4227e5ddc8d248ef35e67978c7c595a7f40
Kostiantyn Poida <kpoida@ebay.com>
2015-01-16 14:30:42 +0200

    MAGETWO-31363: Unit and Integration tests coverage

--- dev/tests/unit/testsuite/Magento/OfflineShipping/Model/Quote/FreeshippingTest.php
+++ dev/tests/unit/testsuite/Magento/OfflineShipping/Model/Quote/FreeshippingTest.php
@@ -36,7 +36,8 @@ class FreeshippingTest extends \PHPUnit_Framework_TestCase
             ->setMethods([
                 'getQuote',
                 'getAllItems',
-                'getFreeShipping'
+                'getFreeShipping',
+                'setFreeShipping'
             ])
             ->getMock();
 
@@ -44,9 +45,17 @@ class FreeshippingTest extends \PHPUnit_Framework_TestCase
             ->disableOriginalConstructor()
             ->getMock();
 
+        $storeMock = $this->getMockBuilder('Magento\Store\Model\Store')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->storeManagerMock->expects($this->once())
+            ->method('getStore')
+            ->willReturn($storeMock);
+
         $this->calculatorMock = $this->getMockBuilder('Magento\OfflineShipping\Model\SalesRule\Calculator')
             ->disableOriginalConstructor()
-            //->setMethods(['init'])
+            ->setMethods(['init', 'processFreeShipping'])
             ->getMock();
 
         $this->model = $helper->getObject('Magento\OfflineShipping\Model\Quote\Freeshipping', [
@@ -75,59 +84,95 @@ class FreeshippingTest extends \PHPUnit_Framework_TestCase
 
     /**
      * @dataProvider scenariosDataProvider
+     * @param $isNoDiscount
      */
-    public function testCollectWithAddressItems(
-        $addressItemMockNoDiscountValue,
-        $addressMockGetFreeShippingExpects
-    ) {
+    public function testCollectWithAddressItems($isNoDiscount)
+    {
         $quoteMock = $this->getMockBuilder('Magento\Sales\Model\Quote')
             ->disableOriginalConstructor()
             ->getMock();
 
-        $this->addressMock->expects($this->any())
+        $this->addressMock->expects($this->once())
             ->method('getQuote')
             ->willReturn($quoteMock);
 
         $addressItemMock = $this->getMockBuilder('Magento\Sales\Model\Quote\Address\Item')
             ->disableOriginalConstructor()
-            ->setMethods(['getNoDiscount', 'getFreeShipping'])
+            ->setMethods([
+                'getNoDiscount',
+                'setFreeShipping',
+                'getParentItemId',
+                'getFreeShipping',
+                'getHasChildren',
+                'isChildrenCalculated',
+                'getChildren'
+            ])
             ->getMock();
-
-        $addressItemMock->expects($this->once())
-            ->method('getNoDiscount')
-            ->willReturn($addressItemMockNoDiscountValue);
-
-        $addressItemMock->expects($this->any())
-            ->method('getFreeShipping')
-            ->willReturn(true);
-
-        $this->addressMock->expects($addressMockGetFreeShippingExpects)
-            ->method('getFreeShipping')
-            ->willReturn(false);
-
-        $storeMock = $this->getMockBuilder('Magento\Store\Model\Store')
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        $this->storeManagerMock->expects($this->once())
-            ->method('getStore')
-            ->willReturn($storeMock);
-
-        $this->calculatorMock->expects($this->once())
-            ->method('init');
 
         $this->addressMock->expects($this->once())
             ->method('getAllItems')
             ->willReturn([$addressItemMock]);
 
-        $this->model->collect($this->addressMock);
+        $this->calculatorMock->expects($this->once())
+            ->method('init');
+
+        $addressItemMock->expects($this->once())
+            ->method('getNoDiscount')
+            ->willReturn($isNoDiscount);
+
+        $addressItemMock->expects($isNoDiscount ? $this->once() : $this->never())
+            ->method('setFreeShipping');
+
+        $addressItemMock->expects($isNoDiscount ? $this->never() : $this->once())
+            ->method('getParentItemId')
+            ->willReturn(false);
+
+        $this->calculatorMock->expects($isNoDiscount ? $this->never() : $this->exactly(2))
+            ->method('processFreeShipping');
+
+        $addressItemMock->expects($isNoDiscount ? $this->never() : $this->once())
+            ->method('getFreeShipping')
+            ->willReturn(true);
+
+        $addressItemMock->expects($isNoDiscount ? $this->never() : $this->once())
+            ->method('getHasChildren')
+            ->willReturn(true);
+
+        $addressItemMock->expects($isNoDiscount ? $this->never() : $this->once())
+            ->method('isChildrenCalculated')
+            ->willReturn(true);
+
+        $childMock = $this->getMockBuilder('Magento\Sales\Model\Quote\Item\AbstractItem')
+            ->disableOriginalConstructor()
+            ->setMethods(['setFreeShipping', 'getQuote', 'getAddress', 'getOptionByCode'])
+            ->getMock();
+
+        $addressItemMock->expects($isNoDiscount ? $this->never() : $this->once())
+            ->method('getChildren')
+            ->willReturn([$childMock]);
+
+        $childMock->expects($isNoDiscount ? $this->never() : $this->once())
+            ->method('setFreeShipping');
+
+        $this->addressMock->expects($isNoDiscount ? $this->never() : $this->once())
+            ->method('getFreeShipping')
+            ->willReturn(false);
+
+        $this->addressMock->expects($isNoDiscount ? $this->once() : $this->exactly(2))
+            ->method('setFreeShipping');
+
+        $this->assertSame($this->model->collect($this->addressMock), $this->model);
     }
 
     public function scenariosDataProvider()
     {
         return [
-            [true, $this->never()],
-            [false, $this->once()]
+            [
+                true,  // there is no a discount
+            ],
+            [
+                false, // there is a discount
+            ]
         ];
     }
 }
