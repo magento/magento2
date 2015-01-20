e8fd48cd66cd95b709ed6a4fe11e5956bac1b275
Maxim Shikula <mshikula@ebay.com>
2015-01-20 12:21:52 +0200

    MAGETWO-32564: Refactor Adminhtml/order/create directory

--- app/code/Magento/Sales/Controller/Adminhtml/Order/Create/ConfigureProductToAdd.php
+++ app/code/Magento/Sales/Controller/Adminhtml/Order/Create/ConfigureProductToAdd.php
@@ -1,11 +1,9 @@
 <?php
 /**
- *
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
 namespace Magento\Sales\Controller\Adminhtml\Order\Create;
-
 
 class ConfigureProductToAdd extends \Magento\Sales\Controller\Adminhtml\Order\Create
 {
@@ -27,7 +25,8 @@ class ConfigureProductToAdd extends \Magento\Sales\Controller\Adminhtml\Order\Cr
         $configureResult->setCurrentCustomerId($sessionQuote->getCustomerId());
 
         // Render page
-        return $this->_objectManager->get('Magento\Catalog\Helper\Product\Composite')
-            ->renderConfigureResult($configureResult);
+        /** @var \Magento\Catalog\Helper\Product\Composite $helper */
+        $helper = $this->_objectManager->get('Magento\Catalog\Helper\Product\Composite');
+        return $helper->renderConfigureResult($configureResult);
     }
 }
