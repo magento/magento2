bc0e1c9361713fbc64ec060139b971216c6ea364
Joan He <joan@x.com>
2015-01-21 15:21:22 -0600

    MAGETWO-32861: added unit tests for classes under lib/internal/Magento/Framework/App/Http

--- dev/tests/unit/testsuite/Magento/Framework/App/Http/ContextTest.php
+++ dev/tests/unit/testsuite/Magento/Framework/App/Http/ContextTest.php
@@ -26,28 +26,28 @@ class ContextTest extends \PHPUnit_Framework_TestCase
 
     public function testGetValue()
     {
-        $this->assertNull($this->object->getValue('value'));
+        $this->assertNull($this->object->getValue('key'));
     }
 
     public function testSetGetValue()
     {
-        $this->object->setValue('value', 'value', 'default');
-        $this->assertEquals('value', $this->object->getValue('value'));
+        $this->object->setValue('key', 'value', 'default');
+        $this->assertEquals('value', $this->object->getValue('key'));
     }
 
     public function testSetUnsetGetValue()
     {
-        $this->object->setValue('value', 'value', 'default');
-        $this->object->unsValue('value');
-        $this->assertEquals('default', $this->object->getValue('value'));
+        $this->object->setValue('key', 'value', 'default');
+        $this->object->unsValue('key');
+        $this->assertEquals('default', $this->object->getValue('key'));
     }
 
     public function testGetData()
     {
-        $this->object->setValue('value1', 'value1', 'default1');
-        $this->object->setValue('value2', 'value2', 'default2');
-        $this->object->setValue('value3', 'value3', 'value3');
-        $this->object->unsValue('value1');
-        $this->assertEquals(['value2' => 'value2'], $this->object->getData());
+        $this->object->setValue('key1', 'value1', 'default1');
+        $this->object->setValue('key2', 'value2', 'default2');
+        $this->object->setValue('key3', 'value3', 'value3');
+        $this->object->unsValue('key1');
+        $this->assertEquals(['key2' => 'value2'], $this->object->getData());
     }
 }
