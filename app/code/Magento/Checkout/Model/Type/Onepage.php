10400b8836bca35f58bbf4366af966032da75647
Dmytro Kvashnin <dkvashnin@ebay.com>
2015-01-21 05:31:58 -0800

    MAGETWO-31578: Implement tool for adding annotations to existing code
    - phpcs annotations added

--- app/code/Magento/Checkout/Model/Type/Onepage.php
+++ app/code/Magento/Checkout/Model/Type/Onepage.php
@@ -4,6 +4,8 @@
  * See COPYING.txt for license details.
  */
 
+// @codingStandardsIgnoreFile
+
 /**
  * One page checkout processing model
  */
--- app/code/Magento/Sales/Block/Adminhtml/Order/Create/Form/Account.php
+++ app/code/Magento/Sales/Block/Adminhtml/Order/Create/Form/Account.php
@@ -3,6 +3,9 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
+
+// @codingStandardsIgnoreFile
+
 namespace Magento\Sales\Block\Adminhtml\Order\Create\Form;
 
 use Magento\Framework\Api\ExtensibleDataObjectConverter;
