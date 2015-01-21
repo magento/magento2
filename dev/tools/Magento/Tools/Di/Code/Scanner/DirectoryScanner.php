6a5c9420a89f13f10928be618fba4406f12f9bd0
Flyingmana <flyingmana@googlemail.com>
2015-01-21 10:47:00 +0100

    allow exclude patterns for DI Directory Scanner

--- dev/tools/Magento/Tools/Di/Code/Scanner/DirectoryScanner.php
+++ dev/tools/Magento/Tools/Di/Code/Scanner/DirectoryScanner.php
@@ -12,9 +12,10 @@ class DirectoryScanner
      *
      * @param string $dir
      * @param array $patterns
+     * @param string[] $excludePatterns
      * @return array
      */
-    public function scan($dir, array $patterns = [])
+    public function scan($dir, array $patterns = [], array $excludePatterns = [])
     {
         $recursiveIterator = new \RecursiveIteratorIterator(
             new \RecursiveDirectoryIterator($dir, \FilesystemIterator::FOLLOW_SYMLINKS)
@@ -27,6 +28,13 @@ class DirectoryScanner
             }
 
             $filePath = str_replace('\\', '/', $file->getRealPath());
+            if (!empty($excludePatterns)) {
+                foreach ($excludePatterns as $excludePattern) {
+                    if (preg_match($excludePattern, $filePath)) {
+                        continue 2;
+                    }
+                }
+            }
             foreach ($patterns as $type => $pattern) {
                 if (preg_match($pattern, $filePath)) {
                     $output[$type][] = $filePath;
