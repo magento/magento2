4556bdcf8f5dd23934f29ab41312ada816c6d9c7
Yuri Kovsher <ikovsher@ebay.com>
2015-01-21 13:06:10 +0200

    MAGETWO-32559: Refactor Adminhtml/order directory

--- app/code/Magento/Sales/Controller/Adminhtml/Order/Shipments.php
+++ app/code/Magento/Sales/Controller/Adminhtml/Order/Shipments.php
@@ -1,23 +1,60 @@
 <?php
 /**
- *
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
 namespace Magento\Sales\Controller\Adminhtml\Order;
 
-
 class Shipments extends \Magento\Sales\Controller\Adminhtml\Order
 {
     /**
+     * @var \Magento\Framework\View\Result\LayoutFactory
+     */
+    protected $resultLayoutFactory;
+
+    /**
+     * @param \Magento\Backend\App\Action\Context $context
+     * @param \Magento\Framework\Registry $coreRegistry
+     * @param \Magento\Framework\App\Response\Http\FileFactory $fileFactory
+     * @param \Magento\Framework\Translate\InlineInterface $translateInline
+     * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
+     * @param \Magento\Backend\Model\View\Result\RedirectFactory $resultRedirectFactory
+     * @param \Magento\Framework\View\Result\LayoutFactory $resultLayoutFactory
+     */
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Magento\Framework\Registry $coreRegistry,
+        \Magento\Framework\App\Response\Http\FileFactory $fileFactory,
+        \Magento\Framework\Translate\InlineInterface $translateInline,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory,
+        \Magento\Backend\Model\View\Result\RedirectFactory $resultRedirectFactory,
+        \Magento\Framework\View\Result\LayoutFactory $resultLayoutFactory
+    ) {
+        $this->resultLayoutFactory = $resultLayoutFactory;
+        parent::__construct(
+            $context,
+            $coreRegistry,
+            $fileFactory,
+            $translateInline,
+            $resultPageFactory,
+            $resultRedirectFactory
+        );
+    }
+
+    /**
      * Generate shipments grid for ajax request
      *
-     * @return void
+     * @return \Magento\Framework\View\Result\Layout|\Magento\Backend\Model\View\Result\Redirect
      */
     public function execute()
     {
-        $this->_initOrder();
-        $this->_view->loadLayout(false);
-        $this->_view->renderLayout();
+        $order = $this->_initOrder();
+        if ($order) {
+            $resultLayout = $this->resultLayoutFactory->create();
+            return $resultLayout;
+        }
+        $resultRedirect = $this->resultRedirectFactory->create();
+        $resultRedirect->setPath('sales/*/');
+        return $resultRedirect;
     }
 }
