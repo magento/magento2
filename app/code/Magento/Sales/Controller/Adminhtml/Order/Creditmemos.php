e0509a676f71d32a4ce7984bece1e603b00298b3
Yuri Kovsher <ikovsher@ebay.com>
2015-01-22 15:46:23 +0200

    MAGETWO-32872: Create pull request for S45 Controller Refactoring

--- app/code/Magento/Sales/Controller/Adminhtml/Order/Creditmemos.php
+++ app/code/Magento/Sales/Controller/Adminhtml/Order/Creditmemos.php
@@ -10,47 +10,6 @@ use Magento\Backend\App\Action;
 class Creditmemos extends \Magento\Sales\Controller\Adminhtml\Order
 {
     /**
-     * @var \Magento\Framework\View\Result\LayoutFactory
-     */
-    protected $resultLayoutFactory;
-
-    /**
-     * @param Action\Context $context
-     * @param \Magento\Framework\Registry $coreRegistry
-     * @param \Magento\Framework\App\Response\Http\FileFactory $fileFactory
-     * @param \Magento\Framework\Translate\InlineInterface $translateInline
-     * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
-     * @param \Magento\Backend\Model\View\Result\RedirectFactory $resultRedirectFactory
-     * @param \Magento\Framework\View\Result\LayoutFactory $resultLayoutFactory
-     * @param \Magento\Framework\Controller\Result\JSONFactory $resultJsonFactory
-     * @param \Magento\Framework\Controller\Result\RawFactory $resultRawFactory
-     */
-    public function __construct(
-        Action\Context $context,
-        \Magento\Framework\Registry $coreRegistry,
-        \Magento\Framework\App\Response\Http\FileFactory $fileFactory,
-        \Magento\Framework\Translate\InlineInterface $translateInline,
-        \Magento\Framework\View\Result\PageFactory $resultPageFactory,
-        \Magento\Backend\Model\View\Result\RedirectFactory $resultRedirectFactory,
-        \Magento\Framework\View\Result\LayoutFactory $resultLayoutFactory,
-        \Magento\Framework\Controller\Result\JSONFactory $resultJsonFactory,
-        \Magento\Framework\Controller\Result\RawFactory $resultRawFactory
-    ) {
-        $this->resultLayoutFactory = $resultLayoutFactory;
-        parent::__construct(
-            $context,
-            $coreRegistry,
-            $fileFactory,
-            $translateInline,
-            $resultPageFactory,
-            $resultRedirectFactory,
-            $resultJsonFactory,
-            $resultLayoutFactory,
-            $resultRawFactory
-        );
-    }
-
-    /**
      * Generate credit memos grid for ajax request
      *
      * @return \Magento\Framework\View\Result\Layout
