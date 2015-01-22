4375051333a4aa6a17a28254efdb3fb271f1d14a
Paul Lewis <plewis@ebay.com>
2015-01-22 16:28:10 -0600

    MAGETWO-32090: Setup "Mvc" Models Unit Test Coverage
     - Refactor InitParamListenerTest and RoutListenerTest to create reusable functions
     - Other formatting changes

--- dev/tests/unit/testsuite/Magento/Setup/Mvc/Console/RouteListenerTest.php
+++ dev/tests/unit/testsuite/Magento/Setup/Mvc/Console/RouteListenerTest.php
@@ -14,112 +14,91 @@ use Zend\Mvc\MvcEvent;
 class RouteListenerTest extends \PHPUnit_Framework_TestCase
 {
 
+    /** @var RouteListener */
+    private $routeListener;
+
+    /** @var PHPUnit_Framework_MockObject_MockObject */
+    private $request;
+
+    /** @var PHPUnit_Framework_MockObject_MockObject */
+    private $router;
+
+    public function setUp()
+    {
+        $this->routeListener = new RouteListener();
+        $this->request = $this->getMockBuilder('Zend\Console\Request')->disableOriginalConstructor()->getMock();
+        $this->router = $this->getMockBuilder('Zend\Mvc\Router\RouteInterface')->disableOriginalConstructor()->getMock();
+    }
+
     public function testOnRouteHttpRequest()
     {
-        $routeListener = new RouteListener();
         /** @var \Zend\Mvc\MvcEvent|PHPUnit_Framework_MockObject_MockObject $mvcEvent */
         $mvcEvent = $this->getMockBuilder('Zend\Mvc\MvcEvent')->disableOriginalConstructor()->getMock();
         $httpRequest = $this->getMockBuilder('Zend\Http\Request')->getMock();
         $mvcEvent->expects($this->any())->method('getRequest')->willReturn($httpRequest);
 
         // Sending an HttpRequest to console RouteListener should return null
-        $this->assertNull($routeListener->onRoute($mvcEvent));
+        $this->assertNull($this->routeListener->onRoute($mvcEvent));
     }
 
     public function testOnRouteMatch()
     {
-        $routeListener = new RouteListener();
-        /** @var \Zend\Mvc\MvcEvent|PHPUnit_Framework_MockObject_MockObject $mvcEvent */
-        $mvcEvent = $this->getMockBuilder('Zend\Mvc\MvcEvent')->disableOriginalConstructor()->getMock();
-        $request = $this->getMockBuilder('Zend\Console\Request')->disableOriginalConstructor()->getMock();
-        $mvcEvent->expects($this->any())->method('getRequest')->willReturn($request);
+        $mvcEvent = $this->prepareEvent();
 
-        $router = $this->getMockBuilder('Zend\Mvc\Router\RouteInterface')->disableOriginalConstructor()->getMock();
         $match = $this->getMockBuilder('Zend\Mvc\Router\RouteMatch')->disableOriginalConstructor()->getMock();
-        $router->expects($this->any())->method('match')->willReturn($match);
-        $mvcEvent->expects($this->any())->method('getRouter')->willReturn($router);
+        $this->router->expects($this->any())->method('match')->willReturn($match);
+        $mvcEvent->expects($this->any())->method('getRouter')->willReturn($this->router);
 
         // There is a RouteMatch, so RouteListener will return null to trigger default listeners
-        $this->assertNull($routeListener->onRoute($mvcEvent));
+        $this->assertNull($this->routeListener->onRoute($mvcEvent));
     }
 
     public function testOnRouteError()
     {
-        $routeListener = new RouteListener();
-        /** @var \Zend\Mvc\MvcEvent|PHPUnit_Framework_MockObject_MockObject $mvcEvent */
-        $mvcEvent = $this->getMockBuilder('Zend\Mvc\MvcEvent')->disableOriginalConstructor()->getMock();
-        $request = $this->getMockBuilder('Zend\Console\Request')->disableOriginalConstructor()->getMock();
-        $mvcEvent->expects($this->any())->method('getRequest')->willReturn($request);
+        $mvcEvent = $this->prepareEvent();
 
-        $router = $this->getMockBuilder('Zend\Mvc\Router\RouteInterface')->disableOriginalConstructor()->getMock();
-        $router->expects($this->any())->method('match')->willReturn(null);
-        $mvcEvent->expects($this->any())->method('getRouter')->willReturn($router);
+        $this->router->expects($this->any())->method('match')->willReturn(null);
+        $mvcEvent->expects($this->any())->method('getRouter')->willReturn($this->router);
 
-        $request->expects($this->any())->method('getContent')->willReturn([]);
-
-        $application = $this->getMockBuilder('Zend\Mvc\ApplicationInterface')->getMock();
-        $serviceManager = $this->getMockBuilder('Zend\ServiceManager\ServiceLocatorInterface')->getMock();
-        $console = $this->getMockBuilder('Zend\Console\Adapter\AdapterInterface')->getMock();
-
-        $configArray = ['console'=>['router'=>['routes'=>['testAction'=>'test']]]];
-        $serviceManager->method('get')->will($this->returnValueMap([['Config', $configArray], ['console', $console]]));
-
-        $application->expects($this->any())->method('getServiceManager')->willReturn($serviceManager);
-        $mvcEvent->expects($this->any())->method('getApplication')->willReturn($application);
+        $this->request->expects($this->any())->method('getContent')->willReturn([]);
+        $this->prepareOnRoute($mvcEvent, ['console'=>['router'=>['routes'=>['testAction'=>'test']]]]);
 
         // Verify the error is set
         $mvcEvent->expects($this->once())->method('setError');
 
         // Should always return null
-        $this->assertNull($routeListener->onRoute($mvcEvent));
+        $this->assertNull($this->routeListener->onRoute($mvcEvent));
     }
 
     public function testOnRouteNoError()
     {
-        $routeListener = new RouteListener();
-        /** @var \Zend\Mvc\MvcEvent|PHPUnit_Framework_MockObject_MockObject $mvcEvent */
-        $mvcEvent = $this->getMockBuilder('Zend\Mvc\MvcEvent')->disableOriginalConstructor()->getMock();
-        $request = $this->getMockBuilder('Zend\Console\Request')->disableOriginalConstructor()->getMock();
-        $mvcEvent->expects($this->any())->method('getRequest')->willReturn($request);
+        $mvcEvent = $this->prepareEvent();
 
-        $router = $this->getMockBuilder('Zend\Mvc\Router\RouteInterface')->disableOriginalConstructor()->getMock();
         $routeMatch = $this->getMockBuilder('Zend\Mvc\Router\RouteMatch')->disableOriginalConstructor()->getMock();
-        $router->expects($this->any())->method('match')->willReturn($routeMatch);
-        $mvcEvent->expects($this->any())->method('getRouter')->willReturn($router);
+        $this->router->expects($this->any())->method('match')->willReturn($routeMatch);
+        $mvcEvent->expects($this->any())->method('getRouter')->willReturn($this->router);
 
         // Verify the error is not set
         $mvcEvent->expects($this->never())->method('setError');
 
         // Should always return null
-        $this->assertNull($routeListener->onRoute($mvcEvent));
+        $this->assertNull($this->routeListener->onRoute($mvcEvent));
     }
 
 
     public function testOnRouteValidationMessage()
     {
-        $routeListener = new RouteListener();
-        /** @var \Zend\Mvc\MvcEvent|PHPUnit_Framework_MockObject_MockObject $mvcEvent */
-        $mvcEvent = $this->getMockBuilder('Zend\Mvc\MvcEvent')->disableOriginalConstructor()->getMock();
-        $request = $this->getMockBuilder('Zend\Console\Request')->disableOriginalConstructor()->getMock();
-        $mvcEvent->expects($this->any())->method('getRequest')->willReturn($request);
+        $mvcEvent = $this->prepareEvent();
 
-        $router = $this->getMockBuilder('Zend\Mvc\Router\RouteInterface')->disableOriginalConstructor()->getMock();
-        //$routeMatch = $this->getMockBuilder('Zend\Mvc\Router\RouteMatch')->disableOriginalConstructor()->getMock();
-        $router->expects($this->any())->method('match')->willReturn(null);
-        $mvcEvent->expects($this->any())->method('getRouter')->willReturn($router);
+        $this->router->expects($this->any())->method('match')->willReturn(null);
+        $mvcEvent->expects($this->any())->method('getRouter')->willReturn($this->router);
 
-        $request->expects($this->any())->method('getContent')->willReturn(['install']);
+        $this->request->expects($this->any())->method('getContent')->willReturn(['install']);
 
-        $application = $this->getMockBuilder('Zend\Mvc\ApplicationInterface')->getMock();
-        $serviceManager = $this->getMockBuilder('Zend\ServiceManager\ServiceLocatorInterface')->getMock();
-        $console = $this->getMockBuilder('Zend\Console\Adapter\AdapterInterface')->getMock();
-
-        //        $configArray = ['console'=>['router'=>['routes'=>['testAction'=>['test']]]]];
-        $configArray = ['console'=>['router'=>['routes'=>['install'=>['options' => ['route' => 'testRoute']]]]]];
-        $serviceManager->method('get')->will($this->returnValueMap([['Config', $configArray], ['console', $console]]));
-
-        $application->expects($this->any())->method('getServiceManager')->willReturn($serviceManager);
-        $mvcEvent->expects($this->any())->method('getApplication')->willReturn($application);
+        $this->prepareOnRoute(
+            $mvcEvent,
+            ['console'=>['router'=>['routes'=>['install'=>['options' => ['route' => 'testRoute']]]]]]
+        );
 
         // Verify the error is set
         $mvcEvent->expects($this->once())->method('setError');
@@ -128,22 +107,53 @@ class RouteListenerTest extends \PHPUnit_Framework_TestCase
             ->with($this->isInstanceOf('Zend\View\Model\ConsoleModel'));
 
         // Should always return null
-        $this->assertNull($routeListener->onRoute($mvcEvent));
+        $this->assertNull($this->routeListener->onRoute($mvcEvent));
     }
 
 
     public function testAttach()
     {
-        $routeListener = new RouteListener();
-        /** @var 'Zend\EventManager\EventManagerInterface|PHPUnit_Framework_MockObject_MockObject $eventManager */
+        /** @var \Zend\EventManager\EventManagerInterface|PHPUnit_Framework_MockObject_MockObject $eventManager */
         $eventManager = $this->getMockBuilder('Zend\EventManager\EventManagerInterface')->getMock();
         $eventManager->expects($this->once())
             ->method('attach')
             ->with(
                 $this->equalTo(MvcEvent::EVENT_ROUTE),
-                $this->contains($routeListener),
+                $this->contains($this->routeListener),
                 10
             );
-        $routeListener->attach($eventManager);
+        $this->routeListener->attach($eventManager);
+    }
+
+    /**
+     * Create a mock MVC event with a console mock console request.
+     *
+     * @return PHPUnit_Framework_MockObject_MockObject|MvcEvent
+     */
+    private function prepareEvent()
+    {
+        $mvcEvent = $this->getMockBuilder('Zend\Mvc\MvcEvent')->disableOriginalConstructor()->getMock();
+        $this->request = $this->getMockBuilder('Zend\Console\Request')->disableOriginalConstructor()->getMock();
+        $mvcEvent->expects($this->any())->method('getRequest')->willReturn($this->request);
+        return $mvcEvent;
+    }
+
+    /**
+     * Add a mock application, service manager and console adapter.
+     *
+     * @param PHPUnit_Framework_MockObject_MockObject $mvcEvent
+     * @param $configArray
+     */
+    private function prepareOnRoute($mvcEvent, $configArray)
+    {
+        $application = $this->getMockBuilder('Zend\Mvc\ApplicationInterface')->getMock();
+        $serviceManager = $this->getMockBuilder('Zend\ServiceManager\ServiceLocatorInterface')->getMock();
+        $console = $this->getMockBuilder('Zend\Console\Adapter\AdapterInterface')->getMock();
+
+        $serviceManager->method('get')->will($this->returnValueMap([['Config', $configArray], ['console', $console]]));
+
+        $application->expects($this->any())->method('getServiceManager')->willReturn($serviceManager);
+        $mvcEvent->expects($this->any())->method('getApplication')->willReturn($application);
+
     }
 }
