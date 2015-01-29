9eb22ff71673ca2c6dbdf66008f58bae47c691ec
Olexandr Lysenko <olysenko@ebay.com>
2015-01-29 15:36:23 +0200

    MAGETWO-33221: Date created column is not populated in invoices and credit memo grid

--- app/code/Magento/Sales/sql/sales_setup/upgrade-2.0.0-2.0.1.php
+++ app/code/Magento/Sales/sql/sales_setup/upgrade-2.0.0-2.0.1.php
@@ -4,24 +4,39 @@
  * See COPYING.txt for license details.
  */
 
+use Magento\Framework\DB\Ddl\Table;
+
 /**
- * update columns created_at and updated_at in sales tables
+ * update columns created_at and updated_at in sales entities tables
  */
 
 $tables = [
-    'sales_creditmemo', 'sales_creditmemo_comment', 'sales_invoice', 'sales_invoice_comment', 'sales_order',
-    'sales_order_item', 'sales_order_status_history', 'sales_payment_transaction', 'sales_shipment',
-    'sales_shipment_comment', 'sales_shipment_track'
+    'sales_creditmemo',
+    'sales_creditmemo_comment',
+    'sales_invoice',
+    'sales_invoice_comment',
+    'sales_order',
+    'sales_order_item',
+    'sales_order_status_history',
+    'sales_payment_transaction',
+    'sales_shipment',
+    'sales_shipment_comment',
+    'sales_shipment_track'
 ];
 /** @var \Magento\Framework\DB\Adapter\AdapterInterface $connection */
 $connection = $this->getConnection();
 foreach ($tables as $table) {
     $columns = $connection->describeTable($table);
-    $createdAt = $columns['created_at'];
-    $createdAt['default'] = 'CURRENT_TIMESTAMP';
-    $updatedAt = $columns['updated_at'];
-    $updatedAt['default'] = 'CURRENT_TIMESTAMP';
-    $connection->modifyColumn($table, 'created_at', $createdAt);
-    $connection->modifyColumn($table, 'updated_at', $updatedAt);
+    if (isset($columns['created_at'])) {
+        $createdAt = $columns['created_at'];
+        $createdAt['DEFAULT'] = Table::TIMESTAMP_INIT;
+        $createdAt['TYPE'] = Table::TYPE_TIMESTAMP;
+        $connection->modifyColumn($table, 'created_at', $createdAt);
+    }
+    if (isset($columns['updated_at'])) {
+        $updatedAt = $columns['updated_at'];
+        $updatedAt['DEFAULT'] = Table::TIMESTAMP_UPDATE;
+        $updatedAt['TYPE'] = Table::TYPE_TIMESTAMP;
+        $connection->modifyColumn($table, 'updated_at', $updatedAt);
+    }
 }
-
