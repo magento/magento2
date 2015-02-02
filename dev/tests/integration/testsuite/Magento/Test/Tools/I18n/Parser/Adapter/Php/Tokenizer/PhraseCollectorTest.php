d01c7109beee261f6cdb8f209401f610b747be8c
Yuri Kovsher <ikovsher@ebay.com>
2015-02-02 13:02:30 +0200

    MAGETWO-33062: Update dictionary search tool to support \Magento\Framework\Phrase

--- dev/tests/integration/testsuite/Magento/Test/Tools/I18n/Parser/Adapter/Php/Tokenizer/PhraseCollectorTest.php
+++ dev/tests/integration/testsuite/Magento/Test/Tools/I18n/Parser/Adapter/Php/Tokenizer/PhraseCollectorTest.php
@@ -3,7 +3,7 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\Tools\I18n\Parser\Adapter\Php\Tokenizer;
+namespace Magento\Test\Tools\I18n\Parser\Adapter\Php\Tokenizer;
 
 use Magento\Framework\ObjectManager;
 use Magento\TestFramework\Helper\Bootstrap;
