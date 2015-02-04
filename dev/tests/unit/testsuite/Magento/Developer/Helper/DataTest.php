723d7f29b725add45d321e5308bbcab9d6bdbc64
Joan He <joan@x.com>
2015-02-04 14:43:20 -0600

    MAGETWO-32374: moved isDevAllowed() method in \Magento\Core\Helper\Data

--- dev/tests/unit/testsuite/Magento/Developer/Helper/DataTest.php
+++ dev/tests/unit/testsuite/Magento/Developer/Helper/DataTest.php
@@ -3,7 +3,9 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\Framework\App\Config\Helper;
+namespace Magento\Developer\Helper;
+
+use Magento\Framework\App\Config\Helper\Data;
 
 class DataTest extends \PHPUnit_Framework_TestCase
 {
