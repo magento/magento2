5f0c95032039ab16bd6d074b3ed47f7d12c8e2f3
Egor Shitikov <yshytikov@ebay.com>
2015-02-05 15:53:00 +0200

    MAGETWO-33444: Cover app/code/Magento/Indexer/Indexer.php

--- dev/tests/unit/testsuite/Magento/Indexer/App/IndexerTest.php
+++ dev/tests/unit/testsuite/Magento/Indexer/App/IndexerTest.php
@@ -44,16 +44,17 @@ class IndexerTest extends \PHPUnit_Framework_TestCase
 
     /**
      * @param bool $isExist
+     * @param array $callCount
      * @dataProvider executeProvider
      */
-    public function testExecute($isExist, $expectsValue)
+    public function testExecute($isExist, $callCount)
     {
         $this->_response->expects($this->once())->method('setCode')->with(0);
         $this->_response->expects($this->once())->method('getCode')->will($this->returnValue(0));
         $dir = $this->getMock('Magento\Framework\Filesystem\Directory\Write', [], [], '', false);
         $dir->expects($this->any())->method('getRelativePath')->will($this->returnArgument(0));
         $dir->expects($this->once())->method('isExist')->will($this->returnValue($isExist));
-        $dir->expects($this->exactly($expectsValue))->method('delete')->will($this->returnValue(true));
+        $dir->expects($this->exactly($callCount))->method('delete')->will($this->returnValue(true));
         $this->filesystem->expects($this->once())->method('getDirectoryWrite')->will($this->returnValue($dir));
         $this->processor->expects($this->once())->method('reindexAll');
         $this->assertEquals(0, $this->entryPoint->launch()->getCode());
