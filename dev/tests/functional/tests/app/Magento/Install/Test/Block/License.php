acad0ce676fe28720d7f090ea7e2d105d94d40d5
Dmytro Vilchynskyi <dvilchynskyi@ebay.com>
2015-02-05 14:49:50 +0200

    MAGETWO-31515: [UI] CSS styling for Installation Form
    - fixed functional tests

--- dev/tests/functional/tests/app/Magento/Install/Test/Block/License.php
+++ dev/tests/functional/tests/app/Magento/Install/Test/Block/License.php
@@ -19,7 +19,7 @@ class License extends Block
      *
      * @var string
      */
-    protected $back = '.btn.btn-primary';
+    protected $back = '.btn.btn-large.btn-prime';
 
     /**
      * License text.
