23cbf4ae7632d59279eb262e44d05d9c0fc0f772
Illia Grybkov <igrybkov@ebay.com>
2015-02-11 15:59:35 +0200

    MAGETWO-31039: Introduce Enterprise Search module functionality
    MAGETWO-33996: Rename module to AdvancedSearch
    
    - Move files and fix tests

---
+++ Model/SearchDataProviderInterface.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\AdvancedSearch\Model;
+
+use Magento\Search\Model\QueryInterface;
+
+interface SearchDataProviderInterface
+{
+    /**
+     * @param QueryInterface $query
+     * @param int $limit
+     * @param array $additionalFilters
+     * @return \Magento\Search\Model\QueryResult[]
+     */
+    public function getSearchData(QueryInterface $query, $limit = null, $additionalFilters = []);
+
+    /**
+     * @return bool
+     */
+    public function isCountResultsEnabled();
+}
