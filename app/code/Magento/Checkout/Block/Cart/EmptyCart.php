166996f45021fc4dd81c8378158582369c6c52dd
Michail Slabko <mslabko@ebay.com>
2015-02-12 15:50:22 +0200

    MAGETWO-31159: Varnish coverage increase
    - MAGETWO-33778: Cart page container

---
+++ app/code/Magento/Checkout/Block/Cart/EmptyCart.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Checkout\Block\Cart;
+
+class EmptyCart extends \Magento\Checkout\Block\Cart
+{
+    /**
+     * @param \Magento\Framework\View\Element\Template\Context $context
+     * @param \Magento\Customer\Model\Session $customerSession
+     * @param \Magento\Checkout\Model\Session $checkoutSession
+     * @param \Magento\Catalog\Model\Resource\Url $catalogUrlBuilder
+     * @param \Magento\Checkout\Helper\Cart $cartHelper
+     * @param \Magento\Framework\App\Http\Context $httpContext
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Customer\Model\Session $customerSession,
+        \Magento\Checkout\Model\Session $checkoutSession,
+        \Magento\Catalog\Model\Resource\Url $catalogUrlBuilder,
+        \Magento\Checkout\Helper\Cart $cartHelper,
+        \Magento\Framework\App\Http\Context $httpContext,
+        array $data = []
+    ) {
+        parent::__construct(
+            $context,
+            $customerSession,
+            $checkoutSession,
+            $catalogUrlBuilder,
+            $cartHelper,
+            $httpContext,
+            $data
+        );
+        $this->_isScopePrivate = false;
+    }
+}
