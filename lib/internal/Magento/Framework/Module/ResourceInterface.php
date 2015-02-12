b852c5c3697280443359d786db39ffbd72ec293a
Ivan Gavryshko <igavryshko@ebay.com>
2015-02-12 10:15:14 -0600

    MAGETWO-33884: Changes to database and module.xml - Fixed parameter naming in ResourceInterface according to CR.

--- lib/internal/Magento/Framework/Module/ResourceInterface.php
+++ lib/internal/Magento/Framework/Module/ResourceInterface.php
@@ -13,34 +13,34 @@ interface ResourceInterface
     /**
      * Get Module version from DB
      *
-     * @param string $resName
+     * @param string $moduleName
      * @return false|string
      */
-    public function getDbVersion($resName);
+    public function getDbVersion($moduleName);
 
     /**
      * Get resource data version
      *
-     * @param string $resName
+     * @param string $moduleName
      * @return string|false
      */
-    public function getDataVersion($resName);
+    public function getDataVersion($moduleName);
 
     /**
      * Set Module version
      *
-     * @param string $resName
+     * @param string $moduleName
      * @param string $version
      * @return int
      */
-    public function setDbVersion($resName, $version);
+    public function setDbVersion($moduleName, $version);
 
     /**
      * Set resource data version
      *
-     * @param string $resName
+     * @param string $moduleName
      * @param string $version
      * @return void
      */
-    public function setDataVersion($resName, $version);
+    public function setDataVersion($moduleName, $version);
 }
