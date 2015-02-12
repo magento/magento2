4679cd38ffb0f1a9e9e66c96926e2e6bd892ac1f
Dmytro Aponasenko <daponasenko@ebay.com>
2015-02-12 19:17:42 +0200

    MTA-1602: Sync qmt repository with mainline - Sprint 6

--- dev/tests/functional/tests/app/Magento/Customer/Test/Handler/Customer/Curl.php
+++ dev/tests/functional/tests/app/Magento/Customer/Test/Handler/Customer/Curl.php
@@ -9,7 +9,6 @@ namespace Magento\Customer\Test\Handler\Customer;
 use Magento\Customer\Test\Fixture\Customer;
 use Magento\Mtf\Fixture\FixtureInterface;
 use Magento\Mtf\Handler\Curl as AbstractCurl;
-use Magento\Mtf\Config;
 use Magento\Mtf\Util\Protocol\CurlInterface;
 use Magento\Mtf\Util\Protocol\CurlTransport;
 use Magento\Mtf\Util\Protocol\CurlTransport\BackendDecorator;
