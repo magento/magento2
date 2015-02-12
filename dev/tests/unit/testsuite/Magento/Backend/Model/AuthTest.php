f886b3835bf50d229899299b5cd50249470e8e3a
Maxim Shikula <mshikula@ebay.com>
2015-02-12 16:27:04 +0200

    MAGETWO-33813: Replace Magento\Framework\Model\Exception to LocalizedException

--- dev/tests/unit/testsuite/Magento/Backend/Model/AuthTest.php
+++ dev/tests/unit/testsuite/Magento/Backend/Model/AuthTest.php
@@ -59,7 +59,7 @@ class AuthTest extends \PHPUnit_Framework_TestCase
             ->method('create')
             ->with('Magento\Backend\Model\Auth\Credential\StorageInterface')
             ->will($this->returnValue($this->_credentialStorage));
-        $exceptionMock = new \Magento\Framework\Exception\LocalizedException();
+        $exceptionMock = new \Magento\Framework\Exception\LocalizedException('message');
         $this->_credentialStorage
             ->expects($this->once())
             ->method('login')
