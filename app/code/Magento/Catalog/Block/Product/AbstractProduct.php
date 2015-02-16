960a3a92cf67e62275c02c5cf9806170cf93d14d
Pavlo Cherniavskyi <pcherniavsky@ebay.com>
2015-02-16 11:46:14 +0200

    MAGETWO-30801: Asynchronous products addition to cart
    - merge fix

--- app/code/Magento/Catalog/Block/Product/AbstractProduct.php
+++ app/code/Magento/Catalog/Block/Product/AbstractProduct.php
@@ -595,7 +595,7 @@ class AbstractProduct extends \Magento\Framework\View\Element\Template
     {
         return $this->_scopeConfig->getValue(
             'checkout/cart/redirect_to_cart',
-            \Magento\Framework\Store\ScopeInterface::SCOPE_STORE
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
         );
     }
 }
