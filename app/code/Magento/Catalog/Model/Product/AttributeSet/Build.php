2bc4291aa4d9702a54038b8d28b8b4c7646c0877
Maxim Shikula <mshikula@ebay.com>
2015-02-17 14:38:31 +0200

    MAGETWO-33852: Replace obsolete exceptions (see description - Part2)

--- app/code/Magento/Catalog/Model/Product/AttributeSet/Build.php
+++ app/code/Magento/Catalog/Model/Product/AttributeSet/Build.php
@@ -3,8 +3,9 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-
 namespace Magento\Catalog\Model\Product\AttributeSet;
+
+use \Magento\Framework\Exception\AlreadyExistsException;
 
 class Build
 {
