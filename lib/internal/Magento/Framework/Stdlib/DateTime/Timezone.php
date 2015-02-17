263441d896a00ea9c405c62349aa62f5403139b0
Andriy Nasinnyk <anasinnyk@ebay.com>
2015-02-17 15:58:39 +0200

    MAGETWO-32616 Replace Zend_Date with native PHP DateTime object/functions

--- lib/internal/Magento/Framework/Stdlib/DateTime/Timezone.php
+++ lib/internal/Magento/Framework/Stdlib/DateTime/Timezone.php
@@ -231,11 +231,7 @@ class Timezone implements TimezoneInterface
     public function scopeTimeStamp($scope = null)
     {
         $timezone = $this->_scopeConfig->getValue($this->getDefaultTimezonePath(), $this->_scopeType, $scope);
-        $currentTimezone = @date_default_timezone_get();
-        @date_default_timezone_set($timezone);
-        $date = date('Y-m-d H:i:s');
-        @date_default_timezone_set($currentTimezone);
-        return strtotime($date);
+        return (new \DateTime('now', new \DateTimeZone($timezone)))->getTimestamp();
     }
 
     /**
