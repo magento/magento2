d8e94716778624cbaac03f49f2f4b626fa79a8a9
Sergey Semenov <ssemenov@ebay.com>
2015-02-19 17:48:38 +0200

    MAGETWO-33900: [Dev] Replace Zend_Date with native PHP DateTime object/functions.

--- lib/internal/Magento/Framework/DB/Adapter/Pdo/Mysql.php
+++ lib/internal/Magento/Framework/DB/Adapter/Pdo/Mysql.php
@@ -275,7 +275,7 @@ class Mysql extends \Zend_Db_Adapter_Pdo_Mysql implements AdapterInterface
     /**
      * Convert date to DB format
      *
-     * @param int|string|\Magento\Framework\Stdlib\DateTime\DateInterface $date
+     * @param int|string|\DateTime $date
      * @return \Zend_Db_Expr
      */
     public function convertDate($date)
@@ -286,7 +286,7 @@ class Mysql extends \Zend_Db_Adapter_Pdo_Mysql implements AdapterInterface
     /**
      * Convert date and time to DB format
      *
-     * @param   int|string|\Magento\Framework\Stdlib\DateTime\DateInterface $datetime
+     * @param   int|string|\DateTime $datetime
      * @return \Zend_Db_Expr
      */
     public function convertDateTime($datetime)
@@ -2628,7 +2628,7 @@ class Mysql extends \Zend_Db_Adapter_Pdo_Mysql implements AdapterInterface
     /**
      * Format Date to internal database date format
      *
-     * @param int|string|\Magento\Framework\Stdlib\DateTime\DateInterface $date
+     * @param int|string|\DateTime $date
      * @param bool $includeTime
      * @return \Zend_Db_Expr
      */
