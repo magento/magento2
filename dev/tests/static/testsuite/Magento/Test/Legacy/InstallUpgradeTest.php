fa226da70891f6ad681f9689545e0df40c2945f6
Maddy Chellathurai <mchellathura@ebay.com>
2015-02-23 17:46:31 -0600

    MAGETWO-34435: Create Legacy Test for install/upgrade schema/data scripts
    - CR fixes.

--- dev/tests/static/testsuite/Magento/Test/Legacy/InstallUpgradeTest.php
+++ dev/tests/static/testsuite/Magento/Test/Legacy/InstallUpgradeTest.php
@@ -4,52 +4,74 @@
  * See COPYING.txt for license details.
  */
 
+namespace Magento\Test\Legacy;
+
+use Magento\Framework\Test\Utility\Files;
+use Magento\Framework\Test\Utility\AggregateInvoker;
+
 /**
  * Tests to find obsolete install/upgrade schema/data scripts
  */
-namespace Magento\Test\Legacy;
-
 class InstallUpgradeTest extends \PHPUnit_Framework_TestCase
 {
     public function testForOldInstallUpgradeScripts()
     {
-        $invoker = new \Magento\Framework\Test\Utility\AggregateInvoker($this);
+        $invoker = new AggregateInvoker($this);
         $invoker(
             /**
              * @param string $file
              */
             function ($file) {
-                $dirName = dirname(dirname($file));
                 $this->assertStringStartsNotWith(
-                    'install',
+                    'install-',
                     basename($file),
-                    'Install scripts are obsolete. Please create class InstallSchema.php or InstallData.php '
-                    . 'in ' . $dirName . '/Setup folder'
+                    'Install scripts are obsolete. Please create class InstallSchema in module\'s Setup folder'
                 );
                 $this->assertStringStartsNotWith(
-                    'upgrade',
+                    'data-install-',
                     basename($file),
-                    'Upgrade scripts are obsolete. Please create class UpgradeSchema.php or UpgradeData.php '
-                    . 'in ' . $dirName . '/Setup folder'
+                    'Install scripts are obsolete. Please create class InstallData in module\'s Setup folder'
+                );
+                $this->assertStringStartsNotWith(
+                    'upgrade-',
+                    basename($file),
+                    'Upgrade scripts are obsolete. Please create class UpgradeSchema in module\'s Setup folder'
+                );
+                $this->assertStringStartsNotWith(
+                    'data-upgrade-',
+                    basename($file),
+                    'Upgrade scripts are obsolete. Please create class UpgradeData in module\'s Setup folder'
                 );
                 $this->assertStringStartsNotWith(
                     'recurring',
                     basename($file),
-                    'Recurring scripts are obsolete. Please create class Recurring.php '
-                    . 'in ' . $dirName . '/Setup folder'
+                    'Recurring scripts are obsolete. Please create class Recurring in module\'s Setup folder'
                 );
-                $this->assertStringEndsNotWith(
-                    '/sql',
-                    dirname($file),
-                    'Invalid directory. Please convert sql scripts to a class within ' . $dirName . '/Setup folder'
-                );
-                $this->assertStringEndsNotWith(
-                    '/data',
-                    dirname($file),
-                    'Invalid directory. Please convert data scripts to a class within ' . $dirName . '/Setup folder'
+                $this->assertFileNotExists(
+                    $file,
+                    'Invalid directory. Please convert data/sql scripts to a class within module\'s Setup folder'
                 );
             },
-            \Magento\Framework\Test\Utility\Files::init()->getPhpFiles(true, false, false)
+            $this->convertArray(Files::init()->getFiles(
+                [BP . '/app/code/*/*/sql', BP . '/app/code/*/*/data'],
+                '*.php'
+                )
+            )
         );
+    }
+
+    /**
+     * Converts from string array to array of arrays.
+     *
+     * @param array $stringArray
+     * @return array
+     */
+    private function convertArray($stringArray)
+    {
+        $array = [];
+        foreach ($stringArray as $item) {
+            $array[] = [$item];
+        }
+        return $array;
     }
 }
