d7244d7c82b23135c8b8dd0c4ccd47e88f3dc449
Pavlo Cherniavskyi <pcherniavsky@ebay.com>
2015-02-25 18:37:00 +0200

    MAGETWO-33631: Advanced Developer Workflow
    - added switcher in admin configuration (changes and unit-test)

--- app/code/Magento/Developer/Model/Config/Source/WorkflowType.php
+++ app/code/Magento/Developer/Model/Config/Source/WorkflowType.php
@@ -3,7 +3,7 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\Backend\Model\Config\Source\Dev;
+namespace Magento\Developer\Model\Config\Source;
 
 use Magento\Framework\App\State;
 use Magento\Framework\Option\ArrayInterface;
--- dev/tests/unit/testsuite/Magento/Developer/Model/Config/Source/WorkflowTypeTest.php
+++ dev/tests/unit/testsuite/Magento/Developer/Model/Config/Source/WorkflowTypeTest.php
@@ -3,7 +3,7 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\Backend\Model\Config\Source\Dev;
+namespace Magento\Developer\Model\Config\Source;
 
 /**
  * Class WorkflowTypeTest
