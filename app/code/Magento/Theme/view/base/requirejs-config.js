215c6f354aca69b6ff7ccce0806b0e4f5c83fb19
Denys Rul <drul@ebay.com>
2015-02-26 19:46:33 +0200

    MAGETWO-34588: Deliver JS changes to support Bundling
    - Remove files that are already in a bundle

--- app/code/Magento/Theme/view/base/requirejs-config.js
+++ app/code/Magento/Theme/view/base/requirejs-config.js
@@ -11,7 +11,6 @@ var config = {
         "jquery/jstree/jquery.hotkeys": ["jquery"],
         "jquery/hover-intent": ["jquery"],
         "mage/adminhtml/backup": ["prototype"],
-        "mage/adminhtml/varienLoader": ["prototype"],
         "mage/captcha": ["prototype"],
         "mage/common": ["jquery"],
         "mage/requirejs/plugin/id-normalizer": ["jquery"],
