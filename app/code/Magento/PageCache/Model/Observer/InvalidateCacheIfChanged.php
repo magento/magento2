ef164ce73d3874aee0ebaa8a0b356bbc293c8b47
Eddie Lau <kahlau@ebay.com>
2015-02-26 17:48:54 -0600

    MAGETWO-33240: FPC is permanently invalidated by cron
    - added new observer

---
+++ app/code/Magento/PageCache/Model/Observer/InvalidateCacheIfChanged.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\PageCache\Model\Observer;
+
+class InvalidateCacheIfChanged
+{
+    /**
+     * @var \Magento\Framework\App\Cache\TypeListInterface
+     */
+    protected $typeList;
+
+    /**
+     * Application config object
+     *
+     * @var \Magento\PageCache\Model\Config
+     */
+    protected $config;
+
+    /**
+     * @param \Magento\PageCache\Model\Config $config
+     * @param \Magento\Framework\App\Cache\TypeListInterface $typeList
+     */
+    public function __construct(
+        \Magento\PageCache\Model\Config $config,
+        \Magento\Framework\App\Cache\TypeListInterface $typeList
+    ) {
+        $this->config = $config;
+        $this->typeList = $typeList;
+    }
+
+    /**
+     * Invalidate full page cache if content is changed
+     *
+     * @return void
+     */
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        if ($this->config->getType() == \Magento\PageCache\Model\Config::BUILT_IN && $this->config->isEnabled()) {
+            $object = $observer->getEvent()->getObject();
+            if ($object instanceof \Magento\Framework\Object\IdentityInterface) {
+                if ($object->getIdentities()) {
+                    $this->typeList->invalidate('full_page');
+                }
+            }
+        }
+    }
+}
