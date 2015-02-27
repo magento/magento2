aee50fdbc12e14606b05fcd912f49a945901ebcf
Egor Shitikov <yshytikov@ebay.com>
2015-02-27 20:01:53 +0200

    MAGETWO-33894: [GITHUB] Admin dashboard Most Viewed Products Tab issue (without product list) #1029

---
+++ dev/tests/integration/testsuite/Magento/Backend/Controller/Adminhtml/Dashboard/ProductsViewedTest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ *
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Backend\Controller\Adminhtml\Dashboard;
+
+class ProductsViewedTest extends \Magento\Backend\Utility\Controller
+{
+
+    public function testExecute()
+    {
+        $this->dispatch('backend/admin/dashboard/productsViewed/');
+        $this->assertEquals(200, $this->getResponse()->getHttpResponseCode());
+
+        $actual = $this->getResponse()->getBody();
+        $this->assertContains('dashboard-item-content', $actual);
+    }
+}
