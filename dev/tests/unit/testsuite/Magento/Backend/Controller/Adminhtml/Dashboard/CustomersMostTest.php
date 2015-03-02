4508a6e68969b7510a63e567e25ee95432c822ec
Joan He <joan@x.com>
2015-03-02 11:13:01 -0600

    MAGETWO-34451: Move and update name space of all classes without suffix 'Test'
    - Reverted last commit

--- dev/tests/unit/testsuite/Magento/Backend/Controller/Adminhtml/Dashboard/CustomersMostTest.php
+++ dev/tests/unit/testsuite/Magento/Backend/Controller/Adminhtml/Dashboard/CustomersMostTest.php
@@ -11,7 +11,7 @@ namespace Magento\Backend\Controller\Adminhtml\Dashboard;
 /**
  * Test for \Magento\Backend\Controller\Adminhtml\Dashboard\CustomersMost
  */
-class CustomersMostTest extends \PHPUnit_Framework_TestCase
+class CustomersMostTest extends AbstractTestCase
 {
     public function testExecute()
     {
@@ -19,43 +19,5 @@ class CustomersMostTest extends \PHPUnit_Framework_TestCase
             'Magento\Backend\Controller\Adminhtml\Dashboard\CustomersMost',
             'Magento\Backend\Block\Dashboard\Tab\Customers\Most'
         );
-    }
-
-    /**
-     * Assertions for controller execute method
-     *
-     * @param $controllerName
-     * @param $blockName
-     */
-    protected function assertExecute($controllerName, $blockName)
-    {
-        $objectManager = new \Magento\TestFramework\Helper\ObjectManager($this);
-        $outPut = "data";
-        $resultRawMock = $this->getMock(
-            'Magento\Framework\Controller\Result\Raw',
-            ['setContents'], [], '', false);
-        $resultRawFactoryMock = $this->getMock(
-            'Magento\Framework\Controller\Result\RawFactory',
-            ['create'], [], '', false);
-        $layoutFactoryMock = $this->getMock(
-            'Magento\Framework\View\LayoutFactory',
-            ['create'], [], '', false);
-        $layoutMock = $this->getMock('Magento\Framework\View\Layout',
-            ['createBlock', 'toHtml'], [], '', false);
-        $layoutFactoryMock->expects($this->once())->method('create')->will($this->returnValue($layoutMock));
-        $layoutMock->expects($this->once())->method('createBlock')->with($blockName)->will($this->returnSelf());
-        $layoutMock->expects($this->once())->method('toHtml')->will($this->returnValue($outPut));
-        $resultRawFactoryMock->expects($this->once())->method('create')->will($this->returnValue($resultRawMock));
-        $resultRawMock->expects($this->once())->method('setContents')->with($outPut)->will($this->returnSelf());
-
-        $controller = $objectManager->getObject(
-            $controllerName,
-            [
-                'resultRawFactory' => $resultRawFactoryMock,
-                'layoutFactory' => $layoutFactoryMock
-            ]
-        );
-        $result = $controller->execute();
-        $this->assertInstanceOf('Magento\Framework\Controller\Result\Raw', $result);
     }
 }
