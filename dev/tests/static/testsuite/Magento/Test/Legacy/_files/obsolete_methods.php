97a71f77af0296203f4bd1f0718d9bdf5fc462f9
Volodymyr Kholoshenko <vkholoshenko@ebay.com>
2015-03-02 19:31:59 +0200

    MAGETWO-34538: Unify common methods in \Magento\Eav\Model\Config

--- dev/tests/static/testsuite/Magento/Test/Legacy/_files/obsolete_methods.php
+++ dev/tests/static/testsuite/Magento/Test/Legacy/_files/obsolete_methods.php
@@ -2085,4 +2085,5 @@ return [
     ['getAlias', 'Magento\Framework\App\Request\Http', 'Magento\Framework\HTTP\PhpEnvironment\Request'],
     ['setAlias', 'Magento\Framework\App\Request\Http', 'Magento\Framework\HTTP\PhpEnvironment\Request'],
     ['getParam', 'Magento\Framework\App\RequestInterface'],
+    ['getCollectionAttribute', 'Magento\Eav\Model\Config'],
 ];
