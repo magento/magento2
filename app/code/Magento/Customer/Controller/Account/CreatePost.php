bfc08e2a02dbaf48839dcee3158dae8261212b60
okarpenko <okarpenko@ebay.com>
2015-03-02 12:37:02 +0200

    MAGETWO-16837: Guest can't add product to Wishlist while registering

--- app/code/Magento/Customer/Controller/Account/CreatePost.php
+++ app/code/Magento/Customer/Controller/Account/CreatePost.php
@@ -29,6 +29,7 @@ use Magento\Customer\Model\CustomerExtractor;
 use Magento\Framework\Exception\StateException;
 use Magento\Framework\Exception\InputException;
 use Magento\Store\Model\ScopeInterface;
+use Magento\Framework\Url\DecoderInterface;
 
 /**
  * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
@@ -82,6 +83,9 @@ class CreatePost extends \Magento\Customer\Controller\Account
      */
     protected $dataObjectHelper;
 
+    /** @var DecoderInterface */
+    protected $urlDecoder;
+
     /**
      * @param Context $context
      * @param Session $customerSession
@@ -102,6 +106,7 @@ class CreatePost extends \Magento\Customer\Controller\Account
      * @param Escaper $escaper
      * @param CustomerExtractor $customerExtractor
      * @param \Magento\Framework\Api\DataObjectHelper $dataObjectHelper
+     * @param DecoderInterface $urlDecoder
      *
      * @SuppressWarnings(PHPMD.ExcessiveParameterList)
      */
@@ -124,7 +129,8 @@ class CreatePost extends \Magento\Customer\Controller\Account
         Registration $registration,
         Escaper $escaper,
         CustomerExtractor $customerExtractor,
-        \Magento\Framework\Api\DataObjectHelper $dataObjectHelper
+        \Magento\Framework\Api\DataObjectHelper $dataObjectHelper,
+        DecoderInterface $urlDecoder
     ) {
         $this->scopeConfig = $scopeConfig;
         $this->storeManager = $storeManager;
@@ -141,6 +147,7 @@ class CreatePost extends \Magento\Customer\Controller\Account
         $this->customerExtractor = $customerExtractor;
         $this->urlModel = $urlFactory->create();
         $this->dataObjectHelper = $dataObjectHelper;
+        $this->urlDecoder = $urlDecoder;
         parent::__construct($context, $customerSession, $resultRedirectFactory, $resultPageFactory);
     }
 
@@ -253,9 +260,8 @@ class CreatePost extends \Magento\Customer\Controller\Account
                 $resultRedirect->setUrl($this->_redirect->success($url));
             } else {
                 $this->_getSession()->setCustomerDataAsLoggedIn($customer);
-
                 $this->messageManager->addSuccess($this->getSuccessMessage());
-                $resultRedirect->setUrl($this->getSuccessRedirect());
+                $resultRedirect = $this->loginPostRedirect();
             }
             return $resultRedirect;
         } catch (StateException $e) {
@@ -279,6 +285,62 @@ class CreatePost extends \Magento\Customer\Controller\Account
         $this->_getSession()->setCustomerFormData($this->getRequest()->getPostValue());
         $defaultUrl = $this->urlModel->getUrl('*/*/create', ['_secure' => true]);
         $resultRedirect->setUrl($this->_redirect->error($defaultUrl));
+        return $resultRedirect;
+    }
+
+    /**
+     * Define target URL and redirect customer after logging in
+     *
+     * @return \Magento\Framework\Controller\Result\Redirect
+     * @SuppressWarnings(PHPMD.CyclomaticComplexity)
+     */
+    protected function loginPostRedirect()
+    {
+        $lastCustomerId = $this->_getSession()->getLastCustomerId();
+        if (isset(
+                $lastCustomerId
+            ) && $this->_getSession()->isLoggedIn() && $lastCustomerId != $this->_getSession()->getId()
+        ) {
+            $this->_getSession()->unsBeforeAuthUrl()->setLastCustomerId($this->_getSession()->getId());
+        }
+        if (!$this->_getSession()->getBeforeAuthUrl() ||
+            $this->_getSession()->getBeforeAuthUrl() == $this->storeManager->getStore()->getBaseUrl()
+        ) {
+            // Set default URL to redirect customer to
+            $this->_getSession()->setBeforeAuthUrl($this->customerUrl->getAccountUrl());
+            // Redirect customer to the last page visited after logging in
+            if ($this->_getSession()->isLoggedIn()) {
+                if (!$this->scopeConfig->isSetFlag(
+                    CustomerUrl::XML_PATH_CUSTOMER_STARTUP_REDIRECT_TO_DASHBOARD,
+                    \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+                )
+                ) {
+                    $referer = $this->getRequest()->getParam(CustomerUrl::REFERER_QUERY_PARAM_NAME);
+                    if ($referer) {
+                        $referer = $this->urlDecoder->decode($referer);
+                        if ($this->_url->isOwnOriginUrl()) {
+                            $this->_getSession()->setBeforeAuthUrl($referer);
+                        }
+                    }
+                } elseif ($this->_getSession()->getAfterAuthUrl()) {
+                    $this->_getSession()->setBeforeAuthUrl($this->_getSession()->getAfterAuthUrl(true));
+                }
+            } else {
+                $this->_getSession()->setBeforeAuthUrl($this->customerUrl->getLoginUrl());
+            }
+        } elseif ($this->_getSession()->getBeforeAuthUrl() == $this->customerUrl->getLogoutUrl()) {
+            $this->_getSession()->setBeforeAuthUrl($this->customerUrl->getDashboardUrl());
+        } else {
+            if (!$this->_getSession()->getAfterAuthUrl()) {
+                $this->_getSession()->setAfterAuthUrl($this->_getSession()->getBeforeAuthUrl());
+            }
+            if ($this->_getSession()->isLoggedIn()) {
+                $this->_getSession()->setBeforeAuthUrl($this->_getSession()->getAfterAuthUrl(true));
+            }
+        }
+        /** @var \Magento\Framework\Controller\Result\Redirect $resultRedirect */
+        $resultRedirect = $this->resultRedirectFactory->create();
+        $resultRedirect->setUrl($this->_getSession()->getBeforeAuthUrl(true));
         return $resultRedirect;
     }
 
