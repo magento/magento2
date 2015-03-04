3e07d79d1892d95e28c4a4e161c6f059b570e9ca
Maddy Chellathurai <mchellathura@ebay.com>
2015-03-04 16:16:57 -0600

    MAGETWO-34694: Implement config options for 'modules' segment
    - moved class to Model

--- setup/src/Magento/Setup/Model/ConfigOptions.php
+++ setup/src/Magento/Setup/Model/ConfigOptions.php
@@ -3,7 +3,7 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\Setup;
+namespace Magento\Setup\Model;
 
 use Magento\Framework\Math\Random;
 use Magento\Framework\Setup\ConfigOptionsInterface;
