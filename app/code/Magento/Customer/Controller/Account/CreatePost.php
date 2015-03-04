70c2fe3c8f83dcc341987abc6ad51a1c37e6aa90
okarpenko <okarpenko@ebay.com>
2015-03-04 15:13:26 +0200

    MAGETWO-16837: Guest can't add product to Wishlist while registering
    - fix bug

--- app/code/Magento/Customer/Controller/Account/CreatePost.php
+++ app/code/Magento/Customer/Controller/Account/CreatePost.php
@@ -34,14 +34,8 @@ use Magento\Framework\Url\DecoderInterface;
 /**
  * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
  */
-class CreatePost extends \Magento\Customer\Controller\Account
+class CreatePost extends \Magento\Customer\Controller\AccountRedirect
 {
-    /** @var ScopeConfigInterface */
-    protected $scopeConfig;
-
-    /** @var StoreManagerInterface */
-    protected $storeManager;
-
     /** @var AccountManagementInterface */
     protected $accountManagement;
 
@@ -78,13 +72,8 @@ class CreatePost extends \Magento\Customer\Controller\Account
     /** @var \Magento\Framework\UrlInterface */
     protected $urlModel;
 
-    /**
-     * @var \Magento\Framework\Api\DataObjectHelper
-     */
+    /** @var \Magento\Framework\Api\DataObjectHelper  */
     protected $dataObjectHelper;
-
-    /** @var DecoderInterface */
-    protected $urlDecoder;
 
     /**
      * @param Context $context
@@ -132,8 +121,6 @@ class CreatePost extends \Magento\Customer\Controller\Account
         \Magento\Framework\Api\DataObjectHelper $dataObjectHelper,
         DecoderInterface $urlDecoder
     ) {
-        $this->scopeConfig = $scopeConfig;
-        $this->storeManager = $storeManager;
         $this->accountManagement = $accountManagement;
         $this->addressHelper = $addressHelper;
         $this->formFactory = $formFactory;
@@ -147,8 +134,15 @@ class CreatePost extends \Magento\Customer\Controller\Account
         $this->customerExtractor = $customerExtractor;
         $this->urlModel = $urlFactory->create();
         $this->dataObjectHelper = $dataObjectHelper;
-        $this->urlDecoder = $urlDecoder;
-        parent::__construct($context, $customerSession, $resultRedirectFactory, $resultPageFactory);
+        parent::__construct(
+            $context,
+            $customerSession,
+            $resultRedirectFactory,
+            $resultPageFactory,
+            $scopeConfig,
+            $storeManager,
+            $urlDecoder
+        );
     }
 
     /**
@@ -285,62 +279,6 @@ class CreatePost extends \Magento\Customer\Controller\Account
         $this->_getSession()->setCustomerFormData($this->getRequest()->getPostValue());
         $defaultUrl = $this->urlModel->getUrl('*/*/create', ['_secure' => true]);
         $resultRedirect->setUrl($this->_redirect->error($defaultUrl));
-        return $resultRedirect;
-    }
-
-    /**
-     * Define target URL and redirect customer after logging in
-     *
-     * @return \Magento\Framework\Controller\Result\Redirect
-     * @SuppressWarnings(PHPMD.CyclomaticComplexity)
-     */
-    protected function loginPostRedirect()
-    {
-        $lastCustomerId = $this->_getSession()->getLastCustomerId();
-        if (isset(
-                $lastCustomerId
-            ) && $this->_getSession()->isLoggedIn() && $lastCustomerId != $this->_getSession()->getId()
-        ) {
-            $this->_getSession()->unsBeforeAuthUrl()->setLastCustomerId($this->_getSession()->getId());
-        }
-        if (!$this->_getSession()->getBeforeAuthUrl() ||
-            $this->_getSession()->getBeforeAuthUrl() == $this->storeManager->getStore()->getBaseUrl()
-        ) {
-            // Set default URL to redirect customer to
-            $this->_getSession()->setBeforeAuthUrl($this->customerUrl->getAccountUrl());
-            // Redirect customer to the last page visited after logging in
-            if ($this->_getSession()->isLoggedIn()) {
-                if (!$this->scopeConfig->isSetFlag(
-                    CustomerUrl::XML_PATH_CUSTOMER_STARTUP_REDIRECT_TO_DASHBOARD,
-                    \Magento\Store\Model\ScopeInterface::SCOPE_STORE
-                )
-                ) {
-                    $referer = $this->getRequest()->getParam(CustomerUrl::REFERER_QUERY_PARAM_NAME);
-                    if ($referer) {
-                        $referer = $this->urlDecoder->decode($referer);
-                        if ($this->_url->isOwnOriginUrl()) {
-                            $this->_getSession()->setBeforeAuthUrl($referer);
-                        }
-                    }
-                } elseif ($this->_getSession()->getAfterAuthUrl()) {
-                    $this->_getSession()->setBeforeAuthUrl($this->_getSession()->getAfterAuthUrl(true));
-                }
-            } else {
-                $this->_getSession()->setBeforeAuthUrl($this->customerUrl->getLoginUrl());
-            }
-        } elseif ($this->_getSession()->getBeforeAuthUrl() == $this->customerUrl->getLogoutUrl()) {
-            $this->_getSession()->setBeforeAuthUrl($this->customerUrl->getDashboardUrl());
-        } else {
-            if (!$this->_getSession()->getAfterAuthUrl()) {
-                $this->_getSession()->setAfterAuthUrl($this->_getSession()->getBeforeAuthUrl());
-            }
-            if ($this->_getSession()->isLoggedIn()) {
-                $this->_getSession()->setBeforeAuthUrl($this->_getSession()->getAfterAuthUrl(true));
-            }
-        }
-        /** @var \Magento\Framework\Controller\Result\Redirect $resultRedirect */
-        $resultRedirect = $this->resultRedirectFactory->create();
-        $resultRedirect->setUrl($this->_getSession()->getBeforeAuthUrl(true));
         return $resultRedirect;
     }
 
--- app/code/Magento/Customer/Controller/Account/LoginPost.php
+++ app/code/Magento/Customer/Controller/Account/LoginPost.php
@@ -22,19 +22,10 @@ use Magento\Framework\Data\Form\FormKey\Validator;
 /**
  * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
  */
-class LoginPost extends \Magento\Customer\Controller\Account
+class LoginPost extends \Magento\Customer\Controller\AccountRedirect
 {
-    /** @var ScopeConfigInterface */
-    protected $scopeConfig;
-
-    /** @var StoreManagerInterface */
-    protected $storeManager;
-
     /** @var AccountManagementInterface */
     protected $customerAccountManagement;
-
-    /** @var DecoderInterface */
-    protected $urlDecoder;
 
     /** @var CustomerUrl */
     protected $customerUrl;
@@ -68,69 +59,18 @@ class LoginPost extends \Magento\Customer\Controller\Account
         CustomerUrl $customerHelperData,
         Validator $formKeyValidator
     ) {
-        $this->scopeConfig = $scopeConfig;
-        $this->storeManager = $storeManager;
         $this->customerAccountManagement = $customerAccountManagement;
-        $this->urlDecoder = $urlDecoder;
         $this->customerUrl = $customerHelperData;
         $this->formKeyValidator = $formKeyValidator;
-        parent::__construct($context, $customerSession, $resultRedirectFactory, $resultPageFactory);
-    }
-
-    /**
-     * Define target URL and redirect customer after logging in
-     *
-     * @return \Magento\Framework\Controller\Result\Redirect
-     * @SuppressWarnings(PHPMD.CyclomaticComplexity)
-     */
-    protected function loginPostRedirect()
-    {
-        $lastCustomerId = $this->_getSession()->getLastCustomerId();
-        if (isset(
-            $lastCustomerId
-            ) && $this->_getSession()->isLoggedIn() && $lastCustomerId != $this->_getSession()->getId()
-        ) {
-            $this->_getSession()->unsBeforeAuthUrl()->setLastCustomerId($this->_getSession()->getId());
-        }
-        if (!$this->_getSession()->getBeforeAuthUrl() ||
-            $this->_getSession()->getBeforeAuthUrl() == $this->storeManager->getStore()->getBaseUrl()
-        ) {
-            // Set default URL to redirect customer to
-            $this->_getSession()->setBeforeAuthUrl($this->customerUrl->getAccountUrl());
-            // Redirect customer to the last page visited after logging in
-            if ($this->_getSession()->isLoggedIn()) {
-                if (!$this->scopeConfig->isSetFlag(
-                    CustomerUrl::XML_PATH_CUSTOMER_STARTUP_REDIRECT_TO_DASHBOARD,
-                    \Magento\Store\Model\ScopeInterface::SCOPE_STORE
-                )
-                ) {
-                    $referer = $this->getRequest()->getParam(CustomerUrl::REFERER_QUERY_PARAM_NAME);
-                    if ($referer) {
-                        $referer = $this->urlDecoder->decode($referer);
-                        if ($this->_url->isOwnOriginUrl()) {
-                            $this->_getSession()->setBeforeAuthUrl($referer);
-                        }
-                    }
-                } elseif ($this->_getSession()->getAfterAuthUrl()) {
-                    $this->_getSession()->setBeforeAuthUrl($this->_getSession()->getAfterAuthUrl(true));
-                }
-            } else {
-                $this->_getSession()->setBeforeAuthUrl($this->customerUrl->getLoginUrl());
-            }
-        } elseif ($this->_getSession()->getBeforeAuthUrl() == $this->customerUrl->getLogoutUrl()) {
-            $this->_getSession()->setBeforeAuthUrl($this->customerUrl->getDashboardUrl());
-        } else {
-            if (!$this->_getSession()->getAfterAuthUrl()) {
-                $this->_getSession()->setAfterAuthUrl($this->_getSession()->getBeforeAuthUrl());
-            }
-            if ($this->_getSession()->isLoggedIn()) {
-                $this->_getSession()->setBeforeAuthUrl($this->_getSession()->getAfterAuthUrl(true));
-            }
-        }
-        /** @var \Magento\Framework\Controller\Result\Redirect $resultRedirect */
-        $resultRedirect = $this->resultRedirectFactory->create();
-        $resultRedirect->setUrl($this->_getSession()->getBeforeAuthUrl(true));
-        return $resultRedirect;
+        parent::__construct(
+            $context,
+            $customerSession,
+            $resultRedirectFactory,
+            $resultPageFactory,
+            $scopeConfig,
+            $storeManager,
+            $urlDecoder
+        );
     }
 
     /**
