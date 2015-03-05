71ad5c20428d8f69c48a55a83767e644fb4c35a2
Dale Sikkema <dsikkema@ebay.com>
2015-03-05 15:38:05 -0600

    MAGETWO-34323: Move Magento/Framework/Webapi framework component unit tests

---
+++ lib/internal/Magento/Framework/Webapi/Test/Unit/ServiceInputProcessor/DataArray.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Framework\Webapi\Test\Unit\ServiceInputProcessor;
+
+use Magento\Framework\Api\AbstractExtensibleObject;
+
+class DataArray extends AbstractExtensibleObject
+{
+    /**
+     * @return \Magento\Framework\Webapi\ServiceInputProcessor\Simple[]|null
+     */
+    public function getItems()
+    {
+        return $this->_get('items');
+    }
+}
