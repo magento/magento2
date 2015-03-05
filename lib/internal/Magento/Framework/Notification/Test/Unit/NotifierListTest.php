c82c2af5ce82689ec7f2a37610b3b8b2308240fb
Joan He <joan@x.com>
2015-03-05 12:13:03 -0600

    MAGETWO-34323: Move Magento/Framework/Notification framework component unit tests

--- lib/internal/Magento/Framework/Notification/Test/Unit/NotifierListTest.php
+++ lib/internal/Magento/Framework/Notification/Test/Unit/NotifierListTest.php
@@ -4,7 +4,7 @@
  * See COPYING.txt for license details.
  */
 
-namespace Magento\Framework\Notification;
+namespace Magento\Framework\Notification\Test\Unit;
 
 use Magento\TestFramework\Helper\ObjectManager as ObjectManagerHelper;
 
