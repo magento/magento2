38cc6b0ba69b10e71aafece4a33df8633f2a155f
Joan He <joan@x.com>
2015-03-04 22:37:35 -0600

    MAGETWO-34323: Move Magento/Usps module unit tests

--- app/code/Magento/Usps/Test/Unit/Model/CarrierTest.php
+++ app/code/Magento/Usps/Test/Unit/Model/CarrierTest.php
@@ -3,7 +3,7 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\Usps\Model;
+namespace Magento\Usps\Test\Unit\Model;
 
 class CarrierTest extends \PHPUnit_Framework_TestCase
 {
--- app/code/Magento/Usps/Test/Unit/Model/Source/GenericTest.php
+++ app/code/Magento/Usps/Test/Unit/Model/Source/GenericTest.php
@@ -3,7 +3,7 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\Usps\Model\Source;
+namespace Magento\Usps\Test\Unit\Model\Source;
 
 class GenericTest extends \PHPUnit_Framework_TestCase
 {
