5a632ae9458f195837ed9469658003497f8e4327
Joan He <joan@x.com>
2015-03-04 22:43:42 -0600

    MAGETWO-34323: Move Magento/Ups module unit tests

--- app/code/Magento/Ups/Test/Unit/Helper/ConfigTest.php
+++ app/code/Magento/Ups/Test/Unit/Helper/ConfigTest.php
@@ -3,7 +3,7 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\Ups\Helper;
+namespace Magento\Ups\Test\Unit\Helper;
 
 /**
  * Config helper Test
--- app/code/Magento/Ups/Test/Unit/Model/CarrierTest.php
+++ app/code/Magento/Ups/Test/Unit/Model/CarrierTest.php
@@ -3,7 +3,7 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\Ups\Model;
+namespace Magento\Ups\Test\Unit\Model;
 
 class CarrierTest extends \PHPUnit_Framework_TestCase
 {
