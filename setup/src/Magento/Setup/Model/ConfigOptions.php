e4c0cef4b64fd8402caa2d0138f9746cb441cb8d
Eddie Lau <kahlau@ebay.com>
2015-03-05 17:52:17 -0600

    MAGETWO-34658: Implement config options for 'session' segment
    - changed hardcoded array index to constant

--- setup/src/Magento/Setup/Model/ConfigOptions.php
+++ setup/src/Magento/Setup/Model/ConfigOptions.php
@@ -15,6 +15,8 @@ use Magento\Framework\Setup\Option\TextConfigOption;
 use Magento\Framework\Module\ModuleList\Loader;
 use Magento\Framework\App\DeploymentConfig;
 use Magento\Framework\App\DeploymentConfig\DbConfig;
+use Magento\Framework\App\DeploymentConfig\EncryptConfig;
+use Magento\Framework\App\DeploymentConfig\SessionConfig;
 
 /**
  * Deployment configuration options needed for Setup application
@@ -155,7 +157,11 @@ class ConfigOptions implements ConfigOptionsInterface
     {
         $configData = [];
         // install segment
-        $configData[] = new ConfigData(ConfigFilePool::APP_CONFIG, 'install', ['date' => date('r')]);
+        $configData[] = new ConfigData(
+            ConfigFilePool::APP_CONFIG,
+            'install',
+            [DeploymentConfigMapper::$paramMap[DeploymentConfigMapper::KEY_DATE] => date('r')]
+        );
 
         // crypt segment
         if (isset($data[self::INPUT_KEY_CRYPT_KEY]) && !$data[self::INPUT_KEY_CRYPT_KEY]) {
@@ -163,9 +169,10 @@ class ConfigOptions implements ConfigOptionsInterface
         }
         $cryptData = [];
         if (!isset($data[self::INPUT_KEY_CRYPT_KEY])) {
-            $cryptData['key'] = md5($this->random->getRandomString(10));
+            $cryptData[DeploymentConfigMapper::$paramMap[self::INPUT_KEY_CRYPT_KEY]] =
+                md5($this->random->getRandomString(10));
         } else {
-            $cryptData['key'] = $data[self::INPUT_KEY_CRYPT_KEY];
+            $cryptData[DeploymentConfigMapper::$paramMap[self::INPUT_KEY_CRYPT_KEY]] = $data[self::INPUT_KEY_CRYPT_KEY];
         }
         $configData[] = new ConfigData(ConfigFilePool::APP_CONFIG, 'crypt', $cryptData);
 
@@ -186,9 +193,10 @@ class ConfigOptions implements ConfigOptionsInterface
             if ($data[self::INPUT_KEY_SESSION_SAVE] != 'files' && $data[self::INPUT_KEY_SESSION_SAVE] != 'db') {
                 throw new \InvalidArgumentException('Invalid session save location.');
             }
-            $sessionData['save'] = $data[self::INPUT_KEY_SESSION_SAVE];
+            $sessionData[DeploymentConfigMapper::$paramMap[self::INPUT_KEY_SESSION_SAVE]] =
+                $data[self::INPUT_KEY_SESSION_SAVE];
         } else {
-            $sessionData['save'] = 'files';
+            $sessionData[DeploymentConfigMapper::$paramMap[self::INPUT_KEY_SESSION_SAVE]] = 'files';
         }
         $configData[] = new ConfigData(ConfigFilePool::APP_CONFIG, 'session', $sessionData);
 
