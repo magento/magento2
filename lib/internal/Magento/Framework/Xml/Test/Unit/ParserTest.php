a2b272b814f4e404d794d3ebceeb5170929e8c78
Joan He <joan@x.com>
2015-03-05 09:00:31 -0600

    MAGETWO-34323: Move Magento/Framework/Xml framework component unit tests

--- lib/internal/Magento/Framework/Xml/Test/Unit/ParserTest.php
+++ lib/internal/Magento/Framework/Xml/Test/Unit/ParserTest.php
@@ -4,7 +4,7 @@
  * See COPYING.txt for license details.
  */
 
-namespace Magento\Framework\Xml;
+namespace Magento\Framework\Xml\Test\Unit;
 
 class ParserTest extends \PHPUnit_Framework_TestCase
 {
