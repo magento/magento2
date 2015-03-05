fc1a7ca2d0f91425db7d5a488e40312174d7aa44
Christopher O'Toole <otoolec@x.com>
2015-03-04 22:12:25 -0600

    MAGETWO-34323: Move Magento/Fedex module unit tests

--- app/code/Magento/Fedex/Test/Unit/Model/CarrierTest.php
+++ app/code/Magento/Fedex/Test/Unit/Model/CarrierTest.php
@@ -3,7 +3,7 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\Fedex\Model;
+namespace Magento\Fedex\Test\Unit\Model;
 
 use Magento\Framework\Object;
 
