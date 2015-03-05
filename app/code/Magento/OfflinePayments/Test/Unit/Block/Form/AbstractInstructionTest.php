3ce0af74a5f4d17a43d0d9ac0323610991ebff87
Dale Sikkema <dsikkema@ebay.com>
2015-03-04 22:59:27 -0600

    MAGETWO-34323: Move Magento/OfflinePayments module unit tests

--- app/code/Magento/OfflinePayments/Test/Unit/Block/Form/AbstractInstructionTest.php
+++ app/code/Magento/OfflinePayments/Test/Unit/Block/Form/AbstractInstructionTest.php
@@ -3,7 +3,7 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\OfflinePayments\Block\Form;
+namespace Magento\OfflinePayments\Test\Unit\Block\Form;
 
 class AbstractInstructionTest extends \PHPUnit_Framework_TestCase
 {
