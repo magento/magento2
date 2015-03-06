484bb7b6ec822b0bd293dd54cbc662caa06a13c4
Joan He <joan@x.com>
2015-03-06 14:53:45 -0600

    MAGETWO-34327: fixing any static test failure
    - fixed testConstructorIntegrity failure

--- app/code/Magento/Sales/Test/Unit/Model/Email/TemplateTest.php
+++ app/code/Magento/Sales/Test/Unit/Model/Email/TemplateTest.php
@@ -51,7 +51,7 @@ class TemplateTest extends \PHPUnit_Framework_TestCase
         $this->mockViewFilesystem->expects($this->once())
             ->method('getTemplateFileName')
             ->with('template')
-            ->will($this->returnValue(__DIR__ . '/_files/test_include.php'));
+            ->will($this->returnValue(__DIR__ . '/_files/test_include.phtml'));
         $include = $this->template->getInclude('template', ['one' => 1, 'two' => 2]);
         $this->assertEquals('Number One = 1. Number Two = 2', $include);
     }
