d6df1285bca062956a1279b2bc930e40f3c417ce
Maddy Chellathurai <mchellathura@ebay.com>
2015-03-06 11:54:30 -0600

    MAGETWO-34657: Implement config options for 'resource' segment
    - reverting debugging echo

--- lib/internal/Magento/Framework/App/Bootstrap.php
+++ lib/internal/Magento/Framework/App/Bootstrap.php
@@ -407,7 +407,6 @@ class Bootstrap
             echo $e;
         } else {
             $message = "An error has happened during application run. See debug log for details.\n";
-            echo $e;
             try {
                 if (!$this->objectManager) {
                     throw new \DomainException();
