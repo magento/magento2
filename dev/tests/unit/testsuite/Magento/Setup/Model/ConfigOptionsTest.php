33b114ba385e71e62b7ab6e8810e3390f71a065b
Maddy Chellathurai <mchellathura@ebay.com>
2015-03-09 09:54:06 -0500

    MAGETWO-34855: Split createConfig in ConfigOptions into smaller functions
    - fixing tests

--- dev/tests/unit/testsuite/Magento/Setup/Model/ConfigOptionsTest.php
+++ dev/tests/unit/testsuite/Magento/Setup/Model/ConfigOptionsTest.php
@@ -19,7 +19,7 @@ class ConfigOptionsTest extends \PHPUnit_Framework_TestCase
 
     protected function setUp()
     {
-        $this->generator = $this->getMock('\Magento\Setup\Model\ConfigDataGenerator', [], [], '', false);
+        $this->generator = $this->getMock('Magento\Setup\Model\ConfigDataGenerator', [], [], '', false);
         $this->object = new ConfigOptions($this->generator);
     }
 
@@ -27,27 +27,53 @@ class ConfigOptionsTest extends \PHPUnit_Framework_TestCase
     {
         $options = $this->object->getOptions();
         $this->assertInstanceOf('Magento\Framework\Setup\Option\TextConfigOption', $options[0]);
+        $this->assertSame('Encryption key', $options[0]->getDescription());
         $this->assertInstanceOf('Magento\Framework\Setup\Option\SelectConfigOption', $options[1]);
+        $this->assertSame('Session save location', $options[1]->getDescription());
         $this->assertInstanceOf('Magento\Framework\Setup\Option\SelectConfigOption', $options[2]);
+        $this->assertSame('Type of definitions used by Object Manager', $options[2]->getDescription());
         $this->assertInstanceOf('Magento\Framework\Setup\Option\TextConfigOption', $options[3]);
+        $this->assertSame('Database server host', $options[3]->getDescription());
         $this->assertInstanceOf('Magento\Framework\Setup\Option\TextConfigOption', $options[4]);
+        $this->assertSame('Database name', $options[4]->getDescription());
         $this->assertInstanceOf('Magento\Framework\Setup\Option\TextConfigOption', $options[5]);
+        $this->assertSame('Database server username', $options[5]->getDescription());
         $this->assertInstanceOf('Magento\Framework\Setup\Option\TextConfigOption', $options[6]);
+        $this->assertSame('Database server password', $options[6]->getDescription());
         $this->assertInstanceOf('Magento\Framework\Setup\Option\TextConfigOption', $options[7]);
+        $this->assertSame('Database table prefix', $options[7]->getDescription());
         $this->assertInstanceOf('Magento\Framework\Setup\Option\TextConfigOption', $options[8]);
+        $this->assertSame('Database type', $options[8]->getDescription());
         $this->assertInstanceOf('Magento\Framework\Setup\Option\TextConfigOption', $options[9]);
+        $this->assertSame('Database  initial set of commands', $options[9]->getDescription());
         $this->assertEquals(10, count($options));
     }
 
     public function testCreateOptions()
     {
-        $this->generator->expects($this->once())->method('createInstallConfig');
-        $this->generator->expects($this->once())->method('createCryptConfig');
-        $this->generator->expects($this->once())->method('createModuleConfig');
-        $this->generator->expects($this->once())->method('createSessionConfig');
-        $this->generator->expects($this->once())->method('createDefinitionsConfig');
-        $this->generator->expects($this->once())->method('createDbConfig');
-        $this->generator->expects($this->once())->method('createResourceConfig');
-        $this->object->createConfig([]);
+        $configDataMock = $this->getMock('Magento\Framework\Config\Data\ConfigData', [], [], '', false);
+        $this->generator->expects($this->once())->method('createInstallConfig')->willReturn($configDataMock);
+        $this->generator->expects($this->once())->method('createCryptConfig')->willReturn($configDataMock);
+        $this->generator->expects($this->once())->method('createModuleConfig')->willReturn($configDataMock);
+        $this->generator->expects($this->once())->method('createSessionConfig')->willReturn($configDataMock);
+        $this->generator->expects($this->once())->method('createDefinitionsConfig')->willReturn($configDataMock);
+        $this->generator->expects($this->once())->method('createDbConfig')->willReturn($configDataMock);
+        $this->generator->expects($this->once())->method('createResourceConfig')->willReturn($configDataMock);
+        $configData = $this->object->createConfig([]);
+        $this->assertEquals(7 ,count($configData));
+    }
+
+    public function testCreateOptionsWithOptionalNull()
+    {
+        $configDataMock = $this->getMock('Magento\Framework\Config\Data\ConfigData', [], [], '', false);
+        $this->generator->expects($this->once())->method('createInstallConfig')->willReturn($configDataMock);
+        $this->generator->expects($this->once())->method('createCryptConfig')->willReturn($configDataMock);
+        $this->generator->expects($this->once())->method('createModuleConfig')->willReturn(null);
+        $this->generator->expects($this->once())->method('createSessionConfig')->willReturn($configDataMock);
+        $this->generator->expects($this->once())->method('createDefinitionsConfig')->willReturn(null);
+        $this->generator->expects($this->once())->method('createDbConfig')->willReturn($configDataMock);
+        $this->generator->expects($this->once())->method('createResourceConfig')->willReturn($configDataMock);
+        $configData = $this->object->createConfig([]);
+        $this->assertEquals(5 ,count($configData));
     }
 }
