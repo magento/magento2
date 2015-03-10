c272934f2f53b76d62cdcffd5f6bf1994beb745e
Robert He <rohe@ebay.com>
2015-03-10 16:31:15 -0500

    MAGETWO-33704: FPT is not applied in shopping cart and order for registered user
     - FPT for item shows up properly in orders, invoices, and credit memos
     - replaced the fieldsetId of 'sales_convert_quote_item' to 'quote_convert_item' when doing a conversion

--- app/code/Magento/Weee/Model/Total/Invoice/Weee.php
+++ app/code/Magento/Weee/Model/Total/Invoice/Weee.php
@@ -104,14 +104,13 @@ class Weee extends \Magento\Sales\Model\Order\Invoice\Total\AbstractTotal
             $newApplied = [];
             $applied = $this->_weeeData->getApplied($orderItem);
             foreach ($applied as $one) {
-                $title = $one['title'];
-                $one['base_row_amount'] = $invoice->roundPrice($one['base_row_amount'] * $ratio, $title.'_base');
-                $one['row_amount'] = $invoice->roundPrice($one['row_amount'] * $ratio, $title);
+                $one['base_row_amount'] = $invoice->roundPrice($one['base_row_amount'] * $ratio, 'base');
+                $one['row_amount'] = $invoice->roundPrice($one['row_amount'] * $ratio, 'regular');
                 $one['base_row_amount_incl_tax'] = $invoice->roundPrice(
                     $one['base_row_amount_incl_tax'] * $ratio,
-                    $title.'_base'
+                    'including_base'
                 );
-                $one['row_amount_incl_tax'] = $invoice->roundPrice($one['row_amount_incl_tax'] * $ratio, $title);
+                $one['row_amount_incl_tax'] = $invoice->roundPrice($one['row_amount_incl_tax'] * $ratio, 'including');
 
                 $newApplied[] = $one;
             }
