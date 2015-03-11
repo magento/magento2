a361ba37182434e975618edd97fe660d8e7fba4e
Sergey Semenov <ssemenov@ebay.com>
2015-03-11 18:00:46 +0200

    MAGETWO-16837: Guest can't add product to Wishlist while registering.

--- app/code/Magento/Customer/Controller/AccountRedirect.php
+++ app/code/Magento/Customer/Controller/AccountRedirect.php
@@ -1,5 +1,8 @@
 <?php
-
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
 namespace Magento\Customer\Controller;
 
 use Magento\Framework\App\Action\Context;
