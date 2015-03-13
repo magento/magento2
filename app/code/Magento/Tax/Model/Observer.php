42d3052678432e4eaa4780f1e598ef37c2ae87d5
Mike Weis <miweis@ebay.com>
2015-03-13 11:04:56 -0500

    MAGETWO-28386: Displaying wrong price on product page when selecting an option for configurable product (with FPC disabled)
     - fixed

--- app/code/Magento/Tax/Model/Observer.php
+++ app/code/Magento/Tax/Model/Observer.php
@@ -311,10 +311,17 @@ class Observer
         if ($this->_taxData->displayBothPrices()) {
             $options['optionTemplate'] = sprintf(
                 '<%%= data.label %%>'
-                . '<%% if(data.finalPrice.value) { %%>'
+                . '<%% if (data.finalPrice.value) { %%>'
                 . ' <%%= data.finalPrice.formatted %%> (%1$s <%%= data.basePrice.formatted %%>)'
                 . '<%% } %%>',
                 __('Excl. tax:')
+            );
+        } elseif ($this->_taxData->priceIncludesTax() && $this->_taxData->displayPriceExcludingTax()) {
+            $options['optionTemplate'] = sprintf(
+                '<%%= data.label %%>'
+                . '<%% if (data.basePrice.value) { %%>'
+                . ' <%%= data.basePrice.formatted %%>'
+                . '<%% } %%>'
             );
         }
 
