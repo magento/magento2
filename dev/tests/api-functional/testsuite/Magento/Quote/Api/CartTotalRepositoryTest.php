2b1fba59ab3fd8e6b06d0ed791f4b34b02846f84
Sviatoslav Mankivskyi <smankivskyi@ebay.com>
2015-03-13 11:50:15 +0200

    MAGETWO-34390: Stabilization of replacing Zend_Locale with Native PHP Implementation

--- dev/tests/api-functional/testsuite/Magento/Quote/Api/CartTotalRepositoryTest.php
+++ dev/tests/api-functional/testsuite/Magento/Quote/Api/CartTotalRepositoryTest.php
@@ -149,7 +149,6 @@ class CartTotalRepositoryTest extends WebapiAbstract
      */
     protected function getQuoteItemTotalsData(\Magento\Quote\Model\Quote $quote)
     {
-        $this->markTestSkipped('Temporary');
         $items = $quote->getAllItems();
         $item = array_shift($items);
 
