18a48b371a64320fd9d522ed9ff0468df2fe6dc4
Bryant Luk <bluk@ebay.com>
2015-03-14 13:04:35 -0500

    MAGETWO-34952: Final cleanup to ensure Core module is properly removed
    - Fix namespace for DesignLoaderTest

--- lib/internal/Magento/Framework/View/Test/Unit/DesignLoaderTest.php
+++ lib/internal/Magento/Framework/View/Test/Unit/DesignLoaderTest.php
@@ -3,7 +3,7 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\Framework\View\Test\Unit\Model;
+namespace Magento\Framework\View\Test\Unit;
 
 class DesignLoaderTest extends \PHPUnit_Framework_TestCase
 {
