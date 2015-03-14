7f8f0625a087144387d096462a691f1a43539492
okarpenko <okarpenko@ebay.com>
2015-03-14 16:50:23 +0200

    MAGETWO-34293: Html minification works incorrectly in Layered Navigation
    - fixed test

--- lib/internal/Magento/Framework/View/Test/Unit/Design/FileResolution/Fallback/TemplateFileTest.php
+++ lib/internal/Magento/Framework/View/Test/Unit/Design/FileResolution/Fallback/TemplateFileTest.php
@@ -9,6 +9,7 @@ namespace Magento\Framework\View\Test\Unit\Design\FileResolution\Fallback;
 use Magento\Framework\App\State;
 use Magento\Framework\View\Design\Fallback\RulePool;
 use Magento\Framework\View\Design\FileResolution\Fallback\TemplateFile;
+use Magento\Framework\View\Design\FileResolution\Fallback\ResolverInterface;
 
 class TemplateFileTest extends \PHPUnit_Framework_TestCase
 {
