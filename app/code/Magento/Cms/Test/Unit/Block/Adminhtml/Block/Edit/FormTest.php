e8819b835ac5953e759dfaf0b5ba256daa7cce6a
vpaladiychuk <vpaladiychuk@ebay.com>
2015-03-15 15:30:01 +0200

    MAGETWO-34878: PR and cleanup

--- app/code/Magento/Cms/Test/Unit/Block/Adminhtml/Block/Edit/FormTest.php
+++ app/code/Magento/Cms/Test/Unit/Block/Adminhtml/Block/Edit/FormTest.php
@@ -3,7 +3,7 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\Cms\Test\Unit\Block\Adminhtml\Block\Block\Edit;
+namespace Magento\Cms\Test\Unit\Block\Adminhtml\Block\Edit;
 
 /**
  * @SuppressWarnings(PHPMD.TooManyFields)
