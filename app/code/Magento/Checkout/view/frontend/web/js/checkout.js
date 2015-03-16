e0d4ba09de37eeac493a0a40df0940ae8a20342a
Anton Kril <akril@ebay.com>
2015-03-16 17:05:22 +0200

    MAGETWO-35069: Investigate basic Js solution

--- app/code/Magento/Checkout/view/frontend/web/js/checkout.js
+++ app/code/Magento/Checkout/view/frontend/web/js/checkout.js
@@ -6,20 +6,43 @@
  */
 /*jshint browser:true jquery:true*/
 /*global alert*/
+/**
+ * {license_notice}
+ *
+ * @copyright   {copyright}
+ * @license     {license_link}
+ */
+/*jshint browser:true jquery:true*/
+/*global alert*/
 define(
     [
+        'mage/view/composite',
         'mage/url',
+        'Magento_Ui/js/view/errors',
         './view/authentication',
-        'Magento_Customer/js/model/customer',
-        'mage/layout'
+        './view/billing-address',
+        './view/shipping-method',
+        './view/payment',
+        './view/review',
+        './view/progress',
+        'Magento_Customer/js/model/customer'
     ],
-    function(url, authentication, customer, layout) {
-        return function (options, hostElement) {
+    function(composite, url, errors, authentication, billingAddress, shipping, payment, review, progress, customer) {
+        var first = composite();
+        var accordion = composite();
+        accordion.addChild(authentication, 'authentication');
+        accordion.addChild(billingAddress, 'billingAddress');
+        accordion.addChild(shipping, 'shipping');
+        accordion.addChild(payment, 'payment');
+        accordion.addChild(review, 'review');
+        first.addChild(errors);
+        first.addChild(progress, 'progress');
+        first.addChild(accordion, 'accordion');
+        return function(options, element) {
             authentication.setFormKey(options.formKey);
             url.setBaseUrl(options.baseUrl);
             customer.setIsLoggedIn(options.isLoggedIn);
-            var root = layout.build(options.layout);
-            return root.render(hostElement);
-        };
+            return first.render(element);
+        }
     }
 );
