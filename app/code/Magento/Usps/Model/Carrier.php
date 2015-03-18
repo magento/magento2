f555e5b3ca658cf4c17daa7393cc46430aa0f786
Iryna Savchenko <isavchenko@ebay.com>
2015-03-18 12:03:56 +0200

    MAGETWO-35076: Impossible to place order with DHL EU shipping method
    - CR Changes

--- app/code/Magento/Usps/Model/Carrier.php
+++ app/code/Magento/Usps/Model/Carrier.php
@@ -512,7 +512,7 @@ class Carrier extends AbstractCarrierOnline implements \Magento\Shipping\Model\C
                         $response
                     );
                 }
-                $xml = $this->parseXml($response, 'Magento\Shipping\Model\Simplexml\Element');
+                $xml = $this->parseXml($response);
 
                 if (is_object($xml)) {
                     $allowedMethods = explode(',', $this->getConfigData('allowed_methods'));
@@ -1042,7 +1042,7 @@ class Carrier extends AbstractCarrierOnline implements \Magento\Shipping\Model\C
         $resultArr = [];
         if (strlen(trim($response)) > 0) {
             if (strpos(trim($response), '<?xml') === 0) {
-                $xml = $this->parseXml($response, 'Magento\Shipping\Model\Simplexml\Element');
+                $xml = $this->parseXml($response);
                 if (is_object($xml)) {
                     if (isset($xml->Number) && isset($xml->Description) && (string)$xml->Description != '') {
                         $errorTitle = (string)$xml->Description;
@@ -1869,7 +1869,7 @@ class Carrier extends AbstractCarrierOnline implements \Magento\Shipping\Model\C
         $client->setParameterGet('XML', $requestXml);
         $response = $client->request()->getBody();
 
-        $response = $this->parseXml($response, 'Magento\Shipping\Model\Simplexml\Element');
+        $response = $this->parseXml($response);
         if ($response === false || $response->getName() == 'Error') {
             $debugData['result'] = [
                 'error' => $response->Description,
