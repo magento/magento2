66f3b638db357a89af26998f110a18ec5f9d677b
Denys Rul <drul@ebay.com>
2015-03-19 14:48:26 +0200

    MAGETWO-31654: Magento Ui module code base clean up
    - Add copyright comments

--- app/code/Magento/Ui/view/base/web/js/lib/loader.js
+++ app/code/Magento/Ui/view/base/web/js/lib/loader.js
@@ -5,10 +5,8 @@
 define([
     'underscore',
     'jquery'
-], function(_, $) {
+], function (_, $) {
     'use strict';
-    
-    var storage = window.localStorage;
 
     /**
      * Formats path of type "path.to.template" to RequireJS compatible
@@ -20,35 +18,26 @@ define([
     }
 
     /**
-     * Waits for all items in passed array of promises to resolve.
-     * @param  {Array} promises - array of promises
-     * @return {Deferred} - promise of promises to resolve
-     */
-    function waitFor(promises) {
-        return $.when.apply(this, promises);
-    }
-
-    /**
      * Removes license from incoming template
-     * 
+     *
      * @param  {String} tmpl
      * @return {String} - template without license
      */
-    function removeLicense(tmpl){
+    function removeLicense(tmpl) {
         var regEx = /<!--[\s\S]*?-->/;
 
-        return tmpl.replace(regEx, function(match){
+        return tmpl.replace(regEx, function (match) {
             return ~match.indexOf('/**') ? '' : match;
         });
     }
 
     /**
      * Loads template by path, resolves promise with it if defined
-     * 
+     *
      * @param  {String} path
      * @param  {Deferred} promise
      */
-    function load(path, promise){
+    function load(path, promise) {
         require([path], function (template) {
             template = removeLicense(template);
 
@@ -61,12 +50,11 @@ define([
          * Loops over arguments and loads template for each.
          * @return {Deferred} - promise of templates to be loaded
          */
-        loadTemplate: function() {
-            var isLoaded    = $.Deferred(),
-                templates   = _.toArray(arguments);
+        loadTemplate: function (source) {
+            var isLoaded = $.Deferred();
 
-            waitFor(templates.map(this._loadTemplate)).done(function () {
-                isLoaded.resolve.apply(isLoaded, arguments);
+            this._loadTemplate(source).done(function (tmpl) {
+                isLoaded.resolve(tmpl);
             });
 
             return isLoaded.promise();
@@ -74,17 +62,17 @@ define([
 
         /**
          * Loads template by it's name
-         * 
+         *
          * @param  {String} name
          * @return {Deferred}
          */
         _loadTemplate: function (name) {
-            var isLoaded    = $.Deferred(),
-                path        = formatTemplatePath(name);
+            var isLoaded = $.Deferred(),
+                path = formatTemplatePath(name);
 
             load(path, isLoaded);
 
             return isLoaded.promise();
         }
-    }
+    };
 });
