bc9c5836b83db624c2cad90d176b77652e5271d0
Volodymyr Kholoshenko <vkholoshenko@ebay.com>
2015-03-19 17:48:47 +0200

    MAGETWO-35242: [IE] A product image can be added only after double click

--- app/code/Magento/Catalog/Controller/Adminhtml/Product/Gallery/Upload.php
+++ app/code/Magento/Catalog/Controller/Adminhtml/Product/Gallery/Upload.php
@@ -11,20 +11,20 @@ use Magento\Framework\App\Filesystem\DirectoryList;
 class Upload extends \Magento\Backend\App\Action
 {
     /**
-     * @var \Magento\Framework\Controller\Result\JsonFactory
+     * @var \Magento\Framework\Controller\Result\RawFactory
      */
-    protected $resultJsonFactory;
+    protected $resultRawFactory;
 
     /**
      * @param \Magento\Backend\App\Action\Context $context
-     * @param \Magento\Framework\Controller\Result\JsonFactory $resultJsonFactory
+     * @param \Magento\Framework\Controller\Result\RawFactory $resultRawFactory
      */
     public function __construct(
         \Magento\Backend\App\Action\Context $context,
-        \Magento\Framework\Controller\Result\JsonFactory $resultJsonFactory
+        \Magento\Framework\Controller\Result\RawFactory $resultRawFactory
     ) {
         parent::__construct($context);
-        $this->resultJsonFactory = $resultJsonFactory;
+        $this->resultRawFactory = $resultRawFactory;
     }
 
     /**
@@ -72,6 +72,10 @@ class Upload extends \Magento\Backend\App\Action
             $result = ['error' => $e->getMessage(), 'errorcode' => $e->getCode()];
         }
 
-        return $this->resultJsonFactory->create()->setData($result);
+        /** @var \Magento\Framework\Controller\Result\Raw $response */
+        $response = $this->resultRawFactory->create();
+        $response->setHeader('Content-type', 'text/plain');
+        $response->setContents(json_encode($result));
+        return $response;
     }
 }
