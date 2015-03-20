d0f64210e14a83c3a72521b90d1e4e5acf7f85f1
Ivan Gavryshko <igavryshko@ebay.com>
2015-03-20 14:58:01 -0500

    MAGETWO-35133: Search for Commands
    - removed unnesesary lines

--- lib/internal/Magento/Framework/Console/Cli.php
+++ lib/internal/Magento/Framework/Console/Cli.php
@@ -61,9 +61,7 @@ class Cli extends SymfonyApplication
 
         if ($objectManager->get('Magento\Framework\App\DeploymentConfig')->isAvailable()) {
             $commandList = $objectManager->create('Magento\Framework\Console\CommandList');
-
             $modulesCommands = $commandList->getCommands();
-
         }
 
         $commandsList = array_merge(
