af3fa3f8f8a1c8d1837a0473fc022a2526724d6b
James Anelay <jamesanelay@gmail.com>
2015-03-24 22:04:26 +0000

    Make topmenu use .phtml to render html

--- app/code/Magento/Theme/Block/Html/Topmenu.php
+++ app/code/Magento/Theme/Block/Html/Topmenu.php
@@ -42,11 +42,11 @@ class Topmenu extends Template implements IdentityInterface
      *
      * @param string $outermostClass
      * @param string $childrenWrapClass
-     * @param int $limit
      * @return string
      */
-    public function getHtml($outermostClass = '', $childrenWrapClass = '', $limit = 0)
+    public function getHtml($outermostClass = '', $childrenWrapClass = '')
     {
+
         $this->_eventManager->dispatch(
             'page_block_html_topmenu_gethtml_before',
             ['menu' => $this->_menu, 'block' => $this]
@@ -55,9 +55,17 @@ class Topmenu extends Template implements IdentityInterface
         $this->_menu->setOutermostClass($outermostClass);
         $this->_menu->setChildrenWrapClass($childrenWrapClass);
 
-        $html = $this->_getHtml($this->_menu, $childrenWrapClass, $limit);
+        $renderer = $this->getChildBlock('catalog.topnav.renderer');
+
+        if(!$renderer){
+            $renderer = $this->getLayout()->createBlock('Magento\Theme\Block\Html\Topmenu\Renderer');
+        }
+
+        $renderer->setMenuTree($this->_menu)->setChildrenWrapClass($childrenWrapClass);
+        $html = $renderer->toHtml();
 
         $transportObject = new \Magento\Framework\Object(['html' => $html]);
+
         $this->_eventManager->dispatch(
             'page_block_html_topmenu_gethtml_after',
             ['menu' => $this->_menu, 'transportObject' => $transportObject]
@@ -129,109 +137,12 @@ class Topmenu extends Template implements IdentityInterface
     }
 
     /**
-     * Add sub menu HTML code for current menu item
-     *
-     * @param \Magento\Framework\Data\Tree\Node $child
-     * @param string $childLevel
-     * @param string $childrenWrapClass
-     * @param int $limit
-     * @return string HTML code
-     */
-    protected function _addSubMenu($child, $childLevel, $childrenWrapClass, $limit)
-    {
-        $html = '';
-        if (!$child->hasChildren()) {
-            return $html;
-        }
-
-        $colStops = null;
-        if ($childLevel == 0 && $limit) {
-            $colStops = $this->_columnBrake($child->getChildren(), $limit);
-        }
-
-        $html .= '<ul class="level' . $childLevel . ' submenu">';
-        $html .= $this->_getHtml($child, $childrenWrapClass, $limit, $colStops);
-        $html .= '</ul>';
-
-        return $html;
-    }
-
-    /**
-     * Recursively generates top menu html from data that is specified in $menuTree
-     *
-     * @param \Magento\Framework\Data\Tree\Node $menuTree
-     * @param string $childrenWrapClass
-     * @param int $limit
-     * @param array $colBrakes
-     * @return string
-     *
-     * @SuppressWarnings(PHPMD.CyclomaticComplexity)
-     * @SuppressWarnings(PHPMD.NPathComplexity)
-     */
-    protected function _getHtml(
-        \Magento\Framework\Data\Tree\Node $menuTree,
-        $childrenWrapClass,
-        $limit,
-        $colBrakes = []
-    ) {
-        $html = '';
-
-        $children = $menuTree->getChildren();
-        $parentLevel = $menuTree->getLevel();
-        $childLevel = $parentLevel === null ? 0 : $parentLevel + 1;
-
-        $counter = 1;
-        $itemPosition = 1;
-        $childrenCount = $children->count();
-
-        $parentPositionClass = $menuTree->getPositionClass();
-        $itemPositionClassPrefix = $parentPositionClass ? $parentPositionClass . '-' : 'nav-';
-
-        foreach ($children as $child) {
-            $child->setLevel($childLevel);
-            $child->setIsFirst($counter == 1);
-            $child->setIsLast($counter == $childrenCount);
-            $child->setPositionClass($itemPositionClassPrefix . $counter);
-
-            $outermostClassCode = '';
-            $outermostClass = $menuTree->getOutermostClass();
-
-            if ($childLevel == 0 && $outermostClass) {
-                $outermostClassCode = ' class="' . $outermostClass . '" ';
-                $child->setClass($outermostClass);
-            }
-
-            if (count($colBrakes) && $colBrakes[$counter]['colbrake']) {
-                $html .= '</ul></li><li class="column"><ul>';
-            }
-
-            $html .= '<li ' . $this->_getRenderedMenuItemAttributes($child) . '>';
-            $html .= '<a href="' . $child->getUrl() . '" ' . $outermostClassCode . '><span>' . $this->escapeHtml(
-                $child->getName()
-            ) . '</span></a>' . $this->_addSubMenu(
-                $child,
-                $childLevel,
-                $childrenWrapClass,
-                $limit
-            ) . '</li>';
-            $itemPosition++;
-            $counter++;
-        }
-
-        if (count($colBrakes) && $limit) {
-            $html = '<li class="column"><ul>' . $html . '</ul></li>';
-        }
-
-        return $html;
-    }
-
-    /**
      * Generates string with all attributes that should be present in menu item element
      *
      * @param \Magento\Framework\Data\Tree\Node $item
      * @return string
      */
-    protected function _getRenderedMenuItemAttributes(\Magento\Framework\Data\Tree\Node $item)
+    public function _getRenderedMenuItemAttributes(\Magento\Framework\Data\Tree\Node $item)
     {
         $html = '';
         $attributes = $this->_getMenuItemAttributes($item);
---
+++ app/code/Magento/Theme/Block/Html/Topmenu/Renderer.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Theme\Block\Html\Topmenu;
+
+use Magento\Theme\Block\Html\Topmenu;
+
+class Renderer extends Topmenu
+{
+    protected $_template = 'html/topmenu/renderer.phtml';
+
+    protected function _toHtml()
+    {
+        $menuTree = $this->getMenuTree();
+        $childrenWrapClass = $this->getChildrenWrapClass();
+
+        if(!$this->getTemplate() || is_null($menuTree) || is_null($childrenWrapClass)) {
+            throw new \Magento\Framework\Exception("Top-menu renderer isn't fully configured.");
+        }
+
+        if (!$this->isTemplateFileValid($this->getTemplateFile())) {
+            throw new \Magento\Framework\Exception('Not valid template file:' . $this->_templateFile);
+        }
+
+        return $this->render($menuTree,$childrenWrapClass);
+    }
+
+    public function render($menuTree,$childrenWrapClass,$limit=0,$colBreaks = [])
+    {
+        $this->assign('menuTree',$menuTree);
+        $this->assign('childrenWrapClass',$childrenWrapClass);
+        $this->assign('colBrakes',$colBreaks);
+        $this->assign('limit',$limit);
+
+        return $this->fetchView($this->getTemplateFile());
+    }
+
+    /**
+     * Add sub menu HTML code for current menu item
+     *
+     * @param \Magento\Framework\Data\Tree\Node $child
+     * @param string $childLevel
+     * @param string $childrenWrapClass
+     * @param int $limit
+     * @return string HTML code
+     */
+    public function _addSubMenu($child, $childLevel, $childrenWrapClass, $limit)
+    {
+        $html = '';
+        if (!$child->hasChildren()) {
+            return $html;
+        }
+
+        $colStops = null;
+        if ($childLevel == 0 && $limit) {
+            $colStops = $this->_columnBrake($child->getChildren(), $limit);
+        }
+
+        $html .= '<ul class="level' . $childLevel . ' submenu">';
+        $html .= $this->render($child, $childrenWrapClass, $limit, $colStops);
+        $html .= '</ul>';
+
+        return $html;
+    }
+}
