7c994bf33e2e73d5c34daf2275901af2457ad3d5
Mykhailo Miroshnikov <mmiroshnikov@ebay.com>
2015-03-27 13:30:59 +0200

    MAGETWO-35118: JavaScript Unit Test Framework supports theme feature
     - Fix fallen test

--- dev/tests/js/jasmine/require.conf.js
+++ dev/tests/js/jasmine/require.conf.js
@@ -23,7 +23,7 @@ require.config({
             'dev/tests/js/jasmine/assets/jsbuild/local.js': 'define([], function () {\'use strict\'; return \'internal module\'; });'
         },
         text: {
-            'dev/tests/js/jasmine/assets/text/local.html': '<span>Local Template</span>'
+            'dev/tests/js/jasmine/assets/text/local.html': '<!--\n/**\n * Copyright © 2015 Magento. All rights reserved.\n * See COPYING.txt for license details.\n */\n-->\n<span>Local Template</span>'
         }
     },
     deps: [
