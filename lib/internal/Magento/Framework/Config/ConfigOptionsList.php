2e1ea3dad0da16e2e8fd45513d181bc81a7d21a4
Ivan Gavryshko <igavryshko@ebay.com>
2015-03-27 12:17:46 -0500

    MAGETWO-35137: Add deployment configuration set command
    - used path constand for db/table_prefix

--- lib/internal/Magento/Framework/Config/ConfigOptionsList.php
+++ lib/internal/Magento/Framework/Config/ConfigOptionsList.php
@@ -25,6 +25,7 @@ class ConfigOptionsList implements ConfigOptionsListInterface
     const CONFIG_PATH_DEFINITION_FORMAT = 'definition/format';
     const CONFIG_PATH_RESOURCE_DEFAULT_SETUP = 'resource/default_setup/connection';
     const CONFIG_PATH_DB_CONNECTION_DEFAULT = 'db/connection/default/';
+    const CONFIG_PATH_DB_PREFIX = 'db/table_prefix';
     /**#@-*/
 
     /**#@+
@@ -179,7 +180,7 @@ class ConfigOptionsList implements ConfigOptionsListInterface
             new TextConfigOption(
                 self::INPUT_KEY_DB_PREFIX,
                 TextConfigOption::FRONTEND_WIZARD_TEXT,
-                self::CONFIG_KEY . '/' . self::KEY_PREFIX,
+                self::CONFIG_PATH_DB_PREFIX,
                 'Database table prefix'
             ),
             new TextConfigOption(
