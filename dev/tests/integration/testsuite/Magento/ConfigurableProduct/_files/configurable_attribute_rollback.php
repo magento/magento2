2acc8c178c9381cc18af63b325f0d782876f25db
valdislav <vshcherbyna@ebay.com>
2015-03-31 15:51:34 +0300

    MAGETWO-35141: Sequence component pull request processing

--- dev/tests/integration/testsuite/Magento/ConfigurableProduct/_files/configurable_attribute_rollback.php
+++ dev/tests/integration/testsuite/Magento/ConfigurableProduct/_files/configurable_attribute_rollback.php
@@ -1,11 +1,12 @@
 <?php
-/**
+/**git add 
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
+
 /** @var \Magento\Eav\Model\Config $eavConfig */
 $eavConfig = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->get('Magento\Eav\Model\Config');
-$attribute = $eavConfig->getAttribute('catalog_product', 'attribute_with_option');
+$attribute = $eavConfig->getAttribute('catalog_product', 'test_configurable');
 if ($attribute instanceof \Magento\Eav\Model\Entity\Attribute\AbstractAttribute
     && $attribute->getId()
 ) {
