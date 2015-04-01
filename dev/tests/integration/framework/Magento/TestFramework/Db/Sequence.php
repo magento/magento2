d0b0ed965f8da29a6a13307756457829df7a94fb
Anton Kaplya <akaplya@ebay.com>
2015-04-01 19:40:11 +0300

    MAGETWO-35141: Sequence component pull request processing

--- dev/tests/integration/framework/Magento/TestFramework/Db/Sequence.php
+++ dev/tests/integration/framework/Magento/TestFramework/Db/Sequence.php
@@ -1,10 +1,16 @@
 <?php
-
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
 namespace Magento\TestFramework\Db;
 
 use Magento\Framework\App\Resource as AppResource;
 use Magento\Framework\DB\Ddl\Sequence as DdlSequence;
 
+/**
+ * Class Sequence
+ */
 class Sequence
 {
     /**
