4987925b3d7f3e2572e705e44ee565cc8d73d599
Andriy Nasinnyk <anasinnyk@ebay.com>
2015-04-02 21:10:32 +0300

    MAGETWO-35656: \Magento\Framework\Stdlib\DateTime\DateTime methods malformed

--- lib/internal/Magento/Framework/Stdlib/DateTime/DateTime.php
+++ lib/internal/Magento/Framework/Stdlib/DateTime/DateTime.php
@@ -114,7 +114,7 @@ class DateTime
             return false;
         }
         $date = $this->_localeDate->date($result);
-        $timestamp = $date->getTimestamp() - $date->getTimezone()->getOffset($date);
+        $timestamp = $date->getTimestamp();
         unset($date);
         return $timestamp;
     }
