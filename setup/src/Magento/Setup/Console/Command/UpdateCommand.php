4ad555c4c6a86ec5e90c89766c9295fddedf7e31
Safwan Khan <safwkhan@paypal.com>
2015-04-03 15:07:55 -0500

    MAGETWO-35659: Move update command
    - Removed unused code.

--- setup/src/Magento/Setup/Console/Command/UpdateCommand.php
+++ setup/src/Magento/Setup/Console/Command/UpdateCommand.php
@@ -5,7 +5,6 @@
  */
 namespace Magento\Setup\Console\Command;
 
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Magento\Setup\Model\InstallerFactory;
