7fea0e5425b0019b890b5ab76c14b87b685bc274
Ievgen Shakhsuvarov <ishakhsuvarov@ebay.com>
2015-04-06 17:24:12 +0300

    MAGETWO-35811: New address form submission  - Error handling

--- app/code/Magento/Ui/view/frontend/web/js/view/errors.js
+++ app/code/Magento/Ui/view/frontend/web/js/view/errors.js
@@ -1,35 +1,10 @@
 /**
- * {license_notice}
- *
- * @copyright   {copyright}
- * @license     {license_link}
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
  */
-/*jshint browser:true jquery:true*/
-/*global alert*/
-/*
-define(
-    [
-        'jquery',
-        'text!./templates/errors.html',
-        '../model/errorlist'
-    ],
-    function($, template, errorlist) {
-        var list = $('<ul class="messages">', {'id': 'errors'});
-        wrapped = errorlist.add;
-        errorlist.add = function(error) {
-            wrapped(error);
-            list.append('<li class="error">' + error + '</li>');
-        }
-        return {
-            render: function (root) {
-                root.append(list);
-            }
-        }
-    }
-);
-*/
-
+/*global define*/
 define(['uiComponent', '../model/errorlist'], function (Component, errors) {
+    "use strict";
     return Component.extend({
         errorList: errors.getAll(),
         defaults: {
