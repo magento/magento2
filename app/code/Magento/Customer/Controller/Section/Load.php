cf11c3311080017b8bc75da1ce96f0c7b65d1d8e
Michail Slabko <mslabko@ebay.com>
2015-04-07 18:05:17 +0300

    MAGETWO-34827: Private content blocks re-factoring
    - MAGETWO-35599: Magento\Wishlist\Block\Customer\Sidebar

--- app/code/Magento/Customer/Controller/Section/Load.php
+++ app/code/Magento/Customer/Controller/Section/Load.php
@@ -61,6 +61,8 @@ class Load extends \Magento\Framework\App\Action\Action
         /** @var \Magento\Framework\Controller\Result\Json $resultJson */
         $resultJson = $this->resultJsonFactory->create();
         try {
+            //temporary solution for load layout dependent data (Price render)
+            $this->_view->loadLayout();
             $sectionNames = $this->getRequest()->getParam('sections');
             $sectionNames = $sectionNames ? \explode(',', $sectionNames) : null;
 
--- app/code/Magento/Wishlist/Model/PrivateData/Section/Wishlist.php
+++ app/code/Magento/Wishlist/Model/PrivateData/Section/Wishlist.php
@@ -18,12 +18,20 @@ class Wishlist implements SectionSourceInterface
      */
     protected $wishlistHelper;
 
+    /** @var \Magento\Catalog\Model\Product\Image\View */
+    protected $productImageView;
+
     /**
      * @param \Magento\Wishlist\Helper\Data $wishlistHelper
      */
-    public function __construct(\Magento\Wishlist\Helper\Data $wishlistHelper)
-    {
+    public function __construct(
+        \Magento\Wishlist\Helper\Data $wishlistHelper,
+        \Magento\Wishlist\Block\Customer\Sidebar $block,
+        \Magento\Catalog\Model\Product\Image\View $productImageView
+    ) {
         $this->wishlistHelper = $wishlistHelper;
+        $this->productImageView = $productImageView;
+        $this->block = $block;
     }
 
     /**
@@ -31,15 +39,17 @@ class Wishlist implements SectionSourceInterface
      */
     public function getData()
     {
+        $counter = $this->getCounter();
         return [
-            'counter' => $this->getCounter(),
+            'counter' => $counter,
+            'items' => $counter ? $this->getItems() : [],
         ];
     }
 
     /**
      * @return string
      */
-    public function getCounter()
+    protected function getCounter()
     {
         return $this->createCounter($this->getItemCount());
     }
@@ -68,5 +78,43 @@ class Wishlist implements SectionSourceInterface
             return __('1 item');
         }
         return null;
+    }
+
+    /**
+     * Get wishlist items
+     *
+     * @return array
+     */
+    protected function getItems()
+    {
+        $collection = $this->wishlistHelper->getWishlistItemCollection();
+        $collection->clear()->setCurPage(1)->setPageSize(3)->setInStockFilter(true)->setOrder('added_at');
+        $items = [];
+        foreach ($collection as $wishlistItem) {
+            /** @var \Magento\Catalog\Model\Product $product */
+            $product = $wishlistItem->getProduct();
+            $this->productImageView->init($product, 'wishlist_sidebar_block', 'Magento_Catalog');
+            $items[] = [
+                'image' => [
+                    'src' => $this->productImageView->getUrl(),
+                    'alt' => $this->productImageView->getLabel(),
+                    'width' => $this->productImageView->getWidth(),
+                    'height' => $this->productImageView->getHeight(),
+                ],
+                'product_url' => $this->block->getProductUrl($wishlistItem),
+                'product_name' => $product->getName(),
+                'product_price' => $this->block->getProductPriceHtml(
+                    $product,
+                    \Magento\Catalog\Pricing\Price\ConfiguredPriceInterface::CONFIGURED_PRICE_CODE,
+                    \Magento\Framework\Pricing\Render::ZONE_ITEM_LIST,
+                    ['item' => $wishlistItem]
+                ),
+                'product_is_saleable_and_visible' => $product->isSaleable() && $product->isVisibleInSiteVisibility(),
+                'product_has_required_options' => $product->getTypeInstance()->hasRequiredOptions($product),
+                'add_to_cart_params' => $this->wishlistHelper->getAddToCartParams($wishlistItem),
+                'delete_item_params' => $this->wishlistHelper->getRemoveParams($wishlistItem),
+            ];
+        }
+        return $items;
     }
 }
