a8f8e67b0a3a41ae1cbc567b1d6d7f1b66acd908
Alex Akimov <aakimov@ebay.com>
2015-04-09 17:43:14 +0300

    MAGETWO-35564: Shipping Address form

--- app/code/Magento/Checkout/view/frontend/web/js/action/select-billing-address.js
+++ app/code/Magento/Checkout/view/frontend/web/js/action/select-billing-address.js
@@ -9,13 +9,24 @@ define(
         '../model/quote',
         '../model/addresslist',
         '../model/step-navigator',
-        './select-shipping-address'
+        './select-shipping-address',
+        'uiRegistry'
     ],
-    function(quote, addressList, navigator, selectShippingAddress) {
+    function(quote, addressList, navigator, selectShippingAddress, registry) {
         "use strict";
         return function(billingAddress, useForShipping) {
             quote.setBillingAddress(billingAddress);
             if (useForShipping === '1' && !quote.isVirtual()) {
+                // update shipping address data in corresponding provider
+                var shippingAddressSource = registry.get('checkoutProvider');
+                var shippingAddress = shippingAddressSource.get('shippingAddress');
+                for (var property in billingAddress) {
+                    if (billingAddress.hasOwnProperty(property)
+                        && shippingAddress.hasOwnProperty(property)
+                    ) {
+                        shippingAddressSource.set('shippingAddress.' + property, billingAddress[property]);
+                    }
+                }
                 selectShippingAddress(billingAddress, useForShipping);
             } else {
                 navigator.setCurrent('billingAddress').goNext();
