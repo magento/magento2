351922e1ce95b450e457914e2ea4b6de483a5c4a
vpaladiychuk <vpaladiychuk@ebay.com>
2015-04-09 12:55:16 +0300

    MAGETWO-35584: Handle thrown exceptions which are never caught

--- app/code/Magento/Eav/Model/Entity/Increment/Alphanum.php
+++ app/code/Magento/Eav/Model/Entity/Increment/Alphanum.php
@@ -31,7 +31,7 @@ class Alphanum extends \Magento\Eav\Model\Entity\Increment\AbstractIncrement
      * Get next id
      *
      * @return string
-     * @throws \Magento\Eav\Exception
+     * @throws \Magento\Framework\Exception\LocalizedException
      */
     public function getNextId()
     {
@@ -52,7 +52,7 @@ class Alphanum extends \Magento\Eav\Model\Entity\Increment\AbstractIncrement
         for ($i = $lid; $i >= 0; $i--) {
             $p = strpos($chars, $lastId[$i]);
             if (false === $p) {
-                throw new \Magento\Eav\Exception(__('Invalid character encountered in increment ID: %1', $lastId));
+                throw new \Magento\Framework\Exception\LocalizedException(__('Invalid character encountered in increment ID: %1', $lastId));
             }
             if ($bumpNextChar) {
                 $p++;
