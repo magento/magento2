631eb217c0142b0cf7eedfcba43098c770861529
Alex Akimov <aakimov@ebay.com>
2015-04-10 14:12:25 +0300

    MAGETWO-35572: Guest flow for Checkout UI rendering in browser

--- app/code/Magento/Checkout/view/frontend/web/js/action/select-billing-address.js
+++ app/code/Magento/Checkout/view/frontend/web/js/action/select-billing-address.js
@@ -17,14 +17,16 @@ define(
         return function(billingAddress, useForShipping) {
             quote.setBillingAddress(billingAddress);
             if (useForShipping === '1' && !quote.isVirtual()) {
-                // update shipping address data in corresponding provider
-                var shippingAddressSource = registry.get('checkoutProvider');
-                var shippingAddress = shippingAddressSource.get('shippingAddress');
-                for (var property in billingAddress) {
-                    if (billingAddress.hasOwnProperty(property)
-                        && shippingAddress.hasOwnProperty(property)
-                    ) {
-                        shippingAddressSource.set('shippingAddress.' + property, billingAddress[property]);
+                if (!billingAddress.id) {
+                    // update shipping address data in corresponding provider
+                    var shippingAddressSource = registry.get('checkoutProvider');
+                    var shippingAddress = shippingAddressSource.get('shippingAddress');
+                    for (var property in billingAddress) {
+                        if (billingAddress.hasOwnProperty(property)
+                            && shippingAddress.hasOwnProperty(property)
+                        ) {
+                            shippingAddressSource.set('shippingAddress.' + property, billingAddress[property]);
+                        }
                     }
                 }
                 selectShippingAddress(billingAddress, useForShipping);
