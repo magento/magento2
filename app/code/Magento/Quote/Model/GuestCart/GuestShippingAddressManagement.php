20cfe4fd725aa782347fd243eb711e1233ac2c36
Anup Dugar <anup@x.com>
2015-04-14 18:55:29 -0500

    MAGETWO-32686: Customer-facing resources in Sales & Checkout APIs
    - Guest Services and APIs for Shipping Address Management

---
+++ app/code/Magento/Quote/Model/GuestCart/GuestShippingAddressManagement.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Quote\Model\GuestCart;
+
+use Magento\Quote\Api\ShippingAddressManagementInterface;
+use Magento\Quote\Model\QuoteAddressValidator;
+use Magento\Quote\Model\QuoteIdMask;
+use Magento\Quote\Model\QuoteIdMaskFactory;
+use Magento\Quote\Model\QuoteRepository;
+use Magento\Quote\Model\ShippingAddressManagement;
+use Psr\Log\LoggerInterface as Logger;
+
+/**
+ * Shipping address management management class for guest carts.
+ */
+class GuestShippingAddressManagement extends ShippingAddressManagement implements ShippingAddressManagementInterface
+{
+    /**
+     * @var QuoteIdMaskFactory
+     */
+    protected $quoteIdMaskFactory;
+
+    /**
+     * Constructs a quote shipping address write service object.
+     *
+     * @param QuoteRepository $quoteRepository
+     * @param QuoteAddressValidator $addressValidator
+     * @param Logger $logger
+     * @param QuoteIdMaskFactory $quoteIdMaskFactory
+     */
+    public function __construct(
+        \Magento\Quote\Model\QuoteRepository $quoteRepository,
+        QuoteAddressValidator $addressValidator,
+        Logger $logger,
+        QuoteIdMaskFactory $quoteIdMaskFactory
+    ) {
+        $this->quoteRepository = $quoteRepository;
+        $this->addressValidator = $addressValidator;
+        $this->logger = $logger;
+        $this->quoteIdMaskFactory = $quoteIdMaskFactory;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function assign($cartId, \Magento\Quote\Api\Data\AddressInterface $address)
+    {
+        /** @var $quoteIdMask QuoteIdMask */
+        $quoteIdMask = $this->quoteIdMaskFactory->create()->load($cartId, 'masked_id');
+        return parent::assign($quoteIdMask->getId(), $address);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function get($cartId)
+    {
+        /** @var $quoteIdMask QuoteIdMask */
+        $quoteIdMask = $this->quoteIdMaskFactory->create()->load($cartId, 'masked_id');
+        return parent::get($cartId);
+    }
+}
