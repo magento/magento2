e7a0988d0c69629fe29f1f653534349925f1d054
Alexander Paliarush <apaliarush@ebay.com>
2015-04-14 19:17:10 +0300

    MAGETWO-32686: Customer-facing resources in Sales & Checkout APIs
    - Create anonymous /guest-carts APIs for Magento\Quote\Api\PaymentMethodManagementInterface (MAGETWO-35843)

---
+++ app/code/Magento/Quote/Model/GuestCart/GuestPaymentMethodManagement.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Quote\Model\GuestCart;
+
+use Magento\Quote\Model\PaymentMethodManagement;
+use Magento\Quote\Api\GuestPaymentMethodManagementInterface;
+use Magento\Quote\Model\QuoteIdMask;
+use Magento\Quote\Model\QuoteIdMaskFactory;
+
+/**
+ * Class for guest cart payment method management.
+ */
+class GuestPaymentMethodManagement extends PaymentMethodManagement implements GuestPaymentMethodManagementInterface
+{
+    /**
+     * @var QuoteIdMaskFactory
+     */
+    protected $quoteIdMaskFactory;
+
+    /**
+     * Initialize dependencies.
+     *
+     * @param \Magento\Quote\Model\QuoteRepository $quoteRepository
+     * @param \Magento\Payment\Model\Checks\ZeroTotal $zeroTotalValidator
+     * @param \Magento\Payment\Model\MethodList $methodList
+     * @param QuoteIdMaskFactory $quoteIdMaskFactory
+     */
+    public function __construct(
+        \Magento\Quote\Model\QuoteRepository $quoteRepository,
+        \Magento\Payment\Model\Checks\ZeroTotal $zeroTotalValidator,
+        \Magento\Payment\Model\MethodList $methodList,
+        QuoteIdMaskFactory $quoteIdMaskFactory
+    ) {
+        parent::__construct($quoteRepository, $zeroTotalValidator, $methodList);
+        $this->quoteIdMaskFactory = $quoteIdMaskFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function set($cartId, \Magento\Quote\Api\Data\PaymentInterface $method)
+    {
+        /** @var $quoteIdMask QuoteIdMask */
+        $quoteIdMask = $this->quoteIdMaskFactory->create()->load($cartId, 'masked_id');
+        return parent::set($quoteIdMask->getId(), $method);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function get($cartId)
+    {
+        /** @var $quoteIdMask QuoteIdMask */
+        $quoteIdMask = $this->quoteIdMaskFactory->create()->load($cartId, 'masked_id');
+        return parent::get($quoteIdMask->getId());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getList($cartId)
+    {
+        /** @var $quoteIdMask QuoteIdMask */
+        $quoteIdMask = $this->quoteIdMaskFactory->create()->load($cartId, 'masked_id');
+        return parent::getList($quoteIdMask->getId());
+    }
+}
