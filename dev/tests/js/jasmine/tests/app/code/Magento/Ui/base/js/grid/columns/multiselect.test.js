41b5143a7417fd7d3924ebd8e8f91fe65fedbff7
Anton Guz <aguz@ebay.com>
2015-04-14 14:11:29 +0300

    MAGETWO-35095: Cover Multiselect Component with Unit Tests
     - All tests covered with test suit MAGETWO-35581

--- dev/tests/js/jasmine/tests/app/code/Magento/Ui/base/js/grid/columns/multiselect.test.js
+++ dev/tests/js/jasmine/tests/app/code/Magento/Ui/base/js/grid/columns/multiselect.test.js
@@ -4,50 +4,149 @@
  */
 
 define([
-    'ko',
-    'jquery',
     'underscore',
     'Magento_Ui/js/grid/columns/multiselect'
-], function (ko, $, _, Multiselect) {
+], function (_, Multiselect) {
     'use strict';
 
     describe('ui/js/grid/columns/multiselect', function () {
-        var observable,
-            element,
-            multiSelect,
-            items;
+        var multiSelect;
 
         beforeEach(function () {
-            element    = $('<input />');
-            observable = ko.observable();
             multiSelect = new Multiselect({
-                rows: [{
-                    id: 1
-                }],
+                rows: [],
+                index: 'index',
+                name: 'name',
                 indexField: 'id',
                 dataScope: 'scope',
                 provider: 'provider'
             });
-
-            $(document.body).append(element);
-
-            ko.applyBindingsToNode(element[0], { datepicker: observable });
+            multiSelect.source = {
+                set: function () {}
+            };
+            spyOn(multiSelect.source, 'set');
         });
 
         afterEach(function () {
-            element.remove();
         });
 
-        it('Default state - Deselected all', function () {
-            //items.page(1);
-            //multiSelect.selectPage();
-            expect(multiSelect.allSelected())
-            expect(multiSelect).toBe();
-            expect(element).toBeDefined();
+        it('Default state - Select no rows', function () {
+            multiSelect.rows.push({id:1});
+            multiSelect.rows.push({id:2});
+            multiSelect.rows.push({id:3});
+
+            expect(multiSelect.allSelected()).toBeFalsy();
+            expect(multiSelect.excluded()).toEqual([]);
+            expect(multiSelect.selected()).toEqual([]);
+            multiSelect.exportSelections();
+            //expect(multiSelect.source.set).toHaveBeenCalledWith([]);
+            expect(multiSelect.source.set.calls.argsFor(1))
+                .toEqual({ selected: [], total: undefined} );
         });
 
-        it('select specific several rows on several pages', function () {
-            expect(element).toBeDefined();
+        it('Select specific several rows on several pages', function () {
+            multiSelect.selected.push(4);
+            multiSelect.selected.push(5);
+
+            expect(multiSelect.allSelected()).toBeFalsy();
+            expect(multiSelect.excluded()).toEqual([]);
+            expect(multiSelect.selected()).toEqual([4,5]);
         });
+
+        it('Select all rows on several pages', function () {
+            multiSelect.rows([
+                {id:1},
+                {id:2}
+            ]);
+            multiSelect.selectPage();
+            multiSelect.rows([
+                {id:3},
+                {id:4}
+            ]);
+            multiSelect.selectPage();
+
+            expect(multiSelect.allSelected()).toBeFalsy();
+            expect(multiSelect.excluded()).toEqual([]);
+            expect(multiSelect.selected()).toEqual([1,2,3,4]);
+        });
+
+        it('Select all rows on current page with some specific rows on another page', function () {
+            multiSelect.rows([
+                {id:1},
+                {id:2}
+            ]);
+            //multiSelect.selectAll();
+            multiSelect.rows([
+                {id:3},
+                {id:4}
+            ]);
+            multiSelect.selectPage();
+            multiSelect.rows([
+                {id:5},
+                {id:6}
+            ]);
+            multiSelect.selected.push(6);
+
+            expect(multiSelect.allSelected()).toBeFalsy();
+            expect(multiSelect.excluded()).toEqual([]);
+            expect(multiSelect.selected()).toEqual([3,4,6]);
+        });
+
+        it('Select all rows on several pages without some specific rows', function () {
+            multiSelect.rows([
+                {id:1},
+                {id:2}
+            ]);
+            //multiSelect.selectAll();
+            multiSelect.rows([
+                {id:3},
+                {id:4}
+            ]);
+            multiSelect.selectPage();
+            multiSelect.selected.remove(4); // remove second
+
+            expect(multiSelect.allSelected()).toBeFalsy();
+            expect(multiSelect.excluded()).toEqual([]);
+            expect(multiSelect.selected()).toEqual([3]);
+        });
+
+        it('Select all rows all over the Grid', function () {
+            multiSelect.rows([
+                {id:1},
+                {id:2}
+            ]);
+            multiSelect.selectAll();
+            multiSelect.rows([
+                {id:3},
+                {id:4}
+            ]);
+
+            expect(multiSelect.allSelected()).toBeTruthy();
+            expect(multiSelect.excluded()).toEqual([]);
+            expect(multiSelect.selected()).toEqual([]);
+        });
+
+        it('Select all rows all over the Grid without all rows on current page but with specific rows on another page', function () {
+            multiSelect.rows([
+                {id:1},
+                {id:2}
+            ]);
+            multiSelect.rows([
+                {id:3},
+                {id:4}
+            ]);
+            multiSelect.selectAll();
+            multiSelect.deselectPage();
+            multiSelect.rows([
+                {id:5},
+                {id:6}
+            ]);
+            multiSelect.selected.push(6);
+
+            expect(multiSelect.allSelected()).toBeFalsy();
+            expect(multiSelect.excluded()).toEqual([3,4]);
+            expect(multiSelect.selected()).toEqual([]);
+        });
+
     });
 });
