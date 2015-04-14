571fe2596e574aaa0615f11602d5afc2f95c9da6
Bryant Luk <bluk@ebay.com>
2015-04-14 18:30:45 -0500

    MAGETWO-32686: Customer-facing resources in Sales & Checkout APIs
    - Add functionality for GuestCartTotalRepositoryInterface

---
+++ app/code/Magento/Quote/Api/GuestCartTotalRepositoryInterface.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Quote\Api;
+
+interface GuestCartTotalRepositoryInterface extends CartTotalRepositoryInterface
+{
+    /**
+     * Returns quote totals data for a specified cart.
+     *
+     * @param int $cartId The cart ID.
+     * @return \Magento\Quote\Api\Data\TotalsInterface Quote totals data.
+     * @throws \Magento\Framework\Exception\NoSuchEntityException The specified cart does not exist.
+     */
+    public function get($cartId);
+}
---
+++ app/code/Magento/Quote/Model/GuestCart/GuestCartTotalRepository.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Quote\Model\GuestCart;
+
+use Magento\Quote\Api\GuestCartTotalRepositoryInterface;
+use Magento\Quote\Model\Cart\CartTotalRepository;
+use Magento\Quote\Model\QuoteRepository;
+use Magento\Quote\Model\QuoteIdMask;
+use Magento\Quote\Model\QuoteIdMaskFactory;
+
+/**
+ * Cart totals repository class for guest carts.
+ */
+class GuestCartTotalRepository extends CartTotalRepository implements \Magento\Quote\Api\GuestCartTotalRepositoryInterface
+{
+    /**
+     * @var QuoteIdMaskFactory
+     */
+    private $quoteIdMaskFactory;
+
+    /**
+     * Constructs a cart totals data object.
+     *
+     * @param \Magento\Quote\Api\Data\TotalsInterfaceFactory $totalsFactory Cart totals factory.
+     * @param QuoteRepository $quoteRepository Quote repository.
+     * @param \Magento\Framework\Api\DataObjectHelper $dataObjectHelper
+     * @param QuoteIdMaskFactory $quoteIdMaskFactory
+     */
+    public function __construct(
+        \Magento\Quote\Api\Data\TotalsInterfaceFactory $totalsFactory,
+        QuoteRepository $quoteRepository,
+        \Magento\Framework\Api\DataObjectHelper $dataObjectHelper,
+        QuoteIdMaskFactory $quoteIdMaskFactory
+    ) {
+        $this->quoteIdMaskFactory = $quoteIdMaskFactory;
+        parent::__construct(
+            $totalsFactory,
+            $quoteRepository,
+            $dataObjectHelper
+        );
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function get($cartId)
+    {
+        /** @var $quoteIdMask QuoteIdMask */
+        $quoteIdMask = $this->quoteIdMaskFactory->create()->load($cartId, 'masked_id');
+        return parent::get($quoteIdMask->getId());
+    }
+}
