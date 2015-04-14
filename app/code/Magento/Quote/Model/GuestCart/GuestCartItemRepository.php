825eafa1409299a8c8484cabf65a26ece7af487d
Bryant Luk <bluk@ebay.com>
2015-04-14 10:28:14 -0500

    MAGETWO-32686 Customer-facing resources in Sales & Checkout APIs
    - Corrects GuestCartItemRepository namespace

--- app/code/Magento/Quote/Model/GuestCart/GuestCartItemRepository.php
+++ app/code/Magento/Quote/Model/GuestCart/GuestCartItemRepository.php
@@ -4,7 +4,7 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\Quote\GuestCart;
+namespace Magento\Quote\Model\GuestCart;
 
 use Magento\Quote\Model\Quote\Item\Repository;
 use Magento\Quote\Model\QuoteIdMask;
