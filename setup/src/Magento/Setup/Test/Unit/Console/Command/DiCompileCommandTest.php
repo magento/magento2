61d4149a6af15a7a3903b2d1aa25350b2e732c92
Eddie Lau <kahlau@ebay.com>
2015-04-20 09:49:05 -0500

    MAGETWO-36073: Move dev/tools/Magento/Tools/Di/singletenant_compiler.php
    - fixed unit tests

--- setup/src/Magento/Setup/Test/Unit/Console/Command/DiCompileCommandTest.php
+++ setup/src/Magento/Setup/Test/Unit/Console/Command/DiCompileCommandTest.php
@@ -39,7 +39,7 @@ class DiCompileCommandTest extends \PHPUnit_Framework_TestCase
     {
         $this->deploymentConfig = $this->getMock('Magento\Framework\App\DeploymentConfig', [], [], '', false);
         $this->objectManagerProvider = $this->getMock(
-            'Magento\Setup\Model\ObjectManagerProviderr',
+            'Magento\Setup\Model\ObjectManagerProvider',
             [],
             [],
             '',
