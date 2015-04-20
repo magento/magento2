9e70be75498f0cb55b9a5a84de6f0c232aa7b9db
Arkadii Chyzhov <achyzhov@ebay.com>
2015-04-20 14:29:22 +0300

    MAGETWO-36381: Magento\Quote\Api\GuestShippingAddressManagement
    - removed excessive line

--- app/code/Magento/Quote/Test/Unit/Model/GuestCart/GuestShippingAddressManagementTest.php
+++ app/code/Magento/Quote/Test/Unit/Model/GuestCart/GuestShippingAddressManagementTest.php
@@ -5,8 +5,6 @@
  * See COPYING.txt for license details.
  */
 
-// @codingStandardsIgnoreFile
-
 namespace Magento\Quote\Test\Unit\Model\GuestCart;
 
 class GuestShippingAddressManagementTest extends \PHPUnit_Framework_TestCase
