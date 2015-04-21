9a6b9e149e47b0690637cdf57b93a9457528da87
Arkadii Chyzhov <achyzhov@ebay.com>
2015-04-21 18:47:16 +0300

    MAGETWO-36379: Magento\Quote\Api\GuestCouponManagement
    - replaced inheritance with composition

--- app/code/Magento/Quote/Test/Unit/Model/GuestCart/GuestCouponManagementTest.php
+++ app/code/Magento/Quote/Test/Unit/Model/GuestCart/GuestCouponManagementTest.php
@@ -17,21 +17,6 @@ class GuestCouponManagementTest extends \PHPUnit_Framework_TestCase
     /**
      * @var \PHPUnit_Framework_MockObject_MockObject
      */
-    protected $quoteRepositoryMock;
-
-    /**
-     * @var \PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $quoteMock;
-
-    /**
-     * @var \PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $quoteAddressMock;
-
-    /**
-     * @var \PHPUnit_Framework_MockObject_MockObject
-     */
     protected $quoteIdMaskFactoryMock;
 
     /**
@@ -39,127 +24,65 @@ class GuestCouponManagementTest extends \PHPUnit_Framework_TestCase
      */
     protected $quoteIdMaskMock;
 
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $couponManagementMock;
+
+    /**
+     * @var string
+     */
+    protected $maskedCartId;
+
+    /**
+     * @var int
+     */
+    protected $cartId;
+
+    /**
+     * @var string
+     */
+    protected $couponCode;
+
     protected function setUp()
     {
         $objectManager = new \Magento\Framework\TestFramework\Unit\Helper\ObjectManager($this);
+        $this->couponManagementMock = $this->getMock( 'Magento\Quote\Model\CouponManagement', [], [], '', false);
 
-        $this->quoteRepositoryMock = $this->getMock('Magento\Quote\Model\QuoteRepository', [], [], '', false);
-        $this->quoteMock = $this->getMock(
-            'Magento\Quote\Model\Quote',
-            [
-                'getItemsCount',
-                'setCouponCode',
-                'collectTotals',
-                'save',
-                'getShippingAddress',
-                'getCouponCode'
-            ],
-            [],
-            '',
-            false
+        $this->couponCode = 'test_coupon_code';
+        $this->maskedCartId = 'f216207248d65c789b17be8545e0aa73';
+        $this->cartId = 123;
+
+        $guestCartTestHelper = new GuestCartTestHelper($this);
+        list($this->quoteIdMaskFactoryMock, $this->quoteIdMaskMock) = $guestCartTestHelper->mockQuoteIdMask(
+            $this->maskedCartId,
+            $this->cartId
         );
-        $this->quoteAddressMock = $this->getMock(
-            'Magento\Quote\Model\Quote\Address',
-            [
-                'setCollectShippingRates'
-            ],
-            [],
-            '',
-            false);
-
-        $this->quoteIdMaskFactoryMock = $this->getMock('Magento\Quote\Model\QuoteIdMaskFactory', [], [], '', false);
-        $this->quoteIdMaskMock = $this->getMock('Magento\Quote\Model\QuoteIdMask', [], [], '', false);
 
         $this->model = $objectManager->getObject(
             'Magento\Quote\Model\GuestCart\GuestCouponManagement',
             [
-                'quoteRepository' => $this->quoteRepositoryMock,
+                'couponManagement' => $this->couponManagementMock,
                 'quoteIdMaskFactory' => $this->quoteIdMaskFactoryMock
             ]
         );
     }
 
-    public function testGetCoupon()
+    public function testGet()
     {
-        $maskedCartId = 'f216207248d65c789b17be8545e0aa73';
-        $cartId = 11;
-        $couponCode = 'test_coupon_code';
-
-        $this->quoteIdMaskFactoryMock->expects($this->once())->method('create')->willReturn($this->quoteIdMaskMock);
-        $this->quoteIdMaskMock->expects($this->once())
-            ->method('load')
-            ->with($maskedCartId, 'masked_id')
-            ->willReturn($this->quoteIdMaskMock);
-        $this->quoteIdMaskMock->expects($this->once())
-            ->method('getId')
-            ->willReturn($cartId);
-
-        $quoteMock = $this->getMock('Magento\Quote\Model\Quote', ['getCouponCode', '__wakeup'], [], '', false);
-        $quoteMock->expects($this->any())->method('getCouponCode')->will($this->returnValue($couponCode));
-
-        $this->quoteRepositoryMock->expects($this->once())
-            ->method('getActive')
-            ->with($cartId)
-            ->will($this->returnValue($quoteMock));
-
-        $this->assertEquals($couponCode, $this->model->get($maskedCartId));
+        $this->couponManagementMock->expects($this->once())->method('get')->willReturn($this->couponCode);
+        $this->assertEquals($this->couponCode, $this->model->get($this->maskedCartId));
     }
 
     public function testSet()
     {
-        $maskedCartId = 'f216207248d65c789b17be8545e0aa73';
-        $cartId = 33;
-        $couponCode = '153a-ABC';
-
-        $this->quoteIdMaskFactoryMock->expects($this->once())->method('create')->willReturn($this->quoteIdMaskMock);
-        $this->quoteIdMaskMock->expects($this->once())
-            ->method('load')
-            ->with($maskedCartId, 'masked_id')
-            ->willReturn($this->quoteIdMaskMock);
-        $this->quoteIdMaskMock->expects($this->once())
-            ->method('getId')
-            ->willReturn($cartId);
-
-        $this->quoteRepositoryMock->expects($this->once())
-            ->method('getActive')->with($cartId)->will($this->returnValue($this->quoteMock));
-        $this->quoteMock->expects($this->once())->method('getItemsCount')->will($this->returnValue(12));
-        $this->quoteMock->expects($this->once())
-            ->method('getShippingAddress')->will($this->returnValue($this->quoteAddressMock));
-        $this->quoteAddressMock->expects($this->once())->method('setCollectShippingRates')->with(true);
-        $this->quoteMock->expects($this->once())->method('setCouponCode')->with($couponCode);
-        $this->quoteMock->expects($this->once())->method('collectTotals')->will($this->returnValue($this->quoteMock));
-        $this->quoteRepositoryMock->expects($this->once())->method('save')->with($this->quoteMock);
-        $this->quoteMock->expects($this->once())->method('getCouponCode')->will($this->returnValue($couponCode));
-
-        $this->assertTrue($this->model->set($maskedCartId, $couponCode));
+        $this->couponManagementMock->expects($this->once())->method('set')->willReturn(true);
+        $this->assertTrue($this->model->set($this->maskedCartId, $this->couponCode));
     }
 
-    public function testDelete()
+    public function testRemove()
     {
-        $maskedCartId = 'f216207248d65c789b17be8545e0aa73';
-        $cartId = 65;
-
-        $this->quoteIdMaskFactoryMock->expects($this->once())->method('create')->willReturn($this->quoteIdMaskMock);
-        $this->quoteIdMaskMock->expects($this->once())
-            ->method('load')
-            ->with($maskedCartId, 'masked_id')
-            ->willReturn($this->quoteIdMaskMock);
-        $this->quoteIdMaskMock->expects($this->once())
-            ->method('getId')
-            ->willReturn($cartId);
-
-        $this->quoteRepositoryMock->expects($this->once())
-            ->method('getActive')->with($cartId)->will($this->returnValue($this->quoteMock));
-        $this->quoteMock->expects($this->once())->method('getItemsCount')->will($this->returnValue(12));
-        $this->quoteMock->expects($this->once())
-            ->method('getShippingAddress')->will($this->returnValue($this->quoteAddressMock));
-        $this->quoteAddressMock->expects($this->once())->method('setCollectShippingRates')->with(true);
-        $this->quoteMock->expects($this->once())->method('setCouponCode')->with('');
-        $this->quoteMock->expects($this->once())->method('collectTotals')->will($this->returnValue($this->quoteMock));
-        $this->quoteMock->expects($this->once())->method('collectTotals')->will($this->returnValue($this->quoteMock));
-        $this->quoteRepositoryMock->expects($this->once())->method('save')->with($this->quoteMock);
-        $this->quoteMock->expects($this->once())->method('getCouponCode')->will($this->returnValue(''));
-
-        $this->assertTrue($this->model->remove($maskedCartId));
+        $this->couponManagementMock->expects($this->once())->method('remove')->willReturn(true);
+        $this->assertTrue($this->model->remove($this->maskedCartId));
     }
 }
