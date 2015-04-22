0ef801cafcac18dc790d46699a70746261b28687
Gurzhyi, Andrii <agurzhyi@ebay.com>
2015-04-22 17:19:38 +0300

    MAGETWO-36037: Adding a new event in the action OrderSave
    - CR

--- app/code/Magento/Checkout/Test/Unit/Controller/Onepage/SaveOrderTest.php
+++ app/code/Magento/Checkout/Test/Unit/Controller/Onepage/SaveOrderTest.php
@@ -64,87 +64,66 @@ class SaveOrderTest extends \PHPUnit_Framework_TestCase
     protected $eventManagerMock;
 
     /**
+     * @var \Magento\Checkout\Model\Type\Onepage|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $onepageMock;
+
+    /**
+     * @var \Magento\Checkout\Model\Agreements\AgreementsValidator|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $agreementsValidatorMock;
+
+    /**
+     * @var \Magento\Quote\Model\Quote|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $quoteMock;
+
+    /**
      * Set up
      *
      * @return void
-     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
      */
     protected function setUp()
     {
         $helper = new ObjectManager($this);
 
-        $contextMock = $this->getMock(
-            'Magento\Framework\App\Action\Context',
-            [],
-            [],
-            '',
-            false
-        );
-
-        $this->requestMock = $this->getMockForAbstractClass(
-            'Magento\Framework\App\RequestInterface',
-            [],
-            '',
-            false,
-            true,
-            true,
-            ['getPost']
-        );
-        $this->responseMock = $this->getMockForAbstractClass(
-            'Magento\Framework\App\ResponseInterface',
-            [],
-            '',
-            false,
-            true,
-            true,
-            []
-        );
-        $this->objectManagerMock = $this->getMockForAbstractClass(
-            'Magento\Framework\ObjectManagerInterface',
-            [],
-            '',
-            false,
-            true,
-            true,
-            []
-        );
-        $this->formKeyValidatorMock = $this->getMock(
-            'Magento\Framework\Data\Form\FormKey\Validator',
-            [],
-            [],
-            '',
-            false
-        );
-        $this->resultRedirectFactoryMock = $this->getMock(
-            'Magento\Framework\Controller\Result\RedirectFactory',
-            [],
-            [],
-            '',
-            false
-        );
-        $this->resultRawFactoryMock = $this->getMock(
-            'Magento\Framework\Controller\Result\RawFactory',
-            [],
-            [],
-            '',
-            false
-        );
-        $this->resultJsonFactoryMock = $this->getMock(
-            'Magento\Framework\Controller\Result\JsonFactory',
-            [],
-            [],
-            '',
-            false
-        );
-        $this->eventManagerMock = $this->getMockForAbstractClass(
-            'Magento\Framework\Event\ManagerInterface',
-            [],
-            '',
-            false,
-            true,
-            true,
-            []
-        );
+        $contextMock = $this->getMockBuilder('Magento\Framework\App\Action\Context')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->requestMock = $this->getMockBuilder('Magento\Framework\App\RequestInterface')
+            ->disableOriginalConstructor()
+            ->setMethods(['getPost'])
+            ->getMockForAbstractClass();
+        $this->responseMock = $this->getMockBuilder('Magento\Framework\App\ResponseInterface')
+            ->disableOriginalConstructor()
+            ->getMockForAbstractClass();
+        $this->objectManagerMock = $this->getMockBuilder('Magento\Framework\ObjectManagerInterface')
+            ->disableOriginalConstructor()
+            ->getMockForAbstractClass();
+        $this->formKeyValidatorMock = $this->getMockBuilder('Magento\Framework\Data\Form\FormKey\Validator')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->resultRedirectFactoryMock = $this->getMockBuilder('Magento\Framework\Controller\Result\RedirectFactory')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->resultRawFactoryMock = $this->getMockBuilder('Magento\Framework\Controller\Result\RawFactory')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->resultJsonFactoryMock = $this->getMockBuilder('Magento\Framework\Controller\Result\JsonFactory')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->eventManagerMock = $this->getMockBuilder('Magento\Framework\Event\ManagerInterface')
+            ->disableOriginalConstructor()
+            ->getMockForAbstractClass();
+        $this->onepageMock = $this->getMockBuilder('Magento\Checkout\Model\Type\Onepage')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->agreementsValidatorMock = $this->getMockBuilder('Magento\Checkout\Model\Agreements\AgreementsValidator')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->quoteMock = $this->getMockBuilder('Magento\Quote\Model\Quote')
+            ->disableOriginalConstructor()
+            ->getMock();
 
         $contextMock->expects($this->once())
             ->method('getRequest')
@@ -174,73 +153,53 @@ class SaveOrderTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
+     * Test method execution _expireAjax (call hasItems === false)
+     *
+     * @return void
+     */
+    protected function expireAjaxFlowHasItemsFalse()
+    {
+        $this->onepageMock->expects($this->atLeastOnce())
+            ->method('getQuote')
+            ->willReturn($this->quoteMock);
+
+        $this->quoteMock->expects($this->once())
+            ->method('hasItems')
+            ->willReturn(false);
+        $this->quoteMock->expects($this->never())
+            ->method('getHasError')
+            ->willReturn(true);
+        $this->quoteMock->expects($this->never())
+            ->method('validateMinimumAmount')
+            ->willReturn(false);
+
+        $this->requestMock->expects($this->never())
+            ->method('getActionName');
+    }
+
+    /**
      * Test for execute method
      *
      * @return void
-     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
      */
-    public function testExecute()
+    public function testExecuteWithSuccessOrderSave()
     {
-        $data = [
-            'payment-key-1' => 'payment-value-1',
-            'checks' => [
-                \Magento\Payment\Model\Method\AbstractMethod::CHECK_USE_CHECKOUT,
-                \Magento\Payment\Model\Method\AbstractMethod::CHECK_USE_FOR_COUNTRY,
-                \Magento\Payment\Model\Method\AbstractMethod::CHECK_USE_FOR_CURRENCY,
-                \Magento\Payment\Model\Method\AbstractMethod::CHECK_ORDER_TOTAL_MIN_MAX,
-                \Magento\Payment\Model\Method\AbstractMethod::CHECK_ZERO_TOTAL,
-            ]
-        ];
+        $testData = $this->getExecuteWithSuccessOrderSaveTestData();
 
-        $onepageMock = $this->getMock(
-            'Magento\Checkout\Model\Type\Onepage',
-            [],
-            [],
-            '',
-            false
-        );
-        $redirectMock = $this->getMock(
-            'Magento\Framework\Controller\Result\Redirect',
-            [],
-            [],
-            '',
-            false
-        );
-        $quoteMock = $this->getMock(
-            'Magento\Quote\Model\Quote',
-            [],
-            [],
-            '',
-            false
-        );
-        $agreementsValidatorMock = $this->getMock(
-            'Magento\Checkout\Model\Agreements\AgreementsValidator',
-            [],
-            [],
-            '',
-            false
-        );
-        $paymentMock = $this->getMock(
-            'Magento\Quote\Model\Quote\Payment',
-            [],
-            [],
-            '',
-            false
-        );
-        $checkoutMock = $this->getMock(
-            'Magento\Checkout\Model\Session',
-            ['getRedirectUrl'],
-            [],
-            '',
-            false
-        );
-        $resultJsonMock = $this->getMock(
-            'Magento\Framework\Controller\Result\Json',
-            [],
-            [],
-            '',
-            false
-        );
+
+        $redirectMock = $this->getMockBuilder('Magento\Framework\Controller\Result\Redirect')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $paymentMock = $this->getMockBuilder('Magento\Quote\Model\Quote\Payment')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $checkoutMock = $this->getMockBuilder('Magento\Checkout\Model\Session')
+            ->disableOriginalConstructor()
+            ->setMethods(['getRedirectUrl'])
+            ->getMock();
+        $resultJsonMock = $this->getMockBuilder('Magento\Framework\Controller\Result\Json')
+            ->disableOriginalConstructor()
+            ->getMock();
 
         $redirectMock->expects($this->never())
             ->method('setPath')
@@ -258,70 +217,34 @@ class SaveOrderTest extends \PHPUnit_Framework_TestCase
 
         $this->objectManagerMock->expects($this->atLeastOnce())
             ->method('get')
-            ->willReturnMap(
-                [
-                    ['Magento\Checkout\Model\Type\Onepage', $onepageMock],
-                    ['Magento\Checkout\Model\Agreements\AgreementsValidator', $agreementsValidatorMock],
-                ]
-            );
+            ->willReturnMap($testData['objectManager.get']);
 
         // call _expireAjax method
-        $onepageMock->expects($this->atLeastOnce())
-            ->method('getQuote')
-            ->willReturn($quoteMock);
-
-        $quoteMock->expects($this->once())
-            ->method('hasItems')
-            ->willReturn(false);
-        $quoteMock->expects($this->never())
-            ->method('getHasError')
-            ->willReturn(true);
-        $quoteMock->expects($this->never())
-            ->method('validateMinimumAmount')
-            ->willReturn(false);
-
-        $this->requestMock->expects($this->never())
-            ->method('getActionName');
-        // -- end
+        $this->expireAjaxFlowHasItemsFalse();
 
         $this->requestMock->expects($this->atLeastOnce())
             ->method('getPost')
-            ->willReturnMap(
-                [
-                    [
-                        'agreement',
-                        [],
-                        [
-                            'test-key-1' => 'test-value-1'
-                        ]
-                    ],
-                    [
-                        'payment',
-                        [],
-                        $data
-                    ],
-                ]
-            );
+            ->willReturnMap($testData['request.getPost']);
 
-        $agreementsValidatorMock->expects($this->once())
+        $this->agreementsValidatorMock->expects($this->once())
             ->method('isValid')
-            ->with(['test-key-1'])
+            ->with($testData['agreementsValidator.isValid'])
             ->willReturn(true);
 
-        $quoteMock->expects($this->atLeastOnce())
+        $this->quoteMock->expects($this->atLeastOnce())
             ->method('getPayment')
             ->willReturn($paymentMock);
 
         $paymentMock->expects($this->once())
             ->method('setQuote')
-            ->with($quoteMock);
+            ->with($this->quoteMock);
         $paymentMock->expects($this->once())
             ->method('importData')
-            ->with($data);
+            ->with($testData['payment.importData']);
 
-        $onepageMock->expects($this->once())
+        $this->onepageMock->expects($this->once())
             ->method('saveOrder');
-        $onepageMock->expects($this->once())
+        $this->onepageMock->expects($this->once())
             ->method('getCheckout')
             ->willReturn($checkoutMock);
 
@@ -342,9 +265,56 @@ class SaveOrderTest extends \PHPUnit_Framework_TestCase
 
         $resultJsonMock->expects($this->once())
             ->method('setData')
-            ->with(['success' => 1, 'error' => false])
+            ->with($testData['resultJson.setData'])
             ->willReturnSelf();
 
         $this->assertEquals($resultJsonMock, $this->controller->execute());
+    }
+
+    /**
+     * Get data for test testExecuteWithSuccessOrderSave
+     *
+     * @return array
+     */
+    protected function getExecuteWithSuccessOrderSaveTestData()
+    {
+        $data = [
+            'payment-key-1' => 'payment-value-1',
+            'checks' => [
+                \Magento\Payment\Model\Method\AbstractMethod::CHECK_USE_CHECKOUT,
+                \Magento\Payment\Model\Method\AbstractMethod::CHECK_USE_FOR_COUNTRY,
+                \Magento\Payment\Model\Method\AbstractMethod::CHECK_USE_FOR_CURRENCY,
+                \Magento\Payment\Model\Method\AbstractMethod::CHECK_ORDER_TOTAL_MIN_MAX,
+                \Magento\Payment\Model\Method\AbstractMethod::CHECK_ZERO_TOTAL,
+            ]
+        ];
+        $testKey = 'test-key-1';
+
+        return [
+            'resultJson.setData' => [
+                'success' => 1,
+                'error' => false
+            ],
+            'request.getPost' => [
+                [
+                    'agreement',
+                    [],
+                    [
+                        $testKey => 'test-value-1'
+                    ]
+                ],
+                [
+                    'payment',
+                    [],
+                    $data
+                ],
+            ],
+            'payment.importData' => $data,
+            'agreementsValidator.isValid' => [$testKey],
+            'objectManager.get' => [
+                ['Magento\Checkout\Model\Type\Onepage', $this->onepageMock],
+                ['Magento\Checkout\Model\Agreements\AgreementsValidator', $this->agreementsValidatorMock],
+            ]
+        ];
     }
 }
