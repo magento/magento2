8fde87972605ba1b34375a7d03a2f0642bb164e5
Joan He <joan@x.com>
2015-04-22 13:53:46 -0500

    MAGETWO-36178: Update constructor
    - Magento\Framework\App\Cache\Type\Config

--- lib/internal/Magento/Framework/App/Test/Unit/Cache/Type/ConfigTest.php
+++ lib/internal/Magento/Framework/App/Test/Unit/Cache/Type/ConfigTest.php
@@ -57,13 +57,7 @@ class ConfigTest extends \PHPUnit_Framework_TestCase
         return [
             ['test', ['record_id'], 111],
             ['load', ['record_id'], '111'],
-//            [
-//                'save',
-//                ['record_value', 'record_id', ['tag'], 555],
-//                true
-//            ],
             ['remove', ['record_id'], true],
-//            ['clean', [\Zend_Cache::CLEANING_MODE_MATCHING_ANY_TAG, ['tag']], true],
             ['getBackend', [], $this->getMock('Zend_Cache_Backend')],
             ['getLowLevelFrontend', [], $this->getMock('Zend_Cache_Core')],
         ];
