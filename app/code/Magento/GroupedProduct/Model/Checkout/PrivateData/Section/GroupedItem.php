63be69af2b330923f1307a4104c21f9148a4056a
Valeriy Nayda <vnayda@ebay.com>
2015-04-28 14:39:50 +0300

    MAGETWO-34829: Private content blocks re-factoring (banners and widgets)
    - MAGETWO-36718: Magento\Checkout\Block\Cart\Sidebar - product type-dependant renderers
    -- grouped item skeleton

---
+++ app/code/Magento/GroupedProduct/Model/Checkout/PrivateData/Section/GroupedItem.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\GroupedProduct\Model\Checkout\PrivateData\Section;
+
+use Magento\Checkout\Model\PrivateData\Section\AbstractItem;
+
+/**
+ * Grouped item
+ */
+class GroupedItem extends AbstractItem
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function doGetItemData()
+    {
+    }
+}
