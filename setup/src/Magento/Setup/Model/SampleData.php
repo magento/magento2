1034f2f858c54f99b0facf2e70ea6da27f02be20
Safwan Khan <safwkhan@ebay.com>
2015-05-04 18:09:44 -0500

    MAGETWO-36080: Move dev/tools/Magento/Tools/SampleData/install.php
    - Corresponding change related to new SampleData command.

--- setup/src/Magento/Setup/Model/SampleData.php
+++ setup/src/Magento/Setup/Model/SampleData.php
@@ -20,19 +20,21 @@ class SampleData
     /**
      * Path to the sample data application
      */
-    const PATH = 'dev/tools/Magento/Tools/SampleData';
+    const PATH = '/Magento/SampleData';
 
     /**
-     * @var \Magento\Framework\Filesystem\Directory\ReadInterface
+     * Filesystem Directory List
+     *
+     * @var DirectoryList
      */
-    private $rootDir;
+    private $directoryList;
 
     /**
-     * @param Filesystem $filesystem
+     * @param DirectoryList $directoryList
      */
-    public function __construct(Filesystem $filesystem)
+    public function __construct(DirectoryList $directoryList)
     {
-        $this->rootDir = $filesystem->getDirectoryRead(DirectoryList::ROOT);
+        $this->directoryList = $directoryList;
     }
 
     /**
@@ -42,7 +44,7 @@ class SampleData
      */
     public function isDeployed()
     {
-        return $this->rootDir->isExist(self::PATH);
+        return file_exists($this->directoryList->getPath(DirectoryList::MODULES) . self::PATH);
     }
 
     /**
@@ -56,8 +58,8 @@ class SampleData
      */
     public function install(ObjectManagerInterface $objectManager, LoggerInterface $logger, $adminUserName)
     {
-        /** @var \Magento\Tools\SampleData\Logger $sampleDataLogger */
-        $sampleDataLogger = $objectManager->get('Magento\Tools\SampleData\Logger');
+        /** @var \Magento\SampleData\Model\Logger $sampleDataLogger */
+        $sampleDataLogger = $objectManager->get('Magento\SampleData\Model\Logger');
         $sampleDataLogger->setSubject($logger);
 
         $areaCode = 'adminhtml';
@@ -72,7 +74,8 @@ class SampleData
         $userFactory = $objectManager->get('Magento\User\Model\UserFactory');
         $user = $userFactory->create()->loadByUsername($adminUserName);
 
-        $installer = $objectManager->get('Magento\Tools\SampleData\Installer');
+        /** @var \Magento\SampleData\Model\Installer $installer */
+        $installer = $objectManager->get('Magento\SampleData\Model\Installer');
         $installer->run($user);
     }
 }
