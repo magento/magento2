200358ef1dc84ef97075cd22c85ea14182701a7a
Bryant Luk <bluk@ebay.com>
2015-05-06 14:01:08 -0500

    MAGETWO-36777: Add @api annotations to Framework/Oauth module

--- lib/internal/Magento/Framework/Oauth/OauthInterface.php
+++ lib/internal/Magento/Framework/Oauth/OauthInterface.php
@@ -6,7 +6,7 @@
 namespace Magento\Framework\Oauth;
 
 /**
- * OauthInterface  provides methods consistent with implementing a 2-legged OAuth authentication mechanism. Methods
+ * OauthInterface provides methods consistent with implementing a 2-legged OAuth authentication mechanism. Methods
  * include creating a request token, getting an access token, and performing certain validations on tokens and
  * token requests. A method is also included for generating an OAuth header that can be used in an HTTP request.
  *
