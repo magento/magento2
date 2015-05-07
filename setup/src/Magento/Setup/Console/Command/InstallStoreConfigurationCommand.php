2c083bae888f636a065c4d89e930523d6267d900
Safwan Khan <safwkhan@ebay.com>
2015-05-07 18:50:58 -0500

    MAGETWO-36903: Validate setup:store-config:set
    - Implemented the validation.

--- setup/src/Magento/Setup/Console/Command/InstallStoreConfigurationCommand.php
+++ setup/src/Magento/Setup/Console/Command/InstallStoreConfigurationCommand.php
@@ -13,6 +13,10 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Magento\Setup\Model\StoreConfigurationDataMapper;
+use Magento\Setup\Model\ObjectManagerProvider;
+use Magento\Framework\ObjectManagerInterface;
+use \Magento\Framework\Exception\LocalizedException;
+use \Magento\Store\Model\Store;
 
 class InstallStoreConfigurationCommand extends AbstractSetupCommand
 {
@@ -29,17 +33,27 @@ class InstallStoreConfigurationCommand extends AbstractSetupCommand
     private $deploymentConfig;
 
     /**
+     * Object Manager
+     *
+     * @var ObjectManagerInterface
+     */
+    private $objectManager;
+
+    /**
      * Inject dependencies
      *
      * @param InstallerFactory $installerFactory
      * @param DeploymentConfig $deploymentConfig
+     * @param ObjectManagerProvider $objectManagerProvider
      */
     public function __construct(
         InstallerFactory $installerFactory,
-        DeploymentConfig $deploymentConfig
+        DeploymentConfig $deploymentConfig,
+        ObjectManagerProvider $objectManagerProvider
     ) {
         $this->installerFactory = $installerFactory;
         $this->deploymentConfig = $deploymentConfig;
+        $this->objectManager = $objectManagerProvider->get();
         parent::__construct();
     }
 
@@ -63,6 +77,11 @@ class InstallStoreConfigurationCommand extends AbstractSetupCommand
             $output->writeln(
                 "<info>Store settings can't be saved because the Magento application is not installed.</info>"
             );
+            return;
+        }
+        $errors = $this->validate($input);
+        if ($errors) {
+            $output->writeln($errors);
             return;
         }
         $installer = $this->installerFactory->create(new ConsoleLogger($output));
@@ -132,5 +151,128 @@ class InstallStoreConfigurationCommand extends AbstractSetupCommand
                 'Whether to use a "security key" feature in Magento Admin URLs and forms'
             ),
         ];
+    }
+
+    /**
+     * Check if option values provided by the user are valid
+     *
+     * @param InputInterface $input
+     * @return string[]
+     */
+    public function validate(InputInterface $input)
+    {
+        $errors = [];
+        $options = $input->getOptions();
+        foreach ($options as $key => $value) {
+            if (!$value) {
+                continue;
+            }
+            /** @var \Magento\Setup\Model\Lists $lists */
+            $lists = $this->objectManager->get('Magento\Setup\Model\Lists');
+            switch ($key) {
+                case StoreConfigurationDataMapper::KEY_BASE_URL:
+                    try {
+                        $this->validateURL($value, Store::XML_PATH_UNSECURE_BASE_URL);
+                    } catch (LocalizedException $e) {
+                        $errors[] = '<error>' . 'Command option \'' . StoreConfigurationDataMapper::KEY_BASE_URL
+                            . '\': ' . $e->getLogMessage() .'</error>';
+                    }
+                    break;
+                case StoreConfigurationDataMapper::KEY_LANGUAGE:
+                    if (!in_array($value, array_keys($lists->getLocaleList()))) {
+                        $errors[] = '<error>' . 'Command option \'' . StoreConfigurationDataMapper::KEY_LANGUAGE
+                            . '\': Invalid value. To see possible values, run command ' .
+                            "'bin/magento info:language:list'.</error>";
+                    }
+                    break;
+                case StoreConfigurationDataMapper::KEY_TIMEZONE:
+                    if (!in_array($value, array_keys($lists->getTimezoneList()))) {
+                        $errors[] = '<error>' . 'Command option \'' . StoreConfigurationDataMapper::KEY_TIMEZONE
+                            . '\': Invalid value. To see possible values, run command ' .
+                            "'bin/magento info:timezone:list'.</error>";
+                    }
+                    break;
+                case StoreConfigurationDataMapper::KEY_CURRENCY:
+                    if (!in_array($value, array_keys($lists->getCurrencyList()))) {
+                        $errors[] = '<error>' . 'Command option \'' . StoreConfigurationDataMapper::KEY_CURRENCY
+                            . '\': Invalid value. To see possible values, run command ' .
+                            "'bin/magento info:currency:list'.</error>";
+                    }
+                    break;
+                case StoreConfigurationDataMapper::KEY_USE_SEF_URL:
+                    $errorMsg = $this->validateBinaryValue($value, StoreConfigurationDataMapper::KEY_USE_SEF_URL);
+                    if ($errorMsg !== '') {
+                        $errors[] = $errorMsg;
+                    }
+                    break;
+                    break;
+                case StoreConfigurationDataMapper::KEY_IS_SECURE:
+                    $errorMsg = $this->validateBinaryValue($value, StoreConfigurationDataMapper::KEY_IS_SECURE);
+                    if ($errorMsg !== '') {
+                        $errors[] = $errorMsg;
+                    }
+                    break;
+                case StoreConfigurationDataMapper::KEY_BASE_URL_SECURE:
+                    try {
+                        $this->validateURL($value, Store::XML_PATH_SECURE_BASE_URL);
+                        if (strpos($value,'https:') === false) {
+                            throw new LocalizedException(new \Magento\Framework\Phrase("Invalid secure URL."));
+                        }
+                    } catch (LocalizedException $e) {
+                        $errors[] = '<error>' . 'Command option \'' . StoreConfigurationDataMapper::KEY_BASE_URL_SECURE
+                            . '\': ' . $e->getLogMessage() .'</error>';
+                    }
+                    break;
+                case StoreConfigurationDataMapper::KEY_IS_SECURE_ADMIN:
+                    $errorMsg = $this->validateBinaryValue($value, StoreConfigurationDataMapper::KEY_IS_SECURE_ADMIN);
+                    if ($errorMsg !== '') {
+                        $errors[] = $errorMsg;
+                    }
+                    break;
+                case StoreConfigurationDataMapper::KEY_ADMIN_USE_SECURITY_KEY:
+                    $errorMsg = $this->validateBinaryValue(
+                        $value,
+                        StoreConfigurationDataMapper::KEY_ADMIN_USE_SECURITY_KEY
+                    );
+                    if ($errorMsg !== '') {
+                        $errors[] = $errorMsg;
+                    }
+                    break;
+            }
+        }
+        return $errors;
+    }
+
+    /**
+     * Validate a URL
+     *
+     * @param string $value
+     * @param string $path
+     * @return void
+     * @throws LocalizedException
+     */
+    private function validateURL($value, $path)
+    {
+        /** @var \Magento\Config\Model\Config\Backend\Baseurl $baseUrl */
+        $baseUrl = $this->objectManager->get('Magento\Config\Model\Config\Backend\Baseurl');
+        $baseUrl->setPath($path);
+        $baseUrl->setValue($value);
+        $baseUrl->beforeSave();
+    }
+
+    /**
+     * Validate binary value for a specified key
+     *
+     * @param string $value
+     * @param string $key
+     * @return string
+     */
+    private function validateBinaryValue($value, $key)
+    {
+        $errorMsg = '';
+        if ($value !== '0' && $value !== '1') {
+            $errorMsg = '<error>' . 'Command option \'' . $key . '\': Invalid value. Possible values (0|1).</error>';
+        }
+        return $errorMsg;
     }
 }
--- setup/src/Magento/Setup/Test/Unit/Console/Command/InstallStoreConfigurationCommandTest.php
+++ setup/src/Magento/Setup/Test/Unit/Console/Command/InstallStoreConfigurationCommandTest.php
@@ -8,6 +8,9 @@ namespace Magento\Setup\Test\Unit\Console\Command;
 
 use Magento\Setup\Console\Command\InstallStoreConfigurationCommand;
 use Symfony\Component\Console\Tester\CommandTester;
+use Magento\Setup\Model\Installer;
+use Magento\Framework\ObjectManagerInterface;
+use Magento\Setup\Model\StoreConfigurationDataMapper;
 
 class InstallStoreConfigurationCommandTest extends \PHPUnit_Framework_TestCase
 {
@@ -27,6 +30,11 @@ class InstallStoreConfigurationCommandTest extends \PHPUnit_Framework_TestCase
     private $installer;
 
     /**
+     * @var ObjectManagerInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $objectManager;
+
+    /**
      * @var InstallStoreConfigurationCommand
      */
     private $command;
@@ -36,7 +44,25 @@ class InstallStoreConfigurationCommandTest extends \PHPUnit_Framework_TestCase
         $this->installerFactory = $this->getMock('Magento\Setup\Model\InstallerFactory', [], [], '', false);
         $this->deploymentConfig = $this->getMock('Magento\Framework\App\DeploymentConfig', [], [], '', false);
         $this->installer = $this->getMock('Magento\Setup\Model\Installer', [], [], '', false);
-        $this->command = new InstallStoreConfigurationCommand($this->installerFactory, $this->deploymentConfig);
+        $objectManagerProvider = $this->getMock(
+            'Magento\Setup\Model\ObjectManagerProvider',
+            [],
+            [],
+            '',
+            false
+        );
+        $this->objectManager = $this->getMockForAbstractClass(
+            'Magento\Framework\ObjectManagerInterface',
+            [],
+            '',
+            false
+        );
+        $objectManagerProvider->expects($this->once())->method('get')->willReturn($this->objectManager );
+        $this->command = new InstallStoreConfigurationCommand(
+            $this->installerFactory,
+            $this->deploymentConfig,
+            $objectManagerProvider
+        );
     }
 
     public function testExecute()
@@ -66,5 +92,100 @@ class InstallStoreConfigurationCommandTest extends \PHPUnit_Framework_TestCase
             "Store settings can't be saved because the Magento application is not installed.%w",
             $tester->getDisplay()
         );
+    }
+
+    /**
+     * @dataProvider validateDataProvider
+     * @param array $option
+     * @param string $error
+     */
+    public function testExecuteInvalidData(array $option, $error)
+    {
+        $localizedException= $this->getMock('Magento\Framework\Exception\LocalizedException', [], [], '', false);
+        $localizedException->expects($this->any())->method('getLogMessage')
+            ->will($this->returnValue('Invalid value. Value must be a URL or one of placeholders:'));
+        $baseUrl= $this->getMock('Magento\Config\Model\Config\Backend\Baseurl', [], [], '', false);
+        $baseUrl->expects($this->any())->method('beforeSave')->will($this->throwException($localizedException));
+
+        $lists= $this->getMock('Magento\Setup\Model\Lists', [], [], '', false);
+        $lists->expects($this->any())->method('getLocaleList')->will($this->returnValue(['A' => 'B']));
+        $lists->expects($this->any())->method('getTimezoneList')->will($this->returnValue(['C' => 'D']));
+        $lists->expects($this->any())->method('getCurrencyList')->will($this->returnValue(['E' => 'F']));
+
+        $returnValueMapOM = [
+            [
+                'Magento\Config\Model\Config\Backend\Baseurl',
+                $baseUrl
+            ],
+            [
+                'Magento\Setup\Model\Lists',
+                $lists
+            ],
+        ];
+        $this->objectManager->expects($this->any())
+            ->method('get')
+            ->will($this->returnValueMap($returnValueMapOM));
+        $this->deploymentConfig->expects($this->once())
+            ->method('isAvailable')
+            ->will($this->returnValue(true));
+        $this->installerFactory->expects($this->never())
+            ->method('create');
+        $commandTester = new CommandTester($this->command);
+        $commandTester->execute($option);
+        $this->assertEquals($error . PHP_EOL, $commandTester->getDisplay());
+    }
+
+    /**
+     * @return array
+     */
+    public function validateDataProvider()
+    {
+        return [
+            [
+                ['--' . StoreConfigurationDataMapper::KEY_BASE_URL => 'sampleUrl'],
+                'Command option \'' . StoreConfigurationDataMapper::KEY_BASE_URL
+                . '\': Invalid value. Value must be a URL or one of placeholders:'
+            ],
+            [
+                ['--' . StoreConfigurationDataMapper::KEY_LANGUAGE => 'sampleLanguage'],
+                'Command option \'' . StoreConfigurationDataMapper::KEY_LANGUAGE
+                . '\': Invalid value. To see possible values, run command \'bin/magento info:language:list\'.'
+            ],
+            [
+                ['--' . StoreConfigurationDataMapper::KEY_TIMEZONE => 'sampleTimezone'],
+                'Command option \'' . StoreConfigurationDataMapper::KEY_TIMEZONE
+                . '\': Invalid value. To see possible values, run command \'bin/magento info:timezone:list\'.'
+            ],
+            [
+                ['--' . StoreConfigurationDataMapper::KEY_CURRENCY => 'sampleLanguage'],
+                'Command option \'' . StoreConfigurationDataMapper::KEY_CURRENCY
+                . '\': Invalid value. To see possible values, run command \'bin/magento info:currency:list\'.'
+            ],
+            [
+                ['--' . StoreConfigurationDataMapper::KEY_USE_SEF_URL => 'invalidValue'],
+                'Command option \'' . StoreConfigurationDataMapper::KEY_USE_SEF_URL
+                . '\': Invalid value. Possible values (0|1).'
+            ],
+            [
+                ['--' . StoreConfigurationDataMapper::KEY_IS_SECURE => 'invalidValue'],
+                'Command option \'' . StoreConfigurationDataMapper::KEY_IS_SECURE
+                . '\': Invalid value. Possible values (0|1).'
+            ],
+            [
+                ['--' . StoreConfigurationDataMapper::KEY_BASE_URL_SECURE => 'sampleUrl'],
+                'Command option \'' . StoreConfigurationDataMapper::KEY_BASE_URL_SECURE
+                . '\': Invalid value. Value must be a URL or one of placeholders:'
+            ],
+            [
+                ['--' . StoreConfigurationDataMapper::KEY_IS_SECURE_ADMIN => 'invalidValue'],
+                'Command option \'' . StoreConfigurationDataMapper::KEY_IS_SECURE_ADMIN
+                . '\': Invalid value. Possible values (0|1).'
+            ],
+            [
+                ['--' . StoreConfigurationDataMapper::KEY_ADMIN_USE_SECURITY_KEY => 'invalidValue'],
+                'Command option \'' . StoreConfigurationDataMapper::KEY_ADMIN_USE_SECURITY_KEY
+                . '\': Invalid value. Possible values (0|1).'
+            ],
+        ];
     }
 }
