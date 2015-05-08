a4f2a544de07a57ee7a9e4019ff45823bd57ea40
valdislav <vshcherbyna@ebay.com>
2015-05-08 15:00:40 +0300

    MAGETWO-36232: Refactor queries in \Magento\Tax\Model\Resource\Sales\Order\Tax\Item

--- app/code/Magento/Sales/Model/Resource/Order/Tax/Item.php
+++ app/code/Magento/Sales/Model/Resource/Order/Tax/Item.php
@@ -3,13 +3,11 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-
+namespace Magento\Sales\Model\Resource\Order\Tax;
 
 /**
  * Sales order tax resource model
  */
-namespace Magento\Sales\Model\Resource\Order\Tax;
-
 class Item extends \Magento\Framework\Model\Resource\Db\AbstractDb
 {
     /**
