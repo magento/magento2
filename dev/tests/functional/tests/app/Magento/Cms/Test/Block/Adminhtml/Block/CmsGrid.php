8c6e964fe569f3e674fe025fd4b5295db657b987
Dmytro Vilchynskyi <dvilchynskyi@ebay.com>
2015-05-13 17:00:38 +0300

    MAGETWO-34680: JS - Advanced Filtering
    - fixed tests

--- dev/tests/functional/tests/app/Magento/Cms/Test/Block/Adminhtml/Block/CmsGrid.php
+++ dev/tests/functional/tests/app/Magento/Cms/Test/Block/Adminhtml/Block/CmsGrid.php
@@ -19,25 +19,37 @@ class CmsGrid extends DataGrid
      * @var array
      */
     protected $filters = [
+        'block_id_from' => [
+            'selector' => '[name="filters[block_id][from]"]',
+        ],
+        'block_id_to' => [
+            'selector' => '[name="filters[block_id][to]"]',
+        ],
         'title' => [
             'selector' => '[name="filters[title]"]',
         ],
         'identifier' => [
-            'selector' => '[name="params[filters][identifier]"]',
+            'selector' => '[name="filters[identifier]"]',
+        ],
+        'store_id' => [
+            'selector' => '[name="filters[store_id]"]',
+            'input' => 'selectstore'
         ],
         'is_active' => [
-            'selector' => '[name="params[filters][is_active]"]',
+            'selector' => '[name="filters[is_active]"]',
             'input' => 'select',
         ],
         'creation_time_from' => [
-            'selector' => '[name="params[filters][creation_time][from]"]',
+            'selector' => '[name="filters[creation_time][from]"]',
+        ],
+        'creation_time_to' => [
+            'selector' => '[name="filters[creation_time][to]"]',
         ],
         'update_time_from' => [
-            'selector' => '[name="params[filters][update_time][from]"]',
+            'selector' => '[name="filters[update_time][from]"]',
         ],
-        'store_id' => [
-            'selector' => '[name="params[filters][store_id]"]',
-            'input' => 'selectstore'
+        'update_time_to' => [
+            'selector' => '[name="filters[update_time][to]"]',
         ],
     ];
 }
