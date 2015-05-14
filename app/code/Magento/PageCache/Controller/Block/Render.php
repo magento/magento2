56a7aec10e260709b6e5fb7f03181329b9ef08a3
Cristian Partica <cpartica@ebay.com>
2015-05-14 16:27:13 -0500

    MAGETWO-34224: Url is not correct for private content
    - fix by passing the original components of the url to the configuration and recomposing it with js when that private content is needed

--- app/code/Magento/PageCache/Controller/Block/Render.php
+++ app/code/Magento/PageCache/Controller/Block/Render.php
@@ -21,11 +21,26 @@ class Render extends \Magento\PageCache\Controller\Block
         }
         // disable profiling during private content handling AJAX call
         \Magento\Framework\Profiler::reset();
+        $currentRoute = $this->getRequest()->getRouteName();
+        $currentControllerName = $this->getRequest()->getControllerName();
+        $currentActionName = $this->getRequest()->getActionName();
+
+        $origRequest = $this->getRequest()->getParam('originalRequest');
+        $origRequest = json_decode($origRequest, true);
+        $this->getRequest()->setRouteName($origRequest['route']);
+        $this->getRequest()->setControllerName($origRequest['controller']);
+        $this->getRequest()->setActionName($origRequest['action']);
+        /** @var \Magento\Framework\View\Element\BlockInterface[] $blocks */
         $blocks = $this->_getBlocks();
         $data = [];
+
         foreach ($blocks as $blockName => $blockInstance) {
             $data[$blockName] = $blockInstance->toHtml();
         }
+
+        $this->getRequest()->setRouteName($currentRoute);
+        $this->getRequest()->setControllerName($currentControllerName);
+        $this->getRequest()->setActionName($currentActionName);
 
         $this->getResponse()->setPrivateHeaders(\Magento\PageCache\Helper\Data::PRIVATE_MAX_AGE_CACHE);
         $this->translateInline->processResponseBody($data);
