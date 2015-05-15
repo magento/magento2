4d96ff26b01c742d7a32abbba3863ed08e1eeeee
Ievgen Shakhsuvarov <ishakhsuvarov@ebay.com>
2015-05-15 18:31:24 +0300

    MAGETWO-36196: Fix functional build (FAT)
     - Stabilization

--- dev/tests/functional/tests/app/Magento/Checkout/Test/Block/Onepage/Billing.php
+++ dev/tests/functional/tests/app/Magento/Checkout/Test/Block/Onepage/Billing.php
@@ -38,6 +38,13 @@ class Billing extends Form
     protected $waitElement = '.loading-mask';
 
     /**
+     * Field wrapper with label on form.
+     *
+     * @var string
+     */
+    protected $fieldLabel = '#billing-new-address-form > .required';
+
+    /**
      * Fill billing address
      *
      * @param Address $billingAddress
@@ -48,9 +55,11 @@ class Billing extends Form
         Address $billingAddress = null,
         $isShippingAddress = false
     ) {
+        $this->waitFields();
         if ($billingAddress) {
             $this->fill($billingAddress);
         }
+
         if ($isShippingAddress) {
             $this->_rootElement->find($this->useForShipping)->click();
         }
@@ -73,5 +82,17 @@ class Billing extends Form
                 return $element->isVisible() == false ? true : null;
             }
         );
+    }
+
+    /**
+     * Wait for User before fill form which calls JS validation on correspondent fields of form.
+     * See details in MAGETWO-31435.
+     *
+     * @return void
+     */
+    protected function waitFields()
+    {
+        /* Wait for field label is visible in the form */
+        $this->waitForElementVisible($this->fieldLabel);
     }
 }
