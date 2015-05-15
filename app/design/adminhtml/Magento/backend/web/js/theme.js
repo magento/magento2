8d8ce3c9e6f67a89f7f5d1804005c37e5b4ca32b
Bogdan Plieshka <bplieshka@ebay.com>
2015-05-15 18:45:57 +0300

    MAGETWO-34929: JS: Smart fixed scroll
    - Added menu smart scroll script
    - Changed menu and page content structure styles
    - Replaced clearfix mixin to extend

--- app/design/adminhtml/Magento/backend/web/js/theme.js
+++ app/design/adminhtml/Magento/backend/web/js/theme.js
@@ -3,9 +3,116 @@
  * See COPYING.txt for license details.
  */
 
+define('globalNavigationScroll', [
+    'jquery'
+], function ($) {
+    'use strict';
+
+    var win = $(window),
+        menu = $('.menu-wrapper'),
+        content = $('.page-wrapper'),
+        winHeight,
+        menuHeight = menu.height(),
+        menuHeightRest = 0,
+        contentHeight,
+        winTop = 0,
+        winTopLast = 0,
+        scrollStep = 0,
+        nextTop = 0,
+        fixedClass = '_fixed';
+
+    function isMenuFixed() {
+        return (menuHeight < contentHeight) && (contentHeight > winHeight);
+    }
+
+    function addFixed(el) {
+        if (!el.hasClass(fixedClass)) {
+            el.addClass(fixedClass);
+        }
+    }
+
+    function removeFixed(el) {
+        if (el.hasClass(fixedClass)) {
+            el.removeClass(fixedClass);
+        }
+    }
+
+    function positionMenu() {
+
+        //  Spot positions and heights
+        winHeight = win.height();
+        contentHeight = content.height();
+        winTop = win.scrollTop();
+        scrollStep = winTop - winTopLast; // scroll step
+        menuHeightRest = menuHeight - winTop; // visible menu height
+
+        // Fixed menu cases
+        if (isMenuFixed()) {
+
+            addFixed(menu);
+
+            //  Smart scroll cases
+            if (menuHeight > winHeight) {
+
+                //  Scroll down
+                if (winTop > winTopLast) {
+
+                    var menuScrollMax = menuHeight - winHeight;
+
+                    nextTop < (menuScrollMax - scrollStep) ?
+                        nextTop += scrollStep : nextTop = menuScrollMax;
+
+                    menu.css('top', -nextTop);
+
+                }
+                //  Scroll up
+                else if (winTop < winTopLast) {
+
+                    nextTop > -scrollStep ?
+                        nextTop += scrollStep : nextTop = 0;
+
+                    menu.css('top', -nextTop);
+
+                }
+
+            }
+            //  Static menu cases
+        } else {
+            removeFixed(menu);
+        }
+
+        //  Save previous window scrollTop
+        winTopLast = winTop;
+
+    }
+
+    //  Page start calculation
+    positionMenu();
+
+    //  Change position on scroll
+    win.on('scroll', function () {
+        positionMenu();
+    });
+
+    win.on('resize', function () {
+
+        winHeight = win.height();
+
+        //  Reset position if fixed and out of smart scroll
+        if (
+            (menuHeight < contentHeight) && (menuHeight <= winHeight)
+        ) {
+            menu.removeAttr('style');
+        }
+
+    });
+
+});
+
 define('globalNavigation', [
     'jquery',
-    'jquery/ui'
+    'jquery/ui',
+    'globalNavigationScroll'
 ], function ($) {
     'use strict';
 
