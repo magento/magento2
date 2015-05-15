fa1dcde9218de7f956f95805279bf4286a8091f4
Maksym Savich <msavich@ebay.com>
2015-05-15 13:48:31 -0500

    MAGETWO-36918: Versions tab is absent after publish CMS page on update

--- app/code/Magento/Cms/Test/Unit/Ui/Component/Listing/Column/PageActionsTest.php
+++ app/code/Magento/Cms/Test/Unit/Ui/Component/Listing/Column/PageActionsTest.php
@@ -3,7 +3,7 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\Cms\Test\Unit\Model\Ui\Component\Listing\Column;
+namespace Magento\Cms\Test\Unit\Ui\Component\Listing\Column;
 
 class PageActionsTest extends \PHPUnit_Framework_TestCase
 {
