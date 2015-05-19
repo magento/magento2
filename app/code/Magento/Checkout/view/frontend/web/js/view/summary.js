c85e298f7f850743fcbb43f9edba0b783ad9bc9d
Serhiy Shkolyarenko <sshkolyarenko@ebay.com>
2015-05-19 18:56:36 +0300

    MAGETWO-37626: Update Order Summary  information based on Shipping information
    layout updates

---
+++ app/code/Magento/Checkout/view/frontend/web/js/view/summary.js
@@ -0,0 +1,19 @@
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+/*browser:true*/
+/*global define*/
+define(
+    [
+        'uiComponent'
+    ],
+    function (Component) {
+        "use strict";
+        return Component.extend({
+            defaults: {
+                displayArea: 'summary'
+            }
+        });
+    }
+);
