434c12918a0a3a35689535db9c74ab203fb880e9
Arkadii Chyzhov <achyzhov@ebay.com>
2015-05-20 17:29:41 +0300

    MAGETWO-37017: Cover \Magento\Integration\Model\Config\Integration\*
    - marked class Reader with @codeCoverageIgnore
    - added unit testcase to SchemaLocator

--- app/code/Magento/Integration/Model/Config/Integration/Reader.php
+++ app/code/Magento/Integration/Model/Config/Integration/Reader.php
@@ -14,6 +14,7 @@ class Reader extends \Magento\Framework\Config\Reader\Filesystem
      * List of id attributes for merge
      *
      * @var array
+     * @codeCoverageIgnore
      */
     protected $_idAttributes = [
         '/integrations/integration' => 'name',
