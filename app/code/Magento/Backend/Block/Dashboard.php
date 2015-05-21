917948868c96503f37eb9a4eb63a64a805f32d0e
Arkadii Chyzhov <achyzhov@ebay.com>
2015-05-21 15:07:43 +0300

    MAGETWO-33618: Merchant isn't redirected to correspondent option if wants to enable Dashboard charts
    - fixed code style

--- app/code/Magento/Backend/Block/Dashboard.php
+++ app/code/Magento/Backend/Block/Dashboard.php
@@ -31,11 +31,11 @@ class Dashboard extends \Magento\Backend\Block\Template
 
         $this->addChild('sales', 'Magento\Backend\Block\Dashboard\Sales');
 
-        if ($this->_scopeConfig->getValue(
+        $isEnabledCharts = $this->_scopeConfig->getValue(
             self::XML_PATH_ENABLE_CHARTS,
             \Magento\Store\Model\ScopeInterface::SCOPE_STORE
-        )
-        ) {
+        );
+        if ($isEnabledCharts) {
             $block = $this->getLayout()->createBlock('Magento\Backend\Block\Dashboard\Diagrams');
         } else {
             $block = $this->getLayout()->createBlock(
