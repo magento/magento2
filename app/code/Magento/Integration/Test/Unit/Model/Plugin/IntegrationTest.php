0487e9100f36ffc9c221b1ab3adc4e42a8de7875
Yuxing Zheng <yuxzheng@ebay.com>
2015-05-22 15:26:02 -0500

    MAGETWO-37024: Cover other classes under Magento\Integration\Model
     - Added unit tests for classes under Magento\Integration\Model namespace
     - Fixed a variable validation in \Magento\Integration\Model\CredentialsValidator

--- app/code/Magento/Integration/Test/Unit/Model/Plugin/IntegrationTest.php
+++ app/code/Magento/Integration/Test/Unit/Model/Plugin/IntegrationTest.php
@@ -8,6 +8,9 @@ namespace Magento\Integration\Test\Unit\Model\Plugin;
 use Magento\Authorization\Model\Acl\AclRetriever;
 use Magento\Integration\Model\Integration;
 
+/**
+ * Unit test for \Magento\Integration\Model\Plugin\Integration
+ */
 class IntegrationTest extends \PHPUnit_Framework_TestCase
 {
     /**
@@ -60,5 +63,122 @@ class IntegrationTest extends \PHPUnit_Framework_TestCase
             ->method('removePermissions')
             ->with($integrationId);
         $this->integrationPlugin->afterDelete($this->subjectMock, $integrationsData);
+    }
+
+    public function testAfterCreateAllResources()
+    {
+        $integrationId = 1;
+        $integrationModelMock = $this->getMockBuilder('Magento\Integration\Model\Integration')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $integrationModelMock->expects($this->exactly(2))
+            ->method('getId')
+            ->will($this->returnValue($integrationId));
+        $integrationModelMock->expects($this->once())
+            ->method('getData')
+            ->with('all_resources')
+            ->will($this->returnValue(1));
+
+        $this->integrationAuthServiceMock->expects($this->once())
+            ->method('grantAllPermissions')
+            ->with($integrationId);
+
+        $this->integrationPlugin->afterCreate($this->subjectMock, $integrationModelMock);
+    }
+
+    public function testAfterCreateSomeResources()
+    {
+        $integrationId = 1;
+        $integrationModelMock = $this->getMockBuilder('Magento\Integration\Model\Integration')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $integrationModelMock->expects($this->exactly(2))
+            ->method('getId')
+            ->will($this->returnValue($integrationId));
+        $integrationModelMock->expects($this->at(1))
+            ->method('getData')
+            ->with('all_resources')
+            ->will($this->returnValue(null));
+        $integrationModelMock->expects($this->at(2))
+            ->method('getData')
+            ->with('resource')
+            ->will($this->returnValue(['testResource']));
+        $integrationModelMock->expects($this->at(4))
+            ->method('getData')
+            ->with('resource')
+            ->will($this->returnValue(['testResource']));
+
+        $this->integrationAuthServiceMock->expects($this->once())
+            ->method('grantPermissions')
+            ->with($integrationId, ['testResource']);
+
+        $this->integrationPlugin->afterCreate($this->subjectMock, $integrationModelMock);
+    }
+
+    public function testAfterCreateNoResource()
+    {
+        $integrationId = 1;
+        $integrationModelMock = $this->getMockBuilder('Magento\Integration\Model\Integration')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $integrationModelMock->expects($this->exactly(2))
+            ->method('getId')
+            ->will($this->returnValue($integrationId));
+        $integrationModelMock->expects($this->at(1))
+            ->method('getData')
+            ->with('all_resources')
+            ->will($this->returnValue(null));
+        $integrationModelMock->expects($this->at(2))
+            ->method('getData')
+            ->with('resource')
+            ->will($this->returnValue(null));
+
+        $this->integrationAuthServiceMock->expects($this->once())
+            ->method('grantPermissions')
+            ->with($integrationId, []);
+
+        $this->integrationPlugin->afterCreate($this->subjectMock, $integrationModelMock);
+    }
+
+    public function testAfterUpdateAllResources()
+    {
+        $integrationId = 1;
+        $integrationModelMock = $this->getMockBuilder('Magento\Integration\Model\Integration')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $integrationModelMock->expects($this->exactly(2))
+            ->method('getId')
+            ->will($this->returnValue($integrationId));
+        $integrationModelMock->expects($this->once())
+            ->method('getData')
+            ->with('all_resources')
+            ->will($this->returnValue(1));
+
+        $this->integrationAuthServiceMock->expects($this->once())
+            ->method('grantAllPermissions')
+            ->with($integrationId);
+
+        $this->integrationPlugin->afterUpdate($this->subjectMock, $integrationModelMock);
+    }
+
+    public function testAfterGet()
+    {
+        $integrationId = 1;
+        $integrationModelMock = $this->getMockBuilder('Magento\Integration\Model\Integration')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $integrationModelMock->expects($this->exactly(2))
+            ->method('getId')
+            ->will($this->returnValue($integrationId));
+        $integrationModelMock->expects($this->once())
+            ->method('setData')
+            ->with('resource', ['testResource']);
+
+        $this->aclRetrieverMock->expects($this->once())
+            ->method('getAllowedResourcesByUser')
+            ->with(\Magento\Authorization\Model\UserContextInterface::USER_TYPE_INTEGRATION, $integrationId)
+            ->will($this->returnValue(['testResource']));
+
+        $this->integrationPlugin->afterGet($this->subjectMock, $integrationModelMock);
     }
 }
