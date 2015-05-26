49fad51476919cecd3a1001ee37161051e53a018
Sergey Semenov <ssemenov@ebay.com>
2015-05-26 17:08:53 +0300

    MAGETWO-36215: Second Product isn't added to Shopping Cart from Wishlist from first atempt

--- dev/tests/functional/tests/app/Magento/Wishlist/Test/TestCase/AddProductsToCartFromCustomerWishlistOnFrontendTest.php
+++ dev/tests/functional/tests/app/Magento/Wishlist/Test/TestCase/AddProductsToCartFromCustomerWishlistOnFrontendTest.php
@@ -77,7 +77,6 @@ class AddProductsToCartFromCustomerWishlistOnFrontendTest extends AbstractWishli
             $this->cmsIndex->getCmsPageBlock()->waitPageInit();
             if (!$this->wishlistIndex->getWishlistBlock()->isVisible()) {
                 $this->catalogProductView->getViewBlock()->addToCart($product);
-                $this->catalogProductView->getMessagesBlock()->waitSuccessMessage();
             }
         }
     }
