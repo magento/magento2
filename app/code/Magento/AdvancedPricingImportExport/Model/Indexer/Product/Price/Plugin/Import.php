010089eb56fd63dfcd71dd4a1dcda815927c9cc0
Siarhei Andreyeu <Siearhei_Andreyeu@epam.com>
2015-05-27 12:32:29 +0300

    MAGNIMEX-183: Import advanced prices
    - price import plugin

---
+++ app/code/Magento/AdvancedPricingImportExport/Model/Indexer/Product/Price/Plugin/Import.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\AdvancedPricingImportExport\Model\Indexer\Product\Price\Plugin;
+
+class Import extends \Magento\Catalog\Model\Indexer\Product\Price\Plugin\AbstractPlugin
+{
+    /**
+     * After import handler
+     */
+    public function afterSaveAdvancedPricing()
+    {
+        $this->invalidateIndexer();
+    }
+}
