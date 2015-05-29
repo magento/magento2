7f8439b2130df2d528fb2fcd409410f55f837d29
Alex Akimov <aakimov@ebay.com>
2015-05-29 11:34:46 +0300

    MAGETWO-37987: Implement address validation and validation rules for each carrier

---
+++ app/code/Magento/Usps/view/frontend/web/js/model/shipping-rates-validation-rules.js
@@ -0,0 +1,25 @@
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+/*global define*/
+define(
+    [],
+    function () {
+        "use strict";
+        return {
+            getRules: function() {
+                //TODO: example rules
+                return {
+                    'postcode': {
+                        'required': true,
+                        'min-length': 3
+                    },
+                    'countryId': {
+                        'required': true
+                    }
+                };
+            }
+        };
+    }
+);
