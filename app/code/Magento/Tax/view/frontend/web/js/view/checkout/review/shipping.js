2a17b38c417466999de0cef210dcb2fcf7e36198
Serhiy Shkolyarenko <sshkolyarenko@ebay.com>
2015-06-02 15:26:29 +0300

    MAGETWO-37910: Summary updates and merging
    taxes refactoring

--- app/code/Magento/Tax/view/frontend/web/js/view/checkout/review/shipping.js
+++ app/code/Magento/Tax/view/frontend/web/js/view/checkout/review/shipping.js
@@ -7,7 +7,7 @@
 define(
     [
         'jquery',
-        'uiComponent',
+        'Magento_Checkout/js/view/shipping',
         'Magento_Checkout/js/model/quote',
         'Magento_Catalog/js/price-utils',
         'Magento_Checkout/js/model/shipping-service'
@@ -19,24 +19,12 @@ define(
                 displayMode: displayMode,
                 template: 'Magento_Tax/checkout/review/shipping'
             },
-            getColspan: 3,
-            style: "",
-            notCalculatedMessage: 'Not yet calculated',
-            quoteIsVirtual: quote.isVirtual(),
-            selectedShippingMethod: quote.getSelectedShippingMethod(),
-            getTitle: function() {
-                return "Shipping";
-            },
             getExcludingLabel: function() {
                 return "Shipping Excl. Tax";
             },
             getIncludingLabel: function() {
                 return "Shipping Incl. Tax";
             },
-            getShippingMethodTitle: function() {
-                return shippingService.getTitleByCode(this.selectedShippingMethod())
-            },
-            totals: quote.getTotals(),
             isBothPricesDisplayed: function() {
                 return 'both' == this.displayMode
             },
@@ -45,15 +33,6 @@ define(
             },
             isExcludingDisplayed: function() {
                 return 'excluding' == this.displayMode;
-            },
-            getValue: function() {
-                var price = 0;
-                if (this.totals() && this.totals().shipping_amount) {
-                    price =  this.totals().shipping_amount;
-                } else {
-                    return this.notCalculatedMessage;
-                }
-                return priceUtils.formatPrice(price, quote.getPriceFormat());
             },
             getIncludingValue: function() {
                 var price = 0;
