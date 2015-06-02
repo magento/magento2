91f0c78323bd64116ceb20343a35ba7dcd80bb79
Serhiy Shkolyarenko <sshkolyarenko@ebay.com>
2015-06-02 19:28:12 +0300

    MAGETWO-37910: Summary updates and merging
    weee module updates

--- app/code/Magento/Weee/view/frontend/web/js/view/checkout/summary/item/price/row_excl_tax.js
+++ app/code/Magento/Weee/view/frontend/web/js/view/checkout/summary/item/price/row_excl_tax.js
@@ -6,13 +6,13 @@
 /*global define*/
 define(
     [
-        'Magento_Weee/js/view/checkout/review/item/price/weee'
+        'Magento_Weee/js/view/checkout/summary/item/price/weee'
     ],
     function (weee) {
         "use strict";
         return weee.extend({
             defaults: {
-                template: 'Magento_Weee/checkout/review/item/price/row_excl_tax'
+                template: 'Magento_Weee/checkout/summary/item/price/row_excl_tax'
             },
 
             getFinalRowDisplayPriceExclTax: function(item) {
