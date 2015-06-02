d1df5485a21d8335410f00ab5f58adcb88be42f5
David Alger <david@classyllama.com>
2015-06-02 11:51:54 -0500

    MAGETWO-37589: Email Templates Localization #2
    - Added unit tests for {{trans ...}} directive

---
+++ app/code/Magento/Email/Test/Unit/Model/Template/FilterTest.php
@@ -0,0 +1,201 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Email\Test\Unit\Model\Template;
+
+class FilterTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \Magento\Email\Model\Template\Filter
+     */
+    protected $filter;
+
+    /**
+     * @var $string \Magento\Framework\Stdlib\String
+     */
+    private $string;
+
+    /**
+     * @var $logger \Psr\Log\LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @var $escaper \Magento\Framework\Escaper
+     */
+    private $escaper;
+
+    /**
+     * @var $assetRepo \Magento\Framework\View\Asset\Repository
+     */
+    private $assetRepo;
+
+    /**
+     * @var $scopeConfig \Magento\Framework\App\Config\ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
+     * @var $coreVariableFactory \Magento\Variable\Model\VariableFactory
+     */
+    private $coreVariableFactory;
+
+    /**
+     * @var $storeManager \Magento\Store\Model\StoreManagerInterface
+     */
+    private $storeManager;
+
+    /**
+     * @var $layout \Magento\Framework\View\LayoutInterface
+     */
+    private $layout;
+
+    /**
+     * @var $layoutFactory \Magento\Framework\View\LayoutFactory
+     */
+    private $layoutFactory;
+
+    /**
+     * @var $appState \Magento\Framework\App\State
+     */
+    private $appState;
+
+    /**
+     * @var $backendUrlBuilder \Magento\Backend\Model\UrlInterface
+     */
+    private $backendUrlBuilder;
+
+
+    protected function setUp()
+    {
+        $this->string = $this->getMockBuilder('\Magento\Framework\Stdlib\String')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->logger = $this->getMockBuilder('\Psr\Log\LoggerInterface')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->escaper = $this->getMockBuilder('\Magento\Framework\Escaper')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->assetRepo = $this->getMockBuilder('\Magento\Framework\View\Asset\Repository')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->scopeConfig = $this->getMockBuilder('\Magento\Framework\App\Config\ScopeConfigInterface')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->coreVariableFactory = $this->getMockBuilder('\Magento\Variable\Model\VariableFactory')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->storeManager = $this->getMockBuilder('\Magento\Store\Model\StoreManagerInterface')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->layout = $this->getMockBuilder('\Magento\Framework\View\LayoutInterface')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->layoutFactory = $this->getMockBuilder('\Magento\Framework\View\LayoutFactory')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->appState = $this->getMockBuilder('\Magento\Framework\App\State')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->backendUrlBuilder = $this->getMockBuilder('\Magento\Backend\Model\UrlInterface')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->filter = new \Magento\Email\Model\Template\Filter(
+            $this->string,
+            $this->logger,
+            $this->escaper,
+            $this->assetRepo,
+            $this->scopeConfig,
+            $this->coreVariableFactory,
+            $this->storeManager,
+            $this->layout,
+            $this->layoutFactory,
+            $this->appState,
+            $this->backendUrlBuilder,
+            []
+        );
+    }
+
+    /**
+     * Tests proper parsing of the {{trans ...}} directive used in email templates
+     *
+     * @dataProvider transDirectiveScenarios
+     */
+    public function testTransDirective($value, $expected)
+    {
+        $this->assertEquals($expected, $this->filter->filter($value));
+    }
+
+    /**
+     * Data provider for various possible {{trans ...}} usages
+     *
+     * @return array
+     */
+    public function transDirectiveScenarios() {
+        return [
+            [   // empty string
+                '{{trans}}',
+                '',
+            ],
+
+            [   // given empty string
+                '{{trans ""}}',
+                '',
+            ],
+
+            [   // no padding
+                '{{trans"Hello cruel coder..."}}',
+                'Hello cruel coder...',
+            ],
+
+            [   // excessive padding
+                "{{trans \t\n\r'Hello cruel coder...' \t\n\r}}",
+                'Hello cruel coder...',
+            ],
+
+            [   // escaped double-quotes
+                '{{trans "Hello \"tested\" world!"}}',
+                'Hello "tested" world!',
+            ],
+
+            [   // escaped single-quotes
+                "{{trans 'Hello \\'tested\\' world!'}}",
+                "Hello 'tested' world!",
+            ],
+
+            [   // basic var
+                '{{trans "Hello %adjective world!" adjective="tested"}}',
+                'Hello tested world!',
+            ],
+
+            [   // auto-escaped var
+                '{{trans "Hello %adjective world!" adjective="<bad>"}}',
+                'Hello &gt;bad&lt; world!',
+            ],
+
+            [   // unescaped var
+                '{{trans "Hello %adjective world!" adjective="<bad>"|raw}}',
+                'Hello <bad> world!',
+            ],
+
+            [   // object.var
+                '{{trans "Hello %adjective world!" adjective="$world.personality"|raw}}',
+                'Hello happy world!',
+            ],
+        ];
+    }
+}
