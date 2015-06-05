0c5de1b824f585bd9614cba9c6d719832fbdebda
Andrii Lugovyi <alugovyi@ebay.com>
2015-06-05 22:09:48 +0300

    MAGETWO-37130: 2nd step. Attribute Options management
    - MAGETWO-38145: Implement management options

--- app/code/Magento/ConfigurableProduct/view/adminhtml/web/js/variations/steps/attributes_values.js
+++ app/code/Magento/ConfigurableProduct/view/adminhtml/web/js/variations/steps/attributes_values.js
@@ -3,31 +3,57 @@
  * See COPYING.txt for license details.
  */
 define([
-    "uiComponent",
-    "jquery"
-], function (Component, $) {
-    "use strict";
+    'uiComponent',
+    'jquery',
+    'ko',
+    'underscore',
+    'Magento_Ui/js/lib/collapsible'
+], function (Component, $, ko, _, Collapsible) {
+    'use strict';
 
-    return Component.extend({
-        initialize: function () {
-            this._super();
+    var viewModel = Collapsible.extend({
+        attributes: ko.observableArray([]),
+        createOption: function (attribute) {
+            attribute.options.push({id:0, label:''});
         },
-        title: 'step1',
-        render: function (wizard) {
+        saveOption: function (option) {
+            this.options.remove(option);
+            //TODO: ajax request to save attribute
+            var id = _.uniqueId()+this.id;
+            this.options.push({id:id, label:option.label});
+            this.chosenOptions.push(id);
+        },
+        removeAttribute: function (attribute) {
+            viewModel.prototype.attributes.remove(attribute);
+        },
+        removeOption: function (option) {
+            this.options.remove(option);
+        },
+        selectAllAttributes: function (attribute) {
+            this.chosenOptions(_.pluck(attribute.options(), 'id'));
+        },
+        showCreateOption: function (element) {
+            $(element).append($('[data-action="addOption"]'));
+        },
+        render: function(wizard) {
             $.ajax({
                 type: "POST",
                 url: this.options_url,
                 data: {attributes: wizard.data},
-                showLoader: true,
-                success: function (res) {
-                    console.log(res)
-                }
+                showLoader: true
+            }).done(function(data){
+                this.attributes(_.map(data.attributes, function (attribute) {
+                    attribute.options = ko.observableArray(attribute.options);
+                    attribute.chosenOptions = ko.observableArray(_.pluck(attribute.options, 'id'));
+                    return attribute;
+                }, this));
             });
-
         },
-        force: function (wizard) {
+        force: function(wizard) {
+            return wizard.data.attributes;
         },
-        back: function (wizard) {
+        back: function(wizard) {
         }
     });
+    return viewModel;
 });
