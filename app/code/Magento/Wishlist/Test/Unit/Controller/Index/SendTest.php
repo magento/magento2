424dd91e3055c407feb36b9ddedd7253300658b8
Natalia Momotenko <nmomotenko@ebay.com>
2015-06-05 17:48:05 +0300

    MAGETWO-36132: Update Content in Magento 2 by Modules - part 4

--- app/code/Magento/Wishlist/Test/Unit/Controller/Index/SendTest.php
+++ app/code/Magento/Wishlist/Test/Unit/Controller/Index/SendTest.php
@@ -377,7 +377,15 @@ class SendTest extends \PHPUnit_Framework_TestCase
             ['test text', 100, null, 1, 0, '', 'Please enter an email address.'],
             ['test text', 100, '', 1, 0, '', 'Please enter an email address.'],
             ['test text', 100, 'user1@example.com', 1, 1, '', 'This wish list can be shared 0 more times.'],
-            ['test text', 100, 'u1@example.com, u2@example.com', 3, 2, '', 'This wish list can be shared 1 more times.'],
+            [
+                'test text',
+                100,
+                'u1@example.com, u2@example.com',
+                3,
+                2,
+                '',
+                'This wish list can be shared 1 more times.'
+            ],
             ['test text', 100, 'wrongEmailAddress', 1, 0, '', 'Please input a valid email address.'],
             ['test text', 100, 'user1@example.com, wrongEmailAddress', 2, 0, '', 'Please input a valid email address.'],
             ['test text', 100, 'wrongEmailAddress, user2@example.com', 2, 0, '', 'Please input a valid email address.'],
