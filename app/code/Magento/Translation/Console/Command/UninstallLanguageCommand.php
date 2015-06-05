3c656cad5dc2103227b93964311ca4db84879140
Ivan Gavryshko <igavryshko@ebay.com>
2015-06-05 12:00:41 -0500

    MAGETWO-38202: Create skelton language uninstall command
    - changes according to CR

--- app/code/Magento/Translation/Console/Command/UninstallLanguageCommand.php
+++ app/code/Magento/Translation/Console/Command/UninstallLanguageCommand.php
@@ -8,15 +8,17 @@ namespace Magento\Translation\Console\Command;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Magento\Framework\Composer\GeneralDependencyChecker;
+use Magento\Framework\Composer\DependencyChecker;
 use Magento\Framework\Composer\Remove;
 use Magento\Framework\Composer\ComposerInformation;
 use Magento\Framework\App\Cache;
+use Magento\Framework\Setup\BackupRollbackFactory;
 
 /**
- * Class UninstallLanguageCommand
+ * Command for uninstalling language and backup-code feature
  */
 class UninstallLanguageCommand extends Command
 {
@@ -24,9 +26,10 @@ class UninstallLanguageCommand extends Command
      * Language code argument name
      */
     const PACKAGE_ARGUMENT = 'package';
+    const BACKUP_CODE_OPTION = 'backup-code';
 
     /**
-     * @var GeneralDependencyChecker
+     * @var DependencyChecker
      */
     private $dependencyChecker;
 
@@ -46,23 +49,31 @@ class UninstallLanguageCommand extends Command
     private $cache;
 
     /**
+     * @var BackupRollbackFactory
+     */
+    private $backupRollbackFactory;
+
+    /**
      * Inject dependencies
      *
-     * @param GeneralDependencyChecker $dependencyChecker
+     * @param DependencyChecker $dependencyChecker
      * @param Remove $remove
      * @param ComposerInformation $composerInfo
      * @param Cache $cache
+     * @param BackupRollbackFactory $backupRollbackFactory
      */
     public function __construct(
-        GeneralDependencyChecker $dependencyChecker,
+        DependencyChecker $dependencyChecker,
         Remove $remove,
         ComposerInformation $composerInfo,
-        Cache $cache
+        Cache $cache,
+        BackupRollbackFactory $backupRollbackFactory
     ) {
         $this->dependencyChecker = $dependencyChecker;
         $this->remove = $remove;
         $this->composerInfo = $composerInfo;
         $this->cache = $cache;
+        $this->backupRollbackFactory = $backupRollbackFactory;
 
         parent::__construct();
     }
@@ -79,7 +90,13 @@ class UninstallLanguageCommand extends Command
                     self::PACKAGE_ARGUMENT,
                     InputArgument::IS_ARRAY | InputArgument::REQUIRED,
                     'Language code'
-                )
+                ),
+                new InputOption(
+                    self::BACKUP_CODE_OPTION,
+                    '-b',
+                    InputOption::VALUE_NONE,
+                    'Take code and configuration files backup (excluding temporary files)'
+                ),
             ]);
 
         parent::configure();
@@ -93,7 +110,7 @@ class UninstallLanguageCommand extends Command
         $languages = $input->getArgument(self::PACKAGE_ARGUMENT);
         $packagesToRemove = [];
 
-        $dependencies = $this->dependencyChecker->checkDependencies($languages);
+        $dependencies = $this->dependencyChecker->checkDependencies($languages, true);
 
         foreach ($languages as $package) {
 
@@ -109,10 +126,19 @@ class UninstallLanguageCommand extends Command
         }
 
         if ($packagesToRemove !== []) {
+
+            if ($input->getOption(self::BACKUP_CODE_OPTION)) {
+                $time = time();
+                $backupRestore = $this->backupRollbackFactory->create($output);
+                $backupRestore->codeBackup($time);
+            } else {
+                $output->writeln('<info>You are removing language package without a code backup.</info>');
+            }
+
             $this->remove->remove($packagesToRemove);
             $this->cache->clean();
         } else {
-            $output->writeln('Nothing is removed.');
+            $output->writeln('<info>Nothing is removed.</info>');
         }
     }
 
--- app/code/Magento/Translation/Test/Unit/Console/Command/UninstallLanguageCommandTest.php
+++ app/code/Magento/Translation/Test/Unit/Console/Command/UninstallLanguageCommandTest.php
@@ -6,17 +6,18 @@
 
 namespace Magento\Translation\Test\Unit\Console\Command;
 
-use Magento\Framework\Composer\GeneralDependencyChecker;
+use Magento\Framework\Composer\DependencyChecker;
 use Magento\Framework\Composer\Remove;
 use Magento\Framework\Composer\ComposerInformation;
 use Magento\Framework\App\Cache;
 use Symfony\Component\Console\Tester\CommandTester;
 use Magento\Translation\Console\Command\UninstallLanguageCommand;
+use Magento\Framework\Setup\BackupRollbackFactory;
 
 class UninstallLanguageCommandTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * @var GeneralDependencyChecker|\PHPUnit_Framework_MockObject_MockObject
+     * @var DependencyChecker|\PHPUnit_Framework_MockObject_MockObject
      */
     private $dependencyChecker;
 
@@ -36,6 +37,11 @@ class UninstallLanguageCommandTest extends \PHPUnit_Framework_TestCase
     private $cache;
 
     /**
+     * @var BackupRollbackFactory|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $backupRollbackFactory;
+
+    /**
      * @var UninstallLanguageCommand
      */
     private $command;
@@ -48,7 +54,7 @@ class UninstallLanguageCommandTest extends \PHPUnit_Framework_TestCase
     public function setUp()
     {
         $this->dependencyChecker = $this->getMock(
-            'Magento\Framework\Composer\GeneralDependencyChecker',
+            'Magento\Framework\Composer\DependencyChecker',
             [],
             [],
             '',
@@ -57,13 +63,21 @@ class UninstallLanguageCommandTest extends \PHPUnit_Framework_TestCase
         $this->remove = $this->getMock('Magento\Framework\Composer\Remove', [], [], '', false);
         $this->composerInfo = $this->getMock('Magento\Framework\Composer\ComposerInformation', [], [], '', false);
         $this->cache = $this->getMock('Magento\Framework\App\Cache', [], [], '', false);
+        $this->backupRollbackFactory = $this->getMock(
+            'Magento\Framework\Setup\BackupRollbackFactory',
+            [],
+            [],
+            '',
+            false
+        );
 
 
         $this->command = new UninstallLanguageCommand(
             $this->dependencyChecker,
             $this->remove,
             $this->composerInfo,
-            $this->cache
+            $this->cache,
+            $this->backupRollbackFactory
         );
 
         $this->tester = new CommandTester($this->command);
@@ -86,10 +100,42 @@ class UninstallLanguageCommandTest extends \PHPUnit_Framework_TestCase
                 ]
             );
 
+        $backupRollback = $this->getMock('Magento\Framework\Setup\BackupRollback', [], [], '', false);
+        $backupRollback->expects($this->once())->method('codeBackup');
+
+        $this->backupRollbackFactory->expects($this->once())
+            ->method('create')
+            ->willReturn($backupRollback);
+
+        $this->remove->expects($this->once())->method('remove');
+        $this->cache->expects($this->once())->method('clean');
+
+        $this->tester->execute(['package' => ['vendor/language-ua_ua'], '--backup-code' => true]);
+    }
+
+    public function testExecuteNoBackupOption()
+    {
+        $dependencies['vendor/language-ua_ua'] = [];
+
+        $this->dependencyChecker->expects($this->once())
+            ->method('checkDependencies')
+            ->with(['vendor/language-ua_ua'])
+            ->willReturn($dependencies);
+
+        $this->composerInfo->expects($this->once())
+            ->method('getRootRequiredPackagesAndTypes')
+            ->willReturn(
+                [
+                    'vendor/language-ua_ua' => 'magento2-language'
+                ]
+            );
+
+        $this->backupRollbackFactory->expects($this->never())->method('create');
         $this->remove->expects($this->once())->method('remove');
         $this->cache->expects($this->once())->method('clean');
 
         $this->tester->execute(['package' => ['vendor/language-ua_ua']]);
+        $this->assertContains('You are removing language package without a code backup.', $this->tester->getDisplay());
     }
 
     public function testExecutePackageHasDependency()
@@ -119,6 +165,8 @@ class UninstallLanguageCommandTest extends \PHPUnit_Framework_TestCase
         );
         $this->assertContains('Nothing is removed.', $this->tester->getDisplay());
     }
+
+
 
     public function testExecutePackageNoLanguage()
     {
