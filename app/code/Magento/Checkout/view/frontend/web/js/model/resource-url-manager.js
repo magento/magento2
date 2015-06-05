1df4802a46d2ce82d8274797a540564019f0c140
Stanislav Idolov <sidolov@ebay.com>
2015-06-05 15:57:11 +0300

    MAGETWO-38340: Create new model that provides API resources URLs for guest and customer checkout

---
+++ app/code/Magento/Checkout/view/frontend/web/js/model/resource-url-manager.js
@@ -0,0 +1,41 @@
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+/*jshint browser:true jquery:true*/
+/*global alert*/
+define(
+    [
+        'Magento_Customer/js/model/customer',
+        'Magento_Checkout/js/model/url-builder',
+        'Magento_Checkout/js/model/resource-urls',
+        'mageUtils'
+    ],
+    function(customer, urlBuilder, resourceUrls, utils) {
+        "use strict";
+        return {
+            /** Get url for service */
+            getUrl: function(serviceCallName, urlParams) {
+                var checkoutMethod = customer.isLoggedIn()() ? 'customer' : 'guest',
+                    serviceUrls = resourceUrls.getServiceUrls(),
+                    url,
+                    params = {};
+
+                if (utils.isEmpty(serviceUrls[serviceCallName])) {
+                    return 'Provided service call does not exist.';
+                }
+
+                if (!utils.isEmpty(serviceUrls[serviceCallName]['default'])) {
+                    url = serviceUrls[serviceCallName]['default'];
+                } else {
+                    url = serviceUrls[serviceCallName][checkoutMethod];
+                }
+
+                if (urlParams && typeof(urlParams[checkoutMethod]) != "undefined") {
+                    params = urlParams[checkoutMethod];
+                }
+                return urlBuilder.createUrl(url, params);
+            }
+        };
+    }
+);
