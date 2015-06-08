97f5625ce6a10c18432c4bdb6c1ebce7c0c2ad73
Sergey Semenov <ssemenov@ebay.com>
2015-06-08 16:56:17 +0300

    MAGETWO-36041: Magento\Framework\Data\Form does not accept data-mage-init parameter

--- app/code/Magento/Backend/Block/Widget/Form.php
+++ app/code/Magento/Backend/Block/Widget/Form.php
@@ -111,6 +111,13 @@ class Form extends \Magento\Backend\Block\Widget
         $this->_form = $form;
         $this->_form->setParent($this);
         $this->_form->setBaseUrl($this->_urlBuilder->getBaseUrl());
+
+        $customAttributes = $this->getData('custom_attributes');
+        if (is_array($customAttributes)) {
+            foreach ($customAttributes as $key => $value) {
+                $this->_form->addCustomAttribute($key, $value);
+            }
+        }
         return $this;
     }
 
