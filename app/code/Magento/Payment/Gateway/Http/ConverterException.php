7759b07ccea0b6f03a97a46ec92c089633427af3
Dmytro Kvashnin <dkvashnin@ebay.com>
2015-06-08 15:12:41 +0300

    MAGETWO-38191: Contribute payment service API to mainline
    - Added TMap type
    - Implemented Gateway\Command infrastructure for Payment module
    - PaymentMethodChecksInterface eliminated usages
    - Extracted interface from AbstractMethod to MethodInterface
    - Created method configuration and handling infrastructure
    - Fixed client code to pass store id as int
    - Created interfaces for command infrastructure
    - Created command pattern
    - Created request builder composite
    - Implemented basic Gateway
    - Implemented Handler chain/interface
    - Created Validators infrastructure
    - Removed redundant methods from interface
    - Created adapter form AbstractMethod
    - Added exceptions for general cases

---
+++ app/code/Magento/Payment/Gateway/Http/ConverterException.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Payment\Gateway\Http;
+
+use Magento\Framework\Exception\LocalizedException;
+
+class ConverterException extends LocalizedException
+{
+}
