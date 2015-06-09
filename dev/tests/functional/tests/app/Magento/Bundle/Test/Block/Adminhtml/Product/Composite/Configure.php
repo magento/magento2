3fd6a6821924fdca6aecfca9d177bb48a87d7b67
Volodymyr Sevostianov <vsevostianov@ebay.com>
2015-06-09 12:17:28 +0300

    MAGETWO-38445: Fix ConfigureProductInCustomerWishlistOnBackendTest functional test

--- dev/tests/functional/tests/app/Magento/Bundle/Test/Block/Adminhtml/Product/Composite/Configure.php
+++ dev/tests/functional/tests/app/Magento/Bundle/Test/Block/Adminhtml/Product/Composite/Configure.php
@@ -19,7 +19,7 @@ class Configure extends \Magento\Catalog\Test\Block\Adminhtml\Product\Composite\
      *
      * @var string
      */
-    protected $option = '//div[@class="composite-bundle"]//label[.="%option_name%"]//following-sibling::*//%selector%';
+    protected $option = '//fieldset[contains(@class,"composite-bundle")]//label[.="%option_name%"]//following-sibling::*//%selector%';
 
     /**
      * Fill options for the product
