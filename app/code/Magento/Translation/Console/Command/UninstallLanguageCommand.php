ff7192a7c915b1ec68631f2996499c245192b0f5
Ivan Gavryshko <igavryshko@ebay.com>
2015-06-09 11:21:51 -0500

    MAGETWO-38202: Create skelton language uninstall command
    - fixed message text

--- app/code/Magento/Translation/Console/Command/UninstallLanguageCommand.php
+++ app/code/Magento/Translation/Console/Command/UninstallLanguageCommand.php
@@ -117,7 +117,7 @@ class UninstallLanguageCommand extends Command
 
         foreach ($languages as $package) {
             if (!$this->validate($package)) {
-                $output->writeln("<info>Package $package is not a magento language and will be skipped.</info>");
+                $output->writeln("<info>Package $package is not a Magento language and will be skipped.</info>");
             } else {
                 if (sizeof($dependencies[$package]) > 0) {
                     $output->writeln("<info>Package $package has dependencies and will be skipped.</info>");
