f72fa7f3a485e23331cade1b487157661b41a4ca
Sviatoslav Mankivskyi <smankivskyi@ebay.com>
2015-06-10 19:30:50 +0300

    MAGETWO-38526: Implement API services for Magento\GiftMessage

--- dev/tests/api-functional/testsuite/Magento/GiftMessage/Api/GuestCartRepositoryTest.php
+++ dev/tests/api-functional/testsuite/Magento/GiftMessage/Api/GuestCartRepositoryTest.php
@@ -8,11 +8,11 @@ namespace Magento\GiftMessage\Api;
 
 use Magento\TestFramework\TestCase\WebapiAbstract;
 
-class CartRepositoryTest extends WebapiAbstract
+class GuestCartRepositoryTest extends WebapiAbstract
 {
     const SERVICE_VERSION = 'V1';
     const SERVICE_NAME = 'giftMessageCartRepositoryV1';
-    const RESOURCE_PATH = '/V1/carts/';
+    const RESOURCE_PATH = '/V1/guest-carts/';
 
     /**
      * @var \Magento\TestFramework\ObjectManager
@@ -32,8 +32,15 @@ class CartRepositoryTest extends WebapiAbstract
         /** @var \Magento\Quote\Model\Quote $quote */
         $quote = $this->objectManager->create('Magento\Quote\Model\Quote');
         $quote->load('message_order_21', 'reserved_order_id');
-
         $cartId = $quote->getId();
+        /** @var \Magento\Quote\Model\QuoteIdMask $quoteIdMask */
+        $quoteIdMask = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()
+            ->create('Magento\Quote\Model\QuoteIdMaskFactory')
+            ->create();
+        $quoteIdMask->load($cartId, 'quote_id');
+        //Use masked cart Id
+        $cartId = $quoteIdMask->getMaskedId();
+
         $serviceInfo = [
             'rest' => [
                 'resourcePath' => self::RESOURCE_PATH . $cartId . '/gift-message',
@@ -61,42 +68,6 @@ class CartRepositoryTest extends WebapiAbstract
     }
 
     /**
-     * @magentoApiDataFixture Magento/GiftMessage/_files/quote_with_customer_and_message.php
-     */
-    public function testGetForMyCart()
-    {
-        $this->_markTestAsRestOnly();
-
-        // get customer ID token
-        /** @var \Magento\Integration\Api\CustomerTokenServiceInterface $customerTokenService */
-        $customerTokenService = $this->objectManager->create(
-            'Magento\Integration\Api\CustomerTokenServiceInterface'
-        );
-        $token = $customerTokenService->createCustomerAccessToken('customer@example.com', 'password');
-
-        $serviceInfo = [
-            'rest' => [
-                'resourcePath' => self::RESOURCE_PATH . 'mine/gift-message',
-                'httpMethod' => \Magento\Framework\Webapi\Rest\Request::HTTP_METHOD_GET,
-                'token' => $token,
-            ],
-        ];
-
-        $expectedMessage = [
-            'recipient' => 'Mercutio',
-            'sender' => 'Romeo',
-            'message' => 'I thought all for the best.',
-        ];
-
-        $requestData = [];
-        $resultMessage = $this->_webApiCall($serviceInfo, $requestData);
-        $this->assertCount(5, $resultMessage);
-        unset($resultMessage['gift_message_id']);
-        unset($resultMessage['customer_id']);
-        $this->assertEquals($expectedMessage, $resultMessage);
-    }
-
-    /**
      * @magentoApiDataFixture Magento/GiftMessage/_files/quote_with_item_message.php
      */
     public function testSave()
@@ -107,8 +78,15 @@ class CartRepositoryTest extends WebapiAbstract
         /** @var \Magento\Quote\Model\Quote $quote */
         $quote = $this->objectManager->create('Magento\Quote\Model\Quote');
         $quote->load('test_order_item_with_message', 'reserved_order_id');
-
         $cartId = $quote->getId();
+        /** @var \Magento\Quote\Model\QuoteIdMask $quoteIdMask */
+        $quoteIdMask = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()
+            ->create('Magento\Quote\Model\QuoteIdMaskFactory')
+            ->create();
+        $quoteIdMask->load($cartId, 'quote_id');
+        //Use masked cart Id
+        $cartId = $quoteIdMask->getMaskedId();
+
         $serviceInfo = [
             'rest' => [
                 'resourcePath' => self::RESOURCE_PATH . $cartId . '/gift-message',
@@ -130,52 +108,6 @@ class CartRepositoryTest extends WebapiAbstract
             ],
         ];
         $this->assertTrue($this->_webApiCall($serviceInfo, $requestData));
-        $quote->load('test_order_item_with_message', 'reserved_order_id');
-        $quote->getGiftMessageId();
-        /** @var  \Magento\GiftMessage\Model\Message $message */
-        $message = $this->objectManager->create('Magento\GiftMessage\Model\Message')->load($quote->getGiftMessageId());
-        $this->assertEquals('John Doe', $message->getRecipient());
-        $this->assertEquals('Jane Roe', $message->getSender());
-        $this->assertEquals('Gift Message Text New', $message->getMessage());
-    }
-
-    /**
-     * @magentoApiDataFixture Magento/GiftMessage/_files/quote_with_item_message.php
-     */
-    public function testSaveForMyCart()
-    {
-        $this->_markTestAsRestOnly();
-
-        // get customer ID token
-        /** @var \Magento\Integration\Api\CustomerTokenServiceInterface $customerTokenService */
-        $customerTokenService = $this->objectManager->create(
-            'Magento\Integration\Api\CustomerTokenServiceInterface'
-        );
-        $token = $customerTokenService->createCustomerAccessToken('customer@example.com', 'password');
-
-        // sales/gift_options/allow_order must be set to 1 in system configuration
-        // @todo remove next statement when \Magento\TestFramework\TestCase\WebapiAbstract::_updateAppConfig is fixed
-        $this->markTestIncomplete('This test relies on system configuration state.');
-
-        $serviceInfo = [
-            'rest' => [
-                'resourcePath' => self::RESOURCE_PATH . 'mine/gift-message',
-                'httpMethod' => \Magento\Framework\Webapi\Rest\Request::HTTP_METHOD_POST,
-                'token' => $token,
-            ],
-        ];
-
-        $requestData = [
-            'giftMessage' => [
-                'recipient' => 'John Doe',
-                'sender' => 'Jane Roe',
-                'message' => 'Gift Message Text New',
-            ],
-        ];
-        $this->assertTrue($this->_webApiCall($serviceInfo, $requestData));
-
-        /** @var \Magento\Quote\Model\Quote $quote */
-        $quote = $this->objectManager->create('Magento\Quote\Model\Quote');
         $quote->load('test_order_item_with_message', 'reserved_order_id');
         $quote->getGiftMessageId();
         /** @var  \Magento\GiftMessage\Model\Message $message */
---
+++ dev/tests/integration/testsuite/Magento/GiftMessage/_files/quote_with_customer_and_message_rollback.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+$objectManager = \Magento\TestFramework\Helper\Bootstrap::getObjectManager();
+
+/** @var \Magento\Quote\Model\Quote $quote */
+$quote = $objectManager->create('Magento\Quote\Model\Quote');
+$quote->load('message_order_21', 'reserved_order_id');
+
+/** @var \Magento\GiftMessage\Model\Message $message */
+$message = $objectManager->create('Magento\GiftMessage\Model\Message');
+$message->load($quote->getGiftMessageId());
+$message->delete();
+
+$quote->delete();
