4f4795588ac3b417bb0a084b9553c25c227253cc
Iryna Lagno <ilagno@ebay.com>
2015-06-10 12:51:04 +0300

    MAGETWO-38120: Fix static tests

--- app/code/Magento/Tax/Api/Data/GrandTotalDetailsInterface.php
+++ app/code/Magento/Tax/Api/Data/GrandTotalDetailsInterface.php
@@ -3,11 +3,9 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-
 namespace Magento\Tax\Api\Data;
 
-
-interface GrandTotalDetailsInterface extends \Magento\Framework\Api\ExtensibleDataInterface
+interface GrandTotalDetailsInterface
 {
     /**
      * Get tax amount value
