dcc9ba4fd8377006fc6325d5edf4bb0dc983d0f5
Bohdan Korablov <bkorablov@ebay.com>
2015-06-11 19:01:20 +0300

    MAGETWO-37969: [GITHUB] Development Errors Result in Too Many Redirects #1310

--- lib/internal/Magento/Framework/App/Test/Unit/Action/AbstractActionTest.php
+++ lib/internal/Magento/Framework/App/Test/Unit/Action/AbstractActionTest.php
@@ -50,6 +50,12 @@ class AbstractActionTest extends \PHPUnit_Framework_TestCase
         $this->context->expects($this->any())
             ->method('getResultRedirectFactory')
             ->willReturn($this->redirectFactory);
+        $this->context->expects($this->once())
+            ->method('getRequest')
+            ->willReturn($this->request);
+        $this->context->expects($this->once())
+            ->method('getResponse')
+            ->willReturn($this->response);
 
         $this->action = $this->getMockForAbstractClass('Magento\Framework\App\Action\AbstractAction', [$this->context]);
     }
