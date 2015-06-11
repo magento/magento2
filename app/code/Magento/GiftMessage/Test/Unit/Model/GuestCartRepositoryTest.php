97a93ef5ca2c129d6eadf7f20c1dc8f6c78cf8bb
Sviatoslav Mankivskyi <smankivskyi@ebay.com>
2015-06-11 12:54:53 +0300

    MAGETWO-38526: Implement API services for Magento\GiftMessage

--- app/code/Magento/GiftMessage/Test/Unit/Model/GuestCartRepositoryTest.php
+++ app/code/Magento/GiftMessage/Test/Unit/Model/GuestCartRepositoryTest.php
@@ -4,7 +4,7 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\GiftMessage\Test\Unit\Model\Plugin;
+namespace Magento\GiftMessage\Test\Unit\Model;
 
 // @codingStandardsIgnoreFile
 
