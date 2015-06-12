565b3d7f295545d375edd02d8488da3d316112cb
Fred Sung <csung@ebay.com>
2015-06-12 08:45:28 -0500

    MAGETWO-38178: Check if Theme in use
     - code style issues fixed.

--- app/code/Magento/Theme/Test/Unit/Console/Command/ThemeUninstallCommandTest.php
+++ app/code/Magento/Theme/Test/Unit/Console/Command/ThemeUninstallCommandTest.php
@@ -105,7 +105,7 @@ class ThemeUninstallCommandTest extends \PHPUnit_Framework_TestCase
             '',
             false
         );
-        $this->themeValidator = $this->getMock('Magento\Theme\Model\ThemeValidator',[], [], '', false);
+        $this->themeValidator = $this->getMock('Magento\Theme\Model\ThemeValidator', [], [], '', false);
         $this->command = new ThemeUninstallCommand(
             $this->cache,
             $this->cleanupFiles,
