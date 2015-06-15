e83392a600355247d9b6e5e644554e44bfcfcd52
Andriy Nasinnyk <anasinnyk@ebay.com>
2015-06-15 14:54:02 +0300

    MAGETWO-37804: Create indexer field handlers

--- app/code/Magento/Indexer/Model/HandlerInterface.php
+++ app/code/Magento/Indexer/Model/HandlerInterface.php
@@ -5,21 +5,23 @@
  */
 namespace Magento\Indexer\Model;
 
+use Magento\Framework\App\Resource\SourceProviderInterface;
+
 interface HandlerInterface
 {
     /**
      * @param \Zend_Db_Select $select
-     * @param SourceInterface $source
+     * @param SourceProviderInterface $source
      * @param array $fieldInfo
      * @return void
      */
-    public function prepareSql(\Zend_Db_Select $select, SourceInterface $source, $fieldInfo);
+    public function prepareSql(\Zend_Db_Select $select, SourceProviderInterface $source, $fieldInfo);
 
     /**
      * @param \Zend_Db_Select $select
-     * @param SourceInterface $source
+     * @param SourceProviderInterface $source
      * @param array $fieldInfo
      * @return void
      */
-    public function prepareData(\Zend_Db_Select $select, SourceInterface $source, $fieldInfo);
+    public function prepareData(\Zend_Db_Select $select, SourceProviderInterface $source, $fieldInfo);
 }
