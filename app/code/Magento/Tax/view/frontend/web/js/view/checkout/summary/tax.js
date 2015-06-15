263b1939ce9669cb65984f07afbacfb75e693d29
Serhiy Shkolyarenko <sshkolyarenko@ebay.com>
2015-06-15 19:48:50 +0300

    MAGETWO-38690: Refactor totals view models to use total segments
    CustomerBalance refactoring

--- app/code/Magento/Tax/view/frontend/web/js/view/checkout/summary/tax.js
+++ app/code/Magento/Tax/view/frontend/web/js/view/checkout/summary/tax.js
@@ -28,7 +28,7 @@ define(
                 if (!isTaxDisplayedInGrandTotal) {
                     return false;
                 }
-                if (!this.totals() || null == totals.getTotalByCode('tax')) {
+                if (!this.totals() || null == totals.getSegment('tax')) {
                     return true;
                 }
                 if (this.getPureValue() == 0) {
@@ -45,7 +45,7 @@ define(
             getPureValue: function() {
                 var amount = 0;
                 if (this.totals()) {
-                    var taxTotal = totals.getTotalByCode('tax');
+                    var taxTotal = totals.getSegment('tax');
                     if (taxTotal) {
                         amount = taxTotal.value;
                     }
@@ -55,7 +55,7 @@ define(
             getValue: function() {
                 var amount = 0;
                 if (this.totals()) {
-                    var taxTotal = totals.getTotalByCode('tax');
+                    var taxTotal = totals.getSegment('tax');
                     if (this.isFullMode() && taxTotal) {
                         amount = taxTotal.value;
                     } else {
