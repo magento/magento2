83d073af40f81ab47c10c81747396f7eabc86441
okarpenko <okarpenko@ebay.com>
2015-06-16 16:45:58 +0300

    MAGETWO-38156: Move gift message to cart

--- app/code/Magento/GiftMessage/Model/GiftMessageConfigProvider.php
+++ app/code/Magento/GiftMessage/Model/GiftMessageConfigProvider.php
@@ -10,6 +10,7 @@ use Magento\GiftMessage\Helper\Message as GiftMessageHelper;
 use Magento\Framework\App\Http\Context as HttpContext;
 use Magento\Customer\Model\Context as CustomerContext;
 use Magento\Framework\UrlInterface;
+use Magento\Framework\Locale\FormatInterface as LocaleFormat;
 
 /**
  * Configuration provider for GiftMessage rendering on "Shipping Method" step of checkout.
@@ -42,12 +43,18 @@ class GiftMessageConfigProvider implements ConfigProviderInterface
     protected $storeManager;
 
     /**
+     * @var LocaleFormat
+     */
+    protected $localeFormat;
+
+    /**
      * @param \Magento\Framework\App\Helper\Context $context
      * @param \Magento\GiftMessage\Api\CartRepositoryInterface $cartRepository
      * @param \Magento\GiftMessage\Api\ItemRepositoryInterface $itemRepository
      * @param \Magento\Checkout\Model\Session $checkoutSession
      * @param HttpContext $httpContext
      * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+     * @param LocaleFormat $localeFormat
      */
     public function __construct(
         \Magento\Framework\App\Helper\Context $context,
@@ -55,7 +62,8 @@ class GiftMessageConfigProvider implements ConfigProviderInterface
         \Magento\GiftMessage\Api\ItemRepositoryInterface $itemRepository,
         \Magento\Checkout\Model\Session $checkoutSession,
         HttpContext $httpContext,
-        \Magento\Store\Model\StoreManagerInterface $storeManager
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        LocaleFormat $localeFormat
     ) {
         $this->scopeConfiguration = $context->getScopeConfig();
         $this->cartRepository = $cartRepository;
@@ -63,6 +71,7 @@ class GiftMessageConfigProvider implements ConfigProviderInterface
         $this->checkoutSession = $checkoutSession;
         $this->httpContext = $httpContext;
         $this->storeManager = $storeManager;
+        $this->localeFormat = $localeFormat;
     }
 
     /**
@@ -90,6 +99,10 @@ class GiftMessageConfigProvider implements ConfigProviderInterface
             $configuration['isItemLevelGiftOptionsEnabled'] = true;
             $configuration['giftMessage']['itemLevel'] = $itemMessages === null ? true : $itemMessages;
         }
+        $configuration['priceFormat'] = $this->localeFormat->getPriceFormat(
+            null,
+            $this->checkoutSession->getQuote()->getQuoteCurrencyCode()
+        );
         $configuration['storeCode'] = $this->getStoreCode();
         $configuration['isCustomerLoggedIn'] = $this->isCustomerLoggedIn();
         $store = $this->storeManager->getStore();
