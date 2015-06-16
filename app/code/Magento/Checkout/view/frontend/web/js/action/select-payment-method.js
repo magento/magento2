7afc4223bc39e2b2fcf96404ed2751ef3030f0f0
Iryna Lagno <ilagno@ebay.com>
2015-06-16 16:11:00 +0300

    MAGETWO-38639: Skeleton implementation for payment methods

--- app/code/Magento/Checkout/view/frontend/web/js/action/select-payment-method.js
+++ app/code/Magento/Checkout/view/frontend/web/js/action/select-payment-method.js
@@ -5,96 +5,13 @@
 /*global define*/
 define(
     [
-        '../model/quote',
-        '../model/url-builder',
-        '../model/step-navigator',
-        '../model/payment-service',
-        'Magento_Ui/js/model/errorlist',
-        'mage/storage',
-        'underscore',
-        'Magento_Customer/js/model/customer'
+        '../model/quote'
     ],
-    function(quote, urlBuilder, navigator, service, errorList, storage, _, customer) {
-        "use strict";
-        return function (methodData, methodInfo, callbacks) {
-            var paymentMethodData = {
-                "cartId": quote.getQuoteId(),
-                "paymentMethod": methodData
-            };
+    function(quote) {
+        'use strict';
+        return function (paymentMethod) {
+            quote.paymentMethod(paymentMethod);
+        }
 
-            var shippingMethodCode = quote.getSelectedShippingMethod()().slice(0).split("_"),
-                shippingMethodData = {
-                    "shippingCarrierCode" : shippingMethodCode.shift(),
-                    "shippingMethodCode" : shippingMethodCode.join('_')
-                },
-                serviceUrl;
-            if (!customer.isLoggedIn()) {
-                serviceUrl = urlBuilder.createUrl('/guest-carts/:quoteId/collect-totals', {quoteId: quote.getQuoteId()});
-            } else {
-                serviceUrl = urlBuilder.createUrl('/carts/mine/collect-totals', {});
-            }
-
-            if (quote.isVirtual()) {
-                return storage.put(
-                    serviceUrl,
-                    JSON.stringify(paymentMethodData)
-                ).done(
-                    function (response) {
-                        var proceed = true;
-                        _.each(callbacks, function(callback) {
-                            proceed = proceed && callback();
-                        });
-                        if (proceed) {
-                            quote.setPaymentMethod(methodData.method);
-                            service.setSelectedPaymentData(methodData);
-                            service.setSelectedPaymentInfo(methodInfo);
-                            quote.setTotals(response);
-                            navigator.setCurrent('paymentMethod').goNext();
-                        }
-                    }
-                ).error(
-                    function (response) {
-                        var error = JSON.parse(response.responseText);
-                        errorList.add(error);
-                        quote.setPaymentMethod(null);
-                    }
-                );
-            } else {
-                if (!_.isEmpty(quote.getShippingCustomOptions()())) {
-                    shippingMethodData = _.extend(
-                        shippingMethodData,
-                        {
-                            additionalData: {
-                                extension_attributes: quote.getShippingCustomOptions()()
-                            }
-                        }
-                    );
-                }
-                return storage.put(
-                    serviceUrl,
-                    JSON.stringify(_.extend(paymentMethodData, shippingMethodData))
-                ).done(
-                    function (response) {
-                        var proceed = true;
-                        _.each(callbacks, function(callback) {
-                            proceed = proceed && callback();
-                        });
-                        if (proceed) {
-                            quote.setPaymentMethod(methodData.method);
-                            service.setSelectedPaymentData(methodData);
-                            service.setSelectedPaymentInfo(methodInfo);
-                            quote.setTotals(response);
-                            navigator.setCurrent('paymentMethod').goNext();
-                        }
-                    }
-                ).error(
-                    function (response) {
-                        var error = JSON.parse(response.responseText);
-                        errorList.add(error);
-                        quote.setPaymentMethod(null);
-                    }
-                );
-            }
-        };
     }
 );
