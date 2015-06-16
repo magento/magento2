b2636606f001d64adc8f5efc235177f3eff342bd
Ievgen Shakhsuvarov <ishakhsuvarov@ebay.com>
2015-06-16 15:33:20 +0300

    MAGETWO-38639: Skeleton implementation for payment methods

--- app/code/Magento/OfflinePayments/view/frontend/web/js/view/payment/method-renderer/checkmo-method.js
+++ app/code/Magento/OfflinePayments/view/frontend/web/js/view/payment/method-renderer/checkmo-method.js
@@ -2,30 +2,16 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-/*browser:true*/
-/*global define*/
 define(
     [
-        'uiComponent',
-        'Magento_Checkout/js/action/place-order'
+        './default'
     ],
-    function (Component, placeOrderAction) {
+    function (Component) {
+        'use strict';
+
         return Component.extend({
             defaults: {
                 template: 'Magento_OfflinePayments/payment/checkmo'
-            },
-            placeOrder: function() {
-                var data = {
-                    "method": this.index,
-                    "po_number": null,
-                    "cc_owner": null,
-                    "cc_number": null,
-                    "cc_type": null,
-                    "cc_exp_year": null,
-                    "cc_exp_month": null,
-                    "additional_data": null
-                };
-                placeOrderAction(data);
             }
         });
     }
---
+++ app/code/Magento/OfflinePayments/view/frontend/web/js/view/payment/method-renderer/default.js
@@ -0,0 +1,38 @@
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+define(
+    [
+        'uiComponent',
+        'Magento_Checkout/js/action/place-order'
+    ],
+    function (Component, placeOrderAction) {
+        'use strict';
+
+        return Component.extend({
+            /**
+             * Place order.
+             */
+            placeOrder: function () {
+                var data = {
+                    "method": this.index,
+                    "po_number": null,
+                    "cc_owner": null,
+                    "cc_number": null,
+                    "cc_type": null,
+                    "cc_exp_year": null,
+                    "cc_exp_month": null,
+                    "additional_data": null
+                };
+                placeOrderAction(data);
+            },
+            /**
+             * Get payment method type.
+             */
+            getTitle: function () {
+                return this.item.method;
+            }
+        });
+    }
+);
