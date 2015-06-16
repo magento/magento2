fa53f962593f402e79463961466a8264c45adfa8
Michail Slabko <mslabko@ebay.com>
2015-06-16 15:43:00 +0300

    MAGETWO-37174: Manual selection product an association
    - MAGETWO-42429: Add products manually to grid
    -- add ability to load grids with AJAX

--- lib/internal/Magento/Framework/View/Layout/Generic.php
+++ lib/internal/Magento/Framework/View/Layout/Generic.php
@@ -3,7 +3,7 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\Ui\Component\Layout;
+namespace Magento\Framework\View\Layout;
 
 use Magento\Framework\View\Element\UiComponent\DataSourceInterface;
 use Magento\Framework\View\Element\UiComponent\LayoutInterface;
