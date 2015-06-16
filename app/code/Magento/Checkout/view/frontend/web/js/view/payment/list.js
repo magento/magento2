74a108622803334a818f5b6d71d05ca4ef0c66bb
Olexii Korshenko <okorshenko@ebay.com>
2015-06-16 15:06:01 +0300

    MAGETWO-38639: Skeleton implementation for payment methods

--- app/code/Magento/Checkout/view/frontend/web/js/view/payment/list.js
+++ app/code/Magento/Checkout/view/frontend/web/js/view/payment/list.js
@@ -9,82 +9,98 @@ define([
     'uiComponent',
     'Magento_Checkout/js/model/payment/payment-list',
     'Magento_Ui/js/core/renderer/layout',
+    'uiRegistry',
     'Magento_Checkout/js/model/payment/renderer-list'
 
-], function (_, ko, utils, Component, paymentMethods, layout, rendererList) {
+], function (_, ko, utils, Component, paymentMethods, layout, registry, rendererList) {
     'use strict';
 
-    //debugger;
+    var createRenderer = function(item) {
+        var renderer = getRendererByType(item.method);
+        if (renderer) {
+            var templateData = {
+                parentName: this.name,
+                name: item.method
+            };
+            var rendererTemplate = {
+                parent: '${ $.$data.parentName }',
+                name: '${ $.$data.name }',
+                component: renderer.component
+            };
+            var rendererComponent = utils.template(rendererTemplate, templateData);
+            utils.extend(rendererComponent, {item: item});
+            layout([rendererComponent]);
+        } else {
+            console.log('There is no registered render for Payment Method: ' + item.method);
+        }
+    };
+
+    var getRendererByType = function(code) {
+        var output = null;
+        rendererList().forEach(function(item) { //todo refactor this code
+            if (item.type == code) {
+                output = item;
+            }
+        });
+        return output;
+    };
+
     return Component.extend({
         defaults: {
             template: 'Magento_Checkout/payment-methods/list',
             visible: paymentMethods().length > 0
         },
         initialize: function () {
-            this._super();
+            this._super()
+                .initChildren();
 
             paymentMethods.subscribe(
                 function (changes) {
                     var self = this;
                     changes.forEach(function(change) {
                         if (change.status === 'added') {
-                            self.addRenderer(change);
+                            console.log(('added ' + change.value.method));
+                            createRenderer(change.value);
                         } else if (change.status === 'deleted') {
-                            self.removeRenderer(change.value, change.index);
+                            console.log(('deleted ' + change.value.method));
+                            self.removeRenderer(change.value);
                         }
                     });
                 },
                 this,
                 'arrayChange'
             );
-
-            this.getPaymentMethods();
-
             return this;
         },
 
-        addRenderer: function (paymentMethod) {
-            console.info('add renderer ' + paymentMethod.method);
+        initChildren: function () {
+            paymentMethods().forEach(function (item ) {
+                createRenderer(item);
+            });
+            return this;
         },
+
+        add: function() {
+            var method = {
+                "method": 'checkmo',
+                "po_number": null,
+                "cc_owner": null,
+                "cc_number": null,
+                "cc_type": null,
+                "cc_exp_year": null,
+                "cc_exp_month": null,
+                "additional_data": null
+            };
+            paymentMethods([method]);
+        },
+
+        remove: function() {
+            paymentMethods([]);
+        },
+
         removeRenderer: function (paymentMethod) {
             console.info('remove renderer');
-        },
-
-        getRendererByType: function(code) {
-            var output = null;
-            rendererList().forEach(function(item) { //todo refactor this code
-                if (item.type == code) {
-                    output = item;
-                }
-            });
-            return output;
-        },
-
-        getPaymentMethods: function() {
-            var output = [];
-            var self = this;
-            paymentMethods().forEach(function(item){
-                var renderer = self.getRendererByType(item.method);
-                var rendererTemplate = {
-                    parent: '${ $.$data.parentName }',
-                    name: '${ $.$data.name }',
-                    component: ''
-                };
-                if (renderer) {
-                    rendererTemplate.component = renderer.component;
-                    var templateData = {
-                        parentName: self.name,
-                        name: 1
-                    };
-                    var rendererComponent = utils.template(rendererTemplate, templateData);
-                    utils.extend(rendererComponent, {item: item});
-                    layout([rendererComponent]);
-                    output.push(rendererComponent);
-                } else {
-                    console.info('no registered render for type ' + item.method);
-                }
-            });
-            return output;
+            registry.remove([paymentMethod.method]);
         }
     });
 });
