53e5c6b2868b39eb930f3182459a02209ec4a966
Dale Sikkema <dsikkema@ebay.com>
2015-06-17 00:47:51 -0500

    MAGETWO-37848: Generate data fails when table prefix exceeds length of 5  - delete a space character

--- lib/internal/Magento/Framework/DB/Test/Unit/ExpressionConverterTest.php
+++ lib/internal/Magento/Framework/DB/Test/Unit/ExpressionConverterTest.php
@@ -51,7 +51,7 @@ class ExpressionConverterTest extends \PHPUnit_Framework_TestCase
         /** Length of 64 characters, to go over max MySql identifier length */
         $length64 = '________________________________________________________________';
         $longPrefix = 'pre_____________________________________';
-        $shortenedName = ExpressionConverter::shortenEntityName($length64 . '_cannotBeAbbreviated', $longPrefix );
+        $shortenedName = ExpressionConverter::shortenEntityName($length64 . '_cannotBeAbbreviated', $longPrefix);
         $this->assertNotSame(0, strpos($shortenedName, 'pre'), 'Entity name not supposed to with long prefix');
     }
 }
