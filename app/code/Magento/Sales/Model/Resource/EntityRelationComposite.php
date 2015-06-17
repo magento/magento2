9782a37398e18d75c2be9e4e6a562430511721e5
Vladyslav Shcherbyna <vshcherbyna@ebay.com>
2015-06-17 11:51:26 +0300

    MAGETWO-38764: Add component to framework (abstract resource model, abstract resource collection, metadata, snapshot)

--- app/code/Magento/Sales/Model/Resource/EntityRelationComposite.php
+++ app/code/Magento/Sales/Model/Resource/EntityRelationComposite.php
@@ -6,51 +6,12 @@
 
 namespace Magento\Sales\Model\Resource;
 
-use Magento\Sales\Model\AbstractModel;
-use Magento\Framework\Event\ManagerInterface as EventManager;
+use Magento\Framework\Model\Resource\Db\VersionControl\RelationComposite;
 
 /**
  * Class EntityRelationComposite
  */
-class EntityRelationComposite
+class EntityRelationComposite extends RelationComposite
 {
-    /**
-     * @var array
-     */
-    protected $relationProcessors;
 
-    /**
-     * @var EventManager
-     */
-    protected $eventManager;
-
-    /**
-     * @param EventManager $eventManager
-     * @param array $relationProcessors
-     */
-    public function __construct(
-        EventManager $eventManager,
-        array $relationProcessors = []
-    ) {
-        $this->eventManager = $eventManager;
-        $this->relationProcessors = $relationProcessors;
-    }
-
-    /**
-     * @param AbstractModel $object
-     * @return void
-     */
-    public function processRelations(AbstractModel $object)
-    {
-        foreach ($this->relationProcessors as $processor) {
-            /**@var $processor \Magento\Sales\Model\Resource\EntityRelationInterface*/
-            $processor->processRelation($object);
-        }
-        $this->eventManager->dispatch(
-            $object->getEventPrefix(). '_process_relation',
-            [
-                'object' => $object
-            ]
-        );
-    }
 }
--- app/code/Magento/Sales/Model/Resource/EntitySnapshot.php
+++ app/code/Magento/Sales/Model/Resource/EntitySnapshot.php
@@ -5,66 +5,12 @@
  */
 namespace Magento\Sales\Model\Resource;
 
-use Magento\Framework\Model\AbstractModel;
+use Magento\Framework\Model\Resource\Db\VersionControl\Snapshot;
 
 /**
  * Class EntitySnapshot register snapshot of entity data, for tracking changes
  */
-class EntitySnapshot
+class EntitySnapshot extends Snapshot
 {
-    /**
-     * Array of snapshots of entities data
-     *
-     * @var array
-     */
-    protected $snapshotData = [];
 
-    /**
-     * @var EntityMetadata
-     */
-    protected $entityMetadata;
-
-    /**
-     * Initialization
-     *
-     * @param EntityMetadata $entityMetadata
-     */
-    public function __construct(
-        EntityMetadata $entityMetadata
-    ) {
-        $this->entityMetadata = $entityMetadata;
-    }
-
-    /**
-     * Register snapshot of entity data, for tracking changes
-     *
-     * @param AbstractModel $entity
-     * @return void
-     */
-    public function registerSnapshot(AbstractModel $entity)
-    {
-        $data = array_intersect_key($entity->getData(), $this->entityMetadata->getFields($entity));
-        $this->snapshotData[get_class($entity)][$entity->getId()] = $data;
-    }
-
-    /**
-     * Check is current entity has changes, by comparing current object state with stored snapshot
-     *
-     * @param AbstractModel $entity
-     * @return bool
-     */
-    public function isModified(AbstractModel $entity)
-    {
-        if (!$entity->getId()) {
-            return true;
-        }
-        if (!isset($this->snapshotData[get_class($entity)][$entity->getId()])) {
-            return true;
-        }
-        $data = array_intersect_key($entity->getData(), $this->entityMetadata->getFields($entity));
-        if ($data !== $this->snapshotData[get_class($entity)][$entity->getId()]) {
-            return true;
-        }
-        return false;
-    }
 }
