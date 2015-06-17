a977b430343d042b9a83b2699fb97ac88e7e158c
Valeriy Nayda <vnayda@ebay.com>
2015-06-17 20:11:03 +0300

    MAGETWO-35896: PHP - Apply New Data Grids in Products Data Grids
    -- MAGETWO-35868:  [Admin] Applying Enhanced Grid Component to Product Grid

---
+++ app/code/Magento/Catalog/Ui/DataProvider/Product/ProductDataProvider.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Catalog\Ui\DataProvider\Product;
+
+use Magento\Catalog\Model\Resource\Product\CollectionFactory;
+use Magento\Store\Model\Store;
+use Magento\Store\Model\StoreManagerInterface;
+
+/**
+ * Class GridDataProvider
+ */
+class ProductDataProvider extends \Magento\Ui\DataProvider\AbstractEavDataProvider
+{
+    /**
+     * @var \Magento\Catalog\Model\Resource\Product\Collection
+     */
+    protected $collection;
+
+    /**
+     * @var StoreManagerInterface
+     */
+    protected $storeManager;
+
+    /**
+     * @param string $name
+     * @param string $primaryFieldName
+     * @param string $requestFieldName
+     * @param CollectionFactory $collectionFactory
+     * @param StoreManagerInterface $storeManager
+     * @param array $meta
+     * @param array $data
+     */
+    public function __construct(
+        $name,
+        $primaryFieldName,
+        $requestFieldName,
+        CollectionFactory $collectionFactory,
+        StoreManagerInterface $storeManager,
+        array $meta = [],
+        array $data = []
+    ) {
+        parent::__construct($name, $primaryFieldName, $requestFieldName, $meta, $data);
+        $this->collection = $collectionFactory->create();
+        $this->storeManager = $storeManager;
+
+        $this->collection
+            ->addAttributeToSelect('sku')
+            ->addAttributeToSelect('name')
+            ->addAttributeToSelect('attribute_set_id')
+            ->addAttributeToSelect('type_id');
+
+        $store = $this->storeManager->getStore();
+        $storeId = $store->getStoreId();
+
+        if ($storeId) {
+            $this->collection->addStoreFilter($store);
+            $this->collection->joinAttribute(
+                'name',
+                'catalog_product/name',
+                'entity_id',
+                null,
+                'inner',
+                Store::DEFAULT_STORE_ID
+            );
+            $this->collection->joinAttribute(
+                'custom_name',
+                'catalog_product/name',
+                'entity_id',
+                null,
+                'inner',
+                $storeId
+            );
+            $this->collection->joinAttribute(
+                'status',
+                'catalog_product/status',
+                'entity_id',
+                null,
+                'inner',
+                $storeId
+            );
+            $this->collection->joinAttribute(
+                'visibility',
+                'catalog_product/visibility',
+                'entity_id',
+                null,
+                'inner',
+                $storeId
+            );
+            $this->collection->joinAttribute('price', 'catalog_product/price', 'entity_id', null, 'left', $store->getId());
+        } else {
+            $this->collection->addAttributeToSelect('price');
+            $this->collection->joinAttribute('status', 'catalog_product/status', 'entity_id', null, 'inner');
+            $this->collection->joinAttribute('visibility', 'catalog_product/visibility', 'entity_id', null, 'inner');
+        }
+        $this->collection->load();
+    }
+
+    /**
+     * @return \Magento\Catalog\Model\Resource\Product\Collection
+     */
+    protected function getCollection()
+    {
+        return $this->collection;
+    }
+
+    /**
+     * Get data
+     *
+     * @return array
+     */
+    public function getData()
+    {
+        $items = $this->getCollection()->toArray();
+
+        return [
+            'totalRecords' => count($items),
+            'items' => array_values($items),
+        ];
+    }
+}
