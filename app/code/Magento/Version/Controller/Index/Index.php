461cb7b47e782c8a9ef5c184fea24282bc2bd188
Dale Sikkema <dsikkema@ebay.com>
2015-06-16 21:32:11 -0500

    MAGETWO-38065: /magento_verion exposes too detailed version information

--- app/code/Magento/Version/Controller/Index/Index.php
+++ app/code/Magento/Version/Controller/Index/Index.php
@@ -31,15 +31,18 @@ class Index extends Action
     }
 
     /**
-     * Sets the response body with ProductName/Version (Edition). E.g.: Magento/0.42.0-beta3 (Community)
+     * Sets the response body to ProductName/Major.MinorVersion (Edition). E.g.: Magento/0.42 (Community). Omits patch
+     * version from response
      *
      * @return void
      */
     public function execute()
     {
+        $fullVersion = explode('.', $this->productMetadata->getVersion());
+        $majorMinorVersion = $fullVersion[0] . '.' . $fullVersion[1];
         $this->getResponse()->setBody(
             $this->productMetadata->getName() . '/' .
-            $this->productMetadata->getVersion() . ' (' .
+            $majorMinorVersion . ' (' .
             $this->productMetadata->getEdition() . ')'
         );
     }
