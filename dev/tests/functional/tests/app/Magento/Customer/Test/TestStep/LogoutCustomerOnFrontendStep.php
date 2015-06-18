cf770038c33894958c72003febf2f158aecbcf0d
Dmytro Vilchynskyi <dvilchynskyi@ebay.com>
2015-06-18 09:47:47 +0300

    MAGETWO-36129: Update Content in Magento 2 by Modules - part 1
    - fixed tests

--- dev/tests/functional/tests/app/Magento/Customer/Test/TestStep/LogoutCustomerOnFrontendStep.php
+++ dev/tests/functional/tests/app/Magento/Customer/Test/TestStep/LogoutCustomerOnFrontendStep.php
@@ -54,8 +54,8 @@ class LogoutCustomerOnFrontendStep implements TestStepInterface
     {
         $this->cmsIndex->open();
         $this->cmsIndex->getCmsPageBlock()->waitPageInit();
-        if ($this->cmsIndex->getLinksBlock()->isLinkVisible("Log Out")) {
-            $this->cmsIndex->getLinksBlock()->openLink("Log Out");
+        if ($this->cmsIndex->getLinksBlock()->isLinkVisible('Sign Out')) {
+            $this->cmsIndex->getLinksBlock()->openLink('Sign Out');
             $this->cmsIndex->getCmsPageBlock()->waitUntilTextIsVisible('Home Page');
             $this->cmsIndex->getCmsPageBlock()->waitPageInit();
         }
