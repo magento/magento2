61e8bb38bec650f0663dcec5edf011df1467c9ce
Roman Ganin <rganin@ebay.com>
2015-06-18 18:32:27 +0300

    MAGETWO-38809: Optimize Magento\Framework\View\Element\Template  for production mode

---
+++ lib/internal/Magento/Framework/View/Element/Template/File/Validator.php
@@ -0,0 +1,133 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Framework\View\Element\Template\File;
+
+use \Magento\Framework\App\Filesystem\DirectoryList;
+
+/**
+ * Class Validator
+ * @package Magento\Framework\View\Element\Template\File
+ */
+class Validator
+{
+    /**
+     * Template files map
+     *
+     * @var []
+     */
+    protected $_templatesValidationResults = [];
+
+    /**
+     * View filesystem
+     *
+     * @var \Magento\Framework\FileSystem
+     */
+    protected $_filesystem;
+
+    /**
+     * @var bool
+     */
+    protected $_isAllowSymlinks = false;
+
+    /**
+     * @var bool
+     */
+    protected $directory = null;
+
+    protected $_themesDir;
+
+    protected $_appDir;
+
+    protected $_compiledDir;
+
+    /**
+     * Class constructor
+     *
+     * @param \Magento\Framework\Filesystem $filesystem
+     */
+    public function __construct(\Magento\Framework\Filesystem $filesystem)
+    {
+        $this->_filesystem = $filesystem;
+        $this->_themesDir = $this->_filesystem->getDirectoryRead(DirectoryList::THEMES)->getAbsolutePath();
+        $this->_appDir = $this->_filesystem->getDirectoryRead(DirectoryList::APP)->getAbsolutePath();
+        $this->_compiledDir = $this->_filesystem->getDirectoryRead(DirectoryList::TEMPLATE_MINIFICATION_DIR)
+            ->getAbsolutePath();
+    }
+
+    /**
+     * Set allow symlinks for validation
+     *
+     * @param bool $flag
+     *
+     * @return $this
+     */
+    public function setIsAllowSymlinks($flag = true)
+    {
+        $this->_isAllowSymlinks = $flag;
+        return $this;
+    }
+
+    /**
+     * Checks whether the provided file can be rendered.
+     *
+     * Available directories which are allowed to be rendered
+     * (the template file should be located under these directories):
+     *  - app
+     *  - design
+     *
+     * @param string $filename
+     * @return bool
+     */
+    public function isValid($filename)
+    {
+        $filename = str_replace('\\', '/', $filename);
+        if (!isset($this->_templatesValidationResults[$filename])) {
+            $this->_templatesValidationResults[$filename] =
+                ($this->isPathInDirectory($filename, $this->_compiledDir)
+                || $this->isPathInDirectory($filename, $this->_appDir)
+                || $this->isPathInDirectory($filename, $this->_themesDir)
+                || $this->_isAllowSymlinks)
+                && $this->getRootDirectory()->isFile($this->getRootDirectory()->getRelativePath($filename));
+        }
+        return $this->_templatesValidationResults[$filename];
+    }
+
+    /**
+     * Checks whether path related to the directory
+     *
+     * @param string $path
+     * @param string $directory
+     * @return bool
+     */
+    protected function isPathInDirectory($path, $directory)
+    {
+        return 0 === strpos($path, $directory);
+    }
+
+    /**
+     * Get is allowed symlinks flag
+     *
+     * @return bool
+     */
+    protected function isAllowSymlinks()
+    {
+        return $this->_isAllowSymlinks;
+    }
+
+    /**
+     * Instantiates filesystem directory
+     *
+     * @return \Magento\Framework\Filesystem\Directory\ReadInterface
+     */
+    protected function getRootDirectory()
+    {
+        if (null === $this->directory) {
+            $this->directory = $this->_filesystem->getDirectoryRead(DirectoryList::ROOT);
+        }
+
+        return $this->directory;
+    }
+}
