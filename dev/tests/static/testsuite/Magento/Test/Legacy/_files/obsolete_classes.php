db90fdf812473b8f48a2d9c3e5c81cd4cb2f99d3
Gurzhyi, Andrii <agurzhyi@ebay.com>
2015-06-19 14:27:38 +0300

    MAGETWO-37956: It's unable to create Customer from Backend if there is multiline form element

--- dev/tests/static/testsuite/Magento/Test/Legacy/_files/obsolete_classes.php
+++ dev/tests/static/testsuite/Magento/Test/Legacy/_files/obsolete_classes.php
@@ -3656,5 +3656,7 @@ return [
     ['Magento\Centinel\Test\TestCase\CentinelPaymentsInvalidCcTest'],
     ['Magento\Centinel\Test\TestCase\CentinelPaymentsValidCcTest'],
     ['Magento\Centinel\CreateOrderTest'],
-    ['Magento\Payment\Model\Checks\PaymentMethodChecksInterface', 'Magento\Payment\Model\MethodInterface']
+    ['Magento\Payment\Model\Checks\PaymentMethodChecksInterface', 'Magento\Payment\Model\MethodInterface'],
+    ['Magento\Ui\DataProvider\EavValidationRul', 'Magento\Ui\DataProvider\EavValidationRules'],
+    ['Magento\Framework\View\Element\UiComponent\JsConfigInterface'],
 ];
