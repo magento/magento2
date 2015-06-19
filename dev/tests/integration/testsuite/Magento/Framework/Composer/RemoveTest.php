78b5e76c2a30cf896e093961e240de6d5704117c
Andriy Nasinnyk <anasinnyk@ebay.com>
2015-06-19 16:17:37 +0300

    MAGETWO-37804: Create indexer field handlers
     - fix unit fatal

--- dev/tests/integration/testsuite/Magento/Framework/Composer/RemoveTest.php
+++ dev/tests/integration/testsuite/Magento/Framework/Composer/RemoveTest.php
@@ -11,7 +11,13 @@ class RemoveTest extends \PHPUnit_Framework_TestCase
 {
     public function testRemove()
     {
-        $composerApp = $this->getMock('Composer\Console\Application', [], [], '', false);
+        $composerApp = $this->getMock(
+            'Composer\Console\Application',
+            ['setAutoExit', 'run', 'resetComposer'],
+            [],
+            '',
+            false
+        );
         $directoryList = $this->getMock('Magento\Framework\App\Filesystem\DirectoryList', [], [], '', false);
         $directoryList->expects($this->once())->method('getRoot');
         $directoryList->expects($this->once())
