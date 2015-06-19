2c5bd97802f286d1d7be1d5925d20254ac7d617e
Vladyslav Shcherbyna <vshcherbyna@ebay.com>
2015-06-19 14:42:59 +0300

    MAGETWO-38773: Refactor Quote address to use added framework components

---
+++ app/code/Magento/Quote/Model/Quote/Address/Relation.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Magento\Quote\Model\Quote\Address;
+
+use Magento\Framework\Model\Resource\Db\VersionControl\RelationInterface;
+
+class Relation implements RelationInterface
+{
+    /**
+     * Process object relations
+     *
+     * @param \Magento\Framework\Model\AbstractModel $object
+     * @return void
+     */
+    public function processRelation(\Magento\Framework\Model\AbstractModel $object)
+    {
+        /**
+         * @var $object \Magento\Quote\Model\Quote\Address
+         */
+        $object->getItemsCollection()->save();
+        $object->getShippingRatesCollection()->save();
+    }
+
+}
