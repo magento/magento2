57885a947d6c963fff16ef9709fd0129dd8aa964
Vladyslav Shcherbyna <vshcherbyna@ebay.com>
2015-06-22 17:56:13 +0300

    MAGETWO-38767: Refactor Customer address to use added framework components

---
+++ app/code/Magento/Customer/Model/Resource/Address/Relation.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Customer address entity resource model
+ *
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Customer\Model\Resource\Address;
+
+use Magento\Framework\Model\Resource\Db\VersionControl\RelationInterface;
+
+/**
+ * Class represents save operations for customer address relations
+ */
+class Relation implements RelationInterface
+{
+    /**
+     * @var \Magento\Customer\Model\CustomerFactory
+     */
+    protected $customerFactory;
+
+    /**
+     * @param \Magento\Customer\Model\CustomerFactory $customerFactory
+     */
+    public function __construct(\Magento\Customer\Model\CustomerFactory $customerFactory)
+    {
+        $this->customerFactory = $customerFactory;
+    }
+
+    /**
+     * Process object relations
+     *
+     * @param \Magento\Framework\Model\AbstractModel $object
+     * @return void
+     */
+    public function processRelation(\Magento\Framework\Model\AbstractModel $object)
+    {
+        /**
+         * @var $object \Magento\Customer\Model\Address
+         */
+        if (!$object->getIsCustomerSaveTransaction() && $this->isAddressDefault($object)) {
+            $customer = $this->customerFactory->create()->load($object->getCustomerId());
+
+            if ($object->getIsDefaultBilling()) {
+                $customer->setDefaultBilling($object->getId());
+            }
+            if ($object->getIsDefaultShipping()) {
+                $customer->setDefaultShipping($object->getId());
+            }
+            $customer->save();
+        }
+    }
+
+    /**
+     * Checks if address has chosen as default and has had an id
+     *
+     * @param \Magento\Framework\Model\AbstractModel $object
+     * @return bool
+     */
+    protected function isAddressDefault(\Magento\Framework\Model\AbstractModel $object)
+    {
+        return $object->getId() && ($object->getIsDefaultBilling() || $object->getIsDefaultShipping());
+    }
+}
