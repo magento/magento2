707d5a6e5247d7e688eb21a77918ab72a43547f0
Serhiy Shkolyarenko <sshkolyarenko@ebay.com>
2015-06-24 11:58:58 +0300

    MAGETWO-39049: Discount(promo code) functionality rendering
    recalculating totals, minor fixes

--- app/code/Magento/SalesRule/view/frontend/web/js/action/set-coupon-code.js
+++ app/code/Magento/SalesRule/view/frontend/web/js/action/set-coupon-code.js
@@ -10,13 +10,15 @@
 define(
     [
         'ko',
+        'jquery',
         'Magento_Checkout/js/model/quote',
         'Magento_Checkout/js/model/resource-url-manager',
         'Magento_Checkout/js/model/payment-service',
         'Magento_Ui/js/model/errorlist',
-        'mage/storage'
+        'mage/storage',
+        'Magento_Checkout/js/action/get-totals'
     ],
-    function (ko, quote, urlManager, paymentService, errorList, storage) {
+    function (ko, $, quote, urlManager, paymentService, errorList, storage, getTotalsAction) {
         'use strict';
         return function (couponCode, isApplied) {
             var quoteId = quote.getQuoteId();
@@ -25,10 +27,17 @@ define(
                 url
             ).done(
                 function (response) {
-                    isApplied(true);
-                    paymentService.setPaymentMethods(
-                        paymentService.getAvailablePaymentMethods()
-                    );
+                    if (response) {
+                        isApplied(true);
+                        var deferred = $.Deferred();
+
+                        getTotalsAction([], deferred);
+                        $.when(deferred).done(function() {
+                            paymentService.setPaymentMethods(
+                                paymentService.getAvailablePaymentMethods()
+                            );
+                        });
+                    }
                 }
             ).error(
                 function (response) {
