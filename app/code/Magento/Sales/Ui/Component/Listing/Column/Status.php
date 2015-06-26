2ad733dde9c75b8c924d644256771c7f5db8d00a
Sergey Ivashchenko <sivashchenko@ebay.com>
2015-06-26 17:33:04 +0300

    MAGETWO-38760: Configure sales order grid using ui components

---
+++ app/code/Magento/Sales/Ui/Component/Listing/Column/Status.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Sales\Ui\Component\Listing\Column;
+
+use Magento\Ui\Component\Listing\Columns\Column;
+
+/**
+ * Class Status
+ */
+class Status extends Column
+{
+    /**
+     * Prepare Data Source
+     *
+     * @param array $dataSource
+     * @return void
+     */
+    public function prepareDataSource(array & $dataSource)
+    {
+        if (isset($dataSource['data']['items'])) {
+            foreach ($dataSource['data']['items'] as & $item) {
+                $item[$this->getData('name')] = ucfirst($item[$this->getData('name')]);
+            }
+        }
+    }
+}
