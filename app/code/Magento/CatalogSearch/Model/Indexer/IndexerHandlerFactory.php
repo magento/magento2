749b4822e322e1e7027cd10b0b54d613f831a380
Andrii Kasian <akasian@ebay.com>
2015-06-26 16:14:06 +0300

    MAGETWO-359654: Refactor Indexer for Mysql Catalog Search

--- app/code/Magento/CatalogSearch/Model/Indexer/IndexerHandlerFactory.php
+++ app/code/Magento/CatalogSearch/Model/Indexer/IndexerHandlerFactory.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
 namespace Magento\CatalogSearch\Model\Indexer;
 
 use Magento\CatalogSearch\Model\Resource\EngineProvider;
