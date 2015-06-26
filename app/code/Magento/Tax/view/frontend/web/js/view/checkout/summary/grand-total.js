7abae621d7c2bde95a3042aecf6dfeca96410c52
Serhiy Shkolyarenko <sshkolyarenko@ebay.com>
2015-06-26 12:52:39 +0300

    MAGETWO-39133: Wrong Tax displaying in Summary Block
    used total segments to fetch proper value

--- app/code/Magento/Tax/view/frontend/web/js/view/checkout/summary/grand-total.js
+++ app/code/Magento/Tax/view/frontend/web/js/view/checkout/summary/grand-total.js
@@ -8,9 +8,10 @@ define(
     [
         'Magento_Checkout/js/view/summary/abstract-total',
         'Magento_Checkout/js/model/quote',
-        'Magento_Catalog/js/price-utils'
+        'Magento_Catalog/js/price-utils',
+        'Magento_Checkout/js/model/totals'
     ],
-    function (Component, quote, priceUtils) {
+    function (Component, quote, priceUtils, totals) {
         "use strict";
         return Component.extend({
             defaults: {
@@ -25,7 +26,7 @@ define(
             getValue: function() {
                 var price = 0;
                 if (this.totals()) {
-                    price = this.totals().grand_total;
+                    price = totals.getSegment('grand_total').value;
                 }
                 return this.getFormattedPrice(price);
             },
