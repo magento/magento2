7a08bfec7b0fe86e1cc92b007d2ff8dec793c031
Eddie Lau <kahlau@ebay.com>
2015-06-26 11:54:05 -0500

    MAGETWO-38839: Implement Code Upgrader
    - changed demo package

--- setup/pub/magento/setup/component-upgrade.js
+++ setup/pub/magento/setup/component-upgrade.js
@@ -8,7 +8,7 @@ angular.module('component-upgrade', ['ngStorage'])
     .controller('componentUpgradeController', ['$scope', '$state', '$localStorage', '$http', '$window', function ($scope, $state, $localStorage, $http, $window) {
         // TODO: hardcode it right now
         $localStorage.packages = [
-            {name: 'composer/composer', version: 'dev-master'}
+            {name: 'symfony/console', version: '2.5'}
         ];
         if ($localStorage.packages) {
             $scope.packages = $localStorage.packages;
