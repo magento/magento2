de4ad057e541162765c738e9b8c4ea1e1684a3d8
Denys Rul <drul@ebay.com>
2015-06-26 17:34:05 +0300

    MAGETWO-34250: JS - Search + Search/Filter Chips
    - Reorganize class.js code
    - Add comments

--- app/code/Magento/Ui/view/base/web/js/lib/class.js
+++ app/code/Magento/Ui/view/base/web/js/lib/class.js
@@ -52,50 +52,6 @@ define([
     }
 
     /**
-     * Analogue of Backbone.extend function.
-     *
-     * @param  {Object} extender - Object, that describes the prototype of
-     *      created constructor.
-     * @returns {Function} New constructor.
-     */
-    function extend(extender) {
-        var parent = this,
-            defaults = extender.defaults || {},
-            parentProto = parent.prototype,
-            child;
-
-        defaults = defaults || {};
-        extender = extender || {};
-
-        delete extender.defaults;
-
-        if (extender.hasOwnProperty('constructor')) {
-            child = extender.constructor;
-        } else {
-            child = function () {
-                parent.apply(this, arguments);
-            };
-        }
-
-        defaults = utils.extend({}, parent.defaults, defaults);
-
-        child.prototype = Object.create(parentProto);
-        child.prototype.constructor = child;
-
-        _.each(extender, function (method, name) {
-            child.prototype[name] = hasSuper(method) ?
-                superWrapper(parentProto, name, method) :
-                method;
-        });
-
-        child.__super__ = parentProto;
-        child.extend = extend;
-        child.defaults = defaults;
-
-        return child;
-    }
-
-    /**
      * Constructor.
      */
     function Class() {
@@ -103,19 +59,60 @@ define([
     }
 
     _.extend(Class, {
-        extend: extend,
         defaults: {
             tmplsIgnore: {
                 templates: true
             }
+        },
+
+        /**
+         * Creates new constructor based on a current prototype properties,
+         * extending them with properties specified in 'exender' object.
+         *
+         * @param {Object} [extender={}]
+         * @returns {Function} New constructor.
+         */
+        extend: function (extender) {
+            var parent = this,
+                defaults = extender.defaults || {},
+                parentProto = parent.prototype,
+                child;
+
+            defaults = defaults || {};
+            extender = extender || {};
+
+            delete extender.defaults;
+
+            if (extender.hasOwnProperty('constructor')) {
+                child = extender.constructor;
+            } else {
+                child = function () {
+                    parent.apply(this, arguments);
+                };
+            }
+
+            child.prototype = Object.create(parentProto);
+            child.prototype.constructor = child;
+
+            _.each(extender, function (method, name) {
+                child.prototype[name] = hasSuper(method) ?
+                    superWrapper(parentProto, name, method) :
+                    method;
+            });
+
+            return _.extend(child, {
+                __super__:  parentProto,
+                extend:     parent.extend,
+                defaults:   utils.extend({}, parent.defaults, defaults)
+            });
         }
     });
 
     _.extend(Class.prototype, {
         /**
-         * Starts initialization of an instance.
+         * Entry point to the initialization of consturctors' instance.
          *
-         * @param {Object} options
+         * @param {Object} [options={}]
          * @returns {Class} Chainable.
          */
         initialize: function (options) {
@@ -126,15 +123,15 @@ define([
 
         /**
          * Recursively extends data specified in constructors' 'defaults'
-         * property with a provided options object.
-         * Evaluates resulting object using string templates (see: mage/utils/template.js).
+         * property with provided options object. Evaluates resulting
+         * object using string templates (see: mage/utils/template.js).
          *
-         * @param {Object} options
+         * @param {Object} [options={}]
          * @returns {Class} Chainable.
          */
         initConfig: function (options) {
             var defaults    = this.constructor.defaults,
-                config      = utils.extend({}, defaults, options),
+                config      = utils.extend({}, defaults, options || {}),
                 ignored     = config.tmplsIgnore || {},
                 cached      = utils.omit(config, ignored);
 
@@ -144,9 +141,7 @@ define([
                 utils.nested(config, key, value);
             });
 
-            _.extend(this, config);
-
-            return this;
+            return _.extend(this, config);
         }
     });
 
