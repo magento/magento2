e867da77b1c8da7f989e11148f48f6a7843b8017
Illia Grybkov <igrybkov@ebay.com>
2015-06-26 15:46:28 +0300

    MAGETWO-36797: Implements Index Action for PhpHandler
    MAGETWO-38734: Implement persistent mechanism
    
    - Fix integration tests

--- dev/tests/integration/testsuite/Magento/Indexer/Model/Config/_files/result.php
+++ dev/tests/integration/testsuite/Magento/Indexer/Model/Config/_files/result.php
@@ -15,66 +15,72 @@ return [
             'description' => __('Rebuild Catalog product fulltext search index'),
             'fieldsets' =>
                 [
-                    'first' =>
-                        [
-                            'source' => 'MagentoModule\\ServiceClassOrRepositoryClass',
-                            'provider' => 'Magento\\Indexer\\Model\\Fieldset\\ProductFieldset',
-                            'fields' =>
-                                [
-                                    'title_alias' =>
-                                        [
-                                            'name' => 'title_alias',
-                                            'handler' => null,
-                                            'origin' => 'title',
-                                            'dataType' => 'text',
-                                            'type' => 'searchable',
-                                            'filters' =>
-                                                [
-                                                    0 => 'Magento\\Framework\\Search\\Index\\Filter\\LowercaseFilter',
-                                                ],
-                                            'reference' =>
-                                                [],
-                                        ],
-                                    'identifier' =>
-                                        [
-                                            'name' => 'identifier',
-                                            'handler' => 'Magento\\Framework\\Search\\Index\\Handler',
-                                            'origin' => 'identifier',
-                                            'dataType' => null,
-                                            'type' => 'filterable',
-                                            'filters' =>
-                                                [],
-                                            'reference' =>
-                                                [],
-                                        ],
-                                ],
-                        ],
-                    'second' =>
-                        [
-                            'source' => 'MagentoModule\\ServiceClassOrRepositoryClass',
-                            'provider' => null,
-                            'fields' =>
-                                [
-                                    'title' =>
-                                        [
-                                            'name' => 'title',
-                                            'handler' => null,
-                                            'origin' => 'title',
-                                            'dataType' => null,
-                                            'type' => 'searchable',
-                                            'filters' =>
-                                                [],
-                                            'reference' =>
-                                                [],
-                                        ],
-                                ],
-                            'reference' =>
-                                [
-                                    'fieldset' => 'first',
-                                    'from' => 'id_field2',
-                                    'to' => 'second_entity_id2',
-                                ],
-                        ],
+                    [
+                        'source' => 'MagentoModule\\ServiceClassOrRepositoryClass',
+                        'name' => 'first',
+                        'provider' => 'Magento\\Indexer\\Model\\Fieldset\\ProductFieldset',
+                        'fields' =>
+                            [
+                                'title_alias' =>
+                                    [
+                                        'name' => 'title_alias',
+                                        'handler' => null,
+                                        'origin' => 'title',
+                                        'dataType' => 'text',
+                                        'type' => 'searchable',
+                                        'filters' =>
+                                            [
+                                                0 => 'Magento\\Framework\\Search\\Index\\Filter\\LowercaseFilter',
+                                            ],
+                                    ],
+                                'identifier' =>
+                                    [
+                                        'name' => 'identifier',
+                                        'handler' => 'Magento\\Framework\\Search\\Index\\Handler',
+                                        'origin' => 'identifier',
+                                        'dataType' => null,
+                                        'type' => 'filterable',
+                                        'filters' =>
+                                            [],
+                                    ],
+                            ],
+                    ],
+                    [
+                        'source' => 'MagentoModule\\ServiceClassOrRepositoryClass',
+                        'name' => 'second',
+                        'provider' => null,
+                        'fields' =>
+                            [
+                                'title' =>
+                                    [
+                                        'name' => 'title',
+                                        'handler' => null,
+                                        'origin' => 'title',
+                                        'dataType' => null,
+                                        'type' => 'searchable',
+                                        'filters' =>
+                                            [],
+                                    ],
+                            ],
+                        'references' =>
+                            [
+                                'first' =>
+                                    [
+                                        0 =>
+                                            [
+                                                'fieldset' => 'first',
+                                                'from' => 'id_field',
+                                                'to' => 'second_entity_id',
+                                            ],
+                                        1 =>
+                                            [
+                                                'fieldset' => 'first',
+                                                'from' => 'id_field2',
+                                                'to' => 'second_entity_id2',
+                                            ],
+                                    ],
+                            ],
+                    ],
                 ],
             'saveHandler' => 'Magento\\Cms\\Model\\Indexer\\StoreResource',
             'structure' => 'Magento\\Cms\\Model\\Indexer\\IndexStructure',
