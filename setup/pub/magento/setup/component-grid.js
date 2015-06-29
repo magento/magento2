e17658761a82f68c5e91e0e890d286e222794dd8
Bryant Luk <bluk@ebay.com>
2015-06-29 16:28:00 -0500

    MAGETWO-38278: Integrate with the UI for the grid
    - Add the initial page

---
+++ setup/pub/magento/setup/component-grid.js
@@ -0,0 +1,9 @@
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+'use strict';
+angular.module('component-grid', ['ngStorage'])
+    .controller('componentGridController', ['$scope', '$state', '$localStorage', '$http', '$window', function ($scope, $state, $localStorage, $http, $window) {
+    }]);
