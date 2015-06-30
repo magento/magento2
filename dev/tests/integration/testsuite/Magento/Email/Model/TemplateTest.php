f4b21cf66b0af00df1b8591a50f5a57fbdb42235
Erik Hansen <erik@classyllama.com>
2015-06-29 23:55:46 -0500

    MAGETWO-37672: Responsive Email Foundation
    - Fixed unit tests that were failing due to merge 2631119

--- dev/tests/integration/testsuite/Magento/Email/Model/TemplateTest.php
+++ dev/tests/integration/testsuite/Magento/Email/Model/TemplateTest.php
@@ -51,8 +51,8 @@ class TemplateTest extends \PHPUnit_Framework_TestCase
         )->setConstructorArgs(
             [
                 $objectManager->get('Magento\Framework\Model\Context'),
-                $objectManager->get('Magento\Framework\Registry'),
                 $objectManager->get('Magento\Framework\View\DesignInterface'),
+                $objectManager->get('Magento\Framework\Registry'),
                 $objectManager->get('Magento\Store\Model\App\Emulation'),
                 $objectManager->get('Magento\Store\Model\StoreManager'),
                 $objectManager->create('Magento\Framework\View\Asset\Repository'),
