158a555f7daaf6888b346740a759f9673d41648b
Olexii Korshenko <okorshenko@ebay.com>
2015-06-30 13:47:47 +0300

    MAGETWO-39289: Estimate Shipping and Tax is affects first checkout step(summary bock)

--- app/code/Magento/Checkout/view/frontend/web/js/view/summary/abstract-total.js
+++ app/code/Magento/Checkout/view/frontend/web/js/view/summary/abstract-total.js
@@ -14,25 +14,17 @@ define(
     function (Component, quote, priceUtils, totals, stepNavigator) {
         "use strict";
         return Component.extend({
-            shippingAvailableFlag: undefined,
             getFormattedPrice: function (price) {
                 return priceUtils.formatPrice(price, quote.getPriceFormat());
             },
             getTotals: function() {
                 return totals.totals();
             },
-            isShippingAvailable: function() {
-                if (undefined !== this.shippingAvailableFlag) {
-                    return this.shippingAvailableFlag;
-                }
-                this.shippingAvailableFlag = stepNavigator.isAvailable('shipping');
-                return this.shippingAvailableFlag;
-            },
             isFullMode: function() {
                 if (!this.getTotals()) {
                     return false;
                 }
-                return !this.isShippingAvailable() || stepNavigator.isProcessed('shipping');
+                return stepNavigator.isProcessed('shipping');
             }
         });
     }
