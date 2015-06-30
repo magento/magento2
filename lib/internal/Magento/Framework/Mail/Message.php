2ba80445c10053211b653084fcf701c27c4e1a21
David Alger <david@classyllama.com>
2015-06-30 16:36:44 -0500

    MAGETWO-37843: All responsive email templates in Blank Theme
    - restored file removed in accidental type change in commit 8b79346

--- lib/internal/Magento/Framework/Mail/Message.php
+++ lib/internal/Magento/Framework/Mail/Message.php
@@ -1,1 +1,59 @@
-../../../../../.modman/magento2-responsive-email-test-data/lib/internal/Magento/Framework/Mail/Message.php
+<?php
+/**
+ * Mail Message
+ *
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Framework\Mail;
+
+class Message extends \Zend_Mail implements MessageInterface
+{
+    /**
+     * @param string $charset
+     */
+    public function __construct($charset = 'utf-8')
+    {
+        parent::__construct($charset);
+    }
+
+    /**
+     * Message type
+     *
+     * @var string
+     */
+    protected $messageType = self::TYPE_TEXT;
+
+    /**
+     * Set message body
+     *
+     * @param string $body
+     * @return $this
+     */
+    public function setBody($body)
+    {
+        return $this->messageType == self::TYPE_TEXT ? $this->setBodyText($body) : $this->setBodyHtml($body);
+    }
+
+    /**
+     * Set message body
+     *
+     * @return string
+     */
+    public function getBody()
+    {
+        return $this->messageType == self::TYPE_TEXT ? $this->getBodyText() : $this->getBodyHtml();
+    }
+
+    /**
+     * Set message type
+     *
+     * @param string $type
+     * @return $this
+     */
+    public function setMessageType($type)
+    {
+        $this->messageType = $type;
+        return $this;
+    }
+}
