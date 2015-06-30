307772468aa5f3869d9664e84f5f42b502bb0377
Anton Ohorodnyk <aohorodnyk@ebay.com>
2015-06-30 11:32:01 +0300

    MAGETWO-36797: Implements Index Action for PhpHandler
     - MAGETWO-38734: Implement persistent mechanism
    
      - Fixed message in exception

--- dev/tests/integration/framework/Magento/TestFramework/Annotation/DataFixtureBeforeTransaction.php
+++ dev/tests/integration/framework/Magento/TestFramework/Annotation/DataFixtureBeforeTransaction.php
@@ -139,7 +139,7 @@ class DataFixtureBeforeTransaction
             }
         } catch (\Exception $e) {
             throw new \Exception(
-                sprintf("Error in fixture: %s.\n %s", json_encode($fixture), $e->getMessage())
+                sprintf("Error in fixture: %s.\n %s", json_encode($fixture), (string)$e)
             );
         }
     }
