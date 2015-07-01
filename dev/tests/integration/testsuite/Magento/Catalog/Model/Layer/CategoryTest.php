b49907b7fae88c16afd2cfb8a2d5fd66db1d926c
Illia Grybkov <igrybkov@ebay.com>
2015-07-01 15:58:16 +0300

    MAGETWO-36797: Implements Index Action for PhpHandler
    MAGETWO-38734: Implement persistent mechanism
    
    - Fix integration tests:
      - Magento\Catalog\Model\Layer\CategoryTest

--- dev/tests/integration/testsuite/Magento/Catalog/Model/Layer/CategoryTest.php
+++ dev/tests/integration/testsuite/Magento/Catalog/Model/Layer/CategoryTest.php
@@ -9,6 +9,7 @@ namespace Magento\Catalog\Model\Layer;
  * Test class for \Magento\Catalog\Model\Layer.
  *
  * @magentoDataFixture Magento/Catalog/_files/categories.php
+ * @magentoDbIsolation disabled
  */
 class CategoryTest extends \PHPUnit_Framework_TestCase
 {
