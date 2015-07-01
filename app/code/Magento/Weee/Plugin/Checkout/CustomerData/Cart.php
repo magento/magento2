f0c40ba875488d61d956d3e403fdf5ecc7439ef6
Cristian Partica <cpartica@ebay.com>
2015-07-01 14:07:51 -0500

    MAGETWO-38960: Taxes aren't displayed for Mini Shopping Cart items on Storefront
    - CR refactoring, optimizations, deletion of unnecessary files
    - Unit tests

--- app/code/Magento/Weee/Plugin/Checkout/CustomerData/Cart.php
+++ app/code/Magento/Weee/Plugin/Checkout/CustomerData/Cart.php
@@ -6,7 +6,7 @@
 
 namespace Magento\Weee\Plugin\Checkout\CustomerData;
 
-class Cart
+class Cart extends \Magento\Tax\Plugin\Checkout\CustomerData\Cart
 {
     /**
      * @var \Magento\Customer\Model\Session
@@ -24,16 +24,6 @@ class Cart
     protected $itemPriceRenderer;
 
     /**
-     * @var \Magento\Quote\Model\Quote|null
-     */
-    protected $quote = null;
-
-    /**
-     * @var array|null
-     */
-    protected $totals = null;
-
-    /**
      * @param \Magento\Checkout\Model\Session $checkoutSession
      * @param \Magento\Checkout\Helper\Data $checkoutHelper
      * @param \Magento\Weee\Block\Item\Price\Renderer $itemPriceRenderer
@@ -46,89 +36,5 @@ class Cart
         $this->checkoutSession = $checkoutSession;
         $this->checkoutHelper = $checkoutHelper;
         $this->itemPriceRenderer = $itemPriceRenderer;
-    }
-
-    /**
-     * Add tax data to result
-     *
-     * @param \Magento\Checkout\CustomerData\Cart $subject
-     * @param array $result
-     * @return array
-     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
-     */
-    public function afterGetSectionData(\Magento\Checkout\CustomerData\Cart $subject, $result)
-    {
-        $result['subtotal_incl_tax'] = $this->checkoutHelper->formatPrice($this->getSubtotalInclTax());
-        $result['subtotal_excl_tax'] = $this->checkoutHelper->formatPrice($this->getSubtotalExclTax());
-
-        $items =$this->getQuote()->getAllVisibleItems();
-        if (is_array($items)) {
-            foreach ($items as $key => $item) {
-                /** @var $item \Magento\Quote\Model\Quote\Item */
-                $this->itemPriceRenderer->setItem($item);
-                $this->itemPriceRenderer->setTemplate('checkout/cart/item/price/sidebar.phtml');
-               // $this->itemPriceRenderer->setTemplate('item/price/unit.phtml');
-                $x=$this->itemPriceRenderer->toHtml();
-                $result['items'][$key]['product_price']=$this->itemPriceRenderer->toHtml();
-            }
-        }
-        return $result;
-    }
-
-    /**
-     * Get subtotal, including tax
-     *
-     * @return float
-     */
-    protected function getSubtotalInclTax()
-    {
-        $subtotal = 0;
-        $totals = $this->getTotals();
-        if (isset($totals['subtotal'])) {
-            $subtotal = $totals['subtotal']->getValueInclTax() ?: $totals['subtotal']->getValue();
-        }
-        return $subtotal;
-    }
-
-    /**
-     * Get subtotal, excluding tax
-     *
-     * @return float
-     */
-    protected function getSubtotalExclTax()
-    {
-        $subtotal = 0;
-        $totals = $this->getTotals();
-        if (isset($totals['subtotal'])) {
-            $subtotal = $totals['subtotal']->getValueExclTax() ?: $totals['subtotal']->getValue();
-        }
-        return $subtotal;
-    }
-
-    /**
-     * Get totals
-     *
-     * @return array
-     */
-    public function getTotals()
-    {
-        // TODO: TODO: MAGETWO-34824 duplicate \Magento\Checkout\CustomerData\Cart::getSectionData
-        if (empty($this->totals)) {
-            $this->totals = $this->getQuote()->getTotals();
-        }
-        return $this->totals;
-    }
-
-    /**
-     * Get active quote
-     *
-     * @return \Magento\Quote\Model\Quote
-     */
-    protected function getQuote()
-    {
-        if (null === $this->quote) {
-            $this->quote = $this->checkoutSession->getQuote();
-        }
-        return $this->quote;
     }
 }
