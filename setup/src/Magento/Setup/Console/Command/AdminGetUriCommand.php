9294826c3cbf2381cea864d39f12eba69652a604
Maksym Savich <msavich@ebay.com>
2015-07-01 16:51:04 -0500

    MAGETWO-39382: Add a command line (CLI) command to show admin url like admin:show if users forget

--- setup/src/Magento/Setup/Console/Command/AdminGetUriCommand.php
+++ setup/src/Magento/Setup/Console/Command/AdminGetUriCommand.php
@@ -39,7 +39,7 @@ class AdminGetUriCommand extends Command
     protected function configure()
     {
         $this->setName('admin:geturi')
-            ->setDescription('Get Admin Backend Frontname');
+            ->setDescription('Get Admin Backend URI');
         parent::configure();
     }
 
