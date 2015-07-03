4328d3bdd32ae730fc3170aecef75658e2fc7aee
Andrii Kasian <akasian@ebay.com>
2015-07-03 14:35:01 +0300

    MAGETWO-36797: Implements Index Action for PhpHandler
     - clean callbacks on rollback

--- lib/internal/Magento/Framework/Model/Resource/AbstractResource.php
+++ lib/internal/Magento/Framework/Model/Resource/AbstractResource.php
@@ -117,6 +117,8 @@ abstract class AbstractResource
     public function rollBack()
     {
         $this->_getWriteAdapter()->rollBack();
+        $adapterKey = spl_object_hash($this->_getWriteAdapter());
+        self::$_commitCallbacks[$adapterKey] = [];
         return $this;
     }
 
