da6d4c47fe822946578c99fd23cf86efe263ad50
Ihor Melnychenko <imelnychenko@ebay.com>
2015-07-07 11:28:58 +0300

    MAGETWO-35891: [Vanilla] Unit and Integration test coverage for MLS 10
    - fixed view.test.js unit test

--- dev/tests/js/jasmine/tests/app/code/Magento/Ui/base/js/grid/controls/bookmarks/view.test.js
+++ dev/tests/js/jasmine/tests/app/code/Magento/Ui/base/js/grid/controls/bookmarks/view.test.js
@@ -18,11 +18,50 @@ define([
                 provider: 'provider'
             });
         });
+        it('has initialize method', function () {
+            spyOn(view, "initialize");
+            view.initialize();
+            expect(view.initialize).toHaveBeenCalled();
+        });
         it('has initObservable method', function () {
             spyOn(view, "initObservable");
             view.initObservable();
             expect(view.initObservable).toHaveBeenCalled();
         });
-
+        it('has getData method', function () {
+            spyOn(view, "getData");
+            view.getData();
+            expect(view.getData).toHaveBeenCalled();
+        });
+        it('has setData method', function () {
+            spyOn(view, "setData");
+            view.setData();
+            expect(view.setData).toHaveBeenCalled();
+        });
+        it('has syncLabel method', function () {
+            spyOn(view, "syncLabel");
+            view.syncLabel();
+            expect(view.syncLabel).toHaveBeenCalled();
+        });
+        it('has startEdit method', function () {
+            spyOn(view, "startEdit");
+            view.startEdit();
+            expect(view.startEdit).toHaveBeenCalled();
+        });
+        it('has exportView method', function () {
+            spyOn(view, "exportView");
+            view.exportView();
+            expect(view.exportView).toHaveBeenCalled();
+        });
+        it('has onActivate method', function () {
+            spyOn(view, "onActivate");
+            view.onActivate();
+            expect(view.onActivate).toHaveBeenCalled();
+        });
+        it('has onActiveChange method', function () {
+            spyOn(view, "onActiveChange");
+            view.onActiveChange();
+            expect(view.onActiveChange).toHaveBeenCalled();
+        });
     })
 });
