3b854268cfb0c1dd4d9fc9cda682741b61dbb532
Michail Slabko <mslabko@ebay.com>
2015-07-07 14:09:08 +0300

    MAGETWO-37133: Variation grid on the Main page of PCF - MAGETWO-39798: Add Action column

--- app/code/Magento/ConfigurableProduct/view/adminhtml/web/js/variations/steps/summary.js
+++ app/code/Magento/ConfigurableProduct/view/adminhtml/web/js/variations/steps/summary.js
@@ -6,8 +6,9 @@ define([
     'uiComponent',
     'jquery',
     'ko',
-    'underscore'
-], function (Component, $, ko, _) {
+    'underscore',
+    'uiRegistry'
+], function (Component, $, ko, _, uiRegistry) {
     'use strict';
 
     var viewModel;
@@ -16,26 +17,8 @@ define([
         attributes: ko.observableArray([]),
         grid: ko.observableArray([]),
         attributesName: ko.observableArray([]),
-        productMatrix: ko.observableArray([]),
         nextLabel: $.mage.__('Generate Products'),
-        productForm: $('[data-form=edit-product]'),
-        getProductValue: function(name) {
-            return $('[name="product[' + name + ']"]', this.productForm).val();
-        },
-        getRowId: function(data, field) {
-            var key = data.variationKey;
-            return 'variations-matrix-' + key + '-' + field;
-        },
-        getVariationRowName: function(data, field) {
-            var key = data.variationKey;
-            return 'variations-matrix[' + key + '][' + field.split('/').join('][') + ']';
-        },
-        getAttributeRowName: function(attribute, field) {
-            return 'product[configurable_attributes_data][' + attribute.id  + '][' + field + ']';
-        },
-        getOptionRowName: function(attribute, option, field) {
-            return 'product[configurable_attributes_data][' + attribute.id  + '][values][' + option.value + '][' + field + ']';
-        },
+        variationsComponent: uiRegistry.async('configurableVariations'),
         /**
          * @param attributes example [['b1', 'b2'],['a1', 'a2', 'a3'],['c1', 'c2', 'c3'],['d1']]
          * @returns {*} example [['b1','a1','c1','d1'],['b1','a1','c2','d1']...]
@@ -62,8 +45,8 @@ define([
         },
         generateGrid: function (variations, getSectionValue) {
             //['a1','b1','c1','d1'] option = {label:'a1', value:'', section:{img:'',inv:'',pri:''}}
-            var productName = this.getProductValue('name');
-            this.productMatrix([]);
+            var productName = this.variationsComponent().getProductValue('name');
+            this.variationsComponent().reset();
             return _.map(variations, function (options) {
                 var variation = [], images, sku, inventory, price;
                 images = getSectionValue('images', options);
@@ -80,27 +63,9 @@ define([
                 });
                 price = getSectionValue('pricing', options);
                 variation.push(price);
-                this.populateVariationMatrix(options, images, sku, inventory, price);
+                this.variationsComponent().populateVariationMatrix(options, images, sku, inventory, price);
                 return variation;
             }, this);
-        },
-        populateVariationMatrix: function(options, images, sku, inventory, price) {
-            var attributes = _.reduce(options, function (memo, option) {
-                var attribute = {};
-                attribute[option.attribute_code] = option.value;
-                return _.extend(memo, attribute);
-            }, {});
-            this.productMatrix.push({
-                associated_product_ids: null,
-                images: images,
-                sku: sku,
-                inventory: inventory,
-                price: price || this.getProductValue('price'),
-                options: options,
-                attribute: JSON.stringify(attributes),
-                variationKey: _.values(attributes).join('-'),
-                weight: this.getProductValue('weight')
-            });
         },
         render: function (wizard) {
             this.sections(wizard.data.sections());
@@ -114,91 +79,11 @@ define([
             this.grid(this.generateGrid(this.generateVariation(this.attributes()), wizard.data.sectionHelper));
         },
         force: function (wizard) {
-            $('[data-role=configurable-attributes-container]').html('');
-            $('[data-role=product-variations-matrix]').html($('[data-role=product-variations-matrix-tmp]').html());
-            this.initImageUpload();
+            this.variationsComponent().attributes(this.attributes());
+            this.variationsComponent().render();
             $('[data-role=step-wizard-dialog]').trigger('closeModal');
         },
         back: function (wizard) {
-        },
-        generateImageGallery: function(variation) {
-            var gallery = [];
-            var imageFields = ['position', 'file', 'disabled', 'label'];
-            _.each(variation.images.images, function(image) {
-                _.each(imageFields, function(field) {
-                    gallery.push(
-                        '<input type="hidden" name="'
-                        + this.getVariationRowName(variation, 'media_gallery/images/' + image.file_id + '/' + field)
-                        + '" value="' + (image[field] || '') + '" />'
-                    );
-                }, this);
-                _.each(image.galleryTypes, function(imageType) {
-                    gallery.push(
-                        '<input type="hidden" name="' + this.getVariationRowName(variation, imageType)
-                        + '" value="' + image.file + '" />'
-                    );
-                }, this);
-            }, this);
-            return gallery.join('\n');
-        },
-        initImageUpload: function() {
-            require([
-                "jquery",
-                "mage/template",
-                "jquery/file-uploader",
-                "mage/mage",
-                "mage/translate"
-            ], function(jQuery, mageTemplate){
-
-                jQuery(function ($) {
-                    var matrix = $('[data-role=product-variations-matrix]');
-                    matrix.find('[data-action=upload-image] [name=image]').each(function() {
-                        $(this).fileupload({
-                            dataType: 'json',
-                            dropZone: $(this).closest('[data-role=row]'),
-                            acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i,
-                            done: function (event, data) {
-                                var tmpl;
-
-                                if (!data.result) {
-                                    return;
-                                }
-                                if (!data.result.error) {
-                                    var parentElement = $(event.target).closest('[data-column=image]'),
-                                        uploaderControl = parentElement.find('[data-action=upload-image]'),
-                                        imageElement = parentElement.find('[data-role=image]');
-
-                                    if (imageElement.length) {
-                                        imageElement.attr('src', data.result.url);
-                                    } else {
-                                        tmpl = mageTemplate(matrix.find('[data-template-for=variation-image]').html());
-
-                                        $(tmpl({
-                                            data: data.result
-                                        })).prependTo(uploaderControl);
-                                    }
-                                    parentElement.find('[name$="[image]"]').val(data.result.file);
-                                    parentElement.find('[data-toggle=dropdown]').show();
-                                } else {
-                                    alert($.mage.__('We don\'t recognize or support this file extension type.'));
-                                }
-                            },
-                            start: function(event) {
-                                $(event.target).closest('[data-action=upload-image]').addClass('loading');
-                            },
-                            stop: function(event) {
-                                $(event.target).closest('[data-action=upload-image]').removeClass('loading');
-                            }
-                        });
-                    });
-                    matrix.find('[data-action=no-image]').click(function (event) {
-                        var parentElement = $(event.target).closest('[data-column=image]');
-                        parentElement.find('[data-role=image]').remove();
-                        parentElement.find('[name$="[image]"]').val('');
-                        parentElement.find('[data-toggle=dropdown]').trigger('close.dropdown').hide();
-                    });
-                });
-            });
         }
     });
     return viewModel;
