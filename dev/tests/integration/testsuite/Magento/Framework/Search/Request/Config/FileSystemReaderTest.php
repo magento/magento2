d5867a230a71d12db8697a8cc0f8c9e986379580
Maksym Aposov <maposov@ebay.com>
2015-07-07 11:07:51 +0300

    MAGETWO-39709: Merge configuration for search_request.xml does not work

--- dev/tests/integration/testsuite/Magento/Framework/Search/Request/Config/FileSystemReaderTest.php
+++ dev/tests/integration/testsuite/Magento/Framework/Search/Request/Config/FileSystemReaderTest.php
@@ -21,9 +21,13 @@ class FileSystemReaderTest extends \PHPUnit_Framework_TestCase
         );
     }
 
-    public function testConvert()
+    public function testRead()
     {
         $result = $this->object->read();
+        // Filter values added by \Magento\CatalogSearch\Model\Search\ReaderPlugin
+        $result = array_filter($result, function ($k) {
+            return in_array($k, ['bool_query', 'filter_query', 'new_match_query']);
+        }, ARRAY_FILTER_USE_KEY);
         $expected = include __DIR__ . '/../../_files/search_request_merged.php';
         $this->assertEquals($expected, $result);
     }
