142eda53daaaad238f4d7941110e85da121f142c
Michail Slabko <mslabko@ebay.com>
2015-07-08 18:13:23 +0300

    MAGETWO-37133: Variation grid on the Main page of PCF - MAGETWO-39799: Adopt import/export

--- dev/tests/static/testsuite/Magento/Test/Legacy/_files/obsolete_constants.php
+++ dev/tests/static/testsuite/Magento/Test/Legacy/_files/obsolete_constants.php
@@ -729,5 +729,7 @@ return [
         'Magento\Framework\App\ScopeInterface',
         'Magento\Framework\App\Config\ScopeConfigInterface::SCOPE_TYPE_DEFAULT',
     ],
-    ['METHOD_WPS', 'Magento\Paypal\Model\Config']
+    ['METHOD_WPS', 'Magento\Paypal\Model\Config'],
+    ['ERROR_INVALID_PRICE_CORRECTION', 'Magento\ConfigurableImportExport\Model\Import\Product\Type\Configurable'],
+
 ];
