a58204cd12d91b1ac9baf4417d9e5d7879a7c921
AnastasiaBuniak <Anastasiia_Buniak@epam.com>
2015-07-08 15:04:58 +0300

    JS-31: Send friend scripts to eliminate (1 script)

---
+++ app/code/Magento/SendFriend/view/frontend/web/back-event.js
@@ -0,0 +1,16 @@
+/**
+* Copyright © 2015 Magento. All rights reserved.
+* See COPYING.txt for license details.
+*/
+define([
+    'jquery'
+], function ($) {
+    'use strict';
+
+    return function (config, element) {
+        $(element).on('click', function() {
+            history.back();
+            return false;
+        });
+    };
+});
