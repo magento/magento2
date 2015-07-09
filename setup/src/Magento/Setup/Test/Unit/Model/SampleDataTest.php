70e0e261c7a31fe4455f53b13603190a7cd5ce19
Leonid Poluyanov <lpoluyanov@ebay.com>
2015-07-09 17:23:00 +0300

    MAGETWO-39764: Create pull request for S24 results

--- setup/src/Magento/Setup/Test/Unit/Model/SampleDataTest.php
+++ setup/src/Magento/Setup/Test/Unit/Model/SampleDataTest.php
@@ -19,11 +19,6 @@ class SampleDataTest extends \PHPUnit_Framework_TestCase
     protected $sampleDataInstall;
 
     /**
-     * @var \Magento\Framework\ObjectManagerInterface|\PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $objectManagerInterface;
-
-    /**
      * @var \Magento\Framework\Setup\LoggerInterface
      */
     protected $loggerInterface;
@@ -35,47 +30,9 @@ class SampleDataTest extends \PHPUnit_Framework_TestCase
 
     protected function setUp()
     {
-        $this->objectManagerInterface = $this->getMock(
-            'Magento\Framework\ObjectManagerInterface',
-            [
-                'get',
-                'create',
-                'configure'
-            ],
-            [],
-            '',
-            false
-        );
         $this->loggerInterface = $this->getMock('Magento\Framework\Setup\LoggerInterface');
         $this->directoryList = $this->getMock('Magento\Framework\App\Filesystem\DirectoryList', [], [], '', false);
         $this->sampleDataInstall = new SampleData($this->directoryList);
-    }
-
-    public function testInstall()
-    {
-        $areaCode = 'adminhtml';
-        $userName = 'admin';
-        $modules = ['module_1', 'module_2'];
-        $configData = ['config_data'];
-        $sampleDataLogger = $this->getMock('Magento\SampleData\Model\Logger', ['setSubject'], [], '', false);
-        $appState = $this->getMock('Magento\Framework\App\State', ['setAreaCode'], [], '', false);
-        $configLoader = $this->getMock('Magento\Framework\App\ObjectManager\ConfigLoader', ['load'], [], '', false);
-        $installer = $this->getMock('Magento\SampleData\Model\Installer', ['run'], [], '', false);
-        $this->objectManagerInterface->expects($this->any())->method('get')->willReturnMap(
-            [
-                ['Magento\SampleData\Model\Logger', $sampleDataLogger],
-                ['Magento\Framework\App\State', $appState],
-                ['Magento\Framework\App\ObjectManager\ConfigLoader', $configLoader],
-                ['Magento\SampleData\Model\Installer', $installer]
-            ]
-        );
-        $sampleDataLogger->expects($this->any())->method('setSubject')->with($this->loggerInterface)->willReturnSelf();
-        $appState->expects($this->once())->method('setAreaCode')->with($areaCode)->willReturnSelf();
-        $configLoader->expects($this->once())->method('load')->with($areaCode)->willReturn($configData);
-        $this->objectManagerInterface->expects($this->once())->method('configure')->with($configData)
-            ->willReturnSelf();
-        $installer->expects($this->once())->method('run')->with($userName, $modules);
-        $this->sampleDataInstall->install($this->objectManagerInterface, $this->loggerInterface, $userName, $modules);
     }
 
     public function testIsDeployed()
