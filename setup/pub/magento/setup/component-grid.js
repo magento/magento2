a800e7a25148ad403cc09b9634adbde1af35a89a
Arkadii Chyzhov <achyzhov@ebay.com>
2015-07-09 19:37:20 +0300

    MAGETWO-39369: Integrate Component Management Grid UI with functionality

--- setup/pub/magento/setup/component-grid.js
+++ setup/pub/magento/setup/component-grid.js
@@ -8,5 +8,14 @@ angular.module('component-grid', ['ngStorage'])
     .controller('componentGridController', ['$scope', '$http', function ($scope, $http) {
       $http.get('index.php/componentGrid/components').success(function(data) {
         $scope.components = data.components;
+        $scope.total = data.total;
       });
+
+      $scope.selectedComponent = null;
+      $scope.isActiveActionsCell = function(component) {
+        return $scope.selectedComponent === component;
+      };
+      $scope.toggleActiveActionsCell = function(component) {
+        $scope.selectedComponent = $scope.selectedComponent == component ? null : component;
+      };
     }]);
