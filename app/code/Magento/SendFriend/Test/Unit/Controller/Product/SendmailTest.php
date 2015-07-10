c2388b1ff844ce808f1aea684eabfb534098ca9f
Sviatoslav Mankivskyi <smankivskyi@ebay.com>
2015-07-10 15:40:21 +0300

    MAGETWO-39788: Increase code coverage

--- app/code/Magento/SendFriend/Test/Unit/Controller/Product/SendmailTest.php
+++ app/code/Magento/SendFriend/Test/Unit/Controller/Product/SendmailTest.php
@@ -3,7 +3,7 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\SendFriend\Controller\Product;
+namespace Magento\SendFriend\Test\Unit\Controller\Product;
 
 use Magento\Framework\TestFramework\Unit\Helper\ObjectManager as ObjectManagerHelper;
 
