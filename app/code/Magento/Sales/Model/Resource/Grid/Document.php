4cd8cff3d3798f4d31a52de8943553b117ad7129
Dmytro Voskoboinikov <dvoskoboinikov@ebay.com>
2015-07-10 14:13:31 +0300

    MAGETWO-39905: UI components compatibility with Search API

--- app/code/Magento/Sales/Model/Resource/Grid/Document.php
+++ app/code/Magento/Sales/Model/Resource/Grid/Document.php
@@ -13,6 +13,12 @@ use Magento\Framework\Object;
  */
 class Document extends Object implements DocumentInterface
 {
+    protected $attributeValueFactory;
+    public function __construct(
+        \Magento\Framework\Api\AttributeValueFactory $attributeValueFactory
+    ) {
+        $this->attributeValueFactory = $attributeValueFactory;
+    }
     protected $id;
 
     public function getId()
@@ -23,5 +29,66 @@ class Document extends Object implements DocumentInterface
     public function setId($id)
     {
         $this->id = $id;
+    }
+
+    /**
+     * Get an attribute value.
+     *
+     * @param string $attributeCode
+     * @return \Magento\Framework\Api\AttributeInterface|null
+     */
+    public function getCustomAttribute($attributeCode)
+    {
+        /** @var \Magento\Framework\Api\AttributeInterface $attributeValue */
+        $attributeValue = $this->attributeValueFactory->create();
+        $attributeValue->setAttributeCode($attributeCode);
+        $attributeValue->setValue($this->getData($attributeCode));
+        return $attributeValue;
+    }
+
+    /**
+     * Set an attribute value for a given attribute code
+     *
+     * @param string $attributeCode
+     * @param mixed $attributeValue
+     * @return $this
+     */
+    public function setCustomAttribute($attributeCode, $attributeValue)
+    {
+        $this->setData($attributeCode, $attributeValue);
+        return $this;
+    }
+
+    /**
+     * Retrieve custom attributes values.
+     *
+     * @return \Magento\Framework\Api\AttributeInterface[]|null
+     */
+    public function getCustomAttributes()
+    {
+        $output = [];
+        foreach ($this->getData() as $key => $value) {
+            $attribute = $this->attributeValueFactory->create();
+            $output[] = $attribute->setValue($key)->setValue($value);
+        }
+        return $output;
+    }
+
+    /**
+     * Set array of custom attributes
+     *
+     * @param \Magento\Framework\Api\AttributeInterface[] $attributes
+     * @return $this
+     * @throws \LogicException
+     */
+    public function setCustomAttributes(array $attributes)
+    {
+        /** @var \Magento\Framework\Api\AttributeInterface $attribute */
+        foreach ($attributes as $attribute) {
+            $this->setData(
+                $attribute->getAttributeCode(),$attribute->getValue()
+            );
+        }
+        return $this;
     }
 }
