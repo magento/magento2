83b8dcca346bde46ed6642ad050d9c91bc84dded
Dmytro Voskoboinikov <dvoskoboinikov@ebay.com>
2015-07-10 19:23:07 +0300

    MAGETWO-39905: UI components compatibility with Search API

--- lib/internal/Magento/Framework/View/Element/UiComponent/DataProvider/DataProviderInterface.php
+++ lib/internal/Magento/Framework/View/Element/UiComponent/DataProvider/DataProviderInterface.php
@@ -91,12 +91,9 @@ interface DataProviderInterface
     /**
      * Add field filter to collection
      *
-     * @param string|int|array|null $condition
-     * @param null|string|array $field
-     * @param string $type
+     * @param \Magento\Framework\Api\Filter $filter
      * @return mixed
      */
-//    public function addFilter($condition, $field = null, $type = 'regular');
     public function addFilter(\Magento\Framework\Api\Filter $filter);
 
     /**
