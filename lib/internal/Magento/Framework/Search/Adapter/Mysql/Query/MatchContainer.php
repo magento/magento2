7019359f9bf65a6c72834edc6428da0b27cc8b0a
Illia Grybkov <igrybkov@ebay.com>
2015-07-10 14:17:26 +0300

    MAGETWO-39942: Low quick search performance(MySql)

---
+++ lib/internal/Magento/Framework/Search/Adapter/Mysql/Query/MatchContainer.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Framework\Search\Adapter\Mysql\Query;
+
+
+use Magento\Framework\Search\Request\QueryInterface;
+
+class MatchContainer
+{
+    /**
+     * @var QueryInterface
+     */
+    private $request;
+    /**
+     * @var string
+     */
+    private $conditionType;
+
+    /**
+     * @param QueryInterface $request
+     * @param string $conditionType
+     * @internal param string $name
+     */
+    public function __construct(QueryInterface $request, $conditionType)
+    {
+        $this->request = $request;
+        $this->conditionType = $conditionType;
+    }
+
+    /**
+     * @return QueryInterface
+     */
+    public function getRequest()
+    {
+        return $this->request;
+    }
+
+    /**
+     * @return string
+     */
+    public function getConditionType()
+    {
+        return $this->conditionType;
+    }
+}
