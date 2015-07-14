853704109e6c4a67de09bb0e6bb2de567665a9ec
Volodymyr Klymenko <vklymenko@ebay.com>
2015-07-14 16:41:12 +0300

    MAGETWO-21754: Public Cache Invalidation works for Product Pages if Do Mass Actions

---
+++ app/code/Magento/Catalog/Model/Plugin/PageCache/Product/Action.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Catalog\Model\Plugin\PageCache\Product;
+
+use Magento\Catalog\Model\Product;
+use Magento\Indexer\Model\CacheContext;
+use Magento\Framework\Event\ManagerInterface as EventManager;
+
+class Action
+{
+    /**
+     * @var CacheContext
+     */
+    protected $cacheContext;
+
+    /**
+     * @var EventManager
+     */
+    protected $eventManager;
+
+    /**
+     * @param CacheContext $cacheContext
+     * @param EventManager $eventManager
+     */
+    public function __construct(
+        CacheContext $cacheContext,
+        EventManager $eventManager
+    ) {
+        $this->cacheContext = $cacheContext;
+        $this->eventManager = $eventManager;
+    }
+
+    /**
+     * @param Product\Action $subject
+     * @param callable $proceed
+     * @param $productIds
+     * @param $attrData
+     * @param $storeId
+     * @return Product\Action
+     *
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function aroundUpdateAttributes(
+        Product\Action $subject,
+        \Closure $proceed,
+        $productIds,
+        $attrData,
+        $storeId
+    ) {
+        $returnValue = $proceed($productIds, $attrData, $storeId);
+
+        $this->cacheContext->registerEntities(Product::CACHE_TAG, $productIds);
+        $this->eventManager->dispatch('clean_cache_by_tags', ['object' => $this->cacheContext]);
+
+        return $returnValue;
+    }
+}
