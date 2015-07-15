12c429faf1f0dde1330bdcbb733d9be351364784
Yuri Kovsher <ikovsher@ebay.com>
2015-07-15 12:42:27 +0300

    MAGETWO-22618: View File Collectors Don't Support All glob() Patterns

---
+++ lib/internal/Magento/Framework/View/Test/Unit/Helper/PathPatternTest.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Framework\View\Test\Unit\Helper;
+
+class PathPatternTest extends \PHPUnit_Framework_TestCase
+{
+
+}
