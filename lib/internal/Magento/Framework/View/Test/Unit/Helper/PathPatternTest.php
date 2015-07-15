30ec3a667d75896f97ef5bbb0f7f31fd983dd38a
Yuri Kovsher <ikovsher@ebay.com>
2015-07-15 14:20:39 +0300

    MAGETWO-22618: View File Collectors Don't Support All glob() Patterns

--- lib/internal/Magento/Framework/View/Test/Unit/Helper/PathPatternTest.php
+++ lib/internal/Magento/Framework/View/Test/Unit/Helper/PathPatternTest.php
@@ -5,7 +5,65 @@
  */
 namespace Magento\Framework\View\Test\Unit\Helper;
 
+use Magento\Framework\TestFramework\Unit\Helper\ObjectManager as ObjectManagerHelper;
+
 class PathPatternTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @var \Magento\Framework\View\Helper\PathPattern
+     */
+    protected $pathPatternHelper;
 
+    /**
+     * @var \Magento\Framework\TestFramework\Unit\Helper\ObjectManager
+     */
+    protected $objectManagerHelper;
+
+    protected function setUp()
+    {
+        $this->objectManagerHelper = new ObjectManagerHelper($this);
+        $this->pathPatternHelper = $this->objectManagerHelper->getObject(
+            'Magento\Framework\View\Helper\PathPattern'
+        );
+    }
+
+    /**
+     * @param string $path
+     * @param string $expectedPattern
+     *
+     * @dataProvider translatePatternFromGlobDataProvider
+     */
+    public function testTranslatePatternFromGlob($path, $expectedPattern)
+    {
+        $this->assertEquals($expectedPattern, $this->pathPatternHelper->translatePatternFromGlob($path));
+    }
+
+    /**
+     * @return array
+     */
+    public function translatePatternFromGlobDataProvider()
+    {
+        return [
+            [
+                'path' => '*.xml',
+                'expectedPattern' => '[^/]*\\.xml'
+            ],
+            [
+                'path' => 'd??.*',
+                'expectedPattern' => 'd[^/][^/]\\.[^/]*'
+            ],
+            [
+                'path' => '[!0-9]?-[a-fA-F0-9].php',
+                'expectedPattern' => '[^0-9][^/]\\-[a-fA-F0-9]\\.php'
+            ],
+            [
+                'path' => 'config.{php,json,xml}',
+                'expectedPattern' => 'config\\.(?:php|json|xml)'
+            ],
+            [
+                'path' => 'c?nf[aio]g{-,}[!0-9/]*.{p,}html',
+                'expectedPattern' => 'c[^/]nf[aio]g(?:\\-|)[^0-9/][^/]*\\.(?:p|)html'
+            ]
+        ];
+    }
 }
