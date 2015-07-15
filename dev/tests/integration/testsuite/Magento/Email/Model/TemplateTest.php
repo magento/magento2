4f51de01469da8891f5579a6653a46988bb6f846
vpaladiychuk <vpaladiychuk@ebay.com>
2015-07-15 16:03:34 +0300

    MAGETWO-38049: Wrong Reset Password link in a Welcome email when customer was created on backend

--- dev/tests/integration/testsuite/Magento/Email/Model/TemplateTest.php
+++ dev/tests/integration/testsuite/Magento/Email/Model/TemplateTest.php
@@ -60,6 +60,7 @@ class TemplateTest extends \PHPUnit_Framework_TestCase
                 $objectManager->create('Magento\Framework\App\Config\ScopeConfigInterface'),
                 $objectManager->get('Magento\Email\Model\Template\Config'),
                 $objectManager->get('Magento\Email\Model\TemplateFactory'),
+                $objectManager->get('Magento\Framework\UrlInterface'),
                 $objectManager->get('Magento\Email\Model\Template\FilterFactory'),
             ]
         )->getMock();
