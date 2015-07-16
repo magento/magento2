54ee330990125381acd7e883c5d47c39d0529744
Safwan Khan <safwkhan@ebay.com>
2015-07-16 13:16:06 -0500

    MAGETWO-40090: User experience improvement on backup pages of Component Manager
    - Implemented the new user experience for backup pages.

--- setup/pub/magento/setup/component-update.js
+++ setup/pub/magento/setup/component-update.js
@@ -6,8 +6,28 @@
 'use strict';
 angular.module('component-update', ['ngStorage'])
     .controller('componentUpdateController', ['$scope', '$state', '$localStorage', '$http', '$window', function ($scope, $state, $localStorage, $http, $window) {
+        $scope.maintenanceCalled = false;
+        $scope.maintenanceStatus = false;
         if ($localStorage.packages) {
             $scope.packages = $localStorage.packages;
+        }
+        if ($localStorage.backupInfo) {
+            $scope.backupInfoPassed = $localStorage.backupInfo;
+        }
+        if (!$scope.backupInfoPassed.options.code && !$scope.backupInfoPassed.options.media && !$scope.backupInfoPassed.options.db) {
+            $scope.maintenanceCalled = true;
+            $http.post('index.php/maintenance/index', $scope.backupInfoPassed)
+                .success(function (data) {
+                    if (data['responseType'] === 'success') {
+                        $scope.maintenanceStatus = true;
+                    } else {
+                        $scope.errorMsg = data['error'];
+                    }
+                })
+                .error(function (data) {
+                    $scope.errorMsg = data;
+                });
+
         }
         $scope.started = false;
         $scope.errorMessage = '';
