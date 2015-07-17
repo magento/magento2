fbd6509d1a32d7e77409ed992b0169b611fcd25f
Volodymyr Kholoshenko <vkholoshenko@ebay.com>
2015-07-17 13:31:59 +0300

    MAGETWO-40331: Improve .min resolver

--- app/code/Magento/RequireJs/Block/Html/Head/Config.php
+++ app/code/Magento/RequireJs/Block/Html/Head/Config.php
@@ -7,6 +7,7 @@
 namespace Magento\RequireJs\Block\Html\Head;
 
 use Magento\Framework\RequireJs\Config as RequireJsConfig;
+use Magento\Framework\View\Asset\Minification;
 
 /**
  * Block responsible for including RequireJs config on the page
@@ -29,11 +30,17 @@ class Config extends \Magento\Framework\View\Element\AbstractBlock
     protected $pageConfig;
 
     /**
+     * @var Minification
+     */
+    protected $minification;
+
+    /**
      * @param \Magento\Framework\View\Element\Context $context
      * @param RequireJsConfig $config
      * @param \Magento\RequireJs\Model\FileManager $fileManager
      * @param \Magento\Framework\View\Page\Config $pageConfig
      * @param \Magento\Framework\View\Asset\ConfigInterface $bundleConfig
+     * @param Minification $minification
      * @param array $data
      */
     public function __construct(
@@ -42,6 +49,7 @@ class Config extends \Magento\Framework\View\Element\AbstractBlock
         \Magento\RequireJs\Model\FileManager $fileManager,
         \Magento\Framework\View\Page\Config $pageConfig,
         \Magento\Framework\View\Asset\ConfigInterface $bundleConfig,
+        Minification $minification,
         array $data = []
     ) {
         parent::__construct($context, $data);
@@ -49,6 +57,7 @@ class Config extends \Magento\Framework\View\Element\AbstractBlock
         $this->fileManager = $fileManager;
         $this->pageConfig = $pageConfig;
         $this->bundleConfig = $bundleConfig;
+        $this->minification = $minification;
     }
 
     /**
@@ -61,11 +70,16 @@ class Config extends \Magento\Framework\View\Element\AbstractBlock
         $requireJsConfig = $this->fileManager->createRequireJsConfigAsset();
         $assetCollection = $this->pageConfig->getAssetCollection();
 
-        $assetCollection->insert(
-            $requireJsConfig->getFilePath(),
-            $requireJsConfig,
-            RequireJsConfig::REQUIRE_JS_FILE_NAME
-        );
+        $after = RequireJsConfig::REQUIRE_JS_FILE_NAME;
+        if ($this->minification->isEnabled('js')) {
+            $minResolver = $this->fileManager->createMinResolverAsset();
+            $assetCollection->insert(
+                $minResolver->getFilePath(),
+                $minResolver,
+                $after
+            );
+            $after = $minResolver->getFilePath();
+        }
 
         if ($this->bundleConfig->isBundlingJsFiles()) {
             $bundleAssets = $this->fileManager->createBundleJsPool();
@@ -78,7 +92,7 @@ class Config extends \Magento\Framework\View\Element\AbstractBlock
                     $assetCollection->insert(
                         $bundleAsset->getFilePath(),
                         $bundleAsset,
-                        $requireJsConfig->getFilePath()
+                        $after
                     );
                 }
                 $assetCollection->insert(
@@ -86,8 +100,15 @@ class Config extends \Magento\Framework\View\Element\AbstractBlock
                     $staticAsset,
                     reset($bundleAssets)->getFilePath()
                 );
+                $after = $staticAsset->getFilePath();
             }
         }
+
+        $assetCollection->insert(
+            $requireJsConfig->getFilePath(),
+            $requireJsConfig,
+            $after
+        );
 
         return parent::_prepareLayout();
     }
