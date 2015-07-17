ad7d04ce9c6edc880e5f3cf4e90694ee18dc6474
Jaanek Liiskmaa <liiskmaa@gmail.com>
2015-07-17 15:12:23 +0300

    M2VM-74: Removed 'Select All' from MassAction menu in 'onoff' grid component

--- app/code/Magento/Ui/view/base/web/js/grid/columns/onoff.js
+++ app/code/Magento/Ui/view/base/web/js/grid/columns/onoff.js
@@ -12,6 +12,39 @@ define([
     return Column.extend({
         defaults: {
             bodyTmpl: 'ui/grid/cells/onoff'
+        },
+
+        /**
+         * Initializes components' static properties.
+         *
+         * @returns {Column} Chainable.
+         */
+        initProperties: function () {
+            this.actions = [{
+                value: 'selectPage',
+                label: $t('Select all on this page')
+            }, {
+                value: 'deselectPage',
+                label: $t('Deselect all on this page')
+            }];
+
+            return this._super();
+        },
+
+        isActionRelevant: function (actionId) {
+            var relevant = true;
+
+            switch (actionId) {
+                case 'selectPage':
+                    relevant = !this.isPageSelected(true);
+                    break;
+
+                case 'deselectPage':
+                    relevant =  this.isPageSelected();
+                    break;
+            }
+
+            return relevant;
         }
     });
 });
