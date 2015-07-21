9643664d90ed1881dad19824edd99e839b64fdda
Arkadii Chyzhov <achyzhov@ebay.com>
2015-07-21 14:39:46 -0500

    MAGETWO-39368: Merge development branches from Ogres, API, and UIMAGETWO-39368: Merge development branches from Ogres, API, and UI

--- setup/src/Magento/Setup/Controller/Navigation.php
+++ setup/src/Magento/Setup/Controller/Navigation.php
@@ -66,6 +66,7 @@ class Navigation extends AbstractActionController
     public function sideMenuAction()
     {
         $this->view->setTemplate('/magento/setup/navigation/side-menu.phtml');
+        $this->view->setVariable('isInstaller', $this->navigation->getType() ==  NavModel::NAV_INSTALLER);
         $this->view->setTerminal(true);
         return $this->view;
     }
