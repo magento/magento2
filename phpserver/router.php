09fea36d0dde212f1009380decf890142e4be729
Ivan Gavryshko <igavryshko@ebay.com>
2015-07-22 10:08:33 -0500

    MAGETWO-40675: Add router.php for php Built-in webserver #1517
    - fixed sanity check test

--- phpserver/router.php
+++ phpserver/router.php
@@ -1,5 +1,10 @@
 <?php
 /**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+/**
  * this is a router file for the php Built-in web server
  * https://secure.php.net/manual/en/features.commandline.webserver.php
  *
