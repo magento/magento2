1dfa084d003c168dc7b8f9997a630bcfaec62c1e
Ivan Gavryshko <igavryshko@ebay.com>
2015-07-23 17:36:57 -0500

    MAGETWO-40675: Add router.php for php Built-in webserver #1517
    - changes according to CR

--- phpserver/router.php
+++ phpserver/router.php
@@ -14,18 +14,28 @@
  * example usage: php -S 127.0.0.41:8082 -t ./pub/ ./router.php
  */
 
-$debug = function($val){
-    return;
-    if(is_array($val)){
-      $val = json_encode($val);
+/**
+ * Set it to true to enable debug mode
+ */
+define('DEBUG_ROUTER', false);
+
+$debug = function ($val) {
+
+    if (!DEBUG_ROUTER) {
+        return;
     }
+
+    if (is_array($val)) {
+        $val = json_encode($val);
+    }
+
     echo 'debug: '.$val.PHP_EOL.'<br/>'.PHP_EOL;
 };
 
 /**
  * Caution, this is very experimental stuff
- * no garant for working result
- * has tons of potencial big security holes
+ * no guarantee for working result
+ * has tons of potential big security holes
  */
 
 if (php_sapi_name() === 'cli-server') {
@@ -42,48 +52,46 @@ if (php_sapi_name() === 'cli-server') {
     $debug($path);
     $debug($route);
 
-    if( $path["basename"] == 'favicon.ico' ){
+    if ($path["basename"] == 'favicon.ico') {
         return false;
     }
 
     $debug($route);
     $debug(strpos($route, 'errors/default/css/'));
 
-    if(strpos($route, 'pub/errors/default/') === 0){
+    if (strpos($route, 'pub/errors/default/') === 0) {
         $route = preg_replace('#pub/errors/default/#', 'errors/default/', $route, 1);
     }
 
     $debug($route);
 
-
-    if(
+    if (
         strpos($route, 'media/') === 0 ||
         strpos($route, 'opt/') === 0 ||
         strpos($route, 'static/') === 0 ||
         strpos($route, 'errors/default/css/') === 0 ||
         strpos($route, 'errors/default/images/') === 0
-    ){
+    ) {
         $magentoPackagePubDir = __DIR__."/../pub";
 
         $file = $magentoPackagePubDir.'/'.$route;
         $debug($file);
-        if(file_exists($file)){
-          $debug('file exists');
-          return false;
-        }else{
-          $debug('file exists not');
-            if(strpos($route, 'static/') === 0){
+        if (file_exists($file)) {
+            $debug('file exists');
+            return false;
+        } else {
+            $debug('file does not exist');
+            if (strpos($route, 'static/') === 0) {
                 $route = preg_replace('#static/#', '', $route, 1);
                 $_GET['resource'] = $route;
                 include($magentoPackagePubDir.'/static.php');
                 exit;
-            }elseif(strpos($route, 'media/') === 0){
+            } elseif (strpos($route, 'media/') === 0) {
                 include($magentoPackagePubDir.'/get.php');
                 exit;
             }
         }
     }
 
-header('HTTP/1.0 404 Not Found');
-
+    header('HTTP/1.0 404 Not Found');
 }
