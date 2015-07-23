798c25e30a5240c54a0ac6efdd58b646ade07669
Roman Ganin <rganin@ebay.com>
2015-07-23 19:49:58 +0300

    MAGETWO-40663: Rename all Adapter* variables and properties to Connection
    - removed redundant wrappers

--- app/code/Magento/CatalogSearch/Test/Unit/Model/Resource/FulltextTest.php
+++ app/code/Magento/CatalogSearch/Test/Unit/Model/Resource/FulltextTest.php
@@ -63,7 +63,7 @@ class FulltextTest extends \PHPUnit_Framework_TestCase
     {
         $this->resource->expects($this->once())
             ->method('getTableName')
-            ->with('search_query', 'core_read')
+            ->with('search_query', 'default')
             ->willReturn('table_name_search_query');
         $this->connection->expects($this->once())
             ->method('update')
