573e6d70f155baba9bd5a0feb0ad0556d5eee96b
Oleksandr Rykh <orykh@ebay.com>
2015-07-23 09:57:16 +0300

    MTA-2405: [PS] Widget module functional tests maintenance
    - Move widget tests

---
+++ dev/tests/functional/tests/app/Magento/Widget/Test/Block/Adminhtml/Widget/Instance/Edit/Tab/WidgetOptionsType/CmsPageLink/Grid.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Widget\Test\Block\Adminhtml\Widget\Instance\Edit\Tab\WidgetOptionsType\CmsPageLink;
+
+use Magento\Backend\Test\Block\Widget\Grid as GridInterface;
+
+/**
+ * Chooser page grid
+ */
+class Grid extends GridInterface
+{
+    /**
+     * An element locator which allows to select entities in grid
+     *
+     * @var string
+     */
+    protected $selectItem = 'tbody tr td.col-title.col-chooser_title';
+
+    /**
+     * Filters array mapping
+     *
+     * @var array
+     */
+    protected $filters = [
+        'title' => [
+            'selector' => 'input[name="chooser_title"]',
+        ],
+    ];
+}
