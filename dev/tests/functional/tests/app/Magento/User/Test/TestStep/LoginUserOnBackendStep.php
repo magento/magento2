b9efd3c56e4cf751c16064450c0ae1e033a66668
Oleksandr Rykh <orykh@ebay.com>
2015-07-23 10:21:27 +0300

    MTA-2405: [PS] Widget module functional tests maintenance
    - Fixed logout user step

--- dev/tests/functional/tests/app/Magento/User/Test/TestStep/LoginUserOnBackendStep.php
+++ dev/tests/functional/tests/app/Magento/User/Test/TestStep/LoginUserOnBackendStep.php
@@ -59,9 +59,12 @@ class LoginUserOnBackendStep implements TestStepInterface
      */
     public function run()
     {
-        $this->logoutUserOnBackendStep->run();
-
         $this->adminAuth->open();
+
+        if (!$this->adminAuth->getLoginBlock()->isVisible()) {
+            $this->logoutUserOnBackendStep->run();
+        }
+
         $this->adminAuth->getLoginBlock()->fill($this->user);
         $this->adminAuth->getLoginBlock()->submit();
         $this->adminAuth->getLoginBlock()->waitFormNotVisible();
