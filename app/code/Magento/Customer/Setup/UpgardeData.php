969e06964d34a6a12ff230607a8639cad44892af
Maxim Medinskiy <mmedinskiy@ebay.com>
2015-07-24 19:47:08 +0300

    MAGETWO-40056: Add ability to make attribute searchable in Grid

---
+++ app/code/Magento/Customer/Setup/UpgardeData.php
@@ -0,0 +1,246 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Customer\Setup;
+
+use Magento\Framework\Module\Setup\Migration;
+use Magento\Framework\Setup\UpgradeDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+/**
+ * @codeCoverageIgnore
+ */
+class UpgradeData implements UpgradeDataInterface
+{
+    /**
+     * Customer setup factory
+     *
+     * @var CustomerSetupFactory
+     */
+    private $customerSetupFactory;
+
+    /**
+     * Init
+     *
+     * @param CustomerSetupFactory $customerSetupFactory
+     */
+    public function __construct(CustomerSetupFactory $customerSetupFactory)
+    {
+        $this->customerSetupFactory = $customerSetupFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+     */
+    public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        if (version_compare($context->getVersion(), '2.0.1', '>=')) {
+            /** @var CustomerSetup $customerSetup */
+            $customerSetup = $this->customerSetupFactory->create(['setup' => $setup]);
+
+            $setup->startSetup();
+
+            $disableAGCAttribute = $customerSetup->getEavConfig()->getAttribute('customer', 'disable_auto_group_change');
+            $disableAGCAttribute->setData('used_in_forms', ['adminhtml_customer']);
+            $disableAGCAttribute->save();
+
+            $entityAttributes = [
+                'customer' => [
+                    'website_id' => [
+                        'is_used_in_grid' => true,
+                        'is_visible_in_grid' => true,
+                        'is_filterable_in_grid' => true,
+                        'is_searchable_in_grid' => false,
+                    ],
+                    'prefix' => [
+                        'is_used_in_grid' => true,
+                        'is_visible_in_grid' => false,
+                        'is_filterable_in_grid' => false,
+                        'is_searchable_in_grid' => false,
+                    ],
+                    'firstname' => [
+                        'is_used_in_grid' => true,
+                        'is_visible_in_grid' => false,
+                        'is_filterable_in_grid' => false,
+                        'is_searchable_in_grid' => false,
+                    ],
+                    'middlename' => [
+                        'is_used_in_grid' => true,
+                        'is_visible_in_grid' => false,
+                        'is_filterable_in_grid' => false,
+                        'is_searchable_in_grid' => false,
+                    ],
+                    'lastname' => [
+                        'is_used_in_grid' => true,
+                        'is_visible_in_grid' => false,
+                        'is_filterable_in_grid' => false,
+                        'is_searchable_in_grid' => false,
+                    ],
+                    'suffix' => [
+                        'is_used_in_grid' => true,
+                        'is_visible_in_grid' => false,
+                        'is_filterable_in_grid' => false,
+                        'is_searchable_in_grid' => false,
+                    ],
+                    'email' => [
+                        'is_used_in_grid' => true,
+                        'is_visible_in_grid' => true,
+                        'is_filterable_in_grid' => true,
+                        'is_searchable_in_grid' => true,
+                    ],
+                    'group_id' => [
+                        'is_used_in_grid' => true,
+                        'is_visible_in_grid' => true,
+                        'is_filterable_in_grid' => true,
+                        'is_searchable_in_grid' => false,
+                    ],
+                    'dob' => [
+                        'is_used_in_grid' => true,
+                        'is_visible_in_grid' => true,
+                        'is_filterable_in_grid' => true,
+                        'is_searchable_in_grid' => false,
+                    ],
+                    'default_billing' => [
+                        'is_used_in_grid' => true,
+                        'is_visible_in_grid' => false,
+                        'is_filterable_in_grid' => false,
+                        'is_searchable_in_grid' => true,
+                    ],
+                    'default_shipping' => [
+                        'is_used_in_grid' => true,
+                        'is_visible_in_grid' => false,
+                        'is_filterable_in_grid' => true,
+                        'is_searchable_in_grid' => false,
+                    ],
+                    'taxvat' => [
+                        'is_used_in_grid' => true,
+                        'is_visible_in_grid' => true,
+                        'is_filterable_in_grid' => false,
+                        'is_searchable_in_grid' => true,
+                    ],
+                    'confirmation' => [
+                        'is_used_in_grid' => true,
+                        'is_visible_in_grid' => true,
+                        'is_filterable_in_grid' => true,
+                        'is_searchable_in_grid' => false,
+                    ],
+                    'created_at' => [
+                        'is_used_in_grid' => true,
+                        'is_visible_in_grid' => true,
+                        'is_filterable_in_grid' => true,
+                        'is_searchable_in_grid' => false,
+                    ],
+                    'gender' => [
+                        'is_used_in_grid' => true,
+                        'is_visible_in_grid' => true,
+                        'is_filterable_in_grid' => true,
+                        'is_searchable_in_grid' => false,
+                    ],
+                ],
+                'customer_address' => [
+                    'prefix' => [
+                        'is_used_in_grid' => true,
+                        'is_visible_in_grid' => false,
+                        'is_filterable_in_grid' => false,
+                        'is_searchable_in_grid' => false,
+                    ],
+                    'firstname' => [
+                        'is_used_in_grid' => true,
+                        'is_visible_in_grid' => false,
+                        'is_filterable_in_grid' => false,
+                        'is_searchable_in_grid' => false,
+                    ],
+                    'middlename' => [
+                        'is_used_in_grid' => true,
+                        'is_visible_in_grid' => false,
+                        'is_filterable_in_grid' => false,
+                        'is_searchable_in_grid' => false,
+                    ],
+                    'lastname' => [
+                        'is_used_in_grid' => true,
+                        'is_visible_in_grid' => false,
+                        'is_filterable_in_grid' => false,
+                        'is_searchable_in_grid' => false,
+                    ],
+                    'suffix' => [
+                        'is_used_in_grid' => true,
+                        'is_visible_in_grid' => false,
+                        'is_filterable_in_grid' => false,
+                        'is_searchable_in_grid' => false,
+                    ],
+                    'company' => [
+                        'is_used_in_grid' => true,
+                        'is_visible_in_grid' => false,
+                        'is_filterable_in_grid' => false,
+                        'is_searchable_in_grid' => false,
+                    ],
+                    'street' => [
+                        'is_used_in_grid' => true,
+                        'is_visible_in_grid' => false,
+                        'is_filterable_in_grid' => false,
+                        'is_searchable_in_grid' => true,
+                    ],
+                    'city' => [
+                        'is_used_in_grid' => false,
+                        'is_visible_in_grid' => false,
+                        'is_filterable_in_grid' => false,
+                        'is_searchable_in_grid' => false,
+                    ],
+                    'country_id' => [
+                        'is_used_in_grid' => true,
+                        'is_visible_in_grid' => true,
+                        'is_filterable_in_grid' => true,
+                        'is_searchable_in_grid' => false,
+                    ],
+                    'region' => [
+                        'is_used_in_grid' => true,
+                        'is_visible_in_grid' => true,
+                        'is_filterable_in_grid' => true,
+                        'is_searchable_in_grid' => false,
+                    ],
+                    'region_id' => [
+                        'is_used_in_grid' => true,
+                        'is_visible_in_grid' => false,
+                        'is_filterable_in_grid' => true,
+                        'is_searchable_in_grid' => false,
+                    ],
+                    'postcode' => [
+                        'is_used_in_grid' => true,
+                        'is_visible_in_grid' => true,
+                        'is_filterable_in_grid' => true,
+                        'is_searchable_in_grid' => true,
+                    ],
+                    'telephone' => [
+                        'is_used_in_grid' => true,
+                        'is_visible_in_grid' => true,
+                        'is_filterable_in_grid' => true,
+                        'is_searchable_in_grid' => true,
+                    ],
+                    'fax' => [
+                        'is_used_in_grid' => true,
+                        'is_visible_in_grid' => false,
+                        'is_filterable_in_grid' => false,
+                        'is_searchable_in_grid' => true,
+                    ],
+                ],
+            ];
+
+            foreach ($entityAttributes as $entityType => $attributes) {
+                foreach ($attributes as $attributeCode => $attributeData) {
+                    $attribute = $customerSetup->getEavConfig()->getAttribute($entityType, $attributeCode);
+                    foreach ($attributeData as $key => $value) {
+                        $attribute->setData($key, $value);
+                    }
+                    $attribute->save();
+                }
+            }
+        }
+
+        $setup->endSetup();
+    }
+}
