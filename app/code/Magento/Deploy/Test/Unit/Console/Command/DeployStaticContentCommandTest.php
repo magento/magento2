f19c032a3d458260253e5a3c9f32143df952310b
Ankur Kaneria <akaneria@ebay.com>
2015-07-27 09:20:15 -0500

    MAGETWO-40677: Fatal error when running setup:static-content:deploy after setup:di:compile

--- app/code/Magento/Deploy/Test/Unit/Console/Command/DeployStaticContentCommandTest.php
+++ app/code/Magento/Deploy/Test/Unit/Console/Command/DeployStaticContentCommandTest.php
@@ -3,7 +3,7 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\Setup\Test\Unit\Console\Command;
+namespace Magento\Deploy\Test\Unit\Console\Command;
 
 use Magento\Deploy\Console\Command\DeployStaticContentCommand;
 use Symfony\Component\Console\Tester\CommandTester;
