447b755470baf95df2ce7a9ebdf94bbb4ae17e3c
Andrey Konosov <akonosov@ebay.com>
2015-07-27 11:51:49 +0300

    MAGETWO-39139: [GITHUB] Products are not shown in category right after import #1396  - Moved popup closing logic in "login" scenario

--- dev/tests/functional/tests/app/Magento/User/Test/TestStep/LogoutUserOnBackendStep.php
+++ dev/tests/functional/tests/app/Magento/User/Test/TestStep/LogoutUserOnBackendStep.php
@@ -48,7 +48,6 @@ class LogoutUserOnBackendStep implements TestStepInterface
     public function run()
     {
         $this->adminAuth->open();
-        $this->dashboard->getSystemMessageDialog()->closePopup();
 
         if ($this->dashboard->getAdminPanelHeader()->isVisible()) {
             $this->dashboard->getAdminPanelHeader()->logOut();
