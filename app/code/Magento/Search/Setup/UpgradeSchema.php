9b35488862cde1b1742951c2abd5621a7dae5167
Maksym Iakusha <miakusha@ebay.com>
2015-07-27 15:29:24 +0300

    MAGETWO-21144: When we will make simultaneously few search queries, then in catalogsearch_query will appear duplicates

---
+++ app/code/Magento/Search/Setup/UpgradeSchema.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Search\Setup;
+
+use Magento\Framework\Setup\InstallSchemaInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\Setup\UpgradeSchemaInterface;
+
+/**
+ * @codeCoverageIgnore
+ */
+class UpgradeSchema implements UpgradeSchemaInterfaceSchemaInterface
+{
+    /**
+     * {@inheritdoc}
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+     */
+    public function upgrade(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $installer = $setup;
+        $connection = $installer->getConnection();
+        if (version_compare($context->getVersion(), '2.0.1') < 0) {
+            $connection->dropIndex(
+                'search_query',
+                $installer->getIdxName('search_query', ['query_text', 'store_id', 'popularity'])
+            );
+            $connection->addIndex(
+                'search_query',
+                $installer->getIdxName(
+                    'search_query',
+                    ['query_text', 'store_id', 'popularity'],
+                    \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_UNIQUE
+                ),
+                ['query_text', 'store_id', 'popularity'],
+                ['type' => \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_UNIQUE]
+            );
+        }
+    }
+}
