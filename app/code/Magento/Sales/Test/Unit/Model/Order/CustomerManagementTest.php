88e82e588585bba4d68041816985f761c438b533
Michael Logvin <mlogvin@ebay.com>
2015-07-28 18:35:32 +0300

    MAGETWO-39905: UI components compatibility with Search API

--- app/code/Magento/Sales/Test/Unit/Model/Order/CustomerManagementTest.php
+++ app/code/Magento/Sales/Test/Unit/Model/Order/CustomerManagementTest.php
@@ -3,7 +3,7 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\Sales\Test\Unit\Model\Order\Customer;
+namespace Magento\Sales\Test\Unit\Model\Order;
 
 /**
  * Class BuilderTest
