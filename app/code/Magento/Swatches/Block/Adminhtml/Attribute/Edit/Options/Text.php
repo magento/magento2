7c5d2e71bccffea0b41974d278df8c9fd4b97679
Volodymyr Klymenko <vklymenko@ebay.com>
2015-07-28 12:12:27 +0300

    MAGETWO-40750: Make Product Attribute Swatches available for both CE and EE
     - Merge  MAGNSWTC JS Fix pull request #16

---
+++ app/code/Magento/Swatches/Block/Adminhtml/Attribute/Edit/Options/Text.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Swatches\Block\Adminhtml\Attribute\Edit\Options;
+
+/**
+ * Block Class for Text Swatch
+ */
+class Text extends AbstractSwatch
+{
+    /**
+     * @var string
+     */
+    protected $_template = 'Magento_Swatches::catalog/product/attribute/text.phtml';
+}
---
+++ app/code/Magento/Swatches/Test/Unit/Controller/Adminhtml/Product/Attribute/Plugin/SaveTest.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Swatches\Test\Unit\Controller\Adminhtml\Product\Attribute\Plugin;
+
+class SaveTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider dataRequest
+     */
+    public function testBeforeDispatch($dataRequest, $runTimes)
+    {
+        $subject = $this->getMock('\Magento\Catalog\Controller\Adminhtml\Product\Attribute\Save', [], [], '', false);
+        $request = $this->getMock(
+            '\Magento\Framework\App\RequestInterface',
+            [
+                'getPostValue',
+                'setPostValue',
+                'getModuleName',
+                'setModuleName',
+                'getActionName',
+                'setActionName',
+                'getParam',
+                'setParams',
+                'getParams',
+                'getCookie',
+                'isSecure'
+            ],
+            [],
+            '',
+            false
+        );
+
+        $objectManager = new \Magento\Framework\TestFramework\Unit\Helper\ObjectManager($this);
+        $controller = $objectManager->getObject('\Magento\Swatches\Controller\Adminhtml\Product\Attribute\Plugin\Save');
+
+        $request->expects($this->once())->method('getPostValue')->willReturn($dataRequest);
+        $request->expects($this->exactly($runTimes))->method('setPostValue')->willReturn($this->returnSelf());
+
+        $controller->beforeDispatch($subject, $request);
+    }
+
+    public function dataRequest()
+    {
+        return [
+            [
+                ['frontend_input' => 'swatch_visual'],
+                1
+            ],
+            [
+                ['frontend_input' => 'swatch_text'],
+                1
+            ],
+            [
+                ['frontend_input' => 'select'],
+                1
+            ],
+            [
+                [],
+                0
+            ],
+            [
+                null,
+                0
+            ],
+        ];
+    }
+}
