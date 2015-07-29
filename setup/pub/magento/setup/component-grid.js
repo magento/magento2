29edb88360248b54d394293637507324e12a38ae
Eddie Lau <kahlau@ebay.com>
2015-07-29 17:44:46 -0500

    MAGETWO-38267: Complete flow with pages from Component Manager
    - added system upgrade flow

--- setup/pub/magento/setup/component-grid.js
+++ setup/pub/magento/setup/component-grid.js
@@ -39,7 +39,7 @@ angular.module('component-grid', ['ngStorage'])
           // regular ng-click directives to work, use a tiny delay in closing the drop down.
           $timeout(function() {
               $scope.toggleActiveActionsCell(component);
-          }, 10);
+          }, 100);
       };
 
       $scope.sync = function() {
