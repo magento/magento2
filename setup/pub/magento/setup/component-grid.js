cb7eb84638bc41c9500010d217ae6a2956b7861c
Cari Spruiell <cspruiell@ebay.com>
2015-07-29 14:50:30 -0500

    MAGETWO-40638: Add pagination to Component Grid page
     - changes based on CR feedback

--- setup/pub/magento/setup/component-grid.js
+++ setup/pub/magento/setup/component-grid.js
@@ -95,7 +95,7 @@ angular.module('component-grid', ['ngStorage'])
     }])
     .filter('startFrom', function() {
         return function(input, start) {
-            start = parseInt(start);
+            start = parseInt(start, 10);
             return input.slice(start);
         }
     })
