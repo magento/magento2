45f666b93d5b687ed90a543dd9c78229f1645292
Vladyslav Shcherbyna <vshcherbyna@ebay.com>
2015-07-29 16:34:58 +0300

    MAGETWO-40498: Apply TransactionManager interfaces

--- app/code/Magento/Sales/Test/Unit/Model/Order/Payment/Transaction/ManagerTest.php
+++ app/code/Magento/Sales/Test/Unit/Model/Order/Payment/Transaction/ManagerTest.php
@@ -77,6 +77,122 @@ class ManagerTest extends \PHPUnit_Framework_TestCase
         );
     }
 
+    /**
+     * @dataProvider isTransactionExistsDataProvider
+     * @param string|null $transactionId
+     * @param bool $isRepositoryReturnTransaction
+     * @param bool $expectedResult
+     */
+    public function testIsTransactionExists($transactionId, $isRepositoryReturnTransaction, $expectedResult)
+    {
+        $paymentId = 1;
+        $orderId = 9;
+
+        if ($transactionId && $isRepositoryReturnTransaction) {
+            $transaction = $this->getMock(
+                'Magento\Sales\Model\Order\Payment\Transaction',
+                [],
+                [],
+                '',
+                false
+            );
+            $this->repositoryMock->expects($this->once())->method('getByTxnId')->willReturn($transaction);
+        }
+
+        $this->assertEquals(
+            $expectedResult,
+            $this->manager->isTransactionExists($transactionId, $paymentId, $orderId)
+        );
+    }
+
+    /**
+     * @dataProvider generateTransactionIdDataProvider
+     * @param string|null $transactionId
+     * @param string|null $parentTransactionId
+     * @param string|null $transactionBasedTxnId
+     * @param string $type
+     * @param string|null $expectedResult
+     */
+    public function testGenerateTransactionId(
+        $transactionId,
+        $parentTransactionId,
+        $transactionBasedTxnId,
+        $type,
+        $expectedResult
+    ) {
+        $transactionBasedOn = false;
+
+        $payment = $this->getMock(
+            'Magento\Sales\Model\Order\Payment',
+            ["setParentTransactionId", "getParentTransactionId", "getTransactionId"],
+            [],
+            '',
+            false
+        );
+        $payment->expects($this->atLeastOnce())->method('getTransactionId')->willReturn($transactionId);
+
+        if (!$parentTransactionId && !$transactionId && $transactionBasedTxnId) {
+            $transactionBasedOn = $this->getMock(
+                'Magento\Sales\Model\Order\Payment\Transaction',
+                [],
+                [],
+                '',
+                false
+            );
+            $transactionBasedOn->expects($this->once())->method('getTxnId')->willReturn($transactionBasedTxnId);
+            $payment->expects($this->once())->method("setParentTransactionId")->with($transactionBasedTxnId);
+        }
+        $payment->expects($this->exactly(2))->method('getParentTransactionId')->willReturnOnConsecutiveCalls(
+            $parentTransactionId,
+            $transactionBasedOn ? $transactionBasedTxnId : $parentTransactionId
+        );
+
+        $this->assertEquals(
+            $expectedResult,
+            $this->manager->generateTransactionId($payment, $type, $transactionBasedOn)
+        );
+    }
+
+    /**
+     * @return array$transactionId, $parentTransactionId, $transactionBasedTxnId
+     */
+    public function generateTransactionIdDataProvider()
+    {
+        return [
+            'withoutTransactionId' => [
+                'transactionId' => null,
+                'parentTransactionId' => 2,
+                'transactionBasedOnId' => 1,
+                'type' => Transaction::TYPE_REFUND,
+                'expectedResult' => "2-" . Transaction::TYPE_REFUND
+            ],
+            'withTransactionId' => [
+                'transactionId' => 33,
+                'parentTransactionId' => 2,
+                'transactionBasedOnId' => 1,
+                'type' => Transaction::TYPE_REFUND,
+                'expectedResult' => 33
+            ],
+            'withBasedTransactionId' => [
+                'transactionId' => null,
+                'parentTransactionId' => null,
+                'transactionBasedOnId' => 4,
+                'type' => Transaction::TYPE_REFUND,
+                'expectedResult' => "4-" . Transaction::TYPE_REFUND
+            ],
+        ];
+    }
+
+    public function isTransactionExistsDataProvider()
+    {
+        return [
+            'withTransactionIdAndTransaction' => ["100-refund", true, true],
+            'withoutTransactionIdAndWithTransaction' => [null, true, false],
+            'withTransactionIdAndWithoutTransaction' => ["100-refund", false, false],
+            'withoutTransactionIdAndWithoutTransaction' => [null, false, false],
+        ];
+    }
+
     public function getAuthorizationDataProvider()
     {
         return [
