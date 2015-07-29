2a44026a7d853fc7acecedf0fe00551f5738f345
David Alger <david@classyllama.com>
2015-07-29 14:34:46 -0500

    REL-12 HTML markup in Plain Text Templates
    - resolving bamboo static tests

--- app/code/Magento/Newsletter/Test/Unit/Model/TemplateTest.php
+++ app/code/Magento/Newsletter/Test/Unit/Model/TemplateTest.php
@@ -3,6 +3,7 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
+
 namespace Magento\Newsletter\Test\Unit\Model;
 use Magento\Framework\App\TemplateTypesInterface;
 
