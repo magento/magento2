29ed78a0236dc333553d37c763ab9976881fa32b
Bryant Luk <bluk@ebay.com>
2015-07-29 18:19:10 -0500

    MAGETWO-39993: Create Producer Interface and RabbitMQ implementation
    - Add the AmqpProducer configuration and send out a message

--- AmqpProducer.php
+++ AmqpProducer.php
@@ -6,16 +6,50 @@
 
 namespace Magento\Framework\Amqp;
 
+use PhpAmqpLib\Connection\AMQPConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+use Magento\Framework\Amqp\RabbitMqConfig;
+use Magento\Framework\Amqp\MessageEncoder;
+
 /**
  * An AMQP Producer to handle publishing a message.
  */
 class AmqpProducer implements ProducerInterface
 {
     /**
+     * @var RabbitMqConfig
+     */
+    private $config;
+
+    public function __construct(
+        RabbitMqConfig $config
+    ) {
+        $this->config = $config;
+    }
+
+    /**
      * {@inheritdoc}
      */
     public function publish($topicName, $data)
     {
-        /* do nothing for now */
+        $exchange = 'magento';
+
+        $connection = new AMQPConnection(
+            $this->config->getValue(RabbitMqConfig::HOST),
+            $this->config->getValue(RabbitMqConfig::PORT),
+            $this->config->getValue(RabbitMqConfig::USERNAME),
+            $this->config->getValue(RabbitMqConfig::PASSWORD),
+            $this->config->getValue(RabbitMqConfig::VIRTUALHOST)
+        );
+        $channel = $connection->channel();
+        $channel->queue_declare($topicName, false, true, false, false);
+        $channel->exchange_declare($exchange, 'direct', false, true, false);
+        $channel->queue_bind($topicName, $exchange);
+
+        $msg = new AMQPMessage($data, array('content_type' => 'text/plain', 'delivery_mode' => 2));
+        $channel->basic_publish($msg, $exchange);
+
+        $channel->close();
+        $connection->close();
     }
 }
