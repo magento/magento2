799599439970ff409eb493d7bc26ee05be695b9a
Ankur Kaneria <akaneria@ebay.com>
2015-07-29 14:19:57 -0500

    MAGETWO-40677: Fatal error when running setup:static-content:deploy after setup:di:compile

--- app/code/Magento/Deploy/Model/Deployer.php
+++ app/code/Magento/Deploy/Model/Deployer.php
@@ -4,7 +4,7 @@
  * See COPYING.txt for license details.
  */
 
-namespace Magento\Deploy;
+namespace Magento\Deploy\Model;
 
 use Magento\Framework\App\ObjectManagerFactory;
 use Magento\Framework\App\View\Deployment\Version;
