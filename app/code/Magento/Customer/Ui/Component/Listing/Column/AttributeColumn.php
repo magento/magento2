649e6a00c0bb099beac3bfa9a700fbf125f0379e
Sviatoslav Mankivskyi <smankivskyi@ebay.com>
2015-07-30 17:51:41 +0300

    MAGETWO-40059: Configure Grid UI

--- app/code/Magento/Customer/Ui/Component/Listing/Column/AttributeColumn.php
+++ app/code/Magento/Customer/Ui/Component/Listing/Column/AttributeColumn.php
@@ -8,29 +8,29 @@ namespace Magento\Customer\Ui\Component\Listing\Column;
 use Magento\Framework\View\Element\UiComponent\ContextInterface;
 use Magento\Framework\View\Element\UiComponentFactory;
 use Magento\Ui\Component\Listing\Columns\Column;
-use Magento\Customer\Api\CustomerMetadataInterface;
+use Magento\Customer\Api\MetadataInterface;
 
-class Gender extends Column
+class AttributeColumn extends Column
 {
-    /** @var CustomerMetadataInterface */
-    protected $customerMetadata;
+    /** @var MetadataInterface */
+    protected $metadata;
 
     /**
      * @param ContextInterface $context
      * @param UiComponentFactory $uiComponentFactory
-     * @param CustomerMetadataInterface $customerMetadata
+     * @param MetadataInterface $metadata
      * @param array $components
      * @param array $data
      */
     public function __construct(
         ContextInterface $context,
         UiComponentFactory $uiComponentFactory,
-        CustomerMetadataInterface $customerMetadata,
+        MetadataInterface $metadata,
         array $components = [],
         array $data = []
     ) {
         parent::__construct($context, $uiComponentFactory, $components, $data);
-        $this->customerMetadata = $customerMetadata;
+        $this->metadata = $metadata;
     }
 
     /**
@@ -39,20 +39,22 @@ class Gender extends Column
      * @param array $dataSource
      * @return void
      */
-    public function prepareDataSource(array & $dataSource)
+    public function prepareDataSource(array &$dataSource)
     {
         if (!isset($dataSource['data']['items'])) {
             return;
         }
-        $genderOptions = $this->customerMetadata->getAttributeMetadata('gender')->getOptions();
-        foreach ($dataSource['data']['items'] as & $item) {
-            if (!isset($item[$this->getData('name')])) {
-                continue;
-            }
-            foreach ($genderOptions as $option) {
-                if ($option->getValue() == $item[$this->getData('name')]) {
-                    $item[$this->getData('name')] = $option->getLabel();
-                    break;
+        $options = $this->metadata->getAttributeMetadata($this->getName())->getOptions();
+        if (count($options)) {
+            foreach ($dataSource['data']['items'] as &$item) {
+                if (!isset($item[$this->getName()])) {
+                    continue;
+                }
+                foreach ($options as $option) {
+                    if ($option->getValue() == $item[$this->getName()]) {
+                        $item[$this->getName()] = $option->getLabel();
+                        break;
+                    }
                 }
             }
         }
