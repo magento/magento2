247df966a40e1d197d3ff239c43a2e345920d4f9
Valeriy Nayda <vnayda@ebay.com>
2015-07-31 14:11:47 +0300

    MAGETWO-31442: Splitting all observers with automated tool
    -- MAGETWO-40835: Manual split of \Magento\CatalogInventory\Model\Observer

---
+++ app/code/Magento/CatalogInventory/Observer/ItemsForReindex.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\CatalogInventory\Observer;
+
+use Magento\Framework\Event\Observer as EventObserver;
+
+/**
+ * Catalog observer share data
+ */
+class ItemsForReindex
+{
+    /**
+     * @var array
+     */
+    protected $itemsForReindex;
+
+    /**
+     * @param array $items
+     * @return void
+     */
+    public function setItems(array $items)
+    {
+        $this->itemsForReindex = $items;
+    }
+
+    /**
+     * @return array
+     */
+    public function getItems()
+    {
+        return $this->itemsForReindex;
+    }
+
+    /**
+     * @return void
+     */
+    public function clear()
+    {
+        $this->itemsForReindex = [];
+    }
+}
