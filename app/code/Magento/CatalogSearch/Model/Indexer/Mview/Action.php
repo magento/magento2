5899b9cc817b6c6943c9096c6703b0b460266576
Andriy Nasinnyk <anasinnyk@ebay.com>
2015-08-03 16:35:29 +0300

    MAGETWO-40747: Resolve dependecy to indexer module

--- app/code/Magento/CatalogSearch/Model/Indexer/Mview/Action.php
+++ app/code/Magento/CatalogSearch/Model/Indexer/Mview/Action.php
@@ -3,13 +3,11 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-
 namespace Magento\CatalogSearch\Model\Indexer\Mview;
-
 
 use Magento\CatalogSearch\Model\Indexer\Fulltext;
 use Magento\Framework\Mview\ActionInterface;
-use Magento\Indexer\Model\IndexerInterfaceFactory;
+use Magento\Framework\Indexer\IndexerInterfaceFactory;
 
 class Action implements ActionInterface
 {
