8453f0b184b182aebfa94fc9f761655a3219774e
Stanislav Lopukhov <slopukhov@ebay.com>
2015-08-03 13:05:43 +0300

    MAGETWO-40881: Redirect "_setup" connection to requested connection

--- lib/internal/Magento/Framework/App/Resource/Config.php
+++ lib/internal/Magento/Framework/App/Resource/Config.php
@@ -54,6 +54,8 @@ class Config extends \Magento\Framework\Config\Data\Scoped implements ConfigInte
     {
         $connectionName = \Magento\Framework\App\Resource::DEFAULT_CONNECTION;
 
+        $resourceName = preg_replace("/_setup$/", '', $resourceName);
+
         if (!isset($this->_connectionNames[$resourceName])) {
             $resourcesConfig = $this->get();
             $pointerResourceName = $resourceName;
