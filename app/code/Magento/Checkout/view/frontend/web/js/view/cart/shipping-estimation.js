e218240a4b2a31090c8a25eef2c08c29c1034cc4
Stanislav Idolov <sidolov@ebay.com>
2015-08-03 15:27:09 +0300

    MAGETWO-40962: [Improvement] Implement Estimate Shipping information on ui component

--- app/code/Magento/Checkout/view/frontend/web/js/view/cart/shipping-estimation.js
+++ app/code/Magento/Checkout/view/frontend/web/js/view/cart/shipping-estimation.js
@@ -6,13 +6,31 @@
 /*global alert*/
 define(
     [
-        'uiComponent'
+        'uiComponent',
+        'Magento_Checkout/js/action/select-shipping-address',
+        'Magento_Checkout/js/model/address-converter'
     ],
-    function(Component) {
+    function(Component, selectShippingAddress, addressConverter) {
         'use strict';
         return Component.extend({
             defaults: {
                 template: 'Magento_Checkout/cart/shipping-estimation'
+            },
+            isStateProvinceRequired: true,
+            isCityActive: true,
+            isCityRequired: true,
+            isZipCodeRequired: true,
+
+            getEstimationInfo: function (elem, event) {
+                event.preventDefault();
+
+                var addressFlat = {
+                    'country': 'US',
+                    'postcode': 11111
+                };
+
+                var address = addressConverter.formAddressDataToQuoteAddress(addressFlat);
+                selectShippingAddress(address);
             }
         });
     }
