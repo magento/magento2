3f874048e4fdea51c675efbb1d1dc60e1b6e8fed
David Alger <david@classyllama.com>
2015-08-03 16:19:01 -0500

    MAGETWO-37635: Default Email Template Customization in Luma Theme
    - implemented address format

--- app/code/Magento/Store/Model/Address/Renderer.php
+++ app/code/Magento/Store/Model/Address/Renderer.php
@@ -7,6 +7,7 @@
 namespace Magento\Store\Model\Address;
 
 use Magento\Framework\Event\ManagerInterface as EventManager;
+use Magento\Framework\Filter\FilterManager;
 use Magento\Framework\Object;
 
 /**
@@ -15,30 +16,54 @@ use Magento\Framework\Object;
 class Renderer
 {
     /**
+     * Template used to format store address information
+     */
+    const DEFAULT_STORE_ADDRESS_FORMAT = "{{var name}}\n"
+        . "{{var street_line1}}\n"
+        . "{{depend street_line2}}{{var street_line2}}\n{{/depend}}"
+        . "{{var city}}, {{var region}} {{var postcode}},\n"
+        . "{{var country}}";
+
+    /**
      * @var EventManager
      */
     protected $eventManager;
 
     /**
+     * @var FilterManager
+     */
+    protected $filterManager;
+
+    /**
      * Constructor
      *
      * @param EventManager $eventManager
+     * @param FilterManager $filterManager
      */
-    public function __construct(EventManager $eventManager)
+    public function __construct(EventManager $eventManager, FilterManager $filterManager)
     {
         $this->eventManager = $eventManager;
+        $this->filterManager = $filterManager;
     }
 
     /**
      * Format address in a specific way
      *
-     * @param Object $address
-     * @param string $type
-     * @return null|string
+     * @param Object $storeInfo
+     * @param $type
+     * @return string
      */
-    public function format(Object $address, $type)
+    public function format(Object $storeInfo, $type = 'html')
     {
-        $this->eventManager->dispatch('store_address_format', ['type' => $type, 'address' => $address]);
-        return implode('<br />', $address->getData()); // todo: @davidalger; implement actual formatting
+        $this->eventManager->dispatch('store_address_format', ['type' => $type, 'store_info' => $storeInfo]);
+        $address = $this->filterManager->template(
+            self::DEFAULT_STORE_ADDRESS_FORMAT,
+            ['variables' => $storeInfo->getData()]
+        );
+
+        if ($type == 'html') {
+            $address = nl2br($address);
+        }
+        return $address;
     }
 }
