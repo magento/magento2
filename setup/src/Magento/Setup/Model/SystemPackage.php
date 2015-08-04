3b26cccd200ed16eadba03ec8bcf37aee4981f20
Ankur Kaneria <akaneria@ebay.com>
2015-08-04 12:14:26 -0500

    MAGETWO-41121: Support meta package and fix welcome message

--- setup/src/Magento/Setup/Model/SystemPackage.php
+++ setup/src/Magento/Setup/Model/SystemPackage.php
@@ -27,7 +27,7 @@ class SystemPackage
     private $magentoComposerApplication;
 
     /**
-     * @var Magento\Framework\Composer\ComposerInformation
+     * @var ComposerInformation
      */
     private $composerInfo;
 
@@ -35,6 +35,7 @@ class SystemPackage
      * Constructor
      *
      * @param MagentoComposerApplicationFactory $composerAppFactory
+     * @param ComposerInformation $composerInfo
      */
     public function __construct(
         MagentoComposerApplicationFactory $composerAppFactory,
