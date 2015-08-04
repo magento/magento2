6d991757d809b867541760966837b1cbfcabc56f
Ankur Kaneria <akaneria@ebay.com>
2015-08-04 12:29:17 -0500

    MAGETWO-41121: Support meta package and fix welcome message

--- lib/internal/Magento/Framework/Composer/ComposerInformation.php
+++ lib/internal/Magento/Framework/Composer/ComposerInformation.php
@@ -277,7 +277,7 @@ class ComposerInformation
      */
     public function isSystemPackage($packageName = '')
     {
-        if(preg_match('/magento\/product-*/', $packageName) == 1) {
+        if (preg_match('/magento\/product-*/', $packageName) == 1) {
             return true;
         }
         return false;
