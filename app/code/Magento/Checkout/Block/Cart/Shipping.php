143c914e31665020438440b036bcf40784df4252
Stanislav Idolov <sidolov@ebay.com>
2015-08-04 17:45:13 +0300

    MAGETWO-40962: [Improvement] Implement Estimate Shipping information on ui component

--- app/code/Magento/Checkout/Block/Cart/Shipping.php
+++ app/code/Magento/Checkout/Block/Cart/Shipping.php
@@ -5,13 +5,6 @@
  */
 namespace Magento\Checkout\Block\Cart;
 
-use Magento\Customer\Api\AddressRepositoryInterface;
-use Magento\Customer\Api\CustomerRepositoryInterface;
-use Magento\Framework\Pricing\PriceCurrencyInterface;
-use Magento\Quote\Api\Data\EstimateAddressInterfaceFactory;
-use Magento\Quote\Api\ShippingMethodManagementInterface;
-use Magento\Quote\Model\QuoteRepository;
-
 /**
  * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
  */
@@ -21,65 +14,25 @@ class Shipping extends \Magento\Checkout\Block\Cart\AbstractCart
      * Available Carriers Instances
      * @var null|array
      */
-    protected $_carriers = null;
+    protected $carriers = null;
 
     /**
      * Estimate Rates
      * @var array
      */
-    protected $_rates = [];
+    protected $rates = [];
 
     /**
      * Address Model
      *
      * @var array
      */
-    protected $_address = [];
-
-    /**
-     * @var \Magento\Directory\Block\Data
-     */
-    protected $_directoryBlock;
+    protected $address = [];
 
     /**
      * @var \Magento\Shipping\Model\CarrierFactoryInterface
      */
-    protected $_carrierFactory;
-
-    /**
-     * @var PriceCurrencyInterface
-     */
-    protected $priceCurrency;
-
-    /**
-     * @var EstimateAddressInterfaceFactory
-     */
-    protected $estimatedAddressFactory;
-
-    /**
-     * @var ShippingMethodManagementInterface
-     */
-    protected $shippingMethodManager;
-
-    /**
-     * @var AddressRepositoryInterface
-     */
-    protected $addressRepository;
-
-    /**
-     * @var CustomerRepositoryInterface
-     */
-    protected $customerRepository;
-
-    /**
-     * @var QuoteRepository
-     */
-    protected $quoteRepository;
-
-    /**
-     * @var \Magento\Checkout\Model\Cart\CollectQuote
-     */
-    protected $collectQuote;
+    protected $carrierFactory;
 
     /**
      * @var \Magento\Checkout\Model\CompositeConfigProvider
@@ -105,15 +58,7 @@ class Shipping extends \Magento\Checkout\Block\Cart\AbstractCart
      * @param \Magento\Framework\View\Element\Template\Context $context
      * @param \Magento\Customer\Model\Session $customerSession
      * @param \Magento\Checkout\Model\Session $checkoutSession
-     * @param \Magento\Directory\Block\Data $directoryBlock
      * @param \Magento\Shipping\Model\CarrierFactoryInterface $carrierFactory
-     * @param PriceCurrencyInterface $priceCurrency
-     * @param EstimateAddressInterfaceFactory $estimatedAddressFactory
-     * @param ShippingMethodManagementInterface $shippingMethodManager
-     * @param AddressRepositoryInterface $addressRepository
-     * @param CustomerRepositoryInterface $customerRepository
-     * @param QuoteRepository $quoteRepository
-     * @param \Magento\Checkout\Model\Cart\CollectQuote $collectQuote
      * @param \Magento\Checkout\Model\CompositeConfigProvider $configProvider
      * @param \Magento\Checkout\Block\Checkout\AttributeMerger $merger
      * @param \Magento\Directory\Model\Resource\Country\Collection $countryCollection
@@ -125,54 +70,20 @@ class Shipping extends \Magento\Checkout\Block\Cart\AbstractCart
         \Magento\Framework\View\Element\Template\Context $context,
         \Magento\Customer\Model\Session $customerSession,
         \Magento\Checkout\Model\Session $checkoutSession,
-        \Magento\Directory\Block\Data $directoryBlock,
         \Magento\Shipping\Model\CarrierFactoryInterface $carrierFactory,
-        PriceCurrencyInterface $priceCurrency,
-        EstimateAddressInterfaceFactory $estimatedAddressFactory,
-        ShippingMethodManagementInterface $shippingMethodManager,
-        AddressRepositoryInterface $addressRepository,
-        CustomerRepositoryInterface $customerRepository,
-        QuoteRepository $quoteRepository,
-        \Magento\Checkout\Model\Cart\CollectQuote $collectQuote,
         \Magento\Checkout\Model\CompositeConfigProvider $configProvider,
         \Magento\Checkout\Block\Checkout\AttributeMerger $merger,
         \Magento\Directory\Model\Resource\Country\Collection $countryCollection,
         \Magento\Directory\Model\Resource\Region\Collection $regionCollection,
         array $data = []
     ) {
-        $this->priceCurrency = $priceCurrency;
-        $this->_directoryBlock = $directoryBlock;
-        $this->_carrierFactory = $carrierFactory;
-        $this->estimatedAddressFactory = $estimatedAddressFactory;
-        $this->shippingMethodManager = $shippingMethodManager;
-        $this->addressRepository = $addressRepository;
-        $this->customerRepository = $customerRepository;
-        $this->quoteRepository = $quoteRepository;
-        $this->collectQuote = $collectQuote;
+        $this->carrierFactory = $carrierFactory;
         $this->configProvider = $configProvider;
         $this->merger = $merger;
         $this->countryCollection = $countryCollection;
         $this->regionCollection = $regionCollection;
         parent::__construct($context, $customerSession, $checkoutSession, $data);
         $this->_isScopePrivate = true;
-    }
-
-    /** Get config
-     *
-     * @param string $path
-     * @return string|null
-     */
-    public function getConfig($path)
-    {
-        return $this->_scopeConfig->getValue($path, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
-    }
-
-    /**
-     * @return \Magento\Directory\Block\Data
-     */
-    public function getDirectoryBlock()
-    {
-        return $this->_directoryBlock;
     }
 
     /**
@@ -182,11 +93,11 @@ class Shipping extends \Magento\Checkout\Block\Cart\AbstractCart
      */
     public function getEstimateRates()
     {
-        if (empty($this->_rates)) {
+        if (empty($this->rates)) {
             $groups = $this->getAddress()->getGroupedAllShippingRates();
-            $this->_rates = $groups;
+            $this->rates = $groups;
         }
-        return $this->_rates;
+        return $this->rates;
     }
 
     /**
@@ -196,38 +107,10 @@ class Shipping extends \Magento\Checkout\Block\Cart\AbstractCart
      */
     public function getAddress()
     {
-        if (empty($this->_address)) {
-            $this->_address = $this->getQuote()->getShippingAddress();
+        if (empty($this->address)) {
+            $this->address = $this->getQuote()->getShippingAddress();
         }
-        return $this->_address;
-    }
-
-    /**
-     * Get Carrier Name
-     *
-     * @param string $carrierCode
-     * @return string
-     */
-    public function getCarrierName($carrierCode)
-    {
-        if ($name = $this->_scopeConfig->getValue(
-            'carriers/' . $carrierCode . '/title',
-            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
-        )
-        ) {
-            return $name;
-        }
-        return $carrierCode;
-    }
-
-    /**
-     * Get Shipping Method
-     *
-     * @return string
-     */
-    public function getAddressShippingMethod()
-    {
-        return $this->getAddress()->getShippingMethod();
+        return $this->address;
     }
 
     /**
@@ -238,46 +121,6 @@ class Shipping extends \Magento\Checkout\Block\Cart\AbstractCart
     public function getEstimateCountryId()
     {
         return $this->getAddress()->getCountryId();
-    }
-
-    /**
-     * Get Estimate Postcode
-     *
-     * @return string
-     */
-    public function getEstimatePostcode()
-    {
-        return $this->getAddress()->getPostcode();
-    }
-
-    /**
-     * Get Estimate City
-     *
-     * @return string
-     */
-    public function getEstimateCity()
-    {
-        return $this->getAddress()->getCity();
-    }
-
-    /**
-     * Get Estimate Region Id
-     *
-     * @return mixed
-     */
-    public function getEstimateRegionId()
-    {
-        return $this->getAddress()->getRegionId();
-    }
-
-    /**
-     * Get Estimate Region
-     *
-     * @return string
-     */
-    public function getEstimateRegion()
-    {
-        return $this->getAddress()->getRegion();
     }
 
     /**
@@ -303,40 +146,24 @@ class Shipping extends \Magento\Checkout\Block\Cart\AbstractCart
     }
 
     /**
-     * Convert price from default currency to current currency
-     *
-     * @param float $price
-     * @return float
-     */
-    public function formatPrice($price)
-    {
-        return $this->priceCurrency->convertAndFormat(
-            $price,
-            true,
-            PriceCurrencyInterface::DEFAULT_PRECISION,
-            $this->getQuote()->getStore()
-        );
-    }
-
-    /**
      * Obtain available carriers instances
      *
      * @return array
      */
     public function getCarriers()
     {
-        if (null === $this->_carriers) {
-            $this->_carriers = [];
+        if (null === $this->carriers) {
+            $this->carriers = [];
             $this->getEstimateRates();
-            foreach ($this->_rates as $rateGroup) {
+            foreach ($this->rates as $rateGroup) {
                 if (!empty($rateGroup)) {
                     foreach ($rateGroup as $rate) {
-                        $this->_carriers[] = $this->_carrierFactory->get($rate->getCarrier());
+                        $this->carriers[] = $this->carrierFactory->get($rate->getCarrier());
                     }
                 }
             }
         }
-        return $this->_carriers;
+        return $this->carriers;
     }
 
     /**
@@ -382,40 +209,6 @@ class Shipping extends \Magento\Checkout\Block\Cart\AbstractCart
             }
         }
         return false;
-    }
-
-    /**
-     * Get shipping price html
-     *
-     * @param \Magento\Quote\Model\Quote\Address\Rate $shippingRate
-     * @return string
-     */
-    public function getShippingPriceHtml(\Magento\Quote\Model\Quote\Address\Rate $shippingRate)
-    {
-        /** @var \Magento\Checkout\Block\Shipping\Price $block */
-        $block = $this->getLayout()->getBlock('checkout.shipping.price');
-        $block->setShippingRate($shippingRate);
-        return $block->toHtml();
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    protected function _beforeToHtml()
-    {
-        $this->collectQuote->collect($this->getQuote());
-        return parent::_beforeToHtml();
-    }
-
-    /**
-     * Check if multiple countries for shipping is allowed
-     *
-     * @return bool
-     */
-    public function isMultipleCountriesAllowed()
-    {
-        $collection = $this->_directoryBlock->getCountryCollection();
-        return $collection->count() > 1 ? true : false;
     }
 
     /**
