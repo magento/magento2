b385673c6e124b7afe3a82e70e570d08aa22c72d
AnastasiaBuniak <Anastasiia_Buniak@epam.com>
2015-08-04 12:14:27 +0300

    JS-20: Gallery Widget Implementation

--- dev/tests/js/jasmine/tests/lib/mage/gallery.test.js
+++ dev/tests/js/jasmine/tests/lib/mage/gallery.test.js
@@ -16,13 +16,17 @@ define([
 
     var dataToUpdate = [
         {
-            img: 'http://i.fotorama.io/ab52465c-b3d8-4bf6-986a-a4bf815dfaed/-/stretch/off/-/resize/1280x/'
+            img: 'data:image/png;base64,' +
+            'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP4Xw8AAoABf5/NhYYAAAAASUVORK5CYII='
         }, {
-            img: 'http://i.fotorama.io/ab52465c-b3d8-4bf6-986a-a4bf815dfaed/-/stretch/off/-/resize/1280x/'
+            img: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAw' +
+            'CAAAAC0lEQVR42mP4Xw8AAoABf5/NhYYAAAAASUVORK5CYII='
         }, {
-            img: 'http://i.fotorama.io/ab52465c-b3d8-4bf6-986a-a4bf815dfaed/-/stretch/off/-/resize/1280x/'
+            img: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAw' +
+            'CAAAAC0lEQVR42mP4Xw8AAoABf5/NhYYAAAAASUVORK5CYII='
         }, {
-            img: 'http://i.fotorama.io/ab52465c-b3d8-4bf6-986a-a4bf815dfaed/-/stretch/off/-/resize/1280x/'
+            img: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAw' +
+            'CAAAAC0lEQVR42mP4Xw8AAoABf5/NhYYAAAAASUVORK5CYII='
         }
     ];
 
@@ -36,42 +40,43 @@ define([
     describe('mage/new-gallery', function () {
         it('gallery loaded', function () {
             expect($('.fotorama__nav__frame').length).toBe(conf.data.length);
-            galleryAPI.last();
         });
 
         it('show last', function () {
-            expect($('.fotorama__nav__shaft .fotorama__nav__frame:eq('+ (conf.data.length-1) +')').hasClass('fotorama__active')).toBeTruthy();
-            galleryAPI.first();
+            galleryAPI.last();
+            expect($('.fotorama__nav__shaft .fotorama__nav__frame:eq('+ (conf.data.length-1) +')')
+                .hasClass('fotorama__active')).toBeTruthy();
         });
 
         it('show first', function () {
+            galleryAPI.first();
             expect($('.fotorama__nav__shaft .fotorama__nav__frame:eq(0)').hasClass('fotorama__active')).toBeTruthy();
-            galleryAPI.next();
         });
 
         it('show next', function () {
+            galleryAPI.next();
             expect($('.fotorama__nav__shaft .fotorama__nav__frame:eq(1)').hasClass('fotorama__active')).toBeTruthy();
-            galleryAPI.prev();
         });
 
         it('show previos', function () {
+            galleryAPI.prev();
             expect($('.fotorama__nav__shaft .fotorama__nav__frame:eq(0)').hasClass('fotorama__active')).toBeTruthy();
-            galleryAPI.seek(3);
         });
 
         it('show by number', function () {
+            galleryAPI.seek(3);
             expect($('.fotorama__nav__shaft .fotorama__nav__frame:eq(2)').hasClass('fotorama__active')).toBeTruthy();
-            galleryAPI.updateOptions({
-                nav: "dots"
-            });
         });
 
         it('update options', function () {
+            galleryAPI.updateOptions({
+                nav: "dots"
+            });
             expect($('.fotorama__nav__shaft .fotorama__nav__frame').hasClass('fotorama__nav__frame--dot')).toBeTruthy();
-            galleryAPI.updateData(dataToUpdate);
         });
 
         it('update data', function () {
+            galleryAPI.updateData(dataToUpdate);
             expect($('.fotorama__nav__frame').length).toBe(dataToUpdate.length);
         });
     });
