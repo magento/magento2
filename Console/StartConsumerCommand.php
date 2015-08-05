f297c5fcc554db0c6ab456b717523b93afbf6403
Alexander Paliarush <apaliarush@ebay.com>
2015-08-05 15:52:03 +0300

    MAGETWO-40928: Implement CLI command to start Consumers

---
+++ Console/StartConsumerCommand.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Amqp\Console;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Magento\Framework\Amqp\ConsumerFactory;
+
+/**
+ * Command for starting AMQP consumers.
+ */
+class StartConsumerCommand extends Command
+{
+    const ARGUMENT_CONSUMER = 'consumer';
+    const ARGUMENT_NUMBER_OF_MESSAGES = 'max-messages';
+    const COMMAND_QUEUE_CONSUMERS_START = 'queue:consumers:start';
+
+    /**
+     * @var ConsumerFactory
+     */
+    private $consumerFactory;
+
+    /**
+     * {@inheritdoc}
+     *
+     * @param ConsumerFactory $consumerFactory
+     */
+    public function __construct(ConsumerFactory $consumerFactory, $name = null)
+    {
+        $this->consumerFactory = $consumerFactory;
+        parent::__construct($name);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $consumerName = $input->getArgument(self::ARGUMENT_CONSUMER);
+        $numberOfMessages = $input->getArgument(self::ARGUMENT_NUMBER_OF_MESSAGES);
+        $consumer = $this->consumerFactory->get($consumerName);
+        $consumer->process($numberOfMessages);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this->setName(self::COMMAND_QUEUE_CONSUMERS_START);
+        $this->setDescription('Start AMQP consumer.');
+        $this->addArgument(
+            self::ARGUMENT_CONSUMER,
+            InputArgument::REQUIRED,
+            'The name of the consumer to be started.'
+        );
+        $this->addArgument(
+            self::ARGUMENT_NUMBER_OF_MESSAGES,
+            InputArgument::OPTIONAL,
+            'The number of messages to be processed by the consumer before process termination. '
+            . 'If not specify - terminate after processing all queued messages.'
+        );
+        $this->setHelp(
+            <<<HELP
+This command starts AMQP consumer by its name.
+
+To start consumer which will process all queued messages and terminate execution:
+
+      <comment>%command.full_name% --consumer=some_consumer</comment>
+
+To specify the number of messages which should be processed by consumer before its termination:
+
+    <comment>%command.full_name% --consumer=some_consumer --max-messages=50</comment>
+HELP
+        );
+        parent::configure();
+    }
+}
---
+++ Test/Unit/Console/StartConsumerCommandTest.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Amqp\Test\Unit\Console;
+
+use Magento\Amqp\Console\StartConsumerCommand;
+
+/**
+ * Unit tests for StartConsumerCommand
+ */
+class StartConsumerCommandTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var \Magento\Framework\TestFramework\Unit\Helper\ObjectManager */
+    private $objectManager;
+
+    /**
+     * @var StartConsumerCommand
+     */
+    private $command;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        $this->objectManager = new \Magento\Framework\TestFramework\Unit\Helper\ObjectManager($this);
+        parent::setUp();
+    }
+
+    /**
+     * Test configure() method implicitly via construct invocation.
+     *
+     * @return void
+     */
+    public function testConfigure()
+    {
+        $this->command = $this->objectManager->getObject('Magento\Amqp\Console\StartConsumerCommand');
+
+        $this->assertEquals(StartConsumerCommand::COMMAND_QUEUE_CONSUMERS_START, $this->command->getName());
+        $this->assertEquals('Start AMQP consumer.', $this->command->getDescription());
+        /** Exception will be thrown if argument is not declared */
+        $this->command->getDefinition()->getArgument(StartConsumerCommand::ARGUMENT_CONSUMER);
+        $this->command->getDefinition()->getArgument(StartConsumerCommand::ARGUMENT_NUMBER_OF_MESSAGES);
+        $this->assertContains('To start consumer which will process', $this->command->getHelp());
+    }
+}
