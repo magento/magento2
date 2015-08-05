86262351f3944b71c453dcdade175146736cbc0c
Stanislav Idolov <sidolov@ebay.com>
2015-08-05 15:18:52 +0300

    MAGETWO-40960: Persist shipping method in the shopping cart

--- app/code/Magento/Checkout/view/frontend/web/js/view/cart/shipping-rates.js
+++ app/code/Magento/Checkout/view/frontend/web/js/view/cart/shipping-rates.js
@@ -11,17 +11,33 @@ define(
         'Magento_Catalog/js/price-utils',
         'Magento_Checkout/js/model/quote',
         'Magento_Checkout/js/action/select-shipping-method',
-        'Magento_Checkout/js/action/set-shipping-information'
+        'Magento_Checkout/js/action/set-shipping-information',
+        'Magento_Checkout/js/checkout-data'
     ],
-    function (ko, _, Component, shippingService, priceUtils, quote, selectShippingMethod, setShippingInformation) {
+    function (
+        ko,
+        _,
+        Component,
+        shippingService,
+        priceUtils,
+        quote,
+        selectShippingMethodAction,
+        setShippingInformationAction,
+        checkoutData
+    ) {
         'use strict';
-
         return Component.extend({
             defaults: {
                 template: 'Magento_Checkout/cart/shipping-rates'
             },
             shippingRates: shippingService.getShippingRates(),
             shippingRateGroups: ko.observableArray([]),
+            selectedShippingMethod: ko.computed(function () {
+                    return quote.shippingMethod()
+                        ? quote.shippingMethod().carrier_code + '_' + quote.shippingMethod().method_code
+                        : null;
+                }
+            ),
 
             /**
              * @override
@@ -63,15 +79,15 @@ define(
 
             /**
              * Set shipping method.
-             * @param {String} methodCode
-             * @param {String} carrierCode
+             * @param {String} methodData
              * @returns void
              */
-            selectShippingMethod: function (methodCode, carrierCode) {
-                selectShippingMethod({
-                    'carrier_code': carrierCode,
-                    'method_code': methodCode
+            selectShippingMethod: function (methodData) {
+                selectShippingMethodAction({
+                    'carrier_code': methodData.carrier_code,
+                    'method_code': methodData.method_code
                 });
+                checkoutData.setSelectedShippingRate(methodData.carrier_code + '_' + methodData.method_code);
             },
 
             /**
@@ -79,7 +95,7 @@ define(
              * @returns void
              */
             setShippingMethod: function () {
-                setShippingInformation().done(
+                setShippingInformationAction().done(
                     function () {
                         console.log(ko.unwrap(quote.getTotals()));
                     }
