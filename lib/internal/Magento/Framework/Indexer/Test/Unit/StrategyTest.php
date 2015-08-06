f7f59a591baebf00c06a12c7c6afe957f00d99e4
okarpenko <okarpenko@ebay.com>
2015-08-06 17:31:29 +0300

    MAGETWO-40863: Stabilization Customer Search

--- lib/internal/Magento/Framework/Indexer/Test/Unit/StrategyTest.php
+++ lib/internal/Magento/Framework/Indexer/Test/Unit/StrategyTest.php
@@ -3,7 +3,7 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\Framework\Indexer\Test\Unit\Table;
+namespace Magento\Framework\Indexer\Test\Unit;
 
 /**
  * Class StrategyTest
