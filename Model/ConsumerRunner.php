85b4c631d538f4aa53b5e069ada94d5dc538e49a
Alexander Paliarush <apaliarush@ebay.com>
2015-08-07 19:22:02 +0300

    MAGETWO-40934: Implement Cron schedulers instantiation

---
+++ Model/ConsumerRunner.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Amqp\Model;
+
+use Magento\Framework\Amqp\ConsumerFactory;
+use Magento\Framework\Exception\LocalizedException;
+
+/**
+ * Consumer runner class is used to run consumer, which name matches the magic method invoked on this class.
+ *
+ * Is used to schedule consumers execution in crontab.xml as follows:
+ * <code>
+ * <job name="consumerConsumerName" instance="Magento\Amqp\Model\ConsumerRunner" method="consumerName">
+ * </code>
+ * Where <i>consumerName</i> should be a valid name of consumer registered in some queue.xml
+ */
+class ConsumerRunner
+{
+    /**
+     * @var ConsumerFactory
+     */
+    private $consumerFactory;
+
+    /**
+     * Initialize dependencies.
+     *
+     * @param ConsumerFactory $consumerFactory
+     */
+    public function __construct(ConsumerFactory $consumerFactory)
+    {
+        $this->consumerFactory = $consumerFactory;
+    }
+
+    /**
+     * Process messages in queue using consumer, which name is equal to the current magic method name.
+     *
+     * @param string $name
+     * @param array $arguments
+     * @throws LocalizedException
+     * @return void
+     */
+    public function __call($name, $arguments)
+    {
+        try {
+            $consumer = $this->consumerFactory->get($name);
+        } catch (\Exception $e) {
+            throw new LocalizedException(
+                __(
+                    '"%callbackMethod" callback method specified in crontab.xml '
+                    . 'must have corresponding consumer declared in some queue.xml.',
+                    ['callbackMethod' => $name]
+                )
+            );
+        }
+        $consumer->process();
+    }
+}
---
+++ Test/Unit/Model/ConsumerRunnerTest.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Amqp\Test\Unit\Console;
+
+use Magento\Amqp\Model\ConsumerRunner;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Phrase;
+
+/**
+ * Unit tests for consumer runner
+ */
+class ConsumerRunnerTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var \Magento\Framework\TestFramework\Unit\Helper\ObjectManager */
+    private $objectManager;
+
+    /**
+     * @var ConsumerRunner
+     */
+    private $consumerRunner;
+
+    /**
+     * @var \Magento\Framework\Amqp\ConsumerFactory|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $consumerFactoryMock;
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setUp()
+    {
+        $this->objectManager = new \Magento\Framework\TestFramework\Unit\Helper\ObjectManager($this);
+        $this->consumerFactoryMock = $this->getMockBuilder('Magento\Framework\Amqp\ConsumerFactory')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->consumerRunner = $this->objectManager->getObject(
+            'Magento\Amqp\Model\ConsumerRunner',
+            ['consumerFactory' => $this->consumerFactoryMock]
+        );
+        parent::setUp();
+    }
+
+    /**
+     * Ensure that consumer, with name equal to invoked magic method name, is run.
+     *
+     * @return void
+     */
+    public function testMagicMethod()
+    {
+        /** @var \Magento\Framework\Amqp\ConsumerInterface|\PHPUnit_Framework_MockObject_MockObject $consumerMock */
+        $consumerMock = $this->getMockBuilder('Magento\Framework\Amqp\ConsumerInterface')->getMock();
+        $consumerMock->expects($this->once())->method('process');
+        $consumerName = 'someConsumerName';
+        $this->consumerFactoryMock
+            ->expects($this->once())
+            ->method('get')
+            ->with($consumerName)
+            ->willReturn($consumerMock);
+
+        $this->consumerRunner->$consumerName();
+    }
+
+    /**
+     * Ensure that exception will be thrown if requested magic method does not correspond to any declared consumer.
+     *
+     * @expectedException \Magento\Framework\Exception\LocalizedException
+     * @expectedExceptionMessage "nonDeclaredConsumer" callback method specified in crontab.xml must
+     * @return void
+     */
+    public function testMagicMethodNoRelatedConsumer()
+    {
+        /** @var \Magento\Framework\Amqp\ConsumerInterface|\PHPUnit_Framework_MockObject_MockObject $consumerMock */
+        $consumerMock = $this->getMockBuilder('Magento\Framework\Amqp\ConsumerInterface')->getMock();
+        $consumerName = 'nonDeclaredConsumer';
+        $this->consumerFactoryMock
+            ->expects($this->once())
+            ->method('get')
+            ->with($consumerName)
+            ->willThrowException(new LocalizedException(new Phrase("Some exception")));
+
+        $this->consumerRunner->$consumerName();
+    }
+}
