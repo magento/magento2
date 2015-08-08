93b64d3c7106b2ecbf102d20d6082593370e23b7
Ivan Gavryshko <igavryshko@ebay.com>
2015-08-07 19:59:42 -0500

    MAGETWO-41177: Consolidate one algorithm for populating system packages for upgrade and other tasks
    - fixed "Try Again" button behavior

--- setup/pub/magento/setup/select-version.js
+++ setup/pub/magento/setup/select-version.js
@@ -15,7 +15,7 @@ angular.module('select-version', ['ngStorage'])
         $scope.upgradeProcessError = false;
         $scope.componentsProcessed = false;
         $scope.componentsProcessError = false;
-        $scope.tryAgain = false;
+        $scope.tryAgain = 0;
 
         $http.get('index.php/select-version/systemPackage', {'responseType' : 'json'})
             .success(function (data) {
@@ -30,26 +30,32 @@ angular.module('select-version', ['ngStorage'])
                     $scope.upgradeProcessError = true;
                 }
                 $scope.upgradeProcessed = true;
-                $scope.tryAgain = true;
+                $scope.tryAgain++;
             })
             .error(function (data) {
                 $scope.upgradeProcessError = true;
+                $scope.tryAgain++;
             });
 
         $scope.updateComponents = {
             yes: false,
             no: true
         };
+
         $scope.$watch('updateComponents.no', function() {
+
             if (angular.equals($scope.updateComponents.no, true)) {
                 $scope.updateComponents.yes = false;
+                if ($scope.tryAgain < 0) {
+                    $scope.tryAgain++;
+                }
             }
         });
 
         $scope.$watch('updateComponents.yes', function() {
             if (angular.equals($scope.updateComponents.yes, true)) {
                 $scope.updateComponents.no = false;
-                $scope.tryAgain = false;
+                $scope.tryAgain--;
                 if (!$scope.componentsProcessed && !$scope.componentsProcessError) {
                     $scope.readyForNext = false;
                     $http.get('index.php/other-components-grid/components', {'responseType': 'json'}).
@@ -70,11 +76,11 @@ angular.module('select-version', ['ngStorage'])
                                 $scope.componentsProcessError = true;
                             }
                             $scope.componentsProcessed = true;
-                            $scope.tryAgain = true;
+                            $scope.tryAgain++;
                         })
                         .error(function (data) {
+                            $scope.tryAgain++;
                             $scope.componentsProcessError = true;
-                            $scope.tryAgain = true;
                         });
                 }
             }
