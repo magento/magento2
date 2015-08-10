c282b944a0dbf7cbcc7c6cb48716187dbb171f0a
Alexandr Skrashuk <Alexandr_Skrashuk@epam.com>
2015-08-10 16:02:25 +0300

    MAGNIMEX-5: Processing Errors for Product Import
     - PHPdoc added
     - Removed useless test

--- app/code/Magento/ImportExport/Test/Unit/Model/Import/ErrorProcessing/ProcessingErrorAggregatorTest.php
+++ app/code/Magento/ImportExport/Test/Unit/Model/Import/ErrorProcessing/ProcessingErrorAggregatorTest.php
@@ -34,6 +34,9 @@ class ProcessingErrorAggregatorTest extends \PHPUnit_Framework_TestCase
      */
     protected $processingErrorMock3;
 
+    /**
+     * Preparing mock objects
+     */
     public function setUp()
     {
         $this->processingErrorFactoryMock = $this->getMock(
@@ -84,17 +87,17 @@ class ProcessingErrorAggregatorTest extends \PHPUnit_Framework_TestCase
         );
     }
 
+    /**
+     * Test for method addError
+     */
     public function testAddError()
     {
         $this->model->addError('systemException', 'critical', 7, 'Some column name', 'Message', 'Description');
     }
 
-    public function testAddErrorNullMessageError()
-    {
-        $this->model->addErrorMessageTemplate('systemException', 'template');
-        $this->model->addError('systemException', 'critical', 7, 'Some column name', null, null);
-    }
-
+    /**
+     * Test for method addErrorMessageTemplate
+     */
     public function testAddErrorMessageTemplate()
     {
         $this->model->addErrorMessageTemplate('columnNotFound', 'Template: No column');
@@ -109,6 +112,9 @@ class ProcessingErrorAggregatorTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($expectedResult, $result);
     }
 
+    /**
+     * Test for method isRowInvalid. Expected true result.
+     */
     public function testIsRowInvalidTrue()
     {
         $this->model->addError('systemException', 'critical', 7, 'Some column name', 'Message', 'Description');
@@ -116,6 +122,9 @@ class ProcessingErrorAggregatorTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue($result);
     }
 
+    /**
+     * Test for method isRowInvalid. Expected false result.
+     */
     public function testIsRowInvalidFalse()
     {
         $this->model->addError('systemException');
@@ -123,12 +132,18 @@ class ProcessingErrorAggregatorTest extends \PHPUnit_Framework_TestCase
         $this->assertFalse($result);
     }
 
+    /**
+     * Test for method getInvalidRowsCount. Expected 0 invalid rows.
+     */
     public function testGetInvalidRowsCountZero()
     {
         $rowsNumber = $this->model->getInvalidRowsCount();
         $this->assertEquals($rowsNumber, 0);
     }
 
+    /**
+     * Test for method getInvalidRowsCount. Expected 1 invalid row.
+     */
     public function testGetInvalidRowsCountOne()
     {
         $this->model->addError('systemException', 'critical', 7, 'Some column name', 'Message', 'Description');
@@ -136,6 +151,9 @@ class ProcessingErrorAggregatorTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($rowsNumber, 1);
     }
 
+    /**
+     * Test for method getInvalidRowsCount. Expected 2 invalid rows.
+     */
     public function testGetInvalidRowsCountTwo()
     {
         $this->model->addError('systemException');
@@ -145,12 +163,18 @@ class ProcessingErrorAggregatorTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($rowsNumber, 2);
     }
 
+    /**
+     * Test for method initValidationStrategy.
+     */
     public function testInitValidationStrategy()
     {
         $this->model->initValidationStrategy('validation-stop-on-errors', 5);
         $this->assertEquals(5, $this->model->getAllowedErrorsCount());
     }
 
+    /**
+     * Test for method initValidationStrategy.
+     */
     public function testInitValidationStrategyExceed()
     {
         $this->model->addError('systemException', 'not-critical', 7, 'Some column name', 'Message', 'Description');
@@ -161,12 +185,18 @@ class ProcessingErrorAggregatorTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue($result);
     }
 
+    /**
+     * Test for method initValidationStrategy. Expected exeption due null incoming parameter
+     */
     public function testInitValidationStrategyException()
     {
         $this->setExpectedException('\Magento\Framework\Exception\LocalizedException');
         $this->model->initValidationStrategy(null);
     }
 
+    /**
+     * Test for method isErrorLimitExceeded. Expects error limit exceeded.
+     */
     public function testIsErrorLimitExceededTrue()
     {
         $this->model->addError('systemException');
@@ -176,6 +206,9 @@ class ProcessingErrorAggregatorTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue($result);
     }
 
+    /**
+     * Test for method isErrorLimitExceeded. Unexpects error limit exceeded.
+     */
     public function testIsErrorLimitExceededFalse()
     {
         $this->model->addError('systemException');
@@ -185,6 +218,9 @@ class ProcessingErrorAggregatorTest extends \PHPUnit_Framework_TestCase
         $this->assertFalse($result);
     }
 
+    /**
+     * Test for method hasFatalExceptions.
+     */
     public function testHasFatalExceptionsTrue()
     {
         $this->model->addError('systemException');
@@ -194,6 +230,9 @@ class ProcessingErrorAggregatorTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue($result);
     }
 
+    /**
+     * Test for method hasFatalExceptions. Expects no any fatal exceptions
+     */
     public function testHasFatalExceptionsFalse()
     {
         $this->model->addError('systemException', 'not-critical', 4, 'Some column name', 'Message', 'Description');
@@ -201,12 +240,18 @@ class ProcessingErrorAggregatorTest extends \PHPUnit_Framework_TestCase
         $this->assertFalse($result);
     }
 
+    /**
+     * Test for method hasToBeTerminated. Unexpects any errors and termination.
+     */
     public function testHasToBeTerminatedFalse()
     {
         $result = $this->model->hasToBeTerminated();
         $this->assertFalse($result);
     }
 
+    /**
+     * Test for method hasToBeTerminated. Expects errors and execute termination.
+     */
     public function testHasToBeTerminatedTrue()
     {
         $this->model->addError('systemException', 'not-critical', 4, 'Some column name', 'Message', 'Description');
@@ -214,6 +259,9 @@ class ProcessingErrorAggregatorTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue($result);
     }
 
+    /**
+     * Test for method getAllErrors
+     */
     public function testGetAllErrors()
     {
         $this->model->addError('systemException', 'not-critical', 4, 'Some column name', 'Message', 'Description');
@@ -226,6 +274,9 @@ class ProcessingErrorAggregatorTest extends \PHPUnit_Framework_TestCase
         $this->assertInstanceOf('\Magento\ImportExport\Model\Import\ErrorProcessing\ProcessingError', $result[0]);
     }
 
+    /**
+     * Test for method getErrorsByCode. Expects receive errors with code, which present in incomming parameter.
+     */
     public function testGetErrorsByCodeInArray()
     {
         $this->model->addError('systemException', 'not-critical', 4, 'Some column name', 'Message', 'Description');
@@ -235,6 +286,9 @@ class ProcessingErrorAggregatorTest extends \PHPUnit_Framework_TestCase
         $this->assertCount(2, $result);
     }
 
+    /**
+     * Test for method getErrorsByCode. Unexpects receive errors with code, which present in incomming parameter.
+     */
     public function testGetErrorsByCodeNotInArray()
     {
         $this->model->addError('columnNotFound', 'not-critical', 4, 'Some column name', 'Message', 'Description');
@@ -244,6 +298,9 @@ class ProcessingErrorAggregatorTest extends \PHPUnit_Framework_TestCase
         $this->assertCount(0, $result);
     }
 
+    /**
+     * Test for method getRowsGroupedByErrorCode. Expects errors.
+     */
     public function testGetRowsGroupedByErrorCodeWithErrors()
     {
         $this->model->addError('systemException');
@@ -257,6 +314,9 @@ class ProcessingErrorAggregatorTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($expectedResult, $result);
     }
 
+    /**
+     * Test for method getRowsGroupedByErrorCode. Unexpects errors.
+     */
     public function testGetRowsGroupedByErrorCodeNoErrors()
     {
         $result = $this->model->getRowsGroupedByErrorCode();
@@ -264,11 +324,17 @@ class ProcessingErrorAggregatorTest extends \PHPUnit_Framework_TestCase
         $this->assertCount(0, $result);
     }
 
+    /**
+     * Test for method getAllowedErrorsCount.
+     */
     public function testGetAllowedErrorsCount()
     {
         $this->assertEquals($this->model->getAllowedErrorsCount(), 0);
     }
 
+    /**
+     * Test for method clear.
+     */
     public function testClear()
     {
         $this->model->addError('systemException', 'not-critical', 4, 'Some column name', 'Message', 'Description');
@@ -278,6 +344,9 @@ class ProcessingErrorAggregatorTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals([], $result);
     }
 
+    /**
+     * Test for method getErrorsCount
+     */
     public function testGetErrorsCount()
     {
         $this->model->addError('systemException', 'not-critical', 4, 'Some column name', 'Message', 'Description');
