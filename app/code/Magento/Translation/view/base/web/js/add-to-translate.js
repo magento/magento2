7fcb8d227c524d702419814489be3d76cc0d4bc4
Anton Guz <aguz@ebay.com>
2015-08-10 10:59:35 +0300

    MAGETWO-39017: [Outsourcing Support - S2] Gallery & inline scripts
    - Fix encoding, code style

--- app/code/Magento/Translation/view/base/web/js/add-to-translate.js
+++ app/code/Magento/Translation/view/base/web/js/add-to-translate.js
@@ -1,14 +1,15 @@
 /**
- * Copyright © 2015 Magento. All rights reserved.
+ * Copyright Â© 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
+
 define([
     'jquery',
     'mage/translate'
-], function ($, _mage) {
+], function ($) {
     'use strict';
 
-    return function (config, element) {
+    return function (config) {
         require([config.config], function (string) {
             if (string.length) {
                 $.mage.translate.add(JSON.parse(string));
