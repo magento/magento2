aefa66c327824ca559cb86cfb19945653a13352b
Eddie Lau <kahlau@ebay.com>
2015-08-10 17:29:06 -0500

    MAGETWO-41188: Remove data from database, setup app cron job
    - added support for dataOption

--- setup/src/Magento/Setup/Test/Unit/Controller/StartUpdaterTest.php
+++ setup/src/Magento/Setup/Test/Unit/Controller/StartUpdaterTest.php
@@ -113,7 +113,19 @@ class StartUpdaterTest extends \PHPUnit_Framework_TestCase
 
     public function testUpdateInvalidRequestMissingType()
     {
-        $content = '{"packages":[{"name":"vendor\/package", "version": "1.0.0"}],"type":"uninstall"}';
+        $content =
+            '{"packages":[{"name":"vendor\/package", "version": "1.0.0"}],"type":"uninstall","dataOption":"true"}';
+        $this->request->expects($this->any())->method('getContent')->willReturn($content);
+        $this->filesystem->expects($this->never())->method('getDirectoryWrite');
+        $this->controller->setEvent($this->mvcEvent);
+        $this->controller->dispatch($this->request, $this->response);
+        $this->controller->updateAction();
+    }
+
+    public function testUpdateInvalidRequestMissingDataOption()
+    {
+        $content =
+            '{"packages":[{"name":"vendor\/package", "type": "module"}],"type":"uninstall"}';
         $this->request->expects($this->any())->method('getContent')->willReturn($content);
         $this->filesystem->expects($this->never())->method('getDirectoryWrite');
         $this->controller->setEvent($this->mvcEvent);
