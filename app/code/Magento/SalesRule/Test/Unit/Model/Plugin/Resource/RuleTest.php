2ea24865f92b5edfe74666d5f23986bf23f6a8e6
Anton Kaplya <akaplya@ebay.com>
2015-08-11 15:00:50 +0300

    MAGETWO-41010: Optimize query in Magento\SalesRule\Model\Validator::init

--- app/code/Magento/SalesRule/Test/Unit/Model/Plugin/Resource/RuleTest.php
+++ app/code/Magento/SalesRule/Test/Unit/Model/Plugin/Resource/RuleTest.php
@@ -4,7 +4,7 @@
  * See COPYING.txt for license details.
  */
 
-namespace Magento\SalesRule\Test\Unit\Model\Plugin;
+namespace Magento\SalesRule\Test\Unit\Model\Plugin\Resource;
 
 class RuleTest extends \PHPUnit_Framework_TestCase
 {
