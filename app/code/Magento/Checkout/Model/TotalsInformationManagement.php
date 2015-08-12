6c6a2e32f8eb64db1c1a3c1357d3d93f6bd8dc61
Ievgen Shakhsuvarov <ishakhsuvarov@ebay.com>
2015-08-12 10:01:28 +0300

    MAGETWO-41309: [Improvement] Implement additional Totals service for cart

--- app/code/Magento/Checkout/Model/TotalsInformationManagement.php
+++ app/code/Magento/Checkout/Model/TotalsInformationManagement.php
@@ -5,21 +5,33 @@
  */
 namespace Magento\Checkout\Model;
 
+use Magento\Framework\Exception;
+
 class TotalsInformationManagement implements \Magento\Checkout\Api\TotalsInformationManagementInterface
 {
     /**
-     * Quote repository.
+     * Cart total repository.
      *
      * @var \Magento\Quote\Api\CartTotalRepositoryInterface
      */
     protected $cartTotalRepository;
 
     /**
+     * Quote repository.
+     *
+     * @var \Magento\Quote\Api\CartRepositoryInterface
+     */
+    protected $cartRepository;
+
+    /**
+     * @param \Magento\Quote\Api\CartRepositoryInterface $cartRepository
      * @param \Magento\Quote\Api\CartTotalRepositoryInterface $cartTotalRepository
      */
     public function __construct(
+        \Magento\Quote\Api\CartRepositoryInterface $cartRepository,
         \Magento\Quote\Api\CartTotalRepositoryInterface $cartTotalRepository
     ) {
+        $this->cartRepository = $cartRepository;
         $this->cartTotalRepository = $cartTotalRepository;
     }
 
@@ -30,6 +42,31 @@ class TotalsInformationManagement implements \Magento\Checkout\Api\TotalsInforma
         $cartId,
         \Magento\Checkout\Api\Data\TotalsInformationInterface $addressInformation
     ) {
+        /** @var \Magento\Quote\Model\Quote $quote */
+        $quote = $this->cartRepository->get($cartId);
+        $this->validateQuote($quote);
+
+        if ($quote->getIsVirtual()) {
+            $quote->setBillingAddress($addressInformation->getAddress());
+        } else {
+            $quote->setShippingAddress($addressInformation->getAddress());
+            $quote->getShippingAddress()->setCollectShippingRates(true)->setShippingMethod(
+                $addressInformation->getShippingCarrierCode() . '_' . $addressInformation->getShippingMethodCode()
+            );
+        }
+        $quote->collectTotals();
+
         return $this->cartTotalRepository->get($cartId);
+    }
+
+    /**
+     * @param \Magento\Quote\Model\Quote $quote
+     * @throws Exception\InputException
+     */
+    protected function validateQuote(\Magento\Quote\Model\Quote $quote)
+    {
+        if ($quote->getItemsCount() === 0) {
+            throw new Exception\InputException(__('Totals calculation is not applicable to empty cart'));
+        }
     }
 }
