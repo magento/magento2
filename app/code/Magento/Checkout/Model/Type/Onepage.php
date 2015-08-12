29eea7b72b294c4287b55e8b07fb691f8527791b
Stanislav Idolov <sidolov@ebay.com>
2015-08-12 10:24:54 +0300

    MAGETWO-40964: Bamboo builds adoption

--- app/code/Magento/Checkout/Model/Type/Onepage.php
+++ app/code/Magento/Checkout/Model/Type/Onepage.php
@@ -474,16 +474,10 @@ class Onepage
 
                     // don't reset original shipping data, if it was not changed by customer
                     foreach ($shipping->getData() as $shippingKey => $shippingValue) {
-                        if (!is_null(
-                            $shippingValue
-                        ) && !is_null(
-                            $billing->getData($shippingKey)
-                        ) && !isset(
-                            $data[$shippingKey]
-                        ) && !in_array(
-                            $shippingKey,
-                            $requiredBillingAttributes
-                        )
+                        if ($shippingValue !== null
+                            && $billing->getData($shippingKey) !== null
+                            && !isset($data[$shippingKey])
+                            && !in_array($shippingKey, $requiredBillingAttributes)
                         ) {
                             $billing->unsetData($shippingKey);
                         }
