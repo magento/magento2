b5417631bb6de38cf662dd2c7e1b5cb5cd22de7c
Vadim Zubovich <vadim_zubovich@epam.com>
2015-08-13 15:47:14 +0300

    MAGNIMEX-129: Generate error report file

--- app/code/Magento/ImportExport/Model/Report/ReportProcessorInterface.php
+++ app/code/Magento/ImportExport/Model/Report/ReportProcessorInterface.php
@@ -8,12 +8,20 @@ namespace Magento\ImportExport\Model\Report;
 
 use Magento\ImportExport\Model\Import\ErrorProcessing\ProcessingErrorAggregatorInterface;
 
+/**
+ * Error report generator interface
+ */
 interface ReportProcessorInterface
 {
     /**
      * @param string $originalFileName
      * @param ProcessingErrorAggregatorInterface $errorAggregator
+     * @param bool $writeOnlyErrorItems
      * @return string
      */
-    public function createReport($originalFileName, ProcessingErrorAggregatorInterface $errorAggregator);
+    public function createReport(
+        $originalFileName,
+        ProcessingErrorAggregatorInterface $errorAggregator,
+        $writeOnlyErrorItems = false
+    );
 }
