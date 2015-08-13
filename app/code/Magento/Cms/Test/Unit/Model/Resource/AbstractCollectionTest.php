9c3a769561940ed395cdd8ab19f1337c883ada12
Dmytro Poperechnyy <dpoperechnyy@ebay.com>
2015-08-13 11:04:04 +0300

    MAGETWO-41361: Invalid method error on cms blocks grid
    - AbstractCollectionTest updated to be abstract;

--- app/code/Magento/Cms/Test/Unit/Model/Resource/AbstractCollectionTest.php
+++ app/code/Magento/Cms/Test/Unit/Model/Resource/AbstractCollectionTest.php
@@ -5,7 +5,7 @@
  */
 namespace Magento\Cms\Test\Unit\Model\Resource;
 
-class AbstractCollectionTest extends \PHPUnit_Framework_TestCase
+abstract class AbstractCollectionTest extends \PHPUnit_Framework_TestCase
 {
     /**
      * @var \Magento\Framework\DB\Select|\PHPUnit_Framework_MockObject_MockObject
