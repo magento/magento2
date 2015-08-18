5969129a52071aaa73ffdab1e2162549c7f51ea5
Cari Spruiell <cspruiell@ebay.com>
2015-08-18 17:17:20 -0500

    MAGETWO-38568: Move PCI module code to new CE module
     - moved Pci module from EE to CE and renamed PasswordManagement

---
+++ app/code/Magento/PasswordManagement/Model/Observer.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\PasswordManagement\Model;
+
+use Magento\Framework\Event\Observer as EventObserver;
+
+/**
+ * PasswordManagement backend observer model
+ *
+ * Implements hashes upgrading
+ */
+class Observer
+{
+    /**
+     * PasswordManagement encryption model
+     *
+     * @var \Magento\Framework\Encryption\EncryptorInterface
+     */
+    protected $_encryptor;
+
+    /**
+     * @param \Magento\Framework\Encryption\EncryptorInterface $encryptor
+     */
+    public function __construct(\Magento\Framework\Encryption\EncryptorInterface $encryptor)
+    {
+        $this->_encryptor = $encryptor;
+    }
+
+    /**
+     * Upgrade customer password hash when customer has logged in
+     *
+     * @param EventObserver $observer
+     * @return void
+     */
+    public function upgradeCustomerPassword($observer)
+    {
+        $password = $observer->getEvent()->getPassword();
+        $model = $observer->getEvent()->getModel();
+        if (!$this->_encryptor->validateHashByVersion($password, $model->getPasswordHash())) {
+            $model->changePassword($password);
+        }
+    }
+}
