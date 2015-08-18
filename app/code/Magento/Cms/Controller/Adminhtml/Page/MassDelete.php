171c7f9c02d4288646185d486bc64d8b9816b096
Dmytro Poperechnyy <dpoperechnyy@ebay.com>
2015-08-18 19:37:55 +0300

    MAGETWO-41501: Wrong success mass delete message on cms pages and blocks

--- app/code/Magento/Cms/Controller/Adminhtml/Page/MassDelete.php
+++ app/code/Magento/Cms/Controller/Adminhtml/Page/MassDelete.php
@@ -46,12 +46,13 @@ class MassDelete extends \Magento\Backend\App\Action
     public function execute()
     {
         $collection = $this->filter->getCollection($this->collectionFactory->create());
-
-        foreach ($collection as $item) {
-            $item->delete();
+        $deletedPages = 0;
+        foreach ($collection as $page) {
+            $page->delete();
+            $deletedPages++;
         }
 
-        $this->messageManager->addSuccess(__('A total of %1 record(s) have been deleted.', $collection->getSize()));
+        $this->messageManager->addSuccess(__('A total of %1 record(s) have been deleted.', $deletedPages));
 
         /** @var \Magento\Backend\Model\View\Result\Redirect $resultRedirect */
         $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
