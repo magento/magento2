9d755d6ee24ed650f561ecdeff8c5d64b0b3b59d
Maddy Chellathurai <mchellathura@ebay.com>
2015-08-19 11:26:44 -0500

    MAGETWO-41769: Add Enable/disable to Component manager grid
    - adding enable/disable option

--- setup/pub/magento/setup/component-grid.js
+++ setup/pub/magento/setup/component-grid.js
@@ -70,6 +70,8 @@ angular.module('component-grid', ['ngStorage'])
 
                 if ($scope.isAvailableUpdatePackage(component.name)) {
                     return indicators.info[type];
+                } else if(component.enable == false) {
+                    return indicators.off[type];
                 }
                 return indicators.on[type];
             };
@@ -100,6 +102,21 @@ angular.module('component-grid', ['ngStorage'])
                 $localStorage.componentType = component.type;
                 $localStorage.moduleName = component.moduleName;
                 $state.go('root.readiness-check-uninstall');
+            };
+
+            $scope.enableDisable = function(type, component) {
+                $localStorage.packages = [
+                    {
+                        name: component.name
+                    }
+                ];
+                if ($localStorage.titles[type].indexOf(component.moduleName) < 0 ) {
+                    $localStorage.titles[type] = type.charAt(0).toUpperCase() + type.slice(1) + ' '
+                        + component.moduleName;
+                }
+                $localStorage.componentType = component.type;
+                $localStorage.moduleName = component.moduleName;
+                $state.go('root.readiness-check-'+type);
             };
 
             $scope.convertDate = function(date) {
