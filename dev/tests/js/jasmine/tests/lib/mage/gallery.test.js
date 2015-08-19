f48150cfb581061eae780311c1401a0cd166753a
AnastasiaBuniak <Anastasiia_Buniak@epam.com>
2015-08-19 12:00:39 +0300

    JS-21: Magnifier on Preview

--- dev/tests/js/jasmine/tests/lib/mage/gallery.test.js
+++ dev/tests/js/jasmine/tests/lib/mage/gallery.test.js
@@ -5,14 +5,16 @@
 define([
     'jquery',
     'text!tests/assets/gallery/config.json',
-    'mage/new-gallery'
-], function ($, config, gallery) {
+    'mage/gallery/gallery',
+    'magnifier/magnify'
+], function ($, config, gallery, magnifier) {
+
     'use strict';
 
     var body = $('body');
     var galleryAPI;
 
-    var conf = JSON.parse(config);
+    var conf =  JSON.parse(config);
 
     var dataToUpdate = [
         {
@@ -30,14 +32,38 @@ define([
         }
     ];
 
-    gallery(conf, body);
+    gallery(magnifier(conf, body), body);
 
-    beforeEach(function (done) {
+    beforeEach(function () {
         galleryAPI = $('.fotorama-item').data('gallery');
-        done();
     });
 
-    describe('mage/new-gallery', function () {
+    describe('gallery magnifier', function () {
+
+        it('magnifier is defined', function () {
+            expect($('.fotorama-item').magnify).toBeDefined();
+            expect(typeof $('.fotorama-item').magnify).toBe('function');
+        });
+
+        it('magnifier largeWrapper config', function () {
+            expect('#' + $('.magnifier-large').parent().attr('id')).toBe(conf.magnifierOpts.largeWrapper);
+        });
+
+        it('magnifier thumb config', function () {
+            expect($('.magnifier-large').attr('src')).toBe($('.fotorama__stage__frame .fotorama__img').attr('src'));
+        });
+
+        it('magnifier appearing', function () {
+            var ev = conf.magnifierOpts.eventType === 'click' ? 'click':'mouseover';
+            expect($($('.magnifier-preview').children()[0]).attr('class')).toBe('magnifier-large hidden');
+            $('.fotorama__stage__frame .fotorama__img').trigger(ev);
+            expect($($('.magnifier-preview').children()[0]).attr('class')).toBe('magnifier-large');
+            $('.fotorama__stage__frame .fotorama__img').trigger('mouseleave');
+        });
+    });
+
+    describe('gallery API', function () {
+
         it('gallery loaded', function () {
             expect($('.fotorama__nav__frame').length).toBe(conf.data.length);
         });
@@ -69,15 +95,50 @@ define([
         });
 
         it('update options', function () {
+            expect($('.fotorama__nav-wrap .fotorama__nav').hasClass('fotorama__nav--thumbs')).toBeTruthy();
             galleryAPI.updateOptions({
                 nav: "dots"
             });
-            expect($('.fotorama__nav__shaft .fotorama__nav__frame').hasClass('fotorama__nav__frame--dot')).toBeTruthy();
+            expect($('.fotorama__nav-wrap .fotorama__nav').hasClass('fotorama__nav--dots')).toBeTruthy();
         });
 
         it('update data', function () {
             galleryAPI.updateData(dataToUpdate);
-            expect($('.fotorama__nav__frame').length).toBe(dataToUpdate.length);
+            expect($('.fotorama__nav__frame--dot').length).toBe(dataToUpdate.length);
+        });
+
+        describe('gallery with breakpoints', function () {
+            it('configuration stays default if breakpoint is not fired', function () {
+                galleryAPI.first();
+                galleryAPI.prev();
+                expect($('.fotorama__nav__shaft .fotorama__nav__frame--dot:eq(' + (dataToUpdate.length - 1) + ')').hasClass('fotorama__active')).toBeTruthy();
+                galleryAPI.first();
+                galleryAPI.updateOptions({
+                    "breakpoints": {
+                        "(max-width: 0px)": {
+                            "loop": false
+                        }
+                    }
+                });
+                galleryAPI.prev();
+                expect($('.fotorama__nav__shaft .fotorama__nav__frame--dot:eq(' + (dataToUpdate.length - 1) + ')').hasClass('fotorama__active')).toBeTruthy();
+            });
+
+            it('configuration could be updated with breakpoints', function () {
+                galleryAPI.first();
+                galleryAPI.prev();
+                expect($('.fotorama__nav__shaft .fotorama__nav__frame--dot:eq(' + (dataToUpdate.length - 1) + ')').hasClass('fotorama__active')).toBeTruthy();
+                galleryAPI.first();
+                galleryAPI.updateOptions({
+                    "breakpoints": {
+                        "(min-width: 1px)": {
+                            "loop": false
+                        }
+                    }
+                });
+                galleryAPI.prev();
+                expect($('.fotorama__nav__shaft .fotorama__nav__frame--dot:eq(0)').hasClass('fotorama__active')).toBeTruthy();
+            });
         });
     });
 });
