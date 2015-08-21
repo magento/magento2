11fb4e99c7aae1a7be9d2c2dcae5f9c200e0f7c4
Arkadii Chyzhov <achyzhov@ebay.com>
2015-08-21 11:38:08 +0300

    MAGETWO-38569: Create command line utility in existing bin/magento tool to unlock account

--- app/code/Magento/PasswordManagement/Console/UnlockAdminAccountCommand.php
+++ app/code/Magento/PasswordManagement/Console/UnlockAdminAccountCommand.php
@@ -18,7 +18,7 @@ class UnlockAdminAccountCommand extends Command
 {
     const ARGUMENT_ADMIN_USERNAME = 'username';
     const ARGUMENT_ADMIN_USERNAME_DESCRIPTION = 'The admin username to unlock';
-    const COMMAND_ADMIN_ACCOUT_UNLOCK = 'admin:user:unlock';
+    const COMMAND_ADMIN_ACCOUNT_UNLOCK = 'admin:user:unlock';
     const COMMAND_DESCRIPTION = 'Unlock Admin Account';
     const USER_ID = 'user_id';
 
@@ -47,12 +47,12 @@ class UnlockAdminAccountCommand extends Command
     {
         $adminUserName = $input->getArgument(self::ARGUMENT_ADMIN_USERNAME);
         $userData = $this->adminUser->loadByUsername($adminUserName);
-        $outputMessage = sprintf('Couldn\'t find the user "%s"', $adminUserName);
+        $outputMessage = sprintf('Couldn\'t find the user account "%s"', $adminUserName);
         if ($userData) {
             if (isset($userData[self::USER_ID]) && $this->adminUser->unlock($userData[self::USER_ID])) {
-                $outputMessage = sprintf('The user "%s" has been unlocked', $adminUserName);
+                $outputMessage = sprintf('The user account "%s" has been unlocked', $adminUserName);
             } else {
-                $outputMessage = sprintf('Couldn\'t unlock the user "%s"', $adminUserName);
+                $outputMessage = sprintf('Couldn\'t unlock the user account "%s"', $adminUserName);
             }
         }
         $output->writeln('<info>' . $outputMessage . '</info>');
@@ -63,7 +63,7 @@ class UnlockAdminAccountCommand extends Command
      */
     protected function configure()
     {
-        $this->setName(self::COMMAND_ADMIN_ACCOUT_UNLOCK);
+        $this->setName(self::COMMAND_ADMIN_ACCOUNT_UNLOCK);
         $this->setDescription(self::COMMAND_DESCRIPTION);
         $this->addArgument(
             self::ARGUMENT_ADMIN_USERNAME,
@@ -72,9 +72,9 @@ class UnlockAdminAccountCommand extends Command
         );
         $this->setHelp(
             <<<HELP
-This command unlocks admin by its username.
+This command unlocks an admin account by its username.
 To unlock:
-      <comment>%command.full_name% user_name</comment>
+      <comment>%command.full_name% username</comment>
 HELP
         );
         parent::configure();
