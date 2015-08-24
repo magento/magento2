da46264af32ffa45a673613f5bca4ba97923535c
Joan He <joan@x.com>
2015-08-24 17:23:32 -0500

    MAGETWO-39386: Restrict var_generation directory permissions
     - create deploy module to contain the mode set command
     - added library functionality of recursively changing permissions

--- setup/src/Magento/Setup/Test/Unit/Module/ConfigOptionsListTest.php
+++ setup/src/Magento/Setup/Test/Unit/Module/ConfigOptionsListTest.php
@@ -84,7 +84,7 @@ class ConfigOptionsListTest extends \PHPUnit_Framework_TestCase
         $this->generator->expects($this->once())->method('createResourceConfig')->willReturn($configDataMock);
         $this->generator->expects($this->once())->method('createXFrameConfig')->willReturn($configDataMock);
         $configData = $this->object->createConfig([], $this->deploymentConfig);
-        $this->assertEquals(7, count($configData));
+        $this->assertEquals(8, count($configData));
     }
 
     public function testCreateOptionsWithOptionalNull()
@@ -98,6 +98,6 @@ class ConfigOptionsListTest extends \PHPUnit_Framework_TestCase
         $this->generator->expects($this->once())->method('createResourceConfig')->willReturn($configDataMock);
         $this->generator->expects($this->once())->method('createXFrameConfig')->willReturn($configDataMock);
         $configData = $this->object->createConfig([], $this->deploymentConfig);
-        $this->assertEquals(6, count($configData));
+        $this->assertEquals(7, count($configData));
     }
 }
