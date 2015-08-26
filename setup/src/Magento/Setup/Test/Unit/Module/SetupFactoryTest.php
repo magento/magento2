8e3106a8eafc7e903f06009824f0740e2ae06b05
Eddie Lau <kahlau@ebay.com>
2015-08-26 14:11:10 -0500

    MAGETWO-42030: Unable to Run the CLI Tool After the DI Compilation
    - added test for SetupFactory

--- setup/src/Magento/Setup/Test/Unit/Module/SetupFactoryTest.php
+++ setup/src/Magento/Setup/Test/Unit/Module/SetupFactoryTest.php
@@ -21,4 +21,16 @@ class SetupFactoryTest extends \PHPUnit_Framework_TestCase
         $factory = new SetupFactory($objectManagerProvider);
         $this->assertInstanceOf('Magento\Setup\Module\Setup', $factory->create());
     }
+
+    public function testCreateWithParam()
+    {
+        $objectManager = $this->getMockForAbstractClass('Magento\Framework\ObjectManagerInterface', [], '', false);
+        $objectManager->expects($this->never())
+            ->method('get');
+        $resource = $this->getMock('Magento\Framework\App\Resource', [], [], '', false);
+        $objectManagerProvider = $this->getMock('Magento\Setup\Model\ObjectManagerProvider', [], [], '', false);
+        $objectManagerProvider->expects($this->once())->method('get')->willReturn($objectManager);
+        $factory = new SetupFactory($objectManagerProvider);
+        $this->assertInstanceOf('Magento\Setup\Module\Setup', $factory->create($resource));
+    }
 }
