a4f211d1b3221d37aeee3684d0c9e606377b6947
Alexandr Skrashuk <Alexandr_Skrashuk@epam.com>
2015-08-27 16:08:41 +0300

    PV-1: providing edit functionality. rename properties. move js from tpl to script files

--- app/code/Magento/ProductVideo/view/adminhtml/web/js/new-video-dialog.js
+++ app/code/Magento/ProductVideo/view/adminhtml/web/js/new-video-dialog.js
@@ -29,7 +29,7 @@ define([
                     class: 'action-primary video-create-button',
                     click: function (e) {
                         var file = $('#new_video_screenshot').get(0).files[0];
-                        var inputFile = uploader.val('').clone(true)
+                        var inputFile = uploader.val('').clone(true);
                         $('#new_video_screenshot').fileupload().fileupload(
                             'send',
                             {
@@ -40,15 +40,22 @@ define([
                             function(result, textStatus, jqXHR)
                             {
                                 var data = JSON.parse(result);
-                                data['video_url'] = $('#new_video_url').val();
-                                data['video_name'] = $('#new_video_name').val();
-                                data['video_description'] = $('#new_video_description').val();
-                                data['disabled'] = $('#new_video_disabled').prop('checked');
+                                data['video_url'] = $('#video_url').val();
+                                data['video_name'] = $('#video_name').val();
+                                data['video_description'] = $('#video_description').val();
+                                data['disabled'] = $('#new_video_disabled').prop('checked') ? 1 : 0;
                                 data['role'] = $('#new_video_role').val();
-                                data['entity_type'] = 'video';
+                                data['media_type'] = 'external-video';
+                                if ($('#video_base_image').prop('checked') == true) {
+                                    $('[name="product[image]"]').prop('checked', true);
+                                } else if (
+                                    $('#video_base_image').prop('checked') == false
+                                    && $('#file_name').val() == $('[name="product[image]"]').val()
+                                ) {
+                                    $('[name="product[image]"]').val('')
+                                }
                                 $('#media_gallery_content').trigger('addItem', data);
                                 $('#new-video').modal('closeModal');
-
                                 uploader.replaceWith(inputFile);
                             }
                         );
@@ -58,15 +65,31 @@ define([
                     text: $.mage.__('Edit'),
                     class: 'action-primary video-edit',
                     click: function (e) {
-                        $('input[name*="' + $('#item_id').val() + '[video_url]"]').val($('#new_video_url').val());
-                        $('input[name*="' + $('#item_id').val() + '[video_name]"]').val($('#new_video_name').val());
-                        $('input[name*="' + $('#item_id').val() + '[video_description]"]').val($('#new_video_description').val());
-                        $('input[name*="' + $('#item_id').val() + '[disabled]"]').val($('#new_video_disabled').prop('checked'));
+                        var inputFile = uploader.val('').clone(true);
+                        $('input[name*="' + $('#item_id').val() + '[video_url]"]').val($('#video_url').val());
+                        $('input[name*="' + $('#item_id').val() + '[video_name]"]').val($('#video_name').val());
+                        $('input[name*="' + $('#item_id').val() + '[video_description]"]').val($('#video_description').val());
+                        var flagChecked = $('#new_video_disabled').prop('checked') ? 1 : 0;
+                        $('input[name*="' + $('#item_id').val() + '[disabled]"]').val(flagChecked);
+                        if (flagChecked == true) {
+                            $('[name*="' + $('#item_id').val() + '"]').siblings('.image-fade').css('visibility', 'visible');
+                        } else {
+                            $('[name*="' + $('#item_id').val() + '"]').siblings('.image-fade').css('visibility', 'hidden');
+                        }
+                        if ($('#video_base_image').prop('checked') == true) {
+                            $('[name="product[image]"]').prop('checked', true);
+                        } else if (
+                            $('#video_base_image').prop('checked') == false
+                            && $('#file_name').val() == $('[name="product[image]"]').val()
+                        ) {
+                            $('[name="product[image]"]').val('')
+                        }
+                        uploader.replaceWith(inputFile);
                         $('#new-video').modal('closeModal');
                     }
                 }],
                 opened: function() {
-                    if ($('#new_video_url').val() != '') {
+                    if ($('#video_url').val() != '') {
                         $('.video-create-button')[0].hide();
                         $('.video-edit')[0].show();
                     } else {
