d6198bfdf3574d30e65551150820d2289e213ad6
Dmytro Aponasenko <daponasenko@ebay.com>
2015-08-27 17:23:36 +0300

    MTA-2393: Update mass action toggle button selector

--- dev/tests/functional/tests/app/Magento/Ui/Test/Block/Adminhtml/DataGrid.php
+++ dev/tests/functional/tests/app/Magento/Ui/Test/Block/Adminhtml/DataGrid.php
@@ -211,6 +211,7 @@ class DataGrid extends Grid
      */
     public function massaction(array $items, $action, $acceptAlert = false, $massActionSelection = '')
     {
+        $this->waitLoader();
         if ($this->_rootElement->find($this->noRecords)->isVisible()) {
             return;
         }
