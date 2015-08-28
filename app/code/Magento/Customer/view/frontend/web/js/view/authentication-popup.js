6e235964881c8dd8c6221dcd09e05f90771f0c62
Dubovyk Oleksandr <odubovyk@ebay.com>
2015-08-28 19:06:57 +0300

    MAGETWO-41680: [UI] Accessibility Improvements
    - Added second bunch of accessability fixes

--- app/code/Magento/Customer/view/frontend/web/js/view/authentication-popup.js
+++ app/code/Magento/Customer/view/frontend/web/js/view/authentication-popup.js
@@ -39,7 +39,9 @@ define(
 
             /** Init popup login window */
             setModalElement: function (element) {
-                authenticationPopup.createPopUp(element);
+                if (authenticationPopup.modalWindow == null) {
+                    authenticationPopup.createPopUp(element);
+                }
             },
 
             /** Is login form enabled for current customer */
