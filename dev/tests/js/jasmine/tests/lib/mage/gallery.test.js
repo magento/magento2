e72ccc2a9204c70fd15bf9da92311dbce69b8ac1
AnastasiaBuniak <Anastasiia_Buniak@epam.com>
2015-08-31 00:48:26 +0300

    JS-19: Gallery Widget on Magento Product Pages

--- dev/tests/js/jasmine/tests/lib/mage/gallery.test.js
+++ dev/tests/js/jasmine/tests/lib/mage/gallery.test.js
@@ -11,134 +11,109 @@ define([
 
     'use strict';
 
-    var body = $('body');
-    var galleryAPI;
-
-    var conf =  JSON.parse(config);
-
-    var dataToUpdate = [
-        {
-            img: 'data:image/png;base64,' +
-            'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP4Xw8AAoABf5/NhYYAAAAASUVORK5CYII='
-        }, {
-            img: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAw' +
-            'CAAAAC0lEQVR42mP4Xw8AAoABf5/NhYYAAAAASUVORK5CYII='
-        }, {
-            img: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAw' +
-            'CAAAAC0lEQVR42mP4Xw8AAoABf5/NhYYAAAAASUVORK5CYII='
-        }, {
-            img: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAw' +
-            'CAAAAC0lEQVR42mP4Xw8AAoABf5/NhYYAAAAASUVORK5CYII='
-        }
-    ];
+    var body = $('body'),
+        galleryAPI,
+        conf = JSON.parse(config),
+        gallerySelector = '[data-gallery-role="gallery"]',
+        magnifierSelector = '[data-gallery-role="magnifier"]',
+        stageSelector = '[data-gallery-role="stage-shaft"]',
+        navSelector = '[data-gallery-role="nav-frame"]',
+        dotSelector = '[data-nav-type="dot"]',
+        dataToUpdate = [
+            {
+                img: 'data:image/png;base64,' +
+                'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP4Xw8AAoABf5/NhYYAAAAASUVORK5CYII='
+            }, {
+                img: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAw' +
+                'CAAAAC0lEQVR42mP4Xw8AAoABf5/NhYYAAAAASUVORK5CYII='
+            }, {
+                img: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAw' +
+                'CAAAAC0lEQVR42mP4Xw8AAoABf5/NhYYAAAAASUVORK5CYII='
+            }, {
+                img: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAw' +
+                'CAAAAC0lEQVR42mP4Xw8AAoABf5/NhYYAAAAASUVORK5CYII='
+            }
+        ];
 
     gallery(magnifier(conf, body), body);
 
     beforeEach(function () {
-        galleryAPI = $("[data-gallery-role='gallery']").data('gallery');
+        galleryAPI = $(gallerySelector).data('gallery');
     });
 
-    describe('gallery magnifier', function () {
+    describe('magnifier/magnify', function () {
 
-        it('magnifier is defined', function () {
-            expect($("[data-gallery-role='gallery']").magnify).toBeDefined();
-            expect(typeof $("[data-gallery-role='gallery']").magnify).toBe('function');
-        });
+        if ('ontouchstart' in document.documentElement) {
+            it('magnifier is not initialized on mobile platforms', function () {
+                expect($(magnifierSelector).length).toBe(0);
+            });
+        } else {
+            it('magnifier is defined', function () {
+                expect($(gallerySelector).magnify).toBeDefined();
+                expect(typeof $(gallerySelector).magnify).toBe('function');
+                expect($(magnifierSelector).hasClass('hidden')).toBeTruthy();
+            });
 
-        it('magnifier thumb config', function () {
-            console.log($("[data-gallery-role='magnifier']").attr('class'));
-            expect($("[data-gallery-role='magnifier'] img").attr('src')).toBe($("[data-gallery-role='stage-shaft'] img").attr('src'));
-        });
-
-        it('magnifier appearing', function () {
-            var ev = conf.magnifierOpts.eventType === 'click' ? 'click':'mouseover';
-            console.log($("[data-gallery-role='magnifier']").attr('class'));
-            expect($($("[data-gallery-role='magnifier']").children()[0]).hasClass('hidden')).toBeTruthy();
-            $("[data-gallery-role='stage-shaft'] img").trigger(ev);
-            expect($($("[data-gallery-role='magnifier']").children()[0]).hasClass('hidden')).toBeFalthy;
-            $("[data-gallery-role='stage-shaft'] img").trigger('mouseleave');
-        });
+            it('magnifier is initialized on desktop platforms', function () {
+                expect($(magnifierSelector + ' img').attr('src')).toBe($(stageSelector + ' img').attr('src'));
+            });
+            it('magnifier appearing on event on desktop platforms', function () {
+                var ev = conf.magnifierOpts.eventType === 'click' ? 'click' : 'mouseover';
+                expect($($(magnifierSelector).children()[0]).hasClass('magnifier-large hidden')).toBeTruthy();
+                $(stageSelector + ' img').trigger(ev);
+                expect($($(magnifierSelector).children()[0]).hasClass('magnifier-large hidden')).toBeFalsy();
+                $(stageSelector + ' img').trigger('mouseleave');
+            });
+        }
     });
 
-    describe('gallery API', function () {
+    describe('mage/gallery/gallery', function () {
 
         it('gallery loaded', function () {
-            expect($("[data-gallery-role='nav-frame']").length).toBe(conf.data.length);
+            expect($(navSelector).length).toBe(conf.data.length);
         });
 
         it('show last', function () {
             galleryAPI.last();
-            expect($('[data-gallery-role="nav-frame"]:eq('+ (conf.data.length-1) +')')
+            expect($(navSelector + ':eq(' + (conf.data.length - 1) + ')')
                 .attr('data-active') === 'true').toBeTruthy();
         });
 
         it('show first', function () {
             galleryAPI.first();
-            expect($('[data-gallery-role="nav-frame"]:eq(0)').attr('data-active') === 'true').toBeTruthy();
+            expect($(navSelector + ':eq(0)').attr('data-active') === 'true').toBeTruthy();
         });
 
         it('show next', function () {
             galleryAPI.next();
-            expect($('[data-gallery-role="nav-frame"]:eq(1)').attr('data-active') === 'true').toBeTruthy();
+            expect($(navSelector + ':eq(1)').attr('data-active') === 'true').toBeTruthy();
         });
 
         it('show previos', function () {
             galleryAPI.prev();
-            expect($('[data-gallery-role="nav-frame"]:eq(0)').attr('data-active') === 'true').toBeTruthy();
+            expect($(navSelector + ':eq(0)').attr('data-active') === 'true').toBeTruthy();
         });
 
         it('show by number', function () {
             galleryAPI.seek(3);
-            expect($('[data-gallery-role="nav-frame"]:eq(2)').attr('data-active') === 'true').toBeTruthy();
+            expect($(navSelector + ':eq(2)').attr('data-active') === 'true').toBeTruthy();
         });
 
         it('update options', function () {
-            expect($('[data-gallery-role="nav-frame"]').attr('data-nav-type') === 'thumb').toBeTruthy();
+            expect($(navSelector).attr('data-nav-type') === 'thumb').toBeTruthy();
             galleryAPI.updateOptions({
-                nav: "dots"
+                nav: 'dots'
             });
-            expect($("[data-nav-type='dot']").length).toBe(conf.data.length);            
+            expect($(dotSelector).length).toBe(conf.data.length);
         });
 
         it('update data', function () {
             galleryAPI.updateData(dataToUpdate);
-            expect($("[data-nav-type='dot']").length).toBe(dataToUpdate.length);
+            expect($(dotSelector).length).toBe(dataToUpdate.length);
         });
 
-        describe('gallery with breakpoints', function () {
-            it('configuration stays default if breakpoint is not fired', function () {
-                galleryAPI.last();
-                galleryAPI.next();
-                console.log($('[data-gallery-role="nav-frame"]:eq(0)').attr('data-active'))
-                expect($('[data-gallery-role="nav-frame"]:eq(0)').attr('data-active') === 'true').toBeTruthy();
-                galleryAPI.updateOptions({
-                    "breakpoints": {
-                        "(max-width: 0px)": {
-                            "loop": false
-                        }
-                    }
-                });
-                galleryAPI.last();
-                galleryAPI.next();
-                expect($('[data-gallery-role="nav-frame"]:eq(0)').attr('data-active') === 'true').toBeTruthy();
-            });
-
-            it('configuration could be updated with breakpoints', function () {
-                galleryAPI.last();
-                galleryAPI.next();
-                expect($('[data-gallery-role="nav-frame"]:eq(0)').attr('data-active') === 'true').toBeTruthy();
-                galleryAPI.updateOptions({
-                    "breakpoints": {
-                        "(min-width: 1px)": {
-                            "loop": false
-                        }
-                    }
-                });
-                galleryAPI.last();
-                galleryAPI.next();
-                expect($('[data-gallery-role="nav-frame"]:eq(0)').attr('data-active') === 'true').toBeTruthy();
-            });
+        it('breakpoints override configs', function () {
+            expect($('.fotorama__arr').css('display')).toBe('none');
         });
-    //});
+    });
 });
-
