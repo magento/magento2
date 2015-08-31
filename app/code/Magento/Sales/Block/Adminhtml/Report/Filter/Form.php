90097c4db12108522c9177e5125206ca36b687e6
Egor Shitikov <yshytikov@ebay.com>
2015-08-31 12:35:15 +0300

    MAGETWO-39802: Sales Tax Report incorrectly displays data for 'Any' order status

--- app/code/Magento/Sales/Block/Adminhtml/Report/Filter/Form.php
+++ app/code/Magento/Sales/Block/Adminhtml/Report/Filter/Form.php
@@ -67,7 +67,7 @@ class Form extends \Magento\Reports\Block\Adminhtml\Filter\Form
                     'name' => 'show_order_statuses',
                     'label' => __('Order Status'),
                     'options' => ['0' => __('Any'), '1' => __('Specified')],
-                    'note' => __('Applies to Any of the Specified Order Statuses except cancelled orders')
+                    'note' => __('Applies to Any of the Specified Order Statuses except canceled orders')
                 ],
                 'to'
             );
