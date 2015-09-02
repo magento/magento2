f4cf2e0f64d602641e5b0f3ee5112be6d201f021
ALIAKSANDR KOMAR <ALIAKSANDR_KOMAR@epam.com>
2015-09-02 15:57:51 +0300

    PV-103: upgrade data

---
+++ app/code/Magento/Catalog/Setup/UpgradeData.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Catalog\Setup;
+
+use Magento\Customer\Model\Customer;
+use Magento\Framework\Indexer\IndexerRegistry;
+use Magento\Framework\Setup\UpgradeDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+/**
+ * @codeCoverageIgnore
+ */
+class UpgradeData implements UpgradeDataInterface
+{
+    /**
+     * Category setup factory
+     *
+     * @var CategorySetupFactory
+     */
+    private $categorySetupFactory;
+
+    /**
+     * Init
+     *
+     * @param CategorySetupFactory $categorySetupFactory
+     */
+    public function __construct(CategorySetupFactory $categorySetupFactory)
+    {
+        $this->categorySetupFactory = $categorySetupFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        echo 'FFFFFFFFFFf';
+        $setup->startSetup();
+        if (version_compare($context->getVersion(), '2.0.1', '<=')) {
+            die('22222222222222');
+            /** @var \Magento\Catalog\Setup\CategorySetup $categorySetup */
+            $categorySetup = $this->categorySetupFactory->create(['setup' => $setup]);
+
+            $entityTypeId = $categorySetup->getEntityTypeId(\Magento\Catalog\Model\Product::ENTITY);
+            $attributeSetId = $categorySetup->getDefaultAttributeSetId($entityTypeId);
+            $attributeGroupId = $categorySetup->getAttributeGroupId($entityTypeId, $attributeSetId, 'Images');
+
+            // update General Group
+            $categorySetup->updateAttributeGroup($entityTypeId, $attributeSetId, $attributeGroupId, 'attribute_group_name', 'Images and Videos');
+        }
+        die('eeeeeeeeeeeeee');
+        $setup->endSetup();
+    }
+}
