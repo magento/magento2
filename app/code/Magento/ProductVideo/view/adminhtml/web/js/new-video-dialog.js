6cdf020689934eeac5f8d2d0e467cad33bbb2f9c
ALIAKSANDR KOMAR <ALIAKSANDR_KOMAR@epam.com>
2015-09-02 15:04:18 +0300

    PV-98: video gallery change

--- app/code/Magento/ProductVideo/view/adminhtml/web/js/new-video-dialog.js
+++ app/code/Magento/ProductVideo/view/adminhtml/web/js/new-video-dialog.js
@@ -498,5 +498,8 @@ define([
 
     });
     $('#group-fields-image-management > legend > span').text('Images and Videos');
+    $('.video-create-button').on('click', function(){
+       $('#media_gallery_content').find('.video-item').parent().addClass('video-item');
+    });
     return $.mage.newVideoDialog;
 });
