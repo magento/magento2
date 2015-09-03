10fea7200a85d0b4c304f0bf2860c8b51145d51b
Sergey Ivashchenko <sivashchenko@ebay.com>
2015-09-03 12:19:35 +0300

    MAGETWO-41971: SQL error while placing order in admin with splitted Magento

--- app/code/Magento/Tax/Model/Resource/Sales/Order/Tax.php
+++ app/code/Magento/Tax/Model/Resource/Sales/Order/Tax.php
@@ -13,6 +13,20 @@ namespace Magento\Tax\Model\Resource\Sales\Order;
 class Tax extends \Magento\Framework\Model\Resource\Db\AbstractDb
 {
     /**
+     * @param \Magento\Framework\Model\Resource\Db\Context $context
+     * @param string $connectionName
+     */
+    public function __construct(
+        \Magento\Framework\Model\Resource\Db\Context $context,
+        $connectionName = null
+    ) {
+        if (null === $connectionName) {
+            $connectionName = 'sales';
+        }
+        parent::__construct($context, $connectionName);
+    }
+
+    /**
      * Resource initialization
      *
      * @return void
