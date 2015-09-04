8b5f521a847cef0ee45dd0369e512c77e6bf49a5
Denys Rul <drul@ebay.com>
2015-09-04 14:17:07 +0300

    MAGETWO-42150: Add ability to declare filter components inside of the column definition
    - Migrate to new filters declaration on cms pages and cms blocks

--- app/code/Magento/Ui/view/base/web/js/grid/filters/filters.js
+++ app/code/Magento/Ui/view/base/web/js/grid/filters/filters.js
@@ -43,13 +43,73 @@ define([
             filters: {
                 placeholder: true
             },
+            templates: {
+                filters: {
+                    base: {
+                        parent: '${ $.$data.filters.name }',
+                        name: '${ $.$data.column.index }',
+                        provider: '${ $.$data.filters.name }',
+                        dataScope: 'filters.${ $.$data.column.index }',
+                        label: '${ $.$data.column.label }',
+                        imports: {
+                            visible: '${ $.$data.column.name }:visible'
+                        }
+                    },
+                    text: {
+                        component: 'Magento_Ui/js/form/element/abstract',
+                        template: 'ui/grid/filters/elements/input'
+                    },
+                    select: {
+                        component: 'Magento_Ui/js/form/element/ui-select',
+                        template: 'ui/grid/filters/elements/ui-select',
+                        options: '${ JSON.stringify($.$data.column.options) }'
+                    },
+                    dateRange: {
+                        component: 'Magento_Ui/js/grid/filters/group',
+                        childDefaults: {
+                            component: 'Magento_Ui/js/form/element/date',
+                            provider: '${ $.parent }.${ $.name }',
+                            dateFormat: 'MM/dd/YYYY',
+                            template: 'ui/grid/filters/elements/date'
+                        },
+                        children: {
+                            from: {
+                                label: 'from',
+                                dataScope: 'from'
+                            },
+                            to: {
+                                label: 'to',
+                                dataScope: 'to'
+                            }
+                        }
+                    },
+                    textRange: {
+                        component: 'Magento_Ui/js/grid/filters/group',
+                        childDefaults: {
+                            component: 'Magento_Ui/js/form/element/abstract',
+                            provider: '${ $.parent }.${ $.name }',
+                            template: 'ui/grid/filters/elements/input'
+                        },
+                        children: {
+                            from: {
+                                label: 'from',
+                                dataScope: 'from'
+                            },
+                            to: {
+                                label: 'to',
+                                dataScope: 'to'
+                            }
+                        }
+                    }
+                }
+            },
             chipsConfig: {
                 name: '${ $.name }_chips',
                 provider: '${ $.chipsConfig.name }',
                 component: 'Magento_Ui/js/grid/filters/chips'
             },
             listens: {
-                active: 'extractPreviews',
+                active: 'updatePreviews',
                 applied: 'cancel extractActive'
             },
             links: {
@@ -58,7 +118,11 @@ define([
             exports: {
                 applied: '${ $.provider }:params.filters'
             },
+            imports: {
+                'onColumnsUpdate': '${ $.columnsProvider }:elems'
+            },
             modules: {
+                columns: '${ $.columnsProvider }',
                 chips: '${ $.chipsConfig.provider }'
             }
         },
@@ -106,6 +170,17 @@ define([
         },
 
         /**
+         *
+         * @param {Object} filter
+         * @returns {Filters} Chainable.
+         */
+        initFilter: function (filter) {
+            layout([filter]);
+
+            return this;
+        },
+
+        /**
          * Called when another element was added to current component.
          *
          * @returns {Filters} Chainable.
@@ -115,6 +190,17 @@ define([
                 .extractActive();
 
             return this;
+        },
+
+        /**
+         *
+         * @param {String} index
+         * @returns {Filter|Undefined}
+         */
+        getFilter: function (index) {
+            return this.elems.findWhere({
+                index: index
+            });
         },
 
         /**
@@ -153,6 +239,72 @@ define([
          */
         cancel: function () {
             this.set('filters', utils.copy(this.applied));
+
+            return this;
+        },
+
+        /**
+         *
+         * @param {Column} column
+         * @returns {Object}
+         */
+        buildFilter: function (column) {
+            var filters = this.templates.filters,
+                filter  = column.filter;
+
+            if (_.isObject(filter) && filter.filterType) {
+                filter = utils.extend({}, filters[filter.filterType], filter);
+            } else if (_.isString(filter)) {
+                filter = filters[filter];
+            }
+
+            filter = utils.extend({}, filters.base, filter);
+            filter = utils.template(filter, {
+                filters: this,
+                column: column
+            }, true, true);
+
+            return filter;
+        },
+
+        /**
+         *
+         * @param {Column} column
+         * @returns {Filters} Chainable
+         */
+        createFilter: function (column) {
+            var index = column.index,
+                filter;
+
+            if (!column.filter || this.getFilter(index)) {
+                return this;
+            }
+
+            filter = this.buildFilter(column);
+
+            this.initFilter(filter);
+
+            return this;
+        },
+
+        /**
+         *
+         * @returns {Filters} Chainable
+         */
+        updateFilters: function () {
+            var columns = this.columns().elems(),
+                filters = [],
+                filter;
+
+            columns.forEach(function (column) {
+                filter = this.getFilter(column.index);
+
+                if (filter) {
+                    filters.push(filter);
+                }
+            }, this);
+
+            this.insertChild(filters);
 
             return this;
         },
@@ -213,12 +365,21 @@ define([
          * @param {Array} filters - Filters to be processed.
          * @returns {Filters} Chainable.
          */
-        extractPreviews: function (filters) {
+        updatePreviews: function (filters) {
             var previews = filters.map(extractPreview);
 
             this.previews(_.compact(previews));
 
             return this;
+        },
+
+        /**
+         *
+         */
+        onColumnsUpdate: function (columns) {
+            columns.forEach(this.createFilter, this);
+
+            this.updateFilters();
         }
     });
 });
