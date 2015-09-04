fc1e80206486bd688da5a9c376213e35d973eede
Evgeniy Kolesov <ikolesov@ebay.com>
2015-09-04 19:47:17 +0300

    MAGETWO-42051: New checkout condition page mark up is broken

--- app/code/Magento/Backend/Block/Store/Switcher.php
+++ app/code/Magento/Backend/Block/Store/Switcher.php
@@ -548,11 +548,11 @@ class Switcher extends \Magento\Backend\Block\Template
         $html = '';
         $url = $this->getHintUrl();
         if ($url) {
-            $html = '<div class="tooltip">' . '<span class="help"><a' . ' href="' . $this->escapeUrl(
+            $html = '<div class="admin__field-tooltip tooltip">' . '<a' . ' href="' . $this->escapeUrl(
                 $url
             ) . '"' . ' onclick="this.target=\'_blank\'"' . ' title="' . __(
                 'What is this?'
-            ) . '"' . ' class="link-store-scope"><span>' . __(
+            ) . '"' . ' class="admin__field-tooltip-action action-help"><span>' . __(
                 'What is this?'
             ) . '</span></a></span>' . ' </div>';
         }
