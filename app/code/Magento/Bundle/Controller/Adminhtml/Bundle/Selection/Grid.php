f22dd0ba917761de0e8b0947638be6baad0f63ed
Maksym Iakusha <miakusha@ebay.com>
2015-09-04 09:56:27 +0300

    MAGETWO-42122: ZAP alert: Cross Site Scripting found in backend / bundle grid

--- app/code/Magento/Bundle/Controller/Adminhtml/Bundle/Selection/Grid.php
+++ app/code/Magento/Bundle/Controller/Adminhtml/Bundle/Selection/Grid.php
@@ -6,40 +6,23 @@
  */
 namespace Magento\Bundle\Controller\Adminhtml\Bundle\Selection;
 
-use Magento\Framework\App\Action\Context;
-use Magento\Framework\Escaper;
-
 class Grid extends \Magento\Backend\App\Action
 {
-    /**
-     * @var Escaper
-     */
-    protected $escaper;
-
-    /**
-     * @param Context $context
-     * @param Escaper $escaper
-     */
-    public function __construct(Context $context, Escaper $escaper)
-    {
-        $this->escaper = $escaper;
-        parent::__construct($context);
-    }
-
     /**
      * @return mixed
      */
     public function execute()
     {
+        $index = $this->getRequest()->getParam('index');
+        if (!preg_match('/^[a-z0-9_.]*$/i', $index)) {
+            throw new \InvalidArgumentException('Invalid parameter "index"');
+        }
+
         return $this->getResponse()->setBody(
             $this->_view->getLayout()->createBlock(
                 'Magento\Bundle\Block\Adminhtml\Catalog\Product\Edit\Tab\Bundle\Option\Search\Grid',
                 'adminhtml.catalog.product.edit.tab.bundle.option.search.grid'
-            )->setIndex(
-                $this->escaper->escapeHtml(
-                    $this->getRequest()->getParam('index')
-                )
-            )->toHtml()
+            )->setIndex($index)->toHtml()
         );
     }
 }
