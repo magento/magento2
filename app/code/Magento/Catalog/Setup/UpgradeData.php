b6dbd78a3257163d3314712aa9cd714f904da5ff
Vadim Zubovich <vadim_zubovich@epam.com>
2015-09-04 16:32:56 +0300

    PV-43: Stabilization

--- app/code/Magento/Catalog/Setup/UpgradeData.php
+++ app/code/Magento/Catalog/Setup/UpgradeData.php
@@ -39,21 +39,19 @@ class UpgradeData implements UpgradeDataInterface
      */
     public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
     {
-        echo 'FFFFFFFFFFf';
         $setup->startSetup();
         if (version_compare($context->getVersion(), '2.0.1', '<=')) {
-            die('22222222222222');
             /** @var \Magento\Catalog\Setup\CategorySetup $categorySetup */
             $categorySetup = $this->categorySetupFactory->create(['setup' => $setup]);
 
             $entityTypeId = $categorySetup->getEntityTypeId(\Magento\Catalog\Model\Product::ENTITY);
             $attributeSetId = $categorySetup->getDefaultAttributeSetId($entityTypeId);
+
             $attributeGroupId = $categorySetup->getAttributeGroupId($entityTypeId, $attributeSetId, 'Images');
 
             // update General Group
             $categorySetup->updateAttributeGroup($entityTypeId, $attributeSetId, $attributeGroupId, 'attribute_group_name', 'Images and Videos');
         }
-        die('eeeeeeeeeeeeee');
         $setup->endSetup();
     }
 }
