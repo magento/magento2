436221a8e95ad7e9fc54ed48a98cfeda88799aaf
Alexander Paliarush <apaliarush@ebay.com>
2015-09-04 17:15:52 +0300

    MAGETWO-42240: Implement mysql publishers

---
+++ Model/Publisher.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\MysqlMq\Model;
+
+use Magento\Framework\Amqp\Config\Data as AmqpConfig;
+use Magento\Framework\Amqp\PublisherInterface;
+use Magento\MysqlMq\Model\Message;
+use Magento\MysqlMq\Model\MessageFactory;
+
+/**
+ * MySQL publisher implementation for message queue.
+ */
+class Publisher implements PublisherInterface
+{
+    /**
+     * @var AmqpConfig
+     */
+    private $amqpConfig;
+
+    /**
+     * @var MessageFactory
+     */
+    private $messageFactory;
+
+    /**
+     * Initialize dependencies.
+     *
+     * @param AmqpConfig $amqpConfig
+     */
+    public function __construct(AmqpConfig $amqpConfig, MessageFactory $messageFactory)
+    {
+        $this->amqpConfig = $amqpConfig;
+        $this->messageFactory = $messageFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function publish($topicName, $data)
+    {
+        $queueNames = $this->amqpConfig->getQueuesForTopic($topicName);
+        foreach ($queueNames as $queueName) {
+            /** @var Message $message */
+            $message = $this->messageFactory->create(
+                [
+                    Message::KEY_BODY => $data,
+                    Message::KEY_TOPIC_NAME => $topicName,
+                    Message::KEY_QUEUE_NAME => $queueName,
+                    Message::KEY_STATUS => Message::STATUS_NEW
+                    // TODO: Verify that updated_at is set automatically on save
+                ]
+            );
+            // TODO: Consider saving all messages with a single request to DB for optimization
+            $message->save();
+        }
+    }
+}
