2dd54e78f7c5beeca3445f6bb4457108bdaab1b3
Sergey Ivashchenko <sivashchenko@ebay.com>
2015-09-07 20:08:20 +0300

    MAGETWO-41971: SQL error while placing order in admin with splitted Magento into 3 DBs

--- app/code/Magento/Tax/Model/Resource/Sales/Order/Tax.php
+++ app/code/Magento/Tax/Model/Resource/Sales/Order/Tax.php
@@ -13,20 +13,6 @@ namespace Magento\Tax\Model\Resource\Sales\Order;
 class Tax extends \Magento\Framework\Model\Resource\Db\AbstractDb
 {
     /**
-     * @param \Magento\Framework\Model\Resource\Db\Context $context
-     * @param string $connectionName
-     */
-    public function __construct(
-        \Magento\Framework\Model\Resource\Db\Context $context,
-        $connectionName = null
-    ) {
-        if (null === $connectionName) {
-            $connectionName = 'sales';
-        }
-        parent::__construct($context, $connectionName);
-    }
-
-    /**
      * Resource initialization
      *
      * @return void
