3ded9544144f98897b9064c357ec0c84d77807db
Andrii Lugovyi <alugovyi@ebay.com>
2015-09-07 19:40:09 +0300

    MAGETWO-37174: Manual selection product an association
    - refactoring

--- app/code/Magento/ConfigurableProduct/view/adminhtml/web/js/variations/variations.js
+++ app/code/Magento/ConfigurableProduct/view/adminhtml/web/js/variations/variations.js
@@ -2,6 +2,7 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
+// jscs:disable jsDoc
 define([
     'uiComponent',
     'jquery',
@@ -26,11 +27,13 @@ define([
         },
         initialize: function () {
             this._super();
+
             if (this.variations.length) {
                 this.render(this.variations, this.productAttributes);
             }
             this.initProductAttributesMap();
         },
+
         /**
          * Select different product in configurations section
          * @param rowIndex
@@ -43,13 +46,16 @@ define([
             newProduct.productId = productToEdit.entity_id;
             newProduct.productUrl = this.buildProductUrl(newProduct.entity_id);
             newProduct.editable = false;
-            newProduct.images = {preview: newProduct.thumbnail_src};
+            newProduct.images = {
+                preview: newProduct.thumbnail_src
+            };
             this.productAttributesMap[this.getVariationKey(newProduct.options)] = newProduct.productId;
             this.productMatrix.splice(this.rowIndexToEdit, 0, newProduct);
             $('#associated-products-container').trigger('closeModal');
         },
         initObservable: function () {
             this._super().observe('actions opened attributes productMatrix');
+
             return this;
         },
         getProductValue: function (name) {
@@ -57,22 +63,26 @@ define([
         },
         getRowId: function (data, field) {
             var key = data.variationKey;
+
             return 'variations-matrix-' + key + '-' + field;
         },
-        getVariationRowName: function(variation, field) {
+        getVariationRowName: function (variation, field) {
+            var result;
+
             if (variation.productId) {
-                return 'configurations[' + variation.productId + '][' + field.split('/').join('][') + ']';
+                result = 'configurations[' + variation.productId + '][' + field.split('/').join('][') + ']';
             } else {
-                var key = variation.variationKey;
-                return 'variations-matrix[' + key + '][' + field.split('/').join('][') + ']';
+                result = 'variations-matrix[' + variation.variationKey + '][' + field.split('/').join('][') + ']';
             }
+
+            return result;
         },
         getAttributeRowName: function (attribute, field) {
             return 'product[configurable_attributes_data][' + attribute.id + '][' + field + ']';
         },
         getOptionRowName: function (attribute, option, field) {
-            return 'product[configurable_attributes_data][' + attribute.id + '][values][' + option.value + ']['
-                + field + ']';
+            return 'product[configurable_attributes_data][' + attribute.id + '][values][' +
+                option.value + '][' + field + ']';
         },
         render: function (variations, attributes) {
             this.changeButtonWizard();
@@ -97,6 +107,7 @@ define([
                 var attributes = _.reduce(variation.options, function (memo, option) {
                     var attribute = {};
                     attribute[option.attribute_code] = option.value;
+
                     return _.extend(memo, attribute);
                 }, {});
                 this.productMatrix.push(_.extend(variation, {
@@ -107,7 +118,7 @@ define([
                     variationKey: _.values(attributes).join('-'),
                     editable: variation.editable === undefined ? !variation.productId : variation.editable,
                     productUrl: this.buildProductUrl(variation.productId),
-                    status: variation.status === undefined ? 1 : parseInt(variation.status)
+                    status: variation.status === undefined ? 1 : parseInt(variation.status, 10)
                 }));
             }, this);
         },
@@ -117,8 +128,9 @@ define([
         removeProduct: function (rowIndex) {
             this.opened(false);
             this.productMatrix.splice(rowIndex, 1);
+
             if (this.productMatrix().length === 0) {
-                this.attributes.each(function(attribute) {
+                this.attributes.each(function (attribute) {
                     $('[data-attribute-code="' + attribute.code + '"] select').removeProp('disabled');
                 });
             }
@@ -131,23 +143,25 @@ define([
             $('#associated-products-container').trigger('openModal');
         },
         toggleProduct: function (rowIndex) {
-            var productChanged = {};
+            var product, row, productChanged = {};
+
             if (this.productMatrix()[rowIndex].editable) {
-                var row = $('[data-row-number=' + rowIndex + ']');
-                _.each('name,sku,qty,weight,price'.split(','), function (column) {
+                row = $('[data-row-number=' + rowIndex + ']');
+                _.each(['name','sku','qty','weight','price'], function (column) {
                     productChanged[column] = $(
                         'input[type=text]',
                         row.find($('[data-column="%s"]'.replace('%s', column)))
                     ).val();
                 });
             }
-            var product = this.productMatrix.splice(rowIndex, 1)[0];
+            product = this.productMatrix.splice(rowIndex, 1)[0];
             product = _.extend(product, productChanged);
             product.status = !product.status * 1;
             this.productMatrix.splice(rowIndex, 0, product);
         },
         toggleList: function (rowIndex) {
             var state = false;
+
             if (rowIndex !== this.opened()) {
                 state = rowIndex;
             }
@@ -169,9 +183,9 @@ define([
             return this.productAttributesMap[this.getVariationKey(options)] || null;
         },
         initProductAttributesMap: function () {
-            if (null === this.productAttributesMap) {
+            if (this.productAttributesMap === null) {
                 this.productAttributesMap = {};
-                _.each(this.variations, function(product) {
+                _.each(this.variations, function (product) {
                     this.productAttributesMap[this.getVariationKey(product.options)] = product.productId;
                 }.bind(this));
             }
@@ -180,38 +194,40 @@ define([
             return variation.images.preview && (!variation.editable || variation.images.file);
         },
         generateImageGallery: function (variation) {
-            var gallery = [];
-            var imageFields = ['position', 'file', 'disabled', 'label'];
+            var gallery = [],
+                imageFields = ['position', 'file', 'disabled', 'label'];
             _.each(variation.images.images, function (image) {
                 _.each(imageFields, function (field) {
                     gallery.push(
-                        '<input type="hidden" name="'
-                        + this.getVariationRowName(variation, 'media_gallery/images/' + image.file_id + '/' + field)
-                        + '" value="' + (image[field] || '') + '" />'
+                        '<input type="hidden" name="' +
+                        this.getVariationRowName(variation, 'media_gallery/images/' + image.file_id + '/' + field) +
+                        '" value="' + (image[field] || '') + '" />'
                     );
                 }, this);
                 _.each(image.galleryTypes, function (imageType) {
                     gallery.push(
-                        '<input type="hidden" name="' + this.getVariationRowName(variation, imageType)
-                        + '" value="' + image.file + '" />'
+                        '<input type="hidden" name="' + this.getVariationRowName(variation, imageType) +
+                        '" value="' + image.file + '" />'
                     );
                 }, this);
             }, this);
+
             return gallery.join('\n');
         },
         initImageUpload: function () {
             require([
-                "jquery",
-                "mage/template",
-                "jquery/file-uploader",
-                "mage/mage",
-                "mage/translate"
+                'jquery',
+                'mage/template',
+                'jquery/file-uploader',
+                'mage/mage',
+                'mage/translate'
             ], function (jQuery, mageTemplate) {
 
                 jQuery(function ($) {
                     var matrix = $('[data-role=product-variations-matrix]');
                     matrix.find('[data-action=upload-image]').find('[name=image]').each(function () {
                         var imageColumn = $(this).closest('[data-column=image]');
+
                         if (imageColumn.find('[data-role=image]').length) {
                             imageColumn.find('[data-toggle=dropdown]').dropdown().show();
                         }
@@ -220,15 +236,16 @@ define([
                             dropZone: $(this).closest('[data-role=row]'),
                             acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i,
                             done: function (event, data) {
-                                var tmpl;
+                                var tmpl, parentElement, uploaderControl, imageElement;
 
                                 if (!data.result) {
                                     return;
                                 }
+
                                 if (!data.result.error) {
-                                    var parentElement = $(event.target).closest('[data-column=image]'),
-                                        uploaderControl = parentElement.find('[data-action=upload-image]'),
-                                        imageElement = parentElement.find('[data-role=image]');
+                                    parentElement = $(event.target).closest('[data-column=image]');
+                                    uploaderControl = parentElement.find('[data-action=upload-image]');
+                                    imageElement = parentElement.find('[data-role=image]');
 
                                     if (imageElement.length) {
                                         imageElement.attr('src', data.result.url);
@@ -262,7 +279,7 @@ define([
                 });
             });
         },
-        disableConfigurableAttributes: function(attributes) {
+        disableConfigurableAttributes: function (attributes) {
             _.each(attributes, function (attribute) {
                 $('[data-attribute-code="' + attribute.code + '"] select').prop('disabled', true);
             });
