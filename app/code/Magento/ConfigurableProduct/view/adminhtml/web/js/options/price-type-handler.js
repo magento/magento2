079857f1836ef7164d231cb05061d1e61d471499
Michail Slabko <mslabko@ebay.com>
2015-09-08 11:20:03 +0300

    MAGETWO-41014: Discounts should be based on selected option of Configurable Product on frontend
    - MAGETWO-42608: Remove Advanced Pricing tab

--- app/code/Magento/ConfigurableProduct/view/adminhtml/web/js/options/price-type-handler.js
+++ app/code/Magento/ConfigurableProduct/view/adminhtml/web/js/options/price-type-handler.js
@@ -1,39 +1,60 @@
 /**
- * Copyright © 2015 Magento. All rights reserved.
+ * Copyright Â© 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
 
 define([
-    'jquery'
+    'jquery',
+    'notification',
+    'mage/translate'
 ], function ($) {
     'use strict';
 
-    var isConfigurable;
-
-    var checkPercentPriceTypeDomExist = function () {
-        if ($('#product_options_container_top').length !== 0) {
-            $('.opt-type > select').trigger('change');
+    var priceTypeHandler = {
+        isConfigurable: false,
+        messageInited: false,
+        messageSelector: '[data-role=product-custom-options-content]',
+        checkPercentPriceTypeDomExist: function () {
+            if ($('#product_options_container_top').length !== 0) {
+                $('.opt-type > select').trigger('change');
+            }
+        },
+        showWarning: function () {
+            if (!this.messageInited) {
+                $(this.messageSelector).notification();
+                this.messageInited = true;
+            }
+            this.hideWarning();
+            $(this.messageSelector).notification('add', {
+                message: $.mage.__('We can\'t save custom-defined options with price type "percent" for ' +
+                    'configurable product.'),
+                error: true,
+                messageContainer: this.messageSelector
+            });
+        },
+        hideWarning: function () {
+            $(this.messageSelector).notification('clear');
         }
     };
 
-    $('#super_config')
-        .on('setIsConfigurable', function () {
-            isConfigurable = true;
-            checkPercentPriceTypeDomExist();
-        }).on('setIsNotConfigurable', function () {
-            isConfigurable = false;
-            checkPercentPriceTypeDomExist();
+    $('[data-form=edit-product]')
+        .on('change_configurable_type', function (event, isConfigurable) {
+            priceTypeHandler.isConfigurable = isConfigurable;
+            priceTypeHandler.checkPercentPriceTypeDomExist();
         });
 
     $('#product-edit-form-tabs').on('change', '.opt-type > select', function () {
         var priceType = $('[data-attr="price-type"]'),
             optionPercentPriceType = priceType.find('option[value="percent"]');
 
-        if (isConfigurable) {
+        if (priceTypeHandler.isConfigurable) {
+            priceTypeHandler.showWarning();
             optionPercentPriceType.hide();
             optionPercentPriceType.parent().val() === 'percent' ? optionPercentPriceType.parent().val('fixed') : '';
-        } else if (!isConfigurable) {
+        } else {
             optionPercentPriceType.show();
+            priceTypeHandler.hideWarning();
         }
     });
+    return priceTypeHandler;
 });
