26688fa2e77091f2d638bd4c30cc012267c2a256
Ankur Kaneria <akaneria@ebay.com>
2015-09-08 11:44:21 -0500

    MAGETWO-41783: Enable/Disable page
    - refactoring to fix cyclomatic complexity issue

--- setup/src/Magento/Setup/Test/Unit/Controller/StartUpdaterTest.php
+++ setup/src/Magento/Setup/Test/Unit/Controller/StartUpdaterTest.php
@@ -17,6 +17,11 @@ class StartUpdaterTest extends \PHPUnit_Framework_TestCase
     private $updater;
 
     /**
+     * @var \Magento\Framework\Module\FullModuleList|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $fullModuleList;
+
+    /**
      * @var \Magento\Framework\Filesystem|\PHPUnit_Framework_MockObject_MockObject
      */
     private $filesystem;
@@ -49,9 +54,15 @@ class StartUpdaterTest extends \PHPUnit_Framework_TestCase
     public function setUp()
     {
         $this->updater = $this->getMock('Magento\Setup\Model\Updater', [], [], '', false);
+        $this->fullModuleList = $this->getMock('\Magento\Framework\Module\FullModuleList', [], [], '', false);
         $this->filesystem = $this->getMock('Magento\Framework\Filesystem', [], [], '', false);
         $this->navigation = $this->getMock('Magento\Setup\Model\Navigation', [], [], '', false);
-        $this->controller = new StartUpdater($this->filesystem, $this->navigation, $this->updater);
+        $this->controller = new StartUpdater(
+            $this->filesystem,
+            $this->navigation,
+            $this->updater,
+            $this->fullModuleList
+        );
         $this->navigation->expects($this->any())
             ->method('getMenuItems')
             ->willReturn([
@@ -172,8 +183,8 @@ class StartUpdaterTest extends \PHPUnit_Framework_TestCase
             . '"headerTitle": "Enable Package 1" }';
         $this->request->expects($this->any())->method('getContent')->willReturn($content);
         $write = $this->getMockForAbstractClass('Magento\Framework\Filesystem\Directory\WriteInterface', [], '', false);
-        $this->filesystem->expects($this->once())->method('getDirectoryWrite')->willReturn($write);
-        $write->expects($this->once())
+        $this->filesystem->expects($this->any())->method('getDirectoryWrite')->willReturn($write);
+        $write->expects($this->any())
             ->method('writeFile')
             ->with('.type.json', '{"type":"enable","headerTitle":"Enable Package 1","titles":["C"]}');
         $this->controller->setEvent($this->mvcEvent);
@@ -187,8 +198,8 @@ class StartUpdaterTest extends \PHPUnit_Framework_TestCase
             . '"headerTitle": "Disable Package 1" }';
         $this->request->expects($this->any())->method('getContent')->willReturn($content);
         $write = $this->getMockForAbstractClass('Magento\Framework\Filesystem\Directory\WriteInterface', [], '', false);
-        $this->filesystem->expects($this->once())->method('getDirectoryWrite')->willReturn($write);
-        $write->expects($this->once())
+        $this->filesystem->expects($this->any())->method('getDirectoryWrite')->willReturn($write);
+        $write->expects($this->any())
             ->method('writeFile')
             ->with('.type.json', '{"type":"disable","headerTitle":"Disable Package 1","titles":["D"]}');
         $this->controller->setEvent($this->mvcEvent);
