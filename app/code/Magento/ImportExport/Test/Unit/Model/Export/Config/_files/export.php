67cee7cde8ea689dc7eb12f460782d7983c3328f
Leonid Poluyanov <lpoluyanov@ebay.com>
2015-09-10 19:44:45 +0300

    MAGETWO-39800: Import Export functionality is available when related Modules are disabled in Admin

--- app/code/Magento/ImportExport/Test/Unit/Model/Export/Config/_files/export.php
+++ app/code/Magento/ImportExport/Test/Unit/Model/Export/Config/_files/export.php
@@ -8,25 +8,25 @@ return [
         'product' => [
             'name' => 'product',
             'label' => 'Label_One',
-            'model' => 'Model_One',
+            'model' => 'Model\One',
             'types' => [
-                'product_type_one' => ['name' => 'product_type_one', 'model' => 'Product_Model_Type_One'],
-                'type_two' => ['name' => 'type_two', 'model' => 'Model_Type_Two'],
+                'product_type_one' => ['name' => 'product_type_one', 'model' => 'Product\Model\Type\One'],
+                'type_two' => ['name' => 'type_two', 'model' => 'Model\Type\Two'],
             ],
             'entityAttributeFilterType' => 'product',
         ],
         'customer' => [
             'name' => 'customer',
             'label' => 'Label_One',
-            'model' => 'Model_One',
+            'model' => 'Model\One',
             'types' => [
-                'type_one' => ['name' => 'type_one', 'model' => 'Model_Type_One'],
-                'type_two' => ['name' => 'type_two', 'model' => 'Model_Type_Two'],
+                'type_one' => ['name' => 'type_one', 'model' => 'Model\Type\One'],
+                'type_two' => ['name' => 'type_two', 'model' => 'Model\Type\Two'],
             ],
             'entityAttributeFilterType' => 'customer',
         ],
     ],
     'fileFormats' => [
-        'name_three' => ['name' => 'name_three', 'model' => 'Model_Three', 'label' => 'Label_Three'],
+        'name_three' => ['name' => 'name_three', 'model' => 'Model\Three', 'label' => 'Label_Three'],
     ]
 ];
