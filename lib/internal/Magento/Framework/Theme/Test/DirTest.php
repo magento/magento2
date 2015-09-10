551abc4b487771d5a791c3cdf4f5b4af7fcfd262
Safwan Khan <safwkhan@ebay.com>
2015-09-10 10:50:36 -0500

    MAGETWO-42267: Apply any other changes from prototype
    - Implemented few unit tests.

---
+++ lib/internal/Magento/Framework/Theme/Test/DirTest.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Framework\Theme\Test;
+
+use Magento\Framework\Theme\Dir;
+
+/**
+ * Tests Dir
+ */
+class DirTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Component registry
+     *
+     * @var \Magento\Framework\Component\ComponentRegistrarInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $componentRegistrarMock;
+
+    public function testGetAreaConfiguration()
+    {
+        $this->componentRegistrarMock = $this->getMockBuilder('Magento\Framework\Component\ComponentRegistrarInterface')
+            ->disableOriginalConstructor()->getMock();
+        $this->componentRegistrarMock->expects($this->once())
+            ->method('getPaths')
+            ->will($this->returnValue(['adminhtml/Magento/sampleTheme' => 'path/to/the/sample/theme']));
+        $expected = ['area' => 'adminhtml', 'theme_path_pieces' => ['Magento', 'sampleTheme']];
+        $dir = new Dir($this->componentRegistrarMock);
+        $this->assertEquals($expected, $dir->getAreaConfiguration("sample/theme"));
+
+    }
+}
