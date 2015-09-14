87e83f1b4dedcc0b10bb5b9ebfaed33ae142b10d
Ankur Kaneria <akaneria@ebay.com>
2015-09-14 15:19:03 -0500

    MAGETWO-42263: Register non-composer components
    - updates to fix failing api-functional tests

--- dev/tests/api-functional/framework/bootstrap.php
+++ dev/tests/api-functional/framework/bootstrap.php
@@ -19,8 +19,27 @@ $logWriter = new \Zend_Log_Writer_Stream('php://output');
 $logWriter->setFormatter(new \Zend_Log_Formatter_Simple('%message%' . PHP_EOL));
 $logger = new \Zend_Log($logWriter);
 
+/** Copy test modules to app/code/Magento to make them visible for Magento instance */
+$pathToCommittedTestModules = __DIR__ . '/../_files/Magento';
+$pathToInstalledMagentoInstanceModules = __DIR__ . '/../../../../app/code/Magento';
+$iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($pathToCommittedTestModules));
+/** @var SplFileInfo $file */
+foreach ($iterator as $file) {
+    if (!$file->isDir()) {
+        $source = $file->getPathname();
+        $relativePath = substr($source, strlen($pathToCommittedTestModules));
+        $destination = $pathToInstalledMagentoInstanceModules . $relativePath;
+        $targetDir = dirname($destination);
+        if (!is_dir($targetDir)) {
+            mkdir($targetDir, 0755, true);
+        }
+        copy($source, $destination);
+    }
+}
+unset($iterator, $file);
+
 // Register the modules under '_files/'
-$pathPattern = dirname(__DIR__) . '/_files/*/*/registration.php';
+$pathPattern = $pathToInstalledMagentoInstanceModules . '/TestModule*/registration.php';
 $files = glob($pathPattern, GLOB_NOSORT);
 if ($files === false) {
     throw new \RuntimeException('glob() returned error while searching in \'' . $pathPattern . '\'');
