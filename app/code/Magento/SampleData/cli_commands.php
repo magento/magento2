d23e3e6de0da4c6f87af2384878fefc02d77dac0
Alex Bomko <abomko@ebay.com>
2015-09-15 02:24:46 +0300

    MAGETWO-38608: Update Sample Data
    - MAGETWO-41733: Implement Sample Data deployment mechanism

---
+++ app/code/Magento/SampleData/cli_commands.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+\Magento\Framework\Console\CommandLocator::register('Magento\SampleData\Console\CommandList');
