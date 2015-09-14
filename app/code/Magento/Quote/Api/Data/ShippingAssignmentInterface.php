74486fc790b2fe30c48ab00941efbe287abcad59
Iryna Lagno <ilagno@ebay.com>
2015-09-14 15:24:01 +0300

    MAGETWO-42308: Stabilization activities

--- app/code/Magento/Quote/Api/Data/ShippingAssignmentInterface.php
+++ app/code/Magento/Quote/Api/Data/ShippingAssignmentInterface.php
@@ -32,4 +32,21 @@ interface ShippingAssignmentInterface extends \Magento\Framework\Api\ExtensibleD
      * @return mixed
      */
     public function setItems($value);
+
+    /**
+     * Retrieve existing extension attributes object or create a new one.
+     *
+     * @return \Magento\Quote\Api\Data\ShippingAssignmentExtensionInterface|null
+     */
+    public function getExtensionAttributes();
+
+    /**
+     * Set an extension attributes object.
+     *
+     * @param \Magento\Quote\Api\Data\ShippingAssignmentExtensionInterface $extensionAttributes
+     * @return $this
+     */
+    public function setExtensionAttributes(
+        \Magento\Quote\Api\Data\ShippingAssignmentExtensionInterface $extensionAttributes
+    );
 }
