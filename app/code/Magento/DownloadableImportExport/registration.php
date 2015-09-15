ca69d1bcc98e739e9804879b5a5f5521c0f06f01
Eddie Lau <kahlau@ebay.com>
2015-09-15 17:08:56 -0500

    MAGETWO-42265: Fix code that depends on module location
    - added registration file

--- app/code/Magento/DownloadableImportExport/registration.php
+++ app/code/Magento/DownloadableImportExport/registration.php
@@ -6,6 +6,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Magento_DesignEditor',
+    'Magento_DownloadableImportExport',
     __DIR__
 );
