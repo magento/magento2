eacf42b9d446aeace4d16d88bd20dc62b6babdfe
Ievgen Shakhsuvarov <ishakhsuvarov@ebay.com>
2015-09-15 12:43:49 +0300

    MAGETWO-42810: Eliminate multiple calls to collectTotals

--- app/code/Magento/Quote/Model/Quote.php
+++ app/code/Magento/Quote/Model/Quote.php
@@ -345,6 +345,11 @@ class Quote extends AbstractExtensibleModel implements \Magento\Quote\Api\Data\C
     protected $shippingAssignmentFactory;
 
     /**
+     * @var \Magento\Quote\Model\Quote\Address\TotalFactory
+     */
+    protected $totalFactory;
+
+    /**
      * @param \Magento\Framework\Model\Context $context
      * @param \Magento\Framework\Registry $registry
      * @param \Magento\Framework\Api\ExtensionAttributesFactory $extensionFactory
@@ -382,6 +387,7 @@ class Quote extends AbstractExtensibleModel implements \Magento\Quote\Api\Data\C
      * @param Quote\TotalsReader $totalsReader
      * @param ShippingFactory $shippingFactory
      * @param ShippingAssignmentFactory $shippingAssignmentFactory
+     * @param \Magento\Quote\Model\Quote\Address\TotalFactory $totalFactory
      * @param \Magento\Framework\Model\Resource\AbstractResource|null $resource
      * @param \Magento\Framework\Data\Collection\AbstractDb|null $resourceCollection
      * @param array $data
@@ -425,6 +431,7 @@ class Quote extends AbstractExtensibleModel implements \Magento\Quote\Api\Data\C
         Quote\TotalsReader $totalsReader,
         \Magento\Quote\Model\ShippingFactory $shippingFactory,
         \Magento\Quote\Model\ShippingAssignmentFactory $shippingAssignmentFactory,
+        \Magento\Quote\Model\Quote\Address\TotalFactory $totalFactory,
         \Magento\Framework\Model\Resource\AbstractResource $resource = null,
         \Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,
         array $data = []
@@ -462,6 +469,7 @@ class Quote extends AbstractExtensibleModel implements \Magento\Quote\Api\Data\C
         $this->totalsReader = $totalsReader;
         $this->shippingFactory = $shippingFactory;
         $this->shippingAssignmentFactory = $shippingAssignmentFactory;
+        $this->totalFactory = $totalFactory;
         parent::__construct(
             $context,
             $registry,
@@ -1941,8 +1949,10 @@ class Quote extends AbstractExtensibleModel implements \Magento\Quote\Api\Data\C
      */
     public function getTotals()
     {
+        $total = $this->totalFactory->create('Magento\Quote\Model\Quote\Address\Total');
+        $total->setData($this->getData());
         return $this->totalsReader->fetch(
-            $this->totalsCollector->collectQuoteTotals($this),
+            $total,
             $this->getStoreId()
         );
     }
