980ea46b817f3e0be92febaf02927c1e0de619eb
Cari Spruiell <cspruiell@ebay.com>
2015-09-15 08:38:23 -0500

    MAGETWO-42241: Implement \Magento\MysqlMq\Model\Resource\Message
     - remove commented code that is no longer needed

--- Test/Unit/Config/XsdTest.php
+++ Test/Unit/Config/XsdTest.php
@@ -242,7 +242,6 @@ class XsdTest extends \PHPUnit_Framework_TestCase
                 </config>',
                 [
                     "Element 'bind': The attribute 'queue' is required but missing.",
-//                    "Element 'bind': No match found for key-sequence ['customer.created'] of keyref 'topic-ref'."
                 ],
             ],
             'bind without exchange' => [
@@ -251,7 +250,6 @@ class XsdTest extends \PHPUnit_Framework_TestCase
                 </config>',
                 [
                     "Element 'bind': The attribute 'exchange' is required but missing.",
-//                    "Element 'bind': No match found for key-sequence ['customer.created'] of keyref 'topic-ref'."
                 ],
             ],
             'bind without topic' => [
