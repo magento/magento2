20cdae77cbababb5d47473411e3a671e8b8c69ef
Maksym Savich <msavich@ebay.com>
2015-09-16 17:48:46 -0500

    MAGETWO-42196: Magento components loaded from vendor directory - XSD filepath resolution
    - Test fix

--- lib/internal/Magento/Framework/Config/Dom.php
+++ lib/internal/Magento/Framework/Config/Dom.php
@@ -12,7 +12,6 @@
 namespace Magento\Framework\Config;
 
 use Magento\Framework\Config\Dom\UrnResolver;
-use Magento\Framework\Exception\LocalizedException;
 
 /**
  * Class Dom
@@ -288,9 +287,7 @@ class Dom
             }
         } catch (\Exception $exception) {
             libxml_use_internal_errors(false);
-            throw new LocalizedException(
-                new \Magento\Framework\Phrase($exception->getMessage())
-            );
+            throw $exception;
         }
         libxml_use_internal_errors(false);
         return $errors;
@@ -313,10 +310,10 @@ class Dom
             $result = str_replace($placeholder, $value, $result);
         }
         if (strpos($result, '%') !== false) {
-            if (preg_match_all('/%.+%/', $format, $matches)) {
+            if (preg_match_all('/%.+%/', $result, $matches)) {
                 $unsupported = [];
                 foreach ($matches[0] as $placeholder) {
-                    if (strpos($result, $placeholder[0])) {
+                    if (strpos($result, $placeholder[0]) !== false) {
                         $unsupported[] = $placeholder[0];
                     }
                 }
