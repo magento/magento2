242f20c377093b07e4845ce1fc74954eb57f5fc9
Oleksandr Rykh <orykh@ebay.com>
2015-09-16 12:19:22 +0300

    MTA-2312: Implement constraints as placeholders

--- dev/tests/functional/tests/app/Magento/Catalog/Test/Constraint/AssertAttributeSearchableByLabel.php
+++ dev/tests/functional/tests/app/Magento/Catalog/Test/Constraint/AssertAttributeSearchableByLabel.php
@@ -6,28 +6,50 @@
 
 namespace Magento\Catalog\Test\Constraint;
 
+use Magento\Cms\Test\Page\CmsIndex;
+use Magento\Mtf\Fixture\InjectableFixture;
 use Magento\Mtf\Constraint\AbstractConstraint;
+use Magento\CatalogSearch\Test\Page\CatalogsearchResult;
+use Magento\Catalog\Test\Fixture\CatalogProductAttribute;
 
 /**
- * Class AssertAttributeSearchableByLabel
+ * Assert that product attribute is searchable on Frontend.
  */
 class AssertAttributeSearchableByLabel extends AbstractConstraint
 {
     /**
      * Assert that product attribute is searchable on Frontend.
      *
+     * @param CatalogProductAttribute $attribute
+     * @param CmsIndex $cmsIndex
+     * @param InjectableFixture $product
+     * @param CatalogsearchResult $catalogSearchResult
      * @return void
      */
-    public function processAssert()
-    {
-        //
+    public function processAssert(
+        CatalogProductAttribute $attribute,
+        CmsIndex $cmsIndex,
+        InjectableFixture $product,
+        CatalogsearchResult $catalogSearchResult
+    ) {
+        $cmsIndex->open();
+        $cmsIndex->getSearchBlock()->search($attribute->getDefaultValueTextarea());
+
+        $isVisible = $catalogSearchResult->getListProductBlock()->getProductItem($product)->isVisible();
+        while (!$isVisible && $catalogSearchResult->getBottomToolbar()->nextPage()) {
+            $isVisible = $catalogSearchResult->getListProductBlock()->getProductItem($product)->isVisible();
+        }
+
+        \PHPUnit_Framework_Assert::assertTrue($isVisible, 'Product attribute is not searchable on Frontend.');
     }
 
     /**
+     * Returns string representation of object.
+     *
      * @return string
      */
     public function toString()
     {
-        //
+        return 'Product attribute is searchable on Frontend';
     }
 }
