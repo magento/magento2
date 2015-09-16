5b562067ebb97e09a8ee771c377cc2c453eb86b2
Oleksandr Rykh <orykh@ebay.com>
2015-09-16 12:19:22 +0300

    MTA-2312: Implement constraints as placeholders

--- dev/tests/functional/tests/app/Magento/Catalog/Test/Constraint/AssertProductAttributeIsUsedPromoRules.php
+++ dev/tests/functional/tests/app/Magento/Catalog/Test/Constraint/AssertProductAttributeIsUsedPromoRules.php
@@ -7,27 +7,45 @@
 namespace Magento\Catalog\Test\Constraint;
 
 use Magento\Mtf\Constraint\AbstractConstraint;
+use Magento\Catalog\Test\Fixture\CatalogProductAttribute;
+use Magento\CatalogRule\Test\Page\Adminhtml\CatalogRuleNew;
+use Magento\CatalogRule\Test\Page\Adminhtml\CatalogRuleIndex;
 
 /**
- * Class AssertProductAttributeIsUsedPromoRules
+ * Create a Catalog Price Rules and check whether this attribute visible in Dropdown on Conditions tab.
  */
 class AssertProductAttributeIsUsedPromoRules extends AbstractConstraint
 {
     /**
      * Assert that product attribute can be used on promo rules conditions.
      *
+     * @param CatalogRuleIndex $catalogRuleIndex
+     * @param CatalogRuleNew $catalogRuleNew
+     * @param CatalogProductAttribute $attribute
      * @return void
      */
-    public function processAssert()
-    {
-        //
+    public function processAssert(
+        CatalogRuleIndex $catalogRuleIndex,
+        CatalogRuleNew $catalogRuleNew,
+        CatalogProductAttribute $attribute
+    ) {
+        $catalogRuleIndex->open();
+        $catalogRuleIndex->getGridPageActions()->addNew();
+        $catalogRuleNew->getEditForm()->openTab('conditions');
+
+        \PHPUnit_Framework_Assert::assertTrue(
+            $catalogRuleNew->getEditForm()->isAttributeInConditions($attribute->getFrontendLabel()),
+            'Product attribute can\'t be used on promo rules conditions.'
+        );
     }
 
     /**
+     * Returns a string representation of the object.
+     *
      * @return string
      */
     public function toString()
     {
-        //
+        return 'Product attribute can be used on promo rules conditions.';
     }
 }
