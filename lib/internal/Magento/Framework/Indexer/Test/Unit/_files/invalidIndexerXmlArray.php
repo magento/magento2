cf09b7b7eb6cc45c94d9b1aaba088690d6edeccc
Maksym Savich <msavich@ebay.com>
2015-09-16 15:46:41 -0500

    MAGETWO-42196: Magento components loaded from vendor directory - XSD filepath resolution
    - Test fix

--- lib/internal/Magento/Framework/Indexer/Test/Unit/_files/invalidIndexerXmlArray.php
+++ lib/internal/Magento/Framework/Indexer/Test/Unit/_files/invalidIndexerXmlArray.php
@@ -6,24 +6,27 @@
 return [
     'without_indexer_handle' => [
         '<?xml version="1.0"?><config></config>',
-        ["Element 'config': Missing child element(s). Expected is ( indexer )."],
+        ["Element 'config': Missing child element(s). Expected is ( indexer ).\nLine: 1\n"],
     ],
     'indexer_with_notallowed_attribute' => [
         '<?xml version="1.0"?><config>' .
         '<indexer id="somename" view_id="view_01" class="Class\Name" notallowed="some value">' .
         '<title>Test</title><description>Test</description></indexer></config>',
-        ["Element 'indexer', attribute 'notallowed': The attribute 'notallowed' is not allowed."],
+        ["Element 'indexer', attribute 'notallowed': The attribute 'notallowed' is not allowed.\nLine: 1\n"],
     ],
     'indexer_without_view_attribute' => [
         '<?xml version="1.0"?><config><indexer id="somename" class="Class\Name">' .
         '<title>Test</title><description>Test</description></indexer></config>',
-        ["Element 'indexer': The attribute 'view_id' is required but missing."],
+        ["Element 'indexer': The attribute 'view_id' is required but missing.\nLine: 1\n"],
     ],
     'indexer_duplicate_view_attribute' => [
         '<?xml version="1.0"?><config><indexer id="somename" view_id="view_01" class="Class\Name">' .
         '<title>Test</title><description>Test</description></indexer>' .
         '<indexer id="somename_two" view_id="view_01" class="Class\Name">' .
         '<title>Test</title><description>Test</description></indexer></config>',
-        ["Element 'indexer': Duplicate key-sequence ['view_01'] in unique identity-constraint 'uniqueViewId'."],
+        [
+            "Element 'indexer': Duplicate key-sequence ['view_01'] in unique identity-constraint"
+                . " 'uniqueViewId'.\nLine: 1\n"
+        ],
     ],
 ];
