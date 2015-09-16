d13cd5ab00a883679448274800ac5e984b7c52f0
Igor Melnikov <imelnikov@ebay.com>
2015-09-16 11:55:59 -0500

    MAGETWO-42765: Contribute MAGETWO-42030 to merchant_beta
    Refactored to inject \Zend\Uri\Uri and \Zend\Http\Client\Adapter\Socket via factories

---
+++ app/code/Magento/CacheInvalidate/Model/UriFactory.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\CacheInvalidate\Model;
+
+class UriFactory
+{
+    /** @var \Magento\Framework\ObjectManagerInterface */
+    private $objectManager;
+
+    /**
+     * @param \Magento\Framework\ObjectManagerInterface $objectManager
+     */
+    public function __construct(\Magento\Framework\ObjectManagerInterface $objectManager)
+    {
+        $this->objectManager = $objectManager;
+    }
+
+    /**
+     * @return \Zend\Uri\Uri
+     */
+    public function create()
+    {
+        return $this->objectManager->get('Zend\Uri\Uri');
+    }
+}
