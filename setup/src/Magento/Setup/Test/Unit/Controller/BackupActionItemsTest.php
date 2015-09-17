e3463d2178070ccc8a9f86364cb9fd21b870ca54
Maddy Chellathurai <mchellathura@ebay.com>
2015-09-17 12:25:33 -0500

    MAGETWO-42909: Setup unit tests fails on PHP7
    - fixing backup action items test

--- setup/src/Magento/Setup/Test/Unit/Controller/BackupActionItemsTest.php
+++ setup/src/Magento/Setup/Test/Unit/Controller/BackupActionItemsTest.php
@@ -48,8 +48,8 @@ class BackupActionItemsTest extends \PHPUnit_Framework_TestCase
         $this->directoryList = $this->getMock('Magento\Framework\App\Filesystem\DirectoryList', [], [], '', false);
         $this->objectManagerProvider = $this->getMock('Magento\Setup\Model\ObjectManagerProvider', [], [], '', false);
         $this->backupRollback = $this->getMock(
-            'Magento\\Setup\Model\\BackupRollback',
-            [],
+            'Magento\Setup\Model\BackupRollback',
+            ['getDBDiskSpace','dbBackup'],
             [],
             '',
             false
@@ -59,16 +59,33 @@ class BackupActionItemsTest extends \PHPUnit_Framework_TestCase
         $this->objectManagerProvider->expects($this->once())->method('get')->willReturn($objectManager);
         $this->log = $this->getMock('Magento\Setup\Model\WebLogger', [], [], '', false);
         $this->filesystem = $this->getMock('Magento\Framework\Backup\Filesystem', [], [], '', false);
+
         $this->controller = new BackupActionItems(
             $this->objectManagerProvider,
             $this->log,
             $this->directoryList,
             $this->filesystem
         );
+
+        $request = $this->getMock('\Zend\Http\PhpEnvironment\Request', [], [], '', false);
+        $response = $this->getMock('\Zend\Http\PhpEnvironment\Response', [], [], '', false);
+        $routeMatch = $this->getMock('\Zend\Mvc\Router\RouteMatch', [], [], '', false);
+
+        $mvcEvent = $this->getMock('\Zend\Mvc\MvcEvent', [], [], '', false);
+        $mvcEvent->expects($this->any())->method('setRequest')->with($request)->willReturn($mvcEvent);
+        $mvcEvent->expects($this->any())->method('setResponse')->with($response)->willReturn($mvcEvent);
+        $mvcEvent->expects($this->any())->method('setTarget')->with($this->controller)->willReturn($mvcEvent);
+        $mvcEvent->expects($this->any())->method('getRouteMatch')->willReturn($routeMatch);
+        $contentArray = '{"options":{"code":false,"media":false,"db":true}}';
+        $request->expects($this->any())->method('getContent')->willReturn($contentArray);
+
+        $this->controller->setEvent($mvcEvent);
+        $this->controller->dispatch($request, $response);
     }
 
     public function testCheckAction()
     {
+        $this->backupRollback->expects($this->once())->method('getDBDiskSpace')->willReturn(500);
         $this->directoryList->expects($this->once())->method('getPath')->willReturn(__DIR__);
         $this->filesystem->expects($this->once())->method('validateAvailableDiscSpace');
         $jsonModel = $this->controller->checkAction();
@@ -97,12 +114,13 @@ class BackupActionItemsTest extends \PHPUnit_Framework_TestCase
 
     public function testCreateAction()
     {
+        $this->backupRollback->expects($this->once())->method('dbBackup')->willReturn('backup/path/');
         $jsonModel = $this->controller->createAction();
         $this->assertInstanceOf('Zend\View\Model\JsonModel', $jsonModel);
         $variables = $jsonModel->getVariables();
         $this->assertArrayHasKey('responseType', $variables);
         $this->assertEquals(ResponseTypeInterface::RESPONSE_TYPE_SUCCESS, $variables['responseType']);
         $this->assertArrayHasKey('files', $variables);
-        $this->assertEquals([], $variables['files']);
+        $this->assertEquals(['backup/path/'], $variables['files']);
     }
 }
