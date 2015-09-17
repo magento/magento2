c5c7a26d9065de24b121214646e92458fc63fe9b
Olga Kopylova <okopylova@ebay.com>
2015-09-17 17:58:17 -0500

    MAGETWO-42264: Check and fix implementation for themes
    - fixed code style

--- app/code/Magento/Integration/Model/Config/SchemaLocator.php
+++ app/code/Magento/Integration/Model/Config/SchemaLocator.php
@@ -4,6 +4,7 @@
  * See COPYING.txt for license details.
  */
 namespace Magento\Integration\Model\Config;
+
 use Magento\Framework\Module\Dir;
 
 /**
@@ -30,7 +31,8 @@ class SchemaLocator implements \Magento\Framework\Config\SchemaLocatorInterface
      */
     public function __construct(\Magento\Framework\Module\Dir\Reader $moduleReader)
     {
-        $this->_schema = $moduleReader->getModuleDir(Dir::MODULE_ETC_DIR, 'Magento_Integration') . '/integration/config.xsd';
+        $this->_schema = $moduleReader->getModuleDir(Dir::MODULE_ETC_DIR, 'Magento_Integration')
+            . '/integration/config.xsd';
     }
 
     /**
