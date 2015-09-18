dafb0fc2742bca12e60ae24909a3b58ba38da960
Yushkin, Dmytro <dyushkin@ebay.com>
2015-09-18 19:28:37 +0300

    MAGETWO-42964: Password upgrade for older M2 versions
    - add password hash upgrade scripts for old Magento version users

--- app/code/Magento/Customer/Setup/UpgradeData.php
+++ app/code/Magento/Customer/Setup/UpgradeData.php
@@ -7,6 +7,7 @@
 namespace Magento\Customer\Setup;
 
 use Magento\Customer\Model\Customer;
+use Magento\Framework\Encryption\Encryptor;
 use Magento\Framework\Indexer\IndexerRegistry;
 use Magento\Framework\Setup\UpgradeDataInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
@@ -227,6 +228,10 @@ class UpgradeData implements UpgradeDataInterface
             );
         }
 
+        if (version_compare($context->getVersion(), '2.0.5', '<')) {
+            $this->upgradeHash($setup);
+        }
+
         $indexer = $this->indexerRegistry->get(Customer::CUSTOMER_GRID_INDEXER_ID);
         $indexer->reindexAll();
         $this->eavConfig->clear();
@@ -248,6 +253,35 @@ class UpgradeData implements UpgradeDataInterface
                 }
                 $attribute->save();
             }
+        }
+    }
+
+    /**
+     * @param ModuleDataSetupInterface $setup
+     */
+    private function upgradeHash($setup)
+    {
+        $customerEntityTable = $setup->getTable('customer_entity');
+
+        $select = $setup->getConnection()->select()->from(
+            $customerEntityTable,
+            ['entity_id', 'password_hash']
+        );
+
+        $customers = $setup->getConnection()->fetchAll($select);
+        foreach ($customers as $customer) {
+            list($hash, $salt) = explode(Encryptor::DELIMITER, $customer['password_hash']);
+
+            $newHash = $customer['password_hash'];
+            if (strlen($hash) === 32) {
+                $newHash = implode(Encryptor::DELIMITER, [$hash, $salt, Encryptor::HASH_VERSION_MD5]);
+            } elseif (strlen($hash) === 64) {
+                $newHash = implode(Encryptor::DELIMITER, [$hash, $salt, Encryptor::HASH_VERSION_SHA256]);
+            }
+
+            $bind = ['password_hash' => $newHash];
+            $where = ['entity_id = ?' => (int)$customer['entity_id']];
+            $setup->getConnection()->update($customerEntityTable, $bind, $where);
         }
     }
 }
