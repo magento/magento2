22303e9ce7b53d797338c9d476ab135afedb7e7a
Andrii Kasian <akasian@ebay.com>
2015-09-21 10:14:33 +0300

    MAGETWO-42577: L4 EE Integration Test Fails

--- dev/tests/integration/testsuite/Magento/Catalog/Model/Layer/Filter/_files/attribute_with_option_rollback.php
+++ dev/tests/integration/testsuite/Magento/Catalog/Model/Layer/Filter/_files/attribute_with_option_rollback.php
@@ -12,6 +12,26 @@ $attribute = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create
     'Magento\Catalog\Model\Resource\Eav\Attribute'
 );
 $attribute->loadByCode($installer->getEntityTypeId('catalog_product'), 'attribute_with_option');
+
+/* Delete simple products per each option */
+/** @var $options \Magento\Eav\Model\Resource\Entity\Attribute\Option\Collection */
+$options = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create(
+    'Magento\Eav\Model\Resource\Entity\Attribute\Option\Collection'
+);
+$options->setAttributeFilter($attribute->getId());
+
+foreach ($options as $option) {
+    /** @var $product \Magento\Catalog\Model\Product */
+    $product = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create('Magento\Catalog\Model\Product');
+    $product->load(
+        'simple_product_' . $option->getId(),
+        'sku'
+    );
+    if ($product->getId()) {
+        $product->delete();
+    }
+}
+
 if ($attribute->getId()) {
     $attribute->delete();
 }
--- dev/tests/integration/testsuite/Magento/Framework/Search/_files/filterable_attribute_rollback.php
+++ dev/tests/integration/testsuite/Magento/Framework/Search/_files/filterable_attribute_rollback.php
@@ -15,6 +15,21 @@ $attribute = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create
     'Magento\Catalog\Model\Resource\Eav\Attribute'
 );
 $attribute->loadByCode($installer->getEntityTypeId('catalog_product'), 'select_attribute');
+
+/** @var $selectOptions \Magento\Eav\Model\Resource\Entity\Attribute\Option\Collection */
+$selectOptions = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create(
+    'Magento\Eav\Model\Resource\Entity\Attribute\Option\Collection'
+);
+$selectOptions->setAttributeFilter($attribute->getId());
+/* Delete simple products per each select(dropdown) option */
+foreach ($selectOptions as $option) {
+    /** @var $product \Magento\Catalog\Model\Product */
+    $product = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create('Magento\Catalog\Model\Product');
+    $product->load('simple_product_' . $option->getId(), 'sku');
+    if ($product->getId()) {
+        $product->delete();
+    }
+}
 if ($attribute->getId()) {
     $attribute->delete();
 }
