182d9d415851d499329d5f903706a14df178f474
Alexander Paliarush <apaliarush@ebay.com>
2015-09-22 11:57:11 +0300

    MAGETWO-41395: Cannot switch between stores in storefront when default store is changed
    - Added integration tests

---
+++ dev/tests/integration/testsuite/Magento/Store/Controller/Store/SwitchActionTest.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Store\Controller\Store;
+
+/**
+ * Test for store switch controller.
+ */
+class SwitchActionTest extends \Magento\TestFramework\TestCase\AbstractController
+{
+    /**
+     * Ensure that proper default store code is calculated.
+     *
+     * Make sure that if default store code is changed from 'default' to something else,
+     * proper code is used in HTTP context. If default store code is still 'default' this may lead to
+     * incorrect work of page cache.
+     *
+     * @magentoDbIsolation enabled
+     */
+    public function testExecuteWithCustomDefaultStore()
+    {
+
+        $defaultStoreCode = 'default';
+        $modifiedDefaultCode = 'modified_default_code';
+        $this->changeStoreCode($defaultStoreCode, $modifiedDefaultCode);
+
+        $this->dispatch('stores/store/switch');
+        /** @var \Magento\Framework\App\Http\Context $httpContext */
+        $httpContext = $this->_objectManager->get('Magento\Framework\App\Http\Context');
+        $httpContext->unsValue(\Magento\Store\Model\Store::ENTITY);
+        $this->assertEquals($modifiedDefaultCode, $httpContext->getValue(\Magento\Store\Model\Store::ENTITY));
+
+        $this->changeStoreCode($modifiedDefaultCode, $defaultStoreCode);
+    }
+
+    /**
+     * Change store code.
+     *
+     * @param string $from
+     * @param string $to
+     */
+    protected function changeStoreCode($from, $to)
+    {
+        /** @var \Magento\Store\Model\Store $store */
+        $store = $this->_objectManager->create('Magento\Store\Model\Store');
+        $store->load($from, 'code');
+        $store->setCode($to);
+        $store->save();
+        /** @var \Magento\Store\Model\StoreManagerInterface $storeManager */
+        $storeManager = $this->_objectManager->get('Magento\Store\Model\StoreManagerInterface');
+        $storeManager->reinitStores();
+    }
+}
