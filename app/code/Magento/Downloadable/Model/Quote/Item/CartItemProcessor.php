c6049d5f963dbb5c7672d3adf58a46694f0924fe
Alex Akimov <aakimov@ebay.com>
2015-09-22 11:53:14 +0300

    MAGETWO-43010: Support Downloadable Product in Quote API

--- app/code/Magento/Downloadable/Model/Quote/Item/CartItemProcessor.php
+++ app/code/Magento/Downloadable/Model/Quote/Item/CartItemProcessor.php
@@ -17,11 +17,40 @@ class CartItemProcessor implements CartItemProcessorInterface
     private $objectFactory;
 
     /**
+     * @var \Magento\Framework\Api\DataObjectHelper
+     */
+    private $dataObjectHelper;
+
+    /**
+     * @var \Magento\Downloadable\Model\DownloadableOptionFactory
+     */
+    private $downloadableOptionFactory;
+
+    /**
+     * @var \Magento\Quote\Model\Quote\ProductOptionFactory
+     */
+    private $productOptionFactory;
+
+    /**
+     * @var \Magento\Quote\Api\Data\ProductOptionExtensionFactory
+     */
+    private $extensionFactory;
+
+    /**
      * @param DataObjectFactory $objectFactory
      */
-    public function __construct(DataObjectFactory $objectFactory)
-    {
+    public function __construct(
+        DataObjectFactory $objectFactory,
+        \Magento\Framework\Api\DataObjectHelper $dataObjectHelper,
+        \Magento\Downloadable\Model\DownloadableOptionFactory $downloadableOptionFactory,
+        \Magento\Quote\Model\Quote\ProductOptionFactory $productOptionFactory,
+        \Magento\Quote\Api\Data\ProductOptionExtensionFactory $extensionFactory
+    ) {
         $this->objectFactory = $objectFactory;
+        $this->dataObjectHelper = $dataObjectHelper;
+        $this->downloadableOptionFactory = $downloadableOptionFactory;
+        $this->productOptionFactory = $productOptionFactory;
+        $this->extensionFactory = $extensionFactory;
     }
 
     /**
@@ -52,7 +81,39 @@ class CartItemProcessor implements CartItemProcessorInterface
      */
     public function processProductOptions(CartItemInterface $cartItem)
     {
+        $options = $cartItem->getOptions();
+        $downloadableLinkIds = [];
+        if (is_array($options)) {
+            /** @var \Magento\Quote\Model\Quote\Item\Option $option */
+            foreach ($options as $option) {
+                if ($option->getCode() == 'downloadable_link_ids') {
+                    $downloadableLinkIds = explode(',', $option->getValue());
+                    break;
+                }
+            }
+        }
+
+        $downloadableOption = $this->downloadableOptionFactory->create();
+        $this->dataObjectHelper->populateWithArray(
+            $downloadableOption,
+            [
+                'downloadable_links' => $downloadableLinkIds
+            ],
+            'Magento\Downloadable\Api\Data\DownloadableOptionInterface'
+        );
+
+        $productOption = ($cartItem->getProductOption())
+            ? $cartItem->getProductOption()
+            : $this->productOptionFactory->create();
+
+        $extensibleAttribute =  ($productOption->getExtensionAttributes())
+            ? $productOption->getExtensionAttributes()
+            : $this->extensionFactory->create();
+
+        $extensibleAttribute->setDownloadableOption($downloadableOption);
+        $productOption->setExtensionAttributes($extensibleAttribute);
+        $cartItem->setProductOption($productOption);
+
         return $cartItem;
     }
-
 }
