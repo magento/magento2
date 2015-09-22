03d841cf9908bb1fb87d3c28c92777c1c7301736
Alexander Paliarush <apaliarush@ebay.com>
2015-09-22 18:26:04 +0300

    MAGETWO-42606: Unable save field "available_sort_by" of category through webapi

--- app/code/Magento/Catalog/Model/CategoryRepository.php
+++ app/code/Magento/Catalog/Model/CategoryRepository.php
@@ -60,7 +60,7 @@ class CategoryRepository implements \Magento\Catalog\Api\CategoryRepositoryInter
      */
     public function save(\Magento\Catalog\Api\Data\CategoryInterface $category)
     {
-        $existingData = $category->toFlatArray();
+        $existingData = array_merge($category->toFlatArray(), ['available_sort_by' => $category->getAvailableSortBy()]);
         if ($category->getId()) {
             $existingCategory = $this->get($category->getId());
             if (isset($existingData['image']) && is_array($existingData['image'])) {
