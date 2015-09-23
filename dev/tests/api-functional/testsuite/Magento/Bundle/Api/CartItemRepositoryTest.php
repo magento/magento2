c94bb6cce3ab9b0cb9fcf9cc9c83f0f8f757a146
Ievgen Shakhsuvarov <ishakhsuvarov@ebay.com>
2015-09-23 17:50:47 +0300

    MAGETWO-43053: Support bundled Product in Quote API

--- dev/tests/api-functional/testsuite/Magento/Bundle/Api/CartItemRepositoryTest.php
+++ dev/tests/api-functional/testsuite/Magento/Bundle/Api/CartItemRepositoryTest.php
@@ -24,12 +24,49 @@ class CartItemRepositoryTest extends WebapiAbstract
     }
 
     /**
+     * @magentoApiDataFixture Magento/Checkout/_files/quote_with_bundle_and_options.php
+     */
+    public function testGetAll()
+    {
+        /** @var $quote \Magento\Quote\Model\Quote */
+        $quote = $this->objectManager->create('Magento\Quote\Model\Quote')->load(
+            'test_order_bundle',
+            'reserved_order_id'
+        );
+        $quoteId = $quote->getId();
+
+        $serviceInfo = [
+            'rest' => [
+                'resourcePath' => self::RESOURCE_PATH . $quoteId . '/items',
+                'httpMethod' => \Magento\Framework\Webapi\Rest\Request::HTTP_METHOD_GET,
+            ],
+            'soap' => [
+                'service' => self::SERVICE_NAME,
+                'serviceVersion' => self::SERVICE_VERSION,
+                'operation' => self::SERVICE_NAME . 'GetList',
+            ],
+        ];
+        $response = $this->_webApiCall($serviceInfo, ['cartId' => $quoteId])[0];
+        $bundleOption = $quote->getItemById($response['item_id'])->getBuyRequest()->getBundleOption();
+        $bundleOptionQty = $quote->getItemById($response['item_id'])->getBuyRequest()->getBundleOptionQty();
+        $actualOptions = $response['product_option']['extension_attributes']['bundle_options'];
+
+        $this->assertEquals(array_keys($bundleOption), array_column($actualOptions, 'option_id'));
+        $this->assertEquals($bundleOptionQty, array_column($actualOptions, 'option_qty', 'option_id'));
+        foreach ($actualOptions as $option) {
+            $id = $option['option_id'];
+            $expectedSelections = is_array($bundleOption[$id]) ? $bundleOption[$id] : [$bundleOption[$id]];
+            $this->assertEquals($expectedSelections, $option['option_selections']);
+        }
+    }
+
+    /**
      * @magentoApiDataFixture Magento/Checkout/_files/quote_with_items_saved.php
      * @magentoApiDataFixture Magento/Bundle/_files/product.php
      */
     public function testAddItem()
     {
-        /** @var  \Magento\Catalog\Model\Product $product */
+        /** @var \Magento\Catalog\Model\Product $product */
         $product = $this->objectManager->create('Magento\Catalog\Model\Product')->load(3);
         $quote = $this->objectManager->create('Magento\Quote\Model\Quote')->load(
             'test_order_item_with_items',
