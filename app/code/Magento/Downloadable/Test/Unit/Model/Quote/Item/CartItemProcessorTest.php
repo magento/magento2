9284d6fcdab7c024f9ce701de30033d1120f8530
Iryna Lagno <ilagno@ebay.com>
2015-09-24 11:48:56 +0300

    MAGETWO-43058: Stabilization activities

--- app/code/Magento/Downloadable/Test/Unit/Model/Quote/Item/CartItemProcessorTest.php
+++ app/code/Magento/Downloadable/Test/Unit/Model/Quote/Item/CartItemProcessorTest.php
@@ -3,7 +3,7 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\Downloadable\Model\Quote\Item;
+namespace Magento\Downloadable\Test\Unit\Model\Quote\Item;
 
 class CartItemProcessorTest extends \PHPUnit_Framework_TestCase
 {
