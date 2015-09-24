bcf9eec9cf5f0e1b9c83ed3f1481c1df927ac0e5
Leonid Poluyanov <lpoluyanov@ebay.com>
2015-09-24 14:04:30 +0300

    MAGETWO-43080: Add ability to log errors during SampleData installation

---
+++ lib/internal/Magento/Framework/Setup/SampleData/StateInterface.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Framework\Setup\SampleData;
+
+/**
+ * Interface for SampleData modules installation
+ */
+interface StateInterface
+{
+    /**
+     * Set error flag to Sample Data state
+     *
+     * @return void
+     */
+    public function setError();
+
+    /**
+     * Check if Sample Data state has error
+     *
+     * @return bool
+     */
+    public function hasError();
+}
