8c2521b432f271c7c7e3613d2d4d3bc225af00f9
Eugene Tulika <etulika@ebay.com>
2015-09-25 17:11:20 -0500

    MAGETWO-43226: Fix tests when Magento is in vendor

--- dev/tests/static/testsuite/Magento/Test/Integrity/ConfigTest.php
+++ dev/tests/static/testsuite/Magento/Test/Integrity/ConfigTest.php
@@ -53,10 +53,13 @@ class ConfigTest extends \PHPUnit_Framework_TestCase
     {
         $configFiles = \Magento\Framework\App\Utility\Files::init()->getConfigFiles('config.xml', [], false);
         $data = [];
-        foreach ($configFiles as $configFile) {
-            preg_match('#/([^/]+?/[^/]+?)/etc/config\.xml$#', $configFile, $moduleName);
-            $moduleName = str_replace('/', '_', $moduleName[1]);
-            $data[$configFile] = $moduleName;
+        $componentRegistrar = new \Magento\Framework\Component\ComponentRegistrar();
+        $modulesPaths = $componentRegistrar->getPaths(\Magento\Framework\Component\ComponentRegistrar::MODULE);
+
+        foreach ($modulesPaths as $moduleName => $path) {
+            if (file_exists($configFile = $path . '/etc/config.xml')) {
+                $data[$configFile] = $moduleName;
+            }
         }
         return $data;
     }
