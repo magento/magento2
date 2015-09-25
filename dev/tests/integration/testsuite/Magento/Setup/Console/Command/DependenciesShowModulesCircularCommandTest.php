d3345d28158d1b7a39e9f5b962eef801a06f5234
Maddy Chellathurai <mchellathura@ebay.com>
2015-09-25 10:57:30 -0500

    MAGETWO-42264: Check and fix implementation for themes
    - fix tests

--- dev/tests/integration/testsuite/Magento/Setup/Console/Command/DependenciesShowModulesCircularCommandTest.php
+++ dev/tests/integration/testsuite/Magento/Setup/Console/Command/DependenciesShowModulesCircularCommandTest.php
@@ -27,20 +27,38 @@ class DependenciesShowModulesCircularCommandTest extends \PHPUnit_Framework_Test
 
     public function setUp()
     {
-        $this->command = new DependenciesShowModulesCircularCommand();
+        $modules = [
+            'Magento_A' => __DIR__ . '/_files/root/app/code/Magento/A',
+            'Magento_B' => __DIR__ . '/_files/root/app/code/Magento/B'
+        ];
+
+        $objectManagerProvider = $this->getMock('Magento\Setup\Model\ObjectManagerProvider', [], [], '', false);
+        $objectManager = $this->getMock('\Magento\Framework\App\ObjectManager', [], [], '', false);
+        $objectManagerProvider->expects($this->once())->method('get')->willReturn($objectManager);
+
+        $themePackageListMock = $this->getMock(
+            'Magento\Framework\View\Design\Theme\ThemePackageList',
+            [],
+            [],
+            '',
+            false
+        );
+        $componentRegistrarMock = $this->getMock('Magento\Framework\Component\ComponentRegistrar', [], [], '', false);
+        $componentRegistrarMock->expects($this->any())->method('getPaths')->will($this->returnValue($modules));
+        $dirSearchMock = $this->getMock('Magento\Framework\Component\DirSearch', [], [], '', false);
+        $objectManager->expects($this->any())->method('get')->will($this->returnValueMap([
+            ['Magento\Framework\View\Design\Theme\ThemePackageList', $themePackageListMock],
+            ['Magento\Framework\Component\ComponentRegistrar', $componentRegistrarMock],
+            ['Magento\Framework\Component\DirSearch', $dirSearchMock]
+        ]));
+
+        $this->command = new DependenciesShowModulesCircularCommand($objectManagerProvider);
         $this->commandTester = new CommandTester($this->command);
         $reflection = new \ReflectionClass('Magento\Framework\Component\ComponentRegistrar');
         $paths = $reflection->getProperty('paths');
         $paths->setAccessible(true);
         $this->backupRegistrar = $paths->getValue();
-        $paths->setValue(
-            [
-                ComponentRegistrar::MODULE => [
-                    'Magento_A' => __DIR__ . '/_files/root/app/code/Magento/A',
-                    'Magento_B' => __DIR__ . '/_files/root/app/code/Magento/B'
-                ]
-            ]
-        );
+        $paths->setValue([ComponentRegistrar::MODULE => $modules]);
         $paths->setAccessible(false);
     }
 
--- dev/tests/integration/testsuite/Magento/Setup/Console/Command/DependenciesShowModulesCommandTest.php
+++ dev/tests/integration/testsuite/Magento/Setup/Console/Command/DependenciesShowModulesCommandTest.php
@@ -27,20 +27,40 @@ class DependenciesShowModulesCommandTest extends \PHPUnit_Framework_TestCase
 
     public function setUp()
     {
-        $this->command = new DependenciesShowModulesCommand();
+
+        $modules = [
+            'Magento_A' => __DIR__ . '/_files/root/app/code/Magento/A',
+            'Magento_B' => __DIR__ . '/_files/root/app/code/Magento/B'
+        ];
+
+        $objectManagerProvider = $this->getMock('Magento\Setup\Model\ObjectManagerProvider', [], [], '', false);
+        $objectManager = $this->getMock('\Magento\Framework\App\ObjectManager', [], [], '', false);
+        $objectManagerProvider->expects($this->once())->method('get')->willReturn($objectManager);
+
+        $themePackageListMock = $this->getMock(
+            'Magento\Framework\View\Design\Theme\ThemePackageList',
+            [],
+            [],
+            '',
+            false
+        );
+        $componentRegistrarMock = $this->getMock('Magento\Framework\Component\ComponentRegistrar', [], [], '', false);
+        $componentRegistrarMock->expects($this->any())->method('getPaths')->will($this->returnValue($modules));
+        $dirSearchMock = $this->getMock('Magento\Framework\Component\DirSearch', [], [], '', false);
+        $objectManager->expects($this->any())->method('get')->will($this->returnValueMap([
+            ['Magento\Framework\View\Design\Theme\ThemePackageList', $themePackageListMock],
+            ['Magento\Framework\Component\ComponentRegistrar', $componentRegistrarMock],
+            ['Magento\Framework\Component\DirSearch', $dirSearchMock]
+        ]));
+
+        $this->command = new DependenciesShowModulesCommand($objectManagerProvider);
+
         $this->commandTester = new CommandTester($this->command);
         $reflection = new \ReflectionClass('Magento\Framework\Component\ComponentRegistrar');
         $paths = $reflection->getProperty('paths');
         $paths->setAccessible(true);
         $this->backupRegistrar = $paths->getValue();
-        $paths->setValue(
-            [
-                ComponentRegistrar::MODULE => [
-                    'Magento_A' => __DIR__ . '/_files/root/app/code/Magento/A',
-                    'Magento_B' => __DIR__ . '/_files/root/app/code/Magento/B'
-                ]
-            ]
-        );
+        $paths->setValue([ComponentRegistrar::MODULE => $modules]);
         $paths->setAccessible(false);
     }
 
