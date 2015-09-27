57153797077b57220570e9cc9d3725b188342e42
Andrii Kasian <akasian@ebay.com>
2015-09-27 13:09:43 +0300

    MAGETWO-42501: PCF: removing grouped price

---
+++ app/code/Magento/Catalog/Setup/UpgradeData.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Catalog\Setup;
+
+use Magento\Customer\Model\Customer;
+use Magento\Framework\Setup\UpgradeDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+/**
+ * @codeCoverageIgnore
+ */
+class UpgradeData implements UpgradeDataInterface
+{
+    /**
+     * {@inheritdoc}
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+     */
+    public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+        if (version_compare($context->getVersion(), '2.0.1', '<')) {
+
+        }
+        $setup->endSetup();
+    }
+}
