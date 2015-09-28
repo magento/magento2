601a9ebb5bbda19be4ad49d2c8af3e782fafef70
Dmytro Aponasenko <daponasenko@ebay.com>
2015-09-28 09:43:47 +0300

    MTA-2635: Create Order Web Api handler

--- dev/tests/functional/tests/app/Magento/Tax/Test/Handler/TaxClass/Webapi.php
+++ dev/tests/functional/tests/app/Magento/Tax/Test/Handler/TaxClass/Webapi.php
@@ -28,7 +28,7 @@ class Webapi extends AbstractWebapi implements TaxClassInterface
 
         $url = $_ENV['app_frontend_url'] . 'rest/V1/taxClasses';
         $this->webapiTransport->write($url, $data);
-        $response = $this->webapiTransport->read();
+        $response = json_decode($this->webapiTransport->read());
         $this->webapiTransport->close();
 
         if (!is_numeric($response)) {
