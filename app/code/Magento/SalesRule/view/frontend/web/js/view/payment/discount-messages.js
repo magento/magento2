b2b425d48c8ccde6dec0a9a7cc859a1d6d3b84f6
Ankur Kaneria <akaneria@ebay.com>
2015-09-28 23:17:52 -0500

    MAGETWO-42268: Fix new builds
    - code updates to fix js static test errors reported by eslint and jscs

--- app/code/Magento/SalesRule/view/frontend/web/js/view/payment/discount-messages.js
+++ app/code/Magento/SalesRule/view/frontend/web/js/view/payment/discount-messages.js
@@ -9,6 +9,9 @@ define([
     'use strict';
 
     return Component.extend({
+        /**
+         * TODO: MAGETWO-43367
+         */
         initialize: function (config) {
             return this._super(config, messageContainer);
         }
