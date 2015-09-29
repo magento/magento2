3ca0084dd9115cccc149acc4cae3d36458b5bcbe
Maddy Chellathurai <mchellathura@ebay.com>
2015-09-29 11:21:51 -0500

    MAGETWO-42268: Fix new builds
    - fixing js errors

--- app/code/Magento/Checkout/view/frontend/web/js/view/authentication-messages.js
+++ app/code/Magento/Checkout/view/frontend/web/js/view/authentication-messages.js
@@ -9,6 +9,8 @@ define([
     'use strict';
 
     return Component.extend({
+
+        /** TODO: MAGETWO-43367 **/
         initialize: function (config) {
             return this._super(config, messageContainer);
         }
