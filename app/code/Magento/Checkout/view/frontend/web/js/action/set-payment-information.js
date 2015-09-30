c6d84585e7e4f6fe24b07071824edf392ea8aa50
Dmytro Kvashnin <dkvashnin@ebay.com>
2015-09-30 12:30:49 +0300

    MAGETWO-43288: Paypal WebAPI exception error when placing order within PayPal Payments Pro
    - Fixed bug in Checkout set-payment-information component
    - Refactored client code

--- app/code/Magento/Checkout/view/frontend/web/js/action/set-payment-information.js
+++ app/code/Magento/Checkout/view/frontend/web/js/action/set-payment-information.js
@@ -13,10 +13,9 @@ define(
     function (quote, urlBuilder, storage, errorProcessor, customer) {
         'use strict';
 
-        return function (messageContainer) {
+        return function (messageContainer, paymentData) {
             var serviceUrl,
-                payload,
-                paymentData = quote.paymentMethod();
+                payload;
 
             /**
              * Checkout for guest and registered customer.
