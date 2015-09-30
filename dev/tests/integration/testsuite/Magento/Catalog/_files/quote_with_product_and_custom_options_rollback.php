acf2f6e432fa3d939e28767c2c2e823babeb6244
Oleksandr Karpenko <okarpenko@ebay.com>
2015-09-30 17:03:35 +0300

    MAGETWO-43174: Support product option in Quote API

---
+++ dev/tests/integration/testsuite/Magento/Catalog/_files/quote_with_product_and_custom_options_rollback.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+require __DIR__ . '/product_simple_rollback.php';
+require __DIR__ . '/../../../Magento/Checkout/_files/quote_with_address_saved_rollback.php';
