60b3089f5c7bf7b190f47a26c25c896bdf76e909
Dale Sikkema <dsikkema@ebay.com>
2015-09-30 16:30:44 -0500

    MAGETWO-43197: Product's options are not saved after running setup:di:compile  - static test fix

--- lib/internal/Magento/Framework/Code/Generator.php
+++ lib/internal/Magento/Framework/Code/Generator.php
@@ -113,7 +113,7 @@ class Generator
      * Include file only if the class is not already defined in memory
      *
      * @param string $fileName
-     * @param $className
+     * @param string $className
      * @return void
      */
     public function tryToIncludeFile($fileName, $className)
