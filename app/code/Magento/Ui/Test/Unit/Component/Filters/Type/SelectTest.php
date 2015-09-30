001544daf1de78f58be840ea6d1808cded44794b
Maxim Medinskiy <mmedinskiy@ebay.com>
2015-09-30 19:10:57 +0300

    MAGETWO-42838: Combine filter and column components definition

--- app/code/Magento/Ui/Test/Unit/Component/Filters/Type/SelectTest.php
+++ app/code/Magento/Ui/Test/Unit/Component/Filters/Type/SelectTest.php
@@ -48,6 +48,10 @@ class SelectTest extends \PHPUnit_Framework_TestCase
             '',
             false
         );
+        $processor = $this->getMockBuilder('Magento\Framework\View\Element\UiComponent\Processor')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->contextMock->expects($this->any())->method('getProcessor')->willReturn($processor);
         $this->uiComponentFactory = $this->getMock(
             'Magento\Framework\View\Element\UiComponentFactory',
             ['create'],
