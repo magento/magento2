57500ca4c297e222e7e019ff049db02803611fb1
Igor Melnikov <imelnikov@ebay.com>
2015-10-01 12:59:41 -0500

    MAGETWO-40801: Remove uses of unserialize where not necessary - partial solution
    Fixing namespace in Magento\Sales\Test\Unit\Controller\Download\DownloadCustomOptionTest

--- app/code/Magento/Sales/Test/Unit/Controller/Download/DownloadCustomOptionTest.php
+++ app/code/Magento/Sales/Test/Unit/Controller/Download/DownloadCustomOptionTest.php
@@ -4,7 +4,7 @@
  * See COPYING.txt for license details.
  */
 
-namespace Magento\Sales\Test\Unit\Controller\Adminhtml\Order;
+namespace Magento\Sales\Test\Unit\Controller\Download;
 
 /**
  * Class DownloadCustomOptionTest
