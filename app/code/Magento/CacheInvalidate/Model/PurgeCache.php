0003b8b572ddca2bcf573792b8f2477d24c644cf
Joan He <joan@x.com>
2015-10-01 16:32:51 -0500

    MAGETWO-43253: CLI tool throws error when varnish settings are set

--- app/code/Magento/CacheInvalidate/Model/PurgeCache.php
+++ app/code/Magento/CacheInvalidate/Model/PurgeCache.php
@@ -8,16 +8,13 @@ namespace Magento\CacheInvalidate\Model;
 use Zend\Uri\Uri;
 use Magento\Framework\Cache\InvalidateLogger;
 use Magento\Framework\App\DeploymentConfig;
-use Magento\Framework\Config\ConfigOptionsListConstants;
-use Magento\Framework\App\RequestInterface;
-use Zend\Uri\UriFactory;
 
 class PurgeCache
 {
     /**
-     * @var \Magento\Framework\UrlInterface
+     * @var \Magento\PageCache\Model\Cache\Server
      */
-    protected $urlBuilder;
+    protected $cacheServer;
 
     /**
      * @var SocketFactory
@@ -30,38 +27,20 @@ class PurgeCache
     private $logger;
 
     /**
-     * @var DeploymentConfig
-     */
-    private $config;
-
-    /**
-     * @var RequestInterface
-     */
-    private $request;
-
-    const DEFAULT_PORT = 80;
-
-    /**
      * Constructor
      *
-     * @param \Magento\Framework\UrlInterface $urlBuilder
+     * @param \Magento\PageCache\Model\Cache\Server $cacheServer
      * @param SocketFactory $socketAdapterFactory
      * @param InvalidateLogger $logger
-     * @param DeploymentConfig $config
-     * @param RequestInterface $request
      */
     public function __construct(
-        \Magento\Framework\UrlInterface $urlBuilder,
+        \Magento\PageCache\Model\Cache\Server $cacheServer,
         SocketFactory $socketAdapterFactory,
-        InvalidateLogger $logger,
-        DeploymentConfig $config,
-        RequestInterface $request
+        InvalidateLogger $logger
     ) {
-        $this->urlBuilder = $urlBuilder;
+        $this->cacheServer = $cacheServer;
         $this->socketAdapterFactory = $socketAdapterFactory;
         $this->logger = $logger;
-        $this->config = $config;
-        $this->request = $request;
     }
 
     /**
@@ -74,7 +53,7 @@ class PurgeCache
     public function sendPurgeRequest($tagsPattern)
     {
         $socketAdapter = $this->socketAdapterFactory->create();
-        $servers = $this->getServers();
+        $servers = $this->cacheServer->getUris();
         $headers = ['X-Magento-Tags-Pattern' => $tagsPattern];
         $socketAdapter->setOptions(['timeout' => 10]);
         foreach ($servers as $server) {
@@ -95,29 +74,5 @@ class PurgeCache
 
         $this->logger->execute(compact('servers', 'tagsPattern'));
         return true;
-    }
-
-    /**
-     * Get cache servers' uris
-     *
-     * @return Uri[]
-     */
-    private function getServers()
-    {
-        $servers = [];
-        $configedHosts = $this->config->get(ConfigOptionsListConstants::CONFIG_PATH_CACHE_HOSTS);
-        if (null == $configedHosts) {
-            $httpHost = $this->request->getHttpHost();
-            $servers[] = $httpHost ?
-                UriFactory::factory('')->setHost($httpHost)->setPort(self::DEFAULT_PORT)->setScheme('http') :
-                UriFactory::factory($this->urlBuilder->getUrl('*'));
-        } else {
-            foreach ($configedHosts as $host) {
-                $servers[] = UriFactory::factory('')->setHost($host['host'])
-                    ->setPort(isset($host['port']) ? $host['port'] : self::DEFAULT_PORT)
-                    ->setScheme('http');
-            }
-        }
-        return $servers;
     }
 }
