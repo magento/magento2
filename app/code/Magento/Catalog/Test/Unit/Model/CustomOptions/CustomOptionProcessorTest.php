11af40f561398bf945d4a62e69d8f7b7bf3b2935
Oleksandr Karpenko <okarpenko@ebay.com>
2015-10-01 12:07:29 +0300

    MAGETWO-43174: Support product option in Quote API

--- app/code/Magento/Catalog/Test/Unit/Model/CustomOptions/CustomOptionProcessorTest.php
+++ app/code/Magento/Catalog/Test/Unit/Model/CustomOptions/CustomOptionProcessorTest.php
@@ -3,7 +3,7 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\Catalog\Test\Unit\Model\CustomOption;
+namespace Magento\Catalog\Test\Unit\Model\CustomOptions;
 
 use Magento\Catalog\Model\CustomOptions\CustomOptionProcessor;
 
