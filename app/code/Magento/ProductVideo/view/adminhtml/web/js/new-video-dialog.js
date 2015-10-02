04bda3f2443b55cd317173cfccdade921f8a42dd
Mikalai_Shostka <mikalai_shostka@epam.com>
2015-10-02 12:06:30 +0300

    PV-223: New Video: After entering URL and tabbing out, URL data is wiped away from the field
    Fix reset

--- app/code/Magento/ProductVideo/view/adminhtml/web/js/new-video-dialog.js
+++ app/code/Magento/ProductVideo/view/adminhtml/web/js/new-video-dialog.js
@@ -153,7 +153,6 @@ define([
          * Update
          */
         update: function () {
-            this.reset();
             $(this.options.DOM.titleField).val(this.options.data.title);
             $(this.options.DOM.descriptionField).val(this.options.data.description);
         },
