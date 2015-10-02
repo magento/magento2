3109579dcce96ac0eda329240e7b6d55e015ce94
Oleksandr Manchenko <omanchenko@ebay.com>
2015-10-02 09:54:20 +0300

    MTA-2493: Extend Create Customer Group

--- dev/tests/functional/tests/app/Magento/CatalogRule/Test/Block/Adminhtml/Promo/Catalog/Edit/Tab/RuleInformation.php
+++ dev/tests/functional/tests/app/Magento/CatalogRule/Test/Block/Adminhtml/Promo/Catalog/Edit/Tab/RuleInformation.php
@@ -4,14 +4,15 @@
  * See COPYING.txt for license details.
  */
 
-namespace Magento\SalesRule\Test\Block\Adminhtml\Promo\Quote\Edit\Tab;
+namespace Magento\CatalogRule\Test\Block\Adminhtml\Promo\Catalog\Edit\Tab;
 
-use Magento\Backend\Test\Block\Widget\Tab;
 use Magento\Customer\Test\Fixture\CustomerGroup;
+use Magento\Mtf\Factory\Factory;
+use Magento\Backend\Test\Block\Widget\Tab;
 use Magento\Mtf\Client\Element\SimpleElement;
 
 /**
- * Backend sales rule 'Rule Information' tab.
+ * Rule Information tab.
  */
 class RuleInformation extends Tab
 {
@@ -21,24 +22,6 @@ class RuleInformation extends Tab
      * @var string
      */
     protected $customerGroup = '#rule_customer_group_ids';
-
-    /**
-     * Get data of tab.
-     *
-     * @param array|null $fields
-     * @param SimpleElement|null $element
-     * @return array
-     */
-    public function getDataFormTab($fields = null, SimpleElement $element = null)
-    {
-        $context = ($element === null) ? $this->_rootElement : $element;
-        $data = $this->dataMapping($fields);
-        if ($this->getElement($context, $data['coupon_type'])->getValue() != 'Specific Coupon') {
-            unset($data['coupon_code']);
-            unset($data['uses_per_coupon']);
-        }
-        return $this->_getData($data, $element);
-    }
 
     /**
      * Check whether Customer Group is visible.
