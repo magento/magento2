5163afbffa5c55969e8e564f08779a8eb66f1b6a
ALIAKSANDR KOMAR <ALIAKSANDR_KOMAR@epam.com>
2015-10-02 19:51:23 +0300

    PV-208: css changes

--- app/code/Magento/ProductVideo/view/frontend/web/js/fotorama-add-video-events.js
+++ app/code/Magento/ProductVideo/view/frontend/web/js/fotorama-add-video-events.js
@@ -1,3 +1,8 @@
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
 require(["jquery", "jquery/ui", "catalogGallery"], function ($) {
     //this section is for helper functions
     function parseHref(href) {
