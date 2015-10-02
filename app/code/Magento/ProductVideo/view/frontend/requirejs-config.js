afbb16d8cb06ed39d6f8f7d404fda3def2b756a8
ALIAKSANDR KOMAR <ALIAKSANDR_KOMAR@epam.com>
2015-10-02 20:33:41 +0300

    PV-208: js style fixes

--- app/code/Magento/ProductVideo/view/frontend/requirejs-config.js
+++ app/code/Magento/ProductVideo/view/frontend/requirejs-config.js
@@ -2,12 +2,12 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-
+/*eslint no-unused-vars: 0*/
 var config = {
     map: {
         '*': {
-            fotoramaAddVideoEvents:  'Magento_ProductVideo/js/fotorama-add-video-events',
-            loadPlayer : 'Magento_ProductVideo/js/load-player'
+            fotoramaAddVideoEvents: 'Magento_ProductVideo/js/fotorama-add-video-events',
+            loadPlayer: 'Magento_ProductVideo/js/load-player'
         }
     }
 };
