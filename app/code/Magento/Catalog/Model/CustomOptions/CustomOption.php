a3621b460ae951ee6387b835e874655fa4fe6eda
Oleksandr Karpenko <okarpenko@ebay.com>
2015-10-06 16:39:51 +0300

    MAGETWO-43174: Support product option in Quote API

--- app/code/Magento/Catalog/Model/CustomOptions/CustomOption.php
+++ app/code/Magento/Catalog/Model/CustomOptions/CustomOption.php
@@ -7,10 +7,48 @@ namespace Magento\Catalog\Model\CustomOptions;
 
 use Magento\Framework\Model\AbstractExtensibleModel;
 use Magento\Catalog\Api\Data\CustomOptionInterface;
+use Magento\Framework\Api\AttributeValueFactory;
+use Magento\Framework\Model\Context;
+use Magento\Framework\Registry;
+use Magento\Framework\Api\ExtensionAttributesFactory;
+use Magento\Framework\Model\Resource\AbstractResource;
+use Magento\Framework\Data\Collection\AbstractDb;
+use Magento\Catalog\Model\Webapi\Product\Option\Type\File\Processor as FileProcessor;
 
-//@codeCoverageIgnoreStart
 class CustomOption extends AbstractExtensibleModel implements CustomOptionInterface
 {
+    /**
+     * @param Context $context
+     * @param Registry $registry
+     * @param ExtensionAttributesFactory $extensionFactory
+     * @param AttributeValueFactory $customAttributeFactory
+     * @param AbstractResource|null $resource
+     * @param AbstractDb|null $resourceCollection
+     * @param FileProcessor $fileProcessor
+     * @param array $data
+     */
+    public function __construct(
+        Context $context,
+        Registry $registry,
+        ExtensionAttributesFactory $extensionFactory,
+        AttributeValueFactory $customAttributeFactory,
+        FileProcessor $fileProcessor,
+        AbstractResource $resource = null,
+        AbstractDb $resourceCollection = null,
+        array $data = []
+    ) {
+        $this->fileProcessor = $fileProcessor;
+        parent::__construct(
+            $context,
+            $registry,
+            $extensionFactory,
+            $customAttributeFactory,
+            $resource,
+            $resourceCollection,
+            $data
+        );
+    }
+
     /**
      * @inheritDoc
      */
@@ -32,7 +70,17 @@ class CustomOption extends AbstractExtensibleModel implements CustomOptionInterf
      */
     public function getOptionValue()
     {
-        return $this->getData(self::OPTION_VALUE);
+        $value =  $this->getData(self::OPTION_VALUE);
+        if ($value == 'file') {
+            /** @var \Magento\Framework\Api\Data\ImageContentInterface $fileInfo */
+            $imageContent = $this->getExtensionAttributes()
+                ? $this->getExtensionAttributes()->getFileInfo()
+                : null;
+            if ($imageContent) {
+                $value = $this->fileProcessor->processFileContent($imageContent);
+            }
+        }
+        return $value;
     }
 
     /**
