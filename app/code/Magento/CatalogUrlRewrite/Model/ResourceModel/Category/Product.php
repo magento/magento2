bed1abc3e5aa9b541ad2cd6598817c726eba11d7
Serhiy Shkolyarenko <sshkolyarenko@ebay.com>
2015-10-06 17:39:03 +0300

    MAGETWO-41527: PHP 7 namespace strict compatibility
    - Updated Magento_CatalogUrlRewrite module for PHP 7 compatibility

--- app/code/Magento/CatalogUrlRewrite/Model/ResourceModel/Category/Product.php
+++ app/code/Magento/CatalogUrlRewrite/Model/ResourceModel/Category/Product.php
@@ -3,7 +3,7 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\CatalogUrlRewrite\Model\Resource\Category;
+namespace Magento\CatalogUrlRewrite\Model\ResourceModel\Category;
 
 use Magento\Framework\Model\Resource\Db\AbstractDb;
 
