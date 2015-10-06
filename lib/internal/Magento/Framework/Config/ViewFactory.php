41d71b2d0b9cc28e8a5964cf354824d9be11e5eb
Ivan Gavryshko <igavryshko@ebay.com>
2015-10-06 18:42:49 -0500

    MAGETWO-43452: Ogre sprint 38 contribution to mainline

--- lib/internal/Magento/Framework/Config/ViewFactory.php
+++ lib/internal/Magento/Framework/Config/ViewFactory.php
@@ -13,6 +13,11 @@ class ViewFactory
      */
     private $objectManager;
 
+    /**
+     * Constructor
+     *
+     * @param \Magento\Framework\ObjectManagerInterface $objectManager
+     */
     public function __construct(\Magento\Framework\ObjectManagerInterface $objectManager)
     {
         $this->objectManager = $objectManager;
