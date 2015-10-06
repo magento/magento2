16fa1dc7496ea3fc2d39661b6e404b31ee9875ee
Maddy Chellathurai <mchellathura@ebay.com>
2015-10-06 16:15:11 -0500

    MAGETWO-43452: Ogre sprint 38 contribution to mainline
    - fixing move of i18n language

--- app/i18n/magento/zh_hans_cn/registration.php
+++ app/i18n/magento/zh_hans_cn/registration.php
@@ -6,6 +6,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
-    'magento_zh_cn',
+    'magento_zh_hans_cn',
     __DIR__
 );
