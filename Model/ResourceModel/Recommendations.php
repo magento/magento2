618435146e9067ffc01b22d4314aaeda5983ca5e
Serhiy Shkolyarenko <sshkolyarenko@ebay.com>
2015-10-06 17:38:15 +0300

    MAGETWO-41527: PHP 7 namespace strict compatibility
    - Updated Magento_AdvancedSearch module for PHP 7 compatibility

--- Model/ResourceModel/Recommendations.php
+++ Model/ResourceModel/Recommendations.php
@@ -3,7 +3,7 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\AdvancedSearch\Model\Resource;
+namespace Magento\AdvancedSearch\Model\ResourceModel;
 
 use Magento\Framework\Model\Resource\Db\AbstractDb;
 
--- Model/ResourceModel/Search/Grid/Collection.php
+++ Model/ResourceModel/Search/Grid/Collection.php
@@ -3,7 +3,7 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\AdvancedSearch\Model\Resource\Search\Grid;
+namespace Magento\AdvancedSearch\Model\ResourceModel\Search\Grid;
 
 class Collection extends \Magento\Search\Model\Resource\Query\Collection
 {
