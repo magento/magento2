28fdd5b27de46d26090377ad6eff5d7805c8b6fa
Serhiy Shkolyarenko <sshkolyarenko@ebay.com>
2015-10-06 17:38:40 +0300

    MAGETWO-41527: PHP 7 namespace strict compatibility
    - Updated Magento_Captcha module for PHP 7 compatibility

--- app/code/Magento/Captcha/Model/ResourceModel/Log.php
+++ app/code/Magento/Captcha/Model/ResourceModel/Log.php
@@ -3,7 +3,7 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\Captcha\Model\Resource;
+namespace Magento\Captcha\Model\ResourceModel;
 
 /**
  * Log Attempts resource
