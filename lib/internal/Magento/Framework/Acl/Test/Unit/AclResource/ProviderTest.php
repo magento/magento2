c7987b8ce1707a51c5019c1ad591e7ed4c4614cb
Serhiy Shkolyarenko <sshkolyarenko@ebay.com>
2015-10-07 16:55:07 +0300

    MAGETWO-41527: PHP 7 namespace strict compatibility
    renamed acl tests

--- lib/internal/Magento/Framework/Acl/Test/Unit/AclResource/ProviderTest.php
+++ lib/internal/Magento/Framework/Acl/Test/Unit/AclResource/ProviderTest.php
@@ -3,7 +3,7 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\Framework\Acl\Test\Unit\Resource;
+namespace Magento\Framework\Acl\Test\Unit\AclResource;
 
 class ProviderTest extends \PHPUnit_Framework_TestCase
 {
