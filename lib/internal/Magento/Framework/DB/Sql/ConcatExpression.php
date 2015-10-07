dc9568432f0480e2b78572b8dffb0e381999353f
Serhiy Shkolyarenko <sshkolyarenko@ebay.com>
2015-10-07 18:33:10 +0300

    MAGETWO-41527: PHP 7 namespace strict compatibility
    fixed resource entry

--- lib/internal/Magento/Framework/DB/Sql/ConcatExpression.php
+++ lib/internal/Magento/Framework/DB/Sql/ConcatExpression.php
@@ -29,12 +29,12 @@ class ConcatExpression extends \Zend_Db_Expr
     protected $separator;
 
     /**
-     * @param Resource $resource
+     * @param ResourceConnection $resource
      * @param array $columns
      * @param string $separator
      */
     public function __construct(
-        Resource $resource,
+        ResourceConnection $resource,
         array $columns,
         $separator = ' '
     ) {
