43a3e64ea48b13781bc00f2fca26816c26043811
Sergey Semenov <ssemenov@ebay.com>
2015-10-12 01:36:28 +0300

    MAGETWO-43701: [TD] API tests for support of custom options in Quote and Sales API

---
+++ dev/tests/integration/testsuite/Magento/ConfigurableProduct/_files/order_item_with_configurable_and_options.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+require 'product_configurable.php';
+
+$objectManager = \Magento\TestFramework\Helper\Bootstrap::getObjectManager();
+
+$addressData = include __DIR__ . '/../../../Magento/Sales/_files/address_data.php';
+
+$billingAddress = $objectManager->create('Magento\Sales\Model\Order\Address', ['data' => $addressData]);
+$billingAddress->setAddressType('billing');
+
+$shippingAddress = clone $billingAddress;
+$shippingAddress->setId(null)->setAddressType('shipping');
+
+$payment = $objectManager->create('Magento\Sales\Model\Order\Payment');
+$payment->setMethod('checkmo');
+
+/** @var $product \Magento\Catalog\Model\Product */
+$product = $objectManager->create('Magento\Catalog\Model\Product');
+$product->load(1);
+
+/** @var $attribute \Magento\Catalog\Model\ResourceModel\Eav\Attribute */
+$eavConfig = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->get('Magento\Eav\Model\Config');
+$attribute = $eavConfig->getAttribute('catalog_product', 'test_configurable');
+
+/** @var $options \Magento\Eav\Model\ResourceModel\Entity\Attribute\Option\Collection */
+$options = $objectManager->create('Magento\Eav\Model\ResourceModel\Entity\Attribute\Option\Collection');
+$option = $options->setAttributeFilter($attribute->getId())
+    ->getFirstItem();
+
+$requestInfo = [
+    'qty' => 1,
+    'super_attribute' => [
+        $attribute->getId() => $option->getId(),
+    ],
+];
+
+/** @var \Magento\Sales\Model\Order\Item $orderItem */
+$orderItem = $objectManager->create('Magento\Sales\Model\Order\Item');
+$orderItem->setProductId($product->getId());
+$orderItem->setQtyOrdered(1);
+$orderItem->setBasePrice($product->getPrice());
+$orderItem->setPrice($product->getPrice());
+$orderItem->setRowTotal($product->getPrice());
+$orderItem->setProductType($product->getTypeId());
+$orderItem->setProductOptions(['info_buyRequest' => $requestInfo]);
+
+/** @var \Magento\Sales\Model\Order $order */
+$order = $objectManager->create('Magento\Sales\Model\Order');
+$order->setIncrementId('100000001');
+$order->setState(\Magento\Sales\Model\Order::STATE_NEW);
+$order->setStatus($order->getConfig()->getStateDefaultStatus(\Magento\Sales\Model\Order::STATE_NEW));
+$order->setCustomerIsGuest(true);
+$order->setCustomerEmail('customer@null.com');
+$order->setCustomerFirstname('firstname');
+$order->setCustomerLastname('lastname');
+$order->setBillingAddress($billingAddress);
+$order->setShippingAddress($shippingAddress);
+$order->setAddresses([$billingAddress, $shippingAddress]);
+$order->setPayment($payment);
+$order->addItem($orderItem);
+$order->setStoreId($objectManager->get('Magento\Store\Model\StoreManagerInterface')->getStore()->getId());
+$order->setSubtotal(100);
+$order->setBaseSubtotal(100);
+$order->setBaseGrandTotal(100);
+$order->save();
