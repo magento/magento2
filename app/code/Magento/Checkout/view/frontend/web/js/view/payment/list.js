41447ec4cf0129c9150a2b4fc518bc63498fd5d9
Iryna Lagno <ilagno@ebay.com>
2015-10-15 18:12:35 +0300

    MAGETWO-43992: Can't select payment method for virtual product if payment is available for specific countries

--- app/code/Magento/Checkout/view/frontend/web/js/view/payment/list.js
+++ app/code/Magento/Checkout/view/frontend/web/js/view/payment/list.js
@@ -30,11 +30,16 @@ define([
             paymentMethods.subscribe(
                 function (changes) {
                     checkoutDataResolver.resolvePaymentMethod();
+                    //remove renderer for "deleted" payment methods
+                    _.each(changes, function (change) {
+                        if (change.status === 'deleted') {
+                            this.removeRenderer(change.value.method);
+                        }
+                    }, this);
+                    //add renderer for "added" payment methods
                     _.each(changes, function (change) {
                         if (change.status === 'added') {
                             this.createRenderer(change.value);
-                        } else if (change.status === 'deleted') {
-                            this.removeRenderer(change.value.method);
                         }
                     }, this);
                 }, this, 'arrayChange');
