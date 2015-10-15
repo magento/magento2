13a03f5ddebad9a9e16b4361038030495504e72e
Hayder Sharhan <hsharhan@ebay.com>
2015-10-15 18:33:19 -0500

    MAGETWO-43935: Translation in html files using $t() broken
    - Added changes to dataProvider class from develop branch. Added updates up to commit 22e5c30130f9eac9ac0b5192b45e98a3761ed2f2 in develop branch of mainline.

--- app/code/Magento/Translation/Test/Unit/Model/Js/DataProviderTest.php
+++ app/code/Magento/Translation/Test/Unit/Model/Js/DataProviderTest.php
@@ -12,6 +12,7 @@ use Magento\Framework\Filesystem\Directory\ReadInterface;
 use Magento\Framework\App\Filesystem\DirectoryList;
 use Magento\Translation\Model\Js\DataProvider;
 use Magento\Translation\Model\Js\Config;
+use Magento\Framework\Phrase\Renderer\Translate;
 
 /**
  * Class DataProviderTest
@@ -44,6 +45,11 @@ class DataProviderTest extends \PHPUnit_Framework_TestCase
     protected $rootDirectoryMock;
 
     /**
+     * @var Translate|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $translateMock;
+
+    /**
      * @return void
      */
     protected function setUp()
@@ -53,15 +59,21 @@ class DataProviderTest extends \PHPUnit_Framework_TestCase
         $this->filesUtilityMock = $this->getMock('Magento\Framework\App\Utility\Files', [], [], '', false);
         $filesystem = $this->getMock('Magento\Framework\Filesystem', [], [], '', false);
         $this->rootDirectoryMock = $this->getMock('Magento\Framework\Filesystem\Directory\Read', [], [], '', false);
+        $this->translateMock = $this->getMock('Magento\Framework\Phrase\Renderer\Translate', [], [], '', false);
         $filesystem->expects($this->once())
             ->method('getDirectoryRead')
             ->with(DirectoryList::ROOT)
             ->willReturn($this->rootDirectoryMock);
-        $this->model = new DataProvider(
-            $this->appStateMock,
-            $this->configMock,
-            $filesystem,
-            $this->filesUtilityMock
+        $objectManager = new \Magento\Framework\TestFramework\Unit\Helper\ObjectManager($this);
+        $this->model = $objectManager->getObject(
+            'Magento\Translation\Model\Js\DataProvider',
+            [
+                'appState' => $this->appStateMock,
+                'config' => $this->configMock,
+                'filesystem' => $filesystem,
+                'filesUtility' => $this->filesUtilityMock,
+                'translate' => $this->translateMock
+            ]
         );
     }
 
@@ -72,15 +84,19 @@ class DataProviderTest extends \PHPUnit_Framework_TestCase
     {
         $themePath = 'blank';
         $areaCode = 'adminhtml';
-        $files = [['path1'], ['path2']];
+        $files = [['path1'], ['path2'], ['path3'], ['path4']];
 
         $relativePathMap = [
             ['path1' => 'relativePath1'],
-            ['path2' => 'relativePath2']
+            ['path2' => 'relativePath2'],
+            ['path3' => 'relativePath3'],
+            ['path4' => 'relativePath4']
         ];
         $contentsMap = [
             ['relativePath1' => 'content1$.mage.__("hello1")content1'],
-            ['relativePath2' => 'content2$.mage.__("hello2")content2']
+            ['relativePath2' => 'content2$.mage.__("hello2")content2'],
+            ['relativePath3' => 'content2$.mage.__("hello3")content3'],
+            ['relativePath4' => 'content2$.mage.__("hello4")content4']
         ];
 
         $patterns = ['~\$\.mage\.__\([\'|\"](.+?)[\'|\"]\)~'];
@@ -88,10 +104,22 @@ class DataProviderTest extends \PHPUnit_Framework_TestCase
         $this->appStateMock->expects($this->once())
             ->method('getAreaCode')
             ->willReturn($areaCode);
-        $this->filesUtilityMock->expects($this->once())
+        $this->filesUtilityMock->expects($this->at(0))
+            ->method('getJsFiles')
+            ->with('base', $themePath)
+            ->willReturn([$files[0]]);
+        $this->filesUtilityMock->expects($this->at(1))
             ->method('getJsFiles')
             ->with($areaCode, $themePath)
-            ->willReturn($files);
+            ->willReturn([$files[1]]);
+        $this->filesUtilityMock->expects($this->at(2))
+            ->method('getStaticHtmlFiles')
+            ->with('base', $themePath)
+            ->willReturn([$files[2]]);
+        $this->filesUtilityMock->expects($this->at(3))
+            ->method('getStaticHtmlFiles')
+            ->with($areaCode, $themePath)
+            ->willReturn([$files[3]]);
 
         $this->rootDirectoryMock->expects($this->any())
             ->method('getRelativePath')
