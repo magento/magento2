69037c56331c7596e51114a8472e5c4e3984e5ac
Misha M.-Kupriyanov <m.kupriyanov@gmail.com>
2015-10-15 22:12:10 +0200

    fixed cutting off long oauth callback urls for oauth clients like google drive apps script by increasing size of callback_url, rejected_callback_url columns from 255 to 512 chars

--- app/code/Magento/Integration/Setup/InstallSchema.php
+++ app/code/Magento/Integration/Setup/InstallSchema.php
@@ -68,13 +68,13 @@ class InstallSchema implements InstallSchemaInterface
         )->addColumn(
             'callback_url',
             \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-            255,
+            512,
             [],
             'Callback URL'
         )->addColumn(
             'rejected_callback_url',
             \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-            255,
+            512,
             ['nullable' => false],
             'Rejected callback URL'
         )->addIndex(
@@ -163,7 +163,7 @@ class InstallSchema implements InstallSchemaInterface
         )->addColumn(
             'callback_url',
             \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-            255,
+            512,
             ['nullable' => false],
             'Token Callback URL'
         )->addColumn(
