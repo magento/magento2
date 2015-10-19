8b2c2b527b0e6beba4a2eb2b96405aea723bc5f5
Zaets Volodymyr <vzaets@ebay.com>
2015-10-19 19:33:38 +0300

    MAGETWO-43767: Can't create product with enabled javascript bundling
    - fix code style

--- app/code/Magento/ProductVideo/view/adminhtml/web/js/video-modal.js
+++ app/code/Magento/ProductVideo/view/adminhtml/web/js/video-modal.js
@@ -15,44 +15,43 @@ define([
 
     $.widget('mage.productGallery', productGallery, {
 
-            /**
-             * Fired when windget initialization start
-             * @private
-             */
-            _create: function () {
-                this._bind();
-            },
+        /**
+         * * Fired when widget initialization start
+         * @private
+         */
+        _create: function () {
+            this._bind();
+        },
 
-            /**
-             * Bind events
-             * @private
-             */
-            _bind: function () {
-                $(this.element).on('click', this.showModal.bind(this));
-                $('.gallery.ui-sortable').on('openDialog', $.proxy(this._onOpenDialog, this));
-            },
+        /**
+         * Bind events
+         * @private
+         */
+        _bind: function () {
+            $(this.element).on('click', this.showModal.bind(this));
+            $('.gallery.ui-sortable').on('openDialog', $.proxy(this._onOpenDialog, this));
+        },
 
-            /**
-             * Open dialog for external video
-             * @private
-             */
-            _onOpenDialog: function (e, imageData) {
+        /**
+         * Open dialog for external video
+         * @private
+         */
+        _onOpenDialog: function (e, imageData) {
 
-                if (imageData['media_type'] !== 'external-video') {
-                    return;
-                }
-                this.showModal();
-            },
-
-            /**
-             * Fired on trigger "openModal"
-             */
-            showModal: function () {
-
-                $('#new-video').modal('openModal');
+            if (imageData['media_type'] !== 'external-video') {
+                return;
             }
+            this.showModal();
+        },
+
+        /**
+         * Fired on trigger "openModal"
+         */
+        showModal: function () {
+
+            $('#new-video').modal('openModal');
         }
-    );
+    });
 
     return $.mage.productGallery;
 });
