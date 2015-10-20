5fe41a9590fe141696cee58c96e1c9d8618a86bd
Ievgen Sentiabov <isentiabov@ebay.com>
2015-10-20 15:00:28 +0300

    MAGETWO-44113: Creating Ratings in the Frontend (not available after Magento installation)
     - Fixed namespace

--- app/code/Magento/Review/Test/Unit/Model/RatingTest.php
+++ app/code/Magento/Review/Test/Unit/Model/RatingTest.php
@@ -3,7 +3,7 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\Review\Model;
+namespace Magento\Review\Test\Unit\Model;
 
 use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
 use Magento\Store\Model\Store;
