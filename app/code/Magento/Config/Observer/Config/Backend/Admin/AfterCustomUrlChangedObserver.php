36592308e2e923d96e308da77f4e6ec297a36426
Paul Lewis <plewis@ebay.com>
2015-10-22 09:56:02 -0500

    MAGETWO-43644: Redirect after setting Custom Admin URL goes to frontend
    - Ignore unused formal param warning (needed for signature), remove unused class var

--- app/code/Magento/Config/Observer/Config/Backend/Admin/AfterCustomUrlChangedObserver.php
+++ app/code/Magento/Config/Observer/Config/Backend/Admin/AfterCustomUrlChangedObserver.php
@@ -34,37 +34,30 @@ class AfterCustomUrlChangedObserver implements ObserverInterface
     protected $_response;
 
     /**
-     * @var \Magento\Store\Model\StoreManagerInterface
-     */
-    protected $_storeManager;
-
-    /**
      * @param \Magento\Backend\Helper\Data $backendData
      * @param \Magento\Framework\Registry $coreRegistry
      * @param \Magento\Backend\Model\Auth\Session $authSession
      * @param \Magento\Framework\App\ResponseInterface $response
-     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
      */
     public function __construct(
         \Magento\Backend\Helper\Data $backendData,
         \Magento\Framework\Registry $coreRegistry,
         \Magento\Backend\Model\Auth\Session $authSession,
-        \Magento\Framework\App\ResponseInterface $response,
-        \Magento\Store\Model\StoreManagerInterface $storeManager
+        \Magento\Framework\App\ResponseInterface $response
     ) {
         $this->_backendData = $backendData;
         $this->_coreRegistry = $coreRegistry;
         $this->_authSession = $authSession;
         $this->_response = $response;
-        $this->_storeManager = $storeManager;
     }
 
     /**
-     * Log out user and redirect him to new admin custom url
+     * Log out user and redirect to new admin custom url
      *
      * @param \Magento\Framework\Event\Observer $observer
      * @return void
      * @SuppressWarnings(PHPMD.ExitExpression)
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
