ca48d029a2435beff1fa650db5a1ada9fe4f4195
Vadim Zubovich <vadim_zubovich@epam.com>
2015-10-23 18:12:31 +0300

    MAGETWO-44274: The issue fixed

--- app/code/Magento/ProductVideo/Model/Plugin/ExternalVideoEntryProcessor.php
+++ app/code/Magento/ProductVideo/Model/Plugin/ExternalVideoEntryProcessor.php
@@ -42,12 +42,19 @@ class ExternalVideoEntryProcessor
     protected $resourceEntryMediaGallery;
 
     /**
+     * @var \Magento\ProductVideo\Model\ResourceModel\Video
+     */
+    protected $videoResourceModel;
+
+    /**
      * @param \Magento\Catalog\Model\ResourceModel\Product\Attribute\Backend\Media $resourceEntryMediaGallery
      */
     public function __construct(
-        \Magento\Catalog\Model\ResourceModel\Product\Attribute\Backend\Media $resourceEntryMediaGallery
+        \Magento\Catalog\Model\ResourceModel\Product\Attribute\Backend\Media $resourceEntryMediaGallery,
+        \Magento\ProductVideo\Model\ResourceModel\Video $videoResourceModel
     ) {
         $this->resourceEntryMediaGallery = $resourceEntryMediaGallery;
+        $this->videoResourceModel = $videoResourceModel;
     }
 
     /**
@@ -141,8 +148,7 @@ class ExternalVideoEntryProcessor
      */
     protected function saveVideoValuesItem(array $item)
     {
-        $this->resourceEntryMediaGallery->saveDataRow(
-            InstallSchema::GALLERY_VALUE_VIDEO_TABLE,
+        $this->videoResourceModel->insertOnDuplicate(
             $this->prepareVideoRowDataForSave($item)
         );
     }
