432e15c8805801206446cfb320bf816d05f694a5
Partica, Cristian <cpartica@ebay.com>
2015-10-24 20:45:30 -0500

    MAGETWO-44290: Translations.json file is not cached on client side
    - added client local storage functionality to store the js-translation.json and parametrizing the timestamp of that file for changes and reload

--- app/code/Magento/Translation/Model/FileManager.php
+++ app/code/Magento/Translation/Model/FileManager.php
@@ -5,6 +5,7 @@
  */
 namespace Magento\Translation\Model;
 
+use Magento\Framework\App\Filesystem\DirectoryList;
 /**
  * A service for handling Translation config files
  */
@@ -15,18 +16,28 @@ class FileManager
      */
     const TRANSLATION_CONFIG_FILE_NAME = 'Magento_Translation/js/i18n-config.js';
 
-    /**
-     * @var \Magento\Framework\View\Asset\Repository
-     */
+    /** @var \Magento\Framework\View\Asset\Repository */
     private $assetRepo;
+
+    /** @var \Magento\Framework\App\Filesystem\DirectoryList */
+    private $directoryList;
+
+    /** @var \Magento\Framework\Filesystem\Driver\File */
+    private $driverFile;
 
     /**
      * @param \Magento\Framework\View\Asset\Repository $assetRepo
+     * @param \Magento\Framework\App\Filesystem\DirectoryList $directoryList,
+     * @param \Magento\Framework\Filesystem\Driver\File $driverFile,
      */
     public function __construct(
-        \Magento\Framework\View\Asset\Repository $assetRepo
+        \Magento\Framework\View\Asset\Repository $assetRepo,
+        \Magento\Framework\App\Filesystem\DirectoryList $directoryList,
+        \Magento\Framework\Filesystem\Driver\File $driverFile
     ) {
         $this->assetRepo = $assetRepo;
+        $this->directoryList = $directoryList;
+        $this->driverFile = $driverFile;
     }
 
     /**
@@ -40,5 +51,39 @@ class FileManager
             $this->assetRepo->getStaticViewFileContext()->getPath() . '/' . self::TRANSLATION_CONFIG_FILE_NAME,
             ''
         );
+    }
+
+    /**
+     * gets current js-translation.json timestamp
+     *
+     * @return string|null
+     */
+    public function getJsonTimestamp()
+    {
+        if ($this->driverFile->isExists($this->getTranslationFileFullPath())) {
+            return filemtime($this->getTranslationFileFullPath());
+        }
+        return null;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTranslationFileFullPath()
+    {
+        return $this->directoryList->getPath(DirectoryList::STATIC_VIEW) .
+        \DIRECTORY_SEPARATOR .
+        $this->assetRepo->getStaticViewFileContext()->getPath() .
+        \DIRECTORY_SEPARATOR .
+        Js\Config::DICTIONARY_FILE_NAME;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTranslationPath()
+    {
+        return $this->assetRepo->getStaticViewFileContext()->getPath();
+
     }
 }
--- app/code/Magento/Translation/view/base/web/js/add-to-translate.js
+++ app/code/Magento/Translation/view/base/web/js/add-to-translate.js
@@ -5,15 +5,27 @@
 
 define([
     'jquery',
-    'mage/translate'
+    'mage/translate',
+    'jquery/jquery-storageapi'
 ], function ($) {
     'use strict';
 
     return function (config) {
-        require([config.config], function (string) {
-            if (string.length) {
-                $.mage.translate.add(JSON.parse(string));
-            }
-        });
+        var translationStorage = $.initNamespaceStorage('mage-translation-storage').localStorage;
+        var translationStorageInvalidation = $.initNamespaceStorage('mage-translation-timeout').localStorage;
+        var timeout = $.localStorage.get('mage-translation-timeout');
+        if ((timeout.timestamp != config.timestamp) || (!timeout.timestamp)) {
+            require([config.config], function (string) {
+                if (string.length) {
+                    $.mage.translate.add(JSON.parse(string));
+                    translationStorage.removeAll();
+                    translationStorageInvalidation.removeAll();
+                    $.localStorage.set('mage-translation-storage', string);
+                    $.localStorage.set('mage-translation-timeout', {timestamp: config.timestamp});
+                }
+            });
+        } else {
+            $.mage.translate.add($.localStorage.get('mage-translation-storage'));
+        }
     };
 });
