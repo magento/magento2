a80f3e9ec2645b2d573255e93b5d8afc75308f80
David Alger <david@classyllama.com>
2015-10-27 16:50:59 -0500

    Update registration files formatting

--- app/i18n/magento/de_de/registration.php
+++ app/i18n/magento/de_de/registration.php
@@ -4,8 +4,6 @@
  * See COPYING.txt for license details.
  */
 
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
-    'magento_de_de',
-    __DIR__
-);
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::LANGUAGE, 'magento_de_de', __DIR__);
