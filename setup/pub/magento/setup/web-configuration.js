a5225286f0301be20ba02d3b786d204873c19c02
Maddy Chellathurai <mchellathura@ebay.com>
2015-10-27 11:52:59 -0500

    MAGETWO-29463: Missing installation features of the new setup wizard - Save Session Data
    - Removing redis from web setup wizard

--- setup/pub/magento/setup/web-configuration.js
+++ setup/pub/magento/setup/web-configuration.js
@@ -119,36 +119,4 @@ angular.module('web-configuration', ['ngStorage'])
                 $scope.webconfig.submitted = false;
             }
         });
-    }])
-    .factory('redisFactory', function($http) {
-        var hasRedisExtension = function() {
-            return $http.get('index.php/web-configuration/has-redis').success(function (data) {
-                return data;
-            })
-        }
-        return { hasRedisExtension: hasRedisExtension }
-    })
-    .directive('checkRedis', ['redisFactory', function (redisFactory) {
-    return {
-        restrict: 'A',
-        require: 'ngModel',
-        link: function (scope, element, attrs, ngModel) {
-            var validator = function(value){
-                if (value === 'redis') {
-                    var ajaxPromise = redisFactory.hasRedisExtension();
-                    ajaxPromise.then(function(result) {
-                        ngModel.$setValidity('checkRedis', result.data.hasRedis);
-                        scope.validate();
-                    })
-                } else {
-                    ngModel.$setValidity('checkRedis', true);
-                    scope.validate();
-                }
-                return value;
-            };
-
-            ngModel.$parsers.unshift(validator);
-            ngModel.$formatters.unshift(validator);
-        }
-    };
-}]);
+    }]);
