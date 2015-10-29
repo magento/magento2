13657728e724751d9d960b1f4237cf3551af6058
Maxim Medinskiy <mmedinskiy@ebay.com>
2015-10-29 02:08:07 +0200

    MAGETWO-44378: Customer Sign In Fail

--- app/code/Magento/Customer/Model/Checkout/ConfigProvider.php
+++ app/code/Magento/Customer/Model/Checkout/ConfigProvider.php
@@ -3,7 +3,7 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\CUstomer\Model\Checkout;
+namespace Magento\Customer\Model\Checkout;
 
 use Magento\Checkout\Model\ConfigProviderInterface;
 use Magento\Customer\Model\Url;
