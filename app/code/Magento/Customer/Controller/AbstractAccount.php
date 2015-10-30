7fc5e4215e0ddc590277d65dffb82dd4485543f5
Yu Tang <ytang1@ebay.com>
2015-10-29 20:30:26 -0500

    MAGETWO-44491: [TD] ActionInterface has Method dispatch() Instead of execute()
        - Fix static test failure

--- app/code/Magento/Customer/Controller/AbstractAccount.php
+++ app/code/Magento/Customer/Controller/AbstractAccount.php
@@ -11,6 +11,7 @@ use Magento\Framework\App\Action\Action;
 /**
  * Class AbstractAccount
  * @package Magento\Customer\Controller
+ * @SuppressWarnings(PHPMD.NumberOfChildren)
  */
 abstract class AbstractAccount extends Action implements AccountInterface
 {
