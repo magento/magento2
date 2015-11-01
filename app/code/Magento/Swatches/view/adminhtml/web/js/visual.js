a0099ffec37863a08366c01be122749a9ca15ee9
Alex Bomko <abomko@ebay.com>
2015-11-01 14:39:22 +0200

    MAGETWO-44704: Swatches attributes: empty value is missing, although "Values Required" = No

--- app/code/Magento/Swatches/view/adminhtml/web/js/visual.js
+++ app/code/Magento/Swatches/view/adminhtml/web/js/visual.js
@@ -26,7 +26,7 @@ define([
                 template: mageTemplate('#swatch-visual-row-template'),
                 add: function (data, render) {
                     var isNewOption = false,
-                        element, visibleRadio;
+                        element;
 
                     if (typeof data.id == 'undefined') {
                         data = {
