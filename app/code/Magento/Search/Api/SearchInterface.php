556cf8aca222d39ad91af21192168aad77abf83f
Anton Ohorodnyk <aohorodnyk@ebay.com>
2015-11-02 13:21:28 +0200

    MAGETWO-44333: Move Search API to Framework to Make Grid Component work through them
     - Added empty classes for webapi

---
+++ app/code/Magento/Search/Api/SearchInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Search\Api;
+
+use Magento\Framework\Api\Search\SearchInterface as FrameworkSearchInterface;
+
+/**
+ * Search API for all requests
+ */
+interface SearchInterface extends FrameworkSearchInterface
+{
+}
---
+++ app/code/Magento/Search/Model/Search.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Search\Model;
+
+use Magento\Framework\Search\Search as FrameworkSearch;
+use Magento\Search\Api\SearchInterface;
+
+/**
+ * Search extended implementation for WebApi
+ */
+class Search extends FrameworkSearch implements SearchInterface
+{
+}
