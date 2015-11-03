73ae09142e785911307a6ebbc3ab0df3a2c7421b
Maddy Chellathurai <mchellathura@ebay.com>
2015-11-03 13:56:40 -0600

    MAGETWO-44725: Broken sorting on "Component manager" page
    - CR fixed to remove unused displayComponents

--- setup/pub/magento/setup/component-grid.js
+++ setup/pub/magento/setup/component-grid.js
@@ -10,7 +10,6 @@ angular.module('component-grid', ['ngStorage'])
             $rootScope.componentsProcessed = false;
             $http.get('index.php/componentGrid/components').success(function(data) {
                 $scope.components = data.components;
-                $scope.displayComponents = data.components;
                 $scope.total = data.total;
                 if(typeof data.lastSyncData.lastSyncDate === "undefined") {
                     $scope.isOutOfSync = true;
@@ -35,9 +34,6 @@ angular.module('component-grid', ['ngStorage'])
                 var begin = (($scope.currentPage - 1) * $scope.rowLimit);
                 var end = parseInt(begin) + parseInt(($scope.rowLimit));
                 $scope.numberOfPages = Math.ceil($scope.total/$scope.rowLimit);
-                if ($scope.components !== undefined) {
-                    $scope.displayComponents = $scope.components.slice(begin, end);
-                }
                 if ($scope.currentPage > $scope.numberOfPages) {
                     $scope.currentPage = $scope.numberOfPages;
                 }
