c89ec6d9d23ca2df3ca374d4296f255ec9085a93
Robert He <rohe@ebay.com>
2015-11-03 14:38:40 -0600

    MAGETWO-44037: FPT description does not consider tax display settings in bundle product options
     -- fix FPT values for options in bundle products

--- app/code/Magento/Weee/Test/Unit/Helper/DataTest.php
+++ app/code/Magento/Weee/Test/Unit/Helper/DataTest.php
@@ -32,6 +32,11 @@ class DataTest extends \PHPUnit_Framework_TestCase
     protected $weeeTax;
 
     /**
+     * @var \Magento\Tax\Helper\Data
+     */
+    protected $taxData;
+
+    /**
      * @var \Magento\Weee\Helper\Data
      */
     protected $helperData;
@@ -44,9 +49,12 @@ class DataTest extends \PHPUnit_Framework_TestCase
         $weeeConfig->expects($this->any())->method('getListPriceDisplayType')->will($this->returnValue(1));
         $this->weeeTax = $this->getMock('Magento\Weee\Model\Tax', [], [], '', false);
         $this->weeeTax->expects($this->any())->method('getWeeeAmount')->will($this->returnValue('11.26'));
+        $this->taxData = $this->getMock('Magento\Tax\Helper\Data', ['getPriceDisplayType', 'priceIncludesTax'],
+            [], '', false);
         $arguments = [
             'weeeConfig' => $weeeConfig,
             'weeeTax' => $this->weeeTax,
+            'taxData' => $this->taxData
         ];
         $helper = new \Magento\Framework\TestFramework\Unit\Helper\ObjectManager($this);
         $this->helperData = $helper->getObject('Magento\Weee\Helper\Data', $arguments);
@@ -156,7 +164,13 @@ class DataTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(self::BASE_TAX_AMOUNT_REFUNDED, $value);
     }
 
-    public function testGetWeeeAttributesForBundle()
+    /**
+     * @dataProvider dataProviderGetWeeeAttributesForBundle
+     * @param int $priceIncludesTax
+     * @param bool $priceDisplay
+     * @param array $expectedAmount
+     */
+    public function testGetWeeeAttributesForBundle($priceDisplay, $priceIncludesTax, $expectedAmount)
     {
         $prodId1 = 1;
         $prodId2 = 2;
@@ -166,22 +180,53 @@ class DataTest extends \PHPUnit_Framework_TestCase
         $weeeObject1 = new \Magento\Framework\DataObject(
             [
                 'code' => $fptCode1,
-                'amount' => '15.0000',
+                'amount' => '15',
+                'amount_excl_tax' => '15.0000',
+                'tax_amount' => '1'
             ]
         );
 
         $weeeObject2 = new \Magento\Framework\DataObject(
             [
                 'code' => $fptCode2,
-                'amount' => '15.0000',
+                'amount' => '10',
+                'amount_excl_tax' => '10.0000',
+                'tax_amount' => '5'
             ]
         );
 
+        $expectedObject1 = new \Magento\Framework\DataObject(
+            [
+                'code' => $fptCode1,
+                'amount' => $expectedAmount[0],
+                'amount_excl_tax' => '15.0000',
+                'tax_amount' => '1'
+            ]
+        );
+
+        $expectedObject2 = new \Magento\Framework\DataObject(
+            [
+                'code' => $fptCode2,
+                'amount' => $expectedAmount[1],
+                'amount_excl_tax' => '10.0000',
+                'tax_amount' => '5'
+            ]
+        );
+
+        $expectedArray = [$prodId1 => [$fptCode1 => $expectedObject1], $prodId2 => [$fptCode2 => $expectedObject2]];
         $testArray = [$prodId1 => [$fptCode1 => $weeeObject1], $prodId2 => [$fptCode2 => $weeeObject2]];
 
         $this->weeeTax->expects($this->any())
             ->method('getProductWeeeAttributes')
             ->will($this->returnValue([$weeeObject1, $weeeObject2]));
+
+        $this->taxData->expects($this->any())
+            ->method('getPriceDisplayType')
+            ->willReturn($priceDisplay);
+
+        $this->taxData->expects($this->any())
+            ->method('priceIncludesTax')
+            ->willReturn($priceIncludesTax);
 
         $productSimple = $this->getMock('\Magento\Catalog\Model\Product\Type\Simple', ['getId'], [], '', false);
 
@@ -228,8 +273,24 @@ class DataTest extends \PHPUnit_Framework_TestCase
             ->method('registry')
             ->with('current_product')
             ->will($this->returnValue($product));
-        
-        $this->assertEquals($testArray, $this->helperData->getWeeeAttributesForBundle($product));
+
+        $result =  $this->helperData->getWeeeAttributesForBundle($product);
+        $this->assertEquals($expectedArray, $result);
+    }
+
+    /**
+     * @return array
+     */
+    public function dataProviderGetWeeeAttributesForBundle()
+    {
+        return [
+            [2, false, ["16.00", "15.00"]],
+            [2, true, ["15.00", "10.00"]],
+            [1, false, ["15.00", "10.00"]],
+            [1, true, ["15.00", "10.00"]],
+            [3, false, ["16.00", "15.00"]],
+            [3, true, ["15.00", "10.00"]],
+        ];
     }
 
     public function testGetAppliedSimple()
