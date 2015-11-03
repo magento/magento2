5e98e6a739c8d2522dd424e99d46c1443df2a173
Andrii Lugovyi <alugovyi@ebay.com>
2015-11-03 15:22:53 +0200

    MAGETWO-44825: Swatches does not update configurable product price
    - refactoring, add undescorejs

--- app/code/Magento/Swatches/view/frontend/web/js/SwatchRenderer.js
+++ app/code/Magento/Swatches/view/frontend/web/js/SwatchRenderer.js
@@ -2,24 +2,15 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-Number.prototype.formatMoney = function (c, d, t) { //this function helps format price
-    var n = this,
-        c = isNaN(c = Math.abs(c)) ? 2 : c,
-        d = d == undefined ? "." : d,
-        t = t == undefined ? "," : t,
-        s = n < 0 ? "-" : "",
-        i = parseInt(n = Math.abs(+n || 0).toFixed(c)) + "",
-        j = (j = i.length) > 3 ? j % 3 : 0;
-    return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
-};
 
-Array.prototype.intersection = function (a) {
-    return this.filter(function (i) {
-        return a.indexOf(i) >= 0;
-    });
-};
+define(['jquery', 'underscore', 'jquery/ui'], function ($, _) {
+    'use strict';
 
-define(["jquery", "jquery/ui"], function ($) {
+    /**
+     * Parse params
+     * @param {String} query
+     * @returns {{}}
+     */
     $.parseParams = function (query) {
         var re = /([^&=]+)=?([^&]*)/g,
             decodeRE = /\+/g,  // Regex for replacing addition symbol with a space
@@ -564,7 +555,7 @@ define(["jquery", "jquery/ui"], function ($) {
                         return;
                     }
 
-                    if (products.intersection($widget.optionsMap[id][option].products).length <= 0) {
+                    if (_.intersection(products, $widget.optionsMap[id][option].products).length <= 0) {
                         $this.attr('disabled', true).addClass('disabled');
                     }
                 });
@@ -597,7 +588,7 @@ define(["jquery", "jquery/ui"], function ($) {
                 if (products.length == 0) {
                     products = $widget.optionsMap[id][option].products;
                 } else {
-                    products = products.intersection($widget.optionsMap[id][option].products);
+                    products = _.intersection(products, $widget.optionsMap[id][option].products);
                 }
             });
 
