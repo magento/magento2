e72482c9fd7eeaac29f9710b6c39c9eafe5ed0b0
Stanislav Idolov <sidolov@ebay.com>
2015-11-03 12:48:51 +0200

    MAGETWO-44757: JS error on checkout for guest with virtual product

--- app/code/Magento/Checkout/view/frontend/web/js/view/billing-address.js
+++ app/code/Magento/Checkout/view/frontend/web/js/view/billing-address.js
@@ -46,8 +46,9 @@ define(
             },
             countryData = customerData.get('directory-data'),
             addressOptions = addressList().filter(function (address) {
-            return address.getType() == 'customer-address';
-        });
+                return address.getType() == 'customer-address';
+            });
+
         addressOptions.push(newAddressOption);
 
         return Component.extend({
