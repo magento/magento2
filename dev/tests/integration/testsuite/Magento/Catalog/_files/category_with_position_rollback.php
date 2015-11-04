149a9363bb33756115cf3076973064c229860835
Arkadii Chyzhov <achyzhov@ebay.com>
2015-11-04 11:35:24 +0200

    MAGETWO-39663: Product save through API service is not visible on frontend(direct url, category page)
    - fixed product saving process in productRepository for reindexing and applying url rewrite
    - moved to global area event listeners from catalogUrlRewrite
    - moved to global area event listeners from CatalogSearch
    - fixed api-functional tests to use url rewrite via api
    - fixed integration tests to use url rewrite
    - fixed unit tests

--- dev/tests/integration/testsuite/Magento/Catalog/_files/category_with_position_rollback.php
+++ dev/tests/integration/testsuite/Magento/Catalog/_files/category_with_position_rollback.php
@@ -4,25 +4,19 @@
  * See COPYING.txt for license details.
  */
 
-/** @var \Magento\Framework\ObjectManagerInterface $objectManager */
 $objectManager = \Magento\TestFramework\Helper\Bootstrap::getObjectManager();
-
 /** @var \Magento\Framework\Registry $registry */
 $registry = $objectManager->get('Magento\Framework\Registry');
 
 $registry->unregister('isSecureArea');
 $registry->register('isSecureArea', true);
 
-/** @var \Magento\Store\Model\Store $store */
-$store = $objectManager->create('Magento\Store\Model\Store');
-$storeCode = 'fixturestore';
-$store->load($storeCode);
-if ($store->getId()) {
-    $store->delete();
+/** @var $category \Magento\Catalog\Model\Category */
+$category = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create('Magento\Catalog\Model\Category');
+$category->load('444');
+if ($category->getId()) {
+    $category->delete();
 }
 
 $registry->unregister('isSecureArea');
 $registry->register('isSecureArea', false);
-
-/* Refresh stores memory cache */
-$objectManager->get('Magento\Store\Model\StoreManagerInterface')->reinitStores();
--- dev/tests/integration/testsuite/Magento/Catalog/_files/url_rewrites_rollback.php
+++ dev/tests/integration/testsuite/Magento/Catalog/_files/url_rewrites_rollback.php
@@ -4,22 +4,20 @@
  * See COPYING.txt for license details.
  */
 
-$objectManager = \Magento\TestFramework\Helper\Bootstrap::getObjectManager();
 /** @var \Magento\Framework\Registry $registry */
-$registry = $objectManager->get('Magento\Framework\Registry');
+$registry = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->get('Magento\Framework\Registry');
 
 $registry->unregister('isSecureArea');
 $registry->register('isSecureArea', true);
 
-foreach (range(1, 4, 1) as $productId) {
-    /** @var $product \Magento\Catalog\Model\Product */
-    $product = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create('Magento\Catalog\Model\Product');
-    $product->load($productId);
-    if ($product->getId()) {
-        $product->delete();
-    }
+/** @var $product \Magento\Catalog\Model\Product */
+$product = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create('Magento\Catalog\Model\Product');
+$product->load(1);
+if ($product->getId()) {
+    $product->delete();
 }
-foreach (range(12, 3, -1) as $categoryId) {
+
+foreach ([3, 4, 5] as $categoryId) {
     /** @var $category \Magento\Catalog\Model\Category */
     $category = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create('Magento\Catalog\Model\Category');
     $category->load($categoryId);
@@ -27,5 +25,6 @@ foreach (range(12, 3, -1) as $categoryId) {
         $category->delete();
     }
 }
+
 $registry->unregister('isSecureArea');
 $registry->register('isSecureArea', false);
