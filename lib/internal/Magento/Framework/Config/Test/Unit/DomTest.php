89de0698cf60f764514288f8353724bb338ae63d
Maddy Chellathurai <mchellathura@ebay.com>
2015-11-04 23:40:57 -0600

    MAGETWO-44931: [HHVM] Tests of different groups failed to start
    - changing skipped test messages

--- lib/internal/Magento/Framework/Config/Test/Unit/DomTest.php
+++ lib/internal/Magento/Framework/Config/Test/Unit/DomTest.php
@@ -164,7 +164,7 @@ class DomTest extends \PHPUnit_Framework_TestCase
     public function testValidateUnknownError()
     {
         if (!function_exists('libxml_set_external_entity_loader')) {
-            $this->markTestSkipped('Skipped on HHVM');
+            $this->markTestSkipped('Skipped on HHVM. Will be fixed in MAGETWO-45033');
         }
         $xml = '<root><node id="id1"/><node id="id2"/></root>';
         $schemaFile = __DIR__ . '/_files/sample.xsd';
