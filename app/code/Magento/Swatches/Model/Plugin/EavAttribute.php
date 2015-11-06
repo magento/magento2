74975bc6cc00e9473645c800fb0830b5851172f5
okarpenko <okarpenko@ebay.com>
2015-11-06 17:51:06 +0200

    MAGETWO-45073: PHP notice during to create Text Swatch product attribute

--- app/code/Magento/Swatches/Model/Plugin/EavAttribute.php
+++ app/code/Magento/Swatches/Model/Plugin/EavAttribute.php
@@ -81,6 +81,8 @@ class EavAttribute
     }
 
     /**
+     * Validate that attribute options is exists
+     *
      * @param Attribute $attribute
      * @return bool
      * @throws InputException
