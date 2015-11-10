46ecfe99919f445e485386113c9d6e8b3662b921
Hayder Sharhan <hsharhan@ebay.com>
2015-11-10 10:03:25 -0600

    MAGETWO-38930: Add unit test coverage for \Magento\User\Block\Role\*
    - Added unit tests.
    - Removed long variable name suppression.
    - Removed block of commented unused code.

--- app/code/Magento/User/Block/Role/Tab/Edit.php
+++ app/code/Magento/User/Block/Role/Tab/Edit.php
@@ -9,7 +9,6 @@ namespace Magento\User\Block\Role\Tab;
 /**
  * Rolesedit Tab Display Block.
  *
- * @SuppressWarnings(PHPMD.LongVariable)
  */
 class Edit extends \Magento\Backend\Block\Widget\Form implements \Magento\Backend\Block\Widget\Tab\TabInterface
 {
@@ -60,9 +59,9 @@ class Edit extends \Magento\Backend\Block\Widget\Form implements \Magento\Backen
      */
     public function __construct(
         \Magento\Backend\Block\Template\Context $context,
+        \Magento\Authorization\Model\Acl\AclRetriever $aclRetriever,
         \Magento\Framework\Acl\RootResource $rootResource,
         \Magento\Authorization\Model\ResourceModel\Rules\CollectionFactory $rulesCollectionFactory,
-        \Magento\Authorization\Model\Acl\AclRetriever $aclRetriever,
         \Magento\Framework\Acl\AclResource\ProviderInterface $aclResourceProvider,
         \Magento\Integration\Helper\Data $integrationData,
         array $data = []
