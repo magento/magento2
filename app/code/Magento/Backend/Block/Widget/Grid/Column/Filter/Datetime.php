3e7b0e1cdbf8441306f2f743f423dcf6dca70954
Igor Melnikov <imelnikov@ebay.com>
2015-11-12 16:54:05 -0600

    MAGETWO-45274: Date Filter Does not Find Rows with "from" Value
    Fixing code formatting and unit test

--- app/code/Magento/Backend/Block/Widget/Grid/Column/Filter/Datetime.php
+++ app/code/Magento/Backend/Block/Widget/Grid/Column/Filter/Datetime.php
@@ -68,7 +68,8 @@ class Datetime extends \Magento\Backend\Block\Widget\Grid\Column\Filter\Date
     {
         if ($this->getColumn()->getFilterTime()) {
             try {
-                $timezone = $this->getColumn()->getTimezone() !== false ? $this->_localeDate->getConfigTimezone() : 'UTC';
+                $timezone = $this->getColumn()->getTimezone() !== false
+                    ? $this->_localeDate->getConfigTimezone() : 'UTC';
                 $adminTimeZone = new \DateTimeZone($timezone);
                 $simpleRes = new \DateTime($date, $adminTimeZone);
                 $simpleRes->setTimezone(new \DateTimeZone('UTC'));
