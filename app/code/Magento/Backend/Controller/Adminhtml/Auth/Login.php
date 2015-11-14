95d07e34d45210082b98d14c76c5d3026a1715fa
Eddie Lau <kahlau@ebay.com>
2015-11-14 00:09:18 -0600

    MAGETWO-45465: CSRF not validated or regenerated in Magento
    - fixed admin unable to login

--- app/code/Magento/Backend/Controller/Adminhtml/Auth/Login.php
+++ app/code/Magento/Backend/Controller/Adminhtml/Auth/Login.php
@@ -6,12 +6,19 @@
  */
 namespace Magento\Backend\Controller\Adminhtml\Auth;
 
+use Magento\Backend\App\Area\FrontNameResolver;
+
 class Login extends \Magento\Backend\Controller\Adminhtml\Auth
 {
     /**
      * @var \Magento\Framework\View\Result\PageFactory
      */
     protected $resultPageFactory;
+
+    /**
+     * @var FrontNameResolver
+     */
+    protected $frontNameResolver;
 
     /**
      * Constructor
@@ -21,9 +28,11 @@ class Login extends \Magento\Backend\Controller\Adminhtml\Auth
      */
     public function __construct(
         \Magento\Backend\App\Action\Context $context,
-        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory,
+        FrontNameResolver $frontNameResolver
     ) {
         $this->resultPageFactory = $resultPageFactory;
+        $this->frontNameResolver = $frontNameResolver;
         parent::__construct($context);
     }
 
@@ -43,6 +52,14 @@ class Login extends \Magento\Backend\Controller\Adminhtml\Auth
             $resultRedirect->setPath($this->_backendUrl->getStartupPageUrl());
             return $resultRedirect;
         }
-        return $this->resultPageFactory->create();
+
+        $requestUrl = $this->getRequest()->getUri();
+        $backendUrl = $this->getUrl('*');
+        // redirect according to rewrite rule
+        if ($requestUrl != $backendUrl) {
+            $this->_redirect('*');
+        } else {
+            return $this->resultPageFactory->create();
+        }
     }
 }
