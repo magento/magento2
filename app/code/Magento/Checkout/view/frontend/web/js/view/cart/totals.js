394feba36b775c3232057a6aa472d90b37562d0a
Oleksandr Dubovyk <odubovyk@ebay.com>
2015-11-20 10:48:42 +0200

    MAGETWO-44131: Sometimes "Estimate Shipping & Tax" section overlaps Newsletter Subscription form in Shopping Cart

--- app/code/Magento/Checkout/view/frontend/web/js/view/cart/totals.js
+++ app/code/Magento/Checkout/view/frontend/web/js/view/cart/totals.js
@@ -6,14 +6,24 @@
 define(
     [
         'uiComponent',
-        'Magento_Checkout/js/model/totals'
+        'Magento_Checkout/js/model/totals',
+        'Magento_Checkout/js/model/shipping-service'
     ],
-    function (Component, totalsService) {
+    function (Component, totalsService, shippingService) {
         'use strict';
 
         return Component.extend({
 
-            isLoading: totalsService.isLoading
+            isLoading: totalsService.isLoading,
+            initialize: function () {
+                this._super();
+                totalsService.totals.subscribe(function() {
+                    window.dispatchEvent(new Event('resize'));
+                });
+                shippingService.getShippingRates().subscribe(function() {
+                    window.dispatchEvent(new Event('resize'));
+                });
+            }
         });
     }
 );
