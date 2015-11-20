709cf6286df17e9d65e20fa1cd168bf0c32623aa
Maksym Savich <msavich@ebay.com>
2015-11-20 16:44:21 -0600

    MAGETWO-44690: Fix up AppInterface::VERSION so it is not a framework constant

--- setup/src/Magento/Setup/Controller/LandingInstaller.php
+++ setup/src/Magento/Setup/Controller/LandingInstaller.php
@@ -3,7 +3,6 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-
 namespace Magento\Setup\Controller;
 
 use Magento\Framework\AppInterface;
@@ -16,6 +15,18 @@ use Zend\View\Model\ViewModel;
 class LandingInstaller extends AbstractActionController
 {
     /**
+     * @var \Magento\Framework\App\ProductMetadata
+     */
+    protected $productMetadata;
+
+    /**
+     * @param \Magento\Framework\App\ProductMetadata $productMetadata
+     */
+    public function __construct(\Magento\Framework\App\ProductMetadata $productMetadata) {
+        $this->productMetadata = $productMetadata;
+    }
+
+    /**
      * @return array|ViewModel
      */
     public function indexAction()
@@ -27,7 +38,7 @@ class LandingInstaller extends AbstractActionController
         $view = new ViewModel;
         $view->setTerminal(true);
         $view->setTemplate('/magento/setup/landing.phtml');
-        $view->setVariable('version', AppInterface::VERSION);
+        $view->setVariable('version', $this->productMetadata->getVersion());
         $view->setVariable('welcomeMsg', $welcomeMsg);
         $view->setVariable('docRef', $docRef);
         $view->setVariable('agreeButtonText', $agreeButtonText);
