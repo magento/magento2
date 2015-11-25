6090e8a668d1399872c76e58b5eb7bd94d19f24a
Felipe Almeida <felipe@labofidea.com>
2015-11-24 22:21:13 -0200

    Making backend load general locale by default.

--- app/code/Magento/Backend/Model/Locale/Manager.php
+++ app/code/Magento/Backend/Model/Locale/Manager.php
@@ -26,6 +26,11 @@ class Manager
      * @var \Magento\Framework\TranslateInterface
      */
     protected $_translator;
+    
+    /**
+     * @var \Magento\Backend\App\ConfigInterface
+     */
+    protected $_backendConfig;
 
     /**
      * Constructor
@@ -33,15 +38,18 @@ class Manager
      * @param \Magento\Backend\Model\Session $session
      * @param \Magento\Backend\Model\Auth\Session $authSession
      * @param \Magento\Framework\TranslateInterface $translator
+     * @param \Magento\Backend\App\ConfigInterface $backendConfig
      */
     public function __construct(
         \Magento\Backend\Model\Session $session,
         \Magento\Backend\Model\Auth\Session $authSession,
-        \Magento\Framework\TranslateInterface $translator
+        \Magento\Framework\TranslateInterface $translator,
+        \Magento\Backend\App\ConfigInterface $backendConfig            
     ) {
         $this->_session = $session;
         $this->_authSession = $authSession;
         $this->_translator = $translator;
+        $this->_backendConfig = $backendConfig;
     }
 
     /**
@@ -62,19 +70,33 @@ class Manager
     }
 
     /**
+     * Get general interface locale 
+     *
+     * @return string
+     */
+    public function getGeneralLocale()
+    {
+    	return $this->_backendConfig->getValue('general/locale/code');
+    }
+
+    /**
      * Get user interface locale stored in session data
      *
      * @return string
      */
     public function getUserInterfaceLocale()
     {
-        $interfaceLocale = \Magento\Framework\Locale\Resolver::DEFAULT_LOCALE;
-
-        $userData = $this->_authSession->getUser();
+       $userData = $this->_authSession->getUser();
+    	
         if ($userData && $userData->getInterfaceLocale()) {
             $interfaceLocale = $userData->getInterfaceLocale();
-        }
+            }elseif ($this->getGeneralLocale()) {
+                $interfaceLocale = $this->getGeneralLocale();
+            }else {
+                $interfaceLocale = \Magento\Framework\Locale\Resolver::DEFAULT_LOCALE;
+	}
 
         return $interfaceLocale;
+
     }
 }
