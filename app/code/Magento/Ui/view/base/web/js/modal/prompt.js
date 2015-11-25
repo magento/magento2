f4a83020fe7975a96b889988a75392582caa9d76
Novykov Ivan <inovykov@ebay.com>
2015-11-25 14:01:35 +0200

    MDVA-17: Check javascript function calls and labels are translatable
     - stabilize tests

--- app/code/Magento/Ui/view/base/web/js/modal/prompt.js
+++ app/code/Magento/Ui/view/base/web/js/modal/prompt.js
@@ -17,33 +17,65 @@ define([
             modalClass: 'prompt',
             promptField: '[data-role="promptField"]',
             actions: {
+
+                /**
+                 * Callback always - called on all actions.
+                 */
                 always: function () {},
+
+                /**
+                 * Callback confirm.
+                 */
                 confirm: function () {},
+
+                /**
+                 * Callback cancel.
+                 */
                 cancel: function () {}
             },
             buttons: [{
                 text: $.mage.__('Cancel'),
                 class: 'action-tertiary',
+
+                /**
+                 * Click handler.
+                 */
                 click: function () {
                     this.closeModal();
                 }
             }, {
                 text: $.mage.__('OK'),
                 class: 'action-secondary',
+
+                /**
+                 * Click handler.
+                 */
                 click: function () {
                     this.closeModal(true);
                 }
             }]
         },
+
+        /**
+         * Create widget.
+         */
         _create: function () {
             this._super();
             this.modal.find(this.options.modalContent).append('<input value="" data-role="promptField" type="text"/>');
             this.modal.find(this.options.modalCloseBtn).off().on('click',  _.bind(this.closeModal, this, false));
             this.openModal();
         },
+
+        /**
+         * Remove widget
+         */
         _remove: function () {
             this.modal.remove();
         },
+
+        /**
+         * Close modal window
+         */
         closeModal: function (result) {
             var value;
 
