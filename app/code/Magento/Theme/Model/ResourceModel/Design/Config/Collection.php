f6b908f33de2b45f69ddfde9878d79fb0b1abca7
Oleksandr Karpenko <okarpenko@ebay.com>
2015-11-26 13:06:58 +0200

    MAGETWO-45966:  Create API service for design config

--- app/code/Magento/Theme/Model/ResourceModel/Design/Config/Collection.php
+++ app/code/Magento/Theme/Model/ResourceModel/Design/Config/Collection.php
@@ -3,7 +3,7 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\Theme\Model\Design\Config;
+namespace Magento\Theme\Model\ResourceModel\Design\Config;
 
 use Magento\Config\Model\ResourceModel\Config\Data\Collection as ConfigCollection;
 
