d8fbf9c2a371db29917de44737c27cb4dcf476bc
Dmytro Kvashnin <dkvashnin@ebay.com>
2015-11-26 18:59:27 +0200

    MAGETWO-46040: Cannot change payment method in case of failure

--- app/code/Magento/Paypal/Test/Unit/Model/PayflowproTest.php
+++ app/code/Magento/Paypal/Test/Unit/Model/PayflowproTest.php
@@ -9,6 +9,9 @@
  */
 namespace Magento\Paypal\Test\Unit\Model;
 
+use Magento\Framework\DataObject;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Payment\Model\Method\ConfigInterface;
 use Magento\Paypal\Model\Config;
 use Magento\Paypal\Model\Payflowpro;
 use Magento\Store\Model\ScopeInterface;
@@ -30,7 +33,7 @@ class PayflowproTest extends \PHPUnit_Framework_TestCase
     protected $helper;
 
     /**
-     * @var \Magento\Payment\Model\Method\ConfigInterface|\PHPUnit_Framework_MockObject_MockObject
+     * @var ConfigInterface|\PHPUnit_Framework_MockObject_MockObject
      */
     protected $configMock;
 
@@ -455,5 +458,42 @@ class PayflowproTest extends \PHPUnit_Framework_TestCase
             ->method('getIncrementId')
             ->willReturn($orderData['increment_id']);
         return $orderMock;
+    }
+
+    public function testPostRequest()
+    {
+        $expectedResult = new DataObject();
+
+        $request = new DataObject();
+
+        /** @var ConfigInterface $config */
+        $config = $this->getMock(ConfigInterface::class);
+
+        $this->gatewayMock->expects(static::once())
+            ->method('postRequest')
+            ->with($request, $config)
+            ->willReturn($expectedResult);
+
+        static::assertSame($expectedResult, $this->payflowpro->postRequest($request, $config));
+    }
+
+    public function testPostRequestException()
+    {
+        $this->setExpectedException(
+            LocalizedException::class,
+            __('Payment Gateway is unreachable at the moment. Please use another payment option.')
+        );
+
+        $request = new DataObject();
+
+        /** @var ConfigInterface $config */
+        $config = $this->getMock(ConfigInterface::class);
+
+        $this->gatewayMock->expects(static::once())
+            ->method('postRequest')
+            ->with($request, $config)
+            ->willThrowException(new \Zend_Http_Client_Exception());
+
+        $this->payflowpro->postRequest($request, $config);
     }
 }
