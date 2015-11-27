dbb5d5261615dfe47ba138bbfd83d050eabf5983
Stanislav Lopukhov <slopukhov@ebay.com>
2015-11-27 17:38:25 +0200

    MAGETWO-46062: Create category metadata provider and prepare form declaration

---
+++ app/code/Magento/Catalog/Model/Category/DataProvider.php
@@ -0,0 +1,152 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Catalog\Model\Category;
+
+use Magento\Catalog\Model\Category;
+use Magento\Eav\Model\Config;
+use Magento\Eav\Model\Entity\Type;
+use Magento\Catalog\Model\ResourceModel\Category\CollectionFactory as CategoryCollectionFactory;
+use Magento\Ui\DataProvider\EavValidationRules;
+use Magento\Framework\View\Element\UiComponent\DataProvider\FilterPool;
+
+/**
+ * Class DataProvider
+ */
+class DataProvider extends \Magento\Ui\DataProvider\AbstractDataProvider
+{
+    /**
+     * @var array
+     */
+    protected $loadedData;
+
+    /**
+     * EAV attribute properties to fetch from meta storage
+     * @var array
+     */
+    protected $metaProperties = [
+        'dataType' => 'frontend_input',
+        'visible' => 'is_visible',
+        'required' => 'is_required',
+        'label' => 'frontend_label',
+        'sortOrder' => 'sort_order',
+        'notice' => 'note',
+        'default' => 'default_value',
+        'size' => 'multiline_count',
+    ];
+
+    /**
+     * Form element mapping
+     *
+     * @var array
+     */
+    protected $formElement = [
+        'text' => 'input',
+        'hidden' => 'input',
+        'boolean' => 'checkbox',
+    ];
+
+    /**
+     * @var EavValidationRules
+     */
+    protected $eavValidationRules;
+
+    /**
+     * Constructor
+     *
+     * @param string $name
+     * @param string $primaryFieldName
+     * @param string $requestFieldName
+     * @param EavValidationRules $eavValidationRules
+     * @param CategoryCollectionFactory $categoryCollectionFactory
+     * @param Config $eavConfig
+     * @param FilterPool $filterPool
+     * @param array $meta
+     * @param array $data
+     */
+    public function __construct(
+        $name,
+        $primaryFieldName,
+        $requestFieldName,
+        EavValidationRules $eavValidationRules,
+        CategoryCollectionFactory $categoryCollectionFactory,
+        Config $eavConfig,
+        FilterPool $filterPool,
+        array $meta = [],
+        array $data = []
+    ) {
+        parent::__construct($name, $primaryFieldName, $requestFieldName, $meta, $data);
+        $this->eavValidationRules = $eavValidationRules;
+        $this->collection = $categoryCollectionFactory->create();
+        $this->collection->addAttributeToSelect('*');
+        $this->eavConfig = $eavConfig;
+        $this->filterPool = $filterPool;
+        $this->meta['general']['fields'] = $this->getAttributesMeta(
+            $this->eavConfig->getEntityType('catalog_category')
+        );
+    }
+    /**
+     * Get data
+     *
+     * @return array
+     */
+    public function getData()
+    {
+        if (isset($this->loadedData)) {
+            return $this->loadedData;
+        }
+        $items = $this->collection->getItems();
+        /** @var Category $category */
+        foreach ($items as $category) {
+            $result['general'] = $category->getData();
+            $this->loadedData[$category->getId()] = $result;
+        }
+
+        return $this->loadedData;
+    }
+
+    /**
+     * Get attributes meta
+     *
+     * @param Type $entityType
+     * @return array
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    protected function getAttributesMeta(Type $entityType)
+    {
+        $meta = [];
+        $attributes = $entityType->getAttributeCollection();
+        /* @var \Magento\Eav\Model\Entity\Attribute\AbstractAttribute $attribute */
+        foreach ($attributes as $attribute) {
+            $code = $attribute->getAttributeCode();
+            // use getDataUsingMethod, since some getters are defined and apply additional processing of returning value
+            foreach ($this->metaProperties as $metaName => $origName) {
+                $value = $attribute->getDataUsingMethod($origName);
+                $meta[$code][$metaName] = ($metaName === 'label') ? __($value) : $value;
+                if ('frontend_input' === $origName) {
+                    $meta[$code]['formElement'] = isset($this->formElement[$value])
+                        ? $this->formElement[$value]
+                        : $value;
+                }
+                if ($attribute->usesSource()) {
+                    $meta[$code]['options'] = $attribute->getSource()->getAllOptions();
+                }
+            }
+
+            $rules = $this->eavValidationRules->build($attribute, $meta[$code]);
+            if (!empty($rules)) {
+                $meta[$code]['validation'] = $rules;
+            }
+        }
+
+        $result = [];
+        foreach ($meta as $key => $item) {
+            $result[$key] = $item;
+            $result[$key]['sortOrder'] = 0;
+        }
+
+        return $result;
+    }
+}
