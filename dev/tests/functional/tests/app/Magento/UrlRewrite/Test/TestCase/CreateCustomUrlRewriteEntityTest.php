7340974ab94f38ce39f1b5cb38e98f39be0455b4
Dmytro Aponasenko <daponasenko@ebay.com>
2015-11-27 13:33:06 +0200

    MTA-2686: Change domain responsibility for UrlRewrite module to MX

--- dev/tests/functional/tests/app/Magento/UrlRewrite/Test/TestCase/CreateCustomUrlRewriteEntityTest.php
+++ dev/tests/functional/tests/app/Magento/UrlRewrite/Test/TestCase/CreateCustomUrlRewriteEntityTest.php
@@ -4,7 +4,7 @@
  * See COPYING.txt for license details.
  */
 
-namespace Magento\Cms\Test\TestCase;
+namespace Magento\UrlRewrite\Test\TestCase;
 
 use Magento\UrlRewrite\Test\Fixture\UrlRewrite;
 use Magento\UrlRewrite\Test\Page\Adminhtml\UrlRewriteEdit;
@@ -12,23 +12,24 @@ use Magento\UrlRewrite\Test\Page\Adminhtml\UrlRewriteIndex;
 use Magento\Mtf\TestCase\Injectable;
 
 /**
- * Preconditions
+ * Preconditions:
  * 1. Create CMS Page.
+ * 2. Create subcategory.
+ * 3. Create simple product.
  *
- * Steps
+ * Steps:
  * 1. Login to backend as Admin.
  * 2. Go to the Marketing > SEO & Search > URL Rewrites.
  * 3. Click "Add Url Rewrite" button.
- * 4. Select "For CMS Page" in Create URL Rewrite dropdown.
- * 5. Select CMS page from preconditions in grid.
- * 6. Fill data according to data set.
- * 7. Save Rewrite.
- * 8. Perform all assertions.
+ * 4. Select "Custom" in Create URL Rewrite dropdown.
+ * 5. Fill data according to data set.
+ * 6. Save Rewrite.
+ * 7. Perform all assertions.
  *
  * @group URL_Rewrites_(MX)
- * @ZephyrId MAGETWO-24847
+ * @ZephyrId MAGETWO-25474
  */
-class CreateCmsPageRewriteEntityTest extends Injectable
+class CreateCustomUrlRewriteEntityTest extends Injectable
 {
     /* tags */
     const MVP = 'yes';
@@ -56,32 +57,23 @@ class CreateCmsPageRewriteEntityTest extends Injectable
      * @param UrlRewriteEdit $urlRewriteEdit
      * @return void
      */
-    public function __inject(
-        UrlRewriteIndex $urlRewriteIndex,
-        UrlRewriteEdit $urlRewriteEdit
-    ) {
+    public function __inject(UrlRewriteIndex $urlRewriteIndex, UrlRewriteEdit $urlRewriteEdit)
+    {
         $this->urlRewriteIndex = $urlRewriteIndex;
         $this->urlRewriteEdit = $urlRewriteEdit;
     }
 
     /**
-     * Create CMS page rewrites.
+     * Create custom URL Rewrite.
      *
      * @param UrlRewrite $urlRewrite
-     * @return array
+     * @return void
      */
     public function test(UrlRewrite $urlRewrite)
     {
-        //Steps
         $this->urlRewriteIndex->open();
         $this->urlRewriteIndex->getPageActionsBlock()->addNew();
-        $this->urlRewriteEdit->getUrlRewriteTypeSelectorBlock()->selectType($urlRewrite->getEntityType());
-        $cmsPage = $urlRewrite->getDataFieldConfig('target_path')['source']->getEntity();
-        $filter = ['title' => $cmsPage->getTitle()];
-        $this->urlRewriteEdit->getCmsGridBlock()->searchAndOpen($filter);
         $this->urlRewriteEdit->getFormBlock()->fill($urlRewrite);
         $this->urlRewriteEdit->getPageMainActions()->save();
-
-        return ['cmsPage' => $cmsPage];
     }
 }
