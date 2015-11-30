73a8e73ac4cb0cd5dd1f186e0ec6c5eb4f01cb78
rliukshyn <rliukshyn@ebay.com>
2015-11-30 12:21:36 +0200

    MAGETWO-45682: Create automated functional tests for Braintree

--- dev/tests/functional/tests/app/Magento/Checkout/Test/TestStep/SelectPaymentMethodStep.php
+++ dev/tests/functional/tests/app/Magento/Checkout/Test/TestStep/SelectPaymentMethodStep.php
@@ -7,6 +7,7 @@
 namespace Magento\Checkout\Test\TestStep;
 
 use Magento\Checkout\Test\Page\CheckoutOnepage;
+use Magento\Mtf\Fixture\FixtureFactory;
 use Magento\Mtf\TestStep\TestStepInterface;
 use Magento\Payment\Test\Fixture\CreditCard;
 
@@ -41,16 +42,20 @@ class SelectPaymentMethodStep implements TestStepInterface
      * @constructor
      * @param CheckoutOnepage $checkoutOnepage
      * @param array $payment
-     * @param CreditCard|null $creditCard
+     * @param FixtureFactory $fixtureFactory
+     * @param string $creditCardClass
+     * @param array|CreditCard|null $creditCard
      */
     public function __construct(
         CheckoutOnepage $checkoutOnepage,
         array $payment,
-        CreditCard $creditCard = null
+        FixtureFactory $fixtureFactory,
+        $creditCardClass = 'credit_card',
+        array $creditCard = []
     ) {
         $this->checkoutOnepage = $checkoutOnepage;
         $this->payment = $payment;
-        $this->creditCard = $creditCard;
+        $this->creditCard = $fixtureFactory->createByCode($creditCardClass, ['dataset' => $creditCard['dataset']]);
     }
 
     /**
