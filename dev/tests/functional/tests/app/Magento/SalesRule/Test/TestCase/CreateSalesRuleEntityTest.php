a7b5efcc9a02c671ba12ae48056ce5f662d90127
manasa <mpotluri@ebay.com>
2015-12-01 16:36:33 -0600

    MAGETWO-41219: FAT coverage for Cart Price rule functionality

--- dev/tests/functional/tests/app/Magento/SalesRule/Test/TestCase/CreateSalesRuleEntityTest.php
+++ dev/tests/functional/tests/app/Magento/SalesRule/Test/TestCase/CreateSalesRuleEntityTest.php
@@ -12,6 +12,7 @@ use Magento\SalesRule\Test\Page\Adminhtml\PromoQuoteIndex;
 use Magento\SalesRule\Test\Page\Adminhtml\PromoQuoteNew;
 use Magento\Mtf\Fixture\FixtureFactory;
 use Magento\Mtf\TestCase\Injectable;
+use Magento\Catalog\Test\Fixture\CatalogProductSimple;
 
 /**
  * Precondition:
@@ -64,68 +65,107 @@ class CreateSalesRuleEntityTest extends Injectable
     protected $salesRuleName;
 
     /**
+     * Fixture factory.
+     *
+     * @var FixtureFactory
+     */
+    protected $fixtureFactory;
+
+    /**
      * Inject pages.
      *
      * @param PromoQuoteNew $promoQuoteNew
      * @param PromoQuoteIndex $promoQuoteIndex
      * @param PromoQuoteEdit $promoQuoteEdit
+     * @param FixtureFactory $fixtureFactory
      * @return void
      */
     public function __inject(
         PromoQuoteNew $promoQuoteNew,
         PromoQuoteIndex $promoQuoteIndex,
-        PromoQuoteEdit $promoQuoteEdit
+        PromoQuoteEdit $promoQuoteEdit,
+        FixtureFactory $fixtureFactory
     ) {
         $this->promoQuoteNew = $promoQuoteNew;
         $this->promoQuoteIndex = $promoQuoteIndex;
         $this->promoQuoteEdit = $promoQuoteEdit;
+        $this->fixtureFactory = $fixtureFactory;
     }
 
     /**
-     * Create customer and 2 simple products with categories before run test.
-     *
-     * @param FixtureFactory $fixtureFactory
-     * @return array
-     */
-    public function __prepare(FixtureFactory $fixtureFactory)
-    {
-        $customer = $fixtureFactory->createByCode('customer', ['dataset' => 'default']);
-        $customer->persist();
-
-        $productForSalesRule1 = $fixtureFactory->createByCode(
-            'catalogProductSimple',
-            ['dataset' => 'simple_for_salesrule_1']
-        );
-        $productForSalesRule1->persist();
-
-        $productForSalesRule2 = $fixtureFactory->createByCode(
-            'catalogProductSimple',
-            ['dataset' => 'simple_for_salesrule_2']
-        );
-        $productForSalesRule2->persist();
-
-        return [
-            'customer' => $customer,
-            'productForSalesRule1' => $productForSalesRule1,
-            'productForSalesRule2' => $productForSalesRule2
-        ];
-    }
-
-    /**
-     * Create Sales Rule Entity.
+     * Create Sales Price Rule.
      *
      * @param SalesRule $salesRule
-     * @return void
+     * @param array $productQuantity
+     * @param string $conditionEntity
+     * @return array
      */
-    public function testCreateSalesRule(SalesRule $salesRule)
+    public function testCreateSalesRule(SalesRule $salesRule, $productQuantity, $conditionEntity = null)
     {
-        // Preconditions
+        $result = [];
+        $replace = null;
+        // Prepare data
+        $customer = $this->fixtureFactory->createByCode('customer', ['dataset' => 'default']);
+        $customer->persist();
+        $result['customer']=$customer;
         $this->salesRuleName = $salesRule->getName();
+        if(isset($productQuantity['productForSalesRule1'])){
+            $productForSalesRule1 = $this->fixtureFactory->createByCode(
+                'catalogProductSimple',
+                ['dataset' => 'simple_for_salesrule_1']
+            );
+            $productForSalesRule1->persist();
+            $result['productForSalesRule1'] = $productForSalesRule1;
+        }
+        if(isset($productQuantity['productForSalesRule2'])){
+            $productForSalesRule2 = $this->fixtureFactory->createByCode(
+                'catalogProductSimple',
+                ['dataset' => 'simple_for_salesrule_2']
+            );
+            $productForSalesRule2->persist();
+            if(!is_null($conditionEntity))
+                $replace = $this->prepareCondition($productForSalesRule2, $conditionEntity);
+            $result['productForSalesRule2'] = $productForSalesRule2;
+        }
 
         // Steps
         $this->promoQuoteNew->open();
-        $this->promoQuoteNew->getSalesRuleForm()->fill($salesRule);
+        $this->promoQuoteNew->getSalesRuleForm()->fill($salesRule,null,$replace);
         $this->promoQuoteNew->getFormPageActions()->save();
+
+        return $result;
+    }
+
+    /**
+     * Prepare condition for catalog price rule.
+     *
+     * @param CatalogProductSimple $productSimple
+     * @param string $conditionEntity
+     * @return array
+     */
+    protected function prepareCondition(CatalogProductSimple $productSimple, $conditionEntity)
+    {
+        $result = [];
+
+        switch ($conditionEntity) {
+            case 'category':
+                $result['%category_id%'] = $productSimple->getDataFieldConfig('category_ids')['source']->getIds()[0];
+                break;
+            case 'attribute':
+                /** @var \Magento\Catalog\Test\Fixture\CatalogProductAttribute[] $attrs */
+                $attributes = $productSimple->getDataFieldConfig('attribute_set_id')['source']
+                    ->getAttributeSet()->getDataFieldConfig('assigned_attributes')['source']->getAttributes();
+
+                $result['%attribute_name%'] = $attributes[0]->getFrontendLabel();
+                $result['%attribute_value%'] = $attributes[0]->getOptions()[0]['view'];
+                break;
+        }
+
+        return [
+            'conditions' => [
+                'conditions_serialized' => $result,
+            ],
+        ];
     }
 
     /**
