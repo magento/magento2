366104e37e07717096ee9f655e17c374a566d75a
Ankur Kaneria <akaneria@ebay.com>
2015-12-02 13:55:06 -0600

    MAGETWO-45219: Create schema scripts to create synonym table
    - Added a missing namespace to  Magento/Search/Setup/UpgradeSchema.php

--- app/code/Magento/Search/Setup/UpgradeSchema.php
+++ app/code/Magento/Search/Setup/UpgradeSchema.php
@@ -8,6 +8,7 @@ namespace Magento\Search\Setup;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\SchemaSetupInterface;
 use Magento\Framework\Setup\UpgradeSchemaInterface;
+use Magento\Framework\DB\Adapter\AdapterInterface;
 
 /**
  * @codeCoverageIgnore
