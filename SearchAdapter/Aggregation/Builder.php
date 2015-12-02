b4009fc9c5158e3e8cfbb6d51fd38a8f3d174c57
Oleksii Pugach <alexey.pugach@phoenix-media.eu>
2015-12-02 12:13:18 +0200

    PRD-73: Search Interfaces Implementation 2 - Basic Layered Navigation

--- SearchAdapter/Aggregation/Builder.php
+++ SearchAdapter/Aggregation/Builder.php
@@ -6,14 +6,37 @@
 namespace Magento\Elasticsearch\SearchAdapter\Aggregation;
 
 use Magento\Framework\Search\RequestInterface;
+use Magento\Framework\Search\Dynamic\DataProviderInterface;
+use Magento\Elasticsearch\SearchAdapter\Aggregation\Builder\BucketBuilderInterface;
 
 class Builder
 {
     /**
+     * @var DataProviderInterface[]
+     */
+    protected $dataProviderContainer;
+
+    /**
+     * @var BucketBuilderInterface[]
+     */
+    protected $aggregationContainer;
+
+    /**
+     * @param  DataProviderInterface[] $dataProviderContainer
+     * @param  BucketBuilderInterface[] $aggregationContainer
+     */
+    public function __construct(
+        array $dataProviderContainer,
+        array $aggregationContainer
+    ) {
+        $this->dataProviderContainer = $dataProviderContainer;
+        $this->aggregationContainer = $aggregationContainer;
+    }
+
+    /**
      * @param RequestInterface $request
      * @param array $queryResult
      * @return array
-     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
     public function build(
         RequestInterface $request,
@@ -21,8 +44,15 @@ class Builder
     ) {
         $aggregations = [];
         $buckets = $request->getAggregation();
+        $dataProvider = $this->dataProviderContainer[$request->getIndex()];
         foreach ($buckets as $bucket) {
-            $aggregations[$bucket->getName()] = [];
+            $aggregationBuilder = $this->aggregationContainer[$bucket->getType()];
+            $aggregations[$bucket->getName()] = $aggregationBuilder->build(
+                $bucket,
+                $request->getDimensions(),
+                $queryResult,
+                $dataProvider
+            );
         }
 
         return $aggregations;
