cd9f1c43593d36309b14e1a2ac67e5f88826c75d
Vitalii Zabaznov <vzabaznov@ebay.com>
2015-12-03 18:12:23 +0200

    MAGETWO-46039: Catalog price rule isn't applied to product created using Web API

---
+++ app/code/Magento/CatalogRule/Plugin/Indexer/Product/Save/ApplyRulesAfterReindex.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\CatalogRule\Plugin\Indexer\Product\Save;
+
+use Magento\CatalogRule\Model\Indexer\Product\ProductRuleProcessor;
+
+class ApplyRulesAfterReindex
+{
+    /**
+     * @var ProductRuleProcessor
+     */
+    protected $productRuleProcessor;
+
+    /**
+     * @param ProductRuleProcessor $productRuleProcessor
+     */
+    public function __construct(ProductRuleProcessor $productRuleProcessor)
+    {
+        $this->productRuleProcessor = $productRuleProcessor;
+    }
+
+    /**
+     * Apply catalog rules after product resource model save
+     *
+     * @param \Magento\Catalog\Model\Product $subject
+     * @param callable $proceed
+     * @return \Magento\Catalog\Model\Product
+     *
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function aroundReindex(
+        \Magento\Catalog\Model\Product $subject,
+        callable $proceed
+    ) {
+        $proceed();
+        $this->productRuleProcessor->reindexRow($subject->getId());
+        return;
+    }
+}
