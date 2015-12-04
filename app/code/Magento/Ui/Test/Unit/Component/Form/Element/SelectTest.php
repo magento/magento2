e0e5d3ca37ddf959d7c2077a99aa6e01f8223853
Oleksandr Karpenko <okarpenko@ebay.com>
2015-12-04 21:08:05 +0200

    MAGETWO-45966:  Create API service for design config

--- app/code/Magento/Ui/Test/Unit/Component/Form/Element/SelectTest.php
+++ app/code/Magento/Ui/Test/Unit/Component/Form/Element/SelectTest.php
@@ -3,7 +3,7 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\Ui\Component\Form\Element;
+namespace Magento\Ui\Test\Unit\Component\Form\Element;
 
 class SelectTest extends \PHPUnit_Framework_TestCase
 {
