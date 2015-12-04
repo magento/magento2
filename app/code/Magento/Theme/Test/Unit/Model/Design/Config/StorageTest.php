3bba80f77b82690058b009a5ffec9851111dab6a
Oleksandr Karpenko <okarpenko@ebay.com>
2015-12-04 19:59:33 +0200

    MAGETWO-45966:  Create API service for design config

--- app/code/Magento/Theme/Test/Unit/Model/Design/Config/StorageTest.php
+++ app/code/Magento/Theme/Test/Unit/Model/Design/Config/StorageTest.php
@@ -3,7 +3,7 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\Theme\Model\Design\Config;
+namespace Magento\Theme\Test\Unit\Model\Design\Config;
 
 class StorageTest extends \PHPUnit_Framework_TestCase
 {
