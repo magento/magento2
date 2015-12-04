55333f65ee5f8a285de8a81a4ecfd4ed972ef9ef
PhoenixPM - BK <bjoern.kraus@phoenix-media.eu>
2015-12-04 12:27:44 +0100

    PRD-33 refactored call stack for SuggestQueries data provider in search engines

--- Model/SuggestedQueries.php
+++ Model/SuggestedQueries.php
@@ -5,25 +5,81 @@
  */
 namespace Magento\AdvancedSearch\Model;
 
+use Magento\Framework\ObjectManagerInterface;
 use Magento\Search\Model\QueryInterface;
+use Magento\Search\Model\EngineResolver;
 
 class SuggestedQueries implements SuggestedQueriesInterface
 {
+    /**
+     * @var EngineResolver
+     */
+    private $engineResolver;
+
+    /**
+     * @var ObjectManager
+     */
+    private $objectManager;
+
+    /**
+     * Array of SuggestedQueriesInterface class names.
+     *
+     * @var array
+     */
+    private $data;
+
+    /**
+     * @var SuggestedQueriesInterface
+     */
+    private $dataProvider;
+
+    /**
+     * SuggestedQueries constructor.
+     *
+     * @param EngineResolver $engineResolver
+     * @param ObjectManagerInterface $objectManager
+     * @param array $data
+     */
+    public function __construct(EngineResolver $engineResolver, ObjectManagerInterface $objectManager, array $data)
+    {
+        $this->engineResolver = $engineResolver;
+        $this->objectManager = $objectManager;
+        $this->data = $data;
+    }
 
     /**
      * {@inheritdoc}
      */
     public function isResultsCountEnabled()
     {
-        return false;
+        return $this->getDataProvider()->isResultsCountEnabled();
     }
 
     /**
-     * @param QueryInterface $query
-     * @return \Magento\Search\Model\QueryResult[]
+     * {@inheritdoc}
      */
     public function getItems(QueryInterface $query)
     {
-        return [];
+        return $this->getDataProvider()->getItems($query);
+    }
+
+    /**
+     * Returns DataProvider for SuggestedQueries
+     *
+     * @return SuggestedQueriesInterface|SuggestedQueriesInterface[]
+     * @throws \Exception
+     */
+    private function getDataProvider()
+    {
+        if (empty($this->dataProvider)) {
+            $currentEngine = $this->engineResolver->getCurrentSearchEngine();
+            $this->dataProvider = $this->objectManager->create($this->data[$currentEngine]);
+            if (!$this->dataProvider instanceof SuggestedQueriesInterface) {
+                throw new \InvalidArgumentException(
+                    'Data provider must implement \Magento\AdvancedSearch\Model\SuggestedQueriesInterface'
+                );
+            }
+        }
+        return $this->dataProvider;
     }
 }
--- Test/Unit/Model/DataProviderTest.php
+++ Test/Unit/Model/DataProviderTest.php
@@ -6,33 +6,117 @@
 
 namespace Magento\AdvancedSearch\Test\Unit\Model;
 
-use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
+use Magento\Framework\TestFramework\Unit\Helper\ObjectManager as ObjectManagerHelper;
 
 class DataProviderTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * @var ObjectManager
+     * @var \Magento\AdvancedSearch\Model\SuggestedQueries;
      */
-    private $objectManager;
+    protected $model;
 
+    /**
+     * @var \Magento\Search\Model\EngineResolver|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $engineResolverMock;
+
+    /**
+     * @var \Magento\Framework\ObjectManagerInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $objectManagerMock;
+
+    /**
+     * @var ObjectManagerHelper
+     */
+    protected $objectManagerHelper;
+
+    /**
+     * Set up test environment.
+     *
+     * @return void
+     */
     protected function setUp()
     {
-        $this->objectManager = new ObjectManager($this);
+        $this->engineResolverMock = $this->getMockBuilder('Magento\Search\Model\EngineResolver')
+            ->setMethods(['getCurrentSearchEngine'])
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->engineResolverMock->expects($this->any())
+            ->method('getCurrentSearchEngine')
+            ->willReturn('my_engine');
+
+        /**
+         * @var \Magento\AdvancedSearch\Model\SuggestedQueriesInterface|
+         *     \PHPUnit_Framework_MockObject_MockObject
+         */
+        $suggestedQueriesInterfaceMock = $this->getMock(
+            'Magento\AdvancedSearch\Model\SuggestedQueriesInterface'
+        );
+        $suggestedQueriesInterfaceMock->expects($this->any())
+            ->method('isResultsCountEnabled')
+            ->willReturn(true);
+        $suggestedQueriesInterfaceMock->expects($this->any())
+            ->method('getItems')
+            ->willReturn([]);
+
+        $this->objectManagerMock = $this->getMockBuilder('Magento\Framework\ObjectManagerInterface')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->objectManagerMock->expects($this->any())
+            ->method('create')
+            ->with('search_engine')
+            ->willReturn($suggestedQueriesInterfaceMock);
+
+        $this->objectManagerHelper = new ObjectManagerHelper($this);
+        $this->model = $this->objectManagerHelper->getObject(
+            '\Magento\AdvancedSearch\Model\SuggestedQueries',
+            [
+                'engineResolver' => $this->engineResolverMock,
+                'objectManager' => $this->objectManagerMock,
+                'data' => ['my_engine' => 'search_engine']
+            ]
+        );
     }
 
-    public function testGetRecommendations()
-    {
-        /** @var \Magento\Search\Model\QueryInterface|\PHPUnit_Framework_MockObject_MockObject $searchQuery */
-        $searchQuery = $this->getMockBuilder('\Magento\Search\Model\QueryInterface')->getMockForAbstractClass();
-        /** @var \Magento\AdvancedSearch\Model\SuggestedQueries $dataProvider */
-        $dataProvider = $this->objectManager->getObject('Magento\AdvancedSearch\Model\SuggestedQueries');
-        $this->assertEquals([], $dataProvider->getItems($searchQuery));
-    }
-
+    /**
+     * Test isResultsCountEnabled method.
+     */
     public function testIsResultsCountEnabled()
     {
-        /** @var \Magento\AdvancedSearch\Model\SuggestedQueries $dataProvider */
-        $dataProvider = $this->objectManager->getObject('Magento\AdvancedSearch\Model\SuggestedQueries');
-        $this->assertFalse($dataProvider->isResultsCountEnabled());
+        $result = $this->model->isResultsCountEnabled();
+        $this->assertTrue($result);
+    }
+
+    /**
+     * Test isResultsCountEnabled() method failure
+     * @expectedException \InvalidArgumentException
+     */
+    public function testIsResultsCountEnabledException()
+    {
+        $objectManagerMock = $this->getMockBuilder('Magento\Framework\ObjectManagerInterface')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $objectManagerMock->expects($this->once())
+            ->method('create')
+            ->willReturn(null);
+
+        $objectManagerHelper = new ObjectManagerHelper($this);
+        /* @var $model \Magento\AdvancedSearch\Model\SuggestedQueries */
+        $model = $objectManagerHelper->getObject(
+            '\Magento\AdvancedSearch\Model\SuggestedQueries',
+            [
+                'engineResolver' => $this->engineResolverMock,
+                'objectManager' => $objectManagerMock,
+                'data' => ['my_engine' => 'search_engine']
+            ]
+        );
+        $model->isResultsCountEnabled();
+    }
+
+    public function testGetItems()
+    {
+        $queryInterfaceMock = $this->getMock('Magento\Search\Model\QueryInterface');
+        $result = $this->model->getItems($queryInterfaceMock);
+        $this->assertEquals([], $result);
     }
 }
