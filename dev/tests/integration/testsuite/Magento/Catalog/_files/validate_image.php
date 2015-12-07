6e4bac200381cab9cfa43d07c6c001e85fab0f28
Vitalii Zabaznov <vzabaznov@ebay.com>
2015-12-07 13:28:49 +0200

    MAGETWO-45953: Internal error exposed with custom option file
    - add test for check empty file

--- dev/tests/integration/testsuite/Magento/Catalog/_files/validate_image.php
+++ dev/tests/integration/testsuite/Magento/Catalog/_files/validate_image.php
@@ -16,3 +16,6 @@ $tmpDirectory->create($tmpDirectory->getAbsolutePath());
 $targetTmpFilePath = $tmpDirectory->getAbsolutePath('magento_small_image.jpg');
 copy(__DIR__ . '/magento_small_image.jpg', $targetTmpFilePath);
 // Copying the image to target dir is not necessary because during product save, it will be moved there from tmp dir
+
+$targetTmpFilePath = $tmpDirectory->getAbsolutePath('magento_empty.jpg');
+copy(__DIR__ . '/magento_empty.jpg', $targetTmpFilePath);
