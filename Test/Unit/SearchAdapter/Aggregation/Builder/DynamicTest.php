165e2bec1033a2bf52cafb72d89692ae851aebe7
Sergey Plakhonin <sergey.plahonin@phoenix-media.eu>
2015-12-07 19:10:54 +0200

    PRD-74: Search Interfaces Implementation 3 - Price Layered Navigation

---
+++ Test/Unit/SearchAdapter/Aggregation/Builder/DynamicTest.php
@@ -0,0 +1,175 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Elasticsearch\Test\Unit\SearchAdapter\Aggregation\Builder;
+
+use Magento\Elasticsearch\SearchAdapter\Aggregation\Builder\Dynamic;
+use Magento\Framework\TestFramework\Unit\Helper\ObjectManager as ObjectManagerHelper;
+
+class DynamicTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Dynamic
+     */
+    private $model;
+
+    /**
+     * @var \Magento\Framework\Search\Request\BucketInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $requestBuckedInterface;
+
+    /**
+     * @var \Magento\Framework\Search\Dynamic\DataProviderInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $dataProviderContainer;
+
+    /**
+     * @var \Magento\Framework\Search\Dynamic\Algorithm\AlgorithmInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $algorithmRepository;
+
+    /**
+     * @var \Magento\Framework\Search\Dynamic\EntityStorageFactory|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $entityStorageFactory;
+
+    /**
+     * @var \Magento\Framework\Search\Dynamic\Algorithm\AlgorithmInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $algorithmInterface;
+
+    /**
+     * @var \Magento\Framework\Search\Request\Aggregation\DynamicBucket|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $bucket;
+
+    /**
+     * @var \Magento\Framework\Search\Dynamic\EntityStorage|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $entityStorage;
+
+    /**
+     * Set up test environment.
+     *
+     * @return void
+     */
+    protected function setUp()
+    {
+        $this->requestBuckedInterface = $this->getMockBuilder('Magento\Framework\Search\Request\BucketInterface')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->dataProviderContainer = $this->getMockBuilder('Magento\Elasticsearch\SearchAdapter\Dynamic\DataProvider')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->algorithmRepository = $this->getMockBuilder('Magento\Framework\Search\Dynamic\Algorithm\Repository')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->entityStorageFactory = $this->getMockBuilder('Magento\Framework\Search\Dynamic\EntityStorageFactory')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->algorithmInterface = $this->getMockBuilder('Magento\Framework\Search\Dynamic\Algorithm\AlgorithmInterface')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->bucket = $this->getMockBuilder('Magento\Framework\Search\Request\Aggregation\DynamicBucket')
+            ->setMethods(['getMethod'])
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->entityStorage = $this->getMockBuilder('Magento\Framework\Search\Dynamic\EntityStorage')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->entityStorageFactory->expects($this->any())
+            ->method('create')
+            ->willReturn($this->entityStorage);
+
+        $objectManagerHelper = new ObjectManagerHelper($this);
+
+        $this->model = $objectManagerHelper->getObject(
+            '\Magento\Elasticsearch\SearchAdapter\Aggregation\Builder\Dynamic',
+            [
+                'algorithmRepository' => $this->algorithmRepository,
+                'entityStorageFactory' => $this->entityStorageFactory,
+            ]
+        );
+    }
+
+    /**
+     * Test build() method
+     */
+    public function testBuild()
+    {
+        $dimensions = [
+            'scope' => [
+                'name' => 'scope',
+                'value' => 1,
+            ],
+        ];
+
+        $queryResult = [
+            'took' => 1,
+            'timed_out' => false,
+            '_shards' => [],
+            'hits' => [
+                'total' => 1,
+                'max_score' => 1,
+                'hits' => [
+                    [
+                        '_id' => 1,
+                    ]
+                ],
+            ],
+            'aggregations' => [],
+        ];
+
+        $this->bucket->expects($this->once())
+            ->method('getMethod')
+            ->willReturn('auto');
+
+        $this->algorithmRepository->expects($this->any())
+            ->method('get')
+            ->with('auto', ['dataProvider' => $this->dataProviderContainer])
+            ->willReturn($this->algorithmInterface);
+
+        $this->algorithmInterface->expects($this->once())
+            ->method('getItems')
+            ->with(
+                $this->bucket,
+                $dimensions,
+                $this->entityStorage
+            )
+            ->willReturn([
+                0 => [
+                    'from' => '',
+                    'to' => 22,
+                    'count' => 2,
+                ],
+                1 => [
+                    'from' => 22,
+                    'to' => 24,
+                    'count' => 4,
+                ],
+                2 => [
+                    'from' => 24,
+                    'to' => '',
+                    'count' => 6,
+                ],
+            ]);
+
+        $this->model->build(
+            $this->bucket,
+            $dimensions,
+            $queryResult,
+            $this->dataProviderContainer
+        );
+
+    }
+}
+
---
+++ Test/Unit/SearchAdapter/Aggregation/Builder/TermTest.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Elasticsearch\Test\Unit\SearchAdapter\Aggregation\Builder;
+
+use Magento\Elasticsearch\SearchAdapter\Aggregation\Builder\Term;
+use Magento\Framework\TestFramework\Unit\Helper\ObjectManager as ObjectManagerHelper;
+
+class TermTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Term
+     */
+    private $model;
+
+    /**
+     * @var \Magento\Framework\Search\Request\BucketInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $requestBuckedInterface;
+
+//    /**
+//     * @var \Magento\Elasticsearch\SearchAdapter\Dynamic\DataProvider|\PHPUnit_Framework_MockObject_MockObject
+//     */
+//    protected $dataProviderContainer;
+
+    /**
+     * @var \Magento\Framework\Search\Dynamic\DataProviderInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $dataProviderContainer;
+
+    /**
+     * @var \Magento\Framework\Search\Request\Aggregation\TermBucket|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $bucket;
+
+
+    /**
+     * Set up test environment.
+     *
+     * @return void
+     */
+    protected function setUp()
+    {
+        $this->requestBuckedInterface = $this->getMockBuilder('Magento\Framework\Search\Request\BucketInterface')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+//        $this->dataProviderContainer = $this->getMockBuilder('Magento\Elasticsearch\SearchAdapter\Dynamic\DataProvider')
+//            ->disableOriginalConstructor()
+//            ->getMock();
+
+        $this->dataProviderContainer = $this->getMockBuilder('Magento\Framework\Search\Dynamic\DataProviderInterface')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->bucket = $this->getMockBuilder('Magento\Framework\Search\Request\Aggregation\TermBucket')
+            ->setMethods(['getName'])
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $objectManagerHelper = new ObjectManagerHelper($this);
+
+        $this->model = $objectManagerHelper->getObject(
+            '\Magento\Elasticsearch\SearchAdapter\Aggregation\Builder\Term',
+            []
+        );
+    }
+
+    /**
+     * Test build() method
+     */
+    public function testBuild()
+    {
+        $dimensions = [
+            'scope' => [
+                'name' => 'scope',
+                'value' => 1,
+            ],
+        ];
+
+        $queryResult = [
+            'took' => 1,
+            'timed_out' => false,
+            '_shards' => [],
+            'hits' => [
+                'total' => 1,
+                'max_score' => 1,
+                'hits' => [],
+            ],
+            'aggregations' => [
+                'category_bucket' => [
+                    'buckets' => [
+                        [
+                            'key' => '23',
+                            'doc_count' => 12,
+                        ],
+                    ],
+                ],
+            ],
+        ];
+
+        $this->bucket->expects($this->once())
+            ->method('getName')
+            ->willReturn('category_bucket');
+
+        $this->model->build(
+            $this->bucket,
+            $dimensions,
+            $queryResult,
+            $this->dataProviderContainer
+        );
+    }
+}
+
