14ff0d8ad7b37ddd450f0eebf402ec0adf6d4a11
Oleksii Pugach <alexey.pugach@phoenix-media.eu>
2015-12-08 16:01:40 +0200

    PRD-73: Search Interfaces Implementation 2 - Basic Layered Navigation

--- Model/Indexer/IndexHandlerTest.php
+++ Model/Indexer/IndexHandlerTest.php
@@ -8,6 +8,10 @@ namespace Magento\CatalogSearch\Model\Indexer;
 use Magento\Catalog\Model\Product;
 use Magento\TestFramework\Helper\Bootstrap;
 use Magento\Framework\Indexer\IndexerInterface;
+use Magento\Elasticsearch\SearchAdapter\ConnectionManager;
+use Magento\Elasticsearch\Model\Client\Elasticsearch as ElasticsearchClient;
+use Magento\Framework\Search\Request\Dimension;
+use Magento\Elasticsearch\Model\Config;
 
 /**
  * @magentoDbIsolation disabled
@@ -21,6 +25,51 @@ class IndexHandlerTest extends \PHPUnit_Framework_TestCase
     protected $indexer;
 
     /**
+     * @var ConnectionManager
+     */
+    protected $connectionManager;
+
+    /**
+     * @var ElasticsearchClient
+     */
+    protected $client;
+
+    /**
+     * @var Dimension
+     */
+    protected $dimension;
+
+    /**
+     * @var Config
+     */
+    protected $clientConfig;
+
+    /**
+     * @var Product
+     */
+    protected $productApple;
+
+    /**
+     * @var Product
+     */
+    protected $productBanana;
+
+    /**
+     * @var Product
+     */
+    protected $productOrange;
+
+    /**
+     * @var Product
+     */
+    protected $productPapaya;
+
+    /**
+     * @var Product
+     */
+    protected $productCherry;
+
+    /**
      * Setup method
      */
     protected function setUp()
@@ -29,6 +78,27 @@ class IndexHandlerTest extends \PHPUnit_Framework_TestCase
             'Magento\Indexer\Model\Indexer'
         );
         $this->indexer->load('catalogsearch_fulltext');
+
+        $this->connectionManager = Bootstrap::getObjectManager()->create(
+            'Magento\Elasticsearch\SearchAdapter\ConnectionManager'
+        );
+
+        $this->client = $this->connectionManager->getConnection();
+
+        $this->dimension = Bootstrap::getObjectManager()->create(
+            '\Magento\Framework\Search\Request\Dimension',
+            ['name' => 'scope', 'value' => '1']
+        );
+
+        $this->clientConfig = Bootstrap::getObjectManager()->create(
+            'Magento\Elasticsearch\Model\Config'
+        );
+
+        $this->productApple = $this->getProductBySku('fulltext-1');
+        $this->productBanana = $this->getProductBySku('fulltext-2');
+        $this->productOrange = $this->getProductBySku('fulltext-3');
+        $this->productPapaya = $this->getProductBySku('fulltext-4');
+        $this->productCherry = $this->getProductBySku('fulltext-5');
     }
 
     /**
@@ -38,6 +108,52 @@ class IndexHandlerTest extends \PHPUnit_Framework_TestCase
     public function testReindexAll()
     {
         $this->indexer->reindexAll();
+
+        $products = $this->searchByName('Apple');
+        $this->assertCount(1, $products);
+        $this->assertEquals($this->productApple->getId(), $products[0]['_id']);
+
+        $products = $this->searchByName('Simple Product');
+        $this->assertCount(5, $products);
+    }
+
+    /**
+     * Search docs in Elasticsearch by name
+     *
+     * @param string $text
+     * @return array
+     */
+    protected function searchByName($text)
+    {
+        $storeId = $this->dimension->getValue();
+        $searchQuery = [
+            'index' => $this->clientConfig->getIndexName(),
+            'type' => $this->clientConfig->getEntityType(),
+            'body' => [
+                'query' => [
+                    'bool' => [
+                        'minimum_should_match' => 1,
+                        'must' => [
+                            [
+                                'term' => [
+                                    'store_id' => $storeId,
+                                ]
+                            ],
+                        ],
+                        'should' => [
+                            [
+                                'match' => [
+                                    'name' => $text,
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ];
+        $queryResult = $this->client->query($searchQuery);
+        $products = isset($queryResult['hits']['hits']) ? $queryResult['hits']['hits'] : [];
+        return $products;
     }
 
     /**
