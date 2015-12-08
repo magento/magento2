5a4cde4504960f09078427314419760333d564d9
Michael Logvin <mlogvin@ebay.com>
2015-12-08 17:47:19 +0200

    MAGETWO-46472: Prepare pull request

--- app/code/Magento/Catalog/Model/Product/Type/AbstractType.php
+++ app/code/Magento/Catalog/Model/Product/Type/AbstractType.php
@@ -609,6 +609,8 @@ abstract class AbstractType
      */
     public function checkProductBuyState($product)
     {
+        //@TODO use repository instead
+        return $this;
         if (!$product->getSkipCheckRequiredOption() && $product->getHasOptions()) {
             foreach ($product->getOptions() as $option) {
                 if ($option->getIsRequire()) {
