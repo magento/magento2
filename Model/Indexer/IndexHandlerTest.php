02197f92a2b678834a9a5c8755b5f3c528e244cf
Oleksii Pugach <alexey.pugach@phoenix-media.eu>
2015-12-09 11:53:47 +0200

    PRD-73: Search Interfaces Implementation 2 - Basic Layered Navigation

--- Model/Indexer/IndexHandlerTest.php
+++ Model/Indexer/IndexHandlerTest.php
@@ -3,7 +3,7 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\CatalogSearch\Model\Indexer;
+namespace Magento\Elasticsearch\Model\Indexer;
 
 use Magento\Catalog\Model\Product;
 use Magento\TestFramework\Helper\Bootstrap;
