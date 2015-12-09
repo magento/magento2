01f1fba80ae5edc13c1905b8129ade908114a666
Vladyslav Shcherbyna <vshcherbyna@ebay.com>
2015-12-09 11:12:21 +0200

    MAGETWO-46472: Prepare pull request

--- app/code/Magento/Bundle/Model/LinkManagement.php
+++ app/code/Magento/Bundle/Model/LinkManagement.php
@@ -254,7 +254,7 @@ class LinkManagement implements \Magento\Bundle\Api\ProductLinkManagementInterfa
 
         try {
             $selectionModel->save();
-            $resource->addProductRelations($product->getId(), [$linkProductModel->getId()]);
+            $resource->addProductRelation($product->getId(), $linkProductModel->getId());
         } catch (\Exception $e) {
             throw new CouldNotSaveException(__('Could not save child: "%1"', $e->getMessage()), $e);
         }
