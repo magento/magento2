f1f9057c7568faa7a0f52cbc26a20fae464982a0
Dale Sikkema <dsikkema@ebay.com>
2015-12-10 14:36:19 -0600

    MAGETWO-46798: create a header manager

--- lib/internal/Magento/Framework/App/Response/Header/XFrameOptions.php
+++ lib/internal/Magento/Framework/App/Response/Header/XFrameOptions.php
@@ -1,15 +1,17 @@
 <?php
-/***
+/**
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
+namespace Magento\Framework\App\Response\Header;
 
-namespace Magento\Framework\App\Response;
+use Magento\Framework\App\Response\HeaderProviderInterface;
+use Magento\Framework\App\Response\Http;
 
 /**
  * Adds an X-FRAME-OPTIONS header to HTTP responses to safeguard against click-jacking.
  */
-class XFrameOptPlugin
+class XFrameOptions implements HeaderProviderInterface
 {
     /** Deployment config key for frontend x-frame-options header value */
     const DEPLOYMENT_CONFIG_X_FRAME_OPT = 'x-frame-options';
@@ -18,7 +20,8 @@ class XFrameOptPlugin
     const BACKEND_X_FRAME_OPT = 'SAMEORIGIN';
 
     /**
-     *The header value
+     * The header value
+     *
      * @var string
      */
     private $xFrameOpt;
@@ -32,12 +35,32 @@ class XFrameOptPlugin
     }
 
     /**
-     * @param \Magento\Framework\App\Response\Http $subject
-     * @return void
-     * @codeCoverageIgnore
+     * Whether the header should be attached to the response
+     *
+     * @return bool
      */
-    public function beforeSendResponse(\Magento\Framework\App\Response\Http $subject)
+    public function canApply()
     {
-        $subject->setXFrameOptions($this->xFrameOpt);
+        return true;
+    }
+
+    /**
+     * Header name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return Http::HEADER_X_FRAME_OPT;
+    }
+
+    /**
+     * Header value
+     *
+     * @return string
+     */
+    public function getValue()
+    {
+        return $this->xFrameOpt;
     }
 }
