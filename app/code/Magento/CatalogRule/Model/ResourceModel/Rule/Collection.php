8613eb95eab748e90542fcd5da525652befead0d
Iryna Lagno <ilagno@ebay.com>
2015-12-10 10:34:52 +0200

    MAGETWO-46647: Real time price calculation

--- app/code/Magento/CatalogRule/Model/ResourceModel/Rule/Collection.php
+++ app/code/Magento/CatalogRule/Model/ResourceModel/Rule/Collection.php
@@ -12,23 +12,41 @@ class Collection extends \Magento\Rule\Model\ResourceModel\Rule\Collection\Abstr
      *
      * @var array
      */
-    protected $_associatedEntitiesMap = [
-        'website' => [
-            'associations_table' => 'catalogrule_website',
-            'rule_id_field' => 'rule_id',
-            'entity_id_field' => 'website_id',
-        ],
-    ];
+    protected $_associatedEntitiesMap;
+
+
+    public function __construct(
+        \Magento\Framework\Data\Collection\EntityFactoryInterface $entityFactory,
+        \Psr\Log\LoggerInterface $logger,
+        \Magento\Framework\Data\Collection\Db\FetchStrategyInterface $fetchStrategy,
+        \Magento\Framework\Event\ManagerInterface $eventManager,
+        \Magento\Framework\DB\Adapter\AdapterInterface $connection = null,
+        \Magento\Framework\Model\ResourceModel\Db\AbstractDb $resource = null,
+        array $associatedEntitiesMap = []
+    ) {
+        parent::__construct(
+            $entityFactory,
+            $logger,
+            $fetchStrategy,
+            $eventManager,
+            $connection,
+            $resource
+        );
+        $this->_associatedEntitiesMap = $associatedEntitiesMap;
+    }
 
     /**
      * Set resource model
      *
+     * @param array $associatedEntitiesMap
      * @return void
      * @codeCoverageIgnore
      */
-    protected function _construct()
-    {
+    protected function _construct(
+
+    ) {
         $this->_init('Magento\CatalogRule\Model\Rule', 'Magento\CatalogRule\Model\ResourceModel\Rule');
+
     }
 
     /**
@@ -43,6 +61,29 @@ class Collection extends \Magento\Rule\Model\ResourceModel\Rule\Collection\Abstr
         $match = sprintf('%%%s%%', substr(serialize(['attribute' => $attributeCode]), 5, -1));
         $this->addFieldToFilter('conditions_serialized', ['like' => $match]);
 
+        return $this;
+    }
+
+    /**
+     * Limit rules collection by specific customer group
+     *
+     * @param int $customerGroupId
+     * @return $this
+     */
+    public function addCustomerGroupFilter($customerGroupId)
+    {
+        $entityInfo = $this->_getAssociatedEntityInfo('customer_group');
+        if (!$this->getFlag('is_customer_group_joined')) {
+            $this->setFlag('is_customer_group_joined', true);
+            $this->getSelect()->join(
+                ['customer_group' => $this->getTable($entityInfo['associations_table'])],
+                $this->getConnection()
+                    ->quoteInto('customer_group.' . $entityInfo['entity_id_field'] . ' = ?', $customerGroupId)
+                    . ' AND main_table.' . $entityInfo['rule_id_field'] . ' = customer_group.'
+                    . $entityInfo['rule_id_field'],
+                []
+            );
+        }
         return $this;
     }
 }
