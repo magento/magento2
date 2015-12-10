b1a7078e66f6eafdef0ea52adc3458aac3adeb7d
akaplya <akaplya@ebay.com>
2015-12-10 18:09:47 +0200

    MAGETWO-46472: Prepare pull request

---
+++ lib/internal/Magento/Framework/Model/EntitySnapshot/AttributeProviderInterface.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Framework\Model\EntitySnapshot;
+
+
+interface AttributeProviderInterface
+{
+    /**
+     * @param string $entityType
+     * @return array
+     */
+    public function getAttributes($entityType);
+}
