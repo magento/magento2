d39e975bbaa7ed125965aa22452024cfce110671
Volodymyr Klymenko <vklymenko@magento.com>
2015-12-11 14:55:10 +0200

    MAGETWO-46196: Category tree edit via drag'n'drop does not work

--- app/code/Magento/Catalog/view/adminhtml/web/catalog/category/form.js
+++ app/code/Magento/Catalog/view/adminhtml/web/catalog/category/form.js
@@ -13,61 +13,53 @@ define([
 ], function($, alert){
     "use strict";
 
-    $.widget("mage.categoryForm", $.mage.form, {
-        options: {
-            categoryIdSelector : 'input[name="general[id]"]',
-            categoryPathSelector : 'input[name="general[path]"]'
-        },
+    return function (config) {
+        var categoryForm = {
+            options: {
+                categoryIdSelector: 'input[name="general[id]"]',
+                categoryPathSelector: 'input[name="general[path]"]'
+            },
 
-        /**
-         * Form creation
-         * @protected
-         */
-        _create: function() {
-            this._super();
-            $('body').on('categoryMove.tree', $.proxy(this.refreshPath, this));
-        },
-
-        /**
-         * Sending ajax to server to refresh field 'general[path]'
-         * @protected
-         */
-        refreshPath: function() {
-            if (!this.element.find(this.options.categoryIdSelector).prop('value')) {
-                return false;
-            }
-            // @TODO delete this prototype functional
-            new Ajax.Request(
-                this.options.refreshUrl,
-                {
-                    method:     'POST',
-                    evalScripts: true,
-                    onSuccess: this._refreshPathSuccess.bind(this)
+            /**
+             * Sending ajax to server to refresh field 'general[path]'
+             * @protected
+             */
+            refreshPath: function () {
+                if (!$(this.options.categoryIdSelector)) {
+                    return false;
                 }
-            );
-        },
+                // @TODO delete this prototype functional
+                new Ajax.Request(
+                    this.options.refreshUrl,
+                    {
+                        method: 'POST',
+                        evalScripts: true,
+                        onSuccess: this._refreshPathSuccess.bind(this)
+                    }
+                );
+            },
 
-        /**
-         * Refresh field 'general[path]' on ajax success
-         * @param {Object} The XMLHttpRequest object returned by ajax
-         * @protected
-         */
-        _refreshPathSuccess: function(transport) {
-            if (transport.responseText.isJSON()) {
-                var response = transport.responseText.evalJSON();
-                if (response.error) {
-                    alert({
-                        content: response.message
-                    });
-                } else {
-                    if (this.element.find(this.options.categoryIdSelector).prop('value') == response.id) {
-                        this.element.find(this.options.categoryPathSelector)
-                            .prop('value', response.path);
+            /**
+             * Refresh field 'general[path]' on ajax success
+             * @param {Object} The XMLHttpRequest object returned by ajax
+             * @protected
+             */
+            _refreshPathSuccess: function (transport) {
+                if (transport.responseText.isJSON()) {
+                    var response = transport.responseText.evalJSON();
+                    if (response.error) {
+                        alert({
+                            content: response.message
+                        });
+                    } else {
+                        if ($(this.options.categoryIdSelector).val(response.id)) {
+                            $(this.options.categoryPathSelector).val(response.path);
+                        }
                     }
                 }
             }
-        }
-    });
-    
-    return $.mage.categoryForm;
+        };
+
+        $('body').on('categoryMove.tree', $.proxy(categoryForm.refreshPath.bind(categoryForm), this));
+    }
 });
