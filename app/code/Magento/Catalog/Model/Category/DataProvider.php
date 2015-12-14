6299854c645c6ce03a7ace2765f6e8076d503ec5
Stanislav Lopukhov <slopukhov@ebay.com>
2015-12-14 17:01:39 +0200

    MAGETWO-46792: Default values for required fields are not set on new category page

--- app/code/Magento/Catalog/Model/Category/DataProvider.php
+++ app/code/Magento/Catalog/Model/Category/DataProvider.php
@@ -155,7 +155,7 @@ class DataProvider extends \Magento\Ui\DataProvider\AbstractDataProvider
         }
         $category = $this->getCurrentCategory();
         if (!$category->getId()) {
-            return $this->getDefaultData();
+            return [];
         } else {
             $categoryData = $category->getData();
             $categoryData = $this->addUseDefaultSettings($category, $categoryData);
@@ -211,6 +211,8 @@ class DataProvider extends \Magento\Ui\DataProvider\AbstractDataProvider
             $result[$key] = $item;
             $result[$key]['sortOrder'] = 0;
         }
+
+        $result = $this->getDefaultMetaData($result);
 
         return $result;
     }
@@ -306,16 +308,18 @@ class DataProvider extends \Magento\Ui\DataProvider\AbstractDataProvider
 
     /**
      * Category's fields default values
+     *
+     * @param array $result
      * @return array
      */
-    public function getDefaultData()
+    public function getDefaultMetaData($result)
     {
-        $result = [];
-        $result['']['general']['parent'] = (int)$this->request->getParam('parent');
-        $result['']['general']['is_anchor'] = false;
-        $result['']['general']['use_config']['available_sort_by'] = true;
-        $result['']['general']['use_config']['default_sort_by'] = true;
-        $result['']['general']['use_config']['filter_price_range'] = true;
+        $result['parent']['default'] = (int)$this->request->getParam('parent');
+        $result['is_anchor']['default'] = false;
+        $result['use_config.available_sort_by']['default'] = true;
+        $result['use_config.default_sort_by']['default'] = true;
+        $result['use_config.filter_price_range']['default'] = true;
+
         return $result;
     }
 }
