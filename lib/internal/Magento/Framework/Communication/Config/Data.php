a83784e00d384c0724fceeae62c4225eb27b1189
Arkadii Chyzhov <achyzhov@ebay.com>
2015-12-14 18:23:06 +0200

    MAGETWO-46739: Add validation to Magento:Framework:MessageQueue::Reader

--- lib/internal/Magento/Framework/Communication/Config/Data.php
+++ lib/internal/Magento/Framework/Communication/Config/Data.php
@@ -15,12 +15,13 @@ class Data extends \Magento\Framework\Config\Data
      *
      * @param \Magento\Framework\Communication\Config\Reader\XmlReader $xmlReader
      * @param \Magento\Framework\Config\CacheInterface $cache
+     * @param \Magento\Framework\Communication\Config\Reader\EnvReader $envReader
      * @param string $cacheId
      */
     public function __construct(
         \Magento\Framework\Communication\Config\Reader\XmlReader $xmlReader,
-        \Magento\Framework\Communication\Config\Reader\EnvReader $envReader,
         \Magento\Framework\Config\CacheInterface $cache,
+        \Magento\Framework\Communication\Config\Reader\EnvReader $envReader,
         $cacheId = 'communication_config_cache'
     ) {
         parent::__construct($xmlReader, $cache, $cacheId);
