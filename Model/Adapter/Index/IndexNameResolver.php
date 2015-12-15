1c8d9647c204faa7ee99302873035048ca8d4740
Sergey Plakhonin <sergey.plahonin@phoenix-media.eu>
2015-12-15 09:42:06 +0200

    PRD-99: Merging PR from sprint 4

--- Model/Adapter/Index/IndexNameResolver.php
+++ Model/Adapter/Index/IndexNameResolver.php
@@ -136,4 +136,5 @@ class IndexNameResolver
             }
         }
         return $storeIndex;
-    }}
+    }
+}
---
+++ Model/Adapter/Index/SearchIndexNameResolver.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Elasticsearch\Model\Adapter\Index;
+
+use Magento\Elasticsearch\Model\Client\Elasticsearch as ElasticsearchClient;
+use Magento\Elasticsearch\SearchAdapter\ConnectionManager;
+
+/**
+ * Alias name resolver
+ */
+class SearchIndexNameResolver
+{
+    /**
+     * @var \Magento\Elasticsearch\Model\Config
+     */
+    protected $clientConfig;
+
+    /**
+     * @var ElasticsearchClient
+     */
+    protected $client;
+
+    /**
+     * @var \Psr\Log\LoggerInterface
+     */
+    protected $logger;
+
+    /**
+     * Constructor for Index Name Resolver.
+     *
+     * @param \Magento\Elasticsearch\Model\Config $clientConfig
+     * @param \Psr\Log\LoggerInterface $logger
+     * @param array $options
+     *
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    public function __construct(
+        ConnectionManager $connectionManager,
+        \Magento\Elasticsearch\Model\Config $clientConfig,
+        \Psr\Log\LoggerInterface $logger,
+        $options = []
+    )
+    {
+        $this->connectionManager = $connectionManager;
+        $this->clientConfig = $clientConfig;
+        $this->logger = $logger;
+
+        try {
+            $this->client = $this->connectionManager->getConnection($options);
+        } catch (\Exception $e) {
+            $this->logger->critical($e);
+            throw new \Magento\Framework\Exception\LocalizedException(
+                __('We were unable to perform the search because of a search engine misconfiguration.')
+            );
+        }
+    }
+
+    /**
+     * Returns the index name.
+     *
+     * @param int $storeId
+     * @param string $entityType
+     * @return string
+     */
+    public function getIndexName($storeId, $entityType)
+    {
+        return $this->clientConfig->getIndexerPrefix() . '_' . $storeId . '_' . $entityType;
+    }
+}
