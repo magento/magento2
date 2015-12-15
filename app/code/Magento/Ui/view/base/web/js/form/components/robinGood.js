6110afe5edce670e27af7ae96a39c5815ace9dd1
Olga Nakonechna <onakonechna@ebay.com>
2015-12-15 14:18:06 +0200

    MAGETWO-46441: Make ability to insert grid inside form

---
+++ app/code/Magento/Ui/view/base/web/js/form/components/robinGood.js
@@ -0,0 +1,55 @@
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+define([
+    'uiElement'
+], function (Element) {
+    'use strict';
+
+    return Element.extend({
+        defaults: {
+            listens: {
+                value: 'updateExternalValue',
+                externalValue: 'updateValue'
+            },
+
+            links:{
+                value: '${ $.provider }:${ $.dataScope }',
+                externalValue: '${ $.externalProvider }:value'
+            },
+
+            valuesFormatter: null//must be plugged-in as a module
+        },
+
+        /**
+         * Invokes initialize method of parent class,
+         * contains initialization logic
+         */
+        initialize: function () {
+            this._super();
+            this.observe('value', 'externalValue');
+
+            return this;
+        },
+
+        updateExternalValue: function (val) {
+            if (this.valuesFormatter && this.valuesFormatter.updateExternalValue) {
+                this.set('externalValue', valuesFormatter.updateExternalValue(val));
+            }
+            else {
+                this.set('externalValue', val);
+            }
+        },
+
+        updateValue: function (extVal) {
+            if (this.valuesFormatter && this.valuesFormatter.updateValue) {
+                this.set('value', valuesFormatter.updateValue(extVal));
+            }
+            else {
+                this.set('value', extVal);
+            }
+        }
+    });
+});
