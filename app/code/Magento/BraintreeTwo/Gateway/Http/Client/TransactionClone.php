33f42531c8978cd90de2387c3249b2a8918b37ac
Ievgen Sentiabov <isentiabov@magento.com>
2015-12-15 15:17:17 +0200

    MAGETWO-46360: Implement processing invoices for Braintree
     - Moved main logic for http client to abstract class
     - Changed TransactionFactory usage to TransactionRepositoryInterface

--- app/code/Magento/BraintreeTwo/Gateway/Http/Client/TransactionClone.php
+++ app/code/Magento/BraintreeTwo/Gateway/Http/Client/TransactionClone.php
@@ -7,39 +7,20 @@ namespace Magento\BraintreeTwo\Gateway\Http\Client;
 
 use Magento\BraintreeTwo\Gateway\Request\CaptureDataBuilder;
 use Magento\Eway\Gateway\Request\PaymentDataBuilder;
-use Magento\Payment\Gateway\Http\ClientException;
-use Magento\Payment\Gateway\Http\TransferInterface;
 
 /**
  * Class TransactionClone
  */
-class TransactionClone extends TransactionSale
+class TransactionClone extends AbstractTransaction
 {
     /**
      * @inheritdoc
      */
-    public function placeRequest(TransferInterface $transferObject)
+    protected function process(array $data)
     {
-        $data = $transferObject->getBody();
-        $log = [
-            'request' => $data,
-            'client' => self::class
-        ];
-        $response['object'] = [];
         $transactionId = $data[CaptureDataBuilder::TRANSACTION_ID];
         unset($data[CaptureDataBuilder::TRANSACTION_ID]);
 
-        try {
-            $response['object'] = $this->transaction->cloneTransaction($transactionId, $data);
-        } catch (\Exception $e) {
-            throw new ClientException(__(
-                $e->getMessage() ?: 'Sorry, but something went wrong'
-            ));
-        } finally {
-            $log['response'] = (array) $response['object'];
-            $this->logger->debug($log);
-        }
-
-        return $response;
+        return $this->transaction->cloneTransaction($transactionId, $data);
     }
 }
