d046859c535a0adab1f58029e8de49dbd763062b
Arkadii Chyzhov <achyzhov@ebay.com>
2015-12-17 17:13:33 +0200

    MAGETWO-46736: Fix unit tests

--- ConfigTest.php
+++ ConfigTest.php
@@ -3,7 +3,7 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\Framework\Communication;
+namespace Magento\Framework\MessageQueue;
 
 /**
  * Test of communication configuration reading and parsing.
