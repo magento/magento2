ad76b7c6905f7d2e343b88537ffa56971b89af6d
Ievgen Sentiabov <isentiabov@magento.com>
2015-12-17 14:36:06 +0200

    MAGETWO-46340: Place order using vault
     - Added vault ui component
     - Created command to get payment nonce

--- app/code/Magento/BraintreeTwo/Controller/Payment/GetNonce.php
+++ app/code/Magento/BraintreeTwo/Controller/Payment/GetNonce.php
@@ -5,16 +5,14 @@
  */
 namespace Magento\BraintreeTwo\Controller\Payment;
 
-use Magento\BraintreeTwo\Model\Adapter\BraintreeAdapter;
+use Magento\BraintreeTwo\Gateway\Command\GetPaymentNonceCommand;
 use Magento\Customer\Model\Session;
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
 use Magento\Framework\Controller\ResultFactory;
 use Magento\Framework\Controller\ResultInterface;
 use Magento\Framework\Webapi\Exception;
-use Magento\Vault\Api\PaymentTokenManagementInterface;
 use Psr\Log\LoggerInterface;
-use Braintree\Result\Successful;
 
 /**
  * Class GetNonce
@@ -32,34 +30,26 @@ class GetNonce extends Action
     private $session;
 
     /**
-     * @var PaymentTokenManagementInterface
+     * @var GetPaymentNonceCommand
      */
-    private $tokenManagement;
-
-    /**
-     * @var BraintreeAdapter
-     */
-    private $adapter;
+    private $command;
 
     /**
      * @param Context $context
      * @param LoggerInterface $logger
      * @param Session $session
-     * @param PaymentTokenManagementInterface $tokenManagement
-     * @param BraintreeAdapter $adapter
+     * @param GetPaymentNonceCommand $command
      */
     public function __construct(
         Context $context,
         LoggerInterface $logger,
         Session $session,
-        PaymentTokenManagementInterface $tokenManagement,
-        BraintreeAdapter $adapter
+        GetPaymentNonceCommand $command
     ) {
         parent::__construct($context);
         $this->logger = $logger;
         $this->session = $session;
-        $this->tokenManagement = $tokenManagement;
-        $this->adapter = $adapter;
+        $this->command = $command;
     }
 
     /**
@@ -72,65 +62,22 @@ class GetNonce extends Action
         try {
             $publicHash = $this->getRequest()->getParam('public_hash');
             if (!$publicHash) {
-                return $this->processInvalidParamRequest($response);
+                throw new \Exception('The "public_hash" should not be empty');
             }
 
             $customerId = $this->session->getCustomerId();
             if (!$customerId) {
-                return $this->processInvalidCustomer($response);
+                throw new \Exception('User not allowed to perform this action');
             }
-            $paymentToken = $this->tokenManagement->getByPublicHash($publicHash, $customerId);
-            if (!$paymentToken) {
-                return $this->processNotFoundPaymentToken($response);
-            }
-            $result = $this->adapter->createNonce($paymentToken->getGatewayToken());
-            if (!$result instanceof Successful || !$result->success) {
-                return $this->processBadRequest($response);
-            }
-            $response->setData(['paymentMethodNonce' => $result->paymentMethodNonce->nonce]);
+            $result = $this->command
+                ->execute(['publicHash' => $publicHash, 'customerId' => $customerId])
+                ->get();
+            $response->setData(['paymentMethodNonce' => $result['paymentMethodNonce']]);
+
         } catch (\Exception $e) {
             $this->logger->critical($e);
             return $this->processBadRequest($response);
         }
-
-        return $response;
-    }
-
-    /**
-     * Return response for invalid publish hash param
-     * @param ResultInterface $response
-     * @return ResultInterface
-     */
-    private function processInvalidParamRequest(ResultInterface $response)
-    {
-        $response->setHttpResponseCode(Exception::HTTP_BAD_REQUEST);
-        $response->setData(['message' => __('The "public_hash" should not be empty')]);
-
-        return $response;
-    }
-
-    /**
-     * Return response for invalid customer session
-     * @param ResultInterface $response
-     * @return ResultInterface
-     */
-    private function processInvalidCustomer(ResultInterface $response)
-    {
-        $response->setHttpResponseCode(Exception::HTTP_UNAUTHORIZED);
-        $response->setData(['message' => __('You are not allowed to perform this action')]);
-
-        return $response;
-    }
-
-    /**
-     * Return response for not found payment token
-     * @param ResultInterface $response
-     * @return ResultInterface
-     */
-    private function processNotFoundPaymentToken(ResultInterface $response)
-    {
-        $response->setHttpResponseCode(Exception::HTTP_NOT_FOUND);
-        $response->setData(['message' => __('No available payment tokens')]);
 
         return $response;
     }
