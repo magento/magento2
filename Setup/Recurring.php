26606b851cf2356121f0abd7b66e8868e9d38756
Hayder Sharhan <hsharhan@ebay.com>
2015-12-18 17:00:28 -0600

    MAGETWO-46009: [MQ] Topology is not updated after module installation
    - Removed InstallData because it is useless now that we have Recurring

--- Setup/Recurring.php
+++ Setup/Recurring.php
@@ -11,8 +11,17 @@ use Magento\Framework\Setup\InstallSchemaInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\SchemaSetupInterface;
 
+/**
+ * Class Recurring
+ *
+ */
 class Recurring implements InstallSchemaInterface
 {
+    /**
+     * @var \Magento\Amqp\Model\Topology
+     */
+    protected $topology;
+
     /**
      * @param Topology $topology
      */
