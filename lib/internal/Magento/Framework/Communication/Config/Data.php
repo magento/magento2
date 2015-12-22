504bbb92a8beba74466febe05d366ed69b3ac650
Arkadii Chyzhov <achyzhov@ebay.com>
2015-12-22 15:16:18 +0200

    MAGETWO-46364: Final Functionality Check and Code Refactoring

--- lib/internal/Magento/Framework/Communication/Config/Data.php
+++ lib/internal/Magento/Framework/Communication/Config/Data.php
@@ -13,18 +13,18 @@ class Data extends \Magento\Framework\Config\Data
     /**
      * Initialize dependencies.
      *
-     * @param \Magento\Framework\Communication\Config\Reader\XmlReader $xmlReader
+     * @param \Magento\Framework\Communication\Config\Reader\XmlReader $reader
      * @param \Magento\Framework\Config\CacheInterface $cache
      * @param \Magento\Framework\Communication\Config\Reader\EnvReader $envReader
      * @param string $cacheId
      */
     public function __construct(
-        \Magento\Framework\Communication\Config\Reader\XmlReader $xmlReader,
+        \Magento\Framework\Communication\Config\Reader\XmlReader $reader,
         \Magento\Framework\Config\CacheInterface $cache,
         \Magento\Framework\Communication\Config\Reader\EnvReader $envReader,
         $cacheId = 'communication_config_cache'
     ) {
-        parent::__construct($xmlReader, $cache, $cacheId);
+        parent::__construct($reader, $cache, $cacheId);
         $this->merge($envReader->read());
     }
 }
