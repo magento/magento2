c25e5b1ef33047f85c25642362bb841531936137
Dale Sikkema <dsikkema@ebay.com>
2015-12-23 11:22:34 -0600

    MAGETWO-47009: Github #2674 Plugins/Interceptors Don't Work with Early Stage Single Instance Objects in Developer Mode

--- lib/internal/Magento/Framework/App/Test/Unit/ObjectManager/Environment/DeveloperTest.php
+++ lib/internal/Magento/Framework/App/Test/Unit/ObjectManager/Environment/DeveloperTest.php
@@ -5,6 +5,7 @@
  */
 namespace Magento\Framework\App\Test\Unit\ObjectManager\Environment;
 
+use Magento\Framework\App\ObjectManager;
 use Magento\Framework\App\ObjectManager\Environment\Developer;
 
 class DeveloperTest extends \PHPUnit_Framework_TestCase
@@ -28,5 +29,62 @@ class DeveloperTest extends \PHPUnit_Framework_TestCase
     public function testGetObjectManagerConfigLoader()
     {
         $this->assertNull($this->_developer->getObjectManagerConfigLoader());
+    }
+
+    public function testConfigureObjectManager()
+    {
+        try {
+            $origObjectManager = ObjectManager::getInstance();
+        } catch (\Exception $e) {
+            $origObjectManager = null;
+        }
+
+
+        $objectManagerMock = $this->getMockBuilder('Magento\Framework\App\ObjectManager')
+            ->disableOriginalConstructor()
+            ->getMock();
+        ObjectManager::setInstance($objectManagerMock);
+        $diConfigMock = $this->getMockBuilder('\Magento\Framework\Interception\ObjectManager\ConfigInterface')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $configLoaderMock = $this->getMockBuilder('Magento\Framework\App\ObjectManager\ConfigLoader')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $configLoaderMock->expects($this->any())->method('load')->willReturn([]);
+        $omReturnMap = [
+            ['Magento\Framework\App\ObjectManager\ConfigLoader',  $configLoaderMock],
+            [
+                'Magento\Framework\Config\ScopeInterface',
+                $this->getMockBuilder('Magento\Framework\Config\ScopeInterface')
+                    ->disableOriginalConstructor()
+                    ->getMock()
+            ],
+            [
+                'Magento\Framework\App\ObjectManager\ConfigCache',
+                $this->getMockBuilder('Magento\Framework\App\ObjectManager\ConfigCache')
+                    ->disableOriginalConstructor()
+                    ->getMock()
+            ],
+            [
+                'Magento\Framework\Interception\Config\Config',
+                $this->getMockBuilder('Magento\Framework\Interception\Config\Config')
+                    ->disableOriginalConstructor()
+                    ->getMock()
+            ]
+        ];
+        $objectManagerMock->expects($this->any())->method('get')->willReturnMap($omReturnMap);
+
+        $sharedInstances = ['class_name' => 'shared_object'];
+        $this->_developer->configureObjectManager($diConfigMock, $sharedInstances);
+
+        $expectedSharedInstances = [
+            'class_name' => 'shared_object',
+            'Magento\Framework\ObjectManager\ConfigLoaderInterface' =>  $configLoaderMock
+        ];
+        $this->assertSame($expectedSharedInstances, $sharedInstances);
+        if (isset($origObjectManager)) {
+            ObjectManager::setInstance($origObjectManager);
+        }
     }
 }
