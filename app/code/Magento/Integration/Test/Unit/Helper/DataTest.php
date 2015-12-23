38642e70d51fd3200cb96e7e222d977c922db47c
Hayder Sharhan <hsharhan@ebay.com>
2015-12-23 15:18:30 -0600

    MAGETWO-46021: [Integration] Unexpected structure of integration/acl.xml
    - Refactored location of methods that I previously placed in helper. Helper now is back to its original implementation.
    - Fixed tests.

--- app/code/Magento/Integration/Test/Unit/Helper/DataTest.php
+++ app/code/Magento/Integration/Test/Unit/Helper/DataTest.php
@@ -25,21 +25,6 @@ class DataTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($expectedData, $this->dataHelper->mapResources($testData));
     }
 
-    public function testHashResources()
-    {
-        $testData = require __DIR__ . '/_files/acl.php';
-        $expectedData = require __DIR__ . '/_files/acl-hash.php';
-        $this->assertEquals($expectedData, $this->dataHelper->hashResources($testData));
-    }
-
-    /**
-     * @dataProvider hashDataProvider
-     */
-    public function testAddParents($hashData, $expectedResult)
-    {
-        $this->assertEquals($expectedResult, $this->dataHelper->addParents($hashData, 'Magento_Sales::actions'));
-    }
-
     /**
      * @dataProvider integrationDataProvider
      */
@@ -70,65 +55,6 @@ class DataTest extends \PHPUnit_Framework_TestCase
                     Integration::SETUP_TYPE => 0,
                 ],
                 false,
-            ]
-        ];
-    }
-
-    public function hashDataProvider()
-    {
-        return [
-            [
-                [
-                    'Magento_Sales::sales' =>
-                        [
-                            'Magento_Sales::sales_operation' =>
-                                [
-                                    'Magento_Sales::sales_order' =>
-                                        [
-                                            'Magento_Sales::actions' =>
-                                                [
-                                                    'Magento_Sales::create' =>
-                                                        [],
-                                                    'Magento_Sales::actions_view' =>
-                                                        [],
-                                                    'Magento_Sales::email' =>
-                                                        [],
-                                                    'Magento_Sales::reorder' =>
-                                                        [],
-                                                    'Magento_Sales::actions_edit' =>
-                                                        []
-                                                ]
-                                        ],
-                                    'Magento_Sales::sales_invoice' =>
-                                        [],
-                                    'Magento_Sales::shipment' =>
-                                        [],
-                                ],
-                        ],
-                    'Magento_Catalog::catalog' =>
-                        [
-                            'Magento_Catalog::catalog_inventory' =>
-                                [
-                                    'Magento_Catalog::products' =>
-                                        [],
-                                    'Magento_Catalog::categories' =>
-                                        [],
-                                ],
-                        ],
-                    'Magento_Customer::customer' =>
-                        [
-                            'Magento_Customer::manage' =>
-                                [],
-                            'Magento_Customer::online' =>
-                                [],
-                        ]
-                ],
-                [
-                    'Magento_Sales::sales',
-                    'Magento_Sales::sales_operation',
-                    'Magento_Sales::sales_order',
-                    'Magento_Sales::actions'
-                ]
             ]
         ];
     }
