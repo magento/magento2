aa98d0ceebee5ecce84ea6733f1d474e607a5541
Hayder Sharhan <hsharhan@ebay.com>
2015-12-23 23:18:30 +0200

    MAGETWO-46021: [Integration] Unexpected structure of integration/acl.xml
    - Refactored location of methods that I previously placed in helper. Helper now is back to its original implementation.
    - Fixed tests.

--- app/code/Magento/Integration/Test/Unit/Model/Config/Integration/ConverterTest.php
+++ app/code/Magento/Integration/Test/Unit/Model/Config/Integration/ConverterTest.php
@@ -18,16 +18,9 @@ class ConverterTest extends \PHPUnit_Framework_TestCase
     /** @var \Magento\Framework\Acl\AclResource\ProviderInterface|\PHPUnit_Framework_MockObject_MockObject */
     protected $resourceProviderMock;
 
-    /** @var \Magento\Integration\Helper\Data|\PHPUnit_Framework_MockObject_MockObject */
-    protected $integrationDataMock;
-
     public function setUp()
     {
         $this->resourceProviderMock = $this->getMockBuilder('Magento\Framework\Acl\AclResource\ProviderInterface')
-            ->disableOriginalConstructor()
-            ->setMethods([])
-            ->getMock();
-        $this->integrationDataMock = $this->getMockBuilder('Magento\Integration\Helper\Data')
             ->disableOriginalConstructor()
             ->setMethods([])
             ->getMock();
@@ -35,33 +28,19 @@ class ConverterTest extends \PHPUnit_Framework_TestCase
         $this->model = $objectManagerHelper->getObject(
             'Magento\Integration\Model\Config\Integration\Converter',
             [
-                'resourceProvider' => $this->resourceProviderMock,
-                'integrationData' => $this->integrationDataMock
+                'resourceProvider' => $this->resourceProviderMock
             ]
         );
     }
 
     public function testConvert()
     {
-        $aclResources = [[], [
-            'children' => [
-                'data'
-            ]
-        ]];
-        $aclHashTree = ['data'];
+        $aclResources = require __DIR__ . '/_files/acl.php';
         $inputData = new \DOMDocument();
         $inputData->load(__DIR__ . '/_files/api.xml');
         $expectedResult = require __DIR__ . '/_files/api.php';
         $this->resourceProviderMock->expects($this->once())->method('getAclResources')->willReturn($aclResources);
-        $this->integrationDataMock->expects($this->once())->method('hashResources')->willReturn($aclHashTree);
-        $this->integrationDataMock->expects($this->at(1))->method('addParents')
-            ->willReturn([$expectedResult['TestIntegration1']['resource'][0],
-                          $expectedResult['TestIntegration1']['resource'][1]]);
-        $this->integrationDataMock->expects($this->at(2))->method('addParents')
-            ->willReturn([$expectedResult['TestIntegration1']['resource'][2],
-                          $expectedResult['TestIntegration1']['resource'][3]]);
-        $this->integrationDataMock->expects($this->at(3))->method('addParents')
-            ->willReturn([$expectedResult['TestIntegration2']['resource'][0]]);
+
         $this->assertEquals($expectedResult, $this->model->convert($inputData));
     }
 }
