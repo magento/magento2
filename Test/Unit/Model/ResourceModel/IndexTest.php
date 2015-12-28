767b0c3f0797d5d1622926f9af68254e18978717
Sergey Plakhonin <sergey.plahonin@phoenix-media.eu>
2015-12-28 19:22:22 +0200

    PRD-101: Re-factor Indexer Implementation to easily support search CMS (or other entities)

--- Test/Unit/Model/ResourceModel/IndexTest.php
+++ Test/Unit/Model/ResourceModel/IndexTest.php
@@ -418,7 +418,10 @@ class IndexTest extends \PHPUnit_Framework_TestCase
 
         $this->assertInternalType(
             'array',
-            $this->model->getFullProductIndexData([1])
+            $this->model->getFullProductIndexData(1, [
+                    1 => '240-LV04'
+                ]
+            )
         );
     }
 
