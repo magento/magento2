7eaf61fd1dc0b6b3b0c6f8d2dc1ee3bd9362ac3c
Oleksii Pugach <alexey.pugach@phoenix-media.eu>
2015-12-30 14:59:12 +0200

    PRD-105: Merge PR from Sprint 5

---
+++ Model/DataProvider/Suggestions.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\AdvancedSearch\Model\DataProvider;
+
+use Magento\Search\Model\QueryInterface;
+use Magento\AdvancedSearch\Model\SuggestedQueriesInterface;
+
+class Suggestions implements SuggestedQueriesInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function isResultsCountEnabled()
+    {
+        return false;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getItems(QueryInterface $query)
+    {
+        return [];
+    }
+}
