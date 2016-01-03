a191664a1d45b866e5ebada312d4fa5102234af9
wanpower <wanpower@qq.com>
2016-01-04 00:31:43 +0800

    change v1.0 to v2.0 for documentation paths, and fix two 404 pages in documentation.

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -15,12 +15,12 @@ umask(0);
 if (version_compare(phpversion(), '5.5.0', '<') === true) {
     if (PHP_SAPI == 'cli') {
         echo 'Magento supports PHP 5.5.0 or later. ' .
-            'Please read http://devdocs.magento.com/guides/v1.0/install-gde/system-requirements.html';
+            'Please read http://devdocs.magento.com/guides/v2.0/install-gde/system-requirements.html';
     } else {
         echo <<<HTML
 <div style="font:12px/1.35em arial, helvetica, sans-serif;">
     <p>Magento supports PHP 5.5.0 or later. Please read
-    <a target="_blank" href="http://devdocs.magento.com/guides/v1.0/install-gde/system-requirements.html">
+    <a target="_blank" href="http://devdocs.magento.com/guides/v2.0/install-gde/system-requirements.html">
     Magento System Requirements</a>.
 </div>
 HTML;
