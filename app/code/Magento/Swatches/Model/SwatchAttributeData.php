cc19dba9c4067ec8be61b8d9cb2022c8668f3026
Leonid Poluyanov <lpoluyanov@ebay.com>
2016-01-03 17:35:44 +0200

    MAGETWO-45880: Text swatch labels not showing correctly on product page

---
+++ app/code/Magento/Swatches/Model/SwatchAttributeData.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Swatches\Model;
+
+use \Magento\ConfigurableProduct\Model\ConfigurableAttributeData as ConfigurableAttributeData;
+use Magento\Catalog\Model\Product;
+/**
+ * Class SwatchAttributeData
+ */
+class SwatchAttributeData extends ConfigurableAttributeData
+{
+    /**
+     * Get product attributes
+     *
+     * @param Product $product
+     * @param array $options
+     * @return array
+     */
+    public function getAttributesData(Product $product, array $options = [])
+    {
+        $defaultValues = [];
+        $attributes = [];
+        foreach ($product->getTypeInstance()->getConfigurableAttributes($product) as $attribute) {
+            $attributeOptionsData = $this->getAttributeOptionsData($attribute, $options);
+            if ($attributeOptionsData) {
+                $productAttribute = $attribute->getProductAttribute();
+                $attributeId = $productAttribute->getId();
+                $attributes[$attributeId] = [
+                    'id' => $attributeId,
+                    'code' => $productAttribute->getAttributeCode(),
+                    'label' => $productAttribute->getStoreLabel($product->getStoreId()),
+                    'options' => $attributeOptionsData,
+                ];
+                $defaultValues[$attributeId] = $this->getAttributeConfigValue($attributeId, $product);
+            }
+        }
+        return [
+            'attributes' => $attributes,
+            'defaultValues' => $defaultValues,
+        ];
+    }
+}
