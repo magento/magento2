c51a3c55d9ac8edec1528847ff579dc09be6ae25
Gurzhyi, Andrii <agurzhyi@magento.com>
2016-01-04 20:38:08 +0200

    MAGETWO-47078: Mark flow for In-Context Checkout with PayPal
    - Fixed tests

--- app/code/Magento/Paypal/Test/Unit/Block/Express/ShortcutTest.php
+++ app/code/Magento/Paypal/Test/Unit/Block/Express/ShortcutTest.php
@@ -5,6 +5,10 @@
  */
 namespace Magento\Paypal\Test\Unit\Block\Express;
 
+use Magento\Paypal\Block\Express\Shortcut;
+use Magento\Paypal\Model\ConfigFactory;
+use Magento\Paypal\Model\Config;
+
 class ShortcutTest extends \PHPUnit_Framework_TestCase
 {
     /**
@@ -12,10 +16,39 @@ class ShortcutTest extends \PHPUnit_Framework_TestCase
      */
     const ALIAS = 'alias';
 
+    /**
+     * @var ConfigFactory|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $_paypalConfigFactory;
+
     public function testGetAlias()
     {
+        $paypalConfigFactoryMock = $this->getMockBuilder(ConfigFactory::class)
+            ->setMethods(['create'])
+            ->disableOriginalConstructor()
+            ->getMockForAbstractClass();
+        $configMock = $this->getMockBuilder(Config::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+
+        $paypalConfigFactoryMock->expects(self::once())
+            ->method('create')
+            ->willReturn($configMock);
+
+        $configMock->expects(self::once())
+            ->method('setMethod')
+            ->with('test-method');
+
         $helper = new \Magento\Framework\TestFramework\Unit\Helper\ObjectManager($this);
-        $model = $helper->getObject('Magento\Paypal\Block\Express\Shortcut', ['alias' => self::ALIAS]);
+        $model = $helper->getObject(
+            Shortcut::class,
+            [
+                'alias' => self::ALIAS,
+                'paymentMethodCode' => 'test-method',
+                'paypalConfigFactory' => $paypalConfigFactoryMock
+            ]
+        );
         $this->assertEquals(self::ALIAS, $model->getAlias());
     }
 }
