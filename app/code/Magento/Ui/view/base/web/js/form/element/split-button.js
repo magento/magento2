c95c3620c52a01914ef872861e50332ff7f77255
Ihor Melnychenko <imelnychenko@ebay.com>
2016-01-04 16:01:56 +0200

    MAGETWO-47244: Create split button component

---
+++ app/code/Magento/Ui/view/base/web/js/form/element/split-button.js
@@ -0,0 +1,43 @@
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+define([
+    'uiComponent'
+], function (Component) {
+    'use strict';
+
+    return Component.extend({
+        defaults: {
+            opened: false
+        },
+
+        /**
+         * Initializes observable properties.
+         *
+         * @returns {Collapsible} Chainable.
+         */
+        initObservable: function () {
+            this._super()
+                .observe('opened');
+
+            return this;
+        },
+        initialize: function(){
+            this._super();
+
+            return this;
+        },
+
+        /**
+         * Toggles value of the 'opened' property.
+         *
+         * @returns {Collapsible} Chainable.
+         */
+        toggleOpened: function () {
+            this.opened(!this.opened());
+
+            return this;
+        }
+    });
+});
