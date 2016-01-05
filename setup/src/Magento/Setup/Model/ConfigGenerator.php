40576c49809a1b70f4ea658d2e3973595c08ec25
Eddie Lau <kahlau@ebay.com>
2016-01-05 10:11:03 -0600

    MAGETWO-47531: [Github] Running setup:config:set doesn't just update, but also deletes values, running it a second time fixes it (kind of)
    - implemented save mode for ConfigData class

--- setup/src/Magento/Setup/Model/ConfigGenerator.php
+++ setup/src/Magento/Setup/Model/ConfigGenerator.php
@@ -262,6 +262,7 @@ class ConfigGenerator
             }
             $configData->set(ConfigOptionsListConstants::CONFIG_PATH_CACHE_HOSTS, $hosts);
         }
+        $configData->setOverrideWhenSave(true);
         return $configData;
     }
 }
