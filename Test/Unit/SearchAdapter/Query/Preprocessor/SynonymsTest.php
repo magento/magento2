2b3e55dcfaa185661805c557d17f02fee24ac835
PhoenixDev - DR <dmitry.rodnykh@phoenix-media.eu>
2016-01-05 12:28:18 +0200

    PRD-106: Merge PR from Sprint 6

---
+++ Test/Unit/SearchAdapter/Query/Preprocessor/SynonymsTest.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Elasticsearch\Test\Unit\SearchAdapter\Query\Preprocessor;
+
+use Magento\Search\Api\SynonymAnalyzerInterface;
+use Magento\Elasticsearch\SearchAdapter\Query\Preprocessor\Synonyms as SynonymsPreprocessor;
+use Magento\Framework\TestFramework\Unit\Helper\ObjectManager as ObjectManagerHelper;
+
+class SynonymsTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var SynonymsPreprocessor
+     */
+    protected $model;
+
+    /**
+     * @var SynonymAnalyzerInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $synonymAnalyzer;
+
+    /**
+     * Set up test environment.
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        $this->synonymAnalyzer = $this->getMockBuilder('Magento\Search\Api\SynonymAnalyzerInterface')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $objectManagerHelper = new ObjectManagerHelper($this);
+        $this->model = $objectManagerHelper->getObject(
+            'Magento\Elasticsearch\SearchAdapter\Query\Preprocessor\Synonyms',
+            [
+                'synonymAnalyzer' => $this->synonymAnalyzer,
+            ]
+        );
+    }
+
+    /**
+     * Test process() method
+     */
+    public function testProcess()
+    {
+        $synonyms = [['red', 'blue']];
+
+        $this->synonymAnalyzer->expects($this->once())
+            ->method('getSynonymsForPhrase')
+            ->willReturn($synonyms);
+
+        $this->assertEquals(
+            'red blue',
+            $this->model->process('blue')
+        );
+    }
+}
