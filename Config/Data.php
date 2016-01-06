8a41efd615ddc2ee88dfd812e5b3e4ffef5fb649
Alexander Paliarush <apaliarush@magento.com>
2016-01-06 18:36:41 +0200

    MAGETWO-47029: [Refactoring] Create Composite Reader instead of injection in Config/Data

--- Config/Data.php
+++ Config/Data.php
@@ -6,8 +6,6 @@
 
 namespace Magento\Framework\MessageQueue\Config;
 
-use Magento\Framework\MessageQueue\Code\Generator\Config\RemoteServiceReader\MessageQueue as RemoteServiceReader;
-
 /**
  * Class for access to MessageQueue configuration data.
  */
@@ -16,26 +14,20 @@ class Data extends \Magento\Framework\Config\Data
     /**
      * Initialize dependencies.
      *
-     * @param \Magento\Framework\MessageQueue\Config\Reader\XmlReader $reader
+     * @param \Magento\Framework\MessageQueue\Config\CompositeReader $reader
      * @param \Magento\Framework\Config\CacheInterface $cache
      * @param \Magento\Framework\MessageQueue\Config\Reader\EnvReader $envReader
      * @param \Magento\Framework\MessageQueue\Config\Reader\EnvReader\Validator $envValidator
-     * @param RemoteServiceReader $remoteServiceReader
      * @param string $cacheId
      */
     public function __construct(
-        \Magento\Framework\MessageQueue\Config\Reader\XmlReader $reader,
+        \Magento\Framework\MessageQueue\Config\CompositeReader $reader,
         \Magento\Framework\Config\CacheInterface $cache,
         \Magento\Framework\MessageQueue\Config\Reader\EnvReader $envReader,
         \Magento\Framework\MessageQueue\Config\Reader\EnvReader\Validator $envValidator,
-        RemoteServiceReader $remoteServiceReader,
         $cacheId = 'message_queue_config_cache'
     ) {
-        $this->merge($remoteServiceReader->read());
         parent::__construct($reader, $cache, $cacheId);
-
-        $envConfigData = $envReader->read();
-        $envValidator->validate($envConfigData, $this->get());
-        $this->merge($envConfigData);
+        $envValidator->validate($envReader->read(), $this->get());
     }
 }
