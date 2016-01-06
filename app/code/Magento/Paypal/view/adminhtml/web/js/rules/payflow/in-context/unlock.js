dc0aabbf83f74189b703b35644dff1d48c9ad14a
Yushkin, Dmytro <dyushkin@magento.com>
2016-01-06 16:40:58 +0200

    MAGETWO-47075: Configuration for In-Context Checkout with PayPal
    - Bugfixing

--- app/code/Magento/Paypal/view/adminhtml/web/js/rules/payflow/in-context/unlock.js
+++ app/code/Magento/Paypal/view/adminhtml/web/js/rules/payflow/in-context/unlock.js
@@ -1,10 +1,4 @@
-/**define([], function () {
-        'use strict';
-
-        return function ($target, $owner, data) {
-            $target.find('tr[id$="_settings_express_checkout"], tr[id$="_payflow_express_checkout"], tr[id$="_settings_payflow_link_express_checkout"]').show();
-        };
-    });
+/**
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
