7282d02c9b27fd09fb25cc568722e40dd4c5e28f
Dale Sikkema <dsikkema@ebay.com>
2016-01-07 13:43:50 -0600

    MAGETWO-46581: X-XSS-Protection is set on IE 8

--- lib/internal/Magento/Framework/App/Response/HeaderProvider/XContentTypeOptions.php
+++ lib/internal/Magento/Framework/App/Response/HeaderProvider/XContentTypeOptions.php
@@ -5,10 +5,13 @@
  */
 namespace Magento\Framework\App\Response\HeaderProvider;
 
-use \Magento\Framework\App\Response\HeaderProvider\AbstractHeaderProvider;
+use Magento\Framework\App\Response\HeaderProvider\AbstractHeaderProvider;
 
 class XContentTypeOptions extends AbstractHeaderProvider
 {
+    /** @var string */
     protected $headerValue = 'nosniff';
+
+    /** @var string */
     protected $headerName = 'X-Content-Type-Options';
 }
