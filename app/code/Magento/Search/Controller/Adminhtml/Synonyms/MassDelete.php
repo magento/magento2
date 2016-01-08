83a904909de7ba15336aa2153c7605b6e5a3778b
Ivan Gavryshko <igavryshko@ebay.com>
2016-01-08 15:46:28 -0600

    MAGETWO-47672: Add Delete function to synonym management
    - used collection for mass delete

--- app/code/Magento/Search/Controller/Adminhtml/Synonyms/MassDelete.php
+++ app/code/Magento/Search/Controller/Adminhtml/Synonyms/MassDelete.php
@@ -7,9 +7,34 @@
 namespace Magento\Search\Controller\Adminhtml\Synonyms;
 
 use Magento\Framework\Controller\ResultFactory;
+use Magento\Backend\App\Action\Context;
+use Magento\Ui\Component\MassAction\Filter;
+use Magento\Search\Model\ResourceModel\SynonymGroup\CollectionFactory;
 
 class MassDelete extends \Magento\Backend\App\Action
 {
+    /**
+     * @var Filter
+     */
+    protected $filter;
+
+    /**
+     * @var CollectionFactory
+     */
+    protected $collectionFactory;
+
+    /**
+     * @param Context $context
+     * @param Filter $filter
+     * @param CollectionFactory $collectionFactory
+     */
+    public function __construct(Context $context, Filter $filter, CollectionFactory $collectionFactory)
+    {
+        $this->filter = $filter;
+        $this->collectionFactory = $collectionFactory;
+        parent::__construct($context);
+    }
+
     /**
      * Execute action
      *
@@ -18,25 +43,17 @@ class MassDelete extends \Magento\Backend\App\Action
      */
     public function execute()
     {
+        $collection = $this->filter->getCollection($this->collectionFactory->create());
+        $collectionSize = $collection->getSize();
+
+        foreach ($collection as $synonymGroup) {
+            $synonymGroup->delete();
+        }
+
+        $this->messageManager->addSuccess(__('A total of %1 record(s) have been deleted.', $collectionSize));
+
         /** @var \Magento\Backend\Model\View\Result\Redirect $resultRedirect */
         $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
-
-        $selected = $this->getRequest()->getParam('selected');
-        if (empty($selected)) {
-            $this->messageManager->addError(__('You haven\'t selected any items!'));
-            return $resultRedirect->setPath('*/*/');
-        }
-        $model = $this->_objectManager->create('Magento\Search\Model\SynonymGroup');
-        foreach ($selected as $itemId) {
-            try {
-                $model->load($itemId);
-                $model->delete();
-            } catch (\Exception $e) {
-                $this->messageManager->addError($e->getMessage());
-            }
-        }
-
-        $this->messageManager->addSuccess(__('A total of %1 record(s) have been deleted.', count($selected)));
         return $resultRedirect->setPath('*/*/');
     }
 }
