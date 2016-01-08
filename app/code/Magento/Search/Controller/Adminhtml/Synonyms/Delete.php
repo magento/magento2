b79b0e40956c46cf227d7b4055d76a205bb46a16
Ivan Gavryshko <igavryshko@ebay.com>
2016-01-08 11:41:43 -0600

    MAGETWO-47672: Add Delete function to synonym management
    - added delete functionality

---
+++ app/code/Magento/Search/Controller/Adminhtml/Synonyms/Delete.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Search\Controller\Adminhtml\Synonyms;
+
+
+class Delete extends \Magento\Backend\App\Action
+{
+    /**
+     * Delete action
+     *
+     * @return \Magento\Backend\Model\View\Result\Redirect
+     */
+    public function execute()
+    {
+        $id = $this->getRequest()->getParam('group_id');
+        /** @var \Magento\Backend\Model\View\Result\Redirect $resultRedirect */
+        $resultRedirect = $this->resultRedirectFactory->create();
+        if ($id) {
+            try {
+                $model = $this->_objectManager->create('Magento\Search\Model\SynonymGroup');
+                $model->load($id);
+                $model->delete();
+                $this->messageManager->addSuccess(__('The synonyms group has been deleted.'));
+                return $resultRedirect->setPath('*/*/');
+            } catch (\Exception $e) {
+                $this->messageManager->addError($e->getMessage());
+                return $resultRedirect->setPath('*/*/');
+            }
+        }
+        $this->messageManager->addError(__('We can\'t find a synonum group to delete.'));
+        return $resultRedirect->setPath('*/*/');
+    }
+}
---
+++ app/code/Magento/Search/Ui/Component/Listing/Column/SynonymActions.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Search\Ui\Component\Listing\Column;
+
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
+use Magento\Ui\Component\Listing\Columns\Column;
+use Magento\Cms\Block\Adminhtml\Page\Grid\Renderer\Action\UrlBuilder;
+use Magento\Framework\UrlInterface;
+
+/**
+ * Class SynonymActions provides grid actions for synonyms
+ */
+class SynonymActions extends Column
+{
+    /** Url path */
+    const SYNONYM_URL_PATH_DELETE = 'search/synonyms/delete';
+    const SYNONYM_URL_PATH_EDIT = 'search/synonyms/edit';
+
+    /** @var UrlBuilder */
+    protected $actionUrlBuilder;
+
+    /** @var UrlInterface */
+    protected $urlBuilder;
+
+    /**
+     * @var string
+     */
+    private $editUrl;
+
+    /**
+     * @param ContextInterface $context
+     * @param UiComponentFactory $uiComponentFactory
+     * @param UrlBuilder $actionUrlBuilder
+     * @param UrlInterface $urlBuilder
+     * @param array $components
+     * @param array $data
+     * @param string $editUrl
+     */
+    public function __construct(
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        UrlBuilder $actionUrlBuilder,
+        UrlInterface $urlBuilder,
+        array $components = [],
+        array $data = [],
+        $editUrl = self::SYNONYM_URL_PATH_EDIT
+    ) {
+        $this->urlBuilder = $urlBuilder;
+        $this->actionUrlBuilder = $actionUrlBuilder;
+        $this->editUrl = $editUrl;
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+    }
+
+    /**
+     * Prepare Data Source
+     *
+     * @param array $dataSource
+     * @return array
+     */
+    public function prepareDataSource(array $dataSource)
+    {
+        if (isset($dataSource['data']['items'])) {
+            foreach ($dataSource['data']['items'] as & $item) {
+                $name = $this->getData('name');
+                $item[$name]['delete'] = [
+                    'href' => $this->urlBuilder->getUrl(self::SYNONYM_URL_PATH_DELETE, ['group_id' => $item['group_id']]),
+                    'label' => __('Delete'),
+                    'confirm' => [
+                        'title' => __('Delete'),
+                        'message' => __('Are you sure you wan\'t to delete this record?')
+                    ]
+                ];
+                $item[$name]['edit'] = [
+                    'href' => $this->urlBuilder->getUrl(self::SYNONYM_URL_PATH_EDIT, ['group_id' => $item['group_id']]),
+                    'label' => __('Edit'),
+                ];
+            }
+        }
+
+        return $dataSource;
+    }
+}
