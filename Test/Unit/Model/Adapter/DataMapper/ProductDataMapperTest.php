8b699fe1eb9eedb47e119374d7b39be34be76146
Sergey Plakhonin <sergey.plahonin@phoenix-media.eu>
2016-01-08 18:01:36 +0200

    PRD-106: Merge PR from Sprint 6

--- Test/Unit/Model/Adapter/DataMapper/ProductDataMapperTest.php
+++ Test/Unit/Model/Adapter/DataMapper/ProductDataMapperTest.php
@@ -167,10 +167,11 @@ class ProductDataMapperTest extends \PHPUnit_Framework_TestCase
      * @param int $storeId
      * @param bool $emptyDate
      * @param string $type
+     * @param string $frontendInput
      *
      * @return void
      */
-    public function testGetMap($productId, $productData, $storeId, $emptyDate, $type)
+    public function testGetMap($productId, $productData, $storeId, $emptyDate, $type, $frontendInput)
     {
         $this->attributeContainerMock->expects($this->any())->method('getAttribute')->will(
             $this->returnValue($this->attribute)
@@ -214,6 +215,9 @@ class ProductDataMapperTest extends \PHPUnit_Framework_TestCase
         $this->attribute->expects($this->any())->method('getBackendType')->will(
             $this->returnValue($type)
         );
+        $this->attribute->expects($this->any())->method('getFrontendInput')->will(
+            $this->returnValue($frontendInput)
+        );
         $this->dateTimeMock->expects($this->any())->method('isEmptyDate')->will(
             $this->returnValue($emptyDate)
         );
@@ -252,14 +256,24 @@ class ProductDataMapperTest extends \PHPUnit_Framework_TestCase
                 ['price'=>'11','created_at'=>'00-00-00 00:00:00', 'color_value'=>'11'],
                 '1',
                 false,
-                'datetime'
+                'datetime',
+                'select',
+            ],
+            [
+                '1',
+                ['price'=>'11','created_at'=>'00-00-00 00:00:00', 'color_value'=>'11'],
+                '1',
+                false,
+                'time',
+                'multiselect',
             ],
             [
                 '1',
                 ['price'=>'11','created_at'=>null,'color_value'=>'11', ],
                 '1',
                 true,
-                'datetime'
+                'datetime',
+                'select',
             ],
             [
                 '1',
@@ -278,14 +292,16 @@ class ProductDataMapperTest extends \PHPUnit_Framework_TestCase
                 ],
                 '1',
                 false,
-                'string'
+                'string',
+                'select',
             ],
             [
                 '1',
                 ['image'=>'11','created_at'=>'00-00-00 00:00:00'],
                 '1',
                 false,
-                'string'
+                'string',
+                'select',
             ],
             [
                 '1',
@@ -315,7 +331,8 @@ class ProductDataMapperTest extends \PHPUnit_Framework_TestCase
                 ],
                 '1',
                 false,
-                'string'
+                'string',
+                'select',
             ],
             [
                 '1',
@@ -350,28 +367,32 @@ class ProductDataMapperTest extends \PHPUnit_Framework_TestCase
                 ],
                 '1',
                 false,
-                'string'
+                'string',
+                'select',
             ],
             [
                 '1',
                 ['quantity_and_stock_status'=>'11','created_at'=>'00-00-00 00:00:00'],
                 '1',
                 false,
-                'string'
+                'string',
+                'select',
             ],
             [
                 '1',
                 ['quantity_and_stock_status'=>['is_in_stock' => '1', 'qty' => '12'],'created_at'=>'00-00-00 00:00:00'],
                 '1',
                 false,
-                'string'
+                'string',
+                'select',
             ],
             [
                 '1',
                 ['price'=>'11','created_at'=>'1995-12-31 23:59:59','options'=>['value1','value2']],
                 '1',
                 false,
-                'string'
+                'string',
+                'select',
             ],
         ];
     }
