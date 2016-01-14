965e0b0c3a2eb6ee5ed9aa9afb3fc1011f40e5f3
Vadim Zubovich <vadim_zubovich@epam.com>
2016-01-15 00:32:41 +0300

    SPM-6: Ensure critical flows require password confirmation
    SPM-21: Password verification for Integration management
    - Added password check functionality

--- app/code/Magento/Integration/Controller/Adminhtml/Integration.php
+++ app/code/Magento/Integration/Controller/Adminhtml/Integration.php
@@ -23,6 +23,10 @@ abstract class Integration extends Action
 
     const REGISTRY_KEY_CURRENT_INTEGRATION = 'current_integration';
 
+    /** Saved API form data session key */
+    const REGISTRY_KEY_CURRENT_RESOURCE = 'current_resource';
+
+
     /**
      * @var \Magento\Framework\Registry
      */
@@ -111,6 +115,21 @@ abstract class Integration extends Action
             return $this;
         } else {
             return parent::_redirect($path, $arguments);
+        }
+    }
+
+    /**
+     * Restore saved form resources
+     */
+    protected function restoreResource()
+    {
+        $restoredFormData = $this->_getSession()->getIntegrationData();
+        if ($restoredFormData) {
+            $resource = isset($restoredFormData['resource']) ? $restoredFormData['resource'] : [];
+            $this->_registry->register(
+                self::REGISTRY_KEY_CURRENT_RESOURCE,
+                ['all_resources' => $restoredFormData['all_resources'], 'resource' => $resource]
+            );
         }
     }
 }
