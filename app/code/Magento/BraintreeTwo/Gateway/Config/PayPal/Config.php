3ce8227c07fa3cc7c3f58ef073fdfb462977896b
Ievgen Sentiabov <isentiabov@magento.com>
2016-01-18 18:04:05 +0200

    MAGETWO-45184: [Refactoring] Accept PayPal Payment using Braintree from One Page Checkout
     - Added tooltips for PayPal Braintree configuration
     - Added method to get PayPal Braintree payment title

--- app/code/Magento/BraintreeTwo/Gateway/Config/PayPal/Config.php
+++ app/code/Magento/BraintreeTwo/Gateway/Config/PayPal/Config.php
@@ -12,6 +12,8 @@ class Config extends \Magento\Payment\Gateway\Config\Config
 {
     const KEY_ACTIVE = 'active';
 
+    const KEY_TITLE = 'title';
+
     const KEY_DISPLAY_ON_SHOPPING_CART = 'display_on_shopping_cart';
 
     const KEY_ALLOW_TO_EDIT_SHIPPING_ADDRESS = 'allow_shipping_address_override';
@@ -66,5 +68,15 @@ class Config extends \Magento\Payment\Gateway\Config\Config
     public function isRequiredBillingAddress()
     {
         return $this->getValue(self::KEY_REQUIRE_BILLING_ADDRESS);
+    }
+
+    /**
+     * Get title of payment
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->getValue(self::KEY_TITLE);
     }
 }
