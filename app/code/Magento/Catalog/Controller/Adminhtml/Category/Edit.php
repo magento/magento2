319feb7b2d47e3db0b5c573a5301a01d09a343d2
Stanislav Lopukhov <slopukhov@ebay.com>
2016-01-21 15:43:08 +0200

    MAGETWO-47746: Cannot open categories if admin user does not have access to default website

--- app/code/Magento/Catalog/Controller/Adminhtml/Category/Edit.php
+++ app/code/Magento/Catalog/Controller/Adminhtml/Category/Edit.php
@@ -59,7 +59,16 @@ class Edit extends \Magento\Catalog\Controller\Adminhtml\Category
             if ($storeId) {
                 $categoryId = (int)$this->storeManager->getStore($storeId)->getRootCategoryId();
             } else {
-                $categoryId = (int)$this->storeManager->getDefaultStoreView()->getRootCategoryId();
+                $defaultStoreView = $this->storeManager->getDefaultStoreView();
+                if ($defaultStoreView) {
+                    $categoryId = (int)$this->storeManager->getDefaultStoreView()->getRootCategoryId();
+                } else {
+                    $stores = $this->storeManager->getStores();
+                    if (count($stores)) {
+                        $store = reset($stores);
+                        $categoryId = (int)$this->storeManager->getStore($store->getId())->getRootCategoryId();
+                    }
+                }
             }
             $this->getRequest()->setParam('id', $categoryId);
         }
