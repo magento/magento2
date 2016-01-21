4437ea9910af2e5093e91394ed9253253c748760
Gurzhyi, Andrii <agurzhyi@magento.com>
2016-01-21 20:33:08 +0200

    MAGETWO-48190: PayPal In-Context pop up is blocked on Product page
    - Remove in-context flow from product page

--- app/code/Magento/Paypal/Block/Express/InContext/Minicart/Button.php
+++ app/code/Magento/Paypal/Block/Express/InContext/Minicart/Button.php
@@ -5,20 +5,105 @@
  */
 namespace Magento\Paypal\Block\Express\InContext\Minicart;
 
+use Magento\Paypal\Model\Config;
+use Magento\Paypal\Model\ConfigFactory;
 use Magento\Paypal\Block\Express\InContext;
+use Magento\Framework\View\Element\Template;
 use Magento\Catalog\Block\ShortcutInterface;
+use Magento\Framework\Locale\ResolverInterface;
+use Magento\Framework\View\Element\Template\Context;
 
 /**
  * Class Button
  */
-class Button extends InContext\Button implements ShortcutInterface
+class Button extends Template implements ShortcutInterface
 {
     const ALIAS_ELEMENT_INDEX = 'alias';
+
+    const PAYPAL_BUTTON_ID = 'paypal-express-in-context-checkout-main';
+
+    const BUTTON_ELEMENT_INDEX = 'button_id';
+
+    const CART_BUTTON_ELEMENT_INDEX = 'add_to_cart_selector';
 
     /**
      * @var bool
      */
     private $isMiniCart = false;
+
+    /**
+     * @var ResolverInterface
+     */
+    private $localeResolver;
+
+    /**
+     * @var Config
+     */
+    private $config;
+
+    /**
+     * @inheritdoc
+     * @param ResolverInterface $localeResolver
+     * @param ConfigFactory $config
+     */
+    public function __construct(
+        Context $context,
+        ResolverInterface $localeResolver,
+        ConfigFactory $configFactory,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+
+        $this->localeResolver = $localeResolver;
+        $this->config = $configFactory->create();
+        $this->config->setMethod(Config::METHOD_EXPRESS);
+    }
+
+    /**
+     * @return bool
+     */
+    private function isInContext()
+    {
+        return (bool)(int) $this->config->getValue('in_context');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function _toHtml()
+    {
+        if (!$this->shouldRender()) {
+            return '';
+        }
+
+        return parent::_toHtml();
+    }
+
+    /**
+     * @return string
+     */
+    public function getContainerId()
+    {
+        return $this->getData(self::BUTTON_ELEMENT_INDEX);
+    }
+
+    /**
+     * @return string
+     */
+    public function getAddToCartSelector()
+    {
+        return $this->getData(self::CART_BUTTON_ELEMENT_INDEX);
+    }
+
+    /**
+     * @return string
+     */
+    public function getImageUrl()
+    {
+        return $this->config->getExpressCheckoutInContextImageUrl(
+            $this->localeResolver->getLocale()
+        );
+    }
 
     /**
      * Get shortcut alias
@@ -46,6 +131,6 @@ class Button extends InContext\Button implements ShortcutInterface
      */
     protected function shouldRender()
     {
-        return parent::shouldRender() && $this->isMiniCart;
+        return $this->isMiniCart;
     }
 }
