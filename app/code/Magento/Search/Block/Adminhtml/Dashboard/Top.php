b0c8a5e074898684132c84b788afda8e2a014282
Maddy Chellathurai <mchellathura@magento.com>
2016-01-22 10:50:08 -0600

    MAGETWO-48145: Remove synonym from search terms
    - fixing tests

--- app/code/Magento/Search/Block/Adminhtml/Dashboard/Top.php
+++ app/code/Magento/Search/Block/Adminhtml/Dashboard/Top.php
@@ -91,7 +91,7 @@ class Top extends \Magento\Backend\Block\Dashboard\Grid
             [
                 'header' => __('Search Term'),
                 'sortable' => false,
-                'index' => 'name',
+                'index' => 'query_text',
                 'renderer' => 'Magento\Backend\Block\Dashboard\Searches\Renderer\Searchquery'
             ]
         );
