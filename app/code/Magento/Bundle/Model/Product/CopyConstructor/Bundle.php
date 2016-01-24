082810141936d493834804c85d02fcf671b24fec
Anton Ohorodnyk <aohorodnyk@ebay.com>
2016-01-24 16:16:37 +0200

    MAGETWO-47594: Cannot duplicate Bundle product with "Radio Buttons" option

--- app/code/Magento/Bundle/Model/Product/CopyConstructor/Bundle.php
+++ app/code/Magento/Bundle/Model/Product/CopyConstructor/Bundle.php
@@ -21,42 +21,7 @@ class Bundle implements \Magento\Catalog\Model\Product\CopyConstructorInterface
             return;
         }
 
-        $product->getTypeInstance()->setStoreFilter($product->getStoreId(), $product);
-        $optionCollection = $product->getTypeInstance()->getOptionsCollection($product);
-        $selectionCollection = $product->getTypeInstance()->getSelectionsCollection(
-            $product->getTypeInstance()->getOptionsIds($product),
-            $product
-        );
-        $optionCollection->appendSelections($selectionCollection);
-
-        $optionRawData = [];
-        $selectionRawData = [];
-
-        $i = 0;
-        foreach ($optionCollection as $option) {
-            $optionRawData[$i] = [
-                'required' => $option->getData('required'),
-                'position' => $option->getData('position'),
-                'type' => $option->getData('type'),
-                'title' => $option->getData('title') ? $option->getData('title') : $option->getData('default_title'),
-                'delete' => '',
-            ];
-            foreach ($option->getSelections() as $selection) {
-                $selectionRawData[$i][] = [
-                    'product_id' => $selection->getProductId(),
-                    'position' => $selection->getPosition(),
-                    'is_default' => $selection->getIsDefault(),
-                    'selection_price_type' => $selection->getSelectionPriceType(),
-                    'selection_price_value' => $selection->getSelectionPriceValue(),
-                    'selection_qty' => $selection->getSelectionQty(),
-                    'selection_can_change_qty' => $selection->getSelectionCanChangeQty(),
-                    'delete' => '',
-                ];
-            }
-            $i++;
-        }
-
-        $duplicate->setBundleOptionsData($optionRawData);
-        $duplicate->setBundleSelectionsData($selectionRawData);
+        $bundleOptions = $product->getExtensionAttributes()->getBundleProductOptions();
+        $duplicate->getExtensionAttributes()->setBundleProductOptions($bundleOptions);
     }
 }
