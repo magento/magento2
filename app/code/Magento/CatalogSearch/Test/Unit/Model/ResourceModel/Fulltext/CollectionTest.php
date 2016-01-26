59995e85ca725899ceb84a5968a22863383f2aea
Paul Lewis <plewis@magento.com>
2016-01-26 14:27:27 -0600

    MAGETWO-48174: Modify AdvancedCollection to use SearchInterface  - Initial implementation and unit test

--- app/code/Magento/CatalogSearch/Test/Unit/Model/ResourceModel/Fulltext/CollectionTest.php
+++ app/code/Magento/CatalogSearch/Test/Unit/Model/ResourceModel/Fulltext/CollectionTest.php
@@ -5,10 +5,9 @@
  */
 namespace Magento\CatalogSearch\Test\Unit\Model\ResourceModel\Fulltext;
 
-use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
-use PHPUnit_Framework_TestCase;
+use Magento\CatalogSearch\Test\Unit\Model\ResourceModel\BaseCollectionTest;
 
-class CollectionTest extends PHPUnit_Framework_TestCase
+class CollectionTest extends BaseCollectionTest
 {
     /**
      * @var \Magento\CatalogSearch\Model\ResourceModel\Fulltext\Collection
@@ -53,72 +52,6 @@ class CollectionTest extends PHPUnit_Framework_TestCase
     public function testGetFacetedData()
     {
         $this->model->getFacetedData('field');
-    }
-
-    /**
-     * @return \PHPUnit_Framework_MockObject_MockObject
-     */
-    protected function getStoreManager()
-    {
-        $store = $this->getMockBuilder('Magento\Store\Model\Store')
-            ->setMethods(['getId'])
-            ->disableOriginalConstructor()
-            ->getMock();
-        $store->expects($this->once())
-            ->method('getId')
-            ->willReturn(1);
-
-        $storeManager = $this->getMockBuilder('Magento\Store\Model\StoreManagerInterface')
-            ->setMethods(['getStore'])
-            ->disableOriginalConstructor()
-            ->getMockForAbstractClass();
-        $storeManager->expects($this->once())
-            ->method('getStore')
-            ->willReturn($store);
-
-        return $storeManager;
-    }
-
-    /**
-     * @return \PHPUnit_Framework_MockObject_MockObject
-     */
-    protected function getUniversalFactory()
-    {
-        $connection = $this->getMockBuilder('Magento\Framework\DB\Adapter\Pdo\Mysql')
-            ->disableOriginalConstructor()
-            ->setMethods(['select'])
-            ->getMockForAbstractClass();
-        $select = $this->getMockBuilder('Magento\Framework\DB\Select')
-            ->disableOriginalConstructor()
-            ->getMock();
-        $connection->expects($this->any())->method('select')->willReturn($select);
-
-        $entity = $this->getMockBuilder('Magento\Eav\Model\Entity\AbstractEntity')
-            ->setMethods(['getConnection', 'getTable', 'getDefaultAttributes', 'getEntityTable'])
-            ->disableOriginalConstructor()
-            ->getMock();
-        $entity->expects($this->once())
-            ->method('getConnection')
-            ->willReturn($connection);
-        $entity->expects($this->exactly(2))
-            ->method('getTable')
-            ->willReturnArgument(0);
-        $entity->expects($this->once())
-            ->method('getDefaultAttributes')
-            ->willReturn(['attr1', 'attr2']);
-        $entity->expects($this->once())
-            ->method('getEntityTable')
-            ->willReturn('table');
-
-        $universalFactory = $this->getMockBuilder('Magento\Framework\Validator\UniversalFactory')
-            ->setMethods(['create'])
-            ->disableOriginalConstructor()
-            ->getMock();
-        $universalFactory->expects($this->once())
-            ->method('create')
-            ->willReturn($entity);
-
-        return $universalFactory;
     }
 
     /**
