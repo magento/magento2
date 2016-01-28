123391af1231d7377ed2a994a5216fe697e4b30e
Partica, Cristian <cpartica@ebay.com>
2016-01-28 16:01:47 -0600

    MAGETWO-48110: Change resource model to use metadata
    - unit tests for SalesRule modifications

--- app/code/Magento/SalesRule/Test/Unit/Model/ResourceModel/RuleTest.php
+++ app/code/Magento/SalesRule/Test/Unit/Model/ResourceModel/RuleTest.php
@@ -6,6 +6,8 @@
 
 namespace Magento\SalesRule\Test\Unit\Model\ResourceModel;
 
+use Magento\SalesRule\Api\Data\RuleInterface;
+
 class RuleTest extends \PHPUnit_Framework_TestCase
 {
     /**
@@ -21,12 +23,32 @@ class RuleTest extends \PHPUnit_Framework_TestCase
     /**
      * @var \PHPUnit_Framework_MockObject_MockObject
      */
+    protected $entityManager;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
     protected $adapter;
 
     /**
      * @var \PHPUnit_Framework_MockObject_MockObject
      */
     protected $select;
+
+    /**
+     * @var \Magento\Framework\App\ResourceConnection|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $resourcesMock;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $transactionManagerMock;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $relationProcessorMock;
 
     public function setUp()
     {
@@ -39,23 +61,46 @@ class RuleTest extends \PHPUnit_Framework_TestCase
             ->disableOriginalConstructor()
             ->getMock();
         $connectionName = 'test';
-        $resources = $this->getMockBuilder('Magento\Framework\App\ResourceConnection')
+        $this->resourcesMock = $this->getMockBuilder('Magento\Framework\App\ResourceConnection')
             ->disableOriginalConstructor()
             ->getMock();
+
+        $this->relationProcessorMock =
+            $this->getMockBuilder('Magento\Framework\Model\ResourceModel\Db\ObjectRelationProcessor')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->transactionManagerMock =
+            $this->getMockBuilder('Magento\Framework\Model\ResourceModel\Db\TransactionManagerInterface')
+            ->disableOriginalConstructor()
+            ->getMock();
+
         $context->expects($this->any())
             ->method('getResources')
-            ->willReturn($resources);
+            ->willReturn($this->resourcesMock);
+
+        $this->entityManager = $this->getMockBuilder('Magento\Framework\Model\EntityManager')
+            ->setMethods(['load', 'save'])
+            ->disableOriginalConstructor()
+            ->getMock();
 
         $this->adapter = $this->getMockBuilder('Magento\Framework\DB\Adapter\AdapterInterface')
             ->disableOriginalConstructor()
             ->getMock();
-        $resources->expects($this->any())
+        $this->resourcesMock->expects($this->any())
             ->method('getConnection')
             ->willReturn($this->adapter);
-        $resources->expects($this->any())
+        $this->resourcesMock->expects($this->any())
             ->method('getTableName')
             ->withAnyParameters()
             ->willReturnArgument(0);
+
+        $context->expects($this->once())
+            ->method('getObjectRelationProcessor')
+            ->willReturn($this->relationProcessorMock);
+        $context->expects($this->once())
+            ->method('getTransactionManager')
+            ->willReturn($this->transactionManagerMock);
 
         $this->select = $this->getMockBuilder('Magento\Framework\DB\Select')
             ->disableOriginalConstructor()
@@ -79,7 +124,8 @@ class RuleTest extends \PHPUnit_Framework_TestCase
             [
                 'context' => $context,
                 'connectionName' => $connectionName,
-                'associatedEntitiesMap' => $associatedEntitiesMap
+                'associatedEntitiesMap' => $associatedEntitiesMap,
+                'entityManager' => $this->entityManager
             ]
         );
     }
@@ -148,5 +194,105 @@ class RuleTest extends \PHPUnit_Framework_TestCase
             ->with('website_ids', $websiteIds);
 
         $this->model->loadWebsiteIds($object);
+    }
+
+    /**
+     * test load
+     */
+    public function testLoad()
+    {
+        $ruleId = 1;
+        /** @var \Magento\Framework\Model\AbstractModel|\PHPUnit_Framework_MockObject_MockObject $abstractModel */
+        $abstractModel = $this->getMockBuilder('Magento\Framework\Model\AbstractModel')
+            ->disableOriginalConstructor()
+            ->getMockForAbstractClass();
+        $this->entityManager->expects($this->once())
+            ->method('load')
+            ->with(RuleInterface::class, $abstractModel, $ruleId);
+        $result = $this->model->load($abstractModel, $ruleId);
+        $this->assertSame($this->model, $result);
+
+    }
+
+    public function testSave()
+    {
+        $connectionMock = $this->getMock('\Magento\Framework\DB\Adapter\AdapterInterface', [], [], '', false);
+        $context = (new \Magento\Framework\TestFramework\Unit\Helper\ObjectManager($this))->getObject(
+            'Magento\Framework\Model\Context'
+        );
+        $registryMock = $this->getMock('\Magento\Framework\Registry', [], [], '', false);
+        $resourceMock = $this->getMock(
+            'Magento\Framework\Model\ResourceModel\Db\AbstractDb',
+            [
+                '_construct',
+                'getConnection',
+                '__wakeup',
+                'getIdFieldName'
+            ],
+            [],
+            '',
+            false
+        );
+        $connectionInterfaceMock = $this->getMock('Magento\Framework\DB\Adapter\AdapterInterface', [], [], '', false);
+        $resourceMock->expects($this->any())
+            ->method('getConnection')
+            ->will($this->returnValue($connectionInterfaceMock));
+        $resourceCollectionMock = $this->getMockBuilder('Magento\Framework\Data\Collection\AbstractDb')
+            ->disableOriginalConstructor()
+            ->getMockForAbstractClass();
+        /** @var \Magento\Framework\Model\AbstractModel|\PHPUnit_Framework_MockObject_MockObject $abstractModelMock */
+        $abstractModelMock = $this->getMockForAbstractClass(
+            'Magento\Framework\Model\AbstractModel',
+            [$context, $registryMock, $resourceMock, $resourceCollectionMock]
+        );
+        $data = 'tableName';
+        $this->resourcesMock->expects($this->any())
+            ->method('getConnection')
+            ->will($this->returnValue($connectionMock));
+        $this->resourcesMock->expects($this->any())->method('getTableName')->with($data)->will(
+            $this->returnValue('tableName')
+        );
+        $mainTableReflection = new \ReflectionProperty(
+            'Magento\Framework\Model\ResourceModel\Db\AbstractDb',
+            '_mainTable'
+        );
+        $mainTableReflection->setAccessible(true);
+        $mainTableReflection->setValue($this->model, 'tableName');
+        $idFieldNameReflection = new \ReflectionProperty(
+            'Magento\Framework\Model\ResourceModel\Db\AbstractDb',
+            '_idFieldName'
+        );
+        $idFieldNameReflection->setAccessible(true);
+        $idFieldNameReflection->setValue($this->model, 'idFieldName');
+        $connectionMock->expects($this->any())->method('save')->with('tableName', 'idFieldName');
+        $connectionMock->expects($this->any())->method('quoteInto')->will($this->returnValue('idFieldName'));
+
+        $abstractModelMock->setIdFieldName('id');
+        $abstractModelMock->setData(
+            [
+                'id'    => 12345,
+                'name'  => 'Test Name',
+                'value' => 'Test Value'
+            ]
+        );
+        $abstractModelMock->afterLoad();
+        $this->assertEquals($abstractModelMock->getData(), $abstractModelMock->getStoredData());
+        $newData = ['value' => 'Test Value New'];
+        $abstractModelMock->addData($newData);
+        $this->assertNotEquals($abstractModelMock->getData(), $abstractModelMock->getStoredData());
+        $abstractModelMock->isObjectNew(false);
+        $connectionMock->expects($this->any())
+            ->method('update')
+            ->with(
+                'tableName',
+                $newData,
+                'idFieldName'
+            );
+        $this->relationProcessorMock->expects($this->once())
+            ->method('validateDataIntegrity');
+        $this->entityManager->expects($this->once())
+            ->method('save');
+
+        $this->model->save($abstractModelMock);
     }
 }
