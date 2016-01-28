eb8fb462fba3340124fea16510a29a8e674cffd7
Hayder Sharhan <hsharhan@ebay.com>
2016-01-27 18:18:24 -0600

    MAGETWO-47439: [Github] i18n:collect-phrases -m can't find many important magento phrases #2630
    - Revised regex.

--- setup/src/Magento/Setup/Module/I18n/Dictionary/Phrase.php
+++ setup/src/Magento/Setup/Module/I18n/Dictionary/Phrase.php
@@ -270,7 +270,7 @@ class Phrase
     {
         $encloseQuote = $this->getQuote() == Phrase::QUOTE_DOUBLE ? Phrase::QUOTE_DOUBLE : Phrase::QUOTE_SINGLE;
         //find all occurrences of ' and ", with no \ before it.
-        preg_match_all('/[^\\\\][\'"]|[\'\"][^\\\\]/', $string, $matches);
+        preg_match_all('/[^\\\\]' . $encloseQuote . '|' . $encloseQuote . '[^\\\\]/', $string, $matches);
         if (count($matches[0])) {
             $string = preg_replace('/([^\\\\])' . $encloseQuote . ' ?\. ?' . $encloseQuote . '/', '$1', $string);
             $string = addslashes($string);
