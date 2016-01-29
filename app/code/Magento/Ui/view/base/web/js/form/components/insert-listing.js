cbae3b584615a3e39cb53782c875f66069864d4e
Iryna Lagno <ilagno@magento.com>
2016-01-29 16:22:50 +0200

    MAGETWO-48403: PR preparation

--- app/code/Magento/Ui/view/base/web/js/form/components/insert-listing.js
+++ app/code/Magento/Ui/view/base/web/js/form/components/insert-listing.js
@@ -390,17 +390,6 @@ define([
         },
 
         /**
-         * Reload source
-         *
-         * @param {bool} state
-         */
-        reload: function (state) {
-            if (!state) {
-                this.externalSource().set('params.t', new Date().getTime());
-            }
-        },
-
-        /**
          * Updates value from external value
          *
          */
