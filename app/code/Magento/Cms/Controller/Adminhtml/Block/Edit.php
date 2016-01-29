138e9f9323ddba0accb719b63c73810bf1ef6730
Sviatoslav Mankivskyi <smankivskyi@magento.com>
2016-01-29 15:01:00 +0200

    MAGETWO-48287: "Create/Edit CMS Page/Block" Admin forms do not support data validation without refreshing

--- app/code/Magento/Cms/Controller/Adminhtml/Block/Edit.php
+++ app/code/Magento/Cms/Controller/Adminhtml/Block/Edit.php
@@ -1,6 +1,5 @@
 <?php
 /**
- *
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
@@ -15,16 +14,14 @@ class Edit extends \Magento\Cms\Controller\Adminhtml\Block
 
     /**
      * @param \Magento\Backend\App\Action\Context $context
-     * @param \Magento\Framework\Registry $coreRegistry
      * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
      */
     public function __construct(
         \Magento\Backend\App\Action\Context $context,
-        \Magento\Framework\Registry $coreRegistry,
         \Magento\Framework\View\Result\PageFactory $resultPageFactory
     ) {
         $this->resultPageFactory = $resultPageFactory;
-        parent::__construct($context, $coreRegistry);
+        parent::__construct($context);
     }
 
     /**
