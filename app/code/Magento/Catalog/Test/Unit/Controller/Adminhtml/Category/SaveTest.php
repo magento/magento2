df320617ea09b8986d2f4d53b01085f055f0712f
Stanislav Lopukhov <slopukhov@ebay.com>
2016-02-02 19:00:01 +0200

    MAGETWO-48520: Category with invalid URL rewrites saves in root category

--- app/code/Magento/Catalog/Test/Unit/Controller/Adminhtml/Category/SaveTest.php
+++ app/code/Magento/Catalog/Test/Unit/Controller/Adminhtml/Category/SaveTest.php
@@ -368,6 +368,10 @@ class SaveTest extends \PHPUnit_Framework_TestCase
             false
         );
 
+        $messagesMock->expects($this->once())
+            ->method('getCountByType')
+            ->will($this->returnValue(0));
+
         $this->resultRedirectFactoryMock->expects($this->once())
             ->method('create')
             ->will($this->returnValue($resultRedirectMock));
@@ -430,7 +434,7 @@ class SaveTest extends \PHPUnit_Framework_TestCase
         $categoryMock->expects($this->once())
             ->method('addData')
             ->with($postData);
-        $categoryMock->expects($this->at(0))
+        $categoryMock->expects($this->any())
             ->method('getId')
             ->will($this->returnValue($categoryId));
 
@@ -521,9 +525,8 @@ class SaveTest extends \PHPUnit_Framework_TestCase
         $layoutMock->expects($this->once())
             ->method('getMessagesBlock')
             ->will($this->returnValue($blockMock));
-        $this->messageManagerMock->expects($this->once())
+        $this->messageManagerMock->expects($this->any())
             ->method('getMessages')
-            ->with(true)
             ->will($this->returnValue($messagesMock));
         $blockMock->expects($this->once())
             ->method('setMessages')
