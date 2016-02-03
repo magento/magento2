c75e23238a427231d0a51f6ba483032b4ce2e9a0
Igor Melnikov <imelnikov@ebay.com>
2016-02-03 13:34:28 -0600

    MAGETWO-48318: Manage Coupon Code tab will appear after save cart price rule
    Adding Coupons tab

---
+++ app/code/Magento/SalesRule/Block/Adminhtml/Promo/Quote/Edit/Tab/Coupons.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\SalesRule\Block\Adminhtml\Promo\Quote\Edit\Tab;
+
+/**
+ * "Manage Coupons Codes" Tab
+ *
+ * @author      Magento Core Team <core@magentocommerce.com>
+ */
+class Coupons extends \Magento\Backend\Block\Text\ListText implements \Magento\Backend\Block\Widget\Tab\TabInterface
+{
+    /**
+     * Core registry
+     *
+     * @var \Magento\Framework\Registry
+     */
+    protected $_coreRegistry = null;
+
+    /**
+     * @param \Magento\Framework\View\Element\Context $context
+     * @param \Magento\Framework\Registry $registry
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\Context $context,
+        \Magento\Framework\Registry $registry,
+        array $data = []
+    ) {
+        $this->_coreRegistry = $registry;
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTabLabel()
+    {
+        return __('Manage Coupon Codes');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTabTitle()
+    {
+        return __('Manage Coupon Codes');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function canShowTab()
+    {
+        return $this->_isEditing();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isHidden()
+    {
+        return !$this->_isEditing();
+    }
+
+    /**
+     * Check whether we edit existing rule or adding new one
+     *
+     * @return bool
+     */
+    protected function _isEditing()
+    {
+        $priceRule = $this->_coreRegistry->registry('current_promo_quote_rule');
+        return $priceRule->getRuleId() !== null;
+    }
+}
