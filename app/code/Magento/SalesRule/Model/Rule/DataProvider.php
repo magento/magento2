76722ccc9d542ce6a5c8efd9345f17afd23e7b8f
Igor Melnikov <imelnikov@ebay.com>
2016-02-03 13:06:32 -0600

    MAGETWO-48314: Refactor form to use ui component and Create a DataProvider (Exclude Manage Coupon code tab)
    Labels tab

--- app/code/Magento/SalesRule/Model/Rule/DataProvider.php
+++ app/code/Magento/SalesRule/Model/Rule/DataProvider.php
@@ -178,14 +178,7 @@ class DataProvider extends \Magento\Ui\DataProvider\AbstractDataProvider
                         ]
                     ],
                 ]
-            ],
-            'labels' => [
-                'fields' => [
-                    'store_labels[0]' => [
-                        'value' => isset($labels[0]) ? $labels[0] : '',
-                    ]
-                ]
-            ],
+            ]
         ];
     }
 
