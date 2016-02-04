7a46faed9437e8414b94f25878e7972176bfc4a5
Ankur Kaneria <akaneria@ebay.com>
2016-02-04 16:53:19 -0600

    MAGETWO-48711: Display error or redirect if directly browse to unsupported feature

--- app/code/Magento/Search/Controller/Adminhtml/Synonyms.php
+++ app/code/Magento/Search/Controller/Adminhtml/Synonyms.php
@@ -28,6 +28,15 @@ abstract class Synonyms extends Action
      */
     protected $registry;
 
+    /**
+     * @var \Magento\Search\Model\EngineResolver $engineResolver
+     */
+    protected $engineResolver;
+
+    /**
+     * @var \Magento\Framework\Search\SearchEngine\ConfigInterface $searchFeatureConfig
+     */
+    protected $searchFeatureConfig;
 
     /**
      * Constructor
@@ -36,16 +45,22 @@ abstract class Synonyms extends Action
      * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
      * @param \Magento\Backend\Model\View\Result\ForwardFactory $forwardFactory
      * @param \Magento\Framework\Registry $registry
+     * @param \Magento\Search\Model\EngineResolver $engineResolver
+     * @param \Magento\Framework\Search\SearchEngine\ConfigInterface $searchFeatureConfig
      */
     public function __construct(
         Action\Context $context,
         \Magento\Framework\View\Result\PageFactory $resultPageFactory,
         \Magento\Backend\Model\View\Result\ForwardFactory $forwardFactory,
-        \Magento\Framework\Registry $registry
+        \Magento\Framework\Registry $registry,
+        \Magento\Search\Model\EngineResolver $engineResolver,
+        \Magento\Framework\Search\SearchEngine\ConfigInterface $searchFeatureConfig
     ) {
         $this->resultPageFactory = $resultPageFactory;
         $this->forwardFactory = $forwardFactory;
         $this->registry = $registry;
+        $this->engineResolver = $engineResolver;
+        $this->searchFeatureConfig = $searchFeatureConfig;
         parent::__construct($context);
     }
 
@@ -56,8 +71,11 @@ abstract class Synonyms extends Action
      */
     protected function _initAction()
     {
+        $this->checkSearchEngineSupport();
         /** @var \Magento\Backend\Model\View\Result\Page  $resultPage **/
         $resultPage = $this->resultPageFactory->create();
+
+        // Make it active on menu and set breadcrumb trail
         $resultPage->setActiveMenu('Magento_Search::search_synonyms');
         $resultPage->addBreadcrumb(__('Marketing'), __('Marketing'));
         $resultPage->addBreadcrumb(__('Search Synonyms'), __('Search Synonyms'));
@@ -70,5 +88,23 @@ abstract class Synonyms extends Action
     protected function _isAllowed()
     {
         return $this->_authorization->isAllowed('Magento_Search::synonyms');
+    }
+
+    protected function checkSearchEngineSupport()
+    {
+        // Display a notice if search engine configuration does not support synonyms
+        $searchEngine = $this->engineResolver->getCurrentSearchEngine();
+        if (!$this->searchFeatureConfig
+            ->isFeatureSupported(
+                \Magento\Framework\Search\SearchEngine\ConfigInterface::SEARCH_ENGINE_FEATURE_SYNONYMS,
+                $searchEngine
+            )
+        ) {
+            // Display anotice indicating search synonyms feature is not supported for the selected search engine
+            $this->messageManager
+                ->addNoticeMessage(
+                    __('Search Synonyms feature is not supported by %1 search engine', $searchEngine)
+                );
+        }
     }
 }
--- dev/tests/integration/testsuite/Magento/Search/Model/SearchEngine/ConfigTest.php
+++ dev/tests/integration/testsuite/Magento/Search/Model/SearchEngine/ConfigTest.php
@@ -7,12 +7,18 @@ namespace Magento\Search\Model\SearchEngine;
 
 class ConfigTest extends \PHPUnit_Framework_TestCase
 {
-    public function testGetDeclaredFeatures()
+    protected  function setUp()
     {
         $xmlPath = __DIR__ . '/../../_files/search_engine.xml';
         $objectManager = \Magento\TestFramework\Helper\Bootstrap::getObjectManager();
-        $fileResolver = $this->getMockForAbstractClass('Magento\Framework\Config\FileResolverInterface', [], '', false);
-        $fileResolver->expects($this->once())->method('get')->willReturn([file_get_contents($xmlPath)]);
+        $fileResolver = $this->getMockForAbstractClass(
+            'Magento\Framework\Config\FileResolverInterface',
+            [],
+            '',
+            false
+        );
+        $fileResolver->expects($this->any())->method('get')->willReturn([file_get_contents($xmlPath)]);
+
         $configReader = $objectManager->create(
             'Magento\Framework\Search\SearchEngine\Config\Reader',
             ['fileResolver' => $fileResolver]
@@ -21,10 +27,34 @@ class ConfigTest extends \PHPUnit_Framework_TestCase
             'Magento\Search\Model\SearchEngine\Config\Data',
             ['reader' => $configReader]
         );
-        $config = $objectManager->create('Magento\Search\Model\SearchEngine\Config', ['dataStorage' => $dataStorage]);
-        $this->assertEquals(['synonyms'], $config->getDeclaredFeatures('mysql'));
-        $this->assertEquals(['synonyms', 'stopwords'], $config->getDeclaredFeatures('other'));
-        $this->assertEquals([], $config->getDeclaredFeatures('none'));
-        $this->assertEquals([], $config->getDeclaredFeatures('non_exist'));
+        $this->config = $objectManager->create(
+            'Magento\Search\Model\SearchEngine\Config',
+            ['dataStorage' => $dataStorage]
+        );
+    }
+
+    public function testGetDeclaredFeatures()
+    {
+        $this->assertEquals(['synonyms'], $this->config->getDeclaredFeatures('mysql'));
+        $this->assertEquals(['synonyms', 'stopwords'], $this->config->getDeclaredFeatures('other'));
+        $this->assertEquals([], $this->config->getDeclaredFeatures('none1'));
+        $this->assertEquals([], $this->config->getDeclaredFeatures('none2'));
+        $this->assertEquals([], $this->config->getDeclaredFeatures('non_exist'));
+    }
+
+    public function testIsFeatureSupported()
+    {
+        $this->assertEquals(true, $this->config->isFeatureSupported('synonyms', 'mysql'));
+        $this->assertEquals(false, $this->config->isFeatureSupported('stopwords', 'mysql'));
+        $this->assertEquals(true, $this->config->isFeatureSupported('synonyms', 'other'));
+        $this->assertEquals(true, $this->config->isFeatureSupported('stopwords', 'other'));
+        $this->assertEquals(false, $this->config->isFeatureSupported('synonyms', 'none1'));
+        $this->assertEquals(false, $this->config->isFeatureSupported('stopwords', 'none1'));
+        $this->assertEquals(false, $this->config->isFeatureSupported('synonyms', 'none2'));
+        $this->assertEquals(false, $this->config->isFeatureSupported('stopwords', 'none2'));
+        $this->assertEquals(false, $this->config->isFeatureSupported('synonyms', 'non_exist'));
+        $this->assertEquals(false, $this->config->isFeatureSupported('stopwords', 'non_exist'));
+        $this->assertEquals(false, $this->config->isFeatureSupported('non_exist', 'non_exist'));
+        $this->assertEquals(false, $this->config->isFeatureSupported('non_exist', 'mysql'));
     }
 }
