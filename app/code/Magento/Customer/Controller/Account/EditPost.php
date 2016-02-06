2b7ff43732eeba3096697c5ba8e29b82808f6576
Anatol Burym <Anatol_Burym@epam.com>
2016-02-06 17:31:40 +0300

    SPM-4-Front-end password protection
    - SPM-15-Stabilization
    - SPM-134 bug fix

--- app/code/Magento/Customer/Controller/Account/EditPost.php
+++ app/code/Magento/Customer/Controller/Account/EditPost.php
@@ -137,6 +137,7 @@ class EditPost extends \Magento\Customer\Controller\AbstractAccount
                         $customerCandidateDataObject,
                         $isPasswordChanged
                     );
+                $this->dispatchSuccessEvent($customerCandidateDataObject);
                 $this->messageManager->addSuccess(__('You saved the account information.'));
                 return $resultRedirect->setPath('customer/account');
             } catch (InvalidEmailOrPasswordException $e) {
@@ -162,6 +163,20 @@ class EditPost extends \Magento\Customer\Controller\AbstractAccount
         }
 
         return $resultRedirect->setPath('*/*/edit');
+    }
+
+    /**
+     * Account editing action completed successfully event
+     *
+     * @param \Magento\Customer\Api\Data\CustomerInterface $customerCandidateDataObject
+     * @return void
+     */
+    protected function dispatchSuccessEvent(\Magento\Customer\Api\Data\CustomerInterface $customerCandidateDataObject)
+    {
+        $this->_eventManager->dispatch(
+            'customer_account_edited',
+            ['email' => $customerCandidateDataObject->getEmail()]
+        );
     }
 
     /**
