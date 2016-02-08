99d76ed30bee0a632e36c8637b37115bb34333f3
Igor Melnikov <imelnikov@ebay.com>
2016-02-08 16:53:05 -0600

    MAGETWO-48314: Refactor form to use ui component and Create a DataProvider (Exclude Manage Coupon code tab)
    Fixed amount discount for whole cart option not being saved

--- app/code/Magento/SalesRule/Model/Rule/DataProvider.php
+++ app/code/Magento/SalesRule/Model/Rule/DataProvider.php
@@ -113,7 +113,7 @@ class DataProvider extends \Magento\Ui\DataProvider\AbstractDataProvider
         $applyOptions = [
             ['label' => __('Percent of product price discount'), 'value' =>  Rule::BY_PERCENT_ACTION],
             ['label' => __('Fixed amount discount'), 'value' => Rule::BY_FIXED_ACTION],
-            ['label' => __('Fixed amount discount for whole cart'), 'value' => Rule::BY_PERCENT_ACTION],
+            ['label' => __('Fixed amount discount for whole cart'), 'value' => Rule::CART_FIXED_ACTION],
             ['label' => __('Buy X get Y free (discount amount is Y)'), 'value' => Rule::BUY_X_GET_Y_ACTION]
         ];
 
@@ -131,59 +131,125 @@ class DataProvider extends \Magento\Ui\DataProvider\AbstractDataProvider
 
         $this->meta = [
             'rule_information' => [
-                'fields' => [
+                'children' => [
                     'website_ids' => [
-                        'options' => $this->store->getWebsiteValuesForForm()
+                        'arguments' => [
+                            'data' => [
+                                'config' => [
+                                    'options' => $this->store->getWebsiteValuesForForm(),
+                                ],
+                            ],
+                        ],
                     ],
                     'is_active' => [
-                        'options' => [
-                            ['label' => __('Active'), 'value' => '1'],
-                            ['label' => __('Inactive'), 'value' => '0']
-                        ]
+                        'arguments' => [
+                            'data' => [
+                                'config' => [
+                                    'options' => [
+                                        ['label' => __('Active'), 'value' => '1'],
+                                        ['label' => __('Inactive'), 'value' => '0']
+                                    ],
+                                ],
+                            ],
+                        ],
                     ],
                     'customer_group_ids' => [
-                        'options' => $this->objectConverter->toOptionArray($customerGroups, 'id', 'code')
+                        'arguments' => [
+                            'data' => [
+                                'config' => [
+                                    'options' => $this->objectConverter->toOptionArray($customerGroups, 'id', 'code'),
+                                ],
+                            ],
+                        ],
                     ],
                     'coupon_type' => [
-                        'options' => $couponTypesOptions
+                        'arguments' => [
+                            'data' => [
+                                'config' => [
+                                    'options' => $couponTypesOptions,
+                                ],
+                            ],
+                        ],
                     ],
                     'is_rss' => [
-                        'options' => [
-                            ['label' => __('Yes'), 'value' => '1'],
-                            ['label' => __('No'), 'value' => '0']
-                        ]
+                        'arguments' => [
+                            'data' => [
+                                'config' => [
+                                    'options' => [
+                                        ['label' => __('Yes'), 'value' => '1'],
+                                        ['label' => __('No'), 'value' => '0']
+                                    ],
+                                ],
+                            ],
+                        ],
                     ],
                 ]
             ],
             'actions' => [
-                'fields' => [
+                'children' => [
                     'simple_action' => [
-                        'options' => $applyOptions
-                    ],
-                    'discount_amount' => [
-                        'value' => '0',
-                    ],
-                    'discount_qty' => [
-                        'value' => '0',
-                    ],
-                    'apply_to_shipping' => [
-                        'options' => [
-                            ['label' => __('Yes'), 'value' => '1'],
-                            ['label' => __('No'), 'value' => '0']
+                        'arguments' => [
+                            'data' => [
+                                'config' => [
+                                    'options' => $applyOptions
+                                ],
+                            ]
                         ]
                     ],
+                    'discount_amount' => [
+                        'arguments' => [
+                            'data' => [
+                                'config' => [
+                                    'value' => '0',
+                                ],
+                            ],
+                        ],
+                    ],
+                    'discount_qty' => [
+                        'arguments' => [
+                            'data' => [
+                                'config' => [
+                                    'value' => '0',
+                                ],
+                            ],
+                        ],
+                    ],
+                    'apply_to_shipping' => [
+                        'arguments' => [
+                            'data' => [
+                                'config' => [
+                                    'options' => [
+                                        ['label' => __('Yes'), 'value' => '1'],
+                                        ['label' => __('No'), 'value' => '0']
+                                    ]
+                                ],
+                            ],
+                        ],
+                    ],
                     'stop_rules_processing' => [
-                        'options' => [
-                            ['label' => __('Yes'), 'value' => '1'],
-                            ['label' => __('No'), 'value' => '0'],
+                        'arguments' => [
+                            'data' => [
+                                'config' => [
+                                    'options' => [
+                                        ['label' => __('Yes'), 'value' => '1'],
+                                        ['label' => __('No'), 'value' => '0'],
+                                    ],
+                                ],
+                            ]
                         ]
                     ],
                 ]
             ],
             'labels' => [
-                'fields' => [
+                'children' => [
                     'store_labels[0]' => [
-                        'value' => isset($labels[0]) ? $labels[0] : '',
+                        'arguments' => [
+                            'data' => [
+                                'config' => [
+                                    'value' => isset($labels[0]) ? $labels[0] : '',
+                                ],
+                            ]
+                        ]
                     ]
                 ]
             ],
