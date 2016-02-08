704b89ff7a671e160284d5156b74e6d07b6ea5dd
Ankur Kaneria <akaneria@ebay.com>
2016-02-08 10:11:02 -0600

    MAGETWO-48709: Hide menu items if not supported
    - Added an "after" plugin to hide "Search Terms" menu if "synonyms" feature is not supported by engine

---
+++ app/code/Magento/Search/Model/SearchEngine/MenuBuilder.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Search\Model\SearchEngine;
+
+use \Magento\Backend\Model\Menu;
+use \Magento\Backend\Model\Menu\Builder;
+use \Magento\Framework\Search\SearchEngine\ConfigInterface;
+use \Magento\Search\Model\EngineResolver;
+use Magento\Backend\Model\Menu\Config as MenuConfig;
+
+/**
+ * A plugin for Magento\Backend\Model\Menu\Builder class. Implements "after" for "getResult()".
+ *
+ * The purpose of this plugin is to go through the menu tree and remove "Search Terms" menu item if the
+ * selected search engine does not support "synonyms" feature.
+ */
+class MenuBuilder
+{
+    /**
+     * A constant to refer to "Search Synonyms" menu item id from etc/adminhtml/menu.xml
+     */
+    const SEARCH_SYNONYMS_MENU_ITEM_ID = 'Magento_Search::search_synonyms';
+
+    /*
+     * @var MenuConfig $menuConfig
+     */
+    protected $menuConfig;
+
+    /**
+     * @var ConfigInterface $searchFeatureConfig
+     */
+    protected $searchFeatureConfig;
+
+    /**
+     * @var EngineResolver $engineResolver
+     */
+    protected $engineResolver;
+
+    public function __construct(MenuConfig $menuConfig, ConfigInterface $searchFeatureConfig, EngineResolver $engineResolver)
+    {
+        $this->menuConfig = $menuConfig;
+        $this->searchFeatureConfig = $searchFeatureConfig;
+        $this->engineResolver = $engineResolver;
+    }
+
+    public function afterGetResult(Builder $subject)
+    {
+        $menu = $this->menuConfig->getMenu();
+        $searchEngine = $this->engineResolver->getCurrentSearchEngine();
+        if (!$this->searchFeatureConfig
+            ->isFeatureSupported(ConfigInterface::SEARCH_ENGINE_FEATURE_SYNONYMS, $searchEngine)) {
+
+            // "Search Synonyms" feature is not supported by the current configured search engine.
+            // Menu will be updated to remove it from the list
+            /*$this->menu = */
+
+            //$subject->getResult($this->menu)->remove(self::SEARCH_SYNONYMS_MENU_ITEM_ID);
+            //$this->menu->remove(self::SEARCH_SYNONYMS_MENU_ITEM_ID);
+            $menu->remove(self::SEARCH_SYNONYMS_MENU_ITEM_ID);
+        }
+        return $menu;
+    }
+}
