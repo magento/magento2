dc00a1455c97932d3fa1edd36ec632352209df37
Oleksii Korshenko <okorshenko@magento.com>
2016-02-08 15:24:11 -0600

    MAGETWO-48900: Disorganized/Disfunctional UI components

--- app/code/Magento/SalesRule/Model/Rule/DataProvider.php
+++ app/code/Magento/SalesRule/Model/Rule/DataProvider.php
@@ -134,59 +134,104 @@ class DataProvider extends \Magento\Ui\DataProvider\AbstractDataProvider
 
         return [
             'rule_information' => [
-                'fields' => [
+                'children' => [
                     'website_ids' => [
-                        'options' => $this->store->getWebsiteValuesForForm()
+                        'arguments' => [
+                            'data' => [
+                                'config' => [
+                                    'options' => $this->store->getWebsiteValuesForForm(),
+                                ],
+                            ],
+                        ],
                     ],
                     'is_active' => [
-                        'options' => [
-                            ['label' => __('Active'), 'value' => '1'],
-                            ['label' => __('Inactive'), 'value' => '0']
-                        ]
+                        'arguments' => [
+                            'data' => [
+                                'config' => [
+                                    'options' => [
+                                        ['label' => __('Active'), 'value' => '1'],
+                                        ['label' => __('Inactive'), 'value' => '0']
+                                    ],
+                                ],
+                            ],
+                        ],
                     ],
                     'customer_group_ids' => [
-                        'options' => $this->objectConverter->toOptionArray($customerGroups, 'id', 'code')
-                    ],
-                    'coupon_type' => [
-                        'options' => $couponTypesOptions
-                    ],
-                    'is_rss' => [
-                        'options' => [
-                            ['label' => __('Yes'), 'value' => '1'],
-                            ['label' => __('No'), 'value' => '0']
-                        ]
-                    ],
+                        'arguments' => [
+                            'data' => [
+                                'config' => [
+                                    'options' => $this->objectConverter->toOptionArray($customerGroups, 'id', 'code'),
+                                ],
+                            ],
+                        ],
+                    ]
                 ]
             ],
             'actions' => [
-                'fields' => [
+                'children' => [
                     'simple_action' => [
-                        'options' => $applyOptions
-                    ],
-                    'discount_amount' => [
-                        'value' => '0',
-                    ],
-                    'discount_qty' => [
-                        'value' => '0',
-                    ],
-                    'apply_to_shipping' => [
-                        'options' => [
-                            ['label' => __('Yes'), 'value' => '1'],
-                            ['label' => __('No'), 'value' => '0']
+                        'arguments' => [
+                            'data' => [
+                                'config' => [
+                                    'options' => $applyOptions
+                                ],
+                            ]
                         ]
                     ],
+                    'discount_amount' => [
+                        'arguments' => [
+                            'data' => [
+                                'config' => [
+                                    'value' => '0',
+                                ],
+                            ],
+                        ],
+                    ],
+                    'discount_qty' => [
+                        'arguments' => [
+                            'data' => [
+                                'config' => [
+                                    'value' => '0',
+                                ],
+                            ],
+                        ],
+                    ],
+                    'apply_to_shipping' => [
+                        'arguments' => [
+                            'data' => [
+                                'config' => [
+                                    'options' => [
+                                        ['label' => __('Yes'), 'value' => '1'],
+                                        ['label' => __('No'), 'value' => '0']
+                                    ]
+                                ],
+                            ],
+                        ],
+                    ],
                     'stop_rules_processing' => [
-                        'options' => [
-                            ['label' => __('Yes'), 'value' => '1'],
-                            ['label' => __('No'), 'value' => '0'],
+                        'arguments' => [
+                            'data' => [
+                                'config' => [
+                                    'options' => [
+                                        ['label' => __('Yes'), 'value' => '1'],
+                                        ['label' => __('No'), 'value' => '0'],
+                                    ],
+                                ],
+                            ]
                         ]
                     ],
                 ]
             ],
             'labels' => [
-                'fields' => [
+                'children' => [
                     'store_labels[0]' => [
-                        'value' => isset($labels[0]) ? $labels[0] : '',
+                        'arguments' => [
+                            'data' => [
+                                'config' => [
+                                    'value' => isset($labels[0]) ? $labels[0] : '',
+                                ],
+                            ]
+                        ]
                     ]
                 ]
             ],
