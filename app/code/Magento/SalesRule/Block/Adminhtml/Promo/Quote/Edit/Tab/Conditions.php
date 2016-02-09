f09cc52be645111046ee61178a0e960c8f550a6b
Hayder Sharhan <hsharhan@ebay.com>
2016-02-09 16:33:54 -0600

    MAGETWO-48874: Update form for conditions
    - Updated form for conditions.
    - Moved location of staging Conditions tab. Was under Sales now under Promo like in SalesRule.

--- app/code/Magento/SalesRule/Block/Adminhtml/Promo/Quote/Edit/Tab/Conditions.php
+++ app/code/Magento/SalesRule/Block/Adminhtml/Promo/Quote/Edit/Tab/Conditions.php
@@ -127,25 +127,48 @@ class Conditions extends \Magento\Backend\Block\Widget\Form\Generic implements
     protected function _prepareForm()
     {
         $model = $this->_coreRegistry->registry(\Magento\SalesRule\Model\RegistryConstants::CURRENT_SALES_RULE);
+        $form = $this->addTabToForm($model);
+        $this->setForm($form);
 
+        return parent::_prepareForm();
+    }
+
+    /**
+     * @param \Magento\SalesRule\Api\Data\RuleInterface $model
+     * @param string $fieldsetId
+     * @param string $formName
+     * @return \Magento\Framework\Data\Form
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    protected function addTabToForm($model, $fieldsetId = 'conditions_fieldset', $formName = 'sales_rule_form')
+    {
         if (!$model) {
             $id = $this->getRequest()->getParam('id');
             $model = $this->ruleFactory->create();
             $model->load($id);
+        } else {
+            $id = $model->getRuleId();
         }
+        $conditionsFieldSetId = $formName . 'rule_conditions_fieldset_' . $id;
+
+        $newChildUrl = $this->getUrl(
+            'sales_rule/promo_quote/newConditionHtml/form/rule_conditions_fieldset_' . $conditionsFieldSetId,
+            ['form_namespace' => $formName]
+        );
 
         /** @var \Magento\Framework\Data\Form $form */
         $form = $this->_formFactory->create();
         $form->setHtmlIdPrefix('rule_');
-
         $renderer = $this->_rendererFieldset->setTemplate(
             'Magento_CatalogRule::promo/fieldset.phtml'
         )->setNewChildUrl(
-            $this->getUrl('sales_rule/promo_quote/newConditionHtml/form/rule_conditions_fieldset')
+            $newChildUrl
+        )->setConditionsFieldSetId(
+            $conditionsFieldSetId
         );
 
         $fieldset = $form->addFieldset(
-            'conditions_fieldset',
+            $fieldsetId,
             [
                 'legend' => __(
                     'Apply the rule only if the following conditions are met (leave blank for all products).'
@@ -154,16 +177,15 @@ class Conditions extends \Magento\Backend\Block\Widget\Form\Generic implements
         )->setRenderer(
             $renderer
         );
-
         $fieldset->addField(
             'conditions',
             'text',
             [
-                'name' => 'conditions',
-                'label' => __('Conditions'),
-                'title' => __('Conditions'),
-                'required' => true,
-                'data-form-part' => 'sales_rule_form'
+                'name'           => 'conditions',
+                'label'          => __('Conditions'),
+                'title'          => __('Conditions'),
+                'required'       => true,
+                'data-form-part' => $formName
             ]
         )->setRule(
             $model
@@ -172,8 +194,22 @@ class Conditions extends \Magento\Backend\Block\Widget\Form\Generic implements
         );
 
         $form->setValues($model->getData());
-        $this->setForm($form);
+        $this->setConditionFormName($model->getConditions(), $formName);
+        return $form;
+    }
 
-        return parent::_prepareForm();
+    /**
+     * @param \Magento\Rule\Model\Condition\AbstractCondition $conditions
+     * @param string $formName
+     * @return void
+     */
+    private function setConditionFormName(\Magento\Rule\Model\Condition\AbstractCondition $conditions, $formName)
+    {
+        $conditions->setFormName($formName);
+        if ($conditions->getConditions() && is_array($conditions->getConditions())) {
+            foreach ($conditions->getConditions() as $condition) {
+                $this->setConditionFormName($condition, $formName);
+            }
+        }
     }
 }
