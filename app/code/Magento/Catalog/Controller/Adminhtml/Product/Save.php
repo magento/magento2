a23042f4414426180899782bcfbd7d5eb32b2de6
Oleksandr Iegorov <oiegorv@magento.com>
2016-02-09 15:50:45 +0200

    MAGETWO-48927: Duplicated bundle product options after update

--- app/code/Magento/Catalog/Controller/Adminhtml/Product/Save.php
+++ app/code/Magento/Catalog/Controller/Adminhtml/Product/Save.php
@@ -196,16 +196,21 @@ class Save extends \Magento\Catalog\Controller\Adminhtml\Product
      */
     protected function copyToStores($data, $productId)
     {
-        if (isset($data['product']['copy_to_stores'])) {
-            foreach ($data['product']['copy_to_stores'] as $group) {
-                foreach ($group as $store) {
-                    $copyFrom = (isset($store['copy_from'])) ? $store['copy_from'] : 0;
-                    $copyTo = (isset($store['copy_to'])) ? $store['copy_to'] : 0;
-                    $this->_objectManager->create('Magento\Catalog\Model\Product')
-                        ->setStoreId($copyFrom)
-                        ->load($productId)
-                        ->setStoreId($copyTo)
-                        ->save();
+        if (!empty($data['product']['copy_to_stores'])) {
+            foreach ($data['product']['copy_to_stores'] as $websiteId => $group) {
+                if (isset($data['product']['website_ids'][$websiteId])
+                    && (bool)$data['product']['website_ids'][$websiteId]) {
+                    foreach ($group as $store) {
+                        $copyFrom = (isset($store['copy_from'])) ? $store['copy_from'] : 0;
+                        $copyTo = (isset($store['copy_to'])) ? $store['copy_to'] : 0;
+                        if ($copyTo) {
+                            $this->_objectManager->create('Magento\Catalog\Model\Product')
+                                ->setStoreId($copyFrom)
+                                ->load($productId)
+                                ->setStoreId($copyTo)
+                                ->save();
+                        }
+                    }
                 }
             }
         }
