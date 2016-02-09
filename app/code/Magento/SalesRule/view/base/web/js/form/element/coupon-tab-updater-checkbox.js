aa9cad130c866a4d1d32eaa0d7272cb724db043e
Igor Melnikov <imelnikov@ebay.com>
2016-02-09 09:50:00 -0600

    MAGETWO-48318: Manage Coupon Code tab will appear after save cart price rule
    Adding todo annotation to remove workaround

--- app/code/Magento/SalesRule/view/base/web/js/form/element/coupon-tab-updater-checkbox.js
+++ app/code/Magento/SalesRule/view/base/web/js/form/element/coupon-tab-updater-checkbox.js
@@ -18,6 +18,7 @@ define([
          * Hide fields on coupon tab
          */
         onUpdate: function () {
+            // @todo: remove this workaround after resolving MAGETWO-48846
             var isDisabled = !this.value(),
                 selector = '[id=coupons_information_fieldset] input, [id=coupons_information_fieldset] select, ' +
                     '[id=coupons_information_fieldset] button, [id=couponCodesGrid] input, ' +
