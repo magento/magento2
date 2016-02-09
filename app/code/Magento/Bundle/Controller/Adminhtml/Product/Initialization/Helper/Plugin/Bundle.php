876e9917f03e98b4815297537db2d2f563b3bfd1
Oleksandr Iegorov <oiegorv@magento.com>
2016-02-09 15:48:11 +0200

    MAGETWO-48927: Duplicated bundle product options after update

--- app/code/Magento/Bundle/Controller/Adminhtml/Product/Initialization/Helper/Plugin/Bundle.php
+++ app/code/Magento/Bundle/Controller/Adminhtml/Product/Initialization/Helper/Plugin/Bundle.php
@@ -146,7 +146,7 @@ class Bundle
                 }
                 $link = $this->linkFactory->create(['data' => $linkData]);
 
-                if ($product->getPriceType()) {
+                if ((int)$product->getPriceType() !== \Magento\Bundle\Model\Product\Price::PRICE_TYPE_DYNAMIC) {
                     if (array_key_exists('selection_price_value', $linkData)) {
                         $link->setPrice($linkData['selection_price_value']);
                     }
