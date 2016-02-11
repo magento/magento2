654237fb65a9bc49d86fb31d90dacafc92c346af
Volodymyr Zaets <vzaets@ebay.com>
2016-02-11 18:02:02 +0200

    MAGETWO-45138: [JS] Table with dynamic rows

--- app/code/Magento/Ui/view/base/web/js/dynamic-rows/dynamic-rows.js
+++ app/code/Magento/Ui/view/base/web/js/dynamic-rows/dynamic-rows.js
@@ -217,6 +217,7 @@ define([
             sorted = this.elems().sort(function (propOne, propTwo) {
                 return parseInt(propOne.position, 10) - parseInt(propTwo.position, 10);
             });
+
             updatedCollection = this.updatePosition(sorted, position, elem.name);
             this.elems(updatedCollection);
         },
@@ -317,9 +318,9 @@ define([
                 this.removeMaxPosition();
                 recordsData = this._getDataByProp(recordId);
                 this._updateData(recordsData);
-                this._sortAfterDelete();
                 --this.recordIterator;
             }
+            this._sort();
         },
 
         /**
@@ -339,7 +340,7 @@ define([
         /**
          * Sort elems by position property
          */
-        _sortAfterDelete: function () {
+        _sort: function () {
             this.elems(this.elems().sort(function (propOne, propTwo) {
                 return parseInt(propOne.position, 10) - parseInt(propTwo.position, 10);
             }));
