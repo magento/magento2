e133d87b84caa23cf9a82f947cf83ef3033c6e70
Olga Nakonechna <onakonechna@ebay.com>
2016-02-11 18:35:59 +0200

    MAGETWO-48841: Create resizeable control for bottom border

--- app/code/Magento/Ui/view/base/web/js/lib/knockout/bindings/resizable.js
+++ app/code/Magento/Ui/view/base/web/js/lib/knockout/bindings/resizable.js
@@ -5,11 +5,19 @@
 define([
     'ko',
     'jquery',
+    'Magento_Ui/js/lib/view/utils/async',
     'underscore',
     '../template/renderer',
     'jquery/ui'
-], function (ko, $, _, renderer) {
+], function (ko, $, async, _, renderer) {
     'use strict';
+
+    var sizeOptions = [
+        'minHeight',
+        'maxHeight',
+        'minWidth',
+        'maxWidth'
+    ];
 
     /**
      * Recalcs allowed min, max width and height based on configured selectors
@@ -21,11 +29,45 @@ define([
     function recalcAllowedSize(event, ui) {
         var size;
 
-        _.each(this, function (constraints, key) {
-            _.each(constraints, function (selector, constraint) {
-                size = key === 'height' ? $(selector).height() : $(selector).width();
-                $(ui.element).resizable('option', constraint, size);
+        _.each(this.sizeConstraints, function (selector, key) {
+            async.async({
+                component: this.ctx,
+                selector: selector
+            }, function (elem) {
+                size = key.includes('Height') ? $(elem).height() : $(elem).width();
+                $(ui.element).resizable('option', key, size);
             });
+        }, this);
+    }
+
+    /**
+     * Preprocess config to separate options,
+     * which must be processed further before applying
+     *
+     * @param {Object} config
+     * @param {Object} viewModel
+     *
+     */
+    function processConfig(config, viewModel) {
+        var sizeConstraint,
+            sizeConstraints = {};
+
+        _.each(sizeOptions, function (key) {
+            sizeConstraint = config[key];
+
+            if (sizeConstraint) {
+                config[key] = _.isNumber(sizeConstraint) ? sizeConstraint :
+                    (function () {
+                        sizeConstraints[key] = sizeConstraint;
+
+                        return null;
+                    })();
+            }
+        });
+
+        config.start = recalcAllowedSize.bind({
+            sizeConstraints: sizeConstraints,
+            ctx: viewModel.name
         });
     }
 
@@ -35,17 +77,19 @@ define([
          * Binding init callback.
          *
          * @param {*} element
+         * @param {Function} valueAccessor
+         * @param {Function} allBindings
+         * @param {Object} viewModel
          *
          */
-        init: function (element, valueAccessor) {
+        init: function (element, valueAccessor, allBindings, viewModel) {
             var config = valueAccessor();
 
-            if (config.sizeConstraints) {
-                config.options.start = recalcAllowedSize.bind(config.sizeConstraints);
-            }
-
-            if (typeof config.options === 'object') {
-                $(element).resizable(config.options);
+            if (typeof config === 'object') {
+                processConfig(config, viewModel);
+                $(element).resizable(config);
+            } else {
+                $(element).resizable();
             }
         }
     };
