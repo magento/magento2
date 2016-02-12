abc16d608802f45cce567ef8c9e7fddf97b31543
Paul Lewis <plewis@magento.com>
2016-02-12 16:29:12 -0600

    MAGETWO-47995: Zend Locale Data Cache issue in Magento File System
    - Added zend cache options to currency constructor
    - Added unit test

--- lib/internal/Magento/Framework/Currency.php
+++ lib/internal/Magento/Framework/Currency.php
@@ -5,6 +5,37 @@
  */
 namespace Magento\Framework;
 
-class Currency extends \Zend_Currency implements \Magento\Framework\CurrencyInterface
+use Magento\Framework\App\Filesystem\DirectoryList;
+use Magento\Framework\Filesystem;
+use Magento\Framework\Filesystem\DriverInterface;
+
+class Currency extends \Zend_Currency implements CurrencyInterface
 {
+    /**
+     * Creates a currency instance.
+     *
+     * @param Filesystem $filesystem
+     * @param  string|array       $options OPTIONAL Options array or currency short name
+     *                                              when string is given
+     * @param  string|\Zend_Locale $locale  OPTIONAL locale name
+     * @throws \Zend_Currency_Exception When currency is invalid
+     */
+    public function __construct(
+        Filesystem $filesystem,
+        $options = null,
+        $locale = null
+    ) {
+        // create zend cache in cache directory and set files to 0660 permissions
+        $cache = \Zend_Cache::factory(
+            'Core',
+            'File',
+            ['automatic_serialization' => true],
+            [
+                'cache_dir' => $filesystem->getDirectoryWrite(DirectoryList::CACHE)->getAbsolutePath(),
+                'cache_file_perm' => DriverInterface::WRITEABLE_FILE_MODE,
+            ]
+        );
+        \Zend_Currency::setCache($cache);
+        parent::__construct($options, $locale);
+    }
 }
