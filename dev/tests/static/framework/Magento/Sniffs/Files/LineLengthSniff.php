73415a67d132f2a44c4f0314b263da13b660a87f
Alexander Paliarush <apaliarush@magento.com>
2016-02-17 12:53:07 +0200

    MAGETWO-49107: [GitHub] Update Code Style checker to allow long strings in translatable phrases

--- dev/tests/static/framework/Magento/Sniffs/Files/LineLengthSniff.php
+++ dev/tests/static/framework/Magento/Sniffs/Files/LineLengthSniff.php
@@ -3,11 +3,12 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
+namespace Magento\Sniffs\Files;
 
 /**
  * Line length sniff which ignores long lines in case they contain strings intended for translation.
  */
-class Magento_Sniffs_Files_LineLengthSniff extends Generic_Sniffs_Files_LineLengthSniff
+class LineLengthSniff extends Generic_Sniffs_Files_LineLengthSniff
 {
     /**
      * Having previous line content allows to ignore long lines in case of multi-line declaration.
@@ -26,17 +27,17 @@ class Magento_Sniffs_Files_LineLengthSniff extends Generic_Sniffs_Files_LineLeng
         $currentLineMatch = preg_match($currentLineRegexp, $lineContent) !== 0;
         $previousLineMatch = preg_match($previousLineRegexp, $this->previousLineContent) !== 0;
         $this->previousLineContent = $lineContent;
-        $errorMessage = 'Variable is not allowed as the first argument of translation function, use string literal instead';
+        $error = 'Variable is not allowed as the first argument of translation function, use string literal instead';
         if ($currentLineMatch) {
             $variableRegexp = '~__\(\$.+\)|Phrase\(\$.+\)~';
             if (preg_match($variableRegexp, $lineContent) !== 0) {
-                $phpcsFile->addError($errorMessage, $stackPtr, 'VariableTranslation');
+                $phpcsFile->addError($error, $stackPtr, 'VariableTranslation');
             }
             return;
         } else if ($previousLineMatch) {
             $variableRegexp = '~^\s*\$.+~';
             if (preg_match($variableRegexp, $lineContent) !== 0) {
-                $phpcsFile->addError($errorMessage, $stackPtr, 'VariableTranslation');
+                $phpcsFile->addError($error, $stackPtr, 'VariableTranslation');
             }
             return;
         }
