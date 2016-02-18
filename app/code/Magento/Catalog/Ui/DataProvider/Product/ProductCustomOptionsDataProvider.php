b3e9635d87eddf8f52b77ff3608943b4e3fec09e
Yuri Kovsher <ikovsher@ebay.com>
2016-02-18 16:48:39 +0200

    MAGETWO-49385: Include custom option data into each product row data on modal window

--- app/code/Magento/Catalog/Ui/DataProvider/Product/ProductCustomOptionsDataProvider.php
+++ app/code/Magento/Catalog/Ui/DataProvider/Product/ProductCustomOptionsDataProvider.php
@@ -7,6 +7,11 @@ namespace Magento\Catalog\Ui\DataProvider\Product;
 
 use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory;
 use Magento\Framework\App\RequestInterface;
+use Magento\Catalog\Model\Product\Option\Repository as ProductOptionRepository;
+use Magento\Catalog\Model\Product\Option\Value as ProductOptionValueModel;
+use Magento\Catalog\Api\Data\ProductInterface;
+use Magento\Catalog\Model\Product\Option as ProductOption;
+use Magento\Framework\DataObject;
 
 class ProductCustomOptionsDataProvider extends ProductDataProvider
 {
@@ -16,6 +21,16 @@ class ProductCustomOptionsDataProvider extends ProductDataProvider
     protected $request;
 
     /**
+     * @var ProductOptionRepository
+     */
+    protected $productOptionRepository;
+
+    /**
+     * @var ProductOptionValueModel
+     */
+    protected $productOptionValueModel;
+
+    /**
      * Construct
      *
      * @param string $name
@@ -23,6 +38,8 @@ class ProductCustomOptionsDataProvider extends ProductDataProvider
      * @param string $requestFieldName
      * @param CollectionFactory $collectionFactory
      * @param RequestInterface $request
+     * @param ProductOptionRepository $productOptionRepository
+     * @param ProductOptionValueModel $productOptionValueModel
      * @param \Magento\Ui\DataProvider\AddFieldToCollectionInterface[] $addFieldStrategies
      * @param \Magento\Ui\DataProvider\AddFilterToCollectionInterface[] $addFilterStrategies
      * @param array $meta
@@ -34,6 +51,8 @@ class ProductCustomOptionsDataProvider extends ProductDataProvider
         $requestFieldName,
         CollectionFactory $collectionFactory,
         RequestInterface $request,
+        ProductOptionRepository $productOptionRepository,
+        ProductOptionValueModel $productOptionValueModel,
         array $addFieldStrategies = [],
         array $addFilterStrategies = [],
         array $meta = [],
@@ -49,7 +68,10 @@ class ProductCustomOptionsDataProvider extends ProductDataProvider
             $meta,
             $data
         );
+
         $this->request = $request;
+        $this->productOptionRepository = $productOptionRepository;
+        $this->productOptionValueModel = $productOptionValueModel;
     }
 
     /**
@@ -61,16 +83,36 @@ class ProductCustomOptionsDataProvider extends ProductDataProvider
     {
         if (!$this->getCollection()->isLoaded()) {
             $currentProductId = (int)$this->request->getParam('current_product_id');
+
             if (0 !== $currentProductId) {
                 $this->getCollection()->getSelect()->where('e.entity_id != ?', $currentProductId);
             }
+
             $this->getCollection()->getSelect()->distinct()->join(
                 ['opt' => $this->getCollection()->getTable('catalog_product_option')],
                 'opt.product_id = e.entity_id',
                 null
             );
             $this->getCollection()->load();
+
+            /** @var ProductInterface $product */
+            foreach ($this->getCollection() as $product) {
+                $options = [];
+
+                /** @var ProductOption|DataObject $option */
+                foreach ($this->productOptionRepository->getProductOptions($product) as $option) {
+                    $option->setData(
+                        'values',
+                        $this->productOptionValueModel->getValuesCollection($option)->toArray()['items']
+                    );
+
+                    $options[] = $option->toArray();
+                }
+
+                $product->setOptions($options);
+            }
         }
+
         $items = $this->getCollection()->toArray();
 
         return [
