f490212621f1d4a064123053837767346b48135b
Ivan Gavryshko <igavryshko@ebay.com>
2016-02-19 16:48:05 -0600

    MAGETWO-48149: Automate functional tests from Ogre Sprint M12 -4
    - fixed code style and typo

--- dev/tests/functional/tests/app/Magento/Search/Test/Handler/SynonymGroup/Curl.php
+++ dev/tests/functional/tests/app/Magento/Search/Test/Handler/SynonymGroup/Curl.php
@@ -57,7 +57,9 @@ class Curl extends AbstractCurl implements SynonymGroupInterface
         $response = $curl->read();
         $curl->close();
         if (!strpos($response, 'data-ui-id="messages-message-success"')) {
-            throw new \Exception("Synonym Group entity creating by curl handler was not successful! Response: $response");
+            throw new \Exception(
+                "Synonym Group entity creating by curl handler was not successful! Response: $response"
+            );
         }
 
         preg_match("`group_id\/(\d*?)\/`", $response, $matches);
