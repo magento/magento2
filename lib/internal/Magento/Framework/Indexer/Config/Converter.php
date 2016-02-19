6d0a8b098c8dd432255d2707f2bf4d5f5c15c682
Roman Ganin <rganin@magento.com>
2016-02-19 14:40:08 +0200

    MAGETWO-49089: Run catalogrule reindex only once in full mode
    - implemented new approach for indexers validation in console and cron
    - removed old implementation

--- lib/internal/Magento/Framework/Indexer/Config/Converter.php
+++ lib/internal/Magento/Framework/Indexer/Config/Converter.php
@@ -29,6 +29,7 @@ class Converter implements ConverterInterface
             $data['primary'] = $this->getAttributeValue($indexerNode, 'primary');
             $data['view_id'] = $this->getAttributeValue($indexerNode, 'view_id');
             $data['action_class'] = $this->getAttributeValue($indexerNode, 'class');
+            $data['shared_index'] = $this->getAttributeValue($indexerNode, 'shared_index');
             $data['title'] = '';
             $data['description'] = '';
 
