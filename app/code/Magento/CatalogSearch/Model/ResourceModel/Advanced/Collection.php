d2a22289cb08c6b5713b3524c5c8745528daacad
Maddy Chellathurai <mchellathura@magento.com>
2016-02-19 10:19:47 -0600

    MAGETWO-49469: Contribute Ogre Bugfixes and Stories for Sprint 44
    - Integrity fixes

--- app/code/Magento/CatalogSearch/Model/ResourceModel/Advanced/Collection.php
+++ app/code/Magento/CatalogSearch/Model/ResourceModel/Advanced/Collection.php
@@ -65,8 +65,8 @@ class Collection extends \Magento\Catalog\Model\ResourceModel\Product\Collection
      * @param \Magento\Customer\Model\Session $customerSession
      * @param \Magento\Framework\Stdlib\DateTime $dateTime
      * @param \Magento\Customer\Api\GroupManagementInterface $groupManagement
-     * @param \Magento\Search\Api\SearchInterface $search
      * @param \Magento\Catalog\Model\ResourceModel\Product\Collection\ProductLimitation $productLimitation
+     * @param \Magento\Search\Api\SearchInterface $search
      * @param \Magento\Framework\Search\Adapter\Mysql\TemporaryStorageFactory $temporaryStorageFactory
      * @param SearchCriteriaBuilder $searchCriteriaBuilder
      * @param FilterBuilder $filterBuilder
@@ -93,8 +93,8 @@ class Collection extends \Magento\Catalog\Model\ResourceModel\Product\Collection
         \Magento\Customer\Model\Session $customerSession,
         \Magento\Framework\Stdlib\DateTime $dateTime,
         \Magento\Customer\Api\GroupManagementInterface $groupManagement,
-        \Magento\Search\Api\SearchInterface $search,
         \Magento\Catalog\Model\ResourceModel\Product\Collection\ProductLimitation $productLimitation,
+        \Magento\Search\Api\SearchInterface $search,
         \Magento\Framework\Search\Adapter\Mysql\TemporaryStorageFactory $temporaryStorageFactory,
         SearchCriteriaBuilder $searchCriteriaBuilder,
         FilterBuilder $filterBuilder,
