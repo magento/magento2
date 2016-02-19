0400a68146c5016f48f67c257e4cc539fe2f7531
Paul Lewis <plewis@magento.com>
2016-02-19 10:49:33 -0600

    MAGETWO-47995: Zend Locale Data Cache issue in Magento File System
    - Change to use Translate cache lowlevel backend

--- lib/internal/Magento/Framework/Currency.php
+++ lib/internal/Magento/Framework/Currency.php
@@ -5,24 +5,24 @@
  */
 namespace Magento\Framework;
 
-use Magento\Framework\App\CacheInterface;
+use Magento\Framework\App\Cache\Type\Translate as TranslateCache;
 
 class Currency extends \Zend_Currency implements CurrencyInterface
 {
     /**
      * Creates a currency instance.
      *
-     * @param CacheInterface $appCache
+     * @param TranslateCache $translateCache
      * @param string|array $options Options array or currency short name when string is given
      * @param string $locale Locale name
      */
     public function __construct(
-        CacheInterface $appCache,
+        TranslateCache $translateCache,
         $options = null,
         $locale = null
     ) {
         // set Zend cache to low level frontend app cache
-        $lowLevelFrontendCache = $appCache->getFrontend()->getLowLevelFrontend();
+        $lowLevelFrontendCache = $translateCache->getLowLevelFrontend();
         \Zend_Currency::setCache($lowLevelFrontendCache);
         parent::__construct($options, $locale);
     }
