5ebb92da7e3e92108946b086f03ebf7b670ad14c
Oleksii Korshenko <okorshenko@magento.com>
2016-02-22 10:57:35 -0600

    MAGETWO-48124: User with permissions restricted to a single store can view orders on all stores

---
+++ app/code/Magento/Sales/Model/ResourceModel/Order/Grid/Collection.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Sales\Model\ResourceModel\Order\Grid;
+
+use Magento\Framework\Data\Collection\Db\FetchStrategyInterface as FetchStrategy;
+use Magento\Framework\Data\Collection\EntityFactoryInterface as EntityFactory;
+use Magento\Framework\Event\ManagerInterface as EventManager;
+use Psr\Log\LoggerInterface as Logger;
+
+/**
+ * Order grid collection
+ */
+class Collection extends \Magento\Framework\View\Element\UiComponent\DataProvider\SearchResult
+{
+    /**
+     * @inheritDoc
+     */
+    public function __construct(
+        EntityFactory $entityFactory,
+        Logger $logger,
+        FetchStrategy $fetchStrategy,
+        EventManager $eventManager,
+        $mainTable = 'sales_order_grid',
+        $resourceModel = '\Magento\Sales\Model\ResourceModel\Order'
+    ) {
+        parent::__construct($entityFactory, $logger, $fetchStrategy, $eventManager, $mainTable, $resourceModel);
+    }
+}
