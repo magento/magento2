183db48fdef55d54c59d12175b1ba4565159bd68
Dale Sikkema <dsikkema@ebay.com>
2016-02-22 09:08:37 -0600

    MAGETWO-48732: Multiple FAT tests randomly fail because success message is not dispalyed on save  - unskip functional tests

--- dev/tests/functional/tests/app/Magento/Store/Test/TestCase/CreateStoreEntityTest.php
+++ dev/tests/functional/tests/app/Magento/Store/Test/TestCase/CreateStoreEntityTest.php
@@ -72,7 +72,6 @@ class CreateStoreEntityTest extends Injectable
      */
     public function test(Store $store)
     {
-        $this->markTestIncomplete('MAGETWO-48723');
         //Steps:
         $this->storeIndex->open();
         $this->storeIndex->getGridPageActions()->addStoreView();
