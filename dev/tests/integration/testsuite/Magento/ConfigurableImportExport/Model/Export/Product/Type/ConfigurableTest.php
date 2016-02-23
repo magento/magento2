eeb46dc9fa8b8aa5e1023c017201f6cfe6c26e1e
Igor Melnikov <imelnikov@ebay.com>
2016-02-23 14:12:02 -0600

    MAGETWO-49013: ConfigurableImportExport Integration Test
    Fixing namespace

--- dev/tests/integration/testsuite/Magento/ConfigurableImportExport/Model/Export/Product/Type/ConfigurableTest.php
+++ dev/tests/integration/testsuite/Magento/ConfigurableImportExport/Model/Export/Product/Type/ConfigurableTest.php
@@ -3,7 +3,7 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\BundleImportExport\Model\Export\Product\Type;
+namespace Magento\ConfigurableImportExport\Model\Export\Product\Type;
 
 use Magento\CatalogImportExport\Model\Export\AbstractProductExportTestCase;
 
