0564ce72634ca489c25d836b4e10bd6aff682926
Joan He <johe@ebay.com>
2016-02-23 13:16:35 -0600

    MAGETWO-49013: ConfigurableImportExport Integration Test
    - added integration test

---
+++ dev/tests/integration/testsuite/Magento/ConfigurableImportExport/Model/Export/Product/Type/ConfigurableTest.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\BundleImportExport\Model\Export\Product\Type;
+
+use Magento\CatalogImportExport\Model\Export\AbstractProductExportTestCase;
+
+class ConfigurableTest extends AbstractProductExportTestCase
+{
+    public function exportDataProvider()
+    {
+        return [
+            'configurable-product' => [
+                'Magento/ConfigurableProduct/_files/product_configurable.php',
+                [
+                    'configurable',
+                ],
+                ['_cache_instance_products', '_cache_instance_configurable_attributes'],
+            ],
+        ];
+    }
+}
