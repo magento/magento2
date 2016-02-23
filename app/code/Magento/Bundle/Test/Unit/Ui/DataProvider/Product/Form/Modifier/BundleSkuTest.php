a6cd947a920ab64b18f7dd6392069aca9911774b
Yuri Kovsher <ikovsher@ebay.com>
2016-02-23 19:24:40 +0200

    MAGETWO-49484: sku_type, weight_type, shipment_type are not correctly displayed after saving bundle product

--- app/code/Magento/Bundle/Test/Unit/Ui/DataProvider/Product/Form/Modifier/BundleSkuTest.php
+++ app/code/Magento/Bundle/Test/Unit/Ui/DataProvider/Product/Form/Modifier/BundleSkuTest.php
@@ -6,11 +6,7 @@
 namespace Magento\Bundle\Test\Unit\Ui\DataProvider\Product\Form\Modifier;
 
 use Magento\Bundle\Ui\DataProvider\Product\Form\Modifier\BundleSku;
-use Magento\Catalog\Api\Data\ProductAttributeInterface;
 
-/**
- * Class BundleSkuTest
- */
 class BundleSkuTest extends AbstractModifierTest
 {
     /**
@@ -18,32 +14,47 @@ class BundleSkuTest extends AbstractModifierTest
      */
     protected function createModel()
     {
-        return $this->objectManager->getObject(BundleSku::class, [
-            'arrayManager' => $this->arrayManagerMock,
-        ]);
+        return $this->objectManager->getObject(
+            BundleSku::class,
+            ['arrayManager' => $this->arrayManagerMock]
+        );
     }
 
-    /**
-     * @return void
-     */
     public function testModifyMeta()
     {
+        $skuTypePath = 'bundle-items/children/' . BundleSku::CODE_SKU_TYPE . BundleSku::META_CONFIG_PATH;
         $sourceMeta = [
-            'testGroup' => [
+            'bundle-items' => [
                 'children' => [
-                    ProductAttributeInterface::CODE_SKU => [
-                        'componentType' => 'testComponent',
-                    ],
+                    BundleSku::CODE_SKU_TYPE => []
                 ]
-            ],
+            ]
         ];
-        $modifiedMeta = $this->getModel()->modifyMeta($sourceMeta);
-        $this->assertArrayHasKey(BundleSku::CODE_SKU_TYPE, $modifiedMeta['testGroup']['children']);
+        $skuTypeParams = [
+            'valueMap' => [
+                'false' => '1',
+                'true' => '0'
+            ],
+            'validation' => [
+                'required-entry' => false
+            ]
+        ];
+        $skuTypeMeta = [
+            'bundle-items' => [
+                'children' => [
+                    BundleSku::CODE_SKU_TYPE => $skuTypeParams
+                ]
+            ]
+        ];
+
+        $this->arrayManagerMock->expects($this->once())
+            ->method('merge')
+            ->with($skuTypePath, $sourceMeta, $skuTypeParams)
+            ->willReturn($skuTypeMeta);
+
+        $this->assertSame($skuTypeMeta, $this->getModel()->modifyMeta($sourceMeta));
     }
 
-    /**
-     * @return void
-     */
     public function testModifyData()
     {
         $expectedData = [];
--- app/code/Magento/Bundle/Test/Unit/Ui/DataProvider/Product/Form/Modifier/BundleWeightTest.php
+++ app/code/Magento/Bundle/Test/Unit/Ui/DataProvider/Product/Form/Modifier/BundleWeightTest.php
@@ -6,10 +6,9 @@
 namespace Magento\Bundle\Test\Unit\Ui\DataProvider\Product\Form\Modifier;
 
 use Magento\Bundle\Ui\DataProvider\Product\Form\Modifier\BundleWeight;
+use Magento\Catalog\Api\Data\ProductAttributeInterface;
+use Magento\Framework\Stdlib\ArrayManager;
 
-/**
- * Class BundleWeightTest
- */
 class BundleWeightTest extends AbstractModifierTest
 {
     /**
@@ -17,32 +16,124 @@ class BundleWeightTest extends AbstractModifierTest
      */
     protected function createModel()
     {
-        return $this->objectManager->getObject(BundleWeight::class, [
-            'arrayManager' => $this->arrayManagerMock,
-        ]);
+        return $this->objectManager->getObject(
+            BundleWeight::class,
+            ['arrayManager' => $this->arrayManagerMock]
+        );
     }
 
     /**
-     * @return void
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
      */
     public function testModifyMeta()
     {
+        $weightTypePath = 'bundle-items/children/' . BundleWeight::CODE_WEIGHT_TYPE . BundleWeight::META_CONFIG_PATH;
+        $weightPath = 'product-details/children/' . ProductAttributeInterface::CODE_WEIGHT
+            . BundleWeight::META_CONFIG_PATH;
+        $hasWeightPath = 'product-details/children/' . ProductAttributeInterface::CODE_HAS_WEIGHT
+            . BundleWeight::META_CONFIG_PATH;
         $sourceMeta = [
-            'testGroup' => [
+            'product-details' => [
                 'children' => [
-                    BundleWeight::CODE_CONTAINER_WEIGHT  => [
-                        'componentType' => 'testComponent',
-                    ],
+                    ProductAttributeInterface::CODE_WEIGHT => [],
+                    ProductAttributeInterface::CODE_HAS_WEIGHT => []
                 ]
             ],
+            'bundle-items' => [
+                'children' => [
+                    BundleWeight::CODE_WEIGHT_TYPE => []
+                ]
+            ]
         ];
-        $modifiedMeta = $this->getModel()->modifyMeta($sourceMeta);
-        $this->assertArrayHasKey(BundleWeight::CODE_WEIGHT_TYPE, $modifiedMeta['testGroup']['children']);
+        $weightTypeParams = [
+            'valueMap' => [
+                'false' => '1',
+                'true' => '0'
+            ],
+            'validation' => [
+                'required-entry' => false
+            ]
+        ];
+        $weightParams = [
+            'imports' => [
+                'disabled' => 'ns = ${ $.ns }, index = ' . BundleWeight::CODE_WEIGHT_TYPE . ':checked',
+            ]
+        ];
+        $hasWeightParams = [
+            'disabled' => true,
+            'visible' => false
+        ];
+        $weightTypeMeta = [
+            'product-details' => [
+                'children' => [
+                    ProductAttributeInterface::CODE_WEIGHT => [],
+                    ProductAttributeInterface::CODE_HAS_WEIGHT => []
+                ]
+            ],
+            'bundle-items' => [
+                'children' => [
+                    BundleWeight::CODE_WEIGHT_TYPE => $weightTypeParams
+                ]
+            ]
+        ];
+        $hasWeightMeta = [
+            'product-details' => [
+                'children' => [
+                    ProductAttributeInterface::CODE_WEIGHT => [],
+                    ProductAttributeInterface::CODE_HAS_WEIGHT => $hasWeightParams
+                ]
+            ],
+            'bundle-items' => [
+                'children' => [
+                    BundleWeight::CODE_WEIGHT_TYPE => $weightTypeParams
+                ]
+            ]
+        ];
+        $weightMeta = [
+            'product-details' => [
+                'children' => [
+                    ProductAttributeInterface::CODE_WEIGHT => $weightParams,
+                    ProductAttributeInterface::CODE_HAS_WEIGHT => $hasWeightParams
+                ]
+            ],
+            'bundle-items' => [
+                'children' => [
+                    BundleWeight::CODE_WEIGHT_TYPE => $weightTypeParams
+                ]
+            ]
+        ];
+
+        $this->arrayManagerMock->expects($this->exactly(3))
+            ->method('merge')
+            ->willReturnMap(
+                [
+                    [
+                        $weightTypePath,
+                        $sourceMeta,
+                        $weightTypeParams,
+                        ArrayManager::DEFAULT_PATH_DELIMITER,
+                        $weightTypeMeta
+                    ],
+                    [
+                        $hasWeightPath,
+                        $weightTypeMeta,
+                        $hasWeightParams,
+                        ArrayManager::DEFAULT_PATH_DELIMITER,
+                        $hasWeightMeta
+                    ],
+                    [
+                        $weightPath,
+                        $hasWeightMeta,
+                        $weightParams,
+                        ArrayManager::DEFAULT_PATH_DELIMITER,
+                        $weightMeta
+                    ]
+                ]
+            );
+
+        $this->assertSame($weightMeta, $this->getModel()->modifyMeta($sourceMeta));
     }
 
-    /**
-     * @return void
-     */
     public function testModifyData()
     {
         $expectedData = [];
