b8cd6e274f9283580300a45a9bf1344435272cb3
Joan He <johe@ebay.com>
2016-02-24 15:01:09 -0600

    MAGETWO-49017: AdvancedPricingImportExport Integration Test

---
+++ dev/tests/integration/testsuite/Magento/AdvancedPricingImportExport/Model/Export/AdvancedPricingTest.php
@@ -0,0 +1,137 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\AdvancedPricingImportExport\Model\Export;
+
+use Magento\Framework\App\Filesystem\DirectoryList;
+
+class AdvancedPricingTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \Magento\AdvancedPricingImportExport\Model\Export\AdvancedPricing
+     */
+    protected $model;
+
+    /**
+     * @var \Magento\Framework\ObjectManagerInterface
+     */
+    protected $objectManager;
+
+    /**
+     * @var \Magento\Framework\Filesystem
+     */
+    protected $fileSystem;
+
+    /**
+     * skipped attributes
+     *
+     * @var array
+     */
+    public static $skippedAttributes = [
+        'options',
+        'updated_at',
+        'extension_attributes',
+        'category_ids',
+    ];
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->objectManager = \Magento\TestFramework\Helper\Bootstrap::getObjectManager();
+        $this->fileSystem = $this->objectManager->get('Magento\Framework\Filesystem');
+        $this->model = $this->objectManager->create(
+            'Magento\AdvancedPricingImportExport\Model\Export\AdvancedPricing'
+        );
+    }
+
+    /**
+     * @magentoAppArea adminhtml
+     * @magentoDbIsolation enabled
+     * @magentoAppIsolation enabled
+     */
+    public function testExport()
+    {
+        $fixture = 'Magento/Catalog/_files/product_simple.php';
+        $skus = ['simple'];
+        $fixturePath = $this->fileSystem->getDirectoryRead(DirectoryList::ROOT)
+            ->getAbsolutePath('/dev/tests/integration/testsuite/' . $fixture);
+        include $fixturePath;
+
+        $skippedAttributes = self::$skippedAttributes;
+        $this->executeExportTest($skus, $skippedAttributes);
+    }
+
+    protected function executeExportTest($skus, $skippedAttributes)
+    {
+        $productRepository = $this->objectManager->create(
+            'Magento\Catalog\Api\ProductRepositoryInterface'
+        );
+        $index = 0;
+        $ids = [];
+        $origPricingData = [];
+        while (isset($skus[$index])) {
+            $ids[$index] = $productRepository->get($skus[$index])->getId();
+            $origPricingData[$index] = $this->objectManager->create('Magento\Catalog\Model\Product')->load($ids[$index])->getTierPrices();
+            $index++;
+        }
+
+        $csvfile = uniqid('importexport_') . '.csv';
+
+        $this->model->setWriter(
+            \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create(
+                'Magento\ImportExport\Model\Export\Adapter\Csv',
+                ['fileSystem' => $this->fileSystem, 'destination' => $csvfile]
+            )
+        );
+        $this->assertNotEmpty($this->model->export());
+
+        /** @var \Magento\CatalogImportExport\Model\Import\Product $importModel */
+        $importModel = $this->objectManager->create(
+            'Magento\AdvancedPricingImportExport\Model\Import\AdvancedPricing'
+        );
+        $directory = $this->fileSystem->getDirectoryWrite(DirectoryList::VAR_DIR);
+        $source = $this->objectManager->create(
+            '\Magento\ImportExport\Model\Import\Source\Csv',
+            [
+                'file' => $csvfile,
+                'directory' => $directory
+            ]
+        );
+        $errors = $importModel->setParameters(
+            [
+                'behavior' => \Magento\ImportExport\Model\Import::BEHAVIOR_APPEND,
+                'entity' => 'advanced_pricing'
+            ]
+        )->setSource(
+            $source
+        )->validateData();
+
+        $this->assertTrue($errors->getErrorsCount() == 0, 'Advanced Pricing import error, imported from file:' . $csvfile);
+        $importModel->importData();
+
+        while ($index > 0) {
+            $index--;
+            $newPricingData = $this->objectManager->create('Magento\Catalog\Model\Product')->load($ids[$index])->getTierPrices();
+            $this->assertEquals(count($origPricingData[$index]), count($newPricingData));
+            $this->assertEqualsOtherThanSkippedAttributes($origPricingData[$index], $newPricingData, $skippedAttributes);
+        }
+    }
+
+    private function assertEqualsOtherThanSkippedAttributes($expected, $actual, $skippedAttributes)
+    {
+        foreach ($expected as $key => $value) {
+            if (in_array($key, $skippedAttributes)) {
+                continue;
+            } else {
+                $this->assertEquals(
+                    $value,
+                    $actual[$key],
+                    'Assert value at key - ' . $key . ' failed'
+                );
+            }
+        }
+    }
+}
