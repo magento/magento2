0a2dce2b0d33ae56cf960b49c8b92e01d532b37b
Bohdan Korablov <bkorablov@magento.com>
2016-02-24 15:15:41 +0200

    MAGETWO-49680: Implement import of products from wizard and grid to dynamic rows

---
+++ app/code/Magento/ConfigurableProduct/view/adminhtml/web/js/components/dynamic-rows-configurable.js
@@ -0,0 +1,192 @@
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+define([
+    'underscore',
+    'Magento_Ui/js/dynamic-rows/dynamic-rows'
+], function (_, dynamicRows) {
+    'use strict';
+
+    return dynamicRows.extend({
+        defaults: {
+            canEditField: 'canEdit',
+            dataProviderFromGrid: '',
+            insertDataFromGrid: [],
+            map: null,
+            cacheGridData: [],
+            deleteProperty: false,
+            dataLength: 0,
+            identificationProperty: 'id',
+            listens: {
+                'insertDataFromGrid': 'processingInsertData',
+                'elems': 'mappingValue'
+            }
+        },
+
+        /**
+         * Calls 'initObservable' of parent
+         *
+         * @returns {Object} Chainable.
+         */
+        initObservable: function () {
+            this._super()
+                .observe([
+                    'insertDataFromGrid'
+                ]);
+
+            return this;
+        },
+
+        /**
+         * Initialize children,
+         * set data from server to grid dataScope
+         */
+        initChildren: function () {
+            var insertData = [];
+
+            if (this.recordData().length) {
+                this.recordData.each(function (recordData) {
+                    var unmappingValue = this.unmappingValue(recordData);
+                    unmappingValue[this.canEditField] = false;
+                    insertData.push(unmappingValue);
+                }, this);
+
+                this.source.set(this.dataProviderFromGrid, insertData);
+            }
+
+            return this;
+        },
+
+        /**
+         * Unmapping value,
+         * unmapping data from server to grid dataScope
+         *
+         * @param {Object} data - data object
+         */
+        unmappingValue: function (data) {
+            var obj = {};
+
+            _.each(this.map, function (prop, index) {
+                obj[prop] = data[index];
+            }, this);
+
+            return obj;
+        },
+
+        /**
+         * Rerender dynamic-rows elems
+         */
+        reload: function () {
+            this.cacheGridData = [];
+            this._super();
+        },
+
+        /**
+         * Parsed data
+         *
+         * @param {Array} data - array with data
+         * about selected records
+         */
+        processingInsertData: function (data) {
+            var changes;
+
+            if (!data.length) {
+                this.elems([]);
+            }
+
+            changes = this._checkGridData(data);
+            this.cacheGridData = data;
+
+            changes.each(function (changedObject) {
+                this.addChild(changedObject, false, parseInt(changedObject[this.map.id], 10));
+            }, this);
+        },
+
+        /**
+         * Delete record instance
+         * update data provider dataScope
+         *
+         * @param {String|Number} index - record index
+         */
+        deleteRecord: function (index, recordId) {
+            var data = this.getElementData(this.insertDataFromGrid(), recordId);
+
+            this.mapping = true;
+            this._super();
+            this.insertDataFromGrid(_.reject(this.source.get(this.dataProviderFromGrid), function (recordData) {
+                return parseInt(recordData[this.map.id], 10) === parseInt(data[this.map.id], 10);
+            }, this));
+            this.mapping = false;
+        },
+
+        /**
+         * Check changed records
+         *
+         * @param {Array} data - array with records data
+         * @returns {Array} Changed records
+         */
+        _checkGridData: function (data) {
+            var cacheLength = this.cacheGridData.length,
+                curData = data.length,
+                max = cacheLength > curData ? this.cacheGridData : data,
+                changes = [],
+                obj = {};
+
+            max.each(function (record, index) {
+                obj[this.map.id] = record[this.map.id];
+
+                if (!_.where(this.cacheGridData, obj).length) {
+                    changes.push(data[index]);
+                }
+            }, this);
+
+            return changes;
+        },
+
+        /**
+         * Mapped value
+         */
+        mappingValue: function () {
+            var path,
+                data,
+                elements = this.elems();
+
+            if (this.mapping) {
+                return false;
+            }
+
+            elements.each(function (record) {
+                data = this.getElementData(this.insertDataFromGrid(), record.recordId);
+                _.each(this.map, function (prop, index) {
+                    path = record.dataScope + '.' + index;
+                    this.source.set(path, data[prop]);
+                }, this);
+                path = record.dataScope + '.' + this.canEditField;
+                this.source.set(path, false);
+            }, this);
+        },
+
+        /**
+         * Find data object by index
+         *
+         * @param {Array} array - data collection
+         * @param {Number} index - element index
+         * @param {String} property - to find by property
+         *
+         * @returns {Object} data object
+         */
+        getElementData: function (array, index, property) {
+            var obj = {},
+                result;
+
+            property ? obj[property] = index : obj[this.map.id] = index;
+            result = _.findWhere(array, obj);
+            !result ? property ? obj[property] = index.toString() : obj[this.map.id] = index.toString() : false;
+            result = _.findWhere(array, obj);
+
+            return result;
+        }
+    });
+});
