8add7aec19bb33866f8cb055add23295f055adae
Stanislav Lopukhov <slopukhov@ebay.com>
2016-02-24 17:33:56 +0200

    MAGETWO-49161: Update image component in the Category form

---
+++ app/code/Magento/Catalog/Controller/Adminhtml/Category/Image/Upload.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Catalog\Controller\Adminhtml\Category\Image;
+
+use Magento\Framework\Controller\ResultFactory;
+
+class Upload extends \Magento\Backend\App\Action
+{
+    /**
+     * @var \Magento\MediaStorage\Model\File\UploaderFactory
+     */
+    private $uploaderFactory;
+
+    /**
+     * @var \Magento\MediaStorage\Helper\File\Storage\Database
+     */
+    private $storageDatabase;
+
+    /**
+     * Media Directory object (writable).
+     *
+     * @var \Magento\Framework\Filesystem\Directory\WriteInterface
+     */
+    protected $mediaDirectory;
+
+    /**
+     * Store manager
+     *
+     * @var \Magento\Store\Model\StoreManagerInterface
+     */
+    protected $storeManager;
+
+    /**
+     * Upload constructor.
+     *
+     * @param \Magento\Backend\App\Action\Context $context
+     * @param \Magento\MediaStorage\Model\File\UploaderFactory $uploaderFactory
+     * @param \Magento\MediaStorage\Helper\File\Storage\Database $storageDatabase
+     * @param \Magento\Framework\Filesystem $filesystem
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+     */
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Magento\MediaStorage\Model\File\UploaderFactory $uploaderFactory,
+        \Magento\MediaStorage\Helper\File\Storage\Database $storageDatabase,
+        \Magento\Framework\Filesystem $filesystem,
+        \Magento\Store\Model\StoreManagerInterface $storeManager
+    ) {
+        parent::__construct($context);
+        $this->mediaDirectory = $filesystem->getDirectoryWrite(\Magento\Framework\App\Filesystem\DirectoryList::MEDIA);
+        $this->uploaderFactory = $uploaderFactory;
+        $this->storageDatabase = $storageDatabase;
+        $this->storeManager = $storeManager;
+    }
+
+    /**
+     * Check admin permissions for this controller
+     *
+     * @return boolean
+     */
+    protected function _isAllowed()
+    {
+        return $this->_authorization->isAllowed('Magento_Catalog::categories');
+    }
+
+    /**
+     * Upload file controller action
+     *
+     * @return \Magento\Framework\Controller\ResultInterface
+     */
+    public function execute()
+    {
+        $tmpPath = 'catalog/tmp/category/';
+
+        try {
+            $uploader = $this->uploaderFactory->create(['fileId' => 'image']);
+            $uploader->setAllowedExtensions(['jpg', 'jpeg', 'gif', 'png']);
+            $uploader->setAllowRenameFiles(true);
+
+            $result = $uploader->save($this->mediaDirectory->getAbsolutePath($tmpPath));
+
+            if (!$result) {
+                throw new \Exception('File can not be moved from temporary folder to the destination folder.');
+            }
+
+            /**
+             * Workaround for prototype 1.7 methods "isJSON", "evalJSON" on Windows OS
+             */
+            $result['tmp_name'] = str_replace('\\', '/', $result['tmp_name']);
+            $result['path'] = str_replace('\\', '/', $result['path']);
+            $result['url'] = $this->storeManager
+                    ->getStore()
+                    ->getBaseUrl(
+                        \Magento\Framework\UrlInterface::URL_TYPE_MEDIA
+                    ) . $tmpPath . $result['name'];
+
+            if (isset($result['file'])) {
+                $relativePath = rtrim($tmpPath, '/') . '/' . ltrim($result['file'], '/');
+                $this->storageDatabase->saveFile($relativePath);
+            }
+
+            $result['cookie'] = [
+                'name' => $this->_getSession()->getName(),
+                'value' => $this->_getSession()->getSessionId(),
+                'lifetime' => $this->_getSession()->getCookieLifetime(),
+                'path' => $this->_getSession()->getCookiePath(),
+                'domain' => $this->_getSession()->getCookieDomain(),
+            ];
+        } catch (\Exception $e) {
+            $result = ['error' => $e->getMessage(), 'errorcode' => $e->getCode()];
+        }
+        return $this->resultFactory->create(ResultFactory::TYPE_JSON)->setData($result);
+    }
+}
