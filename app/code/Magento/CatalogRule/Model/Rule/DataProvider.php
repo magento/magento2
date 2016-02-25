56d6baf3d9911f579da167fae48cfc77cb95e845
Stanislav Idolov <sidolov@magento.com>
2016-02-25 16:29:55 +0200

    MAGETWO-49502: Move logic from \Magento\CatalogRule\Model\Rule\DataProvider::getMetadata to catalog_rule.xml

--- app/code/Magento/CatalogRule/Model/Rule/DataProvider.php
+++ app/code/Magento/CatalogRule/Model/Rule/DataProvider.php
@@ -8,11 +8,6 @@ namespace Magento\CatalogRule\Model\Rule;
 use Magento\CatalogRule\Model\ResourceModel\Rule\Collection;
 use Magento\CatalogRule\Model\ResourceModel\Rule\CollectionFactory;
 use Magento\CatalogRule\Model\Rule;
-use Magento\Store\Model\System\Store;
-use Magento\Customer\Api\GroupRepositoryInterface;
-use Magento\Framework\Api\SearchCriteriaBuilder;
-use Magento\Framework\Convert\DataObject;
-use Magento\CatalogRule\Model\Rule\Action\SimpleActionOptionsProvider;
 use Magento\Framework\App\Request\DataPersistorInterface;
 
 /**
@@ -32,31 +27,6 @@ class DataProvider extends \Magento\Ui\DataProvider\AbstractDataProvider
     protected $loadedData;
 
     /**
-     * @var Store
-     */
-    protected $store;
-
-    /**
-     * @var GroupRepositoryInterface
-     */
-    protected $groupRepository;
-
-    /**
-     * @var SearchCriteriaBuilder
-     */
-    protected $searchCriteriaBuilder;
-
-    /**
-     * @var DataObject
-     */
-    protected $objectConverter;
-
-    /**
-     * @var SimpleActionOptionsProvider
-     */
-    protected $actionOptions;
-
-    /**
      * @var DataPersistorInterface
      */
     protected $dataPersistor;
@@ -66,110 +36,22 @@ class DataProvider extends \Magento\Ui\DataProvider\AbstractDataProvider
      * @param string $primaryFieldName
      * @param string $requestFieldName
      * @param CollectionFactory $collectionFactory
-     * @param Store $store
-     * @param GroupRepositoryInterface $groupRepository
-     * @param SearchCriteriaBuilder $searchCriteriaBuilder
-     * @param DataObject $objectConverter
-     * @param SimpleActionOptionsProvider $actionOptions
      * @param DataPersistorInterface $dataPersistor
      * @param array $meta
      * @param array $data
-     * @SuppressWarnings(PHPMD.ExcessiveParameterList)
      */
     public function __construct(
         $name,
         $primaryFieldName,
         $requestFieldName,
         CollectionFactory $collectionFactory,
-        Store $store,
-        GroupRepositoryInterface $groupRepository,
-        SearchCriteriaBuilder $searchCriteriaBuilder,
-        DataObject $objectConverter,
-        SimpleActionOptionsProvider $actionOptions,
         DataPersistorInterface $dataPersistor,
         array $meta = [],
         array $data = []
     ) {
-        $this->groupRepository = $groupRepository;
         $this->collection = $collectionFactory->create();
-        $this->store = $store;
-        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
-        $this->objectConverter = $objectConverter;
-        $this->actionOptions = $actionOptions;
         $this->dataPersistor = $dataPersistor;
-
-        $meta = array_replace_recursive($meta, $this->getMetadata());
         parent::__construct($name, $primaryFieldName, $requestFieldName, $meta, $data);
-    }
-
-    /**
-     * @return array
-     */
-    protected function getMetadata()
-    {
-        $customerGroups = $this->groupRepository->getList($this->searchCriteriaBuilder->create())->getItems();
-
-        return [
-            'rule_information' => [
-                'children' => [
-                    'website_ids' => [
-                        'arguments' => [
-                            'data' => [
-                                'config' => [
-                                    'options' => $this->store->getWebsiteValuesForForm(),
-                                ],
-                            ],
-                        ],
-                    ],
-                    'is_active' => [
-                        'arguments' => [
-                            'data' => [
-                                'config' => [
-                                    'options' => [
-                                        ['label' => __('Active'), 'value' => '1'],
-                                        ['label' => __('Inactive'), 'value' => '0']
-                                    ],
-                                ],
-                            ],
-                        ],
-                    ],
-                    'customer_group_ids' => [
-                        'arguments' => [
-                            'data' => [
-                                'config' => [
-                                    'options' => $this->objectConverter->toOptionArray($customerGroups, 'id', 'code'),
-                                ],
-                            ],
-                        ],
-                    ]
-                ]
-            ],
-            'actions' => [
-                'children' => [
-                    'simple_action' => [
-                        'arguments' => [
-                            'data' => [
-                                'config' => [
-                                    'options' => $this->actionOptions->toOptionArray()
-                                ],
-                            ],
-                        ],
-                    ],
-                    'stop_rules_processing' => [
-                        'arguments' => [
-                            'data' => [
-                                'config' => [
-                                    'options' => [
-                                        ['label' => __('Yes'), 'value' => '1'],
-                                        ['label' => __('No'), 'value' => '0']
-                                    ]
-                                ],
-                            ],
-                        ],
-                    ],
-                ]
-            ]
-        ];
     }
 
     /**
