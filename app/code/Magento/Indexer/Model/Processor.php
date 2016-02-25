4eb21e52e7b7da5ac868534f6b6d14821fb648dd
Roman Ganin <rganin@magento.com>
2016-02-25 12:04:57 +0200

    MAGETWO-49089: Run catalogrule reindex only once in full mode

--- app/code/Magento/Indexer/Model/Processor.php
+++ app/code/Magento/Indexer/Model/Processor.php
@@ -52,7 +52,6 @@ class Processor
         $this->indexerFactory = $indexerFactory;
         $this->indexersFactory = $indexersFactory;
         $this->mviewProcessor = $mviewProcessor;
-        $config = $this->config->getIndexers();
     }
 
     /**
