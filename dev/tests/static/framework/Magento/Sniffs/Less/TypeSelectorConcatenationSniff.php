7c8976e938ad1ced6799fd2648c679138254d78e
Ruslan Kostiv <rkostiv@ebay.com>
2016-02-25 11:49:59 +0200

    MAGETWO-44622: Less files accordance to code standards should be cover with tests

---
+++ dev/tests/static/framework/Magento/Sniffs/Less/TypeSelectorConcatenationSniff.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Sniffs\Less;
+
+use PHP_CodeSniffer_File;
+use PHP_CodeSniffer_Sniff;
+
+/**
+ * Class TypeSelectorConcatenation
+ *
+ * Ensure that selector in one line, concatenation is not used
+ *
+ * @link http://devdocs.magento.com/guides/v2.0/coding-standards/code-standard-less.html#formatting-1
+ *
+ */
+class TypeSelectorConcatenationSniff implements PHP_CodeSniffer_Sniff
+{
+    /**
+     * A list of tokenizers this sniff supports.
+     *
+     * @var array
+     */
+    public $supportedTokenizers = ['CSS'];
+
+    /**
+     * {@inheritdoc}
+     */
+    public function register()
+    {
+        return [T_BITWISE_AND];
+    }
+
+    /**
+     * @var array
+     */
+    private $symbolsBeforeConcat = ['    ', "\n"];
+
+    /**
+     * {@inheritdoc}
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+
+        if (0 === strpos($tokens[$stackPtr + 1]['content'], '-')
+            && in_array($tokens[$stackPtr - 1]['content'], $this->symbolsBeforeConcat)
+        ) {
+            $phpcsFile->addError('Concatenation is used', $stackPtr);
+        }
+    }
+}
