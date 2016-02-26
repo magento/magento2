92fb4ee46e066da0787541e0b8bcbe5758a9f2ac
David Alger <david@classyllama.com>
2016-02-26 15:38:44 -0600

    Refactored travis scripts out of main travis config file

---
+++ dev/travis/before_install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Copyright © 2015 Magento. All rights reserved.
+# See COPYING.txt for license details.
+
+# install or update composer in casher dir
+if [ "$CASHER_DIR" ]; then
+    if [ -x $HOME/.cache/bin/composer ]; then
+        $HOME/.cache/bin/composer self-update
+    else
+        mkdir -p $HOME/.cache/bin
+        curl --connect-timeout 30 -sS https://getcomposer.org/installer \
+            | php -- --install-dir $HOME/.cache/bin/ --filename composer
+    fi
+fi
