88bb0cf319c7d280696f7822775f5d2199314c17
Joan He <johe@ebay.com>
2016-02-26 16:00:36 -0600

    MAGETWO-49592: Add fixture with product updates for CatalogImportExport integration test

--- dev/tests/integration/testsuite/Magento/CatalogImportExport/Model/AbstractProductExportImportTestCase.php
+++ dev/tests/integration/testsuite/Magento/CatalogImportExport/Model/AbstractProductExportImportTestCase.php
@@ -61,10 +61,10 @@ class AbstractProductExportImportTestCase extends \PHPUnit_Framework_TestCase
      */
     public function testExport($fixtures, $skus, $skippedAttributes = [], $rollbackFixtures = [])
     {
-        $this->executeFixtures($skus, $fixtures);
+        $this->executeFixtures($fixtures);
         $skippedAttributes = array_merge(self::$skippedAttributes, $skippedAttributes);
         $this->executeExportTest($skus, $skippedAttributes);
-        $this->executeFixtures($skus, $rollbackFixtures);
+        $this->executeFixtures($rollbackFixtures);
     }
 
     protected function executeExportTest($skus, $skippedAttributes)
@@ -82,36 +82,9 @@ class AbstractProductExportImportTestCase extends \PHPUnit_Framework_TestCase
             $index++;
         }
 
-        $csvfile = uniqid('importexport_') . '.csv';
+        $csvfile = $this->exportProducts();
+        $this->importProducts($csvfile, \Magento\ImportExport\Model\Import::BEHAVIOR_APPEND);
 
-        $this->model->setWriter(
-            \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create(
-                'Magento\ImportExport\Model\Export\Adapter\Csv',
-                ['fileSystem' => $this->fileSystem, 'destination' => $csvfile]
-            )
-        );
-        $this->assertNotEmpty($this->model->export());
-
-        /** @var \Magento\CatalogImportExport\Model\Import\Product $importModel */
-        $importModel = $this->objectManager->create(
-            'Magento\CatalogImportExport\Model\Import\Product'
-        );
-        $directory = $this->fileSystem->getDirectoryWrite(DirectoryList::VAR_DIR);
-        $source = $this->objectManager->create(
-            '\Magento\ImportExport\Model\Import\Source\Csv',
-            [
-                'file' => $csvfile,
-                'directory' => $directory
-            ]
-        );
-        $errors = $importModel->setParameters(
-            ['behavior' => \Magento\ImportExport\Model\Import::BEHAVIOR_APPEND, 'entity' => 'catalog_product']
-        )->setSource(
-            $source
-        )->validateData();
-
-        $this->assertTrue($errors->getErrorsCount() == 0, 'Product import error, imported from file:' . $csvfile);
-        $importModel->importData();
 
         while ($index > 0) {
             $index--;
@@ -158,9 +131,9 @@ class AbstractProductExportImportTestCase extends \PHPUnit_Framework_TestCase
      */
     public function testImportDelete($fixtures, $skus, $skippedAttributes = [], $rollbackFixtures = [])
     {
-        $this->executeFixtures($skus, $fixtures);
+        $this->executeFixtures($fixtures);
         $this->executeImportDeleteTest($skus);
-        $this->executeFixtures($skus, $rollbackFixtures);
+        $this->executeFixtures($rollbackFixtures);
     }
 
     protected function executeImportDeleteTest($skus)
@@ -173,16 +146,55 @@ class AbstractProductExportImportTestCase extends \PHPUnit_Framework_TestCase
         );
         $index = 0;
         $ids = [];
-        $origProductData = [];
         while (isset($skus[$index])) {
             $ids[$index] = $productRepository->get($skus[$index])->getEntityId();
-            $origProductData[$index] = $this->objectManager->create('Magento\Catalog\Model\Product')
-                ->load($ids[$index])
-                ->getData();
             $index++;
         }
 
-        $csvfile = uniqid('importdelete_') . '.csv';
+        $csvfile = $this->exportProducts();
+        $this->importProducts($csvfile, \Magento\ImportExport\Model\Import::BEHAVIOR_DELETE);
+
+        while ($index > 0) {
+            $index--;
+            $newProduct = $this->objectManager->create('Magento\Catalog\Model\Product')->load($ids[$index]);
+            $newProductData = $newProduct->getData();
+            $this->assertEquals($defaultProductData, $newProductData);
+        }
+    }
+
+    /**
+     * Execute fixtures
+     *
+     * @param array $fixtures
+     * @return void
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    private function executeFixtures($fixtures)
+    {
+        foreach ($fixtures as $fixture) {
+            $fixturePath = $this->fileSystem->getDirectoryRead(DirectoryList::ROOT)
+                ->getAbsolutePath('/dev/tests/integration/testsuite/' . $fixture);
+            include $fixturePath;
+        }
+    }
+
+    /**
+     * @param \Magento\Catalog\Model\Product $expectedProduct
+     * @param \Magento\Catalog\Model\Product $actualProduct
+     */
+    protected function assertEqualsSpecificAttributes($expectedProduct, $actualProduct)
+    {
+        // check custom options
+    }
+
+    /**
+     * Export products in the system
+     *
+     * @return string Return exported file name
+     */
+    private function exportProducts()
+    {
+        $csvfile = uniqid('importexport_') . '.csv';
 
         $this->model->setWriter(
             \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create(
@@ -191,7 +203,18 @@ class AbstractProductExportImportTestCase extends \PHPUnit_Framework_TestCase
             )
         );
         $this->assertNotEmpty($this->model->export());
+        return $csvfile;
+    }
 
+    /**
+     * Import products from the given file
+     *
+     * @param string $csvfile
+     * @param string $behavior
+     * @return void
+     */
+    protected function importProducts($csvfile, $behavior)
+    {
         /** @var \Magento\CatalogImportExport\Model\Import\Product $importModel */
         $importModel = $this->objectManager->create(
             'Magento\CatalogImportExport\Model\Import\Product'
@@ -205,45 +228,12 @@ class AbstractProductExportImportTestCase extends \PHPUnit_Framework_TestCase
             ]
         );
         $errors = $importModel->setParameters(
-            ['behavior' => \Magento\ImportExport\Model\Import::BEHAVIOR_DELETE, 'entity' => 'catalog_product']
+            ['behavior' => $behavior, 'entity' => 'catalog_product']
         )->setSource(
             $source
         )->validateData();
 
         $this->assertTrue($errors->getErrorsCount() == 0, 'Product import error, imported from file:' . $csvfile);
         $importModel->importData();
-
-        while ($index > 0) {
-            $index--;
-            $newProduct = $this->objectManager->create('Magento\Catalog\Model\Product')->load($ids[$index]);
-            $newProductData = $newProduct->getData();
-            $this->assertEquals($defaultProductData, $newProductData);
-        }
-    }
-
-    /**
-     * Execute fixtures
-     *
-     * @param array $skus
-     * @param array $fixtures
-     * @return void
-     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
-     */
-    private function executeFixtures($skus, $fixtures)
-    {
-        foreach ($fixtures as $fixture) {
-            $fixturePath = $this->fileSystem->getDirectoryRead(DirectoryList::ROOT)
-                ->getAbsolutePath('/dev/tests/integration/testsuite/' . $fixture);
-            include $fixturePath;
-        }
-    }
-
-    /**
-     * @param \Magento\Catalog\Model\Product $origProduct
-     * @param \Magento\Catalog\Model\Product $newProduct
-     */
-    protected function assertEqualsSpecificAttributes($origProduct, $newProduct)
-    {
-        // check custom options
     }
 }
