04416c19489624730243c1fb405fe64987731ddd
David Alger <david@classyllama.com>
2016-02-28 00:14:44 -0600

    Moved DatabaseTest.php into Test/Integrity where it belongs (also it won't run outside of a directory during split testing on travis)

--- dev/tests/integration/testsuite/Magento/Test/Integrity/DatabaseTest.php
+++ dev/tests/integration/testsuite/Magento/Test/Integrity/DatabaseTest.php
@@ -3,7 +3,8 @@
  * Copyright © 2015 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento;
+
+namespace Magento\Test\Integrity;
 
 use Magento\TestFramework\Helper\Bootstrap;
 
