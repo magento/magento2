4b1144bdbe574c528d29795d2d2152dbbaad92e4
David Alger <david@classyllama.com>
2016-02-27 22:49:43 -0600

    Switch phpunit setup to use default configuration file loading mechanism
    - this is preferable given the test suite preparation being done on the fly for integration testing

--- dev/travis/script.sh
+++ dev/travis/script.sh
@@ -9,14 +9,14 @@ export PATH="./../../../vendor/bin:$PATH"
 case $TEST_SUITE in
     unit)
         cd dev/tests/unit
-        phpunit -c phpunit.xml.dist
+        phpunit
         ;;
     integration)
         cd dev/tests/integration
-        phpunit -c phpunit.xml.travis$INTEGRATION_INDEX
+        phpunit
         ;;
     static)
         cd dev/tests/static
-        phpunit -c phpunit.xml.dist --filter 'Magento\\Test\\Php\\LiveCodeTest::(testCodeStyle|testAnnotationStandard)'
+        phpunit --filter 'Magento\\Test\\Php\\LiveCodeTest::(testCodeStyle|testAnnotationStandard)'
         ;;
 esac
