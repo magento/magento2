86e2ac868e0c454084102cd9c85f162ec63be257
David Alger <david@classyllama.com>
2016-02-27 22:49:43 -0600

    Switch phpunit setup to use default configuration file loading mechanism
    - this is preferable given the test suite preparation being done on the fly for integration testing

--- dev/travis/before_script.sh
+++ dev/travis/before_script.sh
@@ -19,25 +19,32 @@ case $TEST_SUITE in
     integration)
         cd dev/tests/integration
 
-        test_set_list=$(find testsuite/* -maxdepth 1 -mindepth 1 -type d)
+        test_set_list=$(find -s testsuite/* -maxdepth 1 -mindepth 1 -type d)
         test_set_size=$(($(printf "$test_set_list" | wc -l)/INTEGRATION_SETS))
 
-        # create n testsuites
-        for i in $(seq 1 $INTEGRATION_SETS); do
-            cp phpunit.xml.dist phpunit.xml.travis$i
-        done
+        echo "==> preparing integration testsuite on index $INTEGRATION_INDEX of set size $INTEGRATION_SETS"
+        cp phpunit.xml.dist phpunit.xml
 
-        # remove memory usage and update integration tests from all except testsuite 1
-        for i in $(seq 2 $INTEGRATION_SETS); do
-            perl -pi -0e 's#^\s+<!-- Memory(.*?)</testsuite>\n##ims' phpunit.xml.travis$i
-            perl -pi -e 's#\s+<directory.*>../../../update/dev/tests.*</directory>\n##g' phpunit.xml.travis$i
-        done
+        # remove memory usage and update integration tests if not set 1
+        if [[ $INTEGRATION_INDEX > 1 ]]; then
+            echo "  - excluding testsuite/Magento/MemoryUsageTest.php"
+            perl -pi -0e 's#^\s+<!-- Memory(.*?)</testsuite>\n##ims' phpunit.xml
+            
+            echo "  - excluding ../../../update/dev/tests/integration/testsuite"
+            perl -pi -e 's#\s+<directory.*>../../../update/dev/tests.*</directory>\n##g' phpunit.xml
+        fi
 
-        # create list of folders on which tests are to be run
+        # divide test sets up by indexed testsuites
         i=0; j=1
         for test_set in $test_set_list; do
             test_xml[j]+="            <directory suffix=\"Test.php\">$test_set</directory>\n"
-            
+
+            if [[ $j -eq $INTEGRATION_INDEX ]]; then
+                echo "  + including $test_set"
+            else
+                echo "  - excluding $test_set"
+            fi
+
             i=$((i+1))
             if [ $i -eq $test_set_size ] && [ $j -lt $INTEGRATION_SETS ]; then
                 j=$((j+1))
@@ -45,20 +52,19 @@ case $TEST_SUITE in
             fi
         done
 
-        # Finally replacing in config files.
-        for i in `seq 1 $INTEGRATION_SETS`; do
-            perl -pi -e "s#\s+<directory.*>testsuite</directory>#${test_xml[i]}#g" phpunit.xml.travis$i
-        done
+        # replace test sets for current index into testsuite
+        perl -pi -e "s#\s+<directory.*>testsuite</directory>#${test_xml[INTEGRATION_INDEX]}#g" phpunit.xml
 
-        cd ../../..
-        ;&  # intentional fallthrough
-    integration_integrity)
+        echo "==> testsuite preparation complete"
+
+        # create database and move db config into place
         mysql -uroot -e '
             SET @@global.sql_mode = NO_ENGINE_SUBSTITUTION;
             CREATE DATABASE magento_integration_tests;
         '
-        mv dev/tests/integration/etc/install-config-mysql.travis.php.dist \
-            dev/tests/integration/etc/install-config-mysql.php
+        mv etc/install-config-mysql.travis.php.dist etc/install-config-mysql.php
+
+        cd ../../..
         ;;
 esac
 
