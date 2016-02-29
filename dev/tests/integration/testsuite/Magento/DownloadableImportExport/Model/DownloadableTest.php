8cf888da039359d4c4db8684ccc5ee6a86d9082f
Maksym Savich <msavich@magento.com>
2016-02-29 12:34:44 -0600

    MAGETWO-49593: Add fixture with product updates for DownloadableImportExport integration test

--- dev/tests/integration/testsuite/Magento/DownloadableImportExport/Model/DownloadableTest.php
+++ dev/tests/integration/testsuite/Magento/DownloadableImportExport/Model/DownloadableTest.php
@@ -57,4 +57,33 @@ class DownloadableTest extends AbstractProductExportImportTestCase
     {
         $this->markTestSkipped('Uncomment after MAGETWO-49467 resolved');
     }
+
+    /**
+     * @param \Magento\Catalog\Model\Product $expectedProduct
+     * @param \Magento\Catalog\Model\Product $actualProduct
+     */
+    protected function assertEqualsSpecificAttributes($expectedProduct, $actualProduct)
+    {
+        $expectedProductLinks   = $expectedProduct->getExtensionAttributes()->getDownloadableProductLinks();
+        $expectedProductSamples = $expectedProduct->getExtensionAttributes()->getDownloadableProductSamples();
+
+        $actualProductLinks   = $actualProduct->getExtensionAttributes()->getDownloadableProductLinks();
+        $actualProductSamples = $actualProduct->getExtensionAttributes()->getDownloadableProductSamples();
+
+        $expectedLinksArray = [];
+        foreach ($expectedProductLinks as $link) {
+            $expectedLinksArray[] = $link->getData();
+        }
+        foreach ($actualProductLinks as $link) {
+            $this->assertContains($link->getData(), $expectedLinksArray);
+        }
+
+        $expectedSamplesArray = [];
+        foreach ($expectedProductSamples as $sample) {
+            $expectedSamplesArray[] = $sample->getData();
+        }
+        foreach ($actualProductSamples as $sample) {
+            $this->assertContains($sample->getData(), $expectedSamplesArray);
+        }
+    }
 }
