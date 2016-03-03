5c7b6c80a4b5262fe4af7aa91e3b03761a558c50
Andrii Kasian <akasian@magento.com>
2016-03-03 14:51:42 +0200

    MAGETWO-50010:  [PHP] Implement Media Gallery on Product Page
     - MAGETWO-50051: Fix uploader

---
+++ app/code/Magento/Backend/view/adminhtml/requirejs-config.js
@@ -0,0 +1,12 @@
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+/*eslint no-unused-vars: 0*/
+var config = {
+    map: {
+        '*': {
+            "mediaUploader":  "Magento_Backend/js/media-uploader",
+        }
+    }
+};
