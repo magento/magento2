a812752a7b47eb07cbf884483cb07b41007818b6
Ivan Gavryshko <igavryshko@ebay.com>
2016-03-03 13:46:56 -0600

    MAGETWO-48212: [Git][PR] Bunch of public PR #3029
    - change according CR

--- dev/tests/static/testsuite/Magento/Test/Legacy/_files/blacklist/obsolete_mage.php
+++ dev/tests/static/testsuite/Magento/Test/Legacy/_files/blacklist/obsolete_mage.php
@@ -7,5 +7,5 @@ return [
     'dev/tests/static/testsuite/Magento/Test/Legacy/ObsoleteCodeTest.php',
     'dev/tests/static/testsuite/Magento/Test/Integrity/ClassesTest.php',
     'dev/tests/static/testsuite/Magento/Test/Legacy/_files/*obsolete*.php',
-    'lib/internal/Magento/Framework/ObjectManager/Test/Unit/Factory/CompiledTest.php'
+    'lib/internal/Magento/Framework/ObjectManager/Test/Unit/Factory/CompiledTest.php',
 ];
