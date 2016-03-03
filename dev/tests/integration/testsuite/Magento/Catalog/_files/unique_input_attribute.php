7b82efaf45522a62ae4e58eb4bc4967822bc7048
Kateryna Muntianu <kmuntianu@ebay.com>
2016-03-03 12:44:15 +0200

    MAGETWO-49185: If set Unique Value for attribute to "Yes" then you can see error message after save product

---
+++ dev/tests/integration/testsuite/Magento/Catalog/_files/unique_input_attribute.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * "Input" fixture of product EAV attribute.
+ *
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+/** @var \Magento\Eav\Model\Entity\Type $entityType */
+$entityType = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create('Magento\Eav\Model\Entity\Type');
+$entityType->loadByCode('catalog_product');
+$defaultSetId = $entityType->getDefaultAttributeSetId();
+/** @var \Magento\Eav\Model\Entity\Attribute\Set $defaultSet */
+$defaultSet = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create(
+    'Magento\Eav\Model\Entity\Attribute\Set'
+);
+$defaultSet->load($defaultSetId);
+$defaultGroupId = $defaultSet->getDefaultGroupId();
+
+/** @var $attribute \Magento\Catalog\Model\ResourceModel\Eav\Attribute */
+$attribute = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create(
+    'Magento\Catalog\Model\ResourceModel\Eav\Attribute'
+);
+$attribute->setAttributeCode(
+    'unique_input_attribute'
+)->setEntityTypeId(
+    $entityType->getEntityTypeId()
+)->setAttributeGroupId(
+    $defaultGroupId
+)->setAttributeSetId(
+    $defaultSetId
+)->setFrontendInput(
+    'text'
+)->setFrontendLabel(
+    'Unique Input Attribute'
+)->setBackendType(
+    'varchar'
+)->setIsUserDefined(
+    1
+)->setIsUnique(
+    1
+)->save();
