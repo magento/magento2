456b4605cb97746cce94113bd6966247c2414a7e
Oleh Posyniak <oposyniak@magento.com>
2016-03-03 12:05:55 +0200

    MAGETWO-49943: Merchant can't change CMS Page URL Key from the grid

--- app/code/Magento/Cms/Model/Page.php
+++ app/code/Magento/Cms/Model/Page.php
@@ -91,7 +91,7 @@ class Page extends AbstractModel implements PageInterface, IdentityInterface
      */
     public function getStores()
     {
-        return $this->hasData('stores') ? $this->getData('stores') : $this->getData('store_id');
+        return $this->hasData('stores') ? $this->getData('stores') : (array)$this->getData('store_id');
     }
 
     /**
