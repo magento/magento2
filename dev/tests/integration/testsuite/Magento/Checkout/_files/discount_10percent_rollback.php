3b8c023a0a4fb97303a3bfa7e13637d8c17b9ff8
Michael Logvin <mlogvin@magento.com>
2016-03-03 17:36:18 +0200

    MAGETWO-49684: Add Recurring scripts to all modules

--- dev/tests/integration/testsuite/Magento/Checkout/_files/discount_10percent_rollback.php
+++ dev/tests/integration/testsuite/Magento/Checkout/_files/discount_10percent_rollback.php
@@ -6,7 +6,15 @@
  * See COPYING.txt for license details.
  */
 
-/** @var \Magento\SalesRule\Model\Rule $salesRule */
-$salesRule = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create('Magento\SalesRule\Model\Rule');
-$salesRule->load('Test Coupon', 'name');
-$salesRule->delete();
+/** @var \Magento\Framework\Api\SearchCriteriaBuilder $criteriaBuilder */
+$criteriaBuilder = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()
+    ->get('Magento\Framework\Api\SearchCriteriaBuilder');
+$criteriaBuilder->addFilter('name', 'Test Coupon');
+
+/** @var \Magento\SalesRule\Api\RuleRepositoryInterface $salesRuleRepository */
+$salesRuleRepository = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()
+    ->get('Magento\SalesRule\Api\RuleRepositoryInterface');
+$list = $salesRuleRepository->getList($criteriaBuilder->create());
+foreach ($list->getItems() as $item) {
+    $salesRuleRepository->deleteById($item->getRuleId());
+}
