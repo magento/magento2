c639cfaa69972d2b6baf97ba680bb58fd683e31f
Igor Melnikov <imelnikov@ebay.com>
2016-03-03 19:57:22 -0600

    MAGETWO-49592: Add fixture with product updates for CatalogImportExport integration test
    Refactoring integration tests

--- app/code/Magento/CatalogImportExport/Model/Import/Product.php
+++ app/code/Magento/CatalogImportExport/Model/Import/Product.php
@@ -760,13 +760,6 @@ class Product extends \Magento\ImportExport\Model\Import\Entity\AbstractEntity
      */
     public function isAttributeValid($attrCode, array $attrParams, array $rowData, $rowNum)
     {
-        // @todo remove workaround after issue with price_type validation resolved
-        if (isset($rowData['product_type'])
-            && $rowData['product_type'] == 'bundle'
-            && $attrParams['code'] == 'price_type'
-        ) {
-            $attrParams['type'] = 'int';
-        }
         if (!$this->validator->isAttributeValid($attrCode, $attrParams, $rowData)) {
             foreach ($this->validator->getMessages() as $message) {
                 $this->addRowError($message, $rowNum, $attrCode);
