f73a77b13f9cd7cdbdb510888102e190ffa729b3
Dmytro Voskoboinikov <dvoskoboinikov@ebay.com>
2016-03-06 13:16:25 +0200

    MAGETWO-49314: Magento/Ui datepickers do not consider Magento timezone settings

--- app/code/Magento/Ui/view/base/web/js/form/element/date.js
+++ app/code/Magento/Ui/view/base/web/js/form/element/date.js
@@ -11,10 +11,36 @@ define([
 
     return Abstract.extend({
         defaults: {
+            options: {},
+
+            timeOffset: 0,
+
+            timeFormat: 'HH:mm', // ICU Time Format
+            dateFormat: 'dd/MM/yyyy', // ICU Date Format
+
             elementTmpl: 'ui/form/element/date',
-            dateFormat: 'MM/dd/YYYY',
-            options: {}
+
+            listens: {
+                'value': 'onValueChange',
+                'shiftedValue': 'onShiftedValueChange'
+            }
         },
+
+        /**
+         * Date/time value shifted to corresponding timezone
+         * according to this.timeOffset property.
+         *
+         * @type {String}
+         */
+        shiftedValue: '',
+
+        /**
+         * Date/time format converted to be compatible with
+         * moment.js library.
+         *
+         * @type {String}
+         */
+        momentDatetimeFormat: '',
 
         /**
          * Initializes regular properties of instance.
@@ -24,10 +50,25 @@ define([
         initConfig: function () {
             this._super();
 
-            //this.dateFormat = utils.normalizeDate(this.dateFormat);
-            this.dateFormat = utils.normalizeDate('MM/dd/YYYY');
+            var options = {
+                timeFormat: this.timeFormat,
+                dateFormat: this.dateFormat
+            };
+
+            var datetimeFormat = this.dateFormat + ' ' + this.timeFormat;
+
+            this.momentDatetimeFormat = utils.normalizeDate(datetimeFormat);
+
+            jQuery.extend(this.options, options);
 
             return this;
+        },
+
+        /**
+         * @inheritdoc
+         */
+        initObservable: function () {
+            return this._super().observe(['shiftedValue']);
         },
 
         /**
@@ -39,10 +80,46 @@ define([
             var value = this._super();
 
             if (value) {
-                value = moment(value).format(this.dateFormat);
+                value = moment(value).format(this.momentDatetimeFormat);
             }
 
             return value;
+        },
+
+        /**
+         * Prepares and sets 'shifted' (that only will be displayed
+         * on frontend) date/time value.
+         *
+         * @param {String} value
+         */
+        onValueChange: function (value) {
+            if (value) {
+                var shiftedValue = moment.utc(value).add(this.timeOffset, 'seconds');
+
+                shiftedValue = shiftedValue.format(this.momentDatetimeFormat);
+
+                if (shiftedValue !== this.shiftedValue()) {
+                    this.shiftedValue(shiftedValue);
+                }
+            }
+        },
+
+        /**
+         * Prepares and sets 'real' (that will be sent to backend)
+         * date/time value.
+         *
+         * @param {String} shiftedValue
+         */
+        onShiftedValueChange: function (shiftedValue) {
+            if (shiftedValue) {
+                var value = moment.utc(shiftedValue, this.momentDatetimeFormat);
+
+                value = value.subtract(this.timeOffset, 'seconds').toISOString();
+
+                if (value !== this.value()) {
+                    this.value(value);
+                }
+            }
         }
     });
 });
