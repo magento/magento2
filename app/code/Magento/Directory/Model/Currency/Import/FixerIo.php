magento/magento2

commit 591f16e2c2989b702bb1b916deb5866aba81e5dd

MAGETWO-47492: [GitHub] Currency rates import service is loading all rate for activated currencies. #2757
- added a new currency converter service fixer.io
- fixed errors procession in webservices
- added unit test

app/code/Magento/Directory/Model/Currency/Import/FixerIo.php
@@ -1,0 +1,147 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Directory\Model\Currency\Import;
+
+/**
+ * Currency rate import model (From http://fixer.io/)
+ */
+class FixerIo extends \Magento\Directory\Model\Currency\Import\AbstractImport
+{
+    /**
+     * @var string
+     */
+    const CURRENCY_CONVERTER_URL = 'http://api.fixer.io/latest?base={{CURRENCY_FROM}}&symbols={{CURRENCY_TO}}';
+
+    /**
+     * Http Client Factory
+     *
+     * @var \Magento\Framework\HTTP\ZendClientFactory
+     */
+    protected $httpClientFactory;
+
+    /**
+     * Core scope config
+     *
+     * @var \Magento\Framework\App\Config\ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
+     * Initialize dependencies
+     *
+     * @param \Magento\Directory\Model\CurrencyFactory $currencyFactory
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+     * @param \Magento\Framework\HTTP\ZendClientFactory $httpClientFactory
+     */
+    public function __construct(
+        \Magento\Directory\Model\CurrencyFactory $currencyFactory,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Framework\HTTP\ZendClientFactory $httpClientFactory
+    ) {
+        parent::__construct($currencyFactory);
+        $this->scopeConfig = $scopeConfig;
+        $this->httpClientFactory = $httpClientFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function fetchRates()
+    {
+        $data = [];
+        $currencies = $this->_getCurrencyCodes();
+        $defaultCurrencies = $this->_getDefaultCurrencyCodes();
+
+        foreach ($defaultCurrencies as $currencyFrom) {
+            if (!isset($data[$currencyFrom])) {
+                $data[$currencyFrom] = [];
+            }
+            $data = $this->convertBatch($data, $currencyFrom, $currencies);
+            ksort($data[$currencyFrom]);
+        }
+        return $data;
+    }
+
+
+    /**
+     * Return currencies convert rates in batch mode
+     *
+     * @param array $data
+     * @param string $currencyFrom
+     * @param array $currenciesTo
+     * @return array
+     */
+    private function convertBatch($data, $currencyFrom, $currenciesTo)
+    {
+        $currenciesStr = implode(',', $currenciesTo);
+        $url = str_replace('{{CURRENCY_FROM}}', $currencyFrom, self::CURRENCY_CONVERTER_URL);
+        $url = str_replace('{{CURRENCY_TO}}', $currenciesStr, $url);
+
+        set_time_limit(0);
+        $response = $this->getServiceResponse($url);
+        ini_restore('max_execution_time');
+
+        foreach ($currenciesTo as $currencyTo) {
+            if ($currencyFrom == $currencyTo) {
+                $data[$currencyFrom][$currencyTo] = $this->_numberFormat(1);
+            } else {
+                if (empty($response['rates'][$currencyTo])) {
+                    $this->_messages[] = __('We can\'t retrieve a rate from %1 for %2.', $url, $currencyTo);
+                    $data[$currencyFrom][$currencyTo] = null;
+                } else {
+                    $data[$currencyFrom][$currencyTo] = $this->_numberFormat(
+                        (double)$response['rates'][$currencyTo]
+                    );
+                }
+            }
+        }
+        return $data;
+    }
+
+    /**
+     * Get Fixer.io service response
+     *
+     * @param string $url
+     * @param int $retry
+     * @return array
+     */
+    private function getServiceResponse($url, $retry = 0)
+    {
+        /** @var \Magento\Framework\HTTP\ZendClient $httpClient */
+        $httpClient = $this->httpClientFactory->create();
+        $response = [];
+
+        try {
+            $jsonResponse = $httpClient->setUri(
+                $url
+            )->setConfig(
+                [
+                    'timeout' => $this->scopeConfig->getValue(
+                        'currency/fixerio/timeout',
+                        \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+                    ),
+                ]
+            )->request(
+                'GET'
+            )->getBody();
+
+            $response = json_decode($jsonResponse, true);
+        } catch (\Exception $e) {
+            if ($retry == 0) {
+                $response = $this->getServiceResponse($url, 1);
+            }
+        }
+        return $response;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function _convert($currencyFrom, $currencyTo)
+    {
+    }
+}

app/code/Magento/Directory/Model/Currency/Import/Webservicex.php
@@ -4,45 +4,46 @@
  * See COPYING.txt for license details.
  */
 
-// @codingStandardsIgnoreFile
+namespace Magento\Directory\Model\Currency\Import;
 
 /**
  * Currency rate import model (From www.webservicex.net)
  */
-namespace Magento\Directory\Model\Currency\Import;
-
 class Webservicex extends \Magento\Directory\Model\Currency\Import\AbstractImport
 {
     /**
      * @var string
      */
-    const CURRENCY_CONVERTER_URL = 'http://www.webservicex.net/CurrencyConvertor.asmx/ConversionRate?FromCurrency={{CURRENCY_FROM}}&ToCurrency={{CURRENCY_TO}}';
+    const CURRENCY_CONVERTER_URL = 'http://www.webservicex.net/CurrencyConvertor.asmx/ConversionRate?' .
+        'FromCurrency={{CURRENCY_FROM}}&ToCurrency={{CURRENCY_TO}}';
 
     /**
-     * HTTP client
+     * Http Client Factory
      *
-     * @var \Magento\Framework\HTTP\ZendClient
+     * @var \Magento\Framework\HTTP\ZendClientFactory
      */
-    protected $_httpClient;
+    protected $httpClientFactory;
 
     /**
-     * Core store config
+     * Core scope config
      *
      * @var \Magento\Framework\App\Config\ScopeConfigInterface
      */
-    protected $_scopeConfig;
+    private $scopeConfig;
 
     /**
      * @param \Magento\Directory\Model\CurrencyFactory $currencyFactory
      * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+     * @param \Magento\Framework\HTTP\ZendClientFactory $httpClientFactory
      */
     public function __construct(
         \Magento\Directory\Model\CurrencyFactory $currencyFactory,
-        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Framework\HTTP\ZendClientFactory $httpClientFactory
     ) {
         parent::__construct($currencyFactory);
-        $this->_scopeConfig = $scopeConfig;
-        $this->_httpClient = new \Magento\Framework\HTTP\ZendClient();
+        $this->scopeConfig = $scopeConfig;
+        $this->httpClientFactory = $httpClientFactory;
     }
 
     /**
@@ -55,13 +56,15 @@ class Webservicex extends \Magento\Directory\Model\Currency\Import\AbstractImpor
     {
         $url = str_replace('{{CURRENCY_FROM}}', $currencyFrom, self::CURRENCY_CONVERTER_URL);
         $url = str_replace('{{CURRENCY_TO}}', $currencyTo, $url);
+        /** @var \Magento\Framework\HTTP\ZendClient $httpClient */
+        $httpClient = $this->httpClientFactory->create();
 
         try {
-            $response = $this->_httpClient->setUri(
+            $response = $httpClient->setUri(
                 $url
             )->setConfig(
                 [
-                    'timeout' => $this->_scopeConfig->getValue(
+                    'timeout' => $this->scopeConfig->getValue(
                         'currency/webservicex/timeout',
                         \Magento\Store\Model\ScopeInterface::SCOPE_STORE
                     ),

app/code/Magento/Directory/Model/Currency/Import/AbstractImport.php
@@ -94,7 +94,7 @@ abstract class AbstractImport implements \Magento\Directory\Model\Currency\Impor
         $data = [];
         $currencies = $this->_getCurrencyCodes();
         $defaultCurrencies = $this->_getDefaultCurrencyCodes();
-        @set_time_limit(0);
+        set_time_limit(0);
         foreach ($defaultCurrencies as $currencyFrom) {
             if (!isset($data[$currencyFrom])) {
                 $data[$currencyFrom] = [];
@@ -111,6 +111,7 @@ abstract class AbstractImport implements \Magento\Directory\Model\Currency\Impor
             }
             ksort($data[$currencyFrom]);
         }
+        ini_restore('max_execution_time');
 
         return $data;
     }

app/code/Magento/Directory/Test/Unit/Model/Currency/Import/FixerIoTest.php
@@ -1,0 +1,91 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Directory\Test\Unit\Model\Currency\Import;
+
+class FixerIoTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \Magento\Directory\Model\Currency\Import\FixerIo
+     */
+    private $model;
+
+    /**
+     * @var \Magento\Directory\Model\CurrencyFactory|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $currencyFactoryMock;
+
+    /**
+     * @var \Magento\Framework\HTTP\ZendClientFactory|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $httpClientFactoryMock;
+
+    protected function setUp()
+    {
+        $objectManagerHelper = new \Magento\Framework\TestFramework\Unit\Helper\ObjectManager($this);
+
+        $this->currencyFactoryMock = $this->getMockBuilder('Magento\Directory\Model\CurrencyFactory')
+            ->disableOriginalConstructor()
+            ->setMethods([])
+            ->getMock();
+        $this->httpClientFactoryMock = $this->getMockBuilder('Magento\Framework\HTTP\ZendClientFactory')
+            ->disableOriginalConstructor()
+            ->setMethods(['create'])
+            ->getMock();
+        $scopeMock = $this->getMockBuilder('Magento\Framework\App\Config\ScopeConfigInterface')
+            ->disableOriginalConstructor()
+            ->setMethods([])
+            ->getMock();
+
+        $this->model = $objectManagerHelper->getObject(
+            'Magento\Directory\Model\Currency\Import\FixerIo',
+            [
+                'currencyFactory' => $this->currencyFactoryMock,
+                'scopeConfig' => $scopeMock,
+                'httpClientFactory' => $this->httpClientFactoryMock
+            ]
+        );
+    }
+
+    public function testFetchRates()
+    {
+        $currencyFromList = ['USD'];
+        $currencyToList = ['EUR', 'UAH'];
+        $responseBody = '{"base":"USD","date":"2015-10-07","rates":{"EUR":0.9022}}';
+        $expectedCurrencyRateList = ['USD' => ['EUR' => 0.9022, 'UAH' => null]];
+        $message = "We can't retrieve a rate from http://api.fixer.io/latest?base=USD&symbols=EUR,UAH for UAH.";
+
+        /** @var \Magento\Directory\Model\Currency|\PHPUnit_Framework_MockObject_MockObject $currencyMock */
+        $currencyMock = $this->getMockBuilder('Magento\Directory\Model\Currency')
+            ->disableOriginalConstructor()
+            ->setMethods([])
+            ->getMock();
+        /** @var \Magento\Framework\HTTP\ZendClient|\PHPUnit_Framework_MockObject_MockObject $currencyMock */
+        $httpClientMock = $this->getMockBuilder('Magento\Framework\HTTP\ZendClient')
+            ->disableOriginalConstructor()
+            ->setMethods([])
+            ->getMock();
+        /** @var \Zend_Http_Response|\PHPUnit_Framework_MockObject_MockObject $currencyMock */
+        $httpResponseMock = $this->getMockBuilder('Zend_Http_Response')
+            ->disableOriginalConstructor()
+            ->setMethods([])
+            ->getMock();
+        $this->currencyFactoryMock->expects($this->any())->method('create')->willReturn($currencyMock);
+        $currencyMock->expects($this->once())->method('getConfigBaseCurrencies')->willReturn($currencyFromList);
+        $currencyMock->expects($this->once())->method('getConfigAllowCurrencies')->willReturn($currencyToList);
+        $this->httpClientFactoryMock->expects($this->any())->method('create')->willReturn($httpClientMock);
+        $httpClientMock->expects($this->atLeastOnce())->method('setUri')->willReturnSelf();
+        $httpClientMock->expects($this->atLeastOnce())->method('setConfig')->willReturnSelf();
+        $httpClientMock->expects($this->atLeastOnce())->method('request')->willReturn($httpResponseMock);
+        $httpResponseMock->expects($this->any())->method('getBody')->willReturn($responseBody);
+
+        $this->assertEquals($expectedCurrencyRateList, $this->model->fetchRates());
+        $messages = $this->model->getMessages();
+        $this->assertNotEmpty($messages);
+        $this->assertTrue(is_array($messages));
+        $this->assertEquals($message, (string)$messages[0]);
+    }
+}

app/code/Magento/Directory/etc/di.xml
@@ -13,6 +13,10 @@
                     <item name="label" xsi:type="string">Webservicex</item>
                     <item name="class" xsi:type="string">Magento\Directory\Model\Currency\Import\Webservicex</item>
                 </item>
+                <item name="fixerio" xsi:type="array">
+                    <item name="label" xsi:type="string">Fixer.io</item>
+                    <item name="class" xsi:type="string">Magento\Directory\Model\Currency\Import\FixerIo</item>
+                </item>
             </argument>
         </arguments>
     </type>

commit fe0cd7c8ead84fbc0932e50f38ec653dcc8ccf39

Fixed failing PHP 5.5 travis test due to syntax error introduced in 591f16e

app/code/Magento/Directory/Model/Currency/Import/FixerIo.php
@@ -10,10 +10,14 @@ namespace Magento\Directory\Model\Currency\Import;
  */
 class FixerIo extends \Magento\Directory\Model\Currency\Import\AbstractImport
 {
-    /**
+    /**#@+
      * @var string
      */
-    const CURRENCY_CONVERTER_URL = 'http://api.fixer.io/latest?base={{CURRENCY_FROM}}&symbols={{CURRENCY_TO}}';
+    const CURRENCY_CONVERTER_ENDPOINT = 'http://api.fixer.io/latest';
+    const CURRENCY_CONVERTER_FROM_PARAM = 'base';
+    const CURRENCY_CONVERTER_TO_PARAM = 'symbols';
+    /**#@-*/
+
 
     /**
      * Http Client Factory
@@ -76,8 +80,9 @@ class FixerIo extends \Magento\Directory\Model\Currency\Import\AbstractImport
     private function convertBatch($data, $currencyFrom, $currenciesTo)
     {
         $currenciesStr = implode(',', $currenciesTo);
-        $url = str_replace('{{CURRENCY_FROM}}', $currencyFrom, self::CURRENCY_CONVERTER_URL);
-        $url = str_replace('{{CURRENCY_TO}}', $currenciesStr, $url);
+        $url = self::CURRENCY_CONVERTER_ENDPOINT . 
+            '?' . self::CURRENCY_CONVERTER_FROM_PARAM . '=' . $currencyFrom .
+            '&' . self::CURRENCY_CONVERTER_TO_PARAM . '=' . $currenciesStr;
 
         set_time_limit(0);
         $response = $this->getServiceResponse($url);
@@ -114,18 +119,15 @@ class FixerIo extends \Magento\Directory\Model\Currency\Import\AbstractImport
         $response = [];
 
         try {
-            $jsonResponse = $httpClient->setUri(
-                $url
-            )->setConfig(
-                [
+            $jsonResponse = $httpClient->setUri($url)
+                ->setConfig([
                     'timeout' => $this->scopeConfig->getValue(
                         'currency/fixerio/timeout',
                         \Magento\Store\Model\ScopeInterface::SCOPE_STORE
                     ),
-                ]
-            )->request(
-                'GET'
-            )->getBody();
+                ])
+                ->request('GET')
+                ->getBody();
 
             $response = json_decode($jsonResponse, true);
         } catch (\Exception $e) {

app/code/Magento/Directory/Model/Currency/Import/Webservicex.php
@@ -11,11 +11,13 @@ namespace Magento\Directory\Model\Currency\Import;
  */
 class Webservicex extends \Magento\Directory\Model\Currency\Import\AbstractImport
 {
-    /**
+    /**#@+
      * @var string
      */
-    const CURRENCY_CONVERTER_URL = 'http://www.webservicex.net/CurrencyConvertor.asmx/ConversionRate?' .
-        'FromCurrency={{CURRENCY_FROM}}&ToCurrency={{CURRENCY_TO}}';
+    const CURRENCY_CONVERTER_ENDPOINT = 'http://www.webservicex.net/CurrencyConvertor.asmx/ConversionRate';
+    const CURRENCY_CONVERTER_FROM_PARAM = 'FromCurrency';
+    const CURRENCY_CONVERTER_TO_PARAM = 'ToCurrency';
+    /**#@-*/
 
     /**
      * Http Client Factory
@@ -51,24 +53,23 @@ class Webservicex extends \Magento\Directory\Model\Currency\Import\AbstractImpor
      */
     protected function _convert($currencyFrom, $currencyTo, $retry = 0)
     {
-        $url = str_replace('{{CURRENCY_FROM}}', $currencyFrom, self::CURRENCY_CONVERTER_URL);
-        $url = str_replace('{{CURRENCY_TO}}', $currencyTo, $url);
+        $url = self::CURRENCY_CONVERTER_ENDPOINT . 
+            '?' . self::CURRENCY_CONVERTER_FROM_PARAM . '=' . $currencyFrom .
+            '&' . self::CURRENCY_CONVERTER_TO_PARAM . '=' . $currencyTo;
+
         /** @var \Magento\Framework\HTTP\ZendClient $httpClient */
         $httpClient = $this->getHttpClientFactory()->create();
 
         try {
-            $response = $httpClient->setUri(
-                $url
-            )->setConfig(
-                [
+            $response = $httpClient->setUri($url)
+                ->setConfig([
                     'timeout' => $this->scopeConfig->getValue(
                         'currency/webservicex/timeout',
                         \Magento\Store\Model\ScopeInterface::SCOPE_STORE
                     ),
-                ]
-            )->request(
-                'GET'
-            )->getBody();
+                ])
+                ->request('GET')
+                ->getBody();
 
             $xml = simplexml_load_string($response, null, LIBXML_NOERROR);
             if (!$xml) {

dev/tests/integration/testsuite/Magento/Directory/Model/ObserverTest.php
@@ -8,6 +8,7 @@
 
 namespace Magento\Directory\Model;
 
+use Magento\Directory\Model\Currency\Import\Webservicex;
 use Magento\Framework\ObjectManagerInterface;
 use Magento\Store\Model\ScopeInterface;
 use Magento\TestFramework\Helper\Bootstrap;
@@ -61,13 +62,14 @@ class ObserverTest extends \PHPUnit_Framework_TestCase
     public function testScheduledUpdateCurrencyRates()
     {
         //skipping test if service is unavailable
-        $url = str_replace('{{CURRENCY_FROM}}', 'USD',
-            \Magento\Directory\Model\Currency\Import\Webservicex::CURRENCY_CONVERTER_URL);
-        $url = str_replace('{{CURRENCY_TO}}', 'GBP', $url);
+        $url = Webservicex::CURRENCY_CONVERTER_ENDPOINT . 
+            '?' . Webservicex::CURRENCY_CONVERTER_FROM_PARAM . '=USD' .
+            '&' . Webservicex::CURRENCY_CONVERTER_TO_PARAM . '=GBP';
+
         try {
             file_get_contents($url);
         } catch (\PHPUnit_Framework_Error_Warning $e) {
-            $this->markTestSkipped('http://www.webservicex.net is unavailable ');
+            $this->markTestSkipped(Webservicex::CURRENCY_CONVERTER_ENDPOINT . ' is unavailable ');
         }
 
         $allowedCurrencies = 'USD,GBP,EUR';