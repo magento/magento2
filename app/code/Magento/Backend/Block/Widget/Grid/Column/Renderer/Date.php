7cc168488456dde14db7a911e1b809a718e097e1
Igor Melnikov <imelnikov@ebay.com>
2016-03-07 20:15:15 -0600

    MAGETWO-45575: Disabling formatting according to timezone from default config doesn't work for fields of date type in old grids
    Refactoring fix to keep backwards compatibility

--- app/code/Magento/Backend/Block/Widget/Grid/Column/Renderer/Date.php
+++ app/code/Magento/Backend/Block/Widget/Grid/Column/Renderer/Date.php
@@ -18,6 +18,56 @@ class Date extends \Magento\Backend\Block\Widget\Grid\Column\Renderer\AbstractRe
     protected $_defaultWidth = 160;
 
     /**
+     * Date format string
+     *
+     * @var string
+     */
+    protected static $_format = null;
+
+    /**
+     * @var DateTimeFormatterInterface
+     */
+    protected $dateTimeFormatter;
+
+    /**
+     * @param \Magento\Backend\Block\Context $context
+     * @param DateTimeFormatterInterface $dateTimeFormatter
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Backend\Block\Context $context,
+        DateTimeFormatterInterface $dateTimeFormatter,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+        $this->dateTimeFormatter = $dateTimeFormatter;
+    }
+
+    /**
+     * Retrieve date format
+     *
+     * @return string
+     * @deprecated
+     */
+    protected function _getFormat()
+    {
+        $format = $this->getColumn()->getFormat();
+        if (!$format) {
+            if (self::$_format === null) {
+                try {
+                    self::$_format = $this->_localeDate->getDateFormat(
+                        \IntlDateFormatter::MEDIUM
+                    );
+                } catch (\Exception $e) {
+                    $this->_logger->critical($e);
+                }
+            }
+            $format = self::$_format;
+        }
+        return $format;
+    }
+
+    /**
      * Renders grid column
      *
      * @param   \Magento\Framework\DataObject $row
@@ -26,13 +76,17 @@ class Date extends \Magento\Backend\Block\Widget\Grid\Column\Renderer\AbstractRe
     public function render(\Magento\Framework\DataObject $row)
     {
         $format = $this->getColumn()->getFormat();
-        if ($date = $this->_getValue($row)) {
+        $date = $date = $this->_getValue($row);
+        if ($date) {
+            if (!($date instanceof \DateTimeInterface)) {
+                $date = new \DateTime($date);
+            }
             return $this->_localeDate->formatDateTime(
-                $date instanceof \DateTimeInterface ? $date : new \DateTime($date),
+                $date,
                 $format ?: \IntlDateFormatter::MEDIUM,
                 \IntlDateFormatter::NONE,
                 null,
-                $this->getColumn()->getTimezone() !== false ? null : 'UTC'
+                $this->getColumn()->getTimezone() === false ? 'UTC' : null
             );
         }
         return $this->getColumn()->getDefault();
