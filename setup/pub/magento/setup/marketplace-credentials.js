b4229e28e1b10086cf6c3b2914db49287bf9056e
Paul Lewis <plewis@magento.com>
2016-03-09 16:57:19 -0600

    MAGETWO-48175: [Github]: Upgrading M2 is confusing - not clear need to login first to magento.com #3059
     - Check for git-clone instances first and display error
     - Only show form to enter creds when necessary

--- setup/pub/magento/setup/marketplace-credentials.js
+++ setup/pub/magento/setup/marketplace-credentials.js
@@ -5,39 +5,64 @@
 
 'use strict';
 angular.module('marketplace-credentials', ['ngStorage'])
-    .controller('MarketplaceCredentialsController', ['$scope', '$state', '$http', '$localStorage', '$rootScope',
-        function ($scope, $state, $http, $localStorage, $rootScope) {
+    .controller('MarketplaceCredentialsController', ['$scope', '$state', '$http', '$localStorage', '$rootScope', '$sce',
+        function ($scope, $state, $http, $localStorage, $rootScope, $sce) {
+            $scope.showCredsForm = false;
             $scope.user = {
                 username : $localStorage.marketplaceUsername ? $localStorage.marketplaceUsername : '',
                 password : '',
                 submitted : false
             };
-            $scope.errors = false;
-            if (!$rootScope.authRequest) {
-                $scope.isAuthLoadingComplete = false;
-                $http.post('index.php/marketplace/check-auth', [])
-                    .success(function (response) {
-                        if (response.success) {
-                            $localStorage.marketplaceUsername = $scope.user.username = response.data.username;
-                            $localStorage.isMarketplaceAuthorized = true;
+
+            $scope.upgradeProcessed = false;
+            $scope.upgradeProcessError = false;
+            $scope.isAuthLoadingComplete = false;
+
+            $http.get('index.php/select-version/installedSystemPackage', {'responseType' : 'json'})
+                .success(function (data) {
+                    if (data.responseType == 'error') {
+                        $scope.upgradeProcessError = true;
+                        $scope.upgradeProcessErrorMessage = $sce.trustAsHtml(data.error);
+                    } else {
+                        if (!$rootScope.authRequest || !$rootScope.isMarketplaceAuthorized) {
+                            $scope.isHiddenSpinner = false;
+                            $scope.isAuthLoadingComplete = false;
+                            $http.post('index.php/marketplace/check-auth', [])
+                                .success(function (response) {
+                                    if (response.success) {
+                                        $localStorage.marketplaceUsername = $scope.user.username = response.data.username;
+                                        $localStorage.isMarketplaceAuthorized = true;
+                                        $scope.nextState();
+                                    } else {
+                                        $localStorage.isMarketplaceAuthorized = false;
+                                        $scope.showCredsForm = true;
+                                    }
+                                    $rootScope.isMarketplaceAuthorized = $localStorage.isMarketplaceAuthorized;
+                                    $rootScope.authRequest = true;
+                                    $scope.isAuthLoadingComplete = true;
+                                })
+                                .error(function (data) {
+                                    $scope.isAuthLoadingComplete = true;
+                                });
                         } else {
-                            $localStorage.isMarketplaceAuthorized = false;
+                            $rootScope.isMarketplaceAuthorized = $localStorage.isMarketplaceAuthorized;
+                            $rootScope.isAuthLoadingComplete = true;
+                            $scope.sentToNextState = true;
+                            $scope.nextState();
                         }
-                        $rootScope.isMarketplaceAuthorized = $localStorage.isMarketplaceAuthorized;
-                        $rootScope.authRequest = true;
-                        $scope.isAuthLoadingComplete = true;
-                    })
-                    .error(function (data) {
-                        $scope.isAuthLoadingComplete = true;
-                    });
-            } else {
-                $rootScope.isMarketplaceAuthorized = $localStorage.isMarketplaceAuthorized;
-                $rootScope.isAuthLoadingComplete = true;
+                    }
+                    $scope.upgradeProcessed = true;
+                })
+                .error(function (data) {
+                    $scope.upgradeProcessError = true;
+                });
+
+            $scope.errors = false;
+            if (!$scope.upgradeProcessError && $scope.upgradeProcessed) {
             }
 
             $scope.saveAuthJson = function () {
                 if ($scope.auth.$valid) {
-                    $scope.isAuthLoadingComplete = false;
                     $http.post('index.php/marketplace/save-auth-json', $scope.user)
                         .success(function (data) {
                             $scope.saveAuthJson.result = data;
