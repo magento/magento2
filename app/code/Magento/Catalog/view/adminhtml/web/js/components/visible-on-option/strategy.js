387934bdc3f4496b420f21d35aeafaaaa40e0a02
vpaladiychuk <vpaladiychuk@ebay.com>
2016-03-09 12:53:40 +0200

    MAGETWO-47246: Add Attribute and Attributes Panel

--- app/code/Magento/Catalog/view/adminhtml/web/js/components/visible-on-option/strategy.js
+++ app/code/Magento/Catalog/view/adminhtml/web/js/components/visible-on-option/strategy.js
@@ -6,24 +6,23 @@ define(function () {
     return {
         defaults: {
             valuesForOptions: [],
-            visibilityState: true,
             imports: {
                 toggleVisibility:
                     'product_attribute_add_form.product_attribute_add_form.base_fieldset.frontend_input:value'
             },
             isShown: false,
-            inverse: false
+            inverseVisibility: false
         },
 
         initElement: function (item) {
             this._super();
-            item.set('visible', this.inverse ? !this.visibilityState : this.visibilityState);
+            item.set('visible', this.inverseVisibility ? !this.isShown : this.isShown);
             return this;
         },
 
         toggleVisibility: function (selected) {
-            this.isShown = this.visibilityState = selected in this.valuesForOptions;
-            this.visible(this.inverse ? !this.isShown : this.isShown);
+            this.isShown = selected in this.valuesForOptions;
+            this.visible(this.inverseVisibility ? !this.isShown : this.isShown);
         }
     }
 });
