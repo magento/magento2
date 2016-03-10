8ab9931addf6343a6794fd6b67d278354ae62c20
vpaladiychuk <vpaladiychuk@ebay.com>
2016-03-10 13:38:33 +0200

    MAGETWO-47246: Add Attribute and Attributes Panel

--- app/code/Magento/Catalog/view/adminhtml/web/js/components/disable-hide-select.js
+++ app/code/Magento/Catalog/view/adminhtml/web/js/components/disable-hide-select.js
@@ -5,9 +5,9 @@
 define([
     'Magento_Ui/js/form/element/select',
     'Magento_Catalog/js/components/visible-on-option/strategy',
-    'Magento_Catalog/js/components/visible-on-option/disable-strategy'
-], function (Element, strategy, disableStrategy) {
+    'Magento_Catalog/js/components/disable-on-option/strategy'
+], function (Element, visibleStrategy, disableStrategy) {
     'use strict';
 
-    return Element.extend(strategy).extend(disableStrategy);
+    return Element.extend(visibleStrategy).extend(disableStrategy);
 });
