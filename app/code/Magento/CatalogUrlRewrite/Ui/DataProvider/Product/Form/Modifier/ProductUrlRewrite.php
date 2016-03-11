e7b2b519991fcb02a18da615ceb658588be89a5b
Ihor Melnychenko <imelnychenko@ebay.com>
2016-03-11 10:22:09 +0200

    MAGETWO-49329: [PR] Prepare and process PR

--- app/code/Magento/CatalogUrlRewrite/Ui/DataProvider/Product/Form/Modifier/ProductUrlRewrite.php
+++ app/code/Magento/CatalogUrlRewrite/Ui/DataProvider/Product/Form/Modifier/ProductUrlRewrite.php
@@ -9,7 +9,7 @@ use Magento\Catalog\Ui\DataProvider\Product\Form\Modifier\AbstractModifier;
 use Magento\Store\Model\ScopeInterface;
 use Magento\Catalog\Model\Locator\LocatorInterface;
 use Magento\Framework\App\Config\ScopeConfigInterface;
-use Magento\Catalog\Model\AttributeConstantsInterface as AC;
+use Magento\Catalog\Api\Data\ProductAttributeInterface as AC;
 use Magento\Ui\Component\Form\Element\Checkbox;
 use Magento\Ui\Component\Form\Element\DataType\Text;
 use Magento\Ui\Component\Form\Field;
@@ -114,7 +114,7 @@ class ProductUrlRewrite extends AbstractModifier
                     'urlKey' => '${ $.provider }:data.product.' . AC::CODE_SEO_FIELD_URL_KEY,
                     'handleUseDefault' => '${ $.parentName }.url_key:isUseDefault',
                     'handleChanges' => '${ $.provider }:data.product.'
-                        . ProductAttributeInterface::CODE_SEO_FIELD_URL_KEY,
+                        . AC::CODE_SEO_FIELD_URL_KEY,
                 ],
                 'description' => __('Create Permanent Redirect for old URL'),
                 'dataScope' => 'url_key_create_redirect',
