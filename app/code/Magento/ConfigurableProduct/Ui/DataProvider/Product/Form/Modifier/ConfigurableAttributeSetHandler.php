061b665d0d1e4f772f63b6b74c628b01b49ec5b5
Olga Lytvynenko <olytvynenko@ebay.com>
2016-03-15 18:43:20 +0200

    MAGETWO-50059: [UI] Assistance with Configurable product

--- app/code/Magento/ConfigurableProduct/Ui/DataProvider/Product/Form/Modifier/ConfigurableAttributeSetHandler.php
+++ app/code/Magento/ConfigurableProduct/Ui/DataProvider/Product/Form/Modifier/ConfigurableAttributeSetHandler.php
@@ -73,7 +73,6 @@ class ConfigurableAttributeSetHandler extends AbstractModifier
                                         'prefer' => 'radio',
                                         'description' => __('Add configurable attributes to the current Attribute Set'),
                                         'dataScope' => 'configurable-affected-attribute-set',
-                                        'label' => ' ',
                                         'valueMap' => [
                                             'true' => 'current',
                                             'false' => '0',
@@ -96,7 +95,6 @@ class ConfigurableAttributeSetHandler extends AbstractModifier
                                             'Add configurable attributes to the new Attribute Set based on current'
                                         ),
                                         'dataScope' => 'configurable-affected-attribute-set',
-                                        'label' => ' ',
                                         'valueMap' => [
                                             'true' => 'new',
                                             'false' => '0',
@@ -120,7 +118,6 @@ class ConfigurableAttributeSetHandler extends AbstractModifier
                                             'Add configurable attributes to the existing Attribute Set'
                                         ),
                                         'dataScope' => 'configurable-affected-attribute-set',
-                                        'label' => ' ',
                                         'valueMap' => [
                                             'true' => 'existing',
                                             'false' => '0',
@@ -168,7 +165,6 @@ class ConfigurableAttributeSetHandler extends AbstractModifier
                                 'formElement' => 'container',
                                 'componentType' => 'container',
                                 'component' => 'Magento_Ui/js/form/components/button',
-                                'displayAsLink' => true,
                                 'actions' => [
                                     [
                                         'targetName' => 'configurableVariations',
