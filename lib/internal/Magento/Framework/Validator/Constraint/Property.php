4d9f839b6b8e474f1ee2809d19dc76ad47873e00
Joan He <johe@ebay.com>
2016-03-17 15:28:41 -0500

    MAGETWO-48081: Copyright Year Update

--- lib/internal/Magento/Framework/Validator/Constraint/Property.php
+++ lib/internal/Magento/Framework/Validator/Constraint/Property.php
@@ -2,7 +2,7 @@
 /**
  * Validator constraint delegates validation of value's property to wrapped validator.
  *
- * Copyright © 2015 Magento. All rights reserved.
+ * Copyright © 2016 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
 
