e1278c97838f144ad33abd7055a8e6618133d55c
Stanislav Idolov <sidolov@magento.com>
2016-03-17 12:44:50 +0200

    MAGETWO-57133: [Backport] - Item Level Gift Message can not be saved for guest #3804 for 2.0

--- app/code/Magento/GiftMessage/view/frontend/web/js/model/url-builder.js
+++ app/code/Magento/GiftMessage/view/frontend/web/js/model/url-builder.js
@@ -3,36 +3,19 @@
  * See COPYING.txt for license details.
  */
 /*jshint browser:true jquery:true*/
-/*global alert*/
 define(
-    ['jquery'],
-    function($) {
-        return {
-            method: "rest",
-            storeCode: window.giftOptionsConfig.storeCode,
-            version: 'V1',
-            serviceUrl: ':method/:storeCode/:version',
+    [
+        'jquery',
+        'Magento_Checkout/js/model/url-builder'
+    ],
+    function ($, urlBuilder) {
+        'use strict';
 
-            createUrl: function(url, params) {
-                var completeUrl = this.serviceUrl + url;
-                return this.bindParams(completeUrl, params);
-            },
-            bindParams: function(url, params) {
-                params.method = this.method;
-                params.storeCode = this.storeCode;
-                params.version = this.version;
-
-                var urlParts = url.split("/");
-                urlParts = urlParts.filter(Boolean);
-
-                $.each(urlParts, function(key, part) {
-                    part = part.replace(':', '');
-                    if (params[part] != undefined) {
-                        urlParts[key] = params[part];
-                    }
-                });
-                return urlParts.join('/');
+        return $.extend(
+            urlBuilder,
+            {
+                storeCode: window.giftOptionsConfig.storeCode
             }
-        };
+        );
     }
 );
