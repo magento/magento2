fc7bd69b8d3222758b221fbae413f9de20ae5c7d
Ji Lu <jilu1@ebay.com>
2016-03-18 09:38:16 -0500

    MAGETWO-50603: Consolidated SPM test cases into security module.

--- dev/tests/functional/tests/app/Magento/Security/Test/Constraint/AssertCustomerIsLocked.php
+++ dev/tests/functional/tests/app/Magento/Security/Test/Constraint/AssertCustomerIsLocked.php
@@ -4,7 +4,7 @@
  * See COPYING.txt for license details.
  */
 
-namespace Magento\Customer\Test\Constraint;
+namespace Magento\Security\Test\Constraint;
 
 use Magento\Mtf\Constraint\AbstractConstraint;
 use Magento\Customer\Test\Page\CustomerAccountLogin;
