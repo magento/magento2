4bb6a5b0f8871cce71ba75179d08f407b193e906
Andrii Lugovyi <alugovyi@magento.com>
2016-03-21 18:12:13 +0200

    MAGETWO-50178: Price is required to create a configurable product in UI and this breaks functional tests.

--- dev/tests/functional/tests/app/Magento/Catalog/Test/TestCase/Product/NavigateRelatedProductsTest.php
+++ dev/tests/functional/tests/app/Magento/Catalog/Test/TestCase/Product/NavigateRelatedProductsTest.php
@@ -70,7 +70,6 @@ class NavigateRelatedProductsTest extends AbstractProductPromotedProductsTest
         $productsToVerify,
         CheckoutCart $checkoutCart
     ) {
-        $this->markTestIncomplete('MAGETWO-48732');
         // Preconditions
         $this->createProducts($products);
         $this->assignPromotedProducts($promotedProducts, 'related_products');
