3cd02915bb983eca3cfcb09f5cacd478b24b48ea
Olga Matviienko <omatviienko@ebay.com>
2016-03-23 19:07:10 +0200

    MAGETWO-50126: Code Refactoring: Fix the Less test errors

--- dev/tools/grunt/configs/themes.js
+++ dev/tools/grunt/configs/themes.js
@@ -41,6 +41,16 @@ module.exports = {
         ],
         dsl: 'less'
     },
+    test: {
+        area: 'frontend',
+        name: 'Magento/test',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
     backend: {
         area: 'adminhtml',
         name: 'Magento/backend',
