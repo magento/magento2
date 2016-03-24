586032dcb21be14bcad261cc9d3965f4401e9ae5
Serhiy Shkolyarenko <sshkolyarenko@magento.com>
2016-03-24 11:22:11 +0200

    MAGETWO-50680: [Dev experience]CloningInterface implementations should be easily extensible
    added unit tests

---
+++ app/code/Magento/Catalog/Test/Unit/Model/Product/Link/ResolverTest.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Catalog\Test\Unit\Model\Product\Link;
+
+use Magento\Catalog\Model\Product\Link\Resolver;
+
+class ResolverTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $requestMock;
+
+    /**
+     * @var Resolver
+     */
+    protected $resolver;
+
+    protected function setUp()
+    {
+        $this->requestMock = $this->getMockBuilder(\Magento\Framework\App\RequestInterface::class)
+            ->getMockForAbstractClass();
+
+        $this->resolver = new Resolver($this->requestMock);
+    }
+
+    public function testGetLinksEmpty()
+    {
+        $someLinks = [1, 2, 3];
+        $this->requestMock->expects($this->once())
+            ->method('getParam')
+            ->with('links', [])
+            ->willReturn($someLinks);
+        $this->assertEquals($someLinks, $this->resolver->getLinks());
+
+    }
+
+    public function testGetLinksOverridden()
+    {
+        $overriddenLinks = [3, 5, 7];
+        $this->requestMock->expects($this->never())
+            ->method('getParam');
+
+        $this->resolver->override($overriddenLinks);
+        $this->assertEquals($overriddenLinks, $this->resolver->getLinks());
+
+    }
+}
