e057a5f8e54c5b0b09356f02a6f6996f486f1b2a
Stanislav Lopukhov <slopukhov@ebay.com>
2016-03-25 19:14:09 +0200

    MAGETWO-50141: Cannot update category settings on specific environment

--- app/code/Magento/Catalog/Model/Category.php
+++ app/code/Magento/Catalog/Model/Category.php
@@ -660,10 +660,16 @@ class Category extends \Magento\Catalog\Model\AbstractModel implements
     {
         $url = false;
         $image = $this->getImage();
-        if (is_string($image)) {
-            $url = $this->_storeManager->getStore()->getBaseUrl(
-                \Magento\Framework\UrlInterface::URL_TYPE_MEDIA
-            ) . 'catalog/category/' . $image;
+        if ($image) {
+            if (is_string($image)) {
+                return $this->_storeManager->getStore()->getBaseUrl(
+                    \Magento\Framework\UrlInterface::URL_TYPE_MEDIA
+                ) . 'catalog/category/' . $image;
+            } else {
+                throw new \Magento\Framework\Exception\LocalizedException(
+                    __('Something went wrong while getting the image url.')
+                );
+            }
         }
         return $url;
     }
