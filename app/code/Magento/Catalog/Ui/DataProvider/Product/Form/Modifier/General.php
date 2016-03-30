6cd26f4f2c384e0a9a38281a737259fa79eabae4
Dmytro Poperechnyy <dpoperechnyy@magento.com>
2016-03-30 19:56:00 +0300

    MAGETWO-50713: There is no product_has_weight field on virtual and downloadable products

--- app/code/Magento/Catalog/Ui/DataProvider/Product/Form/Modifier/General.php
+++ app/code/Magento/Catalog/Ui/DataProvider/Product/Form/Modifier/General.php
@@ -196,71 +196,69 @@ class General extends AbstractModifier
         $weightPath = $this->arrayManager->findPath(ProductAttributeInterface::CODE_WEIGHT, $meta, null, 'children');
 
         if ($weightPath) {
-            if ($this->locator->getProduct()->getTypeId() !== \Magento\Catalog\Model\Product\Type::TYPE_VIRTUAL) {
-                $meta = $this->arrayManager->merge(
-                    $weightPath . static::META_CONFIG_PATH,
-                    $meta,
-                    [
-                        'dataScope' => ProductAttributeInterface::CODE_WEIGHT,
-                        'validation' => [
-                            'validate-number' => true,
-                        ],
-                        'additionalClasses' => 'admin__field-small',
-                        'addafter' => $this->locator->getStore()->getConfig('general/locale/weight_unit'),
-                        'imports' => [
-                            'disabled' => '!${$.provider}:' . self::DATA_SCOPE_PRODUCT
-                                . '.product_has_weight:value'
-                        ]
+            $meta = $this->arrayManager->merge(
+                $weightPath . static::META_CONFIG_PATH,
+                $meta,
+                [
+                    'dataScope' => ProductAttributeInterface::CODE_WEIGHT,
+                    'validation' => [
+                        'validate-number' => true,
+                    ],
+                    'additionalClasses' => 'admin__field-small',
+                    'addafter' => $this->locator->getStore()->getConfig('general/locale/weight_unit'),
+                    'imports' => [
+                        'disabled' => '!${$.provider}:' . self::DATA_SCOPE_PRODUCT
+                            . '.product_has_weight:value'
                     ]
-                );
+                ]
+            );
 
-                $containerPath = $this->arrayManager->findPath(
-                    static::CONTAINER_PREFIX . ProductAttributeInterface::CODE_WEIGHT,
-                    $meta,
-                    null,
-                    'children'
-                );
-                $meta = $this->arrayManager->merge($containerPath, $meta, [
+            $containerPath = $this->arrayManager->findPath(
+                static::CONTAINER_PREFIX . ProductAttributeInterface::CODE_WEIGHT,
+                $meta,
+                null,
+                'children'
+            );
+            $meta = $this->arrayManager->merge($containerPath, $meta, [
+                'arguments' => [
+                    'data' => [
+                        'config' => [
+                            'component' => 'Magento_Ui/js/form/components/group',
+                        ],
+                    ],
+                ],
+            ]);
+
+            $hasWeightPath = $this->arrayManager->slicePath($weightPath, 0, -1) . '/'
+                . ProductAttributeInterface::CODE_HAS_WEIGHT;
+            $meta = $this->arrayManager->set(
+                $hasWeightPath,
+                $meta,
+                [
                     'arguments' => [
                         'data' => [
                             'config' => [
-                                'component' => 'Magento_Ui/js/form/components/group',
+                                'dataType' => 'boolean',
+                                'formElement' => Form\Element\Select::NAME,
+                                'componentType' => Form\Field::NAME,
+                                'dataScope' => 'product_has_weight',
+                                'label' => '',
+                                'options' => [
+                                    [
+                                        'label' => __('This item has weight'),
+                                        'value' => 1
+                                    ],
+                                    [
+                                        'label' => __('This item has no weight'),
+                                        'value' => 0
+                                    ],
+                                ],
+                                'value' => (int)$this->locator->getProduct()->getTypeInstance()->hasWeight(),
                             ],
                         ],
-                    ],
-                ]);
-
-                $hasWeightPath = $this->arrayManager->slicePath($weightPath, 0, -1) . '/'
-                    . ProductAttributeInterface::CODE_HAS_WEIGHT;
-                $meta = $this->arrayManager->set(
-                    $hasWeightPath,
-                    $meta,
-                    [
-                        'arguments' => [
-                            'data' => [
-                                'config' => [
-                                    'dataType' => 'boolean',
-                                    'formElement' => Form\Element\Select::NAME,
-                                    'componentType' => Form\Field::NAME,
-                                    'dataScope' => 'product_has_weight',
-                                    'label' => '',
-                                    'options' => [
-                                        [
-                                            'label' => __('This item has weight'),
-                                            'value' => 1
-                                        ],
-                                        [
-                                            'label' => __('This item has no weight'),
-                                            'value' => 0
-                                        ],
-                                    ],
-                                    'value' => (int)$this->locator->getProduct()->getTypeInstance()->hasWeight(),
-                                ],
-                            ],
-                        ]
                     ]
-                );
-            }
+                ]
+            );
         }
 
         return $meta;
