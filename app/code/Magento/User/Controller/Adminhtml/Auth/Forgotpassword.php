bb1a736eefe23b3152c52a1a3ca3fbced2968340
Joan He <johe@ebay.com>
2016-03-30 16:37:35 -0500

    MAGETWO-51352: Unhandled exception happens if unable to send email when admin user request reset password

--- app/code/Magento/User/Controller/Adminhtml/Auth/Forgotpassword.php
+++ app/code/Magento/User/Controller/Adminhtml/Auth/Forgotpassword.php
@@ -37,6 +37,8 @@ class Forgotpassword extends \Magento\User\Controller\Adminhtml\Auth
         $email = (string)$this->getRequest()->getParam('email');
         $params = $this->getRequest()->getParams();
 
+        /** @var \Magento\Framework\Controller\Result\Redirect $resultRedirect */
+        $resultRedirect = $this->resultRedirectFactory->create();
         if (!empty($email) && !empty($params)) {
             // Validate received data to be an email address
             if (\Zend_Validate::is($email, 'EmailAddress')) {
@@ -47,7 +49,6 @@ class Forgotpassword extends \Magento\User\Controller\Adminhtml\Auth
                     );
                 } catch (\Magento\Framework\Exception\SecurityViolationException $exception) {
                     $this->messageManager->addErrorMessage($exception->getMessage());
-                    $resultRedirect = $this->resultRedirectFactory->create();
                     return $resultRedirect->setPath('admin');
                 }
                 $collection = $this->_objectManager->get('Magento\User\Model\ResourceModel\User\Collection');
@@ -55,20 +56,28 @@ class Forgotpassword extends \Magento\User\Controller\Adminhtml\Auth
                 $collection->addFieldToFilter('email', $email);
                 $collection->load(false);
 
-                if ($collection->getSize() > 0) {
-                    foreach ($collection as $item) {
-                        /** @var \Magento\User\Model\User $user */
-                        $user = $this->_userFactory->create()->load($item->getId());
-                        if ($user->getId()) {
-                            $newPassResetToken = $this->_objectManager->get(
-                                'Magento\User\Helper\Data'
-                            )->generateResetPasswordLinkToken();
-                            $user->changeResetPasswordLinkToken($newPassResetToken);
-                            $user->save();
-                            $user->sendPasswordResetConfirmationEmail();
+                try {
+                    if ($collection->getSize() > 0) {
+                        foreach ($collection as $item) {
+                            /** @var \Magento\User\Model\User $user */
+                            $user = $this->_userFactory->create()->load($item->getId());
+                            if ($user->getId()) {
+                                $newPassResetToken = $this->_objectManager->get(
+                                    'Magento\User\Helper\Data'
+                                )->generateResetPasswordLinkToken();
+                                $user->changeResetPasswordLinkToken($newPassResetToken);
+                                $user->save();
+                                $user->sendPasswordResetConfirmationEmail();
+                            }
+                            break;
                         }
-                        break;
                     }
+                } catch (\Exception $exception) {
+                    $this->messageManager->addExceptionMessage(
+                        $exception,
+                        __('We\'re unable to send the password reset email.')
+                    );
+                    return $resultRedirect->setPath('admin');
                 }
                 // @codingStandardsIgnoreStart
                 $this->messageManager->addSuccess(__('We\'ll email you a link to reset your password.'));
