cf8714bc147ea68ee40cc907ca0a7fb6e6702c07
Vitalii Zabaznov <vzabaznov@magento.com>
2016-04-05 16:53:47 +0300

    MAGETWO-51407: Cannot save product after update for it is applied

--- lib/internal/Magento/Framework/Stdlib/DateTime/Filter/Date.php
+++ lib/internal/Magento/Framework/Stdlib/DateTime/Filter/Date.php
@@ -7,43 +7,8 @@
  */
 namespace Magento\Framework\Stdlib\DateTime\Filter;
 
-use Magento\Framework\Stdlib\DateTime\TimezoneInterface;
-
 class Date implements \Zend_Filter_Interface
 {
-    /**
-     * Filter that converts localized input into normalized format
-     *
-     * @var \Zend_Filter_LocalizedToNormalized
-     */
-    protected $_localToNormalFilter;
-
-    /**
-     * Filter that converts normalized input into internal format
-     *
-     * @var \Zend_Filter_NormalizedToLocalized
-     */
-    protected $_normalToLocalFilter;
-
-    /**
-     * @var TimezoneInterface
-     */
-    protected $_localeDate;
-
-    /**
-     * @param TimezoneInterface $localeDate
-     */
-    public function __construct(TimezoneInterface $localeDate)
-    {
-        $this->_localeDate = $localeDate;
-        $this->_localToNormalFilter = new \Zend_Filter_LocalizedToNormalized(
-            ['date_format' => $this->_localeDate->getDateFormat(\IntlDateFormatter::SHORT)]
-        );
-        $this->_normalToLocalFilter = new \Zend_Filter_NormalizedToLocalized(
-            ['date_format' => \Magento\Framework\Stdlib\DateTime::DATE_INTERNAL_FORMAT]
-        );
-    }
-
     /**
      * Convert date from localized to internal format
      *
@@ -52,11 +17,7 @@ class Date implements \Zend_Filter_Interface
      */
     public function filter($value)
     {
-        $value = $this->_normalToLocalFilter->filter($this->_localToNormalFilter->filter($value));
-
-        /**
-         * @todo MAGETWO-51391
-         */
-        return str_replace("\xc2\xa0", '', $value);
+        $value = new \DateTime($value, new \DateTimeZone('UTC'));
+        return $value->format('Y-m-d H:i:s');
     }
 }
