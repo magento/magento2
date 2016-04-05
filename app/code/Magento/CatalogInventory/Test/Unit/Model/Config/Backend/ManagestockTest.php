b798c85231ddfe07d876ead1d0ef59f624f04085
Michail Slabko <mslabko@magento.com>
2016-04-05 10:36:58 +0300

    MAGETWO-50893: No _Threshold_ message is displayed after buying product and its qty is not decreased
    - fix tests

--- app/code/Magento/CatalogInventory/Test/Unit/Model/Config/Backend/ManagestockTest.php
+++ app/code/Magento/CatalogInventory/Test/Unit/Model/Config/Backend/ManagestockTest.php
@@ -3,7 +3,7 @@
  * Copyright © 2016 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\CatalogInventory\Test\Unit\Model;
+namespace Magento\CatalogInventory\Test\Unit\Model\Config\Backend;
 
 class ManagestockTest extends \PHPUnit_Framework_TestCase
 {
