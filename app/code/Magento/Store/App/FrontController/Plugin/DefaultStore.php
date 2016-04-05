3ebecd9c4d8c7d091cfcb550633c1f12ea17bb82
Oleksii Korshenko <okorshenko@magento.com>
2016-04-05 17:30:26 -0500

    MAGETWO-48425: [Github] Allowed countries for customer address in admin using storeview configuration #2946

---
+++ app/code/Magento/Store/App/FrontController/Plugin/DefaultStore.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Store\App\FrontController\Plugin;
+
+class DefaultStore
+{
+    /**
+     * @var \Magento\Store\Model\StoreManagerInterface
+     */
+    protected $_storeManager;
+
+    /**
+     * Initialize dependencies.
+     *
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+     */
+    public function __construct(\Magento\Store\Model\StoreManagerInterface $storeManager)
+    {
+        $this->_storeManager = $storeManager;
+    }
+
+    /**
+     * @param \Magento\Framework\App\FrontController $subject
+     * @param \Magento\Framework\App\RequestInterface $request
+     * @return void
+     */
+    public function beforeDispatch(
+        \Magento\Framework\App\FrontController $subject,
+        \Magento\Framework\App\RequestInterface $request
+    ) {        
+        $this->_storeManager->setCurrentStore(\Magento\Store\Model\Store::ADMIN_CODE);
+    }
+}
