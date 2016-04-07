91580809435b0cb350d3158617575041c09185aa
Novykov Ivan <inovykov@ebay.com>
2016-04-07 16:12:35 +0300

    MDVA-245: Problems when importing product data with store_view_code

--- app/code/Magento/CatalogImportExport/Model/Import/Product.php
+++ app/code/Magento/CatalogImportExport/Model/Import/Product.php
@@ -1168,7 +1168,6 @@ class Product extends \Magento\ImportExport\Model\Import\Entity\AbstractEntity
     {
         foreach ($attributesData as $tableName => $skuData) {
             $tableData = [];
-            $where = [];
             foreach ($skuData as $sku => $attributes) {
                 $productId = $this->skuProcessor->getNewSku($sku)['entity_id'];
 
