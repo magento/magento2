ddce66786235098442f92131909c51ad19004bf3
Ankur Kaneria <akaneria@magento.com>
2016-04-08 10:04:03 -0500

    MAGETWO-50736: [TD] Search related improvements for mysql - synonyms support
    - Minor updates to fix static test issues

--- app/code/Magento/Search/Api/SynonymGroupRepositoryInterface.php
+++ app/code/Magento/Search/Api/SynonymGroupRepositoryInterface.php
@@ -14,7 +14,7 @@ interface SynonymGroupRepositoryInterface
      * Save synonym group data
      *
      * @param \Magento\Search\Api\Data\SynonymGroupInterface $synonymGroup
-     * @param $errorOnMergeConflict
+     * @param bool $errorOnMergeConflict
      * @return \Magento\Search\Api\Data\SynonymGroupInterface saved attribute set
      */
     public function save(\Magento\Search\Api\Data\SynonymGroupInterface $synonymGroup, $errorOnMergeConflict = false);
@@ -30,7 +30,7 @@ interface SynonymGroupRepositoryInterface
     /**
      * Return a paritcular synonym group model based on passed in synonym group id
      *
-     * @param $synGroupId
+     * @param int $synGroupId
      * @return \Magento\Search\Model\SynonymGroup
      */
     public function get($synGroupId);
