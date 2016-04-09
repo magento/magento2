454c5fc9e573db47c8c84e906fc2662968e6563c
Maddy Chellathurai <mchellathura@magento.com>
2016-04-08 19:22:34 -0500

    MAGETWO-50934: setup:upgrade failed during CLI upgrade from CE 2.0.x to 2.1.0
    - PHPMD coupling fix

--- setup/src/Magento/Setup/Controller/StartUpdater.php
+++ setup/src/Magento/Setup/Controller/StartUpdater.php
@@ -6,9 +6,7 @@
 
 namespace Magento\Setup\Controller;
 
-use Magento\Framework\App\Filesystem\DirectoryList;
-use Magento\Setup\Model\Cron\JobComponentUninstall;
-use Zend\Json\Json;
+use Magento\Setup\Model\UpdaterTaskCreator;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\JsonModel;
 use Zend\View\Model\ViewModel;
@@ -18,61 +16,28 @@ use Zend\View\Model\ViewModel;
  */
 class StartUpdater extends AbstractActionController
 {
-    /**#@+
-     * Keys in Post payload
+    /**
+     * @var \Magento\Setup\Model\UpdaterTaskCreator
      */
-    const KEY_POST_JOB_TYPE = 'type';
-    const KEY_POST_PACKAGES = 'packages';
-    const KEY_POST_HEADER_TITLE = 'headerTitle';
-    const KEY_POST_DATA_OPTION = 'dataOption';
-    const KEY_POST_PACKAGE_NAME = 'name';
-    const KEY_POST_PACKAGE_VERSION = 'version';
-    /**#@- */
+    private $updaterTaskCreator;
 
     /**
-     * @var \Magento\Framework\Filesystem
+     * @var \Magento\Setup\Model\PayloadValidator
      */
-    private $filesystem;
+    private $payloadValidator;
 
     /**
-     * @var \Magento\Setup\Model\Navigation
-     */
-    private $navigation;
-
-    /**
-     * @var \Magento\Setup\Model\Updater
-     */
-    private $updater;
-
-    /**
-     * @var \Magento\Framework\Module\FullModuleList
-     */
-    private $moduleList;
-
-    /**
-     * @var \Magento\Framework\App\Cache\Manager
-     */
-    private $cacheManager;
-
-    /**
-     * @param \Magento\Framework\Filesystem $filesystem
-     * @param \Magento\Setup\Model\Navigation $navigation
-     * @param \Magento\Setup\Model\Updater $updater
-     * @param \Magento\Framework\Module\FullModuleList $moduleList
-     * @param \Magento\Framework\App\Cache\Manager $cacheManager
+     * Constructor
+     *
+     * @param \Magento\Setup\Model\UpdaterTaskCreator $updaterTaskCreator
+     * @param \Magento\Setup\Model\PayloadValidator $payloadValidator
      */
     public function __construct(
-        \Magento\Framework\Filesystem $filesystem,
-        \Magento\Setup\Model\Navigation $navigation,
-        \Magento\Setup\Model\Updater $updater,
-        \Magento\Framework\Module\FullModuleList $moduleList,
-        \Magento\Framework\App\Cache\Manager $cacheManager
+        \Magento\Setup\Model\UpdaterTaskCreator $updaterTaskCreator,
+        \Magento\Setup\Model\PayloadValidator $payloadValidator
     ) {
-        $this->filesystem = $filesystem;
-        $this->navigation = $navigation;
-        $this->updater = $updater;
-        $this->moduleList = $moduleList;
-        $this->cacheManager = $cacheManager;
+        $this->updaterTaskCreator = $updaterTaskCreator;
+        $this->payloadValidator = $payloadValidator;
     }
 
     /**
@@ -96,227 +61,18 @@ class StartUpdater extends AbstractActionController
     {
         $postPayload = Json::decode($this->getRequest()->getContent(), Json::TYPE_ARRAY);
         $errorMessage = '';
-        if (isset($postPayload[self::KEY_POST_PACKAGES])
-            && is_array($postPayload[self::KEY_POST_PACKAGES])
-            && isset($postPayload[self::KEY_POST_JOB_TYPE])
+        if (isset($postPayload[UpdaterTaskCreator::KEY_POST_PACKAGES])
+            && is_array($postPayload[UpdaterTaskCreator::KEY_POST_PACKAGES])
+            && isset($postPayload[UpdaterTaskCreator::KEY_POST_JOB_TYPE])
         ) {
-            $errorMessage .= $this->validatePayload($postPayload);
+            $errorMessage .= $this->payloadValidator->validatePayload($postPayload);
             if (empty($errorMessage)) {
-                $errorMessage = $this->createUpdaterTasks($postPayload);
+                $errorMessage = $this->updaterTaskCreator->createUpdaterTasks($postPayload);
             }
         } else {
             $errorMessage .= 'Invalid request';
         }
         $success = empty($errorMessage);
         return new JsonModel(['success' => $success, 'message' => $errorMessage]);
-    }
-
-    /**
-     * Create Update tasks
-     *
-     * @param array $postPayload
-     * @return string
-     */
-    private function createUpdaterTasks(array $postPayload)
-    {
-        $errorMessage = '';
-        $packages = $postPayload[self::KEY_POST_PACKAGES];
-        $jobType = $postPayload[self::KEY_POST_JOB_TYPE];
-        $this->createTypeFlag($jobType, $postPayload[self::KEY_POST_HEADER_TITLE]);
-
-        $additionalOptions = [];
-        $cronTaskType = '';
-        $this->getCronTaskConfigInfo($jobType, $postPayload, $additionalOptions, $cronTaskType);
-
-        $errorMessage .= $this->updater->createUpdaterTask(
-            [],
-            \Magento\Setup\Model\Updater::TASK_TYPE_MAINTENANCE_MODE,
-            ['enable' => true]
-        );
-
-        $errorMessage .= $this->updater->createUpdaterTask(
-            [],
-            \Magento\Setup\Model\Cron\JobFactory::JOB_DISABLE_CACHE,
-            []
-        );
-
-        $errorMessage .= $this->updater->createUpdaterTask(
-            $packages,
-            $cronTaskType,
-            $additionalOptions
-        );
-
-        // for module enable job types, we need to follow up with 'setup:upgrade' task to
-        // make sure enabled modules are properly registered
-        if ($jobType == 'enable') {
-            $errorMessage .= $this->updater->createUpdaterTask(
-                [],
-                \Magento\Setup\Model\Cron\JobFactory::JOB_UPGRADE,
-                []
-            );
-        } elseif ($jobType == 'disable') {
-            $errorMessage .= $this->updater->createUpdaterTask(
-                [],
-                \Magento\Setup\Model\Updater::TASK_TYPE_MAINTENANCE_MODE,
-                ['enable' => false]
-            );
-        }
-
-        $cacheStatus = $this->cacheManager-getStatus();
-        $enabledCaches = [];
-        foreach ($cacheStatus as $cacheName => $value) {
-            if ($value) {
-                $enabledCaches[] = $cacheName;
-            }
-        }
-
-        $errorMessage .= $this->updater->createUpdaterTask(
-            [],
-            \Magento\Setup\Model\Cron\JobFactory::JOB_ENABLE_CACHE,
-            [implode(' ', $enabledCaches)]
-        );
-
-        return $errorMessage;
-    }
-
-    /**
-     * Validate POST request payload
-     *
-     * @param array $postPayload
-     * @return string
-     */
-    private function validatePayload(array $postPayload)
-    {
-        $jobType = $postPayload[self::KEY_POST_JOB_TYPE];
-        $errorMessage = '';
-        switch($jobType) {
-            case 'uninstall':
-                $errorMessage = $this->validateUninstallPayload($postPayload);
-                break;
-
-            case 'update':
-                $errorMessage = $this->validateUpdatePayload($postPayload);
-                break;
-
-            case 'enable':
-            case 'disable':
-                $errorMessage = $this->validateEnableDisablePayload($postPayload);
-                break;
-        }
-        return $errorMessage;
-    }
-
-    /**
-     * Validate 'uninstall' job type payload
-     *
-     * @param array $postPayload
-     * @return string
-     */
-    private function validateUninstallPayload(array $postPayload)
-    {
-        $errorMessage = '';
-        if (!isset($postPayload[self::KEY_POST_DATA_OPTION])) {
-            $errorMessage = 'Missing dataOption' . PHP_EOL;
-        }
-        return $errorMessage;
-    }
-
-    /**
-     * Validate 'update' job type payload
-     *
-     * @param array $postPayload
-     * @return string
-     */
-    private function validateUpdatePayload(array $postPayload)
-    {
-        $errorMessage = '';
-        $packages = $postPayload[self::KEY_POST_PACKAGES];
-        foreach ($packages as $package) {
-            if ((!isset($package[self::KEY_POST_PACKAGE_NAME])) || (!isset($package[self::KEY_POST_PACKAGE_VERSION]))) {
-                $errorMessage .= 'Missing package information' . PHP_EOL;
-                break;
-            }
-        }
-        return $errorMessage;
-    }
-
-    /**
-     * Validate 'enable/disable' job type payload
-     *
-     * @param array $postPayload
-     * @return string
-     */
-    private function validateEnableDisablePayload(array $postPayload)
-    {
-        $errorMessage = '';
-        $packages = $postPayload[self::KEY_POST_PACKAGES];
-        foreach ($packages as $package) {
-            if (!$this->moduleList->has($package[self::KEY_POST_PACKAGE_NAME])) {
-                $errorMessage .= 'Invalid Magento module name: ' . $package[self::KEY_POST_PACKAGE_NAME] . PHP_EOL;
-                break;
-            }
-        }
-        return $errorMessage;
-    }
-
-    /**
-     * Create flag to be used in Updater
-     *
-     * @param string $type
-     * @param string $title
-     * @return void
-     */
-    private function createTypeFlag($type, $title)
-    {
-        $data = [];
-        $data[self::KEY_POST_JOB_TYPE] = $type;
-        $data[self::KEY_POST_HEADER_TITLE] = $title;
-
-        $menuItems = $this->navigation->getMenuItems();
-        $titles = [];
-        foreach ($menuItems as $menuItem) {
-            if (isset($menuItem['type']) && $menuItem['type'] === $type) {
-                $titles[] = str_replace("\n", '<br />', $menuItem['title']);
-            }
-        }
-        $data['titles'] = $titles;
-        $directoryWrite = $this->filesystem->getDirectoryWrite(DirectoryList::VAR_DIR);
-        $directoryWrite->writeFile('.type.json', Json::encode($data));
-    }
-
-    /**
-     * Returns cron config info based on passed in job type
-     *
-     * @param string $jobType
-     * @param array $postPayload
-     * @param array $addtionalOptions
-     * @param string $cronTaskType
-     * @return void
-     */
-    private function getCronTaskConfigInfo($jobType, $postPayload, &$additionalOptions, &$cronTaskType)
-    {
-        $additionalOptions = [];
-        switch($jobType) {
-            case 'uninstall':
-                $additionalOptions = [
-                    JobComponentUninstall::DATA_OPTION => $postPayload[self::KEY_POST_DATA_OPTION]
-                ];
-                $cronTaskType = \Magento\Setup\Model\Cron\JobFactory::JOB_COMPONENT_UNINSTALL;
-                break;
-
-            case 'upgrade':
-            case 'update':
-            case 'install':
-                $cronTaskType = \Magento\Setup\Model\Updater::TASK_TYPE_UPDATE;
-                break;
-
-            case 'enable':
-                $cronTaskType = \Magento\Setup\Model\Cron\JobFactory::JOB_MODULE_ENABLE;
-                break;
-
-            case 'disable':
-                $cronTaskType = \Magento\Setup\Model\Cron\JobFactory::JOB_MODULE_DISABLE;
-                break;
-        }
     }
 }
