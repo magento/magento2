1cf352a66a142a6e814c18acc9ce71acae4785d2
Dmytro Poperechnyy <dpoperechnyy@magento.com>
2016-04-11 15:02:35 +0300

    MAGETWO-50600: Broken value in "Minimum Qty Allowed in Shopping Cart" field

---
+++ app/code/Magento/CatalogInventory/view/adminhtml/web/js/components/use-config-min-sale-qty.js
@@ -0,0 +1,80 @@
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+define([
+    'Magento_Ui/js/form/element/single-checkbox',
+    'underscore',
+    'uiRegistry'
+], function (checkbox, _, registry) {
+    'use strict';
+
+    return checkbox.extend({
+        defaults: {
+            valueFromConfig: ''
+        },
+
+        /**
+         * @returns {Element}
+         */
+        initObservable: function () {
+            return this
+                ._super()
+                .observe(['valueFromConfig']);
+        },
+
+        /**
+         * @inheritdoc
+         */
+        initialize: function () {
+            this._super();
+            this.onCheckedChanged(this.checked());
+
+            return this;
+        },
+
+        /**
+         * @inheritdoc
+         */
+        'onCheckedChanged': function (newChecked) {
+            if (newChecked) {
+                var valueFromConfig = this.valueFromConfig();
+                if (_.isArray(valueFromConfig)) {
+                    this.changeVisibleDisabled(this.inputField, true, true, 1);
+                } else if (_.isObject(valueFromConfig)) {
+                    this.changeVisibleDisabled(this.inputField, false, true, null);
+                    this.changeVisibleDisabled(this.dynamicRowsField, true, true, null);
+                }
+            } else {
+                this.changeVisibleDisabled(this.inputField, true, false, null);
+                this.changeVisibleDisabled(this.dynamicRowsField, false, true, null);
+            }
+
+            this._super(newChecked);
+        },
+
+        /**
+         * Change visible and disabled
+         *
+         * @param {String} filter
+         * @param {Boolean} visible
+         * @param {Boolean} disabled
+         * @param {Null|Number} valueFromConfig
+         */
+        changeVisibleDisabled: function (filter, visible, disabled, valueFromConfig) {
+            registry.async(filter)(
+                function (currentComponent) {
+                    var initialValue = currentComponent.initialValue;
+                    if (_.isString(initialValue) || valueFromConfig === 1) {
+                        currentComponent.value(1);
+                    } else if (initialValue) {
+                        currentComponent.value(initialValue);
+                    }
+                    currentComponent.visible(visible);
+                    currentComponent.disabled(disabled);
+                }
+            );
+        }
+    });
+});
