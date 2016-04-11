79605c5710a7eab4d1847dedc0f76ae49d54c963
Olga Lytvynenko <olytvynenko@ebay.com>
2016-04-11 15:24:22 +0300

    MAGETWO-51364: Some disabled fields has incorrect label color

--- app/code/Magento/CatalogInventory/Ui/DataProvider/Product/Form/Modifier/AdvancedInventory.php
+++ app/code/Magento/CatalogInventory/Ui/DataProvider/Product/Form/Modifier/AdvancedInventory.php
@@ -184,6 +184,8 @@ class AdvancedInventory extends AbstractModifier
                 'visible' => '1',
                 'require' => '0',
                 'additionalClasses' => 'admin__field-small',
+                'label' => __('Quantity'),
+                'scopeLabel' => '[GLOBAL]',
                 'dataScope' => 'qty',
                 'validation' => [
                     'validate-number' => true,
