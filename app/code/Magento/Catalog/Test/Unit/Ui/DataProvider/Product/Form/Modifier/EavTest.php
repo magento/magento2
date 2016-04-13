c98f6f1091a4f7c7b5db2d486a066f3d247d871a
Dmytro Poperechnyy <dpoperechnyy@magento.com>
2016-04-13 19:03:41 +0300

    MAGETWO-50552: Four decimal characters saved in new product attribute with type "Price"
    - Added Eav unit test;
    - Protected properties updated to private;

--- app/code/Magento/Catalog/Test/Unit/Ui/DataProvider/Product/Form/Modifier/EavTest.php
+++ app/code/Magento/Catalog/Test/Unit/Ui/DataProvider/Product/Form/Modifier/EavTest.php
@@ -19,6 +19,15 @@ use Magento\Eav\Model\Entity\Type as EntityType;
 use Magento\Eav\Model\ResourceModel\Entity\Attribute\Collection as AttributeCollection;
 use Magento\Ui\DataProvider\Mapper\FormElement as FormElementMapper;
 use Magento\Ui\DataProvider\Mapper\MetaProperties as MetaPropertiesMapper;
+use Magento\Framework\Api\SearchCriteriaBuilder;
+use Magento\Catalog\Api\ProductAttributeGroupRepositoryInterface;
+use Magento\Framework\Api\SearchCriteria;
+use Magento\Framework\Api\SortOrderBuilder;
+use Magento\Catalog\Api\ProductAttributeRepositoryInterface;
+use Magento\Framework\Api\SearchResultsInterface;
+use Magento\Catalog\Api\Data\ProductAttributeInterface;
+use Magento\Eav\Api\Data\AttributeGroupInterface;
+use Magento\Catalog\Model\ResourceModel\Eav\Attribute;
 
 /**
  * Class EavTest
@@ -88,6 +97,46 @@ class EavTest extends AbstractModifierTest
      */
     protected $metaPropertiesMapperMock;
 
+    /**
+     * @var SearchCriteriaBuilder|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $searchCriteriaBuilderMock;
+
+    /**
+     * @var ProductAttributeGroupRepositoryInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $attributeGroupRepositoryMock;
+
+    /**
+     * @var SearchCriteria|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $searchCriteriaMock;
+
+    /**
+     * @var SortOrderBuilder|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $sortOrderBuilderMock;
+
+    /**
+     * @var ProductAttributeRepositoryInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $attributeRepositoryMock;
+
+    /**
+     * @var AttributeGroupInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $attributeGroupMock;
+
+    /**
+     * @var SearchResultsInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $searchResultsMock;
+
+    /**
+     * @var Attribute|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $eavAttributeMock;
+
     protected function setUp()
     {
         parent::setUp();
@@ -126,6 +175,29 @@ class EavTest extends AbstractModifierTest
             ->disableOriginalConstructor()
             ->getMock();
         $this->metaPropertiesMapperMock = $this->getMockBuilder(MetaPropertiesMapper::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->searchCriteriaBuilderMock = $this->getMockBuilder(SearchCriteriaBuilder::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->attributeGroupRepositoryMock = $this->getMockBuilder(ProductAttributeGroupRepositoryInterface::class)
+            ->getMockForAbstractClass();
+        $this->attributeGroupMock = $this->getMockBuilder(AttributeGroupInterface::class)
+            ->setMethods(['getAttributeGroupCode', 'getApplyTo'])
+            ->getMockForAbstractClass();
+        $this->attributeRepositoryMock = $this->getMockBuilder(ProductAttributeRepositoryInterface::class)
+            ->getMockForAbstractClass();
+        $this->searchCriteriaMock = $this->getMockBuilder(SearchCriteria::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['getItems'])
+            ->getMock();
+        $this->sortOrderBuilderMock = $this->getMockBuilder(SortOrderBuilder::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->searchResultsMock = $this->getMockBuilder(SearchResultsInterface::class)
+            ->getMockForAbstractClass();
+        $this->eavAttributeMock = $this->getMockBuilder(Attribute::class)
+            ->setMethods(['getAttributeGroupCode', 'getApplyTo', 'getFrontendInput', 'getAttributeCode'])
             ->disableOriginalConstructor()
             ->getMock();
 
@@ -179,11 +251,90 @@ class EavTest extends AbstractModifierTest
             'storeManager' => $this->storeManagerMock,
             'formElementMapper' => $this->formElementMapperMock,
             'metaPropertiesMapper' => $this->metaPropertiesMapperMock,
+            'searchCriteriaBuilder' => $this->searchCriteriaBuilderMock,
+            'attributeGroupRepository' => $this->attributeGroupRepositoryMock,
+            'sortOrderBuilder' => $this->sortOrderBuilderMock,
+            'attributeRepository' => $this->attributeRepositoryMock
         ]);
     }
 
     public function testModifyData()
     {
+        $sourceData = [
+            '1' => [
+                'product' => [
+                    ProductAttributeInterface::CODE_PRICE => '19.99'
+                ]
+            ]
+        ];
 
+        $this->locatorMock->expects($this->any())
+            ->method('getProduct')
+            ->willReturn($this->productMock);
+
+        $this->productMock->expects($this->any())
+            ->method('getId')
+            ->willReturn(1);
+        $this->productMock->expects($this->any())
+            ->method('getAttributeSetId')
+            ->willReturn(4);
+        $this->productMock->expects($this->any())
+            ->method('getData')
+            ->with(ProductAttributeInterface::CODE_PRICE)->willReturn('19.9900');
+
+        $this->searchCriteriaBuilderMock->expects($this->any())
+            ->method('addFilter')
+            ->willReturnSelf();
+        $this->searchCriteriaBuilderMock->expects($this->any())
+            ->method('create')
+            ->willReturn($this->searchCriteriaMock);
+        $this->attributeGroupRepositoryMock->expects($this->any())
+            ->method('getList')
+            ->willReturn($this->searchCriteriaMock);
+        $this->searchCriteriaMock->expects($this->any())
+            ->method('getItems')
+            ->willReturn([$this->attributeGroupMock]);
+        $this->sortOrderBuilderMock->expects($this->any())
+            ->method('setField')
+            ->willReturnSelf();
+        $this->sortOrderBuilderMock->expects($this->any())
+            ->method('setAscendingDirection')
+            ->willReturnSelf();
+        $dataObjectMock = $this->getMock('\Magento\Framework\Api\AbstractSimpleObject', [], [], '', false);
+        $this->sortOrderBuilderMock->expects($this->any())
+            ->method('create')
+            ->willReturn($dataObjectMock);
+
+        $this->searchCriteriaBuilderMock->expects($this->any())
+            ->method('addFilter')
+            ->willReturnSelf();
+        $this->searchCriteriaBuilderMock->expects($this->any())
+            ->method('addSortOrder')
+            ->willReturnSelf();
+        $this->searchCriteriaBuilderMock->expects($this->any())
+            ->method('create')
+            ->willReturn($this->searchCriteriaMock);
+
+        $this->attributeRepositoryMock->expects($this->any())
+            ->method('getList')
+            ->with($this->searchCriteriaMock)
+            ->willReturn($this->searchResultsMock);
+        $this->eavAttributeMock->expects($this->any())
+            ->method('getAttributeGroupCode')
+            ->willReturn('product-details');
+        $this->eavAttributeMock->expects($this->any())
+            ->method('getApplyTo')
+            ->willReturn([]);
+        $this->eavAttributeMock->expects($this->any())
+            ->method('getFrontendInput')
+            ->willReturn(ProductAttributeInterface::CODE_PRICE);
+        $this->eavAttributeMock->expects($this->any())
+            ->method('getAttributeCode')
+            ->willReturn(ProductAttributeInterface::CODE_PRICE);
+        $this->searchResultsMock->expects($this->any())
+            ->method('getItems')
+            ->willReturn([$this->eavAttributeMock]);
+
+        $this->assertEquals($sourceData, $this->getModel()->modifyData([]));
     }
 }
