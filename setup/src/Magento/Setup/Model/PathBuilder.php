2aa58df655506bf1ad6f240a7c9d86eaa35777a8
Ankur Kaneria <akaneria@magento.com>
2016-04-14 09:03:48 -0500

    MAGETWO-51544: [Github] Cannot upgrade to Magento 2.0.4 #4013
    - Code review comment update.
    - Consider ["extra"]["map"] section absence in base package a valid scenario

--- setup/src/Magento/Setup/Model/PathBuilder.php
+++ setup/src/Magento/Setup/Model/PathBuilder.php
@@ -45,6 +45,7 @@ class PathBuilder
         // Locate composer.json for magento2-base module and read the extra map section for the list of
         // magento specific files and directories that updater will need access to perform the upgrade
 
+        $filesPathList = [];
         $vendorDir = require VENDOR_PATH;
         $basePackageComposerFilePath = $vendorDir . '/' . self::MAGENTO_BASE_PACKAGE_COMPOSER_JSON_FILE;
         if (!$this->reader->isExist($basePackageComposerFilePath)) {
@@ -59,17 +60,12 @@ class PathBuilder
         }
         $composerJsonFileData = json_decode($this->reader->readFile($basePackageComposerFilePath), true);
         if (!isset($composerJsonFileData[self::COMPOSER_KEY_EXTRA][self::COMPOSER_KEY_MAP])) {
-            throw new \Magento\Setup\Exception(
-                'Could not find "extra" => "map" section within '
-                . self::MAGENTO_BASE_PACKAGE_COMPOSER_JSON_FILE
-                . ' file.'
-            );
+            return $filesPathList;
         }
         $extraMappings = $composerJsonFileData[self::COMPOSER_KEY_EXTRA][self::COMPOSER_KEY_MAP];
-        $fileAndPathList = [];
         foreach ($extraMappings as $map) {
-            $fileAndPathList[] = $map[1];
+            $filesPathList[] = $map[1];
         }
-        return $fileAndPathList;
+        return $filesPathList;
     }
 }
