df759c73e30e753c2a1d7efcedbc8fdf639066a3
Roman Liukshyn <rliukshyn@magento.com>
2016-04-14 11:10:07 +0000

    MAGETWO-51868: [Github] USPS shipping weight calculation rounds ounces to whole number

--- app/code/Magento/Usps/Model/Carrier.php
+++ app/code/Magento/Usps/Model/Carrier.php
@@ -324,7 +324,7 @@ class Carrier extends AbstractCarrierOnline implements \Magento\Shipping\Model\C
 
         $weight = $this->getTotalNumOfBoxes($request->getPackageWeight());
         $r->setWeightPounds(floor($weight));
-        $r->setWeightOunces(round(($weight - floor($weight)) * self::OUNCES_POUND, 1));
+        $r->setWeightOunces(round(($weight - floor($weight)) * self::OUNCES_POUND, 10));
         if ($request->getFreeMethodWeight() != $request->getPackageWeight()) {
             $r->setFreeMethodWeight($request->getFreeMethodWeight());
         }
@@ -371,7 +371,7 @@ class Carrier extends AbstractCarrierOnline implements \Magento\Shipping\Model\C
 
         $weight = $this->getTotalNumOfBoxes($r->getFreeMethodWeight());
         $r->setWeightPounds(floor($weight));
-        $r->setWeightOunces(round(($weight - floor($weight)) * self::OUNCES_POUND, 1));
+        $r->setWeightOunces(round(($weight - floor($weight)) * self::OUNCES_POUND, 10));
         $r->setService($freeMethod);
     }
 
