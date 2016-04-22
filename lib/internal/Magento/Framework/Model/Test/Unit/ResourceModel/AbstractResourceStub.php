c7bb40b561b1686f4a3b8d085ad5a2b40e090d2a
Dmytro Kvashnin <dkvashnin@magento.com>
2016-04-22 15:23:30 +0300

    MAGETWO-52226: Remove conditional serialization of AbstractResource::serializableFields

--- lib/internal/Magento/Framework/Model/Test/Unit/ResourceModel/AbstractResourceStub.php
+++ lib/internal/Magento/Framework/Model/Test/Unit/ResourceModel/AbstractResourceStub.php
@@ -5,6 +5,7 @@
  */
 namespace Magento\Framework\Model\Test\Unit\ResourceModel;
 
+use Magento\Framework\DataObject;
 use Magento\Framework\Model\ResourceModel\AbstractResource;
 
 class AbstractResourceStub extends AbstractResource
@@ -30,23 +31,23 @@ class AbstractResourceStub extends AbstractResource
     }
 
     /**
-     * @param \Magento\Framework\DataObject $object
+     * @param DataObject $object
      * @param string $field
      * @param null $defaultValue
      * @param bool $unsetEmpty
      * @return $this
      */
-    public function _serializeField(\Magento\Framework\DataObject $object, $field, $defaultValue = null, $unsetEmpty = false)
+    public function _serializeField(DataObject $object, $field, $defaultValue = null, $unsetEmpty = false)
     {
         return parent::_serializeField($object, $field, $defaultValue, $unsetEmpty);
     }
 
     /**
-     * @param \Magento\Framework\DataObject $object
+     * @param DataObject $object
      * @param string $field
      * @param null $defaultValue
      */
-    public function _unserializeField(\Magento\Framework\DataObject $object, $field, $defaultValue = null)
+    public function _unserializeField(DataObject $object, $field, $defaultValue = null)
     {
         parent::_unserializeField($object, $field, $defaultValue);
     }
