f6d902fedd8972dfd6e1c592ef4b2c1660b9ca3d
Maddy Chellathurai <mchellathura@magento.com>
2016-04-21 19:15:53 -0500

    MAGETWO-50934: setup:upgrade failed during CLI upgrade from CE 2.0.x to 2.1.0
    - adding area

--- setup/src/Magento/Setup/Test/Unit/Console/Command/UpgradeCommandTest.php
+++ setup/src/Magento/Setup/Test/Unit/Console/Command/UpgradeCommandTest.php
@@ -13,12 +13,18 @@ class UpgradeCommandTest extends \PHPUnit_Framework_TestCase
     public function testExecute()
     {
         $installerFactory = $this->getMock('Magento\Setup\Model\InstallerFactory', [], [], '', false);
+        $objectManagerProvider = $this->getMock('\Magento\Setup\Model\ObjectManagerProvider', [], [], '', false);
+        $objectManager = $this->getMockForAbstractClass('Magento\Framework\ObjectManagerInterface');
+        $state = $this->getMock('Magento\Framework\App\State', [], [], '', false);
+        $objectManagerProvider->expects($this->once())->method('get')->willReturn($objectManager);
+        $objectManager->expects($this->once())->method('get')->willReturn($state);
+        $state->expects($this->once())->method('setAreaCode')->with('adminhtml');
         $installer = $this->getMock('Magento\Setup\Model\Installer', [], [], '', false);
         $installer->expects($this->at(0))->method('updateModulesSequence');
         $installer->expects($this->at(1))->method('installSchema');
         $installer->expects($this->at(2))->method('installDataFixtures');
         $installerFactory->expects($this->once())->method('create')->willReturn($installer);
-        $commandTester = new CommandTester(new UpgradeCommand($installerFactory));
+        $commandTester = new CommandTester(new UpgradeCommand($installerFactory, $objectManagerProvider));
         $commandTester->execute([]);
     }
 }
