ea39011172ef7f27201b6aef9434f8273637ef0c
Oleh Posyniak <oposyniak@magento.com>
2016-04-25 14:02:15 +0300

    MAGETWO-52215: Cms pages isn't synchronized with core_config_data

--- app/code/Magento/Cms/Test/Unit/Model/PageTest.php
+++ app/code/Magento/Cms/Test/Unit/Model/PageTest.php
@@ -14,6 +14,7 @@ use Magento\Framework\Model\ResourceModel\AbstractResource;
 
 /**
  * @covers \Magento\Cms\Model\Page
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
  */
 class PageTest extends \PHPUnit_Framework_TestCase
 {
@@ -57,7 +58,7 @@ class PageTest extends \PHPUnit_Framework_TestCase
             ->getMock();
         $this->resourcePageMock = $this->getMockBuilder(PageResource::class)
             ->disableOriginalConstructor()
-            ->setMethods(['getIdFieldName', 'checkIdentifier',])
+            ->setMethods(['getIdFieldName', 'checkIdentifier'])
             ->getMock();
         $this->eventManagerMock = $this->getMockBuilder(ManagerInterface::class)
             ->disableOriginalConstructor()
