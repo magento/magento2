7d1b10dc7915aebec15de479680df32e98efc3e0
Kateryna Muntianu <kmuntianu@magento.com>
2016-04-26 16:38:51 +0300

    MAGETWO-52147: [FT] CreateProductAttributeEntityFromProductPageTest fails
    - Added waiting for loaders to disappear

--- dev/tests/functional/tests/app/Magento/Catalog/Test/Block/Adminhtml/Product/Modal/AddAttribute.php
+++ dev/tests/functional/tests/app/Magento/Catalog/Test/Block/Adminhtml/Product/Modal/AddAttribute.php
@@ -7,6 +7,8 @@
 namespace Magento\Catalog\Test\Block\Adminhtml\Product\Modal;
 
 use Magento\Ui\Test\Block\Adminhtml\FormSections;
+use Magento\Backend\Test\Block\Template;
+use Magento\Mtf\Client\Locator;
 
 /**
  * Add new attribute modal.
@@ -14,11 +16,31 @@ use Magento\Ui\Test\Block\Adminhtml\FormSections;
 class AddAttribute extends FormSections
 {
     /**
-     * Selector for "Create New Attribute" button.
+     * Xpath selector for "Create New Attribute" button.
      *
      * @var string
      */
     private $createNewAttribute = 'button[data-index="add_new_attribute_button"]';
+
+    /**
+     * Xpath selector for "Add Attribute" form.
+     *
+     * @var string
+     */
+    private $addAttributeBlock = '//*[@data-role="modal"][.//button[@data-index="add_new_attribute_button"]]';
+
+    /**
+     * Get backend abstract block.
+     *
+     * @return Template
+     */
+    protected function getTemplateBlock()
+    {
+        return $this->blockFactory->create(
+            'Magento\Backend\Test\Block\Template',
+            ['element' => $this->_rootElement->find($this->addAttributeBlock, Locator::SELECTOR_XPATH)]
+        );
+    }
 
     /**
      * Click on "Create new attribute" button.
@@ -27,6 +49,8 @@ class AddAttribute extends FormSections
      */
     public function createNewAttribute()
     {
+        $this->getTemplateBlock()->waitLoader();
         $this->_rootElement->find($this->createNewAttribute)->click();
+        $this->getTemplateBlock()->waitLoader();
     }
 }
