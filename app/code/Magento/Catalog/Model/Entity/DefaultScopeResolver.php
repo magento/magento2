72a0f25eddb1af5da2302c53efba161d0847eb32
Partica, Cristian <cpartica@magento.com>
2016-04-28 12:07:32 -0500

    MAGETWO-51732: Catalog product "Use default" check problems causing data loss when updating product on non default site
    - refactoring by only one implementation to override store on creation

--- app/code/Magento/Catalog/Model/Entity/DefaultScopeResolver.php
+++ app/code/Magento/Catalog/Model/Entity/DefaultScopeResolver.php
@@ -10,7 +10,9 @@ use Magento\Framework\Exception\ConfigurationMismatchException;
 use Magento\Framework\EntityManager\MetadataPool;
 use Magento\Framework\Phrase;
 use Magento\Framework\Model\Entity\ScopeResolverInterface;
-use Magento\Catalog\Model\Entity\DefaultStoreScopeProvider;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Store\Model\StoreScopeProvider;
+use Magento\Store\Model\Store;
 
 /**
  * Class DefaultScopeResolver
@@ -18,9 +20,13 @@ use Magento\Catalog\Model\Entity\DefaultStoreScopeProvider;
 class DefaultScopeResolver implements ScopeResolverInterface
 {
     /**
-     * @var DefaultStoreScopeProvider
+     * @var StoreScopeProvider
      */
     private $storeScopeProvider;
+    /**
+     * @var StoreManagerInterface
+     */
+    private $storeManager;
 
     /**
      * @var MetadataPool
@@ -29,14 +35,17 @@ class DefaultScopeResolver implements ScopeResolverInterface
 
     /**
      * ScopeResolver constructor.
-     * @param DefaultStoreScopeProvider $storeScopeProvider
+     * @param StoreScopeProvider $storeScopeProvider
+     * @param StoreManagerInterface $storeManager
      * @param MetadataPool $metadataPool
      */
     public function __construct(
-        DefaultStoreScopeProvider $storeScopeProvider,
+        StoreScopeProvider $storeScopeProvider,
+        StoreManagerInterface $storeManager,
         MetadataPool $metadataPool
     ) {
         $this->storeScopeProvider = $storeScopeProvider;
+        $this->storeManager = $storeManager;
         $this->metadataPool = $metadataPool;
     }
 
@@ -49,11 +58,24 @@ class DefaultScopeResolver implements ScopeResolverInterface
      */
     public function getEntityContext($entityType, $entityData = [])
     {
+        $entityData = $this->resolveScopeForDefault($entityData);
         $entityContext = [];
         $metadata = $this->metadataPool->getMetadata($entityType);
         for ($count=0; $count < count($metadata->getEntityContext()); $count++) {
             $entityContext[] = $this->storeScopeProvider->getContext($entityType, $entityData);
         }
         return $entityContext;
+    }
+
+    /**
+     * @param array $entityData
+     * @return array
+     */
+    private function resolveScopeForDefault($entityData)
+    {
+        if ($entityData[Store::STORE_ID] == $this->storeManager->getDefaultStoreView()->getId()) {
+            $entityData[Store::STORE_ID] = Store::DEFAULT_STORE_ID;
+        }
+        return $entityData;
     }
 }
