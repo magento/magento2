a1293f77e5feec365a6f651fe5c1bb5fef49167e
David Alger <david@classyllama.com>
2016-04-29 20:54:03 -0500

    Corrected quoting in static test call

--- dev/travis/script.sh
+++ dev/travis/script.sh
@@ -17,8 +17,8 @@ case $TEST_SUITE in
     static)
         cd dev/tests/static
         php get_github_changes.php \
-            --output-file='$TRAVIS_BUILD_DIR/dev/tests/static/testsuite/Magento/Test/_files/changed_files_ce.txt' \
-            --base-path='$TRAVIS_BUILD_DIR' \
+            --output-file="$TRAVIS_BUILD_DIR/dev/tests/static/testsuite/Magento/Test/_files/changed_files_ce.txt" \
+            --base-path="$TRAVIS_BUILD_DIR" \
             --repo='https://github.com/magento/magento2.git' \
             --branch='develop'
         phpunit --filter 'Magento\\Test\\Php\\LiveCodeTest'
