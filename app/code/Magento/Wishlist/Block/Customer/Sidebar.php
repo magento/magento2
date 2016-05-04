4d72132bcca31739335984f7c4b5f5968fca25f3
Sergey Semenov <ssemenov@magento.com>
2016-05-04 14:16:54 +0300

    MAGETWO-51993: Product Price isn't displayed in "My Wish List" customer's account widget if another browser is used

--- app/code/Magento/Wishlist/Block/Customer/Sidebar.php
+++ app/code/Magento/Wishlist/Block/Customer/Sidebar.php
@@ -9,6 +9,9 @@
  */
 namespace Magento\Wishlist\Block\Customer;
 
+use Magento\Catalog\Model\Product;
+use Magento\Framework\Pricing\Render;
+
 class Sidebar extends \Magento\Wishlist\Block\AbstractBlock
 {
     /**
@@ -19,5 +22,41 @@ class Sidebar extends \Magento\Wishlist\Block\AbstractBlock
     public function getTitle()
     {
         return __('My Wish List');
+    }
+
+    /**
+     * Return HTML block content
+     *
+     * @param Product $product
+     * @param string $priceType
+     * @param string $renderZone
+     * @param array $arguments
+     * @return string
+     */
+    public function getProductPriceHtml(
+        Product $product,
+        $priceType,
+        $renderZone = Render::ZONE_ITEM_LIST,
+        array $arguments = []
+    ) {
+        if (!isset($arguments['zone'])) {
+            $arguments['zone'] = $renderZone;
+        }
+
+        /** @var Render $priceRender */
+        $priceRender = $this->getLayout()->getBlock('product.price.render.default');
+        if (!$priceRender) {
+            $priceRender = $this->getLayout()->createBlock(
+                'Magento\Framework\Pricing\Render',
+                'product.price.render.default',
+                ['data' => ['price_render_handle' => 'catalog_product_prices']]
+            );
+        }
+
+        $price = '';
+        if ($priceRender) {
+            $price = $priceRender->render($priceType, $product, $arguments);
+        }
+        return $price;
     }
 }
