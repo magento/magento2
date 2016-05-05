6a65ed65043240a18192207b4105c8535f366f6d
Oleksii Korshenko <okorshenko@magento.com>
2016-05-05 14:04:37 -0500

    MAGETWO-49585: [GitHub] Add type declaration to setter methods of generated ExtensionAttribute classes #2451
    - Fixes magento/magento2#2451
    - fixed unit tests

--- lib/internal/Magento/Framework/TestFramework/Unit/Helper/ObjectManager.php
+++ lib/internal/Magento/Framework/TestFramework/Unit/Helper/ObjectManager.php
@@ -162,15 +162,15 @@ class ObjectManager
         $newObject = $reflectionClass->newInstanceArgs($constructArguments);
 
         foreach (array_diff_key($arguments, $constructArguments) as $key => $value) {
-            if ($reflectionClass->hasProperty($key)) {
-                $reflectionProperty = $reflectionClass->getProperty($key);
-                $reflectionProperty->setAccessible(true);
-                $reflectionProperty->setValue($newObject, $value);
-            } elseif ($reflectionClass->getParentClass() && $reflectionClass->getParentClass()->hasProperty($key)) {
-                //catch use case when private property is declared in parent class
-                $reflectionProperty = $reflectionClass->getParentClass()->getProperty($key);
-                $reflectionProperty->setAccessible(true);
-                $reflectionProperty->setValue($newObject, $value);
+            $propertyReflectionClass = $reflectionClass;
+            while ($propertyReflectionClass) {
+                if ($propertyReflectionClass->hasProperty($key)) {
+                    $reflectionProperty = $propertyReflectionClass->getProperty($key);
+                    $reflectionProperty->setAccessible(true);
+                    $reflectionProperty->setValue($newObject, $value);
+                    break;
+                }
+                $propertyReflectionClass = $reflectionClass->getParentClass();
             }
         }
         return $newObject;
