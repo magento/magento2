fffb4e6b9a7be9ba6b2c738a3d98aa871a2983e0
Oleksandr Karpenko <okarpenko@magento.com>
2016-05-11 15:02:00 +0300

    MAGETWO-52593: Minicart does not clean when customer session is over

--- app/code/Magento/Customer/CustomerData/SessionChecker.php
+++ app/code/Magento/Customer/CustomerData/SessionChecker.php
@@ -3,7 +3,7 @@
  * Copyright © 2016 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\Customer\Model\Plugin;
+namespace Magento\Customer\CustomerData;
 
 use Magento\Customer\Model\Session;
 use Magento\Framework\Stdlib\Cookie\CookieMetadataFactory;
