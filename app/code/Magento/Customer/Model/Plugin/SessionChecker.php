627b68c7741bb655aff364b3c8af45d2afaa3c43
Oleksandr Karpenko <okarpenko@magento.com>
2016-05-11 13:53:07 +0300

    MAGETWO-52593: Minicart does not clean when customer session is over

---
+++ app/code/Magento/Customer/Model/Plugin/SessionChecker.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Customer\Model\Plugin;
+
+use Magento\Customer\Model\Session;
+use Magento\Framework\Stdlib\Cookie\CookieMetadataFactory;
+use Magento\Framework\Stdlib\Cookie\PhpCookieManager;
+
+class SessionChecker
+{
+    /**
+     * @var PhpCookieManager
+     */
+    private $cookieManager;
+
+    /**
+     * @var CookieMetadataFactory
+     */
+    private $cookieMetadataFactory;
+
+    /**
+     * @param PhpCookieManager $cookieManager
+     * @param CookieMetadataFactory $cookieMetadataFactory
+     */
+    public function __construct(
+        PhpCookieManager $cookieManager,
+        CookieMetadataFactory $cookieMetadataFactory
+    ) {
+        $this->cookieManager = $cookieManager;
+        $this->cookieMetadataFactory = $cookieMetadataFactory;
+    }
+
+    /**
+     * Delete frontend session cookie if customer session is expired
+     *
+     * @param Session $session
+     * @param bool $isLoggedIn
+     * @return bool
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function afterIsLoggedIn(Session $session, $isLoggedIn)
+    {
+        if (!$isLoggedIn) {
+            $metadata = $this->cookieMetadataFactory->createCookieMetadata();
+            $metadata->setPath('/');
+            $this->cookieManager->deleteCookie('mage-cache-sessid', $metadata);
+        }
+        return $isLoggedIn;
+    }
+}
