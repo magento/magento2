1c56a5cded97916dc98b655e7936698fb5fbcde1
oradchenko <oradchenko@magento.com>
2016-05-12 18:20:15 +0300

    MAGETWO-52230: Product doesn't appear on storefront when indexers updated by schedule

---
+++ app/code/Magento/CatalogSearch/Model/Indexer/Fulltext/Processor.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\CatalogSearch\Model\Indexer\Fulltext;
+
+use Magento\Framework\Indexer\AbstractProcessor;
+use Magento\CatalogSearch\Model\Indexer\Fulltext;
+
+class Processor extends AbstractProcessor
+{
+    /**
+     * Indexer ID
+     */
+    const INDEXER_ID = Fulltext::INDEXER_ID; 
+}
