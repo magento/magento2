d503b27b625664b51c8b694daad350a3a383cd02
Maddy Chellathurai <mchellathura@magento.com>
2016-05-16 14:42:59 -0500

    MAGETWO-52000: [Github][PR]impossible to see what is wrong with cron - unhelpful error message #3189
    - Using OM to create TImeZoneProvider.

--- setup/src/Magento/Setup/Model/PackagesData.php
+++ setup/src/Magento/Setup/Model/PackagesData.php
@@ -41,11 +41,6 @@ class PackagesData
     private $packagesAuth;
 
     /**
-     * @var \Magento\Setup\Model\DateTime\TimeZoneProvider
-     */
-    private $timeZoneProvider;
-
-    /**
      * @var  \Magento\Setup\Model\ObjectManagerProvider
      */
     private $objectManagerProvider;
@@ -54,21 +49,18 @@ class PackagesData
      * PackagesData constructor.
      *
      * @param \Magento\Framework\Composer\ComposerInformation $composerInformation,
-     * @param \Magento\Setup\Model\DateTime\TimeZoneProvider $timeZoneProvider,
      * @param \Magento\Setup\Model\PackagesAuth $packagesAuth,
      * @param \Magento\Framework\Filesystem $filesystem,
      * @param \Magento\Setup\Model\ObjectManagerProvider $objectManagerProvider
      */
     public function __construct(
         \Magento\Framework\Composer\ComposerInformation $composerInformation,
-        \Magento\Setup\Model\DateTime\TimeZoneProvider $timeZoneProvider,
         \Magento\Setup\Model\PackagesAuth $packagesAuth,
         \Magento\Framework\Filesystem $filesystem,
         \Magento\Setup\Model\ObjectManagerProvider $objectManagerProvider
     ) {
         $this->objectManagerProvider = $objectManagerProvider;
         $this->composerInformation = $composerInformation;
-        $this->timeZoneProvider = $timeZoneProvider;
         $this->packagesAuth = $packagesAuth;
         $this->filesystem = $filesystem;
     }
@@ -133,7 +125,9 @@ class PackagesData
      */
     private function formatSyncDate($syncDate)
     {
-        $timezone = $this->timeZoneProvider->get();
+        $objectManager = $this->objectManagerProvider->get();
+        $timeZoneProvider = $objectManager->get('Magento\Setup\Model\DateTime\TimeZoneProvider');
+        $timezone = $timeZoneProvider->get();
         return [
             'date' => $timezone->formatDateTime(
                 new \DateTime('@'.$syncDate),
