ecdbabb95f6f3fddd10ff843dcaccb9328377764
Sviatoslav Mankivskyi <smankivskyi@magento.com>
2016-05-16 16:05:13 +0300

    MAGETWO-46014: delayed error messages

--- app/code/Magento/Theme/view/frontend/web/js/view/messages.js
+++ app/code/Magento/Theme/view/frontend/web/js/view/messages.js
@@ -3,16 +3,24 @@
  * See COPYING.txt for license details.
  */
 define([
+    'jquery',
     'uiComponent',
-    'Magento_Customer/js/customer-data'
-], function (Component, customerData) {
+    'Magento_Customer/js/customer-data',
+    'jquery/jquery-storageapi'
+], function ($, Component, customerData) {
     'use strict';
 
     return Component.extend({
+        defaults: {
+            cookieMessages: [],
+            messages: []
+        },
         initialize: function () {
             this._super();
 
+            this.cookieMessages = $.cookieStorage.get('mage-messages');
             this.messages = customerData.get('messages').extend({disposableCustomerData: 'messages'});
+            $.cookieStorage.setConf({path: '/', expires: -1}).set('mage-messages', null);
         }
     });
 });
