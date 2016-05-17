defa36dcadb7ee42b6faa76d6b2a0d1cd0c5cf07
Anton Kaplya <anton.kaplya@magento.com>
2016-05-17 13:15:53 +0300

    MAGETWO-52836: Enable EAV attributes caching

--- app/code/Magento/Eav/Model/ResourceModel/Entity/Attribute.php
+++ app/code/Magento/Eav/Model/ResourceModel/Entity/Attribute.php
@@ -216,6 +216,19 @@ class Attribute extends \Magento\Framework\Model\ResourceModel\Db\AbstractDb
     }
 
     /**
+     * Perform actions after object delete
+     *
+     * @param \Magento\Framework\Model\AbstractModel|\Magento\Framework\DataObject $object
+     * @return $this
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    protected function _afterDelete(\Magento\Framework\Model\AbstractModel $object)
+    {
+        $this->getConfig()->clear();
+        $this->getAttributeCache()->clear();
+        return $this;
+    }
+    /**
      * @return AttributeCache
      * @deprecated
      */
