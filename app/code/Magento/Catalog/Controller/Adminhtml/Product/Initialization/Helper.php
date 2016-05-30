edcf87c0574391859f1adca7ec66824411b13e0f
aakimov <aakimov@magento.com>
2016-05-30 13:02:17 +0300

    MAGETWO-53111: Duplication of "Use Config Settings" checkbox for some options

--- app/code/Magento/Catalog/Controller/Adminhtml/Product/Initialization/Helper.php
+++ app/code/Magento/Catalog/Controller/Adminhtml/Product/Initialization/Helper.php
@@ -182,6 +182,10 @@ class Helper
         foreach ($useDefaults as $attributeCode => $useDefaultState) {
             if ($useDefaultState) {
                 $product->setData($attributeCode, null);
+                // UI component sends value even if field is disabled, so 'Use Config Settings' must be reset to false
+                if ($product->hasData('use_config_' . $attributeCode)) {
+                    $product->setData('use_config_' . $attributeCode, false);
+                }
             }
         }
 
@@ -219,7 +223,7 @@ class Helper
 
         return $product;
     }
-    
+
     /**
      * Initialize product before saving
      *
@@ -314,7 +318,7 @@ class Helper
         if (!is_array($overwriteOptions)) {
             return $productOptions;
         }
-        
+
         foreach ($productOptions as $index => $option) {
             $optionId = $option['option_id'];
 
