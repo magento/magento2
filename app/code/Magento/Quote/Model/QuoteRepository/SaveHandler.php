52d00f3e2ae9a54e9eec01c77631196e36fa960c
Serhiy Shkolyarenko <sshkolyarenko@magento.com>
2016-06-02 09:48:19 +0300

    MAGETWO-39117: Updating qty that exceeds the qty in stock for registered customer by API should return error
    refactored as factory

--- app/code/Magento/Quote/Model/QuoteRepository/SaveHandler.php
+++ app/code/Magento/Quote/Model/QuoteRepository/SaveHandler.php
@@ -5,9 +5,10 @@
  */
 namespace Magento\Quote\Model\QuoteRepository;
 
+use Magento\Framework\App\ObjectManager;
 use Magento\Quote\Api\Data\CartInterface;
 use Magento\Framework\Exception\InputException;
-use Magento\Framework\Exception\CouldNotSaveMultiMessageException;
+use Magento\Framework\Exception\CouldNotSaveException;
 
 class SaveHandler
 {
@@ -30,6 +31,11 @@ class SaveHandler
      * @var \Magento\Quote\Model\Quote\ShippingAssignment\ShippingAssignmentPersister
      */
     private $shippingAssignmentPersister;
+
+    /**
+     * @var \Magento\Framework\Message\PhraseFactory
+     */
+    private $phraseFactory;
 
     /**
      * @param \Magento\Quote\Model\ResourceModel\Quote $quoteResource
@@ -73,7 +79,10 @@ class SaveHandler
         }
         $errors = $quote->getErrors();
         if (!empty($errors)) {
-            throw new CouldNotSaveMultiMessageException('Following errors occurred on save: %1', $errors);
+            $phraseFactory = $this->getPhraseFactory();
+            throw new CouldNotSaveException(
+                $phraseFactory->create('Following errors occurred on save: %1', $errors)
+            );
         }
 
         // Billing Address processing
@@ -86,6 +95,18 @@ class SaveHandler
 
         $this->quoteResourceModel->save($quote->collectTotals());
         return $quote;
+    }
+
+    /**
+     * @deprecated
+     * @return \Magento\Framework\Message\PhraseFactory
+     */
+    private function getPhraseFactory()
+    {
+        if (!is_object($this->phraseFactory)) {
+            $this->phraseFactory = ObjectManager::getInstance()->get(\Magento\Framework\Message\PhraseFactory::class);
+        }
+        return $this->phraseFactory;
     }
 
     /**
