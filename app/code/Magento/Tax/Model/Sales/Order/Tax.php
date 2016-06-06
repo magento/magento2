df74e8a27610d5fb2071b4e383a78346a94a066c
Partica, Cristian <cpartica@magento.com>
2016-06-06 17:51:13 -0500

    MAGETWO-53366: Tax Report does not display any records
    - modifying logic for converter empty arrays
    - fix indentation for a constant

--- app/code/Magento/Tax/Model/Sales/Order/Tax.php
+++ app/code/Magento/Tax/Model/Sales/Order/Tax.php
@@ -31,7 +31,7 @@ class Tax extends \Magento\Framework\Model\AbstractExtensibleModel implements
     const KEY_PERCENT     = 'percent';
     const KEY_AMOUNT      = 'amount';
     const KEY_BASE_AMOUNT = 'base_amount';
-    const KEY_RATES = 'rates';
+    const KEY_RATES       = 'rates';
     /**#@-*/
 
     /**
