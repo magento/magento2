267471d2d36f15ac3ebd293cb646d246dd4505c6
Shmyheliuk, Oleksandr <oleksandr.shmyheliuk@gmail.com>
2016-06-09 16:52:34 +0300

    MAGETWO-47654 \Magento\Test\Integrity\DependencyTest is not analyzing di.xml

---
+++ dev/tests/static/framework/Magento/TestFramework/Dependency/DiRule.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Rule for searching php file dependency
+ *
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\TestFramework\Dependency;
+
+use DOMDocument;
+use DOMXPath;
+use Magento\Framework\App\Utility\Files;
+
+class DiRule implements RuleInterface
+{
+
+    /**
+     * Gets alien dependencies information for current module by analyzing file's contents
+     *
+     * @param string $currentModule
+     * @param string $fileType
+     * @param string $file
+     * @param string $contents
+     * @return array
+     */
+    public function getDependencyInfo($currentModule, $fileType, $file, &$contents)
+    {
+        if (pathinfo($file, PATHINFO_BASENAME) !== 'di.xml') {
+            return [];
+        }
+
+        $pattern = '~\b(?<class>(?<module>(' . implode(
+                '_|',
+                Files::init()->getNamespaces()
+        ) . '[_\\\\])[a-zA-Z0-9]+)[a-zA-Z0-9_\\\\]*)\b~';
+
+        $dependenciesInfo = [];
+        foreach ($this->fetchPossibleDependencies($file) as $possibleDependency) {
+            if (preg_match($pattern, $possibleDependency, $matches)) {
+                $referenceModule = str_replace('_', '\\', $matches['module']);
+                if ($currentModule == $referenceModule) {
+                    continue;
+                }
+                $dependenciesInfo[] = [
+                    'module' => $referenceModule,
+                    'type' => RuleInterface::TYPE_HARD,
+                    'source' => $matches['class'],
+                ];
+            }
+        }
+        return $dependenciesInfo;
+    }
+
+    /**
+     * @param string $file
+     * @return array
+     */
+    private function fetchPossibleDependencies($file)
+    {
+        $possibleDependencies = [];
+        $doc = new DOMDocument();
+        $doc->loadXML(file_get_contents($file));
+
+        $typeNodes = $doc->getElementsByTagName('type');
+        /** @var \DOMElement $type */
+        foreach ($typeNodes as $type) {
+            $possibleDependencies[] = $type->getAttribute('name');
+        }
+
+        $xpath = new DOMXPath($doc);
+        $textNodes = $xpath->query('//*[contains(text(),\'Magento\')]');
+        /** @var \DOMElement $argument */
+        foreach ($textNodes as $node) {
+            $possibleDependencies[] = $node->nodeValue;
+        }
+        return $possibleDependencies;
+    }
+}
