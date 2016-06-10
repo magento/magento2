99f424fdd19f66d7ef99eff7b341d183b7f5269f
Yuri Kovsher <ikovsher@magento.com>
2016-06-10 13:20:53 +0300

    MAGETWO-50975: Fix functional test Magento\Cms\Test\TestCase\CreateCmsPageEntityTest

--- dev/tests/functional/lib/Magento/Mtf/Client/Element/MultiselectgrouplistElement.php
+++ dev/tests/functional/lib/Magento/Mtf/Client/Element/MultiselectgrouplistElement.php
@@ -324,4 +324,16 @@ class MultiselectgrouplistElement extends MultiselectElement
 
         return $options;
     }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function deselectAll() {
+        $options = $this->getSelectedOptions();
+
+        /** @var SimpleElement $option */
+        foreach ($options as $option) {
+            $option->click();
+        }
+    }
 }
