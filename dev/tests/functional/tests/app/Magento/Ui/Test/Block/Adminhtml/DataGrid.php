5f2761bbffed3fd77ff7a82b4930caa01da5fd8d
Kateryna Muntianu <kmuntianu@magento.com>
2016-06-11 18:09:07 +0300

    MAGETWO-54282: [FT] Improve error reporting on Bamboo #2

--- dev/tests/functional/tests/app/Magento/Ui/Test/Block/Adminhtml/DataGrid.php
+++ dev/tests/functional/tests/app/Magento/Ui/Test/Block/Adminhtml/DataGrid.php
@@ -300,9 +300,10 @@ class DataGrid extends Grid
             $massActionList = '(' . $this->massActionToggleList . ")[$i]";
             if ($this->_rootElement->find($massActionButton, Locator::SELECTOR_XPATH)->isVisible()) {
                 $this->_rootElement->find($massActionButton, Locator::SELECTOR_XPATH)->click();
-                $this->_rootElement
-                    ->find(sprintf($massActionList, $massActionSelection), Locator::SELECTOR_XPATH)
-                    ->click();
+                $massActionElement =
+                    $this->_rootElement->find(sprintf($massActionList, $massActionSelection), Locator::SELECTOR_XPATH);
+                $this->waitForElementVisible($massActionElement);
+                $massActionElement->click();
                 break;
             }
         }
