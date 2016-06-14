8ef2ec163e34e1c3e6f9236e8e4bbc7d95b0e9c7
Andrii Kasian <akasian@magento.com>
2016-06-14 09:52:06 +0300

    MAGETWO-52124: Simple product special price lost if use it in grouped product

---
+++ app/code/Magento/GroupedProduct/Model/ResourceModel/Product/Link/RelationPersister.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\GroupedProduct\Model\ResourceModel\Product\Link;
+
+use Magento\Catalog\Model\ProductLink\LinkFactory;
+use Magento\Catalog\Model\ResourceModel\Product\Link;
+use Magento\Catalog\Model\ResourceModel\Product\Relation;
+
+class RelationPersister
+{
+    /**
+     * @var Relation
+     */
+    private $relationProcessor;
+    /**
+     * @var LinkFactory
+     */
+    private $linkFactory;
+
+    /**
+     * @inheritDoc
+     */
+    public function __construct(Relation $relationProcessor, LinkFactory $linkFactory)
+    {
+        $this->relationProcessor = $relationProcessor;
+        $this->linkFactory = $linkFactory;
+    }
+
+    /**
+     * Save grouped products to product relation table
+     *
+     * @param Link $subject
+     * @param \Closure $proceed
+     * @param $parentId
+     * @param $data
+     * @param $typeId
+     * @return Link
+     */
+    public function aroundSaveProductLinks(Link $subject, \Closure $proceed, $parentId, $data, $typeId)
+    {
+        $result = $proceed($parentId, $data, $typeId);
+        if ($typeId == \Magento\GroupedProduct\Model\ResourceModel\Product\Link::LINK_TYPE_GROUPED) {
+            foreach ($data as $linkData) {
+                $this->relationProcessor->addRelation(
+                    $parentId,
+                    $linkData['product_id']
+                );
+            }
+        }
+        return $result;
+    }
+
+    /**
+     * Remove grouped products from product relation table
+     *
+     * @param Link $subject
+     * @param \Closure $proceed
+     * @param $linkId
+     * @return Link
+     */
+    public function aroundDeleteProductLink(Link $subject, \Closure $proceed, $linkId)
+    {
+        /** @var \Magento\Catalog\Model\ProductLink\Link $link */
+        $link = $this->linkFactory->create();
+        $subject->load($link, $linkId, $subject->getIdFieldName());
+        $result = $proceed($linkId);
+        if ($link->getLinkTypeId() == \Magento\GroupedProduct\Model\ResourceModel\Product\Link::LINK_TYPE_GROUPED) {
+            $this->relationProcessor->removeRelations(
+                $link->getProductId(),
+                $link->getLinkedProductId()
+            );
+        }
+        return $result;
+    }
+}
