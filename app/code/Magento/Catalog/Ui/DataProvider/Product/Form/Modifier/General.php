eeb0520e38389abfe7811c750a3ca88c9ef8959e
andimov <andimov@gmail.com>
2016-06-16 18:54:51 +0300

    MAGETWO-54226: Tier price information is not saved if count field left empty
     - remove intval for tier price qty

--- app/code/Magento/Catalog/Ui/DataProvider/Product/Form/Modifier/General.php
+++ app/code/Magento/Catalog/Ui/DataProvider/Product/Form/Modifier/General.php
@@ -105,7 +105,7 @@ class General extends AbstractModifier
                 $value[ProductAttributeInterface::CODE_TIER_PRICE_FIELD_PRICE] =
                     $this->formatPrice($value[ProductAttributeInterface::CODE_TIER_PRICE_FIELD_PRICE]);
                 $value[ProductAttributeInterface::CODE_TIER_PRICE_FIELD_PRICE_QTY] =
-                    $this->formatNumber((int)$value[ProductAttributeInterface::CODE_TIER_PRICE_FIELD_PRICE_QTY]);
+                    $this->formatNumber($value[ProductAttributeInterface::CODE_TIER_PRICE_FIELD_PRICE_QTY]);
             }
         }
 
