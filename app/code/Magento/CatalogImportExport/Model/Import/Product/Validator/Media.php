1ae41a01d323a4448b39dd6cd9649fd0c6a6d551
Anthony <anthony.goeury@gmail.com>
2016-06-17 14:23:15 +0200

    Fix multiple value separator for additional_images product import

--- app/code/Magento/CatalogImportExport/Model/Import/Product/Validator/Media.php
+++ app/code/Magento/CatalogImportExport/Model/Import/Product/Validator/Media.php
@@ -16,8 +16,6 @@ class Media extends AbstractImportValidator implements RowValidatorInterface
 
     const ADDITIONAL_IMAGES = 'additional_images';
 
-    const ADDITIONAL_IMAGES_DELIMITER = ',';
-
     /** @var array */
     protected $mediaAttributes = ['image', 'small_image', 'thumbnail'];
 
@@ -83,7 +81,7 @@ class Media extends AbstractImportValidator implements RowValidatorInterface
             }
         }
         if (isset($value[self::ADDITIONAL_IMAGES]) && strlen($value[self::ADDITIONAL_IMAGES])) {
-            foreach (explode(self::ADDITIONAL_IMAGES_DELIMITER, $value[self::ADDITIONAL_IMAGES]) as $image) {
+            foreach (explode($this->getMultipleValueSeparator(), $value[self::ADDITIONAL_IMAGES]) as $image) {
                 if (!$this->checkPath($image) && !$this->checkValidUrl($image)) {
                     $this->_addMessages(
                         [
@@ -99,5 +97,13 @@ class Media extends AbstractImportValidator implements RowValidatorInterface
             }
         }
         return $valid;
+    }
+    
+    /**
+     * @return string
+     */
+    protected function getMultipleValueSeparator()
+    {
+        return $this->context->getMultipleValueSeparator();
     }
 }
