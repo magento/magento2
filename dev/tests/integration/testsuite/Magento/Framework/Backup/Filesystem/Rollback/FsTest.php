ce907ab9e0de6955b00be946dbdc3d4b3c98d131
Oleksandr Iegorov <oiegorov@magento.com>
2016-06-29 14:54:23 +0300

    MAGETWO-44479: Rollback database and media backup is not functioning

---
+++ dev/tests/integration/testsuite/Magento/Framework/Backup/Filesystem/Rollback/FsTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* Test for \Magento\Framework\Backup\Filesystem\Rollback\Fs
+*
+* Copyright © 2016 Magento. All rights reserved.
+* See COPYING.txt for license details.
+*/
+
+use Magento\TestFramework\Helper\Bootstrap;
+
+class FsTest extends \PHPUnit_Framework_TestCase
+{
+
+}
