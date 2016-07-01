952534ca6db69072fd9db8aab1940727e26bfcca
Partica, Cristian <cpartica@magento.com>
2016-07-01 16:38:45 -0500

    MAGETWO-54779: [GitHub] Image size for Product Watermarks can't be set #5270
    - adding custom validation for AAAxBBB type of inputs in validation rules with translation

---
+++ app/code/Magento/Catalog/view/adminhtml/web/component/image-size-field.js
@@ -0,0 +1,57 @@
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+define([
+    'jquery',
+    'underscore',
+    'Magento_Ui/js/lib/validation/utils',
+    'Magento_Ui/js/form/element/abstract',
+    'Magento_Ui/js/lib/validation/validator'
+], function ($, _, utils, Abstract, validator) {
+    'use strict';
+
+    validator.addRule(
+        'validate-image-size-range',
+        function (value) {
+            var numValue,
+                dataAttrRange = /^(\d+)x(\d+)?$/,
+                result = false,
+                m;
+
+            if (utils.isEmptyNoTrim(value)) {
+                return true;
+            }
+            numValue = utils.parseNumber(value);
+
+            if (isNaN(numValue)) {
+                return false;
+            }
+
+            m = dataAttrRange.exec(value);
+
+            if (m) {
+                if (m.length === 3) {
+                    result = m[1] && m[2];
+                }
+            }
+
+            return result;
+        },
+        $.mage.__('The value is not within the specified format eg: 200x300')
+    );
+
+    return Abstract.extend({
+
+        /**
+         * Checks for relevant value
+         *
+         * @param {*} value
+         * @returns {Boolean}
+         */
+        isRangeCorrect: function (value) {
+            return validator('validate-image-size-range', value);
+        }
+    });
+});
