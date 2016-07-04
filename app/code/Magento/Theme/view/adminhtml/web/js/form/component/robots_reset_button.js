55f41385237db25f301af3ed67c2f3574be40bd8
Sergii Kovalenko <skovalenko@magento.com>
2016-07-04 12:55:16 +0300

    MAGETWO-54790: [Github] Sections missing from Stores > Configuration > General > Design #5256

---
+++ app/code/Magento/Theme/view/adminhtml/web/js/form/component/robots_reset_button.js
@@ -0,0 +1,40 @@
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+define([
+    'Magento_Ui/js/form/components/button',
+    'uiRegistry',
+    'uiLayout',
+    'mageUtils'
+], function (Button, registry, layout, utils) {
+    'use strict';
+
+    return Button.extend({
+        defaults: {
+            customInstructionField: '${ $.parentName }.custom_instructions',
+            label: '',
+            buttonTpl: 'ui/form/element/button'
+        },
+
+        /**
+         * @param {String} json
+         * @private
+         */
+        _parseJson: function (json) {
+            return JSON.parse(json);
+        },
+
+        /**
+         * @param {String} defaultRobotsTxt
+         */
+        reset: function (defaultRobotsTxt) {
+            var customInstructions = registry.get(this.customInstructionField);
+
+            if (customInstructions) {
+                customInstructions.set('value', this._parseJson(defaultRobotsTxt));
+            }
+        }
+    });
+});
