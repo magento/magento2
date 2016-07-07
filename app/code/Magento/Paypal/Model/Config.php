88afb0171960f16820f1c309d60f729acfe8d9f1
Roman Liukshyn <rliukshyn@magento.com>
2016-07-07 19:14:55 +0300

    MAGETWO-51534: Updated Discover and Maestro/Mastercard BINs
    - Added Diners to accepted cc types for payflow pro

--- app/code/Magento/Paypal/Model/Config.php
+++ app/code/Magento/Paypal/Model/Config.php
@@ -1310,7 +1310,7 @@ class Config extends AbstractConfig
      */
     public function getPayflowproCcTypesAsOptionArray()
     {
-        return $this->_cctypeFactory->create()->setAllowedTypes(['AE', 'VI', 'MC', 'JCB', 'DI'])->toOptionArray();
+        return $this->_cctypeFactory->create()->setAllowedTypes(['AE', 'VI', 'MC', 'JCB', 'DI', 'DN'])->toOptionArray();
     }
 
     /**
