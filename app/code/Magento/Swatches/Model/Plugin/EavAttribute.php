ce6045320cd658e0c3f458e5743333e23f026386
Sergey Shvets <sshvets@magento.com>
2016-07-08 17:28:27 +0300

    MAGETWO-54648: [GitHub] No Swatch Input for Admin Scope. No Fallback to Admin Scope #5143 #5142

--- app/code/Magento/Swatches/Model/Plugin/EavAttribute.php
+++ app/code/Magento/Swatches/Model/Plugin/EavAttribute.php
@@ -277,7 +277,11 @@ class EavAttribute
                     //option was deleted by button with basket
                     continue;
                 }
+                $defaultSwatchValue = reset($storeValues);
                 foreach ($storeValues as $storeId => $value) {
+                    if (!$value) {
+                        $value = $defaultSwatchValue;
+                    }
                     $swatch = $this->loadSwatchIfExists($optionId, $storeId);
                     $swatch->isDeleted($isOptionForDelete);
                     $this->saveSwatchData(
