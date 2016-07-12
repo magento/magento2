7cbb257d4d995ffeb6cdb67adc4433149eb6615d
Bohdan Korablov <bkorablov@magento.com>
2016-07-12 17:13:46 +0300

    MAGETWO-55272: Cover with Functional Tests

---
+++ dev/tests/functional/tests/app/Magento/Setup/Test/Block/ExtensionsGrid.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Setup\Test\Block;
+
+use Magento\Mtf\Block\Block;
+use Magento\Mtf\Client\Locator;
+
+/**
+ * Extensions Grid block.
+ */
+class ExtensionsGrid extends Block
+{
+    /**
+     * @var string
+     */
+    protected $installButton = "//div[contains(@class, 'item-install')]//button[contains(@href,'#install-extension-grid')]";
+
+    /**
+     * @var string
+     */
+    protected $popupLoading = '.popup popup-loading';
+
+    /**
+     * Click to 'Install' button
+     *
+     * @return void
+     */
+    public function clickInstallButton()
+    {
+        $this->waitForElementNotVisible($this->popupLoading);
+        $this->waitForElementVisible($this->installButton, Locator::SELECTOR_XPATH);
+        $this->_rootElement->find($this->installButton, Locator::SELECTOR_XPATH)->click();
+    }
+}
---
+++ dev/tests/functional/tests/app/Magento/Setup/Test/TestCase/InstallExtensionTest.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Setup\Test\TestCase;
+
+use Magento\Mtf\TestCase\Injectable;
+use Magento\Setup\Test\Page\Adminhtml\SetupWizard;
+use Magento\Backend\Test\Page\Adminhtml\Dashboard;
+use Magento\Mtf\Fixture\FixtureFactory;
+use Magento\Setup\Test\Fixture\InstallExtension;
+
+class InstallExtensionTest extends Injectable
+{
+    /**
+     * Page System Upgrade Index.
+     *
+     * @var SetupWizard
+     */
+    protected $setupWizard;
+
+    /**
+     * @var Dashboard
+     */
+    protected $adminDashboard;
+
+    /**
+     * Injection data.
+     *
+     * @param Dashboard $adminDashboard
+     * @param SetupWizard $setupWizard
+     * @return void
+     */
+    public function __inject(
+        Dashboard $adminDashboard,
+        SetupWizard $setupWizard
+    ) {
+        $this->adminDashboard = $adminDashboard;
+        $this->setupWizard = $setupWizard;
+    }
+
+    public function test(
+        FixtureFactory $fixtureFactory,
+        $installExtension = []
+    ) {
+        // Create fixture
+        $installExtensionFixture = $fixtureFactory->create(InstallExtension::class, ['data' => $installExtension]);
+
+        // Authenticate in admin area
+        $this->adminDashboard->open();
+
+        // Open Web Setup Wizard
+        $this->setupWizard->open();
+
+        $this->setupWizard->getSetupHome()->clickComponentManager();
+        $this->setupWizard->getExtensionsGrid()->clickInstallButton();
+    }
+}
