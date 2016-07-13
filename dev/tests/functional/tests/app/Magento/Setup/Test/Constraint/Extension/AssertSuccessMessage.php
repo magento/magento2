6c786f121dce5af4debfb097d5b57e019c46c6e1
Bohdan Korablov <bkorablov@magento.com>
2016-07-13 18:57:08 +0300

    MAGETWO-55272: Cover with Functional Tests

---
+++ dev/tests/functional/tests/app/Magento/Setup/Test/Constraint/Extension/AssertSuccessMessage.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Setup\Test\Constraint\Extension;
+
+use Magento\Setup\Test\Page\Adminhtml\SetupWizard;
+use Magento\Mtf\Constraint\AbstractConstraint;
+
+/**
+ * Check extension installing is successfully
+ */
+class AssertSuccessMessage extends AbstractConstraint
+{
+    /**
+     * Assert extension installing is successfully
+     *
+     * @param SetupWizard $setupWizard
+     * @param string $extension
+     * @return void
+     */
+    public function processAssert(SetupWizard $setupWizard, $extension)
+    {
+        $message = "You installed:";
+        \PHPUnit_Framework_Assert::assertContains(
+            $message,
+            $setupWizard->getSuccessMessage()->getUpdaterStatus(),
+            'Success message is incorrect.'
+        );
+        \PHPUnit_Framework_Assert::assertContains(
+            $extension,
+            $setupWizard->getSuccessMessage()->getUpdaterStatus(),
+            'Installed extension is incorrect.'
+        );
+    }
+
+    /**
+     * Returns a string representation of successful assertion.
+     *
+     * @return string
+     */
+    public function toString()
+    {
+        return "Install Extension success message is correct.";
+    }
+}
--- dev/tests/functional/tests/app/Magento/Setup/Test/TestCase/InstallExtensionTest.php
+++ dev/tests/functional/tests/app/Magento/Setup/Test/TestCase/InstallExtensionTest.php
@@ -11,7 +11,11 @@ use Magento\Setup\Test\Page\Adminhtml\SetupWizard;
 use Magento\Backend\Test\Page\Adminhtml\Dashboard;
 use Magento\Mtf\Fixture\FixtureFactory;
 use Magento\Setup\Test\Fixture\InstallExtension;
-use Magento\Setup\Test\Constraint\AssertFindExtensionOnGrid;
+use Magento\Setup\Test\Constraint\Extension\AssertFindExtensionOnGrid;
+use Magento\Setup\Test\Constraint\Extension\AssertSuccessMessage;
+use Magento\Setup\Test\Constraint\Extension\AssertExtensionAndVersionCheck;
+use Magento\Setup\Test\Constraint\AssertSuccessfulReadinessCheck;
+use Magento\Setup\Test\Block\Extension\AbstractGrid;
 
 class InstallExtensionTest extends Injectable
 {
@@ -45,10 +49,22 @@ class InstallExtensionTest extends Injectable
     public function test(
         FixtureFactory $fixtureFactory,
         AssertFindExtensionOnGrid $assertFindExtensionOnGrid,
+        AssertSuccessfulReadinessCheck $assertReadiness,
+        AssertExtensionAndVersionCheck $assertExtensionAndVersionCheck,
+        AssertSuccessMessage $assertSuccessMessage,
         $installExtension = []
     ) {
-        // Create fixture
-        $installExtensionFixture = $fixtureFactory->create(InstallExtension::class, ['data' => $installExtension]);
+        $createBackupConfig = array_intersect_key(
+            $installExtension,
+            ['optionsCode' => '', 'optionsMedia' => '', 'optionsDb' => '']
+        );
+        $createBackupFixture = $fixtureFactory->create(
+            InstallExtension::class,
+            ['data' => $createBackupConfig]
+        );
+
+        $extension = $installExtension['extension'];
+        $version = $installExtension['version'];
 
         // Authenticate in admin area
         $this->adminDashboard->open();
@@ -56,24 +72,47 @@ class InstallExtensionTest extends Injectable
         // Open Web Setup Wizard
         $this->setupWizard->open();
 
+        // Open Extension Grid with extensions to install
         $this->setupWizard->getSetupHome()->clickComponentManager();
         $this->setupWizard->getExtensionsGrid()->clickInstallButton();
 
-        $this->findExtensionOnGrid('magento/sample-data-media');
-        $assertFindExtensionOnGrid->processAssert($this->setupWizard, 'magento/sample-data-media');
-        $this->setupWizard->getExtensionsInstallGrid()->clickInstall('magento/sample-data-media');
+        // Find extension on grid and click install
+        $this->findExtensionOnGrid($this->setupWizard->getExtensionsInstallGrid(), $extension);
+        $assertFindExtensionOnGrid->processAssert($this->setupWizard->getExtensionsInstallGrid(), $extension);
+        $this->setupWizard->getExtensionsInstallGrid()->chooseExtensionVersion($extension, $version);
+        $this->setupWizard->getExtensionsInstallGrid()->clickInstall($extension);
+
+        // Readiness Check
+        $this->setupWizard->getReadiness()->clickReadinessCheck();
+        $assertReadiness->processAssert($this->setupWizard);
+        $this->setupWizard->getReadiness()->clickNext();
+
+        // Create Backup page
+        $this->setupWizard->getCreateBackup()->fill($createBackupFixture);
+        $this->setupWizard->getCreateBackup()->clickNext();
+
+        // Install Extension
+        $assertExtensionAndVersionCheck->processAssert($this->setupWizard, $extension, '100.1.0-rc3');
+        $this->setupWizard->getInstallExtension()->clickInstallButton();
+        $assertSuccessMessage->processAssert($this->setupWizard, $extension);
+
+        // Open Extension Grid with installed extensions and find installed extension
+        $this->setupWizard->open();
+        $this->setupWizard->getSetupHome()->clickComponentManager();
+        $this->findExtensionOnGrid($this->setupWizard->getExtensionsGrid(), $extension);
+        $assertFindExtensionOnGrid->processAssert($this->setupWizard->getExtensionsGrid(), $extension);
     }
 
     /**
      * Find Extension on the grid by name
      *
+     * @param AbstractGrid $grid
      * @param string $name
      */
-    protected function findExtensionOnGrid($name)
+    protected function findExtensionOnGrid(AbstractGrid $grid, $name)
     {
         while (true) {
-            if ($this->setupWizard->getExtensionsInstallGrid()->isExtensionOnGrid($name)
-                || !$this->setupWizard->getExtensionsInstallGrid()->clickNextPageButton()) {
+            if ($grid->isExtensionOnGrid($name) || !$grid->clickNextPageButton()) {
                 break;
             }
         }
