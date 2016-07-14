03bef70097f0d710d8687338a02fad885c393b10
Alexander Paliarush <apaliarush@magento.com>
2016-07-14 10:15:20 -0500

    MAGETWO-53302: Error on executing messagequeue_clean_outdated_locks job

--- Test/Unit/Model/ResourceModel/LockTest.php
+++ Test/Unit/Model/ResourceModel/LockTest.php
@@ -3,7 +3,7 @@
  * Copyright © 2016 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\MessageQueue\Test\Unit\Model;
+namespace Magento\MessageQueue\Test\Unit\ResourceModel;
 
 use Magento\Framework\App\ResourceConnection;
 use Magento\Framework\DB\Adapter\AdapterInterface;
