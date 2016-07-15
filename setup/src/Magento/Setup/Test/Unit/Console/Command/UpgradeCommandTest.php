ca1de66b76e440e38ec8122dccb22abeec018e6c
Ihor Sytnykov <isitnikov@magento.com>
2016-07-15 11:27:31 +0300

    MAGETWO-55357: Portdown MAGETWO-53777 down to M2.1.x branch

--- setup/src/Magento/Setup/Test/Unit/Console/Command/UpgradeCommandTest.php
+++ setup/src/Magento/Setup/Test/Unit/Console/Command/UpgradeCommandTest.php
@@ -14,12 +14,13 @@ class UpgradeCommandTest extends \PHPUnit_Framework_TestCase
     public function testExecute()
     {
         $installerFactory = $this->getMock('Magento\Setup\Model\InstallerFactory', [], [], '', false);
+        $objectManagerProvider = $this->getMock('\Magento\Setup\Model\ObjectManagerProvider', [], [], '', false);
         $installer = $this->getMock('Magento\Setup\Model\Installer', [], [], '', false);
         $installer->expects($this->at(0))->method('updateModulesSequence');
         $installer->expects($this->at(1))->method('installSchema');
         $installer->expects($this->at(2))->method('installDataFixtures');
         $installerFactory->expects($this->once())->method('create')->willReturn($installer);
-        $commandTester = new CommandTester(new UpgradeCommand($installerFactory));
+        $commandTester = new CommandTester(new UpgradeCommand($installerFactory, $objectManagerProvider));
         $this->assertSame(Cli::RETURN_SUCCESS, $commandTester->execute([]));
     }
 }
