1d4873bf8457afa860e823dcbf1df17894cae318
Yuriy Denyshchenko <ydenyshchenk@magento.com>
2016-07-19 13:42:43 +0300

    MAGETWO-55598: Portdown MAGETWO-54787 down to M2.1.x branch

--- app/code/Magento/Sales/Setup/UpgradeSchema.php
+++ app/code/Magento/Sales/Setup/UpgradeSchema.php
@@ -7,6 +7,7 @@ namespace Magento\Sales\Setup;
 use Magento\Framework\Setup\UpgradeSchemaInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\DB\Adapter\AdapterInterface;
 
 /**
  * @codeCoverageIgnore
@@ -53,5 +54,43 @@ class UpgradeSchema implements UpgradeSchemaInterface
 
             $installer->endSetup();
         }
+        if (version_compare($context->getVersion(), '2.0.3', '<')) {
+            $this->addColumnBaseGrandTotal($installer);
+            $this->addIndexBaseGrandTotal($installer);
+        }
+    }
+
+    /**
+     * @param SchemaSetupInterface $installer
+     * @return void
+     */
+    private function addColumnBaseGrandTotal(SchemaSetupInterface $installer)
+    {
+        $connection = $installer->getConnection();
+        $connection->addColumn(
+            $installer->getTable('sales_invoice_grid'),
+            'base_grand_total',
+            [
+                'type' => \Magento\Framework\DB\Ddl\Table::TYPE_DECIMAL,
+                'nullable' => true,
+                'length' => '12,4',
+                'comment' => 'Base Grand Total',
+                'after' => 'grand_total'
+            ]
+        );
+    }
+
+    /**
+     * @param SchemaSetupInterface $installer
+     * @return void
+     */
+    private function addIndexBaseGrandTotal(SchemaSetupInterface $installer)
+    {
+        $connection = $installer->getConnection();
+        $connection->addIndex(
+            $installer->getTable('sales_invoice_grid'),
+            $installer->getIdxName('sales_invoice_grid', ['base_grand_total']),
+            ['base_grand_total']
+        );
     }
 }
