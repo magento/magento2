9c119e587b5eea890ea84aa8840b62746e942514
Iryna Lagno <ilagno@magento.com>
2016-07-20 08:57:32 +0300

    MAGETWO-52821: Asynchronous Operations Framework
    - fix static test

--- dev/tests/static/framework/Magento/TestFramework/Inspection/WordsFinder.php
+++ dev/tests/static/framework/Magento/TestFramework/Inspection/WordsFinder.php
@@ -87,6 +87,13 @@ class WordsFinder
     protected $componentRegistrar;
 
     /**
+     * Map of phrase to exclude from the file content
+     * 
+     * @var  array
+     */
+    private $exclude = [];
+
+    /**
      * @param string|array $configFiles
      * @param string $baseDir
      * @param \Magento\Framework\Component\ComponentRegistrar $componentRegistrar
@@ -177,6 +184,7 @@ class WordsFinder
     {
         // Load whitelist entries
         $whitelist = [];
+        $exclude = [];
         $nodes = $configXml->xpath('//config/whitelist/item');
         foreach ($nodes as $node) {
             $path = $node->xpath('path');
@@ -203,8 +211,21 @@ class WordsFinder
                     $words[] = (string)$wordNode;
                 }
             }
-
             $whitelist[$path] = $words;
+            
+            $excludeNodes = $node->xpath('exclude');
+            $excludes = [];
+            if ($excludeNodes) {
+                foreach ($excludeNodes as $extractNode) {
+                    $excludes[] = (string)$extractNode;
+                }
+            }
+            
+            if (isset($exclude[$path])) {
+                $exclude[$path] = array_merge($excludes, $exclude[$path]) ;
+            } else {
+                $exclude[$path] = $excludes;
+            }
         }
 
         // Merge with already present whitelist
@@ -214,7 +235,13 @@ class WordsFinder
             }
             $this->_whitelist[$newPath] = array_unique($newWords);
         }
-
+        
+        foreach ($exclude as $newPath => $newWords) {
+            if (isset($this->exclude[$newPath])) {
+                $newWords = array_merge($this->exclude[$newPath], $newWords);
+            }
+            $this->exclude[$newPath] = array_unique($newWords);
+        }
         return $this;
     }
 
@@ -260,7 +287,10 @@ class WordsFinder
         $checkContents = !$this->_isBinaryFile($file);
         $path = $this->getSearchablePath($file);
         $contents = $checkContents ? file_get_contents($file) : '';
-
+        if (isset($this->exclude[$file]) && !empty($this->exclude[$file])) {
+            foreach ($this->exclude[$file] as $stringToEliminate)
+                $contents = str_replace($stringToEliminate, "", $contents);
+        }
         $foundWords = [];
         foreach ($this->_words as $word) {
             if (stripos($path, $word) !== false || stripos($contents, $word) !== false) {
