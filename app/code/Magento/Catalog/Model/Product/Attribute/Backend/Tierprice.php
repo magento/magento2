e93256150f4d878d9da8733d62429b3d4bdf9971
Valeriy Nayda <vnayda@magento.com>
2016-07-21 14:26:24 +0300

    MAGETWO-55261: Update Advanced Pricing Popup on product page
    - tierprice fixes

--- app/code/Magento/Catalog/Model/Product/Attribute/Backend/Tierprice.php
+++ app/code/Magento/Catalog/Model/Product/Attribute/Backend/Tierprice.php
@@ -109,20 +109,6 @@ class Tierprice extends \Magento\Catalog\Model\Product\Attribute\Backend\GroupPr
     /**
      * @inheritdoc
      */
-    protected function modifyPriceData($object, $data)
-    {
-        foreach ($data as $key => $tierPrice) {
-            if ($tierPrice['percentage_value'] > 0) {
-                $data[$key]['price'] = $object->getPrice() * (1 - $tierPrice['percentage_value'] / 100);
-            }
-        }
-
-        return parent::modifyPriceData($object, $data);
-    }
-
-    /**
-     * @inheritdoc
-     */
     public function validate($object)
     {
         $attribute = $this->getAttribute();
