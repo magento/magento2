1a4c311d16ac9aa748df53db335d37c17de117d3
Vasyl Kozyrenko <vkozyrenko@magento.com>
2016-07-21 13:01:12 +0300

    MTA-3241: Catalog Widget: Extend existing functional automated tests

--- dev/tests/functional/tests/app/Magento/Widget/Test/Block/Adminhtml/Widget/Instance/Edit/Tab/Parameters.php
+++ dev/tests/functional/tests/app/Magento/Widget/Test/Block/Adminhtml/Widget/Instance/Edit/Tab/Parameters.php
@@ -61,7 +61,8 @@ class Parameters extends Tab
         $path = $this->path . str_replace(' ', '', $fields['code']) . '.php';
         $path = str_replace('\\', DIRECTORY_SEPARATOR, MTF_TESTS_PATH . $path);
         $paths = glob($path);
+        $path = str_replace([MTF_TESTS_PATH, '.php'], '', $paths[0]);
 
-        return str_replace([MTF_TESTS_PATH, '.php'], '', $paths[0]);
+        return str_replace('/', '\\', $path);
     }
 }
