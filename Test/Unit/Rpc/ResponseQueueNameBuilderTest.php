73e861c80ebf6fb744ff6ac500b748efc6030ca8
Oleksii Korshenko <okorshenko@magento.com>
2016-07-25 18:52:19 -0500

    MAGETWO-55497: Refactor clients to use v3 topology config

---
+++ Test/Unit/Rpc/ResponseQueueNameBuilderTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Framework\MessageQueue\Test\Unit\Rpc;
+
+use Magento\Framework\MessageQueue\Rpc\ResponseQueueNameBuilder;
+
+class ResponseQueueNameBuilderTest extends \PHPUnit_Framework_TestCase
+{
+   public function testGetQueueName()
+   {
+       $model = new ResponseQueueNameBuilder();
+       $this->assertEquals('responseQueue.topic.01', $model->getQueueName('topic.01'));
+   }
+}
