53768d993d3a630bdeecac4b513f0b1867bc8cad
Bohdan Korablov <bkorablov@magento.com>
2016-07-25 12:47:26 +0300

    MAGETWO-55694: Merge and stabilize Functional Tests

--- dev/tests/functional/tests/app/Magento/Setup/Test/Block/Extension/Grid.php
+++ dev/tests/functional/tests/app/Magento/Setup/Test/Block/Extension/Grid.php
@@ -134,7 +134,7 @@ class Grid extends AbstractGrid
         $this->_rootElement->waitUntil(
             function () {
                 //TODO: Sleep will be removed after MAGETWO-52137
-                sleep(3);
+                sleep(5);
                 $message = $this->_rootElement->find($this->notFoundMessage)->isVisible();
                 $grid = $this->_rootElement->find($this->dataGrid)->isVisible();
 
