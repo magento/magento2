7584d4afb8d71bb55e1016cd2c2f195b357c34ac
Alex Paliarush <apaliarush@magento.com>
2016-07-27 14:45:35 -0500

    MAGETWO-55498: Implement compatibility of v2 and v3 message queue configuration formats

--- Config/Publisher/ConfigReaderPlugin.php
+++ Config/Publisher/ConfigReaderPlugin.php
@@ -3,7 +3,7 @@
  * Copyright © 2016 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\Framework\MessageQueue\Config;
+namespace Magento\Framework\MessageQueue\Config\Publisher;
 
 use Magento\Framework\MessageQueue\ConfigInterface as QueueConfig;
 
@@ -12,7 +12,7 @@ use Magento\Framework\MessageQueue\ConfigInterface as QueueConfig;
  *
  * @deprecated
  */
-class PublisherConfigPlugin
+class ConfigReaderPlugin
 {
     /**
      * @var QueueConfig
@@ -32,24 +32,19 @@ class PublisherConfigPlugin
     /**
      * Read values from queue config and make them available via publisher config.
      *
-     * @param \Magento\Framework\MessageQueue\Publisher\Config\Data $subject
+     * @param \Magento\Framework\MessageQueue\Publisher\Config\CompositeReader $subject
      * @param \Closure $proceed
-     * @param string|null $path
-     * @param mixed|null $default
-     * @return mixed
+     * @param string|null $scope
+     * @return array
      *
      * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
-    public function aroundGet(
-        \Magento\Framework\MessageQueue\Publisher\Config\Data $subject,
+    public function aroundRead(
+        \Magento\Framework\MessageQueue\Publisher\Config\CompositeReader $subject,
         \Closure $proceed,
-        $path = null,
-        $default = null
+        $scope = null
     ) {
-        $publisherConfigData = $proceed($path, $default);
-        if ($path !== null || $default !== null) {
-            return $publisherConfigData;
-        }
+        $publisherConfigData = $proceed($scope);
         $publisherConfigDataFromQueueConfig = $this->getPublisherConfigDataFromQueueConfig();
         return array_merge($publisherConfigDataFromQueueConfig, $publisherConfigData);
     }
