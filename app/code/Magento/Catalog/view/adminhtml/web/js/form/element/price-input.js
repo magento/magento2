330c31968dded372f49996871da222d9e4a1a127
Anton Ohorodnyk <aohorodnyk@magento.com>
2016-08-01 16:21:43 +0300

    MAGETWO-49545: [JS] Adding a Field Shortcut to Calculate Price using %
     - Fixed mixin for keeping old validation

---
+++ app/code/Magento/Catalog/view/adminhtml/web/js/form/element/price-input.js
@@ -0,0 +1,23 @@
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+define([
+    'Magento_Ui/js/form/element/abstract'
+], function (Abstract) {
+    'use strict';
+
+    return Abstract.extend({
+        defaults: {
+            elementTmpl: 'Magento_Catalog/form/element/price-input'
+        },
+
+        /**
+         * Callback that fires when 'input' event is performed
+         *
+         * @param {Object} data
+         * @param {Object} event
+         */
+        onInput: function (data, event) {}
+    });
+});
