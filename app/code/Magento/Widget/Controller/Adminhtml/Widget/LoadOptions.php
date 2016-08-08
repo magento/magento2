0fcc919bff9f648596fcf506d98af5fc8294348d
Allan Paiste <allan.paiste@vaimo.com>
2016-08-08 02:36:30 +0200

    Test coverage increased on LoadOptions (covering the widget instance registration)

--- app/code/Magento/Widget/Controller/Adminhtml/Widget/LoadOptions.php
+++ app/code/Magento/Widget/Controller/Adminhtml/Widget/LoadOptions.php
@@ -6,10 +6,18 @@
  */
 namespace Magento\Widget\Controller\Adminhtml\Widget;
 
-use Magento\Framework\App\ObjectManager;
-
 class LoadOptions extends \Magento\Backend\App\Action
 {
+    /**
+     * @var \Magento\Framework\Json\Helper\Data
+     */
+    protected $jsonDataHelper;
+
+    /**
+     * @var \Magento\Widget\Model\Widget\InstanceFactory
+     */
+    protected $widgetInstanceFactory;
+
     /**
      * @var \Magento\Framework\Registry
      */
@@ -21,10 +29,14 @@ class LoadOptions extends \Magento\Backend\App\Action
      */
     public function __construct(
         \Magento\Backend\App\Action\Context $context,
+        \Magento\Framework\Json\Helper\Data $jsonDataHelper,
+        \Magento\Widget\Model\Widget\InstanceFactory $widgetInstanceFactory,
         \Magento\Framework\Registry $registry
     ) {
         parent::__construct($context);
 
+        $this->jsonDataHelper = $jsonDataHelper;
+        $this->widgetInstanceFactory = $widgetInstanceFactory;
         $this->coreRegistry = $registry;
     }
 
@@ -38,15 +50,16 @@ class LoadOptions extends \Magento\Backend\App\Action
         try {
             $this->_view->loadLayout();
             if ($paramsJson = $this->getRequest()->getParam('widget')) {
-                $request = $this->_objectManager->get(\Magento\Framework\Json\Helper\Data::class)
-                    ->jsonDecode($paramsJson);
+                $request = $this->jsonDataHelper->jsonDecode($paramsJson);
                 if (is_array($request)) {
                     $optionsBlock = $this->_view->getLayout()->getBlock('wysiwyg_widget.options');
                     $widgetInstance = null;
 
                     if (isset($request['widget_type'])) {
-                        $widgetInstance = $this->_objectManager->create($request['widget_type']);
+                        $widgetInstance = $this->widgetInstanceFactory->create();
+                        $widgetInstance->setType($request['widget_type']);
                         $optionsBlock->setWidgetType($request['widget_type']);
+                        $this->coreRegistry->register('current_widget_instance', $widgetInstance, true);
                     }
 
                     if (isset($request['values'])) {
@@ -58,17 +71,13 @@ class LoadOptions extends \Magento\Backend\App\Action
 
                         $optionsBlock->setWidgetValues($request['values']);
                     }
-
-                    if ($widgetInstance) {
-                        $this->coreRegistry->register('current_widget_instance', $widgetInstance, true);
-                    }
                 }
                 $this->_view->renderLayout();
             }
         } catch (\Magento\Framework\Exception\LocalizedException $e) {
             $result = ['error' => true, 'message' => $e->getMessage()];
             $this->getResponse()->representJson(
-                $this->_objectManager->get(\Magento\Framework\Json\Helper\Data::class)->jsonEncode($result)
+                $this->jsonDataHelper->jsonEncode($result)
             );
         }
     }
