9f1b44cc9b312bf464bc58fa68d4d26bb02d90e5
Oleksii Korshenko <okorshenko@magento.com>
2016-08-08 11:56:46 -0500

    MAGETWO-56191: Topology installation for multiple connections
    - added support of multiple connections to publisher and consumer logic

---
+++ ExchangeFactory.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Framework\Amqp;
+
+/**
+ * Factory class for @see \Magento\Framework\Amqp\Exchange
+ */
+class ExchangeFactory implements \Magento\Framework\MessageQueue\ExchangeFactoryInterface
+{
+    /**
+     * Object Manager instance
+     *
+     * @var \Magento\Framework\ObjectManagerInterface
+     */
+    private $objectManager = null;
+
+    /**
+     * Instance name to create
+     *
+     * @var string
+     */
+    private $instanceName = null;
+
+    /**
+     * @var ConfigPool
+     */
+    private $configPool;
+
+    /**
+     * Initialize dependencies.
+     *
+     * @param \Magento\Framework\ObjectManagerInterface $objectManager
+     * @param ConfigPool $configPool
+     * @param string $instanceName
+     */
+    public function __construct(
+        \Magento\Framework\ObjectManagerInterface $objectManager,
+        ConfigPool $configPool,
+        $instanceName = \Magento\Framework\Amqp\Exchange::class
+    ) {
+        $this->objectManager = $objectManager;
+        $this->configPool = $configPool;
+        $this->instanceName = $instanceName;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function create($connectionName, array $data = [])
+    {
+        $data['amqpConfig'] = $this->configPool->get($connectionName);
+        return $this->objectManager->create(
+            $this->instanceName,
+            $data
+        );
+    }
+}
---
+++ QueueFactory.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Framework\Amqp;
+
+/**
+ * Factory class for @see \Magento\Framework\Amqp\Queue
+ */
+class QueueFactory implements \Magento\Framework\MessageQueue\QueueFactoryInterface
+{
+    /**
+     * Object Manager instance
+     *
+     * @var \Magento\Framework\ObjectManagerInterface
+     */
+    private $objectManager = null;
+
+    /**
+     * Instance name to create
+     *
+     * @var string
+     */
+    private $instanceName = null;
+
+    /**
+     * @var ConfigPool
+     */
+    private $configPool;
+
+    /**
+     * Initialize dependencies.
+     *
+     * @param \Magento\Framework\ObjectManagerInterface $objectManager
+     * @param ConfigPool $configPool
+     * @param string $instanceName
+     */
+    public function __construct(
+        \Magento\Framework\ObjectManagerInterface $objectManager,
+        ConfigPool $configPool,
+        $instanceName = \Magento\Framework\Amqp\Queue::class
+    ) {
+        $this->objectManager = $objectManager;
+        $this->configPool = $configPool;
+        $this->instanceName = $instanceName;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function create($queueName, $connectionName)
+    {
+        return $this->objectManager->create(
+            $this->instanceName,
+            [
+                'amqpConfig' => $this->configPool->get($connectionName),
+                'queueName' => $queueName
+            ]
+        );
+    }
+}
