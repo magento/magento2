1847b4c9e1056e1683b14b2729a60979d81281a7
Igor Melnikov <imelnikov@magento.com>
2016-08-10 11:58:21 -0500

    MAGETWO-55919: Eliminate @escapeNotVerified in Captcha, Contact, Cookie, Persistent, Rss, Sendfriend Module
    Applying escape functions in templates

--- lib/internal/Magento/Framework/Escaper.php
+++ lib/internal/Magento/Framework/Escaper.php
@@ -32,9 +32,17 @@ class Escaper
         } elseif (strlen($data)) {
             if (is_array($allowedTags) && !empty($allowedTags)) {
                 $allowed = implode('|', $allowedTags);
-                $result = preg_replace('/<([\/\s\r\n]*)(' . $allowed . ')([\/\s\r\n]*)>/si', '##$1$2$3##', $data);
+                $result = preg_replace(
+                    '/<([\/\s\r\n]*)(' . $allowed . '[^>]*)([\/\s\r\n]*)>/si',
+                    '##$1$2$3##',
+                    $data
+                );
                 $result = htmlspecialchars($result, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8', false);
-                $result = preg_replace('/##([\/\s\r\n]*)(' . $allowed . ')([\/\s\r\n]*)##/si', '<$1$2$3>', $result);
+                $result = preg_replace(
+                    '/##([\/\s\r\n]*)(' . $allowed . '[^##]*)([\/\s\r\n]*)##/si',
+                    '<$1$2$3>',
+                    $result
+                );
             } else {
                 $result = htmlspecialchars($data, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8', false);
             }
