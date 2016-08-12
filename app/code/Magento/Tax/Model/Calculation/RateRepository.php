dca94825d8b65a35952df79f7333ff99cffd3b84
Sergii Kovalenko <skovalenko@magento.com>
2016-08-12 17:37:42 +0300

    MAGETWO-56080: SearchCriteria Unified Processing for Quote and Tax modules

--- app/code/Magento/Tax/Model/Calculation/RateRepository.php
+++ app/code/Magento/Tax/Model/Calculation/RateRepository.php
@@ -91,7 +91,7 @@ class RateRepository implements \Magento\Tax\Api\TaxRateRepositoryInterface
         RegionFactory $regionFactory,
         \Magento\Tax\Model\ResourceModel\Calculation\Rate $rateResource,
         \Magento\Framework\Api\ExtensionAttribute\JoinProcessorInterface $joinProcessor,
-        CollectionProcessorInterface $collectionProcessor
+        CollectionProcessorInterface $collectionProcessor = null
     ) {
         $this->converter = $converter;
         $this->rateRegistry = $rateRegistry;
