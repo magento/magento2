2cb3381579fc1fee9ab97632af70199e9224aef4
Islam Elkhalifa <ielkhalifa@magento.com>
2016-08-19 17:19:08 -0500

    MAGETWO-56197: Write functional test for MAGETWO-47822
    - fixing code styles.

--- dev/tests/functional/tests/app/Magento/Config/Test/Block/System/Config/AdminForm.php
+++ dev/tests/functional/tests/app/Magento/Config/Test/Block/System/Config/AdminForm.php
@@ -9,16 +9,17 @@ namespace Magento\Config\Test\Block\System\Config;
 use Magento\Mtf\Block\Form;
 use Magento\Mtf\Client\Locator;
 
-/**
+ /**
  * Admin Security form in admin configurations.
  *
- * this class needs to be created becuase we need to check for the availability of Admin account sharing settings, This is not possible using the form block class only
-*/
+ * Locate Admin account sharing settings, see if its visible
+ */
 class AdminForm extends Form
 {
-    protected $AdminAccountSharingField = "#admin_security_admin_account_sharing";
+    protected $adminAccountSharingField = "#admin_security_admin_account_sharing";
 
-    public function AdminAccountSharingAvailability() {
-        return $this->_rootElement->find($this->AdminAccountSharingField, Locator::SELECTOR_CSS)->isVisible();
+    public function adminAccountSharingAvailability()
+    {
+        return $this->_rootElement->find($this->adminAccountSharingField, Locator::SELECTOR_CSS)->isVisible();
     }
 }
