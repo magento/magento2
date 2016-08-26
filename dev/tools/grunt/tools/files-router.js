494cdbc46de764520cf15f7f1e9a2b0bcb71f5c9
Volodymyr Zaets <vzaets@magento.com>
2016-08-26 11:35:28 +0300

    MAGETWO-55345: Grunt uses actual theme's list

--- dev/tools/grunt/tools/files-router.js
+++ dev/tools/grunt/tools/files-router.js
@@ -5,47 +5,7 @@
 
 'use strict';
 
-module.exports = {
-    defaultConfig: {
-        'themes': 'dev/tools/grunt/configs/themes'
-    },
-
-    /**
-     * Immediately invoked function.
-     * Loads user config file.
-     */
-    userConfig: (function () {
-        try {
-            return require(process.cwd() + '/grunt-config');
-        } catch (error) {
-            return null;
-        }
-    })(),
-
-    /**
-     * Loads file.
-     * Load priority:
-     *      From user config;
-     *      From default config with ".loc" suffix ;
-     *      From default config;
-     *
-     * @returns themes file or error
-     */
-    get: function (file) {
-        if (this.userConfig && this.userConfig[file]) {
-            return require(this.getFullPath(this.userConfig[file]));
-        } else {
-            try {
-                return require(this.getFullPath(this.defaultConfig[file] + '.loc'));
-            } catch (error) {
-                try {
-                    return require(this.getFullPath(this.defaultConfig[file]));
-                } catch (error) {
-                    throw  error;
-                }
-            }
-        }
-    },
+var defaultConfig = {},
 
     /**
      * Generates full path to file.
@@ -54,7 +14,70 @@ module.exports = {
      *
      * @returns {String} Full path to file
      */
-    getFullPath: function (path) {
+    getFullPath = function (path) {
         return process.cwd() + '/' + path;
+    },
+
+    /**
+     * Returns file.
+     *
+     * @param {String} path - relative path to file.
+     *
+     * @returns {Object|Null} File or NULL
+     */
+    getFile = function (path) {
+        try {
+            return require(getFullPath(path));
+        } catch (error) {
+            return null;
+        }
+    },
+
+    /**
+     * Immediately invoked function.
+     * Loads user config file.
+     */
+    userConfig = (function () {
+        try {
+            return require(process.cwd() + '/grunt-config');
+        } catch (error) {
+            return null;
+        }
+    })();
+
+module.exports = {
+
+    /**
+     * Loads file.
+     * Load priority:
+     *      From user config;
+     *      From default config with ".loc" suffix ;
+     *      From default config;
+     *
+     * @param {String} alias
+     *
+     * @returns {Object} themes file or error
+     */
+    get: function (alias) {
+        var tmp;
+
+        if (userConfig && userConfig[alias]) {
+            return require(getFullPath(userConfig[alias]));
+        } else if (tmp = getFile(defaultConfig[alias] + '.loc') ||  getFile(defaultConfig[alias])) {
+            return tmp;
+        } else {
+            throw new Error('Cannot find file. Alias "' + alias + '" not set. ' +
+                'Use "filesRouter.set" method to set it.').stack;
+        }
+    },
+
+    /**
+     * Sets file alias.
+     *
+     * @param {String} alias
+     * @param {String} path
+     */
+    set: function (alias, path) {
+        defaultConfig[alias] = path;
     }
 };
