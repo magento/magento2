05c6eb3e4ca84d90191965b68e166391902893dd
Oleksandr Radchenko <oradchenko@magento.com>
2016-08-31 15:48:05 +0300

    MAGETWO-57763: Implement creditmemoNotifier

---
+++ app/code/Magento/Sales/Model/Order/Creditmemo/Sender/EmailSender.php
@@ -0,0 +1,159 @@
+<?php
+
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Sales\Model\Order\CreditMemo\Sender;
+
+use Magento\Payment\Helper\Data as PaymentHelper;
+use Magento\Sales\Api\Data\CreditmemoCommentCreationInterface;
+use Magento\Sales\Api\Data\CreditmemoInterface;
+use Magento\Sales\Api\Data\OrderInterface;
+use Magento\Sales\Model\Order;
+use Magento\Sales\Model\Order\Creditmemo;
+use Magento\Sales\Model\Order\Email\Container\CreditmemoIdentity;
+use Magento\Sales\Model\Order\Email\Container\Template;
+use Magento\Sales\Model\Order\Email\Sender;
+use Magento\Sales\Model\ResourceModel\Order\Creditmemo as CreditmemoResource;
+use Magento\Sales\Model\Order\Address\Renderer;
+use Magento\Framework\Event\ManagerInterface;
+
+/**
+ * Email notification sender for Invoice.
+ */
+class EmailSender extends Sender
+{
+    /**
+     * @var PaymentHelper
+     */
+    protected $paymentHelper;
+
+    /**
+     * @var CreditmemoResource
+     */
+    protected $creditmemoResource;
+
+    /**
+     * Global configuration storage.
+     *
+     * @var \Magento\Framework\App\Config\ScopeConfigInterface
+     */
+    protected $globalConfig;
+
+    /**
+     * @var Renderer
+     */
+    protected $addressRenderer;
+
+    /**
+     * Application Event Dispatcher
+     *
+     * @var ManagerInterface
+     */
+    protected $eventManager;
+
+    /**
+     * @param Template $templateContainer
+     * @param CreditmemoIdentity $identityContainer
+     * @param Order\Email\SenderBuilderFactory $senderBuilderFactory
+     * @param \Psr\Log\LoggerInterface $logger
+     * @param PaymentHelper $paymentHelper
+     * @param CreditmemoResource $creditmemoResource
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $globalConfig
+     * @param Renderer $addressRenderer
+     * @param ManagerInterface $eventManager
+     */
+    public function __construct(
+        Template $templateContainer,
+        CreditmemoIdentity $identityContainer,
+        \Magento\Sales\Model\Order\Email\SenderBuilderFactory $senderBuilderFactory,
+        \Psr\Log\LoggerInterface $logger,
+        Renderer $addressRenderer,
+        PaymentHelper $paymentHelper,
+        CreditmemoResource $creditmemoResource,
+        \Magento\Framework\App\Config\ScopeConfigInterface $globalConfig,
+        ManagerInterface $eventManager
+    ) {
+        parent::__construct($templateContainer, $identityContainer, $senderBuilderFactory, $logger, $addressRenderer);
+        $this->paymentHelper = $paymentHelper;
+        $this->creditmemoResource = $creditmemoResource;
+        $this->globalConfig = $globalConfig;
+        $this->addressRenderer = $addressRenderer;
+        $this->eventManager = $eventManager;
+    }
+
+    /**
+     * Sends order creditmemo email to the customer.
+     *
+     * Email will be sent immediately in two cases:
+     *
+     * - if asynchronous email sending is disabled in global settings
+     * - if $forceSyncMode parameter is set to TRUE
+     *
+     * Otherwise, email will be sent later during running of
+     * corresponding cron job.
+     *
+     * @param OrderInterface $order
+     * @param CreditmemoInterface $creditmemo
+     * @param CreditmemoCommentCreationInterface $comment
+     * @param bool $forceSyncMode
+     * @return bool
+     */
+    public function send(
+        OrderInterface $order,
+        CreditmemoInterface $creditmemo,
+        CreditmemoCommentCreationInterface $comment,
+        $forceSyncMode = false
+    ) {
+        $creditmemo->setSendEmail(true);
+
+        if (!$this->globalConfig->getValue('sales_email/general/async_sending') || $forceSyncMode) {
+
+            $transport = [
+                'order' => $order,
+                'creditmemo' => $creditmemo,
+                'comment' => $comment ? $comment->getComment() : '',
+                'billing' => $order->getBillingAddress(),
+                'payment_html' => $this->getPaymentHtml($order),
+                'store' => $order->getStore(),
+                'formattedShippingAddress' => $this->getFormattedShippingAddress($order),
+                'formattedBillingAddress' => $this->getFormattedBillingAddress($order),
+            ];
+
+            $this->eventManager->dispatch(
+                'email_creditmemo_set_template_vars_before',
+                ['sender' => $this, 'transport' => $transport]
+            );
+
+            $this->templateContainer->setTemplateVars($transport);
+
+            if ($this->checkAndSend($order)) {
+                $creditmemo->setEmailSent(true);
+                $this->creditmemoResource->saveAttribute($creditmemo, ['send_email', 'email_sent']);
+                return true;
+            }
+        } else {
+            $creditmemo->setEmailSent(null);
+            $this->creditmemoResource->saveAttribute($creditmemo, 'email_sent');
+        }
+
+        $this->creditmemoResource->saveAttribute($creditmemo, 'send_email');
+
+        return false;
+    }
+
+    /**
+     * Return payment info block as html
+     *
+     * @param Order $order
+     * @return string
+     */
+    protected function getPaymentHtml(Order $order)
+    {
+        return $this->paymentHelper->getInfoBlockHtml(
+            $order->getPayment(),
+            $this->identityContainer->getStore()->getStoreId()
+        );
+    }
+}
