8fd1e17d5d3a2a86219e8cc14d3b0e4a0d4eab80
Joan He <johe@magento.com>
2016-08-31 10:27:23 -0500

    MAGETWO-57836: Make \Magento\Framework\Escaper::escapeJs() generate valid Json string

--- lib/internal/Magento/Framework/Escaper.php
+++ lib/internal/Magento/Framework/Escaper.php
@@ -234,7 +234,12 @@ class Escaper
         return preg_replace_callback(
             '/[^a-z0-9,\._]/iSu',
             function ($matches) {
-                return sprintf('\\u%04s', strtoupper(bin2hex($matches[0])));
+                $chr = $matches[0];
+                if (strlen($chr) != 1) {
+                    $chr = mb_convert_encoding($chr, 'UTF-16BE', 'UTF-8');
+                    $chr = ($chr === false) ? '' : $chr;
+                }
+                return sprintf('\\u%04s', strtoupper(bin2hex($chr)));
             },
             $string
         );
