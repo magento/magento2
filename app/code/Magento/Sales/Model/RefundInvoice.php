b7d14575b3b49f74007f6b9dd354982d7692b523
Vladyslav Shcherbyna <vshcherbyna@magento.com>
2016-09-01 14:22:36 +0300

    MAGETWO-57776: Introduce new service refundInvoice

---
+++ app/code/Magento/Sales/Model/RefundInvoice.php
@@ -0,0 +1,196 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Sales\Model;
+
+use Magento\Framework\App\ResourceConnection;
+use Magento\Sales\Api\CreditmemoRepositoryInterface;
+use Magento\Sales\Api\InvoiceRepositoryInterface;
+use Magento\Sales\Api\OrderRepositoryInterface;
+use Magento\Sales\Api\RefundInvoiceInterface;
+use Magento\Sales\Model\Order\Config;
+use Magento\Sales\Model\Order\Creditmemo\CreditmemoValidatorInterface;
+use Magento\Sales\Model\Order\Creditmemo\NotifierInterface;
+use Magento\Sales\Model\Order\Creditmemo\Validation\QuantityValidator;
+use Magento\Sales\Model\Order\Creditmemo\Validation\TotalsValidator;
+use Magento\Sales\Model\Order\CreditmemoDocumentFactory;
+use Magento\Sales\Model\Order\OrderStateResolverInterface;
+use Magento\Sales\Model\Order\OrderValidatorInterface;
+use Magento\Sales\Model\Order\PaymentAdapterInterface;
+use Magento\Sales\Model\Order\Validation\CanRefund;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Class RefundInvoice
+ */
+class RefundInvoice implements RefundInvoiceInterface
+{
+    /**
+     * @var ResourceConnection
+     */
+    private $resourceConnection;
+
+    /**
+     * @var OrderStateResolverInterface
+     */
+    private $orderStateResolver;
+
+    /**
+     * @var OrderRepositoryInterface
+     */
+    private $orderRepository;
+
+    /**
+     * @var InvoiceRepositoryInterface
+     */
+    private $invoiceRepository;
+
+    /**
+     * @var OrderValidatorInterface
+     */
+    private $orderValidator;
+
+    /**
+     * @var CreditmemoValidatorInterface
+     */
+    private $creditmemoValidator;
+
+    /**
+     * @var CreditmemoRepositoryInterface
+     */
+    private $creditmemoRepository;
+
+    /**
+     * @var Order\PaymentAdapterInterface
+     */
+    private $paymentAdapter;
+
+    /**
+     * @var CreditmemoDocumentFactory
+     */
+    private $creditmemoDocumentFactory;
+
+    /**
+     * @var Order\Creditmemo\NotifierInterface
+     */
+    private $notifier;
+
+    /**
+     * @var Config
+     */
+    private $config;
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @inheritDoc
+     */
+    public function __construct(
+        ResourceConnection $resourceConnection,
+        OrderStateResolverInterface $orderStateResolver,
+        OrderRepositoryInterface $orderRepository,
+        InvoiceRepositoryInterface $invoiceRepository,
+        OrderValidatorInterface $orderValidator,
+        CreditmemoValidatorInterface $creditmemoValidator,
+        CreditmemoRepositoryInterface $creditmemoRepository,
+        PaymentAdapterInterface $paymentAdapter,
+        CreditmemoDocumentFactory $creditmemoDocumentFactory,
+        NotifierInterface $notifier,
+        Config $config,
+        LoggerInterface $logger
+    ) {
+        $this->resourceConnection = $resourceConnection;
+        $this->orderStateResolver = $orderStateResolver;
+        $this->orderRepository = $orderRepository;
+        $this->invoiceRepository = $invoiceRepository;
+        $this->orderValidator = $orderValidator;
+        $this->creditmemoValidator = $creditmemoValidator;
+        $this->creditmemoRepository = $creditmemoRepository;
+        $this->paymentAdapter = $paymentAdapter;
+        $this->creditmemoDocumentFactory = $creditmemoDocumentFactory;
+        $this->notifier = $notifier;
+        $this->config = $config;
+        $this->logger = $logger;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function execute(
+        $invoiceId,
+        array $items = [],
+        $isOnline = false,
+        $notify = false,
+        $appendComment = false,
+        \Magento\Sales\Api\Data\CreditmemoCommentCreationInterface $comment = null,
+        \Magento\Sales\Api\Data\CreditmemoCreationArgumentsInterface $arguments = null
+    ) {
+        $connection = $this->resourceConnection->getConnection('sales');
+        $invoice = $this->invoiceRepository->get($invoiceId);
+        $order = $this->orderRepository->get($invoice->getOrderId());
+        $creditmemo = $this->creditmemoDocumentFactory->createFromInvoice(
+            $invoice,
+            $items,
+            $comment,
+            ($appendComment && $notify),
+            $arguments
+        );
+        $orderValidationResult = $this->orderValidator->validate(
+            $order,
+            [
+                CanRefund::class
+            ]
+        );
+        $creditmemoValidationResult = $this->creditmemoValidator->validate(
+            $creditmemo,
+            [
+                QuantityValidator::class,
+                TotalsValidator::class
+            ]
+        );
+        $validationMessages = array_merge($orderValidationResult, $creditmemoValidationResult);
+        if (!empty($validationMessages )) {
+            throw new \Magento\Sales\Exception\DocumentValidationException(
+                __("Creditmemo Document Validation Error(s):\n" . implode("\n", $validationMessages ))
+            );
+        }
+        $connection->beginTransaction();
+        try {
+            $creditmemo->setState(\Magento\Sales\Model\Order\Creditmemo::STATE_REFUNDED);
+            $order = $this->paymentAdapter->refund($creditmemo, $order, $isOnline);
+            $order->setState(
+                $this->orderStateResolver->getStateForOrder($order, [])
+            );
+            $order->setStatus($this->config->getStateDefaultStatus($order->getState()));
+            if (!$isOnline) {
+                $invoice->setIsUsedForRefund(true);
+                $invoice->setBaseTotalRefunded(
+                    $invoice->getBaseTotalRefunded() + $creditmemo->getBaseGrandTotal()
+                );
+            }
+            $this->invoiceRepository->save($invoice);
+            $order = $this->orderRepository->save($order);
+            $creditmemo = $this->creditmemoRepository->save($creditmemo);
+            $connection->commit();
+        } catch (\Exception $e) {
+            $this->logger->critical($e);
+            $connection->rollBack();
+            throw new \Magento\Sales\Exception\CouldNotRefundException(
+                __('Could not save a Creditmemo, see error log for details')
+            );
+        }
+        if ($notify) {
+            if (!$appendComment) {
+                $comment = null;
+            }
+            $this->notifier->notify($order, $creditmemo, $comment);
+        }
+
+        return $creditmemo->getEntityId();
+    }
+}
