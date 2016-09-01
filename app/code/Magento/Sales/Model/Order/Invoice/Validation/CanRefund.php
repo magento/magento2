767151b4dd326c175a2adf7eb8d8842379249132
Vladyslav Shcherbyna <vshcherbyna@magento.com>
2016-09-01 16:51:37 +0300

    MAGETWO-57774: Update  Creditmemo/InvoiceValidator

---
+++ app/code/Magento/Sales/Model/Order/Invoice/Validation/CanRefund.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Sales\Model\Order\Invoice\Validation;
+
+use Magento\Sales\Model\Order\Invoice;
+use Magento\Sales\Model\ValidatorInterface;
+
+/**
+ * Class CanRefund
+ */
+class CanRefund implements ValidatorInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public function validate($entity)
+    {
+        $messages = [];
+        if ($entity->getState() != Invoice::STATE_PAID) {
+            $messages[] = __(
+                'A creditmemo can not be created when an invoice has a status of %1',
+                $entity->getStatus()
+            );
+        }
+        if (abs($entity->getBaseGrandTotal() - $entity->getBaseTotalRefunded()) < .0001) {
+            $messages[] = __('We can\'t create creditmemo for the invoice.');
+        }
+
+        return $messages;
+    }
+}
