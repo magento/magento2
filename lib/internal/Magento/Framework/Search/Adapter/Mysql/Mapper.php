c2a4d8354fd48773b73e2f2cc32a7a5e8c60ff63
Illia Grybkov <igrybkov@magento.com>
2016-09-06 10:02:22 +0300

    MAGETWO-51447: CatalogSearch Fulltext Collection not allows to use filters with multiple values
      - Remove outdated workaround for MySQL 5.7

--- lib/internal/Magento/Framework/Search/Adapter/Mysql/Mapper.php
+++ lib/internal/Magento/Framework/Search/Adapter/Mysql/Mapper.php
@@ -158,7 +158,6 @@ class Mapper
     private function createAroundSelect(Select $select, ScoreBuilder $scoreBuilder)
     {
         $parentSelect = $this->getConnection()->select();
-        $select->limit(10000000000); // MySQL 5.7 workaround
         $parentSelect->from(
             ['main_select' => $select],
             [
