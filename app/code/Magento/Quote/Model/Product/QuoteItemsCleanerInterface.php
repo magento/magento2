6cc55c2eb131015d26091e6c134ce2584f658714
Stanislav Idolov <sidolov@magento.com>
2016-09-06 12:55:09 +0300

    MAGETWO-56397: [Backport] Unable to Upgrade with Split Databases - for 2.1

---
+++ app/code/Magento/Quote/Model/Product/QuoteItemsCleanerInterface.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Quote\Model\Product;
+
+use Magento\Catalog\Api\Data\ProductInterface;
+
+interface QuoteItemsCleanerInterface
+{
+    /**
+     * @return void
+     */
+    public function execute(ProductInterface $product);
+}
