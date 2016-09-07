9b655e7e86a7b285c3f35035d9d08fdb93a4a39e
Arpita Barua <abarua@magento.com>
2016-09-07 12:07:03 -0500

    MAGETWO-57336: Backporting incorrect request params
    - Pushing fix for this incorrect request params

--- app/code/Magento/GoogleOptimizer/Test/Unit/Observer/Block/Category/AddGoogleExperimentTabObserverTest.php
+++ app/code/Magento/GoogleOptimizer/Test/Unit/Observer/Block/Category/AddGoogleExperimentTabObserverTest.php
@@ -10,97 +10,95 @@ namespace Magento\GoogleOptimizer\Test\Unit\Observer\Block\Category;
 class AddGoogleExperimentTabObserverTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * @var \PHPUnit_Framework_MockObject_MockObject
+     * @var string
      */
-    protected $_helperMock;
+    public static $googleOptimizerBlockId ='google-experiment-form';
+
+    /**
+     * @var string
+     */
+    public static $googleOptimizerTabId ='google-experiment-tab';
 
     /**
      * @var \PHPUnit_Framework_MockObject_MockObject
      */
-    protected $_layoutMock;
+    private $helperMock;
 
     /**
      * @var \PHPUnit_Framework_MockObject_MockObject
      */
-    protected $_tabsMock;
+    private $layoutMock;
 
     /**
      * @var \PHPUnit_Framework_MockObject_MockObject
      */
-    protected $_eventObserverMock;
+    private $tabsMock;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    private $eventObserverMock;
 
     /**
      * @var \Magento\GoogleOptimizer\Observer\Block\Category\AddGoogleExperimentTabObserver
      */
-    protected $_modelObserver;
+    private $modelObserver;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    private $categoryMock;
 
     protected function setUp()
     {
-        $this->_helperMock = $this->getMock('Magento\GoogleOptimizer\Helper\Data', [], [], '', false);
-        $this->_layoutMock = $this->getMock('Magento\Framework\View\Layout', [], [], '', false);
-        $this->_tabsMock = $this->getMock(
-            'Magento\Catalog\Block\Adminhtml\Category\Tabs',
-            [],
-            [],
-            '',
-            false
-        );
-        $this->_eventObserverMock = $this->getMock('Magento\Framework\Event\Observer', [], [], '', false);
-
+        $this->helperMock = $this->getMock(\Magento\GoogleOptimizer\Helper\Data::class, [], [], '', false);
+        $this->layoutMock = $this->getMock(\Magento\Framework\View\Layout::class, [], [], '', false);
+        $this->tabsMock = $this->getMock(\Magento\Catalog\Block\Adminhtml\Category\Tabs::class, [], [], '', false);
+        $this->eventObserverMock = $this->getMock(\Magento\Framework\Event\Observer::class, [], [], '', false);
+        $this->categoryMock = $this->getMock(\Magento\Catalog\Model\Category::class, ['getStoreId'], [], '', false);
         $objectManagerHelper = new \Magento\Framework\TestFramework\Unit\Helper\ObjectManager($this);
-        $this->_modelObserver = $objectManagerHelper->getObject(
-            'Magento\GoogleOptimizer\Observer\Block\Category\AddGoogleExperimentTabObserver',
-            ['helper' => $this->_helperMock, 'layout' => $this->_layoutMock]
+        $this->modelObserver = $objectManagerHelper->getObject(
+            \Magento\GoogleOptimizer\Observer\Block\Category\AddGoogleExperimentTabObserver::class,
+            ['helper' => $this->helperMock, 'layout' => $this->layoutMock]
         );
     }
 
     public function testAddGoogleExperimentTabSuccess()
     {
-        $this->_helperMock->expects($this->once())->method('isGoogleExperimentActive')->will($this->returnValue(true));
-
-        $block = $this->getMock('Magento\Framework\View\Element\BlockInterface', [], [], '', false);
+        $this->helperMock->expects($this->once())->method('isGoogleExperimentActive')->will($this->returnValue(true));
+        $block = $this->getMock(\Magento\Framework\View\Element\BlockInterface::class, [], [], '', false);
         $block->expects($this->once())->method('toHtml')->will($this->returnValue('generated html'));
-
-        $this->_layoutMock->expects(
-            $this->once()
-        )->method(
-            'createBlock'
-        )->with(
-            'Magento\GoogleOptimizer\Block\Adminhtml\Catalog\Category\Edit\Tab\Googleoptimizer',
-            'google-experiment-form'
-        )->will(
-            $this->returnValue($block)
-        );
-
-        $event = $this->getMock('Magento\Framework\Event', ['getTabs'], [], '', false);
-        $event->expects($this->once())->method('getTabs')->will($this->returnValue($this->_tabsMock));
-        $this->_eventObserverMock->expects($this->once())->method('getEvent')->will($this->returnValue($event));
-
-        $this->_tabsMock->expects(
-            $this->once()
-        )->method(
-            'addTab'
-        )->with(
-            'google-experiment-tab',
-            ['label' => __('Category View Optimization'), 'content' => 'generated html']
-        );
-
-        $this->_modelObserver->execute($this->_eventObserverMock);
+        $this->layoutMock->expects($this->once())->method('createBlock')
+            ->with(
+                \Magento\GoogleOptimizer\Block\Adminhtml\Catalog\Category\Edit\Tab\Googleoptimizer::class,
+                self::$googleOptimizerBlockId
+            )
+            ->will($this->returnValue($block));
+        $event = $this->getMock(\Magento\Framework\Event::class, ['getTabs'], [], '', false);
+        $event->expects($this->any())->method('getTabs')->will($this->returnValue($this->tabsMock));
+        $this->eventObserverMock->expects($this->any())->method('getEvent')->will($this->returnValue($event));
+        $this->tabsMock->expects($this->once())->method('addTab')
+            ->with(
+                self::$googleOptimizerTabId,
+                ['label' => __('Category View Optimization'), 'content' => 'generated html']
+            );
+        $this->categoryMock->expects($this->any())->method('getStoreId')->will($this->returnValue(1));
+        $this->tabsMock->expects($this->any())->method('getCategory')->will($this->returnValue($this->categoryMock));
+        $this->modelObserver->execute($this->eventObserverMock);
     }
 
     public function testAddGoogleExperimentTabFail()
     {
-        $this->_helperMock->expects(
-            $this->once()
-        )->method(
-            'isGoogleExperimentActive'
-        )->will(
-            $this->returnValue(false)
-        );
-        $this->_layoutMock->expects($this->never())->method('createBlock');
-        $this->_tabsMock->expects($this->never())->method('addTab');
-        $this->_eventObserverMock->expects($this->never())->method('getEvent');
-
-        $this->_modelObserver->execute($this->_eventObserverMock);
+        $this->categoryMock->expects($this->any())->method('getStoreId')->will($this->returnValue(1));
+        $this->tabsMock->expects($this->any())->method('getCategory')
+            ->will($this->returnValue($this->categoryMock));
+        $this->helperMock->expects($this->once())->method('isGoogleExperimentActive')
+            ->will($this->returnValue(false));
+        $this->layoutMock->expects($this->never())->method('createBlock');
+        $this->tabsMock->expects($this->never())->method('addTab');
+        $event = $this->getMock('Magento\Framework\Event', ['getTabs'], [], '', false);
+        $this->eventObserverMock->expects($this->once())->method('getEvent')->will($this->returnValue($event));
+        $event->expects($this->any())->method('getTabs')->will($this->returnValue($this->tabsMock));
+        $this->modelObserver->execute($this->eventObserverMock);
     }
 }
