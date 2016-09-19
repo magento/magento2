c4d4654ed2850acf041bef4a5d1d505973db7c29
Leonid Poluyanov <lpoluyanov@magento.com>
2016-09-19 14:09:41 +0300

    MAGETWO-58053: [GITHUB] Product image issue with multiple store views Magento 2.1.0 #6259

--- dev/tests/integration/testsuite/Magento/Catalog/Model/Product/Gallery/CreateHandlerTest.php
+++ dev/tests/integration/testsuite/Magento/Catalog/Model/Product/Gallery/CreateHandlerTest.php
@@ -18,6 +18,10 @@ class CreateHandlerTest extends \PHPUnit_Framework_TestCase
      */
     protected $createHandler;
 
+    private $fileName = '/m/a/magento_image.jpg';
+
+    private $fileLabel = 'Magento image';
+
     protected function setUp()
     {
         $this->createHandler = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create(
@@ -28,10 +32,8 @@ class CreateHandlerTest extends \PHPUnit_Framework_TestCase
     /**
      * @covers \Magento\Catalog\Model\Product\Gallery\CreateHandler::execute
      */
-    public function testExecute()
+    public function testExecuteWithImageDuplicate()
     {
-        $fileName = '/m/a/magento_image.jpg';
-        $fileLabel = 'Magento image';
         /** @var $product \Magento\Catalog\Model\Product */
         $product = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create(
             \Magento\Catalog\Model\Product::class
@@ -39,20 +41,89 @@ class CreateHandlerTest extends \PHPUnit_Framework_TestCase
         $product->load(1);
         $product->setData(
             'media_gallery',
-            ['images' => ['image' => ['file' => $fileName, 'label' => $fileLabel]]]
+            ['images' => ['image' => ['file' => $this->fileName, 'label' => $this->fileLabel]]]
         );
-        $product->setData('image', $fileName);
+        $product->setData('image', $this->fileName);
         $this->createHandler->execute($product);
         $this->assertStringStartsWith('/m/a/magento_image', $product->getData('media_gallery/images/image/new_file'));
-        $this->assertEquals($fileLabel, $product->getData('image_label'));
+        $this->assertEquals($this->fileLabel, $product->getData('image_label'));
 
         $product->setIsDuplicate(true);
         $product->setData(
             'media_gallery',
-            ['images' => ['image' => ['value_id' => '100', 'file' => $fileName, 'label' => $fileLabel]]]
+            ['images' => ['image' => ['value_id' => '100', 'file' => $this->fileName, 'label' => $this->fileLabel]]]
         );
         $this->createHandler->execute($product);
         $this->assertStringStartsWith('/m/a/magento_image', $product->getData('media_gallery/duplicate/100'));
-        $this->assertEquals($fileLabel, $product->getData('image_label'));
+        $this->assertEquals($this->fileLabel, $product->getData('image_label'));
+    }
+
+    /**
+     * @covers \Magento\Catalog\Model\Product\Gallery\CreateHandler::execute
+     * @dataProvider executeDataProvider
+     * @param array $expectedValues
+     */
+    public function testExecuteWithImageRoles($expectedValues)
+    {
+        /** @var $product \Magento\Catalog\Model\Product */
+        $product = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create(
+            \Magento\Catalog\Model\Product::class
+        );
+        $product->load(1);
+        $product->setData(
+            'media_gallery',
+            ['images' => ['image' => ['file' => $this->fileName, 'label' => '']]]
+        );
+        foreach ($expectedValues as $mediaAttribute => $value) {
+            if ($value) {
+                $product->setData($mediaAttribute, $value);
+            }
+        }
+        $this->createHandler->execute($product);
+        $this->assertStringStartsWith('/m/a/magento_image', $product->getData('media_gallery/images/image/new_file'));
+        $resource = $product->getResource();
+        $attributeValues = [];
+        foreach (array_keys($expectedValues) as $mediaAttribute) {
+            $attributeValues[$mediaAttribute] = $resource->getAttributeRawValue(
+                $product->getId(),
+                $resource->getAttribute($mediaAttribute),
+                $product->getStoreId()
+            );
+        }
+        $this->assertStringStartsWith('/m/a/magento_image', $product->getData('media_gallery/images/image/new_file'));
+        $this->assertEquals($expectedValues, $attributeValues);
+    }
+
+    /**
+     * @return array
+     */
+    public function executeDataProvider()
+    {
+        return [
+            [
+                [
+                    'image' => $this->fileName,
+                    'small_image' => $this->fileName,
+                    'swatch_image' => $this->fileName,
+                    'thumbnail' => $this->fileName
+                ]
+            ],
+            [
+                [
+                    'image' => 'no_selection',
+                    'small_image' => 'no_selection',
+                    'swatch_image' => 'no_selection',
+                    'thumbnail' => 'no_selection'
+                ]
+            ],
+            [
+                [
+                    'image' => null,
+                    'small_image' => null,
+                    'swatch_image' => null,
+                    'thumbnail' => null
+                ]
+            ]
+        ];
     }
 }
