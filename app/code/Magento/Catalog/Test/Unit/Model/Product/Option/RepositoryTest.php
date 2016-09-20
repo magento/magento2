4e28cf11756c4f5d9c4ba237eea96d2348256f23
Yurii Hryhoriev <yhryhoriev@magento.com>
2016-09-20 18:23:30 +0300

    MAGETWO-47698: [Github] Custom options not displayed correctly on a store view level #2908 #5885 - tests fix

--- app/code/Magento/Catalog/Test/Unit/Model/Product/Option/RepositoryTest.php
+++ app/code/Magento/Catalog/Test/Unit/Model/Product/Option/RepositoryTest.php
@@ -9,6 +9,7 @@ namespace Magento\Catalog\Test\Unit\Model\Product\Option;
 
 use \Magento\Catalog\Model\Product\Option\Repository;
 use Magento\Catalog\Api\Data\ProductCustomOptionInterface;
+use \Magento\Catalog\Model\ResourceModel\Product\Option\CollectionFactory;
 
 /**
  * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
@@ -34,6 +35,11 @@ class RepositoryTest extends \PHPUnit_Framework_TestCase
      * @var ProductCustomOptionInterface|\PHPUnit_Framework_MockObject_MockObject
      */
     protected $optionMock;
+
+    /**
+     * @var CollectionFactory|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $optionCollectionFactory;
 
     /**
      * @var \PHPUnit_Framework_MockObject_MockObject
@@ -72,13 +78,9 @@ class RepositoryTest extends \PHPUnit_Framework_TestCase
             '',
             false
         );
-        $optionCollectionFactory = $this->getMock(
-            \Magento\Catalog\Model\ResourceModel\Product\Option\CollectionFactory::class,
-            ['create'],
-            [],
-            '',
-            false
-        );
+        $this->optionCollectionFactory = $this->getMockBuilder(CollectionFactory::class)
+            ->disableOriginalConstructor()
+            ->getMock();
         $metadataPool = $this->getMockBuilder(\Magento\Framework\EntityManager\MetadataPool::class)
             ->disableOriginalConstructor()
             ->getMock();
@@ -97,7 +99,7 @@ class RepositoryTest extends \PHPUnit_Framework_TestCase
             $this->optionRepository,
             [
                 'optionFactory' => $optionFactory,
-                'optionCollectionFactory' => $optionCollectionFactory,
+                'collectionFactory' => $this->optionCollectionFactory,
                 'metadataPool' => $metadataPool
             ]
         );
@@ -275,9 +277,19 @@ class RepositoryTest extends \PHPUnit_Framework_TestCase
 
     public function testSave()
     {
-        $productId = 123;
         $productSku = 'simple_product';
         $optionId = 1;
+        $originalValue1 = $this->getMockBuilder(\Magento\Catalog\Model\Product\Option::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $originalValue2 = clone $originalValue1;
+        $originalValue3 = clone $originalValue1;
+
+        $originalValue1->expects($this->at(0))->method('getData')->with('option_type_id')->willReturn(10);
+        $originalValue1->expects($this->once())->method('setData')->with('is_delete', 1);
+        $originalValue2->expects($this->once())->method('getData')->with('option_type_id')->willReturn(4);
+        $originalValue3->expects($this->once())->method('getData')->with('option_type_id')->willReturn(5);
+
         $this->optionMock->expects($this->once())->method('getProductSku')->willReturn($productSku);
         $this->productRepositoryMock
             ->expects($this->once())
@@ -285,27 +297,21 @@ class RepositoryTest extends \PHPUnit_Framework_TestCase
             ->with($productSku)
             ->willReturn($this->productMock);
         $this->optionMock->expects($this->any())->method('getOptionId')->willReturn($optionId);
-        $resourceModelMock = $this->getMockBuilder(\Magento\Catalog\Model\ResourceModel\Product::class)
+        $this->productMock->expects($this->once())->method('getOptions')->willReturn([]);
+        $this->optionMock->expects($this->once())->method('getData')->with('values')->willReturn([
+            ['option_type_id' => 4],
+            ['option_type_id' => 5]
+        ]);
+        $optionCollection = $this->getMockBuilder(\Magento\Catalog\Model\ResourceModel\Product\Option\Collection::class)
             ->disableOriginalConstructor()
             ->getMock();
-        $resourceModelMock->expects($this->once())
-            ->method('load')
-            ->with($this->productMock, $productId)
-            ->willReturnSelf();
-        $this->productMock->expects($this->once())->method('getId')->willReturn($productId);
-        $this->productMock->expects($this->once())->method('getResource')->willReturn($resourceModelMock);
-        $this->productMock->expects($this->once())->method('getOptions')->willReturn([]);
-        $this->productMock
-            ->expects($this->once())
-            ->method('getOptionById')
-            ->with($optionId)
-            ->willReturn($this->optionMock);
-        $this->optionMock->expects($this->once())->method('getData')->with('values')->willReturn([
-            ['option_type_id' => 3],
-            ['option_type_id' => 4],
-            ['option_type_id' => 5],
+        $optionCollection->expects($this->once())->method('getProductOptions')->willReturn([$this->optionMock]);
+        $this->optionCollectionFactory->expects($this->once())->method('create')->willReturn($optionCollection);
+        $this->optionMock->expects($this->once())->method('getValues')->willReturn([
+            $originalValue1,
+            $originalValue2,
+            $originalValue3
         ]);
-        $this->optionMock->expects($this->once())->method('getValues')->willReturn([]);
         $this->assertEquals($this->optionMock, $this->optionRepository->save($this->optionMock));
     }
 }
