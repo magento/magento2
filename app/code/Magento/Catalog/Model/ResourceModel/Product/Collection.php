23f2d5ce94a412f6a913640f38779a2a7025a435
maciekpaprocki <maciekpaprocki@users.noreply.github.com>
2016-09-20 11:33:55 +0100

    Update addCategoriesFilter to return $this
    To keep consistency addCategoriesFilter should return $this. All Other methods seems to use fluent API.

--- app/code/Magento/Catalog/Model/ResourceModel/Product/Collection.php
+++ app/code/Magento/Catalog/Model/ResourceModel/Product/Collection.php
@@ -852,6 +852,7 @@ class Collection extends \Magento\Catalog\Model\ResourceModel\Collection\Abstrac
      * Filter Product by Categories
      *
      * @param array $categoriesFilter
+     * @return $this
      */
     public function addCategoriesFilter(array $categoriesFilter)
     {
@@ -865,6 +866,7 @@ class Collection extends \Magento\Catalog\Model\ResourceModel\Collection\Abstrac
             ];
             $this->getSelect()->where($this->getConnection()->prepareSqlCondition('e.entity_id' , $selectCondition));
         }
+        return $this;
     }
 
     /**
