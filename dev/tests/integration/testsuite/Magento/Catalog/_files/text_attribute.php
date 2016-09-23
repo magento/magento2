1c56c345bdc36b043082e2431ff616991cb8cc8d
Arpita Barua <abarua@magento.com>
2016-09-23 15:41:40 -0500

    MAGETWO-57981: Backport unable to export bundle product
    - Cleaning up L3 build errors

--- dev/tests/integration/testsuite/Magento/Catalog/_files/text_attribute.php
+++ dev/tests/integration/testsuite/Magento/Catalog/_files/text_attribute.php
@@ -1,7 +1,7 @@
 <?php
 /**
-* Copyright © 2016 Magento. All rights reserved.
-* See COPYING.txt for license details.
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
  */
 $installer = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()
     ->create(\Magento\Catalog\Setup\CategorySetup::class);
@@ -11,26 +11,26 @@ $attribute = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()
 $attribute->setData(
     [
         'attribute_code' => 'text_attribute',
- 	 	'entity_type_id' => $installer->getEntityTypeId('catalog_product'),
- 	 	'is_global' => 1,
- 	 	'is_user_defined' => 1,
- 	    'frontend_input' => 'textarea',
- 	 	'is_unique' => 0,
+        'entity_type_id' => $installer->getEntityTypeId('catalog_product'),
+        'is_global' => 1,
+        'is_user_defined' => 1,
+        'frontend_input' => 'textarea',
+        'is_unique' => 0,
         'is_required' => 0,
- 	 	'is_searchable' => 0,
+        'is_searchable' => 0,
         'is_visible_in_advanced_search' => 0,
- 	 	'is_comparable' => 0,
- 	 	'is_filterable' => 0,
- 	 	'is_filterable_in_search' => 0,
- 	 	'is_used_for_promo_rules' => 0,
- 	    'is_html_allowed_on_front' => 1,
- 	 	'is_visible_on_front' => 0,
+        'is_comparable' => 0,
+        'is_filterable' => 0,
+        'is_filterable_in_search' => 0,
+        'is_used_for_promo_rules' => 0,
+        'is_html_allowed_on_front' => 1,
+        'is_visible_on_front' => 0,
         'used_in_product_listing' => 0,
- 	 	'used_for_sort_by' => 0,
- 	 	'frontend_label' => ['Text Attribute'],
- 	 	'backend_type' => 'text',
+        'used_for_sort_by' => 0,
+        'frontend_label' => ['Text Attribute'],
+        'backend_type' => 'text',
     ]
 );
 $attribute->save();
 /* Assign attribute to attribute set */
-$installer->addAttributeToGroup('catalog_product', 'Default', 'General', $attribute->getId());
+$installer->addAttributeToGroup('catalog_produc t', 'Default', 'General', $attribute->getId());
