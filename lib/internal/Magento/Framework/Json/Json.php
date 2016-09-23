5b5120b08290f7b02dbeab8661395f487f7b7c98
Joan He <johe@magento.com>
2016-09-23 08:27:44 -0500

    MAGETWO-58843: Create Json class

---
+++ lib/internal/Magento/Framework/Json/Json.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Framework\Json;
+
+class Json implements JsonInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function encode($data, $options = 0)
+    {
+        return json_encode($data, $options);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function decode($string, $objectDecodeType = self::TYPE_ARRAY)
+    {
+        return json_decode($string, $objectDecodeType);
+    }
+}
