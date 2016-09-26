f1e0f27a4bbb72c664a33b128c58f99cab6befca
vzabaznov <vzabaznov@magento.com>
2016-09-26 12:32:10 +0300

    MAGETWO-58933: Delete paging functionality for cofigurable product variations
    - Revert "Merge pull request #253 from magento-performance/MAGETWO-55785"
    - This reverts commit b2cceb7261086ee1313e66144460324f85331bb4, reversing
    changes made to b716af701b52c77db8a61134fad543d0ab71d015.

--- app/code/Magento/ConfigurableProduct/Controller/Adminhtml/Product/Initialization/Helper/Plugin/UpdateConfigurations.php
+++ app/code/Magento/ConfigurableProduct/Controller/Adminhtml/Product/Initialization/Helper/Plugin/UpdateConfigurations.php
@@ -47,9 +47,6 @@ class UpdateConfigurations
         \Magento\Catalog\Model\Product $configurableProduct
     ) {
         $configurations = $this->request->getParam('configurations', []);
-        if ($this->request->getParam('configurations_serialized')) {
-            $configurations = json_decode($this->request->getParam('configurations_serialized'), true);
-        }
         $configurations = $this->variationHandler->duplicateImagesForVariations($configurations);
         foreach ($configurations as $productId => $productData) {
             /** @var \Magento\Catalog\Model\Product $product */
