aab055db92afe5ee1057ccda9686eb67b2742787
Riccardo <riccardo@gene.co.uk>
2016-09-30 09:51:55 +0100

    change the if statement after PHPunit validation

--- app/code/Magento/Customer/Controller/Adminhtml/Index/Save.php
+++ app/code/Magento/Customer/Controller/Adminhtml/Index/Save.php
@@ -247,7 +247,7 @@ class Save extends \Magento\Customer\Controller\Adminhtml\Index
                     $isSubscribed = (bool)$this->getRequest()->getPost('subscription');
                 }
                 if ($isSubscribed !== null) {
-                    if ($isSubscribed !== false) {
+                    if ($isSubscribed !== '0') {
                         $this->_subscriberFactory->create()->subscribeCustomerById($customerId);
                     } else {
                         $this->_subscriberFactory->create()->unsubscribeCustomerById($customerId);
