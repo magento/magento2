95cbfafe603219bd24032bb44c2c6e07dcb2efe1
Oleksandr Miroshnichenko <omiroshnichenko@magento.com>
2016-10-03 17:29:25 +0300

    MAGETWO-59416: [Backport] - [Github] Admin can't reset password for more than one customer #5260 - for 2.1

--- app/code/Magento/Customer/Controller/Adminhtml/Index/ResetPassword.php
+++ app/code/Magento/Customer/Controller/Adminhtml/Index/ResetPassword.php
@@ -6,6 +6,7 @@
 namespace Magento\Customer\Controller\Adminhtml\Index;
 
 use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\Exception\SecurityViolationException;
 
 class ResetPassword extends \Magento\Customer\Controller\Adminhtml\Index
 {
@@ -40,6 +41,8 @@ class ResetPassword extends \Magento\Customer\Controller\Adminhtml\Index
                 $messages = $exception->getMessage();
             }
             $this->_addSessionErrorMessages($messages);
+        } catch (SecurityViolationException $exception) {
+            $this->messageManager->addErrorMessage($exception->getMessage());
         } catch (\Exception $exception) {
             $this->messageManager->addException(
                 $exception,
