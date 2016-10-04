ac6f2093d59317a49d755e25ca117994207f2d2c
Andrii Kasian <akasian@magento.com>
2016-10-04 11:10:38 +0300

    MAGETWO-55299: [Customer] Fast Save of Product Variations
         - MAGETWO-55787: Fast saving of product with high number of variations generated

--- app/code/Magento/CatalogInventory/Model/Stock/StockItemRepository.php
+++ app/code/Magento/CatalogInventory/Model/Stock/StockItemRepository.php
@@ -90,6 +90,9 @@ class StockItemRepository implements StockItemRepositoryInterface
      */
     protected $stockRegistryStorage;
 
+    /** @var  \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory */
+    protected $productCollectionFactory;
+
     /**
      * @param StockConfigurationInterface $stockConfiguration
      * @param StockStateProviderInterface $stockStateProvider
@@ -128,6 +131,20 @@ class StockItemRepository implements StockItemRepositoryInterface
         $this->localeDate = $localeDate;
         $this->indexProcessor = $indexProcessor;
         $this->dateTime = $dateTime;
+    }
+
+    /**
+     * @return  \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory
+     */
+    private function getProductCollectionFactory()
+    {
+        if ($this->productCollectionFactory === null) {
+            $this->productCollectionFactory = ObjectManager::getInstance()->get(
+                \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory::class
+            );
+        }
+
+        return $this->productCollectionFactory;
     }
 
     /**
