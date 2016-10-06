726635e948fb66ff109ff8148701e12e808aa77a
Oleksii Korshenko <okorshenko@magento.com>
2016-10-06 11:03:17 -0500

    MAGETWO-59102: Bundle Products - The options you selected are not available - for 2.0.x

--- dev/tests/integration/testsuite/Magento/Bundle/Model/Product/TypeTest.php
+++ dev/tests/integration/testsuite/Magento/Bundle/Model/Product/TypeTest.php
@@ -62,4 +62,97 @@ class TypeTest extends \PHPUnit_Framework_TestCase
         $result = $this->connectionMock->fetchAll($select);
         $this->assertCount(1, $result);
     }
+
+    /**
+     * Test that having valid buyRequest it can be successfully prepared fro shopping cart
+     *
+     * @magentoDataFixture Magento/Bundle/_files/product_with_multiple_options.php
+     * @magentoAppArea frontend
+     * @magentoAppIsolation enabled
+     */
+    public function testPrepareForCart()
+    {
+        /** @var \Magento\Catalog\Api\ProductRepositoryInterface $productRepository */
+        $productRepository = $this->objectManager->create(\Magento\Catalog\Api\ProductRepositoryInterface::class);
+        $product = $productRepository->get('bundle-product');
+
+        /** @var \Magento\Bundle\Api\ProductOptionRepositoryInterface $optionsRepository */
+        $optionsRepository = $this->objectManager->create(\Magento\Bundle\Api\ProductOptionRepositoryInterface::class);
+        $options = $optionsRepository->getList($product->getSku());
+
+        $data = [
+            'id' => '10',
+            'product' => '3',
+            'selected_configurable_option' => '',
+            'related_product' => '',
+            'bundle_option' => [],
+            'bundle_option_qty' => [],
+            'qty' => '1',
+            'options' => [],
+            'reset_count' => true,
+        ];
+
+        foreach ($options as $option) {
+            /** @var \Magento\Bundle\Api\Data\LinkInterface $link */
+            $link = current($option->getProductLinks());
+            $data['bundle_option'][$option->getOptionId()] = $link->getId();
+            $data['bundle_option_qty'][$option->getOptionId()] = 1;
+        }
+
+        $request =  $this->objectManager->create(\Magento\Framework\DataObject::class, ['data' => $data]);
+        /** @var \Magento\Bundle\Model\Product\Type $typeInstance */
+        $typeInstance = $product->getTypeInstance();
+
+        $result = $typeInstance->prepareForCart($request, $product);
+        $this->assertEquals(count($data['bundle_option']) + 1, count($result), 'Incorrect product count');
+    }
+
+    /**
+     * Test that having invalid selection option in buyRequest
+     * prepareForCart method will return meaningful error message
+     *
+     * @magentoDataFixture Magento/Bundle/_files/product_with_multiple_options.php
+     * @magentoAppArea frontend
+     * @magentoAppIsolation enabled
+     */
+    public function testPrepareForCartWithUnavailableOption()
+    {
+        /** @var \Magento\Catalog\Api\ProductRepositoryInterface $productRepository */
+        $productRepository = $this->objectManager->create(\Magento\Catalog\Api\ProductRepositoryInterface::class);
+        $product = $productRepository->get('bundle-product');
+
+        /** @var \Magento\Bundle\Api\ProductOptionRepositoryInterface $optionsRepository */
+        $optionsRepository = $this->objectManager->create(\Magento\Bundle\Api\ProductOptionRepositoryInterface::class);
+        $options = $optionsRepository->getList($product->getSku());
+
+        $data = [
+            'id' => '10',
+            'product' => '3',
+            'selected_configurable_option' => '',
+            'related_product' => '',
+            'bundle_option' => [],
+            'bundle_option_qty' => [],
+            'qty' => '1',
+            'options' => [],
+            'reset_count' => true,
+        ];
+
+        $option = null;
+        foreach ($options as $option) {
+            /** @var \Magento\Bundle\Api\Data\LinkInterface $link */
+            $link = current($option->getProductLinks());
+            $data['bundle_option'][$option->getOptionId()] = $link->getId();
+            $data['bundle_option_qty'][$option->getOptionId()] = 1;
+        }
+
+        /** Set latest option selection to unavailable option */
+        $data['bundle_option'][$option->getOptionId()] = 300;
+
+        $buyRequest =  $this->objectManager->create(\Magento\Framework\DataObject::class, ['data' => $data]);
+        /** @var \Magento\Bundle\Model\Product\Type $typeInstance */
+        $typeInstance = $product->getTypeInstance();
+
+        $result = $typeInstance->prepareForCart($buyRequest, $product);
+        $this->assertEquals('The options you selected are not available.', $result);
+    }
 }
