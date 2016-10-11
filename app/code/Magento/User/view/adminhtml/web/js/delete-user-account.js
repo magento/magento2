5435316d30a19531725debca53afd2d439cf147e
Ruslan Kostiv <rkostiv@magento.com>
2016-10-11 20:04:03 +0300

    MAGETWO-55849: Customer can be deleted without Merchant permissions verification

--- app/code/Magento/User/view/adminhtml/web/js/delete-user-account.js
+++ app/code/Magento/User/view/adminhtml/web/js/delete-user-account.js
@@ -1,16 +1,23 @@
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
 define([
     "jquery"
 ], function($){
+    'use strict';
 
     return function (params, elem) {
 
         elem.on('click', function() {
             if ($.validator.validateElement($('[name="current_password"]'))) {
-                postData = {'data' : {
-                    'user_id': params.objId,
-                    'current_password': $('[name="current_password"]').val()
-                }}
-                deleteConfirm(params.message, params.url, params.postData);
+                var postData = {
+                    'data' : {
+                        'user_id': params.objId,
+                        'current_password': $('[name="current_password"]').val()
+                    }
+                }
+                deleteConfirm(params.message, params.url, postData);
             }
         });
     }
