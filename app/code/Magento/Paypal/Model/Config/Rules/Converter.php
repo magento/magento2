f0fd791c48ab43b78ce6bdea56c6a631dbf2ce7d
Joan He <johe@magento.com>
2016-10-24 14:55:39 -0500

    MAGETWO-59923: Configuration in \Magento\Indexer\Model\Config\Data contains objects
    - revert 920b430

--- app/code/Magento/Paypal/Model/Config/Rules/Converter.php
+++ app/code/Magento/Paypal/Model/Config/Rules/Converter.php
@@ -85,7 +85,7 @@ class Converter implements ConverterInterface
             if ($this->hasNodeElement($child)) {
                 $result = [
                     'name' => $child->getAttribute('name'),
-                    'message' => __($child->getAttribute('message'))->__toString(),
+                    'message' => __($child->getAttribute('message')),
                     'event' => $child->getAttribute('event'),
                     'argument' => $this->createArgument($child),
                 ];
