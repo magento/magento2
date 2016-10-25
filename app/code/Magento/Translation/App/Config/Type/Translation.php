a2a7ea60ecbd403e1392d686d3fd53ba37fb13bb
Sergii Kovalenko <skovalenko@magento.com>
2016-10-25 12:33:11 +0300

    MAGETWO-57343: App Config Reader

--- app/code/Magento/Translation/App/Config/Type/Translation.php
+++ app/code/Magento/Translation/App/Config/Type/Translation.php
@@ -5,11 +5,9 @@
  */
 namespace Magento\Translation\App\Config\Type;
 
-use Magento\Framework\App\Cache\Type\Translate;
 use Magento\Framework\App\Config\ConfigSourceInterface;
 use Magento\Framework\App\Config\ConfigTypeInterface;
 use Magento\Framework\DataObject;
-use Magento\Framework\Cache\FrontendInterface;
 
 /**
  * Class which hold all translation sources and merge them
@@ -31,29 +29,13 @@ class Translation implements ConfigTypeInterface
     private $source;
 
     /**
-     * @var FrontendInterface
-     */
-    private $cache;
-
-    /**
-     * @var int
-     */
-    private $cachingNestedLevel;
-
-    /**
      * Translation constructor.
      * @param ConfigSourceInterface $source
-     * @param FrontendInterface $cache
-     * @param int $cachingNestedLevel
      */
     public function __construct(
-        ConfigSourceInterface $source,
-        FrontendInterface $cache,
-        $cachingNestedLevel = 1
+        ConfigSourceInterface $source
     ) {
         $this->source = $source;
-        $this->cache = $cache;
-        $this->cachingNestedLevel = $cachingNestedLevel;
     }
 
     /**
@@ -61,32 +43,11 @@ class Translation implements ConfigTypeInterface
      */
     public function get($path = '')
     {
-        $cachePath = $this->getCachePath($path);
-        if (!isset($this->data[$cachePath])) {
-            $data = $this->cache->load(self::CONFIG_TYPE . '/' . $cachePath);
-            if (!$data) {
-                $this->data[$cachePath] = new DataObject([$cachePath => $this->source->get($cachePath)]);
-                $this->cache->save(
-                    serialize($this->data),
-                    self::CONFIG_TYPE . '/' . $cachePath,
-                    [Translate::TYPE_IDENTIFIER]
-                );
-            } else {
-                $this->data = unserialize($data);
-            }
+        if (!$this->data) {
+            $this->data = new DataObject($this->source->get());
         }
-        return $this->data[$cachePath]->getData($path);
-    }
 
-    /**
-     * Build cache path
-     *
-     * @param string $path
-     * @return string
-     */
-    private function getCachePath($path)
-    {
-        return implode('/', array_slice(explode('/', $path), 0, $this->cachingNestedLevel));
+        return $this->data->getData($path);
     }
 
     /**
@@ -97,6 +58,5 @@ class Translation implements ConfigTypeInterface
     public function clean()
     {
         $this->data = null;
-        $this->cache->clean(\Zend_Cache::CLEANING_MODE_MATCHING_TAG, [Translate::TYPE_IDENTIFIER]);
     }
 }
