53cc8ece1147800821e6606f326a58002e58cd7c
Andrii Dimov <adimov@magento.com>
2016-10-25 19:04:55 +0300

    MAGETWO-55729: [Customer] Optimize performance for bundled products with lots of product options
     - MAGETWO-59254: Write integration tests for \Magento\Bundle\Pricing\Adjustment\Calculator

--- dev/tests/integration/testsuite/Magento/Bundle/_files/PriceCalculator/dynamic_bundle_product_with_catalog_rule.php
+++ dev/tests/integration/testsuite/Magento/Bundle/_files/PriceCalculator/dynamic_bundle_product_with_catalog_rule.php
@@ -4,5 +4,5 @@
  * See COPYING.txt for license details.
  */
 
-require __DIR__ . '/dynamic_bundle_product.php.php';
+require __DIR__ . '/dynamic_bundle_product.php';
 require __DIR__ . '/../../../CatalogRule/_files/catalog_rule_10_off_not_logged.php';
