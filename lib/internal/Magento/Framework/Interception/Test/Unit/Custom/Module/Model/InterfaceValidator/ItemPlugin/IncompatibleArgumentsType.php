08db17dee9211ca65eb9d6449756b26dbe4a4f6d
Danny Verkade <danny@cream.nl>
2016-10-25 19:42:54 +0200

    Fixed coding standard violation: Line exceeds maximum limit of 120 characters; contains 121 characters

--- lib/internal/Magento/Framework/Interception/Test/Unit/Custom/Module/Model/InterfaceValidator/ItemPlugin/IncompatibleArgumentsType.php
+++ lib/internal/Magento/Framework/Interception/Test/Unit/Custom/Module/Model/InterfaceValidator/ItemPlugin/IncompatibleArgumentsType.php
@@ -6,17 +6,19 @@
 
 namespace Magento\Framework\Interception\Test\Unit\Custom\Module\Model\InterfaceValidator\ItemPlugin;
 
+use \Magento\Framework\Interception\Test\Unit\Custom\Module\Model\InterfaceValidator\ItemWithArguments;
+
 class IncompatibleArgumentsType
 {
     /**
-     * @param \Magento\Framework\Interception\Test\Unit\Custom\Module\Model\InterfaceValidator\ItemWithArguments $subject
+     * @param ItemWithArguments $subject
      * @param array $names
      * @return int
      *
      * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
     public function beforeGetItem(
-        \Magento\Framework\Interception\Test\Unit\Custom\Module\Model\InterfaceValidator\ItemWithArguments $subject,
+        ItemWithArguments $subject,
         array $names
     ) {
         return count($names);
