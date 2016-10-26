210afe6ce81573c0971fb85cdfe7700d9655631f
Danny Verkade <danny@cream.nl>
2016-10-26 15:36:31 +0200

    Fixed coding standard violation: is_null must be avoided. Use strict comparison instead.

--- lib/internal/Magento/Framework/Config/Dom.php
+++ lib/internal/Magento/Framework/Config/Dom.php
@@ -240,7 +240,7 @@ class Dom
      */
     protected function _getNodePathByParent(\DOMElement $node, $parentPath)
     {
-        $prefix = is_null($this->rootNamespace) ? '' : self::ROOT_NAMESPACE_PREFIX . ':';
+        $prefix = $this->rootNamespace === null ? '' : self::ROOT_NAMESPACE_PREFIX . ':';
         $path = $parentPath . '/' . $prefix . $node->tagName;
         $idAttribute = $this->nodeMergingConfig->getIdAttribute($path);
         if (is_array($idAttribute)) {
@@ -430,7 +430,7 @@ class Dom
      */
     private function _getAttributeName($attribute)
     {
-        if (!is_null($attribute->prefix) && !empty($attribute->prefix)) {
+        if ($attribute->prefix !== null && !empty($attribute->prefix)) {
             $attributeName = $attribute->prefix . ':' . $attribute->name;
         } else {
             $attributeName = $attribute->name;
