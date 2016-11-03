6da651aad7c7ce762433f89874230db099d350ec
Iryna Lagno <ilagno@magento.com>
2016-11-03 16:04:46 +0200

    MAGETWO-59916: Problem arose when applying a coupon. ~QUANS~

---
+++ dev/tests/integration/testsuite/Magento/Checkout/Controller/Cart/Index/CouponPostTest.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Checkout\Controller\Cart\Index;
+
+class CouponPostTest extends \Magento\TestFramework\TestCase\AbstractController
+{
+    /**
+     * Test for \Magento\Checkout\Controller\Cart\CouponPost::execute() with simple product
+     *
+     * @magentoDataFixture Magento/Checkout/_files/quote_with_simple_product.php
+     */
+    public function testExecute()
+    {
+        /** @var $session \Magento\Checkout\Model\Session  */
+        $session = $this->_objectManager->create(\Magento\Checkout\Model\Session::class);
+        $quote = $session->getQuote();
+        $quote->setData('trigger_recollect', 1)->setTotalsCollectedFlag(true)->save();
+        $inputData = [
+            'remove' => 0,
+            'coupon_code' => 'test'
+        ];
+        $this->getRequest()->setPostValue($inputData);
+        $this->dispatch(
+            'checkout/cart/couponPost/'
+        );
+
+        $this->assertSessionMessages($this->equalTo(['The coupon code "test" is not valid.']),
+            \Magento\Framework\Message\MessageInterface::TYPE_ERROR);
+    }
+}
