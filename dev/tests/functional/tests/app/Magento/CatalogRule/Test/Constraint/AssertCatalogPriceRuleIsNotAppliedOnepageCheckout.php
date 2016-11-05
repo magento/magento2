60561cfc9f682b0c6dca199646f71503d9451767
Andrei Ziblickiy <andrei_ziblitski@epam.com>
2016-11-05 17:04:03 +0300

    MTO-16: [Constraint] Add verification that catalog price rule isn't applied after deletion
    - Constraint added

---
+++ dev/tests/functional/tests/app/Magento/CatalogRule/Test/Constraint/AssertCatalogPriceRuleIsNotAppliedOnepageCheckout.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\CatalogRule\Test\Constraint;
+
+use Magento\CatalogRule\Test\Constraint\AssertCatalogPriceRuleAppliedOnepageCheckout;
+
+/**
+ * Assert that Catalog Price Rule is not applied on OnePage Checkout page.
+ */
+class AssertCatalogPriceRuleIsNotAppliedOnepageCheckout extends AssertCatalogPriceRuleAppliedOnepageCheckout
+{
+
+}
