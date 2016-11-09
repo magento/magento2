68b317dee00c18315f03bcbd39bb78426b04b22a
Teun Lassche <contact@thlassche.nl>
2016-11-09 13:25:13 +0100

    Add docblock

--- app/code/Magento/Contact/Block/ContactForm.php
+++ app/code/Magento/Contact/Block/ContactForm.php
@@ -32,6 +32,9 @@ class ContactForm extends Template
         return $this->getUrl('contact/index/post', ['_secure' => true]);
     }
 
+	/**
+	 * @return $this
+	 */
 	protected function _prepareLayout()
 	{
 		$this->pageConfig->getTitle()->set(__('Contact Us'));
