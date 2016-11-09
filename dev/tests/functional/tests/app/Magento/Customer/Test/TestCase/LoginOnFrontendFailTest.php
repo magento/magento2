6f07d076881568d6ab01942620875268571f05ce
Vitali Kirychenka <vitali_kirychenka@epam.com>
2016-11-09 17:05:32 +0300

    MTO-10: [Test] Login customer with invalid credentials
    - Test implemented.

---
+++ dev/tests/functional/tests/app/Magento/Customer/Test/TestCase/LoginOnFrontendFailTest.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Customer\Test\TestCase;
+
+use Magento\Customer\Test\Page\CustomerAccountLogin;
+use Magento\Mtf\Fixture\FixtureFactory;
+use Magento\Mtf\TestCase\Injectable;
+use Magento\Customer\Test\Fixture\Customer;
+
+/**
+ * Precondition:
+ * 1. Customer is created.
+ *
+ * Steps:
+ * 1. Open login page.
+ * 2. Fill email.
+ * 3. Fill wrong password.
+ * 3. Click login.
+ * 4. Check error message.
+ *
+ * @group Customer
+ * @ZephyrId MAGETWO-16883
+ */
+class LoginOnFrontendFailTest extends Injectable
+{
+    /* tags */
+    const MVP = 'yes';
+    /* end tags */
+
+    /**
+     * Login on frontend.
+     *
+     * @param Customer $customer
+     * @param CustomerAccountLogin $loginPage
+     * @param FixtureFactory $fixtureFactory
+     * @return void
+     */
+    public function test(Customer $customer, CustomerAccountLogin $loginPage, FixtureFactory $fixtureFactory)
+    {
+        // Precondition
+        $customer->persist();
+        $customerData = $customer->getData();
+        $customerData['password'] = 'fail';
+        $customerData['group_id'] = ['dataset' => 'default'];
+        $failCustomer = $fixtureFactory->createByCode('customer', ['data' => $customerData]);
+
+        // Steps
+        $loginPage->open();
+        $loginPage->getLoginBlock()->fill($failCustomer);
+        $loginPage->getLoginBlock()->submit();
+    }
+}
