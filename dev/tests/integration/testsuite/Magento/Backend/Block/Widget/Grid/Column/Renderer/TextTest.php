4517d31d7a9df9b2cbb0e74d5484ba046740c7f5
Igor Melnikov <imelnikov@magento.com>
2016-11-10 13:55:21 -0600

    MAGETWO-60539: Remove object data in \Magento\Indexer\Model\Config\Data
    Adding integration test

--- dev/tests/integration/testsuite/Magento/Backend/Block/Widget/Grid/Column/Renderer/TextTest.php
+++ dev/tests/integration/testsuite/Magento/Backend/Block/Widget/Grid/Column/Renderer/TextTest.php
@@ -15,7 +15,9 @@ use Magento\Backend\Model\Locale\Manager;
 use Magento\Framework\TranslateInterface;
 
 /**
+ * @magentoComponentsDir Magento/Backend/Block/_files/app/code/Magento
  * @magentoAppIsolation enabled
+ * @magentoDbIsolation enabled
  * @magentoAppArea adminhtml
  */
 class TextTest extends \PHPUnit_Framework_TestCase
