765855c5f324abd0bae8f5774cee7df0fbdc09d4
Myroslav Dobra <dmaraptor@gmail.com>
2016-11-11 11:16:25 +0200

    MAGETWO-56925: [Backport] - “No Payment method available” when customer tries to ship his items to billing restricted country for 2.0.x

--- app/code/Magento/Checkout/view/frontend/web/js/view/billing-address.js
+++ app/code/Magento/Checkout/view/frontend/web/js/view/billing-address.js
@@ -7,6 +7,7 @@
 define(
     [
         'ko',
+        'underscore',
         'Magento_Ui/js/form/form',
         'Magento_Customer/js/model/customer',
         'Magento_Customer/js/model/address-list',
@@ -22,6 +23,7 @@ define(
     ],
     function (
         ko,
+        _,
         Component,
         customer,
         addressList,
@@ -39,15 +41,20 @@ define(
 
         var lastSelectedBillingAddress = null,
             newAddressOption = {
+                /**
+                 * Get new address label
+                 * @returns {String}
+                 */
                 getAddressInline: function () {
-                return $t('New Address');
-            },
+                    return $t('New Address');
+                },
                 customerAddressId: null
             },
             countryData = customerData.get('directory-data'),
             addressOptions = addressList().filter(function (address) {
-            return address.getType() == 'customer-address';
-        });
+                return address.getType() == 'customer-address';
+            });
+
         addressOptions.push(newAddressOption);
 
         return Component.extend({
@@ -78,7 +85,7 @@ define(
                         isAddressDetailsVisible: quote.billingAddress() != null,
                         isAddressFormVisible: !customer.isLoggedIn() || addressOptions.length == 1,
                         isAddressSameAsShipping: false,
-                        saveInAddressBook: true
+                        saveInAddressBook: 1
                     });
 
                 quote.billingAddress.subscribe(function (newAddress) {
@@ -93,6 +100,8 @@ define(
 
                     if (newAddress != null && newAddress.saveInAddressBook !== undefined) {
                         this.saveInAddressBook(newAddress.saveInAddressBook);
+                    } else {
+                        this.saveInAddressBook(1);
                     }
                     this.isAddressDetailsVisible(true);
                 }, this);
@@ -118,9 +127,8 @@ define(
             useShippingAddress: function () {
                 if (this.isAddressSameAsShipping()) {
                     selectBillingAddress(quote.shippingAddress());
-                    if (window.checkoutConfig.reloadOnBillingAddress) {
-                        setBillingAddressAction(globalMessageList);
-                    }
+
+                    this.updateAddresses();
                     this.isAddressDetailsVisible(true);
                 } else {
                     lastSelectedBillingAddress = quote.billingAddress();
@@ -139,36 +147,31 @@ define(
                 if (this.selectedAddress() && this.selectedAddress() != newAddressOption) {
                     selectBillingAddress(this.selectedAddress());
                     checkoutData.setSelectedBillingAddress(this.selectedAddress().getKey());
-                    if (window.checkoutConfig.reloadOnBillingAddress) {
-                        setBillingAddressAction(globalMessageList);
-                    }
                 } else {
                     this.source.set('params.invalid', false);
                     this.source.trigger(this.dataScopePrefix + '.data.validate');
+
                     if (this.source.get(this.dataScopePrefix + '.custom_attributes')) {
                         this.source.trigger(this.dataScopePrefix + '.custom_attributes.data.validate');
-                    };
+                    }
 
                     if (!this.source.get('params.invalid')) {
                         var addressData = this.source.get(this.dataScopePrefix),
                             newBillingAddress;
 
                         if (customer.isLoggedIn() && !this.customerHasAddresses) {
-                            this.saveInAddressBook(true);
+                            this.saveInAddressBook(1);
                         }
-                        addressData.save_in_address_book = this.saveInAddressBook();
+                        addressData['save_in_address_book'] = this.saveInAddressBook() ? 1 : 0;
                         newBillingAddress = createBillingAddress(addressData);
 
                         // New address must be selected as a billing address
                         selectBillingAddress(newBillingAddress);
                         checkoutData.setSelectedBillingAddress(newBillingAddress.getKey());
                         checkoutData.setNewCustomerBillingAddress(addressData);
-
-                        if (window.checkoutConfig.reloadOnBillingAddress) {
-                            setBillingAddressAction(globalMessageList);
-                        }
                     }
                 }
+                this.updateAddresses();
             },
 
             /**
@@ -190,8 +193,8 @@ define(
                     // restore 'Same As Shipping' checkbox state
                     this.isAddressSameAsShipping(
                         quote.billingAddress() != null &&
-                            quote.billingAddress().getCacheKey() == quote.shippingAddress().getCacheKey() &&
-                            !quote.isVirtual()
+                        quote.billingAddress().getCacheKey() == quote.shippingAddress().getCacheKey() &&
+                        !quote.isVirtual()
                     );
                     this.isAddressDetailsVisible(true);
                 }
@@ -219,6 +222,26 @@ define(
              */
             getCountryName: function (countryId) {
                 return countryData()[countryId] != undefined ? countryData()[countryId].name : '';
+            },
+
+            /**
+             * Trigger action to update shipping and billing addresses
+             */
+            updateAddresses: function () {
+                if (window.checkoutConfig.reloadOnBillingAddress ||
+                    !window.checkoutConfig.displayBillingOnPaymentMethod
+                ) {
+                    setBillingAddressAction(globalMessageList);
+                }
+            },
+
+            /**
+             * Get code
+             * @param {Object} parent
+             * @returns {String}
+             */
+            getCode: function (parent) {
+                return _.isFunction(parent.getCode) ? parent.getCode() : 'shared';
             }
         });
     }
