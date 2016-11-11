9b9aa9a76dda3845119aa3f8b38b8bdc479b6b51
Oleh Posyniak <oposyniak@magento.com>
2016-11-11 10:32:45 +0200

    MAGETWO-48516: Most of category fields don't have Use Default Value option in Store/StoreView scope on the redesigned category page

--- app/code/Magento/Catalog/Model/Category/DataProvider.php
+++ app/code/Magento/Catalog/Model/Category/DataProvider.php
@@ -228,15 +228,15 @@ class DataProvider extends \Magento\Ui\DataProvider\AbstractDataProvider
      */
     private function resolveParentInheritance(Category $category, array $meta)
     {
-//        if (!$category->getParentId() || !$this->getArrayManager()->findPath('custom_use_parent_settings', $meta)) {
-//            return $meta;
-//        }
-//
-//        $meta = $this->getArrayManager()->merge(
-//            [$this->getArrayManager()->findPath('custom_use_parent_settings', $meta), 'arguments/data/config'],
-//            $meta,
-//            ['visible' => false]
-//        );
+        if (!$category->getParentId() || !$this->getArrayManager()->findPath('custom_use_parent_settings', $meta)) {
+            return $meta;
+        }
+
+        $meta = $this->getArrayManager()->merge(
+            [$this->getArrayManager()->findPath('custom_use_parent_settings', $meta), 'arguments/data/config'],
+            $meta,
+            ['visible' => false]
+        );
 
         return $meta;
     }
