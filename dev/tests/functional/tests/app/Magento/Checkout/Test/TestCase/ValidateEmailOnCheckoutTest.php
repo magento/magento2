3733ba83716da18c6b10e174493f98aeb018f381
Vitali Kirychenka <vitali_kirychenka@epam.com>
2016-11-14 11:24:58 +0300

    MTO-55: [Test] Email validation for Guest on checkout flow
    - Test implemented

---
+++ dev/tests/functional/tests/app/Magento/Checkout/Test/TestCase/ValidateEmailOnCheckoutTest.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Checkout\Test\TestCase;
+
+use Magento\Catalog\Test\Fixture\CatalogProductSimple;
+use Magento\Catalog\Test\Page\Product\CatalogProductView;
+use Magento\Checkout\Test\Page\CheckoutCart;
+use Magento\Checkout\Test\Page\CheckoutOnepage;
+use Magento\Customer\Test\Fixture\Customer;
+use Magento\Mtf\Client\BrowserInterface;
+use Magento\Mtf\TestCase\Injectable;
+
+/**
+ * Precondition:
+ * 1. Simple product is created
+ * 2. Clear shopping cart
+ *
+ * Steps:
+ * 1. Go to Storefront as Guest
+ * 2. Add simple product to shopping cart
+ * 3. Go to Checkout
+ * 4. Enter the email according to the data set
+ * 5. Perform assertions
+ *
+ * @ZephyrId MAGETWO-42543
+ */
+class ValidateEmailOnCheckoutTest extends Injectable
+{
+    /* tags */
+    const MVP = 'yes';
+    /* end tags */
+
+    public function test(
+        CatalogProductSimple $product,
+        CheckoutCart $cartPage,
+        CatalogProductView $catalogProductView,
+        BrowserInterface $browser,
+        CheckoutOnepage $checkoutOnepage,
+        Customer $customer
+    ) {
+        //Preconditions
+        $product->persist();
+
+        $cartPage->open();
+        $cartPage->getCartBlock()->clearShoppingCart();
+
+        //Step
+        $browser->open($_ENV['app_frontend_url'] . $product->getUrlKey() . '.html');
+        $productView = $catalogProductView->getViewBlock();
+        $productView->fillOptions($product);
+        $productView->setQty($product->getCheckoutData()['qty']);
+        $productView->clickAddToCart();
+        $catalogProductView->getMessagesBlock()->waitSuccessMessage();
+
+        $checkoutOnepage->open();
+        $checkoutOnepage->getShippingBlock()->fill($customer);
+    }
+}
