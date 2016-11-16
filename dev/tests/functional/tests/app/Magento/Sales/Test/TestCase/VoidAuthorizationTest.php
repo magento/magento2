d47cd7845b72ef1c6170e33dfcebfda6f480bcb6
rliukshyn <rliukshyn@magento.com>
2016-11-16 14:23:48 +0200

    MTA-3844: Deliver Issues to mainline

--- dev/tests/functional/tests/app/Magento/Sales/Test/TestCase/VoidAuthorizationTest.php
+++ dev/tests/functional/tests/app/Magento/Sales/Test/TestCase/VoidAuthorizationTest.php
@@ -35,6 +35,7 @@ class VoidAuthorizationTest extends Scenario
     /* tags */
     const MVP = 'yes';
     const TEST_TYPE = '3rd_party_test';
+    const SEVERITY = 'S0';
     /* end tags */
 
     /**
