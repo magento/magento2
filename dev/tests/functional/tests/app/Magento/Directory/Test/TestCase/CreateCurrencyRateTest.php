d2303bd259b99d62f41f4651058c5a97006ed9f9
Vitaliy Goncharenko <vgoncharenko@magento.com>
2016-11-16 11:27:48 +0200

    MAGETWO-60283: Prices of configurable product are incorrectly recalculated when display currency is changed
    - fixed import currency rate action

--- dev/tests/functional/tests/app/Magento/Directory/Test/TestCase/CreateCurrencyRateTest.php
+++ dev/tests/functional/tests/app/Magento/Directory/Test/TestCase/CreateCurrencyRateTest.php
@@ -80,7 +80,6 @@ class CreateCurrencyRateTest extends Injectable
 
         // Steps:
         $this->currencyIndexPage->open();
-        $this->currencyIndexPage->getCurrencyRateForm()->clickImportButton();
         $this->currencyIndexPage->getCurrencyRateForm()->fill($currencyRate);
         $this->currencyIndexPage->getFormPageActions()->save();
 
