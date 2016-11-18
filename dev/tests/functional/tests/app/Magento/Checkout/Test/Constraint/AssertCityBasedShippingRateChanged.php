47fe6dc721594fe3b81bab5e6afe3fe3d4cf5fed
Ostap Smolyar <ostap.smolyar@gmail.com>
2016-11-18 12:18:38 +0200

    MTA-3785: Create auto test to check that shipping rates are reloaded based on City field value

---
+++ dev/tests/functional/tests/app/Magento/Checkout/Test/Constraint/AssertCityBasedShippingRateChanged.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Checkout\Test\Constraint;
+
+use Magento\Checkout\Test\Page\CheckoutOnepage;
+use Magento\Mtf\Constraint\AbstractConstraint;
+
+/**
+ * Check that Shipping rate changes due to City change.
+ */
+class AssertCityBasedShippingRateChanged extends AbstractConstraint
+{
+    /**
+     * Wait element.
+     *
+     * @var string
+     */
+    private $waitElement = '.loading-mask';
+
+    /**
+     * @var CheckoutOnepage
+     */
+    private $checkoutOnepage;
+
+    /**
+     * Assert that Shipping rate changed on City change.
+     *
+     * @param CheckoutOnepage $checkoutOnepage
+     * @return void
+     */
+    public function processAssert(CheckoutOnepage $checkoutOnepage)
+    {
+        $this->checkoutOnepage = $checkoutOnepage;
+        $rateChanged = $this->waitLoader();
+
+        \PHPUnit_Framework_Assert::assertTrue(
+            $rateChanged,
+            'Shipping rate has not been changed.'
+        );
+    }
+
+    /**
+     * Wait load block.
+     *
+     * @return bool
+     */
+    protected function waitLoader()
+    {
+        $shippingBlock = $this->checkoutOnepage->getShippingBlock();
+        return (bool)$shippingBlock->waitUntil(
+            function () use ($shippingBlock) {
+                return $shippingBlock->find($this->waitElement)->isVisible() ? true : null;
+            }
+        );
+    }
+
+    /**
+     * Returns a string representation of successful assertion.
+     *
+     * @return string
+     */
+    public function toString()
+    {
+        return "Shipping rate has been changed.";
+    }
+}
---
+++ dev/tests/functional/tests/app/Magento/Shipping/Test/TestCase/CityBasedShippingRateTest.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Shipping\Test\TestCase;
+
+use Magento\Mtf\TestCase\Scenario;
+
+/**
+ * Preconditions:
+ * 1. Configure shipping method.
+ * 2. Configure payment method.
+ * 3. Create products.
+ * 4. Create and setup customer.
+ * 5. Create sales rule according to dataset.
+ *
+ * Steps:
+ * 1. Go to Frontend.
+ * 2. Add products to the cart.
+ * 3. Apply discounts in shopping cart according to dataset.
+ * 4. In 'Estimate Shipping and Tax' section specify destination using values from Test Data.
+ * 5. Click the 'Get a Quote' button.
+ * 6. In the section appeared select Shipping method, click the 'Update Total' button.
+ * 7. Click the 'Proceed to Checkout' button.
+ * 8. Select checkout method according to dataset.
+ * 9. Fill billing information and select the 'Ship to this address' option.
+ * 10. Select shipping method.
+ * 11. Select payment method (use reward points and store credit if available).
+ * 12. Verify order total on review step.
+ * 13. Place order.
+ * 14. Perform assertions.
+ *
+ * @group Order_Management
+ * @ZephyrId MAGETWO-56124
+ */
+class CityBasedShippingRateTest extends Scenario
+{
+    /* tags */
+    const MVP = 'yes';
+    const TEST_TYPE = '3rd_party_test';
+    /* end tags */
+
+    /**
+     * Runs City Based Shipping Rate test.
+     *
+     * @return void
+     */
+    public function test()
+    {
+        $this->executeScenario();
+    }
+}
