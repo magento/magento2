9933c46a4744cf0b964aec4c9a5151dc98339dff
Danny Verkade <danny@cream.nl>
2016-11-18 23:02:57 +0100

    - Fixed coding standard error: Variable "line_number" is not in valid camel caps format - Fixed coding standard error: Line exceeds maximum limit of 120 characters; - Fixed coding standard error: Variable "tmp_value" is not in valid camel caps format - Fixed coding standard error: Variable "tmp_line" is not in valid camel caps format

--- lib/internal/Magento/Framework/File/CsvMulty.php
+++ lib/internal/Magento/Framework/File/CsvMulty.php
@@ -25,25 +25,23 @@ class CsvMulty extends \Magento\Framework\File\Csv
     {
         $data = [];
         $csvData = $this->getData($file);
-        $line_number = 0;
+        $lineNumber = 0;
         foreach ($csvData as $rowData) {
-            $line_number++;
+            $lineNumber++;
             if (isset($rowData[$keyIndex])) {
                 if (isset($data[$rowData[$keyIndex]])) {
                     if (isset($data[$rowData[$keyIndex]]['duplicate'])) {
-                        #array_push($data[$rowData[$keyIndex]]['duplicate'],array('line' => $line_number,'value' => isset($rowData[$valueIndex]) ? $rowData[$valueIndex] : null));
-                        $data[$rowData[$keyIndex]]['duplicate']['line'] .= ', ' . $line_number;
+                        $data[$rowData[$keyIndex]]['duplicate']['line'] .= ', ' . $lineNumber;
                     } else {
-                        $tmp_value = $data[$rowData[$keyIndex]]['value'];
-                        $tmp_line = $data[$rowData[$keyIndex]]['line'];
+                        $tmpValue = $data[$rowData[$keyIndex]]['value'];
+                        $tmpLine = $data[$rowData[$keyIndex]]['line'];
                         $data[$rowData[$keyIndex]]['duplicate'] = [];
-                        #array_push($data[$rowData[$keyIndex]]['duplicate'],array('line' => $tmp_line.' ,'.$line_number,'value' => $tmp_value));
-                        $data[$rowData[$keyIndex]]['duplicate']['line'] = $tmp_line . ' ,' . $line_number;
-                        $data[$rowData[$keyIndex]]['duplicate']['value'] = $tmp_value;
+                        $data[$rowData[$keyIndex]]['duplicate']['line'] = $tmpLine . ' ,' . $lineNumber;
+                        $data[$rowData[$keyIndex]]['duplicate']['value'] = $tmpValue;
                     }
                 } else {
                     $data[$rowData[$keyIndex]] = [];
-                    $data[$rowData[$keyIndex]]['line'] = $line_number;
+                    $data[$rowData[$keyIndex]]['line'] = $lineNumber;
                     $data[$rowData[$keyIndex]]['value'] = isset($rowData[$valueIndex]) ? $rowData[$valueIndex] : null;
                 }
             }
