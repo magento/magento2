e612fb82fe6280bb76f86e870cf3049148e99efb
Vadim Zubovich <vadim_zubovich@epam.com>
2016-11-21 14:12:25 +0300

    MTO-57: [Variation] Add variation to Update Simple Product test to verify data overrirding on Store View level
    - Added assert

---
+++ dev/tests/functional/tests/app/Magento/Catalog/Test/Constraint/AssertProductNameOnStorefront.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Catalog\Test\Constraint;
+
+use Magento\Catalog\Test\Page\Product\CatalogProductView;
+use Magento\Mtf\Client\BrowserInterface;
+use Magento\Mtf\Constraint\AbstractConstraint;
+use Magento\Mtf\Fixture\FixtureInterface;
+use Magento\Cms\Test\Page\CmsIndex;
+
+/**
+ * Assert product name in different store views on product view page.
+ */
+class AssertProductNameOnStorefront extends AbstractConstraint
+{
+    /**
+     * Assert that product name is correct on the storefront in different store views.
+     *
+     * @param CatalogProductView $catalogProductView
+     * @param CmsIndex $cmsIndex
+     * @param BrowserInterface $browser
+     * @param FixtureInterface $initialProduct
+     * @param array $stores
+     * @param array $productNames
+     * @return void
+     */
+    public function processAssert(
+        CatalogProductView $catalogProductView,
+        CmsIndex $cmsIndex,
+        BrowserInterface $browser,
+        FixtureInterface $initialProduct,
+        array $stores,
+        array $productNames
+    ) {
+        $browser->open($_ENV['app_frontend_url'] . $initialProduct->getUrlKey() . '.html');
+        $cmsIndex->getLinksBlock()->waitWelcomeMessage();
+        foreach ($stores as $store) {
+            $cmsIndex->getStoreSwitcherBlock()->selectStoreView($store->getName());
+            $cmsIndex->getLinksBlock()->waitWelcomeMessage();
+            $browser->open($_ENV['app_frontend_url'] . $initialProduct->getUrlKey() . '.html');
+            \PHPUnit_Framework_Assert::assertEquals(
+                $productNames[$store->getStoreId()],
+                $catalogProductView->getViewBlock()->getProductName(),
+                sprintf('Wrong product name is displayed for %s store view.', $store->getName())
+            );
+        }
+    }
+
+    /**
+     * Returns a string representation of the object.
+     *
+     * @return string
+     */
+    public function toString()
+    {
+        return 'Product name is correct on the storefront';
+    }
+}
