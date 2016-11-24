c54b46f5e9bffad93d440d31eb1d5df3e6d9ac0f
RomaKis <romaikiss@gmail.com>
2016-11-24 16:03:31 +0200

    MAGETWO-60965: [Backport] - New attribute is not visible when create new address - for 2.0

---
+++ dev/tests/functional/tests/app/Magento/Customer/Test/Page/CustomerAddressEdit.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Customer\Test\Page;
+
+use Magento\Mtf\Client\Locator;
+use Magento\Mtf\Factory\Factory;
+use Magento\Mtf\Page\Page;
+
+/**
+ * Customer Address Edit page.
+ */
+class CustomerAddressEdit extends Page
+{
+    /**
+     * URL for Customer Address Edit page.
+     */
+    const MCA = 'customer/address/edit';
+
+    /**
+     * Customer Address Edit form.
+     *
+     * @var string
+     */
+    protected $editForm = '#form-validate';
+
+    /**
+     * Init page. Set page url.
+     *
+     * @return void
+     */
+    protected function initUrl()
+    {
+        $this->url = $_ENV['app_frontend_url'] . self::MCA;
+    }
+
+    /**
+     * Get Customer Address Edit form.
+     *
+     * @return \Magento\Customer\Test\Block\Address\Edit
+     */
+    public function getEditForm()
+    {
+        return Factory::getBlockFactory()->getMagentoCustomerAddressEdit(
+                $this->browser->find($this->editForm, Locator::SELECTOR_CSS)
+        );
+    }
+}
