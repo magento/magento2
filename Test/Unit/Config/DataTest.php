2c83b031fb2fab439f91f101138bdfaa2c6dd5f3
Joan He <johe@magento.com>
2016-11-28 15:28:02 -0600

    MAGETWO-60442: Add optional SerializerInterface dependency to child classes of \Magento\Framework\Config\Data

--- Test/Unit/Config/DataTest.php
+++ Test/Unit/Config/DataTest.php
@@ -52,35 +52,6 @@ class DataTest extends \PHPUnit_Framework_TestCase
             ->disableOriginalConstructor()
             ->getMock();
         $this->serializerMock = $this->getMock(\Magento\Framework\Serialize\SerializerInterface::class);
-        $this->mockObjectManager(
-            [\Magento\Framework\Serialize\SerializerInterface::class => $this->serializerMock]
-        );
-    }
-
-    protected function tearDown()
-    {
-        $reflectionProperty = new \ReflectionProperty(\Magento\Framework\App\ObjectManager::class, '_instance');
-        $reflectionProperty->setAccessible(true);
-        $reflectionProperty->setValue(null);
-    }
-
-    /**
-     * Mock application object manager to return configured dependencies.
-     *
-     * @param array $dependencies
-     * @return void
-     */
-    private function mockObjectManager($dependencies)
-    {
-        $dependencyMap = [];
-        foreach ($dependencies as $type => $instance) {
-            $dependencyMap[] = [$type, $instance];
-        }
-        $objectManagerMock = $this->getMock(\Magento\Framework\ObjectManagerInterface::class);
-        $objectManagerMock->expects($this->any())
-            ->method('get')
-            ->will($this->returnValueMap($dependencyMap));
-        \Magento\Framework\App\ObjectManager::setInstance($objectManagerMock);
     }
 
     public function testGet()
@@ -113,7 +84,8 @@ class DataTest extends \PHPUnit_Framework_TestCase
                 'xmlReader' => $this->xmlReaderMock,
                 'cache' => $this->cacheMock,
                 'envReader' => $this->envReaderMock,
-                'remoteServiceReader' => $this->remoteServiceReaderMock
+                'remoteServiceReader' => $this->remoteServiceReaderMock,
+                'serializer' => $this->serializerMock,
             ]
         );
     }
