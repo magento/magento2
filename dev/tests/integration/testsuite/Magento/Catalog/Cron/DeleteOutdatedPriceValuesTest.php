95daeecb67a98502f49cebfef63a1e6dc9b36582
Yaroslav Onischenko <yonischenko@magento.com>
2016-11-29 18:55:08 +0200

    MAGETWO-54704: [Backport] [GITHUB] Product prices not scoped to Website - scoping seems to be at store view level #5133 #7251 - for 2.1

---
+++ dev/tests/integration/testsuite/Magento/Catalog/Cron/DeleteOutdatedPriceValuesTest.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Catalog\Cron;
+
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Framework\App\Config\MutableScopeConfigInterface;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+
+class DeleteOutdatedPriceValuesTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \Magento\Catalog\Cron\DeleteOutdatedPriceValues
+     */
+    private $cron;
+
+    /**
+     * @var ProductRepositoryInterface
+     */
+    private $productRepository;
+
+    /**
+     * @var \Magento\Store\Model\Store
+     */
+    private $store;
+
+    /**
+     * @var \Magento\Framework\ObjectManagerInterface
+     */
+    private $objectManager;
+
+    public function setUp()
+    {
+        $this->objectManager = \Magento\TestFramework\Helper\Bootstrap::getObjectManager();
+        $this->productRepository = $this->objectManager->create(ProductRepositoryInterface::class);
+        $this->store = $this->objectManager->create(\Magento\Store\Model\Store::class);
+        $this->cron = $this->objectManager->create(\Magento\Catalog\Cron\DeleteOutdatedPriceValues::class);
+    }
+
+    /**
+     * @magentoDataFixture Magento/Catalog/_files/product_simple.php
+     * @magentoDataFixture Magento/Store/_files/second_website_with_two_stores.php
+     * @magentoConfigFixture current_store catalog/price/scope 2
+     * @magentoDbIsolation enabled
+     */
+    public function testExecute()
+    {
+        $secondStoreId = $this->store->load('fixture_second_store')->getId();
+        /** @var \Magento\Catalog\Model\Product\Action $productAction */
+        $productAction = $this->objectManager->create(
+            \Magento\Catalog\Model\Product\Action::class
+        );
+
+        $product = $this->productRepository->get('simple');
+        $productResource = $this->objectManager->create(\Magento\Catalog\Model\ResourceModel\Product::class);
+
+        $productId = $product->getId();
+        $productAction->updateWebsites(
+            [$productId],
+            [$this->store->load('fixture_second_store')->getWebsiteId()],
+            'add'
+        );
+        $product->setOrigData();
+        $product->setStoreId($secondStoreId);
+        $product->setPrice(9.99);
+
+        $productResource->save($product);
+        $attribute = $this->objectManager->get(\Magento\Eav\Model\Config::class)
+            ->getAttribute(
+                'catalog_product',
+                'price'
+            );
+        $this->assertEquals(
+            '9.99',
+            $productResource->getAttributeRawValue($productId, $attribute->getId(), $secondStoreId)
+        );
+        /** @var MutableScopeConfigInterface $config */
+        $config = $this->objectManager->get(
+            MutableScopeConfigInterface::class
+        );
+        $config->setValue(
+            \Magento\Store\Model\Store::XML_PATH_PRICE_SCOPE,
+            \Magento\Store\Model\Store::PRICE_SCOPE_GLOBAL,
+            ScopeConfigInterface::SCOPE_TYPE_DEFAULT
+        );
+        /** @var \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig */
+        $this->cron->execute();
+        $this->assertEquals(
+            '10.0000',
+            $productResource->getAttributeRawValue($productId, $attribute->getId(), $secondStoreId)
+        );
+    }
+}
