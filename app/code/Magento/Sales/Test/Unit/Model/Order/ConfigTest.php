6447324bd406e811b13f7d942c6401ddddab1205
Roman Ganin <rganin@magento.com>
2016-12-05 12:40:03 +0200

    MAGETWO-61652: Magento/Sales/Model/Order/Config.php and unit tests

--- app/code/Magento/Sales/Test/Unit/Model/Order/ConfigTest.php
+++ app/code/Magento/Sales/Test/Unit/Model/Order/ConfigTest.php
@@ -4,6 +4,8 @@
  * See COPYING.txt for license details.
  */
 namespace Magento\Sales\Test\Unit\Model\Order;
+use Magento\Framework\DataObject;
+use Magento\Sales\Model\ResourceModel\Order\Status\Collection;
 
 /**
  * Class ConfigTest
@@ -44,28 +46,28 @@ class ConfigTest extends \PHPUnit_Framework_TestCase
     public function testGetInvisibleOnFrontStatuses()
     {
         $statuses = [
-            new \Magento\Framework\DataObject(
+            new DataObject(
                 [
                     'status' => 'canceled',
                     'is_default' => 1,
                     'visible_on_front' => 1,
                 ]
             ),
-            new \Magento\Framework\DataObject(
+            new DataObject(
                 [
                     'status' => 'complete',
                     'is_default' => 1,
                     'visible_on_front' => 0,
                 ]
             ),
-            new \Magento\Framework\DataObject(
+            new DataObject(
                 [
                     'status' => 'processing',
                     'is_default' => 1,
                     'visible_on_front' => 1,
                 ]
             ),
-            new \Magento\Framework\DataObject(
+            new DataObject(
                 [
                     'status' => 'pending_payment',
                     'is_default' => 1,
@@ -76,7 +78,7 @@ class ConfigTest extends \PHPUnit_Framework_TestCase
         $expectedResult = ['complete', 'pending_payment'];
 
         $collectionMock = $this->getMock(
-            \Magento\Sales\Model\ResourceModel\Order\Status\Collection::class,
+            Collection::class,
             ['create', 'joinStates'],
             [],
             '',
@@ -97,14 +99,14 @@ class ConfigTest extends \PHPUnit_Framework_TestCase
     public function testGetStateLabelByStateAndStatus()
     {
         $statuses = [
-            new \Magento\Framework\DataObject(
+            new DataObject(
                 [
                     'status' => 'fraud',
                     'state' => 'processing',
                     'label' => 'Suspected Fraud',
                 ]
             ),
-            new \Magento\Framework\DataObject(
+            new DataObject(
                 [
                     'status' => 'processing',
                     'state' => 'processing',
@@ -113,7 +115,7 @@ class ConfigTest extends \PHPUnit_Framework_TestCase
             )
         ];
         $collectionMock = $this->getMock(
-            \Magento\Sales\Model\ResourceModel\Order\Status\Collection::class,
+            Collection::class,
             ['create', 'joinStates'],
             [],
             '',
@@ -128,5 +130,99 @@ class ConfigTest extends \PHPUnit_Framework_TestCase
             ->will($this->returnValue($statuses));
         $result = $this->salesConfig->getStateLabelByStateAndStatus('processing', 'fraud');
         $this->assertSame('Suspected Fraud', $result->getText());
+    }
+
+    /**
+     * Test get statuses
+     *
+     * @dataProvider getStatusesDataProvider
+     *
+     * @param string $state
+     * @param bool $joinLabels
+     * @param DataObject[] $collectionData
+     * @param array $expectedResult
+     */
+    public function testGetStatuses($state, $joinLabels, $collectionData, $expectedResult)
+    {
+        $collectionMock = $this->getMock(
+            Collection::class,
+            ['create', 'joinStates', 'addStateFilter', 'orderByLabel'],
+            [],
+            '',
+            false,
+            false
+        );
+        $this->orderStatusCollectionFactoryMock->expects($this->any())
+            ->method('create')
+            ->will($this->returnValue($collectionMock));
+
+        $collectionMock->expects($this->once())
+            ->method('addStateFilter')
+            ->will($this->returnSelf());
+
+        $collectionMock->expects($this->once())
+            ->method('orderByLabel')
+            ->will($this->returnValue($collectionData));
+
+        $collectionMock->expects($this->once())
+            ->method('joinStates')
+            ->will($this->returnValue($collectionData));
+
+        $result = $this->salesConfig->getStateStatuses($state, $joinLabels);
+        $this->assertSame($expectedResult, $result);
+
+        // checking data cached in private property
+        $this->assertSame($result, $this->salesConfig->getStateStatuses($state, $joinLabels));
+    }
+
+    /**
+     * Data provider for testGetStatuses
+     *
+     * @return array
+     */
+    public function getStatusesDataProvider()
+    {
+        return [
+            'processing state' => [
+                'state' => 'processing',
+                'joinLabels' => false,
+                'collectionData' => [
+                    new DataObject(
+                        [
+                            'status' => 'fraud',
+                            'state' => 'processing',
+                            'store_label' => 'Suspected Fraud',
+                        ]
+                    ),
+                    new DataObject(
+                        [
+                            'status' => 'processing',
+                            'state' => 'processing',
+                            'store_label' => 'Processing',
+                        ]
+                    ),
+                ],
+                'expectedResult' => [
+                    0 => 'fraud',
+                    1 => 'processing'
+                ],
+            ],
+            'pending state' => [
+                'state' => 'pending',
+                'joinLabels' => true,
+                'collectionData' => [
+                    new DataObject(
+                        [
+                            'status' => 'pending_status',
+                            'state' => 'pending',
+                            'store_label' => 'Pending label',
+                        ]
+                    ),
+                ],
+                'expectedResult' => [
+                    'pending_status' => 'Pending label'
+                ],
+            ],
+        ];
     }
 }
