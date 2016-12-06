3dd81a20071237fb2abaf2f49c616c5547f3a880
Igor Melnikov <imelnikov@magento.com>
2016-12-06 15:24:15 -0600

    MAGETWO-61526: Create upgrade script for quote_payment table additional_information field
    Changing format from serialized to json of additional_information field in quote_payment table

---
+++ app/code/Magento/Quote/Setup/UpgradeData.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Quote\Setup;
+
+use Magento\Framework\Setup\UpgradeDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+class UpgradeData implements UpgradeDataInterface
+{
+    /**
+     * @var \Magento\Eav\Model\Config
+     */
+    private $eavConfig;
+
+    /**
+     * @var \Magento\Framework\Setup\FieldDataConverterFactory
+     */
+    private $fieldDataConverterFactory;
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\Eav\Model\Config $eavConfig
+     * @param \Magento\Framework\Setup\FieldDataConverterFactory $fieldDataConverterFactory
+     */
+    public function __construct(
+        \Magento\Eav\Model\Config $eavConfig,
+        \Magento\Framework\Setup\FieldDataConverterFactory $fieldDataConverterFactory
+    ) {
+        $this->eavConfig = $eavConfig;
+        $this->fieldDataConverterFactory = $fieldDataConverterFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+        if (version_compare($context->getVersion(), '2.0.4', '<')) {
+            $this->upgradeToVersionTwoZeroFour($setup);
+        }
+        $this->eavConfig->clear();
+        $setup->endSetup();
+    }
+
+    /**
+     * Upgrade to version 2.0.4
+     *
+     * @param ModuleDataSetupInterface $setup
+     * @return void
+     */
+    private function upgradeToVersionTwoZeroFour(ModuleDataSetupInterface $setup)
+    {
+        $fieldDataConverter = $this->fieldDataConverterFactory->create(
+            $setup->getConnection(),
+            \Magento\Framework\Setup\DataConverter\SerializedToJson::class
+        );
+        $fieldDataConverter->convert(
+            $setup->getTable('quote_payment'),
+            'payment_id',
+            'additional_information'
+        );
+    }
+}
