064b2b398c72270a569e094a076d79b56661f319
Dmytro Yushkin <dyushkin@magento.com>
2016-12-09 17:06:57 +0200

    MAGETWO-61967: Generate orderSessionId  on backend

---
+++ Model/Ui/ConfigProvider.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Signifyd\Model\Ui;
+
+use Magento\Checkout\Model\ConfigProviderInterface;
+use Magento\Signifyd\Model\OrderSessionId;
+
+/**
+ * Class ConfigProvider
+ */
+class ConfigProvider implements ConfigProviderInterface
+{
+    const SIGNIFYD_CODE = 'signifyd';
+
+    /**
+     * @var OrderSessionId
+     */
+    private $orderSessionId;
+
+    /**
+     * @param OrderSessionId $orderSessionId
+     */
+    public function __construct(OrderSessionId $orderSessionId)
+    {
+        $this->orderSessionId = $orderSessionId;
+    }
+
+    /**
+     * Retrieve assoc array of checkout configuration
+     *
+     * @return array
+     */
+    public function getConfig()
+    {
+        return [
+            'fraud' => [
+                self::SIGNIFYD_CODE => [
+                    'orderSessionId' => $this->orderSessionId->generate()
+                ]
+            ]
+        ];
+    }
+}
