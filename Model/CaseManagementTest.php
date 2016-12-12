90e49d96cace62ad35e99ca5d1d5cfcdb310074b
Ievgen Sentiabov <isentiabov@magento.com>
2016-12-12 13:00:00 +0200

    MAGETWO-61925: Create case entity infrastructure
     - Added API interfaces for Sygnifyd integration
     - Added implementation of API interfaces
     - Covered Sygnifyd management by integration tests

---
+++ Model/CaseManagementTest.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Signifyd\Model;
+
+use Magento\Framework\Api\FilterBuilder;
+use Magento\Framework\Api\SearchCriteriaBuilder;
+use Magento\Framework\App\ObjectManager;
+use Magento\Sales\Api\Data\OrderInterface;
+use Magento\Sales\Api\OrderRepositoryInterface;
+use Magento\Signifyd\Api\Data\CaseInterface;
+use Magento\Signifyd\Model\CaseManagement;
+use Magento\TestFramework\Helper\Bootstrap;
+
+/**
+ * Contains test methods for case management service
+ */
+class CaseManagementTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var CaseManagement
+     */
+    private $caseManagement;
+
+    /**
+     * @var ObjectManager
+     */
+    private $objectManager;
+
+    protected function setup()
+    {
+        $this->objectManager = Bootstrap::getObjectManager();
+        $this->caseManagement = $this->objectManager->get(CaseManagement::class);
+    }
+
+    /**
+     * @covers \Magento\Signifyd\Model\CaseManagement::create
+     * @magentoDataFixture Magento/Signifyd/_files/order.php
+     */
+    public function testCreate()
+    {
+        $order = $this->getOrder();
+        $case = $this->caseManagement->create($order->getEntityId());
+
+        static::assertNotEmpty($case->getEntityId());
+        static::assertEquals(CaseInterface::STATUS_PROCESSING, $case->getStatus());
+    }
+
+    /**
+     * @covers \Magento\Signifyd\Model\CaseManagement::getByOrderId
+     * @magentoDataFixture Magento/Signifyd/_files/case.php
+     */
+    public function testGetByOrderId()
+    {
+        $order = $this->getOrder();
+        $case = $this->caseManagement->getByOrderId($order->getEntityId());
+
+        static::assertEquals(CaseInterface::GUARANTEE_PENDING, $case->getGuaranteeDisposition());
+        static::assertEquals(CaseInterface::STATUS_PROCESSING, $case->getStatus());
+        static::assertEquals(CaseInterface::DISPOSITION_GOOD, $case->getReviewDisposition());
+        static::assertEquals('2016-12-12 15:17:17', $case->getCreatedAt());
+        static::assertEquals('2016-12-12 19:23:16', $case->getUpdatedAt());
+    }
+
+    /**
+     * Get stored order
+     * @return OrderInterface
+     */
+    private function getOrder()
+    {
+        /** @var FilterBuilder $filterBuilder */
+        $filterBuilder = $this->objectManager->get(FilterBuilder::class);
+        $filters = [
+            $filterBuilder->setField(OrderInterface::INCREMENT_ID)
+                ->setValue('100000001')
+                ->create()
+        ];
+
+        /** @var SearchCriteriaBuilder $searchCriteriaBuilder */
+        $searchCriteriaBuilder = $this->objectManager->get(SearchCriteriaBuilder::class);
+        $searchCriteria = $searchCriteriaBuilder->addFilters($filters)
+            ->create();
+
+        $orderRepository = $this->objectManager->get(OrderRepositoryInterface::class);
+        $orders = $orderRepository->getList($searchCriteria)
+            ->getItems();
+
+        /** @var OrderInterface $order */
+        return array_pop($orders);
+    }
+}
