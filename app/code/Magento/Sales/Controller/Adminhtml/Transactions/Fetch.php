ed7b80eef79579ef7c8c3a215c8bf732f12dc7a1
Hirokazu Nishi <nishi@principle-works.jp>
2016-12-12 23:59:58 +0900

    change getId() to getPaymentId()

--- app/code/Magento/Sales/Controller/Adminhtml/Transactions/Fetch.php
+++ app/code/Magento/Sales/Controller/Adminhtml/Transactions/Fetch.php
@@ -33,7 +33,7 @@ class Fetch extends \Magento\Sales\Controller\Adminhtml\Transactions
             return $resultRedirect->setPath('sales/*/');
         }
         try {
-            $this->orderPaymentRepository->get($txn->getId())->setOrder($txn->getOrder())->importTransactionInfo($txn);
+            $this->orderPaymentRepository->get($txn->getPaymentId())->setOrder($txn->getOrder())->importTransactionInfo($txn);
             $txn->save();
             $this->messageManager->addSuccess(__('The transaction details have been updated.'));
         } catch (\Magento\Framework\Exception\LocalizedException $e) {
