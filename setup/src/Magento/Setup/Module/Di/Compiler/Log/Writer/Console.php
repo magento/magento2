c18602c3c2924175e8d2f0a8f147854781b40529
Pablo Ivulic <pablo@ivulic.com>
2016-12-13 14:55:36 +0100

    Throw exception if errors are found

--- setup/src/Magento/Setup/Module/Di/Compiler/Log/Writer/Console.php
+++ setup/src/Magento/Setup/Module/Di/Compiler/Log/Writer/Console.php
@@ -70,6 +70,7 @@ class Console
 
         if ($errorsCount) {
             $this->console->writeln('<error>' . 'Total Errors Count: ' . $errorsCount . '</error>');
+            throw new \Magento\Framework\Validator\Exception(__('Error durring compilation'));
         }
     }
 
