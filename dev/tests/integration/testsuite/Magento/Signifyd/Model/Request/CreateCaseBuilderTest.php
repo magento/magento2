e4be7f2b00447a03ce11079eb80708ce2e709bce
Iurii Ivashchenko <iivashchenko@magento.com>
2016-12-13 12:06:39 -0600

    MAGETWO-61913: Create request builders
    - integration test

--- dev/tests/integration/testsuite/Magento/Signifyd/Model/Request/CreateCaseBuilderTest.php
+++ dev/tests/integration/testsuite/Magento/Signifyd/Model/Request/CreateCaseBuilderTest.php
@@ -10,6 +10,7 @@ use Magento\Framework\App\Area;
 use Magento\Framework\Intl\DateTimeFactory;
 use Magento\Framework\ObjectManagerInterface;
 use Magento\Sales\Model\Order;
+use Magento\Customer\Api\CustomerRepositoryInterface;
 
 /**
  * Class PurchaseBuilderTest
@@ -66,43 +67,78 @@ class CreateCaseBuilderTest extends \PHPUnit_Framework_TestCase
      */
     public function testPurchaseBuilder()
     {
-        $orderMethod = 'paypal_account';
-        $orderChannel = 'WEB';
-        $shippingProvider = 'Flat Rate';
-        $shippingMethod = 'Fixed';
-
-        $purchaseData = $this->builderData['purchase'];
-
-        $dateTimeFactory = $this->objectManager->get(DateTimeFactory::class);
-        $createdAt = $dateTimeFactory->create(
-            $this->order->getCreatedAt(),
-            new \DateTimeZone('UTC')
-        );
-
-        $orderPayment = $this->order->getPayment();
-
         $orderItems = $this->order->getAllItems();
         $product = $orderItems[0]->getProduct();
-        $purchaseProducts = $purchaseData['products'][0];
+        $payment = $this->order->getPayment();
+        $billingAddress = $this->order->getBillingAddress();
+        $shippingAddress = $this->order->getShippingAddress();
 
-        static::assertEquals($this->order->getRemoteIp(), $purchaseData['browserIpAddress']);
-        static::assertEquals($shippingProvider, $purchaseData['shipments'][0]['shipper']);
-        static::assertEquals($shippingMethod, $purchaseData['shipments'][0]['shippingMethod']);
-        //static::assertEquals($this->order->getShippingAmount(), $purchaseData['shipments'][0]['shippingPrice']);
-        static::assertEquals($this->order->getEntityId(), $purchaseData['orderId']);
-        static::assertEquals($createdAt->format(\DateTime::ISO8601), $purchaseData['createdAt']);
+        $customerRepository = $this->objectManager->create(CustomerRepositoryInterface::class);
+        $customer = $customerRepository->getById(1);
 
-        static::assertEquals($orderMethod, $purchaseData['paymentGateway']);
-        static::assertEquals($orderPayment->getLastTransId(), $purchaseData['transactionId']);
-        static::assertEquals($this->order->getOrderCurrencyCode(), $purchaseData['currency']);
-        static::assertEquals($orderChannel, $purchaseData['orderChannel']);
-        static::assertEquals($this->order->getGrandTotal(), $purchaseData['totalPrice']);
+        $expected = [
+            'purchase' => [
+                'browserIpAddress' => $this->order->getRemoteIp(),
+                'orderId' => $this->order->getEntityId(),
+                'createdAt' => '2016-12-12T12:00:55+00:00',
+                'paymentGateway' => 'paypal_account',
+                'transactionId' => $payment->getLastTransId(),
+                'currency' => $this->order->getOrderCurrencyCode(),
+                'orderChannel' => 'WEB',
+                'totalPrice' => $this->order->getGrandTotal(),
+                'shipments' => [
+                    0 => [
+                        'shipper' => 'Flat Rate',
+                        'shippingMethod' => 'Fixed',
+                        'shippingPrice' => $this->order->getShippingAmount()
+                    ]
+                ],
+                'products' => [
+                    0 => [
+                        'itemId' => $orderItems[0]->getSku(),
+                        'itemName' => $orderItems[0]->getName(),
+                        'itemPrice' => $orderItems[0]->getPrice(),
+                        'itemQuantity' => $orderItems[0]->getQtyOrdered(),
+                        'itemUrl' => $product->getProductUrl(),
+                        'itemWeight' => $product->getWeight()
+                    ]
+                ]
+            ],
+            'card' => [
+                'cardHolderName' => 'firstname lastname',
+                'last4' => $payment->getCcLast4(),
+                'expiryMonth' => $payment->getCcExpMonth(),
+                'expiryYear' =>  $payment->getCcExpYear(),
+                'billingAddress' => [
+                    'streetAddress' => 'street',
+                    'city' => $billingAddress->getCity(),
+                    'provinceCode' => $billingAddress->getRegionCode(),
+                    'postalCode' => $billingAddress->getPostcode(),
+                    'countryCode' => $billingAddress->getCountryId()
+                ]
+            ],
+            'recipient' => [
+                'fullName' => $shippingAddress->getName(),
+                'confirmationEmail' =>  $shippingAddress->getEmail(),
+                'confirmationPhone' => $shippingAddress->getTelephone(),
+                'deliveryAddress' => [
+                    'streetAddress' => 'street',
+                    'city' => $shippingAddress->getCity(),
+                    'provinceCode' => $shippingAddress->getRegionCode(),
+                    'postalCode' => $shippingAddress->getPostcode(),
+                    'countryCode' => $shippingAddress->getCountryId()
+                ]
+            ],
+            'userAccount' => [
+                'email' => $customer->getEmail(),
+                'username' => $customer->getEmail(),
+                'phone' => $this->order->getBillingAddress()->getTelephone(),
+                'accountNumber' => $customer->getId(),
+                'createdDate' => '2016-12-12 11:00:00+00:00',
+                'lastUpdateDate' => '2016-12-12 11:05:00+00:00'
+            ]
+        ];
 
-        //static::assertEquals($product->getSku(), $purchaseProducts['itemId']);
-        //static::assertEquals($product->getName(), $purchaseProducts['itemName']);
-        static::assertEquals($orderItems[0]->getPrice(), $purchaseProducts['itemPrice']);
-        static::assertEquals($orderItems[0]->getQtyOrdered(), $purchaseProducts['itemQuantity']);
-        static::assertEquals($product->getProductUrl(), $purchaseProducts['itemUrl']);
-        static::assertEquals($product->getWeight(), $purchaseProducts['itemWeight']);
+        static::assertEquals($expected['userAccount'], $this->builderData['userAccount']);
     }
 }
