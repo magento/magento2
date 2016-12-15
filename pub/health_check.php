54acc0cfb5078e83af4e88f4f2efc810d73ac3d3
Alex Paliarush <apaliarush@magento.com>
2016-12-15 10:40:09 -0600

    MAGETWO-61942: Create health check endpoint

--- pub/health_check.php
+++ pub/health_check.php
@@ -6,26 +6,15 @@
 
 try {
     require __DIR__ . '/../app/bootstrap.php';
-} catch (\Exception $e) {
-    echo <<<HTML
-<div style="font:12px/1.35em arial, helvetica, sans-serif;">
-    <div style="margin:0 0 25px 0; border-bottom:1px solid #ccc;">
-        <h3 style="margin:0;font-size:1.7em;font-weight:normal;text-transform:none;text-align:left;color:#2f2f2f;">
-        Autoload error</h3>
-    </div>
-    <p>{$e->getMessage()}</p>
-</div>
-HTML;
-    http_response_code(500);
-    exit(1);
-}
-
-try {
     /** @var \Magento\Framework\App\ObjectManagerFactory $objectManagerFactory */
     $objectManagerFactory = \Magento\Framework\App\Bootstrap::createObjectManagerFactory(BP, []);
+    /** @var \Magento\Framework\ObjectManagerInterface $objectManager */
+    $objectManager = $objectManagerFactory->create([]);
     /** @var \Magento\Framework\App\DeploymentConfig $deploymentConfig */
-    $deploymentConfig = $objectManagerFactory->create([])->get(\Magento\Framework\App\DeploymentConfig::class);
+    $deploymentConfig = $objectManager->get(\Magento\Framework\App\DeploymentConfig::class);
     $envConfig = $deploymentConfig->getConfigData();
+    /** @var \Psr\Log\LoggerInterface $logger */
+    $logger = $objectManager->get(\Psr\Log\LoggerInterface::class);
 } catch (\Exception $e) {
     http_response_code(500);
     exit(1);
@@ -39,24 +28,26 @@ foreach ($envConfig['db']['connection'] as $connectionData) {
             $connectionData['username'],
             $connectionData['password']
         );
-    } catch (\PDOException $e) {
-        echo "Connection failed: " . $e->getMessage();
+    } catch (\Exception $e) {
+        $logger->error("MySQL connection failed: " . $e->getMessage());
         http_response_code(500);
     }
 }
 
-// check Redis availability
+// check cache storage availability
 if (isset($envConfig['cache']['frontend']) && is_array($envConfig['cache']['frontend'])) {
     foreach ($envConfig['cache']['frontend'] as $cacheConfig) {
         if (!isset($cacheConfig['backend']) || !isset($cacheConfig['backend_options'])) {
+            $logger->error("Cache configuration is invalid");
             http_response_code(500);
         }
         $cacheBackendClass = $cacheConfig['backend'];
         try {
-            /** @var \Zend_Cache_Backend_ExtendedInterface $backend */
+            /** @var \Zend_Cache_Backend_Interface $backend */
             $backend = new $cacheBackendClass($cacheConfig['backend_options']);
             $backend->test('test_cache_id');
-        } catch (Exception $e) {
+        } catch (\Exception $e) {
+            $logger->error("Cache storage is not accessible");
             http_response_code(500);
         }
     }
