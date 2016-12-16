27d1a09012bcd9c0ee283ad2464026d4d6634ac0
cspruiell <cspruiell@magento.com>
2016-12-16 09:41:24 -0600

    MAGETWO-61945: Edit Varnish configuration to use health check status
     - update test to use varnish5 instead of varnish3

--- dev/tests/integration/testsuite/Magento/PageCache/Block/System/Config/Form/Field/ExportTest.php
+++ dev/tests/integration/testsuite/Magento/PageCache/Block/System/Config/Form/Field/ExportTest.php
@@ -20,8 +20,8 @@ class ExportTest extends \Magento\TestFramework\TestCase\AbstractBackendControll
     {
         $this->dispatch('backend/admin/system_config/edit/section/system/');
         $body = $this->getResponse()->getBody();
-        $this->assertContains('system_full_page_cache_varnish_export_button_version3', $body);
         $this->assertContains('system_full_page_cache_varnish_export_button_version4', $body);
+        $this->assertContains('system_full_page_cache_varnish_export_button_version5', $body);
         $this->assertContains('[id^=system_full_page_cache_varnish_export_button_version]', $body);
     }
 }
