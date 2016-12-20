699105760e00537ccca333426911299fcff6a1b0
Bohdan Korablov <bkorablov@magento.com>
2016-12-20 12:22:36 +0200

    MAGETWO-61500: Remove enabling/disabling of caches from massaction when production mode is enabled

--- lib/internal/Magento/Framework/View/Element/Block/ArgumentInterface.php
+++ lib/internal/Magento/Framework/View/Element/Block/ArgumentInterface.php
@@ -5,6 +5,10 @@
  */
 namespace Magento\Framework\View\Element\Block;
 
+/**
+ * Block argument interface.
+ * All objects that are injected to block arguments should implement this interface.
+ */
 interface ArgumentInterface
 {
 }
