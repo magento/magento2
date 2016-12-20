81730d558e72727487f0d46a362b947493895b9c
Venkata Uppalapati <vuppalapati@magento.com>
2016-12-19 18:12:49 -0600

    MAGETWO-58924: SQL error wait timeout error when saving categories  - adding unit tests for UrlRewritesSet and TemporaryTableService

---
+++ app/code/Magento/UrlRewrite/Test/Unit/Model/UrlRewritesSetTest.php
@@ -0,0 +1,157 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\UrlRewrite\Test\Unit\Model;
+
+use Magento\UrlRewrite\Model\UrlRewritesSet;
+use Magento\UrlRewrite\Service\V1\Data\UrlRewrite;
+use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
+
+class UrlRewritesSetTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var UrlRewritesSet|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $urlRewritesSet;
+
+    /**
+     * Set up
+     *
+     * @return void
+     */
+    protected function setUp()
+    {
+        $this->urlRewritesSet = (new ObjectManager($this))->getObject(
+            UrlRewritesSet::class,
+            []
+        );
+    }
+
+    /**
+     * Run test merge method
+     *
+     * @param array urlRewriteMockArray
+     * @param String expectedData
+     * @param int arrayCount
+     * @dataProvider getMergeTestParameters
+     * @return void
+     */
+    public function testMerge($urlRewriteMockArray, $expectedData, $arrayCount)
+    {
+        $this->urlRewritesSet->merge($urlRewriteMockArray);
+        $this->assertEquals($expectedData, $this->urlRewritesSet->getData());
+        $this->assertCount($arrayCount, $this->urlRewritesSet->getData());
+    }
+
+    /**
+     * Run test getData method when data is Empty
+     *
+     * @return void
+     */
+    public function testGetDataWhenEmpty()
+    {
+        $this->assertEmpty($this->urlRewritesSet->getData());
+    }
+
+    /**
+     * Run test getData method when data is not empty
+     *
+     * @return void
+     */
+    public function testGetDataWhenNotEmpty()
+    {
+        $data = new \ReflectionProperty($this->urlRewritesSet, 'data');
+        $data->setAccessible(true);
+        $data->setValue($this->urlRewritesSet, [new UrlRewrite()]);
+        $data->setAccessible(false);
+        $this->assertNotEmpty($this->urlRewritesSet->getData());
+    }
+
+    /**
+     * Run test resetData method
+     */
+    public function testResetData()
+    {
+        $data = new \ReflectionProperty($this->urlRewritesSet, 'data');
+        $data->setAccessible(true);
+        $data->setValue($this->urlRewritesSet, [new UrlRewrite()]);
+        $data->setAccessible(false);
+        $this->urlRewritesSet->resetData();
+        $this->assertEmpty($this->urlRewritesSet->getData());
+    }
+
+    /**
+     *
+     * @return array
+     */
+    public function getMergeTestParameters()
+    {
+        $urlRewriteMock1 = $this->getMock(UrlRewrite::class, [], [], '', false);
+
+        $requestPathForMock2 = 'magento.tst/products/simpleproduct2';
+        $storeIdForMock2 = 'testStore2';
+        $urlRewriteMock2 = $this->getMock(UrlRewrite::class, [], [], '', false);
+
+        $urlRewriteMock2->expects($this->atLeastOnce())
+            ->method('getRequestPath')
+            ->willReturn($requestPathForMock2);
+
+        $urlRewriteMock2->expects($this->atLeastOnce())
+            ->method('getStoreId')
+            ->willReturn($storeIdForMock2);
+
+        $requestPathForMock3 = 'magento.tst/products/simpleproduct3';
+        $storeIdForMock3 = 'testStore3';
+        $urlRewriteMock3 = $this->getMock(UrlRewrite::class, [], [], '', false);
+
+        $urlRewriteMock3->expects($this->atLeastOnce())
+            ->method('getRequestPath')
+            ->willReturn($requestPathForMock3);
+
+        $urlRewriteMock3->expects($this->atLeastOnce())
+            ->method('getStoreId')
+            ->willReturn($storeIdForMock3);
+
+        return [
+            [
+                [],
+                [],
+                0
+            ],
+            [
+                [$urlRewriteMock1],
+                [$urlRewriteMock1],
+                1
+            ],
+            [
+                [
+                    $urlRewriteMock1,
+                    $urlRewriteMock2,
+                    $urlRewriteMock2
+                ],
+                [
+                    $urlRewriteMock1,
+                    $requestPathForMock2 . '_' . $storeIdForMock2 => $urlRewriteMock2
+                ],
+                2
+            ],
+            [
+                [
+                    $urlRewriteMock1,
+                    $urlRewriteMock2,
+                    $urlRewriteMock3
+                ],
+                [
+                    $urlRewriteMock1,
+                    $requestPathForMock2 . '_' . $storeIdForMock2 => $urlRewriteMock2,
+                    $requestPathForMock3 . '_' . $storeIdForMock3 => $urlRewriteMock3
+                ],
+                3
+            ],
+        ];
+    }
+
+}
---
+++ lib/internal/Magento/Framework/DB/Test/Unit/TemporaryTableServiceTest.php
@@ -0,0 +1,180 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Framework\DB\Test\Unit;
+
+use Magento\Framework\DB\TemporaryTableService;
+use Magento\Framework\DB\Adapter\AdapterInterface;
+use Magento\Framework\DB\Select;
+use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
+use Magento\Framework\Math\Random;
+
+class TemporaryTableServiceTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var TemporaryTableService|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $temporaryTableService;
+
+    /**
+     * @var AdapterInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $adapterMock;
+
+    /**
+     * @var Random|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $randomMock;
+
+    /**
+     * @var Select|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $selectMock;
+
+    /**
+     * Set up
+     *
+     * @return void
+     */
+    protected function setUp()
+    {
+        $this->adapterMock = $this->getMock(AdapterInterface::class);
+        $this->selectMock = $this->getMock(Select::class, [], [], '', false);
+        $this->randomMock = $this->getMock(Random::class, [], [], '', false);
+        $this->temporaryTableService = (new ObjectManager($this))->getObject(
+            TemporaryTableService::class,
+            [
+                'random' => $this->randomMock
+            ]
+        );
+    }
+
+    /**
+     * Run test createFromSelect method
+     *
+     * @param array $indexes
+     * @param string $expectedSelect
+     * @dataProvider getCreateFromSelectTestParameters
+     * @return void
+     */
+    public function testCreateFromSelect($indexes, $expectedSelect)
+    {
+        $selectString = 'select * from sometable';
+        $random = 'random_table';
+
+        $this->randomMock->expects($this->once())
+            ->method('getUniqueHash')
+            ->willReturn($random);
+
+        $this->adapterMock->expects($this->once())
+            ->method('query')
+            ->with($expectedSelect)
+            ->willReturnSelf();
+
+        $this->adapterMock->expects($this->once())
+            ->method('query')
+            ->willReturnSelf();
+
+        $this->adapterMock->expects($this->any())
+            ->method('quoteIdentifier')
+            ->willReturnArgument(0);
+
+        $this->selectMock->expects($this->once())
+            ->method('getBind')
+            ->willReturn(['bind']);
+
+        $this->selectMock->expects($this->any())
+            ->method('__toString')
+            ->willReturn($selectString);
+
+        $this->assertEquals(
+            $random,
+            $this->temporaryTableService->createFromSelect(
+                $this->selectMock,
+                $this->adapterMock,
+                $indexes
+            )
+        );
+    }
+
+    /**
+     * Run test dropTable method when createdTables array of TemporaryTableService is empty
+     *
+     * @return void
+     */
+    public function testDropTableWhenCreatedTablesArrayIsEmpty()
+    {
+        $this->assertFalse($this->temporaryTableService->dropTable('tmp_select_table'));
+    }
+
+    /**
+     * Run test dropTable method when data exists in createdTables array of TemporaryTableService
+     *
+     * @param string $tableName
+     * @param bool $assertion
+     *
+     * @dataProvider getDropTableTestParameters
+     * @return void
+     */
+    public function testDropTableWhenCreatedTablesArrayNotEmpty($tableName, $assertion)
+    {
+        $createdTables = new \ReflectionProperty($this->temporaryTableService, 'createdTables');
+        $createdTables->setAccessible(true);
+        $createdTables->setValue($this->temporaryTableService, ['tmp_select_table' => $this->adapterMock]);
+        $createdTables->setAccessible(false);
+
+        $this->adapterMock->expects($this->any())
+            ->method('dropTemporaryTable')
+            ->willReturn(true);
+
+        $this->assertEquals($this->temporaryTableService->dropTable($tableName), $assertion);
+    }
+
+    /**
+     * @return array
+     */
+    public function getCreateFromSelectTestParameters()
+    {
+        return [
+            [
+                ['PRIMARY' => ['primary_column_name']],
+                'CREATE TEMPORARY TABLE random_table (PRIMARY KEY(primary_column_name)) ENGINE=INNODB IGNORE '
+                . '(select * from sometable)'
+            ],
+            [
+                ['UNQ_INDX' => ['column1', 'column2']],
+                'CREATE TEMPORARY TABLE random_table (UNIQUE UNQ_INDX(column1,column2)) ENGINE=INNODB IGNORE '
+                . '(select * from sometable)'
+            ],
+            [
+                ['OTH_INDX' => ['column3', 'column4']],
+                'CREATE TEMPORARY TABLE random_table (INDEX OTH_INDX USING HASH(column3,column4)) ENGINE=INNODB IGNORE '
+                . '(select * from sometable)'
+            ],
+            [
+                [
+                    'PRIMARY' => ['primary_column_name'],
+                    'OTH_INDX' => ['column3', 'column4'],
+                    'UNQ_INDX' => ['column1', 'column2']
+                ],
+                'CREATE TEMPORARY TABLE random_table '
+                . '(PRIMARY KEY(primary_column_name),'
+                . 'INDEX OTH_INDX USING HASH(column3,column4),UNIQUE UNQ_INDX(column1,column2)) ENGINE=INNODB IGNORE '
+                . '(select * from sometable)'
+            ],
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function getDropTableTestParameters()
+    {
+        return [
+            ['tmp_select_table_1', false],
+            ['tmp_select_table', true],
+        ];
+    }
+}
