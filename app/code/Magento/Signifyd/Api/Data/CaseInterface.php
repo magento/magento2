bfc5f475dc8fec24f9dcc331fd3249861009fbe9
Iurii Ivashchenko <iivashchenko@magento.com>
2016-12-21 08:49:26 -0600

    MAGETWO-54389: Submitting Case Entry to Signifyd on Order Creation
    - replace orderId with orderIncrementId
    - fixes for integration tests
    - db schema update

--- app/code/Magento/Signifyd/Api/Data/CaseInterface.php
+++ app/code/Magento/Signifyd/Api/Data/CaseInterface.php
@@ -147,6 +147,21 @@ interface CaseInterface
     public function setOrderId($orderId);
 
     /**
+     * Returns order increment id for a case.
+     *
+     * @return string
+     */
+    public function getOrderIncrementId();
+
+    /**
+     * Sets order increment id for a case.
+     *
+     * @param string $orderIncrementId
+     * @return $this
+     */
+    public function setOrderIncrementId($orderIncrementId);
+
+    /**
      * Returns data about a team associated with a case.
      *
      * @return array
