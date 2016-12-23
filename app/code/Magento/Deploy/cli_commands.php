fad45f48593c3cf5ec52f2ce05fbdff1de365ab1
Cristian Partica <cpartica@magento.com>
2016-12-23 12:04:10 -0600

    MAGETWO-62400: Modules that use repositories in command line tools fail during setup:di:compile 2.1.x
    - revert usage of class because di compile fails because this is not a class

--- app/code/Magento/Deploy/cli_commands.php
+++ app/code/Magento/Deploy/cli_commands.php
@@ -5,5 +5,5 @@
  */
 
 if (PHP_SAPI == 'cli') {
-    \Magento\Framework\Console\CommandLocator::register(Magento\Deploy\Console\CommandList::class);
+    \Magento\Framework\Console\CommandLocator::register('Magento\Deploy\Console\CommandList');
 }
