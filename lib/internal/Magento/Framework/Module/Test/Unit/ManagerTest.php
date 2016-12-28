c76476190abd19bda258aeb3e39346cf30101da9
Oleh Posyniak <oposyniak@magento.com>
2016-12-28 12:55:18 +0200

    MAGETWO-62710: Remove Advanced section

--- lib/internal/Magento/Framework/Module/Test/Unit/ManagerTest.php
+++ lib/internal/Magento/Framework/Module/Test/Unit/ManagerTest.php
@@ -5,57 +5,60 @@
  */
 namespace Magento\Framework\Module\Test\Unit;
 
-use Magento\Framework\Module\Plugin\DbStatusValidator;
+use Magento\Framework\Module\Manager;
+use Magento\Framework\Module\ModuleListInterface;
+use Magento\Framework\Module\Output\ConfigInterface;
+use PHPUnit_Framework_MockObject_MockObject as Mock;
 
 class ManagerTest extends \PHPUnit_Framework_TestCase
 {
     /**
      * XPath in the configuration of a module output flag
+     * @deprecated
      */
     const XML_PATH_OUTPUT_ENABLED = 'custom/is_module_output_enabled';
 
     /**
-     * @var \Magento\Framework\Module\Manager
+     * @var Manager
      */
-    private $_model;
+    private $model;
 
     /**
-     * @var \PHPUnit_Framework_MockObject_MockObject
+     * @var ModuleListInterface|Mock
      */
-    private $_moduleList;
+    private $moduleList;
 
     /**
-     * @var \PHPUnit_Framework_MockObject_MockObject
+     * @var ConfigInterface|Mock
+     * @deprecated
      */
-    private $_outputConfig;
+    private $outputConfig;
 
     protected function setUp()
     {
-        $this->_moduleList = $this->getMockForAbstractClass(\Magento\Framework\Module\ModuleListInterface::class);
-        $this->_moduleList->expects($this->any())
-            ->method('getOne')
-            ->will($this->returnValueMap([
-                ['Module_One', ['name' => 'One_Module', 'setup_version' => '1']],
-                ['Module_Two', ['name' => 'Two_Module', 'setup_version' => '2']],
-                ['Module_Three', ['name' => 'Two_Three']],
-            ]));
-        $this->_outputConfig = $this->getMockForAbstractClass(\Magento\Framework\Module\Output\ConfigInterface::class);
-        $this->_model = new \Magento\Framework\Module\Manager(
-            $this->_outputConfig,
-            $this->_moduleList,
-            [
-                'Module_Two' => self::XML_PATH_OUTPUT_ENABLED,
-            ]
+        $this->moduleList = $this->getMockBuilder(ModuleListInterface::class)
+            ->getMockForAbstractClass();
+        $this->outputConfig = $this->getMockBuilder(ConfigInterface::class)
+            ->getMockForAbstractClass();
+
+        $this->model = new Manager(
+            $this->outputConfig,
+            $this->moduleList
         );
     }
 
     public function testIsEnabled()
     {
-        $this->_moduleList->expects($this->exactly(2))->method('has')->will($this->returnValueMap([
-            ['Module_Exists', true],
-            ['Module_NotExists', false],
-        ]));
-        $this->assertTrue($this->_model->isEnabled('Module_Exists'));
-        $this->assertFalse($this->_model->isEnabled('Module_NotExists'));
+        $this->moduleList->expects($this->exactly(2))
+            ->method('has')
+            ->willReturnMap(
+                [
+                    ['Module_Exists', true],
+                    ['Module_NotExists', false],
+                ]
+            );
+
+        $this->assertTrue($this->model->isEnabled('Module_Exists'));
+        $this->assertFalse($this->model->isEnabled('Module_NotExists'));
     }
 }
