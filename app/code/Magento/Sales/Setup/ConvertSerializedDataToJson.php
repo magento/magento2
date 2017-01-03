22df83cd5e6b2bab973a0ee2ac13a1a80c08cded
Igor Melnikov <imelnikov@magento.com>
2017-01-03 15:59:00 -0600

    MAGETWO-62902: Bundle and custom options of type file not converted
    - Use \Magento\Sales\Setup\SerializedDataConverter for sales_order_item.product_options

--- app/code/Magento/Sales/Setup/ConvertSerializedDataToJson.php
+++ app/code/Magento/Sales/Setup/ConvertSerializedDataToJson.php
@@ -7,6 +7,7 @@ namespace Magento\Sales\Setup;
 
 use Magento\Framework\DB\FieldDataConverterFactory;
 use Magento\Framework\DB\DataConverter\SerializedToJson;
+use Magento\Framework\DB\FieldDataConverter;
 
 /**
  *  Convert serialized data in sales tables to JSON
@@ -26,26 +27,35 @@ class ConvertSerializedDataToJson
     /**
      * @var array
      */
+    private $fieldDataConverters = [];
+
+    /**
+     * @var array
+     */
     private $fieldsToUpdate = [
         [
             'table' => 'sales_order_item',
             'identifier' => 'item_id',
-            'title' => 'product_options'
+            'title' => 'product_options',
+            'data_converter' => SerializedDataConverter::class
         ],
         [
             'table' => 'sales_shipment',
             'identifier' => 'entity_id',
-            'title' => 'packages'
+            'title' => 'packages',
+            'data_converter' => SerializedToJson::class
         ],
         [
             'table' => 'sales_order_payment',
             'identifier' => 'entity_id',
-            'title' => 'additional_information'
+            'title' => 'additional_information',
+            'data_converter' => SerializedToJson::class
         ],
         [
             'table' => 'sales_payment_transaction',
             'identifier' => 'transaction_id',
-            'title' => 'additional_information'
+            'title' => 'additional_information',
+            'data_converter' => SerializedToJson::class
         ]
     ];
 
@@ -74,8 +84,8 @@ class ConvertSerializedDataToJson
      */
     public function convert()
     {
-        $fieldDataConverter = $this->fieldDataConverterFactory->create(SerializedToJson::class);
         foreach ($this->fieldsToUpdate as $field) {
+            $fieldDataConverter = $this->getFieldDataConverter($field['data_converter']);
             $fieldDataConverter->convert(
                 $this->salesSetup->getConnection(),
                 $this->salesSetup->getTable($field['table']),
@@ -83,5 +93,21 @@ class ConvertSerializedDataToJson
                 $field['title']
             );
         }
+    }
+
+    /**
+     * Get field data converter
+     *
+     * @param string $dataConverterClassName
+     * @return FieldDataConverter
+     */
+    private function getFieldDataConverter($dataConverterClassName)
+    {
+        if (!isset($this->fieldDataConverters[$dataConverterClassName])) {
+            $this->fieldDataConverters[$dataConverterClassName] = $this->fieldDataConverterFactory->create(
+                $dataConverterClassName
+            );
+        }
+        return $this->fieldDataConverters[$dataConverterClassName];
     }
 }
