9192b96d77597f79062142bbef73c6f9978a1491
Oleh Posyniak <oposyniak@magento.com>
2017-01-03 12:17:03 +0200

    MAGETWO-62655: [GitHub] CLI won't work without write permissions #7933

--- setup/src/Magento/Setup/Console/CompilerPreparation.php
+++ setup/src/Magento/Setup/Console/CompilerPreparation.php
@@ -1,11 +1,9 @@
 <?php
 /***
- * Copyright © 2016 Magento. All rights reserved.
+ * Copyright © 2017 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-
 namespace Magento\Setup\Console;
-
 
 use Magento\Framework\App\Bootstrap;
 use Magento\Framework\App\Filesystem\DirectoryList;
