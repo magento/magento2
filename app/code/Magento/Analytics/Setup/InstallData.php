092073bbdeebac2a14dd0f06f477f21a92b7e8ea
Oleksandr Radchenko <oradchenko@magento.com>
2017-01-03 13:37:34 +0200

    MAGETWO-62754: Create API user on installation step

--- app/code/Magento/Analytics/Setup/InstallData.php
+++ app/code/Magento/Analytics/Setup/InstallData.php
@@ -11,6 +11,7 @@ use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Analytics\Model\NotificationTime;
 use Magento\Framework\Flag\FlagResource;
+use Magento\Integration\Model\IntegrationService;
 
 /**
  * @codeCoverageIgnore
@@ -27,12 +28,25 @@ class InstallData implements InstallDataInterface
      */
     private $flagResource;
 
+    /**
+     * @var IntegrationService
+     */
+    private $integrationService;
+
+    /**
+     * InstallData constructor.
+     * @param NotificationTime $notificationTime
+     * @param FlagResource $flagResource
+     * @param IntegrationService $integrationService
+     */
     public function __construct(
         NotificationTime $notificationTime,
-        FlagResource $flagResource
+        FlagResource $flagResource,
+        IntegrationService $integrationService
     ) {
         $this->notificationTime = $notificationTime;
-         $this->flagResource = $flagResource;
+        $this->flagResource = $flagResource;
+        $this->integrationService = $integrationService;
     }
 
     /**
@@ -42,5 +56,19 @@ class InstallData implements InstallDataInterface
     public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
     {
         $this->notificationTime->storeLastTimeNotification(1);
+        $this->integrationService->create($this->getIntegrationData());
     }
+
+    /**
+     * @return array
+     */
+    private function getIntegrationData()
+    {
+        $integrationData['name'] = "Magento Analytics user";
+        $integrationData['all_resources'] = false;
+        $integrationData['resource'][] = "Magento_Analytics::analytics";
+        $integrationData['resource'][] = "Magento_Analytics::analytics-api";
+        return $integrationData;
+    }
+
 }
