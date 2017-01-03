c8845da60429b2f02d5254642f0d0ac9e9d9fde9
Andrei_Ziblitski <andrei_ziblitski@epam.com>
2017-01-03 18:16:36 +0300

    MTO-90: [Test] Reports Refresh statistics
    - Fixed magento_timezone fail

--- dev/tests/functional/tests/app/Magento/Reports/Test/Constraint/AssertReportsUpdatedTimezone.php
+++ dev/tests/functional/tests/app/Magento/Reports/Test/Constraint/AssertReportsUpdatedTimezone.php
@@ -25,7 +25,7 @@ class AssertReportsUpdatedTimezone extends AbstractConstraint
         $reportStatistics->open();
         $dates = $reportStatistics->getGridBlock()->getRowsData(['updated_at']);
         $currentDate = new \DateTime('now', new \DateTimeZone($_ENV['magento_timezone']));
-        $currentDate = date('M d, Y, g', $currentDate->getTimestamp());
+        $currentDate = date('M j, Y, g', $currentDate->getTimestamp());
         foreach ($dates as $date) {
             \PHPUnit_Framework_Assert::assertContains(
                 $currentDate,
