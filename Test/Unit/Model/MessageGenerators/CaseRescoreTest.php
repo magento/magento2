e7a9dcee11f800f9c9ed5ba7fd92fbbcab1e03d2
isavchuk <isavchuk@magento.com>
2017-01-06 13:49:37 +0200

    MAGETWO-62806: Update case entity
    - Added tests for MessageGenerators

---
+++ Test/Unit/Model/MessageGenerators/CaseRescoreTest.php
@@ -0,0 +1,151 @@
+<?php
+/**
+ * Copyright © 2017 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Signifyd\Test\Unit\Model\MessageGenerators;
+
+use Magento\Signifyd\Model\MessageGenerators\CaseRescore;
+use Magento\Signifyd\Model\Validators\CaseDataValidator;
+use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
+use Magento\Signifyd\Api\CaseRepositoryInterface;
+use Magento\Signifyd\Api\Data\CaseInterface;
+
+/**
+ * Tests for Signifyd CaseRescore message generator.
+ *
+ * Class CaseRescoreTest
+ */
+class CaseRescoreTest extends \PHPUnit_Framework_TestCase
+{
+    private static $data = [
+        'caseId' => 100,
+        'score'  => 200
+    ];
+
+    /**
+     * @var ObjectManager
+     */
+    private $objectManager;
+
+    /**
+     * @var CaseRepository|MockObject
+     */
+    private $caseRepository;
+
+    /**
+     * @var CaseRescore|MocObject
+     */
+    private $CaseRescore;
+
+    /**
+     * @var Case|MockObject
+     */
+    private $case;
+
+    /**
+     * @inheritdoc
+     */
+    protected function setUp()
+    {
+        $this->case = $this->getMockBuilder(CaseInterface::class)
+                           ->disableOriginalConstructor()
+                           ->getMock();
+        $this->objectManager = new ObjectManager($this);
+        $this->caseRepository = $this->getMockBuilder(CaseRepositoryInterface::class)
+                                     ->disableOriginalConstructor()
+                                     ->getMock();
+
+        $this->CaseRescore = $this->objectManager->getObject(CaseRescore::class, [
+            'caseDataValidator' => new CaseDataValidator(),
+            'caseRepository'    => $this->caseRepository
+        ]);
+
+    }
+
+    /**
+     * Data array without required attribute caseId.
+     *
+     * @expectedException        \Magento\Signifyd\Model\MessageGeneratorException
+     * @expectedExceptionMessage The "caseId" should not be empty
+     */
+    public function testGenerateEmptyCaseIdException()
+    {
+        $this->CaseRescore->generate([]);
+    }
+
+    /**
+     * Case entity was not found in DB.
+     *
+     * @expectedException        \Magento\Signifyd\Model\MessageGeneratorException
+     * @expectedExceptionMessage Case entity not found.
+     */
+    public function testGenerateNotFoundException()
+    {
+        $this->caseRepository->expects($this->once())
+                             ->method('getByCaseId')
+                             ->with(self::$data['caseId'])
+                             ->willReturn(null);
+
+        $this->CaseRescore = $this->objectManager->getObject(CaseRescore::class, [
+            'caseDataValidator' => new CaseDataValidator(),
+            'caseRepository'    => $this->caseRepository
+        ]);
+
+        $this->CaseRescore->generate(self::$data);
+    }
+
+    /**
+     * Generate case message with not empty previous score.
+     */
+    public function testGenerateWithPreviousScore()
+    {
+        $this->case->expects($this->once())
+                   ->method('getScore')
+                   ->willReturn(self::$data['score']);
+
+        $this->caseRepository->expects($this->once())
+                             ->method('getByCaseId')
+                             ->with(self::$data['caseId'])
+                             ->willReturn($this->case);
+
+        $this->CaseRescore = $this->objectManager->getObject(CaseRescore::class, [
+            'caseDataValidator' => new CaseDataValidator(),
+            'caseRepository'    => $this->caseRepository
+        ]);
+
+        $phrase = __(
+            'Case Update: New score for the order is %1. Previous score was %2.',
+            self::$data['score'],
+            self::$data['score']);
+
+        $message = $this->CaseRescore->generate(self::$data);
+
+        $this->assertEquals($phrase, $message);
+    }
+
+    /**
+     * Generate case message with empty previous score.
+     */
+    public function testGenerateWithoutPreviousScore()
+    {
+        $this->caseRepository->expects($this->once())
+                             ->method('getByCaseId')
+                             ->with(self::$data['caseId'])
+                             ->willReturn($this->case);
+
+        $this->CaseRescore = $this->objectManager->getObject(CaseRescore::class, [
+            'caseDataValidator' => new CaseDataValidator(),
+            'caseRepository'    => $this->caseRepository
+        ]);
+
+        $phrase = __(
+            'Case Update: New score for the order is %1. Previous score was %2.',
+            self::$data['score'],
+            null);
+
+        $message = $this->CaseRescore->generate(self::$data);
+
+        $this->assertEquals($phrase, $message);
+    }
+}
