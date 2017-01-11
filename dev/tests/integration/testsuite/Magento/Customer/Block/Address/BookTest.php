044eb5ab2c7d333376868f03fd8ff117a7e1f512
Douglas Radburn <douglas.radburn@purenet.co.uk>
2017-01-11 15:22:59 +0000

    updated tests relating to mis-matched <br /> tags

--- dev/tests/integration/testsuite/Magento/Customer/Block/Address/BookTest.php
+++ dev/tests/integration/testsuite/Magento/Customer/Block/Address/BookTest.php
@@ -118,8 +118,8 @@ class BookTest extends \PHPUnit_Framework_TestCase
      */
     public function testGetAddressHtml()
     {
-        $expected = "John Smith<br/>\nCompanyName<br />\nGreen str, 67<br />\n\n\n\nCityM,  Alabama, 75477<br/>" .
-            "\nUnited States<br/>\nT: <a href=\"tel:3468676\">3468676</a>\n\n";
+        $expected = "John Smith<br />\nCompanyName<br />\nGreen str, 67<br />\n\n\n\nCityM,  Alabama, 75477<br />" .
+            "\nUnited States<br />\nT: <a href=\"tel:3468676\">3468676</a>\n\n";
         $address = Bootstrap::getObjectManager()->get(
             \Magento\Customer\Api\AddressRepositoryInterface::class
         )->getById(1);
