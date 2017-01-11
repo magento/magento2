8b47d09e57e0b5fa9467fa9e889b08018f89890f
Viktor Tymchynskyi <vtymchynskyi@magento.com>
2017-01-11 09:19:20 -0600

    MAGETWO-62821: Controller to submit for Guarantee
    - Fix static

--- dev/tests/integration/testsuite/Magento/Signifyd/Controller/Adminhtml/Guarantee/CreateTest.php
+++ dev/tests/integration/testsuite/Magento/Signifyd/Controller/Adminhtml/Guarantee/CreateTest.php
@@ -1,7 +1,6 @@
 <?php
-
 /**
- * Copyright © 2013-2017 Magento. All rights reserved.
+ * Copyright © 2013-2017 Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
 namespace Magento\Signifyd\Controller\Adminhtml\Guarantee;
