c7dc5ed64927d1b096090035bd5e84cd86186949
Oleksandr Radchenko <oradchenko@magento.com>
2017-01-11 14:37:42 +0200

    MAGETWO-62757: Implement observer for sign-up operation

--- app/code/Magento/Analytics/Cron/SignUp.php
+++ app/code/Magento/Analytics/Cron/SignUp.php
@@ -8,7 +8,6 @@ namespace Magento\Analytics\Cron;
 use Magento\Analytics\Model\AnalyticsConnector;
 use Magento\Analytics\Model\Config\Backend\Enabled\SubscriptionHandler;
 use Magento\Framework\App\Config\Storage\WriterInterface;
-use Psr\Log\LoggerInterface;
 use Magento\AdminNotification\Model\InboxFactory;
 use Magento\AdminNotification\Model\ResourceModel\Inbox as InboxResource;
 use Magento\Analytics\Model\FlagManager;
@@ -29,11 +28,6 @@ class SignUp
     private $configWriter;
 
     /**
-     * @var LoggerInterface
-     */
-    private $logger;
-
-    /**
      * @var InboxFactory
      */
     private $inboxFactory;
@@ -52,7 +46,6 @@ class SignUp
      * SignUp constructor.
      * @param AnalyticsConnector $analyticsConnector
      * @param WriterInterface $configWriter
-     * @param LoggerInterface $logger
      * @param InboxFactory $inboxFactory
      * @param InboxResource $inboxResource
      * @param FlagManager $flagManager
@@ -60,14 +53,12 @@ class SignUp
     public function __construct(
         AnalyticsConnector $analyticsConnector,
         WriterInterface $configWriter,
-        LoggerInterface $logger,
         InboxFactory $inboxFactory,
         InboxResource $inboxResource,
         FlagManager $flagManager
     ) {
         $this->analyticsConnector = $analyticsConnector;
         $this->configWriter = $configWriter;
-        $this->logger = $logger;
         $this->inboxFactory = $inboxFactory;
         $this->inboxResource = $inboxResource;
         $this->flagManager = $flagManager;
@@ -96,17 +87,11 @@ class SignUp
             $this->inboxResource->save($inboxNotification);
             return false;
         }
+
         $attemptsCount -= 1;
         $this->flagManager->saveFlag(SubscriptionHandler::ATTEMPTS_REVERSE_COUNTER_FLAG_CODE, $attemptsCount);
-        $generateTokenResult = $this->analyticsConnector->execute('generateTokenCommand');
-        if ($generateTokenResult === false) {
-            $this->writeErrorLog("The attempt of subscription was unsuccessful on step generate token.");
-            return false;
-        }
-
         $signUpResult = $this->analyticsConnector->execute('signUp');
         if ($signUpResult === false) {
-            $this->writeErrorLog("The attempt of subscription was unsuccessful on step sign-up.");
             return false;
         }
 
@@ -122,17 +107,6 @@ class SignUp
     private function deleteAnalyticsCronExpr()
     {
         $this->configWriter->delete(SubscriptionHandler::CRON_STRING_PATH);
-        return true;
-    }
-
-    /**
-     * Write Error Log
-     * @param string $value
-     * @return bool
-     */
-    private function writeErrorLog($value)
-    {
-        $this->logger->warning($value);
         return true;
     }
 }
