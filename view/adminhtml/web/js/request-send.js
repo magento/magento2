392024762819a0510a257c5c62ba3d7516bd4c1a
Iurii Ivashchenko <iivashchenko@magento.com>
2017-01-11 19:35:35 +0200

    MAGETWO-62820: Create toolbar button to send Guarantee request

---
+++ view/adminhtml/web/js/request-send.js
@@ -0,0 +1,24 @@
+/**
+ * Copyright © 2013-2017 Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+define([
+    'mageUtils',
+    'Magento_Ui/js/form/components/button'
+], function (utils, Button) {
+    'use strict';
+
+    return Button.extend({
+
+        /**
+         * Creates and submits form for Guarantee create/cancel
+         */
+        sendRequest: function() {
+            utils.submit({
+                url: this.requestURL,
+                data: this.data
+            });
+        }
+    });
+});
