229c2963f3b95123fa76aa7e2227bf7cabfa7678
Iurii Ivashchenko <iivashchenko@magento.com>
2017-01-12 19:20:43 +0200

    MAGETWO-62807: Case information block on order details page in admin panel
    - refactoring

--- Block/Adminhtml/CaseInfo.php
+++ Block/Adminhtml/CaseInfo.php
@@ -5,10 +5,8 @@
  */
 namespace Magento\Signifyd\Block\Adminhtml;
 
-use Magento\Backend\Block\Template\Context;
-use Magento\Framework\Registry;
-use Magento\Sales\Block\Adminhtml\Order\AbstractOrder;
-use Magento\Sales\Helper\Admin;
+use Magento\Framework\View\Element\Template;
+use Magento\Framework\View\Element\Template\Context;
 use Magento\Signifyd\Model\Config;
 use Magento\Signifyd\Model\CaseManagement;
 use Magento\Signifyd\Api\Data\CaseInterface;
@@ -17,12 +15,17 @@ use Magento\Signifyd\Model\Guarantee\CreateGuaranteeAbility;
 /**
  * Get Signifyd Case Info
  */
-class CaseInfo extends AbstractOrder
+class CaseInfo extends Template
 {
     /**
      * @var Config
      */
     private $config;
+
+    /**
+     * @var CaseInterface
+     */
+    private $caseEntity = null;
 
     /**
      * @var CaseManagement
@@ -48,8 +51,6 @@ class CaseInfo extends AbstractOrder
      * Constructor
      *
      * @param Context $context
-     * @param Registry $registry
-     * @param Admin $adminHelper
      * @param Config $config
      * @param CaseManagement $caseManagement
      * @param CreateGuaranteeAbility $createGuaranteeAbility
@@ -57,8 +58,6 @@ class CaseInfo extends AbstractOrder
      */
     public function __construct(
         Context $context,
-        Registry $registry,
-        Admin $adminHelper,
         Config $config,
         CaseManagement $caseManagement,
         CreateGuaranteeAbility $createGuaranteeAbility,
@@ -68,29 +67,7 @@ class CaseInfo extends AbstractOrder
         $this->caseManagement = $caseManagement;
         $this->createGuaranteeAbility = $createGuaranteeAbility;
 
-        parent::__construct($context, $registry, $adminHelper, $data);
-    }
-
-    /**
-     * Retrieve required options from parent
-     *
-     * @throws \Magento\Framework\Exception\LocalizedException
-     * @return void
-     */
-    protected function _beforeToHtml()
-    {
-        if (!$this->getParentBlock()) {
-            throw new \Magento\Framework\Exception\LocalizedException(
-                __('Please correct the parent block for this block.')
-            );
-        }
-        $this->setOrder($this->getParentBlock()->getOrder());
-
-        foreach ($this->getParentBlock()->getOrderInfoData() as $key => $value) {
-            $this->setDataUsingMethod($key, $value);
-        }
-
-        parent::_beforeToHtml();
+        parent::__construct($context, $data);
     }
 
     /**
@@ -108,32 +85,130 @@ class CaseInfo extends AbstractOrder
      *
      * @return CaseInterface|null
      */
-    public function getCaseEntity()
+    private function getCaseEntity()
     {
-        return $this->caseManagement->getByOrderId($this->getOrderId());
+        if (is_null($this->caseEntity)) {
+            $this->caseEntity = $this->caseManagement->getByOrderId(
+                $this->getOrderId()
+            );
+        }
+
+        return $this->caseEntity;
+    }
+
+    /**
+     * Checks if case is exists for order
+     *
+     * @return bool
+     */
+    public function isEmptyCase()
+    {
+        return is_null($this->getCaseEntity());
+    }
+
+    /**
+     * Gets case status
+     *
+     * @return string
+     */
+    public function getCaseStatus()
+    {
+        return $this->isEmptyCase() ? '' : $this->getCaseEntity()->getStatus();
+    }
+
+    /**
+     * Gets case score value
+     *
+     * @return int
+     */
+    public function getCaseScore()
+    {
+        return $this->isEmptyCase() ? 0 : $this->getCaseEntity()->getScore();
     }
 
     /**
      * Gets state of case guarantee eligible.
      *
-     * @param CaseInterface $caseEntity
-     * @return \Magento\Framework\Phrase
+     * @return string|\Magento\Framework\Phrase
      */
-    public function getGuaranteeEligible(CaseInterface $caseEntity)
+    public function getCaseGuaranteeEligible()
     {
-        return $caseEntity->isGuaranteeEligible() ? __('Yes') : __('No');
+        if ($this->isEmptyCase()) {
+            return '';
+        }
+
+        return $this->getCaseEntity()->isGuaranteeEligible() ? __('Yes') : __('No');
     }
 
     /**
-     * Gets associated team name.
+     * Gets case guarantee disposition status.
      *
-     * @param CaseInterface $caseEntity
      * @return string
      */
-    public function getAssociatedTeam(CaseInterface $caseEntity)
+    public function getCaseGuaranteeDisposition()
     {
+        if ($this->isEmptyCase()) {
+            return '';
+        }
+
+        return $this->getCaseEntity()->getGuaranteeDisposition();
+    }
+
+    /**
+     * Gets case review disposition status.
+     *
+     * @return string
+     */
+    public function getCaseReviewDisposition()
+    {
+        if ($this->isEmptyCase()) {
+            return '';
+        }
+
+        return $this->getCaseEntity()->getReviewDisposition();
+    }
+
+    /**
+     * Gets case create date.
+     *
+     * @return string
+     */
+    public function getCaseCreatedAt()
+    {
+        if ($this->isEmptyCase()) {
+            return '';
+        }
+
+        return $this->getCaseEntity()->getCreatedAt();
+    }
+
+    /**
+     * Gets case update date.
+     *
+     * @return string
+     */
+    public function getCaseUpdatedAt()
+    {
+        if ($this->isEmptyCase()) {
+            return '';
+        }
+
+        return $this->getCaseEntity()->getUpdatedAt();
+    }
+
+    /**
+     * Gets case associated team name.
+     *
+     * @return string
+     */
+    public function getCaseAssociatedTeam()
+    {
+        if ($this->isEmptyCase()) {
+            return '';
+        }
+
         $result = 'unknown';
-        $team = $caseEntity->getAssociatedTeam();
+        $team = $this->getCaseEntity()->getAssociatedTeam();
         if (isset($team['teamName'])) {
             $result = $team['teamName'];
         }
@@ -145,12 +220,15 @@ class CaseInfo extends AbstractOrder
      * Returns cell class name according to case score value.
      * It could be used by merchant to customize order view template.
      *
-     * @param CaseInterface $caseEntity
      * @return string
      */
-    public function getScoreClass(CaseInterface $caseEntity)
+    public function getScoreClass()
     {
-        $score = $caseEntity->getScore();
+        if ($this->isEmptyCase()) {
+            return '';
+        }
+
+        $score = $this->getCaseEntity()->getScore();
 
         if (self::$scoreAccept <= $score) {
             $result = 'green';
@@ -201,6 +279,6 @@ class CaseInfo extends AbstractOrder
      */
     private function getOrderId()
     {
-        return $this->getOrder()->getEntityId();
+        return (int) $this->getRequest()->getParam('order_id');
     }
 }
