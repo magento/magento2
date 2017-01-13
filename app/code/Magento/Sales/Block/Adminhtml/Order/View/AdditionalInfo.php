b6082dc22454172dfddbf85eae3127b619581cd9
Dmytro Yushkin <dyushkin@magento.com>
2017-01-13 13:10:37 +0200

    MAGETWO-62807: Case information block on order details page in admin panel
    - Extension point created for sales order view

---
+++ app/code/Magento/Sales/Block/Adminhtml/Order/View/AdditionalInfo.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Copyright © 2013-2017 Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Sales\Block\Adminhtml\Order\View;
+
+use Magento\Backend\Block\Template;
+
+class AdditionalInfo extends Template
+{
+
+}
