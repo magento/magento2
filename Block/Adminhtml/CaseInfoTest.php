05665d5b5ea795bf6dc7a905d7713e1098fc7001
Dmytro Yushkin <dyushkin@magento.com>
2017-01-13 19:17:38 +0200

    MAGETWO-62807: Case information block on order details page in admin panel
    - Fixed integration tests

--- Block/Adminhtml/CaseInfoTest.php
+++ Block/Adminhtml/CaseInfoTest.php
@@ -6,23 +6,26 @@
 
 namespace Magento\Signifyd\Block\Adminhtml;
 
-use Magento\Framework\Registry;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\ObjectManagerInterface;
+use Magento\Framework\View\Element\Template\Context;
 use Magento\Framework\View\LayoutInterface;
-use Magento\Sales\Block\Adminhtml\Order\View\Tab\Info as OrderTabInfo;
+use Magento\Sales\Block\Adminhtml\Order\View\AdditionalInfo as OrderAdditionalInfo;
 use Magento\Sales\Model\Order;
+use Magento\Sales\Model\OrderRepository;
 use Magento\TestFramework\Helper\Bootstrap;
 
 class CaseInfoTest extends \PHPUnit_Framework_TestCase
 {
     /**
+     * @var ObjectManagerInterface
+     */
+    private $objectManager;
+
+    /**
      * @var Order
      */
     private $order;
-
-    /**
-     * @var Registry
-     */
-    private $registry;
 
     /**
      * @var LayoutInterface
@@ -34,17 +37,16 @@ class CaseInfoTest extends \PHPUnit_Framework_TestCase
      */
     protected function setUp()
     {
-        $objectManager = Bootstrap::getObjectManager();
-        $this->order = $objectManager->create(Order::class);
-        $this->registry = $objectManager->get(Registry::class);
-        $this->layout = $objectManager->get(LayoutInterface::class);
+        $this->objectManager = Bootstrap::getObjectManager();
+        $this->order = $this->objectManager->create(Order::class);
+        $this->layout = $this->objectManager->get(LayoutInterface::class);
     }
 
     /**
      * Checks that block does not give contents
      * if Signifyd module is inactive.
      *
-     * @covers CaseInfo::isModuleActive
+     * @covers \Magento\Signifyd\Block\Adminhtml\CaseInfo::isModuleActive
      * @magentoConfigFixture current_store fraud_protection/signifyd/active 0
      * @magentoAppArea adminhtml
      */
@@ -57,14 +59,14 @@ class CaseInfoTest extends \PHPUnit_Framework_TestCase
      * Checks that block does not give contents
      * if there is no case entity created for order.
      *
-     * @covers CaseInfo::getCaseEntity
+     * @covers \Magento\Signifyd\Block\Adminhtml\CaseInfo::getCaseEntity
      * @magentoConfigFixture current_store fraud_protection/signifyd/active 1
      * @magentoDataFixture Magento/Signifyd/_files/order_with_customer_and_two_simple_products.php
      * @magentoAppArea adminhtml
      */
     public function testCaseEntityNotExists()
     {
-        $this->registry->register('current_order', $this->order->loadByIncrementId('100000001'));
+        $this->order->loadByIncrementId('100000001');
 
         static::assertEmpty($this->getBlockContents());
     }
@@ -75,15 +77,15 @@ class CaseInfoTest extends \PHPUnit_Framework_TestCase
      * - associated team displays correct
      * - score class displays correct
      *
-     * @covers CaseInfo::getAssociatedTeam
-     * @covers CaseInfo::getScoreClass
+     * @covers \Magento\Signifyd\Block\Adminhtml\CaseInfo::getAssociatedTeam
+     * @covers \Magento\Signifyd\Block\Adminhtml\CaseInfo::getScoreClass
      * @magentoConfigFixture current_store fraud_protection/signifyd/active 1
      * @magentoDataFixture Magento/Signifyd/_files/case.php
      * @magentoAppArea adminhtml
      */
     public function testCaseEntityExists()
     {
-        $this->registry->register('current_order', $this->order->loadByIncrementId('100000001'));
+        $this->order->loadByIncrementId('100000001');
 
         $html = $this->getBlockContents();
         static::assertNotEmpty($html);
@@ -94,14 +96,14 @@ class CaseInfoTest extends \PHPUnit_Framework_TestCase
     /**
      * Checks that guarantee action buttons is available on order page.
      *
-     * @covers CaseInfo::getButtons
+     * @covers \Magento\Signifyd\Block\Adminhtml\CaseInfo::getButtons
      * @magentoConfigFixture current_store fraud_protection/signifyd/active 1
      * @magentoDataFixture Magento/Signifyd/_files/case.php
      * @magentoAppArea adminhtml
      */
     public function testButtonsAvailable()
     {
-        $this->registry->register('current_order', $this->order->loadByIncrementId('100000001'));
+        $this->order->loadByIncrementId('100000001');
 
         static::assertContains('Submit Guarantee request', $this->getBlockContents());
     }
@@ -109,16 +111,19 @@ class CaseInfoTest extends \PHPUnit_Framework_TestCase
     /**
      * Checks that guarantee action buttons is unavailable on order page.
      *
-     * @covers CaseInfo::getButtons
+     * @covers \Magento\Signifyd\Block\Adminhtml\CaseInfo::getButtons
      * @magentoConfigFixture current_store fraud_protection/signifyd/active 1
      * @magentoDataFixture Magento/Signifyd/_files/case.php
      * @magentoAppArea adminhtml
      */
     public function testButtonsUnavailable()
     {
-        $this->registry->register('current_order', $this->order->loadByIncrementId('100000001'));
-
+        $this->order->loadByIncrementId('100000001');
         $this->order->setState(Order::STATE_CANCELED);
+
+        /** @var OrderRepository $orderRepository */
+        $orderRepository = $this->objectManager->get(OrderRepository::class);
+        $orderRepository->save($this->order);
 
         static::assertNotContains('Submit Guarantee request', $this->getBlockContents());
     }
@@ -131,13 +136,27 @@ class CaseInfoTest extends \PHPUnit_Framework_TestCase
     private function getBlockContents()
     {
         /** @var CaseInfo $block */
-        $block = $this->layout->createBlock(CaseInfo::class, 'order_case_info');
+        $block = $this->layout->createBlock(CaseInfo::class, 'order_case_info', ['context' => $this->getContext()]);
         $block->setTemplate('Magento_Signifyd::case_info.phtml');
 
-        /** @var OrderTabInfo $parent */
-        $parent = $this->layout->createBlock(OrderTabInfo::class, 'order_tab_info');
+        /** @var OrderAdditionalInfo $parent */
+        $parent = $this->layout->createBlock(OrderAdditionalInfo::class, 'order_additional_info');
         $parent->setChild('order_case_info', $block);
 
         return $block->toHtml();
+    }
+
+    /**
+     * Creates template context with necessary order id param.
+     *
+     * @return Context
+     */
+    private function getContext()
+    {
+        /** @var RequestInterface $request */
+        $request = $this->objectManager->get(RequestInterface::class);
+        $request->setParams(['order_id' => $this->order->getEntityId()]);
+
+        return $this->objectManager->create(Context::class, ['request' => $request]);
     }
 }
