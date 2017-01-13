b35187174567bff5ee7664e56fa4869089f32209
Iurii Ivashchenko <iivashchenko@magento.com>
2017-01-13 09:28:35 -0600

    MAGETWO-62807: Case information block on order details page in admin panel
    - refactoring

--- app/code/Magento/Signifyd/Test/Unit/Block/Adminhtml/CaseInfoTest.php
+++ app/code/Magento/Signifyd/Test/Unit/Block/Adminhtml/CaseInfoTest.php
@@ -5,10 +5,15 @@
  */
 namespace Magento\Signifyd\Test\Unit\Block\Adminhtml;
 
-use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
-use Magento\Signifyd\Api\Data\CaseInterface;
-use Magento\Signifyd\Block\Adminhtml\CaseInfo;
 use PHPUnit_Framework_MockObject_MockObject as MockObject;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
+use Magento\Framework\View\Element\Template\Context;
+use Magento\Signifyd\Api\Data\CaseInterface;
+use Magento\Signifyd\Model\Config;
+use Magento\Signifyd\Model\CaseManagement;
+use Magento\Signifyd\Model\Guarantee\CreateGuaranteeAbility;
+use Magento\Signifyd\Block\Adminhtml\CaseInfo;
 
 /**
  * Tests for Signifyd block information.
@@ -28,18 +33,71 @@ class CaseInfoTest extends \PHPUnit_Framework_TestCase
     private $caseInfo;
 
     /**
+     * @var Context
+     */
+    private $context;
+
+    /**
+     * @var Config
+     */
+    private $config;
+
+    /**
+     * @var CaseManagement
+     */
+    private $caseManagement;
+
+    /**
+     * @var CreateGuaranteeAbility
+     */
+    private $createGuaranteeAbility;
+
+    /**
+     * @var RequestInterface
+     */
+    private $request;
+
+    /**
      * @inheritdoc
      */
     protected function setUp()
     {
         $objectManager = new ObjectManager($this);
 
+        $this->context = $this->getMockBuilder(Context::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->request = $this->getMockBuilder(RequestInterface::class)
+            ->getMockForAbstractClass();
+
+        $this->context->expects(self::once())
+            ->method('getRequest')
+            ->willReturn($this->request);
+
+        $this->config = $this->getMockBuilder(Config::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->caseManagement = $this->getMockBuilder(CaseManagement::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->createGuaranteeAbility = $this->getMockBuilder(CreateGuaranteeAbility::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
         $this->caseEntity = $this->getMockBuilder(CaseInterface::class)
             ->disableOriginalConstructor()
             ->setMethods(['getScore'])
             ->getMockForAbstractClass();
 
-        $this->caseInfo = $objectManager->getObject(CaseInfo::class);
+        $this->caseInfo = $objectManager->getObject(CaseInfo::class, [
+            'context' => $this->context,
+            'config' => $this->config,
+            'caseManagement' => $this->caseManagement,
+            'createGuaranteeAbility' => $this->createGuaranteeAbility
+        ]);
     }
 
     /**
@@ -56,9 +114,53 @@ class CaseInfoTest extends \PHPUnit_Framework_TestCase
             ->method('getScore')
             ->willReturn($score);
 
+        $this->caseManagement->expects(self::once())
+            ->method('getByOrderId')
+            ->willReturn($this->caseEntity);
+
         self::assertEquals(
             $expectedClassName,
-            $this->caseInfo->getScoreClass($this->caseEntity)
+            $this->caseInfo->getScoreClass()
+        );
+    }
+
+    /**
+     * Checks case property getter with real case.
+     *
+     * @covers \Magento\Signifyd\Block\CaseInfo::getCaseProperty
+     */
+    public function testCasePropertyWithCaseExists()
+    {
+        $score = 575;
+
+        $this->caseEntity->expects($this->once())
+            ->method('getScore')
+            ->willReturn($score);
+
+        $this->caseManagement->expects(self::once())
+            ->method('getByOrderId')
+            ->willReturn($this->caseEntity);
+
+        self::assertEquals(
+            $score,
+            $this->caseInfo->getCaseScore()
+        );
+    }
+
+    /**
+     * Checks case property getter with empty case.
+     *
+     * @covers \Magento\Signifyd\Block\CaseInfo::getCaseProperty
+     */
+    public function testCasePropertyWithEmptyCase()
+    {
+        $this->caseManagement->expects(self::once())
+            ->method('getByOrderId')
+            ->willReturn(null);
+
+        self::assertEquals(
+            0,
+            $this->caseInfo->getCaseScore()
         );
     }
 
