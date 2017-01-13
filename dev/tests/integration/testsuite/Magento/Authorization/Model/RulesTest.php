d1626827add8c466f6250fff9f31b9dbf44dcfee
Anton Kaplya <anton.kaplya@magento.com>
2017-01-13 15:38:32 +0200

    MAGETWO-62556: Subscription Request

--- dev/tests/integration/testsuite/Magento/Authorization/Model/RulesTest.php
+++ dev/tests/integration/testsuite/Magento/Authorization/Model/RulesTest.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright © 2017 Magento. All rights reserved.
+ * Copyright © 2013-2017 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
 namespace Magento\Authorization\Model;
