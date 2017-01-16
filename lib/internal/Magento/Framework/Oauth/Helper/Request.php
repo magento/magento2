6784a726e6a9757a6184cd36b1a7023f45297513
Carey Sizer <carey@balanceinternet.com.au>
2017-01-17 09:14:37 +1300

    Fix OAuth request helper to support Authorization header value parsing with non-leading OAuth key
    This prevents the Web API request validator from throwing a permissions error when OAuth is used in conjunction with Basic authorization (or other Authorization header values).
    
    Fixes #8149

--- lib/internal/Magento/Framework/Oauth/Helper/Request.php
+++ lib/internal/Magento/Framework/Oauth/Helper/Request.php
@@ -144,10 +144,10 @@ class Request
      */
     protected function _processHeader($authHeaderValue, &$protocolParams)
     {
-        if ($authHeaderValue && 'oauth' === strtolower(substr($authHeaderValue, 0, 5))) {
-            $authHeaderValue = substr($authHeaderValue, 6);
-            // ignore 'OAuth ' at the beginning
-
+        $oauthValuePosition = stripos(($authHeaderValue ? $authHeaderValue : ''), 'oauth ');
+        if ($authHeaderValue && $oauthValuePosition !== false) {
+            // Ignore anything before and including 'OAuth ' (trailing values validated later)
+            $authHeaderValue = substr($authHeaderValue, $oauthValuePosition + 6);
             foreach (explode(',', $authHeaderValue) as $paramStr) {
                 $nameAndValue = explode('=', trim($paramStr), 2);
 
