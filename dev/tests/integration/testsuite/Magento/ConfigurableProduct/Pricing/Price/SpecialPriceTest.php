ebda3e678b94b10b72070ea68bed7ec0ea097109
Valeriy Nayda <vnayda@magento.com>
2017-01-18 11:30:06 +0200

    MAGETWO-57023: [GitHub] Sorting configurable products by price doesn't work when simple product has special_price #4778
    -- integration tests

---
+++ dev/tests/integration/testsuite/Magento/ConfigurableProduct/Pricing/Price/SpecialPriceTest.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Copyright © 2013-2017 Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\ConfigurableProduct\Pricing\Price;
+
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Catalog\Model\Product;
+use Magento\Catalog\Model\Product\Visibility;
+use Magento\Catalog\Model\ResourceModel\Product\Collection as ProductCollection;
+use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory;
+use Magento\Framework\Data\Collection;
+use Magento\TestFramework\Helper\Bootstrap;
+
+class SpecialPriceTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var ProductRepositoryInterface
+     */
+    private $productRepository;
+
+    /**
+     * @var CollectionFactory
+     */
+    private $productCollectionFactory;
+
+    protected function setUp()
+    {
+        $this->productRepository = Bootstrap::getObjectManager()->get(ProductRepositoryInterface::class);
+        $this->productCollectionFactory = Bootstrap::getObjectManager()->get(CollectionFactory::class);
+    }
+
+    /**
+     * @magentoDataFixture Magento/ConfigurableProduct/_files/product_configurable.php
+     * @magentoDataFixture Magento/ConfigurableProduct/_files/product_simple_77.php
+     */
+    public function testSortingProductsWithoutSpecialPrice()
+    {
+        $childProduct = $this->productRepository->get('simple_77', true);
+        $childProduct->setData('price', 5);
+        $this->productRepository->save($childProduct);
+
+        /** @var ProductCollection $collection */
+        $collection = $this->productCollectionFactory->create();
+        $collection
+            ->setVisibility([Visibility::VISIBILITY_IN_CATALOG, Visibility::VISIBILITY_BOTH])
+            ->setOrder('price', Collection::SORT_ORDER_DESC);
+
+        /** @var Product[] $items */
+        $items = array_values($collection->getItems());
+        self::assertEquals('configurable', $items[0]->getSku());
+        self::assertEquals('simple_77', $items[1]->getSku());
+    }
+
+    /**
+     * @magentoDataFixture Magento/ConfigurableProduct/_files/product_configurable.php
+     * @magentoDataFixture Magento/ConfigurableProduct/_files/product_simple_77.php
+     */
+    public function testSortingProductIfChildHasSpecialPrice()
+    {
+        $specialPrice = 2;
+        $childProduct = $this->productRepository->get('simple_20', true);
+        $childProduct->setData('special_price', $specialPrice)->setData('price', 5);
+        $this->productRepository->save($childProduct);
+
+        /** @var ProductCollection $collection */
+        $collection = $this->productCollectionFactory->create();
+        $collection
+            ->setVisibility([Visibility::VISIBILITY_IN_CATALOG, Visibility::VISIBILITY_BOTH])
+            ->setOrder('price', Collection::SORT_ORDER_DESC);
+
+        /** @var Product[] $items */
+        $items = array_values($collection->getItems());
+        self::assertEquals('simple_77', $items[0]->getSku());
+        self::assertEquals('configurable', $items[1]->getSku());
+    }
+}
