ebf7c42a6a3a093e77de41f4fcb91bcbd75fd6c8
Stanislav Idolov <sidolov@magento.com>
2017-01-19 11:08:55 +0200

    MAGETWO-63272: Trimming minicart and checkout summary block

--- app/code/Magento/Checkout/view/frontend/web/js/view/summary/cart-items.js
+++ app/code/Magento/Checkout/view/frontend/web/js/view/summary/cart-items.js
@@ -14,15 +14,60 @@ define(
     ],
     function (ko, totals, Component, stepNavigator, quote) {
         'use strict';
+
         return Component.extend({
             defaults: {
                 template: 'Magento_Checkout/summary/cart-items'
             },
             totals: totals.totals(),
-            getItems: totals.getItems(),
-            getItemsQty: function() {
-                return parseFloat(this.totals.items_qty);
+            items: ko.observable([]),
+            maxCartItemsToDisplay: window.checkoutConfig.maxCartItemsToDisplay,
+            cartUrl: window.checkoutConfig.cartUrl,
+
+            /**
+             * Component init
+             */
+            initialize: function () {
+                var self = this;
+
+                this._super();
+
+                // Set initial items to observable field
+                this.setItems(totals.getItems()());
+
+                // Subscribe for items data changes and refresh items in view
+                totals.getItems().subscribe(function (items) {
+                    self.setItems(items);
+                });
             },
+
+            /**
+             * Set items to observable field
+             *
+             * @param {Object} items
+             */
+            setItems: function (items) {
+
+                if (items && items.length > 0) {
+                    items = items.slice(-this.maxCartItemsToDisplay);
+                }
+                this.items(items);
+            },
+
+            /**
+             * Returns cart items count
+             *
+             * @returns {Number}
+             */
+            getItemsQty: function () {
+                return parseFloat(this.totals['items_qty']);
+            },
+
+            /**
+             * Returns bool value for items block state (expanded or not)
+             *
+             * @returns {*|Boolean}
+             */
             isItemsBlockExpanded: function () {
                 return quote.isVirtual() || stepNavigator.isProcessed('shipping');
             }
