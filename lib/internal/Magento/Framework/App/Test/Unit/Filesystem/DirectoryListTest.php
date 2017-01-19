e0b2623912eab03e01fd17d82d216766f83018e5
Oleh Posyniak <oposyniak@magento.com>
2017-01-19 13:00:32 +0200

    MAGETWO-63295: Installation fails because 'generated' directory is not writable

--- lib/internal/Magento/Framework/App/Test/Unit/Filesystem/DirectoryListTest.php
+++ lib/internal/Magento/Framework/App/Test/Unit/Filesystem/DirectoryListTest.php
@@ -7,6 +7,7 @@
 namespace Magento\Framework\App\Test\Unit\Filesystem;
 
 use \Magento\Framework\App\Filesystem\DirectoryList;
+use Magento\Framework\Code\Generator\Io;
 
 class DirectoryListTest extends \PHPUnit_Framework_TestCase
 {
@@ -37,5 +38,42 @@ class DirectoryListTest extends \PHPUnit_Framework_TestCase
     public function testUnknownDirectory()
     {
         new DirectoryList('/root/dir', ['test' => [DirectoryList::PATH => '/baz']]);
+    }
+
+    public function testGetDefaultConfig()
+    {
+        $this->assertEquals(
+            [
+                DirectoryList::ROOT => [DirectoryList::PATH => ''],
+                DirectoryList::APP => [DirectoryList::PATH => 'app'],
+                DirectoryList::CONFIG => [DirectoryList::PATH => 'app/etc'],
+                DirectoryList::LIB_INTERNAL => [DirectoryList::PATH => 'lib/internal'],
+                DirectoryList::VAR_DIR => [DirectoryList::PATH => 'var'],
+                DirectoryList::CACHE => [DirectoryList::PATH => 'var/cache'],
+                DirectoryList::LOG => [DirectoryList::PATH => 'var/log'],
+                DirectoryList::DI => [DirectoryList::PATH => 'generated/metadata'],
+                DirectoryList::GENERATION => [DirectoryList::PATH => Io::DEFAULT_DIRECTORY],
+                DirectoryList::SESSION => [DirectoryList::PATH => 'var/session'],
+                DirectoryList::MEDIA => [DirectoryList::PATH => 'pub/media', DirectoryList::URL_PATH => 'pub/media'],
+                DirectoryList::STATIC_VIEW => [
+                    DirectoryList::PATH => 'pub/static',
+                    DirectoryList::URL_PATH => 'pub/static'
+                ],
+                DirectoryList::PUB => [DirectoryList::PATH => 'pub', DirectoryList::URL_PATH => 'pub'],
+                DirectoryList::LIB_WEB => [DirectoryList::PATH => 'lib/web'],
+                DirectoryList::TMP => [DirectoryList::PATH => 'var/tmp'],
+                DirectoryList::UPLOAD => [
+                    DirectoryList::PATH => 'pub/media/upload',
+                    DirectoryList::URL_PATH => 'pub/media/upload'
+                ],
+                DirectoryList::TMP_MATERIALIZATION_DIR => [DirectoryList::PATH => 'var/view_preprocessed'],
+                DirectoryList::TEMPLATE_MINIFICATION_DIR => [DirectoryList::PATH => 'var/view_preprocessed/html'],
+                DirectoryList::SETUP => [DirectoryList::PATH => 'setup/src'],
+                DirectoryList::COMPOSER_HOME => [DirectoryList::PATH => 'var/composer_home'],
+                DirectoryList::GENERATED => [DirectoryList::PATH => 'generated'],
+                DirectoryList::SYS_TMP => [DirectoryList::PATH => '']
+            ],
+            DirectoryList::getDefaultConfig()
+        );
     }
 }
