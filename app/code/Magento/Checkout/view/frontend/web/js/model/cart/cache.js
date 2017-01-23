6282f277384367d7f44e48081cd2259ce62270b5
Oleksandr Miroshnichenko <omiroshnichenko@magento.com>
2017-01-23 09:20:11 +0200

    MAGETWO-63273: Caching totals and shipping rates on the cart page

--- app/code/Magento/Checkout/view/frontend/web/js/model/cart/cache.js
+++ app/code/Magento/Checkout/view/frontend/web/js/model/cart/cache.js
@@ -2,229 +2,179 @@
  * Copyright © 2013-2017 Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
-/*jshint browser:true*/
-/*global alert*/
+
 /**
- * Cart adapter for customer data storage
+ * Cart adapter for customer data storage.
+ * It is store cart data in customer data(localStorage) without save on server.
+ * Adapter created for caching shipping and totals data, that eliminates unneeded requests for calculation.
  */
 define([
-    'jquery',
-    'Magento_Customer/js/customer-data',
-    'Magento_Checkout/js/model/quote'
-], function ($, storage, quote) {
+    'underscore',
+    'Magento_Customer/js/customer-data'
+], function (_, storage) {
     'use strict';
 
     var cacheKey = 'cart-data',
-        cart,
-        getData,
-        saveData,
-        cartData;
-
-    /**
-     * Get data from local storage
-     * @returns {Object}
-     */
-    getData = function () {
-        return storage.get(cacheKey)();
-    };
-
-    /**
-     * Save data to local storage
-     * @param {Object} checkoutData
-     */
-    saveData = function (checkoutData) {
-        storage.set(cacheKey, checkoutData);
-    };
-
-    if ($.isEmptyObject(getData())) {
         cartData = {
-            'totals': null,
-            'address': null,
-            'cartVersion': null,
-            'shippingMethodCode': null,
-            'shippingCarrierCode': null,
-            'rates': null
+            totals: null,
+            address: null,
+            cartVersion: null,
+            shippingMethodCode: null,
+            shippingCarrierCode: null,
+            rates: null
+        },
+
+        /**
+         * Get data from local storage.
+         *
+         * @param {String} [key]
+         * @returns {*}
+         */
+        getData = function (key) {
+            return key ? storage.get(cacheKey)()[key] : storage.get(cacheKey)();
+        },
+
+        /**
+         * Set data to local storage.
+         *
+         * @param {Object} checkoutData
+         */
+        setData = function (checkoutData) {
+            storage.set(cacheKey, checkoutData);
         };
-        saveData(cartData);
+
+    if (_.isEmpty(getData())) {
+        setData(cartData);
     }
 
-    cart = storage.get('cart');
-
+    /**
+     * Provides get/set/isChanged/clear methods for work with cart data.
+     * Can be customized via mixin functionality.
+     */
     return {
+        cartData: cartData,
+
         /**
          * Array of required address fields
          */
         requiredFields: ['countryId', 'region', 'regionId', 'postcode'],
 
         /**
-         * Set totals data to cache
+         * Get data from customer data.
+         * Concatenate provided key with method name and call method if it exist or makes get by key.
          *
-         * @param {Object} data
+         * @param {String} key
+         * @return {*}
          */
-        setTotalsCache: function (data) {
-            var obj = getData();
-            obj.totals = data;
-            saveData(obj);
+        get: function (key) {
+            var methodName;
+
+            if (key === cacheKey) {
+                return getData();
+            }
+
+            methodName = '_get' + key.charAt(0).toUpperCase() + key.slice(1);
+
+            if (this[methodName]) {
+                return this[methodName]();
+            }
+
+            return getData(key);
         },
 
         /**
-         * Get totals data from cache
+         * Set data to customer data.
+         * Concatenate provided key with method name and call method if it exist or makes set by key.
+         * @example _setCustomAddress method will be called, if it exist.
+         *  set('address', customAddressValue)
+         * @example Will set value by provided key.
+         *  set('rates', ratesToCompare)
          *
-         * @returns {null|Object}
+         * @param {String} key
+         * @param {*} value
          */
-        getTotalsCache: function () {
-            return getData().totals;
+        set: function (key, value) {
+            var methodName, obj;
+
+            if (key === cacheKey) {
+                _.each(value, function (val, k) {
+                    this.set(k, val);
+                }, this);
+
+                return;
+            }
+
+            methodName = '_set' + key.charAt(0).toUpperCase() + key.slice(1);
+
+            if (this[methodName]) {
+                this[methodName](value);
+            } else {
+                obj = getData();
+                obj[key] = value;
+                setData(obj);
+            }
         },
 
         /**
-         * Set address data to cache
+         * Clear data in cache.
+         * Concatenate provided key with method name and call method if it exist or clear by key.
+         * @example _clearCustomAddress method will be called, if it exist.
+         *  clear('customAddress')
+         * @example Will clear data by provided key.
+         *  clear('rates')
          *
-         * @param {Object} data
+         * @param {String} key
          */
-        setAddress: function (data) {
-            var obj = getData();
-            obj.address = data;
-            saveData(obj);
+        clear: function (key) {
+            var methodName;
+
+            if (key === cacheKey) {
+                setData(this.cartData);
+
+                return;
+            }
+
+            methodName = '_clear' + key.charAt(0).toUpperCase() + key.slice(1);
+
+            if (this[methodName]) {
+                this[methodName]();
+            } else {
+                this.set(key, null);
+            }
         },
 
         /**
-         * Get address object from cache
+         * Check if provided data has difference with cached data.
+         * Concatenate provided key with method name and call method if it exist or makes strict equality.
+         * @example Will call existing _isAddressChanged.
+         *  isChanged('address', addressToCompare)
+         * @example Will get data by provided key and make strict equality with provided value.
+         *  isChanged('rates', ratesToCompare)
          *
-         * @returns {Object}
+         * @param {String} key
+         * @param {*} value
+         * @return {Boolean}
          */
-        getAddress: function () {
-            return getData().address;
+        isChanged: function (key, value) {
+            var methodName = '_is' + key.charAt(0).toUpperCase() + key.slice(1) + 'Changed';
+
+            if (this[methodName]) {
+                return this[methodName](value);
+            }
+
+            return this.get(key) !== value;
         },
 
         /**
-         * Set cart version to cache
-         *
-         * @param {Number} version
-         */
-        setCartVersion: function (version) {
-            var obj = getData();
-            obj.cartVersion = version;
-            saveData(obj);
-        },
-
-        /**
-         * Get cart version from cache
-         *
-         * @returns {null|Number}
-         */
-        getCartVersion: function () {
-            return getData().cartVersion;
-        },
-
-        /**
-         * Set shipping method code to cache
-         *
-         * @param {String} code
-         */
-        setShippingMethodCode: function (code) {
-            var obj = getData();
-            obj.shippingMethodCode = code;
-            saveData(obj);
-        },
-
-        /**
-         * Get shipping method code from cache
-         *
-         * @returns {null|String}
-         */
-        getShippingMethodCode: function () {
-            return getData().shippingMethodCode;
-        },
-
-        /**
-         * Set shipping carrier code to cache
-         *
-         * @param {String} code
-         */
-        setShippingCarrierCode: function (code) {
-            var obj = getData();
-            obj.shippingCarrierCode = code;
-            saveData(obj);
-        },
-
-        /**
-         * Get shipping method carrier from cache
-         *
-         * @returns {null|String}
-         */
-        getShippingCarrierCode: function () {
-            return getData().shippingCarrierCode;
-        },
-
-        /**
-         * Set shipping rates to cache
-         *
-         * @param {Object} rates
-         */
-        setRatesCache: function (rates) {
-            var obj = getData();
-            obj.rates = rates;
-            saveData(obj);
-        },
-
-        /**
-         * Get shipping rates frm cache
-         *
-         * @returns {null|Object}
-         */
-        getRatesCache: function () {
-            return getData().rates;
-        },
-
-        /**
-         * Verify is address changed
+         * Compare cached address with provided.
+         * Custom method for check object equality.
          *
          * @param {Object} address
-         * @returns {boolean}
+         * @returns {Boolean}
          */
-        isAddressChanged: function (address) {
-            return JSON.stringify(this.getAddress()) != JSON.stringify(_.pick(address, this.requiredFields));
-        },
-
-        /**
-         * Verify is cart changed
-         *
-         * @returns {boolean}
-         */
-        isCartVersionChanged: function () {
-            return this.getCartVersion() != cart().data_id;
-        },
-
-        /**
-         * Verify is shipping method code changed
-         *
-         * @returns {boolean}
-         */
-        isShippingMethodCodeChanged: function () {
-            return this.getShippingMethodCode() != quote.shippingMethod()['method_code'];
-        },
-
-        /**
-         * Verify is shipping carrier code changed
-         *
-         * @returns {boolean}
-         */
-        isShippingCarrierCodeChanged: function () {
-            return this.getShippingCarrierCode() != quote.shippingMethod()['carrier_code'];
-        },
-
-        /**
-         * Save sever data to local storage
-         *
-         * @param {Object} address
-         * @param {Object} totals
-         */
-        saveCartDataToCache: function (address, totals) {
-            this.setTotalsCache(totals);
-            this.setCartVersion(cart().data_id);
-            this.setShippingMethodCode(quote.shippingMethod()['method_code']);
-            this.setShippingCarrierCode(quote.shippingMethod()['carrier_code']);
-            this.setAddress(_.pick(address, this.requiredFields));
+        _isAddressChanged: function (address) {
+            return JSON.stringify(_.pick(this.get('address'), this.requiredFields)) !==
+                JSON.stringify(_.pick(address, this.requiredFields));
         }
-    }
+    };
 });
--- app/code/Magento/Checkout/view/frontend/web/js/model/cart/estimate-service.js
+++ app/code/Magento/Checkout/view/frontend/web/js/model/cart/estimate-service.js
@@ -2,65 +2,64 @@
  * Copyright © 2013-2017 Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
-/*global define*/
-define(
-    [
-        'Magento_Checkout/js/model/quote',
-        'Magento_Checkout/js/model/shipping-rate-processor/new-address',
-        'Magento_Checkout/js/model/cart/totals-processor/default',
-        'Magento_Checkout/js/model/shipping-service',
-        'Magento_Checkout/js/model/cart/cache'
-    ],
-    function (quote, defaultProcessor, totalsDefaultProvider, shippingService, cartCache) {
-        'use strict';
 
-        var rateProcessors = [],
-            totalsProcessors = [];
+define([
+    'Magento_Checkout/js/model/quote',
+    'Magento_Checkout/js/model/shipping-rate-processor/new-address',
+    'Magento_Checkout/js/model/cart/totals-processor/default',
+    'Magento_Checkout/js/model/shipping-service',
+    'Magento_Checkout/js/model/cart/cache',
+    'Magento_Customer/js/customer-data'
+], function (quote, defaultProcessor, totalsDefaultProvider, shippingService, cartCache, customerData) {
+    'use strict';
 
-        quote.shippingAddress.subscribe(function () {
-            var type = quote.shippingAddress().getType();
-            if (quote.isVirtual()) {
-                // update totals block when estimated address was set
-                totalsProcessors['default'] = totalsDefaultProvider;
-                totalsProcessors[type]
-                    ? totalsProcessors[type].estimateTotals(quote.shippingAddress())
-                    : totalsProcessors['default'].estimateTotals(quote.shippingAddress());
-            } else {
-                // check if user data not changed -> load rates from cache
-                if (!cartCache.isAddressChanged(quote.shippingAddress())
-                    && !cartCache.isCartVersionChanged()
-                    && cartCache.getRatesCache()
-                ) {
-                    shippingService.setShippingRates(cartCache.getRatesCache());
-                    console.log('Shipping rates loaded from cache.');
-                    return;
-                }
+    var rateProcessors = [],
+        totalsProcessors = [];
 
-                // update rates list when estimated address was set
-                rateProcessors['default'] = defaultProcessor;
-                rateProcessors[type]
-                    ? rateProcessors[type].getRates(quote.shippingAddress())
-                    : rateProcessors['default'].getRates(quote.shippingAddress());
+    quote.shippingAddress.subscribe(function () {
+        var type = quote.shippingAddress().getType();
 
-                // save rates to cache after load
-                shippingService.getShippingRates().subscribe(function (rates) {
-                    cartCache.setRatesCache(rates);
-                    console.log('Shipping rates loaded from server.');
-                });
+        if (quote.isVirtual()) {
+            // update totals block when estimated address was set
+            totalsProcessors['default'] = totalsDefaultProvider;
+            totalsProcessors[type] ?
+                totalsProcessors[type].estimateTotals(quote.shippingAddress()) :
+                totalsProcessors['default'].estimateTotals(quote.shippingAddress());
+        } else {
+            // check if user data not changed -> load rates from cache
+            if (!cartCache.isChanged('address', quote.shippingAddress()) &&
+                !cartCache.isChanged('cartVersion', customerData.get('cart')()['data_id']) &&
+                cartCache.get('rates')
+            ) {
+                shippingService.setShippingRates(cartCache.get('rates'));
+
+                return;
             }
-        });
-        quote.shippingMethod.subscribe(function () {
-            totalsDefaultProvider.estimateTotals(quote.shippingAddress());
-        });
-        quote.billingAddress.subscribe(function () {
-            var type = quote.billingAddress().getType();
-            if (quote.isVirtual()) {
-                // update totals block when estimated address was set
-                totalsProcessors['default'] = totalsDefaultProvider;
-                totalsProcessors[type]
-                    ? totalsProcessors[type].estimateTotals(quote.billingAddress())
-                    : totalsProcessors['default'].estimateTotals(quote.billingAddress());
-            }
-        });
-    }
-);
+
+            // update rates list when estimated address was set
+            rateProcessors['default'] = defaultProcessor;
+            rateProcessors[type] ?
+                rateProcessors[type].getRates(quote.shippingAddress()) :
+                rateProcessors['default'].getRates(quote.shippingAddress());
+
+            // save rates to cache after load
+            shippingService.getShippingRates().subscribe(function (rates) {
+                cartCache.set('rates', rates);
+            });
+        }
+    });
+    quote.shippingMethod.subscribe(function () {
+        totalsDefaultProvider.estimateTotals(quote.shippingAddress());
+    });
+    quote.billingAddress.subscribe(function () {
+        var type = quote.billingAddress().getType();
+
+        if (quote.isVirtual()) {
+            // update totals block when estimated address was set
+            totalsProcessors['default'] = totalsDefaultProvider;
+            totalsProcessors[type] ?
+                totalsProcessors[type].estimateTotals(quote.billingAddress()) :
+                totalsProcessors['default'].estimateTotals(quote.billingAddress());
+        }
+    });
+});
