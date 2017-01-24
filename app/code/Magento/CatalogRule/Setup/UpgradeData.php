408aa0c32ecd7fd2cdfe172a705b67e076bf9ab9
Stanislav Lopukhov <slopukhov@magento.com>
2017-01-24 12:39:41 +0200

    MAGETWO-63344: Remove use of serialize in \Magento\CatalogRule\Model\Rule for catalogrule table

---
+++ app/code/Magento/CatalogRule/Setup/UpgradeData.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Copyright © 2013-2017 Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\CatalogRule\Setup;
+
+use Magento\Framework\DB\FieldDataConverterFactory;
+use Magento\Framework\DB\DataConverter\SerializedToJson;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\UpgradeDataInterface;
+
+class UpgradeData implements UpgradeDataInterface
+{
+    /**
+     * @var FieldDataConverterFactory
+     */
+    private $fieldDataConverterFactory;
+
+    /**
+     * Constructor
+     *
+     * @param FieldDataConverterFactory $fieldDataConverterFactory
+     */
+    public function __construct(
+        FieldDataConverterFactory $fieldDataConverterFactory
+    ) {
+        $this->fieldDataConverterFactory = $fieldDataConverterFactory;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+
+        if (version_compare($context->getVersion(), '2.0.3', '<')) {
+            $this->convertSerializedDataToJson($setup);
+        }
+
+        $setup->endSetup();
+    }
+
+    /**
+     * Convert metadata from serialized to JSON format:
+     *
+     * @param ModuleDataSetupInterface $setup
+     *
+     * @return void
+     */
+    public function convertSerializedDataToJson($setup)
+    {
+        $fieldDataConverter = $this->fieldDataConverterFactory->create(SerializedToJson::class);
+        $fieldDataConverter->convert(
+            $setup->getConnection(),
+            $setup->getTable('catalogrule'),
+            'rule_id',
+            'conditions_serialized'
+        );
+        $fieldDataConverter->convert(
+            $setup->getConnection(),
+            $setup->getTable('catalogrule'),
+            'rule_id',
+            'actions_serialized'
+        );
+    }
+}
