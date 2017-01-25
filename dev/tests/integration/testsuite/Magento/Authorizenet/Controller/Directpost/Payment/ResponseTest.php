142aa9ea42b2e27cb34fb92a6325a366aa4796a7
Cristian Partica <cpartica@magento.com>
2017-01-25 10:08:01 -0600

    MAGETWO-61353: Gift Card Accounts are not being generated with Auth&Capture payment action on Authorize.net DP enabled
    - adding authorize net integration test

---
+++ dev/tests/integration/testsuite/Magento/Authorizenet/Controller/Directpost/Payment/ResponseTest.php
@@ -0,0 +1,249 @@
+<?php
+/**
+ * Copyright © 2013-2017 Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Authorizenet\Controller\Directpost\Payment;
+
+/**
+ * Class ResponseTest
+ *
+ * @magentoAppArea frontend
+ * @magentoDbIsolation enabled
+ * @magentoAppIsolation enabled
+ */
+class ResponseTest extends \Magento\TestFramework\TestCase\AbstractController
+{
+    /**
+     * Tests the controller for declines
+     *
+     * @param int $invoiceNum
+     * @param string $hash
+     * @param string $errorMsg
+     * @param string[] $params
+     *
+     * @magentoDataFixture Magento/Authorizenet/_files/authorizenet_enabled_setting.php
+     * @dataProvider responseActionAuthorizeCaptureDeclineDataProvider
+     */
+    public function testResponseActionAuthorizeCaptureDecline($invoiceNum, $hash, $errorMsg, $params)
+    {
+        $controllerName = 'directpost_payment';
+        $controllerModule = 'authorizenet';
+        $controllerAction = 'response';
+        $params['x_invoice_num'] = $invoiceNum;
+        $params['x_MD5_Hash'] = $hash;
+        $this->getRequest()->setControllerName(
+            $controllerName
+        )->setControllerModule(
+            $controllerModule
+        )->setActionName(
+            $controllerAction
+        )->setRouteName(
+            $controllerModule
+        )->setRequestUri("/{$controllerModule}/{$controllerName}/{$controllerAction}")
+        ->setParams($params);
+
+        $objectManager = \Magento\TestFramework\Helper\Bootstrap::getObjectManager();
+
+        /** @var \Magento\Authorizenet\Controller\Directpost\Payment\Response */
+        $controller = $objectManager->create(\Magento\Authorizenet\Controller\Directpost\Payment\Response::class);
+
+        $response = $controller->execute();
+        $output = $response->getLayout()->getOutput();
+
+        $expectedString = "{$controllerModule}/{$controllerName}/redirect/x_invoice_num/{$params['x_invoice_num']}/"
+        . "success/0/error_msg/{$errorMsg}/controller_action_name/{$controllerName}/";
+
+        $this->assertContains('window.location', $output);
+        $this->assertContains($expectedString, $output);
+    }
+
+
+    /**
+     * Tests the controller for success
+     *
+     * @param string $hash
+     * @param string[] $params
+     *
+     * @magentoDataFixture Magento/Authorizenet/_files/authorizenet_enabled_setting.php
+     * @magentoDataFixture Magento/Authorizenet/_files/order.php
+     * @dataProvider responseActionAuthorizeCaptureSuccessDataProvider
+     */
+    public function testResponseActionAuthorizeCaptureSuccess($hash, $params)
+    {
+        $controllerName = 'directpost_payment';
+        $controllerModule = 'authorizenet';
+        $controllerAction = 'response';
+        $params['x_invoice_num'] = 100000002;
+        $params['x_MD5_Hash'] = $hash;
+        $this->getRequest()->setControllerName(
+            $controllerName
+        )->setControllerModule(
+            $controllerModule
+        )->setActionName(
+            $controllerAction
+        )->setRouteName(
+            $controllerModule
+        )->setRequestUri("/{$controllerModule}/{$controllerName}/{$controllerAction}")
+            ->setParams($params);
+
+        $objectManager = \Magento\TestFramework\Helper\Bootstrap::getObjectManager();
+
+        $directpostFactory = $this->getMockBuilder(\Magento\Authorizenet\Model\DirectpostFactory::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['create'])
+            ->getMock();
+        $directpost = $this->getMockBuilder(\Magento\Authorizenet\Model\Directpost::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $directpostFactory->expects(static::once())
+            ->method('create')
+            ->willReturn($directpost);
+        $directpost->expects(static::once())
+            ->method('process');
+
+        /** @var \Magento\Authorizenet\Controller\Directpost\Payment\Response $controller */
+        $controller = $objectManager->create(
+            \Magento\Authorizenet\Controller\Directpost\Payment\Response::class,
+            [
+                'directpostFactory' => $directpostFactory
+            ]
+        );
+
+        $response = $controller->execute();
+        $output = $response->getLayout()->getOutput();
+
+        $expectedString = "{$controllerModule}/{$controllerName}/redirect/x_invoice_num/{$params['x_invoice_num']}/"
+            . "success/1/controller_action_name/{$controllerName}/";
+
+        $this->assertContains('window.location', $output);
+        $this->assertContains($expectedString, $output);
+    }
+
+    /**
+     * @return array
+     */
+    public function responseActionAuthorizeCaptureDeclineDataProvider()
+    {
+        $postArray = [
+            'x_response_code' => 1,
+            'x_response_reason_code' => 1,
+            'x_response_reason_text' => 'This transaction has been approved.',
+            'x_avs_code' => 'Y',
+            'x_auth_code' => 'G0L0XR',
+            'x_trans_id' => '60016479791',
+            'x_method' => 'CC',
+            'x_card_type' => 'American Express',
+            'x_account_number' => 'XXXX0002',
+            'x_first_name' => 'Name',
+            'x_last_name' => 'Surname',
+            'x_company' => null,
+            'x_address' => 'Address',
+            'x_city' => 'Austin',
+            'x_state' => 'Texas',
+            'x_zip' => '78753',
+            'x_country' => 'US',
+            'x_phone' => '5127242323',
+            'x_fax' => null,
+            'x_email' => 'customer@example.com',
+            'x_description' => null,
+            'x_type' => 'auth_capture',
+            'x_cust_id' => null,
+            'x_ship_to_first_name' => null,
+            'x_ship_to_last_name' => null,
+            'x_ship_to_company' => null,
+            'x_ship_to_address' => null,
+            'x_ship_to_city' => null,
+            'x_ship_to_state' => null,
+            'x_ship_to_zip' => null,
+            'x_ship_to_country' => null,
+            'x_amount' => 100.00,
+            'x_tax' => 0.00,
+            'x_duty' => 0.00,
+            'x_freight' => 0.00,
+            'x_tax_exempt' => false,
+            'x_po_num' => null,
+            'x_SHA2_Hash' => null,
+            'x_cvv2_resp_code' => 'P',
+            'x_cavv_response' => 2,
+            'x_test_request' => false,
+            'controller_action_name' => 'directpost_payment',
+            'is_secure' => null
+        ];
+        return [
+            'error_hash' => [
+                'invoice_num' => '1231231',
+                'x_MD5_Hash' => 'F9AE81A5DA36057D1312D71C904FCCF2',
+                'error_msg' => 'The%20transaction%20was%20declined%20because%20the%20'
+                . 'response%20hash%20validation%20failed.',
+                'post' => $postArray
+            ],
+
+            'error_transaction_not found' => [
+                'invoice_num' => 87,
+                'x_MD5_Hash' => 'BBC3D75DA467F7DF6A1E174CD491CF81',
+                'error_msg' => 'This%20payment%20didn%27t%20work%20out%20because%20'
+                    . 'we%20can%27t%20find%20this%20order.',
+                'post' => $postArray
+            ],
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function responseActionAuthorizeCaptureSuccessDataProvider()
+    {
+        $postArray = [
+            'x_response_code' => 1,
+            'x_response_reason_code' => 1,
+            'x_response_reason_text' => 'This transaction has been approved.',
+            'x_avs_code' => 'Y',
+            'x_auth_code' => 'G0L0XR',
+            'x_trans_id' => '60016479791',
+            'x_method' => 'CC',
+            'x_card_type' => 'American Express',
+            'x_account_number' => 'XXXX0002',
+            'x_first_name' => 'Name',
+            'x_last_name' => 'Surname',
+            'x_company' => null,
+            'x_address' => 'Address',
+            'x_city' => 'Austin',
+            'x_state' => 'Texas',
+            'x_zip' => '78753',
+            'x_country' => 'US',
+            'x_phone' => '5127242323',
+            'x_fax' => null,
+            'x_email' => 'integrationtest@magento.com',
+            'x_description' => null,
+            'x_type' => 'auth_capture',
+            'x_cust_id' => null,
+            'x_ship_to_first_name' => null,
+            'x_ship_to_last_name' => null,
+            'x_ship_to_company' => null,
+            'x_ship_to_address' => null,
+            'x_ship_to_city' => null,
+            'x_ship_to_state' => null,
+            'x_ship_to_zip' => null,
+            'x_ship_to_country' => null,
+            'x_amount' => 120.15,
+            'x_tax' => 0.00,
+            'x_duty' => 0.00,
+            'x_freight' => 0.00,
+            'x_tax_exempt' => false,
+            'x_po_num' => null,
+            'x_SHA2_Hash' => null,
+            'x_cvv2_resp_code' => 'P',
+            'x_cavv_response' => 2,
+            'x_test_request' => false,
+            'controller_action_name' => 'directpost_payment',
+            'is_secure' => null
+        ];
+        return [
+            'error_hash' => [
+                'x_MD5_Hash' => '35DCF749F7760193FB8254886E1D1522',
+                'post' => $postArray
+            ],
+        ];
+    }
+}
