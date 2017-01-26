050e9980a6538d8e27c75c6e41a526e3cb43946d
Yuri Kovsher <ikovsher@magento.com>
2017-01-26 18:17:50 +0200

    MAGETWO-62783: Wrong address template used for order e-mails

--- dev/tests/integration/testsuite/Magento/Sales/_files/order_fixture_store_rollback.php
+++ dev/tests/integration/testsuite/Magento/Sales/_files/order_fixture_store_rollback.php
@@ -6,11 +6,12 @@
 
 use Magento\TestFramework\Helper\Bootstrap;
 use Magento\Framework\Indexer\IndexerRegistry;
+use Magento\CatalogSearch\Model\Indexer\Fulltext as FulltextIndexer;
 
 require __DIR__ . '/default_rollback.php';
 require __DIR__ . '/../../Catalog/_files/product_simple_duplicated_rollback.php';
 require __DIR__ . '/../../Store/_files/core_fixturestore_rollback.php';
 
 Bootstrap::getObjectManager()->get(IndexerRegistry::class)
-    ->get('catalogsearch_fulltext')
+    ->get(FulltextIndexer::INDEXER_ID)
     ->reindexAll();
