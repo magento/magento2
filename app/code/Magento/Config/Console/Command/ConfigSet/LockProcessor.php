1091b3ec8edb518bb9b5bb77843d1417d92bdc09
Oleh Posyniak <oposyniak@magento.com>
2017-02-01 14:51:17 +0200

    MAGETWO-61778: Implement config:set command

--- app/code/Magento/Config/Console/Command/ConfigSet/LockProcessor.php
+++ app/code/Magento/Config/Console/Command/ConfigSet/LockProcessor.php
@@ -12,7 +12,7 @@ use Magento\Config\Model\Config\Structure\Element\Field;
 use Magento\Framework\App\Config\ConfigPathResolver;
 use Magento\Framework\App\Config\Value;
 use Magento\Framework\App\Config\ValueFactory;
-use Magento\Framework\App\DeploymentConfig\Writer;
+use Magento\Framework\App\DeploymentConfig;
 use Magento\Framework\Config\File\ConfigFilePool;
 use Magento\Framework\Exception\CouldNotSaveException;
 use Magento\Framework\Exception\FileSystemException;
@@ -28,7 +28,12 @@ use Symfony\Component\Console\Input\InputInterface;
 class LockProcessor implements ConfigSetProcessorInterface
 {
     /**
-     * @var Writer
+     * @var DeploymentConfig
+     */
+    private $deploymentConfig;
+
+    /**
+     * @var DeploymentConfig\Writer
      */
     private $deploymentConfigWriter;
 
@@ -53,19 +58,22 @@ class LockProcessor implements ConfigSetProcessorInterface
     private $configValueFactory;
 
     /**
-     * @param Writer $writer
+     * @param DeploymentConfig $deploymentConfig
+     * @param DeploymentConfig\Writer $writer
      * @param ArrayManager $arrayManager
      * @param ConfigPathResolver $configPathResolver
      * @param Structure $configStructure
      * @param ValueFactory $configValueFactory
      */
     public function __construct(
-        Writer $writer,
+        DeploymentConfig $deploymentConfig,
+        DeploymentConfig\Writer $writer,
         ArrayManager $arrayManager,
         ConfigPathResolver $configPathResolver,
         Structure $configStructure,
         ValueFactory $configValueFactory
     ) {
+        $this->deploymentConfig = $deploymentConfig;
         $this->deploymentConfigWriter = $writer;
         $this->arrayManager = $arrayManager;
         $this->configPathResolver = $configPathResolver;
@@ -88,9 +96,11 @@ class LockProcessor implements ConfigSetProcessorInterface
         $configPath = $this->configPathResolver->resolve($path, $scope, $scopeCode, System::CONFIG_TYPE);
 
         /** @var Field $field */
-        $field = $this->configStructure->getElement($path);
+        $field = $this->deploymentConfig->isAvailable()
+            ? $this->configStructure->getElement($path)
+            : null;
         /** @var Value $backendModel */
-        $backendModel = $field->hasBackendModel()
+        $backendModel = $field && $field->hasBackendModel()
             ? $field->getBackendModel()
             : $this->configValueFactory->create();
 
