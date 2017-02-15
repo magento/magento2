7f277da56e908f398d35866e2623a7873003efde
Viktor Tymchynskyi <vtymchynskyi@magento.com>
2017-02-15 10:09:41 -0600

    MAGETWO-63640: View Signifyd Guarantee Status in Order Grid
    - Order grid sync after case creation

--- app/code/Magento/Signifyd/Model/CaseServices/CreationService.php
+++ app/code/Magento/Signifyd/Model/CaseServices/CreationService.php
@@ -8,6 +8,7 @@ namespace Magento\Signifyd\Model\CaseServices;
 use Magento\Signifyd\Api\CaseCreationServiceInterface;
 use Magento\Signifyd\Api\CaseManagementInterface;
 use Magento\Signifyd\Api\CaseRepositoryInterface;
+use Magento\Signifyd\Model\SalesOrderGrid\OrderGridUpdater;
 use Magento\Signifyd\Model\SignifydGateway\Gateway;
 use Magento\Signifyd\Model\SignifydGateway\GatewayException;
 use Psr\Log\LoggerInterface;
@@ -40,23 +41,31 @@ class CreationService implements CaseCreationServiceInterface
     private $caseRepository;
 
     /**
+     * @var OrderGridUpdater
+     */
+    private $orderGridUpdater;
+
+    /**
      * CreationService constructor.
      *
      * @param CaseManagementInterface $caseManagement
      * @param Gateway $signifydGateway
      * @param LoggerInterface $logger
      * @param CaseRepositoryInterface $caseRepository
+     * @param OrderGridUpdater $orderGridUpdater
      */
     public function __construct(
         CaseManagementInterface $caseManagement,
         Gateway $signifydGateway,
         LoggerInterface $logger,
-        CaseRepositoryInterface $caseRepository
+        CaseRepositoryInterface $caseRepository,
+        OrderGridUpdater $orderGridUpdater
     ) {
         $this->caseManagement = $caseManagement;
         $this->signifydGateway = $signifydGateway;
         $this->logger = $logger;
         $this->caseRepository = $caseRepository;
+        $this->orderGridUpdater = $orderGridUpdater;
     }
 
     /**
@@ -65,6 +74,7 @@ class CreationService implements CaseCreationServiceInterface
     public function createForOrder($orderId)
     {
         $case = $this->caseManagement->create($orderId);
+        $this->orderGridUpdater->update($orderId);
 
         try {
             $caseId = $this->signifydGateway->createCase($orderId);
