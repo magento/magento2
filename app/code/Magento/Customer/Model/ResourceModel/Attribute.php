184e57386bb2400aec55798f1fab24cee382fc14
Igor Melnikov <imelnikov@magento.com>
2017-02-16 13:30:14 -0600

    MAGETWO-64224: Remove usages of AttributeCache from customer module
    - moving logic to clear attribute metadata cache to model

--- app/code/Magento/Customer/Model/ResourceModel/Attribute.php
+++ app/code/Magento/Customer/Model/ResourceModel/Attribute.php
@@ -3,46 +3,16 @@
  * Copyright © 2013-2017 Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\Customer\Model\ResourceModel;
-
-use Magento\Store\Model\StoreManagerInterface;
-use Magento\Eav\Model\ResourceModel\Entity\Type;
-use Magento\Framework\Model\ResourceModel\Db\Context;
-use Magento\Customer\Model\Metadata\AttributeMetadataCache;
-use Magento\Framework\DataObject;
-use Magento\Framework\App\ObjectManager;
 
 /**
  * Customer attribute resource model
+ *
+ * @author      Magento Core Team <core@magentocommerce.com>
  */
+namespace Magento\Customer\Model\ResourceModel;
+
 class Attribute extends \Magento\Eav\Model\ResourceModel\Attribute
 {
-    /**
-     * @var AttributeMetadataCache
-     */
-    private $attributeMetadataCache;
-
-    /**
-     * Constructor
-     *
-     * @param \Magento\Framework\Model\ResourceModel\Db\Context $context
-     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
-     * @param Type $eavEntityType
-     * @param string $connectionName
-     * @param \Magento\Customer\Model\Metadata\AttributeMetadataCache $attributeMetadataCache
-     */
-    public function __construct(
-        Context $context,
-        StoreManagerInterface $storeManager,
-        Type $eavEntityType,
-        $connectionName = null,
-        AttributeMetadataCache $attributeMetadataCache = null
-    ) {
-        $this->attributeMetadataCache = $attributeMetadataCache ?: ObjectManager::getInstance()
-            ->get(AttributeMetadataCache::class);
-        parent::__construct($context, $storeManager, $eavEntityType, $connectionName);
-    }
-
     /**
      * Get EAV website table
      *
@@ -66,23 +36,5 @@ class Attribute extends \Magento\Eav\Model\ResourceModel\Attribute
     protected function _getFormAttributeTable()
     {
         return $this->getTable('customer_form_attribute');
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function afterSave(DataObject $object)
-    {
-        $this->attributeMetadataCache->clean();
-        parent::afterSave($object);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function afterDelete(DataObject $object)
-    {
-        $this->attributeMetadataCache->clean();
-        parent::afterDelete($object);
     }
 }
