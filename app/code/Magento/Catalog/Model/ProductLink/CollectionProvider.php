308b70baf1144f16ea062c74e96aab832a2a06eb
Oleksii Korshenko <okorshenko@magento.com>
2017-02-20 12:19:48 -0600

    MAGETWO-64403: [GitHub] [PR] Sorting the returning array by the position of the linked products. Fix for magento2/issues/8392 magento/magento2#8467
     - covered changes with unit tests

--- app/code/Magento/Catalog/Model/ProductLink/CollectionProvider.php
+++ app/code/Magento/Catalog/Model/ProductLink/CollectionProvider.php
@@ -48,21 +48,21 @@ class CollectionProvider
         $products = $this->providers[$type]->getLinkedProducts($product);
         $converter = $this->converterPool->getConverter($type);
         $output = [];
-        $realoutput = [];
+        $sorterItems = [];
         foreach ($products as $item) {
             $output[$item->getId()] = $converter->convert($item);
         }
+
         foreach ($output as $item) {
-            $itemPosition = $item["position"];
-            if (!isset($realoutput[$itemPosition])) {
-                $realoutput[$itemPosition] = $item;
-            } 
-            else {
-                $newPosition = $itemPosition+1;
-                $realoutput[$newPosition] = $item;
+            $itemPosition = $item['position'];
+            if (!isset($sorterItems[$itemPosition])) {
+                $sorterItems[$itemPosition] = $item;
+            } else {
+                $newPosition = $itemPosition + 1;
+                $sorterItems[$newPosition] = $item;
             }
         }
-        ksort($realoutput);
-        return $realoutput;
+        ksort($sorterItems);
+        return $sorterItems;
     }
 }
