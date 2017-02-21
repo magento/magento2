9c1f2a0cd06acaa11a4c29853bb2c249b5a145df
Andrei_Ziblitski <Andrei_Ziblitski@epam.com>
2017-02-21 18:33:21 +0300

    MTO-142: [Test] Captcha on front login
    - Defects fixed.

--- dev/tests/functional/tests/app/Magento/Captcha/Test/TestCase/CaptchaOnStoreFrontLoginTest.php
+++ dev/tests/functional/tests/app/Magento/Captcha/Test/TestCase/CaptchaOnStoreFrontLoginTest.php
@@ -11,7 +11,7 @@ use Magento\Mtf\TestStep\TestStepFactory;
 use Magento\Captcha\Test\Constraint\AssertCaptchaFieldOnStorefront;
 use Magento\Customer\Test\Fixture\Customer;
 use Magento\Cms\Test\Page\CmsIndex;
-use Magento\Captcha\Test\Page\CustomerAccountLogin;
+use Magento\Customer\Test\Page\CustomerAccountLogin;
 use Magento\Mtf\Fixture\FixtureFactory;
 
 /**
