a31f4a35c018ad09654e5bd5871086b73fbd3d2d
James Halsall <james.halsall@inviqa.com>
2017-02-22 09:09:36 +0000

    Remove the copyright year from file headers

--- lib/internal/Magento/Framework/View/Test/Unit/Template/Html/MinifierTest.php
+++ lib/internal/Magento/Framework/View/Test/Unit/Template/Html/MinifierTest.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright © 2013-2017 Magento, Inc. All rights reserved.
+ * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
 namespace Magento\Framework\View\Test\Unit\Template\Html;
@@ -115,7 +115,7 @@ class MinifierTest extends \PHPUnit_Framework_TestCase
         $baseContent = <<<TEXT
 <?php
 /**
- * Copyright © 2013-2017 Magento, Inc. All rights reserved.
+ * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
 ?>
