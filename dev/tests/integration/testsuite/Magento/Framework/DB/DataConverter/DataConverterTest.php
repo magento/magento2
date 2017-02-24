83b8759676d2692c338bfa59182334acc4ba999f
Roman Ganin <rganin@magento.com>
2017-02-24 10:44:39 +0200

    MAGETWO-64802: Implement corrupted data reporting for fieldDataConverter
    - covered skipping already converted data with integration test

--- dev/tests/integration/testsuite/Magento/Framework/DB/DataConverter/DataConverterTest.php
+++ dev/tests/integration/testsuite/Magento/Framework/DB/DataConverter/DataConverterTest.php
@@ -72,4 +72,60 @@ class DataConverterTest extends \PHPUnit_Framework_TestCase
             $queryModifier
         );
     }
+
+    /**
+     * @magentoAppArea adminhtml
+     * @magentoDbIsolation enabled
+     * @magentoDataFixture Magento/Framework/DB/DataConverter/_files/broken_admin_user.php
+     */
+    public function testAlreadyConvertedDataSkipped()
+    {
+        /** @var \Magento\User\Model\User $user */
+        $user = $this->objectManager->create(\Magento\User\Model\User::class);
+        $user->loadByUsername('broken_admin');
+        $userId = $user->getId();
+
+        /** @var Serialize $serializer */
+        $serializer = $this->objectManager->create(Serialize::class);
+        $serializedData = $serializer->serialize(['some' => 'data', 'other' => 'other data']);
+        $serializedDataLength = strlen($serializedData);
+        $brokenSerializedData = substr($serializedData, 0, $serializedDataLength - 6);
+        /** @var AdapterInterface $adapter */
+        $adapter = $user->getResource()->getConnection();
+        $adapter->update(
+            $user->getResource()->getTable('admin_user'),
+            ['extra' => $brokenSerializedData],
+            "user_id={$userId}"
+        );
+
+        $adapter->update(
+            $user->getResource()->getTable('admin_user'),
+            ['extra' => '[]'],
+            "user_id={$userId}"
+        );
+
+        /** @var InQueryModifier $queryModifier */
+        $queryModifier = $this->objectManager->create(InQueryModifier::class, ['user_id' => $userId]);
+
+        /** @var SerializedToJson $dataConverter */
+        $dataConverter = $this->objectManager->get(SerializedToJson::class);
+
+        /** @var FieldDataConverter $fieldDataConverter */
+        $fieldDataConverter = $this->objectManager->create(
+            FieldDataConverter::class,
+            [
+                'dataConverter' => $dataConverter
+            ]
+        );
+        $fieldDataConverter->convert(
+            $adapter,
+            'admin_user',
+            'user_id',
+            'extra',
+            $queryModifier
+        );
+
+        $user->load($userId);
+        $this->assertEquals([], $user->getExtra());
+    }
 }
