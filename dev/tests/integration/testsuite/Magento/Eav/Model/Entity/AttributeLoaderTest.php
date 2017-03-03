70c8dfbec85b2028f3991c2ec4c38d674fcecfb7
Joan He <johe@magento.com>
2017-03-03 11:42:47 -0600

    MAGETWO-64216: Add integration test of \Magento\Eav\Model\Entity\AttributeLoader

--- dev/tests/integration/testsuite/Magento/Eav/Model/Entity/AttributeLoaderTest.php
+++ dev/tests/integration/testsuite/Magento/Eav/Model/Entity/AttributeLoaderTest.php
@@ -11,7 +11,7 @@ use Magento\TestFramework\Helper\CacheCleaner;
 
 /**
  * @magentoAppIsolation enabled
- * @magentoDataFixture Magento/Catalog/_files/category_attribute.php
+ * @magentoDataFixture Magento/Eav/_files/attribute_for_search.php
  */
 class AttributeLoaderTest extends \PHPUnit_Framework_TestCase
 {
@@ -24,12 +24,28 @@ class AttributeLoaderTest extends \PHPUnit_Framework_TestCase
      * @var \Magento\Framework\ObjectManagerInterface
      */
     private $objectManager;
-    
+
+    /**
+     * @var \Magento\Eav\Model\Entity\AbstractEntity
+     */
+    private $resource;
+
     protected function setUp()
     {
         CacheCleaner::cleanAll();
         $this->objectManager = Bootstrap::getObjectManager();
         $this->attributeLoader = $this->objectManager->get(AttributeLoader::class);
+        $entityType = $this->objectManager->create(\Magento\Eav\Model\Entity\Type::class)
+            ->loadByCode('test');
+        $context = $this->objectManager->get(\Magento\Eav\Model\Entity\Context::class);
+        $this->resource = $this->getMockBuilder(\Magento\Eav\Model\Entity\AbstractEntity::class)
+            ->setConstructorArgs([$context])
+            ->setMethods(['getEntityType', 'getLinkField'])
+            ->getMock();
+        $this->resource->method('getEntityType')
+            ->willReturn($entityType);
+        $this->resource->method('getLinkField')
+            ->willReturn('link_field');
     }
 
     /**
@@ -43,50 +59,51 @@ class AttributeLoaderTest extends \PHPUnit_Framework_TestCase
         $expectedNumOfAttributesByTable,
         $object
     ) {
-        /** @var \Magento\Catalog\Model\ResourceModel\Category $categoryResourceModel */
-        $categoryResourceModel = $this->objectManager->get(\Magento\Catalog\Model\ResourceModel\Category::class);
-
         // Before load all attributes
-        $attributesByCode = $categoryResourceModel->getAttributesByCode();
-        $attributesByTable = $categoryResourceModel->getAttributesByTable();
+        $attributesByCode = $this->resource->getAttributesByCode();
+        $attributesByTable = $this->resource->getAttributesByTable();
         $this->assertEquals(0, count($attributesByCode));
         $this->assertEquals(0, count($attributesByTable));
 
         // Load all attributes
-        $categoryResourceModel2 = $this->attributeLoader->loadAllAttributes(
-            $categoryResourceModel,
+        $resource2 = $this->attributeLoader->loadAllAttributes(
+            $this->resource,
             $object
         );
-        $attributesByCode2 = $categoryResourceModel2->getAttributesByCode();
-        $attributesByTable2 = $categoryResourceModel2->getAttributesByTable();
+        $attributesByCode2 = $resource2->getAttributesByCode();
+        $attributesByTable2 = $resource2->getAttributesByTable();
         $this->assertEquals($expectedNumOfAttributesByCode, count($attributesByCode2));
         $this->assertEquals($expectedNumOfAttributesByTable, count($attributesByTable2));
     }
 
     public function loadAllAttributesDataProvider()
     {
+        /** @var \Magento\Eav\Model\Entity\Type $entityType */
+        $entityType = Bootstrap::getObjectManager()->create(\Magento\Eav\Model\Entity\Type::class)
+            ->loadByCode('order');
+        $attributeSetId = $entityType->getDefaultAttributeSetId();
         return [
             [
-                40,
-                5,
+                13,
+                2,
                 null
             ],
             [
-                39,
-                5,
+                10,
+                1,
                 new DataObject(
                     [
-                        'attribute_set_id' => 3,
+                        'attribute_set_id' => $attributeSetId,
                         'store_id' => 0
                     ]
                 ),
             ],
             [
-                39,
-                5,
+                10,
+                1,
                 new DataObject(
                     [
-                        'attribute_set_id' => 3,
+                        'attribute_set_id' => $attributeSetId,
                         'store_id' => 10
                     ]
                 ),
