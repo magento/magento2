aad5ba98e0c52a1b7d75f609f7434aae4aa16e7b
Nino Aratari <nino.aratari@soteha.it>
2017-03-03 11:26:59 +0100

    magento/magento2 #8765:
    Magento\Catalog\Model\ResourceModel\AbstractResource::getAttributeRawValue() returns false negative
    - fixed adding a strong check on return value

--- app/code/Magento/Catalog/Model/ResourceModel/AbstractResource.php
+++ app/code/Magento/Catalog/Model/ResourceModel/AbstractResource.php
@@ -568,6 +568,6 @@ abstract class AbstractResource extends \Magento\Eav\Model\Entity\AbstractEntity
             $attributesData = $_data[1];
         }
 
-        return $attributesData ? $attributesData : false;
+        return $attributesData!==false ? $attributesData : false;
     }
 }
