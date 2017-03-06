649aa0c7e1166c590b574e86a2ff6f5dc1a84fd9
lestare <lestare@mail.ru>
2017-03-06 12:12:08 +0200

    MAGETWO-65403: [Backport] - [Performance] Add an opportunity to turn off swatches for category page - for 2.1.6

--- dev/tests/functional/utils/command.php
+++ dev/tests/functional/utils/command.php
@@ -1,12 +1,18 @@
 <?php
-/**
- * Copyright © 2013-2017 Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
- */
+require_once dirname(__FILE__) . '/' . 'bootstrap.php';
+
+$magentoObjectManagerFactory = \Magento\Framework\App\Bootstrap::createObjectManagerFactory(BP, $_SERVER);
+$magentoObjectManager = $magentoObjectManagerFactory->create($_SERVER);
+
+$logger = $objectManager->create(\Magento\Mtf\System\Logger::class);
+$logger->setLogDirectoryPath(__DIR__ . '/../../../../var/log');
+$logger->log('I am in command.php before if. ', 'debug.log');
 
 if (isset($_GET['command'])) {
+    $logger->log('I am in command.php in if. ', 'debug.log');
     $command = urldecode($_GET['command']);
-    exec('../../../../bin/magento ' . $command);
+    exec('php -f ../../../../bin/magento ' . $command);
+    $logger->log('I am in command.php after cache flush. ', 'debug.log');
 } else {
-    throw new \Exception("Command GET parameter is not set.");
+    throw new \InvalidArgumentException("Command GET parameter is not set.");
 }
