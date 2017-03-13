91060dc5d876167f68228d649d1f476ffd458e60
Vladyslav Shcherbyna <vshcherbyna@magento.com>
2017-03-13 19:03:58 +0200

    MAGETWO-65850: Introduce fail admin notification message
    Add notification message.

---
+++ app/code/Magento/Analytics/Model/System/Message/Notification.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Copyright © 2013-2017 Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Analytics\Model\System\Message;
+
+use Magento\Analytics\Model\SubscriptionStatusProvider;
+use Magento\Framework\Notification\MessageInterface;
+use Magento\Framework\UrlInterface;
+
+/**
+ * Represents an analytics notification message.
+ */
+class Notification implements MessageInterface
+{
+    /**
+     * @var SubscriptionStatusProvider
+     */
+    private $subscriptionStatusProvider;
+
+    /**
+     * @var UrlInterface
+     */
+    private $urlBuilder;
+
+    /**
+     * @param SubscriptionStatusProvider $subscriptionStatusProvider
+     * @param UrlInterface $urlBuilder
+     */
+    public function __construct(SubscriptionStatusProvider $subscriptionStatusProvider, UrlInterface $urlBuilder)
+    {
+        $this->subscriptionStatusProvider = $subscriptionStatusProvider;
+        $this->urlBuilder = $urlBuilder;
+    }
+
+    /**
+     * Retrieve unique message identity
+     *
+     * @return string
+     * @codeCoverageIgnore
+     */
+    public function getIdentity()
+    {
+        return md5('ANALYTICS_NOTIFICATION');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isDisplayed()
+    {
+        return $this->subscriptionStatusProvider->getStatus() === SubscriptionStatusProvider::FAILED;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getText()
+    {
+        $messageDetails = '';
+
+        $messageDetails .= __('Failed to synchronize data to the Magento Business Intelligence service. ');
+        $messageDetails .= __(
+            '<a href="%1">Retry Synchronization</a>',
+            $this->urlBuilder->getUrl('analytics/retry')
+        );
+
+        return $messageDetails;
+    }
+
+    /**
+     * Retrieve message severity
+     *
+     * @return int
+     * @codeCoverageIgnore
+     */
+    public function getSeverity()
+    {
+        return self::SEVERITY_MAJOR;
+    }
+}
