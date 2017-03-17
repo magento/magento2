7da183103d4313a0f32f38d77e351f3aade0780e
Maksym Aposov <maposov@magento.com>
2017-03-17 10:48:29 +0200

    MAGETWO-64889: [Performance] Optimize "Is Salable" check on Category page for Configurable Product
    - Fix installation functional tests

--- dev/tests/functional/utils/command.php
+++ dev/tests/functional/utils/command.php
@@ -4,17 +4,9 @@
  * See COPYING.txt for license details.
  */
 
-$commandList = [
-    'cache:flush',
-    'cache:disable',
-    'cache:enable',
-];
-
 if (isset($_GET['command'])) {
     $command = urldecode($_GET['command']);
-    if (in_array($command, $commandList)) {
-        exec('php -f ../../../../bin/magento ' . $command);
-    }
+    exec('php -f ../../../../bin/magento ' . $command);
 } else {
     throw new \InvalidArgumentException("Command GET parameter is not set.");
 }
