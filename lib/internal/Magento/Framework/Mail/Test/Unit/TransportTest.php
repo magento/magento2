e02e28d10385bb6a34b74295430fa48a2f63fe84
Fabian Schmengler <fs@integer-net.de>
2017-03-19 00:45:44 +0000

    Remove redundant unit test
    The transport now can take any implementation of the message interface as argument

--- lib/internal/Magento/Framework/Mail/Test/Unit/TransportTest.php
+++ lib/internal/Magento/Framework/Mail/Test/Unit/TransportTest.php
@@ -5,21 +5,8 @@
  */
 namespace Magento\Framework\Mail\Test\Unit;
 
-use Zend\Mail\Headers;
-
 class TransportTest extends \PHPUnit_Framework_TestCase
 {
-    /**
-     * @expectedException \InvalidArgumentException
-     * @expectedExceptionMessage The message should be an instance of \Zend\Mail\Message
-     */
-    public function testTransportWithIncorrectMessageObject()
-    {
-        new \Magento\Framework\Mail\Transport(
-            $this->getMock(\Magento\Framework\Mail\MessageInterface::class)
-        );
-    }
-
     /**
      * @covers \Magento\Framework\Mail\Transport::sendMessage
      * @expectedException \Magento\Framework\Exception\MailException
