0202a1ae38457779c7050a4ecaad76c438ee1056
Bartek Igielski <igloczek@gmail.com>
2017-03-20 09:07:26 +0100

    NVM setup fix

--- dev/travis/before_install.sh
+++ dev/travis/before_install.sh
@@ -21,6 +21,9 @@ phpenv rehash;
 test -n "$GITHUB_TOKEN" && composer config github-oauth.github.com "$GITHUB_TOKEN" || true
 
 # Setup Node Version Manager
-if [$TEST_SUITE == "js"]; then
+if [ $TEST_SUITE == "js" ]; then
     curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+    command -v nvm
 fi
