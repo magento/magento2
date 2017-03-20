cceba9543dda14bd2ab2d637153d66c7c2338e36
Dmytro Yushkin <dyushkin@magento.com>
2017-03-20 08:35:41 -0500

    MAGETWO-65119: Create/update automated functional tests
    - Guarantee cancel flow using plugins now
    - Webhook wait functions deleted
    
    (cherry picked from commit 8a8759d)

---
+++ app/code/Magento/Signifyd/Plugin/PaymentPlugin.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Copyright © 2013-2017 Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Signifyd\Plugin;
+
+use Magento\Framework\Registry;
+use Magento\Payment\Model\MethodInterface;
+use Magento\Signifyd\Api\GuaranteeCancelingServiceInterface;
+
+/**
+ * Plugin complements MethodInterface::denyPayment method logic with canceling Signifyd guarantee.
+ *
+ * @see MethodInterface::denyPayment
+ */
+class PaymentPlugin
+{
+    /**
+     * @var GuaranteeCancelingServiceInterface
+     */
+    private $guaranteeCancelingService;
+
+    /**
+     * @var Registry
+     */
+    private $registry;
+
+    /**
+     * @param GuaranteeCancelingServiceInterface $guaranteeCancelingService
+     * @param Registry $registry
+     */
+    public function __construct(
+        GuaranteeCancelingServiceInterface $guaranteeCancelingService,
+        Registry $registry
+    ) {
+        $this->guaranteeCancelingService = $guaranteeCancelingService;
+        $this->registry = $registry;
+    }
+
+    /**
+     * Performs Signifyd guarantee cancel operation after payment denying.
+     *
+     * @param MethodInterface $subject
+     * @param MethodInterface|bool $result
+     * @return MethodInterface|bool
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function afterDenyPayment(MethodInterface $subject, $result)
+    {
+        /** @var \Magento\Sales\Api\Data\OrderInterface $order */
+        $order = $this->registry->registry('current_order');
+
+        if ($this->isPaymentDenied($order->getPayment(), $result)) {
+            $this->guaranteeCancelingService->cancelForOrder(
+                $order->getEntityId()
+            );
+        }
+
+        return $result;
+    }
+
+    /**
+     * Checks if deny payment operation was successful.
+     *
+     * Result not false check for payment methods using AbstractMethod.
+     * Transaction is closed check for payment methods using Gateway.
+     *
+     * @param \Magento\Sales\Api\Data\OrderPaymentInterface $payment
+     * @param MethodInterface $result
+     * @return bool
+     */
+    private function isPaymentDenied($payment, $result)
+    {
+        return $result !== false || $payment->getIsTransactionClosed();
+    }
+}
