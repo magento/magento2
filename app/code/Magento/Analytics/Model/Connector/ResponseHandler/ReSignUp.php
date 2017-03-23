9c305911727ddd99db83636acf78c6da946b504a
Vladyslav Shcherbyna <vshcherbyna@magento.com>
2017-03-23 15:23:56 +0200

    MAGETWO-66528: Add response handler
    Add re-signup call.

---
+++ app/code/Magento/Analytics/Model/Connector/ResponseHandler/ReSignUp.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Copyright © 2013-2017 Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Analytics\Model\Connector\ResponseHandler;
+
+use Magento\Analytics\Model\AnalyticsToken;
+use Magento\Analytics\Model\Connector\Http\ResponseHandlerInterface;
+use Magento\Analytics\Model\Subscription;
+
+/**
+ * Class ReSignUp
+ */
+class ReSignUp implements ResponseHandlerInterface
+{
+    /**
+     * @var AnalyticsToken
+     */
+    private $analyticsToken;
+    /**
+     * @var Subscription
+     */
+    private $subscription;
+
+    /**
+     * @param AnalyticsToken $analyticsToken
+     * @param Subscription $subscription
+     */
+    public function __construct(AnalyticsToken $analyticsToken, Subscription $subscription)
+    {
+        $this->analyticsToken = $analyticsToken;
+        $this->subscription = $subscription;
+    }
+
+    /**
+     * @param array $responseBody
+     *
+     * @return bool|string
+     */
+    public function handleResponse(array $responseBody)
+    {
+        $this->analyticsToken->storeToken(null);
+        $this->subscription->retry();
+        return false;
+    }
+}
