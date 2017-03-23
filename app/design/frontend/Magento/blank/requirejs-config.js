dce607f74de7c0c1b42d8bd3b783693c7c75977f
Mike O'Loughlin <justmadmike@gmail.com>
2017-03-23 00:48:51 +0000

    Move blank theme dependencies out of Magento_Theme requirejs-config

---
+++ app/design/frontend/Magento/blank/requirejs-config.js
@@ -0,0 +1,11 @@
+/**
+ * Copyright © 2013-2017 Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+var config = {
+    deps: [
+        "js/responsive",
+        "js/theme"
+    ]
+};
