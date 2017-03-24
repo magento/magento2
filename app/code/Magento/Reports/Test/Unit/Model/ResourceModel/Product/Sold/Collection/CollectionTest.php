a2c2e2c8cad8dcf9c2deb269f8d1e092b8fc9212
Anna Voelkl <anna@voelkl.at>
2017-03-24 18:31:07 +0100

    MAGETWO-65730: [GitHub][PR] Add missing Copyright magento/magento2#8812

--- app/code/Magento/Reports/Test/Unit/Model/ResourceModel/Product/Sold/Collection/CollectionTest.php
+++ app/code/Magento/Reports/Test/Unit/Model/ResourceModel/Product/Sold/Collection/CollectionTest.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * Copyright © 2013-2017 Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
 
 namespace Magento\Reports\Test\Unit\Model\ResourceModel\Product\Sold\Collection;
 
