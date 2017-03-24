e5432959c4354f5b01ebc25381bbae119f116662
Dinh Khoa <mrkhoa99@gmail.com>
2017-03-24 16:51:35 +0700

    Use page result instead of rendering layout directly and add page title in Advanced Search controller.

--- app/code/Magento/CatalogSearch/Controller/Advanced/Result.php
+++ app/code/Magento/CatalogSearch/Controller/Advanced/Result.php
@@ -6,19 +6,12 @@
  */
 namespace Magento\CatalogSearch\Controller\Advanced;
 
-use Magento\Catalog\Model\Layer\Resolver;
 use Magento\CatalogSearch\Model\Advanced as ModelAdvanced;
 use Magento\Framework\App\Action\Context;
-use Magento\Framework\UrlFactory;
+use Magento\Framework\Controller\ResultFactory;
 
 class Result extends \Magento\Framework\App\Action\Action
 {
-    /**
-     * Url factory
-     *
-     * @var UrlFactory
-     */
-    protected $_urlFactory;
 
     /**
      * Catalog search advanced
@@ -32,33 +25,35 @@ class Result extends \Magento\Framework\App\Action\Action
      *
      * @param Context $context
      * @param ModelAdvanced $catalogSearchAdvanced
-     * @param UrlFactory $urlFactory
      */
     public function __construct(
         Context $context,
-        ModelAdvanced $catalogSearchAdvanced,
-        UrlFactory $urlFactory
+        ModelAdvanced $catalogSearchAdvanced
+
     ) {
         parent::__construct($context);
         $this->_catalogSearchAdvanced = $catalogSearchAdvanced;
-        $this->_urlFactory = $urlFactory;
     }
 
     /**
-     * @return void
+     * Advanced Search Result page
+     *
+     * @return \Magento\Framework\Controller\ResultInterface
      */
     public function execute()
     {
         try {
             $this->_catalogSearchAdvanced->addFilters($this->getRequest()->getQueryValue());
-            $this->_view->loadLayout();
-            $this->_view->renderLayout();
         } catch (\Magento\Framework\Exception\LocalizedException $e) {
-            $this->messageManager->addError($e->getMessage());
-            $defaultUrl = $this->_urlFactory->create()
-                ->addQueryParams($this->getRequest()->getQueryValue())
-                ->getUrl('*/*/');
-            $this->getResponse()->setRedirect($this->_redirect->error($defaultUrl));
+            $this->messageManager->addErrorMessage($e->getMessage());
+            /** @var \Magento\Backend\Model\View\Result\Redirect $resultRedirect */
+            $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+            $resultRedirect->setPath('*/*/');
+            return $resultRedirect;
         }
+
+        $resultPage = $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+        $resultPage->getConfig()->getTitle()->set(__('Catalog Advanced Search'));
+        return $resultPage;
     }
 }
