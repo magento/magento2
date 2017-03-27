ba535bdce5861a0ec73ce6c60ec0b7e67be36c50
Ronald Edelschaap <rlwedelschaap@gmail.com>
2017-03-27 11:55:15 +0200

    Update AbstractFrontend.php
    The `getOptionText` method (called by `getOption`) does not support an array as parameter and therefor `getOption` cannot be used with an array.

--- app/code/Magento/Eav/Model/Entity/Attribute/Frontend/AbstractFrontend.php
+++ app/code/Magento/Eav/Model/Entity/Attribute/Frontend/AbstractFrontend.php
@@ -118,9 +118,10 @@ abstract class AbstractFrontend implements \Magento\Eav\Model\Entity\Attribute\F
             }
             $value = $valueOption;
         } elseif ($this->getConfigField('input') == 'multiselect') {
-            $value = $this->getOption($value);
             if (is_array($value)) {
-                $value = implode(', ', $value);
+                $value = implode(', ', array_map([$this, 'getOption'], $value));
+            } else {
+                $value = $this->getOption($value);
             }
         }
 
