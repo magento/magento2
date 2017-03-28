f078aff827837c927e8fc884ea1d0bbf981cd67a
Bartek Igielski <igloczek@gmail.com>
2017-03-28 12:10:54 +0200

    Node version switched to 6.x

--- dev/travis/before_script.sh
+++ dev/travis/before_script.sh
@@ -80,8 +80,9 @@ case $TEST_SUITE in
         curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
         export NVM_DIR="$HOME/.nvm"
         [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
-        nvm install --lts
-        nvm use --lts
+        bash -c "nvm install $NODE_JS_VERSION" || true
+        bash -c "nvm use $NODE_JS_VERSION" || true
+        node --version
 
         cp package.json.sample package.json
         cp Gruntfile.js.sample Gruntfile.js
