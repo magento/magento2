af00449880175dd970e07fb730e85dfe0c760125
Robert He <rohe@magento.com>
2017-03-28 16:52:49 -0500

    MAGETWO-66882: Allowing Web API listing to be public - 2.2
     - SOAP web service methods are listed in the public WSDL

--- app/code/Magento/Webapi/Model/AbstractSchemaGenerator.php
+++ app/code/Magento/Webapi/Model/AbstractSchemaGenerator.php
@@ -83,12 +83,7 @@ abstract class AbstractSchemaGenerator
     {
         $listOfAllowedServices = [];
         foreach ($this->serviceMetadata->getServicesConfig() as $serviceName => $service) {
-            foreach ($service[ServiceMetadata::KEY_SERVICE_METHODS] as $method) {
-                if ($this->authorization->isAllowed($method[ServiceMetadata::KEY_ACL_RESOURCES])) {
-                    $listOfAllowedServices[] = $serviceName;
-                    break;
-                }
-            }
+            $listOfAllowedServices[] = $serviceName;
         }
         return $listOfAllowedServices;
     }
