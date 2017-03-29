3cba1988507153b83a985205f3bb990ec313949c
Oleksii Korshenko <okorshenko@magento.com>
2017-03-29 19:43:04 +0300

    MAGETWO-66875: Remove the copyright year from file headers #8648

--- Model/WildCardHandler.php
+++ Model/WildCardHandler.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright © 2013-2017 Magento, Inc. All rights reserved.
+ * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
 namespace Magento\TestModuleAsyncAmqp\Model;
@@ -18,7 +18,7 @@ class WildCardHandler
             FILE_APPEND
         );
     }
-    
+
     /**
      * @param AsyncTestData $simpleDataItem
      */
