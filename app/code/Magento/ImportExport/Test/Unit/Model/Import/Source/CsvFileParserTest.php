8b8bcaed15a077144ec497388e563bf4cd8bb811
Ivan Chepurnyi <ivan.chepurnyi@ecomdev.org>
2017-04-02 22:00:05 -0700

    Remove wrong license header and specify proper one

--- app/code/Magento/ImportExport/Test/Unit/Model/Import/Source/CsvFileParserTest.php
+++ app/code/Magento/ImportExport/Test/Unit/Model/Import/Source/CsvFileParserTest.php
@@ -1,18 +1,7 @@
 <?php
 /**
- * magento-2-contribution-day
- *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the MIT License
- * that is bundled with this package in the file LICENSE.
- *
- * It is also available through the world-wide-web at this URL:
- * https://opensource.org/licenses/MIT
- *
- * @copyright  Copyright (c) 2017 EcomDev BV (http://www.ecomdev.org)
- * @license    https://opensource.org/licenses/MIT The MIT License (MIT)
- * @author     Ivan Chepurnyi <ivan@ecomdev.org>
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
  */
 
 
