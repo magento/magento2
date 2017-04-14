1f0b8f267c257b676035208361b22f810497f72c
Danny Verkade <danny@cream.nl>
2017-04-14 22:18:48 +0200

    Fixed coding standard violations in the Framework\Validator namespace: - Removed @codingStandardsIgnoreFile - Fixed indentation - Fixed number of chars per line

--- lib/internal/Magento/Framework/Validator/Constraint/Property.php
+++ lib/internal/Magento/Framework/Validator/Constraint/Property.php
@@ -6,8 +6,6 @@
  * See COPYING.txt for license details.
  */
 
-// @codingStandardsIgnoreFile
-
 namespace Magento\Framework\Validator\Constraint;
 
 class Property extends \Magento\Framework\Validator\Constraint
@@ -33,8 +31,8 @@ class Property extends \Magento\Framework\Validator\Constraint
     }
 
     /**
-     * Get value that should be validated. Tries to extract value's property if \Magento\Framework\DataObject or \ArrayAccess or array
-     * is passed
+     * Get value that should be validated. Tries to extract value's property if \Magento\Framework\DataObject or
+     * \ArrayAccess or array is passed
      *
      * @param mixed $value
      * @return mixed
