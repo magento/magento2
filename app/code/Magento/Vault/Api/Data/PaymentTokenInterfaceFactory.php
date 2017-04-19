d2ea4c123b7e37126fbb8a2962a3666b81739148
Danny Verkade <danny@cream.nl>
2017-04-19 10:17:49 +0200

    Added the old interface name with a @deprecated message, which extends the new interface name.

---
+++ app/code/Magento/Vault/Api/Data/PaymentTokenInterfaceFactory.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Vault\Api\Data;
+
+/**
+ * Interface PaymentTokenFactoryInterface
+ * @deprecated
+ */
+interface PaymentTokenInterfaceFactory extends PaymentTokenFactoryInterface
+{
+
+}
