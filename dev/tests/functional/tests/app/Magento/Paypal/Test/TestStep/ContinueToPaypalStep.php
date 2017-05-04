fb10bbfe1bc9804e1c5b7071c414680d09ab334d
OlgaVasyltsun <olga.vasyltsun@transoftgroup.com>
2017-05-04 15:20:46 +0300

    MAGETWO-66634: PayPal ExpressCheckout func tests fail

---
+++ dev/tests/functional/tests/app/Magento/Paypal/Test/TestStep/ContinueToPaypalStep.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Copyright © 2013-2017 Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Paypal\Test\TestStep;
+
+use Magento\Checkout\Test\Page\CheckoutOnepage;
+use Magento\Mtf\TestStep\TestStepInterface;
+
+/**
+ * Continue to PayPal from one page checkout.
+ */
+class ContinueToPaypalStep implements TestStepInterface
+{
+    /**
+     * Onepage checkout page.
+     *
+     * @var CheckoutOnepage
+     */
+    protected $checkoutOnepage;
+
+    /**
+     * @construct
+     * @param CheckoutOnepage $checkoutOnepage
+     */
+    public function __construct(CheckoutOnepage $checkoutOnepage)
+    {
+        $this->checkoutOnepage = $checkoutOnepage;
+    }
+
+    /**
+     * Click Continue to PayPal button.
+     *
+     * @return array
+     */
+    public function run()
+    {
+        $this->checkoutOnepage->getPaymentBlock()->getSelectedPaymentMethodBlock()->clickPlaceOrder();
+    }
+}
