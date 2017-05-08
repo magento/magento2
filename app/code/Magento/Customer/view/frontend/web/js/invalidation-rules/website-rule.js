71ef5e8a28adf23e4ed351fa189af53353b8b898
Oleksandr Osadchyi <oosadchyi@magento.com>
2017-05-08 11:21:48 +0300

    MAGETWO-59135: [Github] Customer session is shared for different customers on two websites #4842 #6468

--- app/code/Magento/Customer/view/frontend/web/js/invalidation-rules/website-rule.js
+++ app/code/Magento/Customer/view/frontend/web/js/invalidation-rules/website-rule.js
@@ -13,9 +13,6 @@ define([
             scopeConfig: {}
         },
 
-        initialize: function () {
-            this._super();
-        },
         /**
          * Takes website id from current customer data and compare it with current website id
          * If customer belongs to another scope, we need to invalidate current section
