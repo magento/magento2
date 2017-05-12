48ffa60f57d1c13932d889b197ba23f4e8afe6c5
Myroslav Dobra <dmaraptor@gmail.com>
2017-05-12 18:17:46 +0300

    MAGETWO-63456: [Backport] Wrong address template used for order e-mails - for 2.0

---
+++ dev/tests/integration/testsuite/Magento/Sales/Model/Order/Address/RendererTest.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Sales\Model\Order\Address;
+
+use Magento\Config\Model\ResourceModel\Config as ConfigResourceModel;
+use Magento\Framework\App\ReinitableConfig;
+use Magento\Framework\ObjectManagerInterface;
+use Magento\Sales\Model\Order;
+use Magento\Sales\Model\Order\Address as OrderAddress;
+use Magento\Sales\Model\Order\Address\Renderer as OrderAddressRenderer;
+use Magento\Store\Model\Store;
+use Magento\TestFramework\Helper\Bootstrap;
+
+/**
+ * Class RendererTest test address templates render in store scope.
+ */
+class RendererTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var ObjectManagerInterface
+     */
+    private $objectManager;
+
+    /**
+     * @var OrderAddressRenderer
+     */
+    private $orderAddressRenderer;
+
+    /**
+     * @var ConfigResourceModel
+     */
+    private $configResourceModel;
+
+    /**
+     * @var ReinitableConfig
+     */
+    private $reinitableConfig;
+
+    /**
+     * Prepare objects for test.
+     */
+    protected function setUp()
+    {
+        $this->objectManager = Bootstrap::getObjectManager();
+        $this->orderAddressRenderer = $this->objectManager->get(OrderAddressRenderer::class);
+        $this->configResourceModel = $this->objectManager->get(ConfigResourceModel::class);
+        $this->reinitableConfig = $this->objectManager->get(ReinitableConfig::class);
+    }
+
+    /**
+     * @magentoDataFixture Magento/Sales/_files/order_fixture_store.php
+     * @magentoDbIsolation enabled
+     * @magentoAppIsolation enabled
+     */
+    public function testFormat()
+    {
+        $addressTemplates = [
+            'text' => 'text_customized',
+            'oneline' => 'oneline_customized',
+            'html' => 'html_customized',
+            'pdf' => 'pdf_customized'
+        ];
+
+        /** @var Store $store */
+        $store = $this->objectManager->create(Store::class);
+        $storeId = $store->load('fixturestore')->getStoreId();
+
+        $this->configResourceModel->saveConfig(
+            'customer/address_templates/text',
+            $addressTemplates['text'],
+            'stores',
+            $storeId
+        );
+        $this->configResourceModel->saveConfig(
+            'customer/address_templates/oneline',
+            $addressTemplates['oneline'],
+            'stores',
+            $storeId
+        );
+        $this->configResourceModel->saveConfig(
+            'customer/address_templates/html',
+            $addressTemplates['html'],
+            'stores',
+            $storeId
+        );
+        $this->configResourceModel->saveConfig(
+            'customer/address_templates/pdf',
+            $addressTemplates['pdf'],
+            'stores',
+            $storeId
+        );
+        $this->reinitableConfig->reinit();
+
+        /** @var Order $order */
+        $order = $this->objectManager->create(Order::class)
+            ->loadByIncrementId('100000004');
+
+        /** @var OrderAddress $address */
+        $address = $order->getBillingAddress();
+
+        $this->assertEquals($addressTemplates['text'], $this->orderAddressRenderer->format($address, 'text'));
+        $this->assertEquals($addressTemplates['oneline'], $this->orderAddressRenderer->format($address, 'oneline'));
+        $this->assertEquals($addressTemplates['html'], $this->orderAddressRenderer->format($address, 'html'));
+        $this->assertEquals($addressTemplates['pdf'], $this->orderAddressRenderer->format($address, 'pdf'));
+    }
+}
