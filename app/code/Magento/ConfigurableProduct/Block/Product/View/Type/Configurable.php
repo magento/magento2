2fcb2bb6369e23db1d8d92d69606e6ee7bbe6781
Andrii Voskoboinikov <avoskoboinikov@magento.com>
2017-05-12 13:06:20 +0300

    MAGETWO-67010: [Performance] Swatches real server side optimization

--- app/code/Magento/ConfigurableProduct/Block/Product/View/Type/Configurable.php
+++ app/code/Magento/ConfigurableProduct/Block/Product/View/Type/Configurable.php
@@ -143,14 +143,14 @@ class Configurable extends \Magento\Catalog\Block\Product\View\AbstractView
     public function getAllowProducts()
     {
         if (!$this->hasAllowProducts()) {
+            $products = [];
             $skipSaleableCheck = $this->catalogProduct->getSkipSaleableCheck();
-
-            if ($skipSaleableCheck) {
-                $products = $this->getProduct()->getTypeInstance()->getUsedProducts($this->getProduct(), null);
-            } else {
-                $products = $this->getProduct()->getTypeInstance()->getSalableUsedProducts($this->getProduct(), null);
+            $allProducts = $this->getProduct()->getTypeInstance()->getUsedProducts($this->getProduct(), null);
+            foreach ($allProducts as $product) {
+                if ($product->isSaleable() || $skipSaleableCheck) {
+                    $products[] = $product;
+                }
             }
-
             $this->setAllowProducts($products);
         }
         return $this->getData('allow_products');
