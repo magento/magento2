716b7f85bfec0da8f061a209f7bb86e38b505438
Danny Verkade <danny@cream.nl>
2017-05-13 14:44:45 +0200

    Added constructor to the PaymentToken class to set the type.

--- app/code/Magento/Vault/Model/PaymentToken.php
+++ app/code/Magento/Vault/Model/PaymentToken.php
@@ -21,6 +21,27 @@ class PaymentToken extends AbstractModel implements PaymentTokenInterface
     protected $_eventPrefix = 'vault_payment_token';
 
     /**
+     * @param \Magento\Framework\Model\Context $context
+     * @param \Magento\Framework\Registry $registry
+     * @param \Magento\Framework\Model\ResourceModel\AbstractResource $resource
+     * @param \Magento\Framework\Data\Collection\AbstractDb $resourceCollection
+     * @param array $data
+     * @param string $type
+     */
+    public function __construct(
+        \Magento\Framework\Model\Context $context,
+        \Magento\Framework\Registry $registry,
+        \Magento\Framework\Model\ResourceModel\AbstractResource $resource = null,
+        \Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,
+        array $data = [],
+        $type
+    )
+    {
+        $this->setType($type);
+        parent::__construct($context, $registry, $resource, $resourceCollection, $data);
+    }
+
+    /**
      * @inheritdoc
      */
     protected function _construct()
