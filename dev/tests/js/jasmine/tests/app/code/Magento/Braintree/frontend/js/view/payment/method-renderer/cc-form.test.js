b4a082a7b65ec9275d52651ab07b92b8008c2f28
Viktor Tymchynskyi <vtymchynskyi@magento.com>
2017-05-18 19:27:26 +0300

    MAGETWO-63460: Braintree saved credit card error for reorder between $2001-$3000 does not display error to customer
    - JsUnit test added

---
+++ dev/tests/js/jasmine/tests/app/code/Magento/Braintree/frontend/js/view/payment/method-renderer/cc-form.test.js
@@ -0,0 +1,69 @@
+/**
+ * Copyright © 2013-2017 Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+/* eslint-disable max-nested-callbacks */
+define([
+    'jquery',
+    'squire',
+    'ko',
+    'Magento_Ui/js/model/messages'
+], function ($, Squire, ko, Messages) {
+    'use strict';
+
+    describe('Magento_Braintree/js/view/payment/method-renderer/cc-form', function () {
+        var injector = new Squire(),
+            mocks = {
+                'Magento_Checkout/js/model/quote': {
+                    billingAddress: ko.observable(),
+                    shippingAddress: ko.observable(),
+                    paymentMethod: ko.observable()
+                },
+                'Magento_Braintree/js/view/payment/validator-handler': jasmine.createSpyObj(
+                    'validator-handler',
+                    ['initialize']
+                ),
+                'Magento_Braintree/js/view/payment/adapter':  jasmine.createSpyObj(
+                    'adapter',
+                    ['setup', 'setConfig']
+                )
+            },
+            braintreeCcForm;
+
+        beforeAll(function (done) {
+            window.checkoutConfig = {
+                quoteData: {},
+                payment: {
+                    braintree: {
+                        hasFraudProtection: true
+                    }
+                }
+            };
+            injector.mock(mocks);
+            injector.require(
+                ['Magento_Braintree/js/view/payment/method-renderer/cc-form'],
+                function (Constr) {
+                    braintreeCcForm = new Constr({
+                        provider: 'provName',
+                        name: 'test',
+                        index: 'test'
+                    });
+
+                    done();
+                });
+        });
+
+        it('Check if payment code and message container are restored after onActiveChange call.', function () {
+            var expectedMessageContainer = braintreeCcForm.messageContainer;
+
+            braintreeCcForm.code = 'braintree-vault';
+            braintreeCcForm.messageContainer = new Messages();
+
+            braintreeCcForm.onActiveChange(true);
+
+            expect(braintreeCcForm.getCode()).toEqual('braintree');
+            expect(braintreeCcForm.messageContainer).toEqual(expectedMessageContainer);
+        });
+    });
+});
