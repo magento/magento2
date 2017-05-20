d8ad06bfaf9686d6c122419acaf3631527dc17eb
Minesh Patel <mineshpatel0111@gmail.com>
2017-05-20 11:29:43 +0530

    Admin Grid Mass action Select / Unselect All issue #9610

--- app/code/Magento/Backend/Block/Widget/Grid/Massaction/AbstractMassaction.php
+++ app/code/Magento/Backend/Block/Widget/Grid/Massaction/AbstractMassaction.php
@@ -277,7 +277,8 @@ abstract class AbstractMassaction extends \Magento\Backend\Block\Widget
 
         /** @var \Magento\Framework\Data\Collection $allIdsCollection */
         $allIdsCollection = clone $this->getParentBlock()->getCollection();
-        $gridIds = $allIdsCollection->clear()->setPageSize(0)->getAllIds();
+        $massActionIdField = $this->getParentBlock()->getMassactionIdField();
+        $gridIds = $allIdsCollection->clear()->setPageSize(0)->getColumnValues($massActionIdField);        
 
         if (!empty($gridIds)) {
             return join(",", $gridIds);
