c2efa1b064c5376d93d9c2b00907c5b44962ef2e
Ievgen Shakhsuvarov <ishakhsuvarov@magento.com>
2017-05-20 15:25:00 +0200

    magento-engcom/magento2#25: Source API Implementation

--- app/code/Magento/InventoryApi/Api/Data/SourceSearchResultsInterface.php
+++ app/code/Magento/InventoryApi/Api/Data/SourceSearchResultsInterface.php
@@ -5,10 +5,25 @@
  */
 namespace Magento\InventoryApi\Api\Data;
 
+use Magento\Framework\Api\SearchResultsInterface;
+
 /**
  * @api
  */
-interface SourceSearchResultsInterface
+interface SourceSearchResultsInterface extends SearchResultsInterface
 {
+    /**
+     * Get sources list.
+     *
+     * @return \Magento\InventoryApi\Api\Data\SourceInterface[]
+     */
+    public function getItems();
 
+    /**
+     * Set sources list.
+     *
+     * @param \Magento\InventoryApi\Api\Data\SourceInterface[] $items
+     * @return $this
+     */
+    public function setItems(array $items);
 }
