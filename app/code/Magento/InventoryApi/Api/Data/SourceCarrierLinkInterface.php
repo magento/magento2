bb10b2ae5db1ea5424773dd45bef7c7c46b04afc
Nils <nils.preuss@gmx.net>
2017-05-20 15:58:44 +0200

    magento-engcom/magento2#7 Add missing empty interface class

---
+++ app/code/Magento/InventoryApi/Api/Data/SourceCarrierLinkInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nhp
+ * Date: 5/20/17
+ * Time: 2:52 PM
+ */
+
+namespace Magento\InventoryApi\Api\Data;
+
+
+interface SourceCarrierLinkInterface
+{
+
+}
