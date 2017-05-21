c5f9131dbdb6c3b47d51c0169fcb379d909f00d7
Lars Röttig <roettigl@Larss-MacBook-Pro.local>
2017-05-21 11:10:17 +0200

    magento-engcom/magento2#3 rename ui grid to listing add doc

--- app/code/Magento/Inventory/Ui/Component/Listing/Column/Magentoinventorysource/PageActions.php
+++ app/code/Magento/Inventory/Ui/Component/Listing/Column/Magentoinventorysource/PageActions.php
@@ -1,27 +1,44 @@
 <?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
 namespace Magento\Inventory\Ui\Component\Listing\Column\Magentoinventorysource;
 
+use Magento\InventoryApi\Api\Data\SourceInterface;
+
+/**
+ * Class PageActions
+ */
 class PageActions extends \Magento\Ui\Component\Listing\Columns\Column
 {
+
+    /**
+     * Add new edit action for the ui grid.   
+     */
     public function prepareDataSource(array $dataSource)
     {
         if (isset($dataSource["data"]["items"])) {
             foreach ($dataSource["data"]["items"] as & $item) {
                 $name = $this->getData("name");
                 $id = "X";
-                if(isset($item["source_id"]))
-                {
-                    $id = $item["source_id"];
+
+                if (isset($item[SourceInterface::SOURCE_ID])) {
+                    $id = $item[SourceInterface::SOURCE_ID];
                 }
+
+                $href = $this->getContext()->getUrl(
+                    "magento_inventory_source/page/edit",
+                    [SourceInterface::SOURCE_ID => $id]
+                );
+
                 $item[$name]["view"] = [
-                    "href"=>$this->getContext()->getUrl(
-                        "magento_inventory_source/page/edit",["source_id"=>$id]),
-                    "label"=>__("Edit")
+                    "href" => $href,
+                    "label" => __("Edit")
                 ];
             }
         }
-
         return $dataSource;
-    }    
-    
+    }
 }
