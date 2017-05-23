7842e90b3beb31b70f45b2fc4150ee1544e336c7
Andrii Kasian <akasian@magento.com>
2017-05-23 18:42:00 +0300

    MAGETWO-68808: catalog_product_attribute is slow

--- lib/internal/Magento/Framework/Indexer/BatchProvider.php
+++ lib/internal/Magento/Framework/Indexer/BatchProvider.php
@@ -58,7 +58,7 @@ class BatchProvider implements BatchProviderInterface
             $connection->quote($batch['to'])
         );
 
-        $ids = $connection->fetchCol($select->where($betweenCondition)->limit($batch['from'] - $batch['to'] + 1));
+        $ids = $connection->fetchCol($select->where($betweenCondition)->limit($batch['to'] - $batch['from'] + 1));
         return array_map('intval', $ids);
     }
 }
