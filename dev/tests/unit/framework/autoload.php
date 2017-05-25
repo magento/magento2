7ff0f2bafde2cd6111751eff3298cbc707b588bf
Olga Kopylova <okopylova@magento.com>
2017-05-25 12:40:30 -0500

    MAGETWO-67626: Auto-generated classes behave differently in unit tests and application
    - refactored generators
    - skip generation of non-conventional classes/interfaces

--- dev/tests/unit/framework/autoload.php
+++ dev/tests/unit/framework/autoload.php
@@ -8,6 +8,7 @@ use Magento\Framework\App\Filesystem\DirectoryList;
 use Magento\Framework\Code\Generator\Io;
 use Magento\Framework\Filesystem\Driver\File;
 use Magento\Framework\TestFramework\Unit\Autoloader\ExtensionAttributesGenerator;
+use Magento\Framework\TestFramework\Unit\Autoloader\ExtensionAttributesInterfaceGenerator;
 use Magento\Framework\TestFramework\Unit\Autoloader\FactoryGenerator;
 use Magento\Framework\TestFramework\Unit\Autoloader\GeneratedClassesAutoloader;
 
@@ -18,6 +19,7 @@ $generatorIo = new Io(
 $generatedCodeAutoloader = new GeneratedClassesAutoloader(
     [
         new ExtensionAttributesGenerator(),
+        new ExtensionAttributesInterfaceGenerator(),
         new FactoryGenerator(),
     ],
     $generatorIo
--- lib/internal/Magento/Framework/TestFramework/Unit/Autoloader/ExtensionAttributesInterfaceGenerator.php
+++ lib/internal/Magento/Framework/TestFramework/Unit/Autoloader/ExtensionAttributesInterfaceGenerator.php
@@ -6,46 +6,45 @@
 
 namespace Magento\Framework\TestFramework\Unit\Autoloader;
 
-use Magento\Framework\Code\Generator\ClassGenerator;
+use Magento\Framework\Code\Generator\InterfaceGenerator;
 
 /**
- * Code generation for the undeclared *Extension types
+ * Code generation for the undeclared *ExtensionInterface
  *
  * These files must be generated since they are referenced in many interfaces/classes and cannot be mocked easily.
  * For unit tests, these are just empty type definitions. You should use integration tests if you want to see the real
  * types be generated with the properties from the extension attributes config.
  */
-class ExtensionAttributesGenerator implements GeneratorInterface
+class ExtensionAttributesInterfaceGenerator implements GeneratorInterface
 {
     /**
-     * Generates a stub class for classes that follow the convention
+     * Generates a stub interface for interfaces that follow the convention
      *
-     * The convention is "<SourceClass>Extension"
+     * The convention is "<SourceClass>ExtensionInterface"
      *
      * @param string $className
      * @return bool|string
      */
     public function generate($className)
     {
-        if (!$this->isExtension($className)) {
+        if (!$this->isExtensionInterface($className)) {
             return false;
         }
-        $classGenerator = new ClassGenerator();
-        $classGenerator->setName($className)
-            ->setImplementedInterfaces(["{$className}Interface"]);
-        return $classGenerator->generate();
-
+        $interfaceGenerator = new InterfaceGenerator();
+        $interfaceGenerator->setName($className)
+            ->setExtendedClass('\Magento\Framework\Api\ExtensionAttributesInterface');
+        return $interfaceGenerator->generate();
     }
 
     /**
-     * Determines if the passed in class name is an Extension type.
+     * Determines if the passed in class name is an ExtensionInterface type.
      *
      * @param string $className
      * @return bool
      */
-    private function isExtension($className)
+    private function isExtensionInterface($className)
     {
-        $suffix = "Extension";
+        $suffix = "ExtensionInterface";
         $sourceName = rtrim(substr($className, 0, -strlen($suffix)), '\\');
         return $sourceName . $suffix == $className;
     }
