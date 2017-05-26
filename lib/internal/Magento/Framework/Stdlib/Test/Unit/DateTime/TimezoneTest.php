52222949eae3db5ed08c9915d63edb59316c9ac0
James Halsall <james.halsall@inviqa.com>
2017-05-26 13:11:33 +0100

    Add unit tests for Timezone in stdlib, fix edge cases

--- lib/internal/Magento/Framework/Stdlib/Test/Unit/DateTime/TimezoneTest.php
+++ lib/internal/Magento/Framework/Stdlib/Test/Unit/DateTime/TimezoneTest.php
@@ -5,46 +5,53 @@
  */
 namespace Magento\Framework\Stdlib\Test\Unit\DateTime;
 
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\App\ScopeResolverInterface;
+use Magento\Framework\Locale\ResolverInterface;
+use Magento\Framework\Stdlib\DateTime;
 use Magento\Framework\Stdlib\DateTime\Timezone;
+use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
 
-/**
- * Test for @see Timezone
- */
 class TimezoneTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * @var \Magento\Framework\TestFramework\Unit\Helper\ObjectManager
+     * @var ObjectManager
      */
     private $objectManager;
 
     /**
-     * @var \PHPUnit_Framework_MockObject_MockObject
+     * @var ScopeResolverInterface|\PHPUnit_Framework_MockObject_MockObject
      */
-    private $scopeConfigMock;
+    private $scopeResolver;
+
+    /**
+     * @var ResolverInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $localeResolver;
+
+    /**
+     * @var ScopeConfigInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $scopeConfig;
 
     protected function setUp()
     {
-        $this->objectManager = new \Magento\Framework\TestFramework\Unit\Helper\ObjectManager($this);
-        $this->scopeConfigMock = $this->getMockBuilder(\Magento\Framework\App\Config\ScopeConfigInterface::class)
-            ->disableOriginalConstructor()
-            ->getMock();
-        parent::setUp();
+        $this->objectManager = new ObjectManager($this);
+        $this->scopeResolver = $this->getMockBuilder(ScopeResolverInterface::class)->getMock();
+        $this->localeResolver = $this->getMockBuilder(ResolverInterface::class)->getMock();
+        $this->scopeConfig = $this->getMockBuilder(ScopeConfigInterface::class)->getMock();
     }
 
     /**
      * Test date parsing with different includeTime options
      *
-     * @param string $date
-     * @param string $locale
-     * @param bool $includeTime
-     * @param int $expectedTimestamp
      * @dataProvider dateIncludeTimeDataProvider
      */
     public function testDateIncludeTime($date, $locale, $includeTime, $expectedTimestamp)
     {
-        $this->scopeConfigMock->method('getValue')->willReturn('America/Chicago');
+        $this->scopeConfig->method('getValue')->willReturn('America/Chicago');
         /** @var Timezone $timezone */
-        $timezone = $this->objectManager->getObject(Timezone::class, ['scopeConfig' => $this->scopeConfigMock]);
+        $timezone = $this->objectManager->getObject(Timezone::class, ['scopeConfig' => $this->scopeConfig]);
 
         /** @var \DateTime $dateTime */
         $dateTime = $timezone->date($date, $locale, true, $includeTime);
@@ -67,5 +74,88 @@ class TimezoneTest extends \PHPUnit_Framework_TestCase
                 1495170060 // expected timestamp
             ],
         ];
+    }
+
+    /**
+     * @dataProvider getConvertConfigTimeToUtcFixtures
+     */
+    public function testConvertConfigTimeToUtc($date, $configuredTimezone, $expectedResult)
+    {
+        $this->scopeConfigWillReturnConfiguredTimezone($configuredTimezone);
+
+        $this->assertEquals($expectedResult, $this->getTimezone()->convertConfigTimeToUtc($date));
+    }
+
+    public function getConvertConfigTimeToUtcFixtures()
+    {
+        return [
+            'string' => [
+                '2016-10-10 10:00:00',
+                'UTC',
+                '2016-10-10 10:00:00'
+            ],
+            'datetime' => [
+                new \DateTime('2016-10-10 10:00:00'),
+                'UTC',
+                '2016-10-10 10:00:00'
+            ],
+            'datetimeimmutable' => [
+                new \DateTimeImmutable('2016-10-10 10:00:00'),
+                'UTC',
+                '2016-10-10 10:00:00'
+            ]
+        ];
+    }
+
+    /**
+     * @dataProvider getDateFixtures
+     */
+    public function testDate($expectedResult, $timezone = 'UTC', $date = null, $locale = null)
+    {
+        $this->localeResolver
+            ->method('getLocale')
+            ->willReturn('en_GB')
+        ;
+
+        $this->scopeConfigWillReturnConfiguredTimezone($timezone);
+
+        $this->assertEquals(
+            $expectedResult,
+            $this->getTimezone()->date($date, $locale, true),
+            '',
+            1
+        );
+    }
+
+    public function getDateFixtures()
+    {
+        return [
+            'now_datetime_utc' => [new \DateTime('now', new \DateTimeZone('UTC')), 'UTC'],
+            'fixed_datetime_utc' => [new \DateTime('2017-01-01 10:00:00', new \DateTimeZone('UTC')), 'UTC', new \DateTime('2017-01-01 10:00:00')],
+            'now_datetime_vancouver' => [new \DateTime('now', new \DateTimeZone('America/Vancouver')), 'America/Vancouver'],
+            'now_datetimeimmutable_utc' => [new \DateTimeImmutable('now', new \DateTimeZone('UTC')), 'UTC'],
+            'fixed_datetimeimmutable_utc' => [new \DateTime('2017-01-01 10:00:00', new \DateTimeZone('UTC')), 'UTC', new \DateTimeImmutable('2017-01-01 10:00:00')],
+            'now_datetimeimmutable_vancouver' => [new \DateTimeImmutable('now', new \DateTimeZone('America/Vancouver')), 'America/Vancouver'],
+        ];
+    }
+
+    private function getTimezone()
+    {
+        return new \Magento\Framework\Stdlib\DateTime\Timezone(
+            $this->scopeResolver,
+            $this->localeResolver,
+            $this->getMockBuilder(DateTime::class)->getMock(),
+            $this->scopeConfig,
+            '',
+            ''
+        );
+    }
+
+    private function scopeConfigWillReturnConfiguredTimezone($configuredTimezone)
+    {
+        $this->scopeConfig
+            ->method('getValue')
+            ->with('', '', null)
+            ->willReturn($configuredTimezone);
     }
 }
