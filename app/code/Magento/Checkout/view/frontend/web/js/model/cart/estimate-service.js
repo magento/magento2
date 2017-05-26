8e185a20a93f5753a192bc06a1b71d359eb09605
pdohogne-magento <pdohogne@magento.com>
2017-05-26 18:17:51 -0500

    MAGETWO-55301: Enable tax rate calculation on address change without shipping methods

--- app/code/Magento/Checkout/view/frontend/web/js/model/cart/estimate-service.js
+++ app/code/Magento/Checkout/view/frontend/web/js/model/cart/estimate-service.js
@@ -19,7 +19,7 @@ define([
     quote.shippingAddress.subscribe(function () {
         var type = quote.shippingAddress().getType();
 
-        if (quote.isVirtual()) {
+        if (quote.isVirtual() || quote.shippingAddress().isEditable()) {
             // update totals block when estimated address was set
             totalsProcessors['default'] = totalsDefaultProvider;
             totalsProcessors[type] ?
--- app/code/Magento/Tax/view/frontend/web/js/view/checkout/summary/tax.js
+++ app/code/Magento/Tax/view/frontend/web/js/view/checkout/summary/tax.js
@@ -11,20 +11,13 @@ define([
     'ko',
     'Magento_Checkout/js/view/summary/abstract-total',
     'Magento_Checkout/js/model/quote',
-    'Magento_Checkout/js/model/totals',
-    'Magento_Checkout/js/model/cart/totals-processor/default'
-], function (ko, Component, quote, totals, totalsDefaultProcessor) {
+    'Magento_Checkout/js/model/totals'
+], function (ko, Component, quote, totals) {
     'use strict';
 
     var isTaxDisplayedInGrandTotal = window.checkoutConfig.includeTaxInGrandTotal,
         isFullTaxSummaryDisplayed = window.checkoutConfig.isFullTaxSummaryDisplayed,
         isZeroTaxDisplayed = window.checkoutConfig.isZeroTaxDisplayed;
-
-    quote.shippingAddress.subscribe(function () {
-        if (quote.shippingAddress().isEditable()) {
-            totalsDefaultProcessor.estimateTotals(quote.shippingAddress());
-        }
-    });
 
     return Component.extend({
         defaults: {
