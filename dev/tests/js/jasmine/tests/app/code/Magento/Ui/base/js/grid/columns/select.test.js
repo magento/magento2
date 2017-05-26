0a44a3444dcf261771c679ca9830c429f1278c67
Ronald Edelschaap <rlwedelschaap@gmail.com>
2017-05-26 09:11:23 +0200

    Update select.test.js
    Added a scenario where options are loaded as an object instead of an array. Options are generated in PHP. This is the case when the option array in PHP has non-numeric keys or has numeric keys but misses some numeric keys.
    
    I noticed the opts test was deleted by someone in an earlier commit, but I think this is too important to remove from the test, so I added it again.

--- dev/tests/js/jasmine/tests/app/code/Magento/Ui/base/js/grid/columns/select.test.js
+++ dev/tests/js/jasmine/tests/app/code/Magento/Ui/base/js/grid/columns/select.test.js
@@ -10,7 +10,23 @@ define([
     'use strict';
 
     describe('Ui/js/grid/columns/select', function () {
-        var select;
+        var opts = [{
+                label : 'a', value : 1
+            }, {
+                label : 'b', value : 2
+            }],
+            optsAsObject = {
+                1 : {
+                    label : 'a', value : 1
+                },
+                2 : {
+                    label : 'b', value : 2
+                },
+                4 : {
+                    label : 'c', value : 3
+                }
+            },
+            select;
 
         beforeEach(function () {
             select = new Select();
@@ -19,6 +35,16 @@ define([
         describe('getLabel method', function () {
             it('get label while options empty', function () {
                 expect(select.getLabel(2)).toBe('');
+            });
+
+            it('get label for existed value', function () {
+                select.options = opts;
+                expect(select.getLabel(2)).toBe('b');
+            });
+
+            it('get label for existed value in case the options are initialized as an object', function () {
+                select.options = optsAsObject;
+                expect(select.getLabel(3)).toBe('c');
             });
         });
     });
