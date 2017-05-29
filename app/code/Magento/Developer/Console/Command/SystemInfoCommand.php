7b8f019cd385feed79dc7ea900c0ca7507ee454d
Abhisek Pandey <abhisek.pandey@vaimo.com>
2017-05-29 16:01:00 +0200

    #9201 - Added tables and data for application, version, application/session/crypt key data, module vendors, number of products/customers/attributes/categories

--- app/code/Magento/Developer/Console/Command/SystemInfoCommand.php
+++ app/code/Magento/Developer/Console/Command/SystemInfoCommand.php
@@ -7,23 +7,132 @@ namespace Magento\Developer\Console\Command;
 
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Helper\TableSeparator;
+use Magento\Framework\App\State as AppState;
 
 class SystemInfoCommand extends \Symfony\Component\Console\Command\Command
 {
+    /**
+     * Constructor
+     */
+    public function __construct(
+        \Magento\Framework\App\ProductMetadataInterface $productMetadataInterface,
+        \Magento\Framework\App\DeploymentConfig $deploymentConfig,
+        \Magento\Framework\Module\ModuleListInterface $moduleList,
+        \Magento\Customer\Model\CustomerFactory $customerFactory,
+        \Magento\Catalog\Model\ProductFactory $productFactory,
+        \Magento\Catalog\Model\CategoryFactory $categoryFactory,
+        \Magento\Eav\Model\Entity\AttributeFactory $attributeFactory
+    ) {
+        parent::__construct('info:system');
+        $this->productMetadataInterface = $productMetadataInterface;
+        $this->deploymentConfig = $deploymentConfig;
+        $this->moduleList = $moduleList;
+        $this->customerFactory = $customerFactory;
+        $this->productFactory = $productFactory;
+        $this->categoryFactory = $categoryFactory;
+        $this->attributeFactory = $attributeFactory;
+    }
 
     protected function configure()
     {
-        $this->setName('info:system')
-            ->setDescription('')
-        ;
         parent::configure();
+        $this->setDescription('Get information about Magento setup and installation');
     }
 
     /**
-     * {@inheritdoc}
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return void
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln("<info>helo</info>");
+        $table = new Table($output);
+
+        $table
+            ->setHeaders(array('Name', 'Value'))
+            ->setRows(array(
+                array('Application', $this->productMetadataInterface->getName() . ' ' . $this->productMetadataInterface->getEdition()),
+                array('Version', $this->productMetadataInterface->getVersion()),
+                new TableSeparator(),
+
+                array('Application Mode', $this->deploymentConfig->get(AppState::PARAM_MODE)),
+                array('Session', $this->deploymentConfig->get('session/save')),
+                array('Crypt Key', $this->deploymentConfig->get('crypt/key')),
+                array('Install Date', $this->deploymentConfig->get('install/date')),
+                array('Module Vendors', $this->getModuleVendors()),
+                new TableSeparator(),
+
+                array('Total Products', $this->getProductCount()),
+                array('Total Categories', $this->getProductCount()),
+                array('Total Attributes', $this->getAttributeCount()),
+                array('Total Customers', $this->getCustomerCount()),
+            ))
+        ;
+        $table->render();
+
+    }
+
+    /**
+     * @return string
+     */
+    protected function getModuleVendors()
+    {
+        $vendors = [];
+        $moduleList = $this->moduleList->getAll();
+
+        foreach ($moduleList as $moduleName => $info) {
+            $moduleNameData = explode('_', $moduleName);
+
+            if (isset($moduleNameData[0])) {
+                $vendors[] = $moduleNameData[0];
+            }
+        }
+
+        return implode(', ', array_unique($vendors));
+    }
+
+    /**
+     * @return number
+     */
+    protected function getProductCount()
+    {
+        return $this->productFactory
+            ->create()
+            ->getCollection()
+            ->getSize();
+    }
+
+    /**
+     * @return number
+     */
+    protected function getCategoryCount()
+    {
+        return $this->categoryFactory
+            ->create()
+            ->getCollection()
+            ->getSize();
+    }
+
+    /**
+     * @return number
+     */
+    protected function getAttributeCount()
+    {
+        return $this->attributeFactory
+            ->create()
+            ->getCollection()
+            ->getSize();
+    }
+
+    /**
+     * @return number
+     */
+    protected function getCustomerCount()
+    {
+        return $this->customerFactory->create()
+            ->getCollection()
+            ->getSize();
     }
 }
