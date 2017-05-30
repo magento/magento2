5319a6f4c3802190b4b4b03b79edf97dbbd650b5
Andrii Meysar <andrii.meysar@transoftgroup.com>
2017-05-30 20:54:09 +0300

    MAGETWO-64676: Functional tests changes.

--- dev/tests/functional/tests/app/Magento/Checkout/Test/Block/Onepage/Review.php
+++ dev/tests/functional/tests/app/Magento/Checkout/Test/Block/Onepage/Review.php
@@ -6,29 +6,10 @@
 
 namespace Magento\Checkout\Test\Block\Onepage;
 
-use Magento\Mtf\Client\Locator;
-
 /**
  * One page checkout status review block.
  */
 class Review extends AbstractReview
 {
-    /**
-     * Review gift card line locator.
-     *
-     * @var string
-     */
-    private $giftCardTotalSelector = '//div[contains(@class, "opc-block-summary")]//tr[contains(@class, "giftcard")]';
 
-    /**
-     * Return if gift card is applied.
-     *
-     * @return bool
-     */
-    public function isGiftCardApplied()
-    {
-        $this->waitForElementNotVisible($this->waitElement);
-
-        return $this->_rootElement->find($this->giftCardTotalSelector, Locator::SELECTOR_XPATH)->isVisible();
-    }
 }
