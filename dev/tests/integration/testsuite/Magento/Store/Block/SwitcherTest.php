b05aa3a390a24118cfc23322c12783410831d954
Anatol Burym <anatol_burym@epam.com>
2017-05-31 15:28:24 +0300

    MAGETWO-64885: Wrong cookies set for store views with multidomain
    - Integration test added.

---
+++ dev/tests/integration/testsuite/Magento/Store/Block/SwitcherTest.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Store\Block;
+
+/**
+ * Integration tests for \Magento\Store\Block\Switcher block.
+ */
+class SwitcherTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \Magento\TestFramework\ObjectManager
+     */
+    private $_objectManager;
+
+    /**
+     * Set up.
+     *
+     * @return void
+     */
+    protected function setUp()
+    {
+        /** @var $objectManager \Magento\TestFramework\ObjectManager */
+        $this->_objectManager = \Magento\TestFramework\Helper\Bootstrap::getObjectManager();
+    }
+
+    /**
+     * Test that GetTargetStorePostData() method return correct store URL.
+     *
+     * @magentoDataFixture Magento/Store/_files/store.php
+     * @return void
+     */
+    public function testGetTargetStorePostData()
+    {
+        $storeCode = 'test';
+        /** @var \Magento\Store\Block\Switcher $block */
+        $block = $this->_objectManager->create(\Magento\Store\Block\Switcher::class);
+        /** @var \Magento\Store\Api\StoreRepositoryInterface $storeRepository */
+        $storeRepository = $this->_objectManager->create(\Magento\Store\Api\StoreRepositoryInterface::class);
+        $store = $storeRepository->get($storeCode);
+        $result = json_decode($block->getTargetStorePostData($store), true);
+        
+        $this->assertContains($storeCode, $result['action']);
+    }
+}
