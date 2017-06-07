255a02ba163ce9e5141fab331845bdf65fcf6804
Ihor Sviziev <ihor-sviziev@users.noreply.github.com>
2017-06-07 12:16:48 +0300

    Fix _addUrlRewrite method for product collection

--- app/code/Magento/Catalog/Model/ResourceModel/Product/Collection.php
+++ app/code/Magento/Catalog/Model/ResourceModel/Product/Collection.php
@@ -1403,6 +1403,11 @@ class Collection extends \Magento\Catalog\Model\ResourceModel\Collection\Abstrac
                 ['cu' => $this->getTable('catalog_url_rewrite_product_category')],
                 'u.url_rewrite_id=cu.url_rewrite_id'
             )->where('cu.category_id IN (?)', $this->_urlRewriteCategory);
+        } else {
+            $select->joinLeft(
+                ['cu' => $this->getTable('catalog_url_rewrite_product_category')],
+                'u.url_rewrite_id=cu.url_rewrite_id'
+            )->where('cu.category_id IS NULL');
         }
 
         // more priority is data with category id
