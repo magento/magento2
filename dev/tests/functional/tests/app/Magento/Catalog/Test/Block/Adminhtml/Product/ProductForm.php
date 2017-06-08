06e916d1ddc581877527f314e27fa3df6f6e7d57
Andrii Meysar <andrii.meysar@transoftgroup.com>
2017-06-08 10:51:58 +0300

    MAGETWO-69474: [FT] CreateProductAttributeEntityFromProductPageTest (CreateProductAttributeEntityFromProductPageTestVariation1_Searchable_Global_Visible_Comparable_HtmlAllowed_UsedForSorting) unstable for 2.1.7 (CE)

--- dev/tests/functional/tests/app/Magento/Catalog/Test/Block/Adminhtml/Product/ProductForm.php
+++ dev/tests/functional/tests/app/Magento/Catalog/Test/Block/Adminhtml/Product/ProductForm.php
@@ -151,7 +151,7 @@ class ProductForm extends FormSections
     public function getAttributesSearchGrid()
     {
         return $this->blockFactory->create(
-            '\Magento\Catalog\Test\Block\Adminhtml\Product\Edit\Section\Attributes\Grid',
+            \Magento\Catalog\Test\Block\Adminhtml\Product\Edit\Section\Attributes\Grid::class,
             ['element' => $this->browser->find($this->attributeSearch)]
         );
     }
@@ -190,7 +190,7 @@ class ProductForm extends FormSections
     public function getAttributeForm()
     {
         return $this->blockFactory->create(
-            'Magento\Catalog\Test\Block\Adminhtml\Product\Attribute\AttributeForm',
+            \Magento\Catalog\Test\Block\Adminhtml\Product\Attribute\AttributeForm::class,
             ['element' => $this->browser->find($this->newAttributeModal)]
         );
     }
@@ -206,7 +206,7 @@ class ProductForm extends FormSections
         return $this->_rootElement->find(
             sprintf($this->attributeBlock, $attribute->getAttributeCode()),
             Locator::SELECTOR_CSS,
-            'Magento\Catalog\Test\Block\Adminhtml\Product\Attribute\CustomAttribute'
+            \Magento\Catalog\Test\Block\Adminhtml\Product\Attribute\CustomAttribute::class
         );
     }
 }
