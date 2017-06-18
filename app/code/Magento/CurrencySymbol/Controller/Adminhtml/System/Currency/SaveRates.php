3a88958bca8b926926f7fa7d6c8577d33d80b659
Danny Verkade <danny@cream.nl>
2017-06-18 11:30:06 +0200

    Fixed coding standard violations: - Removed @codingstanardignorefile from head of file - Fixed line length

--- app/code/Magento/CurrencySymbol/Controller/Adminhtml/System/Currency/SaveRates.php
+++ app/code/Magento/CurrencySymbol/Controller/Adminhtml/System/Currency/SaveRates.php
@@ -5,8 +5,6 @@
  * See COPYING.txt for license details.
  */
 
-// @codingStandardsIgnoreFile
-
 namespace Magento\CurrencySymbol\Controller\Adminhtml\System\Currency;
 
 class SaveRates extends \Magento\CurrencySymbol\Controller\Adminhtml\System\Currency
