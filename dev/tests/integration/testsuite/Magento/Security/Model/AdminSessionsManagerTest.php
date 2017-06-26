acadb5c0ac4fe2c88aa95f2345fdb374365f6738
Stas Kozar <stas.kozar@transoftgroup.com>
2017-06-26 10:15:07 +0300

    MAGETWO-58533: [Backport] - Loading of order page with a lot of items in admin. - for 2.1

--- dev/tests/integration/testsuite/Magento/Security/Model/AdminSessionsManagerTest.php
+++ dev/tests/integration/testsuite/Magento/Security/Model/AdminSessionsManagerTest.php
@@ -37,11 +37,6 @@ class AdminSessionsManagerTest extends \PHPUnit_Framework_TestCase
      */
     protected $objectManager;
 
-//    /**
-//     * @var \Magento\Framework\Session\SessionManager
-//     */
-//    private $sessionManager;
-
     /**
      * Set up
      */
@@ -50,7 +45,6 @@ class AdminSessionsManagerTest extends \PHPUnit_Framework_TestCase
         parent::setUp();
 
         $this->objectManager = \Magento\TestFramework\Helper\Bootstrap::getObjectManager();
-        //$this->sessionManager = $this->objectManager->create(\Magento\Framework\Session\SessionManager::class);
         $this->objectManager->get(\Magento\Framework\Config\ScopeInterface::class)
             ->setCurrentScope(\Magento\Backend\App\Area\FrontNameResolver::AREA_CODE);
         $this->auth = $this->objectManager->create(\Magento\Backend\Model\Auth::class);
