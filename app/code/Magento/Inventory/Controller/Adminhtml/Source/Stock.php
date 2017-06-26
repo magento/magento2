e712957fee0d23cc935663b0db8ca2e5742b8653
jonfres <jonfres@gmail.com>
2017-06-26 14:59:54 +0100

    magento-engcom/magento#34: Source to Stock Mapping UI and API
            - controller Stock and menu option for Manage Stock added

---
+++ app/code/Magento/Inventory/Controller/Adminhtml/Source/Stock.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Inventory\Controller\Adminhtml\Source;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\Model\View\Result\Page;
+use Magento\Framework\Controller\ResultFactory;
+
+/**
+ * Class Index
+ */
+class Stock extends Action
+{
+    /**
+     * @see _isAllowed()
+     */
+    const ADMIN_RESOURCE = 'Magento_Inventory::stock';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function execute()
+    {
+        /** @var Page $resultPage */
+        $resultPage = $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+        $resultPage->setActiveMenu('Magento_Inventory::stock')
+            ->addBreadcrumb(__('Sources'), __('List'));
+        $resultPage->getConfig()->getTitle()->prepend(__('Manage Stock'));
+        return $resultPage;
+    }
+}
