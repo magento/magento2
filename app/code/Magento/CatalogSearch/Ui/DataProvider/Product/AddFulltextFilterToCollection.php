e820074c552b7bd82a3148274c3f6b045382b4b3
Josef Behr <mail@josef-behr.de>
2017-06-28 18:21:32 +0200

    Create AddFulltextFilterToCollection.php

---
+++ app/code/Magento/CatalogSearch/Ui/DataProvider/Product/AddFulltextFilterToCollection.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Magento\CatalogSearch\Ui\DataProvider\Product;
+
+use Magento\Framework\Data\Collection;
+use Magento\Ui\DataProvider\AddFilterToCollectionInterface;
+
+/**
+ * Class AddFulltextFilterToCollection
+ */
+class AddFulltextFilterToCollection implements AddFilterToCollectionInterface
+{
+    protected $collection;
+
+    public function __construct(\Magento\CatalogSearch\Model\ResourceModel\Search\Collection $searchCollection)
+    {
+        $this->searchCollection = $searchCollection;
+    }
+
+    /**
+     * @param Collection $collection
+     * @param string $field
+     * @param null $condition
+     */
+    public function addFilter(Collection $collection, $field, $condition = null)
+    {
+        /** @var $collection \Magento\Catalog\Model\ResourceModel\Product\Collection */
+        if (isset($condition['fulltext']) && !empty($condition['fulltext'])) {
+            $this->searchCollection->addBackendSearchFilter($condition['fulltext']);
+            $productIds = $this->searchCollection->load()->getAllIds();
+            $collection->addIdFilter($productIds);
+        }
+    }
+}
