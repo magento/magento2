c13c01ce033f59f6996bad220f3259fbfcd3fae0
Oleksandr Shmyheliuk <oshmyheliuk@magento.com>
2017-07-05 09:17:16 +0300

    MAGETWO-69964: PHPCS cannot parse correctly syntax of PHP 7.x return types

--- dev/tests/static/framework/Magento/Sniffs/EchoTags/ShortEchoSyntaxSniff.php
+++ dev/tests/static/framework/Magento/Sniffs/EchoTags/ShortEchoSyntaxSniff.php
@@ -5,11 +5,11 @@
  */
 namespace Magento\Sniffs\EchoTags;
 
-use PHP_CodeSniffer_File;
-use PHP_CodeSniffer_Sniff;
-use PHP_CodeSniffer_Tokens;
+use PHP_CodeSniffer\Sniffs\Sniff;
+use PHP_CodeSniffer\Files\File;
+use PHP_CodeSniffer\Util\Tokens;
 
-class ShortEchoSyntaxSniff implements PHP_CodeSniffer_Sniff
+class ShortEchoSyntaxSniff implements Sniff
 {
     /**
      * {@inheritdoc}
@@ -22,7 +22,7 @@ class ShortEchoSyntaxSniff implements PHP_CodeSniffer_Sniff
     /**
      * {@inheritdoc}
      */
-    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    public function process(File $phpcsFile, $stackPtr)
     {
         $tokens  = $phpcsFile->getTokens();
         $openTag = $tokens[$stackPtr];
@@ -32,7 +32,7 @@ class ShortEchoSyntaxSniff implements PHP_CodeSniffer_Sniff
             return;
         }
 
-        $nextToken = $phpcsFile->findNext(PHP_CodeSniffer_Tokens::$emptyTokens, ($stackPtr + 1), null, true);
+        $nextToken = $phpcsFile->findNext(Tokens::$emptyTokens, ($stackPtr + 1), null, true);
         if ($tokens[$nextToken]['code'] == T_ECHO) {
             $phpcsFile->addError(
                 'Short echo tag syntax must be used; expected "<?=" but found "<?php echo"',
