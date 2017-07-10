a0a08d1ad6b64edaf0566550c3127644c5694573
Oleksii Korshenko <okorshenko@magento.com>
2017-07-10 16:49:57 -0500

    MAGETWO-67509: Decimal to Float Convertion #9266
     - reverted original fix due to broken logic in other parts or the system
     - added local fix for Tier Price

--- app/code/Magento/Catalog/Model/ResourceModel/Product/Link/Product/Collection.php
+++ app/code/Magento/Catalog/Model/ResourceModel/Product/Link/Product/Collection.php
@@ -362,29 +362,4 @@ class Collection extends \Magento\Catalog\Model\ResourceModel\Product\Collection
                 );
         }
     }
-
-    /**
-     * After the collection has been loaded
-     *
-     * @SuppressWarnings(PHPMD.CamelCaseMethodName)
-     * @return $this
-     */
-    protected function _afterLoad()
-    {
-        if ($this->getLinkModel()) {
-            $attributes = $this->getLinkAttributes();
-            foreach ($this as $item) {
-                foreach ($attributes as $attribute) {
-                    $code = $attribute['code'];
-                    $value = $item->getData($code);
-                    if ($value !== null) {
-                        if ($attribute['type'] == 'decimal') {
-                            $item->setData($code, (float)$value);
-                        }
-                    }
-                }
-            }
-        }
-        return parent::_afterLoad();
-    }
 }
