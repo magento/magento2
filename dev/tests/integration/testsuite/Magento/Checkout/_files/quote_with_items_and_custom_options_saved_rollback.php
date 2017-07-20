1b7e4fbe160c5119a53d77402c16d351b2308f93
Viktor Tymchynskyi <vtymchynskyi@magento.com>
2017-07-20 10:49:57 +0300

    MAGETWO-70743: Custom options are not listed in API call

--- dev/tests/integration/testsuite/Magento/Checkout/_files/quote_with_items_and_custom_options_saved_rollback.php
+++ dev/tests/integration/testsuite/Magento/Checkout/_files/quote_with_items_and_custom_options_saved_rollback.php
@@ -4,17 +4,13 @@
  * See COPYING.txt for license details.
  */
 $objectManager = \Magento\TestFramework\Helper\Bootstrap::getObjectManager();
-$registry = $objectManager->get(\Magento\Framework\Registry::class);
-$registry->unregister('isSecureArea');
-$registry->register('isSecureArea', true);
+
+/** @var \Magento\Quote\Model\Quote $quote */
 $quote = $objectManager->create(\Magento\Quote\Model\Quote::class);
 $quote->load('test_order_item_with_items_and_custom_options', 'reserved_order_id');
 $quoteId = $quote->getId();
 if ($quote->getId()) {
-    $quote->delete();
+    $objectManager->get(\Magento\Quote\Model\QuoteRepository::class)->delete($quote);
 }
-
-$registry->unregister('isSecureArea');
-$registry->register('isSecureArea', false);
 
 require __DIR__ . '/../../Checkout/_files/quote_with_address_rollback.php';
