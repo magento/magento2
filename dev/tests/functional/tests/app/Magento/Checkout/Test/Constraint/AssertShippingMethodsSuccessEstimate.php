0f27eba2342d2e9a0c3ab73978a7b465492bb3a9
Andrii Meysar <andrii.meysar@transoftgroup.com>
2017-07-27 10:33:04 +0300

    MAGETWO-60958: [Backport] Incorrect province code sent on Checkout to UPS #6564 - for 2.1 - MAGETWO-70727: [GitHub] Shipping method randomly dissapear when page refreshed for 2.1 #7497

--- dev/tests/functional/tests/app/Magento/Checkout/Test/Constraint/AssertShippingMethodsSuccessEstimate.php
+++ dev/tests/functional/tests/app/Magento/Checkout/Test/Constraint/AssertShippingMethodsSuccessEstimate.php
@@ -7,16 +7,12 @@
 namespace Magento\Checkout\Test\Constraint;
 
 use Magento\Checkout\Test\Page\CheckoutOnepage;
-use Magento\Checkout\Test\TestStep\FillShippingAddressStep;
 use Magento\Mtf\Client\BrowserInterface;
-use Magento\Mtf\Constraint\AbstractConstraint;
-use Magento\Mtf\Fixture\FixtureFactory;
-use Magento\Mtf\TestStep\TestStepFactory;
 
 /**
  * Asserts that shipping methods are present on checkout page.
  */
-class AssertShippingMethodsSuccessEstimate extends AbstractConstraint
+class AssertShippingMethodsSuccessEstimate extends AssertShippingMethodsEstimate
 {
     /**
      * Asserts that shipping methods are present on checkout page.
@@ -25,46 +21,9 @@ class AssertShippingMethodsSuccessEstimate extends AbstractConstraint
      * @param BrowserInterface $browser
      * @return void
      */
-    public function processAssert(
-        CheckoutOnepage $checkoutOnepage,
-        BrowserInterface $browser
-    ) {
-        if ($this->shouldOpenCheckout($checkoutOnepage, $browser)) {
-            $checkoutOnepage->open();
-        }
-
-        \PHPUnit_Framework_Assert::assertFalse(
-            $checkoutOnepage->getShippingMethodBlock()->isErrorPresent(),
-            'Shipping estimation error is present.'
-        );
-
-        $methods = $checkoutOnepage->getShippingMethodBlock()->getAvailableMethods();
-        \PHPUnit_Framework_Assert::assertNotEmpty(
-            $methods,
-            'No shipping methods are present.'
-        );
-    }
-
-    /**
-     * Should open checkout page or not.
-     *
-     * @param CheckoutOnepage  $checkoutOnepage
-     * @param BrowserInterface $browser
-     * @return bool
-     */
-    private function shouldOpenCheckout(CheckoutOnepage $checkoutOnepage, BrowserInterface $browser)
+    public function processAssert(CheckoutOnepage $checkoutOnepage, BrowserInterface $browser)
     {
-        $result = true;
-
-        foreach (['checkout/', $checkoutOnepage::MCA] as $path) {
-            $length = strlen($path);
-            if (substr($browser->getUrl(), -$length) === $path) {
-                $result = false;
-                break;
-            }
-        }
-
-        return $result;
+        $this->assert($checkoutOnepage, $browser);
     }
 
     /**
--- dev/tests/functional/tests/app/Magento/Checkout/Test/Constraint/AssertShippingMethodsSuccessEstimateAfterAddressEdit.php
+++ dev/tests/functional/tests/app/Magento/Checkout/Test/Constraint/AssertShippingMethodsSuccessEstimateAfterAddressEdit.php
@@ -9,14 +9,13 @@ namespace Magento\Checkout\Test\Constraint;
 use Magento\Checkout\Test\Page\CheckoutOnepage;
 use Magento\Checkout\Test\TestStep\FillShippingAddressStep;
 use Magento\Mtf\Client\BrowserInterface;
-use Magento\Mtf\Constraint\AbstractConstraint;
 use Magento\Mtf\Fixture\FixtureFactory;
 use Magento\Mtf\TestStep\TestStepFactory;
 
 /**
  * Asserts that shipping methods are present on checkout page after address modification.
  */
-class AssertShippingMethodsSuccessEstimateAfterAddressEdit extends AbstractConstraint
+class AssertShippingMethodsSuccessEstimateAfterAddressEdit extends AssertShippingMethodsEstimate
 {
     /**
      * Asserts that shipping methods are present on checkout page after address modification.
@@ -35,53 +34,18 @@ class AssertShippingMethodsSuccessEstimateAfterAddressEdit extends AbstractConst
         BrowserInterface $browser,
         array $editAddressData = []
     ) {
-        if ($this->shouldOpenCheckout($checkoutOnepage, $browser)) {
-            $checkoutOnepage->open();
-        }
-
         if (!empty ($editAddressData)) {
             $address = $fixtureFactory->createByCode('address', ['data' => $editAddressData]);
             $testStepFactory->create(
                 FillShippingAddressStep::class,
                 [
                     'checkoutOnepage' => $checkoutOnepage,
-                    'shippingAddress' => $address
+                    'shippingAddress' => $address,
                 ]
             )->run();
 
-            \PHPUnit_Framework_Assert::assertFalse(
-                $checkoutOnepage->getShippingMethodBlock()->isErrorPresent(),
-                'Shipping estimation error is present.'
-            );
-
-            $methods = $checkoutOnepage->getShippingMethodBlock()->getAvailableMethods();
-            \PHPUnit_Framework_Assert::assertNotEmpty(
-                $methods,
-                'No shipping methods are present.'
-            );
+            $this->assert($checkoutOnepage, $browser);
         }
-    }
-
-    /**
-     * Should open checkout page or not.
-     *
-     * @param CheckoutOnepage  $checkoutOnepage
-     * @param BrowserInterface $browser
-     * @return bool
-     */
-    private function shouldOpenCheckout(CheckoutOnepage $checkoutOnepage, BrowserInterface $browser)
-    {
-        $result = true;
-        
-        foreach (['checkout/', $checkoutOnepage::MCA] as $path) {
-            $length = strlen($path);
-            if (substr($browser->getUrl(), -$length) === $path) {
-                $result = false;
-                break;
-            }
-        }
-
-        return $result;
     }
 
     /**
