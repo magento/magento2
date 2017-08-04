eb8fb965179eb0f317f9eb0f75215d5210144998
Sviatoslav Mankivskyi <smankivskyi@magento.com>
2017-08-04 17:20:04 +0300

    MAGETWO-70705: [GitHub] Installation error in Tax module #10138

--- app/code/Magento/Tax/Block/Adminhtml/Rate/Grid/Renderer/Data.php
+++ app/code/Magento/Tax/Block/Adminhtml/Rate/Grid/Renderer/Data.php
@@ -18,14 +18,4 @@ namespace Magento\Tax\Block\Adminhtml\Rate\Grid\Renderer;
  */
 class Data extends \Magento\Backend\Block\Widget\Grid\Column\Renderer\AbstractRenderer
 {
-    /**
-     * @param \Magento\Framework\DataObject $row
-     * @return int|string
-     * @since 2.0.0
-     * @deprecated since it doesn't bring any value anymore
-     */
-    protected function _getValue(\Magento\Framework\DataObject $row)
-    {
-        return parent::_getValue($row);
-    }
 }
