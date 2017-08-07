104e664de1ab8487f208925ae203fc7cb745604c
Thiago <thiago.lima@bommercatto.com.br>
2017-08-07 14:33:01 +0200

    Update Template.php
    adding named groups in order to avoid access by indexes, refactoring validations and variables

--- lib/internal/Magento/Framework/Filter/Template.php
+++ lib/internal/Magento/Framework/Filter/Template.php
@@ -28,35 +28,31 @@ class Template implements \Zend_Filter_Interface
     /**
      * Looping regular expression
      */
-    const LOOP_PATTERN = '/{{for(.*? )(in)(.*?)}}(.*?){{\/for}}/si';
+    const LOOP_PATTERN = '/{{for(?P<loopItem>.*? )(in)(?P<loopData>.*?)}}(?P<loopBody>.*?){{\/for}}/si';
 
     /**#@-*/
-
-    /**
-     * Callbacks that will be applied after filtering
-     *
-     * @var array
-     */
-    private $afterFilterCallbacks = [];
-
     /**
      * Assigned template variables
      *
      * @var array
      */
     protected $templateVars = [];
-
     /**
      * Template processor
      *
      * @var callable|null
      */
     protected $templateProcessor = null;
-
     /**
      * @var \Magento\Framework\Stdlib\StringUtils
      */
     protected $string;
+    /**
+     * Callbacks that will be applied after filtering
+     *
+     * @var array
+     */
+    private $afterFilterCallbacks = [];
 
     /**
      * @param \Magento\Framework\Stdlib\StringUtils $string
@@ -80,28 +76,6 @@ class Template implements \Zend_Filter_Interface
             $this->templateVars[$name] = $value;
         }
         return $this;
-    }
-
-    /**
-     * Sets the processor for template directive.
-     *
-     * @param callable $callback it must return string
-     * @return $this
-     */
-    public function setTemplateProcessor(callable $callback)
-    {
-        $this->templateProcessor = $callback;
-        return $this;
-    }
-
-    /**
-     * Sets the processor for template directive.
-     *
-     * @return callable|null
-     */
-    public function getTemplateProcessor()
-    {
-        return is_callable($this->templateProcessor) ? $this->templateProcessor : null;
     }
 
     /**
@@ -158,151 +132,86 @@ class Template implements \Zend_Filter_Interface
     }
 
     /**
-     * Runs callbacks that have been added to filter content after directive processing is finished.
+     * Filter the string as template.
      *
      * @param string $value
+     * @example syntax {{for item in order.all_visible_items}} sku: {{var item.sku}}<br>name: {{var item.name}}<br> {{/for}} order items collection.
+     * @example syntax {{for thing in things}} {{var thing.whatever}} {{/for}} e.g.:custom collection.
      * @return string
      */
-    protected function afterFilter($value)
+    protected function filterFor($value)
     {
-        foreach ($this->afterFilterCallbacks as $callback) {
-            $value = call_user_func($callback, $value);
+        if (preg_match_all(self::LOOP_PATTERN, $value, $constructions, PREG_SET_ORDER)) {
+            foreach ($constructions as $construction) {
+
+                if (!$this->isValidLoop($construction)) {
+                    return $value;
+                }
+
+                $fullTextToReplace = $construction[0];
+                $loopData = $this->getVariable($construction['loopData'], '');
+
+                $loopTextToReplace = $construction['loopBody'];
+                $loopItem = preg_replace('/\s+/', '', $construction['loopItem']);
+
+                if (is_array($loopData) || $loopData instanceof \Traversable) {
+
+                    $loopText = [];
+                    $indexCount = 0;
+                    $loop = new \Magento\Framework\DataObject;
+
+                    foreach ($loopData as $objectData) {
+
+                        if (!$objectData instanceof \Magento\Framework\DataObject) { // is array?
+
+                            if (!is_array($objectData)) {
+                                continue;
+                            }
+
+                            $_item = new \Magento\Framework\DataObject;
+                            $_item->setData($objectData);
+                            $objectData = $_item;
+                        }
+
+                        $loop->setData('index', $indexCount++);
+                        $this->templateVars['loop'] = $loop;
+                        $this->templateVars[$loopItem] = $objectData;
+
+                        if (preg_match_all(self::CONSTRUCTION_PATTERN, $loopTextToReplace, $attributes,
+                            PREG_SET_ORDER)) {
+
+                            $subText = $loopTextToReplace;
+                            foreach ($attributes as $attribute) {
+                                $text = $this->getVariable($attribute[2], '');
+                                $subText = str_replace($attribute[0], $text, $subText);
+                            }
+                            $loopText[] = $subText;
+                        }
+                        unset($this->templateVars[$loopItem]);
+
+                    }
+                    $replaceText = implode('', $loopText);
+                    $value = str_replace($fullTextToReplace, $replaceText, $value);
+                }
+            }
         }
-        // Since a single instance of this class can be used to filter content multiple times, reset callbacks to
-        // prevent callbacks running for unrelated content (e.g., email subject and email body)
-        $this->resetAfterFilterCallbacks();
+
         return $value;
     }
 
     /**
-     * Adds a callback to run after main filtering has happened. Callback must accept a single argument and return
-     * a string of the processed value.
-     *
-     * @param callable $afterFilterCallback
-     * @return $this
+     * @param $construction
+     * @return bool
      */
-    public function addAfterFilterCallback(callable $afterFilterCallback)
+    private function isValidLoop($construction)
     {
-        // Only add callback if it doesn't already exist
-        if (in_array($afterFilterCallback, $this->afterFilterCallbacks)) {
-            return $this;
+        if ((strlen(trim($construction['loopBody'])) == 0) &&
+            (strlen(trim($construction['loopItem'])) == 0) &&
+            (strlen(trim($construction['loopData'])) == 0)
+        ) {
+            return true;
         }
-
-        $this->afterFilterCallbacks[] = $afterFilterCallback;
-        return $this;
-    }
-
-    /**
-     * Resets the after filter callbacks
-     *
-     * @return $this
-     */
-    protected function resetAfterFilterCallbacks()
-    {
-        $this->afterFilterCallbacks = [];
-        return $this;
-    }
-
-    /**
-     * @param string[] $construction
-     * @return string
-     */
-    public function varDirective($construction)
-    {
-        if (count($this->templateVars) == 0) {
-            // If template prepossessing
-            return $construction[0];
-        }
-
-        $replacedValue = $this->getVariable($construction[2], '');
-        return $replacedValue;
-    }
-
-    /**
-     * Allows templates to be included inside other templates
-     *
-     * Usage:
-     *
-     *     {{template config_path="<PATH>"}}
-     *
-     * <PATH> equals the XPATH to the system configuration value that contains the value of the template.
-     * This directive is useful to include things like a global header/footer.
-     *
-     * @param string[] $construction
-     * @return mixed
-     */
-    public function templateDirective($construction)
-    {
-        // Processing of {template config_path=... [...]} statement
-        $templateParameters = $this->getParameters($construction[2]);
-        if (!isset($templateParameters['config_path']) or !$this->getTemplateProcessor()) {
-            // Not specified template or not set include processor
-            $replacedValue = '{Error in template processing}';
-        } else {
-            // Including of template
-            $configPath = $templateParameters['config_path'];
-            unset($templateParameters['config_path']);
-            $templateParameters = array_merge_recursive($templateParameters, $this->templateVars);
-            $replacedValue = call_user_func($this->getTemplateProcessor(), $configPath, $templateParameters);
-        }
-        return $replacedValue;
-    }
-
-    /**
-     * @param string[] $construction
-     * @return string
-     */
-    public function dependDirective($construction)
-    {
-        if (count($this->templateVars) == 0) {
-            // If template processing
-            return $construction[0];
-        }
-
-        if ($this->getVariable($construction[1], '') == '') {
-            return '';
-        } else {
-            return $construction[2];
-        }
-    }
-
-    /**
-     * @param string[] $construction
-     * @return string
-     */
-    public function ifDirective($construction)
-    {
-        if (count($this->templateVars) == 0) {
-            return $construction[0];
-        }
-
-        if ($this->getVariable($construction[1], '') == '') {
-            if (isset($construction[3]) && isset($construction[4])) {
-                return $construction[4];
-            }
-            return '';
-        } else {
-            return $construction[2];
-        }
-    }
-
-    /**
-     * Return associative array of parameters.
-     *
-     * @param string $value raw parameters
-     * @return array
-     */
-    protected function getParameters($value)
-    {
-        $tokenizer = new Template\Tokenizer\Parameter();
-        $tokenizer->setString($value);
-        $params = $tokenizer->tokenize();
-        foreach ($params as $key => $value) {
-            if (substr($value, 0, 1) === '$') {
-                $params[$key] = $this->getVariable(substr($value, 1), null);
-            }
-        }
-        return $params;
+        return false;
     }
 
     /**
@@ -324,7 +233,7 @@ class Template implements \Zend_Filter_Interface
         for ($i = 0; $i < count($stackVars); $i++) {
             if ($i == 0 && isset($this->templateVars[$stackVars[$i]['name']])) {
                 // Getting of template value
-                $stackVars[$i]['variable'] = & $this->templateVars[$stackVars[$i]['name']];
+                $stackVars[$i]['variable'] = &$this->templateVars[$stackVars[$i]['name']];
             } elseif (isset($stackVars[$i - 1]['variable'])
                 && $stackVars[$i - 1]['variable'] instanceof \Magento\Framework\DataObject
             ) {
@@ -380,70 +289,172 @@ class Template implements \Zend_Filter_Interface
     }
 
     /**
-     * Filter the string as template.
+     * Runs callbacks that have been added to filter content after directive processing is finished.
      *
      * @param string $value
-     * @example syntax {{for item in order.all_visible_items}} sku: {{var item.sku}}<br>name: {{var item.name}}<br> {{/for}} order items collection.
-     * @example syntax {{for thing in things}} {{var thing.whatever}} {{/for}} e.g.:custom collection.
      * @return string
      */
-    protected function filterFor($value)
+    protected function afterFilter($value)
     {
-        if (preg_match_all(self::LOOP_PATTERN, $value, $constructions, PREG_SET_ORDER)) {
-            foreach ($constructions as $construction) {
+        foreach ($this->afterFilterCallbacks as $callback) {
+            $value = call_user_func($callback, $value);
+        }
+        // Since a single instance of this class can be used to filter content multiple times, reset callbacks to
+        // prevent callbacks running for unrelated content (e.g., email subject and email body)
+        $this->resetAfterFilterCallbacks();
+        return $value;
+    }
 
-                if (sizeof($construction) < 5) {
-                    return $value;
-                }
+    /**
+     * Resets the after filter callbacks
+     *
+     * @return $this
+     */
+    protected function resetAfterFilterCallbacks()
+    {
+        $this->afterFilterCallbacks = [];
+        return $this;
+    }
 
-                $full_text_to_replace = $construction[0];
-                $objectArrayData = $this->getVariable($construction[3], '');
-                $loop_text_to_replace = $construction[4];
-                $vName = preg_replace('/\s+/', '', $construction[1]);
-
-                if (is_array($objectArrayData) || $objectArrayData instanceof \Traversable) {
-
-                    $loopText = [];
-                    $indexCount = 1;
-                    $loop = new \Magento\Framework\DataObject;
-
-                    foreach ($objectArrayData as $k => $objectData) {
-
-                        if (!$objectData instanceof \Magento\Framework\DataObject) { // is array?
-
-                            if (!is_array($objectData)) {
-                                continue;
-                            }
-
-                            $_item = new \Magento\Framework\DataObject;
-                            $_item->setData($objectData);
-                            $objectData = $_item;
-                        }
-
-                        $loop->setData('index', $indexCount++);
-                        $loop->setData('loop');
-                        $this->templateVars['loop'] = $loop;
-
-                        $this->templateVars[$vName] = $objectData;
-
-                        if (preg_match_all(self::CONSTRUCTION_PATTERN, $loop_text_to_replace, $attributes, PREG_SET_ORDER)) {
-
-                            $subText = $loop_text_to_replace;
-                            foreach ($attributes as $attribute) {
-                                $text = $this->getVariable($attribute[2], '');
-                                $subText = str_replace($attribute[0], $text, $subText);
-                            }
-                            $loopText[] = $subText;
-                        }
-                        unset($this->templateVars[$vName]);
-
-                    }
-                    $replaceText = implode('', $loopText);
-                    $value = str_replace($full_text_to_replace, $replaceText, $value);
-                }
-            }
+    /**
+     * Adds a callback to run after main filtering has happened. Callback must accept a single argument and return
+     * a string of the processed value.
+     *
+     * @param callable $afterFilterCallback
+     * @return $this
+     */
+    public function addAfterFilterCallback(callable $afterFilterCallback)
+    {
+        // Only add callback if it doesn't already exist
+        if (in_array($afterFilterCallback, $this->afterFilterCallbacks)) {
+            return $this;
         }
 
-        return $value;
+        $this->afterFilterCallbacks[] = $afterFilterCallback;
+        return $this;
+    }
+
+    /**
+     * @param string[] $construction
+     * @return string
+     */
+    public function varDirective($construction)
+    {
+        if (count($this->templateVars) == 0) {
+            // If template prepossessing
+            return $construction[0];
+        }
+
+        $replacedValue = $this->getVariable($construction[2], '');
+        return $replacedValue;
+    }
+
+    /**
+     * Allows templates to be included inside other templates
+     *
+     * Usage:
+     *
+     *     {{template config_path="<PATH>"}}
+     *
+     * <PATH> equals the XPATH to the system configuration value that contains the value of the template.
+     * This directive is useful to include things like a global header/footer.
+     *
+     * @param string[] $construction
+     * @return mixed
+     */
+    public function templateDirective($construction)
+    {
+        // Processing of {template config_path=... [...]} statement
+        $templateParameters = $this->getParameters($construction[2]);
+        if (!isset($templateParameters['config_path']) or !$this->getTemplateProcessor()) {
+            // Not specified template or not set include processor
+            $replacedValue = '{Error in template processing}';
+        } else {
+            // Including of template
+            $configPath = $templateParameters['config_path'];
+            unset($templateParameters['config_path']);
+            $templateParameters = array_merge_recursive($templateParameters, $this->templateVars);
+            $replacedValue = call_user_func($this->getTemplateProcessor(), $configPath, $templateParameters);
+        }
+        return $replacedValue;
+    }
+
+    /**
+     * Return associative array of parameters.
+     *
+     * @param string $value raw parameters
+     * @return array
+     */
+    protected function getParameters($value)
+    {
+        $tokenizer = new Template\Tokenizer\Parameter();
+        $tokenizer->setString($value);
+        $params = $tokenizer->tokenize();
+        foreach ($params as $key => $value) {
+            if (substr($value, 0, 1) === '$') {
+                $params[$key] = $this->getVariable(substr($value, 1), null);
+            }
+        }
+        return $params;
+    }
+
+    /**
+     * Sets the processor for template directive.
+     *
+     * @return callable|null
+     */
+    public function getTemplateProcessor()
+    {
+        return is_callable($this->templateProcessor) ? $this->templateProcessor : null;
+    }
+
+    /**
+     * Sets the processor for template directive.
+     *
+     * @param callable $callback it must return string
+     * @return $this
+     */
+    public function setTemplateProcessor(callable $callback)
+    {
+        $this->templateProcessor = $callback;
+        return $this;
+    }
+
+    /**
+     * @param string[] $construction
+     * @return string
+     */
+    public function dependDirective($construction)
+    {
+        if (count($this->templateVars) == 0) {
+            // If template processing
+            return $construction[0];
+        }
+
+        if ($this->getVariable($construction[1], '') == '') {
+            return '';
+        } else {
+            return $construction[2];
+        }
+    }
+
+    /**
+     * @param string[] $construction
+     * @return string
+     */
+    public function ifDirective($construction)
+    {
+        if (count($this->templateVars) == 0) {
+            return $construction[0];
+        }
+
+        if ($this->getVariable($construction[1], '') == '') {
+            if (isset($construction[3]) && isset($construction[4])) {
+                return $construction[4];
+            }
+            return '';
+        } else {
+            return $construction[2];
+        }
     }
 }
