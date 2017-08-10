eea585abd738d183e7c7598e0f3d0047ac150df1
pbaylies <pbaylies@gmail.com>
2017-08-10 10:18:38 -0400

    Update Configurable.php

--- app/code/Magento/ConfigurableProduct/Block/Cart/Item/Renderer/Configurable.php
+++ app/code/Magento/ConfigurableProduct/Block/Cart/Item/Renderer/Configurable.php
@@ -24,7 +24,7 @@ class Configurable extends Renderer implements IdentityInterface
     /**
      * Get item configurable child product
      *
-     * @return \Magento\Catalog\Model\Product
+     * @return \Magento\Catalog\Model\Product|null
      */
     public function getChildProduct()
     {
@@ -66,7 +66,8 @@ class Configurable extends Renderer implements IdentityInterface
         if ($this->_scopeConfig->getValue(
             self::CONFIG_THUMBNAIL_SOURCE,
             \Magento\Store\Model\ScopeInterface::SCOPE_STORE
-        ) == ThumbnailSource::OPTION_USE_PARENT_IMAGE || !($this->getChildProduct() 
+        ) == ThumbnailSource::OPTION_USE_PARENT_IMAGE
+          || !($this->getChildProduct()
           && $this->getChildProduct()->getThumbnail() && $this->getChildProduct()->getThumbnail() != 'no_selection')
         ) {
             $product = $this->getProduct();
