e2e5365033a82e72c9be71e9b3054cb515a60869
Pieter Hoste <hoste.pieter@gmail.com>
2017-08-10 21:03:49 +0200

    Instead of using store code's in the optgroup, use 'WebsiteName > StoreName > StoreviewName'. Also added support for cms pages assigned to multiple storeview.

--- app/code/Magento/Cms/Model/ResourceModel/Page/Collection.php
+++ app/code/Magento/Cms/Model/ResourceModel/Page/Collection.php
@@ -26,6 +26,12 @@ class Collection extends AbstractCollection
     protected $_previewFlag;
 
     /**
+     *
+     * @var array|null
+     */
+    private $storeLabelsCache;
+
+    /**
      * Define resource model
      *
      * @return void
@@ -52,7 +58,7 @@ class Collection extends AbstractCollection
 
             $data['value'] = $identifier;
             $data['label'] = $item->getData('title');
-            $data['store'] = $item->getData('store_code');
+            $data['store_ids'] = $item->getData('store_id');
 
             if (in_array($identifier, $existingIdentifiers)) {
                 $data['value'] .= '|' . $item->getData('page_id');
@@ -68,36 +74,85 @@ class Collection extends AbstractCollection
             return strcmp($optionA['label'], $optionB['label']);
         });
 
-        $res = $this->groupOptionsByStore($res);
-
-        // TODO: transform the storecode to the website/store/storeview name here
-        // we have to do this here, because website/store/storeview combination isn't strictly unique,
-        // whereas the storecode is
-        // if we do the transformation earlier, the 'groupOptionsByStore' method would behave incorrectly
-        // TODO: also sort the array by the website/store/storeview name
+        $res = $this->groupOptionsByStoreLabels($res);
 
         return $res;
     }
 
     /**
-     * Changes the options, so they are grouped by store
+     * Changes the options, so they are grouped by store labels
      *
      * @param array $options
      * @return array
      */
-    private function groupOptionsByStore(array $options)
+    private function groupOptionsByStoreLabels(array $options)
     {
         $grouped = [];
 
         foreach ($options as $option) {
-            $store = $option['store'];
-            if (!array_key_exists($store, $grouped)) {
-                $grouped[$store] = ['label' => $store, 'value' => []];
+            $storeIds = $option['store_ids'];
+            foreach ($storeIds as $storeId) {
+                if (!array_key_exists($storeId, $grouped)) {
+                    $grouped[$storeId] = ['label' => $this->replaceStoreIdWithLabel($storeId), 'value' => []];
+                }
+                $grouped[$storeId]['value'][] = ['label' => $option['label'], 'value' => $option['value']];
             }
-            $grouped[$store]['value'][] = ['label' => $option['label'], 'value' => $option['value']];
         }
 
+        // sort by store labels
+        usort($grouped, function ($storeA, $storeB) {
+            return strcmp($storeA['label'], $storeB['label']);
+        });
+
         return ['values' => ['label' => '', 'value' => $grouped]];
+    }
+
+    /**
+     * Replace the store id we receive with the store label
+     * If label isn't found, we return the store id back (this "should" never happen)
+     *
+     * @param string $storeId
+     * @return string
+     */
+    private function replaceStoreIdWithLabel(string $storeId)
+    {
+        $storeLabels = $this->getStoreLabels();
+
+        if (array_key_exists($storeId, $storeLabels)) {
+            return $storeLabels[$storeId];
+        }
+
+        return $storeId;
+    }
+
+    /**
+     * Get array with all store labels, in the form of 'WebsiteName > StoreName > StoreviewName'
+     * The key of the array is the store id
+     *
+     * @return array
+     */
+    private function getStoreLabels()
+    {
+        if ($this->storeLabelsCache === null) {
+            $this->storeLabelsCache = [];
+
+            $websites = $this->storeManager->getWebsites();
+            foreach ($websites as $website) {
+                $groups = $website->getGroups();
+                foreach ($groups as $group) {
+                    $stores = $group->getStores();
+                    foreach ($stores as $store) {
+                        $this->storeLabelsCache[$store->getStoreId()] =
+                            "{$website->getName()} > {$group->getName()} > {$store->getName()}";
+                    }
+                }
+            }
+
+            // also add a label for the default/admin store id
+            $this->storeLabelsCache[\Magento\Store\Model\Store::DEFAULT_STORE_ID] = __('All Store Views');
+        }
+
+        return $this->storeLabelsCache;
     }
 
     /**
