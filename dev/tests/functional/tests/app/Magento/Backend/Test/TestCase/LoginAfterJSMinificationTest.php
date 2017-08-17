a0c6b2e2c6895d91fdcf23992aef46f313ed3964
Deepty Thampy <dthampy@magento.com>
2017-08-17 10:49:51 -0500

    MAGETWO-71416: Admin login is successful after JS magnification is enabled and mode set to production
    - Added functional test

---
+++ dev/tests/functional/tests/app/Magento/Backend/Test/TestCase/LoginAfterJSMinificationTest.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Backend\Test\TestCase;
+
+use Magento\Mtf\TestCase\Injectable;
+use Magento\Backend\Test\Page\Adminhtml\Dashboard;
+use Magento\Mtf\Util\Command\Cli\DeployMode;
+use Magento\Mtf\TestStep\TestStepFactory;
+
+/**
+ * Verify visibility of form elements on Configuration page.
+ *
+ * @ZephyrId MAGETWO-71416
+ */
+class LoginAfterJSMinificationTest extends Injectable
+{
+
+    /**
+     * Admin dashboard page
+     * @var Dashboard
+     */
+    private $adminDashboardPage;
+
+    /**
+     * Factory for Test Steps.
+     *
+     * @var TestStepFactory
+     */
+    private $stepFactory;
+
+    /**
+     * Configuration setting.
+     *
+     * @var string
+     */
+    private $configData;
+
+    /**
+     * Prepare data for further test execution.
+     *
+     * @param Dashboard $adminDashboardPage
+     * @return void
+     */
+    public function __inject(Dashboard $adminDashboardPage, TestStepFactory $stepFactory)
+    {
+        $this->adminDashboardPage = $adminDashboardPage;
+        $this->stepFactory = $stepFactory;
+    }
+
+    public function test(DeployMode $cli, $configData = null)
+    {
+       //Pre-conditions
+        $cli->setDeployModeToDeveloper();
+        $this->configData = $configData;
+        $this->objectManager->create(
+            \Magento\Config\Test\TestStep\SetupConfigurationStep::class,
+            ['configData' => $this->configData]
+        )->run();
+        $cli->setDeployModeToProduction();
+        $this->adminDashboardPage->open();
+    }
+
+
+
+}
