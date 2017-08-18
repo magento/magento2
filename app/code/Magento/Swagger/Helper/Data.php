ac3803bcce9922e6f83b19838edd52719c0455de
Lewis Voncken <lewis@experius.nl>
2017-08-18 10:24:38 +0000

    [FEATURE] Change the store code in Swagger based on a param

---
+++ app/code/Magento/Swagger/Helper/Data.php
@@ -0,0 +1,39 @@
+<?php
+/***
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Swagger\Helper;
+
+/**
+ * Class Data
+ * @package Magento\Swagger\Helper\Data
+ */
+class Data extends \Magento\Framework\App\Helper\AbstractHelper
+{
+
+    /**
+     * @var \Magento\Framework\App\Request\Http
+     */
+    protected $request;
+
+    /**
+     * @param \Magento\Framework\App\Helper\Context $context
+     * @param \Magento\Framework\App\Request\Http $request
+     */
+    public function __construct(
+        \Magento\Framework\App\Helper\Context $context,
+        \Magento\Framework\App\Request\Http $request
+    ) {
+        $this->request = $request;
+        parent::__construct($context);
+    }
+
+    /**
+     * @return mixed|string
+     */
+    public function getParamStore()
+    {
+        return ($this->request->getParam('store')) ? $this->request->getParam('store') : 'all';
+    }
+}
