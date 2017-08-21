b1e5c5d1a0881d074b29f603009ad829e7319bc0
Oleksandr Iegorov <oiegorov@magento.com>
2017-08-21 16:05:37 +0300

    MAGETWO-69560: [Backport] - Out of stock configurable product displays with zero price - for 2.1

--- app/code/Magento/CatalogInventory/Model/ResourceModel/Product/StockStatusBaseSelectProcessor.php
+++ app/code/Magento/CatalogInventory/Model/ResourceModel/Product/StockStatusBaseSelectProcessor.php
@@ -10,6 +10,7 @@ use Magento\Catalog\Model\ResourceModel\Product\BaseSelectProcessorInterface;
 use Magento\CatalogInventory\Model\Stock;
 use Magento\Framework\App\ResourceConnection;
 use Magento\Framework\DB\Select;
+use Magento\Framework\App\ObjectManager;
 
 /**
  * Class StockStatusBaseSelectProcessor
@@ -22,11 +23,21 @@ class StockStatusBaseSelectProcessor implements BaseSelectProcessorInterface
     private $resource;
 
     /**
-     * @param ResourceConnection $resource
+     * @var \Magento\CatalogInventory\Api\StockConfigurationInterface
      */
-    public function __construct(ResourceConnection $resource)
-    {
+    private $stockConfig;
+
+    /**
+     * @param ResourceConnection $resource
+     * @param \Magento\CatalogInventory\Api\StockConfigurationInterface|null $stockConfig
+     */
+    public function __construct(
+        ResourceConnection $resource,
+        \Magento\CatalogInventory\Api\StockConfigurationInterface $stockConfig = null
+    ) {
         $this->resource = $resource;
+        $this->stockConfig = $stockConfig ?: ObjectManager::getInstance()
+            ->get(\Magento\CatalogInventory\Api\StockConfigurationInterface::class);
     }
 
     /**
@@ -39,13 +50,14 @@ class StockStatusBaseSelectProcessor implements BaseSelectProcessorInterface
     {
         $stockStatusTable = $this->resource->getTableName('cataloginventory_stock_status');
 
-        /** @var Select $select */
-        $select->join(
-            ['stock' => $stockStatusTable],
-            sprintf('stock.product_id = %s.entity_id', BaseSelectProcessorInterface::PRODUCT_TABLE_ALIAS),
-            []
-        )
-            ->where('stock.stock_status = ?', Stock::STOCK_IN_STOCK);
+        if (!$this->stockConfig->isShowOutOfStock()) {
+            /** @var Select $select */
+            $select->join(
+                ['stock' => $stockStatusTable],
+                sprintf('stock.product_id = %s.entity_id', BaseSelectProcessorInterface::PRODUCT_TABLE_ALIAS),
+                []
+            )->where('stock.stock_status = ?', Stock::STOCK_IN_STOCK);
+        }
         return $select;
     }
 }
