d1851abd505edc023f3bf79eb877144f300c1bda
Joan He <johe@magento.com>
2017-08-22 09:21:21 -0500

    MAGETWO-70837: Pricing discrepancy in shopping cart
    - fix JSUNIT test failure

--- app/code/Magento/Checkout/view/frontend/web/js/model/cart/totals-processor/default.js
+++ app/code/Magento/Checkout/view/frontend/web/js/model/cart/totals-processor/default.js
@@ -87,13 +87,12 @@ define([
                 data.shippingCarrierCode = quote.shippingMethod()['carrier_code'];
             }
 
-            var quoteSubtotal = quote.totals().subtotal;
             if (!cartCache.isChanged('cartVersion', customerData.get('cart')()['data_id']) &&
                 !cartCache.isChanged('shippingMethodCode', data.shippingMethodCode) &&
                 !cartCache.isChanged('shippingCarrierCode', data.shippingCarrierCode) &&
                 !cartCache.isChanged('address', address) &&
                 cartCache.get('totals') &&
-                !cartCache.isChanged('subtotal', quoteSubtotal)
+                !cartCache.isChanged('subtotal', quote.totals()['subtotal'])
             ) {
                 quote.setTotals(cartCache.get('totals'));
             } else {
