ca071fa4e4b73a39d89ef42aa46bd29a6cb4de59
Lewis Voncken <lewis@experius.nl>
2017-08-22 13:02:49 +0000

    [TASK] Moved Helper Logic to custom Block Class

---
+++ app/code/Magento/Swagger/Block/Index.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Swagger\Block;
+
+use Magento\Framework\View\Element\Template;
+
+/**
+ * Class Index
+ * @package Magento\Swagger\Block
+ */
+class Index extends Template
+{
+    /**
+     * @return mixed|string
+     */
+    public function getParamStore()
+    {
+        return ($this->getRequest()->getParam('store')) ? $this->getRequest()->getParam('store') : 'all';
+    }
+
+    /**
+     * @return string
+     */
+    public function getSchemaUrl()
+    {
+        return rtrim($this->getBaseUrl(), '/') . '/rest/' . $this->getParamStore() . '/schema?services=all';
+    }
+}
