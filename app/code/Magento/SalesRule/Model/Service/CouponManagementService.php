929103ed5bf7318538374418edcdeacea328d0c1
Krissy Hiserote <khiserote@magento.com>
2017-08-30 11:31:33 -0500

    MAGETWO-69470: Coupon with type 'Auto' can't be generated.
    - use path and not variable to get COUPON_TYPE_AUTO

--- app/code/Magento/SalesRule/Model/Service/CouponManagementService.php
+++ app/code/Magento/SalesRule/Model/Service/CouponManagementService.php
@@ -88,7 +88,7 @@ class CouponManagementService implements \Magento\SalesRule\Api\CouponManagement
                     $couponSpec->getRuleId()
                 );
             }
-            if (!$rule->getUseAutoGeneration() && $rule->getCouponType() != $rule::COUPON_TYPE_AUTO) {
+            if (!$rule->getUseAutoGeneration() && $rule->getCouponType() != \Magento\SalesRule\Model\Rule::COUPON_TYPE_AUTO) {
                 throw new \Magento\Framework\Exception\LocalizedException(
                     __('Specified rule does not allow automatic coupon generation')
                 );
