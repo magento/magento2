72524c49436267da652f6c7176cf1a219e238a4b
Ievgen Shakhsuvarov <ishakhsuvarov@magento.com>
2017-09-07 12:32:51 +0300

    magento/magento2#10521: Set correct Content-Type header for ajax responses in the Magento_UI module

--- app/code/Magento/Ui/Controller/Adminhtml/Index/Render.php
+++ app/code/Magento/Ui/Controller/Adminhtml/Index/Render.php
@@ -9,13 +9,32 @@ use Magento\Backend\App\Action\Context;
 use Magento\Ui\Controller\Adminhtml\AbstractAction;
 use Magento\Framework\View\Element\UiComponentFactory;
 use Magento\Framework\View\Element\UiComponentInterface;
-use Magento\Framework\View\Element\UiComponent\ContentType;
+use Magento\Ui\Model\UiComponentTypeResolver;
 
 /**
  * Class Render
  */
 class Render extends AbstractAction
 {
+    /**
+     * @var \Magento\Ui\Model\UiComponentTypeResolver
+     */
+    private $contentTypeResolver;
+
+    /**
+     * @param Context $context
+     * @param UiComponentFactory $factory
+     * @param UiComponentTypeResolver $contentTypeResolver
+     */
+    public function __construct(
+        Context $context,
+        UiComponentFactory $factory,
+        UiComponentTypeResolver $contentTypeResolver
+    ) {
+        parent::__construct($context, $factory);
+        $this->contentTypeResolver = $contentTypeResolver;
+    }
+
     /**
      * Action for AJAX request
      *
@@ -28,10 +47,12 @@ class Render extends AbstractAction
             return;
         }
 
-        $component = $this->factory->create($this->_request->getParam('namespace'));
+        $component = $this->factory->create($this->getRequest()->getParam('namespace'));
         $this->prepareComponent($component);
-        $this->_response->appendBody((string) $component->render());
-        $this->setResponseContentTypeHeader($component);
+        $this->getResponse()->appendBody((string) $component->render());
+
+        $contentType = $this->contentTypeResolver->resolve($component->getContext());
+        $this->getResponse()->setHeader('Content-Type', $contentType, true);
     }
 
     /**
@@ -46,27 +67,5 @@ class Render extends AbstractAction
             $this->prepareComponent($child);
         }
         $component->prepare();
-    }
-
-    /**
-     * Set the response 'Content-Type' header based on the component's render engine
-     *
-     * @param UiComponentInterface $component
-     * @return void
-     */
-    private function setResponseContentTypeHeader(UiComponentInterface $component)
-    {
-        $contentType = 'text/html';
-
-        if ($component->getContext()) {
-            $renderEngine = $component->getContext()->getRenderEngine();
-            if ($renderEngine instanceof ContentType\Json) {
-                $contentType = 'application/json';
-            } elseif ($renderEngine instanceof ContentType\Xml) {
-                $contentType = 'application/xml';
-            }
-        }
-
-        $this->_response->setHeader('Content-Type', $contentType, true);
     }
 }
