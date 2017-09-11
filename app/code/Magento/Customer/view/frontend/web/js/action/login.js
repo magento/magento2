2992c60b363a5431399821ed2a2cb50941c09556
Rakesh  Jesadiya <jesadiya.rakesh@gmail.com>
2017-09-11 14:52:43 +0530

    Changes Update.

--- app/code/Magento/Customer/view/frontend/web/js/action/login.js
+++ app/code/Magento/Customer/view/frontend/web/js/action/login.js
@@ -37,6 +37,7 @@ define([
                         callback(loginData);
                     });
                     customerData.invalidate(['customer']);
+                    
                     if (response.redirectUrl) {
                         window.location.href = response.redirectUrl;
                     } else if (redirectUrl) {
