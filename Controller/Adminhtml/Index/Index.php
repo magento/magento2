566c7cf4405a11d3227d833424be1821783491d8
Robert He <heyitsroberthe@users.noreply.github.com>
2017-09-12 20:36:49 -0500

    MAGETWO-70580: Merged fixes

--- Controller/Adminhtml/Index/Index.php
+++ Controller/Adminhtml/Index/Index.php
@@ -9,6 +9,13 @@ namespace Magento\AsynchronousOperations\Controller\Adminhtml\Index;
 class Index extends \Magento\Backend\App\Action
 {
     /**
+     * Authorization level of a basic admin session
+     *
+     * @see _isAllowed()
+     */
+    const ADMIN_RESOURCE = 'Magento_Logging::system_magento_logging_bulk_operations';
+
+    /**
      * @var \Magento\Framework\View\Result\PageFactory
      */
     private $resultPageFactory;
@@ -31,7 +38,7 @@ class Index extends \Magento\Backend\App\Action
      */
     protected function _isAllowed()
     {
-        return $this->_authorization->isAllowed('Magento_Logging::system_magento_logging_bulk_operations');
+        return parent::_isAllowed();
     }
 
     /**
