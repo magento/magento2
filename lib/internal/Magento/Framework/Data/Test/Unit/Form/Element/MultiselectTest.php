b28071fda0886013652391421ad9844755a1a367
Oleksandr Miroshnichenko <omiroshnichenko@magento.com>
2017-09-15 17:39:58 +0300

    MAGETWO-72353: [2.2.x] [Magento Cloud] Unable to remove custom attributes from the "Forms to Use in"

--- lib/internal/Magento/Framework/Data/Test/Unit/Form/Element/MultiselectTest.php
+++ lib/internal/Magento/Framework/Data/Test/Unit/Form/Element/MultiselectTest.php
@@ -34,29 +34,29 @@ class MultiselectTest extends \PHPUnit\Framework\TestCase
     }
 
     /**
-    * Verify that hidden input is present in multiselect and it allow indicate is multiselect is disabled.
-    */
+     * Verify that hidden input is present in multiselect and it allow indicate is multiselect is disabled.
+     */
     public function testHiddenDisabledFieldPresentInMultiSelect()
     {
         $fieldName = 'fieldName';
- 	 	$this->_model->setDisabled(true);
- 	 	$this->_model->setName($fieldName);
- 	 	$elementHtml = $this->_model->getElementHtml();
- 	 	$this->assertContains('<input type="hidden" name="' . $fieldName . '_disabled"', $elementHtml);
+        $this->_model->setDisabled(true);
+        $this->_model->setName($fieldName);
+        $elementHtml = $this->_model->getElementHtml();
+        $this->assertContains('<input type="hidden" name="' . $fieldName . '_disabled"', $elementHtml);
     }
 
     /**
-    * Verify that hidden input doesn't present in multiselect and it allow indicate is multiselect is disabled.
-    *
-    * @covers \Magento\Framework\Data\Form\Element\Multiselect::getElementHtml
-    */
+     * Verify that hidden input doesn't present in multiselect and it allow indicate is multiselect is disabled.
+     *
+     * @covers \Magento\Framework\Data\Form\Element\Multiselect::getElementHtml
+     */
     public function testHiddenDisabledFieldNotPresentInMultiSelect()
     {
         $fieldName = 'fieldName';
- 	 	$this->_model->setDisabled(false);
- 	 	$this->_model->setName($fieldName);
- 	 	$elementHtml = $this->_model->getElementHtml();
- 	 	$this->assertNotContains('<input type="hidden" name="' . $fieldName . '_disabled"', $elementHtml);
+        $this->_model->setDisabled(false);
+        $this->_model->setName($fieldName);
+        $elementHtml = $this->_model->getElementHtml();
+        $this->assertNotContains('<input type="hidden" name="' . $fieldName . '_disabled"', $elementHtml);
     }
 
     /**
