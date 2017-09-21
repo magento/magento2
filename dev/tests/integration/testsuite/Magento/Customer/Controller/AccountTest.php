07e25680d80831a30bc0fa942d961905d594d8e6
Sviatoslav Mankivskyi <smankivskyi@magento.com>
2017-09-21 17:08:32 +0300

    MAGETWO-75635: Merge 2.2.0-develop into develop

--- dev/tests/integration/testsuite/Magento/Customer/Controller/AccountTest.php
+++ dev/tests/integration/testsuite/Magento/Customer/Controller/AccountTest.php
@@ -286,7 +286,7 @@ class AccountTest extends \Magento\TestFramework\TestCase\AbstractController
             $this->equalTo([
                 'You must confirm your account. Please check your email for the confirmation link or '
                     . '<a href="http://localhost/index.php/customer/account/confirmation/'
-                    . '?email=test2%40email.com/">click here</a> for a new link.'
+                    . '?email=test2%40email.com">click here</a> for a new link.'
             ]),
             MessageInterface::TYPE_SUCCESS
         );
