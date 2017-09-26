487e216cf956e2d0a371653de60020833229450a
BlackEagle <ike.devolder@gmail.com>
2017-09-26 09:50:22 +0200

    don't add error on setup:cron:run when composer.json is missing
    In BasePackageInfo ther is a check to add
    magento/magento2-base/compser.json, but usually this file is not
    available. When used in scripting the error is added and causes a non 0
    result. To avoid this, remove the "error" because its no acutal failure.
    
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

--- setup/src/Magento/Setup/Model/Cron/ReadinessCheck.php
+++ setup/src/Magento/Setup/Model/Cron/ReadinessCheck.php
@@ -145,7 +145,6 @@ class ReadinessCheck
         } catch (\Exception $e) {
             $errorMessage = $e->getMessage();
             $resultJsonRawData[self::KEY_FILE_PATHS][self::KEY_LIST] = [];
-            $errorLogMessages[] = $errorMessage;
         }
         $resultJsonRawData[self::KEY_FILE_PATHS][self::KEY_ERROR] = $errorMessage;
 
