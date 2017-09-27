5f8fab57afbdcaee6930e68b88813df724570fb9
Iryna Lagno <ilagno@magento.com>
2017-09-27 11:47:01 +0300

    MAGETWO-75456: [Backport] - magento/magento2 #9196 - Products ordered report doesn't show simple (child) products of configurable products #9908 - for 2.2

--- dev/tests/integration/testsuite/Magento/Reports/Controller/Adminhtml/Report/Product/SoldTest.php
+++ dev/tests/integration/testsuite/Magento/Reports/Controller/Adminhtml/Report/Product/SoldTest.php
@@ -11,7 +11,7 @@ namespace Magento\Reports\Controller\Adminhtml\Report\Product;
  */
 class SoldTest extends \Magento\TestFramework\TestCase\AbstractBackendController
 {
-    public function testExecute2()
+    public function testExecute()
     {
         $this->dispatch('backend/reports/report_product/sold');
         $actual = $this->getResponse()->getBody();
