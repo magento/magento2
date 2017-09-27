0253aecce5261c0543b203c727ba28a574caf70a
Fabian Schmengler <fs@integer-net.de>
2017-09-27 20:57:36 +0200

    Distinguish between non existing SKU and missing SKU in API test

--- dev/tests/api-functional/testsuite/Magento/Catalog/Api/_files/product_options_update_negative.php
+++ dev/tests/api-functional/testsuite/Magento/Catalog/Api/_files/product_options_update_negative.php
@@ -5,18 +5,29 @@
  */
 
 return [
-    'missed_product_sku' =>
+    'missing_product_sku' => [
         [
-            [
-                'title' => 'title',
-                'type' => 'field',
-                'sort_order' => 1,
-                'is_require' => 1,
-                'price' => 10.0,
-                'price_type' => 'fixed',
-                'sku' => 'sku1',
-                'max_characters' => 10,
-            ],
-            'ProductSku should be specified',
-        ]
+            'title'          => 'title',
+            'type'           => 'field',
+            'sort_order'     => 1,
+            'is_require'     => 1,
+            'price'          => 10.0,
+            'price_type'     => 'fixed',
+            'max_characters' => 10,
+        ],
+        'ProductSku should be specified',
+    ],
+    'invalid_product_sku' => [
+        [
+            'title'          => 'title',
+            'type'           => 'field',
+            'sort_order'     => 1,
+            'is_require'     => 1,
+            'price'          => 10.0,
+            'price_type'     => 'fixed',
+            'sku'            => 'sku1',
+            'max_characters' => 10,
+        ],
+        'Requested product doesn\'t exist',
+    ],
 ];
