e9938651eeda25bad20ffd662b785b9eace10be6
Bartłomiej Szymański <bartlomiej.szymanski@bold.net.pl>
2017-09-29 00:10:16 +0200

    default stock install

---
+++ app/code/Magento/InventoryCatalog/Setup/InstallData.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\InventoryCatalog\Setup;
+
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\InventoryApi\Api\Data\StockInterfaceFactory;
+use Magento\InventoryApi\Api\Data\StockInterface;
+use Magento\InventoryApi\Api\StockRepositoryInterface;
+use Magento\Framework\Api\DataObjectHelper;
+
+/**
+ * Class InstallData
+ */
+class InstallData implements InstallDataInterface
+{
+    /**
+     * @var StockRepositoryInterface
+     */
+    private $stockRepository;
+
+    /**
+     * @var StockInterfaceFactory
+     */
+    private $stockFactory;
+
+    /**
+     * @var DataObjectHelper
+     */
+    private $dataObjectHelper;
+
+    /**
+     * @param StockRepositoryInterface $stockRepository
+     * @param StockInterfaceFactory $stockFactory
+     * @param DataObjectHelper $dataObjectHelper
+     */
+    public function __construct(
+        StockRepositoryInterface $stockRepository,
+        StockInterfaceFactory $stockFactory,
+        DataObjectHelper $dataObjectHelper
+    ) {
+        $this->stockRepository = $stockRepository;
+        $this->stockFactory = $stockFactory;
+        $this->dataObjectHelper = $dataObjectHelper;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $this->addDefaultStock();
+    }
+
+    /**
+     * Add default stock
+     *
+     * @return void
+     */
+    private function addDefaultStock()
+    {
+        $data = [
+            StockInterface::STOCK_ID => 1,
+            StockInterface::NAME => 'Default Stock'
+        ];
+        $source = $this->stockFactory->create();
+        $this->dataObjectHelper->populateWithArray($source, $data, StockInterface::class);
+        $this->stockRepository->save($source);
+    }
+}
