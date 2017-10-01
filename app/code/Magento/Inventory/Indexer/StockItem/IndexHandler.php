5b0be003671b468b720e37bb076a49a38d0310b1
Igor Miniailo <iminiailo@magento.com>
2017-10-01 13:22:49 +0300

    [task] #91. Fix IndexHandler Contract in the Implementation

--- app/code/Magento/Inventory/Indexer/StockItem/IndexHandler.php
+++ app/code/Magento/Inventory/Indexer/StockItem/IndexHandler.php
@@ -58,22 +58,21 @@ class IndexHandler implements IndexHandlerInterface
     /**
      * @inheritdoc
      */
-    public function saveIndex(IndexName $indexName, \Traversable $documents)
+    public function saveIndex(IndexName $indexName, \Traversable $documents, string $connectionName)
     {
+        $connection = $this->resourceConnection->getConnection($connectionName);
         $tableName = $this->indexNameResolver->resolveName($indexName);
 
         $columns = [IndexStructure::SKU, IndexStructure::QUANTITY];
         foreach ($this->batch->getItems($documents, $this->batchSize) as $batchDocuments) {
-            $this->resourceConnection
-                ->getConnection()
-                ->insertArray($tableName, $columns, $batchDocuments);
+            $connection->insertArray($tableName, $columns, $batchDocuments);
         }
     }
 
     /**
      * @inheritdoc
      */
-    public function cleanIndex(IndexName $indexName, \Traversable $documents, $connectionName)
+    public function cleanIndex(IndexName $indexName, \Traversable $documents, string $connectionName)
     {
         $connection = $this->resourceConnection->getConnection($connectionName);
         $tableName = $this->indexNameResolver->resolveName($indexName);
