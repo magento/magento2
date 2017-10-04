1543f06388f6551f9c0441a221e0d564ac49477d
rossbrandon <rbrandon@magento.com>
2017-10-04 13:38:02 -0500

    MAGETWO-80474: Update Advanced Reporting Configuration Page
    (cherry picked from commit 25202a0)

---
+++ app/code/Magento/Analytics/Block/Adminhtml/System/Config/Vertical.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Analytics\Block\Adminhtml\System\Config;
+
+/**
+ * Provides select with industry information
+ * @since 2.2.2
+ */
+class Vertical extends \Magento\Config\Block\System\Config\Form\Field
+{
+    /**
+     * @param \Magento\Framework\Data\Form\Element\AbstractElement $element
+     * @return string
+     * @since 2.2.2
+     */
+    public function render(\Magento\Framework\Data\Form\Element\AbstractElement $element)
+    {
+        $html = '<div class="config-vertical-title">' . $element->getHint() . '</div>';
+        $html .= '<div class="config-vertical-comment">' . $element->getComment() . '</div>';
+        return $this->decorateRowHtml($element, $html);
+    }
+
+    /**
+     * @param \Magento\Framework\Data\Form\Element\AbstractElement $element
+     * @param string $html
+     * @return string
+     * @since 2.2.2
+     */
+    private function decorateRowHtml(\Magento\Framework\Data\Form\Element\AbstractElement $element, $html)
+    {
+        $rowHtml = sprintf('<tr><td colspan="4">%s</td></tr>', $html);
+        $rowHtml .= sprintf(
+            '<tr id="row_%s"><td class="label config-vertical-label">%s</td><td class="value">%s</td></tr>',
+            $element->getHtmlId(),
+            $element->getLabelHtml($element->getHtmlId(), "[WEBSITE]"),
+            $element->getElementHtml()
+        );
+        return $rowHtml;
+    }
+}
