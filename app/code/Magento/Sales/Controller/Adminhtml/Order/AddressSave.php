23efa6014ea60cc6ebab11532a84eca26fe9917b
Raúl Mateos <raumatbel@gmail.com>
2017-10-04 23:27:32 +0200

    FR#StateNotDisplayedSalesAdminAddress Save region correctly to save sales address from admin

--- app/code/Magento/Sales/Controller/Adminhtml/Order/AddressSave.php
+++ app/code/Magento/Sales/Controller/Adminhtml/Order/AddressSave.php
@@ -4,9 +4,18 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+
 namespace Magento\Sales\Controller\Adminhtml\Order;
 
-class AddressSave extends \Magento\Sales\Controller\Adminhtml\Order
+use Magento\Sales\Controller\Adminhtml\Order;
+use Magento\Backend\App\Action;
+use Magento\Sales\Api\OrderManagementInterface;
+use Magento\Sales\Api\OrderRepositoryInterface;
+use Magento\Directory\Model\RegionFactory;
+use Psr\Log\LoggerInterface;
+use Magento\Framework;
+
+class AddressSave extends Order
 {
     /**
      * Authorization level of a basic admin session
@@ -14,6 +23,57 @@ class AddressSave extends \Magento\Sales\Controller\Adminhtml\Order
      * @see _isAllowed()
      */
     const ADMIN_RESOURCE = 'Magento_Sales::actions_edit';
+
+    /**
+     * @var RegionFactory
+     */
+    private $regionFactory;
+
+    /**
+     * @param Action\Context $context
+     * @param Framework\Registry $coreRegistry
+     * @param Framework\App\Response\Http\FileFactory $fileFactory
+     * @param Framework\Translate\InlineInterface $translateInline
+     * @param Framework\View\Result\PageFactory $resultPageFactory
+     * @param Framework\Controller\Result\JsonFactory $resultJsonFactory
+     * @param Framework\View\Result\LayoutFactory $resultLayoutFactory
+     * @param Framework\Controller\Result\RawFactory $resultRawFactory
+     * @param OrderManagementInterface $orderManagement
+     * @param OrderRepositoryInterface $orderRepository
+     * @param LoggerInterface $logger
+     * @param RegionFactory $regionFactory
+     *
+     * @SuppressWarnings(PHPMD.ExcessiveParameterList)
+     * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+     */
+    public function __construct(
+        Action\Context $context,
+        Framework\Registry $coreRegistry,
+        Framework\App\Response\Http\FileFactory $fileFactory,
+        Framework\Translate\InlineInterface $translateInline,
+        Framework\View\Result\PageFactory $resultPageFactory,
+        Framework\Controller\Result\JsonFactory $resultJsonFactory,
+        Framework\View\Result\LayoutFactory $resultLayoutFactory,
+        Framework\Controller\Result\RawFactory $resultRawFactory,
+        OrderManagementInterface $orderManagement,
+        OrderRepositoryInterface $orderRepository,
+        LoggerInterface $logger,
+        RegionFactory $regionFactory
+    ) {
+        $this->_coreRegistry = $coreRegistry;
+        $this->_fileFactory = $fileFactory;
+        $this->_translateInline = $translateInline;
+        $this->resultPageFactory = $resultPageFactory;
+        $this->resultJsonFactory = $resultJsonFactory;
+        $this->resultLayoutFactory = $resultLayoutFactory;
+        $this->resultRawFactory = $resultRawFactory;
+        $this->orderManagement = $orderManagement;
+        $this->orderRepository = $orderRepository;
+        $this->logger = $logger;
+        $this->regionFactory = $regionFactory;
+        parent::__construct($context, $coreRegistry, $fileFactory, $translateInline, $resultPageFactory,
+            $resultJsonFactory, $resultLayoutFactory, $resultRawFactory, $orderManagement, $orderRepository, $logger);
+    }
 
     /**
      * Save order address
@@ -28,6 +88,7 @@ class AddressSave extends \Magento\Sales\Controller\Adminhtml\Order
             \Magento\Sales\Api\Data\OrderAddressInterface::class
         )->load($addressId);
         $data = $this->getRequest()->getPostValue();
+        $this->updateRegionData($data);
         $resultRedirect = $this->resultRedirectFactory->create();
         if ($data && $address->getId()) {
             $address->addData($data);
@@ -49,6 +110,22 @@ class AddressSave extends \Magento\Sales\Controller\Adminhtml\Order
             return $resultRedirect->setPath('sales/*/address', ['address_id' => $address->getId()]);
         } else {
             return $resultRedirect->setPath('sales/*/');
+        }
+    }
+    
+    /**
+     * Update region data
+     *
+     * @param array $attributeValues
+     * @return void
+     * @SuppressWarnings(PHPMD.NPathComplexity)
+     */
+    protected function updateRegionData(&$attributeValues)
+    {
+        if (!empty($attributeValues['region_id'])) {
+            $newRegion = $this->regionFactory->create()->load($attributeValues['region_id']);
+            $attributeValues['region_code'] = $newRegion->getCode();
+            $attributeValues['region'] = $newRegion->getDefaultName();
         }
     }
 }
