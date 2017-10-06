4cd338313b5bc78baba425748239a10b50624cf3
Franciszek Wawrzak <frank@wawrzak.com>
2017-10-06 10:37:12 +0200

    fix adminhtml file attribute edit form
    fixes #11252
    
    not sure why this line was introduced as the only place this function is used seems to be here:
    https://github.com/magento/magento2/blob/55e9a5cc9d88ef785f39217e96da02b1cdf3e247/app/code/Magento/Customer/Model/FileUploader.php#L110
    and it uses 'path' key right after call.

--- app/code/Magento/Customer/Model/FileProcessor.php
+++ app/code/Magento/Customer/Model/FileProcessor.php
@@ -181,7 +181,6 @@ class FileProcessor
         );
 
         $result = $uploader->save($path);
-        unset($result['path']);
         if (!$result) {
             throw new \Magento\Framework\Exception\LocalizedException(
                 __('File can not be saved to the destination folder.')
