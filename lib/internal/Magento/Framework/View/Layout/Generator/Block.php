2402a7945e8ca6b33b7a0ec6a3a2e4195d14ca0c
Cristian Partica <cpartica@magento.com>
2017-10-06 21:32:25 -0500

    MAGETWO-81188: Make block class not mandatory
    - fix static test

--- lib/internal/Magento/Framework/View/Layout/Generator/Block.php
+++ lib/internal/Magento/Framework/View/Layout/Generator/Block.php
@@ -221,7 +221,8 @@ class Block implements Layout\GeneratorInterface
         }
 
         // create block
-        $className = isset($attributes['class']) && !empty($attributes['class']) ? $attributes['class'] : $this->defaultClass ;
+        $className = isset($attributes['class']) && !empty($attributes['class']) ?
+            $attributes['class'] : $this->defaultClass ;
         $block = $this->createBlock($className, $elementName, [
             'data' => $this->evaluateArguments($data['arguments'])
         ]);
