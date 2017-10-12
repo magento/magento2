a854267c578358adf7b1bcd9d84e57c371e37b47
Lorenzo Stramaccia <lorenzo.stramaccia@magespecialist.it>
2017-10-12 10:33:09 +0200

    Fix coding standards

--- lib/internal/Magento/Framework/View/Page/Config/Renderer.php
+++ lib/internal/Magento/Framework/View/Page/Config/Renderer.php
@@ -136,7 +136,7 @@ class Renderer implements RendererInterface
     protected function processMetadataContent($name, $content)
     {
         $method = 'get' . $this->string->upperCaseWords($name, '_', '');
-        if($name === 'title') {
+        if ($name === 'title') {
             if (!$content) {
                 $content = $this->escaper->escapeHtml($this->pageConfig->$method()->get());
             }
