71df22bbeb85bc8ae3c4d3ec682653c1bb54bf4c
Oleksii Korshenko <okorshenko@magento.com>
2017-10-16 11:50:08 -0400

    MAGETWO-81968: Show product alerts in admin product detail [backport 2.1] #11448
     - fixed copyright

--- app/code/Magento/Catalog/Ui/DataProvider/Product/Form/Modifier/Alerts.php
+++ app/code/Magento/Catalog/Ui/DataProvider/Product/Form/Modifier/Alerts.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright © Magento, Inc. All rights reserved.
+ * Copyright © 2013-2017 Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
 
@@ -38,7 +38,7 @@ class Alerts extends AbstractModifier
      * @var LayoutFactory
      */
     private $layoutFactory;
-    
+
     /**
      * Alerts constructor.
      * @param ScopeConfigInterface $scopeConfig
