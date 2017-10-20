9fa134a6b44a63066b8c7fc8d9fc9f5891a859c1
Ruslan Kostiv <rkostiv@magento.com>
2017-10-20 17:16:31 +0300

    MAGETWO-70498: [Magento Cloud] - /rest/default/V1/order/<order id>/ship and configurable products - for 2.1

--- app/code/Magento/Sales/Model/Order/ShipmentFactory.php
+++ app/code/Magento/Sales/Model/Order/ShipmentFactory.php
@@ -92,9 +92,6 @@ class ShipmentFactory
 
             /** @var \Magento\Sales\Model\Order\Shipment\Item $item */
             $item = $this->converter->itemToShipmentItem($orderItem);
-            if ($orderItem->getIsVirtual() || $orderItem->getParentItemId()) {
-                $item->isDeleted(true);
-            }
             if ($orderItem->isDummy(true)) {
                 $qty = 0;
 
@@ -129,6 +126,9 @@ class ShipmentFactory
                 } else {
                     continue;
                 }
+            }
+            if ($orderItem->getIsVirtual() || $orderItem->getParentItemId()) {
+                $item->isDeleted(true);
             }
 
             $totalQty += $qty;
