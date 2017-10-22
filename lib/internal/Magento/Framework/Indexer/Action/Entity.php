74ba88e6f7d071a20ea4f1d57ceb056a385ad165
Adrián Martínez <adrian.martinez@interactiv4.com>
2017-10-23 01:37:52 +0200

    Fixed case typo in \Magento\Framework\Indexer\Action\Base::prepareDataSource method, to match the interface name : getIdFieldname => getIdFieldName PHPCS fixes
    Removed \Magento\Framework\Indexer\Action\Entity::prepareDataSource, overriding parent method just to match the interface name :
    getIdFieldname => getIdFieldName
    PHPCS Fixes

--- lib/internal/Magento/Framework/Indexer/Action/Entity.php
+++ lib/internal/Magento/Framework/Indexer/Action/Entity.php
@@ -5,25 +5,10 @@
  */
 namespace Magento\Framework\Indexer\Action;
 
-use Magento\Framework\App\ResourceConnection\SourceProviderInterface;
-
 class Entity extends Base
 {
     /**
      * @var string
      */
     protected $tableAlias = 'e';
-
-    /**
-     * Prepare select query
-     *
-     * @param array|int|null $ids
-     * @return SourceProviderInterface
-     */
-    protected function prepareDataSource(array $ids = [])
-    {
-        return !count($ids)
-            ? $this->createResultCollection()
-            : $this->createResultCollection()->addFieldToFilter($this->getPrimaryResource()->getIdFieldName(), $ids);
-    }
 }
