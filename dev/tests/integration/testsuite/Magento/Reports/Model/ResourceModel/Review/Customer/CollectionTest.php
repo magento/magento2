306b9c835b30433d3fe77fab41fc05d2553fa6b9
Ievgen Shakhsuvarov <ishakhsuvarov@magento.com>
2017-10-24 17:26:54 +0300

    magento/magento2#11522: Fix Filter Customer Report Review  - Integration test updated

--- dev/tests/integration/testsuite/Magento/Reports/Model/ResourceModel/Review/Customer/CollectionTest.php
+++ dev/tests/integration/testsuite/Magento/Reports/Model/ResourceModel/Review/Customer/CollectionTest.php
@@ -3,7 +3,7 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\Reports\Model\ResourceModel\Review\Product;
+namespace Magento\Reports\Model\ResourceModel\Review\Customer;
 
 /**
  * @magentoAppArea adminhtml
