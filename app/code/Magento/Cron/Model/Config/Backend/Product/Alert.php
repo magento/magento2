6a320f761f842cc7e6a950106f8d37cda1e9227e
Marc Rodriguez <marc.rodriguez@interactiv4.com>
2017-10-25 18:38:51 +0200

    FR#ProductAlertsCron_22 Magento Product Alerts cron is not generated by schedule

--- app/code/Magento/Cron/Model/Config/Backend/Product/Alert.php
+++ app/code/Magento/Cron/Model/Config/Backend/Product/Alert.php
@@ -79,7 +79,7 @@ class Alert extends \Magento\Framework\App\Config\Value
             $frequency == \Magento\Cron\Model\Config\Source\Frequency::CRON_WEEKLY ? '1' : '*', //Day of the Week
         ];
 
-        $cronExprString = join(' ', $cronExprArray);
+        $cronExprString = implode(' ', $cronExprArray);
 
         try {
             $cronStringPath = $this->_configValueFactory->create()->load(self::CRON_STRING_PATH, 'path');
