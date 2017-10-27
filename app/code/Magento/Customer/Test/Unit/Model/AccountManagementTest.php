4f7fb74f3413e15ef3a3e4e44288861fbd3198e6
Ievgen Shakhsuvarov <ishakhsuvarov@magento.com>
2017-10-27 17:19:07 +0300

    MAGETWO-82854: [Backport 2.2-develop] Fix AccountManagementTest test  fail randomly magento/magento2#11607

--- app/code/Magento/Customer/Test/Unit/Model/AccountManagementTest.php
+++ app/code/Magento/Customer/Test/Unit/Model/AccountManagementTest.php
@@ -1606,6 +1606,8 @@ class AccountManagementTest extends \PHPUnit\Framework\TestCase
         $storeId = 1;
         $hash = '4nj54lkj5jfi03j49f8bgujfgsd';
 
+        $this->prepareDateTimeFactory();
+
         //Handle store
         $store = $this->getMockBuilder(\Magento\Store\Model\Store::class)->disableOriginalConstructor()->getMock();
         $store->expects($this->any())
