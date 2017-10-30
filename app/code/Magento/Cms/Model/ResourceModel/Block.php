d3aa3f8f7ad9d43535d63dee0000e768161d0457
Thiago Lima <thiagolimaufrj@gmail.com>
2017-10-30 17:40:12 +0100

    Changed Validation to get store for block #11601

--- app/code/Magento/Cms/Model/ResourceModel/Block.php
+++ app/code/Magento/Cms/Model/ResourceModel/Block.php
@@ -183,7 +183,7 @@ class Block extends AbstractDb
         $entityMetadata = $this->metadataPool->getMetadata(BlockInterface::class);
         $linkField = $entityMetadata->getLinkField();
 
-        if ($this->_storeManager->hasSingleStore()) {
+        if ($this->_storeManager->isSingleStoreMode()) {
             $stores = [Store::DEFAULT_STORE_ID];
         } else {
             $stores = (array)$object->getData('store_id');
@@ -230,7 +230,7 @@ class Block extends AbstractDb
                 'cbs.' . $linkField . ' = cb.' . $linkField,
                 []
             )
-            ->where('cb.' . $entityMetadata->getIdentifierField()  . ' = :block_id');
+            ->where('cb.' . $entityMetadata->getIdentifierField() . ' = :block_id');
 
         return $connection->fetchCol($select, ['block_id' => (int)$id]);
     }
