b9dbba531f7649e96fe5302d3d874d7a30bf9032
Enrique Guadalupe <enrique.guadalupe@interactiv4.com>
2017-11-05 15:19:20 +0100

    add integration test to check in the new sources the stock

--- app/code/Magento/InventoryCatalog/Test/Integration/UpdateLegacyCatalogInventoryPluginTest.php
+++ app/code/Magento/InventoryCatalog/Test/Integration/UpdateLegacyCatalogInventoryPluginTest.php
@@ -15,6 +15,9 @@ use Magento\CatalogInventory\Api\StockItemCriteriaInterface;
 use Magento\CatalogInventory\Api\StockItemCriteriaInterfaceFactory;
 use Magento\CatalogInventory\Api\Data\StockItemCollectionInterface;
 use Magento\CatalogInventory\Api\Data\StockItemInterface;
+use Magento\InventoryApi\Api\GetProductQuantityInStockInterface;
+use Magento\Indexer\Model\Indexer;
+use Magento\Inventory\Indexer\StockItemIndexerInterface;
 
 class UpdateLegacyCatalogInventoryPluginTest extends TestCase
 {
@@ -27,14 +30,27 @@ class UpdateLegacyCatalogInventoryPluginTest extends TestCase
      * @var ReservationsAppendInterface
      */
     private $reservationsAppend;
+
     /**
      * @var StockItemRepositoryInterface
      */
     private $oldStockItemRepository;
+
     /**
      * @var StockItemCriteriaInterfaceFactory
      */
     private $stockItemCriteriaFactory;
+
+    /**
+     * @var GetProductQuantityInStockInterface
+     */
+    private $getProductQtyInStock;
+
+    /**
+     * @var IndexerInterface
+     */
+    private $indexer;
+
 
     protected function setUp()
     {
@@ -42,6 +58,10 @@ class UpdateLegacyCatalogInventoryPluginTest extends TestCase
         $this->reservationsAppend = Bootstrap::getObjectManager()->get(ReservationsAppendInterface::class);
         $this->oldStockItemRepository = Bootstrap::getObjectManager()->get(StockItemRepositoryInterface::class);
         $this->stockItemCriteriaFactory = Bootstrap::getObjectManager()->get(StockItemCriteriaInterfaceFactory::class);
+
+        $this->indexer = Bootstrap::getObjectManager()->get(Indexer::class);
+        $this->indexer->load(StockItemIndexerInterface::INDEXER_ID);
+        $this->getProductQtyInStock = Bootstrap::getObjectManager()->get(GetProductQuantityInStockInterface::class);
     }
 
     /**
@@ -54,6 +74,7 @@ class UpdateLegacyCatalogInventoryPluginTest extends TestCase
      */
     public function testUpdateStockItemTable()
     {
+        $reservationQuantity = -5;
 
         /** @var StockItemCriteriaInterface  $criteria */
         $criteria = $this->stockItemCriteriaFactory->create();
@@ -66,7 +87,7 @@ class UpdateLegacyCatalogInventoryPluginTest extends TestCase
         $initialQuantity = $oldStockItem->getQty();
 
         $this->reservationsAppend->execute([
-            $this->reservationBuilder->setStockId(1)->setSku('simple')->setQuantity(-5)->build()
+            $this->reservationBuilder->setStockId(1)->setSku('simple')->setQuantity($reservationQuantity)->build()
         ]);
 
         /** @var StockItemCollectionInterface $collectionAfterChange */
@@ -74,6 +95,46 @@ class UpdateLegacyCatalogInventoryPluginTest extends TestCase
         $oldStockItem = current($collectionAfterChange->getItems());
         $quantityAfterCheck = $oldStockItem->getQty();
 
-        $this->assertEquals(5, $initialQuantity - $quantityAfterCheck);
+        $this->assertEquals($initialQuantity + $reservationQuantity, $quantityAfterCheck);
+    }
+
+    /**
+     * @magentoDataFixture ../../../../app/code/Magento/InventoryApi/Test/_files/products.php
+     * @magentoDataFixture ../../../../app/code/Magento/InventoryApi/Test/_files/sources.php
+     * @magentoDataFixture ../../../../app/code/Magento/InventoryApi/Test/_files/stocks.php
+     * @magentoDataFixture ../../../../app/code/Magento/InventoryApi/Test/_files/source_items.php
+     * @magentoDataFixture ../../../../app/code/Magento/InventoryApi/Test/_files/stock_source_link.php
+     * @magentoDataFixture ../../../../app/code/Magento/InventoryApi/Test/_files/products_simple.php
+     */
+    public function testThatReservationPlacedUpdatesBothOldAndNewStocks()
+    {
+        $this->markTestSkipped('finish later');
+        $reservationQuantity = -5;
+
+        $this->indexer->reindexAll();
+        $this->assertEquals(8.5, $this->getProductQtyInStock->execute('SKU-1', 10));
+
+        /** @var StockItemCriteriaInterface  $criteria */
+        $criteria = $this->stockItemCriteriaFactory->create();
+        $criteria->setProductsFilter([3]);
+
+        /** @var StockItemCollectionInterface $collectionBeforeChange */
+        $collectionBeforeChange = $this->oldStockItemRepository->getList($criteria);
+        /** @var StockItemInterface $oldStockItem */
+        $oldStockItem = current($collectionBeforeChange->getItems());
+        $initialQuantity = $oldStockItem->getQty();
+        $this->assertEquals(8.5, $initialQuantity);
+
+        $this->reservationsAppend->execute([
+            $this->reservationBuilder->setStockId(1)->setSku('SKU-1')->setQuantity($reservationQuantity)->build()
+        ]);
+
+        /** @var StockItemCollectionInterface $collectionAfterChange */
+        $collectionAfterChange = $this->oldStockItemRepository->getList($criteria);
+        $oldStockItem = current($collectionAfterChange->getItems());
+        $quantityAfterCheck = $oldStockItem->getQty();
+
+
+        $this->assertEquals($this->getProductQtyInStock->execute('SKU-1', 10), $quantityAfterCheck);
     }
 }
