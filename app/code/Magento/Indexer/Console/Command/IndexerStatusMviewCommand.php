90215616b1d6c3679729dd5a92de5c245b2f15aa
Luke Rodgers <lukerodgers90@gmail.com>
2017-11-05 20:51:31 +0000

    Add command to view mview state and queue
    This is similar to the magerun1 command here: https://github.com/netz98/n98-magerun/pull/891
    
    I like the ability to view the mview queue in realtime as its being processed, it can be quite helpful when debugging indexing issues.
    
    This command will actually show how many items are in the list pending processing, as well information from the `mview_state` table.
    
    ```
    php bin/magento indexer:status:mview
    +---------------------------+----------+--------+---------------------+------------+---------+
    | ID                        | Mode     | Status | Updated             | Version ID | Backlog |
    +---------------------------+----------+--------+---------------------+------------+---------+
    | catalog_category_product  | enabled  | idle   | 2017-11-02 10:00:00 | 1          | 0       |
    | catalog_product_attribute | enabled  | idle   | 2017-11-02 10:00:00 | 1          | 1       |
    | catalog_product_category  | disabled | idle   | 2017-11-02 10:00:00 | 1          | 0       |
    | catalog_product_price     | enabled  | idle   | 2017-11-02 10:00:00 | 1          | 0       |
    +---------------------------+----------+--------+---------------------+------------+---------+
    ```
    
    I'll point this PR into 2.1.x and raise a separate PR to pop it into 2.2.x.

---
+++ app/code/Magento/Indexer/Console/Command/IndexerStatusMviewCommand.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Copyright © 2013-2017 Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Indexer\Console\Command;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Command\Command;
+use Magento\Framework\Mview\View;
+
+/**
+ * Command for displaying status of mview indexers.
+ */
+class IndexerStatusMviewCommand extends Command
+{
+    /** @var \Magento\Framework\Mview\View\CollectionInterface $mviewIndexersCollection */
+    private $mviewIndexersCollection;
+
+    public function __construct(
+        \Magento\Framework\Mview\View\CollectionInterface $collection
+    ) {
+        $this->mviewIndexersCollection = $collection;
+        parent::__construct();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this->setName('indexer:status:mview')
+            ->setDescription('Shows status of Mview Indexers and their queue status');
+
+        parent::configure();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        try {
+            $table = $this->getHelperSet()->get('table');
+            $table->setHeaders(['ID', 'Mode', 'Status', 'Updated', 'Version ID', 'Backlog']);
+
+            $rows = [];
+
+            /** @var \Magento\Framework\Mview\View $indexer */
+            foreach ($this->mviewIndexersCollection as $indexer) {
+                $state = $indexer->getState();
+                $changelog = $indexer->getChangelog();
+
+                try {
+                    $currentVersionId = $changelog->getVersion();
+                } catch (View\ChangelogTableNotExistsException $e) {
+                    continue;
+                }
+
+                $pendingCount = count($changelog->getList($state->getVersionId(), $currentVersionId));
+
+                $pendingString = "<error>$pendingCount</error>";
+                if ($pendingCount <= 0) {
+                    $pendingString = "<info>$pendingCount</info>";
+                }
+
+                $rows[] = [
+                    $indexer->getData('view_id'),
+                    $state->getData('mode'),
+                    $state->getData('status'),
+                    $state->getData('updated'),
+                    $state->getData('version_id'),
+                    $pendingString,
+                ];
+            }
+
+            usort($rows, function($a, $b) {
+                return $a[0] <=> $b[0];
+            });
+
+            $table->addRows($rows);
+            $table->render($output);
+
+            return \Magento\Framework\Console\Cli::RETURN_SUCCESS;
+        } catch (\Exception $e) {
+            $output->writeln('<error>' . $e->getMessage() . '</error>');
+            if ($output->getVerbosity() >= OutputInterface::VERBOSITY_VERBOSE) {
+                $output->writeln($e->getTraceAsString());
+            }
+
+            return \Magento\Framework\Console\Cli::RETURN_FAILURE;
+        }
+    }
+}
