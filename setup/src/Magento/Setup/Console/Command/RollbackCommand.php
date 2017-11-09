23de490d8b707afc5567a9acc95fbedea31963b6
David Manners <dmanners87@gmail.com>
2017-11-09 13:28:00 +0000

    Pass in the type for filesystem backups to fix issue with code rollback not knowing what type it was

--- setup/src/Magento/Setup/Console/Command/RollbackCommand.php
+++ setup/src/Magento/Setup/Console/Command/RollbackCommand.php
@@ -129,7 +129,7 @@ class RollbackCommand extends AbstractSetupCommand
             }
 
             $questionKeep = new ConfirmationQuestion(
-                '<info>you want to keep the backups?[y/N]<info>',
+                '<info>Do you want to keep the backups?[y/N]<info>',
                 false
             );
             $keepSourceFile = $helper->ask($input, $output, $questionKeep);
@@ -161,7 +161,11 @@ class RollbackCommand extends AbstractSetupCommand
         $inputOptionProvided = false;
         $rollbackHandler = $this->backupRollbackFactory->create($output);
         if ($input->getOption(self::INPUT_KEY_CODE_BACKUP_FILE)) {
-            $rollbackHandler->codeRollback($input->getOption(self::INPUT_KEY_CODE_BACKUP_FILE), $keepSourceFile);
+            $rollbackHandler->codeRollback(
+                $input->getOption(self::INPUT_KEY_CODE_BACKUP_FILE),
+                Factory::TYPE_FILESYSTEM,
+                $keepSourceFile
+            );
             $inputOptionProvided = true;
         }
         if ($input->getOption(self::INPUT_KEY_MEDIA_BACKUP_FILE)) {
