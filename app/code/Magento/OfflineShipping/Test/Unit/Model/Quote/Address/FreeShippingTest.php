68f64ddc98ee6c2f945b5546fc0fbe23ec14cc36
Sviatoslav Mankivskyi <smankivskyi@magento.com>
2017-11-13 15:57:33 +0200

    MAGETWO-66447: [Backport] - Free shipping not removed with coupon - for 2.1

--- app/code/Magento/OfflineShipping/Test/Unit/Model/Quote/Address/FreeShippingTest.php
+++ app/code/Magento/OfflineShipping/Test/Unit/Model/Quote/Address/FreeShippingTest.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Copyright © 2016 Magento. All rights reserved.
+ *
+ * Copyright © 2013-2017 Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
-
 namespace Magento\OfflineShipping\Test\Unit\Model\Quote\Address;
 
 class FreeShippingTest extends \PHPUnit_Framework_TestCase
