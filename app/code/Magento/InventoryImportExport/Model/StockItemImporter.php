0ccb5449fe84274c103dc72e6232f6e4d0e79317
josh-carter <josh@webtise.com>
2017-11-18 17:12:57 +0100

    #207 - Inject Preference for StockItemImportyer
    Handles default source Qty import

---
+++ app/code/Magento/InventoryImportExport/Model/StockItemImporter.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: joshuacarter
+ * Date: 18/11/2017
+ * Time: 15:00
+ */
+declare(strict_types=1);
+
+namespace Magento\InventoryImportExport\Model;
+
+use Magento\CatalogImportExport\Model\StockItemImporterInterface;
+use Magento\CatalogImportExport\Model\Import\Product;
+use Magento\Inventory\Model\SourceItemFactory;
+use Magento\InventoryApi\Api\Data\SourceItemInterface;
+use Magento\InventoryApi\Api\SourceItemsSaveInterface;
+use Magento\InventoryCatalog\Api\DefaultSourceProviderInterface;
+
+class StockItemImporter implements StockItemImporterInterface
+{
+    /**
+     * Source Items Save Interface for saving multiple source items
+     *
+     * @var SourceItemsSaveInterface $sourceItemsSave
+     */
+    private $sourceItemsSave;
+
+    /**
+     * Source Item Interface Factory
+     *
+     * @var SourceItemFactory $sourceItemFactory
+     */
+    private $sourceItemFactory;
+
+    /**
+     * Default Source Provider
+     *
+     * @var DefaultSourceProviderInterface $defaultSource
+     */
+    private $defaultSource;
+
+    /**
+     * StockItemImporter constructor
+     *
+     * @param SourceItemsSaveInterface $sourceItemsSave
+     * @param SourceItemFactory $sourceItemFactory
+     * @param DefaultSourceProviderInterface $defaultSourceProvider
+     */
+    public function __construct(
+        SourceItemsSaveInterface $sourceItemsSave,
+        SourceItemFactory $sourceItemFactory,
+        DefaultSourceProviderInterface $defaultSourceProvider
+    ) {
+        $this->sourceItemsSave = $sourceItemsSave;
+        $this->sourceItemFactory = $sourceItemFactory;
+        $this->defaultSource = $defaultSourceProvider;
+    }
+
+    /**
+     * Handle Import of Stock Item Data
+     *
+     * @param array $stockData
+     * @return void
+     */
+    public function import(array $stockData): void
+    {
+        $sourceItems = [];
+        foreach ($stockData as $stockDatum) {
+            if (isset($stockDatum[Product::COL_SKU])) {
+                $sku = $stockDatum[Product::COL_SKU];
+                $sourceItem = $this->sourceItemFactory->create();
+                $sourceItem->setSku($sku);
+                $sourceItem->setSourceId($this->defaultSource->getId());
+                $sourceItem->setQuantity($stockDatum['qty']);
+                $sourceItem->setStatus($stockDatum['is_in_stock']);
+                $sourceItems[] = $sourceItem;
+            }
+        }
+        if (count($sourceItems) > 0) {
+            /** Magento\Inventory\Model\SourceItem[] $sourceItems */
+            $this->sourceItemsSave->execute($sourceItems);
+        }
+    }
+}
