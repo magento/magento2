a43e962f1928ef5065f3778a25ac882a7be5b62c
Sinisa Nedeljkovic <sinisa86@gmail.com>
2017-11-24 03:16:44 +0100

    Added integration tests which checks if Main website has been associated with Default stock

--- app/code/Magento/InventorySales/Test/Integration/Website/AssignWebsiteToDefaultStockTest.php
+++ app/code/Magento/InventorySales/Test/Integration/Website/AssignWebsiteToDefaultStockTest.php
@@ -10,6 +10,7 @@ namespace Magento\InventorySales\Test\Integration\Website;
 use Magento\InventoryApi\Api\StockRepositoryInterface;
 use Magento\InventoryCatalog\Api\DefaultStockProviderInterface;
 use Magento\InventorySalesApi\Api\Data\SalesChannelInterface;
+use Magento\Store\Model\StoreManagerInterface;
 use Magento\Store\Model\Website;
 use Magento\Store\Model\WebsiteFactory;
 use Magento\TestFramework\Helper\Bootstrap;
@@ -32,11 +33,37 @@ class AssignWebsiteToDefaultStockTest extends TestCase
      */
     private $defaultStockProvider;
 
+    /**
+     * @var StoreManagerInterface
+     */
+    private $storeManager;
+
     protected function setUp()
     {
         $this->websiteFactory = Bootstrap::getObjectManager()->get(WebsiteFactory::class);
         $this->stockRepository = Bootstrap::getObjectManager()->get(StockRepositoryInterface::class);
         $this->defaultStockProvider = Bootstrap::getObjectManager()->get(DefaultStockProviderInterface::class);
+        $this->storeManager = Bootstrap::getObjectManager()->get(StoreManagerInterface::class);
+    }
+
+    /**
+     * Test if Main website is associated to Default stock
+     */
+    public function testIfWebsiteIsAssignedToDefaultStock()
+    {
+        $websiteCode = $this->storeManager->getWebsite()->getCode();
+
+        $defaultStockId = $this->defaultStockProvider->getId();
+        $defaultStock = $this->stockRepository->get($defaultStockId);
+
+        $extensionAttributes = $defaultStock->getExtensionAttributes();
+        $salesChannels = $extensionAttributes->getSalesChannels();
+        self::assertContainsOnlyInstancesOf(SalesChannelInterface::class, $salesChannels);
+        self::assertCount(1, $salesChannels);
+
+        $salesChannel = reset($salesChannels);
+        self::assertEquals($websiteCode, $salesChannel->getCode());
+        self::assertEquals(SalesChannelInterface::TYPE_WEBSITE, $salesChannel->getType());
     }
 
     /**
@@ -59,10 +86,15 @@ class AssignWebsiteToDefaultStockTest extends TestCase
         $extensionAttributes = $defaultStock->getExtensionAttributes();
         $salesChannels = $extensionAttributes->getSalesChannels();
         self::assertContainsOnlyInstancesOf(SalesChannelInterface::class, $salesChannels);
-        self::assertCount(1, $salesChannels);
 
-        $salesChannel = reset($salesChannels);
-        self::assertEquals($website->getCode(), $salesChannel->getCode());
-        self::assertEquals(SalesChannelInterface::TYPE_WEBSITE, $salesChannel->getType());
+        $salesChannelsWhichBelongToCreatedWebsite = array_filter($salesChannels, function($aSalesChannel) use ($websiteCode) {
+            return $aSalesChannel->getCode() === $websiteCode;
+        });
+
+        self::assertCount(1, $salesChannelsWhichBelongToCreatedWebsite);
+
+        $aSalesChannelWhichBelongToCreatedWebsite = reset($salesChannelsWhichBelongToCreatedWebsite);
+        self::assertEquals($website->getCode(), $aSalesChannelWhichBelongToCreatedWebsite->getCode());
+        self::assertEquals(SalesChannelInterface::TYPE_WEBSITE, $aSalesChannelWhichBelongToCreatedWebsite->getType());
     }
 }
