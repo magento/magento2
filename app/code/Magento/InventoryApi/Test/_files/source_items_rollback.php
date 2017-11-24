03c1894738ef1b3e8077a64dcf64bc53c53fc483
Bartłomiej Szymański <bartlomiej.szymanski@bold.net.pl>
2017-11-24 11:47:32 +0100

    change single delete to mass delete on source_items_rollback

--- app/code/Magento/InventoryApi/Test/_files/source_items_rollback.php
+++ app/code/Magento/InventoryApi/Test/_files/source_items_rollback.php
@@ -3,21 +3,21 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+
 use Magento\Framework\Api\SearchCriteriaBuilder;
 use Magento\InventoryApi\Api\Data\SourceItemInterface;
 use Magento\InventoryApi\Api\SourceItemRepositoryInterface;
+use Magento\InventoryApi\Api\SourceItemsDeleteInterface;
 use Magento\TestFramework\Helper\Bootstrap;
 
 /** @var SourceItemRepositoryInterface $sourceItemRepository */
 $sourceItemRepository = Bootstrap::getObjectManager()->get(SourceItemRepositoryInterface::class);
+/** @var SourceItemsDeleteInterface $sourceItemsDelete */
+$sourceItemsDelete = Bootstrap::getObjectManager()->get(SourceItemsDeleteInterface::class);
 /** @var SearchCriteriaBuilder $searchCriteriaBuilder */
 $searchCriteriaBuilder = Bootstrap::getObjectManager()->get(SearchCriteriaBuilder::class);
 
-$searchCriteria = $searchCriteriaBuilder
-    ->addFilter(SourceItemInterface::SKU, ['SKU-1', 'SKU-2', 'SKU-3'], 'in')
-    ->create();
+$searchCriteria =
+    $searchCriteriaBuilder->addFilter(SourceItemInterface::SKU, ['SKU-1', 'SKU-2', 'SKU-3'], 'in')->create();
 $sourceItems = $sourceItemRepository->getList($searchCriteria)->getItems();
-
-foreach ($sourceItems as $sourceItem) {
-    $sourceItemRepository->delete($sourceItem);
-}
+$sourceItemsDelete->execute($sourceItems);
