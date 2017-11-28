c8aa113a302f2c2f9c5168133818073827863123
Andrii Voskoboinikov <avoskoboinikov@magento.com>
2017-11-28 14:34:38 +0200

    MAGETWO-81090: Run benchmark in multithread mode

--- setup/src/Magento/Setup/Fixtures/ConfigsApplyFixture.php
+++ setup/src/Magento/Setup/Fixtures/ConfigsApplyFixture.php
@@ -41,8 +41,13 @@ class ConfigsApplyFixture extends Fixture
                 ->setValue($config['value'])
                 ->save();
         }
-        $this->fixtureModel->getObjectManager()->get(\Magento\Framework\App\CacheInterface::class)
+        $this->fixtureModel->getObjectManager()
+            ->get(\Magento\Framework\App\CacheInterface::class)
             ->clean([\Magento\Framework\App\Config::CACHE_TAG]);
+
+        $this->fixtureModel->getObjectManager()
+            ->get(\Magento\Config\App\Config\Type\System::class)
+            ->clean();
     }
 
     /**
