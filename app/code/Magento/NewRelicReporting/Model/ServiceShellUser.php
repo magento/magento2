97735bbca7cbe4224dce5fe91fecf6757a14e60e
Kristof Ringleff, Fooman <kristof@fooman.co.nz>
2017-11-29 13:07:42 +1300

    Skipping stock deploy, adding new command.

---
+++ app/code/Magento/NewRelicReporting/Model/ServiceShellUser.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Magento\NewRelicReporting\Model;
+
+class ServiceShellUser
+{
+    const DEFAULT_USER = 'cron';
+
+    public function get($userFromArgument = false)
+    {
+        if ($userFromArgument) {
+            return $userFromArgument;
+        }
+
+        $user = `echo \$USER`;
+        if ($user) {
+            return $user;
+        }
+
+        return self::DEFAULT_USER;
+    }
+}
