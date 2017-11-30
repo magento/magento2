e49b1e4966b18512bcaa1b5decf3cc6313bf7a30
Stanislav Idolov <sidolov@magento.com>
2017-11-30 15:00:15 +0200

    MAGETWO-83893: Eliminate @codingStandardsIgnoreFile annotation from codebase

--- lib/internal/Magento/Framework/Mview/View.php
+++ lib/internal/Magento/Framework/Mview/View.php
@@ -280,13 +280,10 @@ class View extends \Magento\Framework\DataObject implements ViewInterface
                     ? $this->changelogBatchSize[$this->getChangelog()->getViewId()]
                     : self::DEFAULT_BATCH_SIZE;
 
-                for ($versionFrom = $lastVersionId;
-                     $versionFrom < $currentVersionId;
-                     $versionFrom += $versionBatchSize
-                ) {
+                for ($vsFrom = $lastVersionId; $vsFrom < $currentVersionId; $vsFrom += $versionBatchSize) {
                     // Don't go past the current version for atomicy.
-                    $versionTo = min($currentVersionId, $versionFrom + $versionBatchSize);
-                    $ids = $this->getChangelog()->getList($versionFrom, $versionTo);
+                    $versionTo = min($currentVersionId, $vsFrom + $versionBatchSize);
+                    $ids = $this->getChangelog()->getList($vsFrom, $versionTo);
 
                     // We run the actual indexer in batches.
                     // Chunked AFTER loading to avoid duplicates in separate chunks.
