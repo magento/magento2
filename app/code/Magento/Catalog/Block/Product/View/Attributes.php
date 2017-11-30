bd1b0899e876a2d64bd363008a5fd34ca68f489b
Volodymyr Kublytskyi <vkublytskyi@magento.com>
2017-11-30 14:03:30 +0200

    MAGETWO-71832: Prevent unset Boolean product attributes from displaying in product page #10619

--- app/code/Magento/Catalog/Block/Product/View/Attributes.php
+++ app/code/Magento/Catalog/Block/Product/View/Attributes.php
@@ -83,12 +83,8 @@ class Attributes extends \Magento\Framework\View\Element\Template
             if ($attribute->getIsVisibleOnFront() && !in_array($attribute->getAttributeCode(), $excludeAttr)) {
                 $value = $attribute->getFrontend()->getValue($product);
 
-                if (!$product->hasData($attribute->getAttributeCode())) {
-                    $value = __('N/A');
-                } elseif ($value instanceof Phrase) {
+                if ($value instanceof Phrase) {
                     $value = (string)$value;
-                } elseif ((string)$value == '') {
-                    $value = __('No');
                 } elseif ($attribute->getFrontendInput() == 'price' && is_string($value)) {
                     $value = $this->priceCurrency->convertAndFormat($value);
                 }
