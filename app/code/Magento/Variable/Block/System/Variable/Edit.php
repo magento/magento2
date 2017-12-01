fe9221f2c75b66c981aaa7b385f52cf9133de5ff
Leandro F. L <leandro.luvisotto@empiricus.com.br>
2017-12-01 12:34:31 -0200

    The left and the right parts of assignment are equal

--- app/code/Magento/Variable/Block/System/Variable/Edit.php
+++ app/code/Magento/Variable/Block/System/Variable/Edit.php
@@ -91,9 +91,7 @@ class Edit extends \Magento\Backend\Block\Widget\Form\Container
     public function getFormHtml()
     {
         $formHtml = parent::getFormHtml();
-        if (!$this->_storeManager->isSingleStoreMode() && $this->getVariable()->getId()) {
-            $formHtml = $formHtml;
-        }
+
         return $formHtml;
     }
 
