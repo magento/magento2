db9ad08d1adc3936617f4c7d145f170f5bf184da
David Manners <dmanners87@gmail.com>
2017-12-01 11:15:29 +0000

    Update copyright to be the old 2.1 version of the copyright

--- app/code/Magento/Cms/Model/Page/DomValidationState.php
+++ app/code/Magento/Cms/Model/Page/DomValidationState.php
@@ -2,7 +2,7 @@
 /**
  * Application config file resolver
  *
- * Copyright © Magento, Inc. All rights reserved.
+ * Copyright © 2013-2017 Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
 namespace Magento\Cms\Model\Page;
