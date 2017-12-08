b4d089b0270f636ff08036c259fd15b50a1a7b7b
Oleksandr Miroshnichenko <omiroshnichenko@magento.com>
2017-12-08 16:16:13 +0200

    Added namespace to product videos fotorama events

--- app/code/Magento/ProductVideo/view/frontend/web/js/fotorama-add-video-events.js
+++ app/code/Magento/ProductVideo/view/frontend/web/js/fotorama-add-video-events.js
@@ -205,7 +205,7 @@ define([
                 if (options.dataMergeStrategy === 'prepend') {
                     this.options.videoData = [].concat(
                         this.options.optionsVideoData[options.selectedOption],
-                        this.options.videoData
+                        this.defaultVideoData
                     );
                 } else {
                     this.options.videoData = this.options.optionsVideoData[options.selectedOption];
