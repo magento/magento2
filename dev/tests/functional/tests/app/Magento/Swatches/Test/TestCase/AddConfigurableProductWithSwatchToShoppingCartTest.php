ce5d80ddbc026cbf4d4fd4bfe2c964f8c2655eef
Dmytro Vilchynskyi <dvilchynskyi@magento.com>
2017-12-11 14:10:16 +0200

    MAGETWO-84074: Automate "Update Configurable Product (based on Swatch Attribute) from Shopping Cart" FT
    - Static tests are fixed

--- dev/tests/functional/tests/app/Magento/Swatches/Test/TestCase/AddConfigurableProductWithSwatchToShoppingCartTest.php
+++ dev/tests/functional/tests/app/Magento/Swatches/Test/TestCase/AddConfigurableProductWithSwatchToShoppingCartTest.php
@@ -7,7 +7,7 @@
 namespace Magento\Swatches\Test\TestCase;
 
 use Magento\Mtf\TestCase\Injectable;
-use Magento\ConfigurableProduct\Test\Fixture\ConfigurableProduct;
+use Magento\Swatches\Test\TestStep\AddProductToCartFromCatalogCategoryPageStep as AddToCart;
 use Magento\Mtf\TestStep\TestStepFactory;
 use Magento\Catalog\Test\Page\Category\CatalogCategoryView;
 
@@ -68,7 +68,7 @@ class AddConfigurableProductWithSwatchToShoppingCartTest extends Injectable
     {
         $product->persist();
         $cart = $this->testStepFactory->create(
-            \Magento\Swatches\Test\TestStep\AddProductToCartFromCatalogCategoryPageStep::class,
+            AddToCart::class,
             [
                 'product' => $product
             ]
