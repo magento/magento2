944adbf1150ac7dedc40b3f4c4eebbe28f30ee9f
Mohammad HAJ SALEM <haj-salem@e-conomix.at>
2017-12-12 16:22:17 +0100

    :bug: add fallback for Product_links position attribute if position index was not set in API request

--- app/code/Magento/Catalog/Model/Product/Link/SaveHandler.php
+++ app/code/Magento/Catalog/Model/Product/Link/SaveHandler.php
@@ -31,18 +31,28 @@ class SaveHandler
     private $linkResource;
 
     /**
+     * @var linkTypeProvider
+     */
+    private $linkTypeProvider;
+
+    /**
+     * SaveHandler constructor.
      * @param MetadataPool $metadataPool
      * @param Link $linkResource
      * @param ProductLinkRepositoryInterface $productLinkRepository
+     * @param \Magento\Catalog\Model\Product\LinkTypeProvider $linkTypeProvider
      */
     public function __construct(
         MetadataPool $metadataPool,
         Link $linkResource,
-        ProductLinkRepositoryInterface $productLinkRepository
-    ) {
+        ProductLinkRepositoryInterface $productLinkRepository,
+        \Magento\Catalog\Model\Product\LinkTypeProvider $linkTypeProvider
+    )
+    {
         $this->metadataPool = $metadataPool;
         $this->linkResource = $linkResource;
         $this->productLinkRepository = $productLinkRepository;
+        $this->linkTypeProvider = $linkTypeProvider;
     }
 
     /**
@@ -55,17 +65,74 @@ class SaveHandler
     {
         $link = $entity->getData($this->metadataPool->getMetadata($entityType)->getLinkField());
         if ($this->linkResource->hasProductLinks($link)) {
-            /** @var \Magento\Catalog\Api\Data\ProductInterface $entity*/
+            /** @var \Magento\Catalog\Api\Data\ProductInterface $entity */
             foreach ($this->productLinkRepository->getList($entity) as $link) {
                 $this->productLinkRepository->delete($link);
             }
         }
         $productLinks = $entity->getProductLinks();
+
+        // Build links per type
+        $linksByType = [];
+        foreach ($productLinks as $link) {
+            $linksByType[$link->getLinkType()][] = $link;
+        }
+
+        // Do check
+        $hasPositionLinkType = $this->isPositionsSet($linksByType);
+
+
+        // Bug fix for API if the Position was not set to force set Position attribute in "catalog_product_link_attribute_int"
+        // set Positions attribute values
+        foreach ($hasPositionLinkType as $linkType => $hasPosition) {
+            if (!$hasPosition) {
+                array_walk($linksByType[$linkType], function ($productLink, $position) {
+                    $productLink->setPosition(++$position);
+                });
+            }
+        }
+
+        // Flatten multi-dimensional linksByType in ProductLinks
+        $productLinks = array_reduce($linksByType, 'array_merge', []);
+
         if (count($productLinks) > 0) {
             foreach ($entity->getProductLinks() as $link) {
                 $this->productLinkRepository->save($link);
             }
         }
         return $entity;
+    }
+
+    /**
+     * Check if the position is set for all product links per link type.
+     * array with boolean per type
+     *
+     * @param $linksByType
+     * @return array
+     */
+    private function isPositionsSet($linksByType)
+    {
+        $linkTypes = $this->linkTypeProvider->getLinkTypes();
+
+        // Initialize isPositionSet for existent link types
+        $isPositionSet = [];
+        foreach (array_keys($linkTypes) as $typeName) {
+            if (array_key_exists($typeName, $linksByType)) {
+                $isPositionSet[$typeName] = count($linksByType[$typeName]) > 0;
+            }
+        }
+
+
+        // Check if at least on link without position exists per Link type
+        foreach ($linksByType as $type => $links) {
+            foreach ($links as $link) {
+                if (!array_key_exists('position', $link->getData())) {
+                    $isPositionSet[$type] = false;
+                    break;
+                }
+            }
+        }
+
+        return $isPositionSet;
     }
 }
