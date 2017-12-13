e7529f3e5d15a006848a284dc6d2319aad72229b
Fabian Schmengler <fs@integer-net.de>
2017-12-13 10:52:36 +0100

    Re-add DataObject inheritance, but deprecate all parent methods

--- app/code/Magento/Sales/Model/Order/ShippingMethod.php
+++ app/code/Magento/Sales/Model/Order/ShippingMethod.php
@@ -8,7 +8,7 @@ namespace Magento\Sales\Model\Order;
 /**
  * Value object for shipping_method order attribute
  */
-class ShippingMethod
+class ShippingMethod extends \Magento\Framework\DataObject
 {
     /**
      * @var
@@ -92,4 +92,236 @@ class ShippingMethod
         $this->method = $medhod;
         return $this;
     }
+
+    /**
+     * @inheritDoc
+     * @deprecated DataObject inheritance will be removed
+     */
+    public function addData(array $arr)
+    {
+        return parent::addData($arr);
+    }
+
+    /**
+     * @inheritDoc
+     * @deprecated DataObject inheritance will be removed
+     */
+    public function setData($key, $value = null)
+    {
+        return parent::setData($key, $value);
+    }
+
+    /**
+     * @inheritDoc
+     * @deprecated DataObject inheritance will be removed
+     */
+    public function unsetData($key = null)
+    {
+        return parent::unsetData($key);
+    }
+
+    /**
+     * @inheritDoc
+     * @deprecated DataObject inheritance will be removed
+     */
+    public function getData($key = '', $index = null)
+    {
+        return parent::getData($key, $index);
+    }
+
+    /**
+     * @inheritDoc
+     * @deprecated DataObject inheritance will be removed
+     */
+    public function getDataByPath($path)
+    {
+        return parent::getDataByPath($path);
+    }
+
+    /**
+     * @inheritDoc
+     * @deprecated DataObject inheritance will be removed
+     */
+    public function getDataByKey($key)
+    {
+        return parent::getDataByKey($key);
+    }
+
+    /**
+     * @inheritDoc
+     * @deprecated DataObject inheritance will be removed
+     */
+    public function setDataUsingMethod($key, $args = [])
+    {
+        return parent::setDataUsingMethod($key, $args);
+    }
+
+    /**
+     * @inheritDoc
+     * @deprecated DataObject inheritance will be removed
+     */
+    public function getDataUsingMethod($key, $args = null)
+    {
+        return parent::getDataUsingMethod($key, $args);
+    }
+
+    /**
+     * @inheritDoc
+     * @deprecated DataObject inheritance will be removed
+     */
+    public function hasData($key = '')
+    {
+        return parent::hasData($key);
+    }
+
+    /**
+     * @inheritDoc
+     * @deprecated DataObject inheritance will be removed
+     */
+    public function toArray(array $keys = [])
+    {
+        return parent::toArray($keys);
+    }
+
+    /**
+     * @inheritDoc
+     * @deprecated DataObject inheritance will be removed
+     */
+    public function convertToArray(array $keys = [])
+    {
+        return parent::convertToArray($keys);
+    }
+
+    /**
+     * @inheritDoc
+     * @deprecated DataObject inheritance will be removed
+     */
+    public function toXml(array $keys = [], $rootName = 'item', $addOpenTag = false, $addCdata = true)
+    {
+        return parent::toXml($keys, $rootName, $addOpenTag, $addCdata);
+    }
+
+    /**
+     * @inheritDoc
+     * @deprecated DataObject inheritance will be removed
+     */
+    public function convertToXml(
+        array $arrAttributes = [],
+        $rootName = 'item',
+        $addOpenTag = false,
+        $addCdata = true
+    ) {
+        return parent::convertToXml(
+            $arrAttributes,
+            $rootName,
+            $addOpenTag,
+            $addCdata
+        );
+    }
+
+    /**
+     * @inheritDoc
+     * @deprecated DataObject inheritance will be removed
+     */
+    public function toJson(array $keys = [])
+    {
+        return parent::toJson($keys);
+    }
+
+    /**
+     * @inheritDoc
+     * @deprecated DataObject inheritance will be removed
+     */
+    public function convertToJson(array $keys = [])
+    {
+        return parent::convertToJson($keys);
+    }
+
+    /**
+     * @inheritDoc
+     * @deprecated DataObject inheritance will be removed
+     */
+    public function toString($format = '')
+    {
+        return parent::toString($format);
+    }
+
+    /**
+     * @inheritDoc
+     * @deprecated DataObject inheritance will be removed
+     */
+    public function __call($method, $args)
+    {
+        return parent::__call($method, $args);
+    }
+
+    /**
+     * @inheritDoc
+     * @deprecated DataObject inheritance will be removed
+     */
+    public function isEmpty()
+    {
+        return parent::isEmpty();
+    }
+
+    /**
+     * @inheritDoc
+     * @deprecated DataObject inheritance will be removed
+     */
+    public function serialize($keys = [], $valueSeparator = '=', $fieldSeparator = ' ', $quote = '"')
+    {
+        return parent::serialize(
+            $keys,
+            $valueSeparator,
+            $fieldSeparator,
+            $quote
+        );
+    }
+
+    /**
+     * @inheritDoc
+     * @deprecated DataObject inheritance will be removed
+     */
+    public function debug($data = null, &$objects = [])
+    {
+        return parent::debug($data, $objects);
+    }
+
+    /**
+     * @inheritDoc
+     * @deprecated DataObject inheritance will be removed
+     */
+    public function offsetSet($offset, $value)
+    {
+        parent::offsetSet($offset, $value);
+    }
+
+    /**
+     * @inheritDoc
+     * @deprecated DataObject inheritance will be removed
+     */
+    public function offsetExists($offset)
+    {
+        return parent::offsetExists($offset);
+    }
+
+    /**
+     * @inheritDoc
+     * @deprecated DataObject inheritance will be removed
+     */
+    public function offsetUnset($offset)
+    {
+        parent::offsetUnset($offset);
+    }
+
+    /**
+     * @inheritDoc
+     * @deprecated DataObject inheritance will be removed
+     */
+    public function offsetGet($offset)
+    {
+        return parent::offsetGet($offset);
+    }
+
+
 }
