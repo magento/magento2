47a9573f150e372cd7a8375bfaedaf118472c44c
larsroettig <l.roettig@techdivision.com>
2017-12-13 15:27:56 +0300

    WIP MSI: #288 - Add CodeValidator unit test

--- app/code/Magento/Inventory/Model/Source/Validator/CodeValidator.php
+++ app/code/Magento/Inventory/Model/Source/Validator/CodeValidator.php
@@ -39,7 +39,7 @@ class CodeValidator implements SourceValidatorInterface
         if ('' === trim($value)) {
             $errors[] = __('"%field" can not be empty.', ['field' => SourceInterface::CODE]);
         }
-        if (preg_match('/\s/', $value)) {
+        elseif (preg_match('/\s/', $value)) {
             $errors[] = __('"%field" can not contain whitespaces.', ['field' => SourceInterface::CODE]);
         } else {
             $errors = [];
