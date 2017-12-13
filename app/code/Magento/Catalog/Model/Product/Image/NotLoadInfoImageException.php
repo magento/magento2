81168807b467bba9428cf9d8f3f5e393c8b9f4cc
Stanislav Lopukhov <slopukhov@magento.com>
2017-12-13 12:25:51 +0200

    MAGETWO-85581: Enable metrics validation and run benchmark in multithread mode for PAT

---
+++ app/code/Magento/Catalog/Model/Product/Image/NotLoadInfoImageException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Catalog\Model\Product\Image;
+
+use Magento\Framework\Exception\LocalizedException;
+
+/**
+ * @api
+ * @since 102.0.0
+ */
+class NotLoadInfoImageException extends LocalizedException
+{
+}
