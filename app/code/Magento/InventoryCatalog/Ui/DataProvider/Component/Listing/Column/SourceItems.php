59ca2b17beda3666d1d086a97fb2eac84a7c9a04
alexey.motorny <alexey.motorny@amasty.com>
2017-12-14 13:56:49 +0300

    issue #231 Add column to Product grid with information about source items

---
+++ app/code/Magento/InventoryCatalog/Ui/DataProvider/Component/Listing/Column/SourceItems.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\InventoryCatalog\Ui\Component\Listing\Column;
+
+use Magento\Framework\Api\SearchCriteriaBuilder;
+use Magento\InventoryApi\Api\SourceItemRepositoryInterface;
+use Magento\InventoryApi\Api\SourceRepositoryInterface;
+use Magento\Ui\Component\Listing\Columns\Column;
+use Magento\Framework\View\Element\UiComponentFactory;
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+
+/**
+ * Add grid column for source items. Prepare data
+ */
+class SourceItems extends Column
+{
+    /**
+     * @var SourceItemRepositoryInterface
+     */
+    private $sourceItemRepository;
+
+    /**
+     * @var SearchCriteriaBuilder
+     */
+    private $searchCriteriaBuilder;
+
+    /**
+     * @var SourceRepositoryInterface
+     */
+    private $sourceRepository;
+
+    /**
+     * @param ContextInterface $context
+     * @param UiComponentFactory $uiComponentFactory
+     * @param SourceItemRepositoryInterface $sourceItemRepository
+     * @param SearchCriteriaBuilder $searchCriteriaBuilder
+     * @param SourceRepositoryInterface $sourceRepository
+     * @param array $components
+     * @param array $data
+     */
+    public function __construct(
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        SourceItemRepositoryInterface $sourceItemRepository,
+        SearchCriteriaBuilder $searchCriteriaBuilder,
+        SourceRepositoryInterface $sourceRepository,
+        array $components = [],
+        array $data = []
+    ) {
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+        $this->sourceItemRepository = $sourceItemRepository;
+        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
+        $this->sourceRepository = $sourceRepository;
+    }
+
+    /**
+     * Prepare data source
+     *
+     * @param array $dataSource
+     * @return array
+     */
+    public function prepareDataSource(array $dataSource)
+    {
+        if ($dataSource['data']['totalRecords'] > 0) {
+            foreach ($dataSource['data']['items'] as &$row) {
+                $row['qty'] = isset($row['sku']) ? $this->prepareSourceItemsData($row['sku']) : [];
+            }
+        }
+        unset($row);
+
+        return $dataSource;
+    }
+
+    /**
+     * Prepare source items
+     *
+     * @param string $sku
+     * @return array
+     */
+    private function prepareSourceItemsData(string $sku)
+    {
+        $preparedSourceData = [];
+
+        $searchCriteria = $this->searchCriteriaBuilder
+            ->addFilter('sku', $sku)
+            ->create();
+        $sourceList = $this->sourceItemRepository->getList($searchCriteria);
+
+        /** @var \Magento\InventoryApi\Api\Data\SourceItemInterface $sourceItem */
+        foreach ($sourceList->getItems() as $sourceItem) {
+            $qty = (float)$sourceItem->getQuantity();
+            $source = $this->sourceRepository->get((int)$sourceItem->getSourceId());
+
+            $preparedSourceData[] = [
+                'name' => $source->getName(),
+                'qty' => $qty
+            ];
+        }
+
+        return $preparedSourceData;
+    }
+}
---
+++ app/code/Magento/InventoryCatalog/view/adminhtml/web/js/product/grid/cell/source-items.js
@@ -0,0 +1,28 @@
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+define([
+    'Magento_Ui/js/grid/columns/column',
+    'underscore'
+], function (Column, _) {
+    'use strict';
+
+    return Column.extend({
+        defaults: {
+            bodyTmpl: 'Magento_InventoryCatalog/product/grid/cell/source-items-cell.html'
+        },
+
+        /**
+         * Get source items with qty
+         *
+         * @param {Object} record - Record object
+         * @returns {Array} Result array
+         */
+        getSourceItems: function (record) {
+            var result = record[this.index] ? record[this.index] : [];
+
+            return result;
+        }
+    });
+});
