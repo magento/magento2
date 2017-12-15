ceab796d0e909ab113178ebf9a2213845b180d47
vitaliyboyko <vitaliyboyko@i.ua>
2017-12-15 15:16:17 +0200

    Added shipping algorithm interfaces

---
+++ app/code/Magento/InventorySales/Model/ShippingAlgorithmInterface.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\InventorySales\Model;
+
+use Magento\InventorySales\Model\ShippingAlgorithm\ResultInterface;
+use Magento\Sales\Api\Data\OrderItemInterface;
+
+interface ShippingAlgorithmInterface
+{
+    /**
+     * @param OrderItemInterface $orderItem
+     * @return ResultInterface
+     */
+    public function execute(OrderItemInterface $orderItem);
+}
