befca40e1a000ca0a5bddc6151b93047d3f050b5
organizm <A1s2d3f4>
2017-12-16 11:08:31 +0200

    Add custom Select UI component. #Not finished.

---
+++ app/code/Magento/InventoryCatalog/view/adminhtml/web/js/components/select-handle-disabled.js
@@ -0,0 +1,17 @@
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+/**
+ * @deprecated since version 2.2.0
+ */
+define([
+    'Magento_Ui/js/form/element/select'
+], function (Select) {
+    'use strict';
+    console.log('Init');
+    return Select.extend({
+
+    });
+});
