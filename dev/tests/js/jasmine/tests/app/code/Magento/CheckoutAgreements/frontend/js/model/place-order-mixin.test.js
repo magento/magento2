a35b4686c1b95dc356595246410e527d7c57e2ac
Ricards Z <ricards.z@nnpro.at>
2017-12-18 23:49:00 +0200

    Fix too many nested callbacks in jasmine tests

--- dev/tests/js/jasmine/tests/app/code/Magento/CheckoutAgreements/frontend/js/model/place-order-mixin.test.js
+++ dev/tests/js/jasmine/tests/app/code/Magento/CheckoutAgreements/frontend/js/model/place-order-mixin.test.js
@@ -8,50 +8,48 @@ define([
 ], function (Squire) {
     'use strict';
 
-    describe('Magento_CheckoutAgreements/js/model/place-order-mixin', function () {
-        var injector = new Squire(),
-            mocks = {
-                'Magento_Checkout/js/action/place-order': jasmine.createSpy('placeOrderAction'),
-                'Magento_CheckoutAgreements/js/model/agreements-assigner': jasmine.createSpy('agreementsAssigner')
-            },
-            defaultContext = require.s.contexts._,
-            mixin,
-            placeOrderAction;
+    var injector = new Squire(),
+        mocks = {
+            'Magento_Checkout/js/action/place-order': jasmine.createSpy('placeOrderAction'),
+            'Magento_CheckoutAgreements/js/model/agreements-assigner': jasmine.createSpy('agreementsAssigner')
+        },
+        defaultContext = require.s.contexts._,
+        mixin,
+        placeOrderAction;
 
-        beforeEach(function (done) {
-            window.checkoutConfig = {
-                checkoutAgreements: {
-                    isEnabled: true
-                }
-            };
-            injector.mock(mocks);
-            injector.require([
-                'Magento_CheckoutAgreements/js/model/place-order-mixin',
-                'Magento_Checkout/js/action/place-order'
-            ], function (Mixin, placeOrder) {
-                mixin = Mixin;
-                placeOrderAction = placeOrder;
-                done();
-            });
+    beforeEach(function (done) {
+        window.checkoutConfig = {
+            checkoutAgreements: {
+                isEnabled: true
+            }
+        };
+        injector.mock(mocks);
+        injector.require([
+            'Magento_CheckoutAgreements/js/model/place-order-mixin',
+            'Magento_Checkout/js/action/place-order'
+        ], function (Mixin, placeOrder) {
+            mixin = Mixin;
+            placeOrderAction = placeOrder;
+            done();
+        });
+    });
+
+    describe('Magento_CheckoutAgreements/js/model/place-order-mixin', function () {
+        it('mixin is applied to Magento_Checkout/js/action/place-order', function () {
+            var placeOrderMixins = defaultContext.config.config.mixins['Magento_Checkout/js/action/place-order'];
+
+            expect(placeOrderMixins['Magento_CheckoutAgreements/js/model/place-order-mixin']).toBe(true);
         });
 
-        describe('Magento_Checkout/js/action/place-order', function () {
-            it('Magento_CheckoutAgreements/js/model/place-order-mixin is applied', function () {
-                var placeOrderMixins = defaultContext.config.config.mixins['Magento_Checkout/js/action/place-order'];
+        it('Magento_CheckoutAgreements/js/model/agreements-assigner is called', function () {
+            var messageContainer = jasmine.createSpy('messageContainer'),
+                paymentData = {};
 
-                expect(placeOrderMixins['Magento_CheckoutAgreements/js/model/place-order-mixin']).toBe(true);
-            });
-
-            it('Magento_CheckoutAgreements/js/model/agreements-assigner is called', function () {
-                var messageContainer = jasmine.createSpy('messageContainer'),
-                    paymentData = {};
-
-                mixin(placeOrderAction)(paymentData, messageContainer);
-                expect(mocks['Magento_CheckoutAgreements/js/model/agreements-assigner'])
-                    .toHaveBeenCalledWith(paymentData);
-                expect(mocks['Magento_Checkout/js/action/place-order'])
-                    .toHaveBeenCalledWith(paymentData, messageContainer);
-            });
+            mixin(placeOrderAction)(paymentData, messageContainer);
+            expect(mocks['Magento_CheckoutAgreements/js/model/agreements-assigner'])
+                .toHaveBeenCalledWith(paymentData);
+            expect(mocks['Magento_Checkout/js/action/place-order'])
+                .toHaveBeenCalledWith(paymentData, messageContainer);
         });
     });
 });
