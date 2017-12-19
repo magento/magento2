f867a88e40b255030dcff871aaeb3bd0e8327d74
Oleksandr Iegorov <oiegorov@magento.com>
2017-12-19 16:40:02 +0200

    MAGETWO-69496: Impossible specify Bundle option title on store view level with changes to more than one store view

--- app/code/Magento/Bundle/Model/OptionRepository.php
+++ app/code/Magento/Bundle/Model/OptionRepository.php
@@ -246,10 +246,12 @@ class OptionRepository implements \Magento\Bundle\Api\ProductOptionRepositoryInt
      * @param string $sku
      * @return \Magento\Catalog\Api\Data\ProductInterface
      * @throws \Magento\Framework\Exception\InputException
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     * @return \Magento\Catalog\Api\Data\ProductInterface
      */
     private function getProduct($sku)
     {
-        $product = $this->productRepository->get($sku, true);
+        $product = $this->productRepository->get($sku, true, null, true);
         if ($product->getTypeId() != \Magento\Catalog\Model\Product\Type::TYPE_BUNDLE) {
             throw new InputException(__('Only implemented for bundle product'));
         }
