b15e59039bec1ff9c0aad32447738f706916d2e9
David Manners <dmanners87@gmail.com>
2017-12-19 13:35:46 +0000

    Update the create unit tests to use the result factory instead of object manager

--- app/code/Magento/Checkout/Test/Unit/Controller/Account/CreateTest.php
+++ app/code/Magento/Checkout/Test/Unit/Controller/Account/CreateTest.php
@@ -5,6 +5,8 @@
  */
 namespace Magento\Checkout\Test\Unit\Controller\Account;
 
+use Magento\Framework\Controller\ResultFactory;
+
 /**
  * Shopping cart edit tests
  */
@@ -40,6 +42,9 @@ class CreateTest extends \PHPUnit\Framework\TestCase
      */
     protected $objectManagerMock;
 
+    protected $resultFactory;
+    protected $resultPage;
+
     protected function setUp()
     {
         $objectManagerHelper = new \Magento\Framework\TestFramework\Unit\Helper\ObjectManager($this);
@@ -49,8 +54,20 @@ class CreateTest extends \PHPUnit\Framework\TestCase
         $this->messageManager = $this->createMock(\Magento\Framework\Message\ManagerInterface::class);
 
         $this->objectManagerMock = $this->createMock(\Magento\Framework\ObjectManagerInterface::class);
-        $contextMock = $this->createPartialMock(\Magento\Framework\App\Action\Context::class, ['getObjectManager']);
+        $contextMock = $this->createPartialMock(
+            \Magento\Framework\App\Action\Context::class,
+            ['getObjectManager', 'getResultFactory']
+        );
         $contextMock->expects($this->once())->method('getObjectManager')->willReturn($this->objectManagerMock);
+        $this->resultFactory = $this->getMockBuilder(\Magento\Framework\Controller\ResultFactory::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $contextMock->expects($this->once())
+            ->method('getResultFactory')
+            ->willReturn($this->resultFactory);
+        $this->resultPage = $this->getMockBuilder(\Magento\Framework\Controller\ResultInterface::class)
+            ->setMethods(['setData'])
+            ->getMockForAbstractClass();
 
         $this->action = $objectManagerHelper->getObject(
             \Magento\Checkout\Controller\Account\Create::class,
@@ -66,53 +83,48 @@ class CreateTest extends \PHPUnit\Framework\TestCase
 
     public function testExecuteAddsSessionMessageIfCustomerIsLoggedIn()
     {
-        $jsonFactoryMock = $this->createMock(\Magento\Framework\Controller\Result\JsonFactory::class);
-        $this->objectManagerMock->expects($this->once())
-            ->method('get')
-            ->with(\Magento\Framework\Controller\Result\JsonFactory::class)
-            ->willReturn($jsonFactoryMock);
-        $jsonMock = $this->createMock(\Magento\Framework\Controller\Result\Json::class);
-        $jsonFactoryMock->expects($this->once())->method('create')->willReturn($jsonMock);
-
-        $this->customerSession->expects($this->once())->method('isLoggedIn')->will($this->returnValue(true));
-
-        $jsonMock->expects($this->once())
+        $resultJson = '{"errors": "true", "message": "Customer is already registered"}';
+        $this->customerSession->expects($this->once())
+            ->method('isLoggedIn')
+            ->will($this->returnValue(true));
+        $this->resultFactory->expects($this->once())
+            ->method('create')
+            ->with(ResultFactory::TYPE_JSON)
+            ->willReturn($this->resultPage);
+        $this->resultPage->expects($this->once())
             ->method('setData')
             ->with(
                 [
                     'errors' => true,
                     'message' => __('Customer is already registered')
                 ]
-            )->willReturnSelf();
-        $this->action->execute();
+            )->willReturn($resultJson);
+        $this->assertEquals($resultJson, $this->action->execute());
     }
 
     public function testExecute()
     {
-        $jsonFactoryMock = $this->createMock(\Magento\Framework\Controller\Result\JsonFactory::class);
-        $this->objectManagerMock->expects($this->once())
-            ->method('get')
-            ->with(\Magento\Framework\Controller\Result\JsonFactory::class)
-            ->willReturn($jsonFactoryMock);
-        $jsonMock = $this->createMock(\Magento\Framework\Controller\Result\Json::class);
-        $jsonFactoryMock->expects($this->once())->method('create')->willReturn($jsonMock);
-
         $this->customerSession->expects($this->once())->method('isLoggedIn')->will($this->returnValue(false));
         $this->checkoutSession->expects($this->once())->method('getLastOrderId')->will($this->returnValue(100));
         $customer = $this->createMock(\Magento\Customer\Api\Data\CustomerInterface::class);
-        $this->orderCustomerService->expects($this->once())->method('create')->with(100)->will(
-            $this->returnValue($customer)
-        );
+        $this->orderCustomerService->expects($this->once())
+            ->method('create')
+            ->with(100)
+            ->will($this->returnValue($customer));
 
-        $jsonMock->expects($this->once())
+        $resultJson = '{"errors": "false", "message": "A letter with further instructions will be sent to your email."}';
+        $this->resultFactory->expects($this->once())
+            ->method('create')
+            ->with(ResultFactory::TYPE_JSON)
+            ->willReturn($this->resultPage);
+        $this->resultPage->expects($this->once())
             ->method('setData')
             ->with(
                 [
                     'errors' => false,
                     'message' => __('A letter with further instructions will be sent to your email.')
                 ]
-            )->willReturnSelf();
-
-        $this->action->execute();
+            )->willReturn($resultJson);
+        $this->assertEquals($resultJson, $this->action->execute());
     }
 }
