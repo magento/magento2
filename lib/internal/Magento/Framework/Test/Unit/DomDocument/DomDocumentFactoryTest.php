dcacdbc50ef952f2544e28762746c68d453dec7e
David Manners <dmanners87@gmail.com>
2017-12-20 11:39:31 +0000

    magento/magento2#5188: Update DomDocumentFactoryTest to check that create returns the expected type of object

--- lib/internal/Magento/Framework/Test/Unit/DomDocument/DomDocumentFactoryTest.php
+++ lib/internal/Magento/Framework/Test/Unit/DomDocument/DomDocumentFactoryTest.php
@@ -6,90 +6,16 @@
 
 namespace Magento\Framework\Test\Unit\DomDocument;
 
-use DOMDocument;
 use Magento\Framework\DomDocument\DomDocumentFactory;
-use Magento\Framework\ObjectManagerInterface;
-use Magento\Framework\TestFramework\Unit\Helper\ObjectManager as ObjectManagerHelper;
 
 class DomDocumentFactoryTest extends \PHPUnit\Framework\TestCase
 {
-    /**
-     * @var ObjectManagerHelper
-     */
-    private $objectManagerHelper;
-
-    /**
-     * @var ObjectManagerInterface|\PHPUnit_Framework_MockObject_MockObject
-     */
-    private $objectManagerMock;
-
-    /**
-     * @var DOMDocument|\PHPUnit_Framework_MockObject_MockObject
-     */
-    private $domDocumentMock;
-
-    /**
-     * @var DomDocumentFactory
-     */
-    private $domDocumentFactory;
-
-    /**
-     * @var string
-     */
-    private $xmlSample = <<<EOT
-<?xml version="1.0"?>
-<root>
-</root>
-EOT;
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function setUp()
+    public function testCreateReturnsDomDocument()
     {
-        $this->objectManagerHelper = new ObjectManagerHelper($this);
-        $this->objectManagerMock = $this->createMock(ObjectManagerInterface::class);
-        $this->domDocumentMock = $this->createMock(DOMDocument::class);
-        $this->domDocumentFactory = $this->objectManagerHelper->getObject(
-            DomDocumentFactory::class,
-            [
-                'objectManager' => $this->objectManagerMock
-            ]
+        $domDocumentFactory = new DomDocumentFactory();
+        $this->assertInstanceOf(
+            \DOMDocument::class,
+            $domDocumentFactory->create()
         );
-    }
-
-    /**
-     * @dataProvider createDataProvider
-     */
-    public function testCreate($data = null)
-    {
-        $this->objectManagerMock->expects($this->once())
-            ->method('create')
-            ->with(DOMDocument::class)
-            ->willReturn($this->domDocumentMock);
-
-        if (!empty($data)) {
-            $this->domDocumentMock->expects($this->once())
-                ->method('loadXML')
-                ->with($data)
-                ->willReturn(true);
-        } else {
-            $this->domDocumentMock->expects($this->never())
-                ->method('loadXML');
-        }
-
-        $this->domDocumentFactory->create($data);
-    }
-
-    /**
-     * @return array
-     */
-    public function createDataProvider(): array
-    {
-        return [
-            [null],
-            [''],
-            [$this->xmlSample]
-        ];
     }
 }
