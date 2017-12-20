f9986ece7255346fb94011ebf0892491f2c1b2e8
Andrii Lugovyi <alugovyi@magento.com>
2017-12-20 18:04:51 +0200

    MAGETWO-85755: Enable metrics validation and run benchmark in multithread mode for PAT

--- setup/src/Magento/Setup/Fixtures/ConfigurableProductsFixture.php
+++ setup/src/Magento/Setup/Fixtures/ConfigurableProductsFixture.php
@@ -192,7 +192,6 @@ class ConfigurableProductsFixture extends Fixture
         $this->websiteCategoryProvider = $websiteCategoryProvider;
         $this->priceProvider = $priceProvider;
         $this->swatchesGenerator = $swatchesGenerator;
-        $this->serializer = $serializer;
     }
 
     /**
