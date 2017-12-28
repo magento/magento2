f57602b6a004d34b5fa7c2a27274f02335477a5d
Patrick McLain <pmclain@somethingdigital.com>
2017-12-28 08:06:15 -0500

    Refactor InstantPurchaseOptionTest

--- app/code/Magento/InstantPurchase/Test/Unit/Model/InstantPurchaseOptionTest.php
+++ app/code/Magento/InstantPurchase/Test/Unit/Model/InstantPurchaseOptionTest.php
@@ -5,18 +5,12 @@ namespace Magento\InstantPurchase\Test\Unit\Model;
 use Magento\InstantPurchase\Model\InstantPurchaseOption;
 use PHPUnit\Framework\TestCase;
 use PHPUnit_Framework_MockObject_MockObject as MockObject;
-use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
 use Magento\Vault\Api\Data\PaymentTokenInterface;
 use Magento\Customer\Model\Address;
 use Magento\Quote\Api\Data\ShippingMethodInterface;
 
 class InstantPurchaseOptionTest extends TestCase
 {
-    /**
-     * @var ObjectManager
-     */
-    private $objectManager;
-
     /**
      * @var PaymentTokenInterface|MockObject
      */
@@ -34,58 +28,29 @@ class InstantPurchaseOptionTest extends TestCase
 
     protected function setUp()
     {
-        $this->objectManager = new ObjectManager($this);
-
-        $this->paymentTokenMock = $this->getMockBuilder(PaymentTokenInterface::class)
-            ->getMock();
-        $this->addressMock = $this->getMockBuilder(Address::class)
-            ->disableOriginalConstructor()
-            ->getMock();
-        $this->shippingMethodMock = $this->getMockBuilder(ShippingMethodInterface::class)
-            ->getMock();
+        $this->paymentTokenMock = $this->createMock(PaymentTokenInterface::class);
+        $this->addressMock = $this->createMock(Address::class);
+        $this->shippingMethodMock = $this->createMock(ShippingMethodInterface::class);
+        $this->shippingMethodMock->method('getAvailable')
+            ->willReturn(true);
     }
 
-    /**
-     * @param bool $available
-     * @dataProvider isAvailableDataProvider
-     */
-    public function testIsAvailable($available)
+    public function testIsAvailable()
     {
-        $option = $this->objectManager->getObject(
-            InstantPurchaseOption::class,
-            [
-                'paymentToken' => $this->paymentTokenMock,
-                'shippingAddress' => $this->addressMock,
-                'billingAddress' => $this->addressMock,
-                'shippingMethod' => $this->shippingMethodMock,
-            ]
+        $option = new InstantPurchaseOption(
+            $this->paymentTokenMock,
+            $this->addressMock,
+            $this->addressMock,
+            $this->shippingMethodMock
         );
 
-        $this->shippingMethodMock->expects($this->once())
-            ->method('getAvailable')
-            ->willReturn($available);
-
-        $this->assertEquals($available, $option->isAvailable());
+        $this->assertTrue($option->isAvailable());
     }
 
-    public function isAvailableDataProvider()
+    public function testIsAvailableFalse()
     {
-        return [
-            'available' => [true],
-            'unavailable' => [false],
-        ];
-    }
-
-    public function testGetPaymentToken()
-    {
-        $option = $this->objectManager->getObject(
-            InstantPurchaseOption::class,
-            [
-                'paymentToken' => $this->paymentTokenMock,
-            ]
-        );
-
-        $this->assertInstanceOf(PaymentTokenInterface::class, $option->getPaymentToken());
+        $option = new InstantPurchaseOption();
+        $this->assertFalse($option->isAvailable());
     }
 
     /**
@@ -93,26 +58,8 @@ class InstantPurchaseOptionTest extends TestCase
      */
     public function testGetPaymentTokenException()
     {
-        $option = $this->objectManager->getObject(
-            InstantPurchaseOption::class,
-            [
-                'paymentToken' => null,
-            ]
-        );
-
-        $this->assertInstanceOf(PaymentTokenInterface::class, $option->getPaymentToken());
-    }
-
-    public function testGetShippingAddress()
-    {
-        $option = $this->objectManager->getObject(
-            InstantPurchaseOption::class,
-            [
-                'shippingAddress' => $this->addressMock,
-            ]
-        );
-
-        $this->assertInstanceOf(Address::class, $option->getShippingAddress());
+        $option = new InstantPurchaseOption();
+        $option->getPaymentToken();
     }
 
     /**
@@ -120,26 +67,8 @@ class InstantPurchaseOptionTest extends TestCase
      */
     public function testGetShippingAddressException()
     {
-        $option = $this->objectManager->getObject(
-            InstantPurchaseOption::class,
-            [
-                'shippingAddress' => null,
-            ]
-        );
-
-        $this->assertInstanceOf(Address::class, $option->getShippingAddress());
-    }
-
-    public function testGetBillingAddress()
-    {
-        $option = $this->objectManager->getObject(
-            InstantPurchaseOption::class,
-            [
-                'billingAddress' => $this->addressMock,
-            ]
-        );
-
-        $this->assertInstanceOf(Address::class, $option->getBillingAddress());
+        $option = new InstantPurchaseOption();
+        $option->getShippingAddress();
     }
 
     /**
@@ -147,26 +76,8 @@ class InstantPurchaseOptionTest extends TestCase
      */
     public function testGetBillingAddressException()
     {
-        $option = $this->objectManager->getObject(
-            InstantPurchaseOption::class,
-            [
-                'billingAddress' => null,
-            ]
-        );
-
-        $this->assertInstanceOf(Address::class, $option->getBillingAddress());
-    }
-
-    public function testGetShippingMethod()
-    {
-        $option = $this->objectManager->getObject(
-            InstantPurchaseOption::class,
-            [
-                'shippingMethod' => $this->shippingMethodMock,
-            ]
-        );
-
-        $this->assertInstanceOf(ShippingMethodInterface::class, $option->getShippingMethod());
+        $option = new InstantPurchaseOption();
+        $option->getBillingAddress();
     }
 
     /**
@@ -174,13 +85,7 @@ class InstantPurchaseOptionTest extends TestCase
      */
     public function testGetShippingMethodException()
     {
-        $option = $this->objectManager->getObject(
-            InstantPurchaseOption::class,
-            [
-                'shippingMethod' => null,
-            ]
-        );
-
-        $this->assertInstanceOf(ShippingMethodInterface::class, $option->getShippingMethod());
+        $option = new InstantPurchaseOption();
+        $option->getShippingMethod();
     }
 }
