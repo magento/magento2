20b9575ba10c9de8fc26b550fb3b675bd72d766a
Renon Stewart <srenon@users.noreply.github.com>
2017-12-29 19:22:30 -0500

    Update FinalPriceBox.php

--- app/code/Magento/Catalog/Pricing/Render/FinalPriceBox.php
+++ app/code/Magento/Catalog/Pricing/Render/FinalPriceBox.php
@@ -115,7 +115,7 @@ class FinalPriceBox extends BasePriceBox
     {
         return '<div class="price-box ' . $this->getData('css_classes') . '" ' .
             'data-role="priceBox" ' .
-            'data-product-id="' . $this->getSaleableItem()->getId() . '"' .
+            'data-product-id="' . $this->getSaleableItem()->getId() . '" ' .
             'data-price-box="product-id-' . $this->getSaleableItem()->getId() . '"' .
             '>' . $html . '</div>';
     }
