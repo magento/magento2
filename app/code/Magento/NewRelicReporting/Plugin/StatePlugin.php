4d8c70224079604ce56b484c70f90552a85df450
Max Chadwick <mpchadwick@gmail.com>
2017-12-30 22:41:26 -0500

    Add mechanics for separate appnames

---
+++ app/code/Magento/NewRelicReporting/Plugin/StatePlugin.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\NewRelicReporting\Plugin;
+
+use Magento\Framework\App\State;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\NewRelicReporting\Model\Config;
+use Magento\NewRelicReporting\Model\NewRelicWrapper;
+
+class StatePlugin
+{
+    /**
+     * @var Config
+     */
+    private $config;
+
+    /**
+     * @var NewRelicWrapper
+     */
+    private $newRelicWrapper;
+
+    /**
+     * @param Config $config
+     * @param NewRelicWrapper $newRelicWrapper
+     */
+    public function __construct(
+        Config $config,
+        NewRelicWrapper $newRelicWrapper
+    ) {
+        $this->config = $config;
+        $this->newRelicWrapper = $newRelicWrapper;
+    }
+
+    /**
+     * Set separate appname
+     *
+     * @param State $subject
+     * @param null $result
+     * @return void
+     *
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function afterSetAreaCode(State $state, $result)
+    {
+        if (!$this->shouldSetAppName()) {
+            return;
+        }
+
+        try {
+            $this->newRelicWrapper->setAppName($this->appName($state));
+        } catch (LocalizedException $e) {
+            return;
+        }
+    }
+
+    private function appName(State $state)
+    {
+        $code = $state->getAreaCode();
+        $current = $this->config->getNewRelicAppName();
+
+        return $current . ';' . $current . '_' . $code;
+    }
+
+    private function shouldSetAppName()
+    {
+        if (!$this->config->isNewRelicEnabled()) {
+            return false;
+        }
+
+        if (!$this->config->getNewRelicAppName()) {
+            return false;
+        }
+
+        if (!$this->config->isSeparateApps()) {
+            return false;
+        }
+
+        return true;
+    }
+}
