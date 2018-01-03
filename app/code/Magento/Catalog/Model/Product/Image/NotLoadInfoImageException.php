3a99ed6a5039c4572d7c25a4960a910e6d21404e
Andrii Lugovyi <alugovyi@magento.com>
2018-01-03 13:09:30 +0200

    MAGETWO-85755: Enable metrics validation and run benchmark in multithread mode for PAT

--- app/code/Magento/Catalog/Model/Product/Image/NotLoadInfoImageException.php
+++ app/code/Magento/Catalog/Model/Product/Image/NotLoadInfoImageException.php
@@ -1,8 +1,8 @@
 <?php
 /**
-* Copyright © 2013-2017 Magento, Inc. All rights reserved.
-* See COPYING.txt for license details.
-*/
+ * Copyright © 2013-2017 Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
 namespace Magento\Catalog\Model\Product\Image;
 
 class NotLoadInfoImageException extends \Exception
