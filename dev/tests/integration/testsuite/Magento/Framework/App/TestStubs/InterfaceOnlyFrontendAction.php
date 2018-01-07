6447d89cbe09be367d4ff7e90b274cc65c1c2b19
Vinai Kopp <vinai@netzarbeiter.com>
2018-01-07 14:42:46 +0530

    Remove LoD violation which prohibits non inheritance based action controllers

--- dev/tests/integration/testsuite/Magento/Framework/App/TestStubs/InterfaceOnlyFrontendAction.php
+++ dev/tests/integration/testsuite/Magento/Framework/App/TestStubs/InterfaceOnlyFrontendAction.php
@@ -3,7 +3,6 @@
 namespace Magento\Framework\App\TestStubs;
 
 use Magento\Framework\App\ActionInterface;
-use Magento\Framework\App\RequestInterface;
 use Magento\Framework\View\Result\PageFactory;
 
 class InterfaceOnlyFrontendAction implements ActionInterface
@@ -13,30 +12,13 @@ class InterfaceOnlyFrontendAction implements ActionInterface
      */
     private $pageFactory;
 
-    /**
-     * @var RequestInterface
-     */
-    private $request;
-
-    public function __construct(PageFactory $pageFactory, RequestInterface $request)
+    public function __construct(PageFactory $pageFactory)
     {
         $this->pageFactory = $pageFactory;
-        $this->request = $request;
     }
 
     public function execute()
     {
         return $this->pageFactory->create();
-    }
-
-    /**
-     * This method is a workaround for the interface violation where core code expects
-     * actions to extend the AbstractAction :(((((
-     * 
-     * @return RequestInterface
-     */
-    public function getRequest(): RequestInterface
-    {
-        return $this->request;
     }
 }
