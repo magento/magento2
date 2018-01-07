b85349745c4a593306edf61dae2235cd96a0fcd2
Amit Bera <dev.amitbera@gmail.com>
2018-01-07 13:47:14 +0530

    Fixing the  code style issue. #12787

--- app/code/Magento/Newsletter/Setup/UpgradeSchema.php
+++ app/code/Magento/Newsletter/Setup/UpgradeSchema.php
@@ -1,22 +1,21 @@
 <?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
 namespace Magento\Newsletter\Setup;
-
 
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\SchemaSetupInterface;
 use Magento\Framework\Setup\UpgradeSchemaInterface;
+
 /**
  * Upgrade the Newsletter module DB scheme
  */
 class UpgradeSchema implements  UpgradeSchemaInterface
 {
-
     /**
-     * Upgrades DB schema for a module
-     *
-     * @param SchemaSetupInterface $setup
-     * @param ModuleContextInterface $context
-     * @return void
+     * {@inheritdoc}
      */
     public function upgrade(SchemaSetupInterface $setup, ModuleContextInterface $context)
     {
