3135552991bbdad8fa5341985faa021473babcba
josh-carter <josh@webtise.com>
2018-01-08 20:06:08 +0000

    Add Validation to Sku Validator to make sure product with SKU exists

--- app/code/Magento/InventoryImportExport/Model/Import/Validator/SkuValidator.php
+++ app/code/Magento/InventoryImportExport/Model/Import/Validator/SkuValidator.php
@@ -7,7 +7,8 @@ declare(strict_types=1);
 
 namespace Magento\InventoryImportExport\Model\Import\Validator;
 
-use Magento\Framework\Exception\LocalizedException;
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\Validation\ValidationResultFactory;
 use Magento\InventoryImportExport\Model\Import\Sources;
 
@@ -17,16 +18,24 @@ use Magento\InventoryImportExport\Model\Import\Sources;
 class SkuValidator implements ValidatorInterface
 {
     /**
+     * @var ProductRepositoryInterface
+     */
+    private $productRepository;
+
+    /**
      * @var ValidationResultFactory
      */
     private $validationResultFactory;
 
     /**
+     * @param ProductRepositoryInterface $productRepository
      * @param ValidationResultFactory $validationResultFactory
-     * @throws LocalizedException
      */
-    public function __construct(ValidationResultFactory $validationResultFactory)
-    {
+    public function __construct(
+        ProductRepositoryInterface $productRepository,
+        ValidationResultFactory $validationResultFactory
+    ) {
+        $this->productRepository = $productRepository;
         $this->validationResultFactory = $validationResultFactory;
     }
 
@@ -39,8 +48,27 @@ class SkuValidator implements ValidatorInterface
 
         if (!isset($rowData[Sources::COL_SKU])) {
             $errors[] = __('Missing required column "%column"', ['column' => Sources::COL_SKU]);
+        } elseif (!$this->isValidSku($rowData[Sources::COL_SKU])) {
+            $errors[] = __('Product with SKU "%sku" does not exist', ['sku' => $rowData[Sources::COL_SKU]]);
         }
 
         return $this->validationResultFactory->create(['errors' => $errors]);
+    }
+
+    /**
+     * Attempt to get Product via Repository using SKU catch NoSuchEntityException
+     * If caught product doesn't exist so return false otherwise return true
+     *
+     * @param $sku
+     * @return bool
+     */
+    private function isValidSku($sku)
+    {
+        try {
+            $this->productRepository->get($sku);
+        } catch (NoSuchEntityException $e) {
+            return false;
+        }
+        return true;
     }
 }
