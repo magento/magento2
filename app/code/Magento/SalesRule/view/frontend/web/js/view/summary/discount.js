1721ff185412d7aa04f45bb68717b2083b5b7769
Ihor Sviziev <svizev.igor@gmail.com>
2018-01-11 18:32:36 +0200

    Fix missing discount label in checkout
    (cherry picked from commit 976182b)

--- app/code/Magento/SalesRule/view/frontend/web/js/view/summary/discount.js
+++ app/code/Magento/SalesRule/view/frontend/web/js/view/summary/discount.js
@@ -45,6 +45,23 @@ define([
         },
 
         /**
+         * Get discount title
+         *
+         * @returns {null|string}
+         */
+        getTitle: function () {
+            if (!this.totals()) {
+                return null;
+            }
+
+            var discountSegments = this.totals()['total_segments'].filter(function (segment) {
+                return (segment.code === 'discount');
+            });
+
+            return discountSegments.length ? discountSegments[0].title : null;
+        },
+
+        /**
          * @return {Number}
          */
         getPureValue: function () {
