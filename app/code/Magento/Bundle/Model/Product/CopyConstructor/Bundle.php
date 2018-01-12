d8bd657c94175392eca5ad462932bc2e86c51257
nmalevanec <mikola.malevanec@transoftgroup.com>
2018-01-12 16:48:44 +0200

    magento/magento2#13126: 2.2.2 - Duplicating Bundle Product Removes Bundle Options From Original Product

--- app/code/Magento/Bundle/Model/Product/CopyConstructor/Bundle.php
+++ app/code/Magento/Bundle/Model/Product/CopyConstructor/Bundle.php
@@ -32,7 +32,8 @@ class Bundle implements \Magento\Catalog\Model\Product\CopyConstructorInterface
              * Set option and selection ids to 'null' in order to create new option(selection) for duplicated product,
              * but not modifying existing one, which led to lost of option(selection) in original product.
              */
-            foreach ($duplicatedBundleOption->getProductLinks() as $productLink) {
+            $productLinks = $duplicatedBundleOption->getProductLinks() ?: [];
+            foreach ($productLinks as $productLink) {
                 $productLink->setSelectionId(null);
             }
             $duplicatedBundleOption->setOptionId(null);
