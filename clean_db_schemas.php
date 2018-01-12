35c872e9786ffa77e50ccbba9443680f866abeff
Sergii Kovalenko <skovalenko@magento.com>
2018-01-12 15:57:16 +0200

    MAGETWO-85326: Tool for generating schema
    --working on CE stuff

--- clean_db_schemas.php
+++ clean_db_schemas.php
@@ -45,7 +45,7 @@ $adapter->query('DROP DATABASE ' . $resourceConnection->getSchemaName('default')
 $adapter->query('CREATE DATABASE ' . $resourceConnection->getSchemaName('default'));
 
 foreach ($componentRegistrar->getPaths(\Magento\Framework\Component\ComponentRegistrar::MODULE) as $path) {
-    if (strpos($path, 'magento2b2b') !== false) {
+    if (strpos($path, 'magento2ee') !== false) {
         @unlink($path . DIRECTORY_SEPARATOR . 'etc/db_schema.xml');
     }
 }
