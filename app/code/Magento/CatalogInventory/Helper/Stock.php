d6e415ba66869be64f9ff5296bb7929fca339601
hitarthpattani <hitarthpattani@gmail.com>
2018-01-12 18:25:01 +0530

    white space remove from line 150

--- app/code/Magento/CatalogInventory/Helper/Stock.php
+++ app/code/Magento/CatalogInventory/Helper/Stock.php
@@ -147,7 +147,7 @@ class Stock
      */
     public function addIsInStockFilterToCollection($collection)
     {
-        $stockFlag = 'has_stock_status_filter'; 
+        $stockFlag = 'has_stock_status_filter';
         $childFlag = 'product_children';
         if (!$collection->hasFlag($stockFlag) && !$collection->hasFlag($childFlag)) {
             $isShowOutOfStock = $this->scopeConfig->getValue(
