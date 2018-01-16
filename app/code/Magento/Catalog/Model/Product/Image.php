f31754dab40d308dea4b0cac5b087d255de27525
Stanislav Lopukhov <slopukhov@magento.com>
2018-01-16 16:49:59 +0200

    MAGETWO-84480: Add cache for getimagesize() function for product images

--- app/code/Magento/Catalog/Model/Product/Image.php
+++ app/code/Magento/Catalog/Model/Product/Image.php
@@ -179,8 +179,6 @@ class Image extends \Magento\Framework\Model\AbstractModel
     private $cachePrefix = 'IMG_INFO';
 
     /**
-     * serializer Serializer Instance
-     *
      * @var SerializerInterface
      */
     private $serializer;
