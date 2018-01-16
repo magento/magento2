4f3e87fdd5826ac5f6f26c458201dce9b252ff44
Valeriy Nayda <vnayda@magento.com>
2018-01-16 18:13:42 +0200

    MSI: Introduce InventoryIndex module #417
    - CR

---
+++ app/code/Magento/InventoryIndexer/Indexer/Stock/GetAllAssignedStockIds.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\InventoryIndexer\Indexer\Stock;
+
+use Magento\Framework\App\ResourceConnection;
+use Magento\Inventory\Model\ResourceModel\StockSourceLink as StockSourceLinkResourceModel;
+use Magento\Inventory\Model\StockSourceLink;
+
+/**
+ * Returns all assigned stock ids
+ */
+class GetAllAssignedStockIds
+{
+    /**
+     * @var ResourceConnection
+     */
+    private $resourceConnection;
+
+    /**
+     * @param ResourceConnection $resourceConnection
+     */
+    public function __construct(ResourceConnection $resourceConnection)
+    {
+        $this->resourceConnection = $resourceConnection;
+    }
+
+    /**
+     * @return int[]
+     */
+    public function execute(): array
+    {
+        $connection = $this->resourceConnection->getConnection();
+        $sourceStockLinkTable = $this->resourceConnection->getTableName(
+            StockSourceLinkResourceModel::TABLE_NAME_STOCK_SOURCE_LINK
+        );
+
+        $select = $connection
+            ->select()
+            ->from($sourceStockLinkTable, StockSourceLink::STOCK_ID)
+            ->group(StockSourceLink::STOCK_ID);
+
+        $stockIds = $connection->fetchCol($select);
+        $stockIds = array_map('intval', $stockIds);
+        return $stockIds;
+    }
+}
