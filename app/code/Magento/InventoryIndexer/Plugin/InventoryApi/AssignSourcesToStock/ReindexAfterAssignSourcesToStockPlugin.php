de505dd7db2b49af098a358753b3612b48d065ec
Roman Glushko <weysman@atwix.com>
2018-01-18 01:51:53 +0200

    #347 Reassigned plugins to new interfaces

--- app/code/Magento/InventoryIndexer/Plugin/InventoryApi/AssignSourcesToStock/ReindexAfterAssignSourcesToStockPlugin.php
+++ app/code/Magento/InventoryIndexer/Plugin/InventoryApi/AssignSourcesToStock/ReindexAfterAssignSourcesToStockPlugin.php
@@ -8,8 +8,8 @@ declare(strict_types=1);
 namespace Magento\InventoryIndexer\Plugin\InventoryApi\AssignSourcesToStock;
 
 use Magento\Framework\Indexer\IndexerRegistry;
+use Magento\InventoryApi\Api\StockSourceLinksSaveInterface;
 use Magento\InventoryIndexer\Indexer\Stock\StockIndexer;
-use Magento\InventoryApi\Api\AssignSourcesToStockInterface;
 
 /**
  * Invalidate StockIndexer
@@ -30,11 +30,11 @@ class ReindexAfterAssignSourcesToStockPlugin
     }
 
     /**
-     * @param AssignSourcesToStockInterface $subject
+     * @param StockSourceLinksSaveInterface $subject
      * @return void
      * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
-    public function afterExecute(AssignSourcesToStockInterface $subject)
+    public function afterExecute(StockSourceLinksSaveInterface $subject)
     {
         $indexer = $this->indexerRegistry->get(StockIndexer::INDEXER_ID);
         if ($indexer->isValid()) {
--- app/code/Magento/InventoryIndexer/Plugin/InventoryApi/UnassignSourceFromStock/ReindexAfterUnassignSourcesFromStockPlugin.php
+++ app/code/Magento/InventoryIndexer/Plugin/InventoryApi/UnassignSourceFromStock/ReindexAfterUnassignSourcesFromStockPlugin.php
@@ -8,8 +8,8 @@ declare(strict_types=1);
 namespace Magento\InventoryIndexer\Plugin\InventoryApi\UnassignSourceFromStock;
 
 use Magento\Framework\Indexer\IndexerRegistry;
+use Magento\InventoryApi\Api\StockSourceLinksDeleteInterface;
 use Magento\InventoryIndexer\Indexer\Stock\StockIndexer;
-use Magento\InventoryApi\Api\UnassignSourceFromStockInterface;
 
 /**
  * Invalidate StockIndexer
@@ -30,11 +30,11 @@ class ReindexAfterUnassignSourcesFromStockPlugin
     }
 
     /**
-     * @param UnassignSourceFromStockInterface $subject
+     * @param StockSourceLinksDeleteInterface $subject
      * @return void
      * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
-    public function afterExecute(UnassignSourceFromStockInterface $subject)
+    public function afterExecute(StockSourceLinksDeleteInterface $subject)
     {
         $indexer = $this->indexerRegistry->get(StockIndexer::INDEXER_ID);
         if ($indexer->isValid()) {
