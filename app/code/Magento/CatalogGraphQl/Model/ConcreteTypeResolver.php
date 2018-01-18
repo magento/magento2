6c9f3b72930b3167047c52a9f4c6c02df16b30d6
Eric Bohanon <ebohanon@magento.com>
2018-01-18 11:40:23 -0600

    MAGETWO-85687: Add docblocks to type resolver implementations.

--- app/code/Magento/CatalogGraphQl/Model/ConcreteTypeResolver.php
+++ app/code/Magento/CatalogGraphQl/Model/ConcreteTypeResolver.php
@@ -9,6 +9,9 @@ namespace Magento\CatalogGraphQl\Model;
 use Magento\Framework\GraphQl\Config\Data\TypeResolverInterface;
 use Magento\Framework\Exception\InputException;
 
+/**
+ * {@inheritdoc}
+ */
 class ConcreteTypeResolver implements TypeResolverInterface
 {
     /**
