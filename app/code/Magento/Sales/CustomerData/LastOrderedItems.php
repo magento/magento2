f051af59aa8a2c5824257ab057e4861309ae4b49
Victor Rad <vrad@magento.com>
2018-01-23 11:30:41 +0200

    MAGETWO-75840: Recently Ordered block displays only one of two products if they were bought from different store views

--- app/code/Magento/Sales/CustomerData/LastOrderedItems.php
+++ app/code/Magento/Sales/CustomerData/LastOrderedItems.php
@@ -6,6 +6,7 @@
 namespace Magento\Sales\CustomerData;
 
 use Magento\Customer\CustomerData\SectionSourceInterface;
+use Magento\Catalog\Api\ProductRepositoryInterface;
 
 /**
  * Returns information for "Recently Ordered" widget.
@@ -55,24 +56,33 @@ class LastOrderedItems implements SectionSourceInterface
     private $_storeManager;
 
     /**
+     * @var \Magento\Catalog\Api\ProductRepositoryInterface
+     */
+    private $productRepository;
+
+    /**
      * @param \Magento\Sales\Model\ResourceModel\Order\CollectionFactory $orderCollectionFactory
      * @param \Magento\Sales\Model\Order\Config $orderConfig
      * @param \Magento\Customer\Model\Session $customerSession
      * @param \Magento\CatalogInventory\Api\StockRegistryInterface $stockRegistry
      * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+     * @param ProductRepositoryInterface $productRepository
      */
     public function __construct(
         \Magento\Sales\Model\ResourceModel\Order\CollectionFactory $orderCollectionFactory,
         \Magento\Sales\Model\Order\Config $orderConfig,
         \Magento\Customer\Model\Session $customerSession,
         \Magento\CatalogInventory\Api\StockRegistryInterface $stockRegistry,
-        \Magento\Store\Model\StoreManagerInterface $storeManager
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        ProductRepositoryInterface $productRepository = null
     ) {
         $this->_orderCollectionFactory = $orderCollectionFactory;
         $this->_orderConfig = $orderConfig;
         $this->_customerSession = $customerSession;
         $this->stockRegistry = $stockRegistry;
         $this->_storeManager = $storeManager;
+        $this->productRepository = $productRepository
+            ?: \Magento\Framework\App\ObjectManager::getInstance()->get(ProductRepositoryInterface::class);
     }
 
     /**
@@ -108,11 +118,16 @@ class LastOrderedItems implements SectionSourceInterface
             $website = $this->_storeManager->getStore()->getWebsiteId();
             /** @var \Magento\Sales\Model\Order\Item $item */
             foreach ($order->getParentItemsRandomCollection($limit) as $item) {
-                if ($item->hasData('product') && in_array($website, $item->getProduct()->getWebsiteIds())) {
+                /** @var \Magento\Catalog\Model\Product $product */
+                $product = $this->productRepository->getById(
+                    $item->getProductId(), false, $this->_storeManager->getStore()->getId()
+                );
+                if ($product && in_array($website, $product->getWebsiteIds())) {
+                    $url = $product->isVisibleInSiteVisibility() ? $product->getProductUrl() : null;
                     $items[] = [
                         'id' => $item->getId(),
                         'name' => $item->getName(),
-                        'url' => $item->getProduct()->getProductUrl(),
+                        'url' => $url,
                         'is_saleable' => $this->isItemAvailableForReorder($item),
                     ];
                 }
