1336bb01c71f036f79606a0249dda29f9d0ae959
Valeriy Nayda <vnayda@magento.com>
2018-01-25 14:39:39 +0200

    MSI: 385: Adapt `\Magento\CatalogInventory\Helper\Stock::assignStatusToProduct`

---
+++ app/code/Magento/InventoryCatalog/Plugin/CatalogInventory/Helper/Stock/AdaptAssignStatusToProductPlugin.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\InventoryCatalog\Plugin\CatalogInventory\Helper\Stock;
+
+use Magento\Catalog\Model\Product;
+use Magento\CatalogInventory\Helper\Stock;
+use Magento\InventoryApi\Api\IsProductInStockInterface;
+use Magento\InventoryCatalog\Model\GetStockIdForCurrentWebsite;
+
+/**
+ * Adapt assignStatusToProduct for multi stocks.
+ */
+class AdaptAssignStatusToProductPlugin
+{
+    /**
+     * @var GetStockIdForCurrentWebsite
+     */
+    private $getStockIdForCurrentWebsite;
+
+    /**
+     * @var IsProductInStockInterface
+     */
+    private $isProductInStock;
+
+    /**
+     * @param GetStockIdForCurrentWebsite $getStockIdForCurrentWebsite
+     * @param IsProductInStockInterface $isProductInStock
+     */
+    public function __construct(
+        GetStockIdForCurrentWebsite $getStockIdForCurrentWebsite,
+        IsProductInStockInterface $isProductInStock
+    ) {
+        $this->getStockIdForCurrentWebsite = $getStockIdForCurrentWebsite;
+        $this->isProductInStock = $isProductInStock;
+    }
+
+    /**
+     * @param Stock $subject
+     * @param callable $proceed
+     * @param Product $product
+     * @param int|null $status
+     * @return void
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function aroundAssignStatusToProduct(
+        Stock $subject,
+        callable $proceed,
+        Product $product,
+        $status = null
+    ) {
+        if (null === $status) {
+            $stockId = $this->getStockIdForCurrentWebsite->execute();
+            $status = (int)$this->isProductInStock->execute($product->getSku(), $stockId);
+        }
+        $proceed($product, $status);
+    }
+}
