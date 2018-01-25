8c4859af09554640abfe0550effcec8d59f94da1
Cristian Partica <cpartica@magento.com>
2018-01-25 09:44:41 -0600

    MAGETWO-87004: Graphql Downloadable product implementation
    - refactor

--- app/code/Magento/DownloadableGraphQl/Model/Resolver/Products/Query/DownloadableProductPostProcessor.php
+++ app/code/Magento/DownloadableGraphQl/Model/Resolver/Products/Query/DownloadableProductPostProcessor.php
@@ -7,6 +7,7 @@
 namespace Magento\DownloadableGraphQl\Model\Resolver\Products\Query;
 
 use Magento\Downloadable\Model\Product\Type as Downloadable;
+use Magento\Framework\Data\Collection;
 
 /**
  * Retrieves simple product data for child products, and formats configurable data
@@ -24,50 +25,12 @@ class DownloadableProductPostProcessor implements \Magento\Framework\GraphQl\Que
         foreach ($resultData as $productKey => $product) {
             if ($product['type_id'] === Downloadable::TYPE_DOWNLOADABLE) {
                 if (isset($product['downloadable_product_samples'])) {
-                    $samples = $product['downloadable_product_samples'];
-                    $resultData[$productKey]['downloadable_product_samples'] = [];
-                    
-                    foreach ($samples as $sampleKey => $sample) {
-                        /** @var \Magento\Downloadable\Model\Sample $sample */
-                        $resultData[$productKey]['downloadable_product_samples'][$sampleKey]['id']
-                            = $sample->getId();
-                        $resultData[$productKey]['downloadable_product_samples'][$sampleKey]['title']
-                            = $sample->getTitle();
-                        $resultData[$productKey]['downloadable_product_samples'][$sampleKey]['sort_order']
-                            = $sample->getSortOrder();
-                        $resultData[$productKey]['downloadable_product_samples'][$sampleKey]['sample_type']
-                            = $sample->getSampleType();
-                        $resultData[$productKey]['downloadable_product_samples'][$sampleKey]['sample_file']
-                            = $sample->getSampleFile();
-                        $resultData[$productKey]['downloadable_product_samples'][$sampleKey]['sample_url']
-                            = $sample->getSampleUrl();
-                    }
-
-                    $links = $product['downloadable_product_links'];
-                    $resultData[$productKey]['downloadable_product_links'] = [];
-                    foreach ($links as $linkKey => $link) {
-                        /** @var \Magento\Downloadable\Model\Link $link */
-                        $resultData[$productKey]['downloadable_product_links'][$linkKey]['id']
-                            = $link->getId();
-                        $resultData[$productKey]['downloadable_product_links'][$linkKey]['title']
-                            = $link->getTitle();
-                        $resultData[$productKey]['downloadable_product_links'][$linkKey]['sort_order']
-                            = $link->getSortOrder();
-                        $resultData[$productKey]['downloadable_product_links'][$linkKey]['is_shareable']
-                            = $link->getIsShareable();
-                        $resultData[$productKey]['downloadable_product_links'][$linkKey]['price']
-                            = $link->getPrice();
-                        $resultData[$productKey]['downloadable_product_links'][$linkKey]['number_of_downloads']
-                            = $link->getNumberOfDownloads();
-                        $resultData[$productKey]['downloadable_product_links'][$linkKey]['link_type']
-                            = $link->getLinkType();
-                        $resultData[$productKey]['downloadable_product_links'][$linkKey]['sample_type']
-                            = $link->getSampleType();
-                        $resultData[$productKey]['downloadable_product_links'][$linkKey]['sample_file']
-                            = $link->getSampleFile();
-                        $resultData[$productKey]['downloadable_product_links'][$linkKey]['sample_url']
-                            = $link->getSampleUrl();
-                    }
+                    $resultData[$productKey]['downloadable_product_samples']
+                        = $this->formatSamples($product['downloadable_product_samples']);
+                }
+                if (isset($product['downloadable_product_links'])) {
+                    $resultData[$productKey]['downloadable_product_links']
+                        = $this->formatLinks($product['downloadable_product_links']);
                 }
             }
         }
@@ -76,10 +39,12 @@ class DownloadableProductPostProcessor implements \Magento\Framework\GraphQl\Que
     }
 
     /**
-     * @param \Magento\Downloadable\Model\Link[] $links
+     * Format links from collection as array
+     *
+     * @param Collection $links
      * @return array
      */
-    private function formatLinks(array $links)
+    private function formatLinks(Collection $links)
     {
         $resultData = [];
         foreach ($links as $linkKey => $link) {
@@ -94,6 +59,33 @@ class DownloadableProductPostProcessor implements \Magento\Framework\GraphQl\Que
             $resultData[$linkKey]['sample_type'] = $link->getSampleType();
             $resultData[$linkKey]['sample_file'] = $link->getSampleFile();
             $resultData[$linkKey]['sample_url'] = $link->getSampleUrl();
+        }
+        return $resultData;
+    }
+
+    /**
+     * Format links from collection as array
+     *
+     * @param Collection $samples
+     * @return array
+     */
+    private function formatSamples(Collection $samples)
+    {
+        $resultData = [];
+        foreach ($samples as $sampleKey => $sample) {
+            /** @var \Magento\Downloadable\Model\Sample $sample */
+            $resultData[$sampleKey]['id']
+                = $sample->getId();
+            $resultData[$sampleKey]['title']
+                = $sample->getTitle();
+            $resultData[$sampleKey]['sort_order']
+                = $sample->getSortOrder();
+            $resultData[$sampleKey]['sample_type']
+                = $sample->getSampleType();
+            $resultData[$sampleKey]['sample_file']
+                = $sample->getSampleFile();
+            $resultData[$sampleKey]['sample_url']
+                = $sample->getSampleUrl();
         }
         return $resultData;
     }
