0d5d21bbf3da32b99c455f86ac60053295650b34
vitaliyboyko <vitaliyboyko@i.ua>
2018-01-26 14:39:31 +0200

    Fixed controller test and added plugin to prevent saving bundle product source items

--- app/code/Magento/InventoryBundle/Test/Integration/Catalog/Controller/Adminhtml/ProductControllerSaveActionTest.php
+++ app/code/Magento/InventoryBundle/Test/Integration/Catalog/Controller/Adminhtml/ProductControllerSaveActionTest.php
@@ -10,8 +10,10 @@ namespace Magento\InventoryBundle\Test\Integration\Catalog\Controller\Adminhtml;
 use Magento\Catalog\Api\Data\ProductInterface;
 use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Catalog\Model\Product\Type;
+use Magento\Framework\Api\SearchCriteria;
+use Magento\Framework\Api\SearchCriteriaBuilder;
+use Magento\InventoryApi\Api\SourceItemRepositoryInterface;
 use Magento\TestFramework\TestCase\AbstractBackendController;
-use Magento\Bundle\Api\Data\LinkInterface;
 
 /**
  * @magentoAppArea adminhtml
@@ -25,10 +27,22 @@ class ProductControllerSaveActionTest extends AbstractBackendController
      */
     private $productRepository;
 
+    /**
+     * @var SourceItemRepositoryInterface
+     */
+    private $sourceItemRepository;
+
+    /**
+     * @var SearchCriteriaBuilder
+     */
+    private $searchCriteriaBuilder;
+
     protected function setUp()
     {
         parent::setUp();
         $this->productRepository = $this->_objectManager->create(ProductRepositoryInterface::class);
+        $this->sourceItemRepository = $this->_objectManager->create(SourceItemRepositoryInterface::class);
+        $this->searchCriteriaBuilder = $this->_objectManager->create(SearchCriteriaBuilder::class);
     }
 
     protected function tearDown()
@@ -40,45 +54,9 @@ class ProductControllerSaveActionTest extends AbstractBackendController
     /**
      * @magentoDataFixture ../../../../app/code/Magento/InventoryApi/Test/_files/products.php
      */
-    public function testIsBundleProductWithSimpleProductInStockAfterCreate()
+    public function testBundleProductShouldBeInStockOnCreateButShouldNotProcessSourceItems()
     {
-        $bundleProductOptions = [
-            [
-                "title" => "Test simple",
-                "type" => "select",
-                "required" => false,
-                "product_links" => [
-                    [
-                        "sku" => 'SKU-1',
-                        "qty" => 1.5,
-                        'is_default' => false,
-                        'price' => 1.0,
-                        'price_type' => LinkInterface::PRICE_TYPE_FIXED,
-                    ],
-                ],
-            ],
-        ];
-        $product = [
-            "sku" => self::BUNDLE_PRODUCT_SKU,
-            "name" => 'bundle product',
-            "type_id" => Type::TYPE_BUNDLE,
-            "price" => 50,
-            'attribute_set_id' => 4,
-            "custom_attributes" => [
-                "price_type" => [
-                    'attribute_code' => 'price_type',
-                    'value' => \Magento\Bundle\Model\Product\Price::PRICE_TYPE_FIXED
-                ],
-                "price_view" => [
-                    "attribute_code" => "price_view",
-                    "value" => "1",
-                ],
-            ],
-            "extension_attributes" => [
-                "bundle_product_options" => $bundleProductOptions,
-            ],
-        ];
-        $this->getRequest()->setPostValue(['product' => $product, 'back' => 'new']);
+        $this->getRequest()->setPostValue($this->prepareFormData());
 
         $this->dispatch('backend/catalog/product/save');
 
@@ -89,5 +67,56 @@ class ProductControllerSaveActionTest extends AbstractBackendController
         /** @var ProductInterface  $product */
         $product = $this->productRepository->get(self::BUNDLE_PRODUCT_SKU);
         self::assertEquals(1, $product->getExtensionAttributes()->getStockItem()->getIsInStock());
+        /** @var SearchCriteria $searchCriteria */
+        $searchCriteria = $this->searchCriteriaBuilder->addFilter('sku', self::BUNDLE_PRODUCT_SKU)->create();
+        $sourceItemsCount = $this->sourceItemRepository->getList($searchCriteria)->getTotalCount();
+        self::assertEquals(0, $sourceItemsCount);
+    }
+
+    /**
+     * @return array
+     */
+    private function prepareFormData(): array
+    {
+        $product = [
+            "sku" => self::BUNDLE_PRODUCT_SKU,
+            "name" => 'bundle product',
+            "price" => 50,
+            'attribute_set_id' => 4,
+            'type_id' => Type::TYPE_BUNDLE
+        ];
+        /** @var ProductInterface $simpleProduct */
+        $simpleProduct = $this->productRepository->get('SKU-1');
+        $bundleProductOptions = [
+            [
+                "bundle_options" => [
+                    [
+                        0 => [
+                            'title' => 'Bundle Product Items',
+                            'default_title' => 'Bundle Product Items',
+                            'type' => 'select',
+                            'required' => 1,
+                            'delete' => '',
+                            'bundle_selections' => [
+                                0 => [
+                                    'product_id' => $simpleProduct->getId(),
+                                    'selection_qty' => 1,
+                                    'selection_can_change_qty' => 1,
+                                    'delete' => '',
+                                ]
+                            ]
+                        ]
+                    ],
+                ],
+            ],
+        ];
+        $formData = [
+            'product' => $product,
+            'back' => 'new',
+            'affect_bundle_product_selections' => 1,
+            'bundle_options' => $bundleProductOptions
+        ];
+
+        return $formData;
     }
 }
