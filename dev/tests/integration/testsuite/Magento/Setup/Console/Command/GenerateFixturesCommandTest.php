64abd5debb1e7f401ab94f2dcfc4546df5fc0ab5
Andrii Lugovyi <alugovyi@magento.com>
2018-01-26 22:16:04 +0200

    MAGETWO-84967: Set auto increment to 3 for Performance test plan

--- dev/tests/integration/testsuite/Magento/Setup/Console/Command/GenerateFixturesCommandTest.php
+++ dev/tests/integration/testsuite/Magento/Setup/Console/Command/GenerateFixturesCommandTest.php
@@ -6,7 +6,7 @@
 namespace Magento\Setup\Console\Command;
 
 use Magento\Framework\Console\Cli;
-use Magento\Setup\Console\Command\GenerateFixturesCommand;
+use Magento\Indexer\Console\Command\IndexerReindexCommand;
 use Magento\TestFramework\Helper\Bootstrap;
 use Symfony\Component\Console\Tester\CommandTester;
 
@@ -16,6 +16,7 @@ use Symfony\Component\Console\Tester\CommandTester;
  */
 class GenerateFixturesCommandTest extends \PHPUnit\Framework\TestCase
 {
+    private $fixtureModelMock;
     private $objectManager;
     /**
      * @var GenerateFixturesCommand
@@ -33,16 +34,70 @@ class GenerateFixturesCommandTest extends \PHPUnit\Framework\TestCase
     public function setUp()
     {
         $this->objectManager = Bootstrap::getObjectManager();
-        $this->command = $this->objectManager->create(GenerateFixturesCommand::class);
+
+        $this->fixtureModelMock = $this->getMockBuilder(\Magento\Setup\Fixtures\FixtureModel::class)
+            ->setMethods(['getObjectManager'])
+            ->setConstructorArgs([$this->objectManager->get(IndexerReindexCommand::class)])
+            ->getMock();
+        $this->fixtureModelMock
+            ->method('getObjectManager')
+            ->willReturn($this->objectManager);
+
+        $this->command = $this->objectManager->create(
+            GenerateFixturesCommand::class,
+            [
+                'fixtureModel' => $this->fixtureModelMock
+            ]
+        );
+
+        $this->setIncrement(3);
+
         $this->commandTester = new CommandTester($this->command);
+
+        parent::setUp();
     }
 
+    public function tearDown()
+    {
+        $this->setIncrement(1);
+
+        parent::tearDown();
+    }
+
+    /**
+     * @magentoDbIsolation enabled
+     * @magentoAppIsolation enabled
+     */
     public function testExecute()
     {
         $this->commandTester->execute(
-            [GenerateFixturesCommand::PROFILE_ARGUMENT => '/var/www/magento2ce/setup/performance-toolkit/profiles/ce/small.xml']
+            [GenerateFixturesCommand::PROFILE_ARGUMENT => BP . '/setup/performance-toolkit/profiles/ce/small.xml']
         );
 
-        $this->assertEquals(Cli::RETURN_SUCCESS, $this->commandTester->getStatusCode(), $this->commandTester->getDisplay());
+        static::assertEquals(Cli::RETURN_SUCCESS, $this->commandTester->getStatusCode(), $this->commandTester->getDisplay());
+    }
+
+    /**
+     * @param $value
+     */
+    private function setIncrement($value)
+    {
+        $db = Bootstrap::getInstance()->getBootstrap()->getApplication()->getDbInstance();
+        /** @var \Magento\TestFramework\Db\Adapter\Mysql $connection */
+        $connection = Bootstrap::getObjectManager()->create(
+            \Magento\TestFramework\Db\Adapter\Mysql::class,
+            [
+                'config' => [
+                    'type' => 'pdo_mysql',
+                    'host' => $db->getHost(),
+                    'username' => $db->getUser(),
+                    'password' => $db->getPassword(),
+                    'dbname' => $db->getSchema(),
+                    'active' => true,
+                ]
+            ]
+        );
+        $connection->query("SET GLOBAL auto_increment_increment=$value");
+        $connection->query("SET GLOBAL auto_increment_offset=$value");
     }
 }
