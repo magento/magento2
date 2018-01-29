2a1677b6dd338f61c803bb59c23ea7d52a328e3f
Cristian Partica <cpartica@magento.com>
2018-01-29 15:12:49 -0600

    MAGETWO-87146: Graphql Grouped product implementation
    - add support for grouped product by querying each product link

---
+++ app/code/Magento/GroupedProductGraphQl/Model/Plugin/Model/Resolver/Products/DataProvider/Product/Formatter/GroupedProductLinks.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\GroupedProductGraphQl\Model\Plugin\Model\Resolver\Products\DataProvider\Product\Formatter;
+
+use Magento\Catalog\Model\Product;
+use Magento\GroupedProduct\Model\Product\Type\Grouped as GroupedProduct;
+use Magento\CatalogGraphQl\Model\Resolver\Products\DataProvider\Product\FormatterInterface;
+
+/**
+ * Post formatting plugin to continue formatting data for grouped type products
+ */
+class GroupedProductLinks implements FormatterInterface
+{
+
+    /**
+     * Add grouped options and options to configurable types
+     *
+     * {@inheritdoc}
+     */
+    public function format(Product $product, array $productData = [])
+    {
+        if ($product->getTypeId() === GroupedProduct::TYPE_CODE) {
+            $productData['product_links'] = $product->getProductLinks();
+        }
+
+        return $productData;
+    }
+}
