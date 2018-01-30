d8db3ebd03a707c714f224c5dddb8659f7b64a26
Peter Chmeruk <magexdev@gmail.com>
2018-01-30 22:49:33 +0200

    Fixed checking dhl domestic status for non eu countries

--- app/code/Magento/Dhl/Model/Carrier.php
+++ app/code/Magento/Dhl/Model/Carrier.php
@@ -1901,9 +1901,8 @@ class Carrier extends \Magento\Dhl\Model\AbstractDhl implements \Magento\Shippin
 
         $origCountry = (string)$this->getCountryParams($origCountryCode)->getData('name');
         $destCountry = (string)$this->getCountryParams($destCountryCode)->getData('name');
-        $isDomestic = (string)$this->getCountryParams($destCountryCode)->getData('domestic');
 
-        if (($origCountry == $destCountry && $isDomestic)
+        if (($origCountry == $destCountry)
             || ($this->_carrierHelper->isCountryInEU($origCountryCode)
                 && $this->_carrierHelper->isCountryInEU($destCountryCode)
             )
