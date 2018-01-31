22184f440fcc901ee1175f826ecfb55d392e1d55
Roman Ganin <rganin@magento.com>
2018-01-31 20:20:49 +0200

    MAGETWO-87315: Stabilize bamboo and jenkins builds

--- setup/src/Magento/Setup/Test/Unit/Model/Declaration/Schema/ValidationRules/RealTypesTest.php
+++ setup/src/Magento/Setup/Test/Unit/Model/Declaration/Schema/ValidationRules/RealTypesTest.php
@@ -3,7 +3,7 @@
  * Copyright © 2016 Magento. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\Setup\Test\Unit\Model\Declaration\Declaration\Schema\ValidationRules;
+namespace Magento\Setup\Test\Unit\Model\Declaration\Schema\ValidationRules;
 
 use Magento\Framework\TestFramework\Unit\Helper\ObjectManager as ObjectManagerHelper;
 use Magento\Setup\Model\Declaration\Schema\Declaration\ValidationRules\RealTypes;
