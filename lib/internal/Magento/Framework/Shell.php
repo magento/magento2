f1083fdf2728b86ad11688022c92705f629aceb3
Sergii Kovalenko <skovalenko@magento.com>
2018-02-01 20:02:58 +0200

    MAGETWO-87190: Test coverage for critical logic

--- lib/internal/Magento/Framework/Shell.php
+++ lib/internal/Magento/Framework/Shell.php
@@ -56,6 +56,7 @@ class Shell implements ShellInterface
 
         exec($command, $output, $exitCode);
         $output = implode(PHP_EOL, $output);
+        var_dump($command);
         var_dump($output);
         $this->log($output);
         if ($exitCode) {
