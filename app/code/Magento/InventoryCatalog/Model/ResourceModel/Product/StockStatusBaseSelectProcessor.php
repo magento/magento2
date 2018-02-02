d606a5e7211072c42a7ac2cbf2d449f816af7b98
RomanKis <romaikiss@gmail.com>
2018-02-02 15:47:50 +0200

    MSI: 392: Adapt `Magento\CatalogInventory\Model\ResourceModel\Product\StockStatusBaseSelectProcessor`

---
+++ app/code/Magento/InventoryCatalog/Model/ResourceModel/Product/StockStatusBaseSelectProcessor.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\InventoryCatalog\Model\ResourceModel\Product;
+
+use Magento\Catalog\Model\ResourceModel\Product\BaseSelectProcessorInterface;
+use Magento\CatalogInventory\Api\StockConfigurationInterface;
+use Magento\Framework\DB\Select;
+use Magento\InventoryCatalog\Model\GetStockIdForCurrentWebsite;
+use Magento\InventoryIndexer\Model\StockIndexTableNameResolverInterface;
+
+/**
+ * Add stock item filter to selects.
+ */
+class StockStatusBaseSelectProcessor implements BaseSelectProcessorInterface
+{
+    /**
+     * @var StockIndexTableNameResolverInterface
+     */
+    private $stockIndexTableNameResolver;
+
+    /**
+     * @var StockConfigurationInterface
+     */
+    private $stockConfig;
+
+    /**
+     * @var GetStockIdForCurrentWebsite
+     */
+    private $getStockIdForCurrentWebsite;
+
+    /**
+     * @param StockIndexTableNameResolverInterface $stockIndexTableNameResolver
+     * @param StockConfigurationInterface $stockConfig
+     * @param GetStockIdForCurrentWebsite $getStockIdForCurrentWebsite
+     */
+    public function __construct(
+        StockIndexTableNameResolverInterface $stockIndexTableNameResolver,
+        StockConfigurationInterface $stockConfig,
+        GetStockIdForCurrentWebsite $getStockIdForCurrentWebsite
+    ) {
+        $this->stockIndexTableNameResolver = $stockIndexTableNameResolver;
+        $this->stockConfig = $stockConfig;
+        $this->getStockIdForCurrentWebsite = $getStockIdForCurrentWebsite;
+    }
+
+    /**
+     * @param Select $select
+     * @return Select
+     */
+    public function process(Select $select)
+    {
+        if (!$this->stockConfig->isShowOutOfStock()) {
+            $stockId = $this->getStockIdForCurrentWebsite->execute();
+            $stockTable = $this->stockIndexTableNameResolver->execute($stockId);
+
+            /** @var Select $select */
+            $select->join(
+                ['stock' => $stockTable],
+                sprintf('stock.sku = parent.sku'),
+                []
+            )->where('stock.quantity > 0');
+            //todo )->where('stock.is_salable = ?', 1);
+        }
+
+        return $select;
+    }
+}
---
+++ app/code/Magento/InventoryCatalog/Test/Integration/CatalogInventory/Model/ResourceModel/Product/StockStatusBaseSelectProcessorOnDefaultStockTest.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\InventoryCatalog\Test\Integration\CatalogInventory\Model\ResourceModel\Product;
+
+use Magento\Catalog\Model\ResourceModel\Product\BaseSelectProcessorInterface;
+use Magento\Framework\App\ResourceConnection;
+use Magento\TestFramework\Helper\Bootstrap;
+use PHPUnit\Framework\TestCase;
+
+class StockStatusBaseSelectProcessorOnDefaultStockTest extends TestCase
+{
+    /**
+     * @var BaseSelectProcessorInterface
+     */
+    private $productResourceModel;
+
+    /**
+     * @var ResourceConnection
+     */
+    private $resourceConnection;
+
+    /**
+     * @inheritdoc
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->productResourceModel = Bootstrap::getObjectManager()->get(BaseSelectProcessorInterface::class);
+        $this->resourceConnection = Bootstrap::getObjectManager()->get(ResourceConnection::class);
+    }
+
+    /**
+     * @magentoDataFixture Magento/ConfigurableProduct/_files/product_configurable.php
+     * @magentoDataFixture Magento/ConfigurableProduct/_files/product_configurable_out_of_stock.php
+     *
+     * @return void
+     */
+    public function testProcess()
+    {
+        $select = $this->resourceConnection->getConnection()->select();
+        $productTable = $this->resourceConnection->getTableName('catalog_product_entity');
+        $select
+            ->from(['parent' => $productTable])
+            ->joinInner(
+                ['link' => $this->resourceConnection->getTableName('catalog_product_relation')],
+                'link.parent_id = parent.entity_id',
+                []
+            )
+            ->joinInner(
+                ['child' => $productTable],
+                'child.entity_id = link.child_id',
+                []
+            );
+
+        $this->productResourceModel->process($select);
+
+        self::assertEquals(2, count($select->query()->fetchAll()));
+    }
+}
