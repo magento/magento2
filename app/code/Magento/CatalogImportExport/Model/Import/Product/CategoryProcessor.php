76716b84a92f009253039cd06b9e47367384655f
magento-engcom-team <magento-engcom-team@magento.com>
2018-02-06 13:13:23 +0200

    [2.3-develop] Forwardport of magento/magento2#11049

--- app/code/Magento/CatalogImportExport/Model/Import/Product/CategoryProcessor.php
+++ app/code/Magento/CatalogImportExport/Model/Import/Product/CategoryProcessor.php
@@ -122,7 +122,7 @@ class CategoryProcessor
         try {
             $category->save();
             $this->categoriesCache[$category->getId()] = $category;
-        } catch (\Exception $e){
+        } catch (\Exception $e) {
             $this->addFailedCategory($category, $e);
         }
 
