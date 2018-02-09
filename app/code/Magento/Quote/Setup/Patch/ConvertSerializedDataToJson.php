257b31690fa9f9785ca03fb773ac37e60008613e
Roman Ganin <rganin@magento.com>
2018-02-09 19:37:39 +0200

    MAGETWO-87551: Convert existing data install/upgrade scripts
    - Quote

---
+++ app/code/Magento/Quote/Setup/Patch/ConvertSerializedDataToJson.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Quote\Setup\Patch;
+
+use Magento\Framework\App\ResourceConnection;
+use Magento\Quote\Setup\ConvertSerializedDataToJsonFactory;
+use Magento\Quote\Setup\QuoteSetupFactory;
+use Magento\Setup\Model\Patch\DataPatchInterface;
+use Magento\Setup\Model\Patch\PatchVersionInterface;
+
+/**
+ * Class ConvertSerializedDataToJson
+ * @package Magento\Quote\Setup\Patch
+ */
+class ConvertSerializedDataToJson implements DataPatchInterface, PatchVersionInterface
+{
+    /**
+     * @var ResourceConnection
+     */
+    private $resourceConnection;
+
+    /**
+     * @var QuoteSetupFactory
+     */
+    private $quoteSetupFactory;
+
+    /**
+     * @var ConvertSerializedDataToJsonFactory
+     */
+    private $convertSerializedDataToJsonFactory;
+
+    /**
+     * PatchInitial constructor.
+     * @param ResourceConnection $resourceConnection
+     */
+    public function __construct(
+        ResourceConnection $resourceConnection,
+        QuoteSetupFactory $quoteSetupFactory,
+        ConvertSerializedDataToJsonFactory $convertSerializedDataToJsonFactory
+    ) {
+        $this->resourceConnection = $resourceConnection;
+        $this->quoteSetupFactory = $quoteSetupFactory;
+        $this->convertSerializedDataToJsonFactory = $convertSerializedDataToJsonFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply()
+    {
+        $quoteSetup = $this->quoteSetupFactory->create();
+        $this->convertSerializedDataToJsonFactory->create(['quoteSetup' => $quoteSetup])->convert();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies()
+    {
+        return [
+            InstallEntityTypes::class
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getVersion()
+    {
+        return '2.0.6';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+}
---
+++ app/code/Magento/Quote/Setup/Patch/InstallEntityTypes.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Quote\Setup\Patch;
+
+use Magento\Framework\DB\Ddl\Table;
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\App\ResourceConnection;
+use Magento\Quote\Setup\QuoteSetup;
+use Magento\Quote\Setup\QuoteSetupFactory;
+use Magento\Setup\Model\Patch\DataPatchInterface;
+use Magento\Setup\Model\Patch\PatchVersionInterface;
+
+/***
+ * Class InstallEntityTypes
+ * @package Magento\Quote\Setup\Patch
+ */
+class InstallEntityTypes implements DataPatchInterface, PatchVersionInterface
+{
+    /**
+     * @var ResourceConnection
+     */
+    private $resourceConnection;
+
+    /**
+     * @var QuoteSetupFactory
+     */
+    private $quoteSetupFactory;
+
+    /**
+     * InstallEntityTypes constructor.
+     * @param ResourceConnection $resourceConnection
+     * @param QuoteSetupFactory $quoteSetupFactory
+     */
+    public function __construct(
+        ResourceConnection $resourceConnection,
+        QuoteSetupFactory $quoteSetupFactory
+    ) {
+        $this->resourceConnection = $resourceConnection;
+        $this->quoteSetupFactory = $quoteSetupFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply()
+    {
+        /** @var QuoteSetup $quoteSetup */
+        $quoteSetup = $this->quoteSetupFactory->create();
+
+        /**
+         * Install eav entity types to the eav/entity_type table
+         */
+        $attributes = [
+            'vat_id' => ['type' => Table::TYPE_TEXT],
+            'vat_is_valid' => ['type' => Table::TYPE_SMALLINT],
+            'vat_request_id' => ['type' => Table::TYPE_TEXT],
+            'vat_request_date' => ['type' => Table::TYPE_TEXT],
+            'vat_request_success' => ['type' => Table::TYPE_SMALLINT],
+        ];
+        foreach ($attributes as $attributeCode => $attributeParams) {
+            $quoteSetup->addAttribute('quote_address', $attributeCode, $attributeParams);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getVersion()
+    {
+        return '2.0.0';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+}
