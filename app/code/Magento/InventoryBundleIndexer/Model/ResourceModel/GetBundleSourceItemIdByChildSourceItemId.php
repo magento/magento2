a50222d18034d3d75d7f47bc9a52adaa004fbbe8
RomanKis <romaikiss@gmail.com>
2018-02-09 16:35:43 +0200

    MSI: 380: Bundle product get "Out of stock" status when created

--- app/code/Magento/InventoryBundleIndexer/Model/ResourceModel/GetBundleSourceItemIdByChildSourceItemId.php
+++ app/code/Magento/InventoryBundleIndexer/Model/ResourceModel/GetBundleSourceItemIdByChildSourceItemId.php
@@ -74,5 +74,4 @@ class GetBundleSourceItemIdByChildSourceItemId
 
         return $bundleSourceItemId;
     }
-
 }
--- app/code/Magento/InventoryBundleIndexer/Model/ResourceModel/GetSourceItemsIdsWithProductTypeIds.php
+++ app/code/Magento/InventoryBundleIndexer/Model/ResourceModel/GetSourceItemsIdsWithProductTypeIds.php
@@ -52,5 +52,4 @@ class GetSourceItemsIdsWithProductTypeIds
 
         return $sourceItemsIdsWithTypeIds;
     }
-
 }
