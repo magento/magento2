39db1b79474048ebc8ff51d395e3cba455f50c2f
Roman Ganin <rganin@magento.com>
2018-02-12 12:03:32 +0200

    MAGETWO-87551: Convert existing data install/upgrade scripts
    - User

---
+++ app/code/Magento/User/Setup/Patch/UpgradePasswordHashes.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\User\Setup\Patch;
+
+use Magento\Framework\Encryption\Encryptor;
+use Magento\Framework\App\ResourceConnection;
+use Magento\Setup\Model\Patch\DataPatchInterface;
+use Magento\Setup\Model\Patch\PatchVersionInterface;
+
+/**
+ * Class UpgradePasswordHashes
+ * @package Magento\User\Setup\Patch
+ */
+class UpgradePasswordHashes implements DataPatchInterface, PatchVersionInterface
+{
+    /**
+     * @var ResourceConnection
+     */
+    private $resourceConnection;
+
+    /**
+     * PatchInitial constructor.
+     * @param ResourceConnection $resourceConnection
+     */
+    public function __construct(
+        ResourceConnection $resourceConnection
+    ) {
+        $this->resourceConnection = $resourceConnection;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply()
+    {
+        $this->resourceConnection->getConnection()->startSetup();
+        $this->upgradeHash();
+        $this->resourceConnection->getConnection()->endSetup();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getVersion()
+    {
+        return '2.0.1';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+
+    /**
+     * Upgrade password hashes.
+     */
+    private function upgradeHash()
+    {
+        $connection = $this->resourceConnection->getConnection();
+        $customerEntityTable = $connection->getTableName('admin_user');
+
+        $select = $connection->select()->from(
+            $customerEntityTable,
+            ['user_id', 'password']
+        );
+
+        $customers = $connection->fetchAll($select);
+        foreach ($customers as $customer) {
+            list($hash, $salt) = explode(Encryptor::DELIMITER, $customer['password']);
+
+            $newHash = $customer['password'];
+            if (strlen($hash) === 32) {
+                $newHash = implode(Encryptor::DELIMITER, [$hash, $salt, Encryptor::HASH_VERSION_MD5]);
+            } elseif (strlen($hash) === 64) {
+                $newHash = implode(Encryptor::DELIMITER, [$hash, $salt, Encryptor::HASH_VERSION_SHA256]);
+            }
+
+            $bind = ['password' => $newHash];
+            $where = ['user_id = ?' => (int)$customer['user_id']];
+            $connection->update($customerEntityTable, $bind, $where);
+        }
+    }
+}
---
+++ app/code/Magento/User/Setup/Patch/UpgradeSerializedFields.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\User\Setup\Patch;
+
+use Magento\Framework\DB\DataConverter\SerializedToJson;
+use Magento\Framework\DB\FieldDataConverterFactory;
+use Magento\Framework\App\ResourceConnection;
+use Magento\Setup\Model\Patch\DataPatchInterface;
+use Magento\Setup\Model\Patch\PatchVersionInterface;
+
+/**
+ * Class UpgradeSerializedFields
+ * @package Magento\User\Setup\Patch
+ */
+class UpgradeSerializedFields implements DataPatchInterface, PatchVersionInterface
+{
+    /**
+     * @var ResourceConnection
+     */
+    private $resourceConnection;
+
+    /**
+     * @var FieldDataConverterFactory
+     */
+    private $fieldDataConverterFactory;
+
+    /**
+     * UpgradeSerializedFields constructor.
+     * @param ResourceConnection $resourceConnection
+     * @param FieldDataConverterFactory $fieldDataConverterFactory
+     */
+    public function __construct(
+        ResourceConnection $resourceConnection,
+        FieldDataConverterFactory $fieldDataConverterFactory
+    ) {
+        $this->resourceConnection = $resourceConnection;
+        $this->fieldDataConverterFactory = $fieldDataConverterFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply()
+    {
+        $this->resourceConnection->getConnection()->startSetup();
+        $this->upgradeSerializedFields();
+        $this->resourceConnection->getConnection()->endSetup();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies()
+    {
+        return [
+            UpgradePasswordHashes::class
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getVersion()
+    {
+        return '2.0.2';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+
+    /**
+     * Convert serialized data to json.
+     */
+    private function upgradeSerializedFields()
+    {
+        $fieldDataConverter = $this->fieldDataConverterFactory->create(SerializedToJson::class);
+        $fieldDataConverter->convert(
+            $this->resourceConnection->getConnection(),
+            $this->resourceConnection->getConnection()->getTableName('admin_user'),
+            'user_id',
+            'extra'
+        );
+
+    }
+}
