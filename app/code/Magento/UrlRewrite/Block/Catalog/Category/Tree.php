597d5486c5cb5dab00bf12e08595161854d002b5
Vinay Shah <vinay.sudhir@gmail.com>
2018-02-14 08:53:30 +0530

    category name escapeHTML and test related changes

--- app/code/Magento/UrlRewrite/Block/Catalog/Category/Tree.php
+++ app/code/Magento/UrlRewrite/Block/Catalog/Category/Tree.php
@@ -163,7 +163,7 @@ class Tree extends \Magento\Catalog\Block\Adminhtml\Category\AbstractCategory
             'children_count' => (int)$node->getChildrenCount(),
             'is_active' => (bool)$node->getIsActive(),
             // Scrub names for raw js output
-            'name' => $this->escapeHtmlAttr($node->getName(), false),
+            'name' => $this->escapeHtml($node->getName()),
             'level' => (int)$node->getLevel(),
             'product_count' => (int)$node->getProductCount(),
         ];
