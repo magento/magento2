30be2697f17373dc287eb1b75f68fd197735d86e
nmalevanec <mikola.malevanec@transoftgroup.com>
2018-02-20 10:55:29 +0200

    Add MagentoStyle as Console input/output helper object to allow easier access to io helpers in symfony/console

--- setup/src/Magento/Setup/Console/Style/MagentoStyleInterface.php
+++ setup/src/Magento/Setup/Console/Style/MagentoStyleInterface.php
@@ -8,7 +8,9 @@ namespace Magento\Setup\Console\Style;
 
 use Symfony\Component\Console\Style\StyleInterface;
 
-
+/**
+ * Interface for output decorator.
+ */
 interface MagentoStyleInterface extends StyleInterface
 {
 
