2fc45a109e555593fbeddd0ca2295880d27db8c7
Ruslan Kostiv <rkostiv@magento.com>
2018-02-22 14:30:54 +0200

    MAGETWO-87013: Cart Price Rules - Category Drill Down problems

--- app/code/Magento/SalesRule/Controller/Adminhtml/Promo/Widget/CategoriesJson.php
+++ app/code/Magento/SalesRule/Controller/Adminhtml/Promo/Widget/CategoriesJson.php
@@ -5,6 +5,13 @@
  */
 namespace Magento\SalesRule\Controller\Adminhtml\Promo\Widget;
 
+/**
+ * Class for generation of JSON for building tree catalog.
+ *
+ * Examples of use:
+ * \Magento\Catalog\Block\Adminhtml\Category\Tree::getLoadTreeUrl
+ * \Magento\Catalog\Block\Adminhtml\Category\Widget\Chooser::getLoadTreeUrl
+ */
 class CategoriesJson extends \Magento\CatalogRule\Controller\Adminhtml\Promo\Widget\CategoriesJson
 {
     /**
