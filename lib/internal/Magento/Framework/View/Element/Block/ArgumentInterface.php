bc6913e55d84c1c09d9cb0d17748f0831714f8a3
Vinai Kopp <vinai@netzarbeiter.com>
2018-02-23 11:17:52 +0100

    Remove whitespace at end of line in docblock

--- lib/internal/Magento/Framework/View/Element/Block/ArgumentInterface.php
+++ lib/internal/Magento/Framework/View/Element/Block/ArgumentInterface.php
@@ -8,7 +8,7 @@ namespace Magento\Framework\View\Element\Block;
 /**
  * Block argument interface.
  * All objects that are injected to block arguments should implement this interface.
- * 
+ *
  * @api
  */
 interface ArgumentInterface
