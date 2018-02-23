5de960164073125013589e7c05d2be0ae493624c
Sergey <simpleadm@gmail.com>
2018-02-23 08:54:31 +0300

    Return status for console commands

--- setup/src/Magento/Setup/Console/Command/DiCompileCommand.php
+++ setup/src/Magento/Setup/Console/Command/DiCompileCommand.php
@@ -21,6 +21,7 @@ use Magento\Setup\Module\Di\App\Task\OperationException;
 use Magento\Setup\Module\Di\App\Task\OperationInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\ProgressBar;
+use Magento\Framework\Console\Cli;
 
 /**
  * Command to run compile in single-tenant mode
@@ -127,7 +128,7 @@ class DiCompileCommand extends Command
                 $output->writeln($line);
             }
             // we must have an exit code higher than zero to indicate something was wrong
-            return \Magento\Framework\Console\Cli::RETURN_FAILURE;
+            return Cli::RETURN_FAILURE;
         }
 
         $modulePaths = $this->componentRegistrar->getPaths(ComponentRegistrar::MODULE);
@@ -193,8 +194,9 @@ class DiCompileCommand extends Command
         } catch (OperationException $e) {
             $output->writeln('<error>' . $e->getMessage() . '</error>');
             // we must have an exit code higher than zero to indicate something was wrong
-            return \Magento\Framework\Console\Cli::RETURN_FAILURE;
+            return Cli::RETURN_FAILURE;
         }
+        return Cli::RETURN_SUCCESS;
     }
 
     /**
