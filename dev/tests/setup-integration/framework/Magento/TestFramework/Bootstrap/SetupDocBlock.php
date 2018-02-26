2db410a101cea1a75181f3accd08875d2f2babb1
Sergii Kovalenko <skovalenko@magento.com>
2018-02-26 13:00:39 +0200

    MAGETWO-81030: Schema Validation

--- dev/tests/setup-integration/framework/Magento/TestFramework/Bootstrap/SetupDocBlock.php
+++ dev/tests/setup-integration/framework/Magento/TestFramework/Bootstrap/SetupDocBlock.php
@@ -33,7 +33,6 @@ class SetupDocBlock extends \Magento\TestFramework\Bootstrap\DocBlock
             new \Magento\TestFramework\Annotation\ComponentRegistrarFixture($this->_fixturesBaseDir),
             new \Magento\TestFramework\Annotation\SchemaFixture($this->_fixturesBaseDir),
             new \Magento\TestFramework\Annotation\Cache(),
-            new AppIsolation($application),
             new \Magento\TestFramework\Workaround\CacheClean(),
             new \Magento\TestFramework\Annotation\ReinstallInstance($application),
             new \Magento\TestFramework\Annotation\CopyModules(),
