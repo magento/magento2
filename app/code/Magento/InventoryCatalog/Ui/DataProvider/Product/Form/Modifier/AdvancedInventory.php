b99a40e0e728ec35e8224bf8be3a5333645b1e01
Valeriy Nayda <vnayda@magento.com>
2018-02-26 15:02:23 +0200

    MSI-585: Show different product edit form for stock configuration depend on multi/single source configuration

--- app/code/Magento/InventoryCatalog/Ui/DataProvider/Product/Form/Modifier/AdvancedInventory.php
+++ app/code/Magento/InventoryCatalog/Ui/DataProvider/Product/Form/Modifier/AdvancedInventory.php
@@ -8,9 +8,13 @@ declare(strict_types=1);
 namespace Magento\InventoryCatalog\Ui\DataProvider\Product\Form\Modifier;
 
 use Magento\Catalog\Ui\DataProvider\Product\Form\Modifier\AbstractModifier;
+use Magento\CatalogInventory\Ui\DataProvider\Product\Form\Modifier\AdvancedInventory as AdvancedInventoryModifier;
 use Magento\Framework\Stdlib\ArrayManager;
 use Magento\InventoryCatalog\Model\IsSingleSourceModeInterface;
 
+/**
+ * Hide qty and is_in_stock fields in Advanced Inventory panel
+ */
 class AdvancedInventory extends AbstractModifier
 {
     /**
@@ -44,7 +48,7 @@ class AdvancedInventory extends AbstractModifier
     }
 
     /**
-     * {@inheritdoc}
+     * @inheritdoc
      */
     public function modifyMeta(array $meta)
     {
@@ -52,27 +56,36 @@ class AdvancedInventory extends AbstractModifier
             return $meta;
         }
 
-        $advancedInventoryPath = $this->arrayManager->findPath('advanced_inventory_modal', $meta, null, 'children');
-
-        if ($advancedInventoryPath) {
-            $meta = $this->arrayManager->merge(
-                $advancedInventoryPath . '/children/stock_data/children/qty/arguments/data/config',
-                $meta,
-                [
-                    'visible' => false,
-                    'imports' => ''
-                ]
-            );
-
-            $isInStockContainerPath = $advancedInventoryPath .
-                '/children/stock_data/children/container_is_in_stock/children/is_in_stock/arguments/data/config';
-
-            $meta = $this->arrayManager->set(
-                $isInStockContainerPath,
-                $meta,
-                ['visible' => false]
-            );
+        $stockDataPath = $this->arrayManager->findPath(
+            AdvancedInventoryModifier::STOCK_DATA_FIELDS,
+            $meta,
+            null,
+            'children'
+        );
+        if (null === $stockDataPath) {
+            return $meta;
         }
+
+        $qtyPath = $stockDataPath . '/children/qty/arguments/data/config';
+        $meta = $this->arrayManager->merge(
+            $qtyPath,
+            $meta,
+            [
+                'visible' => 0,
+                'imports' => '',
+            ]
+        );
+
+        $stockStatusPath = $stockDataPath
+            . '/children/container_is_in_stock/children/is_in_stock/arguments/data/config';
+        $meta = $this->arrayManager->set(
+            $stockStatusPath,
+            $meta,
+            [
+                'visible' => 0,
+                'imports' => '',
+            ]
+        );
 
         return $meta;
     }
