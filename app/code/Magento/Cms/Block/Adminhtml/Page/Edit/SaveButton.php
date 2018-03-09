808e5e59bc520ef6f04b7dfdaaeb03fa556927f1
Iryna Lagno <ilagno@magento.com>
2018-03-09 15:01:01 -0600

    MAGETWO-87096: CMS Page Duplication and Reset Removal

--- app/code/Magento/Cms/Block/Adminhtml/Page/Edit/SaveButton.php
+++ app/code/Magento/Cms/Block/Adminhtml/Page/Edit/SaveButton.php
@@ -50,51 +50,50 @@ class SaveButton extends GenericButton implements ButtonProviderInterface
      */
     protected function getOptions()
     {
-            $options = [
-                [
-                    'label' => __('Save & Duplicate'),
-                    'id_hard' => 'save_and_duplicate',
-                    'data_attribute' => [
-                        'mage-init' => [
-                            'buttonAdapter' => [
-                                'actions' => [
-                                    [
-                                        'targetName' => 'cms_page_form.cms_page_form',
-                                        'actionName' => 'save',
-                                        'params' => [
-                                            true,
-                                            [
-                                                'back' => 'duplicate'
-                                            ]
+        $options = [
+            [
+                'label' => __('Save & Duplicate'),
+                'id_hard' => 'save_and_duplicate',
+                'data_attribute' => [
+                    'mage-init' => [
+                        'buttonAdapter' => [
+                            'actions' => [
+                                [
+                                    'targetName' => 'cms_page_form.cms_page_form',
+                                    'actionName' => 'save',
+                                    'params' => [
+                                        true,
+                                        [
+                                            'back' => 'duplicate'
                                         ]
                                     ]
                                 ]
                             ]
                         ]
-                    ],
+                    ]
                 ],
-                [
-                    'id_hard' => 'save_and_close',
-                    'label' => __('Save & Close'),
-                    'data_attribute' => [
-                        'mage-init' => [
-                            'buttonAdapter' => [
-                                'actions' => [
-                                    [
-                                        'targetName' => 'cms_page_form.cms_page_form',
-                                        'actionName' => 'save',
-                                        'params' => [
-                                            true
-                                        ]
+            ],
+            [
+                'id_hard' => 'save_and_close',
+                'label' => __('Save & Close'),
+                'data_attribute' => [
+                    'mage-init' => [
+                        'buttonAdapter' => [
+                            'actions' => [
+                                [
+                                    'targetName' => 'cms_page_form.cms_page_form',
+                                    'actionName' => 'save',
+                                    'params' => [
+                                        true
                                     ]
                                 ]
                             ]
                         ]
-                    ],
-                ]
-            ];
+                    ]
+                ],
+            ]
+        ];
 
         return $options;
     }
-
 }
