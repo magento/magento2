db9be6ae9059c96035dcba32ac6caee32ddde78f
Myroslav Dobra <dmaraptor@gmail.com>
2018-03-12 21:02:43 +0200

    MAGETWO-72624: [2.3] - [Magento Cloud] Unable to clear multiselct in attribute editor.

--- lib/internal/Magento/Framework/Data/Form/Element/Multiselect.php
+++ lib/internal/Magento/Framework/Data/Form/Element/Multiselect.php
@@ -61,6 +61,10 @@ class Multiselect extends AbstractElement
                 <input type="hidden" id="' . $this->getHtmlId() . '_hidden" name="' . parent::getName() . '" value="" />
                 ';
         }
+        if (!empty($this->_data['disabled'])) {
+            $html .= '<input type="hidden" name="' . parent::getName() . '_disabled" value="" />';
+        }
+
         $html .= '<select id="' . $this->getHtmlId() . '" name="' . $this->getName() . '" ' . $this->serialize(
             $this->getHtmlAttributes()
         ) . $this->_getUiId() . ' multiple="multiple">' . "\n";
