164ba27ceb6c78d9d95723beef816b1e3baec8ec
Pavel Bystritsky <p.bystritsky@yandex.ru>
2018-03-14 17:13:55 +0200

    MSI: 654: Exception when trying to create Grouped or Bundle product in admin

--- app/code/Magento/InventoryConfigurableProduct/Ui/DataProvider/Product/Form/Modifier/InventoryConfigurablePanel.php
+++ app/code/Magento/InventoryConfigurableProduct/Ui/DataProvider/Product/Form/Modifier/InventoryConfigurablePanel.php
@@ -7,8 +7,6 @@ declare(strict_types=1);
 
 namespace Magento\InventoryConfigurableProduct\Ui\DataProvider\Product\Form\Modifier;
 
-use Magento\Catalog\Model\Locator\LocatorInterface;
-use Magento\Catalog\Ui\AllowedProductTypes;
 use Magento\Catalog\Ui\DataProvider\Product\Form\Modifier\AbstractModifier;
 use Magento\ConfigurableProduct\Ui\DataProvider\Product\Form\Modifier\ConfigurablePanel;
 use Magento\Ui\Component\Form;
@@ -18,28 +16,6 @@ use Magento\Ui\Component\Form;
  */
 class InventoryConfigurablePanel extends AbstractModifier
 {
-    /**
-     * @var LocatorInterface
-     */
-    private $locator;
-
-    /**
-     * @var AllowedProductTypes
-     */
-    private $allowedProductTypes;
-
-    /**
-     * @param LocatorInterface $locator
-     * @param AllowedProductTypes $allowedProductTypes
-     */
-    public function __construct(
-        LocatorInterface $locator,
-        AllowedProductTypes $allowedProductTypes
-    ) {
-        $this->locator = $locator;
-        $this->allowedProductTypes = $allowedProductTypes;
-    }
-
     /**
      * @inheritdoc
      */
@@ -76,13 +52,9 @@ class InventoryConfigurablePanel extends AbstractModifier
      */
     public function modifyMeta(array $meta)
     {
-        if ($this->allowedProductTypes->isAllowedProductType($this->locator->getProduct())) {
-            $matrix = $meta[ConfigurablePanel::GROUP_CONFIGURABLE]['children'][ConfigurablePanel::CONFIGURABLE_MATRIX];
-
-            $matrix['children']['record']['children']['quantity_container'] = $this->getQuantityContainerConfig();
-
-            $meta[ConfigurablePanel::GROUP_CONFIGURABLE]['children'][ConfigurablePanel::CONFIGURABLE_MATRIX] = $matrix;
-        }
+        $matrix = $meta[ConfigurablePanel::GROUP_CONFIGURABLE]['children'][ConfigurablePanel::CONFIGURABLE_MATRIX];
+        $matrix['children']['record']['children']['quantity_container'] = $this->getQuantityContainerConfig();
+        $meta[ConfigurablePanel::GROUP_CONFIGURABLE]['children'][ConfigurablePanel::CONFIGURABLE_MATRIX] = $matrix;
 
         return $meta;
     }
