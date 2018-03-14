49327d2764595712e23f749896171a03fbf1cb97
Alexander Shkurko <coderimus@gmail.com>
2018-03-14 22:28:53 +0100

    Fix codemess error

--- app/code/Magento/Sales/Model/Order/Invoice/Total/Grand.php
+++ app/code/Magento/Sales/Model/Order/Invoice/Total/Grand.php
@@ -8,8 +8,11 @@ namespace Magento\Sales\Model\Order\Invoice\Total;
 class Grand extends AbstractTotal
 {
     /**
+     * Collect invoice grand total
+     *
      * @param \Magento\Sales\Model\Order\Invoice $invoice
      * @return $this
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
     public function collect(\Magento\Sales\Model\Order\Invoice $invoice)
     {
