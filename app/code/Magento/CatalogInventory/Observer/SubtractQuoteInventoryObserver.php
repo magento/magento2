885aed29c5a1a5c4a42cfdb49849909071e25379
Oleksandr Gorkun <ogorkun@magento.com>
2018-03-20 12:02:29 +0200

    MAGETWO-88925: [Metropolitan Museum]Incorrect product status on order view page for configurable product when place backordered product

--- app/code/Magento/CatalogInventory/Observer/SubtractQuoteInventoryObserver.php
+++ app/code/Magento/CatalogInventory/Observer/SubtractQuoteInventoryObserver.php
@@ -6,9 +6,12 @@
 
 namespace Magento\CatalogInventory\Observer;
 
+use Magento\CatalogInventory\Api\StockRegistryInterface;
+use Magento\Framework\App\ObjectManager;
 use Magento\Framework\Event\ObserverInterface;
 use Magento\CatalogInventory\Api\StockManagementInterface;
 use Magento\Framework\Event\Observer as EventObserver;
+use Magento\Sales\Api\Data\OrderInterface;
 
 /**
  * Catalog inventory module observer
@@ -31,19 +34,28 @@ class SubtractQuoteInventoryObserver implements ObserverInterface
     protected $itemsForReindex;
 
     /**
+     * @var StockRegistryInterface
+     */
+    private $stockRegistry;
+
+    /**
      * SubtractQuoteInventoryObserver constructor.
      * @param StockManagementInterface $stockManagement
      * @param ProductQty $productQty
      * @param ItemsForReindex $itemsForReindex
+     * @param StockRegistryInterface|null $stockRegistry
      */
     public function __construct(
         StockManagementInterface $stockManagement,
         ProductQty $productQty,
-        ItemsForReindex $itemsForReindex
+        ItemsForReindex $itemsForReindex,
+        StockRegistryInterface $stockRegistry = null
     ) {
         $this->stockManagement = $stockManagement;
         $this->productQty = $productQty;
         $this->itemsForReindex = $itemsForReindex;
+        $this->stockRegistry = $stockRegistry
+            ?? ObjectManager::getInstance()->get(StockRegistryInterface::class);
     }
 
     /**
@@ -59,14 +71,16 @@ class SubtractQuoteInventoryObserver implements ObserverInterface
     {
         /** @var \Magento\Quote\Model\Quote $quote */
         $quote = $observer->getEvent()->getQuote();
+        /** @var OrderInterface $order */
+        $order = $observer->getEvent()->getOrder();
 
         // Maybe we've already processed this quote in some event during order placement
         // e.g. call in event 'sales_model_service_quote_submit_before' and later in 'checkout_submit_all_after'
         if ($quote->getInventoryProcessed()) {
             return $this;
         }
-        $items = $this->productQty->getProductQty($quote->getAllItems());
 
+        $items = $this->productQty->getProductQty($quote->getAllItems());
         /**
          * Remember items
          */
@@ -75,6 +89,16 @@ class SubtractQuoteInventoryObserver implements ObserverInterface
             $quote->getStore()->getWebsiteId()
         );
         $this->itemsForReindex->setItems($itemsForReindex);
+        //Marking items as backordered.
+        foreach ($order->getItems() as $item) {
+            $stock = $this->stockRegistry->getStockItem($item->getProductId());
+            if (($qty = $stock->getQty()) < 0) {
+                $item->setQtyBackordered(
+                    $item->getQtyOrdered() > (- $qty)
+                        ? (- $qty) : $item->getQtyOrdered()
+                );
+            }
+        }
 
         $quote->setInventoryProcessed(true);
         return $this;
