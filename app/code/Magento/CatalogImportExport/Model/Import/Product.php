1268692cd2037f3d5df7f458bd91934c4272978a
Sergey <simpleadm@gmail.com>
2018-03-24 21:52:28 +0300

    Fixed use config option for 'Enable Qty Increments' filed in product import

--- app/code/Magento/CatalogImportExport/Model/Import/Product.php
+++ app/code/Magento/CatalogImportExport/Model/Import/Product.php
@@ -2636,7 +2636,10 @@ class Product extends \Magento\ImportExport\Model\Import\Entity\AbstractEntity
     {
         $useConfigFields = [];
         foreach ($rowData as $key => $value) {
-            $useConfigName = self::INVENTORY_USE_CONFIG_PREFIX . $key;
+            $useConfigName = $key === 'enable_qty_increments'
+                ? 'use_config_enable_qty_inc'
+                : self::INVENTORY_USE_CONFIG_PREFIX . $key;
+
             if (isset($this->defaultStockData[$key])
                 && isset($this->defaultStockData[$useConfigName])
                 && !empty($value)
