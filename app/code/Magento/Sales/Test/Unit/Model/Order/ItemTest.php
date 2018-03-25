5b58a3424e8896d66c0aff279065943b7e9c35fd
Matias Montes <matias@semexpert.com.ar>
2018-03-25 16:41:22 -0300

    Fixed an empty line at the end of the class and reduced the number of lines in the test provider method

--- app/code/Magento/Sales/Test/Unit/Model/Order/ItemTest.php
+++ app/code/Magento/Sales/Test/Unit/Model/Order/ItemTest.php
@@ -275,161 +275,87 @@ class ItemTest extends \PHPUnit\Framework\TestCase
         return [
             'empty_item' => [
                 'options' => [
-                    'qty_ordered' => 0,
-                    'qty_invoiced' => 0,
-                    'qty_refunded' => 0,
-                    'qty_shipped' => 0,
-                    'qty_canceled' => 0,
+                    'qty_ordered' => 0, 'qty_invoiced' => 0, 'qty_refunded' => 0, 'qty_shipped' => 0,
+                    'qty_canceled' => 0
                 ],
-                'expectedResult' => [
-                    'to_ship' => 0.0,
-                    'to_invoice' => 0.0
-                ]
+                'expectedResult' => ['to_ship' => 0.0, 'to_invoice' => 0.0]
             ],
             'ordered_item' => [
                 'options' => [
-                    'qty_ordered' => 12,
-                    'qty_invoiced' => 0,
-                    'qty_refunded' => 0,
-                    'qty_shipped' => 0,
-                    'qty_canceled' => 0,
+                    'qty_ordered' => 12, 'qty_invoiced' => 0, 'qty_refunded' => 0, 'qty_shipped' => 0,
+                    'qty_canceled' => 0
                 ],
-                'expectedResult' => [
-                    'to_ship' => 12.0,
-                    'to_invoice' => 12.0
-                ]
+                'expectedResult' => ['to_ship' => 12.0, 'to_invoice' => 12.0]
             ],
             'partially_invoiced' => [
-                'options' => [
-                    'qty_ordered' => 12,
-                    'qty_invoiced' => 4,
-                    'qty_refunded' => 0,
-                    'qty_shipped' => 0,
+                'options' => ['qty_ordered' => 12, 'qty_invoiced' => 4, 'qty_refunded' => 0, 'qty_shipped' => 0,
                     'qty_canceled' => 0,
                 ],
-                'expectedResult' => [
-                    'to_ship' => 12.0,
-                    'to_invoice' => 8.0
-                ]
+                'expectedResult' => ['to_ship' => 12.0, 'to_invoice' => 8.0]
             ],
             'completely_invoiced' => [
                 'options' => [
-                    'qty_ordered' => 12,
-                    'qty_invoiced' => 12,
-                    'qty_refunded' => 0,
-                    'qty_shipped' => 0,
+                    'qty_ordered' => 12, 'qty_invoiced' => 12, 'qty_refunded' => 0, 'qty_shipped' => 0,
                     'qty_canceled' => 0,
                 ],
-                'expectedResult' => [
-                    'to_ship' => 12.0,
-                    'to_invoice' => 0.0
-                ]
+                'expectedResult' => ['to_ship' => 12.0, 'to_invoice' => 0.0]
             ],
             'partially_invoiced_refunded' => [
                 'options' => [
-                    'qty_ordered' => 12,
-                    'qty_invoiced' => 5,
-                    'qty_refunded' => 5,
-                    'qty_shipped' => 0,
+                    'qty_ordered' => 12, 'qty_invoiced' => 5, 'qty_refunded' => 5, 'qty_shipped' => 0,
                     'qty_canceled' => 0,
                 ],
-                'expectedResult' => [
-                    'to_ship' => 7.0,
-                    'to_invoice' => 7.0
-                ]
+                'expectedResult' => ['to_ship' => 7.0, 'to_invoice' => 7.0]
             ],
             'partially_refunded' => [
                 'options' => [
-                    'qty_ordered' => 12,
-                    'qty_invoiced' => 12,
-                    'qty_refunded' => 5,
-                    'qty_shipped' => 0,
+                    'qty_ordered' => 12, 'qty_invoiced' => 12, 'qty_refunded' => 5, 'qty_shipped' => 0,
                     'qty_canceled' => 0,
                 ],
-                'expectedResult' => [
-                    'to_ship' => 7.0,
-                    'to_invoice' => 0.0
-                ]
+                'expectedResult' => ['to_ship' => 7.0, 'to_invoice' => 0.0]
             ],
             'partially_shipped' => [
                 'options' => [
-                    'qty_ordered' => 12,
-                    'qty_invoiced' => 0,
-                    'qty_refunded' => 0,
-                    'qty_shipped' => 4,
-                    'qty_canceled' => 0,
+                    'qty_ordered' => 12, 'qty_invoiced' => 0, 'qty_refunded' => 0, 'qty_shipped' => 4,
+                    'qty_canceled' => 0
                 ],
-                'expectedResult' => [
-                    'to_ship' => 8.0,
-                    'to_invoice' => 12.0
-                ]
+                'expectedResult' => ['to_ship' => 8.0, 'to_invoice' => 12.0]
             ],
             'partially_refunded_partially_shipped' => [
                 'options' => [
-                    'qty_ordered' => 12,
-                    'qty_invoiced' => 12,
-                    'qty_refunded' => 5,
-                    'qty_shipped' => 4,
-                    'qty_canceled' => 0,
+                    'qty_ordered' => 12, 'qty_invoiced' => 12, 'qty_refunded' => 5, 'qty_shipped' => 4,
+                    'qty_canceled' => 0
                 ],
-                'expectedResult' => [
-                    'to_ship' => 3.0,
-                    'to_invoice' => 0.0
-                ]
+                'expectedResult' => ['to_ship' => 3.0, 'to_invoice' => 0.0]
             ],
             'complete' => [
                 'options' => [
-                    'qty_ordered' => 12,
-                    'qty_invoiced' => 12,
-                    'qty_refunded' => 0,
-                    'qty_shipped' => 12,
-                    'qty_canceled' => 0,
+                    'qty_ordered' => 12, 'qty_invoiced' => 12, 'qty_refunded' => 0, 'qty_shipped' => 12,
+                    'qty_canceled' => 0
                 ],
-                'expectedResult' => [
-                    'to_ship' => 0.0,
-                    'to_invoice' => 0.0
-                ]
+                'expectedResult' => ['to_ship' => 0.0, 'to_invoice' => 0.0]
             ],
             'canceled' => [
                 'options' => [
-                    'qty_ordered' => 12,
-                    'qty_invoiced' => 0,
-                    'qty_refunded' => 0,
-                    'qty_shipped' => 0,
-                    'qty_canceled' => 12,
+                    'qty_ordered' => 12, 'qty_invoiced' => 0, 'qty_refunded' => 0, 'qty_shipped' => 0,
+                    'qty_canceled' => 12
                 ],
-                'expectedResult' => [
-                    'to_ship' => 0.0,
-                    'to_invoice' => 0.0
-                ]
+                'expectedResult' => ['to_ship' => 0.0, 'to_invoice' => 0.0]
             ],
             'completely_shipped_using_decimals' => [
                 'options' => [
-                    'qty_ordered' => 4.4,
-                    'qty_invoiced' => 0.4,
-                    'qty_refunded' => 0.4,
-                    'qty_shipped' => 4,
+                    'qty_ordered' => 4.4, 'qty_invoiced' => 0.4, 'qty_refunded' => 0.4, 'qty_shipped' => 4,
                     'qty_canceled' => 0,
                 ],
-                'expectedResult' => [
-                    'to_ship' => 0.0,
-                    'to_invoice' => 4.0
-                ]
+                'expectedResult' => ['to_ship' => 0.0, 'to_invoice' => 4.0]
             ],
             'completely_invoiced_using_decimals' => [
                 'options' => [
-                    'qty_ordered' => 4.4,
-                    'qty_invoiced' => 4,
-                    'qty_refunded' => 0,
-                    'qty_shipped' => 4,
-                    'qty_canceled' => 0.4,
+                    'qty_ordered' => 4.4, 'qty_invoiced' => 4, 'qty_refunded' => 0, 'qty_shipped' => 4,
+                    'qty_canceled' => 0.4
                 ],
-                'expectedResult' => [
-                    'to_ship' => 0.0,
-                    'to_invoice' => 0.0
-                ]
+                'expectedResult' => ['to_ship' => 0.0, 'to_invoice' => 0.0]
             ]
         ];
     }
-
 }
