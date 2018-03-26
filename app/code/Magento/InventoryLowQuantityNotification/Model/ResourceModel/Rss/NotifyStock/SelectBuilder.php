ddc553e5427178c7b522ecdd32aeeda5846968ce
RomanKis <romaikiss@gmail.com>
2018-03-26 17:03:23 +0300

    MSI-765: RSS feed works incorrect

--- app/code/Magento/InventoryLowQuantityNotification/Model/ResourceModel/Rss/NotifyStock/SelectBuilder.php
+++ app/code/Magento/InventoryLowQuantityNotification/Model/ResourceModel/Rss/NotifyStock/SelectBuilder.php
@@ -7,16 +7,8 @@ declare(strict_types=1);
 
 namespace Magento\InventoryLowQuantityNotification\Model\ResourceModel\Rss\NotifyStock;
 
-use Magento\Catalog\Api\Data\ProductInterface;
-use Magento\CatalogInventory\Api\Data\StockItemInterface;
 use Magento\Framework\App\ResourceConnection;
 use Magento\Framework\DB\Select;
-use Magento\Inventory\Model\ResourceModel\Source;
-use Magento\Inventory\Model\ResourceModel\SourceItem;
-use Magento\InventoryApi\Api\Data\SourceInterface;
-use Magento\InventoryApi\Api\Data\SourceItemInterface;
-use Magento\InventoryLowQuantityNotification\Setup\Operation\CreateSourceConfigurationTable;
-use Magento\InventoryLowQuantityNotificationApi\Api\Data\SourceItemConfigurationInterface;
 
 class SelectBuilder
 {
@@ -41,18 +33,26 @@ class SelectBuilder
     private $applyConfigurationCondition;
 
     /**
+     * @var ApplyBaseJoins
+     */
+    private $applyBaseJoins;
+
+    /**
      * @param ResourceConnection $resourceConnection
+     * @param ApplyBaseJoins $applyBaseJoins
      * @param ApplyNameAttributeJoin $applyNameAttributeJoin
      * @param ApplyStatusAttributeJoin $applyStatusAttributeJoin
      * @param ApplyConfigurationCondition $applyConfigurationCondition
      */
     public function __construct(
         ResourceConnection $resourceConnection,
+        ApplyBaseJoins $applyBaseJoins,
         ApplyNameAttributeJoin $applyNameAttributeJoin,
         ApplyStatusAttributeJoin $applyStatusAttributeJoin,
         ApplyConfigurationCondition $applyConfigurationCondition
     ) {
         $this->resourceConnection = $resourceConnection;
+        $this->applyBaseJoins = $applyBaseJoins;
         $this->applyNameAttributeJoin = $applyNameAttributeJoin;
         $this->applyStatusAttributeJoin = $applyStatusAttributeJoin;
         $this->applyConfigurationCondition = $applyConfigurationCondition;
@@ -65,34 +65,7 @@ class SelectBuilder
      */
     public function build(Select $select)
     {
-        $sourceItemConfigurationTable = CreateSourceConfigurationTable::TABLE_NAME_SOURCE_ITEM_CONFIGURATION;
-        $configurationJoinCondition =
-            'source_item_config.' . SourceItemConfigurationInterface::SKU . ' = product.' . ProductInterface::SKU . ' '
-            . Select::SQL_AND
-            . ' source_item_config.' . SourceItemConfigurationInterface::SOURCE_CODE
-            . ' = main_table.' . SourceItemInterface::SOURCE_CODE;
-
-        $select->join(
-            ['source' => $this->resourceConnection->getTableName(Source::TABLE_NAME_SOURCE)],
-            'source.' . SourceInterface::SOURCE_CODE . '= main_table.' . SourceItemInterface::SOURCE_CODE,
-            ['source_name' => 'source.' . SourceInterface::NAME]
-        )->join(
-            ['product' => $this->resourceConnection->getTableName('catalog_product_entity')],
-            'main_table.' . SourceItemInterface::SKU . ' = product.' . ProductInterface::SKU,
-            ['*']
-        )->join(
-            ['source_item_config' => $this->resourceConnection->getTableName($sourceItemConfigurationTable)],
-            $configurationJoinCondition,
-            ['source_item_config.' . SourceItemConfigurationInterface::INVENTORY_NOTIFY_QTY]
-        )->join(
-            ['invtr' => $this->resourceConnection->getTableName('cataloginventory_stock_item')],
-            'invtr.product_id = product.entity_id',
-            [
-                'invtr.' . StockItemInterface::LOW_STOCK_DATE,
-                'use_config' => 'invtr.' . StockItemInterface::USE_CONFIG_NOTIFY_STOCK_QTY
-            ]
-        )->group('main_table.' . SourceItem::ID_FIELD_NAME);
-
+        $this->applyBaseJoins->execute($select);
         $this->applyNameAttributeJoin->execute($select);
         $this->applyStatusAttributeJoin->execute($select);
         $this->applyConfigurationCondition->execute($select);
