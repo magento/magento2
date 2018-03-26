a263da54a6e96a330ddd4888eb1c0aa082751f22
Viktor Sevch <svitja@ukr.net>
2018-03-26 13:53:02 +0300

    MAGETWO-88187: Magento 2.1.3 to 2.2.1 upgrade issue

--- app/code/Magento/Theme/Test/Unit/Model/Config/Processor/DesignThemeTest.php
+++ app/code/Magento/Theme/Test/Unit/Model/Config/Processor/DesignThemeTest.php
@@ -5,7 +5,6 @@
  */
 namespace Magento\Theme\Test\Unit\Model\Config\Processor;
 
-use Magento\Config\App\Config\Source\DumpConfigSourceAggregated;
 use Magento\Framework\Stdlib\ArrayManager;
 use Magento\Framework\View\Design\Theme\ListInterface;
 use Magento\Theme\Model\Config\Processor\DesignTheme;
@@ -159,6 +158,22 @@ class DesignThemeTest extends \PHPUnit\Framework\TestCase
                     'websites' => [
                         'base' => [
                             'design' => ['theme' => ['theme_id' => 3]],
+                        ],
+                    ],
+                ],
+                [
+                    [
+                        'websites' => [
+                            'base' => [
+                                'design' => ['theme' => ['theme_id' => '']],
+                            ],
+                        ],
+                    ],
+                    [
+                        'websites' => [
+                            'base' => [
+                                'design' => ['theme' => ['theme_id' => '']],
+                            ],
                         ],
                     ],
                 ],
