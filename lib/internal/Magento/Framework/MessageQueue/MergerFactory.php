088582896312a0c6af96e56816eaef48c10c5edc
Eugene Tulika <vranen@gmail.com>
2018-03-27 21:14:31 -0500

    magento-engcom/bulk-api#4 Support for Async operations in WebAPI
    - Fixed static tests

--- lib/internal/Magento/Framework/MessageQueue/MergerFactory.php
+++ lib/internal/Magento/Framework/MessageQueue/MergerFactory.php
@@ -3,7 +3,6 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
-
 namespace Magento\Framework\MessageQueue;
 
 use Magento\Framework\ObjectManagerInterface;
@@ -25,7 +24,7 @@ class MergerFactory
      * @param ObjectManagerInterface $objectManager
      * @param string[] $mergers
      */
-    public function __construct(ObjectManagerInterface $objectManager, $mergers)
+    public function __construct(ObjectManagerInterface $objectManager, $mergers = [])
     {
         $this->objectManager = $objectManager;
         $this->mergers = $mergers;
@@ -38,18 +37,11 @@ class MergerFactory
      */
     public function create($consumerName)
     {
-        $matchMergerWildcard = $this->matchConsumer($consumerName);
-
-        if (!isset($this->mergers[$consumerName]) && !isset($matchMergerWildcard)) {
+        if (!isset($this->mergers[$consumerName])) {
             throw new \LogicException("Not found merger for consumer name '{$consumerName}'");
         }
 
-        if (isset($this->mergers[$consumerName])) {
-            $mergerClassName = $this->mergers[$consumerName];
-        } else {
-            $mergerClassName = $this->mergers[$matchMergerWildcard];
-        }
-
+        $mergerClassName = $this->mergers[$consumerName];
         $merger = $this->objectManager->get($mergerClassName);
 
         if (!$merger instanceof MergerInterface) {
@@ -61,41 +53,5 @@ class MergerFactory
         }
 
         return $merger;
-    }
-
-    /**
-     * @param $consumerName
-     * @return string|null
-     */
-    private function matchConsumer($consumerName)
-    {
-        $patterns = [];
-        foreach (array_keys($this->mergers) as $mergerFor) {
-            if (strpos($mergerFor, '*') !== false || strpos($mergerFor, '#') !== false) {
-                $patterns[$mergerFor] = $this->buildWildcardPattern($mergerFor);
-            }
-        }
-
-        foreach ($patterns as $mergerKey => $pattern) {
-            if (preg_match($pattern, $consumerName)) {
-                return $mergerKey;
-            }
-        }
-    }
-
-    /**
-     * Construct perl regexp pattern for matching topic names from wildcard key.
-     *
-     * @param string $wildcardKey
-     * @return string
-     */
-    private function buildWildcardPattern($wildcardKey)
-    {
-        $pattern = '/^' . str_replace('.', '\.', $wildcardKey);
-        $pattern = str_replace('#', '.+', $pattern);
-        $pattern = str_replace('*', '[^\.]+', $pattern);
-        $pattern .= strpos($wildcardKey, '#') === strlen($wildcardKey) ? '/' : '$/';
-
-        return $pattern;
     }
 }
