29ee0f026dacc7b966147d50417e8b80958c7df2
Dan Carlyon <dannycar@hotmail.co.uk>
2018-03-28 09:09:51 +0100

    Fixing returned path configs for multi server setup

--- lib/internal/Magento/Framework/Validator/Factory.php
+++ lib/internal/Magento/Framework/Validator/Factory.php
@@ -11,6 +11,11 @@
 namespace Magento\Framework\Validator;
 
 use Magento\Framework\Cache\FrontendInterface;
+use Magento\Framework\Module\Dir\Reader;
+use Magento\Framework\ObjectManagerInterface;
+use Magento\Framework\App\Filesystem\DirectoryList;
+use Magento\Framework\Config\FileIteratorFactory;
+use Magento\Framework\Phrase;
 
 class Factory
 {
@@ -18,7 +23,7 @@ class Factory
     const CACHE_KEY = __CLASS__;
 
     /**
-     * @var \Magento\Framework\ObjectManagerInterface
+     * @var ObjectManagerInterface
      */
     protected $_objectManager;
 
@@ -35,7 +40,7 @@ class Factory
     private $isDefaultTranslatorInitialized = false;
 
     /**
-     * @var \Magento\Framework\Module\Dir\Reader
+     * @var Reader
      */
     private $moduleReader;
 
@@ -45,20 +50,36 @@ class Factory
     private $cache;
 
     /**
+     * @var string
+     */
+    private $applicationRoot;
+
+    /**
+     * @var FileIteratorFactory
+     */
+    private $fileIteratorFactory;
+
+    /**
      * Initialize dependencies
      *
-     * @param \Magento\Framework\ObjectManagerInterface $objectManager
-     * @param \Magento\Framework\Module\Dir\Reader $moduleReader
+     * @param ObjectManagerInterface $objectManager
+     * @param Reader $moduleReader
      * @param FrontendInterface $cache
+     * @param DirectoryList $directoryList
+     * @param FileIteratorFactory $fileIteratorFactory
      */
     public function __construct(
-        \Magento\Framework\ObjectManagerInterface $objectManager,
-        \Magento\Framework\Module\Dir\Reader $moduleReader,
-        FrontendInterface $cache
+        ObjectManagerInterface $objectManager,
+        Reader $moduleReader,
+        FrontendInterface $cache,
+        DirectoryList $directoryList,
+        FileIteratorFactory $fileIteratorFactory
     ) {
         $this->_objectManager = $objectManager;
         $this->moduleReader = $moduleReader;
         $this->cache = $cache;
+        $this->applicationRoot = rtrim($directoryList->getRoot(), '/') . '/';
+        $this->fileIteratorFactory = $fileIteratorFactory;
     }
 
     /**
@@ -67,14 +88,43 @@ class Factory
     protected function _initializeConfigList()
     {
         if (!$this->_configFiles) {
-            $this->_configFiles = $this->cache->load(self::CACHE_KEY);
-            if (!$this->_configFiles) {
+            $serializedFilePaths = $this->cache->load(self::CACHE_KEY);
+            if (!$serializedFilePaths) {
                 $this->_configFiles = $this->moduleReader->getConfigurationFiles('validation.xml');
-                $this->cache->save(serialize($this->_configFiles), self::CACHE_KEY);
+                $relativeFilePaths = $this->getRelativeFilePathsForConfigFiles();
+                $this->cache->save(serialize($relativeFilePaths), self::CACHE_KEY);
             } else {
-                $this->_configFiles = unserialize($this->_configFiles);
+                $absolutePaths = $this->buildAbsolutePathsForConfigFiles($serializedFilePaths);
+                $this->_configFiles = $this->fileIteratorFactory->create($absolutePaths);
             }
         }
+    }
+
+    /**
+     * @param $serializedFilePaths
+     * @return array
+     */
+    protected function buildAbsolutePathsForConfigFiles($serializedFilePaths)
+    {
+        $absolutePaths = [];
+        /** @var array $serializedFilePaths */
+        foreach ($serializedFilePaths as $relativePath) {
+            $absolutePaths[] = $relativePath[0] === '/' ? $relativePath : $this->applicationRoot . $relativePath;
+        }
+        return $absolutePaths;
+    }
+
+    /**
+     * @return array
+     */
+    protected function getRelativeFilePathsForConfigFiles()
+    {
+        $relativeFilePaths = [];
+        $applicationRootLength = \strlen($this->applicationRoot);
+        foreach ($this->_configFiles as $configFile => $fileContent) {
+            $relativeFilePaths[] = strpos($configFile, $this->applicationRoot) === 0 ? substr($configFile, $applicationRootLength) : $configFile;
+        }
+        return $relativeFilePaths;
     }
 
     /**
@@ -88,12 +138,12 @@ class Factory
             // Pass translations to \Magento\Framework\TranslateInterface from validators
             $translatorCallback = function () {
                 $argc = func_get_args();
-                return (string)new \Magento\Framework\Phrase(array_shift($argc), $argc);
+                return (string)new Phrase(array_shift($argc), $argc);
             };
             /** @var \Magento\Framework\Translate\Adapter $translator */
             $translator = $this->_objectManager->create('Magento\Framework\Translate\Adapter');
             $translator->setOptions(['translator' => $translatorCallback]);
-            \Magento\Framework\Validator\AbstractValidator::setDefaultTranslator($translator);
+            AbstractValidator::setDefaultTranslator($translator);
             $this->isDefaultTranslatorInitialized = true;
         }
     }
@@ -119,6 +169,7 @@ class Factory
      * @param string $groupName
      * @param array|null $builderConfig
      * @return \Magento\Framework\Validator\Builder
+     * @throws \InvalidArgumentException
      */
     public function createValidatorBuilder($entityName, $groupName, array $builderConfig = null)
     {
