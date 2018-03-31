85ae6f5e8603e749a73a1f2d3869f6e2474ffe06
Vadim Justus <v.justus@techdivision.com>
2018-03-31 22:41:22 +0200

    magento-engcom/msi#457: Extend integration tests for the case that all simples are disabled

--- app/code/Magento/InventoryConfigurableProductIndexer/Test/Integration/StockIndexerTest.php
+++ app/code/Magento/InventoryConfigurableProductIndexer/Test/Integration/StockIndexerTest.php
@@ -19,6 +19,7 @@ use Magento\InventoryIndexer\Test\Integration\Indexer\RemoveIndexData;
 use Magento\InventoryApi\Api\SourceItemsSaveInterface;
 use Magento\InventoryApi\Api\Data\SourceItemInterface;
 use Magento\Framework\Api\SearchCriteriaBuilder;
+use Magento\Catalog\Api\ProductRepositoryInterface;
 
 class StockIndexerTest extends TestCase
 {
@@ -63,6 +64,11 @@ class StockIndexerTest extends TestCase
     private $searchCriteriaBuilder;
 
     /**
+     * @var ProductRepositoryInterface
+     */
+    private $productRepository;
+
+    /**
      * @inheritdoc
      */
     protected function setUp()
@@ -76,6 +82,7 @@ class StockIndexerTest extends TestCase
         $this->sourceItemSave = Bootstrap::getObjectManager()->get(SourceItemsSaveInterface::class);
         $this->sourceItemRepository = Bootstrap::getObjectManager()->get(SourceItemRepositoryInterface::class);
         $this->searchCriteriaBuilder = Bootstrap::getObjectManager()->get(SearchCriteriaBuilder::class);
+        $this->productRepository = Bootstrap::getObjectManager()->get(ProductRepositoryInterface::class);
 
         $this->removeIndexData = Bootstrap::getObjectManager()->get(RemoveIndexData::class);
         $this->removeIndexData->execute([10, 20, 30]);
@@ -195,5 +202,40 @@ class StockIndexerTest extends TestCase
 
         $stockItemData = $this->getStockItemData->execute($configurableSku, 30);
         self::assertEquals(1, $stockItemData[GetStockItemDataInterface::IS_SALABLE]);
+    }
+
+
+    // @codingStandardsIgnoreStart
+    /**
+     * @magentoDataFixture ../../../../app/code/Magento/InventorySalesApi/Test/_files/websites_with_stores.php
+     * @magentoDataFixture Magento/ConfigurableProduct/_files/configurable_attribute.php
+     * @magentoDataFixture ../../../../app/code/Magento/InventoryConfigurableProductIndexer/Test/_files/product_configurable_multiple.php
+     * @magentoDataFixture ../../../../app/code/Magento/InventoryApi/Test/_files/sources.php
+     * @magentoDataFixture ../../../../app/code/Magento/InventoryApi/Test/_files/stocks.php
+     * @magentoDataFixture ../../../../app/code/Magento/InventoryApi/Test/_files/stock_source_links.php
+     * @magentoDataFixture ../../../../app/code/Magento/InventoryConfigurableProductIndexer/Test/_files/source_items_configurable_multiple.php
+     * @magentoDataFixture ../../../../app/code/Magento/InventorySalesApi/Test/_files/stock_website_sales_channels.php
+     */
+    // @codingStandardsIgnoreEnd
+    public function testReindexListDisableAllSimples()
+    {
+        $configurableSku = 'configurable_1';
+        $children = $this->linkManagement->getChildren($configurableSku);
+        foreach ($children as $child) {
+            $child->setStatus(0);
+            $this->productRepository->save($child);
+        }
+
+        $this->removeIndexData->execute([10, 20, 30]);
+        $this->stockIndexer->executeList([10, 20, 30]);
+
+        $stockItemData = $this->getStockItemData->execute($configurableSku, 10);
+        self::assertEquals(0, $stockItemData[GetStockItemDataInterface::IS_SALABLE]);
+
+        $stockItemData = $this->getStockItemData->execute($configurableSku, 20);
+        self::assertEquals(0, $stockItemData[GetStockItemDataInterface::IS_SALABLE]);
+
+        $stockItemData = $this->getStockItemData->execute($configurableSku, 30);
+        self::assertEquals(0, $stockItemData[GetStockItemDataInterface::IS_SALABLE]);
     }
 }
