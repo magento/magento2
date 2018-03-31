82563693c2428d4fcbc3df3e66e11652cbbc90aa
Sergey <simpleadm@gmail.com>
2018-03-31 10:10:41 +0300

    Added strict type declaration for collection test

--- app/code/Magento/Theme/Test/Unit/Model/ResourceModel/Theme/Grid/CollectionTest.php
+++ app/code/Magento/Theme/Test/Unit/Model/ResourceModel/Theme/Grid/CollectionTest.php
@@ -3,6 +3,9 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+
+declare(strict_types=1);
+
 namespace Magento\Theme\Test\Unit\Model\ResourceModel\Theme\Grid;
 
 use Magento\Theme\Model\ResourceModel\Theme\Grid\Collection;
