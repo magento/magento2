2019a2245af9813b66ff0ab18815d021dc9143e1
Ihor Vansach <ihor@magefan.com>
2018-04-03 23:03:29 +0300

    Removed unused files + added copyrights

--- view/frontend/web/js/login.js
+++ view/frontend/web/js/login.js
@@ -1,3 +1,8 @@
+/**
+ * Copyright © Magefan (support@magefan.com). All rights reserved.
+ * See LICENSE.txt for license details (http://opensource.org/licenses/osl-3.0.php).
+ */
+
 define([
     'Magento_Customer/js/customer-data'
 ], function (customerData) {
