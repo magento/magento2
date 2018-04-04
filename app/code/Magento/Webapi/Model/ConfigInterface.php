48b90d1447c3bae2532e5c47b2e860d1787f134c
Vasiliev.A <a.vasiliev@sam-solutions.biz>
2018-04-04 11:54:48 +0300

    declare strict type

--- app/code/Magento/Webapi/Model/ConfigInterface.php
+++ app/code/Magento/Webapi/Model/ConfigInterface.php
@@ -3,6 +3,9 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+
+declare(strict_types=1);
+
 namespace Magento\Webapi\Model;
 /**
  * This class gives access to consolidated web API configuration from <Module_Name>/etc/webapi.xml files.
--- app/code/Magento/WebapiAsync/Controller/Rest/Asynchronous/InputParamsResolver.php
+++ app/code/Magento/WebapiAsync/Controller/Rest/Asynchronous/InputParamsResolver.php
@@ -4,6 +4,8 @@
  * See COPYING.txt for license details.
  */
 
+declare(strict_types=1);
+
 namespace Magento\WebapiAsync\Controller\Rest\Asynchronous;
 
 use Magento\Framework\Webapi\ServiceInputProcessor;
