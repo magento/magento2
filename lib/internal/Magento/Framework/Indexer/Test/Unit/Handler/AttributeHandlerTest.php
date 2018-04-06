4a2284c74d268b8bf4dd20eb1e32230baaa412b1
Andrii Meysar <andrii.meysar@transoftgroup.com>
2018-04-06 11:04:31 +0300

    MAGETWO-90109: Customer Grid Indexer not working #10838

---
+++ lib/internal/Magento/Framework/Indexer/Test/Unit/Handler/AttributeHandlerTest.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\Framework\Indexer\Test\Unit\Handler;
+
+use Magento\Framework\Indexer\Handler\AttributeHandler;
+use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
+use Magento\Framework\App\ResourceConnection\SourceProviderInterface;
+
+/**
+ * Unit test for Magento\Framework\Indexer\Handler\AttributeHandler.
+ */
+class AttributeHandlerTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @var SourceProviderInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $source;
+
+    /**
+     * @var AttributeHandler
+     */
+    private $subject;
+
+    /**
+     * @inheritdoc
+     */
+    protected function setUp()
+    {
+        $this->source = $this->getMockBuilder(SourceProviderInterface::class)
+            ->disableOriginalConstructor()
+            ->getMockForAbstractClass();
+
+        $objectManager = new ObjectManager($this);
+
+        $this->subject = $objectManager->getObject(AttributeHandler::class);
+    }
+
+    public function testPrepareSql()
+    {
+        $alias = 'e';
+        $fieldInfo = [
+            'name' => 'is_approved',
+            'origin' => 'is_approved',
+            'type' => 'searchable',
+            'dataType' => 'varchar',
+            'entity' => 'customer',
+            'bind' => null,
+        ];
+        $this->source->expects($this->once())
+            ->method('addFieldToSelect')
+            ->with('is_approved', 'left')
+            ->willReturnSelf();
+
+        $this->subject->prepareSql($this->source, $alias, $fieldInfo);
+    }
+}
