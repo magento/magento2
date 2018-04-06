46d6f72c8b1757c1e49032dc8c1ed69a671d4e8b
OlgaVasyltsun <olga.vasyltsun@transoftgroup.com>
2018-04-06 10:20:28 +0300

    MAGETWO-75787: [2.3] - Incorrect count for category filter at layered navigation for configurable with no available options

--- app/code/Magento/Catalog/Model/Indexer/Product/Category/Action/Rows.php
+++ app/code/Magento/Catalog/Model/Indexer/Product/Category/Action/Rows.php
@@ -7,8 +7,12 @@ namespace Magento\Catalog\Model\Indexer\Product\Category\Action;
 
 use Magento\Catalog\Model\Category;
 use Magento\Catalog\Model\Product;
+use Magento\Framework\App\ObjectManager;
 use Magento\Framework\Indexer\CacheContext;
 
+/**
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+ */
 class Rows extends \Magento\Catalog\Model\Indexer\Category\Product\AbstractAction
 {
     /**
@@ -19,9 +23,38 @@ class Rows extends \Magento\Catalog\Model\Indexer\Category\Product\AbstractActio
     protected $limitationByProducts;
 
     /**
-     * @var \Magento\Framework\Indexer\CacheContext
+     * @var CacheContext
      */
     private $cacheContext;
+
+    /**
+     * @var \Magento\Framework\Event\ManagerInterface|null
+     */
+    private $eventManager;
+
+    /**
+     * @param \Magento\Framework\App\ResourceConnection $resource
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+     * @param \Magento\Catalog\Model\Config $config
+     * @param \Magento\Framework\DB\Query\Generator|null $queryGenerator
+     * @param \Magento\Framework\EntityManager\MetadataPool|null $metadataPool
+     * @param CacheContext|null $cacheContext
+     * @param \Magento\Framework\Event\ManagerInterface|null $eventManager
+     */
+    public function __construct(
+        \Magento\Framework\App\ResourceConnection $resource,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Catalog\Model\Config $config,
+        \Magento\Framework\DB\Query\Generator $queryGenerator = null,
+        \Magento\Framework\EntityManager\MetadataPool $metadataPool = null,
+        CacheContext $cacheContext = null,
+        \Magento\Framework\Event\ManagerInterface $eventManager = null
+    ) {
+        parent::__construct($resource, $storeManager, $config, $queryGenerator, $metadataPool);
+        $this->cacheContext = $cacheContext ?: ObjectManager::getInstance()->get(CacheContext::class);
+        $this->eventManager = $eventManager ?:
+            ObjectManager::getInstance()->get(\Magento\Framework\Event\ManagerInterface::class);
+    }
 
     /**
      * Refresh entities index
@@ -29,20 +62,62 @@ class Rows extends \Magento\Catalog\Model\Indexer\Category\Product\AbstractActio
      * @param int[] $entityIds
      * @param bool $useTempTable
      * @return $this
+     * @throws \Exception if metadataPool doesn't contain metadata for ProductInterface
+     * @throws \DomainException
      */
     public function execute(array $entityIds = [], $useTempTable = false)
     {
-        $this->limitationByProducts = $entityIds;
+        $idsToBeReIndexed = $this->getProductIdsWithParents($entityIds);
+
+        $this->limitationByProducts = $idsToBeReIndexed;
         $this->useTempTable = $useTempTable;
+
+        $affectedCategories = $this->getCategoryIdsFromIndex($idsToBeReIndexed);
 
         $this->removeEntries();
 
         $this->reindex();
 
-        $this->registerProducts($entityIds);
-        $this->registerCategories($entityIds);
+        $affectedCategories = array_merge($affectedCategories, $this->getCategoryIdsFromIndex($idsToBeReIndexed));
+
+        $this->registerProducts($idsToBeReIndexed);
+        $this->registerCategories($affectedCategories);
+        $this->eventManager->dispatch('clean_cache_by_tags', ['object' => $this->cacheContext]);
 
         return $this;
+    }
+
+    /**
+     * Get IDs of parent products by their child IDs.
+     *
+     * Returns identifiers of parent product from the catalog_product_relation.
+     * Please note that returned ids don't contain ids of passed child products.
+     *
+     * @param int[] $childProductIds
+     * @return int[]
+     * @throws \Exception if metadataPool doesn't contain metadata for ProductInterface
+     * @throws \DomainException
+     */
+    private function getProductIdsWithParents(array $childProductIds)
+    {
+        /** @var \Magento\Framework\EntityManager\EntityMetadataInterface $metadata */
+        $metadata = $this->metadataPool->getMetadata(\Magento\Catalog\Api\Data\ProductInterface::class);
+        $fieldForParent = $metadata->getLinkField();
+
+        $select = $this->connection
+            ->select()
+            ->from(['relation' => $this->getTable('catalog_product_relation')], [])
+            ->distinct(true)
+            ->where('child_id IN (?)', $childProductIds)
+            ->join(
+                ['cpe' => $this->getTable('catalog_product_entity')],
+                'relation.parent_id = cpe.' . $fieldForParent,
+                ['cpe.entity_id']
+            );
+
+        $parentProductIds = $this->connection->fetchCol($select);
+
+        return array_unique(array_merge($childProductIds, $parentProductIds));
     }
 
     /**
@@ -53,26 +128,19 @@ class Rows extends \Magento\Catalog\Model\Indexer\Category\Product\AbstractActio
      */
     private function registerProducts($entityIds)
     {
-        $this->getCacheContext()->registerEntities(Product::CACHE_TAG, $entityIds);
+        $this->cacheContext->registerEntities(Product::CACHE_TAG, $entityIds);
     }
 
     /**
      * Register categories assigned to products
      *
-     * @param array $entityIds
+     * @param array $categoryIds
      * @return void
      */
-    private function registerCategories($entityIds)
+    private function registerCategories(array $categoryIds)
     {
-        $categories = $this->connection->fetchCol(
-            $this->connection->select()
-                ->from($this->getMainTable(), ['category_id'])
-                ->where('product_id IN (?)', $entityIds)
-                ->distinct()
-        );
-
-        if ($categories) {
-            $this->getCacheContext()->registerEntities(Category::CACHE_TAG, $categories);
+        if ($categoryIds) {
+            $this->cacheContext->registerEntities(Category::CACHE_TAG, $categoryIds);
         }
     }
 
@@ -98,7 +166,8 @@ class Rows extends \Magento\Catalog\Model\Indexer\Category\Product\AbstractActio
     protected function getNonAnchorCategoriesSelect(\Magento\Store\Model\Store $store)
     {
         $select = parent::getNonAnchorCategoriesSelect($store);
-        return $select->where('ccp.product_id IN (?)', $this->limitationByProducts);
+
+        return $select->where('ccp.product_id IN (?) OR relation.child_id IN (?)', $this->limitationByProducts);
     }
 
     /**
@@ -136,16 +205,25 @@ class Rows extends \Magento\Catalog\Model\Indexer\Category\Product\AbstractActio
     }
 
     /**
-     * Get cache context
+     * Returns a list of category ids which are assigned to product ids in the index
      *
      * @return \Magento\Framework\Indexer\CacheContext
-     * @deprecated 101.0.0
      */
-    private function getCacheContext()
+    private function getCategoryIdsFromIndex(array $productIds)
     {
-        if ($this->cacheContext === null) {
-            $this->cacheContext = \Magento\Framework\App\ObjectManager::getInstance()->get(CacheContext::class);
+        $categoryIds = $this->connection->fetchCol(
+            $this->connection->select()
+                ->from($this->getMainTable(), ['category_id'])
+                ->where('product_id IN (?)', $productIds)
+                ->distinct()
+        );
+        $parentCategories = $categoryIds;
+        foreach ($categoryIds as $categoryId) {
+            $parentIds = explode('/', $this->getPathFromCategoryId($categoryId));
+            $parentCategories = array_merge($parentCategories, $parentIds);
         }
-        return $this->cacheContext;
+        $categoryIds = array_unique($parentCategories);
+
+        return $categoryIds;
     }
 }
