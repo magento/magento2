0933c7a1b5f12f7995c42f0a6393f6c4af5c0ffc
Andrii Meysar <andrii.meysar@transoftgroup.com>
2018-04-06 14:45:32 +0300

    MAGETWO-90109: Customer Grid Indexer not working #10838

--- lib/internal/Magento/Framework/Indexer/Test/Unit/Handler/AttributeHandlerTest.php
+++ lib/internal/Magento/Framework/Indexer/Test/Unit/Handler/AttributeHandlerTest.php
@@ -5,6 +5,23 @@
  */
 declare(strict_types=1);
 
+// @codingStandardsIgnoreStart
+namespace Magento\Framework\Indexer\Handler;
+
+/**
+ * Mock method for built-in function method_exists.
+ *
+ * @param mixed $object
+ * @param string $method_name
+ * @return bool
+ * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+ */
+function method_exists($object, $method_name)
+{
+    return \Magento\Framework\Indexer\Test\Unit\Handler\AttributeHandlerTest::$methodExits;
+}
+// codingStandardsIgnoreEnd
+
 namespace Magento\Framework\Indexer\Test\Unit\Handler;
 
 use Magento\Framework\Indexer\Handler\AttributeHandler;
@@ -16,6 +33,13 @@ use Magento\Framework\App\ResourceConnection\SourceProviderInterface;
  */
 class AttributeHandlerTest extends \PHPUnit\Framework\TestCase
 {
+    /**
+     * Static field responsible for mocking built-in method_exists function result.
+     *
+     * @var bool
+     */
+    public static $methodExits = false;
+
     /**
      * @var SourceProviderInterface|\PHPUnit_Framework_MockObject_MockObject
      */
@@ -33,6 +57,7 @@ class AttributeHandlerTest extends \PHPUnit\Framework\TestCase
     {
         $this->source = $this->getMockBuilder(SourceProviderInterface::class)
             ->disableOriginalConstructor()
+            ->setMethods(['joinAttribute'])
             ->getMockForAbstractClass();
 
         $objectManager = new ObjectManager($this);
@@ -40,7 +65,52 @@ class AttributeHandlerTest extends \PHPUnit\Framework\TestCase
         $this->subject = $objectManager->getObject(AttributeHandler::class);
     }
 
-    public function testPrepareSql()
+    public function testPrepareSqlWithBindAndMissingJoinAttributeMethod()
+    {
+        $alias = 'e';
+        $fieldInfo = [
+            'name' => 'is_approved',
+            'origin' => 'is_approved',
+            'type' => 'searchable',
+            'dataType' => 'varchar',
+            'entity' => 'customer',
+            'bind' => '',
+        ];
+
+        self::$methodExits = false;
+        $this->source->expects($this->never())->method('joinAttribute');
+
+        $this->subject->prepareSql($this->source, $alias, $fieldInfo);
+    }
+
+    public function testPrepareSqlWithBindAndExistingJoinAttributeMethod()
+    {
+        $alias = 'e';
+        $fieldInfo = [
+            'name' => 'is_approved',
+            'origin' => 'is_approved',
+            'type' => 'searchable',
+            'dataType' => 'varchar',
+            'entity' => 'customer',
+            'bind' => '',
+        ];
+
+        self::$methodExits = true;
+        $this->source->expects($this->once())
+            ->method('joinAttribute')
+            ->with(
+                $fieldInfo['name'],
+                $fieldInfo['entity'] . '/' . $fieldInfo['origin'],
+                $fieldInfo['bind'],
+                null,
+                'left'
+            )
+            ->willReturnSelf();
+
+        $this->subject->prepareSql($this->source, $alias, $fieldInfo);
+    }
+
+    public function testPrepareSqlWithoutBind()
     {
         $alias = 'e';
         $fieldInfo = [
@@ -53,8 +123,7 @@ class AttributeHandlerTest extends \PHPUnit\Framework\TestCase
         ];
         $this->source->expects($this->once())
             ->method('addFieldToSelect')
-            ->with('is_approved', 'left')
-            ->willReturnSelf();
+            ->with('is_approved', 'left');
 
         $this->subject->prepareSql($this->source, $alias, $fieldInfo);
     }
