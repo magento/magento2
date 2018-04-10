f5734a81c9dc8a93dd830d25f8514106139abe9f
Jeroen <jeroen@reachdigital.nl>
2018-04-10 11:40:55 +0200

    Update ItemRepository.php

--- app/code/Magento/Sales/Model/Order/ItemRepository.php
+++ app/code/Magento/Sales/Model/Order/ItemRepository.php
@@ -114,6 +114,7 @@ class ItemRepository implements OrderItemRepositoryInterface
             }
 
             $this->addProductOption($orderItem);
+            $this->addParentItem($orderItem);
             $this->registry[$id] = $orderItem;
         }
         return $this->registry[$id];
@@ -134,6 +135,7 @@ class ItemRepository implements OrderItemRepositoryInterface
         /** @var OrderItemInterface $orderItem */
         foreach ($searchResult->getItems() as $orderItem) {
             $this->addProductOption($orderItem);
+            $this->addParentItem($orderItem);
         }
 
         return $searchResult;
@@ -211,6 +213,20 @@ class ItemRepository implements OrderItemRepositoryInterface
         }
 
         return $this;
+    }
+
+    /**
+     * Set parent item.
+     *
+     * @param OrderItemInterface $orderItem
+     * @throws InputException
+     * @throws NoSuchEntityException
+     */
+    private function addParentItem(OrderItemInterface $orderItem)
+    {
+        if ($parentId = $orderItem->getParentItemId()) {
+            $orderItem->setParentItem($this->get($parentId));
+        }
     }
 
     /**
