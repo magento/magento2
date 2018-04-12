c72fed2c8780c26f4b15f776a63fa98fcdfd564c
Vasiliev.A <a.vasiliev@sam-solutions.biz>
2018-04-12 16:07:39 +0300

    Api endpoint for search operations by criteria

---
+++ app/code/Magento/AsynchronousOperations/Model/OperationRepository.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\AsynchronousOperations\Model;
+
+use Magento\Framework\App\ObjectManager;
+use Magento\AsynchronousOperations\Api\Data\DetailedOperationStatusInterfaceFactory;
+use Magento\Framework\EntityManager\EntityManager;
+use Magento\Framework\Api\SearchCriteria\CollectionProcessorInterface;
+use Magento\AsynchronousOperations\Api\Data\OperationSearchResultsInterfaceFactory as SearchResultFactory;
+use Magento\AsynchronousOperations\Model\ResourceModel\Operation\CollectionFactory;
+use Magento\Framework\Exception\InputException;
+use Magento\Framework\Exception\NoSuchEntityException;
+
+/**
+ * Class OperationManagement
+ */
+class OperationRepository implements \Magento\AsynchronousOperations\Api\OperationRepositoryInterface
+{
+    /**
+     * @var EntityManager
+     */
+    private $entityManager;
+
+    /**
+     * @var CollectionFactory
+     */
+    private $collectionFactory;
+
+    /**
+     * @var SearchResultFactory
+     */
+    private $searchResultFactory;
+
+    /**
+     * @var CollectionProcessorInterface
+     */
+    private $collectionProcessor;
+
+    /**
+     * @var \Psr\Log\LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * Initialize dependencies.
+     *
+     * @param \Magento\Framework\EntityManager\EntityManager $entityManager
+     * @param \Magento\AsynchronousOperations\Api\Data\OperationSearchResultsInterfaceFactory $searchResultFactory
+     * @param \Magento\Framework\Api\SearchCriteria\CollectionProcessorInterface|null $collectionProcessor
+     * @param \Psr\Log\LoggerInterface $logger
+     */
+    public function __construct(
+        EntityManager $entityManager,
+        CollectionFactory $collectionFactory,
+        SearchResultFactory $searchResultFactory,
+        CollectionProcessorInterface $collectionProcessor = null,
+        \Psr\Log\LoggerInterface $logger
+    ) {
+        $this->entityManager = $entityManager;
+        $this->collectionFactory = $collectionFactory;
+        $this->searchResultFactory = $searchResultFactory;
+        $this->collectionProcessor = $collectionProcessor ? : ObjectManager::getInstance()
+            ->get(\Magento\Framework\Api\SearchCriteria\CollectionProcessorInterface::class);
+        $this->logger = $logger;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getList(\Magento\Framework\Api\SearchCriteriaInterface $searchCriteria)
+    {
+        /** @var \Magento\AsynchronousOperations\Model\ResourceModel\Operation\Collection $collection */
+        $collection = $this->collectionFactory->create();
+        $this->collectionProcessor->process($searchCriteria, $collection);
+        $collection->load();
+        /** @var \Magento\AsynchronousOperations\Api\Data\OperationSearchResultsInterface $searchResult */
+        $searchResult = $this->searchResultFactory->create();
+        $searchResult->setSearchCriteria($searchCriteria);
+        $searchResult->setItems($collection->getItems());
+        $searchResult->setTotalCount($collection->getSize());
+
+        return $searchResult;
+    }
+}
