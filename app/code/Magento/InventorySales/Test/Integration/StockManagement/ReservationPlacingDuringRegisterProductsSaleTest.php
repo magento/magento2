e7ac476e3d5fcc69b47a9ca8d2fc7f314c9f2cfe
Pavel Bystritsky <p.bystritsky@yandex.ru>
2018-04-18 11:27:27 +0300

    MSI-918: Need to refactor test ReservationPlacingDuringRegisterProductsSaleTest::testRegisterProductsSale as we don't make source deduction at the time of RegisterSales Anymore.

--- app/code/Magento/InventorySales/Test/Integration/StockManagement/ReservationPlacingDuringRegisterProductsSaleTest.php
+++ app/code/Magento/InventorySales/Test/Integration/StockManagement/ReservationPlacingDuringRegisterProductsSaleTest.php
@@ -7,23 +7,72 @@ declare(strict_types=1);
 
 namespace Magento\InventorySales\Test\Integration\StockManagement;
 
+use Magento\Catalog\Api\Data\ProductInterface;
 use Magento\Catalog\Api\ProductRepositoryInterface;
-use Magento\CatalogInventory\Model\StockManagement;
-use Magento\InventoryApi\Api\StockRepositoryInterface;
+use Magento\Framework\Api\SearchCriteriaBuilder;
+use Magento\Framework\Registry;
 use Magento\InventoryReservations\Model\CleanupReservationsInterface;
 use Magento\InventoryReservations\Model\ReservationBuilderInterface;
 use Magento\InventoryReservationsApi\Api\AppendReservationsInterface;
-use Magento\Store\Api\WebsiteRepositoryInterface;
-use PHPUnit\Framework\TestCase;
-use Magento\TestFramework\Helper\Bootstrap;
 use Magento\InventorySalesApi\Api\GetProductSalableQtyInterface;
+use Magento\Quote\Api\CartManagementInterface;
+use Magento\Quote\Api\CartRepositoryInterface;
+use Magento\Quote\Api\Data\CartInterface;
+use Magento\Quote\Api\Data\CartItemInterface;
+use Magento\Quote\Api\Data\CartItemInterfaceFactory;
+use Magento\Sales\Api\OrderManagementInterface;
+use Magento\Sales\Api\OrderRepositoryInterface;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\TestFramework\Helper\Bootstrap;
+use PHPUnit\Framework\TestCase;
 
+/**
+ * Tests correct Product Salable Quantity decreasing after Order placing.
+ *
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+ */
 class ReservationPlacingDuringRegisterProductsSaleTest extends TestCase
 {
+    /**
+     * @var AppendReservationsInterface
+     */
+    private $appendReservations;
+
+    /**
+     * @var CartItemInterfaceFactory
+     */
+    private $cartItemFactory;
+
+    /**
+     * @var CartManagementInterface
+     */
+    private $cartManagement;
+
+    /**
+     * @var CartRepositoryInterface
+     */
+    private $cartRepository;
+
+    /**
+     * @var CleanupReservationsInterface
+     */
+    private $cleanupReservations;
+
+
     /**
      * @var GetProductSalableQtyInterface
      */
     private $getProductSalableQty;
+
+    /**
+     * @var OrderManagementInterface
+     */
+    private $orderManagement;
+
+    /**
+     * @var OrderRepositoryInterface
+     */
+    private $orderRepository;
 
     /**
      * @var ProductRepositoryInterface
@@ -31,14 +80,9 @@ class ReservationPlacingDuringRegisterProductsSaleTest extends TestCase
     private $productRepository;
 
     /**
-     * @var StockRepositoryInterface
+     * @var Registry
      */
-    private $stockRepository;
-
-    /**
-     * @var WebsiteRepositoryInterface
-     */
-    private $websiteRepository;
+    private $registry;
 
     /**
      * @var ReservationBuilderInterface
@@ -46,30 +90,30 @@ class ReservationPlacingDuringRegisterProductsSaleTest extends TestCase
     private $reservationBuilder;
 
     /**
-     * @var AppendReservationsInterface
+     * @var SearchCriteriaBuilder
      */
-    private $appendReservations;
+    private $searchCriteriaBuilder;
 
     /**
-     * @var CleanupReservationsInterface
+     * @var StoreManagerInterface
      */
-    private $cleanupReservations;
-
-    /**
-     * @var StockManagement
-     */
-    private $stockManagement;
+    private $storeManager;
 
     protected function setUp()
     {
-        $this->getProductSalableQty = Bootstrap::getObjectManager()->get(GetProductSalableQtyInterface::class);
-        $this->productRepository = Bootstrap::getObjectManager()->get(ProductRepositoryInterface::class);
-        $this->stockRepository = Bootstrap::getObjectManager()->get(StockRepositoryInterface::class);
-        $this->websiteRepository = Bootstrap::getObjectManager()->get(WebsiteRepositoryInterface::class);
-        $this->reservationBuilder = Bootstrap::getObjectManager()->get(ReservationBuilderInterface::class);
         $this->appendReservations = Bootstrap::getObjectManager()->get(AppendReservationsInterface::class);
+        $this->cartItemFactory = Bootstrap::getObjectManager()->get(CartItemInterfaceFactory::class);
+        $this->cartManagement = Bootstrap::getObjectManager()->get(CartManagementInterface::class);
+        $this->cartRepository = Bootstrap::getObjectManager()->get(CartRepositoryInterface::class);
         $this->cleanupReservations = Bootstrap::getObjectManager()->get(CleanupReservationsInterface::class);
-        $this->stockManagement = Bootstrap::getObjectManager()->get(StockManagement::class);
+        $this->getProductSalableQty = Bootstrap::getObjectManager()->get(GetProductSalableQtyInterface::class);
+        $this->orderManagement = Bootstrap::getObjectManager()->get(OrderManagementInterface::class);
+        $this->orderRepository = Bootstrap::getObjectManager()->get(OrderRepositoryInterface::class);
+        $this->productRepository = Bootstrap::getObjectManager()->get(ProductRepositoryInterface::class);
+        $this->registry = Bootstrap::getObjectManager()->get(Registry::class);
+        $this->reservationBuilder = Bootstrap::getObjectManager()->get(ReservationBuilderInterface::class);
+        $this->searchCriteriaBuilder = Bootstrap::getObjectManager()->get(SearchCriteriaBuilder::class);
+        $this->storeManager = Bootstrap::getObjectManager()->get(StoreManagerInterface::class);
     }
 
     /**
@@ -81,6 +125,8 @@ class ReservationPlacingDuringRegisterProductsSaleTest extends TestCase
     }
 
     /**
+     * Tests correct Product Salable Quantity decreasing after Order placing.
+     *
      * @magentoDataFixture ../../../../app/code/Magento/InventoryApi/Test/_files/products.php
      * @magentoDataFixture ../../../../app/code/Magento/InventoryApi/Test/_files/sources.php
      * @magentoDataFixture ../../../../app/code/Magento/InventoryApi/Test/_files/stocks.php
@@ -89,15 +135,22 @@ class ReservationPlacingDuringRegisterProductsSaleTest extends TestCase
      * @magentoDataFixture ../../../../app/code/Magento/InventoryIndexer/Test/_files/reindex_inventory.php
      * @magentoDataFixture ../../../../app/code/Magento/InventorySalesApi/Test/_files/websites_with_stores.php
      * @magentoDataFixture ../../../../app/code/Magento/InventorySalesApi/Test/_files/stock_website_sales_channels.php
+     * @magentoDataFixture ../../../../app/code/Magento/InventorySalesApi/Test/_files/quote.php
      */
     public function testRegisterProductsSale()
     {
-        $this->markTestSkipped('https://github.com/magento-engcom/msi/issues/918');
         self::assertEquals(8.5, $this->getProductSalableQty->execute('SKU-1', 10));
+        $this->storeManager->setCurrentStore('store_for_eu_website');
+        $sku = 'SKU-1';
+        $quoteItemQty = 3.5;
 
-        $product = $this->productRepository->get('SKU-1');
-        $website = $this->websiteRepository->get('eu_website');
-        $this->stockManagement->registerProductsSale([$product->getId() => 3.5], $website->getId());
+        $cart = $this->getCart();
+        $product = $this->productRepository->get($sku);
+        $cartItem = $this->getCartItem($product, $quoteItemQty, (int)$cart->getId());
+        $cart->addItem($cartItem);
+        $this->cartRepository->save($cart);
+
+        $orderId = $this->cartManagement->placeOrder($cart->getId());
 
         self::assertEquals(5, $this->getProductSalableQty->execute('SKU-1', 10));
 
@@ -105,5 +158,66 @@ class ReservationPlacingDuringRegisterProductsSaleTest extends TestCase
             // unreserved 3.5 units for cleanup
             $this->reservationBuilder->setStockId(10)->setSku('SKU-1')->setQuantity(3.5)->build(),
         ]);
+
+        //cleanup
+        $this->deleteOrderById((int)$orderId);
+    }
+
+    /**
+     * Get Cart for placing Order.
+     *
+     * @return CartInterface
+     */
+    private function getCart(): CartInterface
+    {
+        $searchCriteria = $this->searchCriteriaBuilder
+            ->addFilter('reserved_order_id', 'test_order_1')
+            ->create();
+        /** @var CartInterface $cart */
+        $cart = current($this->cartRepository->getList($searchCriteria)->getItems());
+        $cart->setStoreId(1);
+
+        return $cart;
+    }
+
+    /**
+     * Create Cart Item from Product and Quantity.
+     *
+     * @param ProductInterface $product
+     * @param float $quoteItemQty
+     * @param int $cartId
+     * @return CartItemInterface
+     */
+    private function getCartItem(ProductInterface $product, float $quoteItemQty, int $cartId): CartItemInterface
+    {
+        /** @var CartItemInterface $cartItem */
+        $cartItem = $this->cartItemFactory->create(
+                [
+                    'data' => [
+                        CartItemInterface::KEY_SKU => $product->getSku(),
+                        CartItemInterface::KEY_QTY => $quoteItemQty,
+                        CartItemInterface::KEY_QUOTE_ID => $cartId,
+                        'product_id' => $product->getId(),
+                        'product' => $product
+                    ]
+                ]
+            );
+
+        return $cartItem;
+    }
+
+    /**
+     * Rollback created Order.
+     *
+     * @param int $orderId
+     */
+    private function deleteOrderById(int $orderId)
+    {
+        $this->registry->unregister('isSecureArea');
+        $this->registry->register('isSecureArea', true);
+        $this->orderManagement->cancel($orderId);
+        $this->orderRepository->delete($this->orderRepository->get($orderId));
+        $this->registry->unregister('isSecureArea');
+        $this->registry->register('isSecureArea', false);
     }
 }
