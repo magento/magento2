1dc0abf627519cca0dd16462b87f6e1af2802dac
Sanket Kulkarni <sanket@krishaweb.com>
2018-04-20 16:40:56 +0530

    Change Qty to float in mminicart

--- app/code/Magento/Checkout/Controller/Sidebar/UpdateItemQty.php
+++ app/code/Magento/Checkout/Controller/Sidebar/UpdateItemQty.php
@@ -55,7 +55,7 @@ class UpdateItemQty extends Action
     public function execute()
     {
         $itemId = (int)$this->getRequest()->getParam('item_id');
-        $itemQty = (int)$this->getRequest()->getParam('item_qty');
+        $itemQty = (float)$this->getRequest()->getParam('item_qty');
 
         try {
             $this->sidebar->checkQuoteItem($itemId);
