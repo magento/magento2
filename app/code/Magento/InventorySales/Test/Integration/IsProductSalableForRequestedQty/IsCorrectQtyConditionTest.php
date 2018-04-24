fe108a70afb4d88a714171c9115f31098c69bfe9
RomanKis <romaikiss@gmail.com>
2018-04-24 09:26:34 +0300

    MSI-722: Implement IsCorrectQtyConditionTest::testExecuteWithUseConfigMaxSaleQty

--- app/code/Magento/InventorySales/Test/Integration/IsProductSalableForRequestedQty/IsCorrectQtyConditionTest.php
+++ app/code/Magento/InventorySales/Test/Integration/IsProductSalableForRequestedQty/IsCorrectQtyConditionTest.php
@@ -75,9 +75,63 @@ class IsCorrectQtyConditionTest extends TestCase
         $this->markTestIncomplete('Still to implement');
     }
 
-    public function testExecuteWithUseConfigMaxSaleQty()
+    /**
+     * @magentoDataFixture ../../../../app/code/Magento/InventoryApi/Test/_files/products.php
+     * @magentoDataFixture ../../../../app/code/Magento/InventoryApi/Test/_files/sources.php
+     * @magentoDataFixture ../../../../app/code/Magento/InventoryApi/Test/_files/stocks.php
+     * @magentoDataFixture ../../../../app/code/Magento/InventoryApi/Test/_files/source_items.php
+     * @magentoDataFixture ../../../../app/code/Magento/InventoryApi/Test/_files/stock_source_links.php
+     * @magentoDataFixture ../../../../app/code/Magento/InventoryIndexer/Test/_files/reindex_inventory.php
+     * @magentoConfigFixture current_store cataloginventory/item_options/max_sale_qty 6
+     *
+     * @param string $sku
+     * @param int $stockId
+     * @param int $requestedQty
+     * @param bool $expectedResult
+     *
+     * @return void
+     *
+     * @dataProvider executeWithUseConfigMaxSaleQtyDataProvider
+     */
+    public function testExecuteWithUseConfigMaxSaleQty(
+        string $sku,
+        int $stockId,
+        int $requestedQty,
+        bool $expectedResult
+    ): void {
+        $result = $this->isProductSalableForRequestedQty->execute($sku, $stockId, $requestedQty);
+        $this->assertEquals($expectedResult, $result->isSalable());
+    }
+
+    /**
+     * @return array
+     */
+    public function executeWithUseConfigMaxSaleQtyDataProvider(): array
     {
-        $this->markTestIncomplete('Still to implement');
+        return [
+            ['SKU-1', 10, 1, true],
+            ['SKU-1', 10, 6, true],
+            ['SKU-1', 10, 7, false],
+            ['SKU-2', 10, 1, false],
+            ['SKU-2', 10, 7, false],
+            ['SKU-3', 10, 1, false],
+            ['SKU-3', 10, 7, false],
+            ['SKU-1', 20, 1, false],
+            ['SKU-1', 20, 7, false],
+            ['SKU-2', 20, 1, true],
+            ['SKU-2', 20, 6, false],
+            ['SKU-2', 20, 7, false],
+            ['SKU-3', 20, 1, false],
+            ['SKU-3', 20, 7, false],
+            ['SKU-1', 30, 1, true],
+            ['SKU-1', 30, 6, true],
+            ['SKU-1', 30, 7, false],
+            ['SKU-2', 30, 1, true],
+            ['SKU-2', 30, 6, false],
+            ['SKU-2', 30, 7, false],
+            ['SKU-3', 30, 1, false],
+            ['SKU-3', 30, 7, false],
+        ];
     }
 
     public function testExecuteWithMaxSaleQty()
