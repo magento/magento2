5690962188a08e5233aef7f8587f5dcc4d6b454a
Oleksii Korshenko <okorshenko@magento.com>
2018-04-27 17:35:49 +0300

    Update FeedInterface.php

--- lib/internal/Magento/Framework/App/FeedInterface.php
+++ lib/internal/Magento/Framework/App/FeedInterface.php
@@ -11,16 +11,8 @@ namespace Magento\Framework\App;
 interface FeedInterface
 {
     /**
-     * XML feed output format
-     */
-    const FORMAT_XML = 'xml';
-
-    /**
-     * @param string $format
      *
      * @return string
      */
-    public function getFormattedContentAs(
-        $format = self::FORMAT_XML
-    );
+    public function getFormattedContent();
 }
