16b8b2c206e06945f2aa884e454bf2a4d832ee68
Sergey <simpleadm@gmail.com>
2018-05-01 07:07:44 +0300

    Using constants for qty increments in product import

--- app/code/Magento/CatalogImportExport/Model/Import/Product.php
+++ app/code/Magento/CatalogImportExport/Model/Import/Product.php
@@ -10,6 +10,7 @@ use Magento\Catalog\Model\Product\Visibility;
 use Magento\CatalogImportExport\Model\Import\Product\MediaGalleryProcessor;
 use Magento\CatalogImportExport\Model\Import\Product\ImageTypeProcessor;
 use Magento\CatalogImportExport\Model\Import\Product\RowValidatorInterface as ValidatorInterface;
+use Magento\CatalogInventory\Api\Data\StockItemInterface;
 use Magento\Framework\App\Filesystem\DirectoryList;
 use Magento\Framework\App\ObjectManager;
 use Magento\Framework\Exception\LocalizedException;
@@ -2636,8 +2637,8 @@ class Product extends \Magento\ImportExport\Model\Import\Entity\AbstractEntity
     {
         $useConfigFields = [];
         foreach ($rowData as $key => $value) {
-            $useConfigName = $key === 'enable_qty_increments'
-                ? 'use_config_enable_qty_inc'
+            $useConfigName = $key === StockItemInterface::ENABLE_QTY_INCREMENTS
+                ? StockItemInterface::USE_CONFIG_ENABLE_QTY_INC
                 : self::INVENTORY_USE_CONFIG_PREFIX . $key;
 
             if (isset($this->defaultStockData[$key])
