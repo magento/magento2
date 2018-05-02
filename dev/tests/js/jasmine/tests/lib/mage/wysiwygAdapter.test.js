e429f0ec77b9330bcda81a41a79a4d4e90921a8a
Dan Mooney <dmooney@magento.com>
2018-05-02 13:24:04 -0500

    MAGETWO-90177: Image broken on storefront with secure key enabled
    Fix directive parsing and trailing forward slash issues

--- dev/tests/js/jasmine/tests/lib/mage/wysiwygAdapter.test.js
+++ dev/tests/js/jasmine/tests/lib/mage/wysiwygAdapter.test.js
@@ -22,41 +22,74 @@ define([
         Constr.prototype = wysiwygAdapter;
 
         obj = new Constr();
-        obj.initialize('id', {
-            'directives_url': 'http://example.com/admin/cms/wysiwyg/directive/'
-        });
     });
 
     describe('wysiwygAdapter', function () {
-        var decodedHtml = '<p>' +
-            '<img src="{{media url=&quot;wysiwyg/banana.jpg&quot;}}" alt="" width="612" height="459"></p>',
-            encodedHtml = '<p>' +
-            '<img src="http://example.com/admin/cms/wysiwyg/directive/' +
-            '___directive/e3ttZWRpYSB1cmw9Ind5c2l3eWcvYmFuYW5hLmpwZyJ9fQ%2C%2C" alt="" width="612" height="459">' +
-            '</p>',
-            encodedHtmlWithForwardSlashInImgSrc = encodedHtml.replace('%2C%2C', '%2C%2C/');
+        describe('encoding and decoding directives', function () {
+            function runTests(decodedHtml, encodedHtml) {
+                var encodedHtmlWithForwardSlashInImgSrc = encodedHtml.replace('src="([^"]+)', 'src="$1/"');
 
-        describe('"encodeDirectives" method', function () {
-            it('converts media directive img src to directive URL', function () {
-                expect(obj.encodeDirectives(decodedHtml)).toEqual(encodedHtml);
+                describe('"encodeDirectives" method', function () {
+                    it('converts media directive img src to directive URL', function () {
+                        expect(obj.encodeDirectives(decodedHtml)).toEqual(encodedHtml);
+                    });
+                });
+
+                describe('"decodeDirectives" method', function () {
+                    it(
+                        'converts directive URL img src without a trailing forward slash ' +
+                        'to media url without a trailing forward slash',
+                        function () {
+                            expect(obj.decodeDirectives(encodedHtml)).toEqual(decodedHtml);
+                        }
+                    );
+
+                    it('converts directive URL img src with a trailing forward slash ' +
+                        'to media url without a trailing forward slash',
+                        function () {
+                            expect(obj.decodeDirectives(encodedHtmlWithForwardSlashInImgSrc)).toEqual(decodedHtml);
+                        }
+                    );
+                });
+            }
+
+            describe('without secret key', function () {
+                var decodedHtml = '<p>' +
+                    '<img src="{{media url=&quot;wysiwyg/banana.jpg&quot;}}" alt="" width="612" height="459"></p>',
+                    encodedHtml = '<p>' +
+                    '<img src="http://example.com/admin/cms/wysiwyg/directive/___directive' +
+                    '/e3ttZWRpYSB1cmw9Ind5c2l3eWcvYmFuYW5hLmpwZyJ9fQ%2C%2C" alt="" width="612" height="459">' +
+                    '</p>';
+
+                beforeEach(function () {
+                    obj.initialize('id', {
+                        'directives_url': 'http://example.com/admin/cms/wysiwyg/directive/'
+                    });
+                });
+
+                runTests(decodedHtml, encodedHtml);
             });
-        });
 
-        describe('"decodeDirectives" method', function () {
-            it(
-                'converts directive URL img src without a trailing forward slash ' +
-                'to media url without a trailing forward slash',
-                function () {
-                    expect(obj.decodeDirectives(encodedHtml)).toEqual(decodedHtml);
-                }
-            );
+            describe('with secret key', function () {
+                var decodedHtml = '<p>' +
+                    '<img src="{{media url=&quot;wysiwyg/banana.jpg&quot;}}" alt="" width="612" height="459"></p>',
+                    encodedHtml = '<p>' +
+                    '<img src="http://example.com/admin/cms/wysiwyg/directive/___directive' +
+                    '/e3ttZWRpYSB1cmw9Ind5c2l3eWcvYmFuYW5hLmpwZyJ9fQ%2C%2C/key/' +
+                    '5552655d13a141099d27f5d5b0c58869423fd265687167da12cad2bb39aa9a58" ' +
+                    'alt="" width="612" height="459">' +
+                    '</p>',
+                    directiveUrl = 'http://example.com/admin/cms/wysiwyg/directive/key/' +
+                    '5552655d13a141099d27f5d5b0c58869423fd265687167da12cad2bb39aa9a58/';
 
-            it('converts directive URL img src with a trailing forward slash ' +
-                'to media url without a trailing forward slash',
-                function () {
-                    expect(obj.decodeDirectives(encodedHtmlWithForwardSlashInImgSrc)).toEqual(decodedHtml);
-                }
-            );
+                beforeEach(function () {
+                    obj.initialize('id', {
+                        'directives_url': directiveUrl
+                    });
+                });
+
+                runTests(decodedHtml, encodedHtml);
+            });
         });
     });
 });
