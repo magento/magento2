e2ae3b63a732de96741d6357615e86feda214e2e
Stanislav Lopukhov <slopukhov@magento.com>
2018-05-04 10:08:41 +0300

    MAGETWO-90563: [Forwardport] Implement segmentation for Category Product Indexer

--- app/code/Magento/Catalog/Model/ResourceModel/Product/Collection.php
+++ app/code/Magento/Catalog/Model/ResourceModel/Product/Collection.php
@@ -476,7 +476,10 @@ class Collection extends \Magento\Catalog\Model\ResourceModel\Collection\Abstrac
     protected function _construct()
     {
         if ($this->isEnabledFlat()) {
-            $this->_init(\Magento\Catalog\Model\Product::class, \Magento\Catalog\Model\ResourceModel\Product\Flat::class);
+            $this->_init(
+                \Magento\Catalog\Model\Product::class,
+                \Magento\Catalog\Model\ResourceModel\Product\Flat::class
+            );
         } else {
             $this->_init(\Magento\Catalog\Model\Product::class, \Magento\Catalog\Model\ResourceModel\Product::class);
         }
