magento/magento2

commit f2bfdd941c0c8c6c7c745156d1ce206e38c76820

11740: Sending emails from Admin in Multi-Store Environment defaults to Primary Store

app/code/Magento/Sales/Model/Order/Email/SenderBuilder.php
@@ -5,7 +5,9 @@
  */
 namespace Magento\Sales\Model\Order\Email;
 
+use Magento\Framework\App\ObjectManager;
 use Magento\Framework\Mail\Template\TransportBuilder;
+use Magento\Framework\Mail\Template\TransportBuilderByStore;
 use Magento\Sales\Model\Order\Email\Container\IdentityInterface;
 use Magento\Sales\Model\Order\Email\Container\Template;
 
@@ -27,18 +29,28 @@ class SenderBuilder
     protected $transportBuilder;
 
     /**
+     * @var TransportBuilderByStore
+     */
+    private $transportBuilderByStore;
+
+    /**
      * @param Template $templateContainer
      * @param IdentityInterface $identityContainer
      * @param TransportBuilder $transportBuilder
+     * @param TransportBuilderByStore $transportBuilderByStore
      */
     public function __construct(
         Template $templateContainer,
         IdentityInterface $identityContainer,
-        TransportBuilder $transportBuilder
+        TransportBuilder $transportBuilder,
+        TransportBuilderByStore $transportBuilderByStore = null
     ) {
         $this->templateContainer = $templateContainer;
         $this->identityContainer = $identityContainer;
         $this->transportBuilder = $transportBuilder;
+        $this->transportBuilderByStore = $transportBuilderByStore ?: ObjectManager::getInstance()->get(
+            TransportBuilderByStore::class
+        );
     }
 
     /**
@@ -98,7 +110,7 @@ class SenderBuilder
         $this->transportBuilder->setTemplateIdentifier($this->templateContainer->getTemplateId());
         $this->transportBuilder->setTemplateOptions($this->templateContainer->getTemplateOptions());
         $this->transportBuilder->setTemplateVars($this->templateContainer->getTemplateVars());
-        $this->transportBuilder->setFromByStore(
+        $this->transportBuilderByStore->setFromByStore(
             $this->identityContainer->getEmailIdentity(),
             $this->identityContainer->getStore()->getId()
         );

lib/internal/Magento/Framework/Mail/Template/TransportBuilder.php
@@ -172,20 +172,6 @@ class TransportBuilder
     }
 
     /**
-     * Set mail from address by store.
-     *
-     * @param string|array $from
-     * @param string|int $store
-     * @return $this
-     */
-    public function setFromByStore($from, $store)
-    {
-        $result = $this->_senderResolver->resolve($from, $store);
-        $this->message->setFrom($result['email'], $result['name']);
-        return $this;
-    }
-
-    /**
      * Set template identifier
      *
      * @param string $templateIdentifier

lib/internal/Magento/Framework/Mail/Template/TransportBuilderByStore.php
@@ -1,0 +1,54 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Framework\Mail\Template;
+
+use Magento\Framework\Mail\MessageInterface;
+
+class TransportBuilderByStore
+{
+    /**
+     * Message.
+     *
+     * @var \Magento\Framework\Mail\Message
+     */
+    protected $message;
+
+    /**
+     * Sender resolver.
+     *
+     * @var \Magento\Framework\Mail\Template\SenderResolverInterface
+     */
+    private $senderResolver;
+
+    /**
+     * @param MessageInterface $message
+     * @param SenderResolverInterface $senderResolver
+     */
+    public function __construct(
+        MessageInterface $message,
+        SenderResolverInterface $senderResolver
+    ) {
+        $this->message = $message;
+        $this->senderResolver = $senderResolver;
+    }
+
+    /**
+     * Set mail from address by store.
+     *
+     * @param string|array $from
+     * @param string|int $store
+     *
+     * @return $this
+     */
+    public function setFromByStore($from, $store)
+    {
+        $result = $this->senderResolver->resolve($from, $store);
+        $this->message->setFrom($result['email'], $result['name']);
+
+        return $this;
+    }
+}

app/code/Magento/Sales/Test/Unit/Model/Order/Email/SenderBuilderTest.php
@@ -6,6 +6,7 @@
 
 namespace Magento\Sales\Test\Unit\Model\Order\Email;
 
+use Magento\Framework\Mail\Template\TransportBuilderByStore;
 use Magento\Sales\Model\Order\Email\SenderBuilder;
 
 class SenderBuilderTest extends \PHPUnit\Framework\TestCase
@@ -34,6 +35,11 @@ class SenderBuilderTest extends \PHPUnit\Framework\TestCase
      * @var \PHPUnit_Framework_MockObject_MockObject
      */
     private $storeMock;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    private $transportBuilderByStore;
 
     protected function setUp()
     {
@@ -76,9 +82,10 @@ class SenderBuilderTest extends \PHPUnit\Framework\TestCase
                 'setTemplateIdentifier',
                 'setTemplateOptions',
                 'setTemplateVars',
-                'setFromByStore',
             ]
         );
+
+        $this->transportBuilderByStore = $this->createMock(TransportBuilderByStore::class);
 
         $this->templateContainerMock->expects($this->once())
             ->method('getTemplateId')
@@ -102,7 +109,7 @@ class SenderBuilderTest extends \PHPUnit\Framework\TestCase
         $this->identityContainerMock->expects($this->once())
             ->method('getEmailIdentity')
             ->will($this->returnValue($emailIdentity));
-        $this->transportBuilder->expects($this->once())
+        $this->transportBuilderByStore->expects($this->once())
             ->method('setFromByStore')
             ->with($this->equalTo($emailIdentity));
 
@@ -113,7 +120,8 @@ class SenderBuilderTest extends \PHPUnit\Framework\TestCase
         $this->senderBuilder = new SenderBuilder(
             $this->templateContainerMock,
             $this->identityContainerMock,
-            $this->transportBuilder
+            $this->transportBuilder,
+            $this->transportBuilderByStore
         );
     }
 

lib/internal/Magento/Framework/Mail/Test/Unit/Template/TransportBuilderByStoreTest.php
@@ -1,0 +1,64 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Framework\Mail\Test\Unit\Template;
+
+use Magento\Framework\Mail\Template\TransportBuilderByStore;
+
+class TransportBuilderByStoreTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @var \Magento\Framework\Mail\Template\TransportBuilderByStore
+     */
+    protected $model;
+
+    /**
+     * @var \Magento\Framework\Mail\Message | \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $messageMock;
+
+    /**
+     * @var \Magento\Framework\Mail\Template\SenderResolverInterface | \PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $senderResolverMock;
+
+    /**
+     * @return void
+     */
+    protected function setUp()
+    {
+        $objectManagerHelper = new \Magento\Framework\TestFramework\Unit\Helper\ObjectManager($this);
+        $this->messageMock = $this->createMock(\Magento\Framework\Mail\Message::class);
+        $this->senderResolverMock = $this->createMock(\Magento\Framework\Mail\Template\SenderResolverInterface::class);
+
+        $this->model = $objectManagerHelper->getObject(
+            TransportBuilderByStore::class,
+            [
+                'message' => $this->messageMock,
+                'senderResolver' => $this->senderResolverMock,
+            ]
+        );
+    }
+
+    /**
+     * @return void
+     */
+    public function setFromByStore()
+    {
+        $sender = ['email' => 'from@example.com', 'name' => 'name'];
+        $store = 1;
+        $this->senderResolverMock->expects($this->once())
+            ->method('resolve')
+            ->with($sender, $store)
+            ->willReturn($sender);
+        $this->messageMock->expects($this->once())
+            ->method('setFrom')
+            ->with('from@example.com', 'name')
+            ->willReturnSelf();
+
+        $this->model->setFromByStore($sender, $store);
+    }
+}

lib/internal/Magento/Framework/Mail/Test/Unit/Template/TransportBuilderTest.php
@@ -171,25 +171,6 @@ class TransportBuilderTest extends \PHPUnit\Framework\TestCase
     /**
      * @return void
      */
-    public function setFromByStore()
-    {
-        $sender = ['email' => 'from@example.com', 'name' => 'name'];
-        $store = 1;
-        $this->senderResolverMock->expects($this->once())
-            ->method('resolve')
-            ->with($sender, $store)
-            ->willReturn($sender);
-        $this->messageMock->expects($this->once())
-            ->method('setFrom')
-            ->with('from@example.com', 'name')
-            ->willReturnSelf();
-
-        $this->builder->setFromByStore($sender);
-    }
-
-    /**
-     * @return void
-     */
     public function testSetCc()
     {
         $this->messageMock->expects($this->once())->method('addCc')->with('cc@example.com')->willReturnSelf();

commit 2733d44a06df6b5e7b1d66f85f1e8cf1a0a78f34

Confirmation emails have no FROM or FROM email address 2.2.4 #14952. Undone commit f2bfdd941c0c8c6c7c745156d1ce206e38c76820 which caused issue

app/code/Magento/Sales/Model/Order/Email/SenderBuilder.php
@@ -5,9 +5,7 @@
  */
 namespace Magento\Sales\Model\Order\Email;
 
-use Magento\Framework\App\ObjectManager;
 use Magento\Framework\Mail\Template\TransportBuilder;
-use Magento\Framework\Mail\Template\TransportBuilderByStore;
 use Magento\Sales\Model\Order\Email\Container\IdentityInterface;
 use Magento\Sales\Model\Order\Email\Container\Template;
 
@@ -29,28 +27,18 @@ class SenderBuilder
     protected $transportBuilder;
 
     /**
-     * @var TransportBuilderByStore
-     */
-    private $transportBuilderByStore;
-
-    /**
      * @param Template $templateContainer
      * @param IdentityInterface $identityContainer
      * @param TransportBuilder $transportBuilder
-     * @param TransportBuilderByStore $transportBuilderByStore
      */
     public function __construct(
         Template $templateContainer,
         IdentityInterface $identityContainer,
-        TransportBuilder $transportBuilder,
-        TransportBuilderByStore $transportBuilderByStore = null
+        TransportBuilder $transportBuilder
     ) {
         $this->templateContainer = $templateContainer;
         $this->identityContainer = $identityContainer;
         $this->transportBuilder = $transportBuilder;
-        $this->transportBuilderByStore = $transportBuilderByStore ?: ObjectManager::getInstance()->get(
-            TransportBuilderByStore::class
-        );
     }
 
     /**
@@ -110,7 +98,7 @@ class SenderBuilder
         $this->transportBuilder->setTemplateIdentifier($this->templateContainer->getTemplateId());
         $this->transportBuilder->setTemplateOptions($this->templateContainer->getTemplateOptions());
         $this->transportBuilder->setTemplateVars($this->templateContainer->getTemplateVars());
-        $this->transportBuilderByStore->setFromByStore(
+        $this->transportBuilder->setFromByStore(
             $this->identityContainer->getEmailIdentity(),
             $this->identityContainer->getStore()->getId()
         );

lib/internal/Magento/Framework/Mail/Template/TransportBuilder.php
@@ -279,4 +279,20 @@ class TransportBuilder
 
         return $this;
     }
+
+    /**
+     * Set mail from address by store.
+     *
+     * @param string|array $from
+     * @param string|int $store
+     *
+     * @return $this
+     */
+    public function setFromByStore($from, $store)
+    {
+        $result = $this->senderResolver->resolve($from, $store);
+        $this->message->setFrom($result['email'], $result['name']);
+
+        return $this;
+    }
 }

lib/internal/Magento/Framework/Mail/Template/TransportBuilderByStore.php
@@ -1,54 +1,0 @@
-<?php
-/**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
- */
-
-namespace Magento\Framework\Mail\Template;
-
-use Magento\Framework\Mail\MessageInterface;
-
-class TransportBuilderByStore
-{
-    /**
-     * Message.
-     *
-     * @var \Magento\Framework\Mail\Message
-     */
-    protected $message;
-
-    /**
-     * Sender resolver.
-     *
-     * @var \Magento\Framework\Mail\Template\SenderResolverInterface
-     */
-    private $senderResolver;
-
-    /**
-     * @param MessageInterface $message
-     * @param SenderResolverInterface $senderResolver
-     */
-    public function __construct(
-        MessageInterface $message,
-        SenderResolverInterface $senderResolver
-    ) {
-        $this->message = $message;
-        $this->senderResolver = $senderResolver;
-    }
-
-    /**
-     * Set mail from address by store.
-     *
-     * @param string|array $from
-     * @param string|int $store
-     *
-     * @return $this
-     */
-    public function setFromByStore($from, $store)
-    {
-        $result = $this->senderResolver->resolve($from, $store);
-        $this->message->setFrom($result['email'], $result['name']);
-
-        return $this;
-    }
-}

app/code/Magento/Sales/Test/Unit/Model/Order/Email/SenderBuilderTest.php
@@ -6,7 +6,6 @@
 
 namespace Magento\Sales\Test\Unit\Model\Order\Email;
 
-use Magento\Framework\Mail\Template\TransportBuilderByStore;
 use Magento\Sales\Model\Order\Email\SenderBuilder;
 
 class SenderBuilderTest extends \PHPUnit\Framework\TestCase
@@ -35,11 +34,6 @@ class SenderBuilderTest extends \PHPUnit\Framework\TestCase
      * @var \PHPUnit_Framework_MockObject_MockObject
      */
     private $storeMock;
-
-    /**
-     * @var \PHPUnit_Framework_MockObject_MockObject
-     */
-    private $transportBuilderByStore;
 
     protected function setUp()
     {
@@ -82,10 +76,10 @@ class SenderBuilderTest extends \PHPUnit\Framework\TestCase
                 'setTemplateIdentifier',
                 'setTemplateOptions',
                 'setTemplateVars',
+                'setFromByStore'
             ]
         );
 
-        $this->transportBuilderByStore = $this->createMock(TransportBuilderByStore::class);
 
         $this->templateContainerMock->expects($this->once())
             ->method('getTemplateId')
@@ -109,7 +103,7 @@ class SenderBuilderTest extends \PHPUnit\Framework\TestCase
         $this->identityContainerMock->expects($this->once())
             ->method('getEmailIdentity')
             ->will($this->returnValue($emailIdentity));
-        $this->transportBuilderByStore->expects($this->once())
+        $this->transportBuilder->expects($this->once())
             ->method('setFromByStore')
             ->with($this->equalTo($emailIdentity));
 
@@ -120,8 +114,7 @@ class SenderBuilderTest extends \PHPUnit\Framework\TestCase
         $this->senderBuilder = new SenderBuilder(
             $this->templateContainerMock,
             $this->identityContainerMock,
-            $this->transportBuilder,
-            $this->transportBuilderByStore
+            $this->transportBuilder
         );
     }
 

lib/internal/Magento/Framework/Mail/Test/Unit/Template/TransportBuilderByStoreTest.php
@@ -1,64 +1,0 @@
-<?php
-/**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
- */
-
-namespace Magento\Framework\Mail\Test\Unit\Template;
-
-use Magento\Framework\Mail\Template\TransportBuilderByStore;
-
-class TransportBuilderByStoreTest extends \PHPUnit\Framework\TestCase
-{
-    /**
-     * @var \Magento\Framework\Mail\Template\TransportBuilderByStore
-     */
-    protected $model;
-
-    /**
-     * @var \Magento\Framework\Mail\Message | \PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $messageMock;
-
-    /**
-     * @var \Magento\Framework\Mail\Template\SenderResolverInterface | \PHPUnit_Framework_MockObject_MockObject
-     */
-    protected $senderResolverMock;
-
-    /**
-     * @return void
-     */
-    protected function setUp()
-    {
-        $objectManagerHelper = new \Magento\Framework\TestFramework\Unit\Helper\ObjectManager($this);
-        $this->messageMock = $this->createMock(\Magento\Framework\Mail\Message::class);
-        $this->senderResolverMock = $this->createMock(\Magento\Framework\Mail\Template\SenderResolverInterface::class);
-
-        $this->model = $objectManagerHelper->getObject(
-            TransportBuilderByStore::class,
-            [
-                'message' => $this->messageMock,
-                'senderResolver' => $this->senderResolverMock,
-            ]
-        );
-    }
-
-    /**
-     * @return void
-     */
-    public function testSetFromByStore()
-    {
-        $sender = ['email' => 'from@example.com', 'name' => 'name'];
-        $store = 1;
-        $this->senderResolverMock->expects($this->once())
-            ->method('resolve')
-            ->with($sender, $store)
-            ->willReturn($sender);
-        $this->messageMock->expects($this->once())
-            ->method('setFrom')
-            ->with('from@example.com', 'name')
-            ->willReturnSelf();
-
-        $this->model->setFromByStore($sender, $store);
-    }
-}

lib/internal/Magento/Framework/Mail/Test/Unit/Template/TransportBuilderTest.php
@@ -216,4 +216,23 @@ class TransportBuilderTest extends \PHPUnit\Framework\TestCase
 
         $this->builder->setReplyTo('replyTo@example.com', 'replyName');
     }
+
+    /**
+     * @return void
+     */
+    public function testSetFromByStore()
+    {
+        $sender = ['email' => 'from@example.com', 'name' => 'name'];
+        $store = 1;
+        $this->senderResolverMock->expects($this->once())
+            ->method('resolve')
+            ->with($sender, $store)
+            ->willReturn($sender);
+        $this->messageMock->expects($this->once())
+            ->method('setFrom')
+            ->with('from@example.com', 'name')
+            ->willReturnSelf();
+
+        $this->model->setFromByStore($sender, $store);
+    }
 }