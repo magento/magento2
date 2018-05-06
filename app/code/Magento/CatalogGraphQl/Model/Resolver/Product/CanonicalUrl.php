d6c15d6210203483ff2f9ae35edf184803a82288
austris argalis <austris.argalis@gmail.com>
2018-05-06 20:13:45 +0300

    Return full canonical product URL within product object

---
+++ app/code/Magento/CatalogGraphQl/Model/Resolver/Product/CanonicalUrl.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\CatalogGraphQl\Model\Resolver\Product;
+
+use Magento\Catalog\Model\Product;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Query\Resolver\Value;
+use Magento\Framework\GraphQl\Query\Resolver\ValueFactory;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+
+class CanonicalUrl implements ResolverInterface
+{
+    /**
+     * @var ValueFactory
+     */
+    private $valueFactory;
+
+    /**
+     * @param ValueFactory $valueFactory
+     */
+    public function __construct(
+        ValueFactory $valueFactory
+    ) {
+        $this->valueFactory = $valueFactory;
+    }
+
+    /**
+     * Fetches the data from persistence models and format it according to the GraphQL schema.
+     *
+     * @param \Magento\Framework\GraphQl\Config\Element\Field $field
+     * @param $context
+     * @param ResolveInfo $info
+     * @param array|null $value
+     * @param array|null $args
+     * @throws \Exception
+     * @return Value
+     */
+    public function resolve(
+        Field $field,
+        $context,
+        ResolveInfo $info,
+        array $value = null,
+        array $args = null
+    ): Value {
+        if (!isset($value['model'])) {
+            $result = function () {
+                return null;
+            };
+            return $this->valueFactory->create($result);
+        }
+
+        /* @var $product Product */
+        $product = $value['model'];
+        $url = $product->getUrlModel()->getUrl($product, ['_ignore_category' => true]);
+        $result = function () use ($url) {
+            return $url;
+        };
+
+        return $this->valueFactory->create($result);
+    }
+}
