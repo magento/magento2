87e0682e36074ada8631374271d00fc25000aa4a
Andrii Lugovyi <alugovyi@magento.com>
2018-05-08 18:53:51 +0300

    MAGETWO-91164: Optimize images resizing

--- app/code/Magento/Catalog/Model/ResourceModel/Product/Image.php
+++ app/code/Magento/Catalog/Model/ResourceModel/Product/Image.php
@@ -12,6 +12,9 @@ use Magento\Framework\DB\Query\Generator;
 use Magento\Framework\DB\Select;
 use Magento\Framework\App\ResourceConnection;
 
+/**
+ * Class for fast retrieval of all product images
+ */
 class Image
 {
     /**
---
+++ dev/tests/integration/testsuite/Magento/Catalog/Console/Command/ImagesResizeCommandTest.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Catalog\Console\Command;
+
+use Symfony\Component\Console\Tester\CommandTester;
+
+class ImagesResizeCommandTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @var CommandTester
+     */
+    private $tester;
+
+    /**
+     * @var \Magento\Framework\ObjectManagerInterface
+     */
+    private $objectManager;
+
+    public function setUp()
+    {
+        $this->objectManager = \Magento\TestFramework\Helper\Bootstrap::getObjectManager();
+        $this->tester = new CommandTester($this->objectManager->create(ImagesResizeCommand::class));
+    }
+
+    /**
+     * @magentoApiDataFixture Magento/Catalog/_files/product_with_image.php
+     * @magentoDbIsolation disabled
+     */
+    public function testExecute()
+    {
+        // Execute command
+        $return = $this->tester->execute([]);
+        self::assertEquals(0, $return);
+    }
+}
