8a21ec8ed66eb50c46191dba0e1d036380fb3965
Leonid Poluyanov <lpoluyanov@magento.com>
2018-05-08 18:48:27 +0300

    MAGETWO-83706: Scheduled Update to existing Group Price / Special Price removes the previously configured price, or results in changes not being saved

--- app/code/Magento/Catalog/Model/Product/TierPrice/SaveHandler.php
+++ app/code/Magento/Catalog/Model/Product/TierPrice/SaveHandler.php
@@ -6,16 +6,15 @@
 namespace Magento\Catalog\Model\Product\TierPrice;
 
 use Magento\Framework\EntityManager\Operation\ExtensionInterface;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Catalog\Api\ProductAttributeRepositoryInterface;
 use Magento\Catalog\Api\Data\ProductInterface;
-use Magento\Framework\EntityManager\TypeResolver;
-use Magento\Catalog\Model\Attribute\ScopeOverriddenValue;
-use Magento\Catalog\Model\Product\Attribute\Backend\Price;
 use Magento\Customer\Api\GroupManagementInterface;
 use Magento\Framework\EntityManager\MetadataPool;
 use Magento\Catalog\Model\ResourceModel\Product\Attribute\Backend\Tierprice;
 
 /**
- * Class SaveHandler
+ * Process tier price data for handled new product
  */
 class SaveHandler implements ExtensionInterface
 {
@@ -44,7 +43,6 @@ class SaveHandler implements ExtensionInterface
      */
     private $tierPriceResource;
 
-
     /**
      * @param \Magento\Store\Model\StoreManagerInterface $storeManager
      * @param \Magento\Catalog\Api\ProductAttributeRepositoryInterface $attributeRepository
@@ -53,11 +51,11 @@ class SaveHandler implements ExtensionInterface
      * @param \Magento\Catalog\Model\ResourceModel\Product\Attribute\Backend\Tierprice $tierPriceResource
      */
     public function __construct(
-        \Magento\Store\Model\StoreManagerInterface $storeManager,
-        \Magento\Catalog\Api\ProductAttributeRepositoryInterface $attributeRepository,
-        \Magento\Customer\Api\GroupManagementInterface $groupManagement,
-        \Magento\Framework\EntityManager\MetadataPool $metadataPool,
-        \Magento\Catalog\Model\ResourceModel\Product\Attribute\Backend\Tierprice $tierPriceResource
+        StoreManagerInterface $storeManager,
+        ProductAttributeRepositoryInterface $attributeRepository,
+        GroupManagementInterface $groupManagement,
+        MetadataPool $metadataPool,
+        Tierprice $tierPriceResource
     ) {
         $this->storeManager = $storeManager;
         $this->attributeRepository = $attributeRepository;
@@ -67,6 +65,8 @@ class SaveHandler implements ExtensionInterface
     }
 
     /**
+     * Set tier price data for product entity
+     *
      * @param \Magento\Catalog\Api\Data\ProductInterface|object $entity
      * @param array $arguments
      * @return \Magento\Catalog\Api\Data\ProductInterface|object
@@ -75,82 +75,44 @@ class SaveHandler implements ExtensionInterface
      */
     public function execute($entity, $arguments = [])
     {
-        $websiteId = $this->storeManager->getStore($entity->getStoreId())->getWebsiteId();
         $attribute = $this->attributeRepository->get('tier_price');
-        $isGlobal = $attribute->isScopeGlobal() || $websiteId == 0;
         $priceRows = $entity->getData($attribute->getName());
-        if (null === $priceRows) {
-            return $entity;
-        }
+        if (null !== $priceRows) {
+            $websiteId = $this->storeManager->getStore($entity->getStoreId())->getWebsiteId();
+            $isGlobal = $attribute->isScopeGlobal() || $websiteId === 0;
+            $identifierField = $this->metadataPoll->getMetadata(ProductInterface::class)->getLinkField();
+            $priceRows = array_filter((array)$priceRows);
+            $productId = $entity->getData($identifierField);
 
-        $priceRows = array_filter((array)$priceRows);
-
-        $identifierField = $this->metadataPoll->getMetadata(ProductInterface::class)->getLinkField();
-        $productId = $entity->getData($identifierField);
-        // prepare data for save
-        foreach ($priceRows as $data) {
-            $hasEmptyData = false;
-            foreach ($this->getAdditionalUniqueFields($data) as $field) {
-                if (empty($field)) {
-                    $hasEmptyData = true;
-                    break;
+            // prepare and save data
+            foreach ($priceRows as $data) {
+                $isPriceWebsiteGlobal = (int)$data['website_id'] === 0;
+                if (($isGlobal === $isPriceWebsiteGlobal)
+                    || !empty($data['price_qty'])
+                    || isset($data['cust_group'])
+                ) {
+                    $tierPrice = $this->prepareTierPrice($data);
+                    $price = new \Magento\Framework\DataObject($tierPrice);
+                    $price->setData(
+                        $this->metadataPoll->getMetadata(ProductInterface::class)->getLinkField(),
+                        $productId
+                    );
+                    $this->tierPriceResource->savePriceData($price);
+                    $valueChangedKey = $attribute->getName() . '_changed';
+                    $entity->setData($valueChangedKey, 1);
                 }
             }
-
-            if ($hasEmptyData || !isset($data['cust_group']) || !empty($data['delete'])) {
-                continue;
-            }
-            if ($data['website_id'] > 0 && $attribute->isScopeGlobal()) {
-                continue;
-            }
-            if (!$isGlobal && (int)$data['website_id'] === 0) {
-                continue;
-            }
-
-            $useForAllGroups = $data['cust_group'] === $this->groupManagement->getAllCustomersGroup()->getId();
-            $customerGroupId = !$useForAllGroups ? $data['cust_group'] : 0;
-            $tierPrice = array_merge(
-                $this->getAdditionalFields($data),
-                [
-                    'website_id' => $data['website_id'],
-                    'all_groups' => $useForAllGroups ? 1 : 0,
-                    'customer_group_id' => $customerGroupId,
-                    'value' => $data['price'] ?? null
-                ],
-                $this->getAdditionalUniqueFields($data)
-            );
-
-            $price = new \Magento\Framework\DataObject($tierPrice);
-            $price->setData(
-                $this->metadataPoll->getMetadata(ProductInterface::class)->getLinkField(),
-                $productId
-            );
-            $this->tierPriceResource->savePriceData($price);
-
-            $valueChangedKey = $attribute->getName() . '_changed';
-            $entity->setData($valueChangedKey, 1);
         }
 
         return $entity;
     }
 
     /**
-     * Add price qty to unique fields
-     *
-     * @param array $objectArray
-     * @return array
-     */
-    private function getAdditionalUniqueFields($objectArray)
-    {
-        $uniqueFields['qty'] = $objectArray['price_qty'] * 1;
-        return $uniqueFields;
-    }
-
-    /**
      * Get additional tier price fields
+     *
      * @return array
      */
-    private function getAdditionalFields($objectArray)
+    private function getAdditionalFields(array $objectArray): array
     {
         $percentageValue = $this->getPercentage($objectArray);
         return [
@@ -165,10 +127,35 @@ class SaveHandler implements ExtensionInterface
      * @param array $priceRow
      * @return integer|null
      */
-    private function getPercentage($priceRow)
+    private function getPercentage(array $priceRow)
     {
         return isset($priceRow['percentage_value']) && is_numeric($priceRow['percentage_value'])
             ? $priceRow['percentage_value']
             : null;
+    }
+
+    /**
+     * Prepare tier price data by provided price row data
+     *
+     * @param $data
+     * @return array
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    private function prepareTierPrice(array $data): array
+    {
+        $useForAllGroups = (int)$data['cust_group'] === $this->groupManagement->getAllCustomersGroup()->getId();
+        $customerGroupId = !$useForAllGroups ? $data['cust_group'] : 0;
+        $tierPrice = array_merge(
+            $this->getAdditionalFields($data),
+            [
+                'website_id' => $data['website_id'],
+                'all_groups' => $useForAllGroups ? 1 : 0,
+                'customer_group_id' => $customerGroupId,
+                'value' => $data['price'] ?? null,
+                'qty' => (int)$data['price_qty']
+            ]
+        );
+
+        return $tierPrice;
     }
 }
--- app/code/Magento/Catalog/Model/Product/TierPrice/UpdateHandler.php
+++ app/code/Magento/Catalog/Model/Product/TierPrice/UpdateHandler.php
@@ -7,15 +7,14 @@ namespace Magento\Catalog\Model\Product\TierPrice;
 
 use Magento\Framework\EntityManager\Operation\ExtensionInterface;
 use Magento\Catalog\Api\Data\ProductInterface;
-use Magento\Framework\EntityManager\TypeResolver;
-use Magento\Catalog\Model\Attribute\ScopeOverriddenValue;
-use Magento\Catalog\Model\Product\Attribute\Backend\Price;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Catalog\Api\ProductAttributeRepositoryInterface;
 use Magento\Customer\Api\GroupManagementInterface;
 use Magento\Framework\EntityManager\MetadataPool;
 use Magento\Catalog\Model\ResourceModel\Product\Attribute\Backend\Tierprice;
 
 /**
- * Class UpdateHandler
+ * Process tier price data for handled existing product
  */
 class UpdateHandler implements ExtensionInterface
 {
@@ -53,11 +52,11 @@ class UpdateHandler implements ExtensionInterface
      * @param \Magento\Catalog\Model\ResourceModel\Product\Attribute\Backend\Tierprice $tierPriceResource
      */
     public function __construct(
-        \Magento\Store\Model\StoreManagerInterface $storeManager,
-        \Magento\Catalog\Api\ProductAttributeRepositoryInterface $attributeRepository,
-        \Magento\Customer\Api\GroupManagementInterface $groupManagement,
-        \Magento\Framework\EntityManager\MetadataPool $metadataPool,
-        \Magento\Catalog\Model\ResourceModel\Product\Attribute\Backend\Tierprice $tierPriceResource
+        StoreManagerInterface $storeManager,
+        ProductAttributeRepositoryInterface $attributeRepository,
+        GroupManagementInterface $groupManagement,
+        MetadataPool $metadataPool,
+        Tierprice $tierPriceResource
     ) {
         $this->storeManager = $storeManager;
         $this->attributeRepository = $attributeRepository;
@@ -75,138 +74,69 @@ class UpdateHandler implements ExtensionInterface
      */
     public function execute($entity, $arguments = [])
     {
-        $websiteId = $this->storeManager->getStore($entity->getStoreId())->getWebsiteId();
         $attribute = $this->attributeRepository->get('tier_price');
-        $isGlobal = $attribute->isScopeGlobal() || $websiteId == 0;
         $priceRows = $entity->getData($attribute->getName());
-        if (null === $priceRows) {
-            return $entity;
-        }
+        if (null !== $priceRows) {
+            $websiteId = $this->storeManager->getStore($entity->getStoreId())->getWebsiteId();
+            $isGlobal = $attribute->isScopeGlobal() || $websiteId === 0;
+            $identifierField = $this->metadataPoll->getMetadata(ProductInterface::class)->getLinkField();
+            $priceRows = array_filter((array)$priceRows);
+            $productId = $entity->getData($identifierField);
+            $old = [];
+            $new = [];
 
-        $priceRows = array_filter((array)$priceRows);
-
-        $old = [];
-        $new = [];
-        $identifierField = $this->metadataPoll->getMetadata(ProductInterface::class)->getLinkField();
-        $productId = $entity->getData($identifierField);
-
-        // prepare original data for compare
-        $origPrices = $entity->getOrigData($attribute->getName());
-        if (!is_array($origPrices)) {
-            $origPrices = [];
-        }
-        foreach ($origPrices as $data) {
-            if ($data['website_id'] > 0 || ($data['website_id'] === '0' && $isGlobal)) {
-                $key = implode(
-                    '-',
-                    array_merge(
-                        [$data['website_id'], $data['cust_group']],
-                        $this->getAdditionalUniqueFields($data)
-                    )
-                );
-                $old[$key] = $data;
-            }
-        }
-
-        // prepare data for save
-        foreach ($priceRows as $data) {
-            $hasEmptyData = false;
-            foreach ($this->getAdditionalUniqueFields($data) as $field) {
-                if (empty($field)) {
-                    $hasEmptyData = true;
-                    break;
+            // prepare original data for compare
+            $origPrices = $entity->getOrigData($attribute->getName());
+            if (is_array($origPrices)) {
+                foreach ($origPrices as $data) {
+                    if ($isGlobal === $this->isWebsiteGlobal($data['website_id'])) {
+                        $key = $this->getPriceKey($data);
+                        $old[$key] = $data;
+                    }
                 }
             }
 
-            if ($hasEmptyData || !isset($data['cust_group']) || !empty($data['delete'])) {
-                continue;
+            // prepare data for save
+            foreach ($priceRows as $data) {
+                if (empty($data['delete'])
+                    || !empty($data['price_qty'])
+                    || isset($data['cust_group'])
+                    || ($isGlobal === $this->isWebsiteGlobal($data['website_id']))
+                ) {
+                    $key = $this->getPriceKey($data);
+                    $new[$key] = $this->prepareTierPrice($data);
+                }
             }
-            if ($attribute->isScopeGlobal() && $data['website_id'] > 0) {
-                continue;
+
+            $delete = array_diff_key($old, $new);
+            $insert = array_diff_key($new, $old);
+            $update = array_intersect_key($new, $old);
+
+            $isAttributeChanged = $this->deleteValues($productId, $delete);
+            $isAttributeChanged |= $this->insertValues($productId, $insert);
+            $isAttributeChanged |= $this->updateValues($update, $old);
+
+            if ($isAttributeChanged) {
+                $valueChangedKey = $attribute->getName() . '_changed';
+                $entity->setData($valueChangedKey, 1);
             }
-            if (!$isGlobal && (int)$data['website_id'] == 0) {
-                continue;
-            }
-
-            $key = implode(
-                '-',
-                array_merge([$data['website_id'], $data['cust_group']], $this->getAdditionalUniqueFields($data))
-            );
-
-            $useForAllGroups = $data['cust_group'] == $this->groupManagement->getAllCustomersGroup()->getId();
-            $customerGroupId = !$useForAllGroups ? $data['cust_group'] : 0;
-            $new[$key] = array_merge(
-                $this->getAdditionalFields($data),
-                [
-                    'website_id' => $data['website_id'],
-                    'all_groups' => $useForAllGroups ? 1 : 0,
-                    'customer_group_id' => $customerGroupId,
-                    'value' => isset($data['price']) ? $data['price'] : null,
-                ],
-                $this->getAdditionalUniqueFields($data)
-            );
-        }
-
-        $delete = array_diff_key($old, $new);
-        $insert = array_diff_key($new, $old);
-        $update = array_intersect_key($new, $old);
-
-        $isChanged = false;
-
-        if (!empty($delete)) {
-            foreach ($delete as $data) {
-                $this->tierPriceResource->deletePriceData($productId, null, $data['price_id']);
-                $isChanged = true;
-            }
-        }
-
-        if (!empty($insert)) {
-            foreach ($insert as $data) {
-                $price = new \Magento\Framework\DataObject($data);
-                $price->setData(
-                    $this->metadataPoll->getMetadata(ProductInterface::class)->getLinkField(),
-                    $productId
-                );
-                $this->tierPriceResource->savePriceData($price);
-
-                $isChanged = true;
-            }
-        }
-
-        if (!empty($update)) {
-            $isChanged |= $this->updateValues($update, $old);
-        }
-
-        if ($isChanged) {
-            $valueChangedKey = $attribute->getName() . '_changed';
-            $entity->setData($valueChangedKey, 1);
         }
 
         return $entity;
     }
 
     /**
-     * Add price qty to unique fields
+     * Get additional tier price fields
      *
      * @param array $objectArray
      * @return array
      */
-    private function getAdditionalUniqueFields($objectArray)
-    {
-        $uniqueFields['qty'] = $objectArray['price_qty'] * 1;
-        return $uniqueFields;
-    }
-
-    /**
-     * Get additional tier price fields
-     * @return array
-     */
-    private function getAdditionalFields($objectArray)
+    private function getAdditionalFields(array $objectArray): array
     {
         $percentageValue = $this->getPercentage($objectArray);
         return [
             'value' => $percentageValue ? null : $objectArray['price'],
-            'percentage_value' => $percentageValue ?: null,
+            'percentage_value' => $percentageValue ?: null
         ];
     }
 
@@ -216,7 +146,7 @@ class UpdateHandler implements ExtensionInterface
      * @param array $priceRow
      * @return integer|null
      */
-    private function getPercentage($priceRow)
+    private function getPercentage(array $priceRow)
     {
         return isset($priceRow['percentage_value']) && is_numeric($priceRow['percentage_value'])
             ? $priceRow['percentage_value']
@@ -224,15 +154,17 @@ class UpdateHandler implements ExtensionInterface
     }
 
     /**
+     * Update existing tier prices for processed product
+     *
      * @param array $valuesToUpdate
      * @param array $oldValues
      * @return boolean
      */
-    private function updateValues(array $valuesToUpdate, array $oldValues)
+    private function updateValues(array $valuesToUpdate, array $oldValues): bool
     {
         $isChanged = false;
         foreach ($valuesToUpdate as $key => $value) {
-            if ($oldValues[$key]['price'] != $value['value']) {
+            if ((float)$oldValues[$key]['price'] !== (float)$value['value']) {
                 $price = new \Magento\Framework\DataObject(
                     [
                         'value_id' => $oldValues[$key]['price_id'],
@@ -243,6 +175,100 @@ class UpdateHandler implements ExtensionInterface
                 $isChanged = true;
             }
         }
+
         return $isChanged;
+    }
+
+    /**
+     * Insert new tier prices for processed product
+     *
+     * @param int $productId
+     * @param array $valuesToInsert
+     * @return bool
+     */
+    private function insertValues(int $productId, array $valuesToInsert): bool
+    {
+        $isChanged = false;
+        foreach ($valuesToInsert as $data) {
+            $price = new \Magento\Framework\DataObject($data);
+            $price->setData(
+                $this->metadataPoll->getMetadata(ProductInterface::class)->getLinkField(),
+                $productId
+            );
+            $this->tierPriceResource->savePriceData($price);
+            $isChanged = true;
+        }
+
+        return $isChanged;
+    }
+
+    /**
+     * Delete tier price values for processed product
+     *
+     * @param int $productId
+     * @param array $valuesToDelete
+     * @return bool
+     */
+    private function deleteValues(int $productId, array $valuesToDelete): bool
+    {
+        $isChanged = false;
+        foreach ($valuesToDelete as $data) {
+            $this->tierPriceResource->deletePriceData($productId, null, $data['price_id']);
+            $isChanged = true;
+        }
+
+        return $isChanged;
+    }
+
+    /**
+     * Get generated price key based on price data
+     *
+     * @param array $priceData
+     * @return string
+     */
+    private function getPriceKey(array $priceData): string
+    {
+        $key = implode(
+            '-',
+            array_merge([$priceData['website_id'], $priceData['cust_group']], [(int)$priceData['price_qty']])
+        );
+
+        return $key;
+    }
+
+    /**
+     * Prepare tier price data by provided price row data
+     *
+     * @param array $data
+     * @return array
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    private function prepareTierPrice(array $data): array
+    {
+        $useForAllGroups = (int)$data['cust_group'] === $this->groupManagement->getAllCustomersGroup()->getId();
+        $customerGroupId = !$useForAllGroups ? $data['cust_group'] : 0;
+        $tierPrice = array_merge(
+            $this->getAdditionalFields($data),
+            [
+                'website_id' => $data['website_id'],
+                'all_groups' => $useForAllGroups ? 1 : 0,
+                'customer_group_id' => $customerGroupId,
+                'value' => $data['price'] ?? null,
+                'qty' => (int)$data['price_qty']
+            ]
+        );
+
+        return $tierPrice;
+    }
+
+    /**
+     * Check by id is website global
+     *
+     * @param string|int $websiteId
+     * @return bool
+     */
+    private function isWebsiteGlobal($websiteId): bool
+    {
+        return (int)$websiteId === 0;
     }
 }
