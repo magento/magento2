1d69d52070dc6edd33cf89ba66fa97d0dec23061
Robert He <rohe@magento.com>
2018-05-08 10:43:06 -0500

    MAGETWO-90878: Build stabilization
     - fixing random failures

--- dev/tests/js/jasmine/tests/lib/mage/wysiwygAdapter.test.js
+++ dev/tests/js/jasmine/tests/lib/mage/wysiwygAdapter.test.js
@@ -24,123 +24,118 @@ define([
         obj = new Constr();
     });
 
-    describe('wysiwygAdapter', function () {
-        describe('encoding and decoding directives', function () {
-            /**
-             * Tests encoding and decoding directives
-             *
-             * @param decodedHtml
-             * @param encodedHtml
-             */
-            function runTests(decodedHtml, encodedHtml) {
-                var encodedHtmlWithForwardSlashInImgSrc = encodedHtml.replace(/src="((?:(?!"|\\\?).)*)/, 'src="$1/');
+    describe('wysiwygAdapter - encoding and decoding directives', function () {
 
-                describe('"encodeDirectives" method', function () {
-                    it('converts media directive img src to directive URL', function () {
-                        expect(obj.encodeDirectives(decodedHtml)).toEqual(encodedHtml);
-                    });
-                });
+        /**
+         * Tests encoding and decoding directives
+         *
+         * @param {string} decodedHtml
+         * @param {string} encodedHtml
+         */
+        function runTests(decodedHtml, encodedHtml) {
+            var encodedHtmlWithForwardSlashInImgSrc = encodedHtml.replace(/src="((?:(?!"|\\\?).)*)/, 'src="$1/');
 
-                describe('"decodeDirectives" method', function () {
-                    it(
-                        'converts directive URL img src without a trailing forward slash ' +
-                        'to media url without a trailing forward slash',
-                        function () {
-                            expect(obj.decodeDirectives(encodedHtml)).toEqual(decodedHtml);
-                        }
-                    );
-
-                    it('converts directive URL img src with a trailing forward slash ' +
-                        'to media url without a trailing forward slash',
-                        function () {
-                            expect(encodedHtmlWithForwardSlashInImgSrc).not.toEqual(encodedHtml);
-                            expect(obj.decodeDirectives(encodedHtmlWithForwardSlashInImgSrc)).toEqual(decodedHtml);
-                        }
-                    );
-                });
-            }
-
-            describe('without SID in directive query string', function () {
-                describe('without secret key', function () {
-                    var decodedHtml = '<p>' +
-                        '<img src="{{media url=&quot;wysiwyg/banana.jpg&quot;}}" alt="" width="612" height="459"></p>',
-                        encodedHtml = '<p>' +
-                        '<img src="http://example.com/admin/cms/wysiwyg/directive/___directive' +
-                        '/e3ttZWRpYSB1cmw9Ind5c2l3eWcvYmFuYW5hLmpwZyJ9fQ%2C%2C" alt="" width="612" height="459">' +
-                        '</p>';
-
-                    beforeEach(function () {
-                        obj.initialize('id', {
-                            'directives_url': 'http://example.com/admin/cms/wysiwyg/directive/'
-                        });
-                    });
-
-                    runTests(decodedHtml, encodedHtml);
-                });
-
-                describe('with secret key', function () {
-                    var decodedHtml = '<p>' +
-                        '<img src="{{media url=&quot;wysiwyg/banana.jpg&quot;}}" alt="" width="612" height="459"></p>',
-                        encodedHtml = '<p>' +
-                        '<img src="http://example.com/admin/cms/wysiwyg/directive/___directive' +
-                        '/e3ttZWRpYSB1cmw9Ind5c2l3eWcvYmFuYW5hLmpwZyJ9fQ%2C%2C/key/' +
-                        '5552655d13a141099d27f5d5b0c58869423fd265687167da12cad2bb39aa9a58" ' +
-                        'alt="" width="612" height="459">' +
-                        '</p>',
-                        directiveUrl = 'http://example.com/admin/cms/wysiwyg/directive/key/' +
-                        '5552655d13a141099d27f5d5b0c58869423fd265687167da12cad2bb39aa9a58/';
-
-                    beforeEach(function () {
-                        obj.initialize('id', {
-                            'directives_url': directiveUrl
-                        });
-                    });
-
-                    runTests(decodedHtml, encodedHtml);
+            describe('"encodeDirectives" method', function () {
+                it('converts media directive img src to directive URL', function () {
+                    expect(obj.encodeDirectives(decodedHtml)).toEqual(encodedHtml);
                 });
             });
 
-            describe('with SID in directive query string', function () {
-                describe('without secret key', function () {
-                    var decodedHtml = '<p>' +
-                        '<img src="{{media url=&quot;wysiwyg/banana.jpg&quot;}}" alt="" width="612" height="459"></p>',
-                        encodedHtml = '<p>' +
-                        '<img src="http://example.com/admin/cms/wysiwyg/directive/___directive' +
-                        '/e3ttZWRpYSB1cmw9Ind5c2l3eWcvYmFuYW5hLmpwZyJ9fQ%2C%2C?SID=something" ' +
-                        'alt="" width="612" height="459">' +
-                        '</p>',
-                        directiveUrl = 'http://example.com/admin/cms/wysiwyg/directive?SID=something';
+            describe('"decodeDirectives" method', function () {
+                it(
+                    'converts directive URL img src without a trailing forward slash ' +
+                    'to media url without a trailing forward slash',
+                    function () {
+                        expect(obj.decodeDirectives(encodedHtml)).toEqual(decodedHtml);
+                    }
+                );
 
-                    beforeEach(function () {
-                        obj.initialize('id', {
-                            'directives_url': directiveUrl
-                        });
-                    });
+                it('converts directive URL img src with a trailing forward slash ' +
+                    'to media url without a trailing forward slash',
+                    function () {
+                        expect(encodedHtmlWithForwardSlashInImgSrc).not.toEqual(encodedHtml);
+                        expect(obj.decodeDirectives(encodedHtmlWithForwardSlashInImgSrc)).toEqual(decodedHtml);
+                    }
+                );
+            });
+        }
 
-                    runTests(decodedHtml, encodedHtml);
-                });
+        describe('without SID in directive query string without secret key', function () {
+            var decodedHtml = '<p>' +
+                '<img src="{{media url=&quot;wysiwyg/banana.jpg&quot;}}" alt="" width="612" height="459"></p>',
+                encodedHtml = '<p>' +
+                    '<img src="http://example.com/admin/cms/wysiwyg/directive/___directive' +
+                    '/e3ttZWRpYSB1cmw9Ind5c2l3eWcvYmFuYW5hLmpwZyJ9fQ%2C%2C" alt="" width="612" height="459">' +
+                    '</p>';
 
-                describe('with secret key', function () {
-                    var decodedHtml = '<p>' +
-                        '<img src="{{media url=&quot;wysiwyg/banana.jpg&quot;}}" alt="" width="612" height="459"></p>',
-                        encodedHtml = '<p>' +
-                        '<img src="http://example.com/admin/cms/wysiwyg/directive/___directive' +
-                        '/e3ttZWRpYSB1cmw9Ind5c2l3eWcvYmFuYW5hLmpwZyJ9fQ%2C%2C/key/' +
-                        '5552655d13a141099d27f5d5b0c58869423fd265687167da12cad2bb39aa9a58?SID=something" ' +
-                        'alt="" width="612" height="459">' +
-                        '</p>',
-                        directiveUrl = 'http://example.com/admin/cms/wysiwyg/directive/key/' +
-                        '5552655d13a141099d27f5d5b0c58869423fd265687167da12cad2bb39aa9a58?SID=something';
-
-                    beforeEach(function () {
-                        obj.initialize('id', {
-                            'directives_url': directiveUrl
-                        });
-                    });
-
-                    runTests(decodedHtml, encodedHtml);
+            beforeEach(function () {
+                obj.initialize('id', {
+                    'directives_url': 'http://example.com/admin/cms/wysiwyg/directive/'
                 });
             });
+
+            runTests(decodedHtml, encodedHtml);
+        });
+
+        describe('without SID in directive query string with secret key', function () {
+            var decodedHtml = '<p>' +
+                '<img src="{{media url=&quot;wysiwyg/banana.jpg&quot;}}" alt="" width="612" height="459"></p>',
+                encodedHtml = '<p>' +
+                    '<img src="http://example.com/admin/cms/wysiwyg/directive/___directive' +
+                    '/e3ttZWRpYSB1cmw9Ind5c2l3eWcvYmFuYW5hLmpwZyJ9fQ%2C%2C/key/' +
+                    '5552655d13a141099d27f5d5b0c58869423fd265687167da12cad2bb39aa9a58" ' +
+                    'alt="" width="612" height="459">' +
+                    '</p>',
+                directiveUrl = 'http://example.com/admin/cms/wysiwyg/directive/key/' +
+                    '5552655d13a141099d27f5d5b0c58869423fd265687167da12cad2bb39aa9a58/';
+
+            beforeEach(function () {
+                obj.initialize('id', {
+                    'directives_url': directiveUrl
+                });
+            });
+
+            runTests(decodedHtml, encodedHtml);
+        });
+
+        describe('with SID in directive query string without secret key', function () {
+            var decodedHtml = '<p>' +
+                '<img src="{{media url=&quot;wysiwyg/banana.jpg&quot;}}" alt="" width="612" height="459"></p>',
+                encodedHtml = '<p>' +
+                    '<img src="http://example.com/admin/cms/wysiwyg/directive/___directive' +
+                    '/e3ttZWRpYSB1cmw9Ind5c2l3eWcvYmFuYW5hLmpwZyJ9fQ%2C%2C?SID=something" ' +
+                    'alt="" width="612" height="459">' +
+                    '</p>',
+                directiveUrl = 'http://example.com/admin/cms/wysiwyg/directive?SID=something';
+
+            beforeEach(function () {
+                obj.initialize('id', {
+                    'directives_url': directiveUrl
+                });
+            });
+
+            runTests(decodedHtml, encodedHtml);
+        });
+
+        describe('with SID in directive query string with secret key', function () {
+            var decodedHtml = '<p>' +
+                '<img src="{{media url=&quot;wysiwyg/banana.jpg&quot;}}" alt="" width="612" height="459"></p>',
+                encodedHtml = '<p>' +
+                    '<img src="http://example.com/admin/cms/wysiwyg/directive/___directive' +
+                    '/e3ttZWRpYSB1cmw9Ind5c2l3eWcvYmFuYW5hLmpwZyJ9fQ%2C%2C/key/' +
+                    '5552655d13a141099d27f5d5b0c58869423fd265687167da12cad2bb39aa9a58?SID=something" ' +
+                    'alt="" width="612" height="459">' +
+                    '</p>',
+                directiveUrl = 'http://example.com/admin/cms/wysiwyg/directive/key/' +
+                    '5552655d13a141099d27f5d5b0c58869423fd265687167da12cad2bb39aa9a58?SID=something';
+
+            beforeEach(function () {
+                obj.initialize('id', {
+                    'directives_url': directiveUrl
+                });
+            });
+
+            runTests(decodedHtml, encodedHtml);
         });
     });
 });
