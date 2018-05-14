e43eb88662d6118a87bcaa0d30e413b6250b4ccb
AleksLi <aleksliwork@gmail.com>
2018-05-14 21:59:15 +0300

    MSI-1115: Created validator for default Source Code. Should be enabled

---
+++ app/code/Magento/Inventory/Model/Source/Validator/PreventDisableValidator.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\Inventory\Model\Source\Validator;
+
+use Magento\Framework\Validation\ValidationResult;
+use Magento\Framework\Validation\ValidationResultFactory;
+use Magento\InventoryApi\Api\Data\SourceInterface;
+use Magento\InventoryCatalogApi\Api\DefaultSourceProviderInterface;
+
+/**
+ * Check that default source always enabled
+ */
+class PreventDisableValidator implements SourceValidatorInterface
+{
+    /**
+     * @var ValidationResultFactory
+     */
+    private $validationResultFactory;
+
+    /**
+     * @var DefaultSourceProviderInterface
+     */
+    private $defaultSourceProvider;
+
+    /**
+     * @param ValidationResultFactory $validationResultFactory
+     * @param DefaultSourceProviderInterface $defaultSourceProvider
+     */
+    public function __construct(
+        ValidationResultFactory $validationResultFactory,
+        DefaultSourceProviderInterface $defaultSourceProvider
+    ) {
+        $this->validationResultFactory = $validationResultFactory;
+        $this->defaultSourceProvider = $defaultSourceProvider;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function validate(SourceInterface $source): ValidationResult
+    {
+        $sourceCode = (string)$source->getSourceCode();
+        if ($sourceCode != $this->defaultSourceProvider->getCode()) {
+            return $this->validationResultFactory->create(['errors' => []]);
+        }
+        $value = (bool)$source->getEnabled();
+        if (!$value) {
+            $errors[] = __('Default source can not be disabled.');
+        } else {
+            $errors = [];
+        }
+        return $this->validationResultFactory->create(['errors' => $errors]);
+    }
+}
