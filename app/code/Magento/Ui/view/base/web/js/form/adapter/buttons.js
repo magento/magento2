c95eedc885f3aeff86f55a533b144ef0aede7a94
Jisse Reitsma <info@yireo.com>
2018-05-14 11:25:13 +0200

    Move buttons definition to separate file

---
+++ app/code/Magento/Ui/view/base/web/js/form/adapter/buttons.js
@@ -0,0 +1,15 @@
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+/**
+ * @api
+ */
+define(function () {
+    return {
+        'reset': '#reset',
+        'save': '#save',
+        'saveAndContinue': '#save_and_continue'
+    }
+});
