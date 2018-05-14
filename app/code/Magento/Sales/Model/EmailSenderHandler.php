d28b9a151bc2b026519cfeb9103246e527b66675
Ruslan Kostiv <rkostiv@magento.com>
2018-05-14 19:54:30 +0300

    MAGETWO-89238: [Sales email] Disabled invoice emails produce performance issue

--- app/code/Magento/Sales/Model/EmailSenderHandler.php
+++ app/code/Magento/Sales/Model/EmailSenderHandler.php
@@ -5,6 +5,8 @@
  */
 namespace Magento\Sales\Model;
 
+use Magento\Framework\App\ObjectManager;
+
 /**
  * Sales emails sending
  *
@@ -42,21 +44,30 @@ class EmailSenderHandler
     protected $globalConfig;
 
     /**
+     * @var \Magento\Sales\Model\Order\Email\Container\IdentityInterface
+     */
+    private $identityContainer;
+
+    /**
      * @param \Magento\Sales\Model\Order\Email\Sender $emailSender
      * @param \Magento\Sales\Model\ResourceModel\EntityAbstract $entityResource
      * @param \Magento\Sales\Model\ResourceModel\Collection\AbstractCollection $entityCollection
      * @param \Magento\Framework\App\Config\ScopeConfigInterface $globalConfig
+     * @param \Magento\Sales\Model\Order\Email\Container\IdentityInterface|null $identityContainer
      */
     public function __construct(
         \Magento\Sales\Model\Order\Email\Sender $emailSender,
         \Magento\Sales\Model\ResourceModel\EntityAbstract $entityResource,
         \Magento\Sales\Model\ResourceModel\Collection\AbstractCollection $entityCollection,
-        \Magento\Framework\App\Config\ScopeConfigInterface $globalConfig
+        \Magento\Framework\App\Config\ScopeConfigInterface $globalConfig,
+        \Magento\Sales\Model\Order\Email\Container\IdentityInterface $identityContainer = null
     ) {
         $this->emailSender = $emailSender;
         $this->entityResource = $entityResource;
         $this->entityCollection = $entityCollection;
         $this->globalConfig = $globalConfig;
+        $this->identityContainer = $identityContainer ?: ObjectManager::getInstance()
+            ->get(\Magento\Sales\Model\Order\Email\Container\IdentityInterface::class);
     }
 
     /**
@@ -69,14 +80,45 @@ class EmailSenderHandler
             $this->entityCollection->addFieldToFilter('send_email', ['eq' => 1]);
             $this->entityCollection->addFieldToFilter('email_sent', ['null' => true]);
 
-            /** @var \Magento\Sales\Model\AbstractModel $item */
-            foreach ($this->entityCollection->getItems() as $item) {
-                if ($this->emailSender->send($item, true)) {
-                    $this->entityResource->save(
-                        $item->setEmailSent(true)
-                    );
+            $stores = $this->getEntityStores(clone $this->entityCollection);
+
+            foreach ($stores as $store) {
+                $this->identityContainer->setStore($store);
+                if (!$this->identityContainer->isEnabled()) {
+                    continue;
+                }
+
+                $this->entityCollection->addFieldToFilter('store_id', $store->getId());
+
+                /** @var \Magento\Sales\Model\AbstractModel $item */
+                foreach ($this->entityCollection->getItems() as $item) {
+                    if ($this->emailSender->send($item, true)) {
+                        $this->entityResource->save(
+                            $item->setEmailSent(true)
+                        );
+                    }
                 }
             }
         }
+    }
+
+    /**
+     * Get stores for given entities.
+     *
+     * @param ResourceModel\Collection\AbstractCollection $entityCollection
+     * @return \Magento\Store\Api\Data\StoreInterface[]
+     */
+    private function getEntityStores(\Magento\Sales\Model\ResourceModel\Collection\AbstractCollection $entityCollection)
+    {
+        $stores = [];
+
+        $entityCollection->addAttributeToSelect('store_id')->getSelect()->group('store_id');
+
+        /** @var \Magento\Sales\Model\EntityInterface $item */
+        foreach ($entityCollection->getItems() as $item) {
+            $stores []= $item->getOrder()->getStore();
+        }
+
+        return $stores;
     }
 }
