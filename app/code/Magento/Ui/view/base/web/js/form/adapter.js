a30611d705c24e400ec65124f03e32ccd75945f5
Jisse Reitsma <info@yireo.com>
2018-05-15 07:30:29 +0200

    Make sure colons are in right place

--- app/code/Magento/Ui/view/base/web/js/form/adapter.js
+++ app/code/Magento/Ui/view/base/web/js/form/adapter.js
@@ -9,8 +9,8 @@ define([
 ], function ($, _, buttons) {
     'use strict';
 
-    var selectorPrefix = '',
-        eventPrefix;
+    var selectorPrefix = '';
+    var eventPrefix;
 
     /**
      * Initialize listener.
