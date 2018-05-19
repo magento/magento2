29400dd375d244349466c5a53e8f6db92825d12e
Vishal Gelani <vishalgelani99@gmail.com>
2018-05-19 21:12:59 +0530

    Fixed coding standard issue.

--- app/code/Magento/AdminNotification/view/adminhtml/web/js/system/messages/popup.js
+++ app/code/Magento/AdminNotification/view/adminhtml/web/js/system/messages/popup.js
@@ -4,9 +4,10 @@
 */
 
 define([
-    "jquery",
-    "Magento_Ui/js/modal/modal"
-], function($){
+    'jquery',
+    'Magento_Ui/js/modal/modal'
+], function ($) {
+    
     'use strict';
 
     return function (data, element) {
