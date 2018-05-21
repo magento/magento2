8937697f5f6c3a74acf68ff20c21d70ffa0752a9
Facundo Capua <facundocapua@gmail.com>
2018-05-21 16:18:21 -0300

    Include 'products' in category query

--- dev/tests/api-functional/testsuite/Magento/GraphQl/Catalog/CategoryTest.php
+++ dev/tests/api-functional/testsuite/Magento/GraphQl/Catalog/CategoryTest.php
@@ -9,6 +9,7 @@ namespace Magento\GraphQl\Catalog;
 
 use Magento\Framework\DataObject;
 use Magento\TestFramework\TestCase\GraphQlAbstract;
+use Magento\Catalog\Api\ProductRepositoryInterface;
 
 class CategoryTest extends GraphQlAbstract
 {
@@ -108,5 +109,164 @@ QUERY;
             5,
             $responseDataObject->getData('category/children/7/children/1/children/0/id')
         );
+    }
+
+    /**
+     * @magentoApiDataFixture Magento/Customer/_files/customer.php
+     * @magentoApiDataFixture Magento/Catalog/_files/categories.php
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+     */
+    public function testCategoryProducts()
+    {
+        $categoryId = 4;
+        $query = <<<QUERY
+{
+  category(id: {$categoryId}) {
+    products {
+      total_count
+      page_info {
+        current_page
+        page_size
+      }
+      items {
+        attribute_set_id
+        country_of_manufacture
+        created_at
+        description
+        gift_message_available
+        id
+        categories {
+          name
+          url_path
+          available_sort_by
+          level
+        }
+        image
+        image_label
+        meta_description
+        meta_keyword
+        meta_title
+        media_gallery_entries {
+          disabled
+          file
+          id
+          label
+          media_type
+          position
+          types
+          content {
+            base64_encoded_data
+            type
+            name
+          }
+          video_content {
+            media_type
+            video_description
+            video_metadata
+            video_provider
+            video_title
+            video_url
+          }
+        }
+        name
+        new_from_date
+        new_to_date
+        options_container
+       
+        price {
+          minimalPrice {
+            amount {
+              value
+              currency
+            }
+            adjustments {
+              amount {
+                value
+                currency
+              }
+              code
+              description
+            }
+          }
+          maximalPrice {
+            amount {
+              value
+              currency
+            }
+            adjustments {
+              amount {
+                value
+                currency
+              }
+              code
+              description
+            }
+          }
+          regularPrice {
+            amount {
+              value
+              currency
+            }
+            adjustments {
+              amount {
+                value
+                currency
+              }
+              code
+              description
+            }
+          }
+        }
+        product_links {
+          link_type
+          linked_product_sku
+          linked_product_type
+          position
+          sku
+        }
+        short_description
+        sku
+        small_image
+        small_image_label
+        special_from_date
+        special_price
+        special_to_date
+        swatch_image
+        tax_class_id
+        thumbnail
+        thumbnail_label
+        tier_price
+        tier_prices {
+          customer_group_id
+          percentage_value
+          qty
+          value
+          website_id
+        }
+        type_id
+        updated_at
+        url_key
+        url_path
+        websites {
+          id
+          name
+          code
+          sort_order
+          default_group_id
+          is_default
+        }
+        
+      }
+    }
+  }
+}
+QUERY;
+
+        $response = $this->graphQlQuery($query);
+        $this->assertArrayHasKey('products', $response['category']);
+        $this->assertArrayHasKey('total_count', $response['category']['products']);
+        $this->assertEquals(2, $response['category']['products']['total_count']);
+        $this->assertEquals(1, $response['category']['products']['page_info']['current_page']);
+        $this->assertEquals(20, $response['category']['products']['page_info']['page_size']);
     }
 }
