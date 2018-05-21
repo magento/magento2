5b871a5f02fd4fe3f214fc10d373a6babb4ded84
Sergey <simpleadm@gmail.com>
2018-05-21 14:29:28 +0300

    Added custom search result for theme grid

--- app/code/Magento/Theme/Model/ResourceModel/Theme/Grid/Collection.php
+++ app/code/Magento/Theme/Model/ResourceModel/Theme/Grid/Collection.php
@@ -5,151 +5,22 @@
  */
 namespace Magento\Theme\Model\ResourceModel\Theme\Grid;
 
-use Magento\Theme\Model\ResourceModel\Theme\Collection as ThemeCollection;
-use Magento\Framework\Api\Search\SearchResultInterface;
-use Magento\Framework\Api\Search\AggregationInterface;
-
 /**
  * Theme grid collection
+ * @deprecated
+ * @see \Magento\Theme\Ui\Component\Theme\DataProvider\SearchResult
  */
-class Collection extends ThemeCollection implements SearchResultInterface
+class Collection extends \Magento\Theme\Model\ResourceModel\Theme\Collection
 {
-    /**
-     * @var AggregationInterface
-     */
-    protected $aggregations;
-
-    /**
-     * @param \Magento\Framework\Data\Collection\EntityFactoryInterface $entityFactory
-     * @param \Psr\Log\LoggerInterface $logger
-     * @param \Magento\Framework\Data\Collection\Db\FetchStrategyInterface $fetchStrategy
-     * @param \Magento\Framework\Event\ManagerInterface $eventManager
-     * @param string $mainTable
-     * @param string $eventPrefix
-     * @param string $eventObject
-     * @param string $resourceModel
-     * @param string $model
-     * @param \Magento\Framework\DB\Adapter\AdapterInterface|string|null $connection
-     * @param \Magento\Framework\Model\ResourceModel\Db\AbstractDb $resource
-     *
-     * @SuppressWarnings(PHPMD.ExcessiveParameterList)
-     */
-    public function __construct(
-        \Magento\Framework\Data\Collection\EntityFactoryInterface $entityFactory,
-        \Psr\Log\LoggerInterface $logger,
-        \Magento\Framework\Data\Collection\Db\FetchStrategyInterface $fetchStrategy,
-        \Magento\Framework\Event\ManagerInterface $eventManager,
-        $mainTable,
-        $eventPrefix,
-        $eventObject,
-        $resourceModel,
-        $model = \Magento\Framework\View\Element\UiComponent\DataProvider\Document::class,
-        $connection = null,
-        \Magento\Framework\Model\ResourceModel\Db\AbstractDb $resource = null
-    ) {
-        parent::__construct(
-            $entityFactory,
-            $logger,
-            $fetchStrategy,
-            $eventManager,
-            $connection,
-            $resource
-        );
-        $this->_eventPrefix = $eventPrefix;
-        $this->_eventObject = $eventObject;
-        $this->_init($model, $resourceModel);
-        $this->setMainTable($mainTable);
-    }
-
     /**
      * Add area filter
      *
-     * @return \Magento\Theme\Model\ResourceModel\Theme\Collection
+     * @return $this
      */
     protected function _initSelect()
     {
         \Magento\Theme\Model\ResourceModel\Theme\Collection::_initSelect();
         $this->filterVisibleThemes()->addAreaFilter(\Magento\Framework\App\Area::AREA_FRONTEND)->addParentTitle();
-        $this
-            ->addFilterToMap('theme_id', 'main_table.theme_id')
-            ->addFilterToMap('theme_title', 'main_table.theme_title')
-            ->addFilterToMap('theme_path', 'main_table.theme_path')
-            ->addFilterToMap('parent_theme_title', 'parent.theme_title')
-        ;
-        return $this;
-    }
-
-    /**
-     * @return AggregationInterface
-     */
-    public function getAggregations()
-    {
-        return $this->aggregations;
-    }
-
-    /**
-     * @param AggregationInterface $aggregations
-     * @return $this
-     */
-    public function setAggregations($aggregations)
-    {
-        $this->aggregations = $aggregations;
-        return $this;
-    }
-
-    /**
-     * Get search criteria.
-     *
-     * @return \Magento\Framework\Api\SearchCriteriaInterface|null
-     */
-    public function getSearchCriteria()
-    {
-        return null;
-    }
-
-    /**
-     * Set search criteria.
-     *
-     * @param \Magento\Framework\Api\SearchCriteriaInterface $searchCriteria
-     * @return $this
-     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
-     */
-    public function setSearchCriteria(\Magento\Framework\Api\SearchCriteriaInterface $searchCriteria = null)
-    {
-        return $this;
-    }
-
-    /**
-     * Get total count.
-     *
-     * @return int
-     */
-    public function getTotalCount()
-    {
-        return $this->getSize();
-    }
-
-    /**
-     * Set total count.
-     *
-     * @param int $totalCount
-     * @return $this
-     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
-     */
-    public function setTotalCount($totalCount)
-    {
-        return $this;
-    }
-
-    /**
-     * Set items list.
-     *
-     * @param \Magento\Framework\Api\ExtensibleDataInterface[] $items
-     * @return $this
-     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
-     */
-    public function setItems(array $items = null)
-    {
         return $this;
     }
 }
