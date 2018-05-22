e7327e9053447a4eabe2830e8c18f2e51969ae39
Daniel Renaud <drenaud@magento.com>
2018-05-22 09:39:50 -0500

    MAGETWO-91848: Hint on product option with Maximum Characters should count down as more characters are entered

--- app/code/Magento/Catalog/view/frontend/web/js/product/custom-option.js
+++ app/code/Magento/Catalog/view/frontend/web/js/product/custom-option.js
@@ -4,43 +4,80 @@
  */
 
 define([
-    'jquery'
+    'jquery',
+    'mage/translate'
 ], function ($) {
     'use strict';
-    $.widget('mage.customOption', {
 
-        _create : function(){
-            this.characterCount();
+    $.widget('mage.customOption', {
+        options: {
+            'remainingText': $.mage.__('remaining'),
+            'tooManyText': $.mage.__('too many'),
+            'errorClass': 'mage-error',
+            'inputSelector': '.product-custom-option',
+            'counterSelector': 'span.character-counter',
+            'remainingCountSelector': 'span.count-remaining',
+            'countMessageSelector': 'span.count-message'
         },
 
-        /** @inheritDoc */
-        characterCount : function() {
-            var element = this.element,
-            input = $('.product-custom-option', element),
-            counter = $('.character-counter', element),
-            count = $('.count', element),
-            dataValidateString = input.attr('data-validate'),
-            dataValidate = {};
+        /**
+         * Initializes custom option component
+         *
+         * @private
+         */
+        _create: function () {
+            this.input = this.element.find(this.options.inputSelector);
+            this.counter = this.element.find(this.options.counterSelector);
+            this.countRemaining = this.element.find(this.options.remainingCountSelector);
+            this.countText = this.element.find(this.options.countMessageSelector);
 
             try {
-                dataValidate = JSON.parse(dataValidateString);
-
-                if (dataValidate.hasOwnProperty('maxlength')) {
-                    var maxLength = dataValidate.maxlength;
-
-                    if (maxLength && count.length) {
-                        input.on('change keyup paste', function () {
-                            var length = $(this).val().length,
-                                left = maxLength - length;
-
-                            count.text(left);
-                            counter.toggleClass('hidden', length);
-                        });
-                    }
-                }
+                this.dataValidate = JSON.parse(this.input.attr('data-validate'));
+            } catch (e) {
+                this.dataValidate = {};
             }
-            catch (e){}
+
+            if (this.dataValidate.hasOwnProperty('maxlength')) {
+                this.input.on('change keyup paste', function () {
+                    this.updateCharacterCount();
+                }.bind(this));
+            }
+        },
+
+        /**
+         * Updates counter message
+         */
+        updateCharacterCount: function () {
+            var length = this.input.val().length,
+                diff = this.dataValidate.maxlength - length;
+
+            this.countRemaining.text(Math.abs(diff));
+            this.countText.text(diff < 0 ? this.options.tooManyText : this.options.remainingText);
+            this.counter.toggleClass('no-display', length === 0);
+
+            if (diff < 0) {
+                this.highlight();
+            } else {
+                this.unhighlight();
+            }
+        },
+
+        /**
+         * Highlights input and field note
+         */
+        highlight: function () {
+            this.input.addClass(this.options.errorClass);
+            this.element.find('.note').addClass(this.options.errorClass);
+        },
+
+        /**
+         * Removes highlight from input and field note
+         */
+        unhighlight: function () {
+            this.input.removeClass(this.options.errorClass);
+            this.element.find('.note').removeClass(this.options.errorClass);
         }
     });
+
     return $.mage.customOption;
 });
