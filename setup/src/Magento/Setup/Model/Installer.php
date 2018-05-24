fb1a1a834ff6c19ea393c6af48aa1657ddd14540
Artem Cherednichenko <artemcherednichenkovasikievich@gmail.com>
2018-05-24 19:17:41 +0300

    Update flag_data column size

--- setup/src/Magento/Setup/Model/Installer.php
+++ setup/src/Magento/Setup/Model/Installer.php
@@ -777,7 +777,7 @@ class Installer
             )->addColumn(
                 'flag_data',
                 \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-                '64k',
+                '128k',
                 [],
                 'Flag Data'
             )->addColumn(
