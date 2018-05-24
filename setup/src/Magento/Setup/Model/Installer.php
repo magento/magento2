41b5f8ba366b5b309b8c8090f52febdc369d0715
Artem Cherednichenko <artemcherednichenkovasikievich@gmail.com>
2018-05-24 18:27:30 +0300

    Update data_flag column in installer
    data_flag used for several proposes, a but main is to save app:config:import data as a system_config_snapshot, if store has several scopes + multiple extensions, it can be not enough to save config snapshot

--- setup/src/Magento/Setup/Model/Installer.php
+++ setup/src/Magento/Setup/Model/Installer.php
@@ -741,7 +741,7 @@ class Installer
             )->addColumn(
                 'flag_data',
                 \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-                '64k',
+                '128k',
                 [],
                 'Flag Data'
             )->addColumn(
