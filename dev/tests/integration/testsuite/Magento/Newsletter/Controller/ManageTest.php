a478ff5a6bc1ef6b9cf5d34b23dd00fbbfb3c53a
Alex Lyzun <alyzun@comwrap.com>
2018-05-27 11:58:39 +0200

    Updat messages related to a new logic

--- dev/tests/integration/testsuite/Magento/Newsletter/Controller/ManageTest.php
+++ dev/tests/integration/testsuite/Magento/Newsletter/Controller/ManageTest.php
@@ -67,6 +67,7 @@ class ManageTest extends \Magento\TestFramework\TestCase\AbstractController
      */
     public function testSaveActionRemoveSubscription()
     {
+
         $this->getRequest()
             ->setParam('form_key', 'formKey')
             ->setParam('is_subscribed', '0');
@@ -83,7 +84,7 @@ class ManageTest extends \Magento\TestFramework\TestCase\AbstractController
          * Check that success message
          */
         $this->assertSessionMessages(
-            $this->equalTo(['We removed the subscription.']),
+            $this->equalTo(['We updated the subscription.']),
             \Magento\Framework\Message\MessageInterface::TYPE_SUCCESS
         );
     }
