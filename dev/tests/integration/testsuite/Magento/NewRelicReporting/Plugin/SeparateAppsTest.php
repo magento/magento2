24d7d4afcd9302f2d02b2dba34a60327d32b4224
Max Chadwick <mpchadwick@gmail.com>
2018-05-27 20:28:39 -0400

    Fix indentation

--- dev/tests/integration/testsuite/Magento/NewRelicReporting/Plugin/SeparateAppsTest.php
+++ dev/tests/integration/testsuite/Magento/NewRelicReporting/Plugin/SeparateAppsTest.php
@@ -12,36 +12,36 @@ use Magento\TestFramework\Helper\Bootstrap;
 
 class SeparateAppsTest extends \PHPUnit\Framework\TestCase
 {
-	/**
-	 * @var ObjectManager
-	 */
-	private $objectManager;
+    /**
+     * @var ObjectManager
+     */
+    private $objectManager;
 
-	protected function setUp()
-	{
-		$this->objectManager = Bootstrap::getObjectManager();
-	}
+    protected function setUp()
+    {
+        $this->objectManager = Bootstrap::getObjectManager();
+    }
 
-	/**
-	 * @magentoConfigFixture default/newrelicreporting/general/enable 1
-	 * @magentoConfigFixture default/newrelicreporting/general/app_name beverly_hills
-	 * @magentoConfigFixture default/newrelicreporting/general/separate_apps 1
-	 */
-	public function testAppNameIsSetWhenConfiguredCorrectly()
-	{
-		$newRelicWrapper = $this->getMockBuilder(NewRelicWrapper::class)
-			->setMethods(['setAppName'])
-			->getMock();
+    /**
+     * @magentoConfigFixture default/newrelicreporting/general/enable 1
+     * @magentoConfigFixture default/newrelicreporting/general/app_name beverly_hills
+     * @magentoConfigFixture default/newrelicreporting/general/separate_apps 1
+     */
+    public function testAppNameIsSetWhenConfiguredCorrectly()
+    {
+        $newRelicWrapper = $this->getMockBuilder(NewRelicWrapper::class)
+            ->setMethods(['setAppName'])
+            ->getMock();
 
-		$this->objectManager->configure([NewRelicWrapper::class => ['shared' => true]]);
-		$this->objectManager->addSharedInstance($newRelicWrapper, NewRelicWrapper::class);
+        $this->objectManager->configure([NewRelicWrapper::class => ['shared' => true]]);
+        $this->objectManager->addSharedInstance($newRelicWrapper, NewRelicWrapper::class);
 
-		$newRelicWrapper->expects($this->once())
-			->method('setAppName')
-			->with($this->equalTo('beverly_hills;beverly_hills_90210'));
+        $newRelicWrapper->expects($this->once())
+            ->method('setAppName')
+            ->with($this->equalTo('beverly_hills;beverly_hills_90210'));
 
-		$state = $this->objectManager->get(State::class);
+        $state = $this->objectManager->get(State::class);
 
-		$state->setAreaCode('90210');
-	}
+        $state->setAreaCode('90210');
+    }
 }
