b04093c17be2979d2148d72d3e056710954f958c
Elias Kotlyar <elias.kotlyar@gmail.com>
2018-05-29 12:19:46 +0200

    Fixxes #15565

--- app/code/Magento/Store/Model/PathConfig.php
+++ app/code/Magento/Store/Model/PathConfig.php
@@ -76,6 +76,8 @@ class PathConfig implements \Magento\Framework\App\Router\PathConfigInterface
      */
     public function getDefaultPath()
     {
-        return $this->scopeConfig->getValue('web/default/front', ScopeInterface::SCOPE_STORE);
+        $store = $this->storeManager->getStore();
+        $value =$this->scopeConfig->getValue('web/default/front', ScopeInterface::SCOPE_STORE,$store);
+        return $value;
     }
 }
