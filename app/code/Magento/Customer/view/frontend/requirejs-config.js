b28360cd394d896dfcd4880ab4d062601bfe9759
Piyush Dankhara <dankhrapiyush@gmail.com>
2018-05-29 10:41:37 -0700

    Remove trimUsername declaration from requirejs-config.js and use path inside template

--- app/code/Magento/Customer/view/frontend/requirejs-config.js
+++ app/code/Magento/Customer/view/frontend/requirejs-config.js
@@ -9,7 +9,6 @@ var config = {
             checkoutBalance:    'Magento_Customer/js/checkout-balance',
             address:            'Magento_Customer/address',
             changeEmailPassword: 'Magento_Customer/change-email-password',
-            trimUsername: 'Magento_Customer/js/trim-username',
             passwordStrengthIndicator: 'Magento_Customer/js/password-strength-indicator',
             zxcvbn: 'Magento_Customer/js/zxcvbn',
             addressValidation: 'Magento_Customer/js/addressValidation'
