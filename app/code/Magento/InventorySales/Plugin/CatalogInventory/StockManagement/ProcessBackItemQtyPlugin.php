dfeb05ed042325271651bea51e8cfa878ea65692
Igor Miniailo <iminiailo@magento.com>
2018-05-31 14:46:56 +0300

    MSI-1248: fix comment

--- app/code/Magento/InventorySales/Plugin/CatalogInventory/StockManagement/ProcessBackItemQtyPlugin.php
+++ app/code/Magento/InventorySales/Plugin/CatalogInventory/StockManagement/ProcessBackItemQtyPlugin.php
@@ -98,11 +98,16 @@ class ProcessBackItemQtyPlugin
             throw new LocalizedException(__('$scopeId is required'));
         }
 
-        // As it was decided that the inventory doesn’t depend on the catalog (these two system are independent)
-        // it is necessary for us to have an ability to process orders even with deleted or non-existing products
         try {
             $productSku = $this->getSkusByProductIds->execute([$productId])[$productId];
         } catch (InputException $e) {
+            /**
+             * As it was decided the Inventory should not use data constraints depending on Catalog
+             * (these two systems are not highly coupled, i.e. Magento does not sync data between them, so that
+             * it's possible that SKU exists in Catalog, but does not exist in Inventory and vice versa)
+             * it is necessary for Magento to have an ability to process placed orders even with
+             * deleted or non-existing products
+             */
             return true;
         }
         $productType = $this->getProductTypesBySkus->execute([$productSku])[$productSku];
