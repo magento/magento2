876315eabfa7016e2bb25435ec4748e32d08e7d8
Mark Shust <mark@shust.com>
2018-05-31 17:20:24 -0400

    Create ability to set is_visible_on_front to order status history comment

--- app/code/Magento/Sales/Model/Order.php
+++ app/code/Magento/Sales/Model/Order.php
@@ -994,7 +994,7 @@ class Order extends AbstractModel implements EntityInterface, OrderInterface
      * @param bool|string $status
      * @return OrderStatusHistoryInterface
      */
-    public function addStatusHistoryComment($comment, $status = false)
+    public function addStatusHistoryComment($comment, $status = false, $isVisibleOnFront = false)
     {
         if (false === $status) {
             $status = $this->getStatus();
@@ -1009,6 +1009,8 @@ class Order extends AbstractModel implements EntityInterface, OrderInterface
             $comment
         )->setEntityName(
             $this->entityType
+        )->setIsVisibleOnFront(
+            $isVisibleOnFront
         );
         $this->addStatusHistory($history);
         return $history;
