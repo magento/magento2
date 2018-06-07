d5be0aa37df4dcace02c7a6275cce616062231c4
ilnytskyi_sv <stailx1@gmail.com>
2018-06-07 11:32:03 +0300

    #10790

--- dev/tests/integration/testsuite/Magento/Tax/Model/Sales/Total/Quote/TaxTest.php
+++ dev/tests/integration/testsuite/Magento/Tax/Model/Sales/Total/Quote/TaxTest.php
@@ -138,10 +138,10 @@ class TaxTest extends \Magento\TestFramework\Indexer\TestCase
      */
     public function testFullDiscountWithDeltaRoundingFix()
     {
-        global $fullTaxDiscountWithTax;
-        $configData = $fullTaxDiscountWithTax['config_data'];
-        $quoteData = $fullTaxDiscountWithTax['quote_data'];
-        $expectedResults = $fullTaxDiscountWithTax['expected_result'];
+        global $fullDiscountIncTax;
+        $configData = $fullDiscountIncTax['config_data'];
+        $quoteData = $fullDiscountIncTax['quote_data'];
+        $expectedResults = $fullDiscountIncTax['expected_result'];
 
         /** @var  \Magento\Framework\ObjectManagerInterface $objectManager */
         $objectManager = Bootstrap::getObjectManager();
