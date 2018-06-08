051b2992e65d9dad5363660014ae60fd78a76091
Stanislav Lopukhov <slopukhov@magento.com>
2018-06-08 10:51:11 +0300

    MAGETWO-91821: Handle website or customer group creation/deletion

--- app/code/Magento/Catalog/Test/Unit/Model/Indexer/Product/Price/Plugin/WebsiteTest.php
+++ app/code/Magento/Catalog/Test/Unit/Model/Indexer/Product/Price/Plugin/WebsiteTest.php
@@ -10,38 +10,111 @@ class WebsiteTest extends \PHPUnit\Framework\TestCase
     /**
      * @var \Magento\Framework\TestFramework\Unit\Helper\ObjectManager
      */
-    protected $_objectManager;
+    protected $objectManager;
 
     /**
      * @var \Magento\Catalog\Model\Indexer\Product\Price\Plugin\Website
      */
-    protected $_model;
+    protected $model;
 
     /**
      * @var \Magento\Catalog\Model\Indexer\Product\Price\Processor|\PHPUnit_Framework_MockObject_MockObject
      */
-    protected $_priceProcessorMock;
+    protected $scopeConfig;
+
+    /**
+     * @var \Magento\Framework\Indexer\DimensionFactory|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $dimensionFactory;
+
+    /**
+     * @var \Magento\Catalog\Model\Indexer\Product\Price\TableMaintainer|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $tableMaintainer;
 
     protected function setUp()
     {
-        $this->_objectManager = new \Magento\Framework\TestFramework\Unit\Helper\ObjectManager($this);
+        $this->objectManager = new \Magento\Framework\TestFramework\Unit\Helper\ObjectManager($this);
 
-        $this->_priceProcessorMock = $this->createPartialMock(
-            \Magento\Catalog\Model\Indexer\Product\Price\Processor::class,
-            ['markIndexerAsInvalid']
+        $this->scopeConfig = $this->createPartialMock(
+            \Magento\Framework\App\Config\ScopeConfigInterface::class,
+            ['getValue', 'isSetFlag']
         );
 
-        $this->_model = $this->_objectManager->getObject(
+        $this->dimensionFactory = $this->createPartialMock(
+            \Magento\Framework\Indexer\DimensionFactory::class,
+            ['create']
+        );
+
+        $this->tableMaintainer = $this->createPartialMock(
+            \Magento\Catalog\Model\Indexer\Product\Price\TableMaintainer::class,
+            ['dropTablesForDimensions', 'createTablesForDimensions']
+        );
+
+        $this->model = $this->objectManager->getObject(
             \Magento\Catalog\Model\Indexer\Product\Price\Plugin\Website::class,
-            ['processor' => $this->_priceProcessorMock]
+            [
+                'configReader' => $this->scopeConfig,
+                'dimensionFactory' => $this->dimensionFactory,
+                'tableMaintainer' => $this->tableMaintainer,
+            ]
         );
     }
 
     public function testAfterDelete()
     {
-        $this->_priceProcessorMock->expects($this->once())->method('markIndexerAsInvalid');
+        $dimensionMock = $this->createMock(\Magento\Framework\Indexer\Dimension::class);
 
-        $websiteMock = $this->createMock(\Magento\Store\Model\ResourceModel\Website::class);
-        $this->assertEquals('return_value', $this->_model->afterDelete($websiteMock, 'return_value'));
+        $this->scopeConfig->expects($this->once())->method('getValue')->willReturn(
+            \Magento\Catalog\Model\Indexer\Product\Price\ModeSwitcher::INPUT_KEY_WEBSITE
+        );
+        $this->dimensionFactory->expects($this->once())->method('create')->willReturn(
+            $dimensionMock
+        );
+        $this->tableMaintainer->expects($this->once())->method('dropTablesForDimensions')->with(
+            [$dimensionMock]
+        );
+
+        $subjectMock = $this->createMock(\Magento\Framework\Model\ResourceModel\Db\AbstractDb::class);
+        $objectResourceMock = $this->createMock(\Magento\Framework\Model\ResourceModel\Db\AbstractDb::class);
+        $websiteMock = $this->createMock(\Magento\Framework\Model\AbstractModel::class);
+        $websiteMock->expects($this->once())
+            ->method('getId')
+            ->willReturn(1);
+        
+        $this->assertEquals(
+            $objectResourceMock,
+            $this->model->afterDelete($subjectMock, $objectResourceMock, $websiteMock)
+        );
+    }
+
+    public function testAfterSave()
+    {
+        $dimensionMock = $this->createMock(\Magento\Framework\Indexer\Dimension::class);
+
+        $this->scopeConfig->expects($this->once())->method('getValue')->willReturn(
+            \Magento\Catalog\Model\Indexer\Product\Price\ModeSwitcher::INPUT_KEY_WEBSITE
+        );
+        $this->dimensionFactory->expects($this->once())->method('create')->willReturn(
+            $dimensionMock
+        );
+        $this->tableMaintainer->expects($this->once())->method('createTablesForDimensions')->with(
+            [$dimensionMock]
+        );
+
+        $subjectMock = $this->createMock(\Magento\Framework\Model\ResourceModel\Db\AbstractDb::class);
+        $objectResourceMock = $this->createMock(\Magento\Framework\Model\ResourceModel\Db\AbstractDb::class);
+        $websiteMock = $this->createMock(\Magento\Framework\Model\AbstractModel::class);
+        $websiteMock->expects($this->once())
+            ->method('getId')
+            ->willReturn(1);
+        $websiteMock->expects($this->once())
+            ->method('isObjectNew')
+            ->willReturn(true);
+
+        $this->assertEquals(
+            $objectResourceMock,
+            $this->model->afterSave($subjectMock, $objectResourceMock, $websiteMock)
+        );
     }
 }
