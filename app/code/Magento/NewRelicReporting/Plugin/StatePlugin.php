05e2d82ec55cb97448c246f793dcf6461670b273
Ihor Sviziev <ihor-sviziev@users.noreply.github.com>
2018-06-11 08:08:14 +0300

    Add Ability To Separate Frontend / Adminhtml in New Relic
    Declare strict types

--- app/code/Magento/NewRelicReporting/Plugin/StatePlugin.php
+++ app/code/Magento/NewRelicReporting/Plugin/StatePlugin.php
@@ -3,6 +3,8 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types=1);
+
 namespace Magento\NewRelicReporting\Plugin;
 
 use Magento\Framework\App\State;
