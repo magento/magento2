64fc3c279ebde806af147eee3840601023cb94e6
Riccardo Tempesta <riccardo.tempesta@magespecialist.it>
2018-06-14 16:55:51 +0200

    FIX for apparently random API failures while using array types If the webapi Interface class was using CR+LF end-of-line, TypeProcessor class was not able to determine the class name when an array was used.

--- lib/internal/Magento/Framework/Reflection/TypeProcessor.php
+++ lib/internal/Magento/Framework/Reflection/TypeProcessor.php
@@ -555,7 +555,7 @@ class TypeProcessor
         if ($type == 'array') {
             // try to determine class, if it's array of objects
             $docBlock = $param->getDeclaringFunction()->getDocBlock();
-            $pattern = "/\@param\s+([\w\\\_]+\[\])\s+\\\${$param->getName()}\n/";
+            $pattern = "/\@param\s+([\w\\\_]+\[\])\s+\\\${$param->getName()}[\n\r]/";
             $matches = [];
             if (preg_match($pattern, $docBlock->getContents(), $matches)) {
                 return $matches[1];
