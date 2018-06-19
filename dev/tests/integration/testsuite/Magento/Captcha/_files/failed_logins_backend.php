9912388ab8456b9e60c9339bb5b156fc8663ea68
Yaroslav Rogoza <enarc@atwix.com>
2018-06-19 14:26:12 +0200

    Added integration test for checking admin login attempts cleanup

---
+++ dev/tests/integration/testsuite/Magento/Captcha/_files/failed_logins_backend.php
@@ -0,0 +1,12 @@
+<?php
+
+use Magento\TestFramework\Helper\Bootstrap;
+use Magento\Captcha\Model\ResourceModel\LogFactory;
+use Magento\Captcha\Model\ResourceModel\Log;
+
+$objectManager = Bootstrap::getObjectManager();
+$logFactory = $objectManager->get(LogFactory::class);
+
+/** @var Log $captchaLog */
+$captchaLog = $logFactory->create();
+$captchaLog->logAttempt('mageadmin');
