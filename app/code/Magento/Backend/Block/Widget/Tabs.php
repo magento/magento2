0dd4268c9c2a4398d54cf9c71cd5272ffe2f9e73
Tiago Sampaio <tiago@tiagosampaio.com>
2018-06-20 12:35:07 -0300

    Tiny changes to add the type of a variable.

--- app/code/Magento/Backend/Block/Widget/Tabs.php
+++ app/code/Magento/Backend/Block/Widget/Tabs.php
@@ -238,7 +238,7 @@ class Tabs extends \Magento\Backend\Block\Widget
         $this->_tabs = $this->reorderTabs();
         
         if ($this->_activeTab === null) {
-            /** @var  $tab */
+            /** @var TabInterface $tab */
             foreach ($this->_tabs as $tab) {
                 $this->_activeTab = $tab->getId();
                 break;
@@ -309,7 +309,7 @@ class Tabs extends \Magento\Backend\Block\Widget
     
         $ordered = [];
         
-        /** @var  $tab */
+        /** @var TabInterface $tab */
         foreach ($orderByPosition as $tab) {
             $ordered[$tab->getId()] = $tab;
         }
