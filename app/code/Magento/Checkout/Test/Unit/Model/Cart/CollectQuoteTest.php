52e97269e89432be5aeb19fa1f33857668f66dec
eduard13 <e.chitoraga@atwix.com>
2018-06-20 20:46:19 +0300

    Refactoring to remove some dependencies.

--- app/code/Magento/Checkout/Test/Unit/Model/Cart/CollectQuoteTest.php
+++ app/code/Magento/Checkout/Test/Unit/Model/Cart/CollectQuoteTest.php
@@ -14,13 +14,13 @@ use Magento\Customer\Api\Data\CustomerInterface;
 use Magento\Customer\Api\Data\RegionInterface;
 use Magento\Checkout\Model\Cart\CollectQuote;
 use Magento\Customer\Model\Session as CustomerSession;
-use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
 use Magento\Quote\Api\CartRepositoryInterface;
 use Magento\Quote\Api\Data\EstimateAddressInterface;
 use Magento\Quote\Api\Data\EstimateAddressInterfaceFactory;
 use Magento\Quote\Api\ShippingMethodManagementInterface;
 use Magento\Quote\Model\Quote;
 use PHPUnit\Framework\TestCase;
+
 /**
  * Class CollectQuoteTest
  */
@@ -30,11 +30,6 @@ class CollectQuoteTest extends TestCase
      * @var CollectQuote
      */
     private $model;
-
-    /**
-     * @var ObjectManager
-     */
-    private $objectManager;
 
     /**
      * @var CustomerSession|\PHPUnit_Framework_MockObject_MockObject
@@ -87,16 +82,10 @@ class CollectQuoteTest extends TestCase
     private $addressMock;
 
     /**
-     * @var RegionInterface|\PHPUnit_Framework_MockObject_MockObject
-     */
-    private $regionMock;
-
-    /**
      * Set up
      */
     protected function setUp()
     {
-        $this->objectManager = new ObjectManager($this);
         $this->customerSessionMock = $this->createMock(CustomerSession::class);
         $this->customerRepositoryMock = $this->getMockForAbstractClass(
             CustomerRepositoryInterface::class,
@@ -118,18 +107,14 @@ class CollectQuoteTest extends TestCase
         $this->quoteMock = $this->createMock(Quote::class);
         $this->customerMock = $this->createMock(CustomerInterface::class);
         $this->addressMock = $this->createMock(AddressInterface::class);
-        $this->regionMock = $this->createMock(RegionInterface::class);
 
-        $this->model = $this->objectManager->getObject(
-            CollectQuote::class,
-            [
-                'customerSession' => $this->customerSessionMock,
-                'customerRepository' => $this->customerRepositoryMock,
-                'addressRepository' => $this->addressRepositoryMock,
-                'estimatedAddressFactory' => $this->estimateAddressFactoryMock,
-                'shippingMethodManager' => $this->shippingMethodManagerMock,
-                'quoteRepository' => $this->quoteRepositoryMock
-            ]
+        $this->model = new CollectQuote(
+            $this->customerSessionMock,
+            $this->customerRepositoryMock,
+            $this->addressRepositoryMock,
+            $this->estimateAddressFactoryMock,
+            $this->shippingMethodManagerMock,
+            $this->quoteRepositoryMock
         );
     }
 
@@ -142,36 +127,29 @@ class CollectQuoteTest extends TestCase
         $defaultAddressId = 999;
         $countryId = 'USA';
         $regionId = 'CA';
-        $this->customerSessionMock->expects(self::once())
-            ->method('isLoggedIn')
-            ->willReturn(true);
-        $this->customerSessionMock->expects(self::once())
-            ->method('getCustomerId')
-            ->willReturn($customerId);
-        $this->customerRepositoryMock->expects(self::once())
-            ->method('getById')
-            ->willReturn($this->customerMock);
-        $this->customerMock->expects(self::once())
-            ->method('getDefaultShipping')
-            ->willReturn($defaultAddressId);
-        $this->addressMock->expects(self::once())
-            ->method('getCountryId')
-            ->willReturn($countryId);
-        $this->regionMock->expects(self::once())
-            ->method('getRegion')
-            ->willReturn($regionId);
-        $this->addressMock->expects(self::once())
-            ->method('getRegion')
-            ->willReturn($this->regionMock);
-        $this->addressRepositoryMock->expects(self::once())
-            ->method('getById')
-            ->with($defaultAddressId)
-            ->willReturn($this->addressMock);
-        $this->estimateAddressFactoryMock->expects(self::once())
-            ->method('create')
-            ->willReturn($this->estimateAddressMock);
-        $this->quoteRepositoryMock->expects(self::once())
-            ->method('save');
+        $regionMock = $this->createMock(RegionInterface::class);
+
+        $this->customerSessionMock->expects(self::once())->method('isLoggedIn')->willReturn(true);
+        $this->customerSessionMock->expects(self::once())->method('getCustomerId')->willReturn($customerId);
+        $this->customerRepositoryMock->expects(self::once())->method('getById')->willReturn($this->customerMock);
+        $this->customerMock->expects(self::once())->method('getDefaultShipping')->willReturn($defaultAddressId);
+        $this->addressMock->expects(self::once())->method('getCountryId')->willReturn($countryId);
+        $regionMock->expects(self::once())->method('getRegion')->willReturn($regionId);
+        $this->addressMock->expects(self::once())->method('getRegion')->willReturn($regionMock);
+        $this->addressRepositoryMock->expects(self::once())->method('getById')->with($defaultAddressId)->willReturn($this->addressMock);
+        $this->estimateAddressFactoryMock->expects(self::once())->method('create')->willReturn($this->estimateAddressMock);
+        $this->quoteRepositoryMock->expects(self::once())->method('save');
+
+        $this->model->collect($this->quoteMock);
+    }
+
+    /**
+     * Test with a not logged in customer
+     */
+    public function testCustomerIsNotLoggedIn()
+    {
+        $this->customerSessionMock->expects(self::once())->method('isLoggedIn')->willReturn(false);
+        $this->customerRepositoryMock->expects(self::never())->method('getById');
 
         $this->model->collect($this->quoteMock);
     }
