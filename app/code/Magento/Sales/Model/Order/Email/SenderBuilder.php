6e2fc05e12fa04ece313d4055a78c380ea63c8e3
Rob Conings <rob@versdivers.be>
2018-06-23 09:12:57 +0200

    Fix Email Sender issue 16355
    https://github.com/magento/magento2/issues/16355

--- app/code/Magento/Sales/Model/Order/Email/SenderBuilder.php
+++ app/code/Magento/Sales/Model/Order/Email/SenderBuilder.php
@@ -110,9 +110,6 @@ class SenderBuilder
         $this->transportBuilder->setTemplateIdentifier($this->templateContainer->getTemplateId());
         $this->transportBuilder->setTemplateOptions($this->templateContainer->getTemplateOptions());
         $this->transportBuilder->setTemplateVars($this->templateContainer->getTemplateVars());
-        $this->transportBuilderByStore->setFromByStore(
-            $this->identityContainer->getEmailIdentity(),
-            $this->identityContainer->getStore()->getId()
-        );
+        $this->transportBuilder->setFrom($this->identityContainer->getEmailIdentity(), $this->identityContainer->getStore()->getId());
     }
 }
