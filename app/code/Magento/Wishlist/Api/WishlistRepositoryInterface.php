40fa35bee45690a197cd7775560139b8371f9db5
Wietze Mink <wietze@tickles.nl>
2018-06-25 11:15:56 +0200

    Create new Wishlist Repository Interface with CRUD operations

--- app/code/Magento/Wishlist/Api/WishlistRepositoryInterface.php
+++ app/code/Magento/Wishlist/Api/WishlistRepositoryInterface.php
@@ -2,6 +2,9 @@
 
 namespace Magento\Wishlist\Api;
 
+use Magento\Wishlist\Api\Data\WishlistInterface;
+use Magento\Wishlist\Api\Data\WishlistSearchResultsInterface;
+
 /**
  * Interface WishlistRepositoryInterface
  * @api
@@ -10,18 +13,43 @@ namespace Magento\Wishlist\Api;
 interface WishlistRepositoryInterface
 {
     /**
-     * @param int $customerId
-     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     * Get Wishlist by id
+     * @param int $id
      * @return \Magento\Wishlist\Api\Data\WishlistInterface
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
      */
-    public function getWishlistForCustomer($customerId);
+    public function get($id): \Magento\Wishlist\Api\Data\WishlistInterface;
 
     /**
-     * @param int $customerId
-     * @param int $productId
-     * @throws \Magento\Framework\Exception\StateException
-     * @return int
+     * Get list of wishlists by criteria
+     *
+     * @param \Magento\Framework\Api\SearchCriteriaInterface $searchCriteria
+     * @return \Magento\Wishlist\Api\Data\WishlistSearchResultsInterface
      */
-    public function addWishlistForCustomer($customerId, $productId);
+    public function getList(\Magento\Framework\Api\SearchCriteriaInterface $searchCriteria): WishlistSearchResultsInterface;
 
+    /**
+     * Save wishlist object
+     *
+     * @param Data\WishlistInterface $wishlist
+     * @return \Magento\Wishlist\Api\Data\WishlistInterface
+     * @throws \Magento\Framework\Exception\CouldNotSaveException
+     */
+    public function save(\Magento\Wishlist\Api\Data\WishlistInterface $wishlist): WishlistInterface;
+
+    /**
+     * Delete wishlist by passing object
+     *
+     * @param \Magento\Wishlist\Api\Data\WishlistInterface $wishlist
+     * @return bool
+     */
+    public function delete(\Magento\Wishlist\Api\Data\WishlistInterface $wishlist);
+
+    /**
+     * Delete wishlist by id
+     *
+     * @param int $id
+     * @return bool
+     */
+    public function deleteById($id);
 }
