206ea482ef96115a6350c7dad8459499cbc4e40c
Max Lesechko <mlesechko@magento.com>
2018-06-26 09:58:29 -0500

    MAGETWO-91433: ProductListing: Grid view is getting changed to List view when user adding product from wishlist section.

--- app/code/Magento/Wishlist/Model/Product/AttributeValueProvider.php
+++ app/code/Magento/Wishlist/Model/Product/AttributeValueProvider.php
@@ -8,7 +8,6 @@ declare(strict_types=1);
 namespace Magento\Wishlist\Model\Product;
 
 use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory as ProductCollectionFactory;
-use Magento\Framework\Exception\NoSuchEntityException;
 
 /**
  * Provides existing attribute value for a product entity.
@@ -36,7 +35,6 @@ class AttributeValueProvider
      * @param string $attributeCode
      * @param int|null $storeId
      * @return null|string
-     * @throws NoSuchEntityException
      */
     public function getRawAttributeValue(int $productId, string $attributeCode, int $storeId = null):? string
     {
@@ -46,12 +44,13 @@ class AttributeValueProvider
             ->addStoreFilter($storeId)
             ->addAttributeToSelect($attributeCode);
 
-        $data = $collection->getConnection()->fetchRow($collection->getSelect());
-
-        if (!array_key_exists($attributeCode, $data)) {
-            throw new NoSuchEntityException(__('An attribute value of "%1" does not exist.', $attributeCode));
+        if ($collection->isEnabledFlat()) {
+            $data = $collection->getConnection()->fetchRow($collection->getSelect());
+            $attributeValue = $data[$attributeCode] ?? null;
+        } else {
+            $attributeValue = $collection->getFirstItem()->getData($attributeCode);
         }
 
-        return $data[$attributeCode];
+        return $attributeValue;
     }
 }
