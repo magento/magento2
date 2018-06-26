18c67f0db88274e9dd2fff311cd1de12c099ad9b
Volodymyr Zaets <strpwebstudio@gmail.com>
2018-06-26 10:53:11 +0300

    Remove empty line

--- lib/internal/Magento/Framework/Code/Test/Unit/_files/app/code/Magento/SomeModule/Model/SevenInterface.php
+++ lib/internal/Magento/Framework/Code/Test/Unit/_files/app/code/Magento/SomeModule/Model/SevenInterface.php
@@ -45,7 +45,6 @@ interface SevenInterface extends \Magento\Framework\Code\Generator\CodeGenerator
      */
     public function testMethod2($data = 'test_default', $flag = true);
 
-    
     public function testMethod3();
 
 
