3533e68e1617d8c5ab8381674a6840d5f821bb55
Alex Lyzun <alyzun@comwrap.com>
2018-06-26 13:42:24 +0200

    Change Success messages to appropriated

--- dev/tests/integration/testsuite/Magento/Newsletter/Controller/ManageTest.php
+++ dev/tests/integration/testsuite/Magento/Newsletter/Controller/ManageTest.php
@@ -58,7 +58,7 @@ class ManageTest extends \Magento\TestFramework\TestCase\AbstractController
          * Check that success message
          */
         $this->assertSessionMessages(
-            $this->equalTo(['We saved the subscription.']),
+            $this->equalTo(['We have saved your subscription.']),
             \Magento\Framework\Message\MessageInterface::TYPE_SUCCESS
         );
     }
@@ -85,7 +85,7 @@ class ManageTest extends \Magento\TestFramework\TestCase\AbstractController
          * Check that success message
          */
         $this->assertSessionMessages(
-            $this->equalTo(['We updated the subscription.']),
+            $this->equalTo(['We have updated your subscription.']),
             \Magento\Framework\Message\MessageInterface::TYPE_SUCCESS
         );
     }
