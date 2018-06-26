2f8cb97cfe7213e706e326e3f3bf1d5c63bcc76a
Vasiliev.A <a.vasiliev@sam-solutions.biz>
2018-06-26 03:50:24 +0300

    fix class and method descriptions

--- app/code/Magento/WebapiAsync/Plugin/Communication/Config/CompositeReader.php
+++ app/code/Magento/WebapiAsync/Plugin/Communication/Config/CompositeReader.php
@@ -11,28 +11,36 @@ namespace Magento\WebapiAsync\Plugin\Communication\Config;
 use Magento\AsynchronousOperations\Model\ConfigInterface;
 use Magento\Framework\Communication\ConfigInterface as CommunicationConfig;
 
+/**
+ * Force change topic's defined as ServiceName:methodName from sync to async.
+ * Topic type will be changed if topic name starts with
+ * \Magento\AsynchronousOperations\Model\ConfigInterface::TOPIC_PREFIX
+ *
+ * @SuppressWarnings("unused")
+ */
 class CompositeReader
 {
     /**
-     * Topics with type schema is always are sync
-     * @see \Magento\Framework\Communication\Config\ReflectionGenerator::generateTopicConfigForServiceMethod().
-     * This plugin add support for topic type schema defined as ServiceName:methodName
-     * by force changing sync type to async
-     * but only if topic name starts with ConfigInterface::TOPIC_PREFIX, e.g. async.*
+     * Checking communication.xml merged configuration
+     * to find topics with names started from "async"
+     * \Magento\AsynchronousOperations\Model\ConfigInterface::TOPIC_PREFIX
+     * and change config attribute value "is_synchronous" to false (make async)
+     * for this topics.
      *
      * @param \Magento\Framework\Communication\Config\CompositeReader $subject
      * @param array $result
      *
      * @return array
-     * @SuppressWarnings(PHPMD.UnusedLocalVariable)
      */
-    public function afterRead(\Magento\Framework\Communication\Config\CompositeReader $subject, array $result) {
+    public function afterRead(\Magento\Framework\Communication\Config\CompositeReader $subject, array $result)
+    {
         foreach ($result[CommunicationConfig::TOPICS] as $topicName => $topicConfig) {
             if (strpos($topicName, ConfigInterface::DEFAULT_HANDLER_NAME) === 0) {
                 $topicConfig[CommunicationConfig::TOPIC_IS_SYNCHRONOUS] = false;
                 $result[CommunicationConfig::TOPICS][$topicName] = $topicConfig;
             }
         }
+
         return $result;
     }
 }
