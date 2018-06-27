2a3c1ad7c63d79d0b01683df93697de82a400260
Eugene Tulika <vranen@gmail.com>
2018-06-27 11:44:22 -0500

    magento/bulk-api-ce#12: Operations Status Search API Endpoint

--- dev/tests/integration/testsuite/Magento/AsynchronousOperations/_files/operation_searchable_rollback.php
+++ dev/tests/integration/testsuite/Magento/AsynchronousOperations/_files/operation_searchable_rollback.php
@@ -3,5 +3,8 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+
+declare(strict_types=1);
+
 // when bulks are removed, acknowledged bulk table will be cleared too.
 require __DIR__ . '/bulk_rollback.php';
