7c3cb0e7cd7f6fa99f79dea3208840d126524884
nmalevanec <mikola.malevanec@transoftgroup.com>
2018-06-27 16:33:58 +0300

    MSI-1372  When you set 'Manage Inventory' to No it will still remember the Source levels and show it in the grid

--- app/code/Magento/InventorySalesAdminUi/Model/GetSalableQuantityDataBySku.php
+++ app/code/Magento/InventorySalesAdminUi/Model/GetSalableQuantityDataBySku.php
@@ -37,7 +37,6 @@ class GetSalableQuantityDataBySku
      */
     private $getStockItemConfiguration;
 
-
     /**
      * @param GetProductSalableQtyInterface $getProductSalableQty
      * @param StockRepositoryInterface $stockRepository
