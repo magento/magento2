e0d2e55c7e3a60957b6355e944a021a88803093a
Vishal Gelani <vishalgelani99@gmail.com>
2018-06-27 12:36:33 +0530

    Update billing-address.js

--- app/code/Magento/Checkout/view/frontend/web/js/view/billing-address.js
+++ app/code/Magento/Checkout/view/frontend/web/js/view/billing-address.js
@@ -102,6 +102,7 @@ function (
                     this.saveInAddressBook(1);
                 }
                 this.isAddressDetailsVisible(true);
+                
                 if (!this.canUseShippingAddress()) {
                     this.isAddressDetailsVisible(false);
                 }
