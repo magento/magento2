04d520e6bff721e68c9d04dc8aeb040013cf940a
Andrii Lugovyi <alugovyi@magento.com>
2018-06-27 10:28:00 +0300

    MAGETWO-91798: Implement GroupedProduct Price indexer

--- app/code/Magento/GroupedProduct/Model/ResourceModel/Product/Indexer/Price/Grouped.php
+++ app/code/Magento/GroupedProduct/Model/ResourceModel/Product/Indexer/Price/Grouped.php
@@ -11,7 +11,6 @@ use Magento\Catalog\Api\Data\ProductInterface;
 use Magento\Eav\Model\Config;
 use Magento\Framework\App\ResourceConnection;
 use Magento\Framework\DB\Adapter\AdapterInterface;
-use Magento\Framework\Event\ManagerInterface;
 use Magento\Framework\Indexer\DimensionalIndexerInterface;
 use Magento\Framework\EntityManager\MetadataPool;
 use Magento\Catalog\Model\Indexer\Product\Price\TableMaintainer;
@@ -31,11 +30,6 @@ use Magento\Customer\Model\Indexer\CustomerGroupDimensionProvider;
  */
 class Grouped implements DimensionalIndexerInterface
 {
-    /**
-     * Prefix for temporary table support.
-     */
-    const TRANSIT_PREFIX = 'transit_';
-
     /**
      * @var BaseFinalPrice
      */
@@ -154,32 +148,10 @@ class Grouped implements DimensionalIndexerInterface
             'maxPriceField' => 'max_price',
             'tierPriceField' => 'tier_price',
         ]);
-        $this->fillFinalPrice($dimensions, $entityIds, $temporaryPriceTable);
-        $this->applyPriceModifiers($temporaryPriceTable);
 
-//        if (!$this->tableStrategy->getUseIdxTable()) {
-//            $additionalIdxTable = $this->cteateTempTable($temporaryPriceTable);
-//            $this->fillTemporaryTable($entityIds, $additionalIdxTable);
-//            $this->updateIdxTable($additionalIdxTable, $temporaryPriceTable->getTableName());
-//            $this->connection->dropTemporaryTable($additionalIdxTable);
-//        } else {
-            $query = $this->_prepareGroupedProductPriceDataSelect($dimensions, iterator_to_array($entityIds))
-                ->insertFromSelect($temporaryPriceTable->getTableName());
-            $this->connection->query($query);
-//        }
-    }
-
-    /**
-     * Apply price modifiers to temporary price index table
-     *
-     * @param IndexTableStructure $temporaryPriceTable
-     * @return void
-     */
-    private function applyPriceModifiers(IndexTableStructure $temporaryPriceTable)
-    {
-        foreach ($this->priceModifiers as $priceModifier) {
-            $priceModifier->modifyPrice($temporaryPriceTable);
-        }
+        $query = $this->_prepareGroupedProductPriceDataSelect($dimensions, iterator_to_array($entityIds))
+            ->insertFromSelect($temporaryPriceTable->getTableName());
+        $this->connection->query($query);
     }
 
     /**
@@ -254,108 +226,8 @@ class Grouped implements DimensionalIndexerInterface
         return $select;
     }
 
-    /**
-     * Add website data join to select
-     * If add default store join also limitation of only has default store website
-     * Joined table has aliases
-     *  cw for website table,
-     *  csg for store group table (joined by website default group)
-     *  cs for store table (joined by website default store)
-     *
-     * @param \Magento\Framework\DB\Select $select the select object
-     * @param bool $store add default store join
-     * @param string|\Zend_Db_Expr $joinCondition the limitation for website_id
-     * @return $this
-     */
-    protected function _addWebsiteJoinToSelect($select, $store = true, $joinCondition = null)
-    {
-        if ($joinCondition !== null) {
-            $joinCondition = 'cw.website_id = ' . $joinCondition;
-        }
-
-        $select->join(['cw' => $this->getTable('store_website')], $joinCondition, []);
-
-        if ($store) {
-            $select->join(
-                ['csg' => $this->getTable('store_group')],
-                'csg.group_id = cw.default_group_id',
-                []
-            )->join(
-                ['cs' => $this->getTable('store')],
-                'cs.store_id = csg.default_store_id',
-                []
-            );
-        }
-
-        return $this;
-    }
-
     private function getTable($tableName)
     {
         return $this->resource->getTableName($tableName, $this->connectionName);
-    }
-
-    /**
-     * @param array $dimensions
-     * @param \Traversable $entityIds
-     * @param $temporaryPriceTable
-     */
-    private function fillFinalPrice(array $dimensions, \Traversable $entityIds, $temporaryPriceTable): void
-    {
-        $select = $this->baseFinalPrice->getQuery(
-            $dimensions,
-            GroupedType::TYPE_CODE,
-            iterator_to_array($entityIds)
-        );
-        $query = $select->insertFromSelect($temporaryPriceTable->getTableName(), [], false);
-        $this->tableMaintainer->getConnection()->query($query);
-    }
-     
-    /**
-     * @param $table
-     * @return mixed
-     */
-    private function cteateTempTable($table)
-    {
-
-        $temporaryOptionsTableName = 'catalog_product_index_price_cfg_opt_temp';
-        $this->connection->createTemporaryTableLike(
-            $temporaryOptionsTableName,
-            $this->getTable('catalog_product_index_price_cfg_opt_tmp'),
-            true
-        );
-        $additionalIdxTable = $this->connection->getTableName(self::TRANSIT_PREFIX . $this->getIdxTable());
-        $this->connection->createTemporaryTableLike($additionalIdxTable, $table);
-        return $additionalIdxTable;
-    }
-
-    /**
-     * @param $entityIds
-     * @param $additionalIdxTable
-     */
-    private function fillTemporaryTable($entityIds, $additionalIdxTable)
-    {
-        $query = $this->connection->insertFromSelect(
-            $this->_prepareGroupedProductPriceDataSelect($entityIds),
-            $additionalIdxTable,
-            []
-        );
-        $this->connection->query($query);
-    }
-
-    /**
-     * @param $additionalIdxTable
-     * @param $table
-     */
-    private function updateIdxTable($additionalIdxTable, $table): void
-    {
-        $select = $this->connection->select()->from($additionalIdxTable);
-        $query = $this->connection->insertFromSelect(
-            $select,
-            $table,
-            [],
-            AdapterInterface::INSERT_ON_DUPLICATE
-        );
-        $this->connection->query($query);
     }
 }
