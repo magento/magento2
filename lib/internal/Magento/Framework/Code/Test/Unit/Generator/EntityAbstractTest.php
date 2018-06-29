0083bd399f34b90e6a4fa549d246f0a0ad3ff183
Yevhen Miroshnychenko <ymiroshnychenko@magento.com>
2018-06-29 11:15:55 +0300

    MAGETWO-92986: Write Logs for Failed Process of Generating Factories in Extensions

--- lib/internal/Magento/Framework/Code/Test/Unit/Generator/EntityAbstractTest.php
+++ lib/internal/Magento/Framework/Code/Test/Unit/Generator/EntityAbstractTest.php
@@ -222,8 +222,8 @@ class EntityAbstractTest extends \PHPUnit\Framework\TestCase
             . 'Due to the current error, executing the CLI commands `bin/magento setup:di:compile` or `bin/magento '
             . 'deploy:mode:set production` does not create the required generated classes. '
             . 'Magento cannot write a class file to the "generated" directory that is read-only. Before using the '
-            . 'read-only file system, the classes to be generated must be created beforehand in the "generated" directory. '
-            . 'For details, see the "File systems access permissions" topic at http://devdocs.magento.com.';
+            . 'read-only file system, the classes to be generated must be created beforehand in the "generated" '
+            . 'directory. For details, see the "File systems access permissions" topic at http://devdocs.magento.com.';
 
         $exceptionMessage = 'Some description';
 
