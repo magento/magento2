a77bdfc7e165ecdeda9d6cf8999d2f717fdd1cb2
Jisse Reitsma <jisse@yireo.com>
2018-06-29 16:25:27 +0200

    Update Application.php

--- dev/tests/integration/framework/Magento/TestFramework/Application.php
+++ dev/tests/integration/framework/Magento/TestFramework/Application.php
@@ -516,7 +516,7 @@ class Application
      */
     private function copyGlobalConfigFile()
     {
-        $targetFile = $this->_configDir . '/config.php';
+        $targetFile = $this->_configDir . '/config.local.php';
         copy($this->globalConfigFile, $targetFile);
     }
 
