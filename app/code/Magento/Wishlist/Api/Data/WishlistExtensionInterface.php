9382616d2c12b11bcfc2cf80da96912075f24c82
Wietze Mink <wietze@tickles.nl>
2018-06-30 23:20:54 +0200

    Add extension interface for wishlist

---
+++ app/code/Magento/Wishlist/Api/Data/WishlistExtensionInterface.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Wishlist\Api\Data;
+
+interface WishlistExtensionInterface extends \Magento\Framework\Api\ExtensionAttributesInterface
+{
+
+}
