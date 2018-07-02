24448d01e8afbf9cc698b32fad1c4c6a1446527a
Cristian Partica <cpartica@magento.com>
2018-07-02 10:15:35 -0500

    MAGETWO-8709: [GITHUB] Child product image should be shown in Wishist if options are selected for configurable product #8168
    - relying on item from cart as it has a setting in the admin that wishlist should have it too

--- app/code/Magento/Wishlist/Block/Customer/Wishlist/Item/Column/Image.php
+++ app/code/Magento/Wishlist/Block/Customer/Wishlist/Item/Column/Image.php
@@ -11,10 +11,67 @@
  */
 namespace Magento\Wishlist\Block\Customer\Wishlist\Item\Column;
 
+use Magento\Catalog\Model\Product\Image\UrlBuilder;
+use Magento\Framework\View\ConfigInterface;
+use Magento\Framework\App\ObjectManager;
+use Magento\Quote\Model\Quote\ItemFactory;
+use Magento\Checkout\Block\Cart\Item\Renderer;
+
 /**
  * @api
  * @since 100.0.2
  */
 class Image extends \Magento\Wishlist\Block\Customer\Wishlist\Item\Column
 {
+    /** @var \Magento\Checkout\Block\Cart\Item\Renderer[] */
+    private $renderers = [];
+
+    /** @var \Magento\Quote\Model\Quote\ItemFactory  */
+    private $itemFactory;
+
+    /**
+     * @param \Magento\Catalog\Block\Product\Context $context
+     * @param \Magento\Framework\App\Http\Context $httpContext
+     * @param array $data
+     * @param ConfigInterface|null $config
+     * @param UrlBuilder|null $urlBuilder
+     * @param Renderer[] $renderers
+     * @param ItemFactory|null $itemFactory
+     */
+    public function __construct(
+        \Magento\Catalog\Block\Product\Context $context,
+        \Magento\Framework\App\Http\Context $httpContext,
+        array $data = [],
+        ConfigInterface $config = null,
+        UrlBuilder $urlBuilder = null,
+        array $renderers = [],
+        ItemFactory $itemFactory = null
+    ) {
+        $this->renderers = $renderers;
+        $this->itemFactory = $itemFactory ?? ObjectManager::getInstance()->get(ItemFactory::class);
+        parent::__construct(
+            $context,
+            $httpContext,
+            $data,
+            $config,
+            $urlBuilder
+        );
+    }
+
+    /**
+     * Identify the product from which thumbnail should be taken.
+     *
+     * @return \Magento\Catalog\Model\Product
+     */
+    public function getProductForThumbnail(\Magento\Wishlist\Model\Item $item)
+    {
+        $product = $product = $item->getProduct();
+        if (isset($this->renderers[$product->getTypeId()])) {
+            $quoteItem = $this->itemFactory->create(['data' => $item->getData()]);
+            $quoteItem->setProduct($product);
+            $quoteItem->setOptions($item->getOptions());
+            return $this->renderers[$product->getTypeId()]->setItem($quoteItem)->getProductForThumbnail();
+        }
+        return $product;
+    }
 }
