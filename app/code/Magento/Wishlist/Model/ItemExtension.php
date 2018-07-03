31bab29756ae6ff45c6aa592a24f41bec4370cd5
Wietze Mink <wietze@tickles.nl>
2018-07-03 07:56:24 +0200

    Add preferences

---
+++ app/code/Magento/Wishlist/Model/ItemExtension.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Wishlist\Model;
+
+use Magento\Wishlist\Api\Data\ItemExtensionInterface;
+
+class ItemExtension extends \Magento\Framework\Api\AbstractSimpleObject implements ItemExtensionInterface
+{
+
+}
