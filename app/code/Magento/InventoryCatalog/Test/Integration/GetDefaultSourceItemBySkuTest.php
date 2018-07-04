50b4d73999f6d5cc387ec2d35eebc70a036f99cc
Riccardo Tempesta <riccardo.tempesta@magespecialist.it>
2018-07-04 14:21:19 +0200

    Removed source_items_on_mixed_sources.php fixture

--- app/code/Magento/InventoryCatalog/Test/Integration/GetDefaultSourceItemBySkuTest.php
+++ app/code/Magento/InventoryCatalog/Test/Integration/GetDefaultSourceItemBySkuTest.php
@@ -27,7 +27,7 @@ class GetDefaultSourceItemBySkuTest extends TestCase
     /**
      * @magentoDataFixture ../../../../app/code/Magento/InventoryApi/Test/_files/sources.php
      * @magentoDataFixture ../../../../app/code/Magento/InventoryApi/Test/_files/products.php
-     * @magentoDataFixture ../../../../app/code/Magento/InventoryCatalog/Test/_files/source_items_on_mixed_sources.php
+     * @magentoDataFixture ../../../../app/code/Magento/InventoryCatalog/Test/_files/source_items_on_default_source.php
      */
     public function testExecute()
     {
@@ -38,10 +38,6 @@ class GetDefaultSourceItemBySkuTest extends TestCase
         self::assertNotNull(
             $this->getDefaultSourceItemBySku->execute('SKU-2'),
             'Unable to find default source_item for a product assigned to Default source and others'
-        );
-        self::assertNull(
-            $this->getDefaultSourceItemBySku->execute('SKU-3'),
-            'Returned a wrong default source_item for a product assigned elsewhere'
         );
     }
 }
