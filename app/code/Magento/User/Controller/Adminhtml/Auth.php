b94ba641921badf7371383c404bdf7b1621350a3
Anshu Mishra <mishra.anshu1710@gmail.com>
2018-07-06 10:07:39 +0530

    Changes as per comments on Github

--- app/code/Magento/User/Controller/Adminhtml/Auth.php
+++ app/code/Magento/User/Controller/Adminhtml/Auth.php
@@ -8,11 +8,16 @@ namespace Magento\User\Controller\Adminhtml;
 
 use Magento\Framework\Encryption\Helper\Security;
 use Magento\Framework\App\ObjectManager;
+use Magento\Backend\App\AbstractAction;
+use Magento\Backend\App\Action\Context;
+use Magento\User\Model\UserFactory;
+use Magento\Backend\Helper\Data;
+use Magento\Framework\Exception\LocalizedException;
 
 /**
  * \Magento\User Auth controller
  */
-abstract class Auth extends \Magento\Backend\App\AbstractAction
+abstract class Auth extends AbstractAction
 {
     /**
      * User model factory
@@ -24,24 +29,23 @@ abstract class Auth extends \Magento\Backend\App\AbstractAction
     /**
      * @var \Magento\Backend\Helper\Data
      */
-    protected $_backendHelper;
+    protected $_backendDataHelper;
 
     /**
      * Construct
      *
-     * @param \Magento\Backend\App\Action\Context $context
-     * @param \Magento\User\Model\UserFactory $userFactory
-     * @param \Magento\Backend\Helper\Data $backendHelper
+     * @param Context $context
+     * @param UserFactory $userFactory
+     * @param Data $backendDataHelper
      */
     public function __construct(
-        \Magento\Backend\App\Action\Context $context,
-        \Magento\User\Model\UserFactory $userFactory,
-        \Magento\Backend\Helper\Data $backendHelper = null
+        Context $context,
+        UserFactory $userFactory,
+        Data $backendDataHelper = null
     ) {
         parent::__construct($context);
         $this->_userFactory = $userFactory;
-        $this->_backendHelper = $backendHelper ?:
-                ObjectManager::getInstance()->get(\Magento\Backend\Helper\Data::class);
+        $this->_backendDataHelper = $backendDataHelper ?: ObjectManager::getInstance()->get(Data::class);
     }
 
     /**
@@ -50,7 +54,7 @@ abstract class Auth extends \Magento\Backend\App\AbstractAction
      * @param int $userId
      * @param string $resetPasswordToken
      * @return void
-     * @throws \Magento\Framework\Exception\LocalizedException
+     * @throws LocalizedException
      */
     protected function _validateResetPasswordLinkToken($userId, $resetPasswordToken)
     {
@@ -60,22 +64,20 @@ abstract class Auth extends \Magento\Backend\App\AbstractAction
             $resetPasswordToken
         ) || empty($resetPasswordToken) || empty($userId) || $userId < 0
         ) {
-            throw new \Magento\Framework\Exception\LocalizedException(
-                __('The password reset token is incorrect. Verify the token and try again.')
-            );
+            throw new LocalizedException(__('Please correct the password reset token.'));
         }
 
         /** @var $user \Magento\User\Model\User */
         $user = $this->_userFactory->create()->load($userId);
         if (!$user->getId()) {
-            throw new \Magento\Framework\Exception\LocalizedException(
+            throw new LocalizedException(
                 __('Please specify the correct account and try again.')
             );
         }
 
         $userToken = $user->getRpToken();
         if (!Security::compareStrings($userToken, $resetPasswordToken) || $user->isResetPasswordLinkTokenExpired()) {
-            throw new \Magento\Framework\Exception\LocalizedException(__('Your password reset link has expired.'));
+            throw new LocalizedException(__('Your password reset link has expired.'));
         }
     }
 
