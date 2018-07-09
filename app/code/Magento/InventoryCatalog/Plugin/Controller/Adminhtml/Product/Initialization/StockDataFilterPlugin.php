0a44bb4f766696b5718bb297ef0a5d08c67463aa
Alessandro Ronchi <aleron75@gmail.com>
2018-07-09 22:58:12 +0200

    Allow negative min_qty
    - Add around plugin on \Magento\CatalogInventory\Model\System\Config\Backend\Minqty::beforeSave() to allow min_qty to be assigned a value below 0
    - Add around plugin on \Magento\Catalog\Controller\Adminhtml\Product\Initialization\StockDataFilter::filter() to allow min_qty to be assigned a value below 0
    - Add around plugin on \Magento\CatalogInventory\Api\Data\StockItemInterface::getMinQty() to correctly calculate min qty value
    - Update salable conditions related to backorders in resource model of InventorySales

---
+++ app/code/Magento/InventoryCatalog/Plugin/Controller/Adminhtml/Product/Initialization/StockDataFilterPlugin.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\InventoryCatalog\Plugin\Controller\Adminhtml\Product\Initialization;
+
+use Magento\Catalog\Controller\Adminhtml\Product\Initialization\StockDataFilter;
+
+class StockDataFilterPlugin
+{
+    /**
+     * Allow min_qty to be assigned a value below 0.
+     * @param StockDataFilter $subject
+     * @param callable $proceed
+     * @param array $stockData
+     * @return mixed
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function aroundFilter(
+        StockDataFilter $subject,
+        callable $proceed,
+        array $stockData
+    ) {
+        $originalStockData = $proceed($stockData);
+        $originalStockData['min_qty'] = $stockData['min_qty'];
+        return $originalStockData;
+    }
+}
