b8c32188fb101be81aeccc4612aa3787ff65e059
Roman Leshchenko <rleshchenko@magento.com>
2018-07-11 18:11:05 +0300

    MAGETWO-88005: Added necessary logic to action class for cookie messages behavior fix

--- app/code/Magento/Customer/Controller/Account/EditPost.php
+++ app/code/Magento/Customer/Controller/Account/EditPost.php
@@ -83,7 +83,7 @@ class EditPost extends \Magento\Customer\Controller\AbstractAccount
      * @param CustomerRepositoryInterface $customerRepository
      * @param Validator $formKeyValidator
      * @param CustomerExtractor $customerExtractor
-     * @param Escaper $escaper|null
+     * @param Escaper|null $escaper
      */
     public function __construct(
         Context $context,
