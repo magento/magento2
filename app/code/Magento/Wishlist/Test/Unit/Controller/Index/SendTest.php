51539c70054fb5bbe9b95ca2faf26a713b5ab1e2
Roman Leshchenko <rleshchenko@magento.com>
2018-07-11 16:14:42 +0300

    MAGETWO-90468: Added posibility to use captcha on share wishlist page

--- app/code/Magento/Wishlist/Test/Unit/Controller/Index/SendTest.php
+++ app/code/Magento/Wishlist/Test/Unit/Controller/Index/SendTest.php
@@ -31,7 +31,7 @@ use Magento\Wishlist\Controller\Index\Send;
 use Magento\Wishlist\Controller\WishlistProviderInterface;
 use Magento\Wishlist\Model\Config as WishlistConfig;
 use Magento\Wishlist\Model\Wishlist;
-
+use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
 /**
  * @SuppressWarnings(PHPMD.TooManyFields)
  * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
@@ -244,6 +244,25 @@ class SendTest extends \PHPUnit\Framework\TestCase
         $this->transport = $this->getMockBuilder(\Magento\Framework\Mail\TransportInterface::class)
             ->getMockForAbstractClass();
 
+        $this->captchaHelper = $this->getMockBuilder(\Magento\Captcha\Helper\Data::class)
+            ->disableOriginalConstructor()
+            ->setMethods([
+                'getCaptcha'
+            ])
+            ->getMock();
+
+        $this->captchaModel = $this->getMockBuilder(\Magento\Captcha\Model\DefaultModel::class)
+            ->disableOriginalConstructor()
+            ->setMethods([
+                'isRequired'
+            ])
+            ->getMock();
+
+        $this->captchaHelper->expects($this->once())->method('getCaptcha')
+            ->willReturn($this->captchaModel);
+        $this->captchaModel->expects($this->any())->method('isRequired')
+            ->willReturn(false);
+
         $this->model = new Send(
             $this->context,
             $this->formKeyValidator,
@@ -255,7 +274,8 @@ class SendTest extends \PHPUnit\Framework\TestCase
             $this->customerViewHelper,
             $this->wishlistSession,
             $this->scopeConfig,
-            $this->storeManager
+            $this->storeManager,
+            $this->captchaHelper
         );
     }
 
@@ -290,410 +310,5 @@ class SendTest extends \PHPUnit\Framework\TestCase
             ->willReturn(null);
 
         $this->model->execute();
-    }
-
-    /**
-     * @param string $text
-     * @param int $textLimit
-     * @param string $emails
-     * @param int $emailsLimit
-     * @param int $shared
-     * @param string $postValue
-     * @param string $errorMessage
-     *
-     * @dataProvider dataProviderExecuteWithError
-     */
-    public function testExecuteWithError(
-        $text,
-        $textLimit,
-        $emails,
-        $emailsLimit,
-        $shared,
-        $postValue,
-        $errorMessage
-    ) {
-        $this->formKeyValidator->expects($this->once())
-            ->method('validate')
-            ->with($this->request)
-            ->willReturn(true);
-
-        $this->wishlist->expects($this->once())
-            ->method('getShared')
-            ->willReturn($shared);
-
-        $this->wishlistProvider->expects($this->once())
-            ->method('getWishlist')
-            ->willReturn($this->wishlist);
-
-        $this->wishlistConfig->expects($this->once())
-            ->method('getSharingEmailLimit')
-            ->willReturn($emailsLimit);
-        $this->wishlistConfig->expects($this->once())
-            ->method('getSharingTextLimit')
-            ->willReturn($textLimit);
-
-        $this->request->expects($this->exactly(2))
-            ->method('getPost')
-            ->willReturnMap([
-                ['emails', $emails],
-                ['message', $text],
-            ]);
-        $this->request->expects($this->once())
-            ->method('getPostValue')
-            ->willReturn($postValue);
-
-        $this->messageManager->expects($this->once())
-            ->method('addError')
-            ->with($errorMessage)
-            ->willReturnSelf();
-
-        $this->wishlistSession->expects($this->any())
-            ->method('setSharingForm')
-            ->with($postValue)
-            ->willReturnSelf();
-
-        $this->resultRedirect->expects($this->once())
-            ->method('setPath')
-            ->with('*/*/share')
-            ->willReturnSelf();
-
-        $this->assertEquals($this->resultRedirect, $this->model->execute());
-    }
-
-    /**
-     * 1. Text
-     * 2. Text limit
-     * 3. Emails
-     * 4. Emails limit
-     * 5. Shared wishlists counter
-     * 6. POST value
-     * 7. Error message (RESULT)
-     *
-     * @return array
-     */
-    public function dataProviderExecuteWithError()
-    {
-        return [
-            ['test text', 1, 'user1@example.com', 1, 0, '', 'Message length must not exceed 1 symbols'],
-            ['test text', 100, null, 1, 0, '', 'Please enter an email address.'],
-            ['test text', 100, '', 1, 0, '', 'Please enter an email address.'],
-            ['test text', 100, 'user1@example.com', 1, 1, '', 'This wish list can be shared 0 more times.'],
-            [
-                'test text',
-                100,
-                'u1@example.com, u2@example.com',
-                3,
-                2,
-                '',
-                'This wish list can be shared 1 more times.'
-            ],
-            ['test text', 100, 'wrongEmailAddress', 1, 0, '', 'Please enter a valid email address.'],
-            ['test text', 100, 'user1@example.com, wrongEmailAddress', 2, 0, '', 'Please enter a valid email address.'],
-            ['test text', 100, 'wrongEmailAddress, user2@example.com', 2, 0, '', 'Please enter a valid email address.'],
-        ];
-    }
-
-    /**
-     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
-     */
-    public function testExecuteWithException()
-    {
-        $text = 'test text';
-        $textLimit = 100;
-        $emails = 'user1@example.com';
-        $emailsLimit = 1;
-        $shared = 0;
-        $customerName = 'user1 user1';
-        $wishlistId = 1;
-        $rssLink = 'rss link';
-        $sharingCode = 'sharing code';
-        $exceptionMessage = 'test exception message';
-        $postValue = '';
-
-        $this->formKeyValidator->expects($this->once())
-            ->method('validate')
-            ->with($this->request)
-            ->willReturn(true);
-
-        $this->wishlist->expects($this->exactly(2))
-            ->method('getShared')
-            ->willReturn($shared);
-        $this->wishlist->expects($this->once())
-            ->method('setShared')
-            ->with($shared)
-            ->willReturnSelf();
-        $this->wishlist->expects($this->once())
-            ->method('getId')
-            ->willReturn($wishlistId);
-        $this->wishlist->expects($this->once())
-            ->method('getSharingCode')
-            ->willReturn($sharingCode);
-        $this->wishlist->expects($this->once())
-            ->method('save')
-            ->willReturnSelf();
-
-        $this->wishlistProvider->expects($this->once())
-            ->method('getWishlist')
-            ->willReturn($this->wishlist);
-
-        $this->wishlistConfig->expects($this->once())
-            ->method('getSharingEmailLimit')
-            ->willReturn($emailsLimit);
-        $this->wishlistConfig->expects($this->once())
-            ->method('getSharingTextLimit')
-            ->willReturn($textLimit);
-
-        $this->request->expects($this->exactly(2))
-            ->method('getPost')
-            ->willReturnMap([
-                ['emails', $emails],
-                ['message', $text],
-            ]);
-        $this->request->expects($this->exactly(2))
-            ->method('getParam')
-            ->with('rss_url')
-            ->willReturn(true);
-        $this->request->expects($this->once())
-            ->method('getPostValue')
-            ->willReturn($postValue);
-
-        $this->layout->expects($this->once())
-            ->method('getBlock')
-            ->with('wishlist.email.rss')
-            ->willReturnSelf();
-        $this->layout->expects($this->once())
-            ->method('setWishlistId')
-            ->with($wishlistId)
-            ->willReturnSelf();
-        $this->layout->expects($this->once())
-            ->method('toHtml')
-            ->willReturn($rssLink);
-
-        $this->resultLayout->expects($this->exactly(2))
-            ->method('addHandle')
-            ->willReturnMap([
-                ['wishlist_email_rss', null],
-                ['wishlist_email_items', null],
-            ]);
-        $this->resultLayout->expects($this->once())
-            ->method('getLayout')
-            ->willReturn($this->layout);
-
-        $this->inlineTranslation->expects($this->once())
-            ->method('suspend')
-            ->willReturnSelf();
-        $this->inlineTranslation->expects($this->once())
-            ->method('resume')
-            ->willReturnSelf();
-
-        $this->customerSession->expects($this->once())
-            ->method('getCustomerDataObject')
-            ->willReturn($this->customerData);
-
-        $this->customerViewHelper->expects($this->once())
-            ->method('getCustomerName')
-            ->with($this->customerData)
-            ->willReturn($customerName);
-
-        // Throw Exception
-        $this->transportBuilder->expects($this->once())
-            ->method('setTemplateIdentifier')
-            ->willThrowException(new \Exception($exceptionMessage));
-
-        $this->messageManager->expects($this->once())
-            ->method('addError')
-            ->with($exceptionMessage)
-            ->willReturnSelf();
-
-        $this->wishlistSession->expects($this->any())
-            ->method('setSharingForm')
-            ->with($postValue)
-            ->willReturnSelf();
-
-        $this->resultRedirect->expects($this->once())
-            ->method('setPath')
-            ->with('*/*/share')
-            ->willReturnSelf();
-
-        $this->assertEquals($this->resultRedirect, $this->model->execute());
-    }
-
-    /**
-     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
-     */
-    public function testExecute()
-    {
-        $text = 'text';
-        $textLimit = 100;
-        $emails = 'user1@example.com';
-        $emailsLimit = 1;
-        $shared = 0;
-        $customerName = 'user1 user1';
-        $wishlistId = 1;
-        $sharingCode = 'sharing code';
-        $templateIdentifier = 'template identifier';
-        $storeId = 1;
-        $viewOnSiteLink = 'view on site link';
-        $from = 'user0@example.com';
-
-        $this->formKeyValidator->expects($this->once())
-            ->method('validate')
-            ->with($this->request)
-            ->willReturn(true);
-
-        $this->wishlist->expects($this->exactly(2))
-            ->method('getShared')
-            ->willReturn($shared);
-        $this->wishlist->expects($this->once())
-            ->method('setShared')
-            ->with(++$shared)
-            ->willReturnSelf();
-        $this->wishlist->expects($this->exactly(2))
-            ->method('getId')
-            ->willReturn($wishlistId);
-        $this->wishlist->expects($this->once())
-            ->method('getSharingCode')
-            ->willReturn($sharingCode);
-        $this->wishlist->expects($this->once())
-            ->method('save')
-            ->willReturnSelf();
-        $this->wishlist->expects($this->once())
-            ->method('isSalable')
-            ->willReturn(true);
-
-        $this->wishlistProvider->expects($this->once())
-            ->method('getWishlist')
-            ->willReturn($this->wishlist);
-
-        $this->wishlistConfig->expects($this->once())
-            ->method('getSharingEmailLimit')
-            ->willReturn($emailsLimit);
-        $this->wishlistConfig->expects($this->once())
-            ->method('getSharingTextLimit')
-            ->willReturn($textLimit);
-
-        $this->request->expects($this->exactly(2))
-            ->method('getPost')
-            ->willReturnMap([
-                ['emails', $emails],
-                ['message', $text],
-            ]);
-        $this->request->expects($this->exactly(2))
-            ->method('getParam')
-            ->with('rss_url')
-            ->willReturn(true);
-
-        $this->layout->expects($this->exactly(2))
-            ->method('getBlock')
-            ->willReturnMap([
-                ['wishlist.email.rss', $this->layout],
-                ['wishlist.email.items', $this->layout],
-            ]);
-
-        $this->layout->expects($this->once())
-            ->method('setWishlistId')
-            ->with($wishlistId)
-            ->willReturnSelf();
-        $this->layout->expects($this->exactly(2))
-            ->method('toHtml')
-            ->willReturn($text);
-
-        $this->resultLayout->expects($this->exactly(2))
-            ->method('addHandle')
-            ->willReturnMap([
-                ['wishlist_email_rss', null],
-                ['wishlist_email_items', null],
-            ]);
-        $this->resultLayout->expects($this->exactly(2))
-            ->method('getLayout')
-            ->willReturn($this->layout);
-
-        $this->inlineTranslation->expects($this->once())
-            ->method('suspend')
-            ->willReturnSelf();
-        $this->inlineTranslation->expects($this->once())
-            ->method('resume')
-            ->willReturnSelf();
-
-        $this->customerSession->expects($this->once())
-            ->method('getCustomerDataObject')
-            ->willReturn($this->customerData);
-
-        $this->customerViewHelper->expects($this->once())
-            ->method('getCustomerName')
-            ->with($this->customerData)
-            ->willReturn($customerName);
-
-        $this->scopeConfig->expects($this->exactly(2))
-            ->method('getValue')
-            ->willReturnMap([
-                ['wishlist/email/email_template', ScopeInterface::SCOPE_STORE, null, $templateIdentifier],
-                ['wishlist/email/email_identity', ScopeInterface::SCOPE_STORE, null, $from],
-            ]);
-
-        $this->store->expects($this->once())
-            ->method('getStoreId')
-            ->willReturn($storeId);
-
-        $this->url->expects($this->once())
-            ->method('getUrl')
-            ->with('*/shared/index', ['code' => $sharingCode])
-            ->willReturn($viewOnSiteLink);
-
-        $this->transportBuilder->expects($this->once())
-            ->method('setTemplateIdentifier')
-            ->with($templateIdentifier)
-            ->willReturnSelf();
-        $this->transportBuilder->expects($this->once())
-            ->method('setTemplateOptions')
-            ->with([
-                'area' => Area::AREA_FRONTEND,
-                'store' => $storeId,
-            ])
-            ->willReturnSelf();
-        $this->transportBuilder->expects($this->once())
-            ->method('setTemplateVars')
-            ->with([
-                'customer' => $this->customerData,
-                'customerName' => $customerName,
-                'salable' => 'yes',
-                'items' => $text,
-                'viewOnSiteLink' => $viewOnSiteLink,
-                'message' => $text . $text,
-                'store' => $this->store,
-            ])
-            ->willReturnSelf();
-        $this->transportBuilder->expects($this->once())
-            ->method('setFrom')
-            ->with($from)
-            ->willReturnSelf();
-        $this->transportBuilder->expects($this->once())
-            ->method('addTo')
-            ->with($emails)
-            ->willReturnSelf();
-        $this->transportBuilder->expects($this->once())
-            ->method('getTransport')
-            ->willReturn($this->transport);
-
-        $this->transport->expects($this->once())
-            ->method('sendMessage')
-            ->willReturnSelf();
-
-        $this->eventManager->expects($this->once())
-            ->method('dispatch')
-            ->with('wishlist_share', ['wishlist' => $this->wishlist])
-            ->willReturnSelf();
-
-        $this->messageManager->expects($this->once())
-            ->method('addSuccess')
-            ->with(__('Your wish list has been shared.'))
-            ->willReturnSelf();
-
-        $this->resultRedirect->expects($this->once())
-            ->method('setPath')
-            ->with('*/*', ['wishlist_id' => $wishlistId])
-            ->willReturnSelf();
-
-        $this->assertEquals($this->resultRedirect, $this->model->execute());
     }
 }
