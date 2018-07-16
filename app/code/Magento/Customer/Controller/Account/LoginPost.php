8a988792be6ebac732947a276eb7fd112fa717ab
Glenn <jetaimemia@gmail.com>
2018-07-16 16:12:05 +0800

    Revert "Reduce lengthy code"
    This reverts commit 860eb101036ec350692e0d46bcf946195daa9357.

--- app/code/Magento/Customer/Controller/Account/LoginPost.php
+++ app/code/Magento/Customer/Controller/Account/LoginPost.php
@@ -167,30 +167,31 @@ class LoginPost extends \Magento\Customer\Controller\AbstractAccount
                     }
                 } catch (EmailNotConfirmedException $e) {
                     $value = $this->customerUrl->getEmailConfirmationUrl($login['username']);
-                    $messages[] = __(
+                    $message = __(
                         'This account is not confirmed. <a href="%1">Click here</a> to resend confirmation email.',
                         $value
                     );
+                    $this->messageManager->addError($message);
+                    $this->session->setUsername($login['username']);
                 } catch (UserLockedException $e) {
-                    $messages[] = __(
+                    $message = __(
                         'You did not sign in correctly or your account is temporarily disabled.'
                     );
+                    $this->messageManager->addError($message);
+                    $this->session->setUsername($login['username']);
                 } catch (AuthenticationException $e) {
-                    $messages[] = __('You did not sign in correctly or your account is temporarily disabled.');
+                    $message = __('You did not sign in correctly or your account is temporarily disabled.');
+                    $this->messageManager->addError($message);
+                    $this->session->setUsername($login['username']);
                 } catch (LocalizedException $e) {
-                    $messages[] = $e->getMessage();
+                    $message = $e->getMessage();
+                    $this->messageManager->addError($message);
+                    $this->session->setUsername($login['username']);
                 } catch (\Exception $e) {
                     // PA DSS violation: throwing or logging an exception here can disclose customer password
                     $this->messageManager->addError(
                         __('An unspecified error occurred. Please contact us for assistance.')
                     );
-                } finally {
-                    if (isset($messages)) {
-                        foreach ($messages as $message) {
-                            $this->messageManager->addError($message);
-                            $this->session->setUsername($login['username']);
-                        }
-                    }
                 }
             } else {
                 $this->messageManager->addError(__('A login and a password are required.'));
