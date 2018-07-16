085f89c0c894c79fdad430c3cf5b4d86e5c9ab8e
Marcel Hauri <marcel@hauri.me>
2018-07-16 08:42:03 +0200

    [task] add strval to obsolete methods

--- dev/tests/static/testsuite/Magento/Test/Legacy/_files/obsolete_methods.php
+++ dev/tests/static/testsuite/Magento/Test/Legacy/_files/obsolete_methods.php
@@ -2523,5 +2523,6 @@ return [
     ['_isAttributeValueEmpty', 'Magento\Catalog\Model\ResourceModel\AbstractResource'],
     ['var_dump', ''],
     ['each', ''],
+    ['strval', ''],
     ['isOrderIncrementIdUsed', 'Magento\Quote\Model\ResourceModel\Quote', 'Magento\Sales\Model\OrderIncrementIdChecker::isIncrementIdUsed']
 ];
