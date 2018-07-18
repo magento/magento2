e1f53d45065a6fa292e2213b72a2742f81197a0b
vitaliyboyko <v.boyko@atwix.com>
2018-07-18 06:50:46 +0000

    graphql-ce-120: added store configs schema and resolver

---
+++ app/code/Magento/StoreGraphQl/Model/Resolver/Store/StoreConfigsDataProvider.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\StoreGraphQl\Model\Resolver\Store;
+
+use Magento\Store\Api\Data\StoreConfigInterface;
+use Magento\Store\Api\StoreConfigManagerInterface;
+
+/**
+ * StoreConfig field data provider, used for GraphQL request processing.
+ */
+class StoreConfigsDataProvider
+{
+    /**
+     * @var StoreConfigManagerInterface
+     */
+    private $storeConfigManager;
+
+    /**
+     * @param StoreConfigManagerInterface $storeConfigManager
+     */
+    public function __construct(
+        StoreConfigManagerInterface $storeConfigManager
+    ) {
+        $this->storeConfigManager = $storeConfigManager;
+    }
+
+    /**
+     * Get store configs by store codes
+     *
+     * @param array $storeCodes
+     * @return array
+     */
+    public function getStoreConfigsByStoreCodes(array $storeCodes = null) : array
+    {
+        $storeConfigs = $this->storeConfigManager->getStoreConfigs($storeCodes);
+
+        return ['items' => $this->hidrateStoreConfigs($storeConfigs)];
+    }
+
+    /**
+     * Transform StoreConfig objects to in array format
+     *
+     * @param StoreConfigInterface[] $storeConfigs
+     * @return array
+     */
+    private function hidrateStoreConfigs(array $storeConfigs) : array
+    {
+        $storeConfigsData = [];
+        /** @var StoreConfigInterface $storeConfig */
+        foreach ($storeConfigs as $storeConfig) {
+            $storeConfigsData[] = [
+                'id' => $storeConfig->getId(),
+                'code' => $storeConfig->getCode(),
+                'website_id' => $storeConfig->getWebsiteId(),
+                'locale' => $storeConfig->getLocale(),
+                'base_currency_code' => $storeConfig->getBaseCurrencyCode(),
+                'default_display_currency_code' => $storeConfig->getDefaultDisplayCurrencyCode(),
+                'timezone' => $storeConfig->getTimezone(),
+                'weight_unit' => $storeConfig->getWeightUnit(),
+                'base_url' => $storeConfig->getBaseUrl(),
+                'base_link_url' => $storeConfig->getBaseLinkUrl(),
+                'base_static_url' => $storeConfig->getSecureBaseStaticUrl(),
+                'base_media_url' => $storeConfig->getBaseMediaUrl(),
+                'secure_base_link_url' => $storeConfig->getSecureBaseLinkUrl(),
+                'secure_base_static_url' => $storeConfig->getSecureBaseStaticUrl(),
+                'secure_base_media_url' => $storeConfig->getSecureBaseMediaUrl()
+            ];
+        }
+
+        return $storeConfigsData;
+    }
+}
---
+++ app/code/Magento/StoreGraphQl/Model/Resolver/StoreConfigs.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\StoreGraphQl\Model\Resolver;
+
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Exception\GraphQlInputException;
+use Magento\Framework\GraphQl\Query\Resolver\Value;
+use Magento\Framework\GraphQl\Query\Resolver\ValueFactory;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Magento\StoreGraphQl\Model\Resolver\Store\StoreConfigsDataProvider;
+
+/**
+ * StoreConfig page field resolver, used for GraphQL request processing.
+ */
+class StoreConfigs implements ResolverInterface
+{
+    /**
+     * @var StoreConfigsDataProvider
+     */
+    private $storeConfigsDataProvider;
+
+    /**
+     * @var ValueFactory
+     */
+    private $valueFactory;
+
+    /**
+     * @param StoreConfigsDataProvider $storeConfigsDataProvider
+     * @param ValueFactory $valueFactory
+     */
+    public function __construct(
+        StoreConfigsDataProvider $storeConfigsDataProvider,
+        ValueFactory $valueFactory
+    ) {
+        $this->valueFactory = $valueFactory;
+        $this->storeConfigsDataProvider = $storeConfigsDataProvider;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function resolve(
+        Field $field,
+        $context,
+        ResolveInfo $info,
+        array $value = null,
+        array $args = null
+    ) : Value {
+
+        $storeCodes = $this->getStoreCodes($args);
+        $storeConfigsData = $this->storeConfigsDataProvider->getStoreConfigsByStoreCodes($storeCodes);
+
+        $result = function () use ($storeConfigsData) {
+            return !empty($storeConfigsData) ? $storeConfigsData : [];
+        };
+
+        return $this->valueFactory->create($result);
+    }
+
+    /**
+     * Retrieve store codes
+     *
+     * @param array $args
+     * @return array
+     * @throws GraphQlInputException
+     */
+    private function getStoreCodes($args) : array
+    {
+        if (isset($args['store_codes'])) {
+            if (is_array($args['store_codes'])) {
+                return $args['store_codes'];
+            }
+            throw new GraphQlInputException(__('"store codes should contain a valid array'));
+        }
+
+        return null;
+    }
+}
