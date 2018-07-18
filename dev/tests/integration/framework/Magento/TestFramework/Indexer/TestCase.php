1c4e0ce74ef32cbfc06b4c19d567f1f7dfa2f060
Andrii Lugovyi <alugovyi@magento.com>
2018-07-18 11:04:32 +0300

    MAGETWO-92402: Write tests

--- dev/tests/integration/framework/Magento/TestFramework/Indexer/TestCase.php
+++ dev/tests/integration/framework/Magento/TestFramework/Indexer/TestCase.php
@@ -5,16 +5,50 @@
  */
 namespace Magento\TestFramework\Indexer;
 
+use Symfony\Component\Console\Tester\CommandTester;
+use Magento\TestFramework\App\Config;
+use Magento\TestFramework\Helper\Bootstrap;
+use Magento\Catalog\Console\Command\PriceIndexerDimensionsModeSetCommand as SetModeCommand;
+use Magento\Catalog\Model\Indexer\Product\Price\DimensionModeConfiguration;
+
 class TestCase extends \PHPUnit\Framework\TestCase
 {
+    private $setModeCommand;
+    private $objectManager;
+
     public static function tearDownAfterClass()
     {
-        $db = \Magento\TestFramework\Helper\Bootstrap::getInstance()->getBootstrap()
+        $db = Bootstrap::getInstance()->getBootstrap()
             ->getApplication()
             ->getDbInstance();
         if (!$db->isDbDumpExists()) {
             throw new \LogicException('DB dump does not exist.');
         }
         $db->restoreFromDbDump();
+    }
+
+    public static function setUpBeforeClass()
+    {
+        $db = Bootstrap::getInstance()->getBootstrap()
+            ->getApplication()
+            ->getDbInstance();
+        if (!$db->isDbDumpExists()) {
+            throw new \LogicException('DB dump does not exist.');
+        }
+        $db->restoreFromDbDump();
+
+        parent::setUpBeforeClass();
+    }
+
+    protected function setDimensionMode($mode = DimensionModeConfiguration::DIMENSION_WEBSITE_AND_CUSTOMER_GROUP)
+    {
+        $this->objectManager = Bootstrap::getObjectManager();
+        $this->objectManager->get(Config::class)->clean();
+
+        $command = $this->objectManager->create(SetModeCommand::class);
+        $this->setModeCommand = new CommandTester($command);
+        $this->setModeCommand->execute([SetModeCommand::INPUT_KEY_MODE => $mode]);
+
+        parent::setUp();
     }
 }
