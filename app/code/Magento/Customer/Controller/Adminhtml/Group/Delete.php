96d9bdd93257e53327e236a4a5053e996a03520d
Roman Leshchenko <rleshchenko@magento.com>
2018-07-18 17:24:59 +0300

    MAGETWO-92725: Incorrect group actions behavior

--- app/code/Magento/Customer/Controller/Adminhtml/Group/Delete.php
+++ app/code/Magento/Customer/Controller/Adminhtml/Group/Delete.php
@@ -7,6 +7,7 @@
 namespace Magento\Customer\Controller\Adminhtml\Group;
 
 use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\Exception\NotFoundException;
 
 class Delete extends \Magento\Customer\Controller\Adminhtml\Group
 {
@@ -14,9 +15,14 @@ class Delete extends \Magento\Customer\Controller\Adminhtml\Group
      * Delete customer group.
      *
      * @return \Magento\Backend\Model\View\Result\Redirect
+     * @throws NotFoundException
      */
     public function execute()
     {
+        if (!$this->getRequest()->isPost()) {
+            throw new NotFoundException(__('Page not found'));
+        }
+
         $id = $this->getRequest()->getParam('id');
         /** @var \Magento\Backend\Model\View\Result\Redirect $resultRedirect */
         $resultRedirect = $this->resultRedirectFactory->create();
