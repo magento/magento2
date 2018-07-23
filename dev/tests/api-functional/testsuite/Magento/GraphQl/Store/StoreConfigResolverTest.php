8f3c6e920b7bdb953c31013c843017a4d25e3ed6
vitaliyboyko <v.boyko@atwix.com>
2018-07-23 06:38:07 +0000

    graphql-ce-120: fixed api functional test for store config

--- dev/tests/api-functional/testsuite/Magento/GraphQl/Store/StoreConfigResolverTest.php
+++ dev/tests/api-functional/testsuite/Magento/GraphQl/Store/StoreConfigResolverTest.php
@@ -47,24 +47,22 @@ class StoreConfigResolverTest extends GraphQlAbstract
             = <<<QUERY
 {
   storeConfig{
-      items{
-        id,
-    	code,
-    	website_id,
-    	locale,
-    	base_currency_code,
-    	default_display_currency_code,
-    	timezone,
-    	weight_unit,
-    	base_url,
-    	base_link_url,
-    	base_static_url,
-    	base_media_url,
-    	secure_base_url,
-    	secure_base_link_url,
-    	secure_base_static_url,
-    	secure_base_media_url
-      }
+    id,
+    code,
+    website_id,
+    locale,
+    base_currency_code,
+    default_display_currency_code,
+    timezone,
+    weight_unit,
+    base_url,
+    base_link_url,
+    base_static_url,
+    base_media_url,
+    secure_base_url,
+    secure_base_link_url,
+    secure_base_static_url,
+    secure_base_media_url
   }
 }
 QUERY;
