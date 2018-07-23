c199ed0076b5ab65f6c7b52dcc58aba83790ac54
Myroslav Dobra <dmaraptor@gmail.com>
2018-07-23 10:57:37 +0300

    MAGETWO-86482: Customizable options truncated when displaying ordered product in admin

--- app/code/Magento/Sales/Block/Adminhtml/Items/Column/Name.php
+++ app/code/Magento/Sales/Block/Adminhtml/Items/Column/Name.php
@@ -3,7 +3,11 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types=1);
+
 namespace Magento\Sales\Block\Adminhtml\Items\Column;
+
+use Magento\Framework\Filter\TruncateFilter\Result;
 
 /**
  * Sales Order items name column renderer
@@ -14,6 +18,11 @@ namespace Magento\Sales\Block\Adminhtml\Items\Column;
 class Name extends \Magento\Sales\Block\Adminhtml\Items\Column\DefaultColumn
 {
     /**
+     * @var Result
+     */
+    private $truncateResult = null;
+
+    /**
      * Truncate string
      *
      * @param string $value
@@ -22,13 +31,20 @@ class Name extends \Magento\Sales\Block\Adminhtml\Items\Column\DefaultColumn
      * @param string &$remainder
      * @param bool $breakWords
      * @return string
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
-    public function truncateString($value, $length = 80, $etc = '...', &$remainder = '', $breakWords = true)
-    {
-        return $this->filterManager->truncate(
+    public function truncateString(
+        string $value,
+        int $length = 80,
+        string $etc = '...',
+        string &$remainder = '',
+        bool $breakWords = true
+    ): string {
+        $this->truncateResult = $this->filterManager->truncateFilter(
             $value,
-            ['length' => $length, 'etc' => $etc, 'remainder' => $remainder, 'breakWords' => $breakWords]
+            ['length' => $length, 'etc' => $etc, 'breakWords' => $breakWords]
         );
+        return $this->truncateResult->getValue();
     }
 
     /**
@@ -37,11 +53,14 @@ class Name extends \Magento\Sales\Block\Adminhtml\Items\Column\DefaultColumn
      * @param string $value
      * @return array
      */
-    public function getFormattedOption($value)
+    public function getFormattedOption(string $value): array
     {
         $remainder = '';
-        $value = $this->truncateString($value, 55, '', $remainder);
-        $result = ['value' => nl2br($value), 'remainder' => nl2br($remainder)];
+        $this->truncateString($value, 55, '', $remainder);
+        $result = [
+            'value' => nl2br($this->truncateResult->getValue()),
+            'remainder' => nl2br($this->truncateResult->getRemainder())
+        ];
 
         return $result;
     }
