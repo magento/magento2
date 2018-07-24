1eac6e823bcbb1abefe0dd51fea568203bef3e5c
Dmytro Horytskyi <dhorytskyi@magento.com>
2018-07-24 17:55:46 +0300

    MAGETWO-93668: Unable to use an attribute with a source model to generate simple products for a configurable

--- app/code/Magento/ConfigurableProduct/Model/Product/Type/Configurable.php
+++ app/code/Magento/ConfigurableProduct/Model/Product/Type/Configurable.php
@@ -444,7 +444,7 @@ class Configurable extends \Magento\Catalog\Model\Product\Type\AbstractType
      * Retrieve configurable attributes data
      *
      * @param  \Magento\Catalog\Model\Product $product
-     * @return \Magento\ConfigurableProduct\Model\ResourceModel\Product\Type\Configurable\Attribute\Collection
+     * @return \Magento\ConfigurableProduct\Model\Product\Type\Configurable\Attribute[]
      */
     public function getConfigurableAttributes($product)
     {
