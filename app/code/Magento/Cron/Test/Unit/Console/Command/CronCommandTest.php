863764eb31953555511d0a9b00787f6830e7b24c
Nadiya Syvokonenko <nsyvokonenko@magento.com>
2018-07-24 15:32:44 +0300

    MAGETWO-93701: Make cron:run CLI command react on cron disable configuration

--- app/code/Magento/Cron/Test/Unit/Console/Command/CronCommandTest.php
+++ app/code/Magento/Cron/Test/Unit/Console/Command/CronCommandTest.php
@@ -6,10 +6,29 @@
 namespace Magento\Cron\Test\Unit\Console\Command;
 
 use Magento\Cron\Console\Command\CronCommand;
+use Magento\Framework\App\DeploymentConfig;
+use Magento\Framework\App\ObjectManagerFactory;
+use PHPUnit_Framework_MockObject_MockObject as MockObject;
 use Symfony\Component\Console\Tester\CommandTester;
 
 class CronCommandTest extends \PHPUnit\Framework\TestCase
 {
+    /**
+     * @var ObjectManagerFactory|MockObject
+     */
+    private $objectManagerFactory;
+
+    /**
+     * @var DeploymentConfig|MockObject
+     */
+    private $deploymentConfigMock;
+
+    protected function setUp()
+    {
+        $this->objectManagerFactory = $this->createMock(ObjectManagerFactory::class);
+        $this->deploymentConfigMock = $this->createMock(DeploymentConfig::class);
+    }
+
     /**
      * Test command with disables cron
      *
@@ -17,16 +36,15 @@ class CronCommandTest extends \PHPUnit\Framework\TestCase
      */
     public function testExecuteWithDisabledCrons()
     {
-        $objectManagerFactory = $this->createMock(\Magento\Framework\App\ObjectManagerFactory::class);
-        $deploymentConfigMock = $this->createMock(\Magento\Framework\App\DeploymentConfig::class);
-
-        $objectManagerFactory->expects($this->never())
+        $this->objectManagerFactory->expects($this->never())
             ->method('create');
-        $deploymentConfigMock->expects($this->once())
+        $this->deploymentConfigMock->expects($this->once())
             ->method('get')
             ->with('cron/enabled', 1)
             ->willReturn(0);
-        $commandTester = new CommandTester(new CronCommand($objectManagerFactory, $deploymentConfigMock));
+        $commandTester = new CommandTester(
+            new CronCommand($this->objectManagerFactory, $this->deploymentConfigMock)
+        );
         $commandTester->execute([]);
         $expectedMsg = 'Cron is disabled. Jobs were not run.' . PHP_EOL;
         $this->assertEquals($expectedMsg, $commandTester->getDisplay());
@@ -39,8 +57,6 @@ class CronCommandTest extends \PHPUnit\Framework\TestCase
      */
     public function testExecute()
     {
-        $objectManagerFactory = $this->createMock(\Magento\Framework\App\ObjectManagerFactory::class);
-        $deploymentConfigMock = $this->createMock(\Magento\Framework\App\DeploymentConfig::class);
         $objectManager = $this->createMock(\Magento\Framework\ObjectManagerInterface::class);
         $cron = $this->createMock(\Magento\Framework\App\Cron::class);
         $objectManager->expects($this->once())
@@ -48,14 +64,16 @@ class CronCommandTest extends \PHPUnit\Framework\TestCase
             ->willReturn($cron);
         $cron->expects($this->once())
             ->method('launch');
-        $objectManagerFactory->expects($this->once())
+        $this->objectManagerFactory->expects($this->once())
             ->method('create')
             ->willReturn($objectManager);
-        $deploymentConfigMock->expects($this->once())
+        $this->deploymentConfigMock->expects($this->once())
             ->method('get')
             ->with('cron/enabled', 1)
             ->willReturn(1);
-        $commandTester = new CommandTester(new CronCommand($objectManagerFactory, $deploymentConfigMock));
+        $commandTester = new CommandTester(
+            new CronCommand($this->objectManagerFactory, $this->deploymentConfigMock)
+        );
         $commandTester->execute([]);
         $expectedMsg = 'Ran jobs by schedule.' . PHP_EOL;
         $this->assertEquals($expectedMsg, $commandTester->getDisplay());
