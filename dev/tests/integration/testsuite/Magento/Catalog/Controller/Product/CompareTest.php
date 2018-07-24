e7463c1b4044f9435651a38019b40af5cf8bb65e
Vishal Gelani <vishalgelani99@gmail.com>
2018-07-24 10:34:17 +0530

    Update CompareTest.php

--- dev/tests/integration/testsuite/Magento/Catalog/Controller/Product/CompareTest.php
+++ dev/tests/integration/testsuite/Magento/Catalog/Controller/Product/CompareTest.php
@@ -48,7 +48,12 @@ class CompareTest extends \Magento\TestFramework\TestCase\AbstractController
         );
 
         $this->assertSessionMessages(
-            $this->equalTo(['You added product Simple Product 1 Name to the <a href="http://localhost/index.php/catalog/product_compare/">comparison list</a>.']),
+            $this->equalTo(
+                [
+                    'You added product Simple Product 1 Name to the '.
+                    '<a href="http://localhost/index.php/catalog/product_compare/">comparison list</a>.'
+                ]
+            ),
             MessageInterface::TYPE_SUCCESS
         );
 
