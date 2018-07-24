cea4b8965cc0b254c39fe79b0349bbccde16bcbd
Tiago Sampaio <tiago@tiagosampaio.com>
2018-07-24 15:59:07 -0300

    Refactory to Magento_Backend module class.

--- app/code/Magento/Backend/Helper/Dashboard/Order.php
+++ app/code/Magento/Backend/Helper/Dashboard/Order.php
@@ -5,8 +5,6 @@
  */
 namespace Magento\Backend\Helper\Dashboard;
 
-use Magento\Framework\App\ObjectManager;
-
 /**
  * Adminhtml dashboard helper for orders
  *
@@ -18,13 +16,13 @@ class Order extends \Magento\Backend\Helper\Dashboard\AbstractDashboard
     /**
      * @var \Magento\Reports\Model\ResourceModel\Order\Collection
      */
-    protected $_orderCollection;
+    private $orderCollection;
 
     /**
      * @var \Magento\Store\Model\StoreManagerInterface
      * @since 100.0.6
      */
-    protected $_storeManager;
+    private $storeManager;
 
     /**
      * @param \Magento\Framework\App\Helper\Context $context
@@ -32,48 +30,38 @@ class Order extends \Magento\Backend\Helper\Dashboard\AbstractDashboard
      */
     public function __construct(
         \Magento\Framework\App\Helper\Context $context,
-        \Magento\Reports\Model\ResourceModel\Order\Collection $orderCollection
+        \Magento\Reports\Model\ResourceModel\Order\Collection $orderCollection,
+        \Magento\Store\Model\StoreManagerInterface $storeManager
     ) {
-        $this->_orderCollection = $orderCollection;
+        $this->orderCollection = $orderCollection;
+        $this->storeManager = $storeManager;
         parent::__construct($context);
     }
 
     /**
-     * The getter function to get the new StoreManager dependency
-     *
-     * @return \Magento\Store\Model\StoreManagerInterface
-     *
-     * @deprecated 100.1.0
-     */
-    private function getStoreManager()
-    {
-        if ($this->_storeManager === null) {
-            $this->_storeManager = ObjectManager::getInstance()->get(\Magento\Store\Model\StoreManagerInterface::class);
-        }
-        return $this->_storeManager;
-    }
-
-    /**
      * @return void
+     *
+     * @throws \Magento\Framework\Exception\LocalizedException
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
      */
     protected function _initCollection()
     {
         $isFilter = $this->getParam('store') || $this->getParam('website') || $this->getParam('group');
 
-        $this->_collection = $this->_orderCollection->prepareSummary($this->getParam('period'), 0, 0, $isFilter);
+        $this->_collection = $this->orderCollection->prepareSummary($this->getParam('period'), 0, 0, $isFilter);
 
         if ($this->getParam('store')) {
             $this->_collection->addFieldToFilter('store_id', $this->getParam('store'));
         } elseif ($this->getParam('website')) {
-            $storeIds = $this->getStoreManager()->getWebsite($this->getParam('website'))->getStoreIds();
+            $storeIds = $this->storeManager->getWebsite($this->getParam('website'))->getStoreIds();
             $this->_collection->addFieldToFilter('store_id', ['in' => implode(',', $storeIds)]);
         } elseif ($this->getParam('group')) {
-            $storeIds = $this->getStoreManager()->getGroup($this->getParam('group'))->getStoreIds();
+            $storeIds = $this->storeManager->getGroup($this->getParam('group'))->getStoreIds();
             $this->_collection->addFieldToFilter('store_id', ['in' => implode(',', $storeIds)]);
         } elseif (!$this->_collection->isLive()) {
             $this->_collection->addFieldToFilter(
                 'store_id',
-                ['eq' => $this->getStoreManager()->getStore(\Magento\Store\Model\Store::ADMIN_CODE)->getId()]
+                ['eq' => $this->storeManager->getStore(\Magento\Store\Model\Store::ADMIN_CODE)->getId()]
             );
         }
         $this->_collection->load();
