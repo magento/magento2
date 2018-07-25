0670ce320d7d47a6736fb50c0ef35865b6a6321b
mage2pratik <magepratik@gmail.com>
2018-07-25 21:28:09 +0530

    Remove commented code

--- app/code/Magento/Newsletter/Block/Adminhtml/Template/Edit.php
+++ app/code/Magento/Newsletter/Block/Adminhtml/Template/Edit.php
@@ -67,12 +67,6 @@ class Edit extends Widget
      */
     protected function _prepareLayout()
     {
-        // Load Wysiwyg on demand and Prepare layout
-//        $block = $this->getLayout()->getBlock('head');
-//        if ($this->_wysiwygConfig->isEnabled() && $block) {
-//            $block->setCanLoadTinyMce(true);
-//        }
-
         $this->getToolbar()->addChild(
             'back_button',
             \Magento\Backend\Block\Widget\Button::class,
