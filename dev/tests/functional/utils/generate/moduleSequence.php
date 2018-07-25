28683d4bbb895c04dbc7489695e61f55f66a32f2
KevinBKozan <kkozan@magento.com>
2018-07-25 14:10:32 -0500

    MQE-1112: Bump MFTF version in Magento
    - static fix for moduleSequence

--- dev/tests/functional/utils/generate/moduleSequence.php
+++ dev/tests/functional/utils/generate/moduleSequence.php
@@ -3,6 +3,9 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+
+declare(strict_types=1);
+
 require_once __DIR__ . '/../../../../../app/bootstrap.php';
 
 $magentoObjectManagerFactory = \Magento\Framework\App\Bootstrap::createObjectManagerFactory(BP, $_SERVER);
