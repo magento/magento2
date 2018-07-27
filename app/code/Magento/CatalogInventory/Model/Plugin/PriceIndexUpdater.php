492e8347ff5d354fb2ba93aa07ec9b2776fb4430
Andrii Lugovyi <alugovyi@magento.com>
2018-07-27 10:41:19 +0300

    MAGETWO-93743: [Forwardport] Base Implementation

--- app/code/Magento/CatalogInventory/Model/Plugin/PriceIndexUpdater.php
+++ app/code/Magento/CatalogInventory/Model/Plugin/PriceIndexUpdater.php
@@ -3,6 +3,8 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types=1);
+
 namespace Magento\CatalogInventory\Model\Plugin;
 
 use Magento\CatalogInventory\Model\ResourceModel\Stock\Item;
