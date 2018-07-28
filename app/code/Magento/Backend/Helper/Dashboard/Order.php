a1339ba5eba4af7705ccbbaa8c1b2b666f878219
Tiago Sampaio <tiago@tiagosampaio.com>
2018-07-28 10:56:50 -0300

    Reverting some chenges because of the contributor guide.

--- app/code/Magento/Backend/Helper/Dashboard/Order.php
+++ app/code/Magento/Backend/Helper/Dashboard/Order.php
@@ -5,36 +5,41 @@
  */
 namespace Magento\Backend\Helper\Dashboard;
 
+use Magento\Framework\App\ObjectManager;
+
 /**
  * Adminhtml dashboard helper for orders
  *
  * @api
  * @since 100.0.2
  */
-class Order extends \Magento\Backend\Helper\Dashboard\AbstractDashboard
+class Order extends AbstractDashboard
 {
     /**
      * @var \Magento\Reports\Model\ResourceModel\Order\Collection
      */
-    private $orderCollection;
+    private $_orderCollection;
 
     /**
      * @var \Magento\Store\Model\StoreManagerInterface
      * @since 100.0.6
      */
-    private $storeManager;
+    private $_storeManager;
 
     /**
      * @param \Magento\Framework\App\Helper\Context $context
      * @param \Magento\Reports\Model\ResourceModel\Order\Collection $orderCollection
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
      */
     public function __construct(
         \Magento\Framework\App\Helper\Context $context,
         \Magento\Reports\Model\ResourceModel\Order\Collection $orderCollection,
-        \Magento\Store\Model\StoreManagerInterface $storeManager
+        \Magento\Store\Model\StoreManagerInterface $storeManager = null
     ) {
-        $this->orderCollection = $orderCollection;
-        $this->storeManager = $storeManager;
+        $this->_orderCollection = $orderCollection;
+        $this->_storeManager = $storeManager ?: ObjectManager::getInstance()
+            ->get(\Magento\Store\Model\StoreManagerInterface::class);
+
         parent::__construct($context);
     }
 
@@ -48,20 +53,20 @@ class Order extends \Magento\Backend\Helper\Dashboard\AbstractDashboard
     {
         $isFilter = $this->getParam('store') || $this->getParam('website') || $this->getParam('group');
 
-        $this->_collection = $this->orderCollection->prepareSummary($this->getParam('period'), 0, 0, $isFilter);
+        $this->_collection = $this->_orderCollection->prepareSummary($this->getParam('period'), 0, 0, $isFilter);
 
         if ($this->getParam('store')) {
             $this->_collection->addFieldToFilter('store_id', $this->getParam('store'));
         } elseif ($this->getParam('website')) {
-            $storeIds = $this->storeManager->getWebsite($this->getParam('website'))->getStoreIds();
+            $storeIds = $this->_storeManager->getWebsite($this->getParam('website'))->getStoreIds();
             $this->_collection->addFieldToFilter('store_id', ['in' => implode(',', $storeIds)]);
         } elseif ($this->getParam('group')) {
-            $storeIds = $this->storeManager->getGroup($this->getParam('group'))->getStoreIds();
+            $storeIds = $this->_storeManager->getGroup($this->getParam('group'))->getStoreIds();
             $this->_collection->addFieldToFilter('store_id', ['in' => implode(',', $storeIds)]);
         } elseif (!$this->_collection->isLive()) {
             $this->_collection->addFieldToFilter(
                 'store_id',
-                ['eq' => $this->storeManager->getStore(\Magento\Store\Model\Store::ADMIN_CODE)->getId()]
+                ['eq' => $this->_storeManager->getStore(\Magento\Store\Model\Store::ADMIN_CODE)->getId()]
             );
         }
         $this->_collection->load();
