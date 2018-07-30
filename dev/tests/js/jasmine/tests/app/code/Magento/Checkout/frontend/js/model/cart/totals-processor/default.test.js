c727e90987578c1990dbb71ce2c383dd659234b6
Navarr Barnier <me@navarr.me>
2018-07-30 08:28:00 -0400

    Fix styling issues

--- dev/tests/js/jasmine/tests/app/code/Magento/Checkout/frontend/js/model/cart/totals-processor/default.test.js
+++ dev/tests/js/jasmine/tests/app/code/Magento/Checkout/frontend/js/model/cart/totals-processor/default.test.js
@@ -107,7 +107,7 @@ define([
 
         it('estimateTotals if data wasn\'t cached and request was successfully sent', function () {
             var deferral = new $.Deferred();
-            
+
             spyOn(mocks['Magento_Checkout/js/model/cart/cache'], 'isChanged').and.returnValue(true);
             spyOn(mocks['Magento_Customer/js/customer-data'], 'get').and.returnValue(
                 ko.observable({
@@ -132,7 +132,7 @@ define([
 
         it('estimateTotals if data wasn\'t cached and request returns error', function () {
             var deferral = new $.Deferred();
-            
+
             spyOn(mocks['Magento_Checkout/js/model/cart/cache'], 'isChanged').and.returnValue(true);
             spyOn(mocks['Magento_Customer/js/customer-data'], 'get').and.returnValue(
                 ko.observable({
