cbb98705b79a30e7dc69a15f0687e3d15cceda0b
nmalevanec <mikola.malevanec@transoftgroup.com>
2018-07-30 17:07:26 +0300

    ENGCOM-2569: Fix static tests failure.

--- lib/internal/Magento/Framework/System/Ftp.php
+++ lib/internal/Magento/Framework/System/Ftp.php
@@ -109,7 +109,7 @@ class Ftp
         }
         
         // Decode user & password strings from URL
-        foreach (array_intersect(array_keys($data), ['user','pass']) as $key) {
+        foreach (array_intersect(array_keys($data), ['user', 'pass']) as $key) {
             $data[$key] = urldecode($data[$key]);
         }
         
