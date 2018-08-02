261db8ce7fa46b8cd51c9d142a4bc976a1f0b821
Eugene Tulika <vranen@gmail.com>
2018-08-02 16:11:20 -0500

    MAGETWO-93996: Deprecate CatalogSearch

--- app/code/Magento/CatalogSearch/Block/Plugin/FrontTabPlugin.php
+++ app/code/Magento/CatalogSearch/Block/Plugin/FrontTabPlugin.php
@@ -12,6 +12,8 @@ use Magento\Framework\Data\Form\Element\Fieldset;
 
 /**
  * Plugin for Magento\Catalog\Block\Adminhtml\Product\Attribute\Edit\Tab\Front
+ * @deprecated
+ * @see ElasticSearch module is default search engine starting from 2.3. CatalogSearch would be removed in 2.4
  */
 class FrontTabPlugin
 {
