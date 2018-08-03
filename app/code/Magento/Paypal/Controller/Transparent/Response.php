0d7182794c80b807e1a13ca728f9ddb73b858011
Viktor Tymchynskyi <vtymchynskyi@magento.com>
2018-08-03 21:06:33 +0300

    MAGETWO-92953: [2.3] Once Payflow Pro payment is Declined, customer cannot continue checkout

--- app/code/Magento/Paypal/Controller/Transparent/Response.php
+++ app/code/Magento/Paypal/Controller/Transparent/Response.php
@@ -21,7 +21,7 @@ use Magento\Framework\App\RequestInterface;
 use Magento\Framework\App\Request\InvalidRequestException;
 
 /**
- * Class Response
+ *  @SuppressWarnings(PHPMD.CouplingBetweenObjects)
  */
 class Response extends \Magento\Framework\App\Action\Action implements CsrfAwareActionInterface
 {
