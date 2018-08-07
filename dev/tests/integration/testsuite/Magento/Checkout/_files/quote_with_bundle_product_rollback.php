2e2771d0a4e21360e94bc1fab9a8efd7c10d87e4
Andrii Lugovyi <alugovyi@magento.com>
2018-08-07 17:25:56 +0300

    MAGETWO-93735: [Forwardport] Grouped Product

--- dev/tests/integration/testsuite/Magento/Checkout/_files/quote_with_bundle_product_rollback.php
+++ dev/tests/integration/testsuite/Magento/Checkout/_files/quote_with_bundle_product_rollback.php
@@ -3,6 +3,7 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types=1);
 
 /*
  * Since the bundle product creation GUI doesn't allow to choose values for bundled products' custom options,
