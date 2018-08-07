ea9f240fb37b0f5c92e274d37f5e73a5407af495
Drischie <42138053+Drischie@users.noreply.github.com>
2018-08-07 13:39:28 +0200

    Changed ternary to multiline
    Changed one-line ternary to multiline ternary to not exceed line-size-limit of 120 Characters.

--- app/code/Magento/Customer/Model/Vat.php
+++ app/code/Magento/Customer/Model/Vat.php
@@ -184,10 +184,14 @@ class Vat
 
             $requestParams = [];
             $requestParams['countryCode'] = $countryCode;
-            $this->isCountryInEU($countryCode) ? $vatNumberSanitized = str_replace([' ', '-', $countryCode], ['', '', ''], $vatNumber) : $vatNumberSanitized = str_replace([' ', '-'], ['', ''], $vatNumber);
+            $this->isCountryInEU($countryCode) 
+                ? $vatNumberSanitized = str_replace([' ', '-', $countryCode], ['', '', ''], $vatNumber) 
+                : $vatNumberSanitized = str_replace([' ', '-'], ['', ''], $vatNumber);
             $requestParams['vatNumber'] = $vatNumberSanitized;
             $requestParams['requesterCountryCode'] = $requesterCountryCode;
-            $this->isCountryInEU($requesterCountryCode) ? $requesterVatNumberSanitized = str_replace([' ', '-', $requesterCountryCode], ['', '', ''], $requesterVatNumber) : $requesterVatNumberSanitized = str_replace([' ', '-'], ['', ''], $requesterVatNumber);
+            $this->isCountryInEU($requesterCountryCode) 
+                ? $requesterVatNumberSanitized = str_replace([' ', '-', $requesterCountryCode], ['', '', ''], $requesterVatNumber) 
+                : $requesterVatNumberSanitized = str_replace([' ', '-'], ['', ''], $requesterVatNumber);
             $requestParams['requesterVatNumber'] = $requesterVatNumberSanitized;
             // Send request to service
             $result = $soapClient->checkVatApprox($requestParams);
