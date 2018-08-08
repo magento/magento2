d1021a3da1c16ecf02772d8a17832b1876007cab
Dmytro Poperechnyy <dpoperechnyy@magento.com>
2018-08-08 16:35:07 +0300

    MAGETWO-94070: [2.3.x] Enable/disable EAV indexer from configuration
    - Add declare strict types;

--- app/code/Magento/CatalogSearch/Test/Unit/Plugin/EnableEavIndexerTest.php
+++ app/code/Magento/CatalogSearch/Test/Unit/Plugin/EnableEavIndexerTest.php
@@ -3,6 +3,8 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types=1);
+
 namespace Magento\CatalogSearch\Test\Unit\Plugin;
 
 use Magento\Framework\TestFramework\Unit\Helper\ObjectManager as ObjectManagerHelper;
@@ -19,6 +21,11 @@ class EnableEavIndexerTest extends \PHPUnit\Framework\TestCase
      */
     private $config;
 
+    /**
+     * Set up
+     *
+     * @return void
+     */
     protected function setUp()
     {
         $this->config = $this->getMockBuilder(\Magento\Config\Model\Config::class)
