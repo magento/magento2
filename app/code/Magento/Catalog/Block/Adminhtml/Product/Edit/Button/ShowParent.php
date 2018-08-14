2039843585509144d225e88c130d674f9e12e6bb
Fred Orosko Dias <fred@imaginationmedia.com>
2018-08-14 10:04:35 -0400

    Add Magento copyright

--- app/code/Magento/Catalog/Block/Adminhtml/Product/Edit/Button/ShowParent.php
+++ app/code/Magento/Catalog/Block/Adminhtml/Product/Edit/Button/ShowParent.php
@@ -1,5 +1,9 @@
 <?php
-
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+ 
 namespace Magento\Catalog\Block\Adminhtml\Product\Edit\Button;
 
 use Magento\Catalog\Model\Product\Type;
