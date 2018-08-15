3d7e76d6e4a1cb17e3a718221853a71a7473965f
Oleksandr Gorkun <ogorkun@magento.com>
2018-08-15 17:45:58 +0300

    MAGETWO-88597: Varnish Config Access List

--- app/code/Magento/PageCache/Test/Unit/Model/System/Config/Backend/AccessListTest.php
+++ app/code/Magento/PageCache/Test/Unit/Model/System/Config/Backend/AccessListTest.php
@@ -4,6 +4,8 @@
  * See COPYING.txt for license details.
  */
 
+declare(strict_types=1);
+
 namespace Magento\PageCache\Test\Unit\Model\System\Config\Backend;
 
 use Magento\PageCache\Model\System\Config\Backend\AccessList;
