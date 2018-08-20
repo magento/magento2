aa2e0fa03622db4ec34dce9d3eedbeb6f18ff120
Roman Ganin <rganin@adobe.com>
2018-08-20 16:45:10 -0500

    ENGCOM-2841: Fix external Sodium Encryption PR

--- dev/tests/static/testsuite/Magento/Test/Legacy/_files/blacklist/obsolete_mage.php
+++ dev/tests/static/testsuite/Magento/Test/Legacy/_files/blacklist/obsolete_mage.php
@@ -10,4 +10,5 @@ return [
     'dev/tests/static/testsuite/Magento/Test/Legacy/_files/obsolete_classes.php',
     'lib/internal/Magento/Framework/ObjectManager/Test/Unit/Factory/CompiledTest.php',
     'dev/tests/integration/testsuite/Magento/Indexer/Model/Config/_files/result.php',
+    'lib/internal/Magento/Framework/Encryption/Test/Unit/EncryptorTest.php',
 ];
