e6ad865f122eda048d9b85a9d82ba634339f1d27
Mikalai Shostka <Mikalai_Shostka@epam.com>
2018-08-20 15:55:10 +0300

    MAGETWO-59632: Create Sales > Order from admin add configurable product and change options click OK does not update Items Ordered List
    - Updated event after change options.

--- app/code/Magento/Sales/view/adminhtml/web/order/create/scripts.js
+++ app/code/Magento/Sales/view/adminhtml/web/order/create/scripts.js
@@ -907,6 +907,7 @@ define([
                     qtyElement.value = confirmedCurrentQty.value;
                 }
                 this.productConfigureAddFields['item['+itemId+'][configured]'] = 1;
+                this.itemsUpdate();
 
             }.bind(this));
             productConfigure.setShowWindowCallback(listType, function() {
