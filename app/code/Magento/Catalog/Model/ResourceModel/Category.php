24e715acca8703169d8a75331072d15839e0243c
Willian Keller <wkeller@ciandt.com>
2018-08-27 16:04:08 -0300

    Improve array semicolon and misspelled

--- app/code/Magento/Catalog/Model/ResourceModel/Category.php
+++ app/code/Magento/Catalog/Model/ResourceModel/Category.php
@@ -492,7 +492,7 @@ class Category extends AbstractResource
     }
 
     /**
-     * Get chlden categories count
+     * Get children categories count
      *
      * @param int $categoryId
      * @return int
