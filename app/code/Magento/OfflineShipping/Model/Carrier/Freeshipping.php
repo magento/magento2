3f05fe508d9909e526f72dcde14204fcb93029ac
Saravanan <saravananv14@live.com>
2018-08-27 11:14:10 +0530

    Removed extra spaces in if condition

--- app/code/Magento/OfflineShipping/Model/Carrier/Freeshipping.php
+++ app/code/Magento/OfflineShipping/Model/Carrier/Freeshipping.php
@@ -80,8 +80,7 @@ class Freeshipping extends \Magento\Shipping\Model\Carrier\AbstractCarrier imple
 
         $this->_updateFreeMethodQuote($request);
 
-        if ($request->getBaseSubtotalInclTax() < $this->getConfigData('free_shipping_subtotal')
-            && $this->getConfigData('showmethod')) {
+        if ($request->getBaseSubtotalInclTax() < $this->getConfigData('free_shipping_subtotal') && $this->getConfigData('showmethod')) {
             /** @var Error $error */
             $error = $this->_rateErrorFactory->create();
             $error->setCarrier($this->_code);
