527e4febe49ff6c2ab3b42ddabf798307f0e7222
Andrey Zabara <dr.chrom@gmail.com>
2018-08-28 14:45:22 +0300

    GraphQl-129: Retrieve Customer token

---
+++ app/code/Magento/CustomerGraphQl/Model/Resolver/Customer/Account/GenerateCustomerToken.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\CustomerGraphQl\Model\Resolver\Customer\Account;
+
+use Magento\Authorization\Model\UserContextInterface;
+use Magento\Integration\Api\CustomerTokenServiceInterface;
+use Magento\Customer\Model\Customer;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Exception\GraphQlAuthorizationException;
+use Magento\Framework\GraphQl\Query\Resolver\Value;
+use Magento\Framework\GraphQl\Query\Resolver\ValueFactory;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+
+class GenerateCustomerToken implements ResolverInterface
+{
+    /**
+     * @var UserContextInterface
+     */
+    private $userContext;
+
+    /**
+     * @var CustomerTokenServiceInterface
+     */
+    private $customerTokenService;
+
+    /**
+     * @var ValueFactory
+     */
+    private $valueFactory;
+
+    /**
+     * @param UserContextInterface          $userContext
+     * @param CustomerTokenServiceInterface $customerTokenService
+     * @param ValueFactory                  $valueFactory
+     */
+    public function __construct(
+        UserContextInterface $userContext,
+        CustomerTokenServiceInterface $customerTokenService,
+        ValueFactory $valueFactory
+    ) {
+        $this->userContext          = $userContext;
+        $this->customerTokenService = $customerTokenService;
+        $this->valueFactory         = $valueFactory;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function resolve(
+        Field $field,
+        $context,
+        ResolveInfo $info,
+        array $value = null,
+        array $args = null
+    ): Value {
+
+        $token = $this->customerTokenService->createCustomerAccessToken($args['email'], $args['password']);
+        //TODO: exception
+        $result = function () use ($token) {
+            return !empty($token) ? $token : '';
+        };
+        return $this->valueFactory->create($result);
+    }
+}
