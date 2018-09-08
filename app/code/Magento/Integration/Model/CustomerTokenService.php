9a4f260d33ea4603e34b112a4d72d934700de3de
prakashpatel07 <prakash.patel@krishtechnolabs.com>
2018-09-08 09:46:18 +0000

    Fixed Last Logged-in date when customer authenticate via REST API.

--- app/code/Magento/Integration/Model/CustomerTokenService.php
+++ app/code/Magento/Integration/Model/CustomerTokenService.php
@@ -14,6 +14,7 @@ use Magento\Integration\Model\Oauth\TokenFactory as TokenModelFactory;
 use Magento\Integration\Model\ResourceModel\Oauth\Token\CollectionFactory as TokenCollectionFactory;
 use Magento\Integration\Model\Oauth\Token\RequestThrottler;
 use Magento\Framework\Exception\AuthenticationException;
+use Magento\Framework\Event\ManagerInterface;
 
 class CustomerTokenService implements \Magento\Integration\Api\CustomerTokenServiceInterface
 {
@@ -49,22 +50,30 @@ class CustomerTokenService implements \Magento\Integration\Api\CustomerTokenServ
     private $requestThrottler;
 
     /**
+     * @var Magento\Framework\Event\ManagerInterface
+     */
+    private $eventManager;
+
+    /**
      * Initialize service
      *
      * @param TokenModelFactory $tokenModelFactory
      * @param AccountManagementInterface $accountManagement
      * @param TokenCollectionFactory $tokenModelCollectionFactory
      * @param \Magento\Integration\Model\CredentialsValidator $validatorHelper
+     * @param \Magento\Framework\Event\ManagerInterface $eventManager
      */
     public function __construct(
         TokenModelFactory $tokenModelFactory,
         AccountManagementInterface $accountManagement,
-        TokenCollectionFactory $tokenModelCollectionFactory,
-        CredentialsValidator $validatorHelper
+        TokenCollectionFactory $tokenModelCollectionFactory,      
+        CredentialsValidator $validatorHelper,
+        ManagerInterface $eventManager
     ) {
         $this->tokenModelFactory = $tokenModelFactory;
         $this->accountManagement = $accountManagement;
         $this->tokenModelCollectionFactory = $tokenModelCollectionFactory;
+        $this->eventManager = $eventManager;
         $this->validatorHelper = $validatorHelper;
     }
 
@@ -82,7 +91,8 @@ class CustomerTokenService implements \Magento\Integration\Api\CustomerTokenServ
             throw new AuthenticationException(
                 __('You did not sign in correctly or your account is temporarily disabled.')
             );
-        }
+        }       
+        $this->eventManager->dispatch('customer_login', ['customer' => $customerDataObject]);
         $this->getRequestThrottler()->resetAuthenticationFailuresCount($username, RequestThrottler::USER_TYPE_CUSTOMER);
         return $this->tokenModelFactory->create()->createCustomerToken($customerDataObject->getId())->getToken();
     }
