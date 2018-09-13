b8676e661b6ced49fb37cf2f427479125d8e6a31
OlgaVasyltsun <olga.vasyltsun@transoftgroup.com>
2018-09-13 15:35:08 +0300

    MAGETWO-73894: Shipment with shipping label returns a blank result via REST API

--- app/code/Magento/Sales/Test/Unit/Plugin/ShippingLabelConverterTest.php
+++ app/code/Magento/Sales/Test/Unit/Plugin/ShippingLabelConverterTest.php
@@ -23,11 +23,6 @@ class ShippingLabelConverterTest extends \PHPUnit\Framework\TestCase
     private $model;
 
     /**
-     * @var ShipmentInterface|\PHPUnit_Framework_MockObject_MockObject
-     */
-    private $shipmentMock;
-
-    /**
      * @var ShipmentRepositoryInterface|\PHPUnit_Framework_MockObject_MockObject
      */
     private $shipmentRepositoryMock;
@@ -38,43 +33,81 @@ class ShippingLabelConverterTest extends \PHPUnit\Framework\TestCase
     protected function setUp()
     {
         $this->model = new ShippingLabelConverter();
-
-        $shippingLabel = 'shipping_label_test';
-        $shippingLabelEncoded = base64_encode('shipping_label_test');
-        $this->shipmentMock = $this->getMockBuilder(ShipmentInterface::class)
-            ->disableOriginalConstructor()->getMock();
-        $this->shipmentMock->expects($this->exactly(2))->method('getShippingLabel')->willReturn($shippingLabel);
-        $this->shipmentMock->expects($this->once())
-            ->method('setShippingLabel')
-            ->with($shippingLabelEncoded)
-            ->willReturnSelf();
         $this->shipmentRepositoryMock = $this->getMockBuilder(ShipmentRepositoryInterface::class)
             ->disableOriginalConstructor()->getMock();
     }
 
     /**
      * @covers \Magento\Sales\Plugin\ShippingLabelConverter::afterGet()
+     * @param ShipmentInterface|\PHPUnit_Framework_MockObject_MockObject
+     * @return void
+     * @dataProvider shipmentDataProvider
      */
-    public function testAfterGet()
+    public function testAfterGet($shipmentMock)
     {
         $this->model->afterGet(
             $this->shipmentRepositoryMock,
-            $this->shipmentMock
+            $shipmentMock
         );
     }
 
     /**
      * @covers \Magento\Sales\Plugin\ShippingLabelConverter::afterGetList()
+     * @param ShipmentInterface|\PHPUnit_Framework_MockObject_MockObject
+     * @return void
+     * @dataProvider shipmentDataProvider
      */
-    public function testAfterGetList()
+    public function testAfterGetList($shipmentMock)
     {
         $searchResultMock = $this->getMockBuilder(ShipmentSearchResultInterface::class)
             ->disableOriginalConstructor()->getMock();
-        $searchResultMock->expects($this->once())->method('getItems')->willReturn([$this->shipmentMock]);
+        $searchResultMock->expects($this->once())->method('getItems')->willReturn([$shipmentMock]);
 
         $this->model->afterGetList(
             $this->shipmentRepositoryMock,
             $searchResultMock
         );
+    }
+
+    /**
+     * @return array
+     */
+    public function shipmentDataProvider()
+    {
+        return [
+            ['shipmentMock' => $this->getShipmentMockWithLabel()],
+            ['shipmentMock' => $this->getShipmentMockWithOutLabel()],
+        ];
+    }
+
+    /**
+     * @return ShipmentInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private function getShipmentMockWithLabel()
+    {
+        $shippingLabel = 'shipping_label_test';
+        $shippingLabelEncoded = base64_encode('shipping_label_test');
+        $shipmentMock = $this->getMockBuilder(ShipmentInterface::class)
+            ->disableOriginalConstructor()->getMock();
+        $shipmentMock->expects($this->exactly(2))->method('getShippingLabel')->willReturn($shippingLabel);
+        $shipmentMock->expects($this->once())
+            ->method('setShippingLabel')
+            ->with($shippingLabelEncoded)
+            ->willReturnSelf();
+
+        return $shipmentMock;
+    }
+
+    /**
+     * @return ShipmentInterface|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private function getShipmentMockWithOutLabel()
+    {
+        $shipmentMock = $this->getMockBuilder(ShipmentInterface::class)
+            ->disableOriginalConstructor()->getMock();
+        $shipmentMock->expects($this->once())->method('getShippingLabel')->willReturn(null);
+        $shipmentMock->expects($this->never())->method('setShippingLabel');
+
+        return $shipmentMock;
     }
 }
