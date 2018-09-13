ab84442aa46fa38665c70a2a1868e3ee015567f7
roman <rleshchenko@magento.com>
2018-09-13 12:32:37 +0300

    MAGETWO-92776: Fixed wrong admin notifications behavior

--- app/code/Magento/AdminNotification/Block/Grid/Renderer/Actions.php
+++ app/code/Magento/AdminNotification/Block/Grid/Renderer/Actions.php
@@ -37,7 +37,9 @@ class Actions extends \Magento\Backend\Block\Widget\Grid\Column\Renderer\Abstrac
      */
     public function render(\Magento\Framework\DataObject $row)
     {
-        $readDetailsHtml = $row->getUrl() ? '<a class="action-details" target="_blank" href="' . $row->getUrl() . '">' .
+        $readDetailsHtml = $row->getUrl() ? '<a class="action-details" target="_blank" href="' .
+            $this->escapeUrl($row->getUrl())
+            . '">' .
             __('Read Details') . '</a>' : '';
 
         $markAsReadHtml = !$row->getIsRead() ? '<a class="action-mark" href="' . $this->getUrl(
