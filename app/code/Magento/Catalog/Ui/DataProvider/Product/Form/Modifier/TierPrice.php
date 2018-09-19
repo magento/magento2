78327c1ab246fc20ed84ee54ed67e2d97a1c5131
Alexey Yakimovich <yakimovich@almagy.com>
2018-09-19 14:06:15 +0300

    MAGETWO-91657: Advanced pricing the bulk discounts by percentage returns error when set
    - Added validation for discount value field in Advanced Pricing section of product.

--- app/code/Magento/Catalog/Ui/DataProvider/Product/Form/Modifier/TierPrice.php
+++ app/code/Magento/Catalog/Ui/DataProvider/Product/Form/Modifier/TierPrice.php
@@ -45,7 +45,7 @@ class TierPrice extends AbstractModifier
     }
 
     /**
-     * {@inheritdoc}
+     * @inheritdoc
      * @since 101.1.0
      */
     public function modifyData(array $data)
@@ -54,8 +54,11 @@ class TierPrice extends AbstractModifier
     }
 
     /**
-     * {@inheritdoc}
+     * Add tier price info to meta array. {@inheritdoc}
+     *
      * @since 101.1.0
+     * @param array $meta
+     * @return array
      */
     public function modifyMeta(array $meta)
     {
@@ -150,7 +153,9 @@ class TierPrice extends AbstractModifier
                                     'dataType' => Price::NAME,
                                     'addbefore' => '%',
                                     'validation' => [
+                                        'required-entry' => true,
                                         'validate-number' => true,
+                                        'validate-greater-than-zero' => true,
                                         'less-than-equals-to' => 100
                                     ],
                                     'visible' => $firstOption
