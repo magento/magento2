1f08cd108688d742a5d66a944f142c5738b81ec0
pavel <pavel@newamsterdam.se>
2018-09-20 18:39:54 +0300

    Refactored Implemented 17964: Backend Order creation Authorizenet: If invalid credit card number is entered the loader keeps spinning forever without any error message

--- app/code/Magento/Sales/view/adminhtml/web/order/create/scripts.js
+++ app/code/Magento/Sales/view/adminhtml/web/order/create/scripts.js
@@ -1170,9 +1170,10 @@ define([
 
         submit : function()
         {
-            if(jQuery('#edit_form').valid()) {
-                jQuery('#edit_form').trigger('processStart');
-                jQuery('#edit_form').trigger('submitOrder');
+            var $editForm = jQuery('#edit_form');
+            if ($editForm.valid()) {
+                $editForm.trigger('processStart');
+                $editForm.trigger('submitOrder');
             }
         },
 
