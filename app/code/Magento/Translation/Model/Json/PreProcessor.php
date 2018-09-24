9f6515fd55c2af5d1de92d5af51beca961ac2392
priti <priti@2jcommerce.in>
2018-09-24 14:39:20 +0530

    changes-for-add-to-cart-button-text-translate-with-custom-theme

--- app/code/Magento/Translation/Model/Json/PreProcessor.php
+++ app/code/Magento/Translation/Model/Json/PreProcessor.php
@@ -93,6 +93,7 @@ class PreProcessor implements PreProcessorInterface
             }
 
             $area = $this->areaList->getArea($areaCode);
+            $area->load(\Magento\Framework\App\Area::PART_DESIGN);
             $area->load(\Magento\Framework\App\Area::PART_TRANSLATE);
 
             $this->translate->setLocale($context->getLocale())->loadData($areaCode, true);
