5cfad033e00338456f6eed9da68e3632dfaeaad9
Eugene Shakhsuvarov <ishakhsuvarov@magento.com>
2018-10-01 15:35:08 +0300

    Update doc blocks

--- setup/src/Magento/Setup/Model/ConfigOptionsList/DriverOptions.php
+++ setup/src/Magento/Setup/Model/ConfigOptionsList/DriverOptions.php
@@ -1,15 +1,26 @@
-<?php declare(strict_types=1);
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
 namespace Magento\Setup\Model\ConfigOptionsList;
 
 use Magento\Framework\Config\ConfigOptionsListConstants;
 
+/**
+ * MySql driver options
+ */
 class DriverOptions
 {
     /**
+     * Get driver options.
+     *
      * @params array $options
      * @return array
      */
-    public function getDriverOptions($options)
+    public function getDriverOptions(array $options): array
     {
         $driverOptionKeys = [
             ConfigOptionsListConstants::KEY_MYSQL_SSL_KEY => ConfigOptionsListConstants::INPUT_KEY_DB_SSL_KEY,
@@ -27,11 +38,13 @@ class DriverOptions
     }
 
     /**
+     * Check if provided option exists.
+     *
      * @param array $options
      * @param string $driverOptionKey
      * @return bool
      */
-    private function optionExists($options, $driverOptionKey)
+    private function optionExists(array $options, string $driverOptionKey): bool
     {
         return $options[$driverOptionKey] === false || !empty($options[$driverOptionKey]);
     }
