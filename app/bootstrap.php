d63eeb6faf495536f764119be43db226e76af44a
vitaliyboyko <v.boyko@atwix.com>
2018-10-03 10:09:52 +0300

    MSI-1707: Switch on errors for TRAVIS test

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -8,7 +8,8 @@
  * Environment initialization
  */
 error_reporting(E_ALL);
-#ini_set('display_errors', 1);
+//travis test
+ini_set('display_errors', 1);
 
 /* PHP version validation */
 if (!defined('PHP_VERSION_ID') || !(PHP_VERSION_ID === 70002 || PHP_VERSION_ID === 70004 || PHP_VERSION_ID >= 70006)) {
