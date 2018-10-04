24de687c16d5ce007336f01d70fb9ed800838a00
Pavel Bystritsky <p.bystritsky@yandex.ru>
2018-10-04 14:19:33 +0300

    ENGCOM-2111: Fix static test failure.

--- app/code/Magento/ConfigurableProduct/Plugin/SalesRule/Model/Rule/Condition/Product.php
+++ app/code/Magento/ConfigurableProduct/Plugin/SalesRule/Model/Rule/Condition/Product.php
@@ -1,4 +1,9 @@
-<?php declare(strict_types=1);
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
 
 namespace Magento\ConfigurableProduct\Plugin\SalesRule\Model\Rule\Condition;
 
