c106a62a03e7e7538da09f2054cbaf124816f018
al.kravchuk <al.kravchuk@ism-ukraine.com>
2018-10-05 09:56:25 +0300

    magento/magento2#14510: Creating custom customer attribute with default value 0 will cause not saving value for customer entity. Remove curly braces.

--- app/code/Magento/Downloadable/Setup/UpgradeData.php
+++ app/code/Magento/Downloadable/Setup/UpgradeData.php
@@ -35,7 +35,7 @@ class UpgradeData implements UpgradeDataInterface
     }
 
     /**
-     * {@inheritdoc}
+     * @inheritdoc
      */
     public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
     {
