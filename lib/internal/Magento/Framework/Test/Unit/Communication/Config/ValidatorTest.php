9afdfee251b7ef9098b3ae22e59cf0673c557bb3
Artsiom Bruneuski <artsiom.bruneuski@tudock.de>
2018-10-06 15:20:07 +0200

    catch InvalidArgumentException, throw correct message, unit test

---
+++ lib/internal/Magento/Framework/Test/Unit/Communication/Config/ValidatorTest.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * m2git
+ */
+
+namespace Magento\Framework\Test\Unit\Communication\Config;
+
+
+use Magento\Framework\Communication\Config\Validator;
+use Magento\Framework\Reflection\MethodsMap;
+use Magento\Framework\Reflection\TypeProcessor;
+
+class ValidatorTest extends \PHPUnit\Framework\TestCase
+{
+    protected $typeProcessor;
+    protected $methodsMap;
+
+    public function setUp()
+    {
+        $this->methodsMap = $this->createMock(MethodsMap::class);
+
+        $this->methodsMap->expects(static::any())
+            ->method('getMethodsMap')
+            ->will($this->throwException(new \InvalidArgumentException()));
+
+
+        $this->typeProcessor = $this->createMock(TypeProcessor::class);
+        $this->typeProcessor->expects(static::any())
+            ->method('isTypeSimple')
+            ->willReturn(false);
+
+        $this->typeProcessor->expects(static::any())
+            ->method('isTypeSimple')
+            ->willReturn(false);
+    }
+
+    /**
+     * @expectedException  \LogicException
+     * @expectedExceptionCode 123
+     */
+    public function testValidateResponseSchemaType()
+    {
+        /** @var Validator $validator */
+        $validator = new Validator($this->typeProcessor, $this->methodsMap);
+        $validator->validateResponseSchemaType('123', '123');
+    }
+
+    /**
+     * @expectedException  \LogicException
+     * @expectedExceptionCode 123
+     */
+    public function testValidateRequestSchemaType()
+    {
+        /** @var Validator $validator */
+        $validator = new Validator($this->typeProcessor, $this->methodsMap);
+        $validator->validateRequestSchemaType('123', '123');
+    }
+}
