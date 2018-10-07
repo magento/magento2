a7cdae0e4f80aa8596d4b3a3b958ae2b9938a346
Artsiom Bruneuski <artsiom.bruneuski@tudock.de>
2018-10-07 12:04:19 +0200

    moved strict_types block

--- lib/internal/Magento/Framework/Test/Unit/Communication/Config/ValidatorTest.php
+++ lib/internal/Magento/Framework/Test/Unit/Communication/Config/ValidatorTest.php
@@ -1,8 +1,10 @@
-<?php declare(strict_types=1);
+<?php
 /**
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types=1);
+
 namespace Magento\Framework\Test\Unit\Communication\Config;
 
 use Magento\Framework\Communication\Config\Validator;
