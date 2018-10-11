1b56213a6d7e3c2afeb690ac9828f4dc6c0e8336
Daniel Renaud <drenaud@magento.com>
2018-10-11 16:06:29 -0500

    MAGETWO-95356: Import is successful without notice about category name updates

---
+++ dev/tests/integration/testsuite/Magento/CatalogImportExport/_files/update_category_duplicates_rollback.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+require dirname(dirname(__DIR__)) . '/Catalog/_files/category_duplicates_rollback.php';
+
+//Delete products created in CSV import
+/** @var \Magento\Catalog\Api\ProductRepositoryInterface $productRepository */
+$productRepository = $objectManager->get(\Magento\Catalog\Api\ProductRepositoryInterface::class);
+$csvProductSkus = ['simple1', 'simple2', 'simple3'];
+foreach($csvProductSkus as $sku) {
+    try {
+        $product = $productRepository->get($sku, false, null, true);
+        $productRepository->delete($product);
+    } catch (\Magento\Framework\Exception\NoSuchEntityException $e) {
+    }
+}
