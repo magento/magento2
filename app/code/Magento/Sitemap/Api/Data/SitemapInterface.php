0d645868357656f7b1a84d030725dbb564668dc7
Wietze Mink <wietze.mink@guapa.nl>
2018-10-11 11:46:20 +0200

    Remove SitemapExtensionInterface and make SitemapInterface extend ExtensionAttributesInterface

--- app/code/Magento/Sitemap/Api/Data/SitemapInterface.php
+++ app/code/Magento/Sitemap/Api/Data/SitemapInterface.php
@@ -8,7 +8,7 @@ declare(strict_types=1);
 
 namespace Magento\Sitemap\Api\Data;
 
-interface SitemapInterface extends SitemapExtensionInterface
+interface SitemapInterface extends \Magento\Framework\Api\ExtensionAttributesInterface
 {
 
     /**#@+
