f08455510328dad1c0e01cadb8321ecd329ea55c
shubham sharma <shubhamsharma@shubhams-MacBook-Air.local>
2018-10-13 01:22:52 +0530

    fixed issue 18458

--- app/code/Magento/Ui/view/base/web/js/modal/modal.js
+++ app/code/Magento/Ui/view/base/web/js/modal/modal.js
@@ -428,6 +428,7 @@ define([
             }
             events = $._data(this.overlay.get(0), 'events');
             events ? this.prevOverlayHandler = events.click[0].handler : false;
+            
         },
 
         /**
