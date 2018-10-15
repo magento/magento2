21d5e9b318660539061a0a522b79fa62109b7cd4
Alexander Menk <a.menk@imi.de>
2018-10-15 15:59:17 +0200

    Add MAGE_RUN_TYPE+MAGE_RUN_CODE to FPC identifier
    Currently Magento 2.2.6 full page cache does not work correctly, when using SetEnv or similar techniques on unchanged URLs
    So the MAGE_RUN_TYPE and MAGE_RUN_CODE should be added to the FPC identifier to allow for such use cases.
    A common case is Magento 2 behind a reverse NGINX proxy.
    
    This commit adds the MAGE_RUN_TYPE and MAGE_RUN_CODE to the cache idenfier, if present.
    It is done inline with the design exceptions. Also the existing cache idenfier is change a little to accomondate
    the new information properly.
    
    Of course this will invalidate existing page caches with design exceptions, but as for any deploy the page
    cache usually is invalidated, this is not a problem.

--- app/code/Magento/PageCache/Test/Unit/App/CacheIdentifierPluginTest.php
+++ app/code/Magento/PageCache/Test/Unit/App/CacheIdentifierPluginTest.php
@@ -6,6 +6,8 @@
 namespace Magento\PageCache\Test\Unit\App;
 
 use Magento\PageCache\Model\Config;
+use Magento\Store\Model\Store;
+use Magento\Store\Model\StoreManager;
 
 /**
  * Class CacheIdentifierPluginTest
@@ -56,7 +58,7 @@ class CacheIdentifierPluginTest extends \PHPUnit\Framework\TestCase
     }
 
     /**
-     * Test of adding design exceptions to the kay of cache hash
+     * Test of adding design exceptions + run code to the key of cache hash
      *
      * @param string $cacheType
      * @param bool $isPageCacheEnabled
@@ -95,7 +97,68 @@ class CacheIdentifierPluginTest extends \PHPUnit\Framework\TestCase
             'Built-in + PageCache enabled' => [Config::BUILT_IN, true, null, false, false],
             'Built-in, PageCache enabled, no user-agent exceptions' =>
                 [Config::BUILT_IN, true, 'aa123aa', false, 'aa123aa'],
-            'Built-in, PageCache enabled, with design exception' => [Config::BUILT_IN, true, 'aa123aa', '7', '7aa123aa']
+            'Built-in, PageCache enabled, with design exception' => [Config::BUILT_IN, true, 'aa123aa', '7', 'DESIGN=7|aa123aa']
         ];
+    }
+
+    /**
+     * Tests that different stores cause different identifiers
+     * (property based testing approach)
+     */
+    public function testAfterGetValueRunParamsCauseDifferentIdentifiers()
+    {
+        $identifierMock = $this->createMock(\Magento\Framework\App\PageCache\Identifier::class);
+
+        $this->pageCacheConfigMock->expects($this->any())
+            ->method('getType')
+            ->willReturn(Config::BUILT_IN);
+        $this->pageCacheConfigMock->expects($this->any())
+            ->method('isEnabled')
+            ->willReturn(true);
+
+        $defaultRequestMock = clone $this->requestMock;
+        $defaultRequestMock->expects($this->any())
+            ->method('getServerValue')
+            ->willReturnCallback(function ($param) {
+                if ($param == StoreManager::PARAM_RUN_TYPE) {
+                    return 'store';
+                }
+                if ($param == StoreManager::PARAM_RUN_CODE) {
+                    return 'default';
+                }
+            });
+
+        $nullSha1 = 'da39a3ee5e6b4b0d3255bfef95601890afd80709';
+
+        $defaultPlugin = new \Magento\PageCache\Model\App\CacheIdentifierPlugin(
+            $this->designExceptionsMock,
+            $defaultRequestMock,
+            $this->pageCacheConfigMock
+        );
+
+        $defaultStoreResult = $defaultPlugin->afterGetValue($identifierMock, $nullSha1);
+
+        $otherRequestMock = clone $this->requestMock;
+        $otherRequestMock->expects($this->any())
+            ->method('getServerValue')
+            ->willReturnCallback(function ($param) {
+                if ($param == StoreManager::PARAM_RUN_TYPE) {
+                    return 'store';
+                }
+                if ($param == StoreManager::PARAM_RUN_CODE) {
+                    return 'klingon';
+                }
+            });
+
+        $otherPlugin = new \Magento\PageCache\Model\App\CacheIdentifierPlugin(
+            $this->designExceptionsMock,
+            $otherRequestMock,
+            $this->pageCacheConfigMock
+        );
+        $otherStoreResult = $otherPlugin->afterGetValue($identifierMock, $nullSha1);
+
+        $this->assertNotEquals($nullSha1, $defaultStoreResult);
+        $this->assertNotEquals($nullSha1, $otherStoreResult);
+        $this->assertNotEquals($defaultStoreResult, $otherStoreResult);
     }
 }
