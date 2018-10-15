42ecb9c78de2ebb4c1c475b2ef2e286faded0b5f
Lewis Voncken <lewis@experius.nl>
2018-10-15 15:47:35 +0200

    [BUGFIX] [issue-10205] Always set the entity_type_id for updating product attributes because the route contains the specification products/attributes

--- app/code/Magento/Catalog/Model/Product/Attribute/Repository.php
+++ app/code/Magento/Catalog/Model/Product/Attribute/Repository.php
@@ -106,6 +106,11 @@ class Repository implements \Magento\Catalog\Api\ProductAttributeRepositoryInter
      */
     public function save(\Magento\Catalog\Api\Data\ProductAttributeInterface $attribute)
     {
+        $attribute->setEntityTypeId(
+            $this->eavConfig
+                ->getEntityType(\Magento\Catalog\Api\Data\ProductAttributeInterface::ENTITY_TYPE_CODE)
+                ->getId()
+        );
         if ($attribute->getAttributeId()) {
             $existingModel = $this->get($attribute->getAttributeCode());
 
@@ -143,11 +148,6 @@ class Repository implements \Magento\Catalog\Api\ProductAttributeRepositoryInter
             );
             $attribute->setBackendModel(
                 $this->productHelper->getAttributeBackendModelByInputType($attribute->getFrontendInput())
-            );
-            $attribute->setEntityTypeId(
-                $this->eavConfig
-                    ->getEntityType(\Magento\Catalog\Api\Data\ProductAttributeInterface::ENTITY_TYPE_CODE)
-                    ->getId()
             );
             $attribute->setIsUserDefined(1);
         }
