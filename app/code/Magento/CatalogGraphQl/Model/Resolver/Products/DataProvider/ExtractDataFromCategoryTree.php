b777343da3f63b8cf141404c759c9b9294f73a69
Lyzun Oleksandr <alex.nuzil@gmail.com>
2018-10-15 22:13:01 +0200

    Change a way how categori tree is fetched. Current problem that all childs are always coming to last parent. Rewritte way of tree generation

--- app/code/Magento/CatalogGraphQl/Model/Resolver/Products/DataProvider/ExtractDataFromCategoryTree.php
+++ app/code/Magento/CatalogGraphQl/Model/Resolver/Products/DataProvider/ExtractDataFromCategoryTree.php
@@ -15,10 +15,17 @@ use Magento\Catalog\Api\Data\CategoryInterface;
  */
 class ExtractDataFromCategoryTree
 {
+    const START_CATEGORY_FETCH_LEVEL = 1;
+
     /**
      * @var Hydrator
      */
     private $categoryHydrator;
+
+    /**
+     * @var CategoryInterface;
+     */
+    private $iteratingCategory;
 
     /**
      * @param Hydrator $categoryHydrator
@@ -42,14 +49,61 @@ class ExtractDataFromCategoryTree
             /** @var CategoryInterface $category */
             $category = $iterator->current();
             $iterator->next();
-            $nextCategory = $iterator->current();
-            $tree[$category->getId()] = $this->categoryHydrator->hydrateCategory($category);
-            $tree[$category->getId()]['model'] = $category;
-            if ($nextCategory && (int) $nextCategory->getLevel() !== (int) $category->getLevel()) {
-                $tree[$category->getId()]['children'] = $this->execute($iterator);
+
+            $pathElements = explode("/", $category->getPath());
+            $this->iteratingCategory = $category;
+
+            $currentLevelTree = $this->generateLevelTree($pathElements, self::START_CATEGORY_FETCH_LEVEL);
+            if (empty($tree)) {
+                $tree = $currentLevelTree;
             }
+            $tree = $this->mergeCategoriesTrees($currentLevelTree, $tree);
         }
 
+        return $tree;
+    }
+
+    /**
+     * Merge together complex categories tree
+     *
+     * @param array $tree1
+     * @param array $tree2
+     * @return array
+     */
+    private function mergeCategoriesTrees(array &$tree1, array &$tree2): array
+    {
+        $mergedTree = $tree1;
+        foreach ($tree2 as $currentKey => &$value) {
+            if (is_array($value) && isset($mergedTree[$currentKey]) && is_array($mergedTree[$currentKey])) {
+                $mergedTree[$currentKey] = $this->mergeCategoriesTrees($mergedTree[$currentKey], $value);
+            } else {
+                $mergedTree[$currentKey] = $value;
+            }
+        }
+        return $mergedTree;
+    }
+
+    /**
+     * Recursive method to generate tree for one category path
+     *
+     * @param $elements
+     * @param $index
+     * @return array
+     */
+    private function generateLevelTree($elements, $index): array
+    {
+
+        $tree = [];
+        $tree[$elements[$index]]['id'] = $elements[$index];
+        if ($index === count($elements) - 1) {
+            $tree[$elements[$index]] = $this->categoryHydrator->hydrateCategory($this->iteratingCategory);
+            $tree[$elements[$index]]['model'] = $this->iteratingCategory;
+        }
+        $currentIndex = $index;
+        $index++;
+        if (isset($elements[$index])) {
+            $tree[$elements[$currentIndex]]['children'] = $this->generateLevelTree($elements, $index);
+        }
         return $tree;
     }
 }
