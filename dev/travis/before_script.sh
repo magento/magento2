ecb1a7bf306e87eb54a08a7c5a19eac214838ce6
Yaroslav Rogoza <enarc@atwix.com>
2018-10-16 13:10:15 +0200

    Another attempt to execute tests

--- dev/travis/before_script.sh
+++ dev/travis/before_script.sh
@@ -134,4 +134,33 @@ case $TEST_SUITE in
 
         cd ../../..
         ;;
+
+    api-functional)
+        echo "Installing Magento"
+        mysql -uroot -e 'CREATE DATABASE magento2;'
+        php bin/magento setup:install -q \
+            --language="en_US" \
+            --timezone="UTC" \
+            --currency="USD" \
+            --base-url="http://${MAGENTO_HOST_NAME}/" \
+            --admin-firstname="John" \
+            --admin-lastname="Doe" \
+            --backend-frontname="backend" \
+            --admin-email="admin@example.com" \
+            --admin-user="admin" \
+            --use-rewrites=1 \
+            --admin-use-security-key=0 \
+            --admin-password="123123q"
+
+        echo "Enabling production mode"
+        php bin/magento deploy:mode:set production
+
+        echo "Prepare api-functional tests for running"
+        cd dev/tests/api-functional
+
+        cp ./phpunit_graphql.xml.dist ./phpunit.xml
+        sed -e "s?magento.url?${MAGENTO_HOST_NAME}?g" --in-place ./phpunit.xml
+
+        cd ../../..
+        ;;
 esac
