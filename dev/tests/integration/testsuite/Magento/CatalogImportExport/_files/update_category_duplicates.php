b80b12f4588133656752e32908f8a913b1ceac60
Joan He <johe@adobe.com>
2018-10-17 14:58:53 -0500

    MAGETWO-95433: Customer address custom attributes (Multi select, dropdown) display behavior on storefront is not per standards (PFA snap)
    - fix static test failure

--- dev/tests/integration/testsuite/Magento/CatalogImportExport/_files/update_category_duplicates.php
+++ dev/tests/integration/testsuite/Magento/CatalogImportExport/_files/update_category_duplicates.php
@@ -3,6 +3,7 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types=1);
 
 require dirname(dirname(__DIR__)) . '/Catalog/_files/category_duplicates.php';
 
