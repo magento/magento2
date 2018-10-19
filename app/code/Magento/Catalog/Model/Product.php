fa529366822a4e049ddcab820212af24ee931c2b
Oleksii Korshenko <korshenk@adobe.com>
2018-10-19 14:02:30 -0500

    Issue magento/magento2#18094: Should getQty() return int/float or string
     - remove typecasting from the setter

--- app/code/Magento/Catalog/Model/Product.php
+++ app/code/Magento/Catalog/Model/Product.php
@@ -972,7 +972,7 @@ class Product extends \Magento\Catalog\Model\AbstractModel implements
     public function setQty($qty)
     {
         if ($this->getData('qty') != $qty) {
-            $this->setData('qty', (float)$qty);
+            $this->setData('qty', $qty);
             $this->reloadPriceInfo();
         }
         return $this;
