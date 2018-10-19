5bc40424b9631a1589c77b7ff07aae57ca5a326c
Stas Kozar <stas.kozar@transoftgroup.com>
2018-10-19 12:35:33 +0300

    MAGETWO-95629: Filesystem Directory Improvement

--- app/code/Magento/CatalogImportExport/Test/Unit/Model/Import/UploaderTest.php
+++ app/code/Magento/CatalogImportExport/Test/Unit/Model/Import/UploaderTest.php
@@ -119,7 +119,7 @@ class UploaderTest extends \PHPUnit\Framework\TestCase
     public function testMoveFileUrl($fileUrl, $expectedHost, $expectedFileName)
     {
         $destDir = 'var/dest/dir';
-        $expectedRelativeFilePath = $this->uploader->getTmpDir() . '/' . $expectedFileName;
+        $expectedRelativeFilePath = $expectedFileName;
         $this->directoryMock->expects($this->once())->method('isWritable')->with($destDir)->willReturn(true);
         $this->directoryMock->expects($this->any())->method('getRelativePath')->with($expectedRelativeFilePath);
         $this->directoryMock->expects($this->once())->method('getAbsolutePath')->with($destDir)
@@ -155,7 +155,7 @@ class UploaderTest extends \PHPUnit\Framework\TestCase
     {
         $destDir = 'var/dest/dir';
         $fileName = 'test_uploader_file';
-        $expectedRelativeFilePath = $this->uploader->getTmpDir() . '/' . $fileName;
+        $expectedRelativeFilePath = $fileName;
         $this->directoryMock->expects($this->once())->method('isWritable')->with($destDir)->willReturn(true);
         $this->directoryMock->expects($this->any())->method('getRelativePath')->with($expectedRelativeFilePath);
         $this->directoryMock->expects($this->once())->method('getAbsolutePath')->with($destDir)
