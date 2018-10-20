c9a8c738b0da805042275036a7cabd44db181b5b
Joan He <johe@adobe.com>
2018-10-20 00:33:55 -0500

    MAGETWO-71660: "Recently Ordered" widget conatins no more 5 products if qty of products > 5
    - fix static test failures

--- dev/tests/integration/testsuite/Magento/Sales/_files/order_with_customer_and_multiple_order_items.php
+++ dev/tests/integration/testsuite/Magento/Sales/_files/order_with_customer_and_multiple_order_items.php
@@ -3,6 +3,7 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types=1);
 
 include __DIR__ . '/order_with_multiple_items.php';
 include __DIR__ . '/../../../Magento/Customer/_files/customer.php';
