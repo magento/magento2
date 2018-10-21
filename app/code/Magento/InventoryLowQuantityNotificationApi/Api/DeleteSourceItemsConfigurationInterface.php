7365a808f55975989f2efaec4348731d487cd11c
Riccardo Tempesta <riccardo.tempesta@gmail.com>
2018-10-21 14:10:05 +0200

    FIX failing test due to rework of MassDelete resource model

--- app/code/Magento/InventoryLowQuantityNotificationApi/Api/DeleteSourceItemsConfigurationInterface.php
+++ app/code/Magento/InventoryLowQuantityNotificationApi/Api/DeleteSourceItemsConfigurationInterface.php
@@ -17,7 +17,7 @@ interface DeleteSourceItemsConfigurationInterface
     /**
      * Delete multiple source items configuration for low quantity
      *
-     * @param array $sourceItems
+     * @param \Magento\InventoryApi\Api\Data\SourceItemInterface[] $sourceItems
      * @return void
      */
     public function execute(array $sourceItems): void;
