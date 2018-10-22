7ee631323da937f7fc794f870b768fbb1b9aa0f7
Igor Miniailo <igor.minyaylo@gmail.com>
2018-10-22 16:39:35 +0300

    move StockResolverTest.php integration test

--- app/code/Magento/InventorySales/Test/Integration/StockResolverTest.php
+++ app/code/Magento/InventorySales/Test/Integration/StockResolverTest.php
@@ -5,7 +5,7 @@
  */
 declare(strict_types=1);
 
-namespace Magento\InventorySales\Test\Integration\Model;
+namespace Magento\InventorySales\Test\Integration;
 
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\ObjectManagerInterface;
