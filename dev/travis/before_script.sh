33f40d63ee1d102e81822341fadeb4cd5af68997
Yaroslav Rogoza <enarc@atwix.com>
2018-10-25 14:50:06 +0200

    Remove redundant setup:upgrade

--- dev/travis/before_script.sh
+++ dev/travis/before_script.sh
@@ -158,7 +158,6 @@ case $TEST_SUITE in
         echo "Prepare api-functional tests for running"
         cd dev/tests/api-functional
         cp -r _files/Magento/* ../../../app/code/Magento # Deploy and enable test modules before running tests
-        php ../../../bin/magento setup:upgrade
 
         cp ./phpunit_graphql.xml.dist ./phpunit.xml
         sed -e "s?magento.url?${MAGENTO_HOST_NAME}?g" --in-place ./phpunit.xml
