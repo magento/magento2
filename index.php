fb9119b21cc7c22395f5ee64ec602012a6d71c4f
gourabk121 <44121563+gourabk121@users.noreply.github.com>
2018-10-25 14:13:54 +0530

    php tag  isnot closed and miss ;

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 try {
     require __DIR__ . '/app/bootstrap.php';
 } catch (\Exception $e) {
-    echo <<<HTML
+    echo <<<HTML;
 <div style="font:12px/1.35em arial, helvetica, sans-serif;">
     <div style="margin:0 0 25px 0; border-bottom:1px solid #ccc;">
         <h3 style="margin:0;font-size:1.7em;font-weight:normal;text-transform:none;text-align:left;color:#2f2f2f;">
@@ -37,3 +37,4 @@ $bootstrap = \Magento\Framework\App\Bootstrap::create(BP, $_SERVER);
 /** @var \Magento\Framework\App\Http $app */
 $app = $bootstrap->createApplication(\Magento\Framework\App\Http::class);
 $bootstrap->run($app);
+?>
