b02dbc6f69ef9b2a48946218446302b204a6966e
Oleksii Korshenko <korshenk@adobe.com>
2018-10-26 15:59:32 -0500

    Update app/code/Magento/Sales/Model/Order/Creditmemo/Total/Discount.php
    Co-Authored-By: Rus0 <andonid88@gmail.com>

--- app/code/Magento/Sales/Model/Order/Creditmemo/Total/Discount.php
+++ app/code/Magento/Sales/Model/Order/Creditmemo/Total/Discount.php
@@ -30,7 +30,7 @@ class Discount extends AbstractTotal
 
         /**
          * If credit memo's shipping amount is set and Order's shipping amount is 0,
-         *      throw exception with differente message
+         * throw exception with different message
          */
         if ( $baseShippingAmount && $order->getBaseShippingAmount() <= 0 ) {
             throw new \Magento\Framework\Exception\LocalizedException(
