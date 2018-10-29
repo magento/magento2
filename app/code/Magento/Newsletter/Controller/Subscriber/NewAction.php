6de5586887291e8d82b6c2ac9d2e121e85ea8651
Ravi Chandra <ravi.chandra@krishtechnolabs.com>
2018-10-29 15:04:45 +0530

    #8952 Fixed subscribe to newsletter if you already have an account issue update changes as per suggestion

--- app/code/Magento/Newsletter/Controller/Subscriber/NewAction.php
+++ app/code/Magento/Newsletter/Controller/Subscriber/NewAction.php
@@ -77,14 +77,12 @@ class NewAction extends SubscriberController
     protected function validateEmailAvailable($email)
     {
         $websiteId = $this->_storeManager->getStore()->getWebsiteId();
-        if (isset($this->_customerSession->getCustomerDataObject()->getEmail())) {
-            if ($this->_customerSession->getCustomerDataObject()->getEmail() !== $email
-                && !$this->customerAccountManagement->isEmailAvailable($email, $websiteId)
-            ) {
-                throw new LocalizedException(
-                    __('This email address is already assigned to another user.')
-                );
-            }
+        if (isset($this->_customerSession->getCustomerDataObject()->getEmail()) && ($this->_customerSession->getCustomerDataObject()->getEmail() !== $email
+            && !$this->customerAccountManagement->isEmailAvailable($email, $websiteId)
+        )) {
+            throw new \Magento\Framework\Exception\LocalizedException(
+                __('This email address is already assigned to another user.')
+            );
         }
     }
 
