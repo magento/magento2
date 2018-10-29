7ceea962f0eab3baff1a4881a80b3cc2ac96c51f
Serhiy Yelahin <serhiy.yelahin@transoftgroup.com>
2018-10-29 09:56:28 +0200

    MAGETWO-93988: Product Review "Save and Next" and "Save and Previous" not working

--- dev/tests/integration/testsuite/Magento/Review/Model/ResourceModel/Review/Product/CollectionTest.php
+++ dev/tests/integration/testsuite/Magento/Review/Model/ResourceModel/Review/Product/CollectionTest.php
@@ -3,20 +3,89 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+
 namespace Magento\Review\Model\ResourceModel\Review\Product;
 
+use Magento\Review\Model\Review;
+use Magento\TestFramework\Helper\Bootstrap;
+
+/**
+ * Tests some functionality of the Product Review collection
+ */
 class CollectionTest extends \PHPUnit\Framework\TestCase
 {
     /**
+     * Checks resulting ids count
+     *
+     * @param int $status
+     * @param int $expectedCount
+     * @param string $sortAttribute
+     * @param string $dir
+     * @param callable $assertion
+     * @dataProvider sortOrderAssertionsDataProvider
      * @magentoDataFixture Magento/Review/_files/different_reviews.php
      */
-    public function testGetResultingIds()
-    {
-        $collection = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create(
-            \Magento\Review\Model\ResourceModel\Review\Product\Collection::class
-        );
-        $collection->addStatusFilter(\Magento\Review\Model\Review::STATUS_APPROVED);
+    public function testGetResultingIds(
+        int $status,
+        int $expectedCount,
+        string $sortAttribute,
+        string $dir,
+        callable $assertion
+    ) {
+        $collection = Bootstrap::getObjectManager()->create(Collection::class);
+        if ($status > 0) {
+            $collection->addStatusFilter($status);
+        }
+        $collection->setOrder($sortAttribute, $dir);
         $actual = $collection->getResultingIds();
-        $this->assertCount(2, $actual);
+        $this->assertCount($expectedCount, $actual);
+        $assertion($actual);
+    }
+
+    /**
+     * Sort order assertions data provider
+     *
+     * @return array
+     */
+    public function sortOrderAssertionsDataProvider(): array
+    {
+        return [
+            [
+                Review::STATUS_APPROVED,
+                2,
+                'rt.review_id',
+                'DESC',
+                function (array $actual) {
+                    $this->assertLessThan($actual[0], $actual[1]);
+                }
+            ],
+            [
+                Review::STATUS_APPROVED,
+                2,
+                'rt.review_id',
+                'ASC',
+                function (array $actual) {
+                    $this->assertLessThan($actual[1], $actual[0]);
+                }
+            ],
+            [
+                Review::STATUS_APPROVED,
+                2,
+                'rt.created_at',
+                'ASC',
+                function (array $actual) {
+                    $this->assertLessThan($actual[1], $actual[0]);
+                }
+            ],
+            [
+                0,
+                3,
+                'rt.review_id',
+                'ASC',
+                function (array $actual) {
+                    $this->assertLessThan($actual[1], $actual[0]);
+                }
+            ]
+        ];
     }
 }
