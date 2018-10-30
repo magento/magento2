516ab0a1277f84cb77be788f9dda06ef04e450cc
Hardik Trivedi <hardik.trivedi@krishtechnolabs.com>
2018-10-30 13:16:52 +0530

    code formate

--- app/code/Magento/Ui/view/base/web/js/form/element/select.js
+++ app/code/Magento/Ui/view/base/web/js/form/element/select.js
@@ -44,10 +44,12 @@ define([
         nodes = _.map(nodes, function (node) {
         value = node.value;
 
-            if (value == "") {
-                node.label = " ";
+            if (value == '') {
+                node.label = ' ';
+
                 return node;
             }
+            
             if (value === null || value === captionValue) {
                 if (_.isUndefined(caption)) {
                     caption = node.label;
