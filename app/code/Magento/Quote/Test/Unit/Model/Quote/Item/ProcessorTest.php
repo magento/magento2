e14d5cce259e60d10817e03558c211755059e50b
ashutosh <ashutosh045@webkul.com>
2018-10-30 15:37:19 +0530

    error fixed Whitespace found at end of line

--- app/code/Magento/Quote/Test/Unit/Model/Quote/Item/ProcessorTest.php
+++ app/code/Magento/Quote/Test/Unit/Model/Quote/Item/ProcessorTest.php
@@ -100,11 +100,11 @@ class ProcessorTest extends \PHPUnit\Framework\TestCase
         $this->productMock = $this->createPartialMock(
             \Magento\Catalog\Model\Product::class,
             [
-                'getCustomOptions', 
-                '__wakeup', 
-                'getParentProductId', 
-                'getCartQty', 
-                'getStickWithinParent', 
+                'getCustomOptions',
+                '__wakeup',
+                'getParentProductId',
+                'getCartQty',
+                'getStickWithinParent',
                 'getFinalPrice']
         );
         $this->objectMock = $this->createPartialMock(
