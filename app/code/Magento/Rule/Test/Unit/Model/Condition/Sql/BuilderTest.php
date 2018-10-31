fff8dd3fae6aa65e19d7a5f539123b71d40c7078
Yuliya Labudova <Yuliya_Labudova@epam.com>
2018-10-31 16:54:25 +0300

    MAGETWO-91609: Problems with operator more/less in the "catalog Products List" widget
    - Fix static test

--- app/code/Magento/Rule/Test/Unit/Model/Condition/Sql/BuilderTest.php
+++ app/code/Magento/Rule/Test/Unit/Model/Condition/Sql/BuilderTest.php
@@ -91,30 +91,13 @@ class BuilderTest extends \PHPUnit\Framework\TestCase
             ['getOperatorForValidate', 'getMappedSqlField', 'getAttribute', 'getBindArgumentValue']
         );
 
-        $abstractCondition->expects($this->once())
-            ->method('getMappedSqlField')
-            ->will($this->returnValue('argument'));
+        $abstractCondition->expects($this->once())->method('getMappedSqlField')->will($this->returnValue('argument'));
+        $abstractCondition->expects($this->once())->method('getOperatorForValidate')->will($this->returnValue('&gt;'));
+        $abstractCondition->expects($this->at(1))->method('getAttribute')->will($this->returnValue('attribute'));
+        $abstractCondition->expects($this->at(2))->method('getAttribute')->will($this->returnValue('attribute'));
+        $abstractCondition->expects($this->once())->method('getBindArgumentValue')->will($this->returnValue(10));
 
-        $abstractCondition->expects($this->once())
-            ->method('getOperatorForValidate')
-            ->will($this->returnValue('&gt;'));
-
-        $abstractCondition->expects($this->at(1))
-            ->method('getAttribute')
-            ->will($this->returnValue('attribute'));
-
-        $abstractCondition->expects($this->at(2))
-            ->method('getAttribute')
-            ->will($this->returnValue('attribute'));
-
-        $abstractCondition->expects($this->once())
-            ->method('getBindArgumentValue')
-            ->will($this->returnValue(10));
-
-        $conditions = [
-            $abstractCondition
-        ];
-
+        $conditions = [$abstractCondition];
         $collection = $this->createPartialMock(
             \Magento\Eav\Model\Entity\Collection\AbstractCollection::class,
             [
@@ -122,7 +105,6 @@ class BuilderTest extends \PHPUnit\Framework\TestCase
                 'getSelect'
             ]
         );
-
         $combine = $this->createPartialMock(
             \Magento\Rule\Model\Condition\Combine::class,
             [
@@ -131,10 +113,10 @@ class BuilderTest extends \PHPUnit\Framework\TestCase
                 'getAggregator'
             ]
         );
+
         $resource = $this->createPartialMock(\Magento\Framework\DB\Adapter\Pdo\Mysql::class, ['getConnection']);
         $select = $this->createPartialMock(\Magento\Framework\DB\Select::class, ['where']);
-        $select->expects($this->never())
-            ->method('where');
+        $select->expects($this->never())->method('where');
 
         $connection = $this->getMockForAbstractClass(
             \Magento\Framework\DB\Adapter\AdapterInterface::class,
@@ -143,42 +125,15 @@ class BuilderTest extends \PHPUnit\Framework\TestCase
             false
         );
 
-        $connection->expects($this->once())
-            ->method('quoteInto')
-            ->with(' > ?', 10)
-            ->will($this->returnValue(' > 10'));
-
-        $collection->expects($this->once())
-            ->method('getResource')
-            ->will($this->returnValue($resource));
-
-        $resource->expects($this->once())
-            ->method('getConnection')
-            ->will($this->returnValue($connection));
-
-        $combine->expects($this->once())
-            ->method('getValue')
-            ->willReturn('attribute');
-
-        $combine->expects($this->once())
-            ->method('getAggregator')
-            ->willReturn(' AND ');
-
-        $combine->expects($this->at(0))
-            ->method('getConditions')
-            ->will($this->returnValue($conditions));
-
-        $combine->expects($this->at(1))
-            ->method('getConditions')
-            ->will($this->returnValue($conditions));
-
-        $combine->expects($this->at(2))
-            ->method('getConditions')
-            ->will($this->returnValue($conditions));
-
-        $combine->expects($this->at(3))
-            ->method('getConditions')
-            ->will($this->returnValue($conditions));
+        $connection->expects($this->once())->method('quoteInto')->with(' > ?', 10)->will($this->returnValue(' > 10'));
+        $collection->expects($this->once())->method('getResource')->will($this->returnValue($resource));
+        $resource->expects($this->once())->method('getConnection')->will($this->returnValue($connection));
+        $combine->expects($this->once())->method('getValue')->willReturn('attribute');
+        $combine->expects($this->once())->method('getAggregator')->willReturn(' AND ');
+        $combine->expects($this->at(0))->method('getConditions')->will($this->returnValue($conditions));
+        $combine->expects($this->at(1))->method('getConditions')->will($this->returnValue($conditions));
+        $combine->expects($this->at(2))->method('getConditions')->will($this->returnValue($conditions));
+        $combine->expects($this->at(3))->method('getConditions')->will($this->returnValue($conditions));
 
         $this->_builder->attachConditionToCollection($collection, $combine);
     }
