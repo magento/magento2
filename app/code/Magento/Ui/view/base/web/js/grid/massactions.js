e90bc28be176ec24b027ba05e4d74882738ac1c9
Yevhenii Dumskyi <yevhenii.dumskyi@gmail.com>
2018-11-14 17:41:44 +0200

    Fix static analysis errors

--- app/code/Magento/Ui/view/base/web/js/grid/massactions.js
+++ app/code/Magento/Ui/view/base/web/js/grid/massactions.js
@@ -153,9 +153,9 @@ define([
             var itemsType = data.excludeMode ? 'excluded' : 'selected',
                 selections = {};
 
-            if (itemsType === 'excluded' && data['selected'].length){
-                data['selected'] = _.difference(data['selected'], data['excluded']);
+            if (itemsType === 'excluded' && data.selected && data.selected.length) {
                 itemsType = 'selected';
+                data[itemsType] = _.difference(data.selected, data.excluded);
             }
 
             selections[itemsType] = data[itemsType];
