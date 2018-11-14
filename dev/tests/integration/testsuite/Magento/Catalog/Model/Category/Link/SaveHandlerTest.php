af6f54f784d6392b5f36d72c672721eaa6fd3831
Dmytro Horytskyi <horytsky@adobe.com>
2018-11-14 10:02:09 +0200

    MAGETWO-96288: [Magento Cloud] Cannot update category positions via REST API

--- dev/tests/integration/testsuite/Magento/Catalog/Model/Category/Link/SaveHandlerTest.php
+++ dev/tests/integration/testsuite/Magento/Catalog/Model/Category/Link/SaveHandlerTest.php
@@ -3,7 +3,7 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace  Magento\Catalog\Model\Category\Link;
+namespace Magento\Catalog\Model\Category\Link;
 
 use Magento\Catalog\Api\Data\CategoryLinkInterfaceFactory;
 use Magento\Catalog\Api\Data\ProductInterface;
