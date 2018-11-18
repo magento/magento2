b9790c67d25af83e8cbaecad223c4317d4337e8d
Stanislav Idolov <sidolov@magento.com>
2018-11-18 11:50:50 +0200

    Fixed code style issues

--- app/code/Magento/Review/Model/ResourceModel/Rating.php
+++ app/code/Magento/Review/Model/ResourceModel/Rating.php
@@ -425,7 +425,7 @@ class Rating extends \Magento\Framework\Model\ResourceModel\Db\AbstractDb
 
         $data = $connection->fetchAll($select, [':review_id' => $object->getReviewId()]);
 
-        $currentStore = ($this->_storeManager->isSingleStoreMode()) ? $this->_storeManager->getStore()->getId() : null ;
+        $currentStore = $this->_storeManager->isSingleStoreMode() ? $this->_storeManager->getStore()->getId() : null;
 
         if ($onlyForCurrentStore) {
             foreach ($data as $row) {
