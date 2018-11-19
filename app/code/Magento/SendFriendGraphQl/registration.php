b6d9adeed9bd275e0e819c33af35dff134b17834
Valeriy Nayda <vnayda@magento.com>
2018-11-19 20:40:10 +0200

    GraphQL-202: Products: Email to a Friend
    -- fix static tests

--- app/code/Magento/SendFriendGraphQl/registration.php
+++ app/code/Magento/SendFriendGraphQl/registration.php
@@ -7,4 +7,4 @@ declare(strict_types=1);
 
 use Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Magento_SendFriendGraphGl', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Magento_SendFriendGraphQl', __DIR__);
