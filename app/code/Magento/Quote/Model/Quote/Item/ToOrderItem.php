ef3081749d98fff50d081b1f1518dcfb27a4c44d
Mahesh Singh <mahesh721@webkul.com>
2018-11-20 21:43:30 +0530

    new fieldset added

--- app/code/Magento/Quote/Model/Quote/Item/ToOrderItem.php
+++ app/code/Magento/Quote/Model/Quote/Item/ToOrderItem.php
@@ -63,6 +63,16 @@ class ToOrderItem
             'to_order_item',
             $item
         );
+        if ($item instanceof \Magento\Quote\Model\Quote\Address\Item) {
+            $orderItemData= array_merge(
+                $orderItemData,
+               $this->objectCopyService->getDataFromFieldset(
+                   'quote_convert_address_item',
+                   'to_order_item',
+                   $item
+               )
+           );
+       }
         if (!$item->getNoDiscount()) {
             $data = array_merge(
                 $data,
@@ -73,10 +83,7 @@ class ToOrderItem
                 )
             );
         }
-        if ($item instanceof \Magento\Quote\Model\Quote\Address\Item) {
-            $orderItemData['quote_item_id'] = $item->getQuoteItemId();
-        }
-
+        
         $orderItem = $this->orderItemFactory->create();
         $this->dataObjectHelper->populateWithArray(
             $orderItem,
