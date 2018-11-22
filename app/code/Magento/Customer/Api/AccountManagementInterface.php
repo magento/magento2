500fd029b14978707acca6830c08d65108e16f73
Mikalai Shostka <mikalai_shostka@epam.com>
2018-11-22 17:24:46 +0300

    MAGETWO-96125: Points not added with New Account Email Confirmation enabled
    - Change template functionality

--- app/code/Magento/Customer/Api/AccountManagementInterface.php
+++ app/code/Magento/Customer/Api/AccountManagementInterface.php
@@ -31,7 +31,7 @@ interface AccountManagementInterface
      * @param \Magento\Customer\Api\Data\CustomerInterface $customer
      * @param string $password
      * @param string $redirectUrl
-     * @param array $extensions
+     * @param string[] $extensions
      * @return \Magento\Customer\Api\Data\CustomerInterface
      * @throws \Magento\Framework\Exception\LocalizedException
      */
@@ -50,7 +50,7 @@ interface AccountManagementInterface
      * @param string $hash Password hash that we can save directly
      * @param string $redirectUrl URL fed to welcome email templates. Can be used by templates to, for example, direct
      *                            the customer to a product they were looking at after pressing confirmation link.
-     * @param array $extensions
+     * @param string[] $extensions
      * @return \Magento\Customer\Api\Data\CustomerInterface
      * @throws \Magento\Framework\Exception\InputException If bad input is provided
      * @throws \Magento\Framework\Exception\State\InputMismatchException If the provided email is already used
