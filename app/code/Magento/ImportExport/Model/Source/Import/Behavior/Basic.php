c0664ecc603ba73e2efa4c2da5cc85deb52c3767
Federico Rivollier <federivo@gmail.com>
2018-11-22 18:38:25 -0300

    Added notes and tooltip to import behavior field in import form

--- app/code/Magento/ImportExport/Model/Source/Import/Behavior/Basic.php
+++ app/code/Magento/ImportExport/Model/Source/Import/Behavior/Basic.php
@@ -39,7 +39,9 @@ class Basic extends \Magento\ImportExport\Model\Source\Import\AbstractBehavior
     public function getNotes($entityCode)
     {
         $messages = ['catalog_product' => [
-            \Magento\ImportExport\Model\Import::BEHAVIOR_REPLACE => __("Note: Product IDs will be regenerated.")
+            \Magento\ImportExport\Model\Import::BEHAVIOR_APPEND => __("New product data is added to the existing product data for the existing entries in the database. All fields except sku can be updated."),
+            \Magento\ImportExport\Model\Import::BEHAVIOR_REPLACE => __("The existing product data is replaced with new data. <b>Exercise caution when replacing data because the existing product data will be completely cleared and all references in the system will be lost.</b>"),
+            \Magento\ImportExport\Model\Import::BEHAVIOR_DELETE => __("Any entities in the import data that already exist in the database are deleted from the database."),
         ]];
         return isset($messages[$entityCode]) ? $messages[$entityCode] : [];
     }
