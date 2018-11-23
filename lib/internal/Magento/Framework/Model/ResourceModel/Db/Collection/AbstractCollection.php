daf5fb9b7ca965e6f24ddd05434d051a08e3b8d5
Shikha Mishra <shikhamishra@cedcoss.com>
2018-11-23 11:02:55 +0530

    Updated AbstractCollection.php
    added getColumnValues function  in collection for getting all values with reset limits

--- lib/internal/Magento/Framework/Model/ResourceModel/Db/Collection/AbstractCollection.php
+++ lib/internal/Magento/Framework/Model/ResourceModel/Db/Collection/AbstractCollection.php
@@ -492,6 +492,23 @@ abstract class AbstractCollection extends AbstractDb implements SourceProviderIn
         $idsSelect->columns($this->getResource()->getIdFieldName(), 'main_table');
         return $this->getConnection()->fetchCol($idsSelect, $this->_bindParams);
     }
+    
+    /**
+     * Retrieve field values for collection
+     *
+     * @return array
+     */
+    public function getColumnValues($fieldName)
+    {
+        $idsSelect = clone $this->getSelect();
+        $idsSelect->reset(\Magento\Framework\DB\Select::ORDER);
+        $idsSelect->reset(\Magento\Framework\DB\Select::LIMIT_COUNT);
+        $idsSelect->reset(\Magento\Framework\DB\Select::LIMIT_OFFSET);
+        $idsSelect->reset(\Magento\Framework\DB\Select::COLUMNS);
+
+        $idsSelect->columns($fieldName, 'main_table');
+        return $this->getConnection()->fetchCol($idsSelect, $this->_bindParams);
+    }
 
     /**
      * Join table to collection select
