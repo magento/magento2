c6fdd50a082d1aae5de76958c35aa9da3e1e6212
Jasper Zeinstra <jasper.zeinstra@mediact.nl>
2018-11-23 14:39:25 +0100

    Add unit test classes

--- app/code/Magento/ConfigurableProduct/Model/Plugin/Frontend/ProductIdentitiesExtender.php
+++ app/code/Magento/ConfigurableProduct/Model/Plugin/Frontend/ProductIdentitiesExtender.php
@@ -8,7 +8,6 @@ declare(strict_types=1);
 namespace Magento\ConfigurableProduct\Model\Plugin\Frontend;
 
 use Magento\ConfigurableProduct\Model\Product\Type\Configurable;
-use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Catalog\Model\Product;
 
 /**
@@ -22,18 +21,11 @@ class ProductIdentitiesExtender
     private $configurableType;
 
     /**
-     * @var ProductRepositoryInterface
-     */
-    private $productRepository;
-
-    /**
      * @param Configurable $configurableType
-     * @param ProductRepositoryInterface $productRepository
      */
-    public function __construct(Configurable $configurableType, ProductRepositoryInterface $productRepository)
+    public function __construct(Configurable $configurableType)
     {
         $this->configurableType = $configurableType;
-        $this->productRepository = $productRepository;
     }
 
     /**
