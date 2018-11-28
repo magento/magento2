540f51c63e2bcf97c38a83b755028ff15e4aa50d
OlgaVasyltsun <olga.vasyltsun@transoftgroup.com>
2018-11-28 13:40:09 +0200

    MAGETWO-96141: "Custom price" can't be removed during order creation in admin

--- dev/tests/integration/testsuite/Magento/Sales/_files/quote_with_custom_price_rollback.php
+++ dev/tests/integration/testsuite/Magento/Sales/_files/quote_with_custom_price_rollback.php
@@ -4,3 +4,21 @@
  * See COPYING.txt for license details.
  */
 include __DIR__ . '/quote_rollback.php';
+
+/** @var \Magento\Framework\Registry $registry */
+$registry->unregister('isSecureArea');
+$registry->register('isSecureArea', true);
+
+/** @var \Magento\Catalog\Api\ProductRepositoryInterface $productRepository */
+$productRepository = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()
+    ->create(\Magento\Catalog\Api\ProductRepositoryInterface::class);
+
+try {
+    $product = $productRepository->get('simple_quote_custom_price', false, null, true);
+    $productRepository->delete($product);
+} catch (\Magento\Framework\Exception\NoSuchEntityException $exception) {
+    //Product already removed
+}
+
+$registry->unregister('isSecureArea');
+$registry->register('isSecureArea', false);
