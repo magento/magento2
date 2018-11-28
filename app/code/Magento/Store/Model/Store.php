dfe294622548712c71368f1fa6caa0fd711d0cbe
Nikita Chubukov <nikita_chubukov@epam.com>
2018-11-28 15:52:07 +0300

    MAGETWO-96413: Restricted Admin User Backend Order Creation Issue
    - Added condition to definition current currency code

--- app/code/Magento/Store/Model/Store.php
+++ app/code/Magento/Store/Model/Store.php
@@ -889,7 +889,10 @@ class Store extends AbstractExtensibleModel implements
         if (in_array($code, $this->getAvailableCurrencyCodes())) {
             $this->_getSession()->setCurrencyCode($code);
 
-            $defaultCode = $this->_storeManager->getWebsite()->getDefaultStore()->getDefaultCurrency()->getCode();
+            $defaultCode = ($this->_storeManager->getStore() !== null)
+                ? $this->_storeManager->getStore()->getDefaultCurrency()->getCode()
+                : $this->_storeManager->getWebsite()->getDefaultStore()->getDefaultCurrency()->getCode();
+            
             $this->_httpContext->setValue(Context::CONTEXT_CURRENCY, $code, $defaultCode);
         }
         return $this;
