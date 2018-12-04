255b8f226400a1a5f2e2d93b877bfffe1a8f60ea
Stepan Furman <furman.stepan@gmail.com>
2018-12-04 20:30:20 +0200

    split class, format changes

--- app/code/Magento/CustomerGraphQl/Model/Resolver/CreateCustomer.php
+++ app/code/Magento/CustomerGraphQl/Model/Resolver/CreateCustomer.php
@@ -7,94 +7,62 @@ declare(strict_types=1);
 
 namespace Magento\CustomerGraphQl\Model\Resolver;
 
-use Magento\Authorization\Model\UserContextInterface;
-use Magento\Customer\Api\AccountManagementInterface;
-use Magento\Customer\Api\Data\CustomerInterface;
-use Magento\Customer\Api\Data\CustomerInterfaceFactory;
 use Magento\CustomerGraphQl\Model\Customer\ChangeSubscriptionStatus;
+use Magento\CustomerGraphQl\Model\Customer\CreateAccount;
 use Magento\CustomerGraphQl\Model\Customer\CustomerDataProvider;
-use Magento\Framework\Api\DataObjectHelper;
-use Magento\Framework\Exception\LocalizedException;
-use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\CustomerGraphQl\Model\Customer\SetUpUserContext;
 use Magento\Framework\Exception\State\InputMismatchException;
 use Magento\Framework\GraphQl\Config\Element\Field;
 use Magento\Framework\GraphQl\Exception\GraphQlInputException;
-use Magento\Framework\GraphQl\Query\Resolver\ContextInterface;
-use Magento\Framework\GraphQl\Query\Resolver\Value;
 use Magento\Framework\GraphQl\Query\ResolverInterface;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
 use Magento\Framework\Validator\Exception as ValidatorException;
-use Magento\Newsletter\Model\SubscriberFactory;
-use Magento\Store\Model\StoreManagerInterface;
 
+/**
+ * Create customer account resolver
+ */
 class CreateCustomer implements ResolverInterface
 {
     /**
      * @var CustomerDataProvider
      */
     private $customerDataProvider;
-    /**
-     * @var AccountManagementInterface
-     */
-    private $accountManagement;
-    /**
-     * @var CustomerInterfaceFactory
-     */
-    private $customerFactory;
-    /**
-     * @var DataObjectHelper
-     */
-    private $dataObjectHelper;
-    /**
-     * @var StoreManagerInterface
-     */
-    private $storeManager;
-    /**
-     * @var SubscriberFactory
-     */
-    private $subscriberFactory;
+
     /**
      * @var ChangeSubscriptionStatus
      */
     private $changeSubscriptionStatus;
 
     /**
-     * @param DataObjectHelper $dataObjectHelper
-     * @param CustomerInterfaceFactory $customerFactory
-     * @param AccountManagementInterface $accountManagement
-     * @param StoreManagerInterface $storeManager
-     * @param SubscriberFactory $subscriberFactory
+     * @var CreateAccount
+     */
+    private $createAccount;
+
+    /**
+     * @var SetUpUserContext
+     */
+    private $setUpUserContext;
+
+    /**
      * @param CustomerDataProvider $customerDataProvider
      * @param ChangeSubscriptionStatus $changeSubscriptionStatus
+     * @param SetUpUserContext $setUpUserContext
+     * @param CreateAccount $createAccount
      */
     public function __construct(
-        DataObjectHelper $dataObjectHelper,
-        CustomerInterfaceFactory $customerFactory,
-        AccountManagementInterface $accountManagement,
-        StoreManagerInterface $storeManager,
-        SubscriberFactory $subscriberFactory,
         CustomerDataProvider $customerDataProvider,
-        ChangeSubscriptionStatus $changeSubscriptionStatus
+        ChangeSubscriptionStatus $changeSubscriptionStatus,
+        SetUpUserContext $setUpUserContext,
+        CreateAccount $createAccount
     ) {
         $this->customerDataProvider = $customerDataProvider;
-        $this->accountManagement = $accountManagement;
-        $this->customerFactory = $customerFactory;
-        $this->dataObjectHelper = $dataObjectHelper;
-        $this->storeManager = $storeManager;
-        $this->subscriberFactory = $subscriberFactory;
         $this->changeSubscriptionStatus = $changeSubscriptionStatus;
+        $this->createAccount = $createAccount;
+        $this->setUpUserContext = $setUpUserContext;
     }
 
     /**
-     * @param Field $field
-     * @param ContextInterface $context
-     * @param ResolveInfo $info
-     * @param array|null $value
-     * @param array|null $args
-     * @return array|Value|mixed
-     * @throws GraphQlInputException
-     * @throws LocalizedException
-     * @throws NoSuchEntityException
+     * @inheritdoc
      */
     public function resolve(
         Field $field,
@@ -107,9 +75,9 @@ class CreateCustomer implements ResolverInterface
             throw new GraphQlInputException(__('"input" value should be specified'));
         }
         try {
-            $customer = $this->createUserAccount($args);
+            $customer = $this->createAccount->execute($args);
             $customerId = (int)$customer->getId();
-            $this->setUpUserContext($context, $customer);
+            $this->setUpUserContext->execute($context, $customer);
             if (array_key_exists('is_subscribed', $args['input'])) {
                 if ($args['input']['is_subscribed']) {
                     $this->changeSubscriptionStatus->execute($customerId, true);
@@ -123,38 +91,5 @@ class CreateCustomer implements ResolverInterface
         }
 
         return ['customer' => $data];
-    }
-
-    /**
-     * @param $args
-     * @return CustomerInterface
-     * @throws LocalizedException
-     * @throws NoSuchEntityException
-     */
-    private function createUserAccount($args)
-    {
-        $customerDataObject = $this->customerFactory->create();
-        $this->dataObjectHelper->populateWithArray(
-            $customerDataObject,
-            $args['input'],
-            CustomerInterface::class
-        );
-        $store = $this->storeManager->getStore();
-        $customerDataObject->setWebsiteId($store->getWebsiteId());
-        $customerDataObject->setStoreId($store->getId());
-
-        $password = array_key_exists('password', $args['input']) ? $args['input']['password'] : null;
-
-        return $this->accountManagement->createAccount($customerDataObject, $password);
-    }
-
-    /**
-     * @param $context
-     * @param CustomerInterface $customer
-     */
-    private function setUpUserContext($context, $customer)
-    {
-        $context->setUserId((int)$customer->getId());
-        $context->setUserType(UserContextInterface::USER_TYPE_CUSTOMER);
     }
 }
