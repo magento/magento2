7c5373cc595c8e3b877084542af3127f18f486c2
Alexey Yakimovich <yakimovich@almagy.com>
2018-12-07 17:40:08 +0300

    MAGETWO-95837: Product is added to Wish List with Attributes selected however they were unselected on PDP before
    - Fixed static and functional tests;

--- app/code/Magento/Wishlist/view/frontend/web/js/add-to-wishlist.js
+++ app/code/Magento/Wishlist/view/frontend/web/js/add-to-wishlist.js
@@ -154,15 +154,14 @@ define([
                 $.each(elementValue, function (key, option) {
                     data[elementName + '[' + option + ']'] = option;
                 });
-            } else {
-                if (elementName.substr(elementName.length - 2) == '[]') { //eslint-disable-line eqeqeq, max-depth
-                    elementName = elementName.substring(0, elementName.length - 2);
+            } else if (elementName.substr(elementName.length - 2) == '[]') { //eslint-disable-line eqeqeq, max-depth
+                elementName = elementName.substring(0, elementName.length - 2);
 
-                    data[elementName + '[' + elementValue + ']'] = elementValue;
-                } else {
-                    data[elementName] = elementValue;
-                }
+                data[elementName + '[' + elementValue + ']'] = elementValue;
+            } else {
+                data[elementName] = elementValue;
             }
+
 
             return data;
         },
