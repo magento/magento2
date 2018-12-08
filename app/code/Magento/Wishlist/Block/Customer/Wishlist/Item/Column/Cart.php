334d71368b2d7ad8b060051b7009c730825f9c4f
Khodu Vaishnav <khodu.vaishnav@krishtechnolabs.com>
2018-12-08 16:50:22 +0530

    patch-9155 : set default qty for add to cart from wishlist.
    patch-9155 : set default qty for add to cart from wishlist.
    
    patch-9155 : set default qty for add to cart in wishlist.

--- app/code/Magento/Wishlist/Block/Customer/Wishlist/Item/Column/Cart.php
+++ app/code/Magento/Wishlist/Block/Customer/Wishlist/Item/Column/Cart.php
@@ -17,6 +17,28 @@ use Magento\Catalog\Controller\Adminhtml\Product\Initialization\StockDataFilter;
 class Cart extends \Magento\Wishlist\Block\Customer\Wishlist\Item\Column
 {
     /**
+     * @var \Magento\Catalog\Block\Product\View
+     */
+    protected $productView;
+
+    /**
+     *
+     * @param \Magento\Catalog\Block\Product\Context $context
+     * @param \Magento\Framework\App\Http\Context $httpContext
+     * @param array $data
+     * @param \Magento\Catalog\Block\Product\View $productView
+     */
+    public function __construct(
+        \Magento\Catalog\Block\Product\Context $context,
+        \Magento\Framework\App\Http\Context $httpContext,
+        \Magento\Catalog\Block\Product\View $productView = null,
+        array $data = []
+    ) {
+        $this->productView = $productView ?:
+                \Magento\Framework\App\ObjectManager::getInstance()->get(\Magento\Catalog\Block\Product\View::class);
+        parent::__construct($context, $httpContext, $data);
+    }
+    /**
      * Returns qty to show visually to user
      *
      * @param \Magento\Wishlist\Model\Item $item
@@ -25,6 +47,8 @@ class Cart extends \Magento\Wishlist\Block\Customer\Wishlist\Item\Column
     public function getAddToCartQty(\Magento\Wishlist\Model\Item $item)
     {
         $qty = $item->getQty();
+        $qty = $qty < $this->productView->getProductDefaultQty($this->getProductItem())
+                ? $this->productView->getProductDefaultQty($this->getProductItem()) : $qty ;
         return $qty ? $qty : 1;
     }
 
