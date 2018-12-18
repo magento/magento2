b40ca8a889db99d8a8adbdb564097c4adc673194
Aliaksei_Manenak <Aliaksei_Manenak@epam.com>
2018-12-18 15:54:28 +0300

    MAGETWO-60910: Session initialized during reindex from CLI and result fatal error
    - Don't start session if cli used.

--- dev/tests/integration/etc/di/preferences/ce.php
+++ dev/tests/integration/etc/di/preferences/ce.php
@@ -27,4 +27,5 @@ return [
     \Magento\Framework\App\Config\ScopeConfigInterface::class => \Magento\TestFramework\App\Config::class,
     \Magento\Framework\App\ResourceConnection\ConfigInterface::class =>
         \Magento\Framework\App\ResourceConnection\Config::class,
+    \Magento\Framework\Session\SessionStartChecker::class => \Magento\TestFramework\Session\SessionStartChecker::class
 ];
