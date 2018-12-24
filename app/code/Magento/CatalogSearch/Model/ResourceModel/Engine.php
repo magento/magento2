61120df0704530e1f49b4e839e307455bce3c55f
Oksana_Kremen <Oksana_Kremen@epam.com>
2018-12-24 15:49:19 +0200

    MAGETWO-91750: Multiselect attribute values is not searchable under Quick Search when more than one value is selected
    - Added possibility to retrieve multiselect option values for catalog fulltext search reindexation

--- app/code/Magento/CatalogSearch/Model/ResourceModel/Engine.php
+++ app/code/Magento/CatalogSearch/Model/ResourceModel/Engine.php
@@ -110,7 +110,9 @@ class Engine implements EngineInterface
             && in_array($attribute->getFrontendInput(), ['text', 'textarea'])
         ) {
             $result = $value;
-        } elseif ($this->isTermFilterableAttribute($attribute)) {
+        } elseif ($this->isTermFilterableAttribute($attribute)
+            || ($attribute->getIsSearchable() && in_array($attribute->getFrontendInput(), ['select', 'multiselect']))
+        ) {
             $result = '';
         }
 
@@ -119,6 +121,7 @@ class Engine implements EngineInterface
 
     /**
      * Prepare index array as a string glued by separator
+     *
      * Support 2 level array gluing
      *
      * @param array $index
