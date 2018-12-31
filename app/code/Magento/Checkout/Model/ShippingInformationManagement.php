fb405b1a73e2a516bf4d9f23e169e17759485681
OlgaVasyltsun <olga.vasyltsun@transoftgroup.com>
2018-12-31 09:14:48 +0200

    MAGETWO-72839: Customer can't see available Payment Method for specific country

--- app/code/Magento/Checkout/Model/ShippingInformationManagement.php
+++ app/code/Magento/Checkout/Model/ShippingInformationManagement.php
@@ -150,7 +150,7 @@ class ShippingInformationManagement implements \Magento\Checkout\Api\ShippingInf
             $address->setCustomerAddressId(null);
         }
 
-        if (!$billingAddress->getCustomerAddressId()) {
+        if ($billingAddress && !$billingAddress->getCustomerAddressId()) {
             $billingAddress->setCustomerAddressId(null);
         }
 
