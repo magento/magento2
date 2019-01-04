69eaa77c837ac8a19fb7f2f3dccd9d60dd286b41
Alexey Yakimovich <yakimovich@almagy.com>
2019-01-04 12:00:55 +0300

    MAGETWO-94556: Undefined variables during product save
    - Fixed js unit test;

--- dev/tests/js/jasmine/tests/app/code/Magento/ConfigurableProduct/view/adminhtml/web/js/variations/variations.test.js
+++ dev/tests/js/jasmine/tests/app/code/Magento/ConfigurableProduct/view/adminhtml/web/js/variations/variations.test.js
@@ -164,8 +164,8 @@ define([
 
             variation.serializeData();
 
-            expect(variation.source.data['configurable-matrix']).toBeUndefined();
-            expect(variation.source.data['associated_product_ids']).toBeUndefined();
+            expect(variation.source.data['configurable-matrix']).toEqual(matrix);
+            expect(variation.source.data['associated_product_ids']).toEqual(ids);
             expect(variation.source.data['configurable-matrix-serialized']).toEqual(resultMatrix);
             expect(variation.source.data['associated_product_ids_serialized']).toEqual(resultIds);
 
