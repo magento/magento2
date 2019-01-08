0f17e028fbe391bc3ee26e92b34140a8ab644724
Ji Lu <jilu1@adobe.com>
2019-01-08 17:19:50 -0600

    MQE-1352: bug fix in dev/tests/functional/utils/command.php etc

--- dev/tests/functional/utils/command.php
+++ dev/tests/functional/utils/command.php
@@ -3,31 +3,25 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
-
+include __DIR__ . '/authenticate.php';
 require_once __DIR__ . '/../../../../app/bootstrap.php';
 
 use Symfony\Component\Console\Input\StringInput;
 use Symfony\Component\Console\Output\NullOutput;
 
 if (!empty($_POST['token']) && !empty($_POST['command'])) {
-    $magentoObjectManagerFactory = \Magento\Framework\App\Bootstrap::createObjectManagerFactory(BP, $_SERVER);
-    $magentoObjectManager = $magentoObjectManagerFactory->create($_SERVER);
-    $tokenModel = $magentoObjectManager->get(\Magento\Integration\Model\Oauth\Token::class);
-
-    $tokenPassedIn = urldecode($_POST['token']);
-    $command = urldecode($_POST['command']);
-
-    // Token returned will be null if the token we passed in is invalid
-    $tokenFromMagento = $tokenModel->loadByToken($tokenPassedIn)->getToken();
-    if (!empty($tokenFromMagento) && ($tokenFromMagento == $tokenPassedIn)) {
+    if (authenticate(urldecode($_POST['token']))) {
+        $command = urldecode($_POST['command']);
+        $magentoObjectManagerFactory = \Magento\Framework\App\Bootstrap::createObjectManagerFactory(BP, $_SERVER);
+        $magentoObjectManager = $magentoObjectManagerFactory->create($_SERVER);
         $cli = $magentoObjectManager->create(\Magento\Framework\Console\Cli::class);
         $input = new StringInput(escapeshellcmd($command));
         $input->setInteractive(false);
         $output = new NullOutput();
         $cli->doRun($input, $output);
     } else {
-        throw new \Exception("Command not unauthorized.");
+        echo "Command not unauthorized.";
     }
 } else {
-    throw new \InvalidArgumentException("Command POST parameters are not set.");
+    echo "'token' or 'command' parameter is not set.";
 }
