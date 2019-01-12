eab547224baf316903b5a469463969625af93340
Tristan Hofman <hofmantristan@gmail.com>
2019-01-12 11:59:02 +0100

    added missing spain_country_id constant

--- app/code/Magento/Shipping/Model/Carrier/AbstractCarrierOnline.php
+++ app/code/Magento/Shipping/Model/Carrier/AbstractCarrierOnline.php
@@ -29,6 +29,8 @@ abstract class AbstractCarrierOnline extends AbstractCarrier
 
     const GUAM_REGION_CODE = 'GU';
 
+    const SPAIN_COUNTRY_ID = 'ES';
+
     const CANARY_ISLANDS_COUNTRY_ID = 'IC';
 
     const SANTA_CRUZ_DE_TENERIFE_REGION_ID = 'Santa Cruz de Tenerife';
