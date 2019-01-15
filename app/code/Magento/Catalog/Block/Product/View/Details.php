1ad7d4598504fe89b2f0899a966f61fbdf5c64d5
nmalevanec <mikola.malevanec@transoftgroup.com>
2019-01-15 13:57:00 +0200

    Fix static test.

--- app/code/Magento/Catalog/Block/Product/View/Details.php
+++ app/code/Magento/Catalog/Block/Product/View/Details.php
@@ -6,28 +6,27 @@
 
 declare(strict_types=1);
 
-/**
- * Product details block
- * Holds a group of blocks to show as tabs
- *
- * @author     Magento Core Team <core@magentocommerce.com>
- */
-
 namespace Magento\Catalog\Block\Product\View;
 
 /**
+ * Product details block.
+ *
+ * Holds a group of blocks to show as tabs.
+ *
  * @api
  */
 class Details extends \Magento\Framework\View\Element\Template
 {
     /**
+     * Get sorted child block names.
+     *
      * @param string $groupName
-     * @param $callback
+     * @param string $callback
      * @throws \Magento\Framework\Exception\LocalizedException
      *
      * @return array
      */
-    public function getGroupSortedChildNames(string $groupName, $callback): array
+    public function getGroupSortedChildNames(string $groupName, string $callback): array
     {
         $groupChildNames = $this->getGroupChildNames($groupName, $callback);
         $layout = $this->getLayout();
