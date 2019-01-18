ec8c973fb1665206eebe603bf64e3f6d13fcdea4
Yannis Livasov <ilib@mail.ru>
2019-01-18 14:18:42 +0300

    Shortening the list of currencies
    This will shorten the list of allowed currencies showing only installed currencies
    (Replace closed pr#19946)

--- app/code/Magento/Config/Model/Config/Source/Locale/Currency.php
+++ app/code/Magento/Config/Model/Config/Source/Locale/Currency.php
@@ -24,13 +24,28 @@ class Currency implements \Magento\Framework\Option\ArrayInterface
      * @var \Magento\Framework\Locale\ListsInterface
      */
     protected $_localeLists;
+    
+    /**
+     * @var \Magento\Framework\App\Config\ScopeConfigInterface
+     */
+    protected $_config;
+
+    /**
+     * @var Currency
+     */
+    protected $_installedCurrencies;
+
 
     /**
      * @param \Magento\Framework\Locale\ListsInterface $localeLists
      */
-    public function __construct(\Magento\Framework\Locale\ListsInterface $localeLists)
+    public function __construct(
+    	\Magento\Framework\Locale\ListsInterface $localeLists,
+        \Magento\Framework\App\Config\ScopeConfigInterface $config
+    )
     {
         $this->_localeLists = $localeLists;
+        $this->_config = $config;
     }
 
     /**
@@ -41,7 +56,36 @@ class Currency implements \Magento\Framework\Option\ArrayInterface
         if (!$this->_options) {
             $this->_options = $this->_localeLists->getOptionCurrencies();
         }
-        $options = $this->_options;
+        $options = [];
+        $selected = $this->_getInstalledCurrencies();
+        
+        foreach ($this->_options as $option) {
+            if (!in_array($option['value'], $selected)) {
+                continue;
+            }
+            $options[] = $option;
+        }   
         return $options;
     }
+    
+    /**
+     * Retrieve Installed Currencies
+     *
+     * @return string[]
+     */
+    protected function _getInstalledCurrencies()
+    {
+        if (!$this->_installedCurrencies)
+        {
+        	$this->_installedCurrencies = explode(
+            	',',
+            	$this->_config->getValue(
+                	'system/currency/installed',
+                	\Magento\Store\Model\ScopeInterface::SCOPE_STORE
+            	)
+        	);
+        }
+        return $this->_installedCurrencies;
+    }
+
 }
