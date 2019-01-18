50c50d44a69eeb8f72fc52c87ee6bf319e5e605a
Mastiuhin Olexandr <mastiuhin.olexandr@transoftgroup.com>
2019-01-18 11:29:41 +0200

    MAGETWO-97091: Zip code is not validated for address entered in My Account and for new address entered during checkout

--- app/code/Magento/Customer/Block/DataProviders/PostCodesPatternsAttributeData.php
+++ app/code/Magento/Customer/Block/DataProviders/PostCodesPatternsAttributeData.php
@@ -7,7 +7,7 @@ declare(strict_types=1);
 
 namespace Magento\Customer\Block\DataProviders;
 
-use Magento\Framework\Serialize\Serializer\Json;
+use Magento\Framework\Serialize\SerializerInterface;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
 use Magento\Directory\Model\Country\Postcode\Config as PostCodeConfig;
 
@@ -22,7 +22,7 @@ class PostCodesPatternsAttributeData implements ArgumentInterface
     private $postCodeConfig;
 
     /**
-     * @var Json
+     * @var SerializerInterface
      */
     private $serializer;
 
@@ -30,9 +30,9 @@ class PostCodesPatternsAttributeData implements ArgumentInterface
      * Constructor
      *
      * @param PostCodeConfig $postCodeConfig
-     * @param Json $serializer
+     * @param SerializerInterface $serializer
      */
-    public function __construct(PostCodeConfig $postCodeConfig, Json $serializer)
+    public function __construct(PostCodeConfig $postCodeConfig, SerializerInterface $serializer)
     {
         $this->postCodeConfig = $postCodeConfig;
         $this->serializer = $serializer;
