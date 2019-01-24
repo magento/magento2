a9e303fe45f07ca6c0fba9e336d86b46da119e85
pganapat <prabhuramgr28493@gmail.com>
2019-01-24 12:36:04 -0600

    MC-11034: Enable Paypal Credit option should be disabled/greyed out when a non-US merchant is chosen - Moved logic from source to frontend model. - Addressed review comments.

---
+++ app/code/Magento/Paypal/Block/Adminhtml/System/Config/MultiSelect/DisabledFundingOptions.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pganapat
+ * Date: 1/23/19
+ * Time: 3:21 PM
+ */
+
+namespace Magento\Paypal\Block\Adminhtml\System\Config\MultiSelect;
+
+use Magento\Paypal\Model\Config\StructurePlugin;
+
+/**
+ * Class DisabledFundingOptions
+
+ * @package Magento\Paypal\Block\Adminhtml\System\Config\MultiSelect
+ */
+class DisabledFundingOptions extends \Magento\Config\Block\System\Config\Form\Field
+{
+    const FIELD_CONFIG_PATH = 'general/country/default';
+    /**
+     * DisabledFundingOptions constructor.
+     * @param \Magento\Backend\Block\Template\Context $context
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Backend\Block\Template\Context $context,
+        $data = []
+    ) {
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * Render country field considering request parameter
+     *
+     * @param \Magento\Framework\Data\Form\Element\AbstractElement $element
+     * @return string
+     */
+    public function render(\Magento\Framework\Data\Form\Element\AbstractElement $element)
+    {
+        if (!$this->isSelectedMerchantCountry('US')) {
+            $fundingOptions = $element->getValues();
+            $element->setValues($this->filterValuesForPaypalCredit($fundingOptions));
+        }
+        return parent::render($element);
+    }
+
+    /**
+     * Filters array for PAYPAL_FUNDING_CREDIT
+     *
+     * @param array $options
+     * @return array
+     */
+    private function filterValuesForPaypalCredit($options)
+    {
+        return array_filter($options, function ($opt) {
+            return ($opt['value'] != 'CREDIT');
+        });
+    }
+
+    /**
+     * Checks for chosen Merchant country from the config/url
+     *
+     * @param string $country
+     * @return bool
+     */
+    private function isSelectedMerchantCountry($country): bool
+    {
+        $paypalCountry = $this->getRequest()->getParam(StructurePlugin::REQUEST_PARAM_COUNTRY);
+        $defaultCountry = $this->_scopeConfig->getValue(self::FIELD_CONFIG_PATH);
+        if ($paypalCountry) {
+            return $paypalCountry === $country;
+        }
+        return $defaultCountry === $country;
+    }
+}
--- app/code/Magento/Paypal/Model/System/Config/Source/DisableFundingOptions.php
+++ app/code/Magento/Paypal/Model/System/Config/Source/DisableFundingOptions.php
@@ -7,41 +7,21 @@ declare(strict_types=1);
 
 namespace Magento\Paypal\Model\System\Config\Source;
 
-use Magento\Paypal\Model\Config\StructurePlugin;
-
 /**
  * Get disable funding options
  */
-class DisableFundingOptions implements \Magento\Framework\Data\OptionSourceInterface
+class DisableFundingOptions
 {
-    /**
-     * @var \Magento\Paypal\Model\ConfigFactory
-     */
-    protected $_configFactory;
-
-    protected $requestCountry;
-
-    protected $http;
-
-    /**
-     * DisableFundingOptions constructor.
-     * @param \Magento\Paypal\Model\ConfigFactory $configFactory
-     * @param \Magento\Framework\App\Request\Http $http
-     */
-    public function __construct(
-        \Magento\Paypal\Model\ConfigFactory $configFactory,
-        \Magento\Framework\App\Request\Http $http
-    ) {
-        $this->_configFactory = $configFactory;
-        $this->http = $http;
-    }
-
     /**
      * @inheritdoc
      */
     public function toOptionArray(): array
     {
-        $defaultFundingOptions = [
+        return [
+            [
+                'value' => 'CREDIT',
+                'label' => __('PayPal Credit')
+            ],
             [
                 'value' => 'CARD',
                 'label' => __('PayPal Guest Checkout Credit Card Icons')
@@ -51,40 +31,5 @@ class DisableFundingOptions implements \Magento\Framework\Data\OptionSourceInter
                 'label' => __('Elektronisches Lastschriftverfahren - German ELV')
             ]
         ];
-        $fundingOptions = $this->addPaypalCreditForUS($defaultFundingOptions);
-        return $fundingOptions;
-    }
-
-    /**
-     * Adds Paypal Credit for US
-     *
-     * @param {array} $fundingOptions
-     * @return array
-     */
-    private function addPaypalCreditForUS($fundingOptions): array
-    {
-        $paypalCredit = [
-            'value' => 'CREDIT',
-            'label' => __('PayPal Credit')
-        ];
-        if ($this->checkMerchantCountry('US')) {
-            array_unshift($fundingOptions, $paypalCredit);
-        }
-        return $fundingOptions;
-    }
-
-    /**
-     * Checks for chosen Merchant country from the config/url
-     *
-     * @param {string} $country
-     * @return bool
-     */
-    private function checkMerchantCountry($country): bool
-    {
-        $paypalCountry = $this->http->get(StructurePlugin::REQUEST_PARAM_COUNTRY);
-        if ($paypalCountry) {
-            return $paypalCountry === $country;
-        }
-        return $this->_configFactory->create()->getMerchantCountry() === $country;
     }
 }
