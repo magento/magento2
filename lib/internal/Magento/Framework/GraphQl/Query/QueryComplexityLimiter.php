5b535365aa101a689ff96c722f76ec3137d5ad2d
Patrick McLain <pat@pmclain.com>
2019-01-24 08:59:13 -0500

    Fix value passed to DisableIntrospection
    The constructor value passed to `GraphQL\Validator\Rules\DisableIntrospection`
    must be `int 0` to enable intropection.

--- lib/internal/Magento/Framework/GraphQl/Query/QueryComplexityLimiter.php
+++ lib/internal/Magento/Framework/GraphQl/Query/QueryComplexityLimiter.php
@@ -61,7 +61,7 @@ class QueryComplexityLimiter
     public function execute(): void
     {
         DocumentValidator::addRule(new QueryComplexity($this->queryComplexity));
-        DocumentValidator::addRule(new DisableIntrospection($this->introspectionConfig->isIntrospectionDisabled()));
+        DocumentValidator::addRule(new DisableIntrospection((int) $this->introspectionConfig->isIntrospectionDisabled()));
         DocumentValidator::addRule(new QueryDepth($this->queryDepth));
     }
 }
