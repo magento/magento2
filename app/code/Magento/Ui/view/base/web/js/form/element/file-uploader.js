0b24b39b12e34dfc7ac60746caf420abbd3c2b76
Serhiy Zhovnir <s.zhovnir@atwix.com>
2019-01-26 19:23:38 +0200

    #20461 Add translate component

--- app/code/Magento/Ui/view/base/web/js/form/element/file-uploader.js
+++ app/code/Magento/Ui/view/base/web/js/form/element/file-uploader.js
@@ -13,8 +13,9 @@ define([
     'Magento_Ui/js/modal/alert',
     'Magento_Ui/js/lib/validation/validator',
     'Magento_Ui/js/form/element/abstract',
+    'mage/translate',
     'jquery/file-uploader'
-], function ($, _, utils, uiAlert, validator, Element) {
+], function ($, _, utils, uiAlert, validator, Element, $t) {
     'use strict';
 
     return Element.extend({
