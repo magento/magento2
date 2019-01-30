640cff8527d808c4388f2c09c5547e330b1cec61
roman <rleshchenko@magento.com>
2019-01-30 18:04:32 +0200

    MC-5968: Fixed incorrect config override behavior

--- app/code/Magento/Config/Test/Unit/Controller/Adminhtml/System/Config/_files/groups_array.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
- */
-
-return ['some.key' => 'some.val', 'group.1' => ['fields' => ['g1.1' => ['value' => 'g1.1.val']]]];
