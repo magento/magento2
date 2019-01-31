4d5f8eb09de2d0317b3233ce44db0fda9aa58990
Nathan Smith <nathsmit@adobe.com>
2019-01-31 11:35:37 -0600

    MC-11061: Unit/Integration Testing for MC-4239
    - Added several gateway integration tests

---
+++ dev/tests/integration/testsuite/Magento/AuthorizenetAcceptjs/Gateway/ConfigTest.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Magento\AuthorizenetAcceptjs\Gateway;
+
+use Magento\Payment\Model\Method\Adapter;
+
+class ConfigTest extends AbstractTest
+{
+    public function testVerifyConfiguration()
+    {
+        /** @var Adapter $paymentAdapter */
+        $paymentAdapter = $this->objectManager->get('AuthorizenetAcceptjsFacade');
+
+        $this->assertEquals('authorizenet_acceptjs', $paymentAdapter->getCode());
+        $this->assertTrue($paymentAdapter->canAuthorize());
+        $this->assertTrue($paymentAdapter->canCapture());
+        $this->assertFalse($paymentAdapter->canCapturePartial());
+        $this->assertTrue($paymentAdapter->canRefund());
+        $this->assertTrue($paymentAdapter->canUseCheckout());
+        $this->assertTrue($paymentAdapter->canVoid());
+        $this->assertTrue($paymentAdapter->canUseInternal());
+        $this->assertTrue($paymentAdapter->canEdit());
+    }
+}
