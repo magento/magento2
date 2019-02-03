43ba735b55c09517cdfe9d9748668b3d04ecc934
kunj1988 <joshi_kunj@yahoo.com>
2019-02-03 14:56:08 +0530

    Fixed Issue  #19737 quote issue mm19in

--- app/code/Magento/Cms/Helper/Wysiwyg/Images.php
+++ app/code/Magento/Cms/Helper/Wysiwyg/Images.php
@@ -204,7 +204,7 @@ class Images extends \Magento\Framework\App\Helper\AbstractHelper
         $mediaPath = str_replace($mediaUrl, '', $fileUrl);
         $directive = sprintf('{{media url="%s"}}', $mediaPath);
         if ($renderAsTag) {
-            $src = $this->isUsingStaticUrlsAllowed() ? $fileUrl : $this->escaper->escapeHtml($directive);
+            $src = $this->isUsingStaticUrlsAllowed() ? $fileUrl : htmlspecialchars($directive, ENT_SUBSTITUTE, 'UTF-8', false);
             $html = sprintf('<img src="%s" alt="" />', $src);
         } else {
             if ($this->isUsingStaticUrlsAllowed()) {
