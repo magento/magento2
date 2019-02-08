1efb4a9f3435e0ec1f2b1b9d0b6a3242eee20eb7
Pavel Bystritsky <p.bystritsky@yandex.ru>
2019-02-08 16:40:45 +0200

    ENGCOM-4101: Static test fix.

--- app/code/Magento/Sales/Model/Order/Email/Sender/OrderSender.php
+++ app/code/Magento/Sales/Model/Order/Email/Sender/OrderSender.php
@@ -55,10 +55,10 @@ class OrderSender extends Sender
      * @param OrderIdentity $identityContainer
      * @param Order\Email\SenderBuilderFactory $senderBuilderFactory
      * @param \Psr\Log\LoggerInterface $logger
+     * @param Renderer $addressRenderer
      * @param PaymentHelper $paymentHelper
      * @param OrderResource $orderResource
      * @param \Magento\Framework\App\Config\ScopeConfigInterface $globalConfig
-     * @param Renderer $addressRenderer
      * @param ManagerInterface $eventManager
      */
     public function __construct(
