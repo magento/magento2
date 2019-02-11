4308114635810918035cd9da80070227a7dfb5ff
Jan Tomka <jan@otec.com.au>
2019-02-11 16:12:57 +1100

    magento/magento2#20906: Magento backend catalog "Cost" without currency symbol magento/magento2#20472: Special Price shown without currency symbol in magento backoffice

---
+++ app/code/Magento/Catalog/Ui/Component/Listing/Columns/Cost.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Catalog\Ui\Component\Listing\Columns;
+
+/**
+ * @api
+ * @since 100.0.2
+ */
+class Cost extends Price
+{
+    /**
+     * Column name
+     */
+    const NAME = 'column.cost';
+}
