9088c45081a40180e5a007b9a7edfb7eb5d27172
Vishal Gelani <vishalgelani99@gmail.com>
2019-02-12 11:09:14 +0530

    Update shipping-information.js

--- app/code/Magento/Checkout/view/frontend/web/js/view/shipping-information.js
+++ app/code/Magento/Checkout/view/frontend/web/js/view/shipping-information.js
@@ -30,13 +30,14 @@ define([
         getShippingMethodTitle: function () {
             var shippingMethod = quote.shippingMethod();
 
-            var shippingMethodTitle, shippingTitle = '';
+            var shippingMethodTitle = '';
+            var shippingTitle = '';
 
             if (typeof shippingMethod['method_title'] !== 'undefined' && shippingMethod['method_title'].trim().length != 0) {
                 shippingMethodTitle = shippingMethod['method_title'];
             }
 
-            if(typeof shippingMethod['carrier_title'] !== 'undefined' &&  shippingMethod['carrier_title'].trim().length != 0){
+            if (typeof shippingMethod['carrier_title'] !== 'undefined' &&  shippingMethod['carrier_title'].trim().length != 0) {
                 shippingTitle = shippingMethod['carrier_title'] + ' - ';
             }
 
