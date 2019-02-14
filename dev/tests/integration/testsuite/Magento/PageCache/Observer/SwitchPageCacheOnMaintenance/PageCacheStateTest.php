b94cbd86babdc607d8f59cd018fbdb31a6455b28
Mastiuhin Olexandr <mastiuhin.olexandr@transoftgroup.com>
2019-02-14 14:24:15 +0200

    MAGETWO-90953: Incorrect page caching with enabled maintenance mode when accessing from the exclude IP list.

---
+++ dev/tests/integration/testsuite/Magento/PageCache/Observer/SwitchPageCacheOnMaintenance/PageCacheStateTest.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ *
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Magento\PageCache\Observer\SwitchPageCacheOnMaintenance;
+
+use PHPUnit\Framework\TestCase;
+use Magento\TestFramework\Helper\Bootstrap;
+
+/**
+ * Page Cache state test.
+ */
+class PageCacheStateTest extends TestCase
+{
+    /**
+     * @var PageCacheState
+     */
+    private $pageCacheStateStorage;
+
+    /**
+     * @inheritdoc
+     */
+    protected function setUp()
+    {
+        $objectManager = Bootstrap::getObjectManager();
+        $this->pageCacheStateStorage = $objectManager->get(PageCacheState::class);
+    }
+
+    /**
+     * Tests save state.
+     *
+     * @param bool $state
+     * @return void
+     * @dataProvider saveStateProvider
+     */
+    public function testSave(bool $state)
+    {
+        $this->pageCacheStateStorage->save($state);
+        $this->assertEquals($state, $this->pageCacheStateStorage->isEnabled());
+    }
+
+    /**
+     * Tests flush state.
+     *
+     * @return void
+     */
+    public function testFlush()
+    {
+        $this->pageCacheStateStorage->save(true);
+        $this->assertTrue($this->pageCacheStateStorage->isEnabled());
+        $this->pageCacheStateStorage->flush();
+        $this->assertFalse($this->pageCacheStateStorage->isEnabled());
+    }
+
+    /**
+     * Save state provider.
+     *
+     * @return array
+     */
+    public function saveStateProvider(): array
+    {
+        return [[true], [false]];
+    }
+}
