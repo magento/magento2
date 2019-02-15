9491c0151b39312ff5c0c25aba6d3c9d5981f9f3
Valerii Naida <vnayda@adobe.com>
2019-02-15 12:49:51 -0600

    GraphQL-375: 1. Customer can get shipping/billing address data of any other customer

--- dev/tests/api-functional/testsuite/Magento/GraphQl/Quote/SetBillingAddressOnCartTest.php
+++ dev/tests/api-functional/testsuite/Magento/GraphQl/Quote/SetBillingAddressOnCartTest.php
@@ -336,8 +336,20 @@ mutation {
     input: {
       cart_id: "$maskedQuoteId"
       billing_address: {
-          customer_address_id: 1
-       }
+        customer_address_id: 1
+        address: {
+          firstname: "test firstname"
+          lastname: "test lastname"
+          company: "test company"
+          street: ["test street 1", "test street 2"]
+          city: "test city"
+          region: "test region"
+          postcode: "887766"
+          country_code: "US"
+          telephone: "88776655"
+          save_in_address_book: false
+        }
+      }
     }
   ) {
     cart {
