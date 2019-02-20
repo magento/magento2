38ef69db1bc29ec157ab9401a2a1eed548f70485
Bohdan Korablov <korablov@adobe.com>
2019-02-20 11:32:24 -0600

    MAGETWO-98151: Add support ZooKeeper locks

---
+++ lib/internal/Magento/Framework/Lock/Test/Unit/LockBackendFactoryTest.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\Framework\Lock\Test\Unit;
+
+use Magento\Framework\Lock\Backend\Database as DatabaseLock;
+use Magento\Framework\Lock\Backend\Zookeeper as ZookeeperLock;
+use Magento\Framework\Lock\LockBackendFactory;
+use Magento\Framework\ObjectManagerInterface;
+use Magento\Framework\Lock\LockManagerInterface;
+use Magento\Framework\App\DeploymentConfig;
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\MockObject\MockObject;
+
+class LockBackendFactoryTest extends TestCase
+{
+    /**
+     * @var ObjectManagerInterface|MockObject
+     */
+    private $objectManagerMock;
+
+    /**
+     * @var DeploymentConfig|MockObject
+     */
+    private $deploymentConfigMock;
+
+    /**
+     * @var LockBackendFactory
+     */
+    private $factory;
+
+    /**
+     * @inheritdoc
+     */
+    protected function setUp()
+    {
+        $this->objectManagerMock = $this->getMockForAbstractClass(ObjectManagerInterface::class);
+        $this->deploymentConfigMock = $this->createMock(DeploymentConfig::class);
+        $this->factory = new LockBackendFactory($this->objectManagerMock, $this->deploymentConfigMock);
+    }
+
+    /**
+     * @expectedException \Magento\Framework\Exception\RuntimeException
+     * @expectedExceptionMessage Unknown locks provider.
+     */
+    public function testCreateWithException()
+    {
+        $this->deploymentConfigMock->expects($this->exactly(2))
+            ->method('get')
+            ->withConsecutive(['locks/provider', LockBackendFactory::LOCK_DB], ['locks/config', []])
+            ->willReturnOnConsecutiveCalls('someProvider', []);
+
+        $this->factory->create();
+    }
+
+    /**
+     * @param string $lockProvider
+     * @param string $lockProviderClass
+     * @param array $config
+     * @dataProvider createDataProvider
+     */
+    public function testCreate(string $lockProvider, string $lockProviderClass, array $config)
+    {
+        $lockManagerMock = $this->getMockForAbstractClass(LockManagerInterface::class);
+        $this->deploymentConfigMock->expects($this->exactly(2))
+            ->method('get')
+            ->withConsecutive(['locks/provider', LockBackendFactory::LOCK_DB], ['locks/config', []])
+            ->willReturnOnConsecutiveCalls($lockProvider, $config);
+        $this->objectManagerMock->expects($this->once())
+            ->method('create')
+            ->with($lockProviderClass, $config)
+            ->willReturn($lockManagerMock);
+
+        $this->assertSame($lockManagerMock, $this->factory->create());
+    }
+
+    public function createDataProvider(): array
+    {
+        return [
+            [
+                'lockProvider' => LockBackendFactory::LOCK_DB,
+                'lockProviderClass' => DatabaseLock::class,
+                'config' => ['prefix' => 'somePrefix'],
+            ],
+            [
+                'lockProvider' => LockBackendFactory::LOCK_ZOOKEEPER,
+                'lockProviderClass' => ZookeeperLock::class,
+                'config' => ['host' => 'some host'],
+            ],
+        ];
+    }
+}
