c865bdc25ff73168e2c2e0e093787ed766a2d068
Vitaliy Boyko <v.boyko@atwix.com>
2019-02-20 19:45:43 +0200

    ENGCOM-20434: refactoring, test coverage

--- app/code/Magento/CatalogUrlRewrite/Observer/ProcessUrlRewriteOnChangeProductVisibilityObserver.php
+++ app/code/Magento/CatalogUrlRewrite/Observer/ProcessUrlRewriteOnChangeProductVisibilityObserver.php
@@ -8,64 +8,34 @@ declare(strict_types=1);
 namespace Magento\CatalogUrlRewrite\Observer;
 
 use Magento\Catalog\Api\Data\ProductInterface;
-use Magento\Catalog\Model\Product\Visibility;
-use Magento\CatalogUrlRewrite\Model\ProductUrlPathGenerator;
-use Magento\CatalogUrlRewrite\Model\ProductUrlRewriteGenerator;
-use Magento\UrlRewrite\Model\Exception\UrlAlreadyExistsException;
-use Magento\UrlRewrite\Model\UrlPersistInterface;
-use Magento\UrlRewrite\Service\V1\Data\UrlRewrite;
-use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory;
+use Magento\CatalogUrlRewrite\Model\Products\AdaptUrlRewritesToVisibilityAttribute;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
+use Magento\UrlRewrite\Model\Exception\UrlAlreadyExistsException;
 
 /**
- * Class ProductProcessUrlRewriteSavingObserver
+ * Consider URL rewrites on change product visibility via mass action
  */
 class ProcessUrlRewriteOnChangeProductVisibilityObserver implements ObserverInterface
 {
     /**
-     * @var CollectionFactory
+     * @var AdaptUrlRewritesToVisibilityAttribute
      */
-    private $productCollectionFactory;
+    private $adaptUrlRewritesToVisibility;
 
     /**
-     * @var ProductUrlRewriteGenerator
+     * @param AdaptUrlRewritesToVisibilityAttribute $adaptUrlRewritesToVisibility
      */
-    private $urlRewriteGenerator;
-
-    /**
-     * @var UrlPersistInterface
-     */
-    private $urlPersist;
-
-    /**
-     * @var ProductUrlPathGenerator
-     */
-    private $urlPathGenerator;
-
-    /**
-     * @param CollectionFactory $collectionFactory
-     * @param ProductUrlRewriteGenerator $urlRewriteGenerator
-     * @param UrlPersistInterface $urlPersist
-     * @param ProductUrlPathGenerator|null $urlPathGenerator
-     */
-    public function __construct(
-        CollectionFactory $collectionFactory,
-        ProductUrlRewriteGenerator $urlRewriteGenerator,
-        UrlPersistInterface $urlPersist,
-        ProductUrlPathGenerator $urlPathGenerator
-    ) {
-        $this->productCollectionFactory = $collectionFactory;
-        $this->urlRewriteGenerator = $urlRewriteGenerator;
-        $this->urlPersist = $urlPersist;
-        $this->urlPathGenerator = $urlPathGenerator;
+    public function __construct(AdaptUrlRewritesToVisibilityAttribute $adaptUrlRewritesToVisibility)
+    {
+        $this->adaptUrlRewritesToVisibility = $adaptUrlRewritesToVisibility;
     }
 
     /**
      * Generate urls for UrlRewrites and save it in storage
      *
      * @param Observer $observer
-     * @return array
+     * @return void
      * @throws UrlAlreadyExistsException
      */
     public function execute(Observer $observer)
@@ -73,39 +43,12 @@ class ProcessUrlRewriteOnChangeProductVisibilityObserver implements ObserverInte
         $event = $observer->getEvent();
         $attrData = $event->getAttributesData();
         $productIds = $event->getProductIds();
-        $storeId = $event->getStoreId();
-        $visibility = $attrData[ProductInterface::VISIBILITY] ?? null;
+        $visibility = $attrData[ProductInterface::VISIBILITY] ?? 0;
 
-        if (!$visibility) {
-            return [$attrData, $productIds, $storeId];
+        if (!$visibility || !$productIds) {
+            return;
         }
 
-        $productCollection = $this->productCollectionFactory->create();
-        $productCollection->addAttributeToSelect(ProductInterface::VISIBILITY);
-        $productCollection->addAttributeToSelect('url_key');
-        $productCollection->addFieldToFilter(
-            'entity_id',
-            ['in' => array_unique($productIds)]
-        );
-
-        foreach ($productCollection as $product) {
-            if ($visibility == Visibility::VISIBILITY_NOT_VISIBLE) {
-                $this->urlPersist->deleteByData(
-                    [
-                        UrlRewrite::ENTITY_ID => $product->getId(),
-                        UrlRewrite::ENTITY_TYPE => ProductUrlRewriteGenerator::ENTITY_TYPE,
-                    ]
-                );
-            } elseif ($visibility !== Visibility::VISIBILITY_NOT_VISIBLE) {
-                $product->setVisibility($visibility);
-                $productUrlPath = $this->urlPathGenerator->getUrlPath($product);
-                $productUrlRewrite = $this->urlRewriteGenerator->generate($product);
-                $product->unsUrlPath();
-                $product->setUrlPath($productUrlPath);
-                $this->urlPersist->replace($productUrlRewrite);
-            }
-        }
-
-        return [$attrData, $productIds, $storeId];
+        $this->adaptUrlRewritesToVisibility->execute($productIds, $visibility);
     }
 }
