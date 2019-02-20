bcb6f4964813f07a9c62b480c0a3720f876c8be7
Oleksandr Gorkun <ogorkun@magento.com>
2019-02-20 14:19:27 -0600

    MC-13958: Additional Permissions for Design settings

--- dev/tests/integration/testsuite/Magento/User/_files/user_with_new_role.php
+++ dev/tests/integration/testsuite/Magento/User/_files/user_with_new_role.php
@@ -13,21 +13,27 @@ use Magento\User\Model\User;
 $objectManager = Bootstrap::getObjectManager();
 /** @var Role $role */
 $role = $objectManager->create(Role::class);
-$role->setRoleName('new_role');
-$role->setRoleType('G');
-$role->save();
+$role->load('new_role', 'role_name');
+if (!$role->getId()) {
+    $role->setRoleName('new_role');
+    $role->setRoleType('G');
+    $role->save();
+}
 
 /** @var User $model */
 $model = $objectManager->create(User::class);
-$model->setFirstname("John")
-    ->setLastname("Doe")
-    ->setUsername('admin_with_role')
-    ->setPassword('12345abc')
-    ->setEmail('admin_with_role@example.com')
-    ->setRoleType('G')
-    ->setResourceId('Magento_Backend::all')
-    ->setPrivileges("")
-    ->setAssertId(0)
-    ->setRoleId($role->getId())
-    ->setPermission('allow');
-$model->save();
+$model->load('admin_with_role', 'username');
+if (!$model->getId()) {
+    $model->setFirstname("John")
+        ->setLastname("Doe")
+        ->setUsername('admin_with_role')
+        ->setPassword('12345abc')
+        ->setEmail('admin_with_role@example.com')
+        ->setRoleType('G')
+        ->setResourceId('Magento_Backend::all')
+        ->setPrivileges("")
+        ->setAssertId(0)
+        ->setRoleId($role->getId())
+        ->setPermission('allow');
+    $model->save();
+}
