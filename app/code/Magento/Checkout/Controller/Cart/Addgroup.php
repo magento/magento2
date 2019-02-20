0df8d31c7b55c9fe49339df24ae45170749054f8
mageprince <mail.mageprince@gmail.com>
2019-02-20 16:56:11 +0530

    Show error message if no any product slected from recent order group

--- app/code/Magento/Checkout/Controller/Cart/Addgroup.php
+++ app/code/Magento/Checkout/Controller/Cart/Addgroup.php
@@ -74,6 +74,8 @@ class Addgroup extends \Magento\Checkout\Controller\Cart
                 }
             }
             $this->cart->save();
+        } else {
+            $this->messageManager->addErrorMessage(__('Please select atleast one product to add to cart'));
         }
         return $this->_goBack();
     }
