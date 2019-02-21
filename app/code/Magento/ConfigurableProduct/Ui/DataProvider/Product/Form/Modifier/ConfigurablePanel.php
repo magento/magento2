a1cbb1b7a2b7b3f89c98e100785be5160794d238
Serhii Voloshkov <serhii.voloshkov@transoftgroup.com>
2019-02-21 14:31:05 +0200

    MC-15012: Uneven displayed fields Status and Attributes

--- app/code/Magento/ConfigurableProduct/Ui/DataProvider/Product/Form/Modifier/ConfigurablePanel.php
+++ app/code/Magento/ConfigurableProduct/Ui/DataProvider/Product/Form/Modifier/ConfigurablePanel.php
@@ -591,6 +591,7 @@ class ConfigurablePanel extends AbstractModifier
             'formElement' => Container::NAME,
             'component' => 'Magento_Ui/js/form/components/group',
             'label' => $label,
+            'showLabel' => false,
             'dataScope' => '',
         ];
         $container['children'] = [
