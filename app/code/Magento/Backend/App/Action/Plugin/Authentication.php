ba2719d41a4f1895bfe69d271f307b57735d2701
Nickolas Burr <nickolasburr@gmail.com>
2019-02-21 11:55:41 -0500

    [module-backend] Check dispatch status in aroundDispatch
    [module-backend] Check dispatch status before executing Authentication::aroundDispatch()

--- app/code/Magento/Backend/App/Action/Plugin/Authentication.php
+++ app/code/Magento/Backend/App/Action/Plugin/Authentication.php
@@ -114,32 +114,34 @@ class Authentication
         \Closure $proceed,
         \Magento\Framework\App\RequestInterface $request
     ) {
-        $requestedActionName = $request->getActionName();
-        if (in_array($requestedActionName, $this->_openActions)) {
-            $request->setDispatched(true);
-        } else {
-            if ($this->_auth->getUser()) {
-                $this->_auth->getUser()->reload();
-            }
-            if (!$this->_auth->isLoggedIn()) {
-                $this->_processNotLoggedInUser($request);
+        if (!$request->isDispatched()) {
+            if (in_array($request->getActionName(), $this->_openActions)) {
+                $request->setDispatched(true);
             } else {
-                $this->_auth->getAuthStorage()->prolong();
-
-                $backendApp = null;
-                if ($request->getParam('app')) {
-                    $backendApp = $this->backendAppList->getCurrentApp();
+                if ($this->_auth->getUser()) {
+                    $this->_auth->getUser()->reload();
                 }
 
-                if ($backendApp) {
-                    $resultRedirect = $this->resultRedirectFactory->create();
-                    $baseUrl = \Magento\Framework\App\Request\Http::getUrlNoScript($this->backendUrl->getBaseUrl());
-                    $baseUrl = $baseUrl . $backendApp->getStartupPage();
-                    return $resultRedirect->setUrl($baseUrl);
+                if (!$this->_auth->isLoggedIn()) {
+                    $this->_processNotLoggedInUser($request);
+                } else {
+                    $this->_auth->getAuthStorage()->prolong();
+
+                    /** @var \Magento\Backend\App\BackendApp|null $backendApp */
+                    $backendApp = $request->getParam('app') ? $this->backendAppList->getCurrentApp() : null;
+
+                    if ($backendApp) {
+                        $resultRedirect = $this->resultRedirectFactory->create();
+                        $baseUrl = \Magento\Framework\App\Request\Http::getUrlNoScript($this->backendUrl->getBaseUrl());
+                        $baseUrl = $baseUrl . $backendApp->getStartupPage();
+                        return $resultRedirect->setUrl($baseUrl);
+                    }
                 }
             }
+
+            $this->_auth->getAuthStorage()->refreshAcl();
         }
-        $this->_auth->getAuthStorage()->refreshAcl();
+
         return $proceed($request);
     }
 
