18a590fb52d1268d3db1c5348805a629bf6b28e4
Vinai Kopp <vinai@netzarbeiter.com>
2019-02-26 22:55:47 +0100

    Allow module data fixtures for @magentoDataFixtureBeforeTransaction annotations
    Extend the option to specify a module based data fixture files to the
    @magentoDataFixtureBeforeTransaction annotation.
    Previously they where only available for the @magentoDataFixture annotation.
    
    The syntax is the same:
    @magentoDataFixtureBeforeAnnotation Foo_DataFixtureDummy::Test/Integration/foo.php
    
    There is no backward compatibility break associated with this commit.

--- dev/tests/integration/framework/Magento/TestFramework/Annotation/DataFixtureBeforeTransaction.php
+++ dev/tests/integration/framework/Magento/TestFramework/Annotation/DataFixtureBeforeTransaction.php
@@ -5,10 +5,11 @@
  */
 
 /**
- * Implementation of the @magentoDataFixture DocBlock annotation
+ * Implementation of the @magentoDataFixtureBeforeTransaction DocBlock annotation
  */
 namespace Magento\TestFramework\Annotation;
 
+use Magento\Framework\Component\ComponentRegistrar;
 use PHPUnit\Framework\Exception;
 
 class DataFixtureBeforeTransaction
@@ -93,12 +94,48 @@ class DataFixtureBeforeTransaction
                 $fixtureMethod = [get_class($test), $fixture];
                 if (is_callable($fixtureMethod)) {
                     $result[] = $fixtureMethod;
+                } elseif ($this->isModuleAnnotation($fixture)) {
+                    $result[] = $this->getModulePath($fixture);
                 } else {
                     $result[] = $this->_fixtureBaseDir . '/' . $fixture;
                 }
             }
         }
         return $result;
+    }
+
+    /**
+     * Check is the Annotation like Magento_InventoryApi::Test/_files/products.php
+     *
+     * @param string $fixture
+     * @return bool
+     */
+    private function isModuleAnnotation(string $fixture)
+    {
+        return (strpos($fixture, '::') !== false);
+    }
+
+    /**
+     * Resolve the Fixture
+     *
+     * @param string $fixture
+     * @return string
+     * @throws \Magento\Framework\Exception\LocalizedException
+     * @SuppressWarnings(PHPMD.StaticAccess)
+     */
+    private function getModulePath(string $fixture)
+    {
+        [$moduleName, $fixtureFile] = explode('::', $fixture, 2);
+
+        $modulePath = (new ComponentRegistrar())->getPath(ComponentRegistrar::MODULE, $moduleName);
+
+        if ($modulePath === null) {
+            throw new \Magento\Framework\Exception\LocalizedException(
+                new \Magento\Framework\Phrase('Can\'t find registered Module with name %1 .', [$moduleName])
+            );
+        }
+
+        return $modulePath . '/' . ltrim($fixtureFile, '/');
     }
 
     /**
