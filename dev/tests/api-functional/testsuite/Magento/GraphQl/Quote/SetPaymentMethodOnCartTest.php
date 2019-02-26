287186d61baddc38a64c140e0ee3b20a404e7aaf
Valerii Naida <vnayda@adobe.com>
2019-02-26 12:44:27 -0600

    GraphQL-293: [Payment methods] Set Payment Method on Cart

--- dev/tests/api-functional/testsuite/Magento/GraphQl/Quote/SetPaymentMethodOnCartTest.php
+++ dev/tests/api-functional/testsuite/Magento/GraphQl/Quote/SetPaymentMethodOnCartTest.php
@@ -7,33 +7,19 @@ declare(strict_types=1);
 
 namespace Magento\GraphQl\Quote;
 
-use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Integration\Api\CustomerTokenServiceInterface;
-use Magento\OfflinePayments\Model\Banktransfer;
-use Magento\OfflinePayments\Model\Cashondelivery;
 use Magento\OfflinePayments\Model\Checkmo;
-use Magento\OfflinePayments\Model\Purchaseorder;
-use Magento\Quote\Model\Quote;
+use Magento\Quote\Model\QuoteFactory;
 use Magento\Quote\Model\QuoteIdToMaskedQuoteIdInterface;
 use Magento\Quote\Model\ResourceModel\Quote as QuoteResource;
 use Magento\TestFramework\Helper\Bootstrap;
-use Magento\TestFramework\ObjectManager;
 use Magento\TestFramework\TestCase\GraphQlAbstract;
-use Magento\Framework\App\Cache\TypeListInterface;
-use Magento\Config\Model\ResourceModel\Config;
 
 /**
  * Test for setting payment methods on cart
  */
 class SetPaymentMethodOnCartTest extends GraphQlAbstract
 {
-    private const OFFLINE_METHOD_CODES = [
-        Checkmo::PAYMENT_METHOD_CHECKMO_CODE,
-        Banktransfer::PAYMENT_METHOD_BANKTRANSFER_CODE,
-        Cashondelivery::PAYMENT_METHOD_CASHONDELIVERY_CODE,
-        Purchaseorder::PAYMENT_METHOD_PURCHASEORDER_CODE,
-    ];
-
     /**
      * @var CustomerTokenServiceInterface
      */
@@ -45,9 +31,9 @@ class SetPaymentMethodOnCartTest extends GraphQlAbstract
     private $quoteResource;
 
     /**
-     * @var Quote
+     * @var QuoteFactory
      */
-    private $quote;
+    private $quoteFactory;
 
     /**
      * @var QuoteIdToMaskedQuoteIdInterface
@@ -55,129 +41,46 @@ class SetPaymentMethodOnCartTest extends GraphQlAbstract
     private $quoteIdToMaskedId;
 
     /**
-     * @var Config
-     */
-    private $config;
-
-    /**
-     * @var TypeListInterface
-     */
-    private $cacheList;
-
-    /**
      * @inheritdoc
      */
     protected function setUp()
     {
         $objectManager = Bootstrap::getObjectManager();
-        $this->quoteResource = $objectManager->create(QuoteResource::class);
-        $this->quote = $objectManager->create(Quote::class);
-        $this->quoteIdToMaskedId = $objectManager->create(QuoteIdToMaskedQuoteIdInterface::class);
+        $this->quoteResource = $objectManager->get(QuoteResource::class);
+        $this->quoteFactory = $objectManager->get(QuoteFactory::class);
+        $this->quoteIdToMaskedId = $objectManager->get(QuoteIdToMaskedQuoteIdInterface::class);
         $this->customerTokenService = $objectManager->get(CustomerTokenServiceInterface::class);
-        $this->config = $objectManager->get(Config::class);
-        $this->cacheList = $objectManager->get(TypeListInterface::class);
-
-        foreach (static::OFFLINE_METHOD_CODES as $offlineMethodCode) {
-            $this->config->saveConfig(
-                'payment/' . $offlineMethodCode . '/active',
-                '1',
-                ScopeConfigInterface::SCOPE_TYPE_DEFAULT,
-                0
-            );
-        }
-        $this->cacheList->cleanType('config');
     }
 
     /**
-     * @inheritdoc
-     */
-    protected function tearDown()
-    {
-        foreach (static::OFFLINE_METHOD_CODES as $offlineMethodCode) {
-            //Never no disable checkmo method
-            if ($offlineMethodCode === Checkmo::PAYMENT_METHOD_CHECKMO_CODE) {
-                continue;
-            }
-            $this->config->saveConfig(
-                'payment/' . $offlineMethodCode . '/active',
-                '0',
-                ScopeConfigInterface::SCOPE_TYPE_DEFAULT,
-                0
-            );
-        }
-        $this->cacheList->cleanType('config');
-    }
-
-    /**
-     * @param string $methodCode
-     * @dataProvider dataProviderOfflinePaymentMethods
      * @magentoApiDataFixture Magento/Checkout/_files/quote_with_address_saved.php
      */
-    public function testSetPaymentMethodOnCart(string $methodCode)
+    public function testSetPaymentMethodOnCart()
     {
-        /** @var \Magento\Config\Model\ResourceModel\Config $config */
-        $config = ObjectManager::getInstance()->get(\Magento\Config\Model\ResourceModel\Config::class);
-        $config->saveConfig(
-            'payment/' . $methodCode . '/active',
-            1,
-            ScopeConfigInterface::SCOPE_TYPE_DEFAULT,
-            0
-        );
+        $methodCode = Checkmo::PAYMENT_METHOD_CHECKMO_CODE;
+        $maskedQuoteId = $this->getMaskedQuoteIdByReversedQuoteId('test_order_1');
 
-        $this->quoteResource->load(
-            $this->quote,
-            'test_order_1',
-            'reserved_order_id'
-        );
-        $maskedQuoteId = $this->quoteIdToMaskedId->execute((int)$this->quote->getId());
-
-        $query = $this->prepareMutationQuery(
-            $maskedQuoteId,
-            $methodCode
-        );
-
+        $query = $this->prepareMutationQuery($maskedQuoteId, $methodCode);
         $response = $this->sendRequestWithToken($query);
 
-        $this->assertArrayHasKey('setPaymentMethodOnCart', $response);
-        $this->assertArrayHasKey('cart', $response['setPaymentMethodOnCart']);
-        $this->assertEquals($maskedQuoteId, $response['setPaymentMethodOnCart']['cart']['cart_id']);
-        $this->assertArrayHasKey('payment_method', $response['setPaymentMethodOnCart']['cart']);
-        $this->assertEquals($methodCode, $response['setPaymentMethodOnCart']['cart']['payment_method']['code']);
-    }
-
-    public function dataProviderOfflinePaymentMethods(): array
-    {
-        $methods = [];
-        foreach (static::OFFLINE_METHOD_CODES as $offlineMethodCode) {
-            //Purchase order requires additional input and is tested separately
-            if ($offlineMethodCode === Purchaseorder::PAYMENT_METHOD_PURCHASEORDER_CODE) {
-                continue;
-            }
-            $methods[] = [$offlineMethodCode];
-        }
-
-        return $methods;
+        self::assertArrayHasKey('setPaymentMethodOnCart', $response);
+        self::assertArrayHasKey('cart', $response['setPaymentMethodOnCart']);
+        self::assertEquals($maskedQuoteId, $response['setPaymentMethodOnCart']['cart']['cart_id']);
+        self::assertArrayHasKey('selected_payment_method', $response['setPaymentMethodOnCart']['cart']);
+        self::assertEquals($methodCode, $response['setPaymentMethodOnCart']['cart']['selected_payment_method']['code']);
     }
 
     /**
      * @magentoApiDataFixture Magento/Checkout/_files/quote_with_address_saved.php
+     * @expectedException \Exception
+     * @expectedExceptionMessage The requested Payment Method is not available.
      */
     public function testSetNonExistingPaymentMethod()
     {
-        $paymentMethod = 'noway';
-        $this->quoteResource->load(
-            $this->quote,
-            'test_order_1',
-            'reserved_order_id'
-        );
-        $maskedQuoteId = $this->quoteIdToMaskedId->execute((int)$this->quote->getId());
+        $methodCode = 'noway';
+        $maskedQuoteId = $this->getMaskedQuoteIdByReversedQuoteId('test_order_1');
 
-        $query = $this->prepareMutationQuery(
-            $maskedQuoteId,
-            $paymentMethod
-        );
-
-        $this->expectExceptionMessage('The requested Payment Method is not available.');
+        $query = $this->prepareMutationQuery($maskedQuoteId, $methodCode);
         $this->sendRequestWithToken($query);
     }
 
@@ -186,102 +89,16 @@ class SetPaymentMethodOnCartTest extends GraphQlAbstract
      */
     public function testSetPaymentMethodByGuestToCustomerCart()
     {
-        $paymentMethod = 'checkmo';
-        $this->quoteResource->load(
-            $this->quote,
-            'test_order_1',
-            'reserved_order_id'
-        );
-        $maskedQuoteId = $this->quoteIdToMaskedId->execute((int)$this->quote->getId());
+        $methodCode = 'checkmo';
+        $maskedQuoteId = $this->getMaskedQuoteIdByReversedQuoteId('test_order_1');
 
-        $query = $this->prepareMutationQuery(
-            $maskedQuoteId,
-            $paymentMethod
-        );
+        $query = $this->prepareMutationQuery($maskedQuoteId, $methodCode);
 
         $this->expectExceptionMessage(
             "The current user cannot perform operations on cart \"$maskedQuoteId\""
         );
 
         $this->graphQlQuery($query);
-    }
-
-    /**
-     * @magentoApiDataFixture Magento/Checkout/_files/quote_with_address_saved.php
-     */
-    public function testSetPaymentMethodPurchaseOrderOnCart()
-    {
-        $methodCode = \Magento\OfflinePayments\Model\Purchaseorder::PAYMENT_METHOD_PURCHASEORDER_CODE;
-        $poNumber = 'GQL-19002';
-
-        /** @var \Magento\Config\Model\ResourceModel\Config $config */
-        $config = ObjectManager::getInstance()->get(\Magento\Config\Model\ResourceModel\Config::class);
-        $config->saveConfig(
-            'payment/' . $methodCode . '/active',
-            1,
-            ScopeConfigInterface::SCOPE_TYPE_DEFAULT,
-            0
-        );
-
-        $this->quoteResource->load(
-            $this->quote,
-            'test_order_1',
-            'reserved_order_id'
-        );
-        $maskedQuoteId = $this->quoteIdToMaskedId->execute((int)$this->quote->getId());
-
-        $query = <<<QUERY
-mutation {
-  setPaymentMethodOnCart(input: 
-    {
-      cart_id: "$maskedQuoteId", 
-      payment_method: {
-          code: "$methodCode"
-          po_number: "$poNumber"
-        }
-      }) {
-    
-    cart {
-      cart_id,
-      payment_method {
-        code
-        po_number
-      }
-    }
-  }
-}
-
-QUERY;
-
-        $response = $this->sendRequestWithToken($query);
-
-        $this->assertArrayHasKey('setPaymentMethodOnCart', $response);
-        $this->assertArrayHasKey('cart', $response['setPaymentMethodOnCart']);
-        $this->assertEquals($maskedQuoteId, $response['setPaymentMethodOnCart']['cart']['cart_id']);
-        $this->assertArrayHasKey('payment_method', $response['setPaymentMethodOnCart']['cart']);
-        $this->assertEquals($methodCode, $response['setPaymentMethodOnCart']['cart']['payment_method']['code']);
-        $this->assertEquals($poNumber, $response['setPaymentMethodOnCart']['cart']['payment_method']['po_number']);
-    }
-
-    /**
-     * @magentoApiDataFixture Magento/Checkout/_files/quote_with_address_saved.php
-     */
-    public function testPurchaseOrderPaymentMethodFailingValidation()
-    {
-        $this->quoteResource->load(
-            $this->quote,
-            'test_order_1',
-            'reserved_order_id'
-        );
-        $maskedQuoteId = $this->quoteIdToMaskedId->execute((int)$this->quote->getId());
-
-        $query = $this->prepareMutationQuery(
-            $maskedQuoteId,
-            Purchaseorder::PAYMENT_METHOD_PURCHASEORDER_CODE
-        );
-
-        $this->expectExceptionMessage('Purchase order number is a required field.');
-        $this->sendRequestWithToken($query);
     }
 
     /**
@@ -307,13 +124,12 @@ mutation {
     
     cart {
       cart_id,
-      payment_method {
+      selected_payment_method {
         code
       }
     }
   }
 }
-
 QUERY;
     }
 
@@ -326,10 +142,21 @@ QUERY;
      */
     private function sendRequestWithToken(string $query): array
     {
-
         $customerToken = $this->customerTokenService->createCustomerAccessToken('customer@example.com', 'password');
         $headerMap = ['Authorization' => 'Bearer ' . $customerToken];
 
         return $this->graphQlQuery($query, [], '', $headerMap);
+    }
+
+    /**
+     * @param string $reversedQuoteId
+     * @return string
+     */
+    private function getMaskedQuoteIdByReversedQuoteId(string $reversedQuoteId): string
+    {
+        $quote = $this->quoteFactory->create();
+        $this->quoteResource->load($quote, $reversedQuoteId, 'reserved_order_id');
+
+        return $this->quoteIdToMaskedId->execute((int)$quote->getId());
     }
 }
