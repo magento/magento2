ce2c82ba2f0e624f64bf34c86b3fc4daee4da80b
Oleksandr Gorkun <ogorkun@magento.com>
2019-02-26 18:51:48 -0600

    MC-5865: Same Content Type for Components on Storefront

--- app/code/Magento/Ui/Controller/Index/Render.php
+++ app/code/Magento/Ui/Controller/Index/Render.php
@@ -6,8 +6,11 @@
 namespace Magento\Ui\Controller\Index;
 
 use Magento\Backend\App\Action\Context;
+use Magento\Framework\App\ObjectManager;
+use Magento\Framework\App\Response\Http as HttpResponse;
 use Magento\Framework\View\Element\UiComponentFactory;
 use Magento\Framework\View\Element\UiComponentInterface;
+use Magento\Ui\Model\UiComponentTypeResolver;
 
 /**
  * Is responsible for providing ui components information on store front
@@ -25,15 +28,26 @@ class Render extends \Magento\Framework\App\Action\Action
     private $uiComponentFactory;
 
     /**
+     * @var UiComponentTypeResolver
+     */
+    private $contentTypeResolver;
+
+    /**
      * Render constructor.
      * @param Context $context
      * @param UiComponentFactory $uiComponentFactory
+     * @param UiComponentTypeResolver|null $contentTypeResolver
      */
-    public function __construct(Context $context, UiComponentFactory $uiComponentFactory)
-    {
+    public function __construct(
+        Context $context,
+        UiComponentFactory $uiComponentFactory,
+        ?UiComponentTypeResolver $contentTypeResolver = null
+    ) {
         parent::__construct($context);
         $this->context = $context;
         $this->uiComponentFactory = $uiComponentFactory;
+        $this->contentTypeResolver = $contentTypeResolver
+            ?? ObjectManager::getInstance()->get(UiComponentTypeResolver::class);
     }
 
     /**
@@ -50,7 +64,10 @@ class Render extends \Magento\Framework\App\Action\Action
 
         $component = $this->uiComponentFactory->create($this->_request->getParam('namespace'));
         $this->prepareComponent($component);
-        $this->_response->appendBody((string) $component->render());
+        /** @var HttpResponse $response */
+        $response = $this->getResponse();
+        $response->appendBody((string) $component->render());
+        $response->setHeader('Content-Type', $this->contentTypeResolver->resolve($component->getContext()), true);
     }
 
     /**
