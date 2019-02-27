fa890bed53c33eecf182b90c68605af517f55dd9
Stepan Furman <furman.stepan@gmail.com>
2019-02-27 22:45:35 +0200

    MSI-1988 issue fixed: Database Rollback not working M2.3

---
+++ app/code/Magento/Backup/Model/ResourceModel/View/GetListViews.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\Backup\Model\ResourceModel\View;
+
+use Magento\Framework\App\ResourceConnection;
+
+/**
+ * Class GetListViews
+ */
+class GetListViews
+{
+    private const TABLE_TYPE = 'VIEW';
+    /**
+     * @var ResourceConnection
+     */
+    private $resource;
+
+    /**
+     * @param ResourceConnection $resource
+     */
+    public function __construct(ResourceConnection $resource)
+    {
+        $this->resource = $resource;
+    }
+
+    /**
+     * @return array
+     */
+    public function execute()
+    {
+        return $this->resource->getConnection('backup')->fetchCol(
+            "SHOW FULL TABLES WHERE `Table_type` = ?",
+            self::TABLE_TYPE
+        );
+    }
+}
