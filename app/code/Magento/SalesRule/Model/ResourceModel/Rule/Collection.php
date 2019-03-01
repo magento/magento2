bfd1d4f7c38c0fe3c8e17449b621bbc5c996d59b
nmalevanec <mikola.malevanec@transoftgroup.com>
2019-03-01 15:09:21 +0200

    Fix static tests.

--- app/code/Magento/SalesRule/Model/ResourceModel/Rule/Collection.php
+++ app/code/Magento/SalesRule/Model/ResourceModel/Rule/Collection.php
@@ -157,7 +157,6 @@ class Collection extends \Magento\Rule\Model\ResourceModel\Rule\Collection\Abstr
         Address $address = null
     ) {
         if (!$this->getFlag('validation_filter')) {
-
             $this->prepareSelect($websiteId, $customerGroupId, $now);
 
             $noCouponRules = $this->getNoCouponCodeSelect();
@@ -165,7 +164,7 @@ class Collection extends \Magento\Rule\Model\ResourceModel\Rule\Collection\Abstr
             if ($couponCode) {
                 $couponRules = $this->getCouponCodeSelect($couponCode);
                 $allAllowedRules = $this->getConnection()->select();
-                $allAllowedRules->union([$noCouponRules, $couponRules], \Zend_Db_Select::SQL_UNION_ALL);
+                $allAllowedRules->union([$noCouponRules, $couponRules], Select::SQL_UNION_ALL);
 
                 $wrapper = $this->getConnection()->select();
                 $wrapper->from($allAllowedRules);
@@ -185,9 +184,9 @@ class Collection extends \Magento\Rule\Model\ResourceModel\Rule\Collection\Abstr
     /**
      * Recreate the default select object for specific needs of salesrule evaluation with coupon codes.
      *
-     * @param $websiteId
-     * @param $customerGroupId
-     * @param $now
+     * @param int $websiteId
+     * @param int $customerGroupId
+     * @param string $now
      */
     private function prepareSelect($websiteId, $customerGroupId, $now)
     {
@@ -219,7 +218,7 @@ class Collection extends \Magento\Rule\Model\ResourceModel\Rule\Collection\Abstr
     /**
      * Determine all active rules that are valid for the given coupon code.
      *
-     * @param $couponCode
+     * @param string $couponCode
      * @return Select
      */
     private function getCouponCodeSelect($couponCode)
@@ -256,7 +255,9 @@ class Collection extends \Magento\Rule\Model\ResourceModel\Rule\Collection\Abstr
     }
 
     /**
-     * @param $couponCode
+     * Join coupon table to select.
+     *
+     * @param string $couponCode
      * @param Select $couponSelect
      */
     private function joinCouponTable($couponCode, Select $couponSelect)
