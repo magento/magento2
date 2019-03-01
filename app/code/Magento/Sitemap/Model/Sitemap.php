28a9720406a0d0f22e0b35c6d4e99efb456df9c9
nmalevanec <mikola.malevanec@transoftgroup.com>
2019-03-01 15:26:43 +0200

    Fix static tests.

--- app/code/Magento/Sitemap/Model/Sitemap.php
+++ app/code/Magento/Sitemap/Model/Sitemap.php
@@ -3,6 +3,7 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+
 namespace Magento\Sitemap\Model;
 
 use Magento\Config\Model\Config\Reader\Source\Deployed\DocumentRoot;
@@ -15,6 +16,8 @@ use Magento\Sitemap\Model\ItemProvider\ItemProviderInterface;
 use Magento\Sitemap\Model\ResourceModel\Sitemap as SitemapResource;
 
 /**
+ * Sitemap model.
+ *
  * @method string getSitemapType()
  * @method \Magento\Sitemap\Model\Sitemap setSitemapType(string $value)
  * @method string getSitemapFilename()
