f2f05df5bf954913f99cd12b4907f7fbc5a15850
Valerii Naida <vnayda@adobe.com>
2019-03-04 11:41:09 -0600

    GraphQL-418: [Shipping methods] Set Shipping Methods on Cart

---
+++ dev/tests/api-functional/testsuite/Magento/GraphQl/Quote/Customer/SetShippingMethodsOnCartTest.php
@@ -0,0 +1,193 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\GraphQl\Quote\Customer;
+
+use Magento\Integration\Api\CustomerTokenServiceInterface;
+use Magento\Quote\Model\QuoteFactory;
+use Magento\Quote\Model\QuoteIdToMaskedQuoteIdInterface;
+use Magento\Quote\Model\ResourceModel\Quote as QuoteResource;
+use Magento\TestFramework\Helper\Bootstrap;
+use Magento\TestFramework\TestCase\GraphQlAbstract;
+
+/**
+ * Test for setting shipping methods on cart for customer
+ */
+class SetShippingMethodsOnCartTest extends GraphQlAbstract
+{
+    /**
+     * @var CustomerTokenServiceInterface
+     */
+    private $customerTokenService;
+
+    /**
+     * @var QuoteResource
+     */
+    private $quoteResource;
+
+    /**
+     * @var QuoteFactory
+     */
+    private $quoteFactory;
+
+    /**
+     * @var QuoteIdToMaskedQuoteIdInterface
+     */
+    private $quoteIdToMaskedId;
+
+    /**
+     * @inheritdoc
+     */
+    protected function setUp()
+    {
+        $objectManager = Bootstrap::getObjectManager();
+        $this->quoteResource = $objectManager->get(QuoteResource::class);
+        $this->quoteFactory = $objectManager->get(QuoteFactory::class);
+        $this->quoteIdToMaskedId = $objectManager->get(QuoteIdToMaskedQuoteIdInterface::class);
+        $this->customerTokenService = $objectManager->get(CustomerTokenServiceInterface::class);
+    }
+
+    public function testShippingMethodWithVirtualProduct()
+    {
+
+    }
+
+    public function testShippingMethodWithSimpleProduct()
+    {
+
+    }
+
+    public function testShippingMethodWithSimpleProductWithoutAddress()
+    {
+
+    }
+
+    public function testSetShippingMethodWithMissedRequiredParameters()
+    {
+
+    }
+
+    public function testSetNonExistentShippingMethod()
+    {
+    }
+
+    public function testSetShippingMethodIfAddressIsNotBelongToCart()
+    {
+    }
+
+    public function testSetShippingMethodToNonExistentCart()
+    {
+    }
+
+    public function testSetShippingMethodToGuestCart()
+    {
+
+    }
+
+    public function testSetShippingMethodToAnotherCustomerCart()
+    {
+
+    }
+
+    public function testSetShippingMethodToNonExistentCartAddress()
+    {
+    }
+
+    public function testSetShippingMethodToGuestCartAddress()
+    {
+
+    }
+
+    public function testSetShippingMethodToAnotherCustomerCartAddress()
+    {
+
+    }
+
+    /**
+     * @param string $maskedQuoteId
+     * @param string $shippingMethodCode
+     * @param string $shippingCarrierCode
+     * @param string $shippingAddressId
+     * @return string
+     */
+    private function prepareMutationQuery(
+        string $maskedQuoteId,
+        string $shippingMethodCode,
+        string $shippingCarrierCode,
+        string $shippingAddressId
+    ) : string {
+        return <<<QUERY
+mutation {
+  setShippingMethodsOnCart(input: 
+    {
+      cart_id: "$maskedQuoteId", 
+      shipping_addresses: [{
+        cart_address_id: $shippingAddressId
+        shipping_method: {
+          method_code: "$shippingMethodCode"
+          carrier_code: "$shippingCarrierCode"
+        }
+      }]
+      }) {
+    
+    cart {
+      cart_id,
+      shipping_addresses {
+        selected_shipping_method {
+          carrier_code
+          method_code
+          label
+          amount
+        }
+      }
+    }
+  }
+}
+
+QUERY;
+    }
+
+    /**
+     * @param string $reversedQuoteId
+     * @return string
+     */
+    private function getMaskedQuoteIdByReversedQuoteId(string $reversedQuoteId): string
+    {
+        $quote = $this->quoteFactory->create();
+        $this->quoteResource->load($quote, $reversedQuoteId, 'reserved_order_id');
+
+        return $this->quoteIdToMaskedId->execute((int)$quote->getId());
+    }
+
+    /**
+     * @param string $reversedQuoteId
+     * @param int $customerId
+     * @return string
+     */
+    private function assignQuoteToCustomer(
+        string $reversedQuoteId,
+        int $customerId
+    ): string {
+        $quote = $this->quoteFactory->create();
+        $this->quoteResource->load($quote, $reversedQuoteId, 'reserved_order_id');
+        $quote->setCustomerId($customerId);
+        $this->quoteResource->save($quote);
+        return $this->quoteIdToMaskedId->execute((int)$quote->getId());
+    }
+
+    /**
+     * @param string $username
+     * @param string $password
+     * @return array
+     */
+    private function getHeaderMap(string $username = 'customer@example.com', string $password = 'password'): array
+    {
+        $customerToken = $this->customerTokenService->createCustomerAccessToken($username, $password);
+        $headerMap = ['Authorization' => 'Bearer ' . $customerToken];
+        return $headerMap;
+    }
+}
