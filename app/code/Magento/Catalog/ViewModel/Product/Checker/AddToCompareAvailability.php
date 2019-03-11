e9de16a3a4b02505f146839976bd95d830552ca4
Yuliya Labudova <Yuliya_Labudova@epam.com>
2019-03-11 13:48:43 +0300

    MAGETWO-98522: Adding out of stock items to comparison shows success message but fails
    - Delete button if product is not available for comparison.

---
+++ app/code/Magento/Catalog/ViewModel/Product/Checker/AddToCompareAvailability.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\Catalog\ViewModel\Product\Checker;
+
+use Magento\Framework\View\Element\Block\ArgumentInterface;
+use Magento\Catalog\Api\Data\ProductInterface;
+use Magento\CatalogInventory\Api\StockConfigurationInterface;
+
+/**
+ * Check is available add to compare.
+ */
+class AddToCompareAvailability implements ArgumentInterface
+{
+    /**
+     * @var StockConfigurationInterface
+     */
+    private $stockConfiguration;
+
+    /**
+     * @param StockConfigurationInterface $stockConfiguration
+     */
+    public function __construct(StockConfigurationInterface $stockConfiguration)
+    {
+        $this->stockConfiguration = $stockConfiguration;
+    }
+
+    /**
+     * Is product available for comparison.
+     *
+     * @return bool
+     */
+    public function isAvailableForCompare(ProductInterface $product): bool
+    {
+        return $this->isInStock($product) ||
+            !$this->isInStock($product) && $this->stockConfiguration->isShowOutOfStock();
+    }
+
+    /**
+     * Get is in stock status.
+     *
+     * @return bool
+     */
+    private function isInStock(ProductInterface $product): bool
+    {
+        $quantityAndStockStatus = $product->getQuantityAndStockStatus();
+        if (!$quantityAndStockStatus) {
+            return $product->isSalable();
+        }
+
+        return isset($quantityAndStockStatus['is_in_stock'])
+            ? $quantityAndStockStatus['is_in_stock']
+            : false;
+    }
+}
