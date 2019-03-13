4bdc30ed09f7093c772d359daecd7980e9555e46
Yogesh Suhagiya <yksuhagiya@gmail.com>
2019-03-13 17:32:33 +0530

    Fixed issue of undefined variable: objectManager

--- dev/tests/integration/testsuite/Magento/Checkout/_files/simple_product_rollback.php
+++ dev/tests/integration/testsuite/Magento/Checkout/_files/simple_product_rollback.php
@@ -3,9 +3,11 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+/** @var \Magento\Framework\ObjectManagerInterface $objectManager */
+$objectManager = \Magento\TestFramework\Helper\Bootstrap::getObjectManager();
 
 /** @var \Magento\Framework\Registry $registry */
-$registry = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->get(\Magento\Framework\Registry::class);
+$registry = $objectManager->get(\Magento\Framework\Registry::class);
 
 $registry->unregister('isSecureArea');
 $registry->register('isSecureArea', true);
