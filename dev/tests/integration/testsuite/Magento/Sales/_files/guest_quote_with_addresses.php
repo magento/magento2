091fc8eb46c3fe5bff172c991c2bb6b140fb11f3
Alex Taranovsky <firster@atwix.com>
2019-03-18 11:07:59 +0200

    magento/graphql-ce#480: [Test Coverage] 'GetBillingAddress' functionality 1. Fix PHP Fatal error:  Uncaught Exception: Exception occurred when running the "\/home\/travis\/build\/magento\/graphql-ce\/dev\/tests\/integration\/testsuite\/Magento\/Sales\/_files\/guest_quote_with_addresses.php" fixture: URL key for specified store already exists. in /home/travis/build/magento/graphql-ce/dev/tests/api-functional/framework/Magento/TestFramework/Annotation/ApiDataFixture.php:114

--- dev/tests/integration/testsuite/Magento/Sales/_files/guest_quote_with_addresses.php
+++ dev/tests/integration/testsuite/Magento/Sales/_files/guest_quote_with_addresses.php
@@ -10,27 +10,31 @@ require __DIR__ . '/address_list.php';
 \Magento\TestFramework\Helper\Bootstrap::getInstance()->loadArea(\Magento\Framework\App\Area::AREA_FRONTEND);
 
 $objectManager = \Magento\TestFramework\Helper\Bootstrap::getObjectManager();
-/** @var \Magento\Catalog\Model\Product $product */
-$product = $objectManager->create(\Magento\Catalog\Model\Product::class);
-$product->setTypeId('simple')
-    ->setAttributeSetId($product->getDefaultAttributeSetId())
-    ->setName('Simple Product')
-    ->setSku('simple-product-guest-quote')
-    ->setPrice(10)
-    ->setTaxClassId(0)
-    ->setMetaTitle('meta title')
-    ->setMetaKeyword('meta keyword')
-    ->setMetaDescription('meta description')
-    ->setVisibility(\Magento\Catalog\Model\Product\Visibility::VISIBILITY_BOTH)
-    ->setStatus(\Magento\Catalog\Model\Product\Attribute\Source\Status::STATUS_ENABLED)
-    ->setStockData(
-        [
-            'qty' => 100,
-            'is_in_stock' => 1,
-        ]
-    )->save();
-
 $productRepository = $objectManager->create(\Magento\Catalog\Api\ProductRepositoryInterface::class);
+
+try {
+    /** @var \Magento\Catalog\Model\Product $product */
+    $product = $objectManager->create(\Magento\Catalog\Model\Product::class);
+    $product->setTypeId('simple')
+        ->setAttributeSetId($product->getDefaultAttributeSetId())
+        ->setName('Simple Product')
+        ->setSku('simple-product-guest-quote')
+        ->setPrice(10)
+        ->setTaxClassId(0)
+        ->setMetaTitle('meta title')
+        ->setMetaKeyword('meta keyword')
+        ->setMetaDescription('meta description')
+        ->setVisibility(\Magento\Catalog\Model\Product\Visibility::VISIBILITY_BOTH)
+        ->setStatus(\Magento\Catalog\Model\Product\Attribute\Source\Status::STATUS_ENABLED)
+        ->setStockData(
+            [
+                'qty' => 100,
+                'is_in_stock' => 1,
+            ]
+        )->save();
+} catch (\Magento\Framework\Exception\AlreadyExistsException $exception) {
+    //Product already exists
+}
 $product = $productRepository->get('simple-product-guest-quote');
 
 $addressData = reset($addresses);
