c6a56f6d4befd1b971e7414bdc389bf6959c60de
Stas Kozar <stas.kozar@transoftgroup.com>
2019-03-20 13:22:54 +0200

    MC-5859: Fix action behavior

---
+++ dev/tests/integration/testsuite/Magento/Sales/Controller/Guest/ViewTest.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+use Magento\TestFramework\Request;
+use Magento\TestFramework\TestCase\AbstractController;
+
+/**
+ * Test for \Magento\Sales\Controller\Guest\View class.
+ */
+class ViewTest extends AbstractController
+{
+    /**
+     * Check that controller applied only POST requests.
+     */
+    public function testExecuteWithNonPostRequest()
+    {
+        $this->getRequest()->setMethod(Request::METHOD_GET);
+        $this->dispatch('sales/guest/view/');
+
+        $this->assert404NotFound();
+    }
+}
