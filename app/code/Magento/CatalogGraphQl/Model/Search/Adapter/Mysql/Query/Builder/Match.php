756a9258521867bbf7b6c90978acbebd16ecaded
Evgeny Petrov <evgeny_petrov@epam.com>
2019-03-25 09:28:52 +0300

    MAGETWO-58764: [GitHub] Minimal Query Length For Catalog Search #6681

--- app/code/Magento/CatalogGraphQl/Model/Search/Adapter/Mysql/Query/Builder/Match.php
+++ app/code/Magento/CatalogGraphQl/Model/Search/Adapter/Mysql/Query/Builder/Match.php
@@ -3,6 +3,8 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types=1);
+
 namespace Magento\CatalogGraphQl\Model\Search\Adapter\Mysql\Query\Builder;
 
 use Magento\Framework\DB\Helper\Mysql\Fulltext;
