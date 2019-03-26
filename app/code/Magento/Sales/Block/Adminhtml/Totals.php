4dcc18aba77d27dd7f284bf4f5421be2cf9a585d
Roman Zhupanyn <roma.dj.elf@gmail.com>
2019-03-26 16:01:09 +0200

    MAGETWO-98680: Cart Price Rule code is absent on "view Order/Invoice" Admin page if had been used for Free Shipping

--- app/code/Magento/Sales/Block/Adminhtml/Totals.php
+++ app/code/Magento/Sales/Block/Adminhtml/Totals.php
@@ -5,6 +5,8 @@
  */
 namespace Magento\Sales\Block\Adminhtml;
 
+use Magento\Sales\Model\Order;
+
 class Totals extends \Magento\Sales\Block\Order\Totals
 {
     /**
@@ -67,12 +69,16 @@ class Totals extends \Magento\Sales\Block\Order\Totals
         if (!$this->getSource()->getIsVirtual() && ((double)$this->getSource()->getShippingAmount() ||
             $this->getSource()->getShippingDescription())
         ) {
+            $shippingLabel = __('Shipping & Handling');
+            if ($this->isHasFreeShipping($this->getSource()) && $this->getSource()->getDiscountDescription()) {
+                $shippingLabel .= sprintf(' (%s)', $this->getSource()->getDiscountDescription());
+            }
             $this->_totals['shipping'] = new \Magento\Framework\DataObject(
                 [
                     'code' => 'shipping',
                     'value' => $this->getSource()->getShippingAmount(),
                     'base_value' => $this->getSource()->getBaseShippingAmount(),
-                    'label' => __('Shipping & Handling'),
+                    'label' => $shippingLabel,
                 ]
             );
         }
@@ -108,5 +114,24 @@ class Totals extends \Magento\Sales\Block\Order\Totals
         );
 
         return $this;
+    }
+
+    /**
+     * Availability of free shipping in at least one order item
+     *
+     * @param Order $order
+     * @return bool
+     */
+    private function isHasFreeShipping(Order $order): bool
+    {
+        $isActive = false;
+        foreach ($order->getItems() as $orderItem) {
+            if ($orderItem->getFreeShipping() === '1') {
+                $isActive = true;
+                break;
+            }
+        }
+
+        return $isActive;
     }
 }
---
+++ dev/tests/integration/testsuite/Magento/Sales/Block/Adminhtml/TotalsTest.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Sales\Block\Adminhtml;
+
+use Magento\Framework\View\LayoutInterface;
+use Magento\Sales\Model\Order;
+use Magento\Sales\Model\OrderFactory;
+
+/**
+ * Test class for \Magento\Sales\Block\Adminhtml\Totals
+ */
+class TotalsTest extends \Magento\TestFramework\TestCase\AbstractBackendController
+{
+    /** @var LayoutInterface */
+    protected $_layout;
+
+    /** @var Totals */
+    protected $_block;
+
+    /** @var OrderFactory */
+    private $orderFactory;
+
+    /**
+     * @inheritdoc
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->_layout = $this->_objectManager->get(LayoutInterface::class);
+        $this->_block = $this->_layout->createBlock(Totals::class, 'totals_block');
+        $this->orderFactory = $this->_objectManager->get(OrderFactory::class);
+    }
+
+    /**
+     * @magentoDataFixture Magento/Sales/_files/order_with_free_shipping_by_coupon.php
+     */
+    public function testShowShippingCoupon()
+    {
+        /** @var Order $order */
+        $order = $this->orderFactory->create();
+        $order->loadByIncrementId('100000001');
+
+        $this->_block->setOrder($order);
+        $this->_block->toHtml();
+
+        $shippingTotal = $this->_block->getTotal('shipping');
+        $this->assertNotFalse($shippingTotal, 'Shipping method is absent on the total\'s block.');
+        $this->assertContains('1234567890', $shippingTotal->getLabel(), 'Coupon code is absent in the shipping method label name.');
+    }
+}
