47233d8cd6f62ec8e233679c3fbab185de00009a
RomanKis <roman.kis.y@gmail.com>
2019-03-26 14:27:31 +0200

    #21907 Place order button disabled after failed email address validation check with braintree credit card

--- app/code/Magento/Braintree/view/frontend/web/js/view/payment/method-renderer/hosted-fields.js
+++ app/code/Magento/Braintree/view/frontend/web/js/view/payment/method-renderer/hosted-fields.js
@@ -10,8 +10,9 @@ define([
     'Magento_Braintree/js/view/payment/method-renderer/cc-form',
     'Magento_Braintree/js/validator',
     'Magento_Vault/js/view/payment/vault-enabler',
-    'mage/translate'
-], function ($, Component, validator, VaultEnabler, $t) {
+    'mage/translate',
+    'Magento_Checkout/js/model/payment/additional-validators'
+], function ($, Component, validator, VaultEnabler, $t, additionalValidators) {
     'use strict';
 
     return Component.extend({
@@ -154,7 +155,7 @@ define([
          * Trigger order placing
          */
         placeOrderClick: function () {
-            if (this.validateCardType()) {
+            if (this.validateCardType() && additionalValidators.validate()) {
                 this.isPlaceOrderActionAllowed(false);
                 $(this.getSelector('submit')).trigger('click');
             }
