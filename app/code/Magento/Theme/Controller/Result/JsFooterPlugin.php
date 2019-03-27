bfe315e65e2a907c6f67237e1068a04b1e6c164e
Vitalii Zabaznov <vzabaznov@magento.com>
2019-03-27 14:11:00 -0500

    MC-15439: Deferred loading / parsing of JS

---
+++ app/code/Magento/Theme/Controller/Result/JsFooterPlugin.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Theme\Controller\Result;
+
+/**
+ * Plugin for putting messages to cookies
+ */
+class JsFooterPlugin
+{
+    /**
+     * Put all javascript to footer before sending the response
+     *
+     * @param \Magento\Framework\App\Response\Http $subject
+     * @return void
+     */
+    public function beforeSendResponse(\Magento\Framework\App\Response\Http $subject)
+    {
+        $content = $subject->getContent();
+        $script = [];
+        if (strpos($content, '</body') > 0) {
+            $content = preg_replace('#<!--(?!\s?/?ko).*?-->#s', '', $content);
+            $pattern = '#<script.*?</script>#is';
+            $content = preg_replace_callback(
+                $pattern,
+                function ($matchPart) use (&$script) {
+                    $script[] = $matchPart[0];
+                    return '';
+                },
+                $content
+            );
+            $subject->setContent(
+                str_replace('</body', implode("\n", $script) . "\n</body", $content)
+            );
+        }
+    }
+}
