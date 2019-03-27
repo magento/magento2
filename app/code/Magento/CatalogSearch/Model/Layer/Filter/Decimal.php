7cb0cf8dc21612761b9e58de600447c12ef340dd
Nazarn96 <nazarn96@gmail.com>
2019-03-27 14:41:42 +0200

    fix issue-21960

--- app/code/Magento/CatalogSearch/Model/Layer/Filter/Decimal.php
+++ app/code/Magento/CatalogSearch/Model/Layer/Filter/Decimal.php
@@ -115,7 +115,7 @@ class Decimal extends AbstractFilter
             }
             $label = $this->renderRangeLabel(
                 empty($from) ? 0 : $from,
-                empty($to) ? 0 : $to
+                empty($to) ? $to : $to
             );
             $value = $from . '-' . $to;
 
