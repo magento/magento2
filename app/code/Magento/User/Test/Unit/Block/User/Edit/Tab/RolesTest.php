b901f6dfda1452a7958806bee2f3d19606d4b457
Hwashiang Yu <hwyu@adobe.com>
2019-03-29 11:55:51 -0500

    MAGETWO-98157: User Role Template Update
    - Updated request param collection logic to include non-json
    - Removed unused mocks in rolesTest

--- app/code/Magento/User/Test/Unit/Block/User/Edit/Tab/RolesTest.php
+++ app/code/Magento/User/Test/Unit/Block/User/Edit/Tab/RolesTest.php
@@ -15,69 +15,20 @@ class RolesTest extends \PHPUnit_Framework_TestCase
     /** @var \Magento\User\Block\User\Edit\Tab\Roles */
     protected $model;
 
-    /** @var \Magento\Backend\Helper\Data|\PHPUnit_Framework_MockObject_MockObject */
-    protected $backendHelperMock;
-
     /** @var \Magento\Framework\Json\EncoderInterface|\PHPUnit_Framework_MockObject_MockObject */
     protected $jsonEncoderMock;
-
-    /** @var \Magento\Framework\Registry|\PHPUnit_Framework_MockObject_MockObject */
-    protected $registryMock;
-
-    /** @var \Magento\User\Model\ResourceModel\Role\User\CollectionFactory|\PHPUnit_Framework_MockObject_MockObject */
-    protected $userRolesFactoryMock;
 
     /** @var \Magento\Framework\App\RequestInterface|\PHPUnit_Framework_MockObject_MockObject */
     protected $requestInterfaceMock;
 
-    /** @var \Magento\Framework\UrlInterface|\PHPUnit_Framework_MockObject_MockObject */
-    protected $urlInterfaceMock;
-
-    /** @var \Magento\Framework\View\LayoutInterface|\PHPUnit_Framework_MockObject_MockObject */
-    protected $layoutMock;
-
-    /** @var \Magento\Framework\Filesystem|\PHPUnit_Framework_MockObject_MockObject */
-    protected $filesystemMock;
-
     protected function setUp()
     {
-        $this->backendHelperMock = $this->getMockBuilder('Magento\Backend\Helper\Data')
-            ->disableOriginalConstructor()
-            ->setMethods([])
-            ->getMock();
-
         $this->jsonEncoderMock = $this->getMockBuilder('Magento\Framework\Json\EncoderInterface')
             ->disableOriginalConstructor()
             ->setMethods([])
             ->getMock();
 
-        $this->registryMock = $this->getMockBuilder('Magento\Framework\Registry')
-            ->disableOriginalConstructor()
-            ->setMethods([])
-            ->getMock();
-
-        $this->userRolesFactoryMock = $this
-            ->getMockBuilder('Magento\Authorization\Model\ResourceModel\Role\CollectionFactory')
-            ->disableOriginalConstructor()
-            ->setMethods(['create'])
-            ->getMock();
-
         $this->requestInterfaceMock = $this->getMockBuilder('Magento\Framework\App\RequestInterface')
-            ->disableOriginalConstructor()
-            ->setMethods([])
-            ->getMock();
-
-        $this->urlInterfaceMock = $this->getMockBuilder('Magento\Framework\UrlInterface')
-            ->disableOriginalConstructor()
-            ->setMethods([])
-            ->getMock();
-
-        $this->layoutMock = $this->getMockBuilder('Magento\Framework\View\LayoutInterface')
-            ->disableOriginalConstructor()
-            ->setMethods([])
-            ->getMock();
-
-        $this->filesystemMock = $this->getMockBuilder('Magento\Framework\Filesystem')
             ->disableOriginalConstructor()
             ->setMethods([])
             ->getMock();
@@ -86,14 +37,8 @@ class RolesTest extends \PHPUnit_Framework_TestCase
         $this->model = $objectManagerHelper->getObject(
             'Magento\User\Block\User\Edit\Tab\Roles',
             [
-                'backendHelper' => $this->backendHelperMock,
                 'jsonEncoder' => $this->jsonEncoderMock,
-                'userRolesFactory' => $this->userRolesFactoryMock,
-                'coreRegistry' => $this->registryMock,
                 'request' => $this->requestInterfaceMock,
-                'urlBuilder' => $this->urlInterfaceMock,
-                'layout' => $this->layoutMock,
-                'filesystem' => $this->filesystemMock
             ]
         );
     }
