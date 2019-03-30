ba877303cdc45d721a0ea8fcf10a6b2a3f4e6127
Bartłomiej Szymański <bartlomiej.szymanski@bold.net.pl>
2019-03-30 13:47:38 +0100

    Get Rid of MySQL View usage on Default Stock

--- app/code/Magento/InventoryCatalog/Model/ResourceModel/AddIsInStockFieldToCollection.php
+++ app/code/Magento/InventoryCatalog/Model/ResourceModel/AddIsInStockFieldToCollection.php
@@ -8,6 +8,7 @@ declare(strict_types=1);
 namespace Magento\InventoryCatalog\Model\ResourceModel;
 
 use Magento\Catalog\Model\ResourceModel\Product\Collection;
+use Magento\InventoryCatalogApi\Api\DefaultStockProviderInterface;
 use Magento\InventoryIndexer\Indexer\IndexStructure;
 use Magento\InventoryIndexer\Model\StockIndexTableNameResolverInterface;
 
@@ -22,27 +23,44 @@ class AddIsInStockFieldToCollection
     private $stockIndexTableProvider;
 
     /**
+     * @var DefaultStockProviderInterface
+     */
+    private $defaultStockProvider;
+
+    /**
      * @param StockIndexTableNameResolverInterface $stockIndexTableProvider
+     * @param DefaultStockProviderInterface $defaultStockProvider
      */
     public function __construct(
-        StockIndexTableNameResolverInterface $stockIndexTableProvider
+        StockIndexTableNameResolverInterface $stockIndexTableProvider,
+        DefaultStockProviderInterface $defaultStockProvider
     ) {
         $this->stockIndexTableProvider = $stockIndexTableProvider;
+        $this->defaultStockProvider = $defaultStockProvider;
     }
 
     /**
      * @param Collection $collection
      * @param int $stockId
+     *
      * @return void
      */
-    public function execute($collection, int $stockId)
+    public function execute($collection, int $stockId): void
     {
-        $tableName = $this->stockIndexTableProvider->execute($stockId);
+        if ($stockId === $this->defaultStockProvider->getId()) {
+            $isSalableColumnName = 'is_in_stock';
+            $resource = $collection->getResource();
+            $collection->getSelect()->join(
+                ['inventory_in_stock' => $resource->getTable('cataloginventory_stock_item')],
+                sprintf('%s.entity_id = inventory_in_stock.product_id', Collection::MAIN_TABLE_ALIAS),
+                [IndexStructure::IS_SALABLE => $isSalableColumnName]
+            );
+        } else {
+            $tableName = $this->stockIndexTableProvider->execute($stockId);
 
-        $collection->getSelect()->join(
-            ['inventory_in_stock' => $tableName],
-            'e.sku = inventory_in_stock.sku',
-            []
-        )->where('inventory_in_stock.' . IndexStructure::IS_SALABLE . ' = ?', 1);
+            $collection->getSelect()->join(
+                ['inventory_in_stock' => $tableName], 'e.sku = inventory_in_stock.sku', []
+            )->where('inventory_in_stock.' . IndexStructure::IS_SALABLE . ' = ?', 1);
+        }
     }
 }
