ad7c873791157982f7cf7e652b4b8dfa9ec0fcfc
Shikha Mishra <shikhamishra@cedcoss.com>
2019-03-31 21:12:30 +0530

    Updated Builder.php

--- app/code/Magento/Catalog/Controller/Adminhtml/Product/Builder.php
+++ app/code/Magento/Catalog/Controller/Adminhtml/Product/Builder.php
@@ -3,20 +3,25 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types=1);
+
 namespace Magento\Catalog\Controller\Adminhtml\Product;
 
-use Magento\Catalog\Api\ProductRepositoryInterface;
-use Magento\Catalog\Model\Product;
-use Magento\Catalog\Model\Product\Type as ProductTypes;
+use Magento\Catalog\Api\Data\ProductInterface;
 use Magento\Catalog\Model\ProductFactory;
 use Magento\Cms\Model\Wysiwyg as WysiwygModel;
 use Magento\Framework\App\RequestInterface;
-use Magento\Framework\Registry;
 use Magento\Store\Model\StoreFactory;
 use Psr\Log\LoggerInterface as Logger;
+use Magento\Framework\Registry;
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Catalog\Model\Product;
+use Magento\Catalog\Model\Product\Type as ProductTypes;
 
 /**
- *  Product builder service.
+ * Build a product based on a request
+ *
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
  */
 class Builder
 {
@@ -82,10 +87,11 @@ class Builder
      * Build product based on user request
      *
      * @param RequestInterface $request
-     * @return \Magento\Catalog\Model\Product
+     * @return ProductInterface
      * @throws \RuntimeException
+     * @throws \Magento\Framework\Exception\LocalizedException
      */
-    public function build(RequestInterface $request)
+    public function build(RequestInterface $request): ProductInterface
     {
         $productId = (int) $request->getParam('id');
         $storeId = $request->getParam('store', 0);
@@ -95,6 +101,9 @@ class Builder
         if ($productId) {
             try {
                 $product = $this->productRepository->getById($productId, true, $storeId);
+                if ($attributeSetId) {
+                    $product->setAttributeSetId($attributeSetId);
+                }
             } catch (\Exception $e) {
                 $product = $this->createEmptyProduct(ProductTypes::DEFAULT_TYPE, $attributeSetId, $storeId);
                 $this->logger->critical($e);
@@ -105,11 +114,6 @@ class Builder
 
         $store = $this->storeFactory->create();
         $store->load($storeId);
-        
-        $setId = (int)$request->getParam('set');
-        if ($setId) {
-            $product->setAttributeSetId($setId);
-        }
 
         $this->registry->register('product', $product);
         $this->registry->register('current_product', $product);
@@ -121,7 +125,7 @@ class Builder
     }
 
     /**
-     * Create new product.
+     * Create a product with the given properties
      *
      * @param int $typeId
      * @param int $attributeSetId
