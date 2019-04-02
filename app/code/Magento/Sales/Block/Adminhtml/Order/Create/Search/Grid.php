92b0e8b1247c2e039046bfc33a6c5930ecf67bbe
Oleksandr Iegorov <oiegorov@magento.com>
2019-04-02 15:46:57 -0500

    MAGETWO-98831: The SKU was not found in the catalog

--- app/code/Magento/Sales/Block/Adminhtml/Order/Create/Search/Grid.php
+++ app/code/Magento/Sales/Block/Adminhtml/Order/Create/Search/Grid.php
@@ -5,6 +5,10 @@
  */
 namespace Magento\Sales\Block\Adminhtml\Order\Create\Search;
 
+use Magento\Sales\Block\Adminhtml\Order\Create\Search\Grid\DataProvider\ProductCollection
+    as ProductCollectionDataProvider;
+use Magento\Framework\App\ObjectManager;
+
 /**
  * Adminhtml sales order create search products block
  *
@@ -43,6 +47,11 @@ class Grid extends \Magento\Backend\Block\Widget\Grid\Extended
     protected $_productFactory;
 
     /**
+     * @var ProductCollectionDataProvider $productCollectionProvider
+     */
+    private $productCollectionProvider;
+
+    /**
      * @param \Magento\Backend\Block\Template\Context $context
      * @param \Magento\Backend\Helper\Data $backendHelper
      * @param \Magento\Catalog\Model\ProductFactory $productFactory
@@ -50,6 +59,7 @@ class Grid extends \Magento\Backend\Block\Widget\Grid\Extended
      * @param \Magento\Backend\Model\Session\Quote $sessionQuote
      * @param \Magento\Sales\Model\Config $salesConfig
      * @param array $data
+     * @param ProductCollectionDataProvider|null $productCollectionProvider
      */
     public function __construct(
         \Magento\Backend\Block\Template\Context $context,
@@ -58,12 +68,15 @@ class Grid extends \Magento\Backend\Block\Widget\Grid\Extended
         \Magento\Catalog\Model\Config $catalogConfig,
         \Magento\Backend\Model\Session\Quote $sessionQuote,
         \Magento\Sales\Model\Config $salesConfig,
-        array $data = []
+        array $data = [],
+        ProductCollectionDataProvider $productCollectionProvider = null
     ) {
         $this->_productFactory = $productFactory;
         $this->_catalogConfig = $catalogConfig;
         $this->_sessionQuote = $sessionQuote;
         $this->_salesConfig = $salesConfig;
+        $this->productCollectionProvider = $productCollectionProvider
+            ?: ObjectManager::getInstance()->get(ProductCollectionDataProvider::class);
         parent::__construct($context, $backendHelper, $data);
     }
 
@@ -140,8 +153,21 @@ class Grid extends \Magento\Backend\Block\Widget\Grid\Extended
      */
     protected function _prepareCollection()
     {
+
         $attributes = $this->_catalogConfig->getProductAttributes();
+        $store = $this->getStore();
+
         /* @var $collection \Magento\Catalog\Model\ResourceModel\Product\Collection */
+        $collection = $this->productCollectionProvider->getCollectionForStore($store);
+        $collection->addAttributeToSelect(
+            $attributes
+        );
+        $collection->addAttributeToFilter(
+            'type_id',
+            $this->_salesConfig->getAvailableProductTypes()
+        );
+
+        /*
         $collection = $this->_productFactory->create()->getCollection();
         $collection->setStore(
             $this->getStore()
@@ -155,6 +181,7 @@ class Grid extends \Magento\Backend\Block\Widget\Grid\Extended
         )->addAttributeToSelect(
             'gift_message_available'
         );
+        */
 
         $this->setCollection($collection);
         return parent::_prepareCollection();
