ae3d7dfdafc89070db3f6f68debe2587ca1d6f1e
Vitaliy Boyko <v.boyko@atwix.com>
2019-04-02 19:56:44 +0300

    magento2-20434: fixed rollback

--- dev/tests/integration/testsuite/Magento/CatalogUrlRewrite/_files/product_rewrite_multistore_rollback.php
+++ dev/tests/integration/testsuite/Magento/CatalogUrlRewrite/_files/product_rewrite_multistore_rollback.php
@@ -3,10 +3,32 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+
 use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\Registry;
 use Magento\TestFramework\Helper\Bootstrap;
 
-$objectManager = Bootstrap::getObjectManager();
+Bootstrap::getInstance()->getInstance()->reinitialize();
+
+/** @var Registry $registry */
+$registry = Bootstrap::getObjectManager()->get(Registry::class);
+
+$registry->unregister('isSecureArea');
+$registry->register('isSecureArea', true);
+
+/** @var ProductRepositoryInterface $productRepository */
+$productRepository = Bootstrap::getObjectManager()
+    ->get(ProductRepositoryInterface::class);
+try {
+    $product = $productRepository->get('product1', true);
+    if ($product->getId()) {
+        $productRepository->delete($product);
+    }
+} catch (NoSuchEntityException $e) {
+}
+$registry->unregister('isSecureArea');
+$registry->register('isSecureArea', false);
 
 require __DIR__ . '/../../Store/_files/store_rollback.php';
 require __DIR__ . '/../../Store/_files/second_store_rollback.php';
