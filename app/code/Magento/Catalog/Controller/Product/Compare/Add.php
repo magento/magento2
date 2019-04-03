5551435b7a4b81346fb6100bd83bdd264b04a3d8
Serhii Voloshkov <serhii.voloshkov@transoftgroup.com>
2019-04-03 11:32:01 +0300

    MAGETWO-98231: Compare action minor changes

--- app/code/Magento/Catalog/Controller/Product/Compare/Add.php
+++ app/code/Magento/Catalog/Controller/Product/Compare/Add.php
@@ -8,10 +8,13 @@ namespace Magento\Catalog\Controller\Product\Compare;
 
 use Magento\Framework\Exception\NoSuchEntityException;
 
+/**
+ * Add item to compare list action.
+ */
 class Add extends \Magento\Catalog\Controller\Product\Compare
 {
     /**
-     * Add item to compare list
+     * Add item to compare list.
      *
      * @return \Magento\Framework\Controller\ResultInterface
      */
@@ -26,12 +29,13 @@ class Add extends \Magento\Catalog\Controller\Product\Compare
         if ($productId && ($this->_customerVisitor->getId() || $this->_customerSession->isLoggedIn())) {
             $storeId = $this->_storeManager->getStore()->getId();
             try {
+                /** @var \Magento\Catalog\Model\Product $product */
                 $product = $this->productRepository->getById($productId, false, $storeId);
             } catch (NoSuchEntityException $e) {
                 $product = null;
             }
 
-            if ($product) {
+            if ($product && $product->isSalable()) {
                 $this->_catalogProductCompareList->addProduct($product);
                 $productName = $this->_objectManager->get(\Magento\Framework\Escaper::class)
                     ->escapeHtml($product->getName());
@@ -43,6 +47,7 @@ class Add extends \Magento\Catalog\Controller\Product\Compare
 
             $this->_objectManager->get(\Magento\Catalog\Helper\Product\Compare::class)->calculate();
         }
+
         return $resultRedirect->setRefererOrBaseUrl();
     }
 
