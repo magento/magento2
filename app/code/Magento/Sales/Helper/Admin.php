ab7bbf5f3f2ef7827e7f9eb0ea88a06144f36a94
OlgaVasyltsun <olga.vasyltsun@gmail.com>
2019-04-03 14:26:03 +0300

    MAGETWO-97447: Escaper refactor

--- app/code/Magento/Sales/Helper/Admin.php
+++ app/code/Magento/Sales/Helper/Admin.php
@@ -6,6 +6,9 @@
 
 namespace Magento\Sales\Helper;
 
+use Magento\Framework\App\ObjectManager;
+use Symfony\Component\DomCrawler\Crawler;
+
 /**
  * Sales admin helper.
  */
@@ -149,30 +152,35 @@ class Admin extends \Magento\Framework\App\Helper\AbstractHelper
     public function escapeHtmlWithLinks($data, $allowedTags = null)
     {
         if (!empty($data) && is_array($allowedTags) && in_array('a', $allowedTags)) {
-            $links = [];
-            $i = 1;
-            $data = str_replace('%', '%%', $data);
-            $regexp = "#(?J)<a"
-                ."(?:(?:\s+(?:(?:href\s*=\s*(['\"])(?<link>.*?)\\1\s*)|(?:\S+\s*=\s*(['\"])(.*?)\\3)\s*)*)|>)"
-                .">?(?:(?:(?<text>.*?)(?:<\/a\s*>?|(?=<\w))|(?<text>.*)))#si";
-            while (preg_match($regexp, $data, $matches)) {
-                $text = '';
-                if (!empty($matches['text'])) {
-                    $text = str_replace('%%', '%', $matches['text']);
+            $crawler = ObjectManager::getInstance()->create(
+                Crawler::class,
+                [
+                    'node' => '<html><body>' . $data . '</body></html>',
+                ]
+            );
+
+            $linkTags = $crawler->filter('a');
+
+            foreach ($linkTags as $linkNode) {
+                $linkAttributes = [];
+                foreach ($linkNode->attributes as $attribute) {
+                    $linkAttributes[$attribute->name] = $attribute->value;
                 }
-                $url = $this->filterUrl($matches['link'] ?? '');
-                //Recreate a minimalistic secure a tag
-                $links[] = sprintf(
-                    '<a href="%s">%s</a>',
-                    htmlspecialchars($url, ENT_QUOTES, 'UTF-8', false),
-                    $this->escaper->escapeHtml($text)
-                );
-                $data = str_replace($matches[0], '%' . $i . '$s', $data);
-                ++$i;
+
+                foreach ($linkAttributes as $attributeName => $attributeValue) {
+                    if ($attributeName === 'href') {
+                        $url = $this->filterUrl($attributeValue ?? '');
+                        $url = $this->escaper->escapeUrl($url);
+                        $linkNode->setAttribute('href', $url);
+                    } else {
+                        $linkNode->removeAttribute($attributeName);
+                    }
+                }
             }
-            $data = $this->escaper->escapeHtml($data, $allowedTags);
-            return vsprintf($data, $links);
+
+            $data = mb_convert_encoding($crawler->filter('body')->html(), 'UTF-8', 'HTML-ENTITIES');
         }
+
         return $this->escaper->escapeHtml($data, $allowedTags);
     }
 
@@ -186,8 +194,9 @@ class Admin extends \Magento\Framework\App\Helper\AbstractHelper
     {
         if ($url) {
             //Revert the sprintf escaping
-            $url = str_replace('%%', '%', $url);
+            //phpcs:disable
             $urlScheme = parse_url($url, PHP_URL_SCHEME);
+            //phpcs:enable
             $urlScheme = $urlScheme ? strtolower($urlScheme) : '';
             if ($urlScheme !== 'http' && $urlScheme !== 'https') {
                 $url = null;
---
+++ dev/tests/integration/testsuite/Magento/Sales/Helper/AdminTest.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\Sales\Helper;
+
+use Magento\TestFramework\Helper\Bootstrap;
+
+/**
+ * Tests \Magento\Sales\Helper\Admin
+ */
+class AdminTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @var Admin
+     */
+    private $helper;
+
+    /**
+     * @inheritdoc
+     */
+    protected function setUp()
+    {
+        $this->helper = Bootstrap::getObjectManager()->create(Admin::class);
+    }
+
+    /**
+     * @param string $data
+     * @param string $expected
+     * @param null|array $allowedTags
+     * @return void
+     *
+     * @dataProvider escapeHtmlWithLinksDataProvider
+     */
+    public function testEscapeHtmlWithLinks(string $data, string $expected, $allowedTags = null)
+    {
+        $actual = $this->helper->escapeHtmlWithLinks($data, $allowedTags);
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * @return array
+     */
+    public function escapeHtmlWithLinksDataProvider(): array
+    {
+        return [
+            [
+                '<a>some text in tags</a>',
+                '&lt;a&gt;some text in tags&lt;/a&gt;',
+                'allowedTags' => null,
+            ],
+            [
+                'Transaction ID: "<a target="_blank" href="https://www.paypal.com/?id=XX123XX">XX123XX</a>"',
+                'Transaction ID: &quot;<a href="https://www.paypal.com/?id=XX123XX">XX123XX</a>&quot;',
+                'allowedTags' => ['b', 'br', 'strong', 'i', 'u', 'a'],
+            ],
+            [
+                '<a>some text in tags</a>',
+                '<a>some text in tags</a>',
+                'allowedTags' => ['a'],
+            ],
+            [
+                "<a><script>alert(1)</script></a>",
+                '<a>alert(1)</a>',
+                'allowedTags' => ['a'],
+            ],
+            [
+                '<a href=\"#\">Foo</a>',
+                '<a href="#">Foo</a>',
+                'allowedTags' => ['a'],
+            ],
+            [
+                "<a href=http://example.com?foo=1&bar=2&baz[name]=BAZ>Foo</a>",
+                '<a href="http://example.com?foo=1&amp;bar=2&amp;baz%5Bname%5D=BAZ">Foo</a>',
+                'allowedTags' => ['a'],
+            ],
+            [
+                "<a href=\"javascript&colon;alert(59)\">Foo</a>",
+                '<a href="#">Foo</a>',
+                'allowedTags' => ['a'],
+            ],
+            [
+                "<a href=\"http://example1.com\" href=\"http://example2.com\">Foo</a>",
+                '<a href="http://example1.com">Foo</a>',
+                'allowedTags' => ['a'],
+            ],
+            [
+                "<a href=\"http://example.com?foo=text with space\">Foo</a>",
+                '<a href="http://example.com?foo=text%20with%20space">Foo</a>',
+                'allowedTags' => ['a'],
+            ],
+        ];
+    }
+}
