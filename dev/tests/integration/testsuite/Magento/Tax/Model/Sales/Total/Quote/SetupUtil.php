5698d5610acd6558f4f82450ce9907a275a11ba3
rostyslav.hymon <rostyslav.hymon@transoftgroup.com>
2019-04-04 10:17:20 +0300

    MAGETWO-93592: Tax calculation process does not follow "Apply Tax On" setting

--- dev/tests/integration/testsuite/Magento/Tax/Model/Sales/Total/Quote/SetupUtil.php
+++ dev/tests/integration/testsuite/Magento/Tax/Model/Sales/Total/Quote/SetupUtil.php
@@ -9,10 +9,19 @@
 namespace Magento\Tax\Model\Sales\Total\Quote;
 
 use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Quote\Model\Quote;
 use Magento\Tax\Model\Config;
 use Magento\Tax\Model\Calculation;
+use Magento\Quote\Model\Quote\Item\Updater;
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Catalog\Api\Data\ProductInterface;
+use Magento\Framework\Api\Filter;
+use Magento\Framework\Api\Search\FilterGroup;
+use Magento\Framework\Api\SearchCriteriaInterface;
 
 /**
+ * Setup utility for quote
+ *
  * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
  */
 class SetupUtil
@@ -593,7 +602,7 @@ class SetupUtil
      *
      * @param array $quoteData
      * @param \Magento\Customer\Api\Data\CustomerInterface $customer
-     * @return \Magento\Quote\Model\Quote
+     * @return Quote
      */
     protected function createQuote($quoteData, $customer)
     {
@@ -618,8 +627,8 @@ class SetupUtil
         $quoteBillingAddress = $this->objectManager->create(\Magento\Quote\Model\Quote\Address::class);
         $quoteBillingAddress->importCustomerAddressData($addressService->getById($billingAddress->getId()));
 
-        /** @var \Magento\Quote\Model\Quote $quote */
-        $quote = $this->objectManager->create(\Magento\Quote\Model\Quote::class);
+        /** @var Quote $quote */
+        $quote = $this->objectManager->create(Quote::class);
         $quote->setStoreId(1)
             ->setIsActive(true)
             ->setIsMultiShipping(false)
@@ -633,7 +642,7 @@ class SetupUtil
     /**
      * Add products to quote
      *
-     * @param \Magento\Quote\Model\Quote $quote
+     * @param Quote $quote
      * @param array $itemsData
      * @return $this
      */
@@ -656,7 +665,8 @@ class SetupUtil
      * Create a quote based on given data
      *
      * @param array $quoteData
-     * @return \Magento\Quote\Model\Quote
+     *
+     * @return Quote
      */
     public function setupQuote($quoteData)
     {
@@ -665,7 +675,9 @@ class SetupUtil
         $quote = $this->createQuote($quoteData, $customer);
 
         $this->addProductToQuote($quote, $quoteData['items']);
-
+        if (isset($quoteData['update_items'])) {
+            $this->updateItems($quote, $quoteData['update_items']);
+        }
         //Set shipping amount
         if (isset($quoteData['shipping_method'])) {
             $quote->getShippingAddress()->setShippingMethod($quoteData['shipping_method']);
@@ -681,5 +693,34 @@ class SetupUtil
         }
 
         return $quote;
+    }
+
+    /**
+     * Update quote items
+     *
+     * @param Quote $quote
+     * @param array $items
+     *
+     * @return void
+     */
+    private function updateItems(Quote $quote, array $items)
+    {
+        $updater = $this->objectManager->get(Updater::class);
+        $productRepository = $this->objectManager->get(ProductRepositoryInterface::class);
+        $filter = $this->objectManager->create(Filter::class);
+        $filter->setField('sku')->setValue(array_keys($items));
+        $filterGroup = $this->objectManager->create(FilterGroup::class);
+        $filterGroup->setFilters([$filter]);
+        $searchCriteria = $this->objectManager->create(SearchCriteriaInterface::class);
+        $searchCriteria->setFilterGroups([$filterGroup]);
+        $products = $productRepository->getList($searchCriteria)->getItems();
+        /** @var ProductInterface $product */
+        foreach ($products as $product) {
+            $quoteItem = $quote->getItemByProduct($product);
+            $updater->update(
+                $quoteItem,
+                $items[$product->getSku()]
+            );
+        }
     }
 }
