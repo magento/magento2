7d7f79884714523371412921ff8181c8a3f1031d
Oleksandr Miroshnichenko <omiroshnichenko@magento.com>
2019-04-05 09:43:40 -0500

    MC-13806: Replace rand with random_int - fix static tests

--- lib/internal/Magento/Framework/Encryption/Crypt.php
+++ lib/internal/Magento/Framework/Encryption/Crypt.php
@@ -41,13 +41,13 @@ class Crypt
     /**
      * Constructor
      *
-     * @param  string      $key        Secret encryption key.
-     *                                 It's unsafe to store encryption key in memory, so no getter for key exists.
-     * @param  string      $cipher     Cipher algorithm (one of the MCRYPT_ciphername constants)
-     * @param  string      $mode       Mode of cipher algorithm (MCRYPT_MODE_modeabbr constants)
-     * @param  string|bool $initVector Initial vector to fill algorithm blocks.
-     *                                 TRUE generates a random initial vector.
-     *                                 FALSE fills initial vector with zero bytes to not use it.
+     * @param string $key Secret encryption key.
+     *                    It's unsafe to store encryption key in memory, so no getter for key exists.
+     * @param string $cipher Cipher algorithm (one of the MCRYPT_ciphername constants)
+     * @param string $mode Mode of cipher algorithm (MCRYPT_MODE_modeabbr constants)
+     * @param string|bool $initVector Initial vector to fill algorithm blocks.
+     *                                TRUE generates a random initial vector.
+     *                                FALSE fills initial vector with zero bytes to not use it.
      * @throws \Exception
      */
     public function __construct(
