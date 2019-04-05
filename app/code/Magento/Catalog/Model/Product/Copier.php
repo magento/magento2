48b0ab873183966d3f797df97d64cf7fccc1bd5d
Yuriy <Vechirko.Yuriy@gmail.com>
2019-04-05 16:20:10 +0300

    #21737 Duplicating product with translated url keys over multiple storeviews causes non-unique url keys to be generated

--- app/code/Magento/Catalog/Model/Product/Copier.php
+++ app/code/Magento/Catalog/Model/Product/Copier.php
@@ -74,28 +74,81 @@ class Copier
         $duplicate->setUpdatedAt(null);
         $duplicate->setId(null);
         $duplicate->setStoreId(\Magento\Store\Model\Store::DEFAULT_STORE_ID);
-
         $this->copyConstructor->build($product, $duplicate);
-        $isDuplicateSaved = false;
-        do {
-            $urlKey = $duplicate->getUrlKey();
-            $urlKey = preg_match('/(.*)-(\d+)$/', $urlKey, $matches)
-                ? $matches[1] . '-' . ($matches[2] + 1)
-                : $urlKey . '-1';
-            $duplicate->setUrlKey($urlKey);
-            $duplicate->setData('url_path', null);
-            try {
-                $duplicate->save();
-                $isDuplicateSaved = true;
-            } catch (\Magento\Framework\Exception\AlreadyExistsException $e) {
-            }
-        } while (!$isDuplicateSaved);
+        $this->setDefaultUrl($product, $duplicate);
+        $this->setStoresUrl($product, $duplicate);
         $this->getOptionRepository()->duplicate($product, $duplicate);
         $product->getResource()->duplicate(
             $product->getData($metadata->getLinkField()),
             $duplicate->getData($metadata->getLinkField())
         );
         return $duplicate;
+    }
+
+    /**
+     * Set default URL.
+     *
+     * @param Product $product
+     * @param Product $duplicate
+     * @return void
+     */
+    private function setDefaultUrl(Product $product, Product $duplicate) : void
+    {
+        $duplicate->setStoreId(\Magento\Store\Model\Store::DEFAULT_STORE_ID);
+        $resource = $product->getResource();
+        $attribute = $resource->getAttribute('url_key');
+        $productId = $product->getId();
+        $urlKey = $resource->getAttributeRawValue($productId, 'url_key', \Magento\Store\Model\Store::DEFAULT_STORE_ID);
+        do {
+            $urlKey = $this->modifyUrl($urlKey);
+            $duplicate->setUrlKey($urlKey);
+        } while (!$attribute->getEntity()->checkAttributeUniqueValue($attribute, $duplicate));
+        $duplicate->setData('url_path', null);
+        $duplicate->save();
+    }
+
+    /**
+     * Set URL for each store.
+     *
+     * @param Product $product
+     * @param Product $duplicate
+     * @return void
+     */
+    private function setStoresUrl(Product $product, Product $duplicate) : void
+    {
+        $storeIds = $duplicate->getStoreIds();
+        $resource = $product->getResource();
+        $productId = $product->getId();
+        $duplicate->setData('save_rewrites_history', false);
+        foreach ($storeIds as $storeId) {
+            $isDuplicateSaved = false;
+            $duplicate->setStoreId($storeId);
+            $urlKey = $resource->getAttributeRawValue($productId, 'url_key', $storeId);
+            do {
+                $urlKey = $this->modifyUrl($urlKey);
+                $duplicate->setUrlKey($urlKey);
+                $duplicate->setData('url_path', null);
+                try {
+                    $duplicate->save();
+                    $isDuplicateSaved = true;
+                } catch (\Magento\Framework\Exception\AlreadyExistsException $e) {
+                }
+            } while (!$isDuplicateSaved);
+        }
+        $duplicate->setStoreId(\Magento\Store\Model\Store::DEFAULT_STORE_ID);
+    }
+
+    /**
+     * Modify URL key.
+     *
+     * @param string $urlKey
+     * @return string
+     */
+    private function modifyUrl(string $urlKey) : string
+    {
+        return preg_match('/(.*)-(\d+)$/', $urlKey, $matches)
+                    ? $matches[1] . '-' . ($matches[2] + 1)
+                    : $urlKey . '-1';
     }
 
     /**
