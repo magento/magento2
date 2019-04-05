673291c2ca92cc7b9591a377e3a1bd88da259772
Bettina Cerban <bettinacerban@gmail.com>
2019-04-05 16:05:05 -0300

    specify full class path for interface

--- app/code/Magento/InventoryCatalogApi/Api/BulkPartialInventoryTransferInterface.php
+++ app/code/Magento/InventoryCatalogApi/Api/BulkPartialInventoryTransferInterface.php
@@ -7,16 +7,13 @@ declare(strict_types=1);
 
 namespace Magento\InventoryCatalogApi\Api;
 
-use Magento\InventoryApi\Api\Data\SourceItemInterface;
-use Magento\InventoryCatalogApi\Api\Data\PartialInventoryTransferInterface;
-
 interface BulkPartialInventoryTransferInterface
 {
     /**
      * Run bulk partial inventory transfer for specified items.
      *
-     * @param PartialInventoryTransferInterface[] $items
-     * @return SourceItemInterface[]
+     * @param \Magento\InventoryCatalogApi\Api\Data\PartialInventoryTransferInterface[] $items
+     * @return \Magento\InventoryApi\Api\Data\SourceItemInterface[]
      */
     public function execute(array $items): array;
 }
