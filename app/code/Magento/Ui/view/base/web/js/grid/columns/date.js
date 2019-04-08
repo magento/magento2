3f22c453d621f193222201536228a15b0593a05e
Karla Saaremäe <karlasaaremae@gmail.com>
2019-04-08 14:23:30 +0300

    remove spaces
    code clean up

--- app/code/Magento/Ui/view/base/web/js/grid/columns/date.js
+++ app/code/Magento/Ui/view/base/web/js/grid/columns/date.js
@@ -25,7 +25,7 @@ define([
          */
         initConfig: function () {
             this._super();
-            
+
             this.dateFormat = utils.normalizeDate(this.dateFormat ? this.dateFormat : this.options.dateFormat);
 
             return this;
