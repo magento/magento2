78e7c35f363556d99f46c7c30c703ff0af27626e
Stepan Furman <furman.stepan@gmail.com>
2019-04-11 01:22:57 +0300

    MSI-2062: Inventory Export Stock implementation. Static fix

--- app/code/Magento/InventoryExportStock/Model/ExportStockData.php
+++ app/code/Magento/InventoryExportStock/Model/ExportStockData.php
@@ -1,11 +1,20 @@
 <?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
 
 namespace Magento\InventoryExportStock\Model;
 
+use Exception;
 use Magento\Framework\Api\SearchCriteriaInterface;
 use Magento\InventoryExportStockApi\Api\Data\ExportStockDataSearchResultInterface;
 use Magento\InventoryExportStockApi\Api\ExportStockDataInterface;
 
+/**
+ * Class ExportStockData
+ */
 class ExportStockData implements ExportStockDataInterface
 {
     /**
@@ -26,8 +35,7 @@ class ExportStockData implements ExportStockDataInterface
     /**
      * @inheritDoc
      *
-     * @throws \Magento\Framework\Exception\InputException
-     * @throws \Magento\Framework\Exception\LocalizedException
+     * @throws Exception
      */
     public function execute(
         SearchCriteriaInterface $searchCriteria,
