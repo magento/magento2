67375909d85eb93e5c56e46ba2be50d532292e8b
Eugene Shakhsuvarov <shakhsuv@adobe.com>
2019-04-10 19:26:16 +0300

    Minor coding standards fixes

--- app/code/Magento/InventoryShipping/Plugin/Sales/Model/Order/GetListShipmentRepositoryPlugin.php
+++ app/code/Magento/InventoryShipping/Plugin/Sales/Model/Order/GetListShipmentRepositoryPlugin.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
 declare(strict_types=1);
 
 namespace Magento\InventoryShipping\Plugin\Sales\Model\Order;
@@ -7,19 +11,22 @@ use Magento\InventoryShipping\Model\ResourceModel\ShipmentSource\GetSourceCodeBy
 use Magento\Sales\Api\Data\ShipmentExtensionFactory;
 use Magento\Sales\Model\Order\ShipmentRepository;
 
+/**
+ * Add Source Information to shipments loaded with Magento\Sales\Api\ShipmentRepositoryInterface::getList
+ */
 class GetListShipmentRepositoryPlugin
 {
     /**
      * @var ShipmentExtensionFactory
      */
     private $shipmentExtensionFactory;
+
     /**
      * @var GetSourceCodeByShipmentId
      */
     private $getSourceCodeByShipmentId;
 
     /**
-     * GetListShipmentRepositoryPlugin constructor.
      * @param ShipmentExtensionFactory $shipmentExtensionFactory
      * @param GetSourceCodeByShipmentId $getSourceCodeByShipmentId
      */
@@ -32,14 +39,17 @@ class GetListShipmentRepositoryPlugin
     }
 
     /**
+     * Add Source Information to shipments.
+     *
      * @param ShipmentRepository $subject
      * @param \Magento\Sales\Api\Data\ShipmentInterface[] $searchResult
      * @return \Magento\Sales\Api\Data\ShipmentInterface[]
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
     public function afterGetList(ShipmentRepository $subject, $searchResult)
     {
         /** @var \Magento\Sales\Api\Data\ShipmentInterface $shipment */
-        foreach ($searchResult->getItems() as &$shipment) {
+        foreach ($searchResult->getItems() as $shipment) {
             $shipmentExtension = $shipment->getExtensionAttributes();
             if (empty($shipmentExtension)) {
                 $shipmentExtension = $this->shipmentExtensionFactory->create();
