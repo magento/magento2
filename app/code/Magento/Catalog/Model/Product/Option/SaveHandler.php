9df2936fd709a38b7930d42783713a683d064d04
DianaRusin <rusind95@gmail.com>
2019-04-12 16:03:32 +0300

    MAGETWO-73157: [GITHUB] Import customizable options adds it to another product if same SKU is filled#9457

--- app/code/Magento/Catalog/Model/Product/Option/SaveHandler.php
+++ app/code/Magento/Catalog/Model/Product/Option/SaveHandler.php
@@ -60,7 +60,7 @@ class SaveHandler implements ExtensionInterface
             }
         }
         if ($options) {
-            $this->processOptionsSaving($options, (bool)$entity->dataHasChangedFor('sku'), (string)$entity->getSku());
+            $this->processOptionsSaving($options, $entity->dataHasChangedFor('sku'), $entity->getSku());
         }
 
         return $entity;
