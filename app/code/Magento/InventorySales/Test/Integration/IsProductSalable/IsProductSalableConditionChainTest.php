fd03b4bdc0e91a0cd6a0c4df06b6140d55c710b0
Maksym Novik <m.novik@ism-ukraine.com>
2019-04-13 15:26:31 +0300

    Product status is 'In Stock' on storefront even it is Out of Stock in all Sources #2156. Added integration test for case when there are backorders along with out of stock items

---
+++ app/code/Magento/InventorySales/Test/Integration/IsProductSalable/IsProductSalableConditionChainTest.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ *  Copyright © Magento, Inc. All rights reserved.
+ *  See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\InventorySales\Test\Integration\IsProductSalable;
+
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\CatalogInventory\Api\Data\StockItemInterface;
+use Magento\CatalogInventory\Api\StockItemCriteriaInterfaceFactory;
+use Magento\CatalogInventory\Api\StockItemRepositoryInterface;
+use Magento\InventorySales\Model\IsProductSalableCondition\IsProductSalableConditionChain;
+use Magento\TestFramework\Helper\Bootstrap;
+use PHPUnit\Framework\TestCase;
+
+class IsProductSalableConditionChainTest extends TestCase
+{
+    /**
+     * @var IsProductSalableConditionChain
+     */
+    private $isProductSalableConditionChain;
+
+    /**
+     * @var ProductRepositoryInterface
+     */
+    private $productRepository;
+
+    /**
+     * @var StockItemCriteriaInterfaceFactory
+     */
+    private $stockItemCriteriaFactory;
+
+    /**
+     * @var StockItemRepositoryInterface
+     */
+    private $stockItemRepository;
+
+    protected function setUp()
+    {
+        $objectManager = Bootstrap::getObjectManager();
+        $this->isProductSalableConditionChain = $objectManager->get(
+            IsProductSalableConditionChain::class
+        );
+        $this->productRepository = $objectManager->get(ProductRepositoryInterface::class);
+        $this->stockItemCriteriaFactory = $objectManager->get(StockItemCriteriaInterfaceFactory::class);
+        $this->stockItemRepository = $objectManager->get(StockItemRepositoryInterface::class);
+    }
+
+    /**
+     * @magentoDataFixture ../../../../app/code/Magento/InventoryApi/Test/_files/products.php
+     * @magentoDataFixture ../../../../app/code/Magento/InventoryApi/Test/_files/sources.php
+     * @magentoDataFixture ../../../../app/code/Magento/InventoryApi/Test/_files/stocks.php
+     * @magentoDataFixture ../../../../app/code/Magento/InventoryApi/Test/_files/stock_source_links.php
+     * @magentoDataFixture ../../../../app/code/Magento/InventoryApi/Test/_files/source_items_out_of_stock.php
+     *
+     * @magentoDbIsolation disabled
+     */
+    public function testSourcesItemsAreOutOfStock()
+    {
+        $product = $this->productRepository->get('SKU-1');
+        $stockItemSearchCriteria = $this->stockItemCriteriaFactory->create();
+        $stockItemSearchCriteria->setProductsFilter($product->getId());
+        $stockItemsCollection = $this->stockItemRepository->getList($stockItemSearchCriteria);
+
+        /** @var StockItemInterface $legacyStockItem */
+        $legacyStockItem = current($stockItemsCollection->getItems());
+        $legacyStockItem->setBackorders(1);
+        $legacyStockItem->setUseConfigBackorders(0);
+        $this->stockItemRepository->save($legacyStockItem);
+        $this->assertFalse($this->isProductSalableConditionChain->execute('SKU-1', 10));
+    }
+}
