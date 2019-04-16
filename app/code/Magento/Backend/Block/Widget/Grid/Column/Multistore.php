3a9d783341929861946feb84c3b03fc3496389dc
Aliaksei Yakimovich2 <aliaksei_yakimovich2@epam.com>
2019-04-16 14:41:29 +0300

    MAGETWO-88965: Magento\Test\Legacy\LayoutTest is failing for B2B
    - Fixed static test;

--- app/code/Magento/Backend/Block/Widget/Grid/Column/Multistore.php
+++ app/code/Magento/Backend/Block/Widget/Grid/Column/Multistore.php
@@ -17,15 +17,6 @@ namespace Magento\Backend\Block\Widget\Grid\Column;
 class Multistore extends \Magento\Backend\Block\Widget\Grid\Column
 {
     /**
-     * @param \Magento\Backend\Block\Template\Context $context
-     * @param array $data
-     */
-    public function __construct(\Magento\Backend\Block\Template\Context $context, array $data = [])
-    {
-        parent::__construct($context, $data);
-    }
-
-    /**
      * Get header css class name
      *
      * @return string
