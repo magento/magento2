e995c1ba60c6eef45c3523fb0ca14d0bf57ef2f0
Prabhu Ram <pganapat@adobe.com>
2019-04-16 12:50:51 -0500

    596: Category with Products with...(deep nesting) (Integration Test for Tag Cache Generation) - Added integration test

--- dev/tests/integration/testsuite/Magento/GraphQl/Controller/GraphQlCacheControllerTest.php
+++ dev/tests/integration/testsuite/Magento/GraphQl/Controller/GraphQlCacheControllerTest.php
@@ -13,6 +13,8 @@ use Magento\Framework\App\Request\Http;
 use Magento\Framework\EntityManager\MetadataPool;
 use Magento\Framework\Serialize\SerializerInterface;
 use Magento\TestFramework\Helper\Bootstrap;
+use Magento\TestFramework\ObjectManager;
+use Magento\Catalog\Api\CategoryRepositoryInterface;
 
 /**
  * Tests cache debug headers and cache tag validation for a simple product query
@@ -44,7 +46,6 @@ class GraphQlCacheControllerTest extends \Magento\TestFramework\Indexer\TestCase
 
     /** @var \Magento\Framework\App\Response\Http */
     private $response;
-
 
     /**
      * @inheritdoc
@@ -159,8 +160,100 @@ QUERY;
         $actualCacheTags = explode(',', $response->getHeader('X-Magento-Tags')->getFieldValue());
         $expectedCacheTags = ['cat_c','cat_c_' . $categoryId,'FPC'];
         foreach (array_keys($actualCacheTags) as $key) {
-            $this->assertEquals($expectedCacheTags[$key], $actualCacheTags[$key]
+            $this->assertEquals($expectedCacheTags[$key], $actualCacheTags[$key]);
+        }
+    }
+
+    /**
+     * Test cache tags and debug header for deep nested queries involving category and products
+     *
+     * @magentoCache all enabled
+     * @magentoDataFixture Magento/Catalog/_files/product_in_multiple_categories.php
+     *
+     */
+    public function testDispatchForCacheHeadersOnDeepNestedQueries(): void
+    {
+        $categoryId ='333';
+        $query
+            = <<<QUERY
+        {
+  category(id: $categoryId) {
+    products {
+      items {
+        attribute_set_id
+        country_of_manufacture
+        created_at
+        description {
+            html
+        }
+        gift_message_available
+        id
+        categories {
+          name
+          url_path
+          available_sort_by
+          level
+          products {
+            items {
+              name
+              id
+            }
+          }
+        }
+              }
+    }
+  }
+}
+QUERY;
+        /** @var CategoryRepositoryInterface $categoryRepository */
+        $categoryRepository = ObjectManager::getInstance()->get(CategoryRepositoryInterface::class);
+        /** @var ProductRepositoryInterface $productRepository */
+        $productRepository = ObjectManager::getInstance()->get(ProductRepositoryInterface::class);
+        $categoryIds = [];
+        $category = $categoryRepository->get('333');
+
+        $productIdsFromCategory = $category->getProductCollection()->getAllIds();
+        foreach ($productIdsFromCategory as $productId) {
+            $categoryIds = array_merge($categoryIds, $productRepository->getById($productId)->getCategoryIds());
+        }
+
+        $categoryIds = array_merge($categoryIds, ['333']);
+        foreach ($categoryIds as $categoryId) {
+            $category = $categoryRepository->get($categoryId);
+            $productIdsFromCategory= array_merge(
+                $productIdsFromCategory,
+                $category->getProductCollection()->getAllIds()
             );
         }
+
+        $uniqueProductIds = array_unique($productIdsFromCategory);
+        $uniqueCategoryIds = array_unique($categoryIds);
+        $expectedCacheTags = ['cat_c', 'cat_p', 'FPC'];
+        foreach ($uniqueProductIds as $productId) {
+            $expectedCacheTags = array_merge($expectedCacheTags, ['cat_p_'.$productId]);
+        }
+        foreach ($uniqueCategoryIds as $categoryId) {
+            $expectedCacheTags = array_merge($expectedCacheTags, ['cat_c_'.$categoryId]);
+        }
+
+        $this->request->setPathInfo('/graphql');
+        $this->request->setMethod('GET');
+        $this->request->setQueryValue('query', $query);
+        /** @var \Magento\Framework\Controller\Result\Json $result */
+        $result = $this->graphql->dispatch($this->request);
+        /** @var \Magento\Framework\App\Response\Http $response */
+        $response = $this->objectManager->get(\Magento\Framework\App\Response\Http::class);
+        /** @var  $registry \Magento\Framework\Registry */
+        $registry = $this->objectManager->get(\Magento\Framework\Registry::class);
+        $registry->register('use_page_cache_plugin', true, true);
+        $result->renderResult($response);
+        $this->assertEquals('MISS', $response->getHeader('X-Magento-Cache-Debug')->getFieldValue());
+        $actualCacheTags = explode(',', $response->getHeader('X-Magento-Tags')->getFieldValue());
+        $this->assertEmpty(
+            array_merge(
+                array_diff($expectedCacheTags, $actualCacheTags),
+                array_diff($actualCacheTags, $expectedCacheTags)
+            )
+        );
     }
 }
