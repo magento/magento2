786a20a5eccb3ec4a207a8d08b1d7fd66b739690
Viktor Sevch <svitja@ukr.net>
2019-04-22 09:57:46 +0300

    MAGETWO-97672: Invalid action behavior

--- app/code/Magento/Email/Test/Unit/Controller/Adminhtml/Email/Template/PreviewTest.php
+++ app/code/Magento/Email/Test/Unit/Controller/Adminhtml/Email/Template/PreviewTest.php
@@ -16,9 +16,7 @@ use Magento\Framework\View\Page\Title;
 use Magento\Framework\View\Result\Page;
 
 /**
- * Preview Test
- *
- * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+ * Preview Test.
  */
 class PreviewTest extends \PHPUnit\Framework\TestCase
 {
@@ -129,7 +127,7 @@ class PreviewTest extends \PHPUnit\Framework\TestCase
             ->willReturnSelf();
         $this->responseMock->expects($this->once())
             ->method('setHeader')
-            ->with('Content-Security-Policy', "script-src 'none'");
+            ->with('Content-Security-Policy', "script-src 'self'");
 
         $this->assertNull($this->object->execute());
     }
