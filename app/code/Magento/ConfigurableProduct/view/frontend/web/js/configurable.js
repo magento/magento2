magento/magento2

commit ce8c46f990aefae2bff126f2be0d7473888fe88e

magento/magento2#17695 ConfigurableProduct show prices in select options
Fix js static test failure

app/code/Magento/ConfigurableProduct/view/frontend/web/js/configurable.js
@@ -393,8 +393,8 @@ define([
                     } else {
                         allowedProducts = options[i].products.slice(0);
 
-                        if (typeof allowedProducts[0] !== 'undefined'
-                            && typeof optionPrices[allowedProducts[0]] !== 'undefined') {
+                        if (typeof allowedProducts[0] !== 'undefined' &&
+                            typeof optionPrices[allowedProducts[0]] !== 'undefined') {
 
                             optionFinalPrice = parseFloat(optionPrices[allowedProducts[0]].finalPrice.amount);
                             optionPriceDiff = optionFinalPrice - basePrice;

commit d8d8fed62ea570359c6cc60227b338dbb79d00a2

Use final price rather than base price to calculate price increases / decreases on the configurable attribute dropdown. This fixes a bug introduced in PR #17695 as detailed on issue #22270

app/code/Magento/ConfigurableProduct/view/frontend/web/js/configurable.js
@@ -373,7 +373,7 @@ define([
                 allowedProducts,
                 i,
                 j,
-                basePrice = parseFloat(this.options.spConfig.prices.basePrice.amount),
+                finalPrice = parseFloat(this.options.spConfig.prices.finalPrice.amount),
                 optionFinalPrice,
                 optionPriceDiff,
                 optionPrices = this.options.spConfig.optionPrices,
@@ -410,7 +410,7 @@ define([
                             typeof optionPrices[allowedProducts[0]] !== 'undefined') {
                             allowedProductMinPrice = this._getAllowedProductWithMinPrice(allowedProducts);
                             optionFinalPrice = parseFloat(optionPrices[allowedProductMinPrice].finalPrice.amount);
-                            optionPriceDiff = optionFinalPrice - basePrice;
+                            optionPriceDiff = optionFinalPrice - finalPrice;
 
                             if (optionPriceDiff !== 0) {
                                 options[i].label = options[i].label + ' ' + priceUtils.formatPrice(