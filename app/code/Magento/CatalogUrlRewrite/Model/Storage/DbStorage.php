7de050e7334b458320d27146814ea8a0f9fabc29
Andrii Lugovyi <alugovyi@adobe.com>
2019-04-24 16:07:18 -0500

    MC-4244: Skip URL rewrites multiplication

--- app/code/Magento/CatalogUrlRewrite/Model/Storage/DbStorage.php
+++ app/code/Magento/CatalogUrlRewrite/Model/Storage/DbStorage.php
@@ -7,57 +7,15 @@ declare(strict_types=1);
 
 namespace Magento\CatalogUrlRewrite\Model\Storage;
 
-use Magento\Catalog\Model\ResourceModel\ProductFactory;
-use Magento\Framework\Api\DataObjectHelper;
-use Magento\Framework\App\Config\ScopeConfigInterface;
-use Magento\Framework\App\ResourceConnection;
 use Magento\CatalogUrlRewrite\Model\ResourceModel\Category\Product;
-use Magento\UrlRewrite\Model\OptionProvider;
 use Magento\UrlRewrite\Model\Storage\DbStorage as BaseDbStorage;
 use Magento\UrlRewrite\Service\V1\Data\UrlRewrite;
-use Magento\UrlRewrite\Service\V1\Data\UrlRewriteFactory;
-use Psr\Log\LoggerInterface;
 
 /**
  * Class DbStorage
  */
 class DbStorage extends BaseDbStorage
 {
-    /**
-     * @var ScopeConfigInterface
-     */
-    private $config;
-
-    /**
-     * @var ProductFactory
-     */
-    private $productFactory;
-
-    /**
-     * DbStorage constructor.
-     *
-     * @param UrlRewriteFactory $urlRewriteFactory
-     * @param DataObjectHelper $dataObjectHelper
-     * @param ResourceConnection $resource
-     * @param LoggerInterface|null $logger
-     * @param ScopeConfigInterface|null $config
-     * @param ProductFactory|null $productFactory
-     */
-    public function __construct(
-        UrlRewriteFactory $urlRewriteFactory,
-        DataObjectHelper $dataObjectHelper,
-        ResourceConnection $resource,
-        LoggerInterface $logger = null,
-        ScopeConfigInterface $config = null,
-        ProductFactory $productFactory = null
-    ) {
-        parent::__construct($urlRewriteFactory, $dataObjectHelper, $resource, $logger);
-        $this->config = $config ?? \Magento\Framework\App\ObjectManager::getInstance()
-            ->get(ScopeConfigInterface::class);
-        $this->productFactory = $productFactory ?? \Magento\Framework\App\ObjectManager::getInstance()
-            ->get(ProductFactory::class);
-    }
-
     /**
      * @inheritDoc
      */
@@ -68,6 +26,7 @@ class DbStorage extends BaseDbStorage
             $metadata = $data[UrlRewrite::METADATA];
             unset($data[UrlRewrite::METADATA]);
         }
+
         $select = $this->connection->select();
         $select->from([
             'url_rewrite' => $this->resource->getTableName(self::TABLE_NAME)
@@ -76,6 +35,7 @@ class DbStorage extends BaseDbStorage
             ['relation' => $this->resource->getTableName(Product::TABLE_NAME)],
             'url_rewrite.url_rewrite_id = relation.url_rewrite_id'
         );
+
         foreach ($data as $column => $value) {
             $select->where('url_rewrite.' . $column . ' IN (?)', $value);
         }
@@ -87,162 +47,7 @@ class DbStorage extends BaseDbStorage
                 $metadata['category_id']
             );
         }
+
         return $select;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    protected function doFindOneByData(array $data)
-    {
-        if (isset($data[UrlRewrite::REQUEST_PATH])
-            && isset($data[UrlRewrite::STORE_ID])
-            && is_string($data[UrlRewrite::REQUEST_PATH])
-        ) {
-            return $this->findProductRewriteByRequestPath($data);
-        }
-
-        $filterResults = $this->findProductRewritesByFilter($data);
-        if (!empty($filterResults)) {
-            return reset($filterResults);
-        } else {
-            return null;
-        }
-    }
-
-    /**
-     * @inheritdoc
-     */
-    protected function doFindAllByData(array $data)
-    {
-        $rewrites = parent::doFindAllByData($data);
-
-        $remainingProducts = [];
-        if (isset($data[UrlRewrite::ENTITY_ID]) && is_array($data[UrlRewrite::ENTITY_ID])) {
-            $remainingProducts = array_fill_keys($data[UrlRewrite::ENTITY_ID], 1);
-            foreach ($rewrites as $rewrite) {
-                $id = $rewrite[UrlRewrite::ENTITY_ID];
-                if (isset($remainingProducts[$id])) {
-                    unset($remainingProducts[$id]);
-                }
-            }
-        }
-
-        if (!empty($remainingProducts)) {
-            $data[UrlRewrite::ENTITY_ID] = array_keys($remainingProducts);
-            $rewrites = array_merge($rewrites, $this->findProductRewritesByFilter($data));
-        }
-
-        return $rewrites;
-    }
-
-    /**
-     * Get category urlSuffix from config
-     *
-     * @param int $storeId
-     * @return string
-     */
-    private function getCategoryUrlSuffix($storeId = null)
-    {
-        return $this->config->getValue(
-            \Magento\CatalogUrlRewrite\Model\CategoryUrlPathGenerator::XML_PATH_CATEGORY_URL_SUFFIX,
-            \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
-            $storeId
-        );
-    }
-
-    /**
-     * Find product rewrite by request data
-     *
-     * @param array $data
-     * @return array|null
-     */
-    private function findProductRewriteByRequestPath(array $data)
-    {
-        $requestPath = $data[UrlRewrite::REQUEST_PATH] ?? null;
-
-        $productUrl = basename($requestPath);
-        $data[UrlRewrite::REQUEST_PATH] = [$productUrl];
-
-        $productFromDb = $this->connection->fetchRow($this->prepareSelect($data));
-        if ($productFromDb === false) {
-            return null;
-        }
-        $categorySuffix = $this->getCategoryUrlSuffix($data[UrlRewrite::STORE_ID]);
-        $productResource = $this->productFactory->create();
-        $categoryPath = str_replace('/' . $productUrl, '', $requestPath);
-        if ($productFromDb[UrlRewrite::REDIRECT_TYPE]) {
-            $productUrl = $productFromDb[UrlRewrite::TARGET_PATH];
-        }
-        if ($categoryPath) {
-            $data[UrlRewrite::REQUEST_PATH] = [$categoryPath . $categorySuffix];
-            unset($data[UrlRewrite::IS_AUTOGENERATED]);
-            $categoryFromDb = $this->connection->fetchRow($this->prepareSelect($data));
-
-            if ($categoryFromDb[UrlRewrite::REDIRECT_TYPE]) {
-                $productFromDb[UrlRewrite::REDIRECT_TYPE] = OptionProvider::PERMANENT;
-                $categoryPath = str_replace($categorySuffix, '', $categoryFromDb[UrlRewrite::TARGET_PATH]);
-            }
-
-            if ($categoryFromDb === false
-                || !$productResource->canBeShowInCategory(
-                    $productFromDb[UrlRewrite::ENTITY_ID],
-                    $categoryFromDb[UrlRewrite::ENTITY_ID]
-                )
-            ) {
-                return null;
-            }
-
-            $productFromDb[UrlRewrite::TARGET_PATH] = $productFromDb[UrlRewrite::TARGET_PATH]
-                . '/category/' . $categoryFromDb[UrlRewrite::ENTITY_ID];
-        }
-
-        if ($productFromDb[UrlRewrite::REDIRECT_TYPE]) {
-            $productFromDb[UrlRewrite::TARGET_PATH] = $categoryPath . '/' . $productUrl;
-        }
-
-        $productFromDb[UrlRewrite::REQUEST_PATH] = $requestPath;
-
-        return $productFromDb;
-    }
-
-    /**
-     * Find product rewrites by filter array
-     *
-     * @param array $data
-     * @return array
-     */
-    private function findProductRewritesByFilter(array $data)
-    {
-        if (empty($data[UrlRewrite::ENTITY_TYPE]) || $data[UrlRewrite::ENTITY_TYPE] != 'product') {
-            return [];
-        }
-        $rewrites = [];
-        $metadata = $data[UrlRewrite::METADATA] ?? [];
-        if (isset($data[UrlRewrite::METADATA])) {
-            unset($data[UrlRewrite::METADATA]);
-        }
-        $productsFromDb = $this->connection->fetchAll($this->prepareSelect($data));
-
-        if (!empty($metadata['category_id'])) {
-            $categoryId = $metadata['category_id'];
-            $data[UrlRewrite::ENTITY_ID] = $categoryId;
-            $data[UrlRewrite::ENTITY_TYPE] = 'category';
-            $categoryFromDb = $this->connection->fetchRow($this->prepareSelect($data));
-            foreach ($productsFromDb as $productFromDb) {
-                $productUrl = basename($productFromDb[UrlRewrite::REQUEST_PATH]);
-                $productFromDb[UrlRewrite::REQUEST_PATH] = str_replace(
-                    $this->getCategoryUrlSuffix($data[UrlRewrite::STORE_ID]),
-                    '',
-                    $categoryFromDb[UrlRewrite::REQUEST_PATH]
-                )
-                    . '/' . $productUrl;
-                $rewrites[] = $productFromDb;
-            }
-        } else {
-            $rewrites = $productsFromDb;
-        }
-
-        return $rewrites;
     }
 }
---
+++ app/code/Magento/CatalogUrlRewrite/Plugin/DynamicCategoryRewrites.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\CatalogUrlRewrite\Plugin;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Api\StoreResolverInterface;
+use Magento\Store\Model\ScopeInterface;
+use Magento\UrlRewrite\Service\V1\Data\UrlRewrite;
+use Magento\CatalogUrlRewrite\Model\Storage\DynamicStorage;
+use Magento\CatalogUrlRewrite\Model\Storage\DbStorage;
+
+/**
+ * Class DbStorage
+ */
+class DynamicCategoryRewrites
+{
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $config;
+
+    /**
+     * @var DynamicStorage
+     */
+    private $dynamicStorage;
+
+    /**
+     * @var StoreResolverInterface
+     */
+    private $storeResolver;
+
+    /**
+     * @param ScopeConfigInterface|null $config
+     * @param StoreResolverInterface $storeResolver
+     * @param DynamicStorage $dynamicStorage
+     */
+    public function __construct(
+        ScopeConfigInterface $config,
+        StoreResolverInterface $storeResolver,
+        DynamicStorage $dynamicStorage
+    ) {
+        $this->config = $config;
+        $this->dynamicStorage = $dynamicStorage;
+        $this->storeResolver = $storeResolver;
+    }
+
+    /**
+     * Check config value of generate_rewrites_on_save
+     *
+     * @param int $storeId
+     * @return bool
+     */
+    private function isCategoryRewritesEnabled($storeId)
+    {
+        return (bool)$this->config->getValue(
+            'catalog/seo/generate_rewrites_on_save',
+            ScopeInterface::SCOPE_STORE,
+            $storeId
+        );
+    }
+
+    /**
+     * Execute proxy
+     *
+     * @param callable $proceed
+     * @param array $data
+     * @param string $functionName
+     * @return mixed
+     */
+    private function proxy(callable $proceed, array $data, string $functionName)
+    {
+        $store = isset($data[UrlRewrite::STORE_ID])
+            ? $data[UrlRewrite::STORE_ID]
+            : $this->storeResolver->getCurrentStoreId();
+        if ($this->isCategoryRewritesEnabled((int)$store)) {
+            return $proceed($data);
+        }
+
+        return $this->dynamicStorage->$functionName($data);
+    }
+
+    /**
+     * Find rewrite by specific data
+     *
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     * @param DbStorage $subject
+     * @param callable $proceed
+     * @param array $data
+     * @return UrlRewrite|null
+     */
+    public function aroundFindOneByData(DbStorage $subject, callable $proceed, array $data)
+    {
+        return $this->proxy($proceed, $data, 'findOneByData');
+    }
+
+    /**
+     * Find rewrites by specific data
+     *
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     * @param DbStorage $subject
+     * @param callable $proceed
+     * @param array $data
+     * @return UrlRewrite[]
+     */
+    public function aroundFindAllByData(DbStorage $subject, callable $proceed, array $data)
+    {
+        return $this->proxy($proceed, $data, 'findAllByData');
+    }
+}
