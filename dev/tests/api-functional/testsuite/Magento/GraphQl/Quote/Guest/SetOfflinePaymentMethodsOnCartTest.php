05e7ec2a8a82c6f5751c5fc3f10f57495825dc87
Patrick McLain <pat@pmclain.com>
2019-04-24 20:59:18 -0400

    Test for offline payment methods
    Fixes #601

---
+++ dev/tests/api-functional/testsuite/Magento/GraphQl/Quote/Guest/SetOfflinePaymentMethodsOnCartTest.php
@@ -0,0 +1,147 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\GraphQl\Quote\Guest;
+
+use Magento\GraphQl\Quote\GetMaskedQuoteIdByReservedOrderId;
+use Magento\OfflinePayments\Model\Banktransfer;
+use Magento\OfflinePayments\Model\Cashondelivery;
+use Magento\OfflinePayments\Model\Checkmo;
+use Magento\OfflinePayments\Model\Purchaseorder;
+use Magento\TestFramework\Helper\Bootstrap;
+use Magento\TestFramework\TestCase\GraphQlAbstract;
+
+/**
+ * Test for setting offline payment methods on cart
+ */
+class SetOfflinePaymentMethodsOnCartTest extends GraphQlAbstract
+{
+    /**
+     * @var GetMaskedQuoteIdByReservedOrderId
+     */
+    private $getMaskedQuoteIdByReservedOrderId;
+
+    /**
+     * @inheritdoc
+     */
+    protected function setUp()
+    {
+        $objectManager = Bootstrap::getObjectManager();
+        $this->getMaskedQuoteIdByReservedOrderId = $objectManager->get(GetMaskedQuoteIdByReservedOrderId::class);
+    }
+
+    /**
+     * @magentoApiDataFixture Magento/GraphQl/Catalog/_files/simple_product.php
+     * @magentoApiDataFixture Magento/GraphQl/Quote/_files/guest/create_empty_cart.php
+     * @magentoApiDataFixture Magento/GraphQl/Quote/_files/add_simple_product.php
+     * @magentoApiDataFixture Magento/GraphQl/Quote/_files/set_new_shipping_address.php
+     * @magentoApiDataFixture Magento/GraphQl/Quote/_files/enable_offline_payment_methods.php
+     *
+     * @param string $methodCode
+     * @dataProvider offlinePaymentMethodDataProvider
+     */
+    public function testSetOfflinePaymentMethod(string $methodCode)
+    {
+        $maskedQuoteId = $this->getMaskedQuoteIdByReservedOrderId->execute('test_quote');
+
+        $query = $this->getQuery(
+            $maskedQuoteId,
+            $methodCode
+        );
+        $response = $this->graphQlMutation($query);
+
+        self::assertArrayHasKey('setPaymentMethodOnCart', $response);
+        self::assertArrayHasKey('cart', $response['setPaymentMethodOnCart']);
+        self::assertArrayHasKey('selected_payment_method', $response['setPaymentMethodOnCart']['cart']);
+        self::assertArrayHasKey('code', $response['setPaymentMethodOnCart']['cart']['selected_payment_method']);
+        self::assertEquals($methodCode, $response['setPaymentMethodOnCart']['cart']['selected_payment_method']['code']);
+    }
+
+    /**
+     * @magentoApiDataFixture Magento/GraphQl/Catalog/_files/simple_product.php
+     * @magentoApiDataFixture Magento/GraphQl/Quote/_files/guest/create_empty_cart.php
+     * @magentoApiDataFixture Magento/GraphQl/Quote/_files/add_simple_product.php
+     * @magentoApiDataFixture Magento/GraphQl/Quote/_files/set_new_shipping_address.php
+     * @magentoApiDataFixture Magento/GraphQl/Quote/_files/enable_offline_payment_methods.php
+     *
+     * @param string $methodCode
+     */
+    public function testSetPurchaseOrderPaymentMethod()
+    {
+        $maskedQuoteId = $this->getMaskedQuoteIdByReservedOrderId->execute('test_quote');
+        $methodCode = Purchaseorder::PAYMENT_METHOD_PURCHASEORDER_CODE;
+        $poNumber = 'abc123';
+
+        $query = <<<QUERY
+mutation {
+  setPaymentMethodOnCart(input: {
+    cart_id: "{$maskedQuoteId}", 
+    payment_method: {
+      code: "{$methodCode}"
+      purchase_order_number: "{$poNumber}"
+    }
+  }) {
+    cart {
+      selected_payment_method {
+        code
+        purchase_order_number
+      }
+    }
+  }
+}
+QUERY;
+
+        $response = $this->graphQlMutation($query);
+
+        self::assertArrayHasKey('setPaymentMethodOnCart', $response);
+        self::assertArrayHasKey('cart', $response['setPaymentMethodOnCart']);
+        self::assertArrayHasKey('selected_payment_method', $response['setPaymentMethodOnCart']['cart']);
+        self::assertArrayHasKey('code', $response['setPaymentMethodOnCart']['cart']['selected_payment_method']);
+        self::assertArrayHasKey('purchase_order_number', $response['setPaymentMethodOnCart']['cart']['selected_payment_method']);
+        self::assertEquals($methodCode, $response['setPaymentMethodOnCart']['cart']['selected_payment_method']['code']);
+        self::assertEquals($poNumber, $response['setPaymentMethodOnCart']['cart']['selected_payment_method']['purchase_order_number']);
+    }
+
+    /**
+     * @return array
+     */
+    public function offlinePaymentMethodDataProvider(): array
+    {
+        return [
+            'check_mo' => [Checkmo::PAYMENT_METHOD_CHECKMO_CODE],
+            'bank_transfer' => [Banktransfer::PAYMENT_METHOD_BANKTRANSFER_CODE],
+            'cash_on_delivery' => [Cashondelivery::PAYMENT_METHOD_CASHONDELIVERY_CODE],
+        ];
+    }
+
+    /**
+     * @param string $maskedQuoteId
+     * @param string $methodCode
+     * @return string
+     */
+    private function getQuery(
+        string $maskedQuoteId,
+        string $methodCode
+    ) : string {
+        return <<<QUERY
+mutation {
+  setPaymentMethodOnCart(input: {
+    cart_id: "{$maskedQuoteId}", 
+    payment_method: {
+      code: "{$methodCode}"
+    }
+  }) {
+    cart {
+      selected_payment_method {
+        code
+      }
+    }
+  }
+}
+QUERY;
+    }
+}
