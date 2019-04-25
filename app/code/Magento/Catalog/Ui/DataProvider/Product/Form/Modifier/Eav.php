25e7ae6d50d6fe4acbb7637361cda7842c9af820
Oleksandr Gorkun <ogorkun@magento.com>
2019-04-25 12:43:29 -0500

    MC-15901: Recursion in filter widget

--- app/code/Magento/Catalog/Ui/DataProvider/Product/Form/Modifier/Eav.php
+++ app/code/Magento/Catalog/Ui/DataProvider/Product/Form/Modifier/Eav.php
@@ -671,6 +671,7 @@ class Eav extends AbstractModifier
             'scopeLabel' => $this->getScopeLabel($attribute),
             'globalScope' => $this->isScopeGlobal($attribute),
             'sortOrder' => $sortOrder * self::SORT_ORDER_MULTIPLIER,
+            '__disableTmpl' => ['label' => true, 'code' => true]
         ]);
 
         // TODO: Refactor to $attribute->getOptions() when MAGETWO-48289 is done
@@ -816,6 +817,7 @@ class Eav extends AbstractModifier
                 'breakLine' => false,
                 'label' => $attribute->getDefaultFrontendLabel(),
                 'required' => $attribute->getIsRequired(),
+                '__disableTmpl' => ['label' => true]
             ]
         );
 
