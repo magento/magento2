443f31784c2e9ca769d259ee2e4397e6b5a53039
Anthoula Wojczak <awojczak@adobe.com>
2019-04-26 14:44:10 -0500

    MAGETWO-53347: Char flag update
    - update existing tests

--- app/code/Magento/Ui/Test/Unit/Component/Control/ButtonTest.php
+++ app/code/Magento/Ui/Test/Unit/Component/Control/ButtonTest.php
@@ -55,7 +55,7 @@ class ButtonTest extends \PHPUnit\Framework\TestCase
     public function testGetAttributesHtml()
     {
         $expected = 'type="button" class="action- scalable classValue disabled" '
-            . 'onclick="location.href = \'url2\';" disabled="disabled" data-attributeKey="attributeValue" ';
+            . 'onclick="location.href = &#039;url2&#039;;" disabled="disabled" data-attributeKey="attributeValue" ';
         $this->button->setDisabled(true);
         $this->button->setData('url', 'url2');
         $this->button->setData('class', 'classValue');
