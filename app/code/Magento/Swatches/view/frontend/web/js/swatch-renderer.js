73ef11915ffa0ed8f8d38b785d59d3ad8ace9218
Pavel Bystritsky <p.bystritsky@yandex.ru>
2019-04-30 13:12:43 +0300

    magento/magento2#19184: Static test fix.

--- app/code/Magento/Swatches/view/frontend/web/js/swatch-renderer.js
+++ app/code/Magento/Swatches/view/frontend/web/js/swatch-renderer.js
@@ -713,7 +713,8 @@ define([
                 $wrapper = $this.parents('.' + $widget.options.classes.attributeOptionsWrapper),
                 $label = $parent.find('.' + $widget.options.classes.attributeSelectedOptionLabelClass),
                 attributeId = $parent.attr('attribute-id'),
-                $input = $parent.find('.' + $widget.options.classes.attributeInput);
+                $input = $parent.find('.' + $widget.options.classes.attributeInput),
+                checkAdditionalData = JSON.parse(this.options.jsonSwatchConfig[attributeId]['additional_data']);
 
             if ($widget.inProductList) {
                 $input = $widget.productForm.find(
@@ -753,9 +754,7 @@ define([
                     $widget.options.jsonConfig.optionPrices
                 ]);
 
-            var checkAdditionalData = JSON.parse(this.options.jsonSwatchConfig[attributeId]['additional_data']);
-
-            if (1 == checkAdditionalData['update_product_preview_image']) {
+            if (checkAdditionalData['update_product_preview_image'] === '1') {
                 $widget._loadMedia();
             }
 
