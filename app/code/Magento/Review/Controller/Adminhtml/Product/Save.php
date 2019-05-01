62fd9d2703717a273904384e05f48bfecaf78b24
Ravi Chandra <ravi.chandra@krishtechnolabs.com>
2019-05-01 09:49:12 +0530

    Fixed wrong url redirect when edit product review from Customer view page and product view page

--- app/code/Magento/Review/Controller/Adminhtml/Product/Save.php
+++ app/code/Magento/Review/Controller/Adminhtml/Product/Save.php
@@ -72,6 +72,14 @@ class Save extends ProductController
             } else {
                 $resultRedirect->setPath('*/*/');
             }
+            $productId = $this->getRequest()->getParam('productId');
+            if ($productId) {
+                $resultRedirect->setPath("catalog/product/edit/id/$productId");
+            }
+            $customerId = (int)$this->getRequest()->getParam('customerId');
+            if ($customerId) {
+                $resultRedirect->setPath("customer/index/edit/id/$customerId");
+            }
             return $resultRedirect;
         }
         $resultRedirect->setPath('review/*/');
