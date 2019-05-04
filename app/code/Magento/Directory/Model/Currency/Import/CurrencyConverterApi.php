fc8debc2d39880773a78a9c9d08a824a0e405b54
Jay <jayakanthr@dckap.com>
2019-05-04 15:28:47 +0530

    Handling error messages

--- app/code/Magento/Directory/Model/Currency/Import/CurrencyConverterApi.php
+++ app/code/Magento/Directory/Model/Currency/Import/CurrencyConverterApi.php
@@ -86,6 +86,8 @@ class CurrencyConverterApi extends AbstractImport
                     if (empty($response)) {
                         $this->_messages[] = __('We can\'t retrieve a rate from %1 for %2.', $url, $to);
                         $data[$currencyFrom][$to] = null;
+                    } elseif (array_key_exists('error', $response) && $response['error']) {
+                        $this->_messages[] = $response['error'];
                     } else {
                         $data[$currencyFrom][$to] = $this->_numberFormat(
                             (double)$response[$currencyFrom . '_' . $to]
