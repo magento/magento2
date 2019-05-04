fbcfd11559def0891e829746a75ff5df2db438ee
Satya Prakash <p.satyaprakash.viet.2009@gmail.com>
2019-05-04 16:41:52 +0530

    Fixed:#22395
    Fixed  #22395: config:set -le and -lc short form options don't work

--- app/code/Magento/Config/Console/Command/ConfigSetCommand.php
+++ app/code/Magento/Config/Console/Command/ConfigSetCommand.php
@@ -114,13 +114,13 @@ class ConfigSetCommand extends Command
                 ),
                 new InputOption(
                     static::OPTION_LOCK_ENV,
-                    'le',
+                    'e',
                     InputOption::VALUE_NONE,
                     'Lock value which prevents modification in the Admin (will be saved in app/etc/env.php)'
                 ),
                 new InputOption(
                     static::OPTION_LOCK_CONFIG,
-                    'lc',
+                    'c',
                     InputOption::VALUE_NONE,
                     'Lock and share value with other installations, prevents modification in the Admin '
                     . '(will be saved in app/etc/config.php)'
