aa23860090672a9b844bc1eb5f8ad9a693303a26
Alex Taranovsky <firster@atwix.com>
2019-05-06 23:34:33 +0300

    magento/graphql-ce#675: [Test coverage] Generate customer token

--- dev/tests/api-functional/testsuite/Magento/GraphQl/Customer/GenerateCustomerTokenTest.php
+++ dev/tests/api-functional/testsuite/Magento/GraphQl/Customer/GenerateCustomerTokenTest.php
@@ -8,7 +8,6 @@ declare(strict_types=1);
 namespace Magento\GraphQl\Customer;
 
 use Magento\TestFramework\TestCase\GraphQlAbstract;
-use PHPUnit\Framework\TestResult;
 
 /**
  * Class GenerateCustomerTokenTest
@@ -23,14 +22,14 @@ class GenerateCustomerTokenTest extends GraphQlAbstract
      */
     public function testGenerateCustomerValidToken()
     {
-        $userName = 'customer@example.com';
+        $email = 'customer@example.com';
         $password = 'password';
 
         $mutation
             = <<<MUTATION
 mutation {
 	generateCustomerToken(
-        email: "{$userName}"
+        email: "{$email}"
         password: "{$password}"
     ) {
         token
@@ -48,14 +47,14 @@ MUTATION;
      */
     public function testGenerateCustomerTokenWithInvalidCredentials()
     {
-        $userName = 'customer@example.com';
+        $email = 'customer@example.com';
         $password = 'bad-password';
 
         $mutation
             = <<<MUTATION
 mutation {
 	generateCustomerToken(
-        email: "{$userName}"
+        email: "{$email}"
         password: "{$password}"
     ) {
         token
@@ -66,6 +65,28 @@ MUTATION;
         $this->expectException(\Exception::class);
         $this->expectExceptionMessage('GraphQL response contains errors: The account sign-in' . ' ' .
             'was incorrect or your account is disabled temporarily. Please wait and try again later.');
+        $this->graphQlMutation($mutation);
+    }
+
+    /**
+     * Verify customer with invalid email
+     */
+    public function testGenerateCustomerTokenWithInvalidEmail()
+    {
+        $email = 'customer@example';
+        $password = 'password';
+
+        $mutation
+            = <<<MUTATION
+mutation {
+	generateCustomerToken(
+        email: "{$email}"
+        password: "{$password}"
+    ) {
+        token
+    }
+}
+MUTATION;
         $this->graphQlMutation($mutation);
     }
 
