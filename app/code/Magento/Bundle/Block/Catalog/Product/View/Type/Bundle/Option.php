6fbf4f2f105dd65cdb9fa15d82eedd0925f7c09f
Hwashiang Yu <hwyu@adobe.com>
2019-05-06 15:56:28 -0500

    MAGETWO-99527: Eliminate @escapeNotVerified in Product Modules
    - Replaced escapeNotVerified from Bundle, ConfigurableProduct, Downloadable, and GroupedProduct module templates

--- app/code/Magento/Bundle/Block/Catalog/Product/View/Type/Bundle/Option.php
+++ app/code/Magento/Bundle/Block/Catalog/Product/View/Type/Bundle/Option.php
@@ -236,7 +236,7 @@ class Option extends \Magento\Bundle\Block\Catalog\Product\Price
      * @param bool $includeContainer
      * @return string
      */
-    public function getSelectionQtyTitlePrice($selection, $includeContainer = true)
+    public function getSelectionQtyTitlePriceHtml($selection, $includeContainer = true)
     {
         $this->setFormatProduct($selection);
         $priceTitle = '<span class="product-name">' . $selection->getSelectionQty() * 1 . ' x ' . $this->escapeHtml($selection->getName()) . '</span>';
@@ -277,7 +277,7 @@ class Option extends \Magento\Bundle\Block\Catalog\Product\Price
      * @param bool $includeContainer
      * @return string
      */
-    public function getSelectionTitlePrice($selection, $includeContainer = true)
+    public function getSelectionTitlePriceHtml($selection, $includeContainer = true)
     {
         $priceTitle = '<span class="product-name">' . $this->escapeHtml($selection->getName()) . '</span>';
         $priceTitle .= ' &nbsp; ' . ($includeContainer ? '<span class="price-notice">' : '') . '+'
