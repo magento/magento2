3df25b873d9487afdad3285400c1ad5bb36b604c
Nikita Shcherbatykh <nikita.shcherbatykh@transoftgroup.com>
2019-05-08 16:25:05 +0300

    MAGETWO-99605: Exact match search in the Backend

--- lib/internal/Magento/Framework/View/Element/UiComponent/DataProvider/FulltextFilter.php
+++ lib/internal/Magento/Framework/View/Element/UiComponent/DataProvider/FulltextFilter.php
@@ -69,7 +69,7 @@ class FulltextFilter implements FilterApplierInterface
      */
     private function escapeAgainstValue(string $value): string
     {
-        return preg_replace('/([+\-><\(\)~*\"@]+)/', ' ', $value);
+        return preg_replace('/([+\-><\(\)~*\'@]+)/', ' ', $value);
     }
 
     /**
