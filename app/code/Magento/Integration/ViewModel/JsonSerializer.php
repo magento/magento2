94aeb853bddb4ec2146fe2adcf4093dd0d62511e
Cari Spruiell <spruiell@adobe.com>
2019-05-09 11:53:32 -0500

    MAGETWO-99482: Use escaper methods
     - refactored $this->helper out of templates

---
+++ app/code/Magento/Integration/ViewModel/JsonSerializer.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Integration\ViewModel;
+
+/**
+ * JsonSerializer
+ */
+class JsonSerializer implements \Magento\Framework\View\Element\Block\ArgumentInterface
+{
+
+    /**
+     * @var \Magento\Framework\Serialize\Serializer\Json
+     */
+    private $serializer;
+
+    /**
+     * @param \Magento\Framework\Serialize\Serializer\Json $serializer
+     */
+    public function __construct(\Magento\Framework\Serialize\Serializer\Json $serializer)
+    {
+        $this->serializer = $serializer;
+    }
+
+    /**
+     * Returns serialized version of data
+     *
+     * @param array $data
+     * @return string
+     */
+    public function serialize(array $data): string
+    {
+        return $this->serializer->serialize($data);
+    }
+}
