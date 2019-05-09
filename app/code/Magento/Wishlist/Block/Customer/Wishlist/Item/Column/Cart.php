7cb816d0c90e55ef91b4dff88dbd31474cbf1d27
Raoul Rego <rrego@adobe.com>
2019-05-09 15:10:58 -0500

    MAGETWO-99613: Reverting: [Bakport] Adding product from wishlist not adding to cart showing warning message. #22536
    - This reverts commit c62ab93a45ef9f4dc6fb86d6cd03de127a9665a6.

--- app/code/Magento/Wishlist/Block/Customer/Wishlist/Item/Column/Cart.php
+++ app/code/Magento/Wishlist/Block/Customer/Wishlist/Item/Column/Cart.php
@@ -15,28 +15,6 @@ namespace Magento\Wishlist\Block\Customer\Wishlist\Item\Column;
 class Cart extends \Magento\Wishlist\Block\Customer\Wishlist\Item\Column
 {
     /**
-     * @var View
-     */
-    private $productView;
-
-    /**
-     * @param \Magento\Catalog\Block\Product\Context $context
-     * @param \Magento\Framework\App\Http\Context $httpContext
-     * @param \Magento\Catalog\Block\Product\View $productView
-     * @param array $data
-     */
-    public function __construct(
-        \Magento\Catalog\Block\Product\Context $context,
-        \Magento\Framework\App\Http\Context $httpContext,
-        array $data = [],
-        \Magento\Catalog\Block\Product\View $productView = null
-    ) {
-        $this->productView = $productView ?:
-                \Magento\Framework\App\ObjectManager::getInstance()->get(\Magento\Catalog\Block\Product\View::class);
-        parent::__construct($context, $httpContext, $data);
-    }
-
-    /**
      * Returns qty to show visually to user
      *
      * @param \Magento\Wishlist\Model\Item $item
@@ -45,9 +23,7 @@ class Cart extends \Magento\Wishlist\Block\Customer\Wishlist\Item\Column
     public function getAddToCartQty(\Magento\Wishlist\Model\Item $item)
     {
         $qty = $item->getQty();
-        $qty = $qty < $this->productView->getProductDefaultQty($this->getProductItem())
-                ? $this->productView->getProductDefaultQty($this->getProductItem()) : $qty;
-        return $qty ?: 1;
+        return $qty ? $qty : 1;
     }
 
     /**
