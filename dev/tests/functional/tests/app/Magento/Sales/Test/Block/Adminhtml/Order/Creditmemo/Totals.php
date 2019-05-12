4da3876a3d02ce242d944f9312eb779230c69450
Gaurav Agarwal <37572719+gauravagarwal1001@users.noreply.github.com>
2019-05-13 00:50:57 +0530

    Fixed #22774

--- dev/tests/functional/tests/app/Magento/Sales/Test/Block/Adminhtml/Order/Creditmemo/Totals.php
+++ dev/tests/functional/tests/app/Magento/Sales/Test/Block/Adminhtml/Order/Creditmemo/Totals.php
@@ -3,11 +3,8 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
-
 namespace Magento\Sales\Test\Block\Adminhtml\Order\Creditmemo;
-
 use Magento\Mtf\Client\Locator;
-
 /**
  * Invoice totals block.
  */
@@ -19,14 +16,36 @@ class Totals extends \Magento\Sales\Test\Block\Adminhtml\Order\Totals
      * @var string
      */
     protected $submit = '[data-ui-id="order-items-submit-button"]';
-
     /**
      * Capture amount select selector.
      *
      * @var string
      */
     protected $capture = '[name="invoice[capture_case]"]';
-
+    /**
+     * Refund Shipping css selector.
+     *
+     * @var string
+     */
+    private $refundShippingSelector = '//input[@id=\'shipping_amount\']';
+    /**
+     * Adjustment Refund css selector.
+     *
+     * @var string
+     */
+    private $adjustmentRefundSelector = '//input[@id=\'adjustment_positive\']';
+    /**
+     * Adjustment Fee css selector.
+     *
+     * @var string
+     */
+    private $adjustmentFeeSelector = '//input[@id=\'adjustment_negative\']';
+    /**
+     * 'Update Totals button css selector.
+     *
+     * @var string
+     */
+    private $updateTotalsSelector = '.update-totals-button';
     /**
      * Submit invoice.
      *
@@ -45,7 +64,6 @@ class Totals extends \Magento\Sales\Test\Block\Adminhtml\Order\Totals
         );
         $this->_rootElement->find($this->submit)->click();
     }
-
     /**
      * Set Capture amount option:
      * Capture Online|Capture Offline|Not Capture
@@ -56,5 +74,41 @@ class Totals extends \Magento\Sales\Test\Block\Adminhtml\Order\Totals
     public function setCaptureOption($option)
     {
         $this->_rootElement->find($this->capture, Locator::SELECTOR_CSS, 'select')->setValue($option);
+    }
+    /**
+     * Get Refund Shipping input element.
+     *
+     * @return \Magento\Mtf\Client\ElementInterface
+     */
+    public function getRefundShippingElement()
+    {
+        return $this->_rootElement->find($this->refundShippingSelector, Locator::SELECTOR_XPATH);
+    }
+    /**
+     * Get Adjustment Refund input element.
+     *
+     * @return \Magento\Mtf\Client\ElementInterface
+     */
+    public function getAdjustmentRefundElement()
+    {
+        return $this->_rootElement->find($this->adjustmentRefundSelector, Locator::SELECTOR_XPATH);
+    }
+    /**
+     * Get Adjustment Fee input element.
+     *
+     * @return \Magento\Mtf\Client\ElementInterface
+     */
+    public function getAdjustmentFeeElement()
+    {
+        return $this->_rootElement->find($this->adjustmentFeeSelector, Locator::SELECTOR_XPATH);
+    }
+    /**
+     * Click update totals button.
+     *
+     * @return void
+     */
+    public function clickUpdateTotals()
+    {
+        $this->_rootElement->find($this->updateTotalsSelector)->click();
     }
 }
