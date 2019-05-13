4da128d52a6a749830df904e2b36ec906cc32f6c
Deepty Thampy <dthampy@adobe.com>
2019-05-13 14:34:42 -0500

    MC-16073: POC to process a payment using Authorize.net method
    - integraton test

--- dev/tests/integration/testsuite/Magento/GraphQl/Controller/GraphQlControllerTest.php
+++ dev/tests/integration/testsuite/Magento/GraphQl/Controller/GraphQlControllerTest.php
@@ -12,6 +12,7 @@ use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Framework\App\Request\Http;
 use Magento\Framework\EntityManager\MetadataPool;
 use Magento\Framework\Serialize\SerializerInterface;
+use Magento\GraphQl\Quote\GetMaskedQuoteIdByReservedOrderId;
 use Magento\TestFramework\Helper\Bootstrap;
 
 /**
@@ -28,6 +29,10 @@ class GraphQlControllerTest extends \Magento\TestFramework\Indexer\TestCase
 
     /** @var \Magento\Framework\ObjectManagerInterface */
     private $objectManager;
+
+    /** @var  GetMaskedQuoteIdByReservedOrderId */
+    private $getMaskedQuoteIdByReservedOrderId;
+
 
     /** @var GraphQl */
     private $graphql;
@@ -61,6 +66,7 @@ class GraphQlControllerTest extends \Magento\TestFramework\Indexer\TestCase
         $this->jsonSerializer = $this->objectManager->get(SerializerInterface::class);
         $this->metadataPool = $this->objectManager->get(MetadataPool::class);
         $this->request = $this->objectManager->get(Http::class);
+        $this->getMaskedQuoteIdByReservedOrderId = $this->objectManager->get(GetMaskedQuoteIdByReservedOrderId::class);
     }
 
     /**
@@ -111,6 +117,64 @@ QUERY;
         $this->assertEquals($product->getData($linkField), $output['data']['products']['items'][0]['id']);
         $this->assertEquals($product->getSku(), $output['data']['products']['items'][0]['sku']);
         $this->assertEquals($product->getName(), $output['data']['products']['items'][0]['name']);
+    }
+
+    /**
+     *
+     * @magentoConfigFixture default_store payment/authorizenet_acceptjs/active 1
+     * @magentoConfigFixture default_store payment/authorizenet_acceptjs/environment sandbox
+     * @magentoConfigFixture default_store payment/authorizenet_acceptjs/login someusername
+     * @magentoConfigFixture default_store payment/authorizenet_acceptjs/trans_key somepassword
+     * @magentoConfigFixture default_store payment/authorizenet_acceptjs/trans_signature_key abc
+     * @magentoDataFixture Magento/GraphQl/Catalog/_files/simple_product.php
+     * @magentoDataFixture Magento/GraphQl/Quote/_files/guest/create_empty_cart.php
+     * @magentoDataFixture Magento/GraphQl/Quote/_files/add_simple_product.php
+     * @magentoDataFixture Magento/GraphQl/Quote/_files/set_new_shipping_address.php
+     */
+    public function testDispatchToSetPaymentMethodWithAuthorizenet(): void
+    {
+        $methodCode = 'authorizenet_acceptjs';
+        $maskedQuoteId = $this->getMaskedQuoteIdByReservedOrderId->execute('test_quote');
+        $query
+            = <<<QUERY
+ mutation {
+  setPaymentMethodOnCart(input: {
+      cart_id: "$maskedQuoteId"
+      payment_method: {
+          code: "$methodCode"
+          additional_data:
+         {authorizenet_acceptjs: 
+            {opaque_data_descriptor: "COMMON.ACCEPT.INAPP.PAYMENT",
+             opaque_data_value: "abx",
+             cc_last_4: 1111}}
+      }
+  }) {    
+    cart {
+      selected_payment_method {
+        code
+        purchase_order_number
+      }
+    }
+  }
+}
+QUERY;
+        $postData = [
+            'query' => $query,
+            'variables' => null,
+            'operationName' => null
+        ];
+        $this->request->setPathInfo('/graphql');
+        $this->request->setMethod('POST');
+        $this->request->setContent(json_encode($postData));
+        $headers = $this->objectManager->create(\Zend\Http\Headers::class)
+            ->addHeaders(['Content-Type' => 'application/json']);
+        $this->request->setHeaders($headers);
+        $response = $this->graphql->dispatch($this->request);
+        $output = $this->jsonSerializer->unserialize($response->getContent());
+        $this->assertArrayNotHasKey('errors', $output, 'Response has errors');
+        $this->assertArrayHasKey('setPaymentMethodOnCart', $output['data']);
+        $selectedPaymentMethod = $output['data']['setPaymentMethodOnCart']['cart']['selected_payment_method'];
+        $this->assertEquals($methodCode, $selectedPaymentMethod['code']);
     }
 
     /**
