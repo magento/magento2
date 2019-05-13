5c887eacd5cc5e1f907a5cbb2c2a85ba23d81ff3
Stas Kozar <stas.kozar@transoftgroup.com>
2019-05-13 11:06:32 +0300

    MC-15448: One can't download or delete export csv file from export index page grid

--- dev/tests/integration/testsuite/Magento/ImportExport/Controller/Adminhtml/Export/File/DeleteTest.php
+++ dev/tests/integration/testsuite/Magento/ImportExport/Controller/Adminhtml/Export/File/DeleteTest.php
@@ -8,22 +8,19 @@ declare(strict_types=1);
 namespace Magento\ImportExport\Controller\Adminhtml\Export\File;
 
 use Magento\Framework\App\Filesystem\DirectoryList;
+use Magento\Framework\Data\Form\FormKey;
 use Magento\Framework\Filesystem;
-use Magento\ImportExport\Controller\Adminhtml\Export\File\Delete;
+use Magento\Framework\Filesystem\Directory\WriteInterface;
 use Magento\TestFramework\Helper\Bootstrap;
+use Magento\TestFramework\TestCase\AbstractBackendController;
 
 /**
  * Test for \Magento\ImportExport\Controller\Adminhtml\Export\File\Delete class.
  */
-class DeleteTest extends \PHPUnit\Framework\TestCase
+class DeleteTest extends AbstractBackendController
 {
     /**
-     * @var Delete
-     */
-    private $model;
-
-    /**
-     * @var \Magento\Framework\Filesystem\Directory\WriteInterface
+     * @var WriteInterface
      */
     private $varDirectory;
 
@@ -43,24 +40,19 @@ class DeleteTest extends \PHPUnit\Framework\TestCase
     private $filesystem;
 
     /**
-     * @var \Magento\Framework\ObjectManagerInterface
-     */
-    private $objectManager;
-
-    /**
      * @inheritdoc
      */
     protected function setUp()
     {
-        $this->objectManager = Bootstrap::getObjectManager();
-        $this->filesystem = $this->objectManager->get(Filesystem::class);
+        parent::setUp();
+
+        $this->filesystem = $this->_objectManager->get(Filesystem::class);
         $this->varDirectory = $this->filesystem->getDirectoryWrite(DirectoryList::VAR_DIR);
         $this->varDirectory->create($this->varDirectory->getRelativePath('export'));
         $this->fullDirectoryPath = $this->varDirectory->getAbsolutePath('export');
         $filePath =  $this->fullDirectoryPath . DIRECTORY_SEPARATOR . $this->fileName;
         $fixtureDir = realpath(__DIR__ . '/../../Import/_files');
         copy($fixtureDir . '/' . $this->fileName, $filePath);
-        $this->model = $this->objectManager->get(Delete::class);
     }
 
     /**
@@ -68,14 +60,32 @@ class DeleteTest extends \PHPUnit\Framework\TestCase
      *
      * @return void
      */
-    public function testExecute()
+    public function testExecute(): void
     {
-        $this->model->getRequest()->setMethod('GET')->setParams(['filename' => 'catalog_product.csv']);
-        $this->model->execute();
+        $uri = 'backend/admin/export_file/delete/filename/' . $this->fileName;
+        $this->prepareRequest($uri);
+        $this->dispatch($uri);
 
         $this->assertFalse(
             $this->varDirectory->isExist($this->varDirectory->getRelativePath('export/' . $this->fileName))
         );
+    }
+
+    /**
+     * Prepares GET request for file deletion.
+     *
+     * @param string $uri
+     * @return void
+     */
+    private function prepareRequest(string $uri): void
+    {
+        /** @var FormKey $formKey */
+        $formKey = $this->_objectManager->get(FormKey::class);
+        $request = $this->getRequest();
+        $request->setMethod('GET');
+        $request->setParam('form_key', $formKey->getFormKey());
+        $request->setRequestUri($uri);
+        $request->setParams(['filename' => 'catalog_product.csv']);
     }
 
     /**
@@ -84,7 +94,7 @@ class DeleteTest extends \PHPUnit\Framework\TestCase
     public static function tearDownAfterClass()
     {
         $filesystem = Bootstrap::getObjectManager()->get(Filesystem::class);
-        /** @var \Magento\Framework\Filesystem\Directory\WriteInterface $directory */
+        /** @var WriteInterface $directory */
         $directory = $filesystem->getDirectoryWrite(DirectoryList::VAR_DIR);
         if ($directory->isExist('export')) {
             $directory->delete('export');
