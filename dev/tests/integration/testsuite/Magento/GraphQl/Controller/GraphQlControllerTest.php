ec3b0c3eb094e084d86a93cfc2c12bf01095a2cf
Deepty Thampy <dthampy@adobe.com>
2019-05-13 16:43:37 -0500

    MC-16073: POC to process a payment using Authorize.net method
    - rearranging test order

--- dev/tests/integration/testsuite/Magento/GraphQl/Controller/GraphQlControllerTest.php
+++ dev/tests/integration/testsuite/Magento/GraphQl/Controller/GraphQlControllerTest.php
@@ -120,65 +120,8 @@ QUERY;
     }
 
     /**
-     *
-     * @magentoConfigFixture default_store payment/authorizenet_acceptjs/active 1
-     * @magentoConfigFixture default_store payment/authorizenet_acceptjs/environment sandbox
-     * @magentoConfigFixture default_store payment/authorizenet_acceptjs/login someusername
-     * @magentoConfigFixture default_store payment/authorizenet_acceptjs/trans_key somepassword
-     * @magentoConfigFixture default_store payment/authorizenet_acceptjs/trans_signature_key abc
-     * @magentoDataFixture Magento/GraphQl/Catalog/_files/simple_product.php
-     * @magentoDataFixture Magento/GraphQl/Quote/_files/guest/create_empty_cart.php
-     * @magentoDataFixture Magento/GraphQl/Quote/_files/add_simple_product.php
-     * @magentoDataFixture Magento/GraphQl/Quote/_files/set_new_shipping_address.php
-     */
-    public function testDispatchToSetPaymentMethodWithAuthorizenet(): void
-    {
-        $methodCode = 'authorizenet_acceptjs';
-        $maskedQuoteId = $this->getMaskedQuoteIdByReservedOrderId->execute('test_quote');
-        $query
-            = <<<QUERY
- mutation {
-  setPaymentMethodOnCart(input: {
-      cart_id: "$maskedQuoteId"
-      payment_method: {
-          code: "$methodCode"
-          additional_data:
-         {authorizenet_acceptjs: 
-            {opaque_data_descriptor: "COMMON.ACCEPT.INAPP.PAYMENT",
-             opaque_data_value: "abx",
-             cc_last_4: 1111}}
-      }
-  }) {    
-    cart {
-      selected_payment_method {
-        code
-      }
-    }
-  }
-}
-QUERY;
-        $postData = [
-            'query' => $query,
-            'variables' => null,
-            'operationName' => null
-        ];
-        $this->request->setPathInfo('/graphql');
-        $this->request->setMethod('POST');
-        $this->request->setContent(json_encode($postData));
-        $headers = $this->objectManager->create(\Zend\Http\Headers::class)
-            ->addHeaders(['Content-Type' => 'application/json']);
-        $this->request->setHeaders($headers);
-        $response = $this->graphql->dispatch($this->request);
-        $output = $this->jsonSerializer->unserialize($response->getContent());
-        $this->assertArrayNotHasKey('errors', $output, 'Response has errors');
-        $this->assertArrayHasKey('setPaymentMethodOnCart', $output['data']);
-        $selectedPaymentMethod = $output['data']['setPaymentMethodOnCart']['cart']['selected_payment_method'];
-        $this->assertEquals($methodCode, $selectedPaymentMethod['code']);
-    }
-
-    /**
      * Test request is dispatched and response generated when using GET request with query string
-     *
+     * @magentoDataFixture Magento/Catalog/_files/product_simple_with_url_key.php
      * @return void
      */
     public function testDispatchWithGet() : void
@@ -327,5 +270,62 @@ QUERY;
                 }
             }
         }
+    }
+
+    /**
+     *
+     * @magentoConfigFixture default_store payment/authorizenet_acceptjs/active 1
+     * @magentoConfigFixture default_store payment/authorizenet_acceptjs/environment sandbox
+     * @magentoConfigFixture default_store payment/authorizenet_acceptjs/login someusername
+     * @magentoConfigFixture default_store payment/authorizenet_acceptjs/trans_key somepassword
+     * @magentoConfigFixture default_store payment/authorizenet_acceptjs/trans_signature_key abc
+     * @magentoDataFixture Magento/GraphQl/Catalog/_files/simple_product.php
+     * @magentoDataFixture Magento/GraphQl/Quote/_files/guest/create_empty_cart.php
+     * @magentoDataFixture Magento/GraphQl/Quote/_files/add_simple_product.php
+     * @magentoDataFixture Magento/GraphQl/Quote/_files/set_new_shipping_address.php
+     */
+    public function testDispatchToSetPaymentMethodWithAuthorizenet(): void
+    {
+        $methodCode = 'authorizenet_acceptjs';
+        $maskedQuoteId = $this->getMaskedQuoteIdByReservedOrderId->execute('test_quote');
+        $query
+            = <<<QUERY
+ mutation {
+  setPaymentMethodOnCart(input: {
+      cart_id: "$maskedQuoteId"
+      payment_method: {
+          code: "$methodCode"
+          additional_data:
+         {authorizenet_acceptjs: 
+            {opaque_data_descriptor: "COMMON.ACCEPT.INAPP.PAYMENT",
+             opaque_data_value: "abx",
+             cc_last_4: 1111}}
+      }
+  }) {    
+    cart {
+      selected_payment_method {
+        code
+      }
+    }
+  }
+}
+QUERY;
+        $postData = [
+            'query' => $query,
+            'variables' => null,
+            'operationName' => null
+        ];
+        $this->request->setPathInfo('/graphql');
+        $this->request->setMethod('POST');
+        $this->request->setContent(json_encode($postData));
+        $headers = $this->objectManager->create(\Zend\Http\Headers::class)
+            ->addHeaders(['Content-Type' => 'application/json']);
+        $this->request->setHeaders($headers);
+        $response = $this->graphql->dispatch($this->request);
+        $output = $this->jsonSerializer->unserialize($response->getContent());
+        $this->assertArrayNotHasKey('errors', $output, 'Response has errors');
+        $this->assertArrayHasKey('setPaymentMethodOnCart', $output['data']);
+        $selectedPaymentMethod = $output['data']['setPaymentMethodOnCart']['cart']['selected_payment_method'];
+        $this->assertEquals($methodCode, $selectedPaymentMethod['code']);
     }
 }
