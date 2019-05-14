079dbb14aafd97f03ad9ecccbcc6a1b8dce72fad
Amit Vishvakarma <amitvishvakarma@cedcommerce.com>
2019-05-15 00:20:39 +0530

    Added functionality of deleting product images from folder

--- app/code/Magento/Catalog/Controller/Adminhtml/Product/MassDelete.php
+++ app/code/Magento/Catalog/Controller/Adminhtml/Product/MassDelete.php
@@ -12,7 +12,7 @@ use Magento\Backend\App\Action\Context;
 use Magento\Ui\Component\MassAction\Filter;
 use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory;
 use Magento\Catalog\Api\ProductRepositoryInterface;
-
+use Magento\Catalog\Model\ProductFactory;
 class MassDelete extends \Magento\Catalog\Controller\Adminhtml\Product implements HttpPostActionInterface
 {
     /**
@@ -31,6 +31,11 @@ class MassDelete extends \Magento\Catalog\Controller\Adminhtml\Product implement
      * @var ProductRepositoryInterface
      */
     private $productRepository;
+    
+     /**
+     * @var ProductFactory
+     */
+    private $productFactory;
 
     /**
      * @param Context $context
@@ -38,18 +43,21 @@ class MassDelete extends \Magento\Catalog\Controller\Adminhtml\Product implement
      * @param Filter $filter
      * @param CollectionFactory $collectionFactory
      * @param ProductRepositoryInterface $productRepository
+     * @param ProductFactory $productFactory
      */
     public function __construct(
         Context $context,
         Builder $productBuilder,
         Filter $filter,
         CollectionFactory $collectionFactory,
-        ProductRepositoryInterface $productRepository = null
+        ProductRepositoryInterface $productRepository = null,
+        ProductFactory $productFactory
     ) {
         $this->filter = $filter;
         $this->collectionFactory = $collectionFactory;
         $this->productRepository = $productRepository
             ?: \Magento\Framework\App\ObjectManager::getInstance()->create(ProductRepositoryInterface::class);
+        $this->productFactory = $productFactory;
         parent::__construct($context, $productBuilder);
     }
 
@@ -62,6 +70,7 @@ class MassDelete extends \Magento\Catalog\Controller\Adminhtml\Product implement
         $productDeleted = 0;
         /** @var \Magento\Catalog\Model\Product $product */
         foreach ($collection->getItems() as $product) {
+            $this->productFactory->create()->load($product->getId())->deleteMediaGalleryImages();
             $this->productRepository->delete($product);
             $productDeleted++;
         }
