b549800f0e234bf7e2ff498c7e743778c3d8e7b7
Oleksandr Gorkun <ogorkun@magento.com>
2019-05-17 16:33:33 -0500

    MAGETWO-99673: Implement deferred

--- dev/tests/integration/testsuite/Magento/Dhl/Model/CarrierTest.php
+++ dev/tests/integration/testsuite/Magento/Dhl/Model/CarrierTest.php
@@ -7,11 +7,15 @@ declare(strict_types=1);
 
 namespace Magento\Dhl\Model;
 
+use Magento\Framework\App\Config\ReinitableConfigInterface;
+use Magento\Framework\DataObject;
 use Magento\Framework\HTTP\AsyncClient\Response;
 use Magento\Framework\HTTP\AsyncClientInterface;
 use Magento\Framework\Simplexml\Element;
+use Magento\Shipping\Model\Shipment\Request;
 use Magento\Shipping\Model\Tracking\Result\Status;
 use Magento\TestFramework\HTTP\AsyncClientInterfaceMock;
+use Magento\Shipping\Model\Simplexml\Element as ShippingElement;
 
 /**
  * Test for DHL integration.
@@ -29,6 +33,16 @@ class CarrierTest extends \PHPUnit\Framework\TestCase
     private $httpClient;
 
     /**
+     * @var ReinitableConfigInterface
+     */
+    private $config;
+
+    /**
+     * @var string
+     */
+    private $restoreCountry;
+
+    /**
      * @inheritDoc
      */
     protected function setUp()
@@ -36,6 +50,21 @@ class CarrierTest extends \PHPUnit\Framework\TestCase
         $objectManager = \Magento\TestFramework\Helper\Bootstrap::getObjectManager();
         $this->dhlCarrier = $objectManager->get(\Magento\Dhl\Model\Carrier::class);
         $this->httpClient = $objectManager->get(AsyncClientInterface::class);
+        $this->config = $objectManager->get(ReinitableConfigInterface::class);
+        $this->restoreCountry = $this->config->getValue('shipping/origin/country_id', 'store', 'default_store');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function tearDown()
+    {
+        $this->config->setValue(
+            'shipping/origin/country_id',
+            $this->restoreCountry,
+            'store',
+            'default_store'
+        );
     }
 
     /**
@@ -211,5 +240,144 @@ class CarrierTest extends \PHPUnit\Framework\TestCase
                 $this->assertEquals($expectedTrackingData[$ctr++], $trackingResult->getData());
             }
         }
+    }
+
+    /**
+     * Test sending shipping requests.
+     *
+     * @magentoConfigFixture default_store carriers/dhl/id some ID
+     * @magentoConfigFixture default_store carriers/dhl/password some password
+     * @magentoConfigFixture default_store carriers/dhl/account 1234567890
+     * @magentoConfigFixture default_store carriers/dhl/gateway_url https://xmlpi-ea.dhl.com/XMLShippingServlet
+     * @magentoConfigFixture default_store carriers/dhl/content_type N
+     * @magentoConfigFixture default_store carriers/dhl/nondoc_methods 1,3,4,8,P,Q,E,F,H,J,M,V,Y
+     * @magentoConfigFixture default_store carriers/dhl/unit_of_measure C
+     * @param string $origCountryId
+     * @param string $expectedRegionCode
+     * @param string $destCountryId
+     * @dataProvider requestToShipmentDataProvider
+     */
+    public function testRequestToShip(string $origCountryId, string $expectedRegionCode, string $destCountryId): void
+    {
+        $this->config->setValue(
+            'shipping/origin/country_id',
+            $origCountryId,
+            'store',
+            null
+        );
+        $this->httpClient->nextResponses(
+            [
+                new Response(
+                    200,
+                    [],
+                    utf8_encode(file_get_contents(__DIR__ . '/../_files/response_shipping_label.xml'))
+                )
+            ]
+        );
+        $request = new Request(
+            [
+                'packages' => [
+                    'package' => [
+                        'params' => [
+                            'width' => '3',
+                            'length' => '3',
+                            'height' => '3',
+                            'dimension_units' => 'CENTIMETER',
+                            'weight_units' => 'KILOGRAM',
+                            'weight' => '0.454000000001',
+                            'customs_value' => '10.00',
+                            'container' => Carrier::DHL_CONTENT_TYPE_NON_DOC,
+                        ],
+                        'items' => [
+                            'item1' => [
+                                'name' => 'item_name',
+                            ],
+                        ],
+                    ],
+                ],
+                'orig_country_id' => $origCountryId,
+                'dest_country_id' => $destCountryId,
+                'shipper_address_country_code' => $origCountryId,
+                'recipient_address_country_code' => $destCountryId,
+                'package_weight' => '0.454000000001',
+                'free_method_weight' => '0.454000000001',
+                'recipient_address_street_1' => '15099 Some Blvd',
+                'shipper_address_street_1' => '4956 Some Way',
+                'order_shipment' => new DataObject([
+                    'order' => new DataObject([
+                        'subtotal' => '10.00'
+                    ])
+                ])
+            ]
+        );
+
+        //Generating labels
+        $labels = $this->dhlCarrier->requestToShipment($request);
+        $this->assertNotEmpty($labels);
+        $this->assertNotEmpty($labels->getInfo());
+        $request = $this->httpClient->getLastRequest()->getBody();
+        $requestElement = new Element($request);
+        $messageReference = $requestElement->Request->ServiceHeader->MessageReference->__toString();
+        $this->assertStringStartsWith('MAGE_SHIP_', $messageReference);
+        $this->assertGreaterThanOrEqual(28, strlen($messageReference));
+        $this->assertLessThanOrEqual(32, strlen($messageReference));
+        $requestElement->Request->ServiceHeader->MessageReference = 'MAGE_SHIP_28TO32_Char_CHECKED';
+        $requestElement->Request->ServiceHeader->MessageTime = 'currentTime';
+        $requestElement->ShipmentDetails->Date = 'currentTime';
+        $this->assertXmlStringEqualsXmlString(
+            $this->getExpectedLabelRequestXml($origCountryId, $destCountryId, $expectedRegionCode),
+            $requestElement->asXML()
+        );
+    }
+
+    /**
+     * Cases with different countries.
+     *
+     * @return array
+     */
+    public function requestToShipmentDataProvider(): array
+    {
+        return [
+            [
+                'GB', 'EU', 'US'
+            ],
+            [
+                'SG', 'AP', 'US'
+            ],
+            [
+                'DE', 'EU', 'DE'
+            ]
+        ];
+    }
+
+    /**
+     * Generate expected labels request XML.
+     *
+     * @param string $origCountryId
+     * @param string $destCountryId
+     * @param string $regionCode
+     * @return string
+     */
+    private function getExpectedLabelRequestXml(string $origCountryId, string $destCountryId, string $regionCode): string
+    {
+        $countryNames = [
+            'US' => 'United States of America',
+            'SG' => 'Singapore',
+            'GB' => 'United Kingdom',
+            'DE' => 'Germany',
+        ];
+        $requestXmlPath = $origCountryId == $destCountryId
+            ? '/../_files/domestic_shipment_request.xml'
+            : '/../_files/shipment_request.xml';
+
+        $expectedRequestElement = new ShippingElement(file_get_contents(__DIR__ . $requestXmlPath));
+
+        $expectedRequestElement->Consignee->CountryCode = $destCountryId;
+        $expectedRequestElement->Consignee->CountryName = $countryNames[$destCountryId];
+        $expectedRequestElement->Shipper->CountryCode = $origCountryId;
+        $expectedRequestElement->Shipper->CountryName = $countryNames[$origCountryId];
+        $expectedRequestElement->RegionCode = $regionCode;
+
+        return $expectedRequestElement->asXML();
     }
 }
