069cbe507f6eba0eaf82bf2b14870d876b86b9c8
Iryna Lagno <ilagno@adobe.com>
2019-05-20 13:58:32 -0500

    MAGETWO-80146: Forward port tests for community pull request to develop branch
    -fix CR comments

--- app/code/Magento/Catalog/Test/Unit/Pricing/Price/TierPriceTest.php
+++ app/code/Magento/Catalog/Test/Unit/Pricing/Price/TierPriceTest.php
@@ -414,6 +414,9 @@ class TierPriceTest extends \PHPUnit\Framework\TestCase
         $this->assertEquals($expectedValue, $tierPrice->getQuantity());
     }
 
+    /**
+     * @return array
+     */
     public function getQuantityDataProvider()
     {
         return [
