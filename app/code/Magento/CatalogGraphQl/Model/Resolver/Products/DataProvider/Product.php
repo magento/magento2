d8e7d961e51224abe2732bfb2f9157d5db57a44d
Daniel Renaud <drenaud@magento.com>
2019-05-20 11:00:09 -0500

    MC-16644: Configurable Product Result w/ UrlKey Filter

--- app/code/Magento/CatalogGraphQl/Model/Resolver/Products/DataProvider/Product.php
+++ app/code/Magento/CatalogGraphQl/Model/Resolver/Products/DataProvider/Product.php
@@ -86,6 +86,9 @@ class Product
         $collection->load();
 
         // Methods that perform extra fetches post-load
+        if (in_array('media_gallery_entries', $attributes)) {
+            $collection->addMediaGalleryData();
+        }
         if (in_array('options', $attributes)) {
             $collection->addOptionsToResult();
         }
