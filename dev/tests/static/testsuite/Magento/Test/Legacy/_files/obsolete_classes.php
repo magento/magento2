9a766614e8bf9945a9dada86b8485b03889649d0
Cari Spruiell <spruiell@adobe.com>
2019-05-22 10:34:34 -0500

    MC-16611: Fix Unrelated Static Test Failures
     - clean up code

--- dev/tests/static/testsuite/Magento/Test/Legacy/_files/obsolete_classes.php
+++ dev/tests/static/testsuite/Magento/Test/Legacy/_files/obsolete_classes.php
@@ -1790,7 +1790,7 @@ return [
     ],
     ['Magento\Adminhtml\Block\Report\Shopcart\Product', 'Magento\Reports\Block\Adminhtml\Shopcart\Product'],
     ['Magento\Adminhtml\Block\Report\Wishlist\Grid', 'Magento\Reports\Block\Adminhtml\Wishlist\Grid'],
-    ['Magento\Adminhtml\Block\Report\Wishlist', 'Magento\Reports\Block\Adminhtml\Wishlist'],
+    ['Magento\Adminhtml\Block\Report\Wishlist'],
     ['Magento\Backend\Helper\Addresses'],
     ['Magento\Backend\Controller\Adminhtml\System\Variable', 'Magento\Variable\Controller\Adminhtml\System\Variable'],
     [
@@ -3198,6 +3198,8 @@ return [
     ['Magento\Tax\Model\ResourceModel\Sales\Order\Tax\Item', 'Magento\Sales\Model\ResourceModel\Order\Tax\Item'],
     ['Magento\Tax\Model\Sales\Order\Tax\Item', 'Magento\Sales\Model\Order\Tax\Item'],
     ['Magento\Reports\Block\Adminhtml\Product\Grid'],
+    ['Magento\Reports\Block\Adminhtml\Product\Widget\Viewed\Item'],
+    ['Magento\Reports\Block\Adminhtml\Wishlist'],
     ['Magento\Reports\Model\Totals'],
     ['Magento\Log\Model\Shell'],
     ['Magento\Log\App\Shell'],
