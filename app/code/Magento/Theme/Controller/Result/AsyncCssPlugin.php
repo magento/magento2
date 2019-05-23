53f41cce762e5e344b6605e8879df1d79316c31e
Dmytro Poperechnyy <dpoperechnyy@magento.com>
2019-05-23 15:51:40 -0500

    MC-16872: Add loadCss library for preload fallback
    - Move loadCSS polyfill script to layout;

--- app/code/Magento/Theme/Controller/Result/AsyncCssPlugin.php
+++ app/code/Magento/Theme/Controller/Result/AsyncCssPlugin.php
@@ -38,8 +38,6 @@ class AsyncCssPlugin
     public function beforeSendResponse(Http $subject): void
     {
         $content = $subject->getContent();
-        // loadCSS rel=preload polyfill script https://github.com/filamentgroup/loadCSS/blob/v2.0.1/src/cssrelpreload.js
-        $loadCssScript = '!function(t){"use strict";t.loadCSS||(t.loadCSS=function(){});var e=loadCSS.relpreload={};if(e.support=function(){var e;try{e=t.document.createElement("link").relList.supports("preload")}catch(t){e=!1}return function(){return e}}(),e.bindMediaToggle=function(t){var e=t.media||"all";function a(){t.media=e}t.addEventListener?t.addEventListener("load",a):t.attachEvent&&t.attachEvent("onload",a),setTimeout(function(){t.rel="stylesheet",t.media="only x"}),setTimeout(a,3e3)},e.poly=function(){if(!e.support())for(var a=t.document.getElementsByTagName("link"),n=0;n<a.length;n++){var o=a[n];"preload"!==o.rel||"style"!==o.getAttribute("as")||o.getAttribute("data-loadcss")||(o.setAttribute("data-loadcss",!0),e.bindMediaToggle(o))}},!e.support()){e.poly();var a=t.setInterval(e.poly,500);t.addEventListener?t.addEventListener("load",function(){e.poly(),t.clearInterval(a)}):t.attachEvent&&t.attachEvent("onload",function(){e.poly(),t.clearInterval(a)})}"undefined"!=typeof exports?exports.loadCSS=loadCSS:t.loadCSS=loadCSS}("undefined"!=typeof global?global:this);';
 
         if (strpos($content, '</body') !== false && $this->scopeConfig->isSetFlag(
             self::XML_PATH_USE_CSS_CRITICAL_PATH,
@@ -67,10 +65,6 @@ class AsyncCssPlugin
                 },
                 $content
             );
-            // add CSS rel preload polyfill script
-            $pattern = '@</head>@';
-            $replacement = '<script>' . $loadCssScript . '</script></head>';
-            $content = preg_replace($pattern, $replacement, $content);
 
             $subject->setContent($content);
         }
