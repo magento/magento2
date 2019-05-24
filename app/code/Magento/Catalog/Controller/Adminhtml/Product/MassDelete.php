73710402b08454e0a5c16f6a2db89252146690d8
Amit Vishvakarma <amitvishvakarma@cedcommerce.com>
2019-05-24 06:30:44 +0000

    Previous changes reverted

--- app/code/Magento/Catalog/Controller/Adminhtml/Product/MassDelete.php
+++ app/code/Magento/Catalog/Controller/Adminhtml/Product/MassDelete.php
@@ -5,14 +5,12 @@
  * See COPYING.txt for license details.
  */
 namespace Magento\Catalog\Controller\Adminhtml\Product;
-
 use Magento\Framework\App\Action\HttpPostActionInterface as HttpPostActionInterface;
 use Magento\Framework\Controller\ResultFactory;
 use Magento\Backend\App\Action\Context;
 use Magento\Ui\Component\MassAction\Filter;
 use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory;
 use Magento\Catalog\Api\ProductRepositoryInterface;
-use Magento\Catalog\Model\ProductFactory;
 class MassDelete extends \Magento\Catalog\Controller\Adminhtml\Product implements HttpPostActionInterface
 {
     /**
@@ -21,46 +19,34 @@ class MassDelete extends \Magento\Catalog\Controller\Adminhtml\Product implement
      * @var Filter
      */
     protected $filter;
-
     /**
      * @var CollectionFactory
      */
     protected $collectionFactory;
-
     /**
      * @var ProductRepositoryInterface
      */
     private $productRepository;
-    
-     /**
-     * @var ProductFactory
-     */
-    private $productFactory;
-
     /**
      * @param Context $context
      * @param Builder $productBuilder
      * @param Filter $filter
      * @param CollectionFactory $collectionFactory
      * @param ProductRepositoryInterface $productRepository
-     * @param ProductFactory $productFactory
      */
     public function __construct(
         Context $context,
         Builder $productBuilder,
         Filter $filter,
         CollectionFactory $collectionFactory,
-        ProductRepositoryInterface $productRepository = null,
-        ProductFactory $productFactory
+        ProductRepositoryInterface $productRepository = null
     ) {
         $this->filter = $filter;
         $this->collectionFactory = $collectionFactory;
         $this->productRepository = $productRepository
             ?: \Magento\Framework\App\ObjectManager::getInstance()->create(ProductRepositoryInterface::class);
-        $this->productFactory = $productFactory;
         parent::__construct($context, $productBuilder);
     }
-
     /**
      * @return \Magento\Backend\Model\View\Result\Redirect
      */
@@ -70,17 +56,14 @@ class MassDelete extends \Magento\Catalog\Controller\Adminhtml\Product implement
         $productDeleted = 0;
         /** @var \Magento\Catalog\Model\Product $product */
         foreach ($collection->getItems() as $product) {
-            $this->productFactory->create()->load($product->getId())->deleteMediaGalleryImages();
             $this->productRepository->delete($product);
             $productDeleted++;
         }
-
         if ($productDeleted) {
             $this->messageManager->addSuccessMessage(
                 __('A total of %1 record(s) have been deleted.', $productDeleted)
             );
         }
-
         return $this->resultFactory->create(ResultFactory::TYPE_REDIRECT)->setPath('catalog/*/index');
     }
 }
