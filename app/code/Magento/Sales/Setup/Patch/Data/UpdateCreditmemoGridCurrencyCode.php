18a6653e3fa419331c1909dfdb38ec8e402d1502
IvanPletnyov <ivan.pletnyov@transoftgroup.com>
2019-06-04 15:02:46 +0300

    pull request 22123: Fix database compare test

---
+++ app/code/Magento/Sales/Setup/Patch/Data/UpdateCreditmemoGridCurrencyCode.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\Sales\Setup\Patch\Data;
+
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\Patch\PatchVersionInterface;
+use Magento\Sales\Setup\SalesSetupFactory;
+
+class UpdateCreditmemoGridCurrencyCode implements DataPatchInterface, PatchVersionInterface
+{
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+
+    /**
+     * @var SalesSetupFactory
+     */
+    private $salesSetupFactory;
+
+    /**
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param SalesSetupFactory $salesSetupFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        SalesSetupFactory $salesSetupFactory
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->salesSetupFactory = $salesSetupFactory;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function apply()
+    {
+        $salesSetup = $this->salesSetupFactory->create(['setup' => $this->moduleDataSetup]);
+        $connection = $salesSetup->getConnection();
+        $creditMemoGridTable = $salesSetup->getTable('sales_creditmemo_grid');
+        $orderTable = $salesSetup->getTable('sales_order');
+
+        $sql = "UPDATE {$creditMemoGridTable} AS scg 
+        JOIN {$orderTable} AS so ON so.entity_id = scg.order_id 
+        SET scg.order_currency_code = so.order_currency_code, 
+        scg.base_currency_code = so.base_currency_code;";
+
+        $connection->query($sql);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function getVersion()
+    {
+        return '2.0.13';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+}
