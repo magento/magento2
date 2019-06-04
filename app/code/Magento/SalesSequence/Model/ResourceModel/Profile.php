7c078cf8158580bf704c6fb79e0bcd8e26ffe5d7
Bartłomiej Szubert <bartlomiejszubert@gmail.com>
2019-06-05 00:17:14 +0200

    Fix #14958 - add declare strict_types

--- app/code/Magento/SalesSequence/Model/ResourceModel/Profile.php
+++ app/code/Magento/SalesSequence/Model/ResourceModel/Profile.php
@@ -3,7 +3,6 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
-
 namespace Magento\SalesSequence\Model\ResourceModel;
 
 use Magento\Framework\Model\ResourceModel\Db\Context as DatabaseContext;
