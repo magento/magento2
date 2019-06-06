7fea44962bc8caa4822b77ee383cc145f218449e
Daniel Renaud <drenaud@magento.com>
2019-06-06 13:23:03 -0500

    MC-17041: Gift Card - checking the card balance

--- dev/tests/integration/testsuite/Magento/Store/_files/second_store_with_second_currency.php
+++ dev/tests/integration/testsuite/Magento/Store/_files/second_store_with_second_currency.php
@@ -9,28 +9,29 @@ require_once 'second_store.php';
 
 $objectManager = \Magento\TestFramework\Helper\Bootstrap::getObjectManager();
 $store = $objectManager->create(\Magento\Store\Model\Store::class);
-$storeId = $store->load('fixture_second_store', 'code')->getId();
-/** @var \Magento\Config\Model\ResourceModel\Config $configResource */
-$configResource = $objectManager->get(\Magento\Config\Model\ResourceModel\Config::class);
-$configResource->saveConfig(
-    \Magento\Directory\Model\Currency::XML_PATH_CURRENCY_DEFAULT,
-    'EUR',
-    \Magento\Store\Model\ScopeInterface::SCOPE_STORES,
-    $storeId
-);
-$configResource->saveConfig(
-    \Magento\Directory\Model\Currency::XML_PATH_CURRENCY_ALLOW,
-    'EUR',
-    \Magento\Store\Model\ScopeInterface::SCOPE_STORES,
-    $storeId
-);
+if ($storeId = $store->load('fixture_second_store', 'code')->getId()) {
+    /** @var \Magento\Config\Model\ResourceModel\Config $configResource */
+    $configResource = $objectManager->get(\Magento\Config\Model\ResourceModel\Config::class);
+    $configResource->saveConfig(
+        \Magento\Directory\Model\Currency::XML_PATH_CURRENCY_DEFAULT,
+        'EUR',
+        \Magento\Store\Model\ScopeInterface::SCOPE_STORES,
+        $storeId
+    );
+    $configResource->saveConfig(
+        \Magento\Directory\Model\Currency::XML_PATH_CURRENCY_ALLOW,
+        'EUR',
+        \Magento\Store\Model\ScopeInterface::SCOPE_STORES,
+        $storeId
+    );
+    /**
+     * Configuration cache clean is required to reload currency setting
+     */
+    /** @var Magento\Config\App\Config\Type\System $config */
+    $config = $objectManager->get(\Magento\Config\App\Config\Type\System::class);
+    $config->clean();
+}
 
-/**
- * Configuration cache clean is required to reload currency setting
- */
-/** @var Magento\Config\App\Config\Type\System $config */
-$config = $objectManager->get(\Magento\Config\App\Config\Type\System::class);
-$config->clean();
 
 /** @var \Magento\Directory\Model\ResourceModel\Currency $rate */
 $rate = $objectManager->create(\Magento\Directory\Model\ResourceModel\Currency::class);
