c6cc171cec8d7196fde62da94ba91b455d69aace
Viktor Petryk <victor.petryk@transoftgroup.com>
2019-06-10 13:13:11 +0300

    MAGETWO-73047: [GITHUB] Product Edit Page Can't Load #5967

--- app/code/Magento/Translation/Model/Js/PreProcessor.php
+++ app/code/Magento/Translation/Model/Js/PreProcessor.php
@@ -3,6 +3,8 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types=1);
+
 namespace Magento\Translation\Model\Js;
 
 use Magento\Framework\App\AreaList;
@@ -85,13 +87,13 @@ class PreProcessor implements PreProcessorInterface
     }
 
     /**
-     * Replace callback for preg_replace_callback function
+     * Replace callback for preg_replace_callback function.
      *
      * @param array $matches
      * @return string
      */
     protected function replaceCallback($matches)
     {
-        return '"' . __($matches[1]) . '"';
+        return '\'' . __($matches['translate']) . '\'';
     }
 }
