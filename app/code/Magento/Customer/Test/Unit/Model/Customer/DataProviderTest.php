7d37b7836258811efc2184b42e45ed093ccd57a7
Hwashiang Yu <hwyu@adobe.com>
2019-06-17 13:27:15 -0500

    MC-17486: New Customer Attribute Options Issue
    - Resolved static test failures

--- app/code/Magento/Customer/Test/Unit/Model/Customer/DataProviderTest.php
+++ app/code/Magento/Customer/Test/Unit/Model/Customer/DataProviderTest.php
@@ -22,6 +22,7 @@ use Magento\Ui\DataProvider\EavValidationRules;
  * Test for class \Magento\Customer\Model\Customer\DataProvider
  *
  * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+ * @SuppressWarnings(PHPMD.ExcessiveClassComplexity)
  */
 class DataProviderTest extends \PHPUnit\Framework\TestCase
 {
