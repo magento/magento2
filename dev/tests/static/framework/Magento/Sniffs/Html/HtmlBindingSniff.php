478360de8707789f0f8dfef1755211320707ab2c
Oleksandr Gorkun <ogorkun@magento.com>
2019-06-19 14:32:35 -0500

    MC-17489: Require specific suffix for HTML binding

--- dev/tests/static/framework/Magento/Sniffs/Html/HtmlBindingSniff.php
+++ dev/tests/static/framework/Magento/Sniffs/Html/HtmlBindingSniff.php
@@ -9,6 +9,9 @@ namespace Magento\Sniffs\Html;
 use PHP_CodeSniffer\Sniffs\Sniff;
 use PHP_CodeSniffer\Files\File;
 
+/**
+ * Sniffing improper HTML bindings.
+ */
 class HtmlBindingSniff implements Sniff
 {
     /**
@@ -30,6 +33,7 @@ class HtmlBindingSniff implements Sniff
             $html = $phpcsFile->getTokensAsString($stackPtr, count($phpcsFile->getTokens()));
             $dom = new \DOMDocument();
             try {
+                //phpcs: ignore
                 @$dom->loadHTML($html);
                 $loaded = true;
             } catch (\Throwable $exception) {
@@ -41,13 +45,14 @@ class HtmlBindingSniff implements Sniff
                 $dataBindAttributes = $domXpath->query('//@*[name() = "data-bind"]');
                 foreach ($dataBindAttributes as $dataBindAttribute) {
                     $knockoutBinding = $dataBindAttribute->nodeValue;
-                    preg_match('/^(.+\s+)*?html\:\s*?([a-z0-9\.\_\(\)]+)/i', $knockoutBinding, $htmlBinding);
+                    preg_match('/^(.+\s*?)?html\s*?\:\s*?([a-z0-9\.\(\)\_]+)/ims', $knockoutBinding, $htmlBinding);
                     if ($htmlBinding && !preg_match('/UnsanitizedHtml[\(\)]*?$/', $htmlBinding[2])) {
                         $phpcsFile->addError(
-                            'Variables/functions used for HTML binding must have UnsanitizedHtml suffix, '
+                            'Variables/functions used for HTML binding must have UnsanitizedHtml suffix'
+                            .' - "' .$htmlBinding[2] .'" doesn\'t,' .PHP_EOL
                             .'consider using text binding if the value is supposed to be text',
                             null,
-                            'UIComponentTemplate.HtmlSuffix'
+                            'UIComponentTemplate.KnockoutBinding.HtmlSuffix'
                         );
                     }
                 }
