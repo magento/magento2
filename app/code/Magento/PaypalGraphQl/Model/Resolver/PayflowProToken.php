1bfc5e28d0158c15bd826d288b71f911b71182d3
Anusha Vattam <avattam@adobe.com>
2019-06-21 14:10:57 -0500

    MC-17405: Add resolver for Pay Flow Pro support
    - added fixes for review comments for the resolver

--- app/code/Magento/PaypalGraphQl/Model/Resolver/PayflowProToken.php
+++ app/code/Magento/PaypalGraphQl/Model/Resolver/PayflowProToken.php
@@ -12,18 +12,12 @@ use Magento\Framework\GraphQl\Exception\GraphQlInputException;
 use Magento\Framework\GraphQl\Query\ResolverInterface;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
 use Magento\Framework\Url\Validator as UrlValidator;
-use Magento\Checkout\Helper\Data as CheckoutHelper;
-use Magento\PaypalGraphQl\Model\Provider\Checkout as CheckoutProvider;
-use Magento\Paypal\Model\Config as ConfigProvider;
 use Magento\QuoteGraphQl\Model\Cart\GetCartForUser;
-use Magento\Paypal\Controller\Transparent\RequestSecureToken as RequestSecureTokenHelper;
-use Magento\Framework\Session\SessionManager;
-use Magento\Framework\Session\SessionManagerInterface;
 use Magento\Paypal\Model\Payflow\Service\Request\SecureToken;
 use Magento\Framework\Exception\LocalizedException;
 
 /**
- * Resolver for generating Paypal token
+ * Resolver for generating PayflowProToken
  */
 class PayflowProToken implements ResolverInterface
 {
@@ -33,34 +27,9 @@ class PayflowProToken implements ResolverInterface
     private $getCartForUser;
 
     /**
-     * @var ConfigProvider
-     */
-    private $configProvider;
-
-    /**
-     * @var CheckoutProvider
-     */
-    private $checkoutProvider;
-
-    /**
      * @var UrlValidator
      */
     private $urlValidator;
-
-    /**
-     * @var CheckoutHelper
-     */
-    private $checkoutHelper;
-
-    /**
-     * @var RequestSecureTokenHelper
-     */
-    private $requestSecureTokenHelper;
-
-    /**
-     * @var SessionManager|SessionManagerInterface
-     */
-    private $sessionManager;
 
     /**
      * @var SecureToken
@@ -69,34 +38,16 @@ class PayflowProToken implements ResolverInterface
 
     /**
      * @param GetCartForUser $getCartForUser
-     * @param CheckoutProvider $checkoutProvider
-     * @param ConfigProvider $configProvider
      * @param UrlValidator $urlValidator
-     * @param CheckoutHelper $checkoutHelper
-     * @param SessionManager $sessionManager
-     * @param SessionManagerInterface|null $sessionInterface
-     * @param RequestSecureTokenHelper $requestSecureTokenHelper
      * @param SecureToken $secureTokenService
-     *
      */
     public function __construct(
         GetCartForUser $getCartForUser,
-        CheckoutProvider $checkoutProvider,
-        ConfigProvider $configProvider,
         UrlValidator $urlValidator,
-        CheckoutHelper $checkoutHelper,
-        SessionManager $sessionManager,
-        RequestSecureTokenHelper $requestSecureTokenHelper,
-        SecureToken $secureTokenService,
-        SessionManagerInterface $sessionInterface = null
+        SecureToken $secureTokenService
     ) {
         $this->getCartForUser = $getCartForUser;
-        $this->checkoutProvider = $checkoutProvider;
-        $this->configProvider = $configProvider;
         $this->urlValidator = $urlValidator;
-        $this->checkoutHelper = $checkoutHelper;
-        $this->requestSecureTokenHelper = $requestSecureTokenHelper;
-        $this->sessionManager = $sessionInterface ?: $sessionManager;
         $this->secureTokenService = $secureTokenService;
     }
 
@@ -111,6 +62,7 @@ class PayflowProToken implements ResolverInterface
         array $args = null
     ) {
         $cartId = $args['input']['cart_id'] ?? '';
+
         $customerId = $context->getUserId();
 
         $cart = $this->getCartForUser->execute($cartId, $customerId);
