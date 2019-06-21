de17471abf8a110c04e843629643cb31d1085861
Nathan Smith <nathsmit@adobe.com>
2019-06-21 11:27:19 -0500

    MC-16725: Prevent errors from incorrect import data

---
+++ app/code/Magento/CatalogImportExport/Test/Unit/Model/Import/Product/Validator/LayoutUpdateTest.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Magento\CatalogImportExport\Test\Unit\Model\Import\Product\Validator;
+
+use Magento\CatalogImportExport\Model\Import\Product;
+use Magento\CatalogImportExport\Model\Import\Product\Validator\LayoutUpdate;
+use Magento\Framework\Config\ValidationStateInterface;
+use Magento\Framework\View\Model\Layout\Update\Validator;
+use Magento\Framework\View\Model\Layout\Update\ValidatorFactory;
+use PHPUnit\Framework\TestCase;
+use PHPUnit_Framework_MockObject_MockObject as MockObject;
+
+/**
+ * Test validation for layout update
+ */
+class LayoutUpdateTest extends TestCase
+{
+    /**
+     * @var LayoutUpdate|MockObject
+     */
+    private $validator;
+
+    /**
+     * @var Validator|MockObject
+     */
+    private $layoutValidator;
+
+    protected function setUp()
+    {
+        $validatorFactory = $this->createMock(ValidatorFactory::class);
+        $validationState = $this->createMock(ValidationStateInterface::class);
+        $this->layoutValidator = $this->createMock(Validator::class);
+        $validatorFactory->method('create')
+            ->with(['validationState' => $validationState])
+            ->willReturn($this->layoutValidator);
+
+        $this->validator = new LayoutUpdate(
+            $validatorFactory,
+            $validationState
+        );
+    }
+
+    public function testValidationIsSkippedWithDataNotPresent()
+    {
+        $this->layoutValidator
+            ->expects($this->never())
+            ->method('isValid');
+
+        $result = $this->validator->isValid([]);
+        self::assertTrue($result);
+    }
+
+    public function testValidationFailsProperly()
+    {
+        $this->layoutValidator
+            ->method('isValid')
+            ->with('foo')
+            ->willReturn(false);
+
+        $contextMock = $this->createMock(Product::class);
+        $contextMock
+            ->method('retrieveMessageTemplate')
+            ->with('invalidLayoutUpdate')
+            ->willReturn('oh no');
+        $this->validator->init($contextMock);
+
+        $result = $this->validator->isValid(['custom_layout_update' => 'foo']);
+        $messages = $this->validator->getMessages();
+        self::assertFalse($result);
+        self::assertSame(['oh no'], $messages);
+    }
+
+    public function testInvalidDataException()
+    {
+        $this->layoutValidator
+            ->method('isValid')
+            ->willThrowException(new \Exception('foo'));
+
+        $contextMock = $this->createMock(Product::class);
+        $contextMock
+            ->method('retrieveMessageTemplate')
+            ->with('invalidLayoutUpdate')
+            ->willReturn('oh no');
+        $this->validator->init($contextMock);
+
+        $result = $this->validator->isValid(['custom_layout_update' => 'foo']);
+        $messages = $this->validator->getMessages();
+        self::assertFalse($result);
+        self::assertSame(['oh no'], $messages);
+    }
+}
