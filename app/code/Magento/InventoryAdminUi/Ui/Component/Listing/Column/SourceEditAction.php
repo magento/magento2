02b462ed197c01e4fe8b0a6afe57d6898bd642d0
Sachin Admane <sadmane@magento.com>
2019-06-21 15:15:47 -0500

    MC-16116: Incorrect inventory source

---
+++ app/code/Magento/InventoryAdminUi/Ui/Component/Listing/Column/SourceEditAction.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\InventoryAdminUi\Ui\Component\Listing\Column;
+
+use Magento\Framework\UrlInterface;
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
+use Magento\Ui\Component\Listing\Columns\Column;
+
+/**
+ * Represents Edit link in grid for entity by its identifier field
+ *
+ *
+ */
+class SourceEditAction extends Column
+{
+    /**
+     * @var UrlInterface
+     */
+    private $urlBuilder;
+
+    /**
+     * @param ContextInterface $context
+     * @param UiComponentFactory $uiComponentFactory
+     * @param UrlInterface $urlBuilder
+     * @param array $components
+     * @param array $data
+     */
+    public function __construct(
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        UrlInterface $urlBuilder,
+        array $components = [],
+        array $data = []
+    ) {
+        $this->urlBuilder = $urlBuilder;
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+    }
+
+    /**
+     *  Prepare Data source for Actions
+     *
+     * @param array $dataSource
+     * @return array
+     */
+    public function prepareDataSource(array $dataSource)
+    {
+        if (isset($dataSource['data']['items'])) {
+            foreach ($dataSource['data']['items'] as &$item) {
+                if (isset($item[$item['id_field_name']])) {
+                    $editUrlPath = $this->getData('config/editUrlPath') ?: '#';
+                    $item[$this->getData('name')] = [
+                        'edit' => [
+                            'href' => $this->urlBuilder->getUrl(
+                                $editUrlPath,
+                                [
+                                    $item['id_field_name'] => $item[$item['id_field_name']],
+                                ]
+                            ),
+                            'label' => __('Edit'),
+                            '__disableTmpl' => true
+                        ]
+                    ];
+                    unset($item);
+                }
+            }
+        }
+        return $dataSource;
+    }
+}
