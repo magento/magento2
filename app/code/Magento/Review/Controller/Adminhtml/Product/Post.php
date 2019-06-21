78af5b8a06033a5d969cb820de7b54006fc9bd38
Danilo Argentiero <danilo.argentiero@magespecialist.it>
2019-06-21 11:57:24 +0200

    Get review entity id by code instead hard-coded.

--- app/code/Magento/Review/Controller/Adminhtml/Product/Post.php
+++ app/code/Magento/Review/Controller/Adminhtml/Product/Post.php
@@ -10,6 +10,7 @@ use Magento\Review\Controller\Adminhtml\Product as ProductController;
 use Magento\Framework\Controller\ResultFactory;
 use Magento\Store\Model\Store;
 use Magento\Framework\Exception\LocalizedException;
+use Magento\Review\Model\Review;
 
 class Post extends ProductController implements HttpPostActionInterface
 {
@@ -33,7 +34,7 @@ class Post extends ProductController implements HttpPostActionInterface
             }
             $review = $this->reviewFactory->create()->setData($data);
             try {
-                $review->setEntityId(1) // product
+                $review->setEntityId($review->getEntityIdByCode(Review::ENTITY_PRODUCT_CODE))
                     ->setEntityPkValue($productId)
                     ->setStoreId(Store::DEFAULT_STORE_ID)
                     ->setStatusId($data['status_id'])
