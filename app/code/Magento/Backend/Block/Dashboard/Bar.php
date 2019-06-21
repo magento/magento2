fff2c0d72a6e19ad873bb7b86758416e9b9860c3
rostyslav.hymon <rostyslav.hymon@transoftgroup.com>
2019-06-21 09:12:19 +0300

    MAGETWO-99719: Dasboard displays incorrect currency codes if default base currency differs from default website currency

--- app/code/Magento/Backend/Block/Dashboard/Bar.php
+++ app/code/Magento/Backend/Block/Dashboard/Bar.php
@@ -75,6 +75,7 @@ class Bar extends \Magento\Backend\Block\Dashboard\AbstractDashboard
      * Retrieve currency model if not set then return currency model for current store
      *
      * @return \Magento\Directory\Model\Currency
+     * @SuppressWarnings(PHPMD.RequestAwareBlockMethod)
      */
     public function getCurrency()
     {
@@ -92,7 +93,8 @@ class Bar extends \Magento\Backend\Block\Dashboard\AbstractDashboard
                     $this->getRequest()->getParam('group')
                 )->getWebsite()->getBaseCurrency();
             } else {
-                $this->_currentCurrencyCode = $this->_storeManager->getStore(Store::DEFAULT_STORE_ID)->getBaseCurrency();
+                $this->_currentCurrencyCode = $this->_storeManager->getStore(Store::DEFAULT_STORE_ID)
+                    ->getBaseCurrency();
             }
         }
 
