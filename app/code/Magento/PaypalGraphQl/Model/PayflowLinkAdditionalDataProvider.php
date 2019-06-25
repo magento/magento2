9433a6c7a906de09865dd6dcd16e8c51d3e5d36b
Daniel Renaud <drenaud@magento.com>
2019-06-25 11:30:36 -0500

    MC-17118: PayPal :: Payflow link support

--- app/code/Magento/PaypalGraphQl/Model/PayflowLinkAdditionalDataProvider.php
+++ app/code/Magento/PaypalGraphQl/Model/PayflowLinkAdditionalDataProvider.php
@@ -7,6 +7,8 @@ declare(strict_types=1);
 
 namespace Magento\PaypalGraphQl\Model;
 
+use Magento\Framework\GraphQl\Exception\GraphQlInputException;
+use Magento\Framework\Url\Validator as UrlValidator;
 use Magento\Paypal\Model\Config;
 use Magento\QuoteGraphQl\Model\Cart\Payment\AdditionalDataProviderInterface;
 
@@ -16,13 +18,50 @@ use Magento\QuoteGraphQl\Model\Cart\Payment\AdditionalDataProviderInterface;
 class PayflowLinkAdditionalDataProvider implements AdditionalDataProviderInterface
 {
     /**
+     * @var UrlValidator
+     */
+    private $urlValidator;
+
+    /**
+     * @param UrlValidator $urlValidator
+     */
+    public function __construct(UrlValidator $urlValidator)
+    {
+        $this->urlValidator = $urlValidator;
+    }
+
+    /**
      * Returns additional data
      *
      * @param array $data
      * @return array
+     * @throws GraphQlInputException
      */
     public function getData(array $data): array
     {
-        return $data[Config::METHOD_PAYFLOWLINK] ?? [];
+        $additionalData = $data[Config::METHOD_PAYFLOWLINK] ?? [];
+        $this->validateUrls($additionalData);
+
+        return $additionalData;
+    }
+
+    /**
+     * Validate redirect urls
+     *
+     * @param array $data
+     * @throws GraphQlInputException
+     */
+    private function validateUrls(array $data): void
+    {
+        $urlKeys = ['cancel_url', 'return_url', 'error_url'];
+
+        foreach ($urlKeys as $urlKey) {
+            if (isset($data[$urlKey])) {
+                if (!$this->urlValidator->isValid($data[$urlKey])) {
+                    $errorMessage = $this->urlValidator->getMessages()['invalidUrl'] ?? "Invalid Url.";
+                    throw new GraphQlInputException(__($errorMessage));
+                }
+            }
+        }
     }
 }
