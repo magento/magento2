fb04efb994105bd360ff008a142e7208c97f9842
Tan Sezer <t.sezer@youwe.nl>
2019-06-25 16:46:05 +0200

    If the condition is true, return the variable to itself otherwise it was returning condition result

--- app/code/Magento/Config/Model/Config/Backend/Baseurl.php
+++ app/code/Magento/Config/Model/Config/Backend/Baseurl.php
@@ -57,7 +57,7 @@ class Baseurl extends \Magento\Framework\App\Config\Value
     public function beforeSave()
     {
         $value = $this->getValue();
-        $value = $value{strlen($value) - 1} === '/' ?: $value . '/';
+        $value = $value{strlen($value) - 1} === '/' ? $value : $value . '/';
         try {
             if (!$this->_validateUnsecure($value) && !$this->_validateSecure($value)) {
                 $this->_validateFullyQualifiedUrl($value);
