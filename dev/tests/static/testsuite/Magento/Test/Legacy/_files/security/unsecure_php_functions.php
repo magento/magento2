868b9cd062947a8d7913431773457c0886124046
Raoul Rego <rrego@adobe.com>
2019-06-26 10:58:27 -0500

    MC-15603: Merge release branch into 2.2-develop
    - Manually added changes from 2.2

--- dev/tests/static/testsuite/Magento/Test/Legacy/_files/security/unsecure_php_functions.php
+++ dev/tests/static/testsuite/Magento/Test/Legacy/_files/security/unsecure_php_functions.php
@@ -57,7 +57,22 @@ return [
                 'type' => 'module',
                 'name' => 'Magento_Catalog',
                 'path' => 'view/adminhtml/templates/catalog/product/edit/serializer.phtml'
-            ]
+            ],
+            [
+                'type' => 'library',
+                'name' => 'magento/framework',
+                'path' => 'App/Utility/Files.php'
+            ],
+            [
+                'type' => 'module',
+                'name' => 'Magento_Support',
+                'path' => 'Console/Command/AbstractBackupDumpCommand.php'
+            ],
+            [
+                'type' => 'module',
+                'name' => 'Magento_Authorizenet',
+                'path' => 'Model/Directpost/Response.php',
+            ],
         ],
     ],
     'srand' => [
@@ -66,6 +81,48 @@ return [
     ],
     'mt_srand' => [
         'replacement' => '',
-        'exclude' => []
+        'exclude' => [
+            [
+                'type' => 'setup',
+                'path' => 'src/Magento/Setup/Fixtures/ConfigurableProductsFixture.php'
+            ],
+            [
+                'type' => 'setup',
+                'path' => 'src/Magento/Setup/Fixtures/PriceProvider.php'
+            ],
+            [
+                'type' => 'setup',
+                'path' => 'src/Magento/Setup/Fixtures/SimpleProductsFixture.php'
+            ],
+        ]
+    ],
+    'mt_rand' => [
+        'replacement' => 'random_int',
+        'exclude' => [
+            [
+                'type' => 'setup',
+                'path' => 'src/Magento/Setup/Fixtures/ImagesGenerator/ImagesGenerator.php'
+            ],
+            [
+                'type' => 'setup',
+                'path' => 'src/Magento/Setup/Fixtures/BundleProductsFixture.php'
+            ],
+            [
+                'type' => 'setup',
+                'path' => 'src/Magento/Setup/Fixtures/ConfigurableProductsFixture.php'
+            ],
+            [
+                'type' => 'setup',
+                'path' => 'src/Magento/Setup/Fixtures/OrdersFixture.php'
+            ],
+            [
+                'type' => 'setup',
+                'path' => 'src/Magento/Setup/Fixtures/PriceProvider.php'
+            ],
+            [
+                'type' => 'setup',
+                'path' => 'src/Magento/Setup/Fixtures/SimpleProductsFixture.php'
+            ],
+        ]
     ],
 ];
