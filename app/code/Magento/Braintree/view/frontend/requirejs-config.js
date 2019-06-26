faf6d59bb01b029cda71a4b8859de2084836e991
Viktor Tymchynskyi <vtymchynskyi@magento.com>
2019-06-26 15:16:15 -0500

    MAGETWO-99867: Braintree - 3D Secure 2.0 Support for 2.3

--- app/code/Magento/Braintree/view/frontend/requirejs-config.js
+++ app/code/Magento/Braintree/view/frontend/requirejs-config.js
@@ -6,11 +6,11 @@
 var config = {
     map: {
         '*': {
-            braintreeClient: 'https://js.braintreegateway.com/web/3.44.1/js/client.min.js',
-            braintreeHostedFields: 'https://js.braintreegateway.com/web/3.44.1/js/hosted-fields.min.js',
-            braintreePayPal: 'https://js.braintreegateway.com/web/3.44.1/js/paypal-checkout.min.js',
-            braintree3DSecure: 'https://js.braintreegateway.com/web/3.44.1/js/three-d-secure.min.js',
-            braintreeDataCollector: 'https://js.braintreegateway.com/web/3.44.1/js/data-collector.min.js'
+            braintreeClient: 'https://js.braintreegateway.com/web/3.46.0-beta-3ds.8/js/client.min.js',
+            braintreeHostedFields: 'https://js.braintreegateway.com/web/3.46.0-beta-3ds.8/js/hosted-fields.min.js',
+            braintreePayPal: 'https://js.braintreegateway.com/web/3.46.0-beta-3ds.8/js/paypal-checkout.min.js',
+            braintree3DSecure: 'https://js.braintreegateway.com/web/3.46.0-beta-3ds.8/js/three-d-secure.min.js',
+            braintreeDataCollector: 'https://js.braintreegateway.com/web/3.46.0-beta-3ds.8/js/data-collector.min.js'
         }
     },
     paths: {
