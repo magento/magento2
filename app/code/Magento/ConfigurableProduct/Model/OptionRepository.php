ab294f5cda8980d8a589c2fb8fb8ffcb072a3c0d
Lieven Pouwelse <l.pouwelse@xsarus.nl>
2019-06-28 21:27:30 +0200

    Fix code formatting issues

--- app/code/Magento/ConfigurableProduct/Model/OptionRepository.php
+++ app/code/Magento/ConfigurableProduct/Model/OptionRepository.php
@@ -22,6 +22,7 @@ use Magento\Framework\EntityManager\MetadataPool;
 use Magento\Store\Model\Store;
 
 /**
+ * Repository for performing CRUD operations for a configurable product's options.
  * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
  */
 class OptionRepository implements \Magento\ConfigurableProduct\Api\OptionRepositoryInterface
@@ -112,7 +113,7 @@ class OptionRepository implements \Magento\ConfigurableProduct\Api\OptionReposit
     }
 
     /**
-     * {@inheritdoc}
+     * @inheritdoc
      */
     public function get($sku, $id)
     {
@@ -131,7 +132,7 @@ class OptionRepository implements \Magento\ConfigurableProduct\Api\OptionReposit
     }
 
     /**
-     * {@inheritdoc}
+     * @inheritdoc
      */
     public function getList($sku)
     {
@@ -141,7 +142,7 @@ class OptionRepository implements \Magento\ConfigurableProduct\Api\OptionReposit
     }
 
     /**
-     * {@inheritdoc}
+     * @inheritdoc}
      */
     public function delete(OptionInterface $option)
     {
@@ -167,7 +168,7 @@ class OptionRepository implements \Magento\ConfigurableProduct\Api\OptionReposit
     }
 
     /**
-     * {@inheritdoc}
+     * @inheritdoc
      */
     public function deleteById($sku, $id)
     {
@@ -184,7 +185,7 @@ class OptionRepository implements \Magento\ConfigurableProduct\Api\OptionReposit
     }
 
     /**
-     * {@inheritdoc}
+     * @inheritdoc
      * @SuppressWarnings(PHPMD.CyclomaticComplexity)
      */
     public function save($sku, OptionInterface $option)
@@ -297,6 +298,7 @@ class OptionRepository implements \Magento\ConfigurableProduct\Api\OptionReposit
 
     /**
      * Get MetadataPool instance
+     *
      * @return MetadataPool
      */
     private function getMetadataPool()
