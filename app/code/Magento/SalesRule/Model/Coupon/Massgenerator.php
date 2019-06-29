a404d333f43093b5ee93b061f51e480183325b57
praful-rajput <rajput.praful@gmail.com>
2019-06-29 19:12:22 +0530

    Fixed - Generate coupon code with code length.

--- app/code/Magento/SalesRule/Model/Coupon/Massgenerator.php
+++ app/code/Magento/SalesRule/Model/Coupon/Massgenerator.php
@@ -245,4 +245,16 @@ class Massgenerator extends \Magento\Framework\Model\AbstractModel implements
     {
         return $this->generatedCount;
     }
+    
+    /**
+     * Calculate Maximum Probability based current coupon generate request
+     * 
+     * @return int
+     */
+    public function getMaxProbability()
+    {
+        $chars = count($this->salesRuleCoupon->getCharset($this->getFormat()));
+        $length = (int)$this->getLength();
+        return (pow($chars, $length) - 1) / pow($chars, $length);
+    }
 }
