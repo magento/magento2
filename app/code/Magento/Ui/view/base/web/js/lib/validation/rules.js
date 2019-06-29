54ae63aa73dfd0cb6ee267cb0fdfc0670428cebd
vrutik.suthar <vrutiksuthar@gmail.com>
2019-06-29 17:41:57 +0530

    #22692 updated static test errors

--- app/code/Magento/Ui/view/base/web/js/lib/validation/rules.js
+++ app/code/Magento/Ui/view/base/web/js/lib/validation/rules.js
@@ -1054,9 +1054,12 @@ define([
         ],
         'validate-future-date': [
             function (value, params) {
-                var currentDate = new Date().toDateString();
-                var insertedDate = new Date(value).toDateString();
-                return moment.utc(currentDate, params.dateFormat).unix() > moment.utc(insertedDate, params.dateFormat).unix();
+                var currentDate = new Date().toDateString(),
+                    insertedDate = new Date(value).toDateString(),
+                    currentDateUnix = moment.utc(currentDate, params.dateFormat).unix(),
+                    insertedDateUnix = moment.utc(insertedDate, params.dateFormat).unix();
+
+                return insertedDate > insertedDateUnix;
             },
             $.mage.__('The date should be earlier than the current date.')
         ],
