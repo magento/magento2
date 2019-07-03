2f792144542dff772357e9bc974b47b901fcb7b2
rostyslav.hymon <rostyslav.hymon@transoftgroup.com>
2019-07-03 12:49:47 +0300

    MC-17919: In stock products are showing as out of stock after they are enabled via a Scheduled Update

--- dev/tests/integration/testsuite/Magento/CatalogInventory/Model/Indexer/Stock/Action/FullTest.php
+++ dev/tests/integration/testsuite/Magento/CatalogInventory/Model/Indexer/Stock/Action/FullTest.php
@@ -5,24 +5,42 @@
  */
 namespace Magento\CatalogInventory\Model\Indexer\Stock\Action;
 
+use Magento\TestFramework\Helper\Bootstrap;
+use Magento\Framework\ObjectManagerInterface;
+use Magento\CatalogInventory\Model\Indexer\Stock\Processor;
+use Magento\Catalog\Model\CategoryFactory;
+use Magento\Catalog\Block\Product\ListProduct;
+use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory;
+use Magento\Catalog\Model\Product;
+use PHPUnit\Framework\TestCase;
+
 /**
  * Full reindex Test
  */
-class FullTest extends \PHPUnit\Framework\TestCase
+class FullTest extends TestCase
 {
     /**
-     * @var \Magento\CatalogInventory\Model\Indexer\Stock\Processor
+     * @var ObjectManagerInterface
+     */
+    private $objectManager;
+
+    /**
+     * @var Processor
      */
     protected $_processor;
 
+    /**
+     * @inheritdoc
+     */
     protected function setUp()
     {
-        $this->_processor = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->get(
-            \Magento\CatalogInventory\Model\Indexer\Stock\Processor::class
-        );
+        $this->objectManager = Bootstrap::getObjectManager();
+        $this->_processor = $this->objectManager->get(Processor::class);
     }
 
     /**
+     * Reindex all
+     *
      * @magentoDbIsolation disabled
      * @magentoAppIsolation enabled
      * @magentoDataFixture Magento/Catalog/_files/product_simple.php
@@ -31,13 +49,9 @@ class FullTest extends \PHPUnit\Framework\TestCase
     {
         $this->_processor->reindexAll();
 
-        $categoryFactory = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->get(
-            \Magento\Catalog\Model\CategoryFactory::class
-        );
-        /** @var \Magento\Catalog\Block\Product\ListProduct $listProduct */
-        $listProduct = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->get(
-            \Magento\Catalog\Block\Product\ListProduct::class
-        );
+        $categoryFactory = $this->objectManager->get(CategoryFactory::class);
+        /** @var ListProduct $listProduct */
+        $listProduct = $this->objectManager->get(ListProduct::class);
 
         $category = $categoryFactory->create()->load(2);
         $layer = $listProduct->getLayer();
@@ -59,6 +73,39 @@ class FullTest extends \PHPUnit\Framework\TestCase
             $this->assertEquals('Simple Product', $product->getName());
             $this->assertEquals('Short description', $product->getShortDescription());
             $this->assertEquals(100, $product->getQty());
+        }
+    }
+
+    /**
+     * Reindex with disabled product
+     *
+     * @return void
+     * @magentoDbIsolation disabled
+     * @magentoAppIsolation enabled
+     * @magentoDataFixture Magento/Catalog/_files/products_with_layered_navigation_attribute.php
+     */
+    public function testReindexAllWithDisabledProduct(): void
+    {
+        $productCollectionFactory = $this->objectManager->get(CollectionFactory::class);
+        $productCollection = $productCollectionFactory
+            ->create()
+            ->addAttributeToSelect('*')
+            ->addAttributeToFilter('sku', ['eq' => 'simple3'])
+            ->addAttributeToSort('created_at', 'DESC')
+            ->joinField(
+                'stock_status',
+                'cataloginventory_stock_status',
+                'stock_status',
+                'product_id=entity_id',
+                '{{table}}.stock_id=1',
+                'left'
+            )->load();
+
+        $this->assertCount(1, $productCollection);
+
+        /** @var Product $product */
+        foreach ($productCollection as $product) {
+            $this->assertEquals(1, $product->getData('stock_status'));
         }
     }
 }
