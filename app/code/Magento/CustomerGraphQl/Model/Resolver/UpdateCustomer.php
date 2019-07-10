02abb7352c616a3ef87ba89da6d0e21abfa9cf0d
Yaroslav Rogoza <enarc@atwix.com>
2019-07-10 11:04:00 +0200

    Failing static tests fix

--- app/code/Magento/CustomerGraphQl/Model/Resolver/UpdateCustomer.php
+++ app/code/Magento/CustomerGraphQl/Model/Resolver/UpdateCustomer.php
@@ -74,7 +74,8 @@ class UpdateCustomer implements ResolverInterface
         $customer = $this->getCustomer->execute($context);
         $this->updateCustomerAccount->execute(
             $customer,
-            $args['input'], $context->getExtensionAttributes()->getStore()
+            $args['input'],
+            $context->getExtensionAttributes()->getStore()
         );
 
         $data = $this->extractCustomerData->execute($customer);
