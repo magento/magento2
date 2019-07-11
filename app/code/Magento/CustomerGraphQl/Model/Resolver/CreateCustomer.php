51b433c519ad304225a8c952fbcc30c9c32a9720
Sergey Dovbenko <sdovbenko@magecom.us>
2019-07-11 21:31:59 +0000

    Moved logic from plugin to CreateCustomer

--- app/code/Magento/CustomerGraphQl/Model/Resolver/CreateCustomer.php
+++ app/code/Magento/CustomerGraphQl/Model/Resolver/CreateCustomer.php
@@ -13,12 +13,24 @@ use Magento\Framework\GraphQl\Config\Element\Field;
 use Magento\Framework\GraphQl\Exception\GraphQlInputException;
 use Magento\Framework\GraphQl\Query\ResolverInterface;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Magento\Store\Model\ScopeInterface;
+use Magento\Framework\App\Config\ScopeConfigInterface;
 
 /**
  * Create customer account resolver
  */
 class CreateCustomer implements ResolverInterface
 {
+    /**
+     * Configuration path to newsletter active setting
+     */
+    const XML_PATH_NEWSLETTER_ACTIVE = 'newsletter/general/active';
+
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
     /**
      * @var ExtractCustomerData
      */
@@ -30,13 +42,18 @@ class CreateCustomer implements ResolverInterface
     private $createCustomerAccount;
 
     /**
+     * CreateCustomer constructor.
+     *
      * @param ExtractCustomerData $extractCustomerData
      * @param CreateCustomerAccount $createCustomerAccount
+     * @param ScopeConfigInterface $scopeConfig
      */
     public function __construct(
         ExtractCustomerData $extractCustomerData,
-        CreateCustomerAccount $createCustomerAccount
+        CreateCustomerAccount $createCustomerAccount,
+        ScopeConfigInterface $scopeConfig
     ) {
+        $this->scopeConfig = $scopeConfig;
         $this->extractCustomerData = $extractCustomerData;
         $this->createCustomerAccount = $createCustomerAccount;
     }
@@ -53,6 +70,14 @@ class CreateCustomer implements ResolverInterface
     ) {
         if (empty($args['input']) || !is_array($args['input'])) {
             throw new GraphQlInputException(__('"input" value should be specified'));
+        }
+
+        if (!$this->scopeConfig->getValue(
+            self::XML_PATH_NEWSLETTER_ACTIVE,
+            ScopeInterface::SCOPE_STORE
+        )
+        ) {
+            $args['input']['is_subscribed'] = false;
         }
 
         $customer = $this->createCustomerAccount->execute($args['input']);
