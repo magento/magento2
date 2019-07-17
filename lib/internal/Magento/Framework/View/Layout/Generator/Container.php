a40815ebf0a35cd0cf6efbcb7c63c8645520ad4c
Francis Gallagher <francis917@gmail.com>
2019-07-17 12:01:49 +0100

    Add Header (h1>h6) tags to layout htmlTags Allowed types

--- lib/internal/Magento/Framework/View/Layout/Generator/Container.php
+++ lib/internal/Magento/Framework/View/Layout/Generator/Container.php
@@ -39,6 +39,12 @@ class Container implements Layout\GeneratorInterface
         'table',
         'tfoot',
         'ul',
+        'h1',
+        'h2',
+        'h3',
+        'h4',
+        'h5',
+        'h6',
     ];
 
     /**
