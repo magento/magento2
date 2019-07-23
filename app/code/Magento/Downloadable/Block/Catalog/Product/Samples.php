6a31ed37d7738ccac25b6f66bd28be502c681fd5
OlgaVasyltsun <olga.vasyltsun@gmail.com>
2019-07-23 10:48:52 +0300

    MC-18210: Finalize @codingStandardsIgnoreFile elimination in CE

--- app/code/Magento/Downloadable/Block/Catalog/Product/Samples.php
+++ app/code/Magento/Downloadable/Block/Catalog/Product/Samples.php
@@ -4,8 +4,6 @@
  * See COPYING.txt for license details.
  */
 
-// @codingStandardsIgnoreFile
-
 namespace Magento\Downloadable\Block\Catalog\Product;
 
 use Magento\Downloadable\Model\ResourceModel\Sample\Collection as SampleCollection;
@@ -58,7 +56,11 @@ class Samples extends \Magento\Catalog\Block\Product\AbstractProduct
         if ($this->getProduct()->getSamplesTitle()) {
             return $this->getProduct()->getSamplesTitle();
         }
-        return $this->_scopeConfig->getValue(\Magento\Downloadable\Model\Sample::XML_PATH_SAMPLES_TITLE, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+
+        return $this->_scopeConfig->getValue(
+            \Magento\Downloadable\Model\Sample::XML_PATH_SAMPLES_TITLE,
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+        );
     }
 
     /**
@@ -69,6 +71,9 @@ class Samples extends \Magento\Catalog\Block\Product\AbstractProduct
      */
     public function getIsOpenInNewWindow()
     {
-        return $this->_scopeConfig->isSetFlag(\Magento\Downloadable\Model\Link::XML_PATH_TARGET_NEW_WINDOW, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+        return $this->_scopeConfig->isSetFlag(
+            \Magento\Downloadable\Model\Link::XML_PATH_TARGET_NEW_WINDOW,
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+        );
     }
 }
--- app/code/Magento/Newsletter/Block/Adminhtml/Template/Edit.php
+++ app/code/Magento/Newsletter/Block/Adminhtml/Template/Edit.php
@@ -4,8 +4,6 @@
  * See COPYING.txt for license details.
  */
 
-// @codingStandardsIgnoreFile
-
 /**
  * Newsletter Template Edit Block
  *
@@ -207,7 +205,7 @@ class Edit extends Widget
     public function getForm()
     {
         return $this->getLayout()->createBlock(
-             \Magento\Newsletter\Block\Adminhtml\Template\Edit\Form::class
+            \Magento\Newsletter\Block\Adminhtml\Template\Edit\Form::class
         )->toHtml();
     }
 
