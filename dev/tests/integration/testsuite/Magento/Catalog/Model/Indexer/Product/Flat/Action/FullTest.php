1172c2ea2aab95677ab2fd7508e1ab12aac3aada
Andrii Meysar <andrii.meysar@transoftgroup.com>
2019-07-23 16:19:35 +0300

    MC-18327: [backport 2.2] 2.3.2 - Product Flat Data index taking long time to reindex?

--- dev/tests/integration/testsuite/Magento/Catalog/Model/Indexer/Product/Flat/Action/FullTest.php
+++ dev/tests/integration/testsuite/Magento/Catalog/Model/Indexer/Product/Flat/Action/FullTest.php
@@ -5,29 +5,44 @@
  */
 namespace Magento\Catalog\Model\Indexer\Product\Flat\Action;
 
+use Magento\Catalog\Api\Data\ProductInterface;
+use Magento\Catalog\Block\Product\ListProduct;
+use Magento\Catalog\Model\CategoryFactory;
+use Magento\Catalog\Model\Indexer\Product\Flat\Processor;
+use Magento\Catalog\Model\Indexer\Product\Flat\State;
+use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory as ProductCollectionFactory;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\TestFramework\Helper\Bootstrap;
+use Magento\TestFramework\ObjectManager;
+
 /**
  * Full reindex Test
  */
 class FullTest extends \Magento\TestFramework\Indexer\TestCase
 {
     /**
-     * @var \Magento\Catalog\Model\Indexer\Product\Flat\State
+     * @var State
      */
     protected $_state;
 
     /**
-     * @var \Magento\Catalog\Model\Indexer\Product\Flat\Processor
+     * @var Processor
      */
     protected $_processor;
 
+    /**
+     * @var ObjectManager
+     */
+    private $objectManager;
+
+    /**
+     * @inheritdoc
+     */
     protected function setUp()
     {
-        $this->_state = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->get(
-            \Magento\Catalog\Model\Indexer\Product\Flat\State::class
-        );
-        $this->_processor = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->get(
-            \Magento\Catalog\Model\Indexer\Product\Flat\Processor::class
-        );
+        $this->objectManager = Bootstrap::getObjectManager();
+        $this->_state = $this->objectManager->get(State::class);
+        $this->_processor = $this->objectManager->get(Processor::class);
     }
 
     /**
@@ -41,12 +56,8 @@ class FullTest extends \Magento\TestFramework\Indexer\TestCase
         $this->assertTrue($this->_state->isFlatEnabled());
         $this->_processor->reindexAll();
 
-        $categoryFactory = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->get(
-            \Magento\Catalog\Model\CategoryFactory::class
-        );
-        $listProduct = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->get(
-            \Magento\Catalog\Block\Product\ListProduct::class
-        );
+        $categoryFactory = $this->objectManager->get(CategoryFactory::class);
+        $listProduct = $this->objectManager->get(ListProduct::class);
 
         $category = $categoryFactory->create()->load(2);
         $layer = $listProduct->getLayer();
@@ -59,6 +70,51 @@ class FullTest extends \Magento\TestFramework\Indexer\TestCase
         foreach ($productCollection as $product) {
             $this->assertEquals('Simple Product', $product->getName());
             $this->assertEquals('Short description', $product->getShortDescription());
+        }
+    }
+
+    /**
+     * @magentoAppArea frontend
+     * @magentoDbIsolation disabled
+     * @magentoAppIsolation enabled
+     * @magentoDataFixture Magento/Catalog/_files/product_simple_multistore.php
+     * @magentoDataFixture Magento/Catalog/_files/enable_catalog_product_flat_indexer.php
+     */
+    public function testReindexAllMultipleStores()
+    {
+        $this->_state = $this->objectManager->create(State::class);
+        $this->_processor = $this->objectManager->create(Processor::class);
+
+        $this->assertTrue($this->_state->isFlatEnabled());
+        $this->_processor->reindexAll();
+
+        /** @var ProductCollectionFactory $productCollectionFactory */
+        $productCollectionFactory = $this->objectManager->create(ProductCollectionFactory::class);
+        /** @var StoreManagerInterface $storeManager */
+        $storeManager = $this->objectManager->get(StoreManagerInterface::class);
+        $store = $storeManager->getStore('fixturestore');
+
+        $expectedData = [
+            $storeManager->getDefaultStoreView()->getId() => 'Simple Product One',
+            $store->getId() => 'StoreTitle',
+        ];
+
+        foreach ($expectedData as $storeId => $productName) {
+            $storeManager->setCurrentStore($storeId);
+            $productCollection = $productCollectionFactory->create();
+
+            $this->assertTrue(
+                $productCollection->isEnabledFlat(),
+                'Flat should be enabled for product collection.'
+            );
+
+            $productCollection->addIdFilter(1)->addAttributeToSelect(ProductInterface::NAME);
+
+            $this->assertEquals(
+                $productName,
+                $productCollection->getFirstItem()->getName(),
+                'Wrong product name specified per store.'
+            );
         }
     }
 }
