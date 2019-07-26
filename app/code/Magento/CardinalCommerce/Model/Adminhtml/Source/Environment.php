f8ec66e7d08dec2b2a39207adc5080a269c2b42f
rostyslav.hymon <rostyslav.hymon@transoftgroup.com>
2019-07-26 12:07:27 +0300

    MAGETWO-99737: Authorize.net - 3D Secure 2.0 Support for 2.2

---
+++ app/code/Magento/CardinalCommerce/Model/Adminhtml/Source/Environment.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\CardinalCommerce\Model\Adminhtml\Source;
+
+/**
+ * CardinalCommerce Environment Dropdown source
+ */
+class Environment implements \Magento\Framework\Data\OptionSourceInterface
+{
+    const ENVIRONMENT_PRODUCTION = 'production';
+    const ENVIRONMENT_SANDBOX = 'sandbox';
+
+    /**
+     * Possible environment types
+     *
+     * @return array
+     */
+    public function toOptionArray(): array
+    {
+        return [
+            [
+                'value' => self::ENVIRONMENT_SANDBOX,
+                'label' => 'Sandbox',
+            ],
+            [
+                'value' => self::ENVIRONMENT_PRODUCTION,
+                'label' => 'Production'
+            ]
+        ];
+    }
+}
