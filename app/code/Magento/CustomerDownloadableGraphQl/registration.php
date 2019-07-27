3a403eabe8f87a009bd615a666ca01724c4d1b9d
Dusan Lukic <ldusan84@gmail.com>
2019-07-27 18:47:04 +0200

    Fixed some naming issues

--- app/code/Magento/CustomerDownloadableGraphQl/registration.php
+++ app/code/Magento/CustomerDownloadableGraphQl/registration.php
@@ -6,4 +6,4 @@
 
 use Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Magento_DownloadableGraphQl', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Magento_CustomerDownloadableGraphQl', __DIR__);
