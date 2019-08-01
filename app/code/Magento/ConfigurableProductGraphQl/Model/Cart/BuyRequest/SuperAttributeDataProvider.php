702e6d85e3f5a0c1023025f656f33155472b401d
Lena Orobei <oorobei@magento.com>
2019-08-01 14:14:31 -0500

    magento/graphql-ce#438: Adjust adding configurable products to the shopping cart

--- app/code/Magento/ConfigurableProductGraphQl/Model/Cart/BuyRequest/SuperAttributeDataProvider.php
+++ app/code/Magento/ConfigurableProductGraphQl/Model/Cart/BuyRequest/SuperAttributeDataProvider.php
@@ -7,12 +7,14 @@ declare(strict_types=1);
 
 namespace Magento\ConfigurableProductGraphQl\Model\Cart\BuyRequest;
 
+use Magento\Catalog\Api\Data\ProductInterface;
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\GraphQl\Exception\GraphQlNoSuchEntityException;
 use Magento\Framework\Stdlib\ArrayManager;
 use Magento\QuoteGraphQl\Model\Cart\BuyRequest\BuyRequestDataProviderInterface;
 use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\ConfigurableProductGraphQl\Model\Options\Collection as OptionCollection;
+use Magento\Framework\EntityManager\MetadataPool;
 
 /**
  * DataProvider for building super attribute options in buy requests
@@ -35,19 +37,26 @@ class SuperAttributeDataProvider implements BuyRequestDataProviderInterface
     private $optionCollection;
 
     /**
-     * SuperAttributeDataProvider constructor.
+     * @var MetadataPool
+     */
+    private $metadataPool;
+
+    /**
      * @param ArrayManager $arrayManager
      * @param ProductRepositoryInterface $productRepository
      * @param OptionCollection $optionCollection
+     * @param MetadataPool $metadataPool
      */
     public function __construct(
         ArrayManager $arrayManager,
         ProductRepositoryInterface $productRepository,
-        OptionCollection $optionCollection
+        OptionCollection $optionCollection,
+        MetadataPool $metadataPool
     ) {
         $this->arrayManager = $arrayManager;
         $this->productRepository = $productRepository;
         $this->optionCollection = $optionCollection;
+        $this->metadataPool = $metadataPool;
     }
 
     /**
@@ -67,9 +76,9 @@ class SuperAttributeDataProvider implements BuyRequestDataProviderInterface
         } catch (NoSuchEntityException $e) {
             throw new GraphQlNoSuchEntityException(__('Could not find specified product.'));
         }
-
-        $this->optionCollection->addProductId((int)$parentProduct->getId());
-        $options = $this->optionCollection->getAttributesByProductId((int)$parentProduct->getId());
+        $linkField = $this->metadataPool->getMetadata(ProductInterface::class)->getLinkField();
+        $this->optionCollection->addProductId((int)$parentProduct->getData($linkField));
+        $options = $this->optionCollection->getAttributesByProductId((int)$parentProduct->getData($linkField));
 
         $superAttributesData = [];
         foreach ($options as $option) {
