4617391097d2806959d64be5e981538478362c98
Eden <quocviet312@gmail.com>
2019-08-03 08:50:37 +0700

    "Conditions" field of Catalog Price Rule no reset after clicking reset button issue 23996

--- app/code/Magento/CatalogRule/Block/Adminhtml/Edit/ResetButton.php
+++ app/code/Magento/CatalogRule/Block/Adminhtml/Edit/ResetButton.php
@@ -3,14 +3,22 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types=1);
 
 namespace Magento\CatalogRule\Block\Adminhtml\Edit;
 
 use Magento\Framework\View\Element\UiComponent\Control\ButtonProviderInterface;
 
+/**
+ * Render reset button
+ *
+ * Class \Magento\CatalogRule\Block\Adminhtml\Edit\ResetButton
+ */
 class ResetButton extends GenericButton implements ButtonProviderInterface
 {
     /**
+     * Get data for reset button
+     *
      * @return array
      * @codeCoverageIgnore
      */
@@ -19,6 +27,7 @@ class ResetButton extends GenericButton implements ButtonProviderInterface
         $data = [];
         if ($this->canRender('reset')) {
             $data = [
+                'id' => 'reset_button',
                 'label' => __('Reset'),
                 'class' => 'reset',
                 'on_click' => 'location.reload();',
