a0a7c7a917f163cf030be168d2504ece6c54215b
Eden <quocviet312@gmail.com>
2019-08-03 08:05:05 +0700

    Store view specific label text field not clear when click on reset issue 23991

--- app/code/Magento/SalesRule/Block/Adminhtml/Promo/Quote/Edit/ResetButton.php
+++ app/code/Magento/SalesRule/Block/Adminhtml/Promo/Quote/Edit/ResetButton.php
@@ -3,14 +3,20 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types=1);
 
 namespace Magento\SalesRule\Block\Adminhtml\Promo\Quote\Edit;
 
 use Magento\Framework\View\Element\UiComponent\Control\ButtonProviderInterface;
 
+/**
+ * Class \Magento\SalesRule\Block\Adminhtml\Promo\Quote\Edit\ResetButton
+ */
 class ResetButton extends GenericButton implements ButtonProviderInterface
 {
     /**
+     * Get data of reset button
+     *
      * @return array
      * @codeCoverageIgnore
      */
@@ -19,6 +25,7 @@ class ResetButton extends GenericButton implements ButtonProviderInterface
         $data = [];
         if ($this->canRender('reset')) {
             $data = [
+                'id' => 'reset_button',
                 'label' => __('Reset'),
                 'class' => 'reset',
                 'on_click' => 'location.reload();',
