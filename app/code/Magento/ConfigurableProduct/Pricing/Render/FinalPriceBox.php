76b348d90f74b8f4857829b84d4f736acdbf186c
Dmytro Horytskyi <horytsky@adobe.com>
2019-08-06 14:57:10 -0500

    MC-18847: Special Price & Regular Price Display Issues on Configurable Product

--- app/code/Magento/ConfigurableProduct/Pricing/Render/FinalPriceBox.php
+++ app/code/Magento/ConfigurableProduct/Pricing/Render/FinalPriceBox.php
@@ -17,12 +17,20 @@ use Magento\Framework\Pricing\Render\RendererPool;
 use Magento\Framework\Pricing\SaleableInterface;
 use Magento\Framework\View\Element\Template\Context;
 
+/**
+ * Class for final_price box rendering
+ */
 class FinalPriceBox extends \Magento\Catalog\Pricing\Render\FinalPriceBox
 {
     /**
      * @var LowestPriceOptionsProviderInterface
      */
     private $lowestPriceOptionsProvider;
+
+    /**
+     * @var ConfigurableOptionsProviderInterface
+     */
+    private $configurableOptionsProvider;
 
     /**
      * @param Context $context
@@ -34,7 +42,6 @@ class FinalPriceBox extends \Magento\Catalog\Pricing\Render\FinalPriceBox
      * @param LowestPriceOptionsProviderInterface $lowestPriceOptionsProvider
      * @param SalableResolverInterface|null $salableResolver
      * @param MinimalPriceCalculatorInterface|null $minimalPriceCalculator
-     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
     public function __construct(
         Context $context,
@@ -56,6 +63,7 @@ class FinalPriceBox extends \Magento\Catalog\Pricing\Render\FinalPriceBox
             $salableResolver,
             $minimalPriceCalculator
         );
+        $this->configurableOptionsProvider = $configurableOptionsProvider;
         $this->lowestPriceOptionsProvider = $lowestPriceOptionsProvider ?:
             ObjectManager::getInstance()->get(LowestPriceOptionsProviderInterface::class);
     }
@@ -68,7 +76,7 @@ class FinalPriceBox extends \Magento\Catalog\Pricing\Render\FinalPriceBox
     public function hasSpecialPrice()
     {
         $product = $this->getSaleableItem();
-        foreach ($this->lowestPriceOptionsProvider->getProducts($product) as $subProduct) {
+        foreach ($this->configurableOptionsProvider->getProducts($product) as $subProduct) {
             $regularPrice = $subProduct->getPriceInfo()->getPrice(RegularPrice::PRICE_CODE)->getValue();
             $finalPrice = $subProduct->getPriceInfo()->getPrice(FinalPrice::PRICE_CODE)->getValue();
             if ($finalPrice < $regularPrice) {
