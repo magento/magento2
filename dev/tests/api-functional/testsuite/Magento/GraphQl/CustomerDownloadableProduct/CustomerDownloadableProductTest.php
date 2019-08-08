486d3833033b9992ecefabd09b698a776f5efe72
Dusan Lukic <ldusan84@gmail.com>
2019-08-08 19:08:38 +0200

    Moved old acceptance test

--- dev/tests/api-functional/testsuite/Magento/GraphQl/CustomerDownloadableProduct/CustomerDownloadableProductTest.php
+++ dev/tests/api-functional/testsuite/Magento/GraphQl/CustomerDownloadableProduct/CustomerDownloadableProductTest.php
@@ -7,139 +7,100 @@ declare(strict_types=1);
 
 namespace Magento\GraphQl\CustomerDownloadableProduct;
 
+use Magento\Integration\Api\CustomerTokenServiceInterface;
+use Magento\TestFramework\Helper\Bootstrap;
 use Magento\TestFramework\TestCase\GraphQlAbstract;
-use Magento\TestFramework\ObjectManager;
-use Magento\TestFramework\TestCase\GraphQl\ResponseContainsErrorsException;
-use Magento\Customer\Api\CustomerRepositoryInterface;
-use Magento\Sales\Api\OrderRepositoryInterface;
-use Magento\Framework\Api\SearchCriteriaBuilder;
 
+/**
+ * Test retrieving of customer downloadable products.
+ */
 class CustomerDownloadableProductTest extends GraphQlAbstract
 {
-
     /**
-     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+     * @var CustomerTokenServiceInterface
+     */
+    private $customerTokenService;
+    /**
+     * @inheritdoc
+     */
+    protected function setUp()
+    {
+        $objectManager = Bootstrap::getObjectManager();
+        $this->customerTokenService = $objectManager->get(CustomerTokenServiceInterface::class);
+    }
+    /**
      * @magentoApiDataFixture Magento/Customer/_files/customer.php
      * @magentoApiDataFixture Magento/Downloadable/_files/product_downloadable.php
-     * @magentoApiDataFixture Magento/Downloadable/_files/order_with_downloadable_product.php
+     * @magentoApiDataFixture Magento/Downloadable/_files/customer_order_with_downloadable_product.php
      */
-    public function testGetCustomerDownloadableProducts()
+    public function testCustomerDownloadableProducts()
     {
-        $query = <<<MUTATION
-mutation {
-  generateCustomerToken(
-    email: "customer@example.com"
-    password: "password"
-  ) {
-    token
-  }
-}
-MUTATION;
-        $response = $this->graphQlMutation($query);
-        $token = $response['generateCustomerToken']['token'];
-        $this->headers = ['Authorization' => 'Bearer ' . $token];
+        $query = $this->getQuery();
+        $response = $this->graphQlQuery($query, [], '', $this->getHeaderMap());
 
-        $query = <<<QUERY
-        {
-    customerDownloadableProducts{
-        items{
-            order_increment_id
-            date
-            status
-            download_url
-            remaining_downloads
-        }
+        self::assertArrayHasKey('items', $response['customerDownloadableProducts']);
+        self::assertCount(1, $response['customerDownloadableProducts']['items']);
+        self::assertArrayHasKey('date', $response['customerDownloadableProducts']['items'][0]);
+        self::assertNotEmpty($response['customerDownloadableProducts']['items'][0]['date']);
+        self::assertArrayHasKey('download_url', $response['customerDownloadableProducts']['items'][0]);
+        self::assertNotEmpty($response['customerDownloadableProducts']['items'][0]['download_url']);
+        self::assertArrayHasKey('order_increment_id', $response['customerDownloadableProducts']['items'][0]);
+        self::assertNotEmpty($response['customerDownloadableProducts']['items'][0]['order_increment_id']);
+        self::assertArrayHasKey('remaining_downloads', $response['customerDownloadableProducts']['items'][0]);
+        self::assertNotEmpty($response['customerDownloadableProducts']['items'][0]['remaining_downloads']);
+        self::assertArrayHasKey('status', $response['customerDownloadableProducts']['items'][0]);
+        self::assertNotEmpty($response['customerDownloadableProducts']['items'][0]['status']);
     }
-
-}
-QUERY;
-        $objectManager = ObjectManager::getInstance();
-
-        $searchCriteria = $objectManager->get(SearchCriteriaBuilder::class)->create();
-
-        $orderRepository = $objectManager->create(OrderRepositoryInterface::class);
-        $orders = $orderRepository->getList($searchCriteria)->getItems();
-        $order = array_pop($orders);
-
-        $searchCriteria = $objectManager->get(
-            SearchCriteriaBuilder::class
-        )->addFilter(
-            'email',
-            'customer@example.com'
-        )->create();
-
-        $customerRepository = $objectManager->create(CustomerRepositoryInterface::class);
-        $customers = $customerRepository->getList($searchCriteria)
-            ->getItems();
-        $customer = array_pop($customers);
-
-        $order->setCustomerId($customer->getId())->setCustomerIsGuest(false)->save();
-        $response = $this->graphQlQuery($query, [], '', $this->headers);
-
-        $this->assertEquals(
-            $order->getIncrementId(),
-            $response['customerDownloadableProducts']['items'][0]['order_increment_id']
-        );
-    }
-
     /**
-     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
      * @magentoApiDataFixture Magento/Customer/_files/customer.php
+     * @magentoApiDataFixture Magento/Downloadable/_files/product_downloadable.php
+     * @magentoApiDataFixture Magento/Downloadable/_files/customer_order_with_downloadable_product.php
+     *
+     * @expectedException \Exception
+     * @expectedExceptionMessage The current customer isn't authorized.
      */
-    public function testGetCustomerDownloadableProductsIfProductsDoNotExist()
-    {
-        $query = <<<MUTATION
-mutation {
-  generateCustomerToken(
-    email: "customer@example.com"
-    password: "password"
-  ) {
-    token
-  }
-}
-MUTATION;
-        $response = $this->graphQlMutation($query);
-        $token = $response['generateCustomerToken']['token'];
-        $this->headers = ['Authorization' => 'Bearer ' . $token];
-
-        $query = <<<QUERY
-        {
-    customerDownloadableProducts{
-        items{
-            order_increment_id
-            date
-            status
-            download_url
-            remaining_downloads
-        }
-    }
-
-}
-QUERY;
-
-        $response = $this->graphQlQuery($query, [], '', $this->headers);
-        $this->assertEmpty($response['customerDownloadableProducts']['items']);
-    }
-
     public function testGuestCannotAccessDownloadableProducts()
     {
-        $query = <<<QUERY
-        {
-    customerDownloadableProducts{
-        items{
-            order_increment_id
-            date
-            status
-            download_url
-            remaining_downloads
-        }
+        $this->graphQlQuery($this->getQuery());
     }
-
+    /**
+     * @magentoApiDataFixture Magento/Customer/_files/customer.php
+     */
+    public function testCustomerHasNoOrders()
+    {
+        $query = $this->getQuery();
+        $response = $this->graphQlQuery($query, [], '', $this->getHeaderMap());
+        self::assertArrayHasKey('items', $response['customerDownloadableProducts']);
+        self::assertCount(0, $response['customerDownloadableProducts']['items']);
+    }
+    /**
+     * @return string
+     */
+    private function getQuery(): string
+    {
+        return <<<QUERY
+{
+  customerDownloadableProducts {
+    items {
+      date
+      download_url
+      order_increment_id
+      remaining_downloads
+      status
+    }
+  }
 }
 QUERY;
-
-        $this->expectException(ResponseContainsErrorsException::class);
-        $this->expectExceptionMessage('GraphQL response contains errors: The current customer isn\'t authorized');
-        $this->graphQlQuery($query);
+    }
+    /**
+     * @param string $username
+     * @param string $password
+     * @return array
+     * @throws \Magento\Framework\Exception\AuthenticationException
+     */
+    private function getHeaderMap(string $username = 'customer@example.com', string $password = 'password'): array
+    {
+        $customerToken = $this->customerTokenService->createCustomerAccessToken($username, $password);
+        return ['Authorization' => 'Bearer ' . $customerToken];
     }
 }
