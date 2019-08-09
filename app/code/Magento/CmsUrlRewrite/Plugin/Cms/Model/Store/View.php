d62890f48a8289027d792ef941f4a020abfa2f2f
Nikita Shcherbatykh <nikita.shcherbatykh@transoftgroup.com>
2019-08-09 11:26:11 +0300

    MC-18561: After changing store view the cms page is not redirecting correctly

---
+++ app/code/Magento/CmsUrlRewrite/Plugin/Cms/Model/Store/View.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\CmsUrlRewrite\Plugin\Cms\Model\Store;
+
+use Magento\Cms\Api\PageRepositoryInterface;
+use Magento\CmsUrlRewrite\Model\CmsPageUrlRewriteGenerator;
+use Magento\Framework\Api\SearchCriteriaBuilder;
+use Magento\Framework\Model\AbstractModel;
+use Magento\Store\Model\ResourceModel\Store;
+use Magento\UrlRewrite\Model\UrlPersistInterface;
+use Magento\UrlRewrite\Service\V1\Data\UrlRewrite;
+
+/**
+ * Plugin which is listening store resource model and on save replace cms page url rewrites
+ *
+ * @see Store
+ */
+class View
+{
+    /**
+     * @var AbstractModel
+     */
+    private $origStore;
+
+    /**
+     * @var UrlPersistInterface
+     */
+    private $urlPersist;
+
+    /**
+     * @var CmsPageUrlRewriteGenerator
+     */
+    private $cmsPageUrlRewriteGenerator;
+
+    /**
+     * @var PageRepositoryInterface
+     */
+    private $pageRepository;
+
+    /**
+     * @var SearchCriteriaBuilder
+     */
+    private $searchCriteriaBuilder;
+
+    /**
+     * @param UrlPersistInterface $urlPersist
+     * @param SearchCriteriaBuilder $searchCriteriaBuilder
+     * @param PageRepositoryInterface $pageRepository
+     * @param CmsPageUrlRewriteGenerator $cmsPageUrlRewriteGenerator
+     */
+    public function __construct(
+        UrlPersistInterface $urlPersist,
+        SearchCriteriaBuilder $searchCriteriaBuilder,
+        PageRepositoryInterface $pageRepository,
+        CmsPageUrlRewriteGenerator $cmsPageUrlRewriteGenerator
+    ) {
+        $this->urlPersist = $urlPersist;
+        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
+        $this->pageRepository = $pageRepository;
+        $this->cmsPageUrlRewriteGenerator = $cmsPageUrlRewriteGenerator;
+    }
+
+    /**
+     * @param Store $object
+     * @param AbstractModel $store
+     * @return void
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function beforeSave(
+        Store $object,
+        AbstractModel $store
+    ) {
+        $this->origStore = $store;
+    }
+
+    /**
+     * Regenerate urls on store after save
+     *
+     * @param Store $store
+     * @return Store
+     */
+    public function afterSave(
+        Store $store
+    ) {
+        if ($this->origStore->isObjectNew() || $this->origStore->dataHasChangedFor('group_id')) {
+            if (!$this->origStore->isObjectNew()) {
+                $this->urlPersist->deleteByData([UrlRewrite::STORE_ID => $this->origStore->getId()]);
+            }
+            $this->urlPersist->replace(
+                $this->generateCmsPagesUrls($this->origStore->getId())
+            );
+        }
+        return $store;
+    }
+
+    /**
+     * Generate url rewrites for cms pages to store view
+     *
+     * @param int $storeId
+     * @return array
+     */
+    private function generateCmsPagesUrls($storeId): array
+    {
+        $urls = [];
+        $searchCriteria = $this->searchCriteriaBuilder->create();
+        $cmsPagesCollection = $this->pageRepository->getList($searchCriteria)->getItems();
+        foreach ($cmsPagesCollection as $page) {
+            $page->setStoreId($storeId);
+            /** @var \Magento\Cms\Model\Page $page */
+            $urls = array_merge(
+                $urls,
+                $this->cmsPageUrlRewriteGenerator->generate($page)
+            );
+        }
+        return $urls;
+    }
+}
---
+++ dev/tests/integration/testsuite/Magento/CmsUrlRewrite/Plugin/Cms/Model/Store/ViewTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Magento\CmsUrlRewrite\Plugin\Cms\Model\Store;
+
+use Magento\Store\Model\StoreFactory;
+use Magento\TestFramework\Helper\Bootstrap;
+use Magento\UrlRewrite\Model\UrlFinderInterface;
+use Magento\UrlRewrite\Service\V1\Data\UrlRewrite;
+
+/**
+ * Test for plugin which is listening store resource model and on save replace cms page url rewrites
+ */
+class ViewTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @var UrlFinderInterface
+     */
+    private $urlFinder;
+
+    /**
+     * @var StoreFactory
+     */
+    private $storeFactory;
+
+    /**
+     * @inheritdoc
+     */
+    protected function setUp()
+    {
+        $this->storeFactory = Bootstrap::getObjectManager()->create(StoreFactory::class);
+        $this->urlFinder = Bootstrap::getObjectManager()->create(UrlFinderInterface::class);
+    }
+
+    /**
+     * @magentoDataFixture Magento/Cms/_files/pages.php
+     * @magentoDataFixture Magento/Store/_files/store.php
+     * @magentoAppArea adminhtml
+     */
+    public function testAfterSave()
+    {
+        $data = [
+            UrlRewrite::REQUEST_PATH => 'page100',
+        ];
+        $urlRewrites = $this->urlFinder->findAllByData($data);
+        $this->assertCount(2, $urlRewrites);
+    }
+}
