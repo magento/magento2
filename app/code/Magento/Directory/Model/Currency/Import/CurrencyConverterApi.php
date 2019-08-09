23d566ba0357d8728d66847f4a9418bf258a6ad3
Oleksandr Kravchuk <swnsma@gmail.com>
2019-08-09 10:28:02 +0300

    Update CurrencyConverterApi.php

--- app/code/Magento/Directory/Model/Currency/Import/CurrencyConverterApi.php
+++ app/code/Magento/Directory/Model/Currency/Import/CurrencyConverterApi.php
@@ -103,7 +103,7 @@ class CurrencyConverterApi extends AbstractImport
                         $data[$currencyFrom][$to] = null;
                     } else {
                         if (isset($response['error']) && $response['error']) {
-                            $this->_messages[] = __($response['error']);
+                            $this->_messages[] = $response['error'];
                             $data[$currencyFrom][$to] = null;
                         } else {
                             $data[$currencyFrom][$to] = $this->_numberFormat(
