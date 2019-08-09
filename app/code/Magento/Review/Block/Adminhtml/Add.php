02bf00723a76cd7f86cfb8b1d628b6d3d4482ebc
Arushi Bansal <53007383+arushibansal013@users.noreply.github.com>
2019-08-09 22:59:30 +0530

    Fixing number of line issue for static build failure

--- app/code/Magento/Review/Block/Adminhtml/Add.php
+++ app/code/Magento/Review/Block/Adminhtml/Add.php
@@ -28,7 +28,6 @@ class Add extends \Magento\Backend\Block\Widget\Form\Container
         $this->buttonList->update('save', 'id', 'save_button');
         $this->buttonList->remove('reset');
         $this->buttonList->add('reset_button', ['label' => 'Reset']);
-        
         $this->_formScripts[] = '
             require(["prototype"], function(){
                 toggleParentVis("add_review_form");
