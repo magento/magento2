34d6d5599178d5939baf9d11bb4780b145f46e84
Vinai Kopp <vinai@netzarbeiter.com>
2019-08-16 09:08:41 -0500

    Fix config backend model PDF file name extension

--- app/code/Magento/Config/Model/Config/Backend/File/Pdf.php
+++ app/code/Magento/Config/Model/Config/Backend/File/Pdf.php
@@ -8,16 +8,14 @@ namespace Magento\Config\Model\Config\Backend\Image;
 
 /**
  * @api
- * @deprecated The wrong file type extensions are returned.
- * @see \Magento\Config\Model\Config\Backend\File\Pdf
  */
-class Pdf extends \Magento\Config\Model\Config\Backend\Image
+class Pdf extends \Magento\Config\Model\Config\Backend\File
 {
     /**
      * @return string[]
      */
     protected function _getAllowedExtensions()
     {
-        return ['tif', 'tiff', 'png', 'jpg', 'jpe', 'jpeg', 'pdf'];
+        return ['pdf'];
     }
 }
