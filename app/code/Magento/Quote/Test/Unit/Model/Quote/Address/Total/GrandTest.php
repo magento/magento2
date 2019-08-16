c9060b41f1c603d1b7ee6cd30fa603ed3673bce1
Yauhen_Lyskavets <yauhen_lyskavets@epam.com>
2019-08-16 17:56:36 +0300

    MC-17869: Cart Total is shown as NaN when 100% discount applied through Cart Rule
    - Grand totals correct rounding added.

--- app/code/Magento/Quote/Test/Unit/Model/Quote/Address/Total/GrandTest.php
+++ app/code/Magento/Quote/Test/Unit/Model/Quote/Address/Total/GrandTest.php
@@ -6,18 +6,43 @@
 namespace Magento\Quote\Test\Unit\Model\Quote\Address\Total;
 
 use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
+use Magento\Quote\Model\Quote\Address\Total\Grand;
+use Magento\Framework\Pricing\PriceCurrencyInterface as PriceRounder;
+use PHPUnit_Framework_MockObject_MockObject as ObjectMock;
+use PHPUnit\Framework\TestCase;
 
-class GrandTest extends \PHPUnit\Framework\TestCase
+/**
+ * Grand totals collector test.
+ */
+class GrandTest extends TestCase
 {
     /**
-     * @var \Magento\Quote\Model\Quote\Address\Total\Grand
+     * @var PriceRounder|ObjectMock
      */
-    protected $model;
+    private $priceRounder;
 
+    /**
+     * @var Grand
+     */
+    private $model;
+
+    /**
+     * @inheritDoc
+     */
     protected function setUp()
     {
-        $objectManager = new ObjectManager($this);
-        $this->model = $objectManager->getObject(\Magento\Quote\Model\Quote\Address\Total\Grand::class);
+        $this->priceRounder = $this->getMockBuilder(PriceRounder::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['roundPrice'])
+            ->getMockForAbstractClass();
+
+        $helper = new ObjectManager($this);
+        $this->model = $helper->getObject(
+            Grand::class,
+            [
+                'priceRounder' => $this->priceRounder,
+            ]
+        );
     }
 
     public function testCollect()
@@ -27,14 +52,20 @@ class GrandTest extends \PHPUnit\Framework\TestCase
         $grandTotal = 6.4; // 1 + 2 + 3.4
         $grandTotalBase = 15.7; // 4 + 5 + 6.7
 
-        $totalMock = $this->createPartialMock(\Magento\Quote\Model\Quote\Address\Total::class, [
+        $this->priceRounder->expects($this->at(0))->method('roundPrice')->willReturn($grandTotal + 2);
+        $this->priceRounder->expects($this->at(1))->method('roundPrice')->willReturn($grandTotalBase + 2);
+
+        $totalMock = $this->createPartialMock(
+            \Magento\Quote\Model\Quote\Address\Total::class,
+            [
                 'getAllTotalAmounts',
                 'getAllBaseTotalAmounts',
                 'setGrandTotal',
                 'setBaseGrandTotal',
                 'getGrandTotal',
                 'getBaseGrandTotal'
-            ]);
+            ]
+        );
         $totalMock->expects($this->once())->method('getGrandTotal')->willReturn(2);
         $totalMock->expects($this->once())->method('getBaseGrandTotal')->willReturn(2);
         $totalMock->expects($this->once())->method('getAllTotalAmounts')->willReturn($totals);
