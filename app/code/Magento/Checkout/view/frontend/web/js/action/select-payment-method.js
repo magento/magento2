a23275380e54a00188af6040d87a31488c317e9c
Andrew Stepanchuk <styopchik@gmail.com>
2019-08-20 20:59:22 +0300

    Request for applying payment method and refreshing totals was added to select-payment-method action. It is necessary for applying shopping cart rules, based on payment method in checkout.

--- app/code/Magento/Checkout/view/frontend/web/js/action/select-payment-method.js
+++ app/code/Magento/Checkout/view/frontend/web/js/action/select-payment-method.js
@@ -7,11 +7,39 @@
  * @api
  */
 define([
-    '../model/quote'
-], function (quote) {
+    'mage/storage',
+    './get-totals',
+    '../model/quote',
+    '../model/full-screen-loader',
+    '../model/url-builder',
+    'Magento_Customer/js/model/customer',
+], function (storage, getTotalsAction, quote, fullScreenLoader, urlBuilder, customer) {
     'use strict';
 
     return function (paymentMethod) {
+        var serviceUrl, payload;
+
         quote.paymentMethod(paymentMethod);
+
+        delete paymentMethod.title;
+
+        payload = {
+            method: paymentMethod
+        };
+        if (customer.isLoggedIn()) {
+            serviceUrl = urlBuilder.createUrl('/carts/mine/selected-payment-method', {});
+        } else {
+            serviceUrl = urlBuilder.createUrl('/guest-carts/:cartId/selected-payment-method', {
+                cartId: quote.getQuoteId()
+            });
+        }
+        fullScreenLoader.startLoader();
+        storage.put(
+            serviceUrl,
+            JSON.stringify(payload)
+        ).success(function () {
+            getTotalsAction([]);
+            fullScreenLoader.stopLoader();
+        });
     };
 });
