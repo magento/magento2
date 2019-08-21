f084e748cba5c3d24e70ddc49b7c9b9f1a274db0
Rus0 <andonidominguez@wolfsellers.com>
2019-08-21 13:30:12 -0500

    Cart price rule based on payment methodis not aplied in checkout #24206. Added the functionality to mark the payment method as selected in the quote

--- app/code/Magento/Checkout/view/frontend/web/js/action/select-payment-method.js
+++ app/code/Magento/Checkout/view/frontend/web/js/action/select-payment-method.js
@@ -7,11 +7,44 @@
  * @api
  */
 define([
-    '../model/quote'
-], function (quote) {
+    'Magento_Checkout/js/model/quote',
+    'Magento_Checkout/js/model/url-builder',
+    'mage/storage',
+    'Magento_Checkout/js/model/error-processor',
+    'Magento_Customer/js/model/customer',
+    'Magento_Checkout/js/action/get-totals',
+    'Magento_Checkout/js/model/full-screen-loader'
+], function (quote, urlBuilder, storage, errorProcessor, customer, getTotalsAction, fullScreenLoader) {
     'use strict';
 
     return function (paymentMethod) {
-        quote.paymentMethod(paymentMethod);
+        var serviceUrl,
+            payload;
+
+        fullScreenLoader.startLoader();
+
+        payload = {
+            cartId: quote.getQuoteId(),
+            method: paymentMethod
+        };
+
+        if (!customer.isLoggedIn()) {
+            serviceUrl = urlBuilder.createUrl('/guest-carts/:cartId/selected-payment-method', {
+                cartId: quote.getQuoteId()
+            });
+            payload.email = quote.guestEmail;
+        } else {
+            serviceUrl = urlBuilder.createUrl('/carts/mine/selected-payment-method', {});
+        }
+
+        return storage.put(
+            serviceUrl, JSON.stringify(payload)
+        ).always(
+            function () {
+                getTotalsAction({}, true);
+                quote.paymentMethod(paymentMethod);
+                fullScreenLoader.stopLoader();
+            }
+        );
     };
 });
