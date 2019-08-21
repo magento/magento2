38f729bdaa9ce619a6cd17f0702fd8897106f4da
Dmytro Horytskyi <horytsky@adobe.com>
2019-08-21 06:37:44 +0000

    MC-18954: Nightly build jobs were failing lately after un-skipping tests in MC-5777

--- app/code/Magento/CatalogRule/Test/Unit/Model/Indexer/ReindexRuleProductPriceTest.php
+++ app/code/Magento/CatalogRule/Test/Unit/Model/Indexer/ReindexRuleProductPriceTest.php
@@ -11,7 +11,6 @@ use Magento\CatalogRule\Model\Indexer\ProductPriceCalculator;
 use Magento\CatalogRule\Model\Indexer\ReindexRuleProductPrice;
 use Magento\CatalogRule\Model\Indexer\RuleProductPricesPersistor;
 use Magento\CatalogRule\Model\Indexer\RuleProductsSelectBuilder;
-use Magento\Framework\Stdlib\DateTime\DateTime;
 use Magento\Framework\Stdlib\DateTime\TimezoneInterface;
 use Magento\Store\Api\Data\GroupInterface;
 use Magento\Store\Api\Data\WebsiteInterface;
@@ -41,45 +40,37 @@ class ReindexRuleProductPriceTest extends \PHPUnit\Framework\TestCase
     private $productPriceCalculatorMock;
 
     /**
-     * @var DateTime|MockObject
+     * @var TimezoneInterface|MockObject
      */
-    private $dateTimeMock;
+    private $localeDate;
 
     /**
      * @var RuleProductPricesPersistor|MockObject
      */
     private $pricesPersistorMock;
 
-    /**
-     * @var TimezoneInterface|MockObject
-     */
-    private $localeDate;
-
     protected function setUp()
     {
         $this->storeManagerMock = $this->createMock(StoreManagerInterface::class);
         $this->ruleProductsSelectBuilderMock = $this->createMock(RuleProductsSelectBuilder::class);
         $this->productPriceCalculatorMock = $this->createMock(ProductPriceCalculator::class);
-        $this->dateTimeMock = $this->createMock(DateTime::class);
-        $this->pricesPersistorMock = $this->createMock(RuleProductPricesPersistor::class);
         $this->localeDate = $this->createMock(TimezoneInterface::class);
+        $this->pricesPersistorMock = $this->createMock(RuleProductPricesPersistor::class);
 
         $this->model = new ReindexRuleProductPrice(
             $this->storeManagerMock,
             $this->ruleProductsSelectBuilderMock,
             $this->productPriceCalculatorMock,
-            $this->dateTimeMock,
-            $this->pricesPersistorMock,
-            $this->localeDate
+            $this->localeDate,
+            $this->pricesPersistorMock
         );
     }
 
     public function testExecute()
     {
         $websiteId = 234;
-        $storeGroupId = 30;
-        $storeId = 40;
-        $productMock = $this->createMock(Product::class);
+        $defaultGroupId = 11;
+        $defaultStoreId = 22;
 
         $websiteMock = $this->createMock(WebsiteInterface::class);
         $websiteMock->expects($this->once())
@@ -87,19 +78,22 @@ class ReindexRuleProductPriceTest extends \PHPUnit\Framework\TestCase
             ->willReturn($websiteId);
         $websiteMock->expects($this->once())
             ->method('getDefaultGroupId')
-            ->willReturn($storeGroupId);
+            ->willReturn($defaultGroupId);
         $this->storeManagerMock->expects($this->once())
             ->method('getWebsites')
             ->willReturn([$websiteMock]);
-        $storeGroupMock = $this->createMock(GroupInterface::class);
-        $storeGroupMock->expects($this->once())
+        $groupMock = $this->createMock(GroupInterface::class);
+        $groupMock->method('getId')
+            ->willReturn($defaultStoreId);
+        $groupMock->expects($this->once())
             ->method('getDefaultStoreId')
-            ->willReturn($storeId);
+            ->willReturn($defaultStoreId);
         $this->storeManagerMock->expects($this->once())
             ->method('getGroup')
-            ->with($storeGroupId)
-            ->willReturn($storeGroupMock);
+            ->with($defaultGroupId)
+            ->willReturn($groupMock);
 
+        $productMock = $this->createMock(Product::class);
         $statementMock = $this->createMock(\Zend_Db_Statement_Interface::class);
         $this->ruleProductsSelectBuilderMock->expects($this->once())
             ->method('build')
@@ -115,22 +109,10 @@ class ReindexRuleProductPriceTest extends \PHPUnit\Framework\TestCase
             'action_stop' => true
         ];
 
-        $this->dateTimeMock->expects($this->at(0))
-            ->method('date')
-            ->with('Y-m-d 00:00:00', $ruleData['from_time'])
-            ->willReturn($ruleData['from_time']);
-        $this->dateTimeMock->expects($this->at(1))
-            ->method('timestamp')
-            ->with($ruleData['from_time'])
-            ->willReturn($ruleData['from_time']);
-        $this->dateTimeMock->expects($this->at(2))
-            ->method('date')
-            ->with('Y-m-d 00:00:00', $ruleData['to_time'])
-            ->willReturn($ruleData['to_time']);
-        $this->dateTimeMock->expects($this->at(3))
-            ->method('timestamp')
-            ->with($ruleData['to_time'])
-            ->willReturn($ruleData['to_time']);
+        $this->localeDate->expects($this->once())
+            ->method('scopeDate')
+            ->with($defaultStoreId, null, true)
+            ->willReturn(new \DateTime());
 
         $statementMock->expects($this->at(0))
             ->method('fetch')
