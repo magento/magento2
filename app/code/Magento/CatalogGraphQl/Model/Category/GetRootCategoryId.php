3babf2ae50c7ec79aa5bd2a6906a244f6f4cfed5
Rus0 <andonid88@gmail.com>
2019-08-26 09:47:19 -0500

    Code Style changes

--- app/code/Magento/CatalogGraphQl/Model/Category/GetRootCategoryId.php
+++ app/code/Magento/CatalogGraphQl/Model/Category/GetRootCategoryId.php
@@ -11,6 +11,9 @@ use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Store\Model\StoreManagerInterface;
 
+/**
+ * Gets Root Category for Current Store.
+ */
 class GetRootCategoryId
 {
     /**
@@ -50,3 +53,4 @@ class GetRootCategoryId
         return $this->rootCategoryId;
     }
 }
+
