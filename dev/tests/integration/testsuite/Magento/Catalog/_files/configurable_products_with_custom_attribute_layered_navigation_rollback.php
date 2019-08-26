bb996868319ae8efa47088c9f8f2b217c4727819
Deepty Thampy <dthampy@adobe.com>
2019-08-26 13:59:22 -0500

    MC-19441: Fix web api test failures
    - fix fixture and tests

--- dev/tests/integration/testsuite/Magento/Catalog/_files/configurable_products_with_custom_attribute_layered_navigation_rollback.php
+++ dev/tests/integration/testsuite/Magento/Catalog/_files/configurable_products_with_custom_attribute_layered_navigation_rollback.php
@@ -7,14 +7,3 @@ declare(strict_types=1);
 
 // phpcs:ignore Magento2.Security.IncludeFile
 require __DIR__ . '/../../ConfigurableProduct/_files/configurable_products_rollback.php';
-
-use Magento\TestFramework\Helper\Bootstrap;
-use Magento\Eav\Api\AttributeRepositoryInterface;
-
-$eavConfig = Bootstrap::getObjectManager()->get(\Magento\Eav\Model\Config::class);
-
-/** @var AttributeRepositoryInterface $attributeRepository */
-$attributeRepository = Bootstrap::getObjectManager()->get(AttributeRepositoryInterface::class);
-/** @var \Magento\Eav\Api\Data\AttributeInterface $attribute */
-$attribute = $attributeRepository->get('catalog_product', 'test_configurable');
-$attributeRepository->delete($attribute);
