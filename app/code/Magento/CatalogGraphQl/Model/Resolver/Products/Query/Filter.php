211f2a3860562c73a9f65fbaeea2761411bda213
Daniel Renaud <drenaud@magento.com>
2019-08-27 09:28:07 -0500

    MC-18450: Allow custom attributes to be filterable and also returned in the layered navigation the product filter section in GraphQL
    - Add relevance to sort options
    - Refactor and decouple searching and filtering

--- app/code/Magento/CatalogGraphQl/Model/Resolver/Products/Query/Filter.php
+++ app/code/Magento/CatalogGraphQl/Model/Resolver/Products/Query/Filter.php
@@ -7,13 +7,11 @@ declare(strict_types=1);
 
 namespace Magento\CatalogGraphQl\Model\Resolver\Products\Query;
 
-use GraphQL\Language\AST\SelectionNode;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
 use Magento\Framework\Api\SearchCriteriaInterface;
 use Magento\CatalogGraphQl\Model\Resolver\Products\DataProvider\Product;
 use Magento\CatalogGraphQl\Model\Resolver\Products\SearchResult;
 use Magento\CatalogGraphQl\Model\Resolver\Products\SearchResultFactory;
-use Magento\Framework\GraphQl\Query\FieldTranslator;
 
 /**
  * Retrieve filtered product data based off given search criteria in a format that GraphQL can interpret.
@@ -31,31 +29,31 @@ class Filter
     private $productDataProvider;
 
     /**
-     * @var FieldTranslator
-     */
-    private $fieldTranslator;
-
-    /**
      * @var \Magento\Catalog\Model\Layer\Resolver
      */
     private $layerResolver;
 
     /**
+     * FieldSelection
+     */
+    private $fieldSelection;
+
+    /**
      * @param SearchResultFactory $searchResultFactory
      * @param Product $productDataProvider
      * @param \Magento\Catalog\Model\Layer\Resolver $layerResolver
-     * @param FieldTranslator $fieldTranslator
+     * @param FieldSelection $fieldSelection
      */
     public function __construct(
         SearchResultFactory $searchResultFactory,
         Product $productDataProvider,
         \Magento\Catalog\Model\Layer\Resolver $layerResolver,
-        FieldTranslator $fieldTranslator
+        FieldSelection $fieldSelection
     ) {
         $this->searchResultFactory = $searchResultFactory;
         $this->productDataProvider = $productDataProvider;
-        $this->fieldTranslator = $fieldTranslator;
         $this->layerResolver = $layerResolver;
+        $this->fieldSelection = $fieldSelection;
     }
 
     /**
@@ -71,7 +69,7 @@ class Filter
         ResolveInfo $info,
         bool $isSearch = false
     ): SearchResult {
-        $fields = $this->getProductFields($info);
+        $fields = $this->fieldSelection->getProductsFieldSelection($info);
         $products = $this->productDataProvider->getList($searchCriteria, $fields, $isSearch);
         $productArray = [];
         /** @var \Magento\Catalog\Model\Product $product */
@@ -86,56 +84,5 @@ class Filter
                 'productsSearchResult' => $productArray
             ]
         );
-    }
-
-    /**
-     * Return field names for all requested product fields.
-     *
-     * @param ResolveInfo $info
-     * @return string[]
-     */
-    private function getProductFields(ResolveInfo $info) : array
-    {
-        $fieldNames = [];
-        foreach ($info->fieldNodes as $node) {
-            if ($node->name->value !== 'products') {
-                continue;
-            }
-            foreach ($node->selectionSet->selections as $selection) {
-                if ($selection->name->value !== 'items') {
-                    continue;
-                }
-                $fieldNames[] = $this->collectProductFieldNames($selection, $fieldNames);
-            }
-        }
-
-        $fieldNames = array_merge(...$fieldNames);
-
-        return $fieldNames;
-    }
-
-    /**
-     * Collect field names for each node in selection
-     *
-     * @param SelectionNode $selection
-     * @param array $fieldNames
-     * @return array
-     */
-    private function collectProductFieldNames(SelectionNode $selection, array $fieldNames = []): array
-    {
-        foreach ($selection->selectionSet->selections as $itemSelection) {
-            if ($itemSelection->kind === 'InlineFragment') {
-                foreach ($itemSelection->selectionSet->selections as $inlineSelection) {
-                    if ($inlineSelection->kind === 'InlineFragment') {
-                        continue;
-                    }
-                    $fieldNames[] = $this->fieldTranslator->translate($inlineSelection->name->value);
-                }
-                continue;
-            }
-            $fieldNames[] = $this->fieldTranslator->translate($itemSelection->name->value);
-        }
-
-        return $fieldNames;
     }
 }
--- app/code/Magento/CatalogGraphQl/Model/Resolver/Products/Query/Search.php
+++ app/code/Magento/CatalogGraphQl/Model/Resolver/Products/Query/Search.php
@@ -7,9 +7,9 @@ declare(strict_types=1);
 
 namespace Magento\CatalogGraphQl\Model\Resolver\Products\Query;
 
+use Magento\CatalogGraphQl\Model\Resolver\Products\DataProvider\ProductSearch;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
 use Magento\Framework\Api\Search\SearchCriteriaInterface;
-use Magento\CatalogGraphQl\Model\Resolver\Products\SearchCriteria\Helper\Filter as FilterHelper;
 use Magento\CatalogGraphQl\Model\Resolver\Products\SearchResult;
 use Magento\CatalogGraphQl\Model\Resolver\Products\SearchResultFactory;
 use Magento\Search\Api\SearchInterface;
@@ -26,24 +26,9 @@ class Search
     private $search;
 
     /**
-     * @var FilterHelper
-     */
-    private $filterHelper;
-
-    /**
-     * @var Filter
-     */
-    private $filterQuery;
-
-    /**
      * @var SearchResultFactory
      */
     private $searchResultFactory;
-
-    /**
-     * @var \Magento\Framework\EntityManager\MetadataPool
-     */
-    private $metadataPool;
 
     /**
      * @var \Magento\Search\Model\Search\PageSizeProvider
@@ -56,30 +41,37 @@ class Search
     private $searchCriteriaFactory;
 
     /**
+     * @var FieldSelection
+     */
+    private $fieldSelection;
+
+    /**
+     * @var ProductSearch
+     */
+    private $productsProvider;
+
+    /**
      * @param SearchInterface $search
-     * @param FilterHelper $filterHelper
-     * @param Filter $filterQuery
      * @param SearchResultFactory $searchResultFactory
-     * @param \Magento\Framework\EntityManager\MetadataPool $metadataPool
      * @param \Magento\Search\Model\Search\PageSizeProvider $pageSize
      * @param SearchCriteriaInterfaceFactory $searchCriteriaFactory
+     * @param FieldSelection $fieldSelection
+     * @param ProductSearch $productsProvider
      */
     public function __construct(
         SearchInterface $search,
-        FilterHelper $filterHelper,
-        Filter $filterQuery,
         SearchResultFactory $searchResultFactory,
-        \Magento\Framework\EntityManager\MetadataPool $metadataPool,
         \Magento\Search\Model\Search\PageSizeProvider $pageSize,
-        SearchCriteriaInterfaceFactory $searchCriteriaFactory
+        SearchCriteriaInterfaceFactory $searchCriteriaFactory,
+        FieldSelection $fieldSelection,
+        ProductSearch $productsProvider
     ) {
         $this->search = $search;
-        $this->filterHelper = $filterHelper;
-        $this->filterQuery = $filterQuery;
         $this->searchResultFactory = $searchResultFactory;
-        $this->metadataPool = $metadataPool;
         $this->pageSizeProvider = $pageSize;
         $this->searchCriteriaFactory = $searchCriteriaFactory;
+        $this->fieldSelection = $fieldSelection;
+        $this->productsProvider = $productsProvider;
     }
 
     /**
@@ -87,20 +79,15 @@ class Search
      *
      * @param SearchCriteriaInterface $searchCriteria
      * @param ResolveInfo $info
-     * @param array $args
      * @return SearchResult
      * @throws \Exception
      */
     public function getResult(
         SearchCriteriaInterface $searchCriteria,
-        ResolveInfo $info,
-        array $args = []
+        ResolveInfo $info
     ): SearchResult {
-        $idField = $this->metadataPool->getMetadata(
-            \Magento\Catalog\Api\Data\ProductInterface::class
-        )->getIdentifierField();
+        $queryFields = $this->fieldSelection->getProductsFieldSelection($info);
 
-        $isSearch = isset($args['search']);
         $realPageSize = $searchCriteria->getPageSize();
         $realCurrentPage = $searchCriteria->getCurrentPage();
         // Current page must be set to 0 and page size to max for search to grab all ID's as temporary workaround
@@ -109,35 +96,32 @@ class Search
         $searchCriteria->setCurrentPage(0);
         $itemsResults = $this->search->search($searchCriteria);
 
-        $ids = [];
-        $searchIds = [];
-        foreach ($itemsResults->getItems() as $item) {
-            $ids[$item->getId()] = null;
-            $searchIds[] = $item->getId();
-        }
+        //Create copy of search criteria without conditions (conditions will be applied by joining search result)
+        $searchCriteriaCopy = $this->searchCriteriaFactory->create()
+            ->setSortOrders($searchCriteria->getSortOrders())
+            ->setPageSize($realPageSize)
+            ->setCurrentPage($realCurrentPage);
 
-        $searchCriteriaIds = $this->searchCriteriaFactory->create();
-        $filter = $this->filterHelper->generate($idField, 'in', $searchIds);
-        $searchCriteriaIds = $this->filterHelper->add($searchCriteriaIds, $filter);
-        $searchCriteriaIds->setSortOrders($searchCriteria->getSortOrders());
-        $searchCriteriaIds->setPageSize($realPageSize);
-        $searchCriteriaIds->setCurrentPage($realCurrentPage);
+        $searchResults = $this->productsProvider->getList($searchCriteriaCopy, $itemsResults, $queryFields);
 
-        $searchResult = $this->filterQuery->getResult($searchCriteriaIds, $info, $isSearch);
-
-        $searchCriteria->setPageSize($realPageSize);
-        $searchCriteria->setCurrentPage($realCurrentPage);
         //possible division by 0
         if ($realPageSize) {
-            $maxPages = (int)ceil($searchResult->getTotalCount() / $realPageSize);
+            $maxPages = (int)ceil($searchResults->getTotalCount() / $realPageSize);
         } else {
             $maxPages = 0;
         }
 
+        $productArray = [];
+        /** @var \Magento\Catalog\Model\Product $product */
+        foreach ($searchResults->getItems() as $product) {
+            $productArray[$product->getId()] = $product->getData();
+            $productArray[$product->getId()]['model'] = $product;
+        }
+
         return $this->searchResultFactory->create(
             [
-                'totalCount' => $searchResult->getTotalCount(),
-                'productsSearchResult' => $searchResult->getProductsSearchResult(),
+                'totalCount' => $searchResults->getTotalCount(),
+                'productsSearchResult' => $productArray,
                 'searchAggregation' => $itemsResults->getAggregations(),
                 'pageSize' => $realPageSize,
                 'currentPage' => $realCurrentPage,
