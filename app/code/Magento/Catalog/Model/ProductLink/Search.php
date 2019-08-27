54d6fa00a60a03049fc2db82193addd0850f5f71
Cari Spruiell <spruiell@adobe.com>
2019-08-27 09:50:54 -0500

    MC-19633: Disabled Products Do Not Appear in Search Results of Link Attribute
     - update to show all products regardless of status

--- app/code/Magento/Catalog/Model/ProductLink/Search.php
+++ app/code/Magento/Catalog/Model/ProductLink/Search.php
@@ -60,7 +60,6 @@ class Search
     ): \Magento\Catalog\Model\ResourceModel\Product\Collection {
         $productCollection = $this->productCollectionFactory->create();
         $productCollection->addAttributeToSelect(ProductInterface::NAME);
-        $productCollection->setVisibility($this->catalogVisibility->getVisibleInCatalogIds());
         $productCollection->setPage($pageNum, $limit);
         $this->filter->addFilter($productCollection, 'fulltext', ['fulltext' => $searchKey]);
         $productCollection->setPage($pageNum, $limit);
