c35ac36f888de3aba4c755c617d3c58a7cd9da4b
Sergey Dovbenko <sdovbenko@magecom.us>
2019-08-29 19:46:54 +0000

    Corrected code styles

--- dev/tests/integration/testsuite/Magento/Customer/_files/customer_subscribe_rollback.php
+++ dev/tests/integration/testsuite/Magento/Customer/_files/customer_subscribe_rollback.php
@@ -1,14 +1,15 @@
 <?php
 /**
-* Copyright © Magento, Inc. All rights reserved.
-* See COPYING.txt for license details.
-*/
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
 // TODO: Should be removed in scope of https://github.com/magento/graphql-ce/issues/167
 
 declare(strict_types=1);
 
 use Magento\Framework\App\Config\Storage\WriterInterface;
 use Magento\TestFramework\Helper\Bootstrap;
+
 $objectManager = Bootstrap::getObjectManager();
 /** @var Writer  $configWriter */
 $configWriter = $objectManager->create(WriterInterface::class);
