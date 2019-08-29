50acd4f556ec313e4ad1ec6efc1739af4e8e9dbe
Dmytro Horytskyi <horytsky@adobe.com>
2019-08-28 20:04:20 -0500

    MC-19712: Full page cache issue with multiple stores

--- app/code/Magento/Store/App/Action/Plugin/Context.php
+++ app/code/Magento/Store/App/Action/Plugin/Context.php
@@ -13,6 +13,8 @@ use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\Exception\NotFoundException;
 use Magento\Store\Api\Data\StoreInterface;
 use Magento\Store\Api\StoreCookieManagerInterface;
+use Magento\Store\Model\ScopeInterface;
+use Magento\Store\Model\StoreManager;
 use Magento\Store\Model\StoreManagerInterface;
 
 /**
@@ -83,36 +85,37 @@ class Context
         );
         if (is_array($storeCode)) {
             if (!isset($storeCode['_data']['code'])) {
-                $this->processInvalidStoreRequested();
+                $this->processInvalidStoreRequested($request);
             }
             $storeCode = $storeCode['_data']['code'];
         }
         if ($storeCode === '') {
             //Empty code - is an invalid code and it was given explicitly
             //(the value would be null if the code wasn't found).
-            $this->processInvalidStoreRequested();
+            $this->processInvalidStoreRequested($request);
         }
         try {
             $currentStore = $this->storeManager->getStore($storeCode);
+            $this->updateContext($request, $currentStore);
         } catch (NoSuchEntityException $exception) {
-            $this->processInvalidStoreRequested($exception);
+            $this->processInvalidStoreRequested($request, $exception);
         }
-
-        $this->updateContext($currentStore);
     }
 
     /**
      * Take action in case of invalid store requested.
      *
+     * @param RequestInterface $request
      * @param \Throwable|null $previousException
      * @return void
      * @throws NotFoundException
      */
     private function processInvalidStoreRequested(
+        RequestInterface $request,
         \Throwable $previousException = null
     ) {
         $store = $this->storeManager->getStore();
-        $this->updateContext($store);
+        $this->updateContext($request, $store);
 
         throw new NotFoundException(
             $previousException
@@ -125,16 +128,28 @@ class Context
     /**
      * Update context accordingly to the store found.
      *
+     * @param RequestInterface $request
      * @param StoreInterface $store
      * @return void
      * @throws \Magento\Framework\Exception\LocalizedException
      */
-    private function updateContext(StoreInterface $store)
+    private function updateContext(RequestInterface $request, StoreInterface $store)
     {
+        switch (true) {
+            case $store->isUseStoreInUrl():
+                $defaultStoreCode = $store->getCode();
+                break;
+            case ScopeInterface::SCOPE_STORE == $request->getServerValue(StoreManager::PARAM_RUN_TYPE):
+                $defaultStoreCode = $request->getServerValue(StoreManager::PARAM_RUN_CODE);
+                break;
+            default:
+                $defaultStoreCode = $this->storeManager->getDefaultStoreView()->getCode();
+                break;
+        }
         $this->httpContext->setValue(
             StoreManagerInterface::CONTEXT_STORE,
             $store->getCode(),
-            $store->isUseStoreInUrl() ? $store->getCode() : $this->storeManager->getDefaultStoreView()->getCode()
+            $defaultStoreCode
         );
 
         /** @var StoreInterface $defaultStore */
