09cd623cc03f28b2fafdc9f497e8c606c45de23e
Stanislav Idolov <sidolov@adobe.com>
2019-09-04 12:17:00 -0500

    Fixed code style

--- app/code/Magento/Bundle/Ui/DataProvider/Product/Form/Modifier/BundlePrice.php
+++ app/code/Magento/Bundle/Ui/DataProvider/Product/Form/Modifier/BundlePrice.php
@@ -64,7 +64,7 @@ class BundlePrice extends AbstractModifier
             $this->arrayManager->findPath(
                 ProductAttributeInterface::CODE_PRICE,
                 $meta,
-                self::DEFAULT_GENERAL_PANEL.'/children',
+                self::DEFAULT_GENERAL_PANEL . '/children',
                 'children'
             ) . static::META_CONFIG_PATH,
             $meta,
