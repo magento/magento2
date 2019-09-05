739fe4dc041d9721befb8ee2a4c783adadd57734
Evgeny Petrov <evgeny_petrov@epam.com>
2019-09-05 11:32:02 +0300

    MC-15759: Elasticsearch: Searches That Contain Question Mark Followed by Semicolon Will Result In Error Page (Multiple Queries Error)

--- app/code/Magento/Elasticsearch/SearchAdapter/Query/Builder/Match.php
+++ app/code/Magento/Elasticsearch/SearchAdapter/Query/Builder/Match.php
@@ -3,8 +3,6 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
-declare(strict_types=1);
-
 namespace Magento\Elasticsearch\SearchAdapter\Query\Builder;
 
 use Magento\Elasticsearch\Model\Adapter\FieldMapper\Product\AttributeProvider;
