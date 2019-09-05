686a71cc69a79a6d48ff2e92e556df3cf5234709
Serhiy Yelahin <serhiy.yelahin@transoftgroup.com>
2019-09-05 14:17:18 +0300

    MC-19716: Cannot change action settings of scheduled update for cart rule

--- dev/tests/integration/testsuite/Magento/SalesRule/Controller/Adminhtml/Promo/Quote/NewActionHtmlTest.php
+++ dev/tests/integration/testsuite/Magento/SalesRule/Controller/Adminhtml/Promo/Quote/NewActionHtmlTest.php
@@ -7,14 +7,30 @@ declare(strict_types=1);
 
 namespace Magento\SalesRule\Controller\Adminhtml\Promo\Quote;
 
-use Magento\TestFramework\Helper\Bootstrap;
 use Magento\TestFramework\TestCase\AbstractBackendController;
 
 /**
  * New action html test
+ *
+ * @magentoAppArea adminhtml
  */
 class NewActionHtmlTest extends AbstractBackendController
 {
+    /**
+     * @var string
+     */
+    protected $resource = 'Magento_SalesRule::quote';
+
+    /**
+     * @var string
+     */
+    protected $uri = 'backend/sales_rule/promo_quote/newActionHtml';
+
+    /**
+     * @var string
+     */
+    private $formName = 'test_form';
+
     /**
      * Test verifies that execute method has the proper data-form-part value in html response
      *
@@ -22,20 +38,44 @@ class NewActionHtmlTest extends AbstractBackendController
      */
     public function testExecute(): void
     {
-        $formName = 'test_form';
+        $this->prepareRequest();
+        $this->dispatch($this->uri);
+        $html = $this->getResponse()
+            ->getBody();
+        $this->assertContains($this->formName, $html);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function testAclHasAccess()
+    {
+        $this->prepareRequest();
+        parent::testAclHasAccess();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function testAclNoAccess()
+    {
+        $this->prepareRequest();
+        parent::testAclNoAccess();
+    }
+
+    /**
+     * Prepare request
+     *
+     * @return void
+     */
+    private function prepareRequest(): void
+    {
         $this->getRequest()->setParams(
             [
                 'id' => 1,
-                'form_namespace' => $formName,
+                'form_namespace' => $this->formName,
                 'type' => 'Magento\SalesRule\Model\Rule\Condition\Product|quote_item_price',
             ]
-        );
-        $objectManager = Bootstrap::getObjectManager();
-        /** @var NewActionHtml $controller */
-        $controller = $objectManager->create(NewActionHtml::class);
-        $controller->execute();
-        $html = $this->getResponse()
-            ->getBody();
-        $this->assertContains($formName, $html);
+        )->setMethod('POST');
     }
 }
