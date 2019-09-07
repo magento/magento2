5847d9f64dad423d83bf0c742d810783ca213639
Eden <quocviet312@gmail.com>
2019-09-07 19:13:14 +0700

    Fix static test issue 24491

--- app/code/Magento/Checkout/view/frontend/web/js/shopping-cart.js
+++ app/code/Magento/Checkout/view/frontend/web/js/shopping-cart.js
@@ -14,10 +14,11 @@ define([
         _create: function () {
             var items, i, reload;
 
-            $(this.options.emptyCartButton).on('click', $.proxy(function (event) {
+            $(this.options.emptyCartButton).on('click', $.proxy(function () {
                 $(this.options.emptyCartButton).attr('name', 'update_cart_action_temp');
                 $(this.options.updateCartActionContainer)
                     .attr('name', 'update_cart_action').attr('value', 'empty_cart');
+
                 if ($(this.options.emptyCartButton).parents('form').length > 0) {
                     $(this.options.emptyCartButton).parents('form').submit();
                 }
