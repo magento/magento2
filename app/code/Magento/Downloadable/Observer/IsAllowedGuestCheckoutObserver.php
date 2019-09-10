9f37b2f3a283735257b8d3ff6df8132b1e7b68d8
Rani Priya <rani.priya439@webkul.com>
2019-09-10 16:10:30 +0530

    item wise check added for shareable links

--- app/code/Magento/Downloadable/Observer/IsAllowedGuestCheckoutObserver.php
+++ app/code/Magento/Downloadable/Observer/IsAllowedGuestCheckoutObserver.php
@@ -8,9 +8,6 @@ namespace Magento\Downloadable\Observer;
 use Magento\Framework\Event\ObserverInterface;
 use Magento\Store\Model\ScopeInterface;
 
-/**
- * Check is allowed guest checkout for downloadable items.
- */
 class IsAllowedGuestCheckoutObserver implements ObserverInterface
 {
     /**
@@ -21,7 +18,7 @@ class IsAllowedGuestCheckoutObserver implements ObserverInterface
     /**
      *  Xml path to get downloadable Shareable setting
      */
-    private const XML_PATH_DOWNLOADABLE_SHAREABLE = 'catalog/downloadable/shareable';
+    const XML_PATH_DOWNLOADABLE_SHAREABLE = 'catalog/downloadable/shareable';
 
     /**
      * Core store config
@@ -31,12 +28,21 @@ class IsAllowedGuestCheckoutObserver implements ObserverInterface
     protected $_scopeConfig;
 
     /**
+     * Downloadable link collection factory
+     *
+     * @var \Magento\Downloadable\Model\ResourceModel\Link\CollectionFactory
+     */
+    protected $_linksFactory;
+
+    /**
      * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
      */
     public function __construct(
-        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Downloadable\Model\ResourceModel\Link\CollectionFactory $linksFactory
     ) {
         $this->_scopeConfig = $scopeConfig;
+        $this->_linksFactory = $linksFactory;
     }
 
     /**
@@ -50,19 +56,6 @@ class IsAllowedGuestCheckoutObserver implements ObserverInterface
         $store = $observer->getEvent()->getStore();
         $result = $observer->getEvent()->getResult();
 
-        if (!$this->_scopeConfig->isSetFlag(
-            self::XML_PATH_DISABLE_GUEST_CHECKOUT,
-            ScopeInterface::SCOPE_STORE,
-            $store
-        ) && $this->_scopeConfig->isSetFlag(
-            self::XML_PATH_DOWNLOADABLE_SHAREABLE,
-            ScopeInterface::SCOPE_STORE,
-            $store
-        )
-        ) {
-            return $this;
-        }
-
         /* @var $quote \Magento\Quote\Model\Quote */
         $quote = $observer->getEvent()->getQuote();
 
@@ -70,11 +63,45 @@ class IsAllowedGuestCheckoutObserver implements ObserverInterface
             if (($product = $item->getProduct())
                 && $product->getTypeId() == \Magento\Downloadable\Model\Product\Type::TYPE_DOWNLOADABLE
             ) {
-                $result->setIsAllowed(false);
-                break;
+                if ($this->_scopeConfig->isSetFlag(
+                    self::XML_PATH_DISABLE_GUEST_CHECKOUT,
+                    ScopeInterface::SCOPE_STORE,
+                    $store
+                ) || !$this->checkForShareableLinks($item)) {
+                    $result->setIsAllowed(false);
+                    break;
+                }
             }
         }
-
         return $this;
+    }
+
+    /**
+     * Check for shareable link
+     *
+     * @param \Magento\Quote\Api\Data\CartItemInterface $item
+     * @return boolean
+     */
+    private function checkForShareableLinks($item)
+    {
+        $isSharable = true;
+        $option = $item->getOptionByCode('downloadable_link_ids');
+        if (!empty($option)) {
+            $downloadableLinkIds = explode(',', $option->getValue());
+            $links = $this->linksFactory->create()->addFieldToFilter("link_id", ["in" => $downloadableLinkIds]);
+            foreach ($links as $link) {
+                if (!$link->getIsShareable() ||
+                    ($link->getIsShareable() == 2 && !$this->_scopeConfig->isSetFlag(
+                        self::XML_PATH_DOWNLOADABLE_SHAREABLE,
+                        ScopeInterface::SCOPE_STORE,
+                        $store
+                    )
+                    )
+                ) {
+                    $isSharable = false;
+                }
+            }
+        }
+        return $isSharable;
     }
 }
