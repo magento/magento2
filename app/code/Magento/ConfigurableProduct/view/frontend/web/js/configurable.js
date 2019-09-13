86595d640c7881d98e5f28b4e2ed48ab9b47356d
mohith227 <mohithnanjundamurthy05@gmail.com>
2019-09-14 01:05:41 +0530

    configurable product's associate are not selected issue fix

--- app/code/Magento/ConfigurableProduct/view/frontend/web/js/configurable.js
+++ app/code/Magento/ConfigurableProduct/view/frontend/web/js/configurable.js
@@ -432,9 +432,15 @@ define([
                         element.options[index].config = options[i];
                         index++;
                     }
-
+                    if (i == 0) {
+                        this.options.values[attributeId] = options[i].id;
+                    }
                     /* eslint-enable max-depth */
                 }
+                if (window.location.href.indexOf('#') !== -1) {
+                    this._parseQueryParams(window.location.href.substr(window.location.href.indexOf('#') + 1));
+                }
+
             }
         },
 
