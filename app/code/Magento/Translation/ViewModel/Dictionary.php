95d5d29392665d5b635bc0b42605c90309251496
Dmytro Poperechnyy <dpoperechnyy@magento.com>
2019-09-13 10:06:28 -0500

    MC-19247: Broken translations with advanced bundling

--- app/code/Magento/Translation/ViewModel/Dictionary.php
+++ app/code/Magento/Translation/ViewModel/Dictionary.php
@@ -6,11 +6,14 @@
 
 namespace Magento\Translation\ViewModel;
 
+use Magento\Framework\App\Filesystem\DirectoryList;
 use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Filesystem;
 use Magento\Framework\View\Asset\File\NotFoundException;
 use Magento\Framework\View\Asset\Repository as AssetRepository;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
 use Magento\Translation\Model\Js\Config as JsConfig;
+use Magento\Framework\App\State as AppState;
 
 /**
  * View model responsible for getting translate dictionary file content for the layout.
@@ -23,12 +26,28 @@ class Dictionary implements ArgumentInterface
     private $assetRepo;
 
     /**
+     * @var AppState
+     */
+    private $appState;
+
+    /**
+     * @var Filesystem
+     */
+    private $filesystem;
+
+    /**
      * @param AssetRepository $assetRepo
+     * @param AppState $appState
+     * @param Filesystem $filesystem
      */
     public function __construct(
-        AssetRepository $assetRepo
+        AssetRepository $assetRepo,
+        AppState $appState,
+        Filesystem $filesystem
     ) {
         $this->assetRepo = $assetRepo;
+        $this->appState = $appState;
+        $this->filesystem = $filesystem;
     }
 
     /**
@@ -38,11 +57,23 @@ class Dictionary implements ArgumentInterface
      */
     public function getTranslationDictionary(): string
     {
-        try {
-            $asset = $this->assetRepo->createAsset(JsConfig::DICTIONARY_FILE_NAME);
-            $content = $asset->getContent();
-        } catch (LocalizedException | NotFoundException $e) {
-            $content = '';
+        if ($this->appState->getMode() === AppState::MODE_PRODUCTION) {
+            try {
+                $asset = $this->assetRepo->createAsset(JsConfig::DICTIONARY_FILE_NAME);
+                $staticViewFilePath = $this->filesystem->getDirectoryRead(
+                    DirectoryList::STATIC_VIEW
+                )->getAbsolutePath();
+                $content = file_get_contents($staticViewFilePath . $asset->getPath());
+            } catch (LocalizedException $e) {
+                $content = '';
+            }
+        } else {
+            try {
+                $asset = $this->assetRepo->createAsset(JsConfig::DICTIONARY_FILE_NAME);
+                $content = $asset->getContent();
+            } catch (LocalizedException | NotFoundException $e) {
+                $content = '';
+            }
         }
 
         return $content;
