3aeaacffbcc043396f82af1dbffed47c20e256ef
Dmytro Poperechnyy <dpoperechnyy@magento.com>
2019-09-17 14:21:27 -0500

    MC-19247: Broken translations with advanced bundling
    - Add json parse to dictionary file content in template;

--- dev/tests/js/jasmine/tests/lib/mage/fake-dictionary.js
+++ dev/tests/js/jasmine/tests/lib/mage/fake-dictionary.js
@@ -2,7 +2,7 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
-// Fake dictionary for js unit tests as real dictionary is defined is template and cannot be initialized by requirejs
+// Fake dictionary for js unit tests as real dictionary is defined in template and cannot be initialized by requirejs
 define([], function () {
     'use strict';
 
