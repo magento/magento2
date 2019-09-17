8632e6df4f4146e230bbb5b13432d8af797eeb9a
Roman Lytvynenko <lytvynen@adobe.com>
2019-09-17 09:58:15 -0500

    MC-19661: Export Products bug. I can't exclude attributes from CSV file.

--- app/code/Magento/CatalogImportExport/Model/Export/Product.php
+++ app/code/Magento/CatalogImportExport/Model/Export/Product.php
@@ -717,11 +717,11 @@ class Product extends \Magento\ImportExport\Model\Export\Entity\AbstractEntity
 
      * @return array
      */
-    private function getNonSystemAttributes()
+    private function getNonSystemAttributes(): array
     {
         $attrKeys = [];
         foreach ($this->filterAttributeCollection($this->getAttributeCollection()) as $attribute) {
-            $attrKeys []= $attribute->getAttributeCode();
+            $attrKeys[] = $attribute->getAttributeCode();
         }
 
         return array_diff($this->_getExportMainAttrCodes(), $this->_customHeadersMapping($attrKeys));
