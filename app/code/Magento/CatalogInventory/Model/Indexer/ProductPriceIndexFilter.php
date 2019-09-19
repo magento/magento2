4b1b1dc7dee1832ba2ff3598c6c0afb766941bda
bruce.ayko <bruce@ayko.com>
2019-09-19 20:34:02 +0100

    Price Indexer Performance Issue With Out of Stock Products - Relates to issue https://github.com/magento/magento2/issues/24414 - Made changes based on feedback on PR

--- app/code/Magento/CatalogInventory/Model/Indexer/ProductPriceIndexFilter.php
+++ app/code/Magento/CatalogInventory/Model/Indexer/ProductPriceIndexFilter.php
@@ -104,13 +104,7 @@ class ProductPriceIndexFilter implements PriceModifierInterface
             $select->where('stock_item.use_config_manage_stock = 0 AND stock_item.manage_stock = 1');
         }
 
-        if ($entityIds !== null) {
-            if (count($entityIds) > 1) {
-                $select->where('stock_item.product_id in (?)', $entityIds);
-            } else {
-                $select->where('stock_item.product_id = ?', $entityIds);
-            }
-        }
+        $select->where('stock_item.product_id in (?)', $entityIds);
 
         $select->group('stock_item.product_id');
         $select->having('max_is_in_stock = 0');
