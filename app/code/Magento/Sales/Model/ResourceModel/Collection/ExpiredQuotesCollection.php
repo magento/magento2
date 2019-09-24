9c9172f2e5ecf68257fc14968b0775714418e10c
Oleksandr Dubovyk <odubovyk@magento.com>
2019-09-24 17:10:25 -0500

    MC-20212: [On premise] Data loss due to core cron job called sales_clean_quotes
    - small changes

--- app/code/Magento/Sales/Model/ResourceModel/Collection/ExpiredQuotesCollection.php
+++ app/code/Magento/Sales/Model/ResourceModel/Collection/ExpiredQuotesCollection.php
@@ -3,6 +3,8 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types=1);
+
 namespace Magento\Sales\Model\ResourceModel\Collection;
 
 use Magento\Framework\App\Config\ScopeConfigInterface;
@@ -57,7 +59,7 @@ class ExpiredQuotesCollection
      * @param StoreInterface $store
      * @return Collection
      */
-    public function getExpiredQuotes(StoreInterface $store)
+    public function getExpiredQuotes(StoreInterface $store): Collection
     {
         $lifetime = $this->config->getValue(
             $this->quoteLifetime,
