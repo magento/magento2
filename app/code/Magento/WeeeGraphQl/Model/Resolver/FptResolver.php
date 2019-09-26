21699ee749483c5dbe7fab04f368a8e6ae095013
Cristian Partica <cpartica@magento.com>
2019-09-26 16:50:11 -0500

    MC-20244: Pricing :: FPT calculation
    - initial schema

---
+++ app/code/Magento/WeeeGraphQl/Model/Resolver/FptResolver.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\CatalogGraphQl\Model\Resolver;
+
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+
+class FptResolver implements ResolverInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public function resolve(
+        Field $field,
+        $context,
+        ResolveInfo $info,
+        array $value = null,
+        array $args = null
+    ) {
+
+        return [];
+    }
+}
