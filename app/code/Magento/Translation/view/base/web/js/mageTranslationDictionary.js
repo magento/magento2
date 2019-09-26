8e3f3ace6f9303f4b7a404ae7e08ac917934300b
Dmytro Poperechnyy <dpoperechnyy@magento.com>
2019-09-26 14:44:50 -0500

    MC-19247: Broken translations with advanced bundling
    - Load translation dictionary from js component;

--- app/code/Magento/Translation/view/base/web/js/mageTranslationDictionary.js
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
- */
-
-define(['text!js-translation.json'],
-    function (dict) {
-        return JSON.parse(dict);
-});
