218c16459e07c0b072c2949cae1a9f0b9f0a7754
Ivan Koliadynskyy <i.koliadynskyy@gmail.com>
2019-09-27 17:35:07 +0300

    https://github.com/magento/magento2/issues/24716 Not Able to set Rest API oAuth admin token expire in minutes or seconds. #24716

--- app/code/Magento/Webapi/Model/Authorization/TokenUserContext.php
+++ app/code/Magento/Webapi/Model/Authorization/TokenUserContext.php
@@ -137,7 +137,7 @@ class TokenUserContext implements UserContextInterface
             return false;
         }
 
-        if ($this->dateTime->strToTime($token->getCreatedAt()) < ($this->date->gmtTimestamp() - $tokenTtl * 3600)) {
+        if ($this->date->timestamp($token->getCreatedAt()) < ($this->date->gmtTimestamp() - $tokenTtl * 3600)) {
             return true;
         }
 
