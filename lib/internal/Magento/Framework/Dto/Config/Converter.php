6fcd68c91d0865d2a8a0d6a0ede6657f1375cfef
Riccardo Tempesta <riccardo.tempesta@gmail.com>
2019-09-27 09:39:35 +0200

    Config change for isolation support

--- lib/internal/Magento/Framework/Dto/Config/Converter.php
+++ lib/internal/Magento/Framework/Dto/Config/Converter.php
@@ -6,9 +6,13 @@ namespace Magento\Framework\Dto\Config;
 use DOMDocument;
 use DOMNode;
 use DOMNodeList;
+use InvalidArgumentException;
 use Magento\Framework\Api\SimpleDataObjectConverter;
 use Magento\Framework\Config\ConverterInterface;
 
+/**
+ * Configuration converter class for DTO definition
+ */
 class Converter implements ConverterInterface
 {
     /**
@@ -29,20 +33,32 @@ class Converter implements ConverterInterface
 
         /** @var DOMNode $dto */
         foreach ($dtos as $dto) {
-            $dtoClass = $dto->getAttribute('class');
-            $isMutable = filter_var($dto->getAttribute('mutable'), FILTER_VALIDATE_BOOLEAN);
+            $dtoId = $dto->getAttribute('id');
 
-            $interface = $dto->getAttribute('interface');
+            $dtoClassNodes = $dto->getElementsByTagName('class');
+            $dtoInterfaceNodes = $dto->getElementsByTagName('interface');
+
+            if (count($dtoClassNodes) !== 1) {
+                throw new InvalidArgumentException('DTO ' . $dtoId . ' must have a class definition');
+            }
+            if (count($dtoInterfaceNodes) !== 1) {
+                throw new InvalidArgumentException('DTO ' . $dtoId . ' must have an interface definition');
+            }
+
+            $dtoClass = $dtoClassNodes[0]->nodeValue;
+            $dtoInterface = $dtoInterfaceNodes[0]->nodeValue;
+
+            $isMutable = filter_var($dto->getAttribute('mutable'), FILTER_VALIDATE_BOOLEAN);
 
             $output[$dtoClass] = [
                 'mutable' => $isMutable,
                 'type' => 'class',
-                'interface' => $dto->getAttribute('interface'),
+                'interface' => $dtoInterface,
                 'properties' => []
             ];
 
-            if ($interface) {
-                $output[$interface] = [
+            if ($dtoInterface) {
+                $output[$dtoInterface] = [
                     'type' => 'interface',
                     'mutable' => $isMutable,
                     'class' => $dtoClass,
@@ -63,8 +79,8 @@ class Converter implements ConverterInterface
                     'nullable' => $propertyNullable
                 ];
 
-                if ($interface) {
-                    $output[$interface]['properties'][$propertyId] = [
+                if ($dtoInterface) {
+                    $output[$dtoInterface]['properties'][$propertyId] = [
                         'type' => $propertyType,
                         'optional' => $propertyOptional,
                         'nullable' => $propertyNullable
