645819483f32506c0540f036633eb181aeb7685b
Stanislav Ilnytskyi <stailx1@gmail.com>
2019-09-27 15:58:03 +0200

    #23180 attempt to fix final_price, price indexing for configurable product

--- app/code/Magento/ConfigurableProduct/Model/ResourceModel/Product/Indexer/Price/Configurable.php
+++ app/code/Magento/ConfigurableProduct/Model/ResourceModel/Product/Indexer/Price/Configurable.php
@@ -248,6 +248,8 @@ class Configurable implements DimensionalIndexerInterface
         // adds price of custom option, that was applied in DefaultPrice::_applyCustomOption
         $selectForCrossUpdate->columns(
             [
+                'price' => new \Zend_Db_Expr('i.max_price - i.price + io.max_price'),
+                'final_price' => new \Zend_Db_Expr('i.min_price - i.price + io.min_price'),
                 'min_price' => new \Zend_Db_Expr('i.min_price - i.price + io.min_price'),
                 'max_price' => new \Zend_Db_Expr('i.max_price - i.price + io.max_price'),
                 'tier_price' => 'io.tier_price',
