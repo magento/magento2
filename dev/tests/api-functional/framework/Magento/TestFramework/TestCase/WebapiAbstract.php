13ca551d457546c5b9e2d95ddefd66fb3eead53d
Valerii Naida <vnayda@adobe.com>
2019-09-30 11:41:54 -0500

    #async-import/issues/207: Provide the ability to check errors in WebAPI tests in a single approach for different adapters (SOAP/REST)

--- dev/tests/api-functional/framework/Magento/TestFramework/TestCase/WebapiAbstract.php
+++ dev/tests/api-functional/framework/Magento/TestFramework/TestCase/WebapiAbstract.php
@@ -7,6 +7,7 @@ namespace Magento\TestFramework\TestCase;
 
 use Magento\Framework\App\Filesystem\DirectoryList;
 use Magento\Framework\Filesystem;
+use Magento\Framework\Webapi\Exception as WebapiException;
 use Magento\Webapi\Model\Soap\Fault;
 use Magento\TestFramework\Helper\Bootstrap;
 
@@ -671,6 +672,12 @@ abstract class WebapiAbstract extends \PHPUnit\Framework\TestCase
      */
     private function getActualWrappedErrors(\stdClass $errorNode)
     {
+        if (!isset($errorNode->parameters)) {
+            return [
+                'message' => $errorNode->message,
+            ];
+        }
+
         $actualParameters = [];
         $parameterNode = $errorNode->parameters->parameter;
         if (is_array($parameterNode)) {
@@ -685,5 +692,41 @@ abstract class WebapiAbstract extends \PHPUnit\Framework\TestCase
             // Can not rename on parameters due to Backward Compatibility
             'params' => $actualParameters,
         ];
+    }
+
+    /**
+     * @param array $serviceInfo
+     * @param array $data
+     * @param array $expectedErrorData
+     * @return void
+     * @throws \Exception
+     */
+    protected function assertWebApiCallErrors(array $serviceInfo, array $data, array $expectedErrorData)
+    {
+        try {
+            $this->_webApiCall($serviceInfo, $data);
+            $this->fail('Expected throwing exception');
+        } catch (\Exception $e) {
+            if (TESTS_WEB_API_ADAPTER === self::ADAPTER_REST) {
+                self::assertEquals($expectedErrorData, $this->processRestExceptionResult($e));
+                self::assertEquals(WebapiException::HTTP_BAD_REQUEST, $e->getCode());
+            } elseif (TESTS_WEB_API_ADAPTER === self::ADAPTER_SOAP) {
+                $this->assertInstanceOf('SoapFault', $e);
+                $expectedWrappedErrors = [];
+                foreach ($expectedErrorData['errors'] as $error) {
+                    // @see \Magento\TestFramework\TestCase\WebapiAbstract::getActualWrappedErrors()
+                    $expectedWrappedError = [
+                        'message' => $error['message'],
+                    ];
+                    if (isset($error['parameters'])) {
+                        $expectedWrappedError['params'] = $error['parameters'];
+                    }
+                    $expectedWrappedErrors[] = $expectedWrappedError;
+                }
+                $this->checkSoapFault($e, $expectedErrorData['message'], 'env:Sender', [], $expectedWrappedErrors);
+            } else {
+                throw $e;
+            }
+        }
     }
 }
