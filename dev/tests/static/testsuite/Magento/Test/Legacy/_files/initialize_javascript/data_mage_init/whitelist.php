c416cbf8e8154a9061fa16e980f0e79ca7549128
Dmytro Poperechnyy <dpoperechnyy@magento.com>
2019-10-02 10:54:37 -0500

    MC-19248: Add static test to prevent regressing mage-init directive fixes made in the phtml files
    - Fix BIC changes;

--- dev/tests/static/testsuite/Magento/Test/Legacy/_files/initialize_javascript/data_mage_init/whitelist.php
+++ dev/tests/static/testsuite/Magento/Test/Legacy/_files/initialize_javascript/data_mage_init/whitelist.php
@@ -6,7 +6,8 @@
 /**
  * List of templates with data-mage-init attribute where JS component is not correctly called.
  *
- * JS component is initialized in php here. These templates cannot be refactored easily.
+ * JS component is initialized in php here. These templates cannot be refactored easily. This list consists of
+ * module name and template path within module.
  */
 return [
     ['Magento_Braintree', 'view/frontend/templates/paypal/button_shopping_cart.phtml']
