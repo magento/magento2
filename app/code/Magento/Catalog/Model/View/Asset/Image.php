6496f4e2bff89ee709f45cc16f3f64d31c971cdf
Roman Kabanov <roman@convert.no>
2019-10-04 09:45:56 +0300

    Correct image params - allow to disable image frame, fix image params hash

--- app/code/Magento/Catalog/Model/View/Asset/Image.php
+++ app/code/Magento/Catalog/Model/View/Asset/Image.php
@@ -200,11 +200,11 @@ class Image implements LocalInterface
         $miscParams['image_width'] = 'w:' . ($miscParams['image_width'] ?? 'empty');
         $miscParams['quality'] = 'q:' . ($miscParams['quality'] ?? 'empty');
         $miscParams['angle'] = 'r:' . ($miscParams['angle'] ?? 'empty');
-        $miscParams['keep_aspect_ratio'] = (isset($miscParams['keep_aspect_ratio']) ? '' : 'non') . 'proportional';
-        $miscParams['keep_frame'] = (isset($miscParams['keep_frame']) ? '' : 'no') . 'frame';
-        $miscParams['keep_transparency'] = (isset($miscParams['keep_transparency']) ? '' : 'no') . 'transparency';
-        $miscParams['constrain_only'] = (isset($miscParams['constrain_only']) ? 'do' : 'not') . 'constrainonly';
-        $miscParams['background'] = isset($miscParams['background'])
+        $miscParams['keep_aspect_ratio'] = (!empty($miscParams['keep_aspect_ratio']) ? '' : 'non') . 'proportional';
+        $miscParams['keep_frame'] = (!empty($miscParams['keep_frame']) ? '' : 'no') . 'frame';
+        $miscParams['keep_transparency'] = (!empty($miscParams['keep_transparency']) ? '' : 'no') . 'transparency';
+        $miscParams['constrain_only'] = (!empty($miscParams['constrain_only']) ? 'do' : 'not') . 'constrainonly';
+        $miscParams['background'] = !empty($miscParams['background'])
             ? 'rgb' . implode(',', $miscParams['background'])
             : 'nobackground';
         return $miscParams;
