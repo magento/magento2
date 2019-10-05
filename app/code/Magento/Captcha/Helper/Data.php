e05cb682e61c4833c71b300f389dee46cb728d7c
Gaurav Agarwal <37572719+gauravagarwal1001@users.noreply.github.com>
2019-10-05 11:50:48 +0530

    Update Data.php

--- app/code/Magento/Captcha/Helper/Data.php
+++ app/code/Magento/Captcha/Helper/Data.php
@@ -3,46 +3,111 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+namespace Magento\Captcha\Helper;
 
-/**
- * Captcha helper for adminhtml area
- *
- * @author     Magento Core Team <core@magentocommerce.com>
- */
-namespace Magento\Captcha\Helper\Adminhtml;
-
+use Magento\Framework\App\Filesystem\DirectoryList;
+use Magento\Framework\Filesystem;
+use Magento\Framework\Filesystem\DriverInterface;
 use Magento\Store\Model\ScopeInterface;
 
-class Data extends \Magento\Captcha\Helper\Data
+/**
+ * Captcha image model
+ *
+ * @api
+ * @since 100.0.2
+ */
+class Data extends \Magento\Framework\App\Helper\AbstractHelper
 {
-    // Configuration path
-    const XML_PATH_ENABLED_BACKEND = 'msp_securitysuite_recaptcha/backend/enabled';
-    
     /**
-     * @var \Magento\Backend\App\ConfigInterface
+     * Used for "name" attribute of captcha's input field
      */
-    protected $_backendConfig;
+    const INPUT_NAME_FIELD_VALUE = 'captcha';
+
+    /**
+     * Always show captcha
+     */
+    const MODE_ALWAYS = 'always';
+
+    /**
+     * Show captcha only after certain number of unsuccessful attempts
+     */
+    const MODE_AFTER_FAIL = 'after_fail';
+
+    /**
+     * Captcha fonts path
+     */
+    const XML_PATH_CAPTCHA_FONTS = 'captcha/fonts';
+
+    /**
+     * Default captcha type
+     */
+    const DEFAULT_CAPTCHA_TYPE = 'Zend';
+    
+    // Configuration path
+    const XML_PATH_ENABLED_FRONTEND = 'msp_securitysuite_recaptcha/frontend/enabled';
+
+    /**
+     * List uses Models of Captcha
+     * @var array
+     */
+    protected $_captcha = [];
+
+    /**
+     * @var Filesystem
+     */
+    protected $_filesystem;
+
+    /**
+     * @var \Magento\Store\Model\StoreManagerInterface
+     */
+    protected $_storeManager;
+
+    /**
+     * @var \Magento\Captcha\Model\CaptchaFactory
+     */
+    protected $_factory;
 
     /**
      * @param \Magento\Framework\App\Helper\Context $context
-     * @param \Magento\Store\Model\StoreManager $storeManager
-     * @param \Magento\Framework\Filesystem $filesystem
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+     * @param Filesystem $filesystem
      * @param \Magento\Captcha\Model\CaptchaFactory $factory
-     * @param \Magento\Backend\App\ConfigInterface $backendConfig
      */
     public function __construct(
         \Magento\Framework\App\Helper\Context $context,
-        \Magento\Store\Model\StoreManager $storeManager,
-        \Magento\Framework\Filesystem $filesystem,
-        \Magento\Captcha\Model\CaptchaFactory $factory,
-        \Magento\Backend\App\ConfigInterface $backendConfig
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        Filesystem $filesystem,
+        \Magento\Captcha\Model\CaptchaFactory $factory
     ) {
-        $this->_backendConfig = $backendConfig;
-        parent::__construct($context, $storeManager, $filesystem, $factory);
+        $this->_storeManager = $storeManager;
+        $this->_filesystem = $filesystem;
+        $this->_factory = $factory;
+        parent::__construct($context);
     }
 
     /**
-     * Returns config value for admin captcha
+     * Get Captcha
+     *
+     * @param string $formId
+     * @return \Magento\Captcha\Model\CaptchaInterface
+     */
+    public function getCaptcha($formId)
+    {
+        if (!array_key_exists($formId, $this->_captcha)) {
+            $captchaType = ucfirst($this->getConfig('type'));
+            if (!$captchaType) {
+                $captchaType = self::DEFAULT_CAPTCHA_TYPE;
+            } elseif ($captchaType == 'Default') {
+                $captchaType = $captchaType . 'Model';
+            }
+
+            $this->_captcha[$formId] = $this->_factory->create($captchaType, $formId);
+        }
+        return $this->_captcha[$formId];
+    }
+
+    /**
+     * Returns config value
      *
      * @param string $key The last part of XML_PATH_$area_CAPTCHA_ constant (case insensitive)
      * @param \Magento\Store\Model\Store $store
@@ -50,7 +115,49 @@ class Data extends \Magento\Captcha\Helper\Data
      */
     public function getConfig($key, $store = null)
     {
-        return $this->_backendConfig->getValue('admin/captcha/' . $key);
+        return $this->scopeConfig->getValue(
+            'customer/captcha/' . $key,
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
+            $store
+        );
+    }
+
+    /**
+     * Get list of available fonts.
+     *
+     * Return format:
+     * [['arial'] => ['label' => 'Arial', 'path' => '/www/magento/fonts/arial.ttf']]
+     *
+     * @return array
+     */
+    public function getFonts()
+    {
+        $fontsConfig = $this->scopeConfig->getValue(\Magento\Captcha\Helper\Data::XML_PATH_CAPTCHA_FONTS, 'default');
+        $fonts = [];
+        if ($fontsConfig) {
+            $libDir = $this->_filesystem->getDirectoryRead(DirectoryList::LIB_INTERNAL);
+            foreach ($fontsConfig as $fontName => $fontConfig) {
+                $fonts[$fontName] = [
+                    'label' => $fontConfig['label'],
+                    'path' => $libDir->getAbsolutePath($fontConfig['path']),
+                ];
+            }
+        }
+        return $fonts;
+    }
+
+    /**
+     * Get captcha image directory
+     *
+     * @param mixed $website
+     * @return string
+     */
+    public function getImgDir($website = null)
+    {
+        $mediaDir = $this->_filesystem->getDirectoryWrite(DirectoryList::MEDIA);
+        $captchaDir = '/captcha/' . $this->_getWebsiteCode($website);
+        $mediaDir->create($captchaDir);
+        return $mediaDir->getAbsolutePath($captchaDir) . '/';
     }
 
     /**
@@ -61,19 +168,34 @@ class Data extends \Magento\Captcha\Helper\Data
      */
     protected function _getWebsiteCode($website = null)
     {
-        return 'admin';
+        return $this->_storeManager->getWebsite($website)->getCode();
+    }
+
+    /**
+     * Get captcha image base URL
+     *
+     * @param mixed $website
+     * @return string
+     */
+    public function getImgUrl($website = null)
+    {
+        return $this->_storeManager->getStore()->getBaseUrl(
+            DirectoryList::MEDIA
+        ) . 'captcha' . '/' . $this->_getWebsiteCode(
+            $website
+        ) . '/';
     }
     
      /**
-     * Check if reCaptcha is enable for backend
+     * Check if reCaptcha is enable for frontend
      *
      * @return bool
      */
     public function reCaptchaEnable()
     {
         return (bool) $this->scopeConfig->getValue(
-            XML_PATH_ENABLED_BACKEND,
-            ScopeInterface::SCOPE_STORE
+             XML_PATH_ENABLED_FRONTEND,
+             ScopeInterface::SCOPE_STORE
         );
     }
 }
