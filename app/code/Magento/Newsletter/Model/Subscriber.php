082e00785902b76419a0ab32e13626fbbc805d2b
Hwashiang Yu <hwyu@adobe.com>
2019-10-07 15:43:00 -0500

    MC-20550: Refactor CE and EE modules E-mail templates to use scalars variables
    - Updated all but getUrl varaibles in templates

--- app/code/Magento/Newsletter/Model/Subscriber.php
+++ app/code/Magento/Newsletter/Model/Subscriber.php
@@ -728,7 +728,7 @@ class Subscriber extends \Magento\Framework\Model\AbstractModel
                 'store' => $this->_storeManager->getStore()->getId(),
             ]
         )->setTemplateVars(
-            ['subscriber' => $this, 'store' => $this->_storeManager->getStore()]
+            ['subscriber_data' => ['confirmation_link' => $this->getConfirmationLink()], 'store' => $this->_storeManager->getStore()]
         )->setFrom(
             $this->_scopeConfig->getValue(
                 self::XML_PATH_CONFIRM_EMAIL_IDENTITY,
--- app/code/Magento/Wishlist/Controller/Index/Send.php
+++ app/code/Magento/Wishlist/Controller/Index/Send.php
@@ -259,7 +259,6 @@ class Send extends \Magento\Wishlist\Controller\AbstractIndex implements Action\
                             'items' => $this->getWishlistItems($resultLayout),
                             'viewOnSiteLink' => $this->_url->getUrl('*/shared/index', ['code' => $sharingCode]),
                             'message' => $message,
-                            'store' => $this->storeManager->getStore(),
                         ]
                     )->setFrom(
                         $this->scopeConfig->getValue(
