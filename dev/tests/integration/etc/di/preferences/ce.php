7de28074137014672621b4b452e6d9bd7d6edd13
Ivan Chepurnyi <ivan.chepurnyi@ecomdev.org>
2019-10-07 17:12:47 +0200

    Revert dummy locker for integration tests, as some tests run longer than database lock timeout

--- dev/tests/integration/etc/di/preferences/ce.php
+++ dev/tests/integration/etc/di/preferences/ce.php
@@ -28,7 +28,7 @@ return [
     \Magento\Framework\App\ResourceConnection\ConfigInterface::class =>
         \Magento\Framework\App\ResourceConnection\Config::class,
     \Magento\Framework\Lock\Backend\Cache::class =>
-        \Magento\Framework\Lock\Backend\Database::class,
+        \Magento\TestFramework\Lock\Backend\DummyLocker::class,
     \Magento\Framework\Session\SessionStartChecker::class => \Magento\TestFramework\Session\SessionStartChecker::class,
     \Magento\Framework\HTTP\AsyncClientInterface::class => \Magento\TestFramework\HTTP\AsyncClientInterfaceMock::class,
 ];
