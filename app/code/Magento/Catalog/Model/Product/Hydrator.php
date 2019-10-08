325ea6e845864497aa3c416cfd39611a09b0afb4
Oleksandr Dubovyk <odubovyk@magento.com>
2019-10-08 15:16:46 -0500

    MC-19013: Scheduled update is not created by the admin from not default site
    - fixed
    - added integration test

---
+++ app/code/Magento/Catalog/Model/Product/Hydrator.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Catalog\Model\Product;
+
+use Magento\Framework\EntityManager\HydratorInterface;
+
+/**
+ * Class is used to extract data and populate entity with data
+ */
+class Hydrator implements HydratorInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public function extract($entity)
+    {
+        return $entity->getData();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function hydrate($entity, array $data)
+    {
+        $lockedAttributes = $entity->getLockedAttributes();
+        $entity->unlockAttributes();
+        $entity->setData(array_merge($entity->getData(), $data));
+        foreach ($lockedAttributes as $attribute) {
+            $entity->lockAttribute($attribute);
+        }
+
+        return $entity;
+    }
+}
