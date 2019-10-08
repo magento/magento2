d5f961081f720bcde2f9aba9ea225d8ac0902152
Mahesh Singh <mahesh721@webkul.com>
2019-10-08 12:32:37 +0530

    Fixed customer_is_guest issue 24736

--- app/code/Magento/Quote/Model/Quote.php
+++ app/code/Magento/Quote/Model/Quote.php
@@ -853,8 +853,11 @@ class Quote extends AbstractExtensibleModel implements \Magento\Quote\Api\Data\C
             $this->setIsChanged(0);
         }
 
-        if ($this->_customer) {
+        if ($this->_customer && $this->_customer->getId()) {
             $this->setCustomerId($this->_customer->getId());
+            $this->setCustomerIsGuest(0);
+        } else {
+            $this->setCustomerIsGuest(1);
         }
 
         //mark quote if it has virtual products only
