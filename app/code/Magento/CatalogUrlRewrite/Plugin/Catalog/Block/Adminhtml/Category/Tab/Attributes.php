9ec7fdfe33353474c6d5f04cc5a63d6235fd4a27
Gaurav Agarwal <37572719+gauravagarwal1001@users.noreply.github.com>
2019-10-09 03:16:32 +0530

    Update Attributes.php

--- app/code/Magento/CatalogUrlRewrite/Plugin/Catalog/Block/Adminhtml/Category/Tab/Attributes.php
+++ app/code/Magento/CatalogUrlRewrite/Plugin/Catalog/Block/Adminhtml/Category/Tab/Attributes.php
@@ -3,6 +3,7 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+
 namespace Magento\CatalogUrlRewrite\Plugin\Catalog\Block\Adminhtml\Category\Tab;
 
 /**
@@ -11,8 +12,35 @@ namespace Magento\CatalogUrlRewrite\Plugin\Catalog\Block\Adminhtml\Category\Tab;
 class Attributes
 {
     /**
+     * XML path for category rewrites history
+     */
+    const XML_PATH_CATEGORY_REWRITES_HISTORY = 'catalog/seo/save_rewrites_history';
+
+    /**
+     * @var \Magento\Framework\App\Config\ScopeConfigInterface
+     */
+    protected $scopeConfig;
+
+    /**
+     * @var \Magento\Store\Model\StoreManagerInterface
+     */
+    protected $storeManager;
+
+    /**
+     * @param \Magento\Store\Model\StoreManagerInterface         $storeManager
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+     */
+    public function __construct(
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+    ) {
+        $this->storeManager = $storeManager;
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    /**
      * @param \Magento\Catalog\Model\Category\DataProvider $subject
-     * @param array $result
+     * @param array                                        $result
      *
      * @return array
      */
@@ -20,14 +48,24 @@ class Attributes
         \Magento\Catalog\Model\Category\DataProvider $subject,
         $result
     ) {
-        /** @var \Magento\Catalog\Model\Category $category */
+
+        /**
+ * @var \Magento\Catalog\Model\Category $category 
+*/
         $category = $subject->getCurrentCategory();
         if (isset($result['url_key'])) {
             if ($category && $category->getId()) {
                 if ($category->getLevel() == 1) {
                     $result['url_key_group']['componentDisabled'] = true;
                 } else {
-                    $result['url_key_create_redirect']['valueMap']['true'] = $category->getUrlKey();
+                    $storeId = $this->storeManager->getStore()->getId();
+                    $categoryRewritesHistory = (bool)$this->scopeConfig->getValue(
+                        self::XML_PATH_CATEGORY_REWRITES_HISTORY,
+                        \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
+                        $storeId
+                    );
+
+                    $result['url_key_create_redirect']['valueMap']['true'] = $categoryRewritesHistory ? $category->getUrlKey() : false;
                     $result['url_key_create_redirect']['value'] = $category->getUrlKey();
                     $result['url_key_create_redirect']['disabled'] = true;
                 }
