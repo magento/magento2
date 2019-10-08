95f2c640402c7cec553c9563272a11633fa97524
Tomash Khamlai <tomash.khamlai@gmail.com>
2019-10-08 15:17:22 +0300

    Cover GraphQlAuthenticationException "This account isn't confirmed. Verify and try again." Ignore coverage for functionality that is related to the DB constraint violations. Reformat lines for LiveCodeTest
    Signed-off-by: Tomash Khamlai <tomash.khamlai@gmail.com>

--- dev/tests/api-functional/testsuite/Magento/GraphQl/Customer/GetCustomerTest.php
+++ dev/tests/api-functional/testsuite/Magento/GraphQl/Customer/GetCustomerTest.php
@@ -7,6 +7,8 @@ declare(strict_types=1);
 
 namespace Magento\GraphQl\Customer;
 
+use Magento\Customer\Api\AccountManagementInterface;
+use Magento\Customer\Api\CustomerRepositoryInterface;
 use Magento\Customer\Model\CustomerAuthUpdate;
 use Magento\Customer\Model\CustomerRegistry;
 use Magento\Integration\Api\CustomerTokenServiceInterface;
@@ -30,13 +32,25 @@ class GetCustomerTest extends GraphQlAbstract
      */
     private $customerAuthUpdate;
 
+    /**
+     * @var AccountManagementInterface
+     */
+    private $accountManagement;
+
+    /**
+     * @var CustomerRepositoryInterface
+     */
+    private $customerRepository;
+
     protected function setUp()
     {
         parent::setUp();
 
         $this->customerTokenService = Bootstrap::getObjectManager()->get(CustomerTokenServiceInterface::class);
+        $this->accountManagement = Bootstrap::getObjectManager()->get(AccountManagementInterface::class);
         $this->customerRegistry = Bootstrap::getObjectManager()->get(CustomerRegistry::class);
         $this->customerAuthUpdate = Bootstrap::getObjectManager()->get(CustomerAuthUpdate::class);
+        $this->customerRepository = Bootstrap::getObjectManager()->get(CustomerRepositoryInterface::class);
     }
 
     /**
@@ -57,7 +71,12 @@ query {
     }
 }
 QUERY;
-        $response = $this->graphQlQuery($query, [], '', $this->getCustomerAuthHeaders($currentEmail, $currentPassword));
+        $response = $this->graphQlQuery(
+            $query,
+            [],
+            '',
+            $this->getCustomerAuthHeaders($currentEmail, $currentPassword)
+        );
 
         $this->assertEquals(null, $response['customer']['id']);
         $this->assertEquals('John', $response['customer']['firstname']);
@@ -104,7 +123,28 @@ query {
     }
 }
 QUERY;
-        $this->graphQlQuery($query, [], '', $this->getCustomerAuthHeaders($currentEmail, $currentPassword));
+        $this->graphQlQuery(
+            $query,
+            [],
+            '',
+            $this->getCustomerAuthHeaders($currentEmail, $currentPassword)
+        );
+    }
+
+    /**
+     * @magentoApiDataFixture Magento/Customer/_files/customer_confirmation_config_enable.php
+     * @magentoApiDataFixture Magento/Customer/_files/customer.php
+     * @expectedException \Exception
+     * @expectedExceptionMessage The account sign-in was incorrect or your account is disabled temporarily
+     */
+    public function testAccountIsNotConfirmed()
+    {
+        $confirmation_required = $this->accountManagement::ACCOUNT_CONFIRMATION_REQUIRED;
+        $customerEmail = 'customer@example.com';
+        $currentPassword = 'password';
+        $customer = $this->customerRepository->getById(1)->setConfirmation($confirmation_required);
+        $this->customerRepository->save($customer);
+        $this->getCustomerAuthHeaders($customerEmail, $currentPassword);
     }
 
     /**
