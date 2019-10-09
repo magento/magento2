d5605e827c6b0be404364875646e9eeb321e3366
Krissy Hiserote <khiserote@magento.com>
2019-10-09 15:22:47 -0500

    MC-18527: Merge release branch into 2.3-develop
    - fix integration tests

--- dev/tests/integration/testsuite/Magento/Sales/_files/quotes.php
+++ dev/tests/integration/testsuite/Magento/Sales/_files/quotes.php
@@ -19,12 +19,17 @@ $quoteFactory = $objectManager->get(QuoteFactory::class);
 /** @var QuoteRepository $quoteRepository */
 $quoteRepository = $objectManager->get(QuoteRepository::class);
 
+/** @var Magento\Store\Model\Store $store */
+$store = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create(\Magento\Store\Model\Store::class);
+$store->load('fixture_second_store');
+$storeId = $store->getId();
+
 $quotes = [
     'quote for first store' => [
         'store' => 1,
     ],
     'quote for second store' => [
-        'store' => 2,
+        'store' => $storeId,
     ],
 ];
 
