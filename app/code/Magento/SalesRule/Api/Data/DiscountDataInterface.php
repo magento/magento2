6a98119287c3ae3b9d90e3cc1e70fb13602227b1
Prabhu Ram <pganapat@adobe.com>
2019-10-15 10:56:45 -0500

    MC-20648: Implement the changes - Added interface for discount data

---
+++ app/code/Magento/SalesRule/Api/Data/DiscountDataInterface.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\SalesRule\Api\Data;
+
+interface DiscountDataInterface
+{
+    /**
+     * Get Amount
+     *
+     * @return float
+     */
+    public function getAmount();
+
+    /**
+     * Get Base Amount
+     *
+     * @return float
+     */
+    public function getBaseAmount();
+
+    /**
+     * Get Original Amount
+     *
+     * @return float
+     */
+    public function getOriginalAmount();
+
+    /**
+     * Get Base Original Amount
+     *
+     * @return float
+     */
+    public function getBaseOriginalAmount();
+}
