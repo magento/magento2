e917b007ed8a233007c1a19d22776315eb7870d4
Andrey Legayev <andrey@ven.com>
2019-10-17 21:59:32 +0300

    Replace two calls to str_replace() by single strtr()
    I've tested: str_replace() is slower than strtr() in this case

--- lib/internal/Magento/Framework/Phrase/Renderer/Translate.php
+++ lib/internal/Magento/Framework/Phrase/Renderer/Translate.php
@@ -50,8 +50,7 @@ class Translate implements RendererInterface
     {
         $text = end($source);
         /* If phrase contains escaped quotes then use translation for phrase with non-escaped quote */
-        $text = str_replace('\"', '"', $text);
-        $text = str_replace("\\'", "'", $text);
+        $text = strtr($text, ['\"' => '"', "\\'" => "'"]);
 
         try {
             $data = $this->translator->getData();
