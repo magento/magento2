8aafc695c751fdb785041a6654f63ca5ca2aadd2
Eden <quocviet312@gmail.com>
2019-10-18 23:13:22 +0700

    Resolve "Redirect to CMS-page if Cookies are Disabled" is "No" but it still redirect issue25148

--- app/code/Magento/Cookie/Block/RequireCookie.php
+++ app/code/Magento/Cookie/Block/RequireCookie.php
@@ -3,15 +3,20 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
-
+declare(strict_types=1);
 /**
  * Frontend form key content block
  */
 namespace Magento\Cookie\Block;
 
+use Magento\Framework\App\ObjectManager;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+
 /**
  * @api
  * @since 100.0.2
+ *
+ * Class \Magento\Cookie\Block\RequireCookie
  */
 class RequireCookie extends \Magento\Framework\View\Element\Template
 {
@@ -22,9 +27,12 @@ class RequireCookie extends \Magento\Framework\View\Element\Template
      */
     public function getScriptOptions()
     {
+        $isRedirectCmsPage = ObjectManager::getInstance()->get(ScopeConfigInterface::class)
+            ->getValue('web/browser_capabilities/cookies');
         $params = [
             'noCookieUrl' => $this->escapeUrl($this->getUrl('cookie/index/noCookies/')),
-            'triggers' => $this->escapeHtml($this->getTriggers())
+            'triggers' => $this->escapeHtml($this->getTriggers()),
+            'isRedirectCmsPage' => (boolean)$isRedirectCmsPage
         ];
         return json_encode($params);
     }
