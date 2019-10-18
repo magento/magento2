02438a7fbaffb193ceb6ab7281b1d20c86e5673b
ivan.pletnyov <ivan.pletnyov@transoftgroup.com>
2019-10-18 16:56:02 +0300

    MC-20691: Admin: Update attribute set

---
+++ dev/tests/integration/testsuite/Magento/Catalog/_files/product_with_test_attribute_set.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Catalog\Model\Product\Attribute\Source\Status;
+use Magento\Catalog\Model\Product\Visibility;
+use Magento\Catalog\Model\ProductFactory;
+use Magento\TestFramework\Eav\Model\AttributeSet;
+use Magento\TestFramework\Helper\Bootstrap;
+
+$objectManager = Bootstrap::getObjectManager();
+/** @var ProductRepositoryInterface $productRepository */
+$productRepository = $objectManager->get(ProductRepositoryInterface::class);
+/** @var ProductFactory $productFactory */
+$productFactory = $objectManager->get(ProductFactory::class);
+/** @var AttributeSet $attributeSet */
+$attributeSet = $objectManager->get(AttributeSet::class);
+$customAttributeSet = $attributeSet->getAttributeSetByName('new_attribute_set');
+$product = $productFactory->create();
+$product
+    ->setTypeId('simple')
+    ->setId(1)
+    ->setAttributeSetId($customAttributeSet->getAttributeSetId())
+    ->setWebsiteIds([1])
+    ->setName('Simple Product')
+    ->setSku('simple')
+    ->setPrice(10)
+    ->setMetaTitle('meta title')
+    ->setMetaKeyword('meta keyword')
+    ->setMetaDescription('meta description')
+    ->setVisibility(Visibility::VISIBILITY_BOTH)
+    ->setStatus(Status::STATUS_ENABLED)
+    ->setStockData(['use_config_manage_stock' => 1, 'qty' => 22, 'is_in_stock' => 1])
+    ->setQty(22);
+$productRepository->save($product);
---
+++ dev/tests/integration/testsuite/Magento/Catalog/_files/product_with_test_attribute_set_rollback.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\CatalogInventory\Model\StockRegistryStorage;
+use Magento\Framework\Registry;
+use Magento\TestFramework\Helper\Bootstrap;
+
+$objectManager = Bootstrap::getObjectManager();
+/** @var Registry $registry */
+$registry = $objectManager->get(Registry::class);
+$registry->unregister('isSecureArea');
+$registry->register('isSecureArea', true);
+/** @var ProductRepositoryInterface $productRepository */
+$productRepository = $objectManager->get(ProductRepositoryInterface::class);
+/** @var StockRegistryStorage $stockRegistryStorage */
+$stockRegistryStorage = $objectManager->get(StockRegistryStorage::class);
+try {
+    $product = $productRepository->get('simple');
+    $productRepository->delete($product);
+} catch (\Exception $e) {
+
+}
+$stockRegistryStorage->clean();
+$registry->unregister('isSecureArea');
+$registry->register('isSecureArea', false);
