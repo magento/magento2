4745f33bf938c45252edf0156e8c9becb21c94ff
Cristian Partica <cpartica@magento.com>
2019-10-18 09:57:28 -0500

    MC-15991: Category image return only image name

--- app/code/Magento/CatalogGraphQl/Model/Resolver/Category/Image.php
+++ app/code/Magento/CatalogGraphQl/Model/Resolver/Category/Image.php
@@ -12,12 +12,24 @@ use Magento\Framework\GraphQl\Query\ResolverInterface;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Store\Api\Data\StoreInterface;
+use Magento\Framework\Filesystem\DirectoryList;
 
 /**
  * Resolve category image to a fully qualified URL
  */
 class Image implements ResolverInterface
 {
+    /** @var DirectoryList  */
+    private $directoryList;
+
+    /**
+     * @param DirectoryList $directoryList
+     */
+    public function __construct(DirectoryList $directoryList)
+    {
+        $this->directoryList = $directoryList;
+    }
+
     /**
      * @inheritdoc
      */
@@ -31,6 +43,7 @@ class Image implements ResolverInterface
         if (!isset($value['model'])) {
             throw new LocalizedException(__('"model" value should be specified'));
         }
+        /** @var \Magento\Catalog\Model\Category $category */
         $category = $value['model'];
         $imagePath = $category->getImage();
         if (empty($imagePath)) {
@@ -38,7 +51,13 @@ class Image implements ResolverInterface
         }
         /** @var StoreInterface $store */
         $store = $context->getExtensionAttributes()->getStore();
-        $baseUrl = $store->getBaseUrl();
+        $baseUrl = $store->getBaseUrl('media');
+
+        $mediaPath = $this->directoryList->getUrlPath('media');
+        $pos = strpos($imagePath, $mediaPath);
+        if ($pos !== false) {
+            $imagePath = substr($imagePath, $pos + strlen($mediaPath), strlen($baseUrl));
+        }
         $imageUrl = rtrim($baseUrl, '/') . '/' . ltrim($imagePath, '/');
 
         return $imageUrl;
