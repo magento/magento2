caafd17ac49037ca0873ee8f1fdd5cc9b738e14a
Tomash Khamlai <tomash.khamlai@gmail.com>
2019-10-18 21:52:27 +0300

    Fix static tests
    Signed-off-by: Tomash Khamlai <tomash.khamlai@gmail.com>

--- dev/tests/integration/testsuite/Magento/Customer/_files/attribute_user_defined_address_custom_attribute_rollback.php
+++ dev/tests/integration/testsuite/Magento/Customer/_files/attribute_user_defined_address_custom_attribute_rollback.php
@@ -6,6 +6,8 @@
  */
 
 /** @var \Magento\Customer\Model\Attribute $attributeModel */
-$attributeModel = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create(\Magento\Customer\Model\Attribute::class);
+$attributeModel = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create(
+    \Magento\Customer\Model\Attribute::class
+);
 $attributeModel->load('custom_attribute1', 'attribute_code')->delete();
 $attributeModel->load('custom_attribute2', 'attribute_code')->delete();
