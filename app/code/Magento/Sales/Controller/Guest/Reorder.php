81c72dc232d1c8e9b136d5e4b53ecacaffa0b551
rahul <rahul@webkul.com>
2019-10-19 15:03:01 +0530

    annotaion added

--- app/code/Magento/Sales/Controller/Guest/Reorder.php
+++ app/code/Magento/Sales/Controller/Guest/Reorder.php
@@ -1,11 +1,13 @@
 <?php
 /**
- *
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
 namespace Magento\Sales\Controller\Guest;
 
+/**
+ * Guest reorder action.
+ */
 class Reorder extends \Magento\Sales\Controller\AbstractController\Reorder
 {
     /**
