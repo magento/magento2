d6dab81c8c3413322f29b88929930188dfc2e8ef
Bartłomiej Szubert <bartlomiejszubert@gmail.com>
2019-10-19 14:51:19 +0200

    Fix #23853 - Form "Create New Customer" and Create New Address" at the backend doesn't display Prefix and Suffix Options

--- app/code/Magento/Customer/Model/AttributeMetadataResolver.php
+++ app/code/Magento/Customer/Model/AttributeMetadataResolver.php
@@ -6,15 +6,21 @@ declare(strict_types=1);
  */
 namespace Magento\Customer\Model;
 
+use Magento\Config\Model\Config\Source\Nooptreq;
 use Magento\Customer\Api\Data\AddressInterface;
 use Magento\Customer\Api\Data\CustomerInterface;
+use Magento\Customer\Helper\Address as AddressHelper;
 use Magento\Customer\Model\Config\Share as ShareConfig;
+use Magento\Customer\Model\Options as CustomerOptions;
 use Magento\Customer\Model\ResourceModel\Address\Attribute\Source\CountryWithWebsites;
 use Magento\Eav\Api\Data\AttributeInterface;
 use Magento\Eav\Model\Entity\Attribute\AbstractAttribute;
 use Magento\Eav\Model\Entity\Type;
 use Magento\Framework\App\ObjectManager;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Store\Model\StoreManagerInterface;
 use Magento\Ui\Component\Form\Field;
 use Magento\Ui\DataProvider\EavValidationRules;
 
@@ -79,6 +85,20 @@ class AttributeMetadataResolver
      * @var GroupManagement
      */
     private $groupManagement;
+    /**
+     * @var StoreManagerInterface
+     */
+    private $storeManager;
+
+    /**
+     * @var CustomerOptions
+     */
+    private $customerOptions;
+
+    /**
+     * @var AddressHelper
+     */
+    private $addressHelper;
 
     /**
      * @param CountryWithWebsites $countryWithWebsiteSource
@@ -87,6 +107,9 @@ class AttributeMetadataResolver
      * @param ContextInterface $context
      * @param ShareConfig $shareConfig
      * @param GroupManagement|null $groupManagement
+     * @param StoreManagerInterface|null $storeManager
+     * @param Options|null $customerOptions
+     * @param AddressHelper|null $addressHelper
      */
     public function __construct(
         CountryWithWebsites $countryWithWebsiteSource,
@@ -94,7 +117,10 @@ class AttributeMetadataResolver
         FileUploaderDataResolver $fileUploaderDataResolver,
         ContextInterface $context,
         ShareConfig $shareConfig,
-        ?GroupManagement $groupManagement = null
+        ?GroupManagement $groupManagement = null,
+        StoreManagerInterface $storeManager = null,
+        CustomerOptions $customerOptions = null,
+        AddressHelper $addressHelper = null
     ) {
         $this->countryWithWebsiteSource = $countryWithWebsiteSource;
         $this->eavValidationRules = $eavValidationRules;
@@ -102,6 +128,9 @@ class AttributeMetadataResolver
         $this->context = $context;
         $this->shareConfig = $shareConfig;
         $this->groupManagement = $groupManagement ?? ObjectManager::getInstance()->get(GroupManagement::class);
+        $this->storeManager = $storeManager ?: ObjectManager::getInstance()->get(StoreManagerInterface::class);
+        $this->customerOptions = $customerOptions ?: ObjectManager::getInstance()->get(CustomerOptions::class);
+        $this->addressHelper = $addressHelper ?: ObjectManager::getInstance()->get(AddressHelper::class);
     }
 
     /**
@@ -111,7 +140,7 @@ class AttributeMetadataResolver
      * @param Type $entityType
      * @param bool $allowToShowHiddenAttributes
      * @return array
-     * @throws \Magento\Framework\Exception\LocalizedException
+     * @throws LocalizedException
      */
     public function getAttributesMeta(
         AbstractAttribute $attribute,
@@ -166,6 +195,13 @@ class AttributeMetadataResolver
             $attribute,
             $meta['arguments']['data']['config']
         );
+
+        $this->modifyPrefixSuffixMeta(
+            $attribute,
+            $meta['arguments']['data']['config'],
+            $meta[CustomerInterface::WEBSITE_ID] ?? null
+        );
+
         return $meta;
     }
 
@@ -238,5 +274,68 @@ class AttributeMetadataResolver
                 'field' => 'website_ids'
             ];
         }
+    }
+
+    /**
+     * Change prefix and suffix to select with correct options if configured
+     *
+     * @param AttributeInterface $attribute
+     * @param $meta
+     * @param int|null $websiteId
+     * @throws NoSuchEntityException
+     * @throws LocalizedException
+     */
+    private function modifyPrefixSuffixMeta(AttributeInterface $attribute, &$meta, $websiteId): void
+    {
+        $attributeCode = $attribute->getAttributeCode();
+        if (!in_array($attributeCode, [AddressInterface::PREFIX, AddressInterface::SUFFIX], false)) {
+            return;
+        }
+
+        $storeId = null;
+
+        if ($websiteId) {
+            $groupId = $this->storeManager->getWebsite($websiteId)->getDefaultGroupId();
+            $storeId = $this->storeManager->getGroup($groupId)->getDefaultStoreId();
+        } elseif ($defaultStoreView = $this->storeManager->getDefaultStoreView()) {
+            $storeId = $defaultStoreView->getId();
+        }
+
+        $isRequired = $this->addressHelper->getConfig(
+            $attributeCode . '_show',
+            $storeId
+        ) === Nooptreq::VALUE_REQUIRED;
+
+        if (!$isRequired) {
+            $meta['required'] = 0;
+            unset($meta['validation']);
+        }
+
+        $options = $attributeCode === AddressInterface::PREFIX ?
+            $this->customerOptions->getNamePrefixOptions($storeId) :
+            $this->customerOptions->getNameSuffixOptions($storeId);
+
+        if ($options !== false) {
+            $meta['dataType'] = 'select';
+            $meta['formElement'] = 'select';
+            $meta['options'] = $this->mapPrefixSuffixOptions($options);
+        }
+    }
+
+    /**
+     * Map options array to valid source for UI select
+     *
+     * @param array $options
+     * @return array
+     */
+    private function mapPrefixSuffixOptions(array $options): array
+    {
+        $result = [];
+
+        foreach ($options as $value => $label) {
+            $result[] = ['label' => $label, 'value' => trim($value)];
+        }
+
+        return $result;
     }
 }
