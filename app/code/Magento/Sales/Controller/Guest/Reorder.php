b86cce1add733300452c7e21abe827c2bcd922fb
Rahul Mahto <rahul@webkul.com>
2019-10-19 22:38:15 +0530

    fixed error "useless method overriding detected"
    fixed error "useless method overriding detected"

--- app/code/Magento/Sales/Controller/Guest/Reorder.php
+++ app/code/Magento/Sales/Controller/Guest/Reorder.php
@@ -10,18 +10,4 @@ namespace Magento\Sales\Controller\Guest;
  */
 class Reorder extends \Magento\Sales\Controller\AbstractController\Reorder
 {
-    /**
-     * @param \Magento\Framework\App\Action\Context $context
-     * @param \Magento\Sales\Controller\Guest\OrderLoader $orderLoader
-     * @param \Magento\Framework\Registry $registry
-     * @param \Magento\Sales\Helper\Reorder $reorderHelper
-     */
-    public function __construct(
-        \Magento\Framework\App\Action\Context $context,
-        \Magento\Sales\Controller\Guest\OrderLoader $orderLoader,
-        \Magento\Framework\Registry $registry,
-        \Magento\Sales\Helper\Reorder $reorderHelper
-    ) {
-        parent::__construct($context, $orderLoader, $registry, $reorderHelper);
-    }
 }
