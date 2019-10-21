8fdf24762182c191176e9b5cdf769392f1e48a23
Tomash Khamlai <tomash.khamlai@gmail.com>
2019-10-21 18:26:28 +0300

    Refactoring  - replaced some of the deprecated calls  - performed DRY optimizations
    Signed-off-by: Tomash Khamlai <tomash.khamlai@gmail.com>

--- dev/tests/integration/testsuite/Magento/Customer/_files/attribute_user_defined_address_custom_attribute.php
+++ dev/tests/integration/testsuite/Magento/Customer/_files/attribute_user_defined_address_custom_attribute.php
@@ -4,57 +4,37 @@
  * See COPYING.txt for license details.
  */
 
-/** @var \Magento\Customer\Model\Attribute $model1 */
-$model1 = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create(\Magento\Customer\Model\Attribute::class);
-$model1->setName(
-    'custom_attribute1'
-)->setEntityTypeId(
-    2
-)->setAttributeSetId(
-    2
-)->setAttributeGroupId(
-    1
-)->setFrontendInput(
-    'text'
-)->setFrontendLabel(
-    'custom_attribute_frontend_label'
-)->setIsUserDefined(
-    1
-);
-$model1->save();
+/** @var \Magento\Framework\ObjectManagerInterface $objectManager */
+$objectManager = \Magento\TestFramework\Helper\Bootstrap::getObjectManager();
 
-/** @var \Magento\Customer\Model\Attribute $model2 */
-$model2 = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create(\Magento\Customer\Model\Attribute::class);
-$model2->setName(
-    'custom_attribute2'
-)->setEntityTypeId(
-    2
-)->setAttributeSetId(
-    2
-)->setAttributeGroupId(
-    1
-)->setFrontendInput(
-    'text'
-)->setFrontendLabel(
-    'custom_attribute_frontend_label'
-)->setIsUserDefined(
-    1
-);
-$model2->save();
+/** @var \Magento\Customer\Model\AttributeFactory $attributeFactory */
+$attributeFactory = $objectManager->create(\Magento\Customer\Model\AttributeFactory::class);
+
+/** @var \Magento\Eav\Api\AttributeRepositoryInterface $attributeRepository */
+$attributeRepository =  $objectManager->create(\Magento\Eav\Api\AttributeRepositoryInterface::class);
 
 /** @var \Magento\Customer\Setup\CustomerSetup $setupResource */
-$setupResource = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create(
-    \Magento\Customer\Setup\CustomerSetup::class
-);
+$setupResource = $objectManager->create(\Magento\Customer\Setup\CustomerSetup::class);
 
-$data1 = [['form_code' => 'customer_address_edit', 'attribute_id' => $model1->getAttributeId()]];
-$setupResource->getSetup()->getConnection()->insertMultiple(
-    $setupResource->getSetup()->getTable('customer_form_attribute'),
-    $data1
-);
+$attributeNames = ['custom_attribute1', 'custom_attribute2'];
+foreach ($attributeNames as $attributeName) {
+    /** @var \Magento\Customer\Model\Attribute $attribute */
+    $attribute = $attributeFactory->create();
 
-$data2 = [['form_code' => 'customer_address_edit', 'attribute_id' => $model2->getAttributeId()]];
-$setupResource->getSetup()->getConnection()->insertMultiple(
-    $setupResource->getSetup()->getTable('customer_form_attribute'),
-    $data2
-);
+    $attribute->setName($attributeName)
+        ->setEntityTypeId(2)
+        ->setAttributeSetId(2)
+        ->setAttributeGroupId(1)
+        ->setFrontendInput('text')
+        ->setFrontendLabel('custom_attribute_frontend_label')
+        ->setIsUserDefined(true);
+
+    $attributeRepository->save($attribute);
+
+    $setupResource->getSetup()
+        ->getConnection()
+        ->insertMultiple(
+            $setupResource->getSetup()->getTable('customer_form_attribute'),
+            [['form_code' => 'customer_address_edit', 'attribute_id' => $attribute->getAttributeId()]]
+        );
+}
