fd14fbf4747b378d5961ff33f9965b6a5ffbf18f
keyur.a.shah@accenture.com <keyur.a.shah@accenture.com>
2019-10-21 16:08:10 +0200

    fix for tier price 0

--- app/code/Magento/Catalog/Model/Product/Attribute/Backend/TierPrice/UpdateHandler.php
+++ app/code/Magento/Catalog/Model/Product/Attribute/Backend/TierPrice/UpdateHandler.php
@@ -134,7 +134,7 @@ class UpdateHandler extends AbstractHandler
     {
         $isChanged = false;
         foreach ($valuesToUpdate as $key => $value) {
-            if ((!is_null($value['value'])
+            if ((($value['value']) !== NULL
                     && (float)$oldValues[$key]['price'] !== $this->localeFormat->getNumber($value['value'])
                 ) || $this->getPercentage($oldValues[$key]) !== $this->getPercentage($value)
             ) {
