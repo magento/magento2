d304f8185c2cf40b7d110cabb313a5d111a9a27a
Eden <quocviet312@gmail.com>
2019-10-24 13:04:44 +0700

    Fix automation test Magento Health Index

--- app/code/Magento/Customer/Ui/Component/Form/Field/DisableAutoGroupChange.php
+++ app/code/Magento/Customer/Ui/Component/Form/Field/DisableAutoGroupChange.php
@@ -29,7 +29,7 @@ class DisableAutoGroupChange extends \Magento\Ui\Component\Form\Field
      *
      * @var AddressHelper
      */
-    protected $addressHelper;
+    private $addressHelper;
 
     /**
      * Constructor
