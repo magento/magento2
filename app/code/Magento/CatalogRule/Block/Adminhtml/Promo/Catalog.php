2e4569f153a83a6cd5c04029ad7e7e745360cdad
Eden <quocviet312@gmail.com>
2019-10-27 15:53:14 +0700

    Resolve Display success message for Apply rules if haven't added rule (issue 24834)

--- app/code/Magento/CatalogRule/Block/Adminhtml/Promo/Catalog.php
+++ app/code/Magento/CatalogRule/Block/Adminhtml/Promo/Catalog.php
@@ -4,6 +4,8 @@
  * See COPYING.txt for license details.
  */
 
+declare(strict_types=1);
+
 /**
  * Catalog price rules
  *
@@ -11,13 +13,44 @@
  */
 namespace Magento\CatalogRule\Block\Adminhtml\Promo;
 
+use Magento\Backend\Block\Widget\Context;
+use Magento\Framework\App\ObjectManager;
+use Magento\CatalogRule\Model\ResourceModel\Rule\CollectionFactory as CatalogRuleCollectionFactory;
+
 /**
+ * Catalog Rule block
+ *
  * @api
  * @since 100.0.2
  */
 class Catalog extends \Magento\Backend\Block\Widget\Grid\Container
 {
     /**
+     * @var CatalogRuleCollectionFactory
+     */
+    private $catalogRuleCollectionFactory;
+
+    /**
+     * Constructor
+     *
+     * @param Context $context
+     * @param array $data
+     * @param CatalogRuleCollectionFactory|null $catalogRuleCollectionFactory
+     */
+    public function __construct(
+        Context $context,
+        array $data = [],
+        CatalogRuleCollectionFactory $catalogRuleCollectionFactory = null
+    ) {
+        $this->catalogRuleCollectionFactory = $catalogRuleCollectionFactory ?: ObjectManager::getInstance()->get(
+            CatalogRuleCollectionFactory::class
+        );
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * Sets block template and necessary data
+     *
      * @return void
      */
     protected function _construct()
@@ -27,14 +60,15 @@ class Catalog extends \Magento\Backend\Block\Widget\Grid\Container
         $this->_headerText = __('Catalog Price Rule');
         $this->_addButtonLabel = __('Add New Rule');
         parent::_construct();
-
-        $this->buttonList->add(
-            'apply_rules',
-            [
-                'label' => __('Apply Rules'),
-                'onclick' => "location.href='" . $this->getUrl('catalog_rule/*/applyRules') . "'",
-                'class' => 'apply'
-            ]
-        );
+        if ($this->catalogRuleCollectionFactory->create()->getSize()) {
+            $this->buttonList->add(
+                'apply_rules',
+                [
+                    'label' => __('Apply Rules'),
+                    'onclick' => "location.href='" . $this->getUrl('catalog_rule/*/applyRules') . "'",
+                    'class' => 'apply'
+                ]
+            );
+        }
     }
 }
