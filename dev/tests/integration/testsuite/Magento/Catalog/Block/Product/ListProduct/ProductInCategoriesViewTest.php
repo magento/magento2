aee5a5040392d2147734acdb2a317d67ac8ee5b0
Yurii Sapiha <yurasapiga93@gmail.com>
2019-10-28 18:10:18 +0200

    MC-21456: Storefront: Product view in a category

--- dev/tests/integration/testsuite/Magento/Catalog/Block/Product/ListProduct/ProductInCategoriesViewTest.php
+++ dev/tests/integration/testsuite/Magento/Catalog/Block/Product/ListProduct/ProductInCategoriesViewTest.php
@@ -7,19 +7,13 @@ declare(strict_types=1);
 
 namespace Magento\Catalog\Block\Product\ListProduct;
 
-use Magento\Catalog\Api\CategoryLinkManagementInterface;
 use Magento\Catalog\Api\CategoryRepositoryInterface;
-use Magento\Catalog\Api\Data\CategoryInterface;
 use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Catalog\Block\Product\ListProduct;
-use Magento\Catalog\Model\Layer;
-use Magento\Catalog\Model\Layer\Resolver;
 use Magento\Catalog\Model\Product\Visibility;
 use Magento\Eav\Model\Entity\Collection\AbstractCollection;
 use Magento\Framework\ObjectManagerInterface;
-use Magento\Framework\Registry;
 use Magento\Framework\View\LayoutInterface;
-use Magento\Store\Api\StoreRepositoryInterface;
 use Magento\Store\Model\StoreManagerInterface;
 use Magento\TestFramework\Helper\Bootstrap;
 use PHPUnit\Framework\TestCase;
@@ -28,9 +22,9 @@ use PHPUnit\Framework\TestCase;
  * Checks products displaying on category page
  *
  * @magentoDbIsolation disabled
- * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+ * @magentoAppIsolation enabled
  */
-class AbstractTest extends TestCase
+class ProductInCategoriesViewTest extends TestCase
 {
     /** @var ObjectManagerInterface */
     private $objectManager;
@@ -41,20 +35,11 @@ class AbstractTest extends TestCase
     /** @var CategoryRepositoryInterface */
     private $categoryRepository;
 
-    /** @var Registry */
-    private $registry;
-
     /** @var ProductRepositoryInterface */
     private $productRepository;
 
-    /** @var CategoryLinkManagementInterface */
-    private $categoryLinkManagement;
-
     /** @var StoreManagerInterface */
     private $storeManager;
-
-    /** @var StoreRepositoryInterface */
-    private $storeRepository;
 
     /** @var LayoutInterface */
     private $layout;
@@ -68,25 +53,22 @@ class AbstractTest extends TestCase
 
         $this->objectManager = Bootstrap::getObjectManager();
         $this->categoryRepository = $this->objectManager->create(CategoryRepositoryInterface::class);
-        $this->block = $this->objectManager->get(LayoutInterface::class)->createBlock(ListProduct::class);
-        $this->registry = $this->objectManager->get(Registry::class);
         $this->productRepository = $this->objectManager->create(ProductRepositoryInterface::class);
-        $this->categoryLinkManagement = $this->objectManager->create(CategoryLinkManagementInterface::class);
         $this->storeManager = $this->objectManager->get(StoreManagerInterface::class);
-        $this->storeRepository = $this->objectManager->create(StoreRepositoryInterface::class);
         $this->layout = $this->objectManager->get(LayoutInterface::class);
+        $this->block = $this->layout->createBlock(ListProduct::class);
     }
 
     /**
      * @magentoDataFixture Magento/Catalog/_files/category_with_two_products.php
-     * @magentoAppIsolation enabled
      * @dataProvider productDataProvider
      * @param array $data
      * @return void
      */
     public function testCategoryProductView(array $data): void
     {
-        $collection = $this->processCategoryViewTest($data['sku'], $data);
+        $this->updateProduct($data['sku'], $data);
+        $collection = $this->getCategoryProductCollection(333);
 
         $this->assertEquals(1, $collection->getSize());
         $this->assertEquals('simple333', $collection->getFirstItem()->getSku());
@@ -120,14 +102,14 @@ class AbstractTest extends TestCase
 
     /**
      * @magentoDataFixture Magento/Catalog/_files/category_product.php
-     * @magentoAppIsolation enabled
      * @dataProvider productVisibilityProvider
      * @param array $data
      * @return void
      */
     public function testCategoryProductVisibility(array $data): void
     {
-        $collection = $this->processCategoryViewTest($data['data']['sku'], $data['data']);
+        $this->updateProduct($data['data']['sku'], $data['data']);
+        $collection = $this->getCategoryProductCollection(333);
 
         $this->assertEquals($data['expected_count'], $collection->getSize());
     }
@@ -180,34 +162,38 @@ class AbstractTest extends TestCase
     }
 
     /**
-     * @magentoAppIsolation enabled
      * @magentoDataFixture Magento/Catalog/_files/category_tree.php
      * @magentoDataFixture Magento/Catalog/_files/second_product_simple.php
      * @return void
      */
     public function testAnchorCategoryProductVisibility(): void
     {
-        $collections = $this->processAnchorTest(true);
+        $this->updateCategoryIsAnchor(400, true);
+        $this->assignProductCategories(['simple2'], [402]);
+        $parentCategoryCollection = $this->getCategoryProductCollection(400);
+        $childCategoryCollection = $this->getCategoryProductCollection(402, true);
 
-        $this->assertEquals(1, $collections['parent_collection']->getSize());
+        $this->assertEquals(1, $parentCategoryCollection->getSize());
         $this->assertEquals(
-            $collections['child_collection']->getAllIds(),
-            $collections['parent_collection']->getAllIds()
+            $childCategoryCollection->getAllIds(),
+            $parentCategoryCollection->getAllIds()
         );
     }
 
     /**
-     * @magentoAppIsolation enabled
      * @magentoDataFixture Magento/Catalog/_files/category_tree.php
      * @magentoDataFixture Magento/Catalog/_files/second_product_simple.php
      * @return void
      */
     public function testNonAnchorCategoryProductVisibility(): void
     {
-        $collections = $this->processAnchorTest(false);
+        $this->updateCategoryIsAnchor(400, false);
+        $this->assignProductCategories(['simple2'], [402]);
+        $parentCategoryCollection = $this->getCategoryProductCollection(400);
+        $childCategoryCollection = $this->getCategoryProductCollection(402, true);
 
-        $this->assertCount(0, $collections['parent_collection']);
-        $this->assertCount(1, $collections['child_collection']);
+        $this->assertCount(0, $parentCategoryCollection);
+        $this->assertCount(1, $childCategoryCollection);
     }
 
     /**
@@ -217,85 +203,94 @@ class AbstractTest extends TestCase
      */
     public function testCategoryProductViewOnMultiWebsite(): void
     {
-        $this->setCategoriesToProducts(['simple-1', 'simple-2']);
-        $store = $this->storeRepository->get('fixture_second_store');
-        $this->storeManager->setCurrentStore($store->getId());
-        $category = $this->categoryRepository->get(333);
-        $this->registerCategory($category);
-        $collection = $this->block->getLoadedProductCollection();
-        $this->assertEquals(1, $collection->getSize());
-        $this->assertEquals('simple-2', $collection->getFirstItem()->getSku());
+        $this->assignProductCategories(['simple-1', 'simple-2'], [3, 333]);
+        $store = $this->storeManager->getStore('fixture_second_store');
+        $currentStore = $this->storeManager->getStore();
+
+        try {
+            $this->storeManager->setCurrentStore($store->getId());
+            $collection = $this->block->getLoadedProductCollection();
+            $collectionSize = $collection->getSize();
+        } finally {
+            $this->storeManager->setCurrentStore($currentStore);
+        }
+
+        $this->assertEquals(1, $collectionSize);
+        $this->assertNull($collection->getItemByColumnValue('sku', 'simple-1'));
+        $this->assertNotNull($collection->getItemByColumnValue('sku', 'simple-2'));
     }
 
     /**
      * Set categories to the products
      *
      * @param array $skus
+     * @param $categoryIds
      * @return void
      */
-    private function setCategoriesToProducts(array $skus): void
+    private function assignProductCategories(array $skus, array $categoryIds): void
     {
         foreach ($skus as $sku) {
             $product = $this->productRepository->get($sku);
-            $product->setCategoryIds([2, 333]);
+            $product->setCategoryIds($categoryIds);
             $this->productRepository->save($product);
         }
     }
 
     /**
-     * Proccess for anchor and non anchor category test
-     *
-     * @param bool $isAnchor
-     * @return array
-     */
-    private function processAnchorTest(bool $isAnchor): array
-    {
-        $category = $this->categoryRepository->get(400);
-        $category->setIsAnchor($isAnchor);
-        $this->categoryRepository->save($category);
-        $childCategory = $this->categoryRepository->get(402);
-        $this->categoryLinkManagement->assignProductToCategories('simple2', [$childCategory->getId()]);
-        $this->registerCategory($category);
-        $parentCategoryCollection = $this->block->getLoadedProductCollection();
-        $this->objectManager->removeSharedInstance(Resolver::class);
-        $this->objectManager->removeSharedInstance(Layer::class);
-        $this->registerCategory($childCategory);
-        $newBlock = $this->layout->createBlock(ListProduct::class);
-        $childCategoryCollection = $newBlock->getLoadedProductCollection();
-
-        return [
-            'parent_collection' => $parentCategoryCollection,
-            'child_collection' => $childCategoryCollection,
-        ];
-    }
-
-    /**
-     * Proccess category view test
+     * Update product
      *
      * @param string $sku
      * @param array $data
-     * @return AbstractCollection
+     * @return void
      */
-    private function processCategoryViewTest(string $sku, array $data): AbstractCollection
+    private function updateProduct(string $sku, array $data): void
     {
         $product = $this->productRepository->get($sku);
         $product->addData($data);
         $this->productRepository->save($product);
-        $category = $this->categoryRepository->get(333);
-        $this->registerCategory($category);
-
-        return $this->block->getLoadedProductCollection();
     }
 
     /**
-     * Register current category
+     * Returns category collection by category id
      *
-     * @param CategoryInterface $category
-     * @retun void
+     * @param int $categoryId
+     * @param bool $refreshBlock
+     * @return AbstractCollection
      */
-    private function registerCategory(CategoryInterface $category): void
+    private function getCategoryProductCollection(int $categoryId, bool $refreshBlock = false): AbstractCollection
     {
-        $this->registry->unregister('current_category');
-        $this->registry->register('current_category', $category);
+        $block = $this->getListingBlock($refreshBlock);
+        $block->getLayer()->setCurrentCategory($categoryId);
+
+        return $block->getLoadedProductCollection();
+    }
+
+    /**
+     * Update is_anchor attribute of the category
+     *
+     * @param int $categoryId
+     * @param bool $isAnchor
+     * @return void
+     */
+    private function updateCategoryIsAnchor(int $categoryId, bool $isAnchor): void
+    {
+        $category = $this->categoryRepository->get($categoryId);
+        $category->setIsAnchor($isAnchor);
+        $this->categoryRepository->save($category);
+    }
+
+    /**
+     * Get product listing block
+     *
+     * @param bool $refresh
+     * @return ListProduct
+     */
+    private function getListingBlock(bool $refresh): ListProduct
+    {
+        if ($refresh) {
+            $this->block = $this->layout->createBlock(ListProduct::class);
+        }
+
+        return $this->block;
     }
 }
