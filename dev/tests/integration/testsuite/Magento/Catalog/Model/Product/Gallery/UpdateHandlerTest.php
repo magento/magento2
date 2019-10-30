a0a025c7d7f64107afdfe580fda1d1608759d4df
Mykhailo Matiola <mykhailo.matiola@transoftgroup.com>
2019-10-30 18:26:07 +0200

    MC-20660: Admin: Assign/delete image(s) from simple product in single/multiple store views mode

--- dev/tests/integration/testsuite/Magento/Catalog/Model/Product/Gallery/UpdateHandlerTest.php
+++ dev/tests/integration/testsuite/Magento/Catalog/Model/Product/Gallery/UpdateHandlerTest.php
@@ -269,12 +269,13 @@ class UpdateHandlerTest extends \PHPUnit\Framework\TestCase
     public function testExecuteWithImageToDelete(): void
     {
         $product = $this->getProduct();
+        $image = $product->getImage();
         $this->updateProductGalleryImages($product, ['removed' => '1']);
         $this->updateHandler->execute($product);
         $productImages = $this->galleryResource->loadProductGalleryByAttributeId($product, $this->mediaAttributeId);
         $this->assertCount(0, $productImages);
         $this->assertFileNotExists(
-            $this->mediaDirectory->getAbsolutePath($this->config->getBaseMediaPath() . '/m/a/magento_image.jpg')
+            $this->mediaDirectory->getAbsolutePath($this->config->getBaseMediaPath() . $image)
         );
         $defaultImages = $this->productResource->getAttributeRawValue(
             $product->getId(),
