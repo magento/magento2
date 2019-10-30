f19f7270ecc066a32226fbd90f6eda6f18b22c61
Prabhu Ram <pganapat@adobe.com>
2019-10-30 09:36:10 -0500

    MC-22213: Implementation - Merge cart - review fixes

--- app/code/Magento/QuoteGraphQl/Model/Resolver/MergeCarts.php
+++ app/code/Magento/QuoteGraphQl/Model/Resolver/MergeCarts.php
@@ -12,6 +12,7 @@ use Magento\Framework\GraphQl\Exception\GraphQlInputException;
 use Magento\Framework\GraphQl\Query\ResolverInterface;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
 use Magento\QuoteGraphQl\Model\Cart\GetCartForUser;
+use Magento\Quote\Api\CartRepositoryInterface;
 
 /**
  * Merge Carts Resolver
@@ -24,12 +25,20 @@ class MergeCarts implements ResolverInterface
     private $getCartForUser;
 
     /**
+     * @var CartRepositoryInterface
+     */
+    private $cartRepository;
+
+    /**
      * @param GetCartForUser $getCartForUser
+     * @param CartRepositoryInterface $cartRepository
      */
     public function __construct(
-        GetCartForUser $getCartForUser
+        GetCartForUser $getCartForUser,
+        CartRepositoryInterface $cartRepository
     ) {
         $this->getCartForUser = $getCartForUser;
+        $this->cartRepository = $cartRepository;
     }
 
     /**
@@ -38,7 +47,7 @@ class MergeCarts implements ResolverInterface
     public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null)
     {
         if (empty($args['source_cart_id'])) {
-            throw new GraphQlInputException(__('Required parameter "cart_id" is missing'));
+            throw new GraphQlInputException(__('Required parameter "source_cart_id" is missing'));
         }
 
         if (empty($args['destination_cart_id'])) {
@@ -49,12 +58,14 @@ class MergeCarts implements ResolverInterface
         $customerMaskedCartId = $args['destination_cart_id'];
 
         $currentUserId = $context->getUserId();
-        $storeId = $storeId = (int)$context->getExtensionAttributes()->getStore()->getId();
+        $storeId = (int)$context->getExtensionAttributes()->getStore()->getId();
         // passing customerId as null enforces source cart should always be a guestcart
         $guestCart = $this->getCartForUser->execute($guestMaskedCartId, null, $storeId);
         $customerCart = $this->getCartForUser->execute($customerMaskedCartId, $currentUserId, $storeId);
         $customerCart->merge($guestCart);
-
+        $guestCart->setIsActive(false);
+        $this->cartRepository->save($customerCart);
+        $this->cartRepository->save($guestCart);
         return [
             'model' => $customerCart,
         ];
