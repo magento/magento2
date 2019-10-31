be24d9005aecac7cbeabdefc2a49771a3d4790cd
torhoehn <torhoehn@gmail.com>
2019-10-31 23:21:37 +0100

    add check for attribute option values

--- app/code/Magento/ConfigurableProduct/view/adminhtml/web/js/variations/steps/attributes_values.js
+++ app/code/Magento/ConfigurableProduct/view/adminhtml/web/js/variations/steps/attributes_values.js
@@ -106,6 +106,7 @@ define([
                 errorOption,
                 allOptions = [];
 
+            newOption.label = $.trim(option.label);
             if (_.isEmpty(newOption.label)) {
                 return false;
             }
