98f53e703f6238f37c9a5948e32764de9ee1212e
Anusha Vattam <avattam@adobe.com>
2019-11-01 17:19:29 -0500

    MC-22215: Add customerCart Query and cart_id changes to schema and related resolvers
    - Fixed static failures on dependency test

--- app/code/Magento/QuoteGraphQl/Model/Resolver/CustomerCart.php
+++ app/code/Magento/QuoteGraphQl/Model/Resolver/CustomerCart.php
@@ -9,11 +9,11 @@ namespace Magento\QuoteGraphQl\Model\Resolver;
 
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\GraphQl\Config\Element\Field;
-use Magento\Framework\GraphQl\Exception\GraphQlInputException;
 use Magento\Framework\GraphQl\Query\ResolverInterface;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
 use Magento\QuoteGraphQl\Model\Cart\CreateEmptyCartForCustomer;
-use Magento\Authorization\Model\UserContextInterface;
+use Magento\GraphQl\Model\Query\ContextInterface;
+use Magento\Framework\GraphQl\Exception\GraphQlAuthorizationException;
 use Magento\Quote\Api\CartManagementInterface;
 
 /**
@@ -49,35 +49,20 @@ class CustomerCart implements ResolverInterface
     public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null)
     {
         $currentUserId = $context->getUserId();
-        $currentUserType = $context->getUserType();
-        $isCustomerLoggedIn = $this->isCustomer($currentUserId, $currentUserType);
 
-        if ($isCustomerLoggedIn) {
-            try {
-                $cart = $this->cartManagement->getCartForCustomer($currentUserId);
-            } catch (NoSuchEntityException $e) {
-                $this->createEmptyCartForCustomer->execute($currentUserId, null);
-                $cart =  $this->cartManagement->getCartForCustomer($currentUserId);
-            }
-
-        } else {
-                throw new GraphQlInputException(__('The request is allowed for logged in customer'));
+        /** @var ContextInterface $context */
+        if (false === $context->getExtensionAttributes()->getIsCustomer()) {
+            throw new GraphQlAuthorizationException(__('The request is allowed for logged in customer'));
+        }
+        try {
+            $cart = $this->cartManagement->getCartForCustomer($currentUserId);
+        } catch (NoSuchEntityException $e) {
+            $this->createEmptyCartForCustomer->execute($currentUserId, null);
+            $cart =  $this->cartManagement->getCartForCustomer($currentUserId);
         }
 
         return [
             'model' => $cart
         ];
-    }
-
-    /**
-     * Check if current user is logged in
-     *
-     * @param int|null $customerId
-     * @param int|null $customerType
-     * @return bool
-     */
-    private function isCustomer(?int $customerId, ?int $customerType): bool
-    {
-        return !empty($customerId) && !empty($customerType) && $customerType !== UserContextInterface::USER_TYPE_GUEST;
     }
 }
