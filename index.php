1b14e1042220c71872a175a458378a30b867486d
Callum Atwal <42971429+callum-atwal@users.noreply.github.com>
2019-11-02 13:27:16 +0000

    Coding Standards Fix
    - HTML Tags should have closing tag within same indentation as the opening tag

--- index.php
+++ index.php
@@ -25,7 +25,8 @@ try {
 <div style="font:12px/1.35em arial, helvetica, sans-serif;">
     <div style="margin:0 0 25px 0; border-bottom:1px solid #ccc;">
         <h3 style="margin:0;font-size:1.7em;font-weight:normal;text-transform:none;text-align:left;color:#2f2f2f;">
-        Autoload error</h3>
+            Autoload error
+        </h3>
     </div>
     <p>{$e->getMessage()}</p>
 </div>
