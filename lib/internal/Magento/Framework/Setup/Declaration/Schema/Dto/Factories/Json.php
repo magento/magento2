bedbb377740644f236e93b2e34fc8fd70530aaa6
Anton Kaplia <akaplya@adobe.com>
2019-11-05 11:13:10 -0600

    JSON fields support

---
+++ lib/internal/Magento/Framework/Setup/Declaration/Schema/Dto/Factories/Json.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Framework\Setup\Declaration\Schema\Dto\Factories;
+
+use Magento\Framework\ObjectManagerInterface;
+
+/**
+ * Class Json
+ */
+class Json implements FactoryInterface
+{
+
+    /**
+     * @var ObjectManagerInterface
+     */
+    private $objectManager;
+
+    /**
+     * @var string
+     */
+    private $className;
+
+    /**
+     * Constructor.
+     *
+     * @param ObjectManagerInterface $objectManager
+     * @param string                 $className
+     */
+    public function __construct(
+        ObjectManagerInterface $objectManager,
+        $className = \Magento\Framework\Setup\Declaration\Schema\Dto\Columns\Blob::class
+    ) {
+        $this->objectManager = $objectManager;
+        $this->className = $className;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function create(array $data)
+    {
+        return $this->objectManager->create($this->className, $data);
+    }
+}
