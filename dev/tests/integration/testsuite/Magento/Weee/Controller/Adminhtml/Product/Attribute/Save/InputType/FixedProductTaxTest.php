a271a9277c8d78ad01d6a7c73308328a231f3274
ivan.pletnyov <ivan.pletnyov@transoftgroup.com>
2019-11-05 10:48:38 +0200

    MC-21827: Admin: Create product Attribute

--- dev/tests/integration/testsuite/Magento/Weee/Controller/Adminhtml/Product/Attribute/Save/InputType/FixedProductTaxTest.php
+++ dev/tests/integration/testsuite/Magento/Weee/Controller/Adminhtml/Product/Attribute/Save/InputType/FixedProductTaxTest.php
@@ -5,7 +5,7 @@
  */
 declare(strict_types=1);
 
-namespace Magento\Eav\Controller\Adminhtml\Product\Attribute\Save\InputType;
+namespace Magento\Weee\Controller\Adminhtml\Product\Attribute\Save\InputType;
 
 use Magento\Catalog\Controller\Adminhtml\Product\Attribute\Save\InputType\AbstractSaveAttributeTest;
 
