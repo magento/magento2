5a24a699c6b3ef06908a8f7dce8353df4e992ce0
Viktor Sevch <svitja@ukr.net>
2019-11-08 17:33:28 +0200

    MC-22829: Revert ticket MAGETWO-96975 which introduce BIC on 2.3.4

--- dev/tests/integration/testsuite/Magento/Backend/Model/Locale/ResolverTest.php
+++ dev/tests/integration/testsuite/Magento/Backend/Model/Locale/ResolverTest.php
@@ -77,6 +77,38 @@ class ResolverTest extends \PHPUnit\Framework\TestCase
     }
 
     /**
+     * Tests setLocale() with parameter
+     *
+     * @param string|null $localeParam
+     * @param string|null $localeRequestParam
+     * @param string $localeExpected
+     * @dataProvider setLocaleWithParameterDataProvider
+     */
+    public function testSetLocaleWithParameter(
+        ?string $localeParam,
+        ?string $localeRequestParam,
+        string $localeExpected
+    ) {
+        $request = Bootstrap::getObjectManager()
+            ->get(\Magento\Framework\App\RequestInterface::class);
+        $request->setPostValue(['locale' => $localeRequestParam]);
+        $this->_model->setLocale($localeParam);
+        $this->assertEquals($localeExpected, $this->_model->getLocale());
+    }
+
+    /**
+     * @return array
+     */
+    public function setLocaleWithParameterDataProvider(): array
+    {
+        return [
+            ['ko_KR', 'ja_JP', 'ja_JP'],
+            ['ko_KR', null, 'ko_KR'],
+            [null, 'ja_JP', 'ja_JP'],
+        ];
+    }
+
+    /**
      * Check set locale
      *
      * @param string $localeCodeToCheck
