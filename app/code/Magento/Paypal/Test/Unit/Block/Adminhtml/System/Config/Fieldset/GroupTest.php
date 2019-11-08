33c761e0023758a409e662643925ac7570a73dc5
Myroslav Dobra <dmaraptor@gmail.com>
2019-11-08 23:28:30 +0200

    MC-22829: Revert ticket MAGETWO-96975 which introduce BIC on 2.3.4

--- app/code/Magento/Paypal/Test/Unit/Block/Adminhtml/System/Config/Fieldset/GroupTest.php
+++ app/code/Magento/Paypal/Test/Unit/Block/Adminhtml/System/Config/Fieldset/GroupTest.php
@@ -11,28 +11,31 @@ class GroupTest extends \PHPUnit\Framework\TestCase
     /**
      * @var Group
      */
-    protected $_model;
+    private $_model;
 
     /**
      * @var \Magento\Framework\Data\Form\Element\AbstractElement
      */
-    protected $_element;
+    private $_element;
 
     /**
      * @var \Magento\Backend\Model\Auth\Session|\PHPUnit_Framework_MockObject_MockObject
      */
-    protected $_authSession;
+    private $_authSession;
 
     /**
      * @var \Magento\User\Model\User|\PHPUnit_Framework_MockObject_MockObject
      */
-    protected $_user;
+    private $_user;
 
     /**
      * @var \Magento\Config\Model\Config\Structure\Element\Group|\PHPUnit_Framework_MockObject_MockObject
      */
-    protected $_group;
+    private $_group;
 
+    /**
+     * @inheritdoc
+     */
     protected function setUp()
     {
         $helper = new \Magento\Framework\TestFramework\Unit\Helper\ObjectManager($this);
