ccc4301356130c4bac27ab99e2fe4f1fdcba8ec3
Dmytro Horytskyi <horytsky@adobe.com>
2019-11-08 14:02:50 -0600

    MC-20533: Large amount of Block Cache.

--- app/code/Magento/Swatches/Block/Product/Renderer/Listing/Configurable.php
+++ app/code/Magento/Swatches/Block/Product/Renderer/Listing/Configurable.php
@@ -9,7 +9,6 @@ use Magento\Catalog\Block\Product\Context;
 use Magento\Catalog\Helper\Product as CatalogProduct;
 use Magento\Catalog\Model\Product;
 use Magento\Catalog\Model\Layer\Resolver;
-use Magento\Catalog\Model\Layer\Category as CategoryLayer;
 use Magento\ConfigurableProduct\Helper\Data;
 use Magento\ConfigurableProduct\Model\ConfigurableAttributeData;
 use Magento\Customer\Helper\Session\CurrentCustomer;
@@ -154,7 +153,7 @@ class Configurable extends \Magento\Swatches\Block\Product\Renderer\Configurable
         $this->unsetData('allow_products');
         return parent::getJsonConfig();
     }
-    
+
     /**
      * Composes configuration for js price format
      *
@@ -256,17 +255,5 @@ class Configurable extends \Magento\Swatches\Block\Product\Renderer\Configurable
         }
 
         return $layeredAttributes;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function getCacheKeyInfo()
-    {
-        $cacheKeyInfo = parent::getCacheKeyInfo();
-        /** @var CategoryLayer $catalogLayer */
-        $catalogLayer = $this->layerResolver->get();
-        $cacheKeyInfo[] = $catalogLayer->getStateKey();
-        return $cacheKeyInfo;
     }
 }
