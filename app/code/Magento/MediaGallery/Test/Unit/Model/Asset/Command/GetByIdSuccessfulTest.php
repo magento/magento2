b0fb36394eaca8f656c61d3c2e24f27ac5c35e33
Alexander Shkurko <coderimus@gmail.com>
2019-11-12 15:51:48 +0200

    Change the way the GetById command is tested

--- app/code/Magento/MediaGallery/Test/Unit/Model/Asset/Command/GetByIdSuccessfulTest.php
+++ app/code/Magento/MediaGallery/Test/Unit/Model/Asset/Command/GetByIdSuccessfulTest.php
@@ -10,22 +10,18 @@ namespace Magento\MediaGallery\Test\Unit\Model\Asset\Command;
 use Magento\Framework\App\ResourceConnection;
 use Magento\Framework\DB\Adapter\AdapterInterface;
 use Magento\Framework\DB\Select;
-use Magento\Framework\Exception\IntegrationException;
-use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
 use Magento\MediaGallery\Model\Asset\Command\GetById;
 use Magento\MediaGalleryApi\Api\Data\AssetInterface;
 use Magento\MediaGalleryApi\Api\Data\AssetInterfaceFactory;
 use PHPUnit\Framework\MockObject\MockObject;
-use PHPUnit\Framework\TestCase;
 use Psr\Log\LoggerInterface;
 use Zend\Db\Adapter\Driver\Pdo\Statement;
 
 /**
  * Test the GetById command model
- * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
  */
-class GetByIdTest extends TestCase
+class GetByIdTest extends \PHPUnit\Framework\TestCase
 {
     private const MEDIA_ASSET_STUB_ID = 1;
 
@@ -57,25 +53,20 @@ class GetByIdTest extends TestCase
     private $statementMock;
 
     /**
-     * @var LoggerInterface|MockObject
-     */
-    private $logger;
-
-    /**
      * Initialize basic test class mocks
      */
     protected function setUp(): void
     {
         $resourceConnection = $this->createMock(ResourceConnection::class);
         $this->assetFactory = $this->createMock(AssetInterfaceFactory::class);
-        $this->logger = $this->createMock(LoggerInterface::class);
+        $logger = $this->createMock(LoggerInterface::class);
 
         $this->getMediaAssetById = (new ObjectManager($this))->getObject(
             GetById::class,
             [
                 'resourceConnection' => $resourceConnection,
                 'assetFactory' => $this->assetFactory,
-                'logger' =>  $this->logger,
+                'logger' =>  $logger,
             ]
         );
         $this->adapter = $this->createMock(AdapterInterface::class);
@@ -104,49 +95,5 @@ class GetByIdTest extends TestCase
             $mediaAssetStub,
             $this->getMediaAssetById->execute(self::MEDIA_ASSET_STUB_ID)
         );
-    }
-
-    /**
-     * Test an exception during the get asset data query.
-     */
-    public function testExceptionDuringGetMediaAssetData(): void
-    {
-        $this->statementMock->method('fetch')->willReturn(self::MEDIA_ASSET_DATA);
-        $this->adapter->method('query')->willThrowException(new \Exception());
-
-        $this->expectException(IntegrationException::class);
-
-        $this->getMediaAssetById->execute(self::MEDIA_ASSET_STUB_ID);
-    }
-
-    /**
-     * Test case when there is no found media asset by id.
-     */
-    public function testNotFoundMediaAssetException(): void
-    {
-        $this->statementMock->method('fetch')->willReturn([]);
-        $this->adapter->method('query')->willReturn($this->statementMock);
-
-        $this->expectException(NoSuchEntityException::class);
-
-        $this->getMediaAssetById->execute(self::MEDIA_ASSET_STUB_ID);
-    }
-
-    /**
-     * Test case when a problem occurred during asset initialization from received data.
-     */
-    public function testErrorDuringMediaAssetInitializationException(): void
-    {
-        $this->statementMock->method('fetch')->willReturn(self::MEDIA_ASSET_DATA);
-        $this->adapter->method('query')->willReturn($this->statementMock);
-
-        $this->assetFactory->expects($this->once())->method('create')->willThrowException(new \Exception());
-
-        $this->expectException(IntegrationException::class);
-        $this->logger->expects($this->any())
-            ->method('critical')
-            ->willReturnSelf();
-
-        $this->getMediaAssetById->execute(self::MEDIA_ASSET_STUB_ID);
     }
 }
