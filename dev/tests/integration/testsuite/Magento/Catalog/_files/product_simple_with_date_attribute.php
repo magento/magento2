0ba961533a2eb357df7d71964fcff154a4689be8
Viktor Sevch <svitja@ukr.net>
2019-11-15 13:05:46 +0200

    MC-18057: Product doesn'y match by "Date" attribute condition

---
+++ dev/tests/integration/testsuite/Magento/Catalog/_files/product_simple_with_date_attribute.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+declare(strict_types=1);
+
+include __DIR__ . '/product_date_attribute.php';
+
+$attribute->setData('is_used_for_promo_rules', 1);
+
+/** @var $product \Magento\Catalog\Model\Product */
+$product = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create(\Magento\Catalog\Model\Product::class);
+$product->setTypeId(\Magento\Catalog\Model\Product\Type::TYPE_SIMPLE)
+    ->setAttributeSetId(4)
+    ->setWebsiteIds([1])
+    ->setName('Simple Product with date')
+    ->setSku('simple_with_date')
+    ->setPrice(10)
+    ->setDescription('Description with <b>html tag</b>')
+    ->setVisibility(\Magento\Catalog\Model\Product\Visibility::VISIBILITY_BOTH)
+    ->setStatus(\Magento\Catalog\Model\Product\Attribute\Source\Status::STATUS_ENABLED)
+    ->setCategoryIds([2])
+    ->setStockData(['use_config_manage_stock' => 1, 'qty' => 100, 'is_qty_decimal' => 0, 'is_in_stock' => 1])
+    ->setDateAttribute(date('Y-m-d'))
+    ->save();
