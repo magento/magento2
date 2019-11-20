b011040ca0c09b1feebd270e875e3faa142ecd2d
Benjamin Rosenberger <rosenberger@e-conomix.at>
2019-11-20 06:23:33 +0100

    :ok_hand: adding type hints as suggested by review

--- app/code/Magento/Indexer/Console/Command/IndexerStatusCommand.php
+++ app/code/Magento/Indexer/Console/Command/IndexerStatusCommand.php
@@ -65,6 +65,10 @@ class IndexerStatusCommand extends AbstractIndexerManageCommand
         usort(
             $rows,
             function ($comp1, $comp2) {
+                /**
+                 * @var array $comp1
+                 * @var array $comp2
+                 */
                 return strcmp($comp1['Title'], $comp2['Title']);
             }
         );
