1edbd0b99299b106e354909b9cf7a89f823d678f
Nicholas Scott Fish <nicholas.fish@fastwebmedia.com>
2019-11-26 13:43:25 +0000

    Fixes #24448 #25153 templateVars and templateOptions set to [] on reset

--- lib/internal/Magento/Framework/Mail/Template/TransportBuilder.php
+++ lib/internal/Magento/Framework/Mail/Template/TransportBuilder.php
@@ -350,8 +350,8 @@ class TransportBuilder
     {
         $this->messageData = [];
         $this->templateIdentifier = null;
-        $this->templateVars = null;
-        $this->templateOptions = null;
+        $this->templateVars = [];
+        $this->templateOptions = [];
         return $this;
     }
 
