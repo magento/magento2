b804ffa4a6dfcf50068504b0cdc2e047d87c1d86
Buba Suma <soumah@adobe.com>
2019-11-27 18:11:36 -0600

    MC-23224: Special from / to date wrong on administrator Dutch locale
    - Fix MySql date format is wrongly parsed by IntlDateFormatter

--- app/code/Magento/Catalog/Controller/Adminhtml/Product/Initialization/Helper.php
+++ app/code/Magento/Catalog/Controller/Adminhtml/Product/Initialization/Helper.php
@@ -6,7 +6,6 @@
 
 namespace Magento\Catalog\Controller\Adminhtml\Product\Initialization;
 
-use DateTime;
 use Magento\Backend\Helper\Js;
 use Magento\Catalog\Api\Data\ProductCustomOptionInterfaceFactory as CustomOptionFactory;
 use Magento\Catalog\Api\Data\ProductLinkInterfaceFactory as ProductLinkFactory;
@@ -15,6 +14,8 @@ use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Catalog\Api\ProductRepositoryInterface\Proxy as ProductRepository;
 use Magento\Catalog\Controller\Adminhtml\Product\Initialization\Helper\AttributeFilter;
 use Magento\Catalog\Model\Product;
+use Magento\Catalog\Model\Product\Authorization as ProductAuthorization;
+use Magento\Catalog\Model\Product\DateTime as DateTimeFilter;
 use Magento\Catalog\Model\Product\Initialization\Helper\ProductLinks;
 use Magento\Catalog\Model\Product\Link\Resolver as LinkResolver;
 use Magento\Catalog\Model\Product\LinkTypeProvider;
@@ -24,13 +25,13 @@ use Magento\Framework\Locale\FormatInterface;
 use Magento\Framework\Stdlib\DateTime\Filter\Date;
 use Magento\Store\Model\StoreManagerInterface;
 use Zend_Filter_Input;
-use Magento\Catalog\Model\Product\Authorization as ProductAuthorization;
 
 /**
  * Product helper
  *
  * @api
  * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+ * @SuppressWarnings(PHPMD.TooManyFields)
  * @since 100.0.2
  */
 class Helper
@@ -92,7 +93,7 @@ class Helper
     /**
      * @var \Magento\Framework\Stdlib\DateTime\Filter\DateTime
      */
-    private $dateTimeFilter;
+    private $stdlibDateTimeFilter;
 
     /**
      * @var LinkTypeProvider
@@ -115,6 +116,11 @@ class Helper
     private $localeFormat;
 
     /**
+     * @var DateTimeFilter
+     */
+    private $dateTimeFilter;
+
+    /**
      * Constructor
      *
      * @param RequestInterface $request
@@ -130,6 +136,7 @@ class Helper
      * @param AttributeFilter|null $attributeFilter
      * @param FormatInterface|null $localeFormat
      * @param ProductAuthorization|null $productAuthorization
+     * @param DateTimeFilter|null $dateTimeFilter
      * @SuppressWarnings(PHPMD.ExcessiveParameterList)
      */
     public function __construct(
@@ -145,7 +152,8 @@ class Helper
         LinkTypeProvider $linkTypeProvider = null,
         AttributeFilter $attributeFilter = null,
         FormatInterface $localeFormat = null,
-        ?ProductAuthorization $productAuthorization = null
+        ?ProductAuthorization $productAuthorization = null,
+        ?DateTimeFilter $dateTimeFilter = null
     ) {
         $this->request = $request;
         $this->storeManager = $storeManager;
@@ -162,6 +170,7 @@ class Helper
         $this->attributeFilter = $attributeFilter ?: $objectManager->get(AttributeFilter::class);
         $this->localeFormat = $localeFormat ?: $objectManager->get(FormatInterface::class);
         $this->productAuthorization = $productAuthorization ?? $objectManager->get(ProductAuthorization::class);
+        $this->dateTimeFilter = $dateTimeFilter ?? $objectManager->get(DateTimeFilter::class);
     }
 
     /**
@@ -185,7 +194,6 @@ class Helper
         }
 
         $productData = $this->normalize($productData);
-        $productData = $this->convertSpecialFromDateStringToObject($productData);
 
         if (!empty($productData['is_downloadable'])) {
             $productData['product_has_weight'] = 0;
@@ -209,7 +217,7 @@ class Helper
         foreach ($attributes as $attrKey => $attribute) {
             if ($attribute->getBackend()->getType() == 'datetime') {
                 if (array_key_exists($attrKey, $productData) && $productData[$attrKey] != '') {
-                    $dateFieldFilters[$attrKey] = $this->getDateTimeFilter();
+                    $dateFieldFilters[$attrKey] = $this->dateTimeFilter;
                 }
             }
         }
@@ -413,15 +421,16 @@ class Helper
      *
      * @return \Magento\Framework\Stdlib\DateTime\Filter\DateTime
      * @deprecated 101.0.0
+     * @SuppressWarnings(PHPMD.UnusedPrivateMethod)
      */
     private function getDateTimeFilter()
     {
-        if ($this->dateTimeFilter === null) {
-            $this->dateTimeFilter = ObjectManager::getInstance()
+        if ($this->stdlibDateTimeFilter === null) {
+            $this->stdlibDateTimeFilter = ObjectManager::getInstance()
                 ->get(\Magento\Framework\Stdlib\DateTime\Filter\DateTime::class);
         }
 
-        return $this->dateTimeFilter;
+        return $this->stdlibDateTimeFilter;
     }
 
     /**
@@ -496,21 +505,5 @@ class Helper
         }
 
         return $product->setOptions($customOptions);
-    }
-
-    /**
-     * Convert string date presentation into object
-     *
-     * @param array $productData
-     * @return array
-     */
-    private function convertSpecialFromDateStringToObject($productData)
-    {
-        if (isset($productData['special_from_date']) && $productData['special_from_date'] != '') {
-            $productData['special_from_date'] = $this->getDateTimeFilter()->filter($productData['special_from_date']);
-            $productData['special_from_date'] = new DateTime($productData['special_from_date']);
-        }
-
-        return $productData;
     }
 }
