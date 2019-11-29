a081d51dc6014b91e3b2e814533c08070366f58a
Maximilian Fickers <m.fickers@basecom.de>
2019-11-29 11:50:59 +0100

    Only evaluate single product option for wishlist product option update, instead of every option on the whole page

--- app/code/Magento/Wishlist/view/frontend/web/js/add-to-wishlist.js
+++ app/code/Magento/Wishlist/view/frontend/web/js/add-to-wishlist.js
@@ -60,40 +60,28 @@ define([
          */
         _updateWishlistData: function (event) {
             var dataToAdd = {},
-                isFileUploaded = false,
-                self = this;
+                self = this,
+                element = $(event.target);
 
-            if (event.handleObj.selector == this.options.qtyInfo) { //eslint-disable-line eqeqeq
-                this._updateAddToWishlistButton({});
-                event.stopPropagation();
-
-                return;
-            }
-            $(event.handleObj.selector).each(function (index, element) {
-                if ($(element).is('input[type=text]') ||
-                    $(element).is('input[type=email]') ||
-                    $(element).is('input[type=number]') ||
-                    $(element).is('input[type=hidden]') ||
-                    $(element).is('input[type=checkbox]:checked') ||
-                    $(element).is('input[type=radio]:checked') ||
-                    $(element).is('textarea') ||
-                    $('#' + element.id + ' option:selected').length
-                ) {
-                    if ($(element).data('selector') || $(element).attr('name')) {
-                        dataToAdd = $.extend({}, dataToAdd, self._getElementData(element));
-                    }
-
-                    return;
-                }
-
-                if ($(element).is('input[type=file]') && $(element).val()) {
-                    isFileUploaded = true;
-                }
-            });
-
-            if (isFileUploaded) {
+            if (element.is('input[type=file]') && element.val()) {
                 this.bindFormSubmit();
             }
+
+            if (!element.is(this.options.qtyInfo) &&
+                element.is('input[type=text]') ||
+                element.is('input[type=email]') ||
+                element.is('input[type=number]') ||
+                element.is('input[type=hidden]') ||
+                element.is('input[type=checkbox]:checked') ||
+                element.is('input[type=radio]:checked') ||
+                element.is('textarea') ||
+                $('#' + element.id + ' option:selected').length
+            ) {
+                if (element.data('selector') || element.attr('name')) {
+                    dataToAdd = $.extend(dataToAdd, self._getElementData(element));
+                }
+            }
+
             this._updateAddToWishlistButton(dataToAdd);
             event.stopPropagation();
         },
