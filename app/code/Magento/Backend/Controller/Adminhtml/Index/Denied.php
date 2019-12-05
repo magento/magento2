1f12acfbf94dcfc4acaeba7f74e1b78596817213
Eden <eden@magestore.com>
2019-12-05 16:00:44 +0700

    Fix static test issue25881

--- app/code/Magento/Backend/Controller/Adminhtml/Index/Denied.php
+++ app/code/Magento/Backend/Controller/Adminhtml/Index/Denied.php
@@ -9,13 +9,17 @@ declare(strict_types=1);
 namespace Magento\Backend\Controller\Adminhtml\Index;
 
 use Magento\Backend\Controller\Adminhtml\Denied as DeniedController;
+use Magento\Framework\App\Action\HttpGetActionInterface;
 
 /**
  * To display Denied Page
  *
  * Class Denied
  */
-class Denied extends DeniedController
+class Denied extends DeniedController implements HttpGetActionInterface
 {
-
+    /**
+     * Authorization level of a basic admin session
+     */
+    const ADMIN_RESOURCE = 'Magento_Backend::admin';
 }
