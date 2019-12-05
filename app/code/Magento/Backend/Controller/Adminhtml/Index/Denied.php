da1daaeeb64f29d7f23bc71e63987ffbc7994099
Eden <eden@magestore.com>
2019-12-05 14:19:30 +0700

    Resolve Admin panel is not accessible after limited permissions set to at least one admin account issue25881

---
+++ app/code/Magento/Backend/Controller/Adminhtml/Index/Denied.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ *
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\Backend\Controller\Adminhtml\Index;
+
+use Magento\Backend\Controller\Adminhtml\Denied as DeniedController;
+
+/**
+ * To display Denied Page
+ *
+ * Class Denied
+ */
+class Denied extends DeniedController
+{
+
+}
