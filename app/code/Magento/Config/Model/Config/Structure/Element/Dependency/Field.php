fd723af73f30f684176a9aec28738fe129475dff
Volodymyr Zaets <vzaets@magento.com>
2019-12-06 16:07:58 -0600

    Fix static

--- app/code/Magento/Config/Model/Config/Structure/Element/Dependency/Field.php
+++ app/code/Magento/Config/Model/Config/Structure/Element/Dependency/Field.php
@@ -6,10 +6,13 @@
 namespace Magento\Config\Model\Config\Structure\Element\Dependency;
 
 /**
- * Field
- *
  * @api
  * @since 100.0.2
+ */
+
+/**
+ * Class Field
+ * @package Magento\Config\Model\Config\Structure\Element\Dependency
  */
 class Field
 {
