26e5774754cd2ce52707bd1327345c6a89f12d19
Dmytro Cheshun <d.cheshun@atwix.com>
2019-12-11 21:06:27 +0200

    Minor code style fixes

--- dev/tests/js/jasmine/tests/lib/mage/browser.test.js
+++ dev/tests/js/jasmine/tests/lib/mage/browser.test.js
@@ -12,7 +12,6 @@ define([
     var obj;
 
     beforeEach(function () {
-
         /**
          * Dummy constructor to use for instantiation
          * @constructor
@@ -27,14 +26,14 @@ define([
     describe('"openDialog" method', function () {
         it('Opens dialog with provided targetElementId', function () {
             var options = {
-                        'targetElementId': 1
-                    };
+                'targetElementId': 1
+            };
 
             spyOn($, 'ajax').and.callFake(
-             function () {
+                function () {
                     return {
                         /**
-                         * Succes result of ajax request
+                         * Success result of ajax request
                          */
                         done: function () {
                             obj.targetElementId = 1;
@@ -44,22 +43,21 @@ define([
             obj.openDialog('instance/url', 100, 100, 'title', options);
             obj.openDialog('instance/url', 100, 100, 'title', options);
             expect($.ajax.calls.count()).toBe(1);
-
         });
 
         it('Opens dialog with provided url param', function () {
             spyOn($, 'ajax').and.callFake(
-             function () {
-                return {
-                    /**
-                     * Succes result of ajax request
-                     */
-                    done: function () {
-                        obj.targetElementId = 'instance/url';
-                        obj.modalLoaded = true;
-                    }
-                };
-            });
+                function () {
+                    return {
+                        /**
+                         * Success result of ajax request
+                         */
+                        done: function () {
+                            obj.targetElementId = 'instance/url';
+                            obj.modalLoaded = true;
+                        }
+                    };
+                });
             obj.openDialog('instance/url', 100, 100, 'title', undefined);
             obj.openDialog('instance/url', 100, 100, 'title', undefined);
             expect($.ajax.calls.count()).toBe(1);
