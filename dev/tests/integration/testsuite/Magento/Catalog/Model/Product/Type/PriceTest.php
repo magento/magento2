426065a5bcf12302baf3aa1b52dc285e962c6c28
Dmytro Horytskyi <horytsky@adobe.com>
2019-12-18 14:40:05 -0600

    MC-29865: getBasePrice function returns a string sometimes

--- dev/tests/integration/testsuite/Magento/Catalog/Model/Product/Type/PriceTest.php
+++ dev/tests/integration/testsuite/Magento/Catalog/Model/Product/Type/PriceTest.php
@@ -6,8 +6,6 @@
 namespace Magento\Catalog\Model\Product\Type;
 
 use Magento\Catalog\Model\Product;
-use Magento\Catalog\Model\ProductRepository;
-use Magento\TestFramework\Helper\Bootstrap;
 
 /**
  * @magentoDataFixture Magento/Catalog/_files/product_simple.php
@@ -21,55 +19,44 @@ class PriceTest extends \PHPUnit\Framework\TestCase
 
     protected function setUp()
     {
-        $this->_model = Bootstrap::getObjectManager()->create(
+        $this->_model = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create(
             \Magento\Catalog\Model\Product\Type\Price::class
         );
     }
 
     public function testGetPrice()
     {
-        $this->assertSame(9.0, $this->_model->getPrice(new \Magento\Framework\DataObject(['price' => '9.0'])));
-
-        $repository = Bootstrap::getObjectManager()->create(ProductRepository::class);
-        $product = $repository->get('simple');
-        $this->assertSame(10.0, $this->_model->getPrice($product));
-    }
-
-    public function testGetBasePrice()
-    {
-        $this->assertSame(9.0, $this->_model->getPrice(new \Magento\Framework\DataObject(['price' => '9.0'])));
-
-        $repository = Bootstrap::getObjectManager()->create(ProductRepository::class);
-        $product = $repository->get('simple');
-        $this->assertSame(10.0, $this->_model->getBasePrice($product));
+        $this->assertEquals('test', $this->_model->getPrice(new \Magento\Framework\DataObject(['price' => 'test'])));
     }
 
     public function testGetFinalPrice()
     {
-        $repository = Bootstrap::getObjectManager()->create(ProductRepository::class);
+        $repository = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create(
+            \Magento\Catalog\Model\ProductRepository::class
+        );
         $product = $repository->get('simple');
         // fixture
 
         // regular & tier prices
-        $this->assertSame(10.0, $this->_model->getFinalPrice(1, $product));
-        $this->assertSame(8.0, $this->_model->getFinalPrice(2, $product));
-        $this->assertSame(5.0, $this->_model->getFinalPrice(5, $product));
+        $this->assertEquals(10.0, $this->_model->getFinalPrice(1, $product));
+        $this->assertEquals(8.0, $this->_model->getFinalPrice(2, $product));
+        $this->assertEquals(5.0, $this->_model->getFinalPrice(5, $product));
 
         // with options
         $buyRequest = $this->prepareBuyRequest($product);
         $product->getTypeInstance()->prepareForCart($buyRequest, $product);
 
         //product price + options price(10+1+2+3+3)
-        $this->assertSame(19.0, $this->_model->getFinalPrice(1, $product));
+        $this->assertEquals(19.0, $this->_model->getFinalPrice(1, $product));
 
         //product tier price + options price(5+1+2+3+3)
-        $this->assertSame(14.0, $this->_model->getFinalPrice(5, $product));
+        $this->assertEquals(14.0, $this->_model->getFinalPrice(5, $product));
     }
 
     public function testGetFormatedPrice()
     {
-        $repository = Bootstrap::getObjectManager()->create(
-            ProductRepository::class
+        $repository = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create(
+            \Magento\Catalog\Model\ProductRepository::class
         );
         $product = $repository->get('simple');
         // fixture
@@ -78,19 +65,19 @@ class PriceTest extends \PHPUnit\Framework\TestCase
 
     public function testCalculatePrice()
     {
-        $this->assertSame(10.0, $this->_model->calculatePrice(10.0, 8.0, '1970-12-12 23:59:59', '1971-01-01 01:01:01'));
-        $this->assertSame(8.0, $this->_model->calculatePrice(10.0, 8.0, '1970-12-12 23:59:59', '2034-01-01 01:01:01'));
+        $this->assertEquals(10, $this->_model->calculatePrice(10, 8, '1970-12-12 23:59:59', '1971-01-01 01:01:01'));
+        $this->assertEquals(8, $this->_model->calculatePrice(10, 8, '1970-12-12 23:59:59', '2034-01-01 01:01:01'));
     }
 
     public function testCalculateSpecialPrice()
     {
-        $this->assertSame(
-            10.0,
-            $this->_model->calculateSpecialPrice(10.0, 8.0, '1970-12-12 23:59:59', '1971-01-01 01:01:01')
+        $this->assertEquals(
+            10,
+            $this->_model->calculateSpecialPrice(10, 8, '1970-12-12 23:59:59', '1971-01-01 01:01:01')
         );
-        $this->assertSame(
-            8.0,
-            $this->_model->calculateSpecialPrice(10.0, 8.0, '1970-12-12 23:59:59', '2034-01-01 01:01:01')
+        $this->assertEquals(
+            8,
+            $this->_model->calculateSpecialPrice(10, 8, '1970-12-12 23:59:59', '2034-01-01 01:01:01')
         );
     }
 
