ae831af39b49b64bf420e840c6fd9b4d3eadf3c5
Łukasz Bajsarowicz <lukasz.bajsarowicz@gmail.com>
2019-12-19 03:47:23 +0100

    Unit Test for WeeeGraphQl

---
+++ app/code/Magento/WeeeGraphQl/Test/Unit/FixedProductTaxTest.php
@@ -0,0 +1,69 @@
+<?php
+declare(strict_types=1);
+
+namespace Magento\WeeeGraphQl\Test\Unit;
+
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
+use Magento\WeeeGraphQl\Model\Resolver\FixedProductTax;
+use PHPUnit\Framework\TestCase;
+use PHPUnit_Framework_MockObject_MockObject as MockObject;
+
+class FixedProductTaxTest extends TestCase
+{
+    /**
+     * @var FixedProductTax
+     */
+    private $resolver;
+
+    /**
+     * Build the Testing Environment
+     */
+    protected function setUp()
+    {
+        $objectManager = new ObjectManager($this);
+        $this->resolver = $objectManager->getObject(FixedProductTax::class);
+    }
+
+    public function testExceptionWhenNoModelSpecified(): void
+    {
+        $this->expectException(LocalizedException::class);
+        $this->expectExceptionMessageRegExp('/value should be specified/');
+
+        $this->resolver->resolve(
+            $this->getFieldStub(),
+            null,
+            $this->getResolveInfoStub()
+        );
+    }
+
+    /**
+     * Returns stub for Field
+     *
+     * @return MockObject|Field
+     */
+    private function getFieldStub(): Field
+    {
+        /** @var MockObject|Field $fieldMock */
+        $fieldMock = $this->getMockBuilder(Field::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        return $fieldMock;
+    }
+
+    /**
+     * Returns stub for ResolveInfo
+     *
+     * @return MockObject|ResolveInfo
+     */
+    private function getResolveInfoStub(): ResolveInfo
+    {
+        /** @var MockObject|ResolveInfo $resolveInfoMock */
+        $resolveInfoMock = $this->getMockBuilder(ResolveInfo::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        return $resolveInfoMock;
+    }
+}
