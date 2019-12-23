526817cb1919b722eeb3c27562ed277b7530fb20
Yaroslav Rogoza <enarc@atwix.com>
2019-12-23 17:57:26 +0100

    Jasmine test coverage

---
+++ dev/tests/js/jasmine/tests/lib/jquery/jstree/jquery.hotkeys.test.js
@@ -0,0 +1,73 @@
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+define([
+    'jquery',
+    'jquery/jstree/jquery.hotkeys'
+], function ($) {
+    'use strict';
+
+    describe('Test for jquery/jstree/jquery.hotkeys', function () {
+        var divElement = $('<div></div>'),
+            divBodyAfterTrigger = 'pressed',
+            inputNumberElement = $('<input type="number">');
+
+        beforeAll(function () {
+            $(document).bind('keyup', 'right', function () {
+                // Change element body to track a trigger action
+                divElement.html(divBodyAfterTrigger);
+            });
+
+            $(document).bind('keyup', 'left', function () {
+                // Change element body to track a trigger action
+                divElement.html(divBodyAfterTrigger);
+            });
+
+        });
+
+        beforeEach(function () {
+            inputNumberElement.appendTo(document.body);
+            divElement.appendTo(document.body);
+        });
+
+        afterEach(function () {
+            divElement.remove();
+            inputNumberElement.remove();
+        });
+
+        it('Check "left key" hotkey is not being processed when number input is focused', function () {
+            var keypress = $.Event("keyup");
+            keypress.which = 37; // "left arrow" key
+            inputNumberElement.trigger(keypress);
+
+            expect(divElement.html()).toEqual('');
+        });
+
+        it('Check "right key" hotkey is not being processed when number input is focused', function () {
+            var keypress = $.Event("keyup");
+            keypress.which = 39; // "right arrow" key
+            inputNumberElement.trigger(keypress);
+
+            expect(divElement.html()).toEqual('');
+        });
+
+        it('Check "left key" hotkey is being processed when registered on the page', function () {
+            var keypress = $.Event("keyup");
+            keypress.which = 37; // "left arrow" key
+            divElement.trigger(keypress);
+
+            expect(divElement.html()).toEqual(divBodyAfterTrigger);
+        });
+
+        it('Check "right key" hotkey is being processed when registered on the page', function () {
+            var keypress = $.Event("keyup");
+            keypress.which = 39; // "right arrow" key
+            $('body').trigger(keypress);
+
+            expect(divElement.html()).toEqual(divBodyAfterTrigger);
+        });
+
+    });
+});
