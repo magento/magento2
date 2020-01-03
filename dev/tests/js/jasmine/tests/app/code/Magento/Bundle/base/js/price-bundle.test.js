de8575232d9211b8462f250e226e4d5387da126e
Iryna Lagno <ilagno@adobe.com>
2020-01-03 15:09:51 -0600

    MC-29938: Price of a Bundle Product is calculated incorrectly at the product page - add js unit test

---
+++ dev/tests/js/jasmine/tests/app/code/Magento/Bundle/base/js/price-bundle.test.js
@@ -0,0 +1,56 @@
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+define([
+    'jquery',
+    'Magento_Bundle/js/price-bundle',
+    'Magento_Catalog/js/price-box'
+], function ($) {
+    'use strict';
+
+    describe('Magento_Bundle/js/price-bundle', function () {
+
+        var htmlContainer;
+
+        beforeEach(function () {
+            htmlContainer = $('<div class="price-final_price" data-role="priceBox"><ul class="price-box"></ul></div>');
+        });
+
+        afterEach(function () {
+            htmlContainer.remove();
+        });
+
+        it('Widget extends jQuery object.', function () {
+            expect($.fn.priceBundle).toBeDefined();
+        });
+
+        it('Check _updatePriceBox method call.', function () {
+
+            spyOn($.mage.priceBundle.prototype, '_updatePriceBox');
+
+            htmlContainer.priceBundle();
+
+            expect($.mage.priceBundle.prototype._updatePriceBox).toEqual(jasmine.any(Function));
+            expect($.mage.priceBundle.prototype._updatePriceBox).toHaveBeenCalled();
+            expect($.mage.priceBundle.prototype._updatePriceBox).toHaveBeenCalledTimes(1);
+        });
+
+        it('Check _updatePriceBox method call after priceBox was initialized.', function () {
+            spyOn($.mage.priceBundle.prototype, '_updatePriceBox').and.callThrough();
+            htmlContainer.priceBundle();
+            $('.price-box', htmlContainer).priceBox();
+            expect($.mage.priceBundle.prototype._updatePriceBox).toEqual(jasmine.any(Function));
+            expect($.mage.priceBundle.prototype._updatePriceBox).toHaveBeenCalledTimes(2);
+        });
+
+        it('Check _updatePriceBox method call before priceBox was initialized.', function () {
+            spyOn($.mage.priceBundle.prototype, '_updatePriceBox').and.callThrough();
+            $('.price-box', htmlContainer).priceBox();
+            htmlContainer.priceBundle();
+            expect($.mage.priceBundle.prototype._updatePriceBox).toEqual(jasmine.any(Function));
+            expect($.mage.priceBundle.prototype._updatePriceBox).toHaveBeenCalledTimes(1);
+        });
+    });
+});
