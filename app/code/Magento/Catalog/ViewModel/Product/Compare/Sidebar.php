a6f39798a79200235f1b0bd36a0b3f400e57face
Paweł Tylek <pawel.tylek@creativestyle.pl>
2020-01-07 15:19:49 +0100

    M2C-22090 Fix namespace

--- app/code/Magento/Catalog/ViewModel/Product/Compare/Sidebar.php
+++ app/code/Magento/Catalog/ViewModel/Product/Compare/Sidebar.php
@@ -4,7 +4,7 @@
  * See COPYING.txt for license details.
  */
 
-namespace Magento\Compare\ViewModel;
+namespace Magento\Catalog\ViewModel;
 
 use Magento\Catalog\Helper\Product\Compare as CompareHelper;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
