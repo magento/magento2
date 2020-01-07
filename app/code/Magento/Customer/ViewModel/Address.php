ff4ead556dc0540e321bc2f9de6e835b04bf73d0
Daniel Ruf <mac1@daniel-ruf.de>
2020-01-08 00:04:10 +0100

    Resolve codestyle errors

--- app/code/Magento/Customer/ViewModel/Address.php
+++ app/code/Magento/Customer/ViewModel/Address.php
@@ -14,6 +14,9 @@ use Magento\Framework\View\Element\Block\ArgumentInterface;
  * Customer address view model.
  */
 
+ /**
+ * Address view model
+ */
 class Address implements ArgumentInterface
 {
     /**
@@ -31,43 +34,75 @@ class Address implements ArgumentInterface
     private $helperAddress;
 
     /**
-     * @param Data $helperData
-     * @param Address $helperAddress
+     * Constructor
+     * 
+     * @param DataHelper $helperData
+     * @param AddressHelper $helperAddress
      * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
     public function __construct(
-        Data $helperData,
-        Address $helperAddress
+        DataHelper $helperData,
+        AddressHelper $helperAddress
     ) {
         $this->helperData= $helperData;
         $this->helperAddress= $helperAddress;
     }
 
+    /**
+     * Returns data validation class
+     * 
+     * @return mixed
+     */
     public function dataGetAttributeValidationClass($param)
     {
-        return $this->dataAddress->getAttributeValidationClass($param);
+        return $this->helperData->getAttributeValidationClass($param);
     }
 
+    /**
+     * Returns address validation class
+     * 
+     * @return mixed
+     */
     public function addressGetAttributeValidationClass($param)
     {
         return $this->helperAddress->getAttributeValidationClass($param);
     }
 
+    /**
+     * Returns street lines
+     * 
+     * @return mixed
+     */
     public function addressGetStreetLines()
     {
         return $this->helperAddress->getStreetLines();
     }
 
+    /**
+     * Returns if VAT attribute is visible
+     * 
+     * @return boolean
+     */
     public function addressIsVatAttributeVisible()
     {
         return $this->helperAddress->isVatAttributeVisible();
     }
 
+    /**
+     * Returns region JSON
+     * 
+     * @return mixed
+     */
     public function dataGetRegionJson()
     {
         return $this->helperData->getRegionJson();
     }
 
+    /**
+     * Returns rcountries with optional zip
+     * 
+     * @return mixed
+     */
     public function dataGetCountriesWithOptionalZip()
     {
         return $this->helperData->getCountriesWithOptionalZip();
