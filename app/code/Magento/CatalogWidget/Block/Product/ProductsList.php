31f13e584de55f80a847b7ffc6ae87a7defd464a
Roman Lytvynenko <lytvynen@adobe.com>
2020-01-07 14:07:41 -0600

    MC-30260: CatalogWidget products list doesn't display products from the children categories of 2nd level and up

--- app/code/Magento/CatalogWidget/Block/Product/ProductsList.php
+++ app/code/Magento/CatalogWidget/Block/Product/ProductsList.php
@@ -373,9 +373,9 @@ class ProductsList extends AbstractProduct implements BlockInterface, IdentityIn
                 return $condition;
             }
 
-            if ($category->getIsAnchor() && $category->getChildren(true)) {
-                $children = explode(',', $category->getChildren(true));
-
+            $children = $category->getIsAnchor() ? $category->getChildren(true) : [];
+            if ($children) {
+                $children = explode(',', $children);
                 $condition['operator'] = "()";
                 $condition['value'] = array_merge([$categoryId], $children);
             }
