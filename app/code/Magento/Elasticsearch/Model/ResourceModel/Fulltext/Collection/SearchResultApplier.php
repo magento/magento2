4b34eeab2563581e5d373a824c2b0cac574094a1
Myroslav Dobra <dmaraptor@gmail.com>
2020-01-08 10:48:21 +0200

    MC-25138: 24/36 product limit on Category page occur fatal

--- app/code/Magento/Elasticsearch/Model/ResourceModel/Fulltext/Collection/SearchResultApplier.php
+++ app/code/Magento/Elasticsearch/Model/ResourceModel/Fulltext/Collection/SearchResultApplier.php
@@ -60,6 +60,7 @@ class SearchResultApplier implements SearchResultApplierInterface
     {
         if (empty($this->searchResult->getItems())) {
             $this->collection->getSelect()->where('NULL');
+
             return;
         }
 
@@ -85,13 +86,33 @@ class SearchResultApplier implements SearchResultApplierInterface
     private function sliceItems(array $items, int $size, int $currentPage): array
     {
         if ($size !== 0) {
-            $offset = ($currentPage - 1) * $size;
-            if ($offset < 0) {
-                $offset = 0;
+            // Check that current page is in a range of allowed page numbers, based on items count and items per page,
+            // than calculate offset for slicing items array.
+            $itemsCount = count($items);
+            $maxAllowedPageNumber = ceil($itemsCount/$size);
+            if ($currentPage < 1) {
+                $currentPage = 1;
             }
-            $items = array_slice($items, $offset, $this->size);
+            if ($currentPage > $maxAllowedPageNumber) {
+                $currentPage = $maxAllowedPageNumber;
+            }
+
+            $offset = $this->getOffset($currentPage, $size);
+            $items = array_slice($items, $offset, $size);
         }
 
         return $items;
+    }
+
+    /**
+     * Get offset for given page.
+     *
+     * @param int $pageNumber
+     * @param int $pageSize
+     * @return int
+     */
+    private function getOffset(int $pageNumber, int $pageSize): int
+    {
+        return ($pageNumber - 1) * $pageSize;
     }
 }
