08996dba645bfba42a2a1db24074f50e4bb1989b
Nazar Klovanych <nazarn96@gmail.com>
2020-01-13 11:06:57 +0200

    Add case with Please select option

--- dev/tests/js/jasmine/tests/app/code/Magento/ConfigurableProduct/view/frontend/js/configurable.test.js
+++ dev/tests/js/jasmine/tests/app/code/Magento/ConfigurableProduct/view/frontend/js/configurable.test.js
@@ -9,16 +9,30 @@ define([
 ], function ($, Configurable) {
     'use strict';
 
-    var widget;
+    var widget,
+        option = '<select name=\'super_attribute[93]\'' +
+            'data-selector=\'super_attribute[93]\'' +
+            'data-validate=\'{required:true}\'' +
+            'id=\'attribute93\'' +
+            'class=\'super-attribute-select\'>' +
+            '<option value=\'\'></option>' +
+            '</select>',
+            selectElement = $(option);
 
     beforeEach(function () {
         widget = new Configurable();
         widget.options = {
             spConfig: {
+                chooseText: 'Chose an Option...',
                 attributes:
                 {
                     'size': {
                         options: $('<div><p class="2"></p></div>')
+                    }
+                },
+                prices: {
+                    finalPrice: {
+                        amount: 12
                     }
                 }
             },
@@ -31,6 +45,14 @@ define([
 
         it('check if attribute value is possible to be set as configurable option', function () {
             expect($.mage.configurable).toBeDefined();
+            widget._parseQueryParams('http://magento.com/product?color=red&size=2');
+            expect(widget.options.values.size).toBe('2');
+        });
+
+        it('check if attribute value is possible to be set as option with "please select option"', function () {
+            expect($.mage.configurable).toBeDefined();
+            widget._fillSelect(selectElement[0]);
+            expect(selectElement[0].options[0].innerHTML).toBe(widget.options.spConfig.chooseText);
             widget._parseQueryParams('http://magento.com/product?color=red&size=2');
             expect(widget.options.values.size).toBe('2');
         });
