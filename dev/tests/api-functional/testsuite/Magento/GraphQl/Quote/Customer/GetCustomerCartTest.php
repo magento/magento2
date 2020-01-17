b65d6e995444d4795aa11c36bd0fdc38ab57d010
Anusha Vattam <avattam@adobe.com>
2020-01-17 09:55:55 -0600

    MC-29425: [WebAPI Test] Failing Test: Magento.GraphQl.Quote.Customer.GetCustomerCartTest.testGetCustomerCartSecondStore
    - Refactored the tear down

--- dev/tests/api-functional/testsuite/Magento/GraphQl/Quote/Customer/GetCustomerCartTest.php
+++ dev/tests/api-functional/testsuite/Magento/GraphQl/Quote/Customer/GetCustomerCartTest.php
@@ -45,20 +45,11 @@ class GetCustomerCartTest extends GraphQlAbstract
      */
     protected function tearDown()
     {
-        /** @var \Magento\Framework\Registry $registry */
-        $registry = Bootstrap::getObjectManager()->get(\Magento\Framework\Registry::class);
-        $registry->unregister('isSecureArea');
-        $registry->register('isSecureArea', true);
-
         /** @var $order \Magento\Quote\Model\Quote */
         $quoteCollection = Bootstrap::getObjectManager()->create(\Magento\Quote\Model\ResourceModel\Quote\Collection::class);
         foreach ($quoteCollection as $quote) {
             $quote->delete();
         }
-
-        $registry->unregister('isSecureArea');
-        $registry->register('isSecureArea', false);
-
         parent::tearDown();
     }
 
