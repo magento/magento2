162622f66cba3dc7cb45a2162384df967edd6542
Myroslav Dobra <dmaraptor@gmail.com>
2020-01-17 12:50:05 +0200

    MC-30510: [Integration] PaymentFailuresServiceTest::testHandlerWithCustomer is flaky

--- dev/tests/integration/testsuite/Magento/Sales/Model/Service/PaymentFailuresServiceTest.php
+++ dev/tests/integration/testsuite/Magento/Sales/Model/Service/PaymentFailuresServiceTest.php
@@ -7,10 +7,12 @@ declare(strict_types=1);
 
 namespace Magento\Sales\Model\Service;
 
+use Magento\Framework\Stdlib\DateTime\TimezoneInterface;
 use Magento\Quote\Api\CartRepositoryInterface;
 use Magento\Quote\Model\Quote;
 use Magento\Sales\Api\PaymentFailuresInterface;
 use Magento\TestFramework\Helper\Bootstrap;
+use PHPUnit\Framework\MockObject\MockObject;
 
 /**
  * Tests \Magento\Sales\Api\PaymentFailuresInterface.
@@ -28,9 +30,14 @@ class PaymentFailuresServiceTest extends \PHPUnit\Framework\TestCase
     private $quote;
 
     /**
-     * @var CartRepositoryInterface|\PHPUnit_Framework_MockObject_MockObject
+     * @var CartRepositoryInterface|MockObject
      */
     private $cartRepositoryMock;
+
+    /**
+     * @var TimezoneInterface|MockObject
+     */
+    private $localeDateMock;
 
     /**
      * @inheritdoc
@@ -42,11 +49,15 @@ class PaymentFailuresServiceTest extends \PHPUnit\Framework\TestCase
             ->disableOriginalConstructor()
             ->setMethods(['get'])
             ->getMockForAbstractClass();
+        $this->localeDateMock = $this->getMockBuilder(TimezoneInterface::class)
+            ->setMethods(['formatDateTime'])
+            ->getMockForAbstractClass();
 
         $this->paymentFailures = Bootstrap::getObjectManager()->create(
             PaymentFailuresInterface::class,
             [
                 'cartRepository' => $this->cartRepositoryMock,
+                'localeDate' => $this->localeDateMock,
             ]
         );
     }
@@ -69,20 +80,19 @@ class PaymentFailuresServiceTest extends \PHPUnit\Framework\TestCase
             ->with($this->quote->getId())
             ->willReturn($this->quote);
 
+        $dateAndTime = 'Nov 22, 2019, 1:00:00 AM';
+        $this->localeDateMock->expects($this->atLeastOnce())->method('formatDateTime')->willReturn($dateAndTime);
         $this->paymentFailures->handle((int)$this->quote->getId(), $errorMessage->render());
 
         $paymentReflection = new \ReflectionClass($this->paymentFailures);
-        $templateTimeMethod = $paymentReflection->getMethod('getLocaleDate');
-        $templateTimeMethod->setAccessible(true);
-
         $templateVarsMethod = $paymentReflection->getMethod('getTemplateVars');
         $templateVarsMethod->setAccessible(true);
 
         $templateVars = $templateVarsMethod->invoke($this->paymentFailures, $this->quote, $errorMessage, $checkoutType);
         $expectedVars = [
-            'reason' => $errorMessage,
+            'reason' => $errorMessage->render(),
             'checkoutType' => $checkoutType,
-            'dateAndTime' => $templateTimeMethod->invoke($this->paymentFailures),
+            'dateAndTime' => $dateAndTime,
             'customer' => 'John Smith',
             'customerEmail' => 'aaa@aaa.com',
             'paymentMethod' => 'Some Title Of The Method',
