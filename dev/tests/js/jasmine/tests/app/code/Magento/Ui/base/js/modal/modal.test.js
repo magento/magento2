a1212509c813239ef8ab0236d623414831bbac12
Nazar Klovanych <nazarn96@gmail.com>
2020-01-20 12:37:34 +0200

    cover changes with jasmine test

--- dev/tests/js/jasmine/tests/app/code/Magento/Ui/base/js/modal/modal.test.js
+++ dev/tests/js/jasmine/tests/app/code/Magento/Ui/base/js/modal/modal.test.js
@@ -10,18 +10,34 @@ define([
     'use strict';
 
     describe('ui/js/modal/modal', function () {
-        var element = $('<div>some element</div>'),
+
+        var element = $('<div>Element</div>'),
             modal = element.modal({}).data('mage-modal');
+
+        $(element).append('<h1 class="modal-title"' +
+            ' data-role="title">Title</h1>' +
+            '<span class="modal-subtitle"' +
+            ' data-role="subTitle"></span>');
 
         it('Check for modal definition', function () {
             expect(modal).toBeDefined();
         });
+
         it('Show/hide function check', function () {
             expect(element.trigger('openModal')).toBe(element);
             expect(element.trigger('closeModal')).toBe(element);
         });
+
         it('Integration: modal created on page', function () {
             expect($(modal).length).toEqual(1);
+        });
+
+        it('Verify stTitle() method set title', function () {
+            var newTitle = 'New modal title';
+
+            modal.setTitle(newTitle);
+            expect($(modal.options.modalTitle).text()).toBe(newTitle);
+            expect($(modal.options.modalTitle).find(modal.options.modalSubTitle).length).toBe(1);
         });
     });
 });
