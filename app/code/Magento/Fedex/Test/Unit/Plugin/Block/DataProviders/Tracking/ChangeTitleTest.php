7a70a47677f64b7a4edf45c9e072a20b3deabeb8
Karyna Tsymbal <k.tsymbal@atwix.com>
2020-01-21 18:36:50 +0200

    Unit Test for Magento\Fedex\Plugin\Block\DataProviders\Tracking\ChangeTitle

---
+++ app/code/Magento/Fedex/Test/Unit/Plugin/Block/DataProviders/Tracking/ChangeTitleTest.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Fedex\Test\Unit\Plugin\Block\DataProviders\Tracking;
+
+use Magento\Fedex\Plugin\Block\DataProviders\Tracking\ChangeTitle;
+use Magento\Framework\Phrase;
+use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
+use Magento\Shipping\Block\DataProviders\Tracking\DeliveryDateTitle;
+use Magento\Shipping\Model\Tracking\Result\Status;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Unit Test for @see \Magento\Fedex\Plugin\Block\DataProviders\Tracking\ChangeTitle
+ */
+class ChangeTitleTest extends TestCase
+{
+    /**
+     * @var ChangeTitle|MockObject
+     */
+    private $plugin;
+
+    /**
+     * @inheritDoc
+     */
+    protected function setUp()
+    {
+        $objectManagerHelper = new ObjectManager($this);
+        $this->plugin = $objectManagerHelper->getObject(ChangeTitle::class);
+    }
+
+    /**
+     * Check if Title was changed
+     *
+     * @param string $carrierCode
+     * @param string $originalResult
+     * @param Phrase|string $finalResult
+     * @dataProvider testAfterGetTitleDataProvider
+     */
+    public function testAfterGetTitle($carrierCode, $originalResult, $finalResult)
+    {
+        /** @var DeliveryDateTitle|MockObject $subjectMock */
+        $subjectMock = $this->getMockBuilder(DeliveryDateTitle::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        /** @var Status|MockObject $trackingStatusMock */
+        $trackingStatusMock = $this->getMockBuilder(Status::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['getCarrier'])
+            ->getMock();
+        $trackingStatusMock->expects($this::once())
+            ->method('getCarrier')
+            ->willReturn($carrierCode);
+
+        $actual = $this->plugin->afterGetTitle($subjectMock, $originalResult, $trackingStatusMock);
+
+        $this->assertEquals($finalResult, $actual);
+    }
+
+    /**
+     * Data provider
+     *
+     * @return array
+     */
+    public function testAfterGetTitleDataProvider()
+    {
+        return [
+            [\Magento\Fedex\Model\Carrier::CODE, 'Original Title', __('Expected Delivery:')],
+            ['not-fedex', 'Original Title', 'Original Title'],
+        ];
+    }
+}
