c885c9fbf99c672a06222a33a9b123108ae2ccf9
Timon de Groot <timon@mooore.nl>
2020-01-22 14:55:16 +0100

    Fix confusing phpdoc in curl client

--- lib/internal/Magento/Framework/HTTP/Client/Curl.php
+++ lib/internal/Magento/Framework/HTTP/Client/Curl.php
@@ -107,9 +107,9 @@ class Curl implements \Magento\Framework\HTTP\ClientInterface
     protected $_headerCount = 0;
 
     /**
-     * Set request timeout, msec
+     * Set request timeout
      *
-     * @param int $value
+     * @param int $value value in seconds
      * @return void
      */
     public function setTimeout($value)
