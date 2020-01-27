79d4375cd94ca85cc54219ae4c75b23f73b41658
Krissy Hiserote <khiserote@magento.com>
2020-01-27 13:42:23 -0600

    MC-22040: Merge release branch into 2.3-develop
    - remove deprecated tag in PatchVersionInterface

--- lib/internal/Magento/Framework/Setup/Patch/PatchVersionInterface.php
+++ lib/internal/Magento/Framework/Setup/Patch/PatchVersionInterface.php
@@ -9,8 +9,6 @@ namespace Magento\Framework\Setup\Patch;
  * For backward compatibility with versioned style module installation.
  * The interface should be used for migration from the legacy installation approach to the declarative installation
  * mechanism. The usage of this interface prohibited for the new data or schema patches.
- *
- * @deprecated 102.0.0
  */
 interface PatchVersionInterface
 {
@@ -21,7 +19,6 @@ interface PatchVersionInterface
      * by old mechanism of UpgradeData.php script
      *
      * @return string
-     * @deprecated 102.0.0 since appearance, required for backward compatibility
      */
     public static function getVersion();
 }
