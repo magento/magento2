988309d6d0ad534846e37b5cb264e0ea51794679
eduard13 <e.chitoraga@atwix.com>
2020-01-28 09:27:03 +0200

    Covering the model classes by Unit Tests

--- app/code/Magento/Csp/Test/Unit/Model/Mode/ConfigManagerTest.php
+++ app/code/Magento/Csp/Test/Unit/Model/Mode/ConfigManagerTest.php
@@ -6,7 +6,7 @@
 
 declare(strict_types=1);
 
-namespace Magento\Csp\Test\Unit\Model;
+namespace Magento\Csp\Test\Unit\Model\Mode;
 
 use Magento\Csp\Model\Mode\ConfigManager;
 use Magento\Csp\Model\Mode\Data\ModeConfigured;
