1bb9a791e6fbf74a68cea5aeac48da0da2406e86
Nikita Shcherbatykh <nikita.shcherbatykh@transoftgroup.com>
2020-01-28 13:53:10 +0200

    MC-30391: Category not considered Configurable product in cart rule

--- dev/tests/integration/testsuite/Magento/SalesRule/Model/Rule/Condition/ProductTest.php
+++ dev/tests/integration/testsuite/Magento/SalesRule/Model/Rule/Condition/ProductTest.php
@@ -6,10 +6,6 @@
 
 namespace Magento\SalesRule\Model\Rule\Condition;
 
-use Magento\Quote\Api\CartRepositoryInterface;
-use Magento\Framework\Api\SearchCriteriaBuilder;
-use Magento\Quote\Api\Data\CartInterface;
-use Magento\SalesRule\Api\RuleRepositoryInterface;
 use Magento\Framework\Registry;
 use Magento\SalesRule\Model\Rule;
 
@@ -18,6 +14,8 @@ use Magento\SalesRule\Model\Rule;
  */
 class ProductTest extends \PHPUnit\Framework\TestCase
 {
+    use ConditionHelper;
+
     /**
      * @var \Magento\Framework\ObjectManagerInterface
      */
@@ -131,51 +129,6 @@ class ProductTest extends \PHPUnit\Framework\TestCase
         $this->assertFalse(
             $rule->validate($quote->getBillingAddress())
         );
-    }
-
-    /**
-     * Gets quote by reserved order id.
-     *
-     * @param string $reservedOrderId
-     * @return CartInterface
-     */
-    private function getQuote($reservedOrderId)
-    {
-        /** @var SearchCriteriaBuilder $searchCriteriaBuilder */
-        $searchCriteriaBuilder = $this->objectManager->get(SearchCriteriaBuilder::class);
-        $searchCriteria = $searchCriteriaBuilder->addFilter('reserved_order_id', $reservedOrderId)
-            ->create();
-
-        /** @var CartRepositoryInterface $quoteRepository */
-        $quoteRepository = $this->objectManager->get(CartRepositoryInterface::class);
-        $items = $quoteRepository->getList($searchCriteria)->getItems();
-        return array_pop($items);
-    }
-
-    /**
-     * Gets rule by name.
-     *
-     * @param string $name
-     * @return \Magento\SalesRule\Model\Rule
-     * @throws \Magento\Framework\Exception\InputException
-     * @throws \Magento\Framework\Exception\NoSuchEntityException
-     */
-    private function getSalesRule(string $name): \Magento\SalesRule\Model\Rule
-    {
-        /** @var SearchCriteriaBuilder $searchCriteriaBuilder */
-        $searchCriteriaBuilder = $this->objectManager->get(SearchCriteriaBuilder::class);
-        $searchCriteria = $searchCriteriaBuilder->addFilter('name', $name)
-            ->create();
-
-        /** @var CartRepositoryInterface $quoteRepository */
-        $ruleRepository = $this->objectManager->get(RuleRepositoryInterface::class);
-        $items = $ruleRepository->getList($searchCriteria)->getItems();
-
-        $rule = array_pop($items);
-        /** @var \Magento\SalesRule\Model\Converter\ToModel $converter */
-        $converter = $this->objectManager->get(\Magento\SalesRule\Model\Converter\ToModel::class);
-
-        return $converter->toModel($rule);
     }
 
     /**
