2c09a86a21ea04f0625495f48166a10e12cb4c11
Mykhailo Matiola <mykhailo.matiola@transoftgroup.com>
2020-01-29 14:00:16 +0200

    MC-30809: Indexer price calculation for configurable product

---
+++ dev/tests/integration/testsuite/Magento/CatalogRuleConfigurable/_files/configurable_product_with_percent_rules_for_children_rollback.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+use Magento\CatalogRule\Api\CatalogRuleRepositoryInterface;
+use Magento\CatalogRule\Model\Indexer\IndexBuilder;
+use Magento\CatalogRule\Model\ResourceModel\Rule\CollectionFactory;
+use Magento\TestFramework\Helper\Bootstrap;
+
+$objectManager = Bootstrap::getObjectManager();
+/** @var IndexBuilder $indexBuilder */
+$indexBuilder = $objectManager->get(IndexBuilder::class);
+/** @var CatalogRuleRepositoryInterface $ruleRepository */
+$ruleRepository = $objectManager->create(CatalogRuleRepositoryInterface::class);
+/** @var CollectionFactory $ruleCollectionFactory */
+$ruleCollectionFactory = $objectManager->get(CollectionFactory::class);
+$ruleCollection = $ruleCollectionFactory->create();
+$ruleCollection->addFieldToFilter(
+    'name',
+    [
+        'in' => [
+            'Percent rule for configurable product',
+            'Percent rule for first simple product',
+            'Percent rule for second simple product',
+        ]
+    ]
+);
+foreach ($ruleCollection as $rule) {
+    $ruleRepository->delete($rule);
+}
+$indexBuilder->reindexFull();
