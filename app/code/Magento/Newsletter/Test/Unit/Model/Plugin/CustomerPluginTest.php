97dfc74a574cfcea7654b0b05621898ca1f23416
DianaRusin <rusind95@gmail.com>
2020-02-04 12:31:02 +0200

    MC-29102: [2.4.x] [Magento Cloud] Customer receives newsletter unsubscription email after registering for new account

--- app/code/Magento/Newsletter/Test/Unit/Model/Plugin/CustomerPluginTest.php
+++ app/code/Magento/Newsletter/Test/Unit/Model/Plugin/CustomerPluginTest.php
@@ -134,7 +134,7 @@ class CustomerPluginTest extends TestCase
         }
         $this->subscriberFactory->method('create')->willReturn($subscriber);
 
-        $customerExtension = $this->createPartialMock(CustomerExtensionInterface::class, ['getIsSubscribed']);
+        $customerExtension = $this->getMockForAbstractClass(CustomerExtensionInterface::class);
         $customerExtension->method('getIsSubscribed')->willReturn($newValue);
         /** @var CustomerInterface|MockObject $customer */
         $customer = $this->createMock(CustomerInterface::class);
@@ -152,7 +152,7 @@ class CustomerPluginTest extends TestCase
             $this->subscriptionManager->expects($this->never())->method('subscribeCustomer');
             $this->subscriptionManager->expects($this->never())->method('unsubscribeCustomer');
         }
-        $resultExtension = $this->createPartialMock(CustomerExtensionInterface::class, ['setIsSubscribed']);
+        $resultExtension = $this->getMockForAbstractClass(CustomerExtensionInterface::class);
         $resultExtension->expects($this->once())->method('setIsSubscribed')->with($resultIsSubscribed);
         /** @var CustomerInterface|MockObject $result */
         $result = $this->createMock(CustomerInterface::class);
