f08cff3f06ab66ec52fdbfa9d2ffda932cfb55ab
Mykhailo Matiola <mykhailo.matiola@transoftgroup.com>
2020-02-04 16:55:45 +0200

    MC-30583: Storefront: Create/update customer

--- dev/tests/integration/testsuite/Magento/Customer/Model/AccountManagement/CreateAccountTest.php
+++ dev/tests/integration/testsuite/Magento/Customer/Model/AccountManagement/CreateAccountTest.php
@@ -54,7 +54,6 @@ class CreateAccountTest extends TestCase
         'email' => 'customer@example.com',
         'firstname' => 'First name',
         'lastname' => 'Last name',
-        'id' => null
     ];
 
     /**
@@ -72,16 +71,16 @@ class CreateAccountTest extends TestCase
     /**
      * @dataProvider createInvalidAccountDataProvider
      * @param array $customerData
+     * @param string $password
      * @param string $errorType
      * @param string $errorMessage
-     * @param string|null $password
      * @return void
      */
     public function testCreateAccountWithInvalidFields(
         array $customerData,
+        string $password,
         string $errorType,
-        array $errorMessage,
-        string $password
+        array $errorMessage
     ): void {
         $data = array_merge($this->defaultCustomerData, $customerData);
         $customerEntity = $this->customerFactory->create();
@@ -99,63 +98,63 @@ class CreateAccountTest extends TestCase
         return [
             'empty_firstname' => [
                 'customer_data' => ['firstname' => ''],
+                'password' => '_aPassword1',
                 'error_type' =>  Exception::class,
                 'error_message' => ['"%1" is a required value.', 'First Name'],
-                'password' => '_aPassword1',
             ],
             'empty_lastname' => [
                 'customer_data' => ['lastname' => ''],
+                'password' => '_aPassword1',
                 'error_type' =>  Exception::class,
                 'error_message' => ['"%1" is a required value.', 'Last Name'],
-                'password' => '_aPassword1',
             ],
             'empty_email' => [
                 'customer_data' => ['email' => ''],
+                'password' => '_aPassword1',
                 'error_type' => Exception::class,
                 'error_message' => ['The customer email is missing. Enter and try again.'],
-                'password' => '_aPassword1',
             ],
             'invalid_email' => [
                 'customer_data' => ['email' => 'zxczxczxc'],
+                'password' => '_aPassword1',
                 'error_type' => Exception::class,
                 'error_message' => ['"%1" is not a valid email address.', 'Email'],
-                'password' => '_aPassword1',
             ],
             'empty_password' => [
                 'customer_data' => [],
+                'password' => '',
                 'error_type' => InputException::class,
                 'error_message' => ['The password needs at least 8 characters. Create a new password and try again.'],
-                'password' => '',
             ],
             'invalid_password_minimum_length' => [
                 'customer_data' => [],
+                'password' => 'test',
                 'error_type' => InputException::class,
                 'error_message' => ['The password needs at least 8 characters. Create a new password and try again.'],
-                'password' => 'test',
             ],
             'invalid_password_maximum_length' => [
                 'customer_data' => [],
+                'password' => $this->getRandomNumericString(257),
                 'error_type' => InputException::class,
                 'error_message' => ['Please enter a password with at most 256 characters.'],
-                'password' => $this->getRandomString(257)
             ],
             'invalid_password_without_minimum_characters_classes' => [
                 'customer_data' => [],
+                'password' => 'test_password',
                 'error_type' => InputException::class,
                 'error_message' => [
                     'Minimum of different classes of characters in password is %1.'
                     . ' Classes of characters: Lower Case, Upper Case, Digits, Special Characters.',
                     3,
                 ],
-                'password' => 'test_password',
             ],
             'password_same_as_email' => [
                 'customer_data' => ['email' => 'test1@test.com'],
+                'password' => 'test1@test.com',
                 'error_type' => LocalizedException::class,
                 'error_message' => [
                     'The password can\'t be the same as the email address. Create a new password and try again.',
                 ],
-                'password' => 'test1@test.com',
             ],
         ];
     }
@@ -166,7 +165,7 @@ class CreateAccountTest extends TestCase
      * @param int $length
      * @return string
      */
-    private function getRandomString(int $length): string
+    private function getRandomNumericString(int $length): string
     {
         $string = '';
         for ($i = 0; $i <= $length; $i++) {
