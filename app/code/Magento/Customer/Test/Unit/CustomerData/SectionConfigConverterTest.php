66cbfaf6c70f46f59db3f587981e5464309c2ab2
Vitalii Zabaznov <vzabaznov@magento.com>
2020-02-05 13:33:29 -0600

    MC-30786: [Escalation] Re: Magento 2.3.4 | serious issues with local storage if custom sections.xml invalidations are active

--- app/code/Magento/Customer/Test/Unit/CustomerData/SectionConfigConverterTest.php
+++ app/code/Magento/Customer/Test/Unit/CustomerData/SectionConfigConverterTest.php
@@ -6,6 +6,7 @@
 
 namespace Magento\Customer\Test\Unit\CustomerData;
 
+use Magento\Framework\App\Arguments\ValidationState;
 use Magento\Framework\TestFramework\Unit\Helper\ObjectManager as ObjectManagerHelper;
 
 class SectionConfigConverterTest extends \PHPUnit\Framework\TestCase
@@ -19,6 +20,12 @@ class SectionConfigConverterTest extends \PHPUnit\Framework\TestCase
     /** @var \DOMDocument */
     protected $source;
 
+    /** @var \Magento\Framework\Config\Dom config merger */
+    private $configMergerClass;
+
+    /**  @var ValidationState */
+    private $validationStateMock;
+
     protected function setUp()
     {
         $this->source = new \DOMDocument();
@@ -26,21 +33,53 @@ class SectionConfigConverterTest extends \PHPUnit\Framework\TestCase
         $this->converter = $this->objectManagerHelper->getObject(
             \Magento\Customer\CustomerData\SectionConfigConverter::class
         );
+        $this->validationStateMock = $this->createMock(ValidationState::class);
+    }
+
+    /**
+     * Return newly created instance of a config merger
+     *
+     * @param string $mergerClass
+     * @param string $initialContents
+     * @return \Magento\Framework\Config\Dom
+     * @throws \UnexpectedValueException
+     */
+    private function createConfig($mergerClass, $initialContents)
+    {
+        $this->validationStateMock->method('isValidationRequired')->willReturn(\false);
+        return new $mergerClass(
+            $initialContents,
+            $this->validationStateMock,
+            [
+                '/config/action' => 'name',
+                '/config/action/section' => 'name',
+            ],
+            null,
+            null
+        );
     }
 
     public function testConvert()
     {
         $this->source->loadXML(file_get_contents(__DIR__ . '/_files/sections.xml'));
 
+        $this->configMergerClass = $this->createConfig(
+            'Magento\Framework\Config\Dom',
+            file_get_contents(__DIR__ . '/_files/sections.xml')
+        );
+
+        $this->configMergerClass->merge(file_get_contents(__DIR__ . '/_files/sections2.xml'));
+
         $this->assertEquals(
             [
                 'sections' => [
                     'customer/account/logout' => ['*'],
+                    'empty/test' => ['account'],
                     'customer/account/editpost' => ['account'],
                     'customer/account/createpost' => ['account', 'acc'],
                 ],
             ],
-            $this->converter->convert($this->source)
+            $this->converter->convert($this->configMergerClass->getDom())
         );
     }
 }
