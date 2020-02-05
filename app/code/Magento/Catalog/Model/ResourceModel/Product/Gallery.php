a05d61541314399465857f9713c3684bc87010a1
Anton Kaplya <a.kaplya@gmail.com>
2020-02-05 15:48:56 -0600

    Update app/code/Magento/Catalog/Model/ResourceModel/Product/Gallery.php
    Co-Authored-By: Alex Paliarush <paliarus@adobe.com>

--- app/code/Magento/Catalog/Model/ResourceModel/Product/Gallery.php
+++ app/code/Magento/Catalog/Model/ResourceModel/Product/Gallery.php
@@ -38,7 +38,7 @@ class Gallery extends \Magento\Framework\Model\ResourceModel\Db\AbstractDb
       * Gallery constructor.
       * @param \Magento\Framework\Model\ResourceModel\Db\Context $context
       * @param \Magento\Framework\EntityManager\MetadataPool $metadataPool
-      * @param null $connectionName
+      * @param string $connectionName
       * @throws \Exception
       */
     public function __construct(
