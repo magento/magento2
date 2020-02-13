4cf2ec8b0f4e5e4141e4804ea74a937b5552964e
Viktor Petryk <victor.petryk@transoftgroup.com>
2020-02-13 10:19:49 +0200

    MC-24260: Automate (convert) integration test MC-26219

--- dev/tests/integration/testsuite/Magento/CatalogImportExport/Model/Import/ProductTest.php
+++ dev/tests/integration/testsuite/Magento/CatalogImportExport/Model/Import/ProductTest.php
@@ -3049,5 +3049,6 @@ class ProductTest extends \Magento\TestFramework\Indexer\TestCase
 
         $simpleProduct = $this->productRepository->get('simple', false, null, true);
         $this->assertEquals('Varchar default value', $simpleProduct->getData('varchar_attribute'));
+        $this->assertEquals('Short description', $simpleProduct->getData('short_description'));
     }
 }
