4801a94b205658aec7420cc94bc3572e922d6231
DianaRusin <rusind95@gmail.com>
2020-02-14 12:31:22 +0200

    MC-31424: Create test for issue "<" and ">" symbols are changed to "&lt;" and "&gt;" in the frontend catalog search line

--- dev/tests/integration/testsuite/Magento/CatalogSearch/Block/ResultTest.php
+++ dev/tests/integration/testsuite/Magento/CatalogSearch/Block/ResultTest.php
@@ -48,7 +48,7 @@ class ResultTest extends \PHPUnit\Framework\TestCase
     /**
      * Verify search value escaping process
      *
-     * @magentoConfigFixture default/catalog/search/engine elasticsearch7
+     * @magentoConfigFixture default/catalog/search/engine elasticsearch6
      * @dataProvider toEscapeSearchTextDataProvider
      * @magentoAppArea frontend
      * @param string $searchValue
