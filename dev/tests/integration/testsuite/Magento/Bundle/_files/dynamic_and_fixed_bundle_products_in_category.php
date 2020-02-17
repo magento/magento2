78d6bd216e936b8f1ce46af66c555fab89f4a516
Mykhailo Matiola <mykhailo.matiola@transoftgroup.com>
2020-02-17 15:44:42 +0200

    MC-30188: Storefront: Layered Navigation with bundle product

--- dev/tests/integration/testsuite/Magento/Bundle/_files/dynamic_and_fixed_bundle_products_in_category.php
+++ dev/tests/integration/testsuite/Magento/Bundle/_files/dynamic_and_fixed_bundle_products_in_category.php
@@ -6,13 +6,18 @@
 declare(strict_types=1);
 
 use Magento\Catalog\Api\CategoryLinkManagementInterface;
-use Magento\TestFramework\Helper\Bootstrap;
+use Magento\Catalog\Helper\DefaultCategory;
 
 require __DIR__ . '/product.php';
 require __DIR__ . '/bundle_product_dropdown_options.php';
 require __DIR__ . '/../../Catalog/_files/category.php';
 
 /** @var CategoryLinkManagementInterface $categoryLinkManagement */
-$categoryLinkManagement = Bootstrap::getObjectManager()->create(CategoryLinkManagementInterface::class);
+$categoryLinkManagement = $objectManager->create(CategoryLinkManagementInterface::class);
+/** @var DefaultCategory $categoryHelper */
+$categoryHelper = $objectManager->get(DefaultCategory::class);
 $categoryLinkManagement->assignProductToCategories('bundle-product', [2, $category->getId()]);
-$categoryLinkManagement->assignProductToCategories('bundle-product-dropdown-options', [2, $category->getId()]);
+$categoryLinkManagement->assignProductToCategories(
+    'bundle-product-dropdown-options',
+    [$categoryHelper->getId(), $category->getId()]
+);
