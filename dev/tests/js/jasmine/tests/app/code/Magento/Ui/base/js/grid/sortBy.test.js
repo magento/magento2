9404f53dcc4e5104f9ba92d02724e42ac05e2e00
Shankar Konar <konar.shankar2013@gmail.com>
2020-02-19 18:11:17 +0530

    Refactoring jasmine test

--- dev/tests/js/jasmine/tests/app/code/Magento/Ui/base/js/grid/sortBy.test.js
+++ dev/tests/js/jasmine/tests/app/code/Magento/Ui/base/js/grid/sortBy.test.js
@@ -14,57 +14,54 @@ define([
         });
 
         describe('"preparedOptions" method', function () {
-            it('return empty array if sorting is disabled for the columns', function () {
-                var columns = [{
-                        sortable: false,
-                        label: 'magento',
-                        index: 'name'
-                    }],
-                    options = [];
-
-                sortByObj.preparedOptions(columns);
-                expect(sortByObj.options).toEqual(options);
-            });
-
-            it('return array of options if sorting is enabled for the columns', function () {
-                var columns = [{
-                        sortable: true,
-                        label: 'magento',
-                        index: 'name'
-                    }],
-                    options = [{
-                        value: 'name',
-                        label: 'magento'
-                    }];
-
-                sortByObj.preparedOptions(columns);
-                expect(sortByObj.options).toEqual(options);
-            });
-
-            it('return "isVisible" method true if column is sortable', function () {
-                var columns = [{
-                        sortable: true,
-                        label: 'magento',
-                        index: 'name'
-                    }];
-
-                sortByObj.preparedOptions(columns);
-                expect(sortByObj.isVisible()).toBeTruthy();
-            });
-
-            it('return "isVisible" method false if column is sortable', function () {
-                var columns = [{
+            it('sort option will not available if sorting is disabled for the columns', function () {
+                var columns = {
                     sortable: false,
                     label: 'magento',
                     index: 'name'
-                }];
+                };
 
-                sortByObj.preparedOptions(columns);
+                sortByObj.preparedOptions([columns]);
+                expect(sortByObj.options[0]).toBeUndefined();
+                expect(sortByObj.options[0]).toBeUndefined();
+            });
+
+            it('sort option will available if sorting is enabled for the columns', function () {
+                var columns = {
+                    sortable: true,
+                    label: 'magento',
+                    index: 'name'
+                };
+
+                sortByObj.preparedOptions([columns]);
+                expect(sortByObj.options[0].value).toEqual('name');
+                expect(sortByObj.options[0].label).toEqual('magento');
+            });
+
+            it('return "isVisible" method true if sorting is enabled for column', function () {
+                var columns = {
+                    sortable: true,
+                    label: 'magento',
+                    index: 'name'
+                };
+
+                sortByObj.preparedOptions([columns]);
+                expect(sortByObj.isVisible()).toBeTruthy();
+            });
+
+            it('return "isVisible" method false if sorting is disabled for column', function () {
+                var columns = {
+                    sortable: false,
+                    label: 'magento',
+                    index: 'name'
+                };
+
+                sortByObj.preparedOptions([columns]);
                 expect(sortByObj.isVisible()).toBeFalsy();
             });
         });
         describe('"applyChanges" method', function () {
-            it('return applied option', function () {
+            it('return applied options for sorting column', function () {
                 var applied = {
                     field: 'selectedOption',
                     direction: 'desc'
