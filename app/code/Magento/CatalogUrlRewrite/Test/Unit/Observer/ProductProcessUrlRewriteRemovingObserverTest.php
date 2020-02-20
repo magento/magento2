7256c67404c078fb8bbc78a411da04c6d871c001
m.mezhensky <m.mezhensky@atwix.com>
2020-02-20 12:17:14 +0200

    Unit test for Magento\CatalogUrlRewrite\Observer\ProductProcessUrlRewriteRemovingObserver

--- app/code/Magento/CatalogUrlRewrite/Test/Unit/Observer/ProductProcessUrlRewriteRemovingObserverTest.php
+++ app/code/Magento/CatalogUrlRewrite/Test/Unit/Observer/ProductProcessUrlRewriteRemovingObserverTest.php
@@ -11,19 +11,24 @@ namespace Magento\CatalogUrlRewrite\Test\Unit\Observer;
 use Magento\Catalog\Model\Product;
 use Magento\CatalogUrlRewrite\Model\ProductUrlRewriteGenerator;
 use Magento\CatalogUrlRewrite\Observer\ProductProcessUrlRewriteRemovingObserver;
-use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
 use Magento\Framework\Event;
 use Magento\Framework\Event\Observer;
-use Magento\UrlRewrite\Service\V1\Data\UrlRewrite;
+use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
 use Magento\UrlRewrite\Model\UrlPersistInterface;
-use PHPUnit\Framework\TestCase;
+use Magento\UrlRewrite\Service\V1\Data\UrlRewrite;
 use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
 
 /**
  * Unit test for Magento\CatalogUrlRewrite\Observer\ProductProcessUrlRewriteRemovingObserver
  */
 class ProductProcessUrlRewriteRemovingObserverTest extends TestCase
 {
+    /*
+     * Stub product ID
+     */
+    private const STUB_PRODUCT_ID = 333;
+
     /**
      * Testable Object
      *
@@ -54,7 +59,7 @@ class ProductProcessUrlRewriteRemovingObserverTest extends TestCase
     /**
      * @var UrlPersistInterface|MockObject
      */
-    private $urlPersist;
+    private $urlPersistMock;
 
     /**
      * @inheritdoc
@@ -74,25 +79,23 @@ class ProductProcessUrlRewriteRemovingObserverTest extends TestCase
             ->setMethods(['getId'])
             ->getMock();
 
-        $this->urlPersist = $this->getMockBuilder(UrlPersistInterface::class)
+        $this->urlPersistMock = $this->getMockBuilder(UrlPersistInterface::class)
             ->disableOriginalConstructor()
             ->getMock();
 
         $this->observer = $this->objectManager->getObject(
             ProductProcessUrlRewriteRemovingObserver::class,
             [
-                'urlPersist' => $this->urlPersist
+                'urlPersist' => $this->urlPersistMock
             ]
         );
     }
 
     /**
-     * Test for execute()
+     * Test for execute(), covers test case for removing product URLs from storage
      */
-    public function testRemoveProductUrlsFromStorage()
+    public function testRemoveProductUrlsFromStorage(): void
     {
-        $productId = 333;
-
         $this->observerMock
             ->expects($this->once())
             ->method('getEvent')
@@ -106,14 +109,39 @@ class ProductProcessUrlRewriteRemovingObserverTest extends TestCase
         $this->productMock
             ->expects($this->exactly(2))
             ->method('getId')
-            ->willReturn($productId);
+            ->willReturn(self::STUB_PRODUCT_ID);
 
-        $this->urlPersist->expects($this->once())
+        $this->urlPersistMock->expects($this->once())
             ->method('deleteByData')
             ->with([
-                UrlRewrite::ENTITY_ID => $productId,
+                UrlRewrite::ENTITY_ID => self::STUB_PRODUCT_ID,
                 UrlRewrite::ENTITY_TYPE => ProductUrlRewriteGenerator::ENTITY_TYPE,
             ]);
+
+        $this->observer->execute($this->observerMock);
+    }
+
+    /**
+     * Test for execute(), covers test case for removing product URLs when the product doesn't have an ID
+     */
+    public function testRemoveProductUrlsWithEmptyProductId()
+    {
+        $this->observerMock
+            ->expects($this->once())
+            ->method('getEvent')
+            ->willReturn($this->eventMock);
+
+        $this->eventMock
+            ->expects($this->once())
+            ->method('getProduct')
+            ->willReturn($this->productMock);
+
+        $this->productMock
+            ->expects($this->once())
+            ->method('getId')
+            ->willReturn(null);
+
+        $this->urlPersistMock->expects($this->never())->method('deleteByData');
 
         $this->observer->execute($this->observerMock);
     }
