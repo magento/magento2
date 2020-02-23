bcb6705b5456e21934193d726070a0b4278c832a
Tu Nguyen <ladiesman9x@gmail.com>
2020-02-23 16:58:00 +0700

    Add coverage test for checkout email component

---
+++ dev/tests/js/jasmine/tests/app/code/Magento/Checkout/frontend/js/view/form/element/email.test.js
@@ -0,0 +1,101 @@
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+/* eslint max-nested-callbacks: 0 */
+
+define(['squire', 'ko', 'jquery'], function (Squire, ko, $) {
+    'use strict';
+
+    var injector = new Squire(),
+        mocks = {
+            'Magento_Customer/js/model/customer': {
+                isLoggedIn: ko.observable()
+            },
+            'Magento_Customer/js/action/check-email-availability': jasmine.createSpy(),
+            'Magento_Customer/js/action/login': jasmine.createSpy(),
+            'Magento_Checkout/js/model/quote': {
+                isVirtual: jasmine.createSpy(),
+                shippingAddress: jasmine.createSpy()
+            },
+            'Magento_Checkout/js/checkout-data': jasmine.createSpyObj(
+                'checkoutData',
+                [
+                    'setInputFieldEmailValue',
+                    'setValidatedEmailValue',
+                    'setCheckedEmailValue',
+                    'getInputFieldEmailValue',
+                    'getValidatedEmailValue',
+                    'getCheckedEmailValue'
+                ]
+            ),
+            'Magento_Checkout/js/model/full-screen-loader': jasmine.createSpy(),
+            'mage/validation': jasmine.createSpy()
+        },
+        obj;
+
+    beforeEach(function (done) {
+        window.checkoutConfig = {
+            quoteData: {
+                entity_id: 1
+            },
+            /** Stub validated email */
+            validatedEmailValue: 'test@gmail.com',
+            formKey: 'formKey'
+        };
+
+        injector.mock(mocks);
+        injector.require(['Magento_Checkout/js/view/form/element/email'], function (Constr) {
+            obj = new Constr({
+                provider: 'provName',
+                name: '',
+                index: ''
+            });
+            done();
+        });
+    });
+
+    afterEach(function () {
+        try {
+            injector.clean();
+            injector.remove();
+        } catch (e) {}
+    });
+
+    describe('Magento_Checkout/js/view/form/element/email', function () {
+        describe('"setInputFieldEmailValue" method', function () {
+            it('Check method call setInputFieldEmailValue.', function () {
+                expect(mocks['Magento_Checkout/js/checkout-data'].setInputFieldEmailValue)
+                    .toHaveBeenCalledWith(window.checkoutConfig.validatedEmailValue);
+            });
+        });
+
+        describe('"initObservable" method', function () {
+            it('Check for defined ', function () {
+                expect(obj.hasOwnProperty('initObservable')).toBeDefined();
+            });
+            it('Check method type', function () {
+                var type = typeof obj.initialize;
+
+                expect(type).toEqual('function');
+            });
+        });
+
+        describe('"initConfig" method', function () {
+            it('Check for return type of current method.', function () {
+                expect(obj.initConfig()).toEqual(obj);
+            });
+
+            it('Check isPasswordVisible variable type', function () {
+                expect(typeof obj.isPasswordVisible()).toEqual('boolean');
+            });
+        });
+
+        describe('"resolveInitialPasswordVisibility" method', function () {
+            it('Check return type of method.', function () {
+                expect(typeof obj.resolveInitialPasswordVisibility()).toEqual('boolean');
+            });
+        });
+    });
+});
