df48d3e69fc17c7b54ddf2e33840da3ca3af2460
Nazar Klovanych <nazarn96@gmail.com>
2020-02-24 15:33:25 +0200

    refactore per review comment

--- dev/tests/js/jasmine/tests/app/code/Magento/Ui/frontend/js/model/messages.test.js
+++ dev/tests/js/jasmine/tests/app/code/Magento/Ui/frontend/js/model/messages.test.js
@@ -10,120 +10,101 @@ define([
     'use strict';
 
     describe('Magento_Ui/js/model/messages', function () {
-        var obj = new Constr({
-            provider: 'provName',
-            name: '',
-            index: ''
-        });
 
-        registry.set('provName', {
-            /** Stub */
-            on: function () {
-            },
+        var obj,
+            errorMessageText,
+            successMessageText,
+            messageObj;
 
-            /** Stub */
-            get: function () {
-            },
+        beforeEach(function () {
+            obj = new Constr(
+                {
+                    provider: 'provName',
+                    name: '',
+                    index: ''
+                });
+            errorMessageText = 'Error message test';
+            successMessageText = 'Success message test';
 
-            /** Stub */
-            set: function () {
-            }
-        });
+            registry.set('provName', {
+                /** Stub */
+                on: function () {
+                },
 
-        describe('initialize method', function () {
-            it('check for existing', function () {
-                expect(obj).toBeDefined();
+                /** Stub */
+                get: function () {
+                },
+
+                /** Stub */
+                set: function () {
+                }
             });
         });
 
-        describe('add method', function () {
-            it('simple message', function () {
-                var messageObj = {
-                        message: 'Message test'
-                    },
-                    type = [],
-                    returnedObj = ['Message test'];
+        it('simple message', function () {
+            var messageObj =
+                {
+                    message: 'Message test'
+                },
+                type = [];
 
-                expect(obj.add(messageObj, type)).toEqual(true);
-                expect(type).toEqual(returnedObj);
-            });
-
-            it('message with parameters', function () {
-                var messageObj = {
-                        message: 'Message test case %1, case %2 and case %3',
-                        parameters: [
-                            'one',
-                            'two',
-                            'three'
-                        ]
-                    },
-                    type = [],
-                    returnedObj = ['Message test case ' + messageObj.parameters[0] + ', case ' +
-                    messageObj.parameters[1] + ' and case ' + messageObj.parameters[2]];
-
-                expect(obj.add(messageObj, type)).toEqual(true);
-                expect(type).toEqual(returnedObj);
-            });
+            expect(obj.add(messageObj, type)).toEqual(true);
+            expect(type).toEqual([messageObj.message]);
         });
 
-        describe('check methods: hasMessages, addErrorMessage, getErrorMessages', function () {
-            var errorMessageText = 'Error message test';
+        it('message with parameters', function () {
+            var returnedObj,
+                type = [];
 
-            it('hasMessages method before adding messages', function () {
-                expect(obj.hasMessages()).toEqual(false);
-            });
+            messageObj = {
+                message: 'Message test case %1, case %2 and case %3',
+                parameters: [
+                    'one',
+                    'two',
+                    'three'
+                ]
+            };
+            returnedObj = ['Message test case ' + messageObj.parameters[0] + ', case ' +
+                messageObj.parameters[1] + ' and case ' + messageObj.parameters[2]];
 
-            it('check addErrorMessage method', function () {
-                var messageObj = {
-                    message: errorMessageText
-                };
-
-                expect(obj.addErrorMessage(messageObj)).toEqual(true);
-            });
-
-            it('check getErrorMessage method', function () {
-                expect(obj.getErrorMessages()()).toEqual([errorMessageText]);
-            });
-
-            it('hasMessages method after adding Error messages', function () {
-                expect(obj.hasMessages()).toEqual(true);
-            });
+            expect(obj.add(messageObj, type)).toEqual(true);
+            expect(type).toEqual(returnedObj);
         });
 
-        describe('check clean method for Error messages', function () {
-            it('check for cleaning messages', function () {
-                obj.clear();
-                expect(obj.getErrorMessages()()).toEqual([]);
-                expect(obj.hasMessages()).toEqual(false);
-            });
+        it('check addErrorMessage && getErrorMessage && hasMessages', function () {
+            messageObj = {
+                message: errorMessageText
+            };
+
+            expect(obj.hasMessages()).toEqual(false);
+            expect(obj.addErrorMessage(messageObj)).toEqual(true);
+            expect(obj.getErrorMessages()()).toEqual([errorMessageText]);
+            expect(obj.hasMessages()).toEqual(true);
         });
 
-        describe('check methods: hasMessages, addSuccessMessage, getSuccessMessages', function () {
-            var successMessageText = 'Success message test';
+        it('check addSuccessMessage && getSuccessMessage && hasMessages', function () {
+            messageObj = {
+                message: successMessageText
+            };
 
-            it('check addSuccessMessage and getSuccessMessage', function () {
-                var messageObj = {
-                    message: successMessageText
-                };
-
-                expect(obj.addSuccessMessage(messageObj)).toEqual(true);
-            });
-
-            it('check method getSuccessMessage', function () {
-                expect(obj.getSuccessMessages()()).toEqual([successMessageText]);
-            });
-
-            it('hasMessages method after adding Success messages', function () {
-                expect(obj.hasMessages()).toEqual(true);
-            });
+            expect(obj.addSuccessMessage(messageObj)).toEqual(true);
+            expect(obj.getSuccessMessages()()).toEqual([successMessageText]);
+            expect(obj.hasMessages()).toEqual(true);
         });
 
-        describe('check clean method for Success messages', function () {
-            it('check for cleaning messages', function () {
-                obj.clear();
-                expect(obj.getSuccessMessages()()).toEqual([]);
-                expect(obj.hasMessages()).toEqual(false);
-            });
+        it('check for cleaning messages', function () {
+            messageObj = {
+                message: 'Message test case %1, case %2 and case %3',
+                parameters: [
+                    'one',
+                    'two',
+                    'three'
+                ]
+            };
+            expect(obj.addErrorMessage(messageObj)).toEqual(true);
+            obj.clear();
+            expect(obj.getErrorMessages()()).toEqual([]);
+            expect(obj.hasMessages()).toEqual(false);
         });
     });
 });
