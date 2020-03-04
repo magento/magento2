2bdcc11de0bd8d127a3115383ea960577c840920
vadim.malesh <engcom-vendorworker-charlie@adobe.com>
2020-03-04 16:23:26 +0200

    14086: Guest cart API ignoring cartId in url for some methods

---
+++ app/code/Magento/Quote/Plugin/UpdateCartId.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Magento\Quote\Plugin;
+
+use Magento\Framework\Webapi\Rest\Request as RestRequest;
+use Magento\Quote\Api\Data\CartItemInterface;
+use Magento\Quote\Api\GuestCartItemRepositoryInterface;
+
+/**
+ * Update cart id from request param
+ */
+class UpdateCartId
+{
+    /**
+     * @var RestRequest $request
+     */
+    private $request;
+
+    /**
+     * @param RestRequest $request
+     */
+    public function __construct(RestRequest $request)
+    {
+        $this->request = $request;
+    }
+
+    /**
+     * Update id from request if param cartId exist
+     *
+     * @param GuestCartItemRepositoryInterface $guestCartItemRepository
+     * @param CartItemInterface $cartItem
+     * @return void
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function beforeSave(
+        GuestCartItemRepositoryInterface $guestCartItemRepository,
+        CartItemInterface $cartItem
+    ): void {
+        if ($cartId = $this->request->getParam('cartId')) {
+            $cartItem->setQuoteId($cartId);
+        }
+    }
+}
