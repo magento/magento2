5fbd204f9dbe0afab00abd888ed3ac48047f2e55
Cody Nguyen <cody@simicart.com>
2020-03-05 09:11:24 +0700

    Update Unit test

--- app/code/Magento/CatalogGraphQl/Test/Unit/Model/Resolver/Category/ImageTest.php
+++ app/code/Magento/CatalogGraphQl/Test/Unit/Model/Resolver/Category/ImageTest.php
@@ -17,17 +17,14 @@ class ImageTest extends TestCase
      */
     private $image;
 
-    /**
-     * @var ObjectManager
-     */
-    private $objectManager;
-
     protected function setUp()
     {
         $directoryList = $this->getMockBuilder(\Magento\Framework\Filesystem\DirectoryList::class)
             ->disableOriginalConstructor()
             ->setMethods(['getUrlPath'])
             ->getMock();
+        $directoryList->expects($this->once())->method('getUrlPath')
+            ->will($this->returnValue('http://example.com/pub/media/'));
         $this->image = new Image($directoryList);
     }
 
@@ -36,16 +33,19 @@ class ImageTest extends TestCase
         $field = $this->createMock(\Magento\Framework\GraphQl\Config\Element\Field::class);
         $store = $this->createMock(\Magento\Store\Model\Store::class);
         $store->expects($this->once())->method('getBaseUrl')->will($this->returnValue('http://example.com'));
-        $extensionAttribute = $this->createPartialMock(\Magento\GraphQl\Model\Query\ContextExtension::class, ['getStore']);
+        $extensionAttribute = $this
+            ->createPartialMock(\Magento\GraphQl\Model\Query\ContextExtension::class, ['getStore']);
         $extensionAttribute->expects($this->once())->method('getStore')->will($this->returnValue($store));
         $context = $this->createPartialMock(\Magento\GraphQl\Model\Query\Context::class, ['getExtensionAttributes']);
-        $context->expects($this->once())->method('getExtensionAttributes')->will($this->returnValue($extensionAttribute));
+        $context
+            ->expects($this->once())->method('getExtensionAttributes')->will($this->returnValue($extensionAttribute));
         $info = $this->createMock(\Magento\Framework\GraphQl\Schema\Type\ResolveInfo::class);
         $categoryMock  = $this->createPartialMock(
             \Magento\Catalog\Model\Category::class,
             ['getImage']
         );
-        $categoryMock->expects($this->once())->method('getImage')->will($this->returnValue('media/catalog/tmp/category/image.jpeg'));
+        $categoryMock->expects($this->once())->method('getImage')
+            ->will($this->returnValue('media/catalog/tmp/category/image.jpeg'));
         $value = ['model' =>  $categoryMock];
         $imageUrl = $this->image->resolve($field, $context, $info, $value);
         if ($imageUrl) {
