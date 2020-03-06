a5b2b11c17edad0997399454168363c95edc5295
Cris Pini <cris_pini@yahoo.co.uk>
2020-03-06 11:05:14 +0000

    Tweaked Dockerfile and k8s changes

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 #/bin/mv "/www/var/magento/"* /var/www/html/
 
-find /var/www/magento -type f -or -type d -exec mv -f '{}' /var/www/html/ \;
+find /var/www/magento/* -type f -or -type d -exec mv -f '{}' /var/www/html/ \;
