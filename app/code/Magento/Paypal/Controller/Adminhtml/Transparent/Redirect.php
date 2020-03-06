45b9e193ddd8a413cb041976de6f1d9da5f53d5f
Serhii Balko <serhii.balko@transoftgroup.com>
2020-03-06 16:49:50 +0200

    MC-31573: PayflowPro Checkout Broken with SameSite Cookie Changes from Chrome 80

--- app/code/Magento/Paypal/Controller/Adminhtml/Transparent/Redirect.php
+++ app/code/Magento/Paypal/Controller/Adminhtml/Transparent/Redirect.php
@@ -5,87 +5,9 @@
  */
 namespace Magento\Paypal\Controller\Adminhtml\Transparent;
 
-use Magento\Backend\App\AbstractAction;
-use Magento\Backend\App\Action\Context;
-use Magento\Framework\App\Action\HttpPostActionInterface;
-use Magento\Framework\App\CsrfAwareActionInterface;
-use Magento\Framework\App\Request\InvalidRequestException;
-use Magento\Framework\App\RequestInterface;
-use Magento\Framework\View\Result\LayoutFactory;
-use Magento\Payment\Model\Method\Logger;
-use Magento\Paypal\Model\Payflow\Transparent;
-
 /**
  * Class for redirecting the Paypal response result to Magento controller.
  */
-class Redirect extends AbstractAction implements HttpPostActionInterface, CsrfAwareActionInterface
+class Redirect extends \Magento\Paypal\Controller\Transparent\Redirect
 {
-    /**
-     * @var LayoutFactory
-     */
-    private $resultLayoutFactory;
-
-    /**
-     * @var Transparent
-     */
-    private $transparent;
-
-    /**
-     * @var Logger
-     */
-    private $logger;
-
-    /**
-     * @param Context $context
-     * @param LayoutFactory $resultLayoutFactory
-     * @param Transparent $transparent
-     * @param Logger $logger
-     */
-    public function __construct(
-        Context $context,
-        LayoutFactory $resultLayoutFactory,
-        Transparent $transparent,
-        Logger $logger
-    ) {
-        parent::__construct($context);
-        $this->transparent = $transparent;
-        $this->logger = $logger;
-        $this->resultLayoutFactory = $resultLayoutFactory;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function execute()
-    {
-        $gatewayResponse = (array)$this->getRequest()->getPostValue();
-        $this->logger->debug(
-            ['PayPal PayflowPro redirect:' => $gatewayResponse],
-            $this->transparent->getDebugReplacePrivateDataKeys(),
-            $this->transparent->getDebugFlag()
-        );
-
-        $resultLayout = $this->resultLayoutFactory->create();
-        $resultLayout->addDefaultHandle();
-        $resultLayout->getLayout()->getUpdate()->load(['transparent_payment_redirect']);
-
-        return $resultLayout;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function createCsrfValidationException(
-        RequestInterface $request
-    ): ?InvalidRequestException {
-        return null;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function validateForCsrf(RequestInterface $request): ?bool
-    {
-        return true;
-    }
 }
