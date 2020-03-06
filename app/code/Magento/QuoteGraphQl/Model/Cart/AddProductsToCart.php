af251eb2311a7d477f5c6516195b7032704c755a
AleksLi <aleksliwork@gmail.com>
2020-03-06 21:10:28 +0100

    MC-26683: Some sort of example how I see the solution of this.

--- app/code/Magento/QuoteGraphQl/Model/Cart/AddProductsToCart.php
+++ app/code/Magento/QuoteGraphQl/Model/Cart/AddProductsToCart.php
@@ -8,11 +8,12 @@ declare(strict_types=1);
 namespace Magento\QuoteGraphQl\Model\Cart;
 
 use Magento\Framework\GraphQl\Exception\GraphQlInputException;
+use Magento\Framework\Message\MessageInterface;
 use Magento\Quote\Api\CartRepositoryInterface;
 use Magento\Quote\Model\Quote;
 
 /**
- * Adding products to cart using GraphQL
+ * Add products to cart
  */
 class AddProductsToCart
 {
@@ -43,14 +44,27 @@ class AddProductsToCart
      *
      * @param Quote $cart
      * @param array $cartItems
+     * @return \Magento\Framework\GraphQl\Exception\GraphQlCartInputException
      * @throws GraphQlInputException
      * @throws \Magento\Framework\Exception\LocalizedException
      * @throws \Magento\Framework\GraphQl\Exception\GraphQlNoSuchEntityException
      */
-    public function execute(Quote $cart, array $cartItems): void
+    public function execute(Quote $cart, array $cartItems): \Magento\Framework\GraphQl\Exception\GraphQlCartInputException
     {
         foreach ($cartItems as $cartItemData) {
             $this->addProductToCart->execute($cart, $cartItemData);
+        }
+
+        if ($cart->getData('has_error')) {
+            $e = new \Magento\Framework\GraphQl\Exception\GraphQlCartInputException(__('Shopping cart errors'));
+            $errors = $cart->getErrors();
+            foreach ($errors as $error) {
+                /** @var MessageInterface $error */
+                $e->addError(new GraphQlInputException(__($error->getText())));
+            }
+            $e->addData($cartItems);
+
+            throw $e;
         }
 
         $this->cartRepository->save($cart);
