fb49aa9ae658faab78c322e0303ec257a2741d36
Cris Pini <cris_pini@yahoo.co.uk>
2020-03-09 17:11:05 +0000

    Changes 2

--- release.sh
+++ release.sh
@@ -11,6 +11,6 @@ find /var/www/magento -type f -or -type d -exec mv -f '{}' /var/www/html/ \;
 
 ## reset sample data
 ## clear cache etc
-cd /var/www/html/magento
+#cd /var/www/html/magento
 #php -d memory_limit=2G bin/magento sampledata:deploy 
-php bin/magento cache:flush
+#php bin/magento cache:flush
