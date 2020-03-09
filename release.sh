4cde9febc3f8370ff3c0344dec30f4850b0783a1
Cris Pini <cris_pini@yahoo.co.uk>
2020-03-09 14:05:00 +0000

    Tweaked release script

--- release.sh
+++ release.sh
@@ -12,5 +12,5 @@ find /var/www/magento -type f -or -type d -exec mv -f '{}' /var/www/html/ \;
 ## reset sample data
 ## clear cache etc
 cd /var/www/html/magento
-php bin/magento sampledata:reset
+php -d memory_limit=2G bin/magento sampledata:deploy 
 php bin/magento cache:flush
