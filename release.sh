ab7dd61977104135df09af6c072ff70f3e0cabcd
Cris Pini <cris_pini@yahoo.co.uk>
2020-03-09 18:20:10 +0000

    Changes 5

--- release.sh
+++ release.sh
@@ -1,18 +1,3 @@
 #!/bin/sh
 
-##########################################
-##
-## Release Actions
-##
-##########################################
-
-## move files into shared folder so apache and fpm have access
 find /var/www/magento -type f -or -type d -exec mv -f '{}' /var/www/html/ \;
-
-## reset sample data
-## clear cache etc
-cd /var/www/html/magento || return 0
-php -d memory_limit=2G bin/magento sampledata:deploy 
-php bin/magento cache:flush
-
-exit 1;
