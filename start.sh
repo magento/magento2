9157784ab7bd79d78957f00a26c2cef5ca0f2183
Cris Pini <cris_pini@yahoo.co.uk>
2020-03-09 20:12:50 +0000

    Changes 11

--- start.sh
+++ start.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+#############################################
+#
+# Release Actions !!!
+#
+#############################################
+
 find /var/www/magento -type f -or -type d -exec mv -f '{}' /var/www/html/ \;
 
 cd /var/www/html/magento
-php bin/magento sampledata:deploy
+php -d memory_limit=2G bin/magento sampledata:deploy
 php bin/magento cache:flush
