f8e5bf06a16319849407f39c2312b30451f5765c
Nikita Shcherbatykh <nikita.shcherbatykh@transoftgroup.com>
2020-03-10 14:19:06 +0200

    MC-31878: [Magento Cloud] - Order bulk update using rest api

--- app/code/Magento/WebapiAsync/Model/OperationRepository.php
+++ app/code/Magento/WebapiAsync/Model/OperationRepository.php
@@ -3,21 +3,20 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
-
 declare(strict_types=1);
 
-namespace Magento\AsynchronousOperations\Model\ResourceModel\Operation;
+namespace Magento\WebapiAsync\Model;
 
 use Magento\AsynchronousOperations\Api\Data\OperationInterface;
 use Magento\AsynchronousOperations\Api\Data\OperationInterfaceFactory;
 use Magento\AsynchronousOperations\Model\OperationRepositoryInterface;
 use Magento\Framework\MessageQueue\MessageValidator;
-use Magento\Framework\MessageQueue\MessageEncoder;
 use Magento\Framework\Serialize\Serializer\Json;
 use Magento\Framework\EntityManager\EntityManager;
+use Magento\WebapiAsync\Controller\Rest\Asynchronous\InputParamsResolver;
 
 /**
- * Create operation for list of bulk operations.
+ * Repository class to create operation
  */
 class OperationRepository implements OperationRepositoryInterface
 {
@@ -37,50 +36,50 @@ class OperationRepository implements OperationRepositoryInterface
     private $entityManager;
 
     /**
-     * @var MessageEncoder
-     */
-    private $messageEncoder;
-
-    /**
      * @var MessageValidator
      */
     private $messageValidator;
+    /**
+     * @var InputParamsResolver
+     */
+    private $inputParamsResolver;
 
     /**
+     * Initialize dependencies.
+     *
      * @param OperationInterfaceFactory $operationFactory
      * @param EntityManager $entityManager
      * @param MessageValidator $messageValidator
-     * @param MessageEncoder $messageEncoder
      * @param Json $jsonSerializer
+     * @param InputParamsResolver $inputParamsResolver
      */
     public function __construct(
         OperationInterfaceFactory $operationFactory,
         EntityManager $entityManager,
         MessageValidator $messageValidator,
-        MessageEncoder $messageEncoder,
-        Json $jsonSerializer
+        Json $jsonSerializer,
+        InputParamsResolver $inputParamsResolver
     ) {
         $this->operationFactory = $operationFactory;
         $this->jsonSerializer = $jsonSerializer;
-        $this->messageEncoder = $messageEncoder;
         $this->messageValidator = $messageValidator;
         $this->entityManager = $entityManager;
+        $this->inputParamsResolver = $inputParamsResolver;
     }
 
     /**
-     * Create operation by topic, parameters and group ID
-     *
-     * @param string $topicName
-     * @param array $entityParams
-     * @param string $groupId
-     * @return OperationInterface
-     * @deprecated No longer used.
-     * @see create()
+     * @inheritDoc
      */
-    public function createByTopic($topicName, $entityParams, $groupId)
+    public function create($topicName, $entityParams, $groupId, $operationId = null): OperationInterface
     {
         $this->messageValidator->validate($topicName, $entityParams);
-        $encodedMessage = $this->messageEncoder->encode($topicName, $entityParams);
+        $requestData = $this->inputParamsResolver->getInputData();
+        if ($operationId === null || !isset($requestData[$operationId])) {
+            throw new \InvalidArgumentException(
+                'Parameter "$operationId" must not be NULL and must exist in input data'
+            );
+        }
+        $encodedMessage = $this->jsonSerializer->serialize($requestData[$operationId]);
 
         $serializedData = [
             'entity_id'        => null,
@@ -99,13 +98,5 @@ class OperationRepository implements OperationRepositoryInterface
         /** @var OperationInterface $operation */
         $operation = $this->operationFactory->create($data);
         return $this->entityManager->save($operation);
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function create($topicName, $entityParams, $groupId, $operationId = null): OperationInterface
-    {
-        return $this->createByTopic($topicName, $entityParams, $groupId);
     }
 }
