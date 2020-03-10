213b9916495878fc732c8742850085d0b072bfe5
Cris Pini <cris_pini@yahoo.co.uk>
2020-03-10 16:22:57 +0000

    Modified to rollout sample code

--- start.sh
+++ start.sh
@@ -12,3 +12,4 @@ cd /var/www/html/magento
 php -d memory_limit=2G bin/magento sampledata:deploy
 php bin/magento setup:upgrade
 #php bin/magento setup:static-content:deploy -f
+rm -rf var/cache/* var/page_cache/*
