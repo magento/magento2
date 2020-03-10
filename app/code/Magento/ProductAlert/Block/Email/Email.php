3470d88c266a44a200b4da47a95ae586f6e8c6d0
Oleksandr Dubovyk <odubovyk@magento.com>
2020-03-10 13:33:03 -0500

    MC-31962: Product stock alert - unsubscribe for the product not working because cannot be accessed with GET method
    - fixed

---
+++ app/code/Magento/ProductAlert/Block/Email/Email.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Magento\ProductAlert\Block\Email;
+
+/**
+ * Email Block used to render the form with Back In Stock product id subject to unsubscription
+ */
+class Email extends \Magento\ProductAlert\Block\Email\AbstractEmail
+{
+    /**
+     * Retrieve unsubscribe url for product
+     *
+     * @return string
+     */
+    public function getProductUnsubscribeUrl(): string
+    {
+        return $this->getUrl('productalert/unsubscribe/stock');
+    }
+}
