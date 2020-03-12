677fe49c144bef827c95877993b728d6335c9ba9
Andrii Beziazychnyi <a.beziazychnyi@atwix.com>
2020-03-12 23:59:30 +0200

    magento/magento2: Additional fixes for the cache configuration

--- lib/internal/Magento/Framework/Cache/Config/Reader.php
+++ lib/internal/Magento/Framework/Cache/Config/Reader.php
@@ -3,8 +3,12 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+
 namespace Magento\Framework\Cache\Config;
 
+/**
+ * Cache configuration reader
+ */
 class Reader extends \Magento\Framework\Config\Reader\Filesystem
 {
     /**
@@ -15,6 +19,8 @@ class Reader extends \Magento\Framework\Config\Reader\Filesystem
     protected $_idAttributes = ['/config/type' => 'name'];
 
     /**
+     * Initialize dependencies.
+     *
      * @param \Magento\Framework\Config\FileResolverInterface $fileResolver
      * @param Converter $converter
      * @param SchemaLocator $schemaLocator
@@ -24,7 +30,7 @@ class Reader extends \Magento\Framework\Config\Reader\Filesystem
      * @param string $domDocumentClass
      * @param string $defaultScope
      */
-    public function __construct(
+    public function __construct( //phpcs:ignore Generic.CodeAnalysis.UselessOverridingMethod
         \Magento\Framework\Config\FileResolverInterface $fileResolver,
         \Magento\Framework\Cache\Config\Converter $converter,
         \Magento\Framework\Cache\Config\SchemaLocator $schemaLocator,
