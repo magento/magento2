dc27742d408aa144c85b8ef49318113228c43f1c
Yurii Sapiha <yurasapiga93@gmail.com>
2020-03-16 17:15:43 +0200

    MC-32136: Email product to friend

--- dev/tests/integration/testsuite/Magento/SendFriend/Model/SendFriendTest.php
+++ dev/tests/integration/testsuite/Magento/SendFriend/Model/SendFriendTest.php
@@ -7,13 +7,13 @@ declare(strict_types=1);
 
 namespace Magento\SendFriend\Model;
 
-use Laminas\Stdlib\Parameters;
 use Magento\Framework\App\RequestInterface;
 use Magento\Framework\ObjectManagerInterface;
 use Magento\Framework\Stdlib\CookieManagerInterface;
 use Magento\SendFriend\Helper\Data as SendFriendHelper;
 use Magento\TestFramework\Helper\Bootstrap;
 use PHPUnit\Framework\TestCase;
+use Zend\Stdlib\Parameters;
 
 /**
  * Class checks send friend model behavior
