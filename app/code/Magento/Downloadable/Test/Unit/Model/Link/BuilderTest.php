796255795513a80b6aa8d85b4bc7840185989100
tna <truongngocanh2794@gmail.com>
2020-03-16 23:07:50 +0700

    Fix the error that is wrong link title of a downloadable product when enabling "Use Default Value" - Update unit test

--- app/code/Magento/Downloadable/Test/Unit/Model/Link/BuilderTest.php
+++ app/code/Magento/Downloadable/Test/Unit/Model/Link/BuilderTest.php
@@ -160,7 +160,8 @@ class BuilderTest extends \PHPUnit\Framework\TestCase
         if (isset($data['is_unlimited'])) {
             $this->linkMock->expects($this->once())->method('setNumberOfDownloads')->with(0);
         }
-        if (isset($data['use_default_title']) && $data['use_default_title'] == '1') {
+        $useDefaultTitle = $data['use_default_title'] ?? false;
+        if ($useDefaultTitle) {
             $this->linkMock->expects($this->once())->method('setTitle')->with(null);
         }
         if (isset($data['price'])) {
