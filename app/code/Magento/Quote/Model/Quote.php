faced716b53f4d152113d0a0171cfc171ecbefa0
Leonid Poluianov <poluyano@addobe.com>
2020-03-19 14:42:32 -0500

    MC-32201: Reorder functionality

--- app/code/Magento/Quote/Model/Quote.php
+++ app/code/Magento/Quote/Model/Quote.php
@@ -947,8 +947,8 @@ class Quote extends AbstractExtensibleModel implements \Magento\Quote\Api\Data\C
             } else {
                 try {
                     $defaultBillingAddress = $this->addressRepository->getById($customer->getDefaultBilling());
+                    // phpcs:ignore Magento2.CodeAnalysis.EmptyBlock
                 } catch (\Magento\Framework\Exception\NoSuchEntityException $e) {
-                    // phpcs:ignore Magento2.CodeAnalysis.EmptyStatement
                 }
                 if (isset($defaultBillingAddress)) {
                     /** @var \Magento\Quote\Model\Quote\Address $billingAddress */
@@ -961,8 +961,8 @@ class Quote extends AbstractExtensibleModel implements \Magento\Quote\Api\Data\C
             if (null === $shippingAddress) {
                 try {
                     $defaultShippingAddress = $this->addressRepository->getById($customer->getDefaultShipping());
+                    // phpcs:ignore Magento2.CodeAnalysis.EmptyBlock
                 } catch (\Magento\Framework\Exception\NoSuchEntityException $e) {
-                    // phpcs:ignore Magento2.CodeAnalysis.EmptyStatement
                 }
                 if (isset($defaultShippingAddress)) {
                     /** @var \Magento\Quote\Model\Quote\Address $shippingAddress */
