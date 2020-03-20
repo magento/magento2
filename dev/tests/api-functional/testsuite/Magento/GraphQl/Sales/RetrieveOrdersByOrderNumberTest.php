16bcdac090f0d5d071913d35e95cf105a683e535
Deepty Thampy <dthampy@adobe.com>
2020-03-20 11:10:06 -0500

    MC-32491: Api funcional test coverage for retrieve customer order for similar product types
    - test for order retrieval for simple product

---
+++ dev/tests/api-functional/testsuite/Magento/GraphQl/Sales/RetrieveOrdersByOrderNumberTest.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\GraphQl\Sales;
+
+use Exception;
+use Magento\Integration\Api\CustomerTokenServiceInterface;
+use Magento\TestFramework\TestCase\GraphQlAbstract;
+use Magento\TestFramework\Helper\Bootstrap;
+
+/**
+ * Class RetrieveOrdersTest
+ */
+class RetrieveOrdersByOrderNumberTest extends GraphQlAbstract
+{
+    /**
+     * @var CustomerTokenServiceInterface
+     */
+    private $customerTokenService;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->customerTokenService = Bootstrap::getObjectManager()->get(CustomerTokenServiceInterface::class);
+    }
+
+    /**
+     * @magentoApiDataFixture Magento/Customer/_files/customer.php
+     * @magentoApiDataFixture Magento/Sales/_files/order_new.php
+     */
+    public function testGetCustomerOrdersSimpleProductQuery()
+    {
+        $query =
+            <<<QUERY
+{
+  customer
+  {
+   orders(filter:{number:{eq:"100000001"}}){
+    total_count
+    items
+    {
+      id
+      number
+      status
+      order_date
+      items{
+        quantity_ordered
+        product_sku
+        product_url
+        product_name
+        parent_product_sku
+        product_sale_price{currency value}
+      }
+    }
+   }
+ }
+}
+QUERY;
+
+        $currentEmail = 'customer@example.com';
+        $currentPassword = 'password';
+        $response = $this->graphQlQuery($query, [], '', $this->getCustomerAuthHeaders($currentEmail, $currentPassword));
+        $i =0;
+
+
+    }
+
+    /**
+     * @param string $email
+     * @param string $password
+     * @return array
+     * @throws \Magento\Framework\Exception\AuthenticationException
+     */
+    private function getCustomerAuthHeaders(string $email, string $password): array
+    {
+        $customerToken = $this->customerTokenService->createCustomerAccessToken($email, $password);
+        return ['Authorization' => 'Bearer ' . $customerToken];
+    }
+}
