062369e5389de0fda31198153580bb8cb861e013
Anusha Vattam <avattam@adobe.com>
2020-03-20 09:13:10 -0500

    MC-32479: Resolvers implementation for orderTotals
    - Added order totals resolver changes

--- app/code/Magento/SalesGraphQl/Model/Resolver/OrderTotals.php
+++ app/code/Magento/SalesGraphQl/Model/Resolver/OrderTotals.php
@@ -7,29 +7,16 @@ declare(strict_types=1);
 
 namespace Magento\SalesGraphQl\Model\Resolver;
 
+use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\GraphQl\Config\Element\Field;
 use Magento\Framework\GraphQl\Exception\GraphQlAuthorizationException;
 use Magento\Framework\GraphQl\Query\ResolverInterface;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
 use Magento\GraphQl\Model\Query\ContextInterface;
 use Magento\Sales\Model\Order;
-use Magento\Sales\Model\ResourceModel\Order\CollectionFactoryInterface;
 
 class OrderTotals implements ResolverInterface
-{/**
- * @var CollectionFactoryInterface
- */
-    private $collectionFactory;
-
-    /**
-     * @param CollectionFactoryInterface $collectionFactory
-     */
-    public function __construct(
-        CollectionFactoryInterface $collectionFactory
-    ) {
-        $this->collectionFactory = $collectionFactory;
-    }
-
+{
     /**
      * @inheritdoc
      */
@@ -44,23 +31,22 @@ class OrderTotals implements ResolverInterface
         if (false === $context->getExtensionAttributes()->getIsCustomer()) {
             throw new GraphQlAuthorizationException(__('The current customer isn\'t authorized.'));
         }
-        
-        $orderTotals = [];
-        $orders = $this->collectionFactory->create($context->getUserId());
 
-
-        /** @var Order $order */
-        foreach ($orders as $order) {
-            $currency = $order->getOrderCurrencyCode();
-            $orderTotals[] = [
-                'base_grand_total' => ['value' => $order->getBaseGrandTotal(), 'currency' => $currency],
-                'grand_total' => ['value' =>  $order->getGrandTotal(), 'currency' => $currency],
-                'sub_total' => ['value' =>  $order->getSubtotal(), 'currency' => $currency],
-                'tax' => ['value' =>  $order->getTaxAmount(), 'currency' => $currency],
-                'discounts' =>['value' =>  $order->getDiscountAmount(), 'currency' => $currency],
-                'shipping_handling' => ['value' => $order->getShippingAmount(), 'currency' => $currency]
-            ];
+        if (!isset($value['model']) && !($value['model'] instanceof Order)) {
+            throw new LocalizedException(__('"model" value should be specified'));
         }
-        return ['orderTotals' => $orderTotals];
+
+        /** @var Order $orderModel */
+        $orderModel = $value['model'];
+        $currency = $orderModel->getOrderCurrencyCode();
+        $totals = [
+                'base_grand_total' => ['value' => $orderModel->getBaseGrandTotal(), 'currency' => $currency],
+                'grand_total' => ['value' =>  $orderModel->getGrandTotal(), 'currency' => $currency],
+                'subtotal' => ['value' =>  $orderModel->getSubtotal(), 'currency' => $currency],
+                'tax' => ['value' =>  $orderModel->getTaxAmount(), 'currency' => $currency],
+                'shipping_handling' => ['value' => $orderModel->getShippingAmount(), 'currency' => $currency,
+            ]
+        ];
+        return $totals;
     }
 }
