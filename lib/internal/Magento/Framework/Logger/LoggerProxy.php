e1bd9367be16003ba59fad2b1935c8ac318b6e11
Andrii Kasian <akasian@magento.com>
2020-03-20 19:15:20 -0500

    Create and use Logger implementation based on deployment configuration

---
+++ lib/internal/Magento/Framework/Logger/LoggerProxy.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace Magento\Framework\Logger;
+
+use Magento\Framework\App\DeploymentConfig;
+use Magento\Framework\App\ObjectManager;
+use Magento\Framework\Exception\FileSystemException;
+use Magento\Framework\Exception\RuntimeException;
+use Magento\Framework\ObjectManager\NoninterceptableInterface;
+use Magento\Framework\ObjectManagerInterface;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Create and use Logger implementation based on deployment configuration
+ */
+class LoggerProxy implements LoggerInterface, NoninterceptableInterface
+{
+    /**
+     * @var ObjectManagerInterface
+     */
+    private $objectManager;
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @var DeploymentConfig
+     */
+    private $deploymentConfig;
+
+    /**
+     * Proxy constructor
+     *
+     * @param ObjectManagerInterface $objectManager
+     * @param DeploymentConfig $deploymentConfig
+     */
+    public function __construct(
+        ObjectManagerInterface $objectManager,
+        DeploymentConfig $deploymentConfig
+    ) {
+        $this->objectManager = $objectManager;
+        $this->deploymentConfig = $deploymentConfig;
+    }
+
+    /**
+     * Remove links to other objects.
+     *
+     * @return array
+     */
+    public function __sleep()
+    {
+        return ['logger'];
+    }
+
+    /**
+     * Retrieve ObjectManager from global scope
+     *
+     * @return void
+     */
+    public function __wakeup()
+    {
+        $this->objectManager = ObjectManager::getInstance();
+    }
+
+    /**
+     * Clone instance
+     *
+     * @return void
+     * @throws FileSystemException
+     * @throws RuntimeException
+     */
+    public function __clone()
+    {
+        $this->logger = clone $this->getLogger();
+    }
+
+    /**
+     * Get Logger instance
+     *
+     * @return LoggerInterface
+     * @throws FileSystemException
+     * @throws RuntimeException
+     */
+    private function getLogger(): LoggerInterface
+    {
+        if (!$this->logger) {
+            $instanceName = $this->deploymentConfig->get('log/type') ?? Monolog::class;
+            $args = $this->deploymentConfig->get('log/args');
+
+            if ($args) {
+                $this->logger = $this->objectManager->create($instanceName, $args);
+            } else {
+                $this->logger = $this->objectManager->get($instanceName);
+            }
+        }
+        return $this->logger;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function emergency($message, array $context = [])
+    {
+        $this->getLogger()->emergency($message, $context);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function alert($message, array $context = [])
+    {
+        $this->getLogger()->alert($message, $context);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function critical($message, array $context = [])
+    {
+        $this->getLogger()->critical($message, $context);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function error($message, array $context = [])
+    {
+        $this->getLogger()->error($message, $context);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function warning($message, array $context = [])
+    {
+        $this->getLogger()->warning($message, $context);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function notice($message, array $context = [])
+    {
+        $this->getLogger()->notice($message, $context);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function info($message, array $context = [])
+    {
+        $this->getLogger()->info($message, $context);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function debug($message, array $context = [])
+    {
+        $this->getLogger()->debug($message, $context);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function log($level, $message, array $context = [])
+    {
+        $this->getLogger()->log($level, $message, $context);
+    }
+}
