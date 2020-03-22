5af28c16212c2a91878fe0f49f4bdec796225050
Lukasz Bajsarowicz <lukasz.bajsarowicz@gmail.com>
2020-03-22 02:18:58 +0100

    Make change backwards-compatible

---
+++ app/code/Magento/Backend/Model/View/Result/Redirect.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\Backend\Model\View\Result;
+
+/**
+ * @deprecated Class responsibility moved to Plugin, use \Magento\Framework\Controller\Result\Redirect
+ * @see \Magento\Backend\Plugin\ControllerRedirectPlugin
+ * @see \Magento\Framework\Controller\Result\Redirect
+ *
+ * @api
+ * @since 100.0.2
+ */
+class Redirect extends \Magento\Framework\Controller\Result\Redirect
+{
+}
