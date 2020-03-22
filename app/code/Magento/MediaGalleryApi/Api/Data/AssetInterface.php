e31dafdd66aed1a64ea9f54d494dbadba558fb3a
konarshankar07 <konar.shankar2013@gmail.com>
2020-03-22 11:16:39 +0530

    Updated PHPDOC block

--- app/code/Magento/MediaGalleryApi/Api/Data/AssetInterface.php
+++ app/code/Magento/MediaGalleryApi/Api/Data/AssetInterface.php
@@ -66,7 +66,7 @@ interface AssetInterface extends ExtensibleDataInterface
     public function getWidth(): int;
 
     /**
-     * Retrieve full licensed asset's width
+     * Retrieve full licensed asset's size
      *
      * @return int
      */
