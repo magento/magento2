1b15e46974e28ca8f14ce9ee6f4f64908c8e9562
Oleksandr Gorkun <ogorkun@magento.com>
2020-03-23 16:03:33 -0500

    MC-24063: [2.4.x Port] Implement CSP

---
+++ app/code/Magento/Csp/Model/Collector/ControllerCollector.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\Csp\Model\Collector;
+
+use Magento\Csp\Api\CspAwareActionInterface;
+use Magento\Csp\Api\PolicyCollectorInterface;
+
+/**
+ * Asks for route-specific policies from a compatible controller.
+ */
+class ControllerCollector implements PolicyCollectorInterface
+{
+    /**
+     * @var CspAwareActionInterface|null
+     */
+    private $controller;
+
+    /**
+     * Set the action interface that is responsible for processing current HTTP request.
+     *
+     * @param CspAwareActionInterface $cspAwareAction
+     * @return void
+     */
+    public function setCurrentActionInstance(CspAwareActionInterface $cspAwareAction): void
+    {
+        $this->controller = $cspAwareAction;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function collect(array $defaultPolicies = []): array
+    {
+        if ($this->controller) {
+            return $this->controller->modifyCsp($defaultPolicies);
+        }
+
+        return $defaultPolicies;
+    }
+}
