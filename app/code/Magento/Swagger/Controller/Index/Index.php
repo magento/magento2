5c433fec3aff6e94ed756b196853ab51d567f040
Lukasz Bajsarowicz <lukasz.bajsarowicz@gmail.com>
2020-03-25 01:53:39 +0100

    Remove leftovers after core code

--- app/code/Magento/Swagger/Controller/Index/Index.php
+++ app/code/Magento/Swagger/Controller/Index/Index.php
@@ -3,18 +3,14 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types=1);
+
 namespace Magento\Swagger\Controller\Index;
 
 use Magento\Framework\App\Action\HttpGetActionInterface;
 use Magento\Framework\View\Page\Config as PageConfig;
-use Magento\Framework\View\Result\Page;
 use Magento\Framework\View\Result\PageFactory as PageFactory;
 
-/**
- * Class Index
- *
- * @package Magento\Swagger\Controller\Index
- */
 class Index implements HttpGetActionInterface
 {
     /**
