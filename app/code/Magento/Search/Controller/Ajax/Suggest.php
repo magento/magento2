54e8ead48d28a0089534376531523854ca982c83
Bartłomiej Szubert <bartlomiejszubert@gmail.com>
2020-03-25 01:26:18 +0100

    Implement ActionInterface for /search/ajax/suggest

--- app/code/Magento/Search/Controller/Ajax/Suggest.php
+++ app/code/Magento/Search/Controller/Ajax/Suggest.php
@@ -3,53 +3,90 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types=1);
+
 namespace Magento\Search\Controller\Ajax;
 
-use Magento\Framework\App\Action\HttpGetActionInterface as HttpGetActionInterface;
-use Magento\Framework\App\Action\Action;
-use Magento\Framework\App\Action\Context;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\Controller\Result\JsonFactory as ResultJsonFactory;
+use Magento\Framework\Controller\Result\RedirectFactory as ResultRedirectFactory;
+use Magento\Framework\UrlInterface;
 use Magento\Search\Model\AutocompleteInterface;
-use Magento\Framework\Controller\ResultFactory;
 
-class Suggest extends Action implements HttpGetActionInterface
+/**
+ * Ajax search autocomplete
+ */
+class Suggest implements HttpGetActionInterface
 {
     /**
-     * @var  \Magento\Search\Model\AutocompleteInterface
+     * @var RequestInterface
+     */
+    private $request;
+
+    /**
+     * @var ResultJsonFactory
+     */
+    private $resultJsonFactory;
+
+    /**
+     * @var ResultRedirectFactory
+     */
+    private $resultRedirectFactory;
+
+    /**
+     * @var UrlInterface
+     */
+    private $url;
+
+    /**
+     * @var AutocompleteInterface
      */
     private $autocomplete;
 
     /**
-     * @param \Magento\Framework\App\Action\Context $context
-     * @param \Magento\Search\Model\AutocompleteInterface $autocomplete
+     * @param RequestInterface $request
+     * @param ResultJsonFactory $resultJsonFactory
+     * @param ResultRedirectFactory $resultRedirectFactory
+     * @param UrlInterface $url
+     * @param AutocompleteInterface $autocomplete
      */
     public function __construct(
-        Context $context,
+        RequestInterface $request,
+        ResultJsonFactory $resultJsonFactory,
+        ResultRedirectFactory $resultRedirectFactory,
+        UrlInterface $url,
         AutocompleteInterface $autocomplete
     ) {
+        $this->request = $request;
+        $this->resultJsonFactory = $resultJsonFactory;
+        $this->resultRedirectFactory = $resultRedirectFactory;
+        $this->url = $url;
         $this->autocomplete = $autocomplete;
-        parent::__construct($context);
     }
 
     /**
-     * @return \Magento\Framework\Controller\ResultInterface
+     * @inheritDoc
      */
     public function execute()
     {
-        if (!$this->getRequest()->getParam('q', false)) {
-            /** @var \Magento\Framework\Controller\Result\Redirect $resultRedirect */
-            $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
-            $resultRedirect->setUrl($this->_url->getBaseUrl());
+        if (!$this->request->getParam('q', false)) {
+            $resultRedirect = $this->resultRedirectFactory->create();
+            $resultRedirect->setUrl($this->url->getBaseUrl());
+
             return $resultRedirect;
         }
 
         $autocompleteData = $this->autocomplete->getItems();
         $responseData = [];
+
         foreach ($autocompleteData as $resultItem) {
             $responseData[] = $resultItem->toArray();
         }
-        /** @var \Magento\Framework\Controller\Result\Json $resultJson */
-        $resultJson = $this->resultFactory->create(ResultFactory::TYPE_JSON);
+
+        $resultJson = $this->resultJsonFactory->create();
         $resultJson->setData($responseData);
+
         return $resultJson;
     }
 }
