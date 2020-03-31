bd9a345649bb15f35e370aff7f3358dbf935bf0d
Slava Mankivski <mankivsk@adobe.com>
2020-03-31 12:45:30 -0500

    Fixed static test

--- setup/src/Magento/Setup/Model/ConfigOptionsList/DriverOptions.php
+++ setup/src/Magento/Setup/Model/ConfigOptionsList/DriverOptions.php
@@ -46,7 +46,7 @@ class DriverOptions
      */
     private function optionExists($options, $driverOptionKey): bool
     {
-        return isset($options[$driverOptionKey]) 
+        return isset($options[$driverOptionKey])
             && ($options[$driverOptionKey] === false || !empty($options[$driverOptionKey]));
     }
 }
