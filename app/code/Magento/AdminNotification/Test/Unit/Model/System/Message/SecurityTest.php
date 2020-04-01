dbbd8261d6b9de9c11d07df6db8cf2c1c2abfc25
Ihor Sviziev <ihor-sviziev@users.noreply.github.com>
2020-04-01 21:18:27 +0300

    Update SecurityTest.php

--- app/code/Magento/AdminNotification/Test/Unit/Model/System/Message/SecurityTest.php
+++ app/code/Magento/AdminNotification/Test/Unit/Model/System/Message/SecurityTest.php
@@ -17,47 +17,42 @@ use PHPUnit\Framework\TestCase;
 class SecurityTest extends TestCase
 {
     /**
-     * @var MockObject
+     * @var CacheInterface|MockObject
      */
-    protected $_cacheMock;
+    private $cacheMock;
 
     /**
-     * @var MockObject
+     * @var ScopeConfigInterface|MockObject
      */
-    protected $_scopeConfigMock;
+    private $scopeConfigMock;
 
     /**
-     * @var MockObject
+     * @var CurlFactory|MockObject
      */
-    protected $_configMock;
-
-    /**
-     * @var MockObject
-     */
-    protected $_curlFactoryMock;
+    private $curlFactoryMock;
 
     /**
      * @var Security
      */
-    protected $_messageModel;
+    private $messageModel;
 
     protected function setUp(): void
     {
         //Prepare objects for constructor
-        $this->_cacheMock = $this->createMock(CacheInterface::class);
-        $this->_scopeConfigMock = $this->createMock(ScopeConfigInterface::class);
-        $this->_curlFactoryMock = $this->createPartialMock(
+        $this->cacheMock = $this->createMock(CacheInterface::class);
+        $this->scopeConfigMock = $this->createMock(ScopeConfigInterface::class);
+        $this->curlFactoryMock = $this->createPartialMock(
             CurlFactory::class,
             ['create']
         );
 
         $objectManagerHelper = new ObjectManager($this);
         $arguments = [
-            'cache' => $this->_cacheMock,
-            'scopeConfig' => $this->_scopeConfigMock,
-            'curlFactory' => $this->_curlFactoryMock,
+            'cache' => $this->cacheMock,
+            'scopeConfig' => $this->scopeConfigMock,
+            'curlFactory' => $this->curlFactoryMock,
         ];
-        $this->_messageModel = $objectManagerHelper->getObject(
+        $this->messageModel = $objectManagerHelper->getObject(
             Security::class,
             $arguments
         );
@@ -73,16 +68,16 @@ class SecurityTest extends TestCase
      */
     public function testIsDisplayed($expectedResult, $cached, $response)
     {
-        $this->_cacheMock->expects($this->any())->method('load')->will($this->returnValue($cached));
-        $this->_cacheMock->expects($this->any())->method('save')->will($this->returnValue(null));
+        $this->cacheMock->expects($this->any())->method('load')->will($this->returnValue($cached));
+        $this->cacheMock->expects($this->any())->method('save')->will($this->returnValue(null));
 
         $httpAdapterMock = $this->createMock(Curl::class);
         $httpAdapterMock->expects($this->any())->method('read')->will($this->returnValue($response));
-        $this->_curlFactoryMock->expects($this->any())->method('create')->will($this->returnValue($httpAdapterMock));
+        $this->curlFactoryMock->expects($this->any())->method('create')->will($this->returnValue($httpAdapterMock));
 
-        $this->_scopeConfigMock->expects($this->any())->method('getValue')->will($this->returnValue(null));
+        $this->scopeConfigMock->expects($this->any())->method('getValue')->will($this->returnValue(null));
 
-        $this->assertEquals($expectedResult, $this->_messageModel->isDisplayed());
+        $this->assertEquals($expectedResult, $this->messageModel->isDisplayed());
     }
 
     /**
@@ -101,6 +96,6 @@ class SecurityTest extends TestCase
     {
         $messageStart = 'Your web server is set up incorrectly';
 
-        $this->assertStringStartsWith($messageStart, (string)$this->_messageModel->getText());
+        $this->assertStringStartsWith($messageStart, (string)$this->messageModel->getText());
     }
 }
