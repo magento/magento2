83236655d16df09ce1123af24eb832c52ac66ce1
Oleh Usik <o.usik@atwix.com>
2020-04-02 19:07:04 +0300

    Fix Static check issue

--- app/code/Magento/Catalog/Block/Product/Widget/NewWidget.php
+++ app/code/Magento/Catalog/Block/Product/Widget/NewWidget.php
@@ -400,7 +400,7 @@ class NewWidget extends NewProduct implements BlockInterface
      *
      * @return bool
      */
-    public function getAllowedWishList()
+    public function hasAllowedWishList()
     {
         return $this->wishlistDataHelper->isAllow();
     }
