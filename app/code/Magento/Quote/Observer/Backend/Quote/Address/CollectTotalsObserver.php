b8c07a7681963ebef9c43a0599fab9306f17f302
tna <truongngocanh2794@gmail.com>
2020-04-03 21:53:33 +0700

    Fix bug 27490: Fix code standard

--- app/code/Magento/Quote/Observer/Backend/Quote/Address/CollectTotalsObserver.php
+++ app/code/Magento/Quote/Observer/Backend/Quote/Address/CollectTotalsObserver.php
@@ -8,7 +8,6 @@ declare(strict_types=1);
 namespace Magento\Quote\Observer\Backend\Quote\Address;
 
 use Magento\Framework\App\Area;
-use Magento\Framework\App\ObjectManager;
 use Magento\Framework\App\State;
 use Magento\Framework\Event\ObserverInterface;
 use Magento\Quote\Observer\Frontend\Quote\Address\VatValidator;
