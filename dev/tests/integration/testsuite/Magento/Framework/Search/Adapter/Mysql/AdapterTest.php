57521105fd6c5e4c3c70b94ee3bf645b84b724a6
Cristian Partica <cpartica@magento.com>
2020-04-03 19:15:58 -0500

    MC-18948: Switch default search engine from MySQL to ElasticSearch
    - skip integration tests

--- dev/tests/integration/testsuite/Magento/Framework/Search/Adapter/Mysql/AdapterTest.php
+++ dev/tests/integration/testsuite/Magento/Framework/Search/Adapter/Mysql/AdapterTest.php
@@ -44,7 +44,7 @@ class AdapterTest extends \PHPUnit\Framework\TestCase
 
     protected function setUp()
     {
-        $this->markTestSkipped ("MC-18948: Mysql Adapter and Search Engine is deprecated");
+        $this->markTestSkipped("MC-18948: Mysql Adapter and Search Engine is deprecated");
         $this->objectManager = Bootstrap::getObjectManager();
 
         /** @var \Magento\Framework\Search\Request\Config\Converter $converter */
