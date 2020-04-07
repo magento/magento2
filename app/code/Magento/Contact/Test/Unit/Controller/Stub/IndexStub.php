ed062834dead7fafd1cfc80d7ff5b87f2ab3f1ba
Sathish <srsathish92@gmail.com>
2020-04-07 09:29:42 +0530

    [Contact] Fix PHPUnit 8 compatibility issues

--- app/code/Magento/Contact/Test/Unit/Controller/Stub/IndexStub.php
+++ app/code/Magento/Contact/Test/Unit/Controller/Stub/IndexStub.php
@@ -3,13 +3,18 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types=1);
 
 namespace Magento\Contact\Test\Unit\Controller\Stub;
 
-class IndexStub extends \Magento\Contact\Controller\Index
+use Magento\Contact\Controller\Index;
+use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\Controller\ResultInterface;
+
+class IndexStub extends Index
 {
     /**
-     * @return \Magento\Framework\App\ResponseInterface|\Magento\Framework\Controller\ResultInterface|void
+     * @return ResponseInterface|ResultInterface|void
      */
     public function execute()
     {
