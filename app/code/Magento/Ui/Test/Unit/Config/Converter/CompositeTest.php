0a6caa94cce1ff02d2f50f8bf3f64ddf45e06fda
Lukasz Bajsarowicz <lukasz.bajsarowicz@gmail.com>
2020-04-11 21:38:14 +0000

    #27500 Prepare Ui module Tests for PHPUnit 8

--- app/code/Magento/Ui/Test/Unit/Config/Converter/CompositeTest.php
+++ app/code/Magento/Ui/Test/Unit/Config/Converter/CompositeTest.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 /**
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
@@ -7,15 +7,17 @@ namespace Magento\Ui\Test\Unit\Config\Converter;
 
 use Magento\Ui\Config\Converter\Composite;
 use Magento\Ui\Config\ConverterInterface;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
 
-class CompositeTest extends \PHPUnit\Framework\TestCase
+class CompositeTest extends TestCase
 {
     /**
-     * @var ConverterInterface|\PHPUnit_Framework_MockObject_MockObject
+     * @var ConverterInterface|MockObject
      */
     private $converter;
 
-    public function setUp()
+    public function setUp(): void
     {
         $this->converter = $this->getMockBuilder(ConverterInterface::class)->getMockForAbstractClass();
     }
@@ -37,11 +39,11 @@ class CompositeTest extends \PHPUnit\Framework\TestCase
 
     /**
      * @return void
-     * @expectedException \InvalidArgumentException
-     * @expectedExceptionMessage Argument converter named 'missedKey' has not been defined.
      */
     public function testConvertWithMissedConverter()
     {
+        $this->expectException('InvalidArgumentException');
+        $this->expectExceptionMessage('Argument converter named \'missedKey\' has not been defined.');
         $element = new \DOMElement('name');
         $composite = new Composite(['key' => $this->converter], 'type');
         $composite->convert($element, ['type' => 'missedKey']);
@@ -49,11 +51,11 @@ class CompositeTest extends \PHPUnit\Framework\TestCase
 
     /**
      * @return void
-     * @expectedException \InvalidArgumentException
-     * @expectedExceptionMessage Converter named 'key' is expected to be an argument converter instance.
      */
     public function testConvertWithInvalidConverter()
     {
+        $this->expectException('InvalidArgumentException');
+        $this->expectExceptionMessage('Converter named \'key\' is expected to be an argument converter instance.');
         $element = new \DOMElement('name');
         $std = new \stdClass();
         $composite = new Composite(['key' => $std], 'type');
