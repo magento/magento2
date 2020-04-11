082449373b17bb6c19114eb4a8e030f915a28fde
Lukasz Bajsarowicz <lukasz.bajsarowicz@gmail.com>
2020-04-11 08:04:59 +0000

    #27500 Code Review adjustments to AdminAnalytics module

--- app/code/Magento/AdminAnalytics/Test/Unit/Condition/CanViewNotificationTest.php
+++ app/code/Magento/AdminAnalytics/Test/Unit/Condition/CanViewNotificationTest.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 /**
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
@@ -8,9 +8,9 @@ namespace Magento\AdminAnalytics\Test\Unit\Condition;
 use Magento\AdminAnalytics\Model\Condition\CanViewNotification;
 use Magento\AdminAnalytics\Model\ResourceModel\Viewer\Logger;
 use Magento\AdminAnalytics\Model\Viewer\Log;
+use Magento\Framework\App\CacheInterface;
 use Magento\Framework\App\ProductMetadataInterface;
 use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
-use Magento\Framework\App\CacheInterface;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
