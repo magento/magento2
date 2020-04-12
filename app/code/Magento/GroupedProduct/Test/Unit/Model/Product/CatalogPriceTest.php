99e517fc9cd1cb332adf1553e6f503e74a9c2ca6
Lukasz Bajsarowicz <lukasz.bajsarowicz@gmail.com>
2020-04-12 00:04:31 +0000

    #27500 Prepare GroupedProduct module Tests for PHPUnit 8

--- app/code/Magento/GroupedProduct/Test/Unit/Model/Product/CatalogPriceTest.php
+++ app/code/Magento/GroupedProduct/Test/Unit/Model/Product/CatalogPriceTest.php
@@ -1,62 +1,71 @@
-<?php
+<?php declare(strict_types=1);
 /**
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
 namespace Magento\GroupedProduct\Test\Unit\Model\Product;
 
-class CatalogPriceTest extends \PHPUnit\Framework\TestCase
+use Magento\Catalog\Model\Product;
+use Magento\Catalog\Model\Product\Type\Price;
+use Magento\GroupedProduct\Model\Product\CatalogPrice;
+use Magento\GroupedProduct\Model\Product\Type\Grouped;
+use Magento\Store\Api\Data\StoreInterface;
+use Magento\Store\Model\StoreManagerInterface;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+class CatalogPriceTest extends TestCase
 {
     /**
-     * @var \Magento\GroupedProduct\Model\Product\CatalogPrice
+     * @var CatalogPrice
      */
     protected $catalogPrice;
 
     /**
-     * @var \PHPUnit_Framework_MockObject_MockObject
+     * @var MockObject
      */
     protected $storeManagerMock;
 
     /**
-     * @var \PHPUnit_Framework_MockObject_MockObject
+     * @var MockObject
      */
     protected $commonPriceMock;
 
     /**
-     * @var \PHPUnit_Framework_MockObject_MockObject
+     * @var MockObject
      */
     protected $productMock;
 
     /**
-     * @var \PHPUnit_Framework_MockObject_MockObject
+     * @var MockObject
      */
     protected $priceModelMock;
 
     /**
-     * @var \PHPUnit_Framework_MockObject_MockObject
+     * @var MockObject
      */
     protected $productTypeMock;
 
     /**
-     * @var \PHPUnit_Framework_MockObject_MockObject
+     * @var MockObject
      */
     protected $associatedProductMock;
 
-    protected function setUp()
+    protected function setUp(): void
     {
-        $this->storeManagerMock = $this->createMock(\Magento\Store\Model\StoreManagerInterface::class);
+        $this->storeManagerMock = $this->createMock(StoreManagerInterface::class);
         $this->commonPriceMock = $this->createMock(\Magento\Catalog\Model\Product\CatalogPrice::class);
         $productMethods = ['getWebsiteId', 'getCustomerGroupId', '__wakeup', 'getTypeInstance', 'setTaxClassId'];
-        $this->productMock = $this->createPartialMock(\Magento\Catalog\Model\Product::class, $productMethods);
+        $this->productMock = $this->createPartialMock(Product::class, $productMethods);
         $methods = ['setWebsiteId', 'isSalable', '__wakeup', 'setCustomerGroupId', 'getTaxClassId'];
-        $this->associatedProductMock = $this->createPartialMock(\Magento\Catalog\Model\Product::class, $methods);
+        $this->associatedProductMock = $this->createPartialMock(Product::class, $methods);
         $this->priceModelMock = $this->createPartialMock(
-            \Magento\Catalog\Model\Product\Type\Price::class,
+            Price::class,
             ['getTotalPrices']
         );
-        $this->productTypeMock = $this->createMock(\Magento\GroupedProduct\Model\Product\Type\Grouped::class);
+        $this->productTypeMock = $this->createMock(Grouped::class);
 
-        $this->catalogPrice = new \Magento\GroupedProduct\Model\Product\CatalogPrice(
+        $this->catalogPrice = new CatalogPrice(
             $this->storeManagerMock,
             $this->commonPriceMock
         );
@@ -130,9 +139,9 @@ class CatalogPriceTest extends \PHPUnit\Framework\TestCase
 
     public function testGetCatalogPriceWithCustomStoreAndSubProductIsSalable()
     {
-        $storeMock = $this->createMock(\Magento\Store\Api\Data\StoreInterface::class);
+        $storeMock = $this->createMock(StoreInterface::class);
         $storeMock->expects($this->once())->method('getId')->willReturn('store_id');
-        $currentStoreMock = $this->createMock(\Magento\Store\Api\Data\StoreInterface::class);
+        $currentStoreMock = $this->createMock(StoreInterface::class);
         $currentStoreMock->expects($this->once())->method('getId')->willReturn('current_store_id');
 
         $this->productMock->expects(
--- app/code/Magento/GroupedProduct/Test/Unit/Model/Product/CopyConstructor/GroupedTest.php
+++ app/code/Magento/GroupedProduct/Test/Unit/Model/Product/CopyConstructor/GroupedTest.php
@@ -1,53 +1,61 @@
-<?php
+<?php declare(strict_types=1);
 /**
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
 namespace Magento\GroupedProduct\Test\Unit\Model\Product\CopyConstructor;
 
-class GroupedTest extends \PHPUnit\Framework\TestCase
+use Magento\Catalog\Model\Product;
+use Magento\Catalog\Model\Product\Link;
+use Magento\Catalog\Model\ResourceModel\Product\Link\Collection;
+use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
+use Magento\GroupedProduct\Model\Product\CopyConstructor\Grouped;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+class GroupedTest extends TestCase
 {
     /**
-     * @var \Magento\GroupedProduct\Model\Product\CopyConstructor\Grouped
+     * @var Grouped
      */
     protected $_model;
 
     /**
-     * @var \PHPUnit_Framework_MockObject_MockObject
+     * @var MockObject
      */
     protected $_productMock;
 
     /**
-     * @var \PHPUnit_Framework_MockObject_MockObject
+     * @var MockObject
      */
     protected $_duplicateMock;
 
     /**
-     * @var \PHPUnit_Framework_MockObject_MockObject
+     * @var MockObject
      */
     protected $_linkMock;
 
     /**
-     * @var \PHPUnit_Framework_MockObject_MockObject
+     * @var MockObject
      */
     protected $_linkCollectionMock;
 
-    protected function setUp()
+    protected function setUp(): void
     {
-        $this->_model = new \Magento\GroupedProduct\Model\Product\CopyConstructor\Grouped();
+        $this->_model = new Grouped();
 
         $this->_productMock = $this->createPartialMock(
-            \Magento\Catalog\Model\Product::class,
+            Product::class,
             ['getTypeId', '__wakeup', 'getLinkInstance']
         );
 
         $this->_duplicateMock = $this->createPartialMock(
-            \Magento\Catalog\Model\Product::class,
+            Product::class,
             ['setGroupedLinkData', '__wakeup']
         );
 
         $this->_linkMock = $this->createPartialMock(
-            \Magento\Catalog\Model\Product\Link::class,
+            Link::class,
             ['setLinkTypeId', '__wakeup', 'getAttributes', 'getLinkCollection']
         );
 
@@ -71,7 +79,7 @@ class GroupedTest extends \PHPUnit\Framework\TestCase
 
     public function testBuild()
     {
-        $helper = new \Magento\Framework\TestFramework\Unit\Helper\ObjectManager($this);
+        $helper = new ObjectManager($this);
         $expectedData = ['100500' => ['some' => 'data']];
 
         $this->_productMock->expects(
@@ -110,7 +118,7 @@ class GroupedTest extends \PHPUnit\Framework\TestCase
         );
 
         $collectionMock = $helper->getCollectionMock(
-            \Magento\Catalog\Model\ResourceModel\Product\Link\Collection::class,
+            Collection::class,
             [$productLinkMock]
         );
         $collectionMock->expects($this->once())->method('setProduct')->with($this->_productMock);
