a2f366aac8dcbc7d7528e483c82e96ef0741e80f
Lukasz Bajsarowicz <lukasz.bajsarowicz@gmail.com>
2020-04-12 15:27:29 +0200

    #27500 Fix static checks

--- app/code/Magento/Analytics/Test/Unit/Model/Connector/NotifyDataChangedCommandTest.php
+++ app/code/Magento/Analytics/Test/Unit/Model/Connector/NotifyDataChangedCommandTest.php
@@ -19,6 +19,9 @@ use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Psr\Log\LoggerInterface;
 
+/**
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+ */
 class NotifyDataChangedCommandTest extends TestCase
 {
     /**
