d97a0039d218155a95e3de3b12647efba8ababb9
engcom-Echo <engcom-vendorworker-echo@adobe.com>
2020-04-13 17:00:49 +0300

    Add integration test

---
+++ dev/tests/integration/testsuite/Magento/Sales/Model/ResourceModel/Order/Customer/CollectionTest.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\Sales\Model\ResourceModel\Order\Customer;
+
+use Magento\TestFramework\Helper\Bootstrap;
+use PHPUnit\Framework\TestCase;
+
+class CollectionTest extends TestCase
+{
+    /**
+     * @var Collection
+     */
+    private $collection;
+
+    /**
+     * @inheritDoc
+     */
+    public function setUp()
+    {
+        $objectManager = Bootstrap::getObjectManager();
+        $this->collection = $objectManager->get(Collection::class);
+    }
+
+    /**
+     * Attribute presence test
+     *
+     * @magentoDataFixture Magento/Customer/_files/customer.php
+     * @magentoDataFixture Magento/Customer/_files/customer_address.php
+     * @dataProvider joinAttribute
+     * @param string $attribute
+     * @return void
+     */
+    public function testAttributePresent($attribute): void
+    {
+        $customers = $this->collection->getItems();
+        foreach ($customers as $customer) {
+            $this->assertNotEmpty($customer->getData($attribute), "Attribute '$attribute' does not present");
+        }
+    }
+
+    /**
+     * Attribute data provider
+     *
+     * @return array
+     */
+    public function joinAttribute():array
+    {
+        return [
+            ['billing_postcode'],
+            ['billing_city'],
+            ['billing_telephone'],
+            ['billing_region'],
+            ['billing_country_id']
+        ];
+    }
+}
