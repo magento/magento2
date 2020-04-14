b219c4e8a260bd6742a0d19954aa157791ac6727
Alexander Steshuk <grp-engcom-vendorworker-Kilo@adobe.com>
2020-04-14 15:53:08 +0300

    Added test for API functional. Check for non exist cookie PHPSESSID, for non exist session of REST call.

---
+++ dev/tests/api-functional/testsuite/Magento/Webapi/RestSessionCookieTest.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Webapi;
+
+use Magento\TestFramework\Helper\Bootstrap;
+
+/**
+ * Class for RestSessionCookieTest
+ */
+class RestSessionCookieTest extends \Magento\TestFramework\TestCase\WebapiAbstract
+{
+
+    /**
+     * Check for non exist cookie PHPSESSID
+     */
+    public function testRestNoAcceptHeader()
+    {
+        $this->_markTestAsRestOnly();
+        /** @var $curlClient CurlClientWithCookies */
+
+        $curlClient = Bootstrap::getObjectManager()
+            ->get(\Magento\TestFramework\TestCase\HttpClient\CurlClientWithCookies::class);
+        $phpSessionCookieName =
+            [
+                'cookie_name' => 'PHPSESSID',
+            ];
+
+        $response = $curlClient->get('/rest/V1/directory/countries', []);
+
+        $cookie = $this->findCookie($phpSessionCookieName['cookie_name'], $response['cookies']);
+        $this->assertNull($cookie);
+    }
+
+    /**
+     * Find cookie with given name in the list of cookies
+     *
+     * @param string $cookieName
+     * @param array $cookies
+     * @return $cookie|null
+     * @SuppressWarnings(PHPMD.UnusedLocalVariable)
+     */
+    private function findCookie($cookieName, $cookies)
+    {
+        foreach ($cookies as $cookieIndex => $cookie) {
+            if ($cookie['name'] === $cookieName) {
+                return $cookie;
+            }
+        }
+        return null;
+    }
+}
