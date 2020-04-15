99924e5c8260752d6be82b964edcde4ad062c75d
Hwashiang Yu <hwyu@adobe.com>
2020-04-14 20:57:21 -0500

    MC-30175: UI component rendering config
    - Added changes from MC-21979

--- dev/tests/integration/testsuite/Magento/Setup/Console/Command/_files/root/lib/internal/Magento/Framework/Test/Unit/View/Element/UiComponentFactoryTest.php
+++ dev/tests/integration/testsuite/Magento/Setup/Console/Command/_files/root/lib/internal/Magento/Framework/Test/Unit/View/Element/UiComponentFactoryTest.php
@@ -6,6 +6,7 @@
 namespace Magento\Framework\Test\Unit\View\Element;
 
 use Magento\Framework\TestFramework\Unit\Helper\ObjectManager as ObjectManagerHelper;
+use Magento\Framework\View\Element\UiComponent\DataProvider\Sanitizer;
 use PHPUnit\Framework\MockObject\MockObject;
 
 class UiComponentFactoryTest extends \PHPUnit\Framework\TestCase
@@ -59,6 +60,9 @@ class UiComponentFactoryTest extends \PHPUnit\Framework\TestCase
             ->disableOriginalConstructor()
             ->getMock();
         $this->dataMock = $this->createMock(\Magento\Framework\Config\DataInterface::class);
+        $sanitizerMock = $this->createMock(Sanitizer::class);
+        $sanitizerMock->method('sanitize')->willReturnArgument(0);
+        $sanitizerMock->method('sanitizeComponentMetadata')->willReturnArgument(0);
         $this->objectManagerHelper = new ObjectManagerHelper($this);
         $this->model = $this->objectManagerHelper->getObject(
             \Magento\Framework\View\Element\UiComponentFactory::class,
@@ -69,7 +73,8 @@ class UiComponentFactoryTest extends \PHPUnit\Framework\TestCase
                 'configFactory' => $this->dataInterfaceFactoryMock,
                 'data' => [],
                 'componentChildFactories' => [],
-                'definitionData' => $this->dataMock
+                'definitionData' => $this->dataMock,
+                'sanitizer' => $sanitizerMock
             ]
         );
     }
