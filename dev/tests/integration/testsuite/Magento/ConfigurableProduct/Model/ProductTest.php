8280a9a6478d27131e7a12fa4b112baaf84ca86f
Oleksandr Iegorov <oiegorov@magento.com>
2020-04-16 16:17:10 -0500

    MC-32022: X-Magento-Tags header too large

--- dev/tests/integration/testsuite/Magento/ConfigurableProduct/Model/ProductTest.php
+++ dev/tests/integration/testsuite/Magento/ConfigurableProduct/Model/ProductTest.php
@@ -22,7 +22,42 @@ class ProductTest extends \PHPUnit\Framework\TestCase
         $simple10Product = $productRepository->get('simple_10');
         $simple20Product = $productRepository->get('simple_20');
 
-        $this->assertEmpty(array_diff($confProduct->getIdentities(), $simple10Product->getIdentities()));
-        $this->assertEmpty(array_diff($confProduct->getIdentities(), $simple20Product->getIdentities()));
+        $this->assertNotEmpty(array_diff($confProduct->getIdentities(), $simple10Product->getIdentities()));
+        $this->assertNotEmpty(array_diff($confProduct->getIdentities(), $simple20Product->getIdentities()));
+    }
+
+    /**
+     * Check that no children identities are added to the parent product in frontend area
+     *
+     * @magentoDataFixture Magento/ConfigurableProduct/_files/product_configurable.php
+     * @magentoAppArea frontend
+     * @return void
+     */
+    public function testGetIdentitiesForConfigurableProductOnStorefront(): void
+    {
+        $productRepository = Bootstrap::getObjectManager()->get(ProductRepositoryInterface::class);
+        $configurableProduct = $productRepository->get('configurable');
+        $expectedIdentities = [
+            'cat_p_' . $configurableProduct->getId(),
+            'cat_p'
+        ];
+        $this->assertEquals($expectedIdentities, $configurableProduct->getIdentities());
+    }
+
+    /**
+     * Check that no children identities are added to the parent product in frontend area
+     *
+     * @magentoDataFixture Magento/ConfigurableProduct/_files/product_configurable.php
+     * @magentoAppArea adminhtml
+     * @return void
+     */
+    public function testGetIdentitiesForConfigurableProductInAdminArea(): void
+    {
+        $productRepository = Bootstrap::getObjectManager()->get(ProductRepositoryInterface::class);
+        $configurableProduct = $productRepository->get('configurable');
+        $expectedIdentities = [
+            'cat_p_' . $configurableProduct->getId(),
+        ];
+        $this->assertEquals($expectedIdentities, $configurableProduct->getIdentities());
     }
 }
