120aa1b9d48b55ce0a405325eea4d45220de450f
Lukasz Bajsarowicz <lukasz.bajsarowicz@gmail.com>
2020-04-16 21:53:32 +0000

    #27500 Code Review adjustments to Widget module

--- app/code/Magento/Widget/Test/Unit/Model/ResourceModel/Widget/Instance/Options/ThemesTest.php
+++ app/code/Magento/Widget/Test/Unit/Model/ResourceModel/Widget/Instance/Options/ThemesTest.php
@@ -1,8 +1,10 @@
-<?php declare(strict_types=1);
+<?php
 /**
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types=1);
+
 namespace Magento\Widget\Test\Unit\Model\ResourceModel\Widget\Instance\Options;
 
 use Magento\Theme\Model\ResourceModel\Theme\Collection as ThemeCollection;
