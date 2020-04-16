3020fcd3e762409077545103f514a58b43f0f844
Lukasz Bajsarowicz <lukasz.bajsarowicz@gmail.com>
2020-04-16 22:28:20 +0000

    #27500 Code Review adjustments to Search module

--- app/code/Magento/Search/Test/Unit/Controller/Adminhtml/Term/IndexTest.php
+++ app/code/Magento/Search/Test/Unit/Controller/Adminhtml/Term/IndexTest.php
@@ -1,9 +1,11 @@
-<?php declare(strict_types=1);
+<?php
 /**
  *
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types=1);
+
 namespace Magento\Search\Test\Unit\Controller\Adminhtml\Term;
 
 use Magento\Backend\Model\View\Result\Page;
