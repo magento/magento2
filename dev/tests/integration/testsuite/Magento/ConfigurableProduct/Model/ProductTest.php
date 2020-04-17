520590a2db348a1f8e6d4c1a8274a4f248fcdcb8
Oleksandr Iegorov <oiegorov@magento.com>
2020-04-17 11:22:23 -0500

    MC-29755: X-Magento-Tags header too large

--- dev/tests/integration/testsuite/Magento/ConfigurableProduct/Model/ProductTest.php
+++ dev/tests/integration/testsuite/Magento/ConfigurableProduct/Model/ProductTest.php
@@ -13,20 +13,6 @@ use Magento\Catalog\Api\ProductRepositoryInterface;
 class ProductTest extends \PHPUnit\Framework\TestCase
 {
     /**
-     * @magentoDataFixture Magento/ConfigurableProduct/_files/product_configurable.php
-     */
-    public function testGetIdentities()
-    {
-        $productRepository = Bootstrap::getObjectManager()->get(ProductRepositoryInterface::class);
-        $confProduct = $productRepository->get('configurable');
-        $simple10Product = $productRepository->get('simple_10');
-        $simple20Product = $productRepository->get('simple_20');
-
-        $this->assertNotEmpty(array_diff($confProduct->getIdentities(), $simple10Product->getIdentities()));
-        $this->assertNotEmpty(array_diff($confProduct->getIdentities(), $simple20Product->getIdentities()));
-    }
-
-    /**
      * Check that no children identities are added to the parent product in frontend area
      *
      * @magentoDataFixture Magento/ConfigurableProduct/_files/product_configurable.php
