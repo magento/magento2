68b760b50c52eca8993e78d8bb23196e478736a2
Lukasz Bajsarowicz <lukasz.bajsarowicz@gmail.com>
2020-04-21 23:25:25 +0000

    #27500 PHPUnit9 for AdminAnalytics module

--- app/code/Magento/AdminAnalytics/Test/Unit/Condition/CanViewNotificationTest.php
+++ app/code/Magento/AdminAnalytics/Test/Unit/Condition/CanViewNotificationTest.php
@@ -1,8 +1,10 @@
-<?php declare(strict_types=1);
+<?php
 /**
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types=1);
+
 namespace Magento\AdminAnalytics\Test\Unit\Condition;
 
 use Magento\AdminAnalytics\Model\Condition\CanViewNotification;
@@ -28,7 +30,7 @@ class CanViewNotificationTest extends TestCase
     /** @var  Log|MockObject */
     private $logMock;
 
-    /** @var  $cacheStorageMock MockObject|CacheInterface */
+    /** @var MockObject|CacheInterface $cacheStorageMock */
     private $cacheStorageMock;
 
     protected function setUp(): void
