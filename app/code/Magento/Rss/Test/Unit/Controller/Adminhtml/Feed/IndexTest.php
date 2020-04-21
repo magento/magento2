bc5f24639abcb0b6d84873a75d2bdb4c9ba8f1ad
Lukasz Bajsarowicz <lukasz.bajsarowicz@gmail.com>
2020-04-21 23:25:10 +0000

    #27500 PHPUnit9 for Rss module

--- app/code/Magento/Rss/Test/Unit/Controller/Adminhtml/Feed/IndexTest.php
+++ app/code/Magento/Rss/Test/Unit/Controller/Adminhtml/Feed/IndexTest.php
@@ -1,8 +1,10 @@
-<?php declare(strict_types=1);
+<?php
 /**
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types=1);
+
 namespace Magento\Rss\Test\Unit\Controller\Adminhtml\Feed;
 
 use Magento\Backend\Model\UrlInterface;
