4ba5fa412d4259771b326efa06d61e742d88f4ba
Lukasz Bajsarowicz <lukasz.bajsarowicz@gmail.com>
2020-04-22 00:06:11 +0000

    #27500 PHPUnit9 for ReleaseNotification module

--- app/code/Magento/ReleaseNotification/Test/Unit/Model/ContentProvider/Http/HttpContentProviderTest.php
+++ app/code/Magento/ReleaseNotification/Test/Unit/Model/ContentProvider/Http/HttpContentProviderTest.php
@@ -1,8 +1,10 @@
-<?php declare(strict_types=1);
+<?php
 /**
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types=1);
+
 namespace Magento\ReleaseNotification\Test\Unit\Model\ContentProvider\Http;
 
 use Magento\Framework\HTTP\ClientInterface;
