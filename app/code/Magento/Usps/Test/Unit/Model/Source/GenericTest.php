14ac92b1be794a8f346287c599f70b17117fce8f
Lukasz Bajsarowicz <lukasz.bajsarowicz@gmail.com>
2020-04-22 20:44:17 +0000

    #27500 PHPUnit9 for Usps module

--- app/code/Magento/Usps/Test/Unit/Model/Source/GenericTest.php
+++ app/code/Magento/Usps/Test/Unit/Model/Source/GenericTest.php
@@ -1,8 +1,10 @@
-<?php declare(strict_types=1);
+<?php
 /**
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types=1);
+
 namespace Magento\Usps\Test\Unit\Model\Source;
 
 use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
@@ -29,7 +31,8 @@ class GenericTest extends TestCase
             Carrier::class
         )->setMethods(
             ['getCode']
-        )->disableOriginalConstructor()->getMock();
+        )->disableOriginalConstructor()
+            ->getMock();
 
         $this->_generic = $helper->getObject(
             Generic::class,
