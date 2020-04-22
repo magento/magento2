e72e4a7056ca7f567ad26555fedeab1708bcd2c2
Cris Pini <cris_pini@yahoo.co.uk>
2020-04-22 21:32:25 +0100

    Changed startup script

--- start.sh
+++ start.sh
@@ -6,9 +6,11 @@
 #
 #############################################
 
-find /var/www/magento -type f -or -type d -exec mv -f '{}' /var/www/html/ \;
+#find /var/www/magento -type f -or -type d -exec mv -f '{}' /var/www/html/ \;
+#
+#cd /var/www/html/magento
+#php -d memory_limit=2G bin/magento sampledata:deploy
+#php bin/magento setup:upgrade
 
-cd /var/www/html/magento
-php -d memory_limit=2G bin/magento sampledata:deploy
-php bin/magento setup:upgrade
+echo "<h1>HELLO WORLD!!!</h1>" > /var/www/html/magento/index.html
 
