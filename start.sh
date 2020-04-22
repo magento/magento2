b2086b8357b4ee8ea664e0a78d4dad09761578d7
Cris Pini <cris_pini@yahoo.co.uk>
2020-04-22 19:57:38 +0100

    Added cpu resource limits for the containers

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@
 
 find /var/www/magento -type f -or -type d -exec mv -f '{}' /var/www/html/ \;
 
-#cd /var/www/html/magento
-#php -d memory_limit=2G bin/magento sampledata:deploy
-#php bin/magento setup:upgrade
+cd /var/www/html/magento
+php -d memory_limit=2G bin/magento sampledata:deploy
+php bin/magento setup:upgrade
 
