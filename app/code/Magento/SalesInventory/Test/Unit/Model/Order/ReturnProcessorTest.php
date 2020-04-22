a0a445616b57c8ee326e3edb9152e9607f14f692
Lukasz Bajsarowicz <lukasz.bajsarowicz@gmail.com>
2020-04-22 01:46:20 +0000

    #27500 PHPUnit9 for SalesInventory module

--- app/code/Magento/SalesInventory/Test/Unit/Model/Order/ReturnProcessorTest.php
+++ app/code/Magento/SalesInventory/Test/Unit/Model/Order/ReturnProcessorTest.php
@@ -1,8 +1,10 @@
-<?php declare(strict_types=1);
+<?php
 /**
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types=1);
+
 namespace Magento\SalesInventory\Test\Unit\Model\Order;
 
 use Magento\CatalogInventory\Api\StockManagementInterface;
--- app/code/Magento/SalesInventory/Test/Unit/Model/Plugin/Order/ReturnToStockOrderTest.php
+++ app/code/Magento/SalesInventory/Test/Unit/Model/Plugin/Order/ReturnToStockOrderTest.php
@@ -1,9 +1,11 @@
-<?php declare(strict_types=1);
+<?php
 /**
  *
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types=1);
+
 namespace Magento\SalesInventory\Test\Unit\Model\Plugin\Order;
 
 use Magento\CatalogInventory\Api\StockConfigurationInterface;
