59d4d4ec4f741bdc6ac4472baf851dce2f61878d
Cris Pini <cris_pini@yahoo.co.uk>
2020-04-22 21:52:51 +0100

    Changed startup script

--- start.sh
+++ start.sh
@@ -12,5 +12,6 @@
 #php -d memory_limit=2G bin/magento sampledata:deploy
 #php bin/magento setup:upgrade
 
+mkdir /var/www/html/magento
 echo "<h1>HELLO WORLD!!!</h1>" > /var/www/html/magento/index.html
 
