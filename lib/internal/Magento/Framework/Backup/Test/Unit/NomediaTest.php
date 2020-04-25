e0c9bc0f7304e01033db0d7a2c1b702d8463ad92
Lukasz Bajsarowicz <lukasz.bajsarowicz@gmail.com>
2020-04-25 15:52:02 +0000

    #27500 PHPUnit9 for Backup Framework Module

--- lib/internal/Magento/Framework/Backup/Test/Unit/NomediaTest.php
+++ lib/internal/Magento/Framework/Backup/Test/Unit/NomediaTest.php
@@ -1,8 +1,10 @@
-<?php declare(strict_types=1);
+<?php 
 /**
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types=1);
+
 namespace Magento\Framework\Backup\Test\Unit;
 
 use Magento\Framework\Backup\Db;
@@ -57,37 +59,37 @@ class NomediaTest extends TestCase
     {
         $this->objectManager = new ObjectManager($this);
         $this->_backupDbMock = $this->createMock(Db::class);
-        $this->_backupDbMock->expects($this->any())->method('setBackupExtension')->will($this->returnSelf());
+        $this->_backupDbMock->expects($this->any())->method('setBackupExtension')->willReturnSelf();
 
-        $this->_backupDbMock->expects($this->any())->method('setTime')->will($this->returnSelf());
+        $this->_backupDbMock->expects($this->any())->method('setTime')->willReturnSelf();
 
-        $this->_backupDbMock->expects($this->any())->method('setBackupsDir')->will($this->returnSelf());
+        $this->_backupDbMock->expects($this->any())->method('setBackupsDir')->willReturnSelf();
 
-        $this->_backupDbMock->expects($this->any())->method('setResourceModel')->will($this->returnSelf());
+        $this->_backupDbMock->expects($this->any())->method('setResourceModel')->willReturnSelf();
 
         $this->_backupDbMock->expects(
             $this->any()
         )->method(
             'getBackupPath'
-        )->will(
-            $this->returnValue('\unexistingpath')
+        )->willReturn(
+            '\unexistingpath'
         );
 
-        $this->_backupDbMock->expects($this->any())->method('create')->will($this->returnValue(true));
+        $this->_backupDbMock->expects($this->any())->method('create')->willReturn(true);
 
         $this->_filesystemMock = $this->createMock(Filesystem::class);
         $dirMock = $this->getMockForAbstractClass(WriteInterface::class);
         $this->_filesystemMock->expects($this->any())
             ->method('getDirectoryWrite')
-            ->will($this->returnValue($dirMock));
+            ->willReturn($dirMock);
 
         $this->_backupFactoryMock = $this->createMock(Factory::class);
         $this->_backupFactoryMock->expects(
             $this->once()
         )->method(
             'create'
-        )->will(
-            $this->returnValue($this->_backupDbMock)
+        )->willReturn(
+            $this->_backupDbMock
         );
 
         $this->fsMock = $this->createMock(Fs::class);
