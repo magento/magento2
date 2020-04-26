f137547e648c6b65b3b55192dbf32f4f006b20aa
Evgeny Levinsky <el@ideal-code.ru>
2020-04-26 14:31:10 +0300

    Added custom template for admin export grid

---
+++ app/code/Magento/ImportExport/view/adminhtml/web/js/export/filter-grid.js
@@ -0,0 +1,16 @@
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+define([
+    'uiComponent'
+], function (Component) {
+    'use strict';
+
+    return Component.extend({
+        defaults: {
+            template: 'Magento_ImportExport/export/filter-grid'
+        }
+    });
+});
