f92ee249dbf95f756ff9b3a56d023c09ce232b82
Myroslav Dobra <dmaraptor@gmail.com>
2020-04-27 18:49:33 +0300

    MC-33699: Stabilize MFTF tests

--- app/code/Magento/Swatches/Block/Adminhtml/Attribute/Edit/Options/Visual.php
+++ app/code/Magento/Swatches/Block/Adminhtml/Attribute/Edit/Options/Visual.php
@@ -94,13 +94,16 @@ class Visual extends AbstractSwatch
         }
         $newSwatch = [];
         foreach ($swatchStoreValue as $key => $value) {
-            if ($value[0] == '#') {
-                $newSwatch[$key] = 'background: '.$value;
-            } elseif ($value[0] == '/') {
-                $mediaUrl = $this->swatchHelper->getSwatchMediaUrl();
-                $newSwatch[$key] = 'background: url('.$mediaUrl.$value.'); background-size: cover;';
+            if (is_string($value) && !empty($value)) {
+                if ($value[0] == '#') {
+                    $newSwatch[$key] = 'background: ' . $value;
+                } elseif ($value[0] == '/') {
+                    $mediaUrl = $this->swatchHelper->getSwatchMediaUrl();
+                    $newSwatch[$key] = 'background: url(' . $mediaUrl . $value . '); background-size: cover;';
+                }
             }
         }
+
         return $newSwatch;
     }
 }
