0f690e79177a76ec1340416fb7a57640c37dc083
Cris Pini <cris_pini@yahoo.co.uk>
2020-04-29 13:50:07 +0100

    Fix unit test and run sample data

--- start.sh
+++ start.sh
@@ -8,9 +8,9 @@
 
 find /var/www/magento -type f -or -type d -exec mv -f '{}' /var/www/html/ \;
 #
-#cd /var/www/html/magento
-#php -d memory_limit=2G bin/magento sampledata:deploy
-#php bin/magento setup:upgrade
+cd /var/www/html/magento
+php bin/magento sampledata:deploy
+php bin/magento setup:upgrade
 
 #mkdir /var/www/html/magento
 #echo "<h1>HELLO WORLD!!!</h1>" > /var/www/html/magento/index.html
