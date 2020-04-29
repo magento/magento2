6fc61b6c3c5f1d9592f9acac383b6ae53fce1ed4
Sergii Ivashchenko <serg.ivashchenko@gmail.com>
2020-04-29 12:49:45 +0100

    Added media content cms and catalog fixtures

--- app/code/Magento/MediaContentApi/Model/GetEntityContentsInterface.php
+++ app/code/Magento/MediaContentApi/Model/GetEntityContentsInterface.php
@@ -3,6 +3,7 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types=1);
 
 namespace Magento\MediaContentApi\Model;
 
@@ -10,6 +11,7 @@ use Magento\MediaContentApi\Api\Data\ContentIdentityInterface;
 
 /**
  * Get Entity Contents.
+ * @api
  */
 interface GetEntityContentsInterface
 {
