bc18a8fd5141a2748b4938e2468413ba7a3625ba
Alexander Menk <a.menk@imi.de>
2020-04-30 17:50:30 +0200

    #27338 Add testmodule with extension attribute

---
+++ dev/tests/api-functional/_files/Magento/TestModuleExtensionAttributes/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Magento_TestModuleExtensionAttributes',
+    __DIR__
+);
