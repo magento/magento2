aa25663a4ce82c9549c17ae675b0321b2d0560a3
Cris Pini <cris_pini@yahoo.co.uk>
2020-05-04 17:11:11 +0100

    Sorted - 10

--- start.sh
+++ start.sh
@@ -12,6 +12,8 @@ cd /var/www/html/magento
 php bin/magento sampledata:deploy
 php bin/magento setup:upgrade
 
+chmod 0775 /var/www/magento/media /var/www/magento/var
+chgrp 82 /var/www/magento/media /var/www/magento/var
 #mkdir /var/www/html/magento
 #echo "<h1>HELLO WORLD!!!</h1>" > /var/www/html/magento/index.html
 
