d5043752cba21ff0994c35163b0d101cfde4388e
abdarrahman abouzaid <abdarrahman.abouzaid@gmail.com>
2020-05-04 10:21:50 +0200

    checking that an area's elemants has items

--- app/code/Magento/Ui/view/base/web/js/lib/core/collection.js
+++ app/code/Magento/Ui/view/base/web/js/lib/core/collection.js
@@ -224,7 +224,7 @@ define([
         regionHasElements: function (name) {
             var region = this.getRegion(name);
 
-            return region.length > 0;
+            return region().length > 0 && 'item' in region.first();
         },
 
         /**
