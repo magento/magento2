cbf109057b079306b91f061e6033a80535b7aec3
Evgeny Levinsky <el@ideal-code.ru>
2020-05-04 10:42:54 +0300

    Refactored filters on export grid

--- app/code/Magento/ImportExport/view/adminhtml/web/js/export/filter-grid/columns/filter.js
+++ app/code/Magento/ImportExport/view/adminhtml/web/js/export/filter-grid/columns/filter.js
@@ -5,58 +5,128 @@
 
 define([
     'underscore',
+    'mageUtils',
     'mage/translate',
-    'Magento_Ui/js/grid/columns/column',
-], function (_, $t, Column) {
+    'uiLayout',
+    'uiRegistry',
+    'Magento_Ui/js/grid/columns/column'
+], function (_, utils, $t, layout, registry, Column) {
     'use strict';
 
     return Column.extend({
         defaults: {
             bodyTmpl: 'Magento_ImportExport/export/filter-grid/columns/filter',
             templates: {
-                base: 'ui/grid/cells/text',
-                input: 'Magento_ImportExport/export/filter-grid/columns/filter/input',
-                select: 'Magento_ImportExport/export/filter-grid/columns/filter/select',
-                multiselect: 'Magento_ImportExport/export/filter-grid/columns/filter/multiselect'
+                base: {
+                    index: '${ $.$data.index }',
+                    parent: '${ $.$data.column.name }',
+                    name: '${ $.index }',
+                    provider: '${ $.$data.column.name }',
+                    dataScope: 'filters.${ $.index }'
+                },
+                text: {
+                    component: 'Magento_Ui/js/grid/columns/column',
+                    elementTmpl: 'ui/grid/cells/text'
+                },
+                input: {
+                    component: 'Magento_Ui/js/form/element/abstract'
+                },
+                select: {
+                    component: 'Magento_Ui/js/form/element/select',
+                    options: '${ JSON.stringify($.$data.options) }',
+                    caption: $t('-- Not Selected --')
+                },
+                multiselect: {
+                    component: 'Magento_Ui/js/form/element/multiselect',
+                    options: '${ JSON.stringify($.$data.options) }'
+                }
             },
             filters: {},
-            notSelectedLabel: '-- Not Selected --'
-        },
-
-        /**
-         * Returns path to the template based on data type.
-         *
-         * @param {Object} row
-         * @returns {String}
-         */
-        getTemplateByType: function (row) {
-            let filter = row[this.index];
-
-            if (_.isObject(filter) && this.templates[filter.type]) {
-                return this.templates[filter.type];
+            imports: {
+                onDataUpdate: '${ $.provider }:data'
             }
-
-            return this.templates.base;
         },
 
         /**
-         * Retrieves options for selects, including "not selected" value if needed.
+         * Initializes observable properties.
          *
-         * @param {Object} row
-         * @param {Boolean} notSelected
-         * @returns {Array}
+         * @returns {Object} Chainable
          */
-        getOptions: function (row, notSelected = false) {
-            let options = _.toArray(row[this.index].options);
-
-            if (notSelected) {
-                options.unshift({
-                    value: '',
-                    label: $t(this.notSelectedLabel)
+        initObservable: function () {
+            this._super()
+                .observe({
+                    elems: []
                 });
+
+            return this;
+        },
+
+        /**
+         * Retrieves ui component from the 'elems' array, related with the row.
+         *
+         * @param {Object} row
+         * @returns {Object[]}
+         */
+        getChild: function (row) {
+            let elem = _.findWhere(this.elems(), {
+                index: row[this.indexField]
+            });
+
+            return elem ? [elem] : [];
+        },
+
+        /**
+         * Requests specified components to insert them into 'elems' array.
+         *
+         * @param {(String|Array)} elems
+         * @returns {Object} Chainable
+         */
+        insertChild: function (elems) {
+            if (!_.isArray(elems)) {
+                elems = [elems];
             }
 
-            return options;
+            elems.forEach(function (elem) {
+                registry.get(elem, function (elem) {
+                    this.elems.push(elem);
+                }.bind(this));
+            }, this);
+
+            return this;
+        },
+
+        /**
+         * Creates template for ui component, based on the row type.
+         *
+         * @param {Object} row
+         * @returns {Object}
+         */
+        getRowTemplate: function (row) {
+            let templates = this.templates,
+                type      = row[this.index].type || 'text',
+                template  = utils.extend({}, templates.base, templates[type]);
+
+            return utils.template(template, {
+                column: this,
+                index: row[this.indexField],
+                options: row[this.index].options || {}
+            }, true, true);
+        },
+
+        /**
+         * Executed whenever data in the provider is updated,
+         * creates appropriate ui components.
+         *
+         * @param {Object} data
+         */
+        onDataUpdate: function (data) {
+            let children = [];
+
+            data.items.forEach(function (row) {
+                children.push(this.getRowTemplate(row));
+            }, this);
+
+            layout(children);
         }
     });
 });
