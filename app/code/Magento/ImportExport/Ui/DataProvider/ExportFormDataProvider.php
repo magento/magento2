edacef85cfd8b56f0af1a6eb6c23e17cee9abd4e
Evgeny Levinsky <el@ideal-code.ru>
2020-05-05 11:10:58 +0300

    Created Ui export form

---
+++ app/code/Magento/ImportExport/Ui/DataProvider/ExportFormDataProvider.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\ImportExport\Ui\DataProvider;
+
+use Magento\Framework\View\Element\UiComponent\DataProvider\DataProvider;
+
+class ExportFormDataProvider extends DataProvider
+{
+    /**
+     * Get data
+     *
+     * @return array
+     */
+    public function getData()
+    {
+        return [];
+    }
+}
