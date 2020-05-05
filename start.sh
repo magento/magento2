cd1a2b0cb23b4e650ed1426a19a4e057093b3c35
Cris Pini <cris_pini@yahoo.co.uk>
2020-05-05 19:20:04 +0100

    added sample data

--- start.sh
+++ start.sh
@@ -6,11 +6,13 @@
 #
 #############################################
 
-find /var/www/magento -type f -or -type d -exec mv -f '{}' /var/www/html/ \;
+#find /var/www/magento -type f -or -type d -exec mv -f '{}' /var/www/html/ \;
 #
-cd /var/www/html/magento
-php bin/magento sampledata:deploy
-php bin/magento setup:upgrade
+cd /var/www/html
+cp -a pub.new/* pub/
+
+#php bin/magento sampledata:deploy
+#php bin/magento setup:upgrade
 
 #mkdir /var/www/html/magento
 #echo "<h1>HELLO WORLD!!!</h1>" > /var/www/html/magento/index.html
