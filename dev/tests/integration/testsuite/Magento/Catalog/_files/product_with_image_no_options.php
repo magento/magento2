1b8dfb27053cf03e98c19a0b47a98dfeb5e54459
Daniel Renaud <drenaud@magento.com>
2020-05-07 13:32:54 -0500

    MC-29945: GraphQL Cart endpoint returns incorrect or different product images

---
+++ dev/tests/integration/testsuite/Magento/Catalog/_files/product_with_image_no_options.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+require __DIR__ . '/product_image.php';
+require __DIR__ . '/product_simple_without_custom_options.php';
+
+$objectManager = \Magento\TestFramework\Helper\Bootstrap::getObjectManager();
+$productRepository = $objectManager->create(\Magento\Catalog\Api\ProductRepositoryInterface::class);
+$product = $productRepository->get('simple-2');
+
+/** @var $product \Magento\Catalog\Model\Product */
+$product->setStoreId(0)
+    ->setImage('/m/a/magento_image.jpg')
+    ->setSmallImage('/m/a/magento_image.jpg')
+    ->setThumbnail('/m/a/magento_image.jpg')
+    ->setData('media_gallery', ['images' => [
+        [
+            'file' => '/m/a/magento_image.jpg',
+            'position' => 1,
+            'label' => 'Image Alt Text',
+            'disabled' => 0,
+            'media_type' => 'image'
+        ],
+    ]])
+    ->save();
