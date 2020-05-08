8ff5fa6a8c0be454dd03618d866cfbc627c24926
Yehuda Schwartz <judaschwartz@users.noreply.github.com>
2020-05-08 15:52:05 -0400

    Update ProductRepositoryInterface.php

--- app/code/Magento/Catalog/Api/ProductRepositoryInterface.php
+++ app/code/Magento/Catalog/Api/ProductRepositoryInterface.php
@@ -14,7 +14,7 @@ namespace Magento\Catalog\Api;
 interface ProductRepositoryInterface
 {
     /**
-     * use POST to Create product and PUT to Update product
+     * Create product
      *
      * @param \Magento\Catalog\Api\Data\ProductInterface $product
      * @param bool $saveOptions
