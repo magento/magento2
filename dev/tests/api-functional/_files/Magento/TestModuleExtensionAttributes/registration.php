07538606525755713ffbe6691bd998735f640491
Alexander Steshuk <grp-engcom-vendorworker-Kilo@adobe.com>
2020-05-12 12:48:29 +0300

    Fix Static test

--- dev/tests/api-functional/_files/Magento/TestModuleExtensionAttributes/registration.php
+++ dev/tests/api-functional/_files/Magento/TestModuleExtensionAttributes/registration.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
     'Magento_TestModuleExtensionAttributes',
