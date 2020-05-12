fa481b10944ee57ee7567d4667da985f1ff4a198
engcom-Echo <engcom-vendorworker-echo@adobe.com>
2020-05-12 13:02:56 +0300

    fix unit test

--- app/code/Magento/Catalog/Test/Unit/Model/CategoryRepositoryTest.php
+++ app/code/Magento/Catalog/Test/Unit/Model/CategoryRepositoryTest.php
@@ -152,15 +152,14 @@ class CategoryRepositoryTest extends TestCase
         $this->assertEquals($categoryMock, $this->model->get($categoryId));
     }
 
-    /**
-     * @expectedException \Magento\Framework\Exception\SerializationException
-     * @expectedExceptionMessage The "default" value's type is invalid. The "int" type was expected. Verify and
-     * try again.
-     */
     public function testGetWithStoreCodeException()
     {
         $categoryId = 5;
-        $categoryMock = $this->createMock(\Magento\Catalog\Model\Category::class);
+        $categoryMock = $this->createMock(CategoryModel::class);
+        $this->expectException('\Magento\Framework\Exception\SerializationException');
+        $this->expectExceptionMessage(
+            'The "default" value\'s type is invalid. The "int" type was expected. Verify and try again.'
+        );
         $this->assertEquals($categoryMock, $this->model->get($categoryId, 'default'));
     }
 
