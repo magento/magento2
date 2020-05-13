52a3eb1e6b8ec9388df841068ed0602cfc8e26bf
Myroslav Dobra <dmaraptor@gmail.com>
2020-05-13 20:21:45 +0300

    MC-34231: 2.4.0-develop-pr33 PR stabilization

--- dev/tests/integration/testsuite/Magento/Customer/Model/AttributeTest.php
+++ dev/tests/integration/testsuite/Magento/Customer/Model/AttributeTest.php
@@ -41,7 +41,7 @@ class AttributeTest extends \PHPUnit\Framework\TestCase
     /**
      * @inheritDoc
      */
-    protected function setUp()
+    protected function setUp() : void
     {
         $this->objectManager = Bootstrap::getObjectManager();
         $this->model = $this->objectManager->get(Attribute::class);
@@ -69,13 +69,12 @@ class AttributeTest extends \PHPUnit\Framework\TestCase
     /**
      * @magentoDataFixture Magento/Customer/_files/attribute_user_defined_customer.php
      *
-     * @expectedException \Magento\Framework\Exception\LocalizedException
-     * @expectedExceptionMessage Do not change entity type.
-     *
      * @return void
      */
     public function testAttributeSaveWithChangedEntityType(): void
     {
+        $this->expectException(\Magento\Framework\Exception\LocalizedException::class);
+        $this->expectExceptionMessage('Do not change entity type.');
         $attribute = $this->attributeRepository->get($this->customerEntityType, 'user_attribute');
         $attribute->setEntityTypeId(5);
         $attribute->save();
