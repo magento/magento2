4a14460043055a008d018743d367a1c90c121b62
Cris Pini <cris_pini@yahoo.co.uk>
2020-05-15 11:25:01 +0100

    Stuff sorted

--- app/etc/env.php
+++ app/etc/env.php
@@ -30,7 +30,7 @@ return [
         ]
     ],
     'x-frame-options' => 'SAMEORIGIN',
-    'MAGE_MODE' => 'developer',
+    'MAGE_MODE' => 'production',
     'session' => [
         'save' => 'files'
     ],
