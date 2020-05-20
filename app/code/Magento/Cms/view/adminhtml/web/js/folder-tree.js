817ec4f9da7ae08cf5ae1e3baa70173d5c8bc5f7
Nazar Klovanych <nazarn96@gmail.com>
2020-05-20 14:22:02 +0300

    Provide pathId parameter to media browser

--- app/code/Magento/Cms/view/adminhtml/web/js/folder-tree.js
+++ app/code/Magento/Cms/view/adminhtml/web/js/folder-tree.js
@@ -3,7 +3,6 @@
  * See COPYING.txt for license details.
  */
 
-/* global Base64 */
 define([
     'jquery',
     'jquery/ui',
@@ -79,92 +78,30 @@ define([
             var path = this.options.currentPath,
                 tree = this.element,
                 lastExistentFolderEl,
-                pathId,
 
-            /**
-             * Recursively open folders specified in path array.
-             */
-            recursiveOpen = function () {
-                var folderEl = $('[data-id="' + path.pop() + '"]');
+                /**
+                 * Recursively open folders specified in path array.
+                 */
+                recursiveOpen = function () {
+                    var folderEl = $('[data-id="' + path.pop() + '"]');
 
-                // if folder doesn't exist, select the last opened folder
-                if (!folderEl.length) {
-                    tree.jstree('select_node', lastExistentFolderEl);
+                    // if folder doesn't exist, select the last opened folder
+                    if (!folderEl.length) {
+                        tree.jstree('select_node', lastExistentFolderEl);
 
-                    return;
-                }
-
-                lastExistentFolderEl = folderEl;
-
-                if (path.length) {
-                    tree.jstree('open_node', folderEl, recursiveOpen);
-                } else {
-                    tree.jstree('open_node', folderEl, function () {
-                        tree.jstree('select_node', folderEl);
-                    });
-                }
-            },
-
-            /**
-             * Encode path value
-             *
-             * @param {String} val
-             */
-            _encodePathId = function (val) {
-                return Base64.encode(val)
-                    .replace(/\+/g, ':')
-                    .replace(/\//g, '_')
-                    .replace(/=/g, '-');
-            },
-
-            /**
-             * Decode path value
-             *
-             * @param {String} val
-             */
-            _decodePathId = function (val) {
-                return Base64.decode(val)
-                    .replace(/\:/g, '+')
-                    .replace(/\_/g, '/')
-                    .replace(/-/g, '=');
-            },
-
-            /**
-             * Get currentPath decode it returns new path array
-             */
-            _parseCurrentPath = function () {
-                var paths = [],
-                    decodedPath = _decodePathId(window.MediabrowserUtility.pathId.replace(/--|,,/, '==')).split('/');
-
-                $.each(decodedPath, function (i, val) {
-                    var isLastElement = i === decodedPath.length - 1;
-
-                    if (isLastElement) {
-                        paths[i] = window.MediabrowserUtility.pathId.replace(',,', '--');
-                    } else {
-                        paths[i] = _encodePathId(val);
+                        return;
                     }
-                });
-                paths.unshift('root');
-                paths.reverse();
 
-                return paths;
-            };
+                    lastExistentFolderEl = folderEl;
 
-            $(window).on('reload.MediaGallery', function () {
-                pathId =  window.MediabrowserUtility.pathId.replace(',,', '--');
-                path = _parseCurrentPath();
-
-                tree.jstree('deselect_all');
-
-                if (path.length > 1) {
-                    recursiveOpen();
-                } else {
-                    tree.jstree('open_node', $('[data-id="' + pathId + '"]'), function () {
-                        tree.jstree('select_node',  $('[data-id="' + pathId + '"]'));
-                    });
-                }
-            });
+                    if (path.length) {
+                        tree.jstree('open_node', folderEl, recursiveOpen);
+                    } else {
+                        tree.jstree('open_node', folderEl, function () {
+                            tree.jstree('select_node', folderEl);
+                        });
+                    }
+                };
 
             recursiveOpen();
         },
