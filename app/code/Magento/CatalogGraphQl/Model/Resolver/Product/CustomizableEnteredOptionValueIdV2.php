f4e2665cb24337d8ee4da412d0506e7f5a95d5ff
eduard13 <e.chitoraga@atwix.com>
2020-05-26 16:46:17 +0300

    Implementing the ID_V2 for entered options, bundle and downloadable products

--- app/code/Magento/CatalogGraphQl/Model/Resolver/Product/CustomizableEnteredOptionValueIdV2.php
+++ app/code/Magento/CatalogGraphQl/Model/Resolver/Product/CustomizableEnteredOptionValueIdV2.php
@@ -7,34 +7,36 @@ declare(strict_types=1);
 
 namespace Magento\CatalogGraphQl\Model\Resolver\Product;
 
-use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Exception\GraphQlInputException;
 use Magento\Framework\GraphQl\Query\Resolver\ContextInterface;
-use Magento\Framework\GraphQl\Query\Resolver\Value;
 use Magento\Framework\GraphQl\Query\ResolverInterface;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
 
 /**
- * @inheritdoc
- *
- * Format new option id_v2 in base64 encode for custom options
+ * Format new option id_v2 in base64 encode for entered custom options
  */
-class CustomizableOptionValueIdV2 implements ResolverInterface
+class CustomizableEnteredOptionValueIdV2 implements ResolverInterface
 {
+    /**
+     * Option type name
+     */
     private const OPTION_TYPE = 'custom-option';
 
     /**
-     * @inheritdoc
-     *
-     * Create new option id_v2 that encodes details for each option and in most cases can be presented
-     * as base64("<option-type>/<option-id>/<option-value-id>")
+     * Create a option id_v2 for entered option in "<option-type>/<option-id>" format
      *
      * @param Field $field
      * @param ContextInterface $context
      * @param ResolveInfo $info
      * @param array|null $value
      * @param array|null $args
-     * @return Value|mixed|void
+     *
+     * @return string
+     *
+     * @throws GraphQlInputException
+     *
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
     public function resolve(
         Field $field,
@@ -43,23 +45,18 @@ class CustomizableOptionValueIdV2 implements ResolverInterface
         array $value = null,
         array $args = null
     ) {
+        if (!isset($value['option_id']) || empty($value['option_id'])) {
+            throw new GraphQlInputException(__('Wrong format option data: option_id should not be empty.'));
+        }
+
         $optionDetails = [
             self::OPTION_TYPE,
-            $value['option_id'],
-            $value['option_type_id']
+            $value['option_id']
         ];
 
-        if (!isset($value['option_id']) || empty($value['option_id'])) {
-            throw new LocalizedException(__('Wrong format option data: option_id should not be empty.'));
-        }
-
-        if (!isset($value['option_type_id']) || empty($value['option_type_id'])) {
-            throw new LocalizedException(__('Wrong format option data: option_type_id should not be empty.'));
-        }
+        $content = implode('/', $optionDetails);
 
         // phpcs:ignore Magento2.Functions.DiscouragedFunction
-        $content = \implode('/', $optionDetails);
-
         return base64_encode($content);
     }
 }
---
+++ app/code/Magento/CatalogGraphQl/Model/Resolver/Product/CustomizableSelectedOptionValueIdV2.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\CatalogGraphQl\Model\Resolver\Product;
+
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Exception\GraphQlInputException;
+use Magento\Framework\GraphQl\Query\Resolver\ContextInterface;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+
+/**
+ * Format new option id_v2 in base64 encode for selected custom options
+ */
+class CustomizableSelectedOptionValueIdV2 implements ResolverInterface
+{
+    /**
+     * Option type name
+     */
+    private const OPTION_TYPE = 'custom-option';
+
+    /**
+     * Create a option id_v2 for selected option in "<option-type>/<option-id>/<option-value-id>" format
+     *
+     * @param Field $field
+     * @param ContextInterface $context
+     * @param ResolveInfo $info
+     * @param array|null $value
+     * @param array|null $args
+     *
+     * @return string
+     *
+     * @throws GraphQlInputException
+     *
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function resolve(
+        Field $field,
+        $context,
+        ResolveInfo $info,
+        array $value = null,
+        array $args = null
+    ) {
+        if (!isset($value['option_id']) || empty($value['option_id'])) {
+            throw new GraphQlInputException(__('Wrong format option data: option_id should not be empty.'));
+        }
+
+        if (!isset($value['option_type_id']) || empty($value['option_type_id'])) {
+            throw new GraphQlInputException(__('Wrong format option data: option_type_id should not be empty.'));
+        }
+
+        $optionDetails = [
+            self::OPTION_TYPE,
+            $value['option_id'],
+            $value['option_type_id']
+        ];
+
+        $content = implode('/', $optionDetails);
+
+        // phpcs:ignore Magento2.Functions.DiscouragedFunction
+        return base64_encode($content);
+    }
+}
