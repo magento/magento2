7e7eb07c48bdaf21c416e8073d8ee8576ce827ee
Vasya Tsviklinskyi <tsviklinskyi@gmail.com>
2020-05-27 15:09:12 +0300

    MC-34483: Moving a store view to a different website resets URLs

--- app/code/Magento/CatalogUrlRewrite/Test/Unit/Model/Category/Plugin/Store/ViewTest.php
+++ app/code/Magento/CatalogUrlRewrite/Test/Unit/Model/Category/Plugin/Store/ViewTest.php
@@ -121,7 +121,7 @@ class ViewTest extends TestCase
             ->getMock();
         $this->productCollectionMock = $this->getMockBuilder(ProductCollection::class)
             ->disableOriginalConstructor()
-            ->setMethods(['addCategoryIds', 'addAttributeToSelect', 'addWebsiteFilter', 'getIterator'])
+            ->setMethods(['addCategoryIds', 'addAttributeToSelect', 'getIterator', 'addStoreFilter'])
             ->getMock();
         $this->productMock = $this->getMockBuilder(Product::class)
             ->disableOriginalConstructor()
@@ -174,7 +174,7 @@ class ViewTest extends TestCase
             ->method('addAttributeToSelect')
             ->willReturn($this->productCollectionMock);
         $this->productCollectionMock->expects($this->once())
-            ->method('addWebsiteFilter')
+            ->method('addStoreFilter')
             ->willReturn($this->productCollectionMock);
         $iterator = new \ArrayIterator([$this->productMock]);
         $this->productCollectionMock->expects($this->once())
