c930320fdfc47846ac440f207bf021e0e9fb799d
Daniel Renaud <drenaud@magento.com>
2020-05-28 15:32:10 -0500

    MC-20636: MyAccount :: Order Details :: Order Details by Order Number
    - move sales item logic out of resolver so it is reusable

--- app/code/Magento/SalesGraphQl/Model/Resolver/OrderItem.php
+++ app/code/Magento/SalesGraphQl/Model/Resolver/OrderItem.php
@@ -15,9 +15,20 @@ use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
 use Magento\GraphQl\Model\Query\ContextInterface;
 use Magento\Sales\Api\Data\OrderItemInterface;
 use Magento\Sales\Model\Order;
+use Magento\SalesGraphQl\Model\SalesItem\SalesItemFactory;
 
 class OrderItem implements ResolverInterface
 {
+    /**
+     * @var SalesItemFactory
+     */
+    private $salesItemFactory;
+
+    public function __construct(SalesItemFactory $salesItemFactory)
+    {
+        $this->salesItemFactory = $salesItemFactory;
+    }
+
     /**
      * @inheritDoc
      */
@@ -35,90 +46,13 @@ class OrderItem implements ResolverInterface
         /** @var OrderItemInterface $item */
         $orderItems = [];
         foreach ($parentOrder->getItems() as $key => $item) {
-            $options = $this->getItemOptions($item);
-            $orderItems[$key] = [
-                'parent_product_sku' => $item->getParentItem() ? $item->getParentItem()->getSku() : null,
-                'product_name' => $item->getName(),
-                'product_sale_price' => [
-                    'currency' => $parentOrder->getOrderCurrencyCode(),
-                    'value' => $item->getPrice(),
-                ],
-                'product_sku' => $item->getSku(),
-                'product_url' => 'url',
-                'quantity_ordered' => $item->getQtyOrdered(),
-                'selected_options' => $options['selected_options'] ?? [],
-                'entered_options' => $options['entered_options'] ?? [],
-            ];
+            $salesOrderItem = $this->salesItemFactory->create(
+                $item,
+                $parentOrder,
+                ['quantity_ordered' => $item->getQtyOrdered()]
+            );
+            $orderItems[] = $salesOrderItem->convertToArray();
         }
         return $orderItems;
-    }
-
-    /**
-     * Get Order item options.
-     *
-     * @param OrderItemInterface $orderItem
-     * @return array
-     */
-    public function getItemOptions(OrderItemInterface $orderItem): array
-    {
-        //build options array
-        $optionsTypes = ['selected_options' => [], 'entered_options' => []];
-        $options = $orderItem->getProductOptions();
-        if ($options) {
-            if (isset($options['options'])) {
-                $optionsTypes = $this->processOptions($options['options']);
-            } elseif (isset($options['attributes_info'])) {
-                $optionsTypes = $this->processAttributesInfo($options['attributes_info']);
-            } elseif (isset($options['additional_options'])) {
-                // TODO $options['additional_options']
-            }
-        }
-        return $optionsTypes;
-    }
-
-    /**
-     * Process options data
-     *
-     * @param array $options
-     * @return array
-     */
-    public function processOptions(array $options): array
-    {
-        $selectedOptions = [];
-        $enteredOptions = [];
-        foreach ($options ?? [] as $option) {
-            if (isset($option['option_type'])) {
-                if (in_array($option['option_type'], ['field', 'area', 'file', 'date', 'date_time', 'time'])) {
-                    $selectedOptions[] = [
-                        'id' => $option['label'],
-                        'value' => $option['print_value'] ?? $option['value'],
-                    ];
-                } elseif (in_array($option['option_type'], ['drop_down', 'radio', 'checkbox', 'multiple'])) {
-                    $enteredOptions[] = [
-                        'id' => $option['label'],
-                        'value' => $option['print_value'] ?? $option['value'],
-                    ];
-                }
-            }
-        }
-        return ['selected_options' => $selectedOptions, 'entered_options' => $enteredOptions];
-    }
-
-    /**
-     * Process attributes info data
-     *
-     * @param array $attributesFnfo
-     * @return array
-     */
-    public function processAttributesInfo(array $attributesFnfo): array
-    {
-        $selectedOptions = [];
-        foreach ($attributesFnfo ?? [] as $option) {
-            $selectedOptions[] = [
-                'id' => $option['label'],
-                'value' => $option['print_value'] ?? $option['value'],
-            ];
-        }
-        return ['selected_options' => $selectedOptions, 'entered_options' => []];
     }
 }
---
+++ app/code/Magento/SalesGraphQl/Model/SalesItem/SalesItemFactory.php
@@ -0,0 +1,131 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\SalesGraphQl\Model\SalesItem;
+
+use Magento\Framework\ObjectManagerInterface;
+use Magento\Sales\Api\Data\OrderInterface;
+use Magento\Sales\Api\Data\OrderItemInterface;
+use Magento\SalesGraphQl\Model\SalesItem\Data\SalesItem;
+
+/**
+ * Create SalesItem object with data from OrderItem
+ */
+class SalesItemFactory
+{
+    /**
+     * @var ObjectManagerInterface
+     */
+    private $objectManager;
+
+    /**
+     * @param ObjectManagerInterface $objectManager
+     */
+    public function __construct(ObjectManagerInterface $objectManager)
+    {
+        $this->objectManager = $objectManager;
+    }
+
+    /**
+     * Create SalesItem object
+     *
+     * @param OrderItemInterface $orderItem
+     * @param OrderInterface $order
+     * @param array $additionalData
+     * @return SalesItem
+     */
+    public function create(OrderItemInterface $orderItem, OrderInterface $order, array $additionalData = []): SalesItem
+    {
+        $options = $this->getItemOptions($orderItem);
+
+        $salesItemData = [
+            'product_name' => $orderItem->getName(),
+            'product_sku' => $orderItem->getSku(),
+            'product_sale_price' => [
+                'currency' => $order->getOrderCurrencyCode(),
+                'value' => $orderItem->getPrice(),
+            ],
+            'parent_product_name' => $orderItem->getParentItem() ? $orderItem->getParentItem()->getName() : null,
+            'parent_product_sku' => $orderItem->getParentItem() ? $orderItem->getParentItem()->getSku() : null,
+            'selected_options' => $options['selected_options'],
+            'entered_options' => $options['entered_options'],
+        ];
+
+        $salesItemData = array_merge_recursive($salesItemData, $additionalData);
+
+        return $this->objectManager->create(SalesItem::class, ['data' => $salesItemData]);
+    }
+
+    /**
+     * Get Order item options.
+     *
+     * @param OrderItemInterface $orderItem
+     * @return array
+     */
+    private function getItemOptions(OrderItemInterface $orderItem): array
+    {
+        //build options array
+        $optionsTypes = ['selected_options' => [], 'entered_options' => []];
+        $options = $orderItem->getProductOptions();
+        if ($options) {
+            if (isset($options['options'])) {
+                $optionsTypes = $this->processOptions($options['options']);
+            } elseif (isset($options['attributes_info'])) {
+                $optionsTypes = $this->processAttributesInfo($options['attributes_info']);
+            } elseif (isset($options['additional_options'])) {
+                // TODO $options['additional_options']
+            }
+        }
+        return $optionsTypes;
+    }
+
+    /**
+     * Process options data
+     *
+     * @param array $options
+     * @return array
+     */
+    private function processOptions(array $options): array
+    {
+        $selectedOptions = [];
+        $enteredOptions = [];
+        foreach ($options ?? [] as $option) {
+            if (isset($option['option_type'])) {
+                if (in_array($option['option_type'], ['field', 'area', 'file', 'date', 'date_time', 'time'])) {
+                    $selectedOptions[] = [
+                        'id' => $option['label'],
+                        'value' => $option['print_value'] ?? $option['value'],
+                    ];
+                } elseif (in_array($option['option_type'], ['drop_down', 'radio', 'checkbox', 'multiple'])) {
+                    $enteredOptions[] = [
+                        'id' => $option['label'],
+                        'value' => $option['print_value'] ?? $option['value'],
+                    ];
+                }
+            }
+        }
+        return ['selected_options' => $selectedOptions, 'entered_options' => $enteredOptions];
+    }
+
+    /**
+     * Process attributes info data
+     *
+     * @param array $attributesInfo
+     * @return array
+     */
+    private function processAttributesInfo(array $attributesInfo): array
+    {
+        $selectedOptions = [];
+        foreach ($attributesInfo ?? [] as $option) {
+            $selectedOptions[] = [
+                'id' => $option['label'],
+                'value' => $option['print_value'] ?? $option['value'],
+            ];
+        }
+        return ['selected_options' => $selectedOptions, 'entered_options' => []];
+    }
+}
