0e7bedfbeba94b19d82843c739e477e98f1e0966
Dmytro Horytskyi <dhorytskyi@magento.com>
2020-05-27 19:14:26 -0500

    MC-34499: [Integration Tests] 85 Failing in CE on 2.3-develop

--- dev/tests/integration/testsuite/Magento/Catalog/_files/product_simple.php
+++ dev/tests/integration/testsuite/Magento/Catalog/_files/product_simple.php
@@ -201,9 +201,12 @@ $product->setOptions($options);
 
 /** @var \Magento\Catalog\Api\ProductRepositoryInterface $productRepository */
 $productRepository = $objectManager->create(\Magento\Catalog\Api\ProductRepositoryInterface::class);
-$productRepository->save($product);
+$product = $productRepository->save($product);
 
 $categoryLinkManagement->assignProductToCategories(
     $product->getSku(),
     [2]
 );
+
+$indexerProcessor = $objectManager->get(\Magento\Catalog\Model\Indexer\Product\Price\Processor::class);
+$indexerProcessor->reindexRow($product->getId());
