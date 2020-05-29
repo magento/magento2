c8d6c06f722d11e6fe3792d3f0dd6609d458baaa
Oleh Usik <o.usik@atwix.com>
2020-05-29 16:47:27 +0300

    Added resolvers which need to implement

--- app/code/Magento/CompareListGraphQl/Model/Resolver/AddItemsToCompareList.php
+++ app/code/Magento/CompareListGraphQl/Model/Resolver/AddItemsToCompareList.php
@@ -3,6 +3,8 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types=1);
+
 namespace Magento\CompareListGraphQl\Model\Resolver;
 
 use Magento\Framework\GraphQl\Config\Element\Field;
@@ -11,17 +13,18 @@ use Magento\Framework\GraphQl\Query\Resolver\Value;
 use Magento\Framework\GraphQl\Query\ResolverInterface;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
 
-class ComparableAttributeResolver implements ResolverInterface
+class AddItemsToCompareList implements ResolverInterface
 {
     /**
+     * Add items to compare list
+     *
      * @param Field            $field
      * @param ContextInterface $context
      * @param ResolveInfo      $info
      * @param array|null       $value
      * @param array|null       $args
      *
-     * @return array|Value|mixed
-     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     * @return Value|mixed|void
      */
     public function resolve(
         Field $field,
@@ -30,12 +33,6 @@ class ComparableAttributeResolver implements ResolverInterface
         array $value = null,
         array $args = null
     ) {
-        $attributes = [];
-        $attributes[] = [
-            'code' => 12,
-            'title' => 'Attr name'
-        ];
-
-        return $attributes;
+        // TODO: Implement resolve() method.
     }
 }
--- app/code/Magento/CompareListGraphQl/Model/Resolver/CompareList.php
+++ app/code/Magento/CompareListGraphQl/Model/Resolver/CompareList.php
@@ -7,32 +7,17 @@ declare(strict_types=1);
 
 namespace Magento\CompareListGraphQl\Model\Resolver;
 
-use Magento\Catalog\CustomerData\CompareProducts;
 use Magento\Framework\GraphQl\Config\Element\Field;
-use Magento\Framework\GraphQl\Exception\GraphQlAuthorizationException;
 use Magento\Framework\GraphQl\Query\Resolver\ContextInterface;
 use Magento\Framework\GraphQl\Query\Resolver\Value;
 use Magento\Framework\GraphQl\Query\ResolverInterface;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
-use Magento\GraphQl\Model\Query\ContextInterface as QueryContextInterface;
 
-class CompareListResolver implements ResolverInterface
+class CompareList implements ResolverInterface
 {
     /**
-     * @var CompareProducts
-     */
-    private $customerCompareList;
-
-    /**
-     * @param CompareProducts $customerCompareList
-     */
-    public function __construct(
-        CompareProducts $customerCompareList
-    ) {
-        $this->customerCompareList = $customerCompareList;
-    }
-
-    /**
+     * Get compare list
+     *
      * @param Field            $field
      * @param ContextInterface $context
      * @param ResolveInfo      $info
@@ -41,7 +26,6 @@ class CompareListResolver implements ResolverInterface
      *
      * @return array|Value|mixed
      *
-     * @throws GraphQlAuthorizationException
      * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
     public function resolve(
@@ -51,10 +35,6 @@ class CompareListResolver implements ResolverInterface
         array $value = null,
         array $args = null
     ) {
-        /** @var QueryContextInterface $context */
-        if (false === $context->getExtensionAttributes()->getIsCustomer()) {
-            throw new GraphQlAuthorizationException(__('The current customer isn\'t authorized.'));
-        }
-        return ['list_id' => $context->getUserId()];
+        // TODO: Implement resolve() method.
     }
 }
