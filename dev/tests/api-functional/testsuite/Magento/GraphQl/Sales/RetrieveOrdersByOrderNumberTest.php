30956ae33b3aba64e036ab3d0eac0b23bbd64ef7
Deepty Thampy <dthampy@adobe.com>
2020-06-02 22:37:59 -0500

    MC-32658: MyAccount :: Order Details :: Order Details by Order Number Taxes and Discounts
    - fixtures and test for tax and shipping for orders

--- dev/tests/api-functional/testsuite/Magento/GraphQl/Sales/RetrieveOrdersByOrderNumberTest.php
+++ dev/tests/api-functional/testsuite/Magento/GraphQl/Sales/RetrieveOrdersByOrderNumberTest.php
@@ -7,11 +7,13 @@ declare(strict_types=1);
 
 namespace Magento\GraphQl\Sales;
 
+use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Framework\Api\SearchCriteriaBuilder;
 use Magento\Integration\Api\CustomerTokenServiceInterface;
 use Magento\Sales\Api\OrderRepositoryInterface;
 use Magento\Sales\Model\Order;
 use Magento\TestFramework\Helper\Bootstrap;
+use Magento\Sales\Model\ResourceModel\Order\Collection;
 use Magento\TestFramework\TestCase\GraphQlAbstract;
 
 /**
@@ -38,7 +40,6 @@ class RetrieveOrdersByOrderNumberTest extends GraphQlAbstract
         parent::setUp();
         $objectManager = Bootstrap::getObjectManager();
         $this->customerTokenService = $objectManager->get(CustomerTokenServiceInterface::class);
-
         $this->orderRepository = $objectManager->get(OrderRepositoryInterface::class);
         $this->searchCriteriaBuilder = $objectManager->get(SearchCriteriaBuilder::class);
         $this->orderItem = $objectManager->get(Order\Item::class);
@@ -126,7 +127,7 @@ QUERY;
             [ 'quantity_ordered'=> 2,
                 'product_sku'=> 'simple',
                 'product_name'=> 'Simple Product',
-                'product_sale_price'=> ['currency'=> null, 'value'=> 10]
+                'product_sale_price'=> ['currency'=> 'USD', 'value'=> 10]
             ];
         $actualOrderItemsFromResponse = $customerOrderItemsInResponse['order_items'][0];
         $this->assertEquals($expectedOrderItems, $actualOrderItemsFromResponse);
@@ -654,12 +655,390 @@ QUERY;
         return ['Authorization' => 'Bearer ' . $customerToken];
     }
     /**
+     *  Verify that the customer order has the tax information on shipping and totals
      * @magentoApiDataFixture Magento/Customer/_files/customer.php
-     * @magentoApiDataFixture Magento/Sales/_files/order_with_tax.php
+     * @magentoApiDataFixture Magento/Catalog/_files/product_simple_with_url_key.php
+     * @magentoApiDataFixture Magento/GraphQl/Tax/_files/tax_rule_for_region_1.php
+     * @magentoApiDataFixture Magento/GraphQl/Tax/_files/tax_calculation_shipping_and_order_display_settings.php
      */
-    public function testCustomerOrderWithTaxes()
+    public function testCustomerOrderWithTaxesOnShippingAndPrices()
     {
+        $quantity = 2;
+        $sku = 'simple1';
+        $cartId = $this->createEmptyCart();
+        $this->addProductToCart($cartId, $quantity, $sku);
 
+        $this->setBillingAddress($cartId);
+        $shippingMethod = $this->setShippingAddress($cartId);
+
+        $paymentMethod = $this->setShippingMethod($cartId, $shippingMethod);
+        $this->setPaymentMethod($cartId, $paymentMethod);
+
+        $orderNumber = $this->placeOrder($cartId);
+        $customerOrderResponse = $this->getCustomerOrderQuery($orderNumber);
+        $customerOrderItem = $customerOrderResponse[0];
+        $this->assertTotalsAndShippingWithTaxes($customerOrderItem);
+        $this->deleteOrder();
+    }
+
+    /**
+     * @return string
+     */
+    private function createEmptyCart(): string
+    {
+        $query = <<<QUERY
+mutation {
+  createEmptyCart
+}
+QUERY;
+        $currentEmail = 'customer@example.com';
+        $currentPassword = 'password';
+        $response = $this->graphQlMutation($query, [], '', $this->getCustomerAuthHeaders($currentEmail, $currentPassword));
+        return $response['createEmptyCart'];
+    }
+
+    /**
+     * @param string $cartId
+     * @param float $qty
+     * @param string $sku
+     * @return void
+     */
+    private function addProductToCart(string $cartId, float $qty, string $sku): void
+    {
+        $query = <<<QUERY
+mutation {
+  addSimpleProductsToCart(
+    input: {
+      cart_id: "{$cartId}"
+      cart_items: [
+        {
+          data: {
+            quantity: {$qty}
+            sku: "{$sku}"
+          }
+        }
+      ]
+    }
+  ) {
+    cart {items{quantity product {sku}}}}
+}
+QUERY;
+        $currentEmail = 'customer@example.com';
+        $currentPassword = 'password';
+        $this->graphQlMutation($query, [], '', $this->getCustomerAuthHeaders($currentEmail, $currentPassword));
+    }
+
+    /**
+     * @param string $cartId
+     * @param array $auth
+     * @return array
+     */
+    private function setBillingAddress(string $cartId): void
+    {
+        $query = <<<QUERY
+mutation {
+  setBillingAddressOnCart(
+    input: {
+      cart_id: "{$cartId}"
+      billing_address: {
+         address: {
+          firstname: "John"
+          lastname: "Smith"
+          company: "Test company"
+          street: ["test street 1", "test street 2"]
+          city: "Texas City"
+          postcode: "78717"
+          telephone: "5123456677"
+          region: "TX"
+          country_code: "US"
+         }
+      }
+    }
+  ) {
+    cart {
+      billing_address {
+        __typename
+      }
+    }
+  }
+}
+QUERY;
+        $currentEmail = 'customer@example.com';
+        $currentPassword = 'password';
+        $this->graphQlMutation($query, [], '', $this->getCustomerAuthHeaders($currentEmail, $currentPassword));
+    }
+
+    /**
+     * @param string $cartId
+     * @return array
+     */
+    private function setShippingAddress(string $cartId): array
+    {
+        $query = <<<QUERY
+mutation {
+  setShippingAddressesOnCart(
+    input: {
+      cart_id: "$cartId"
+      shipping_addresses: [
+        {
+          address: {
+            firstname: "test shipFirst"
+            lastname: "test shipLast"
+            company: "test company"
+            street: ["test street 1", "test street 2"]
+            city: "Montgomery"
+            region: "AL"
+            postcode: "36013"
+            country_code: "US"
+            telephone: "3347665522"
+          }
+        }
+      ]
+    }
+  ) {
+    cart {
+      shipping_addresses {
+        available_shipping_methods {
+          carrier_code
+          method_code
+          amount {value}
+        }
+      }
+    }
+  }
+}
+QUERY;
+        $currentEmail = 'customer@example.com';
+        $currentPassword = 'password';
+        $response = $this->graphQlMutation($query, [], '', $this->getCustomerAuthHeaders($currentEmail, $currentPassword));
+        $shippingAddress = current($response['setShippingAddressesOnCart']['cart']['shipping_addresses']);
+        $availableShippingMethod = current($shippingAddress['available_shipping_methods']);
+        return $availableShippingMethod;
+    }
+    /**
+     * @param string $cartId
+     * @param array $method
+     * @return array
+     */
+    private function setShippingMethod(string $cartId, array $method): array
+    {
+        $query = <<<QUERY
+mutation {
+  setShippingMethodsOnCart(input:  {
+    cart_id: "{$cartId}",
+    shipping_methods: [
+      {
+         carrier_code: "{$method['carrier_code']}"
+         method_code: "{$method['method_code']}"
+      }
+    ]
+  }) {
+    cart {
+      available_payment_methods {
+        code
+        title
+      }
+    }
+  }
+}
+QUERY;
+        $currentEmail = 'customer@example.com';
+        $currentPassword = 'password';
+        $response = $this->graphQlMutation($query, [], '', $this->getCustomerAuthHeaders($currentEmail, $currentPassword));
+
+        $availablePaymentMethod = current($response['setShippingMethodsOnCart']['cart']['available_payment_methods']);
+        return $availablePaymentMethod;
+    }
+
+    /**
+     * @param string $cartId
+     * @param array $method
+     * @return void
+     */
+    private function setPaymentMethod(string $cartId, array $method): void
+    {
+        $query = <<<QUERY
+mutation {
+  setPaymentMethodOnCart(
+    input: {
+      cart_id: "{$cartId}"
+      payment_method: {
+        code: "{$method['code']}"
+      }
+    }
+  ) {
+    cart {selected_payment_method {code}}
+  }
+}
+QUERY;
+        $currentEmail = 'customer@example.com';
+        $currentPassword = 'password';
+        $this->graphQlMutation($query, [], '', $this->getCustomerAuthHeaders($currentEmail, $currentPassword));
+    }
+
+    /**
+     * @param string $cartId
+     * @return string
+     */
+    private function placeOrder(string $cartId): string
+    {
+        $query = <<<QUERY
+mutation {
+  placeOrder(
+    input: {
+      cart_id: "{$cartId}"
+    }
+  ) {
+    order {
+      order_number
+    }
+  }
+}
+QUERY;
+        $currentEmail = 'customer@example.com';
+        $currentPassword = 'password';
+        $response = $this->graphQlMutation($query, [], '', $this->getCustomerAuthHeaders($currentEmail, $currentPassword));
+        return $response['placeOrder']['order']['order_number'];
+    }
+
+    /**
+     * Get customer order query
+     *
+     * @param string $orderNumber
+     * @return array
+     */
+    private function getCustomerOrderQuery($orderNumber):array
+    {
+        $query =
+            <<<QUERY
+{
+     customer {
+       email
+       orders(filter:{number:{eq:"{$orderNumber}"}}) {
+         total_count
+         items {
+           id
+           number
+           order_date
+           status
+           order_items{product_name product_sku quantity_ordered}
+           totals {
+             base_grand_total{value currency}
+             grand_total{value currency}
+             total_tax{value}
+             subtotal { value currency }
+             taxes {amount{value currency} title rate}
+             total_shipping{value}
+             shipping_handling
+             {
+               amount_inc_tax{value}
+               amount_exc_tax{value}
+               total_amount{value}
+               taxes {amount{value} title rate}
+             }
+             discounts {amount{value currency} label}
+           }
+         }
+       }
+     }
+   }
+QUERY;
+        $currentEmail = 'customer@example.com';
+        $currentPassword = 'password';
+        $response = $this->graphQlQuery($query, [], '', $this->getCustomerAuthHeaders($currentEmail, $currentPassword));
+
+        $this->assertArrayHasKey('orders', $response['customer']);
+        $this->assertArrayHasKey('items', $response['customer']['orders']);
+        $customerOrderItemsInResponse = $response['customer']['orders']['items'];
+        return $customerOrderItemsInResponse;
+    }
+
+    /**
+     * @return void
+     */
+    private function deleteOrder(): void
+    {
+        /** @var \Magento\Framework\Registry $registry */
+        $registry = Bootstrap::getObjectManager()->get(\Magento\Framework\Registry::class);
+        $registry->unregister('isSecureArea');
+        $registry->register('isSecureArea', true);
+
+        /** @var $order \Magento\Sales\Model\Order */
+        $orderCollection = Bootstrap::getObjectManager()->create(Collection::class);
+        //$orderCollection = $this->orderCollectionFactory->create();
+        foreach ($orderCollection as $order) {
+            $this->orderRepository->delete($order);
+        }
+        $registry->unregister('isSecureArea');
+        $registry->register('isSecureArea', false);
+    }
+
+    /**
+     * Assert order totals including shipping_handling and taxes
+     *
+     * @param array $customerOrderItem
+     */
+    private function assertTotalsAndShippingWithTaxes(array $customerOrderItem): void
+    {
+        $this->assertEquals(
+            31.43,
+            $customerOrderItem['totals']['base_grand_total']['value']
+        );
+
+        $this->assertEquals(
+            31.43,
+            $customerOrderItem['totals']['grand_total']['value']
+        );
+        $this->assertEquals(
+            20,
+            $customerOrderItem['totals']['subtotal']['value']
+        );
+        $this->assertEquals(
+            2.19,
+            $customerOrderItem['totals']['total_tax']['value']
+        );
+
+        $this->assertEquals(
+            9.24,
+            $customerOrderItem['totals']['total_shipping']['value']
+        );
+        $this->assertEquals(
+            2.19,
+            $customerOrderItem['totals']['taxes'][0]['amount']['value']
+        );
+        $this->assertEquals(
+            'US-TEST-*-Rate-1',
+            $customerOrderItem['totals']['taxes'][0]['title']
+        );
+        $this->assertEquals(
+            7.5,
+            $customerOrderItem['totals']['taxes'][0]['rate']
+        );
+        $this->assertEquals(
+            9.93,
+            $customerOrderItem['totals']['shipping_handling']['amount_inc_tax']['value']
+        );
+        $this->assertEquals(
+            9.24,
+            $customerOrderItem['totals']['shipping_handling']['amount_exc_tax']['value']
+        );
+        $this->assertEquals(
+            9.24,
+            $customerOrderItem['totals']['shipping_handling']['total_amount']['value']
+        );
+
+        $this->assertEquals(
+            2.19,
+            $customerOrderItem['totals']['shipping_handling']['taxes'][0]['amount']['value']
+        );
+        $this->assertEquals(
+            'US-TEST-*-Rate-1',
+            $customerOrderItem['totals']['shipping_handling']['taxes'][0]['title']
+        );
+        $this->assertEquals(
+            7.5,
+            $customerOrderItem['totals']['shipping_handling']['taxes'][0]['rate']
+        );
     }
 
     /**
