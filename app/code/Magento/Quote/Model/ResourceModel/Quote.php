9996ae8af08c35ff82d4bd0df1132aed76df38c4
Serhii Balko <serhii.balko@transoftgroup.com>
2020-06-04 18:00:28 +0300

    MC-34695: Quote doesn't expire at time set when updated_at table gets updated

--- app/code/Magento/Quote/Model/ResourceModel/Quote.php
+++ app/code/Magento/Quote/Model/ResourceModel/Quote.php
@@ -230,7 +230,8 @@ class Quote extends AbstractDb
                 'items_qty' => new \Zend_Db_Expr(
                     $connection->quoteIdentifier('q.items_qty') . ' - ' . $connection->quoteIdentifier('qi.qty')
                 ),
-                'items_count' => new \Zend_Db_Expr($ifSql)
+                'items_count' => new \Zend_Db_Expr($ifSql),
+                'updated_at' => 'q.updated_at',
             ]
         )->join(
             ['qi' => $this->getTable('quote_item')],
@@ -277,21 +278,27 @@ class Quote extends AbstractDb
     {
         $tableQuote = $this->getTable('quote');
         $tableItem = $this->getTable('quote_item');
-        $subSelect = $this->getConnection()->select()->from(
-            $tableItem,
-            ['entity_id' => 'quote_id']
-        )->where(
-            'product_id IN ( ? )',
-            $productIds
-        )->group(
-            'quote_id'
-        );
-
-        $select = $this->getConnection()->select()->join(
-            ['t2' => $subSelect],
-            't1.entity_id = t2.entity_id',
-            ['trigger_recollect' => new \Zend_Db_Expr('1')]
-        );
+        $subSelect = $this->getConnection()
+            ->select()
+            ->from(
+                $tableItem,
+                ['entity_id' => 'quote_id']
+            )->where(
+                'product_id IN ( ? )',
+                $productIds
+            )->group(
+                'quote_id'
+            );
+        $select = $this->getConnection()
+            ->select()
+            ->join(
+                ['t2' => $subSelect],
+                't1.entity_id = t2.entity_id',
+                [
+                    'trigger_recollect' => new \Zend_Db_Expr('1'),
+                    'updated_at' => 't1.updated_at',
+                ]
+            );
         $updateQuery = $select->crossUpdateFromSelect(['t1' => $tableQuote]);
         $this->getConnection()->query($updateQuery);
 
