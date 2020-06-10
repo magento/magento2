308ef0e7932653cfedfdc1cc3176da3b8b8faaec
Marjan Petkovski <petkovski.marjan@gmail.com>
2020-06-10 14:39:50 +0200

    Make Customer Group Id available to Context

--- app/code/Magento/CustomerGraphQl/Model/Context/AddUserInfoToContext.php
+++ app/code/Magento/CustomerGraphQl/Model/Context/AddUserInfoToContext.php
@@ -10,6 +10,8 @@ namespace Magento\CustomerGraphQl\Model\Context;
 use Magento\Authorization\Model\UserContextInterface;
 use Magento\GraphQl\Model\Query\ContextParametersInterface;
 use Magento\GraphQl\Model\Query\ContextParametersProcessorInterface;
+use Magento\Customer\Api\CustomerRepositoryInterface;
+use Magento\Customer\Api\Data\GroupInterface;
 
 /**
  * @inheritdoc
@@ -22,12 +24,20 @@ class AddUserInfoToContext implements ContextParametersProcessorInterface
     private $userContext;
 
     /**
+     * @var CustomerRepositoryInterface
+     */
+    private $customerRepository;
+
+    /**
      * @param UserContextInterface $userContext
+     * @param CustomerRepositoryInterface $customerRepository
      */
     public function __construct(
-        UserContextInterface $userContext
+        UserContextInterface $userContext,
+        CustomerRepositoryInterface $customerRepository
     ) {
         $this->userContext = $userContext;
+        $this->customerRepository = $customerRepository;
     }
 
     /**
@@ -47,7 +57,19 @@ class AddUserInfoToContext implements ContextParametersProcessorInterface
         }
         $contextParameters->setUserType($currentUserType);
 
-        $contextParameters->addExtensionAttribute('is_customer', $this->isCustomer($currentUserId, $currentUserType));
+        if ($isCustomer = $this->isCustomer($currentUserId, $currentUserType)) {
+
+            $contextParameters->addExtensionAttribute('is_customer', $isCustomer);
+
+            try {
+                $customerGroupId = $this->customerRepository->getById($currentUserId)->getGroupId();
+            } catch (\Exception $e) {
+                $customerGroupId = GroupInterface::NOT_LOGGED_IN_ID;
+            }
+
+            $contextParameters->addExtensionAttribute('customer_group_id', $customerGroupId);
+        }
+
         return $contextParameters;
     }
 
