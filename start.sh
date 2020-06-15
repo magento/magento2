32926b4c57b30a03a668d257e25cab5a413b4fa2
Cris Pini <cris_pini@yahoo.co.uk>
2020-06-15 16:16:11 +0100

    Modify startup script so actions happen once

--- start.sh
+++ start.sh
@@ -8,13 +8,17 @@
 
 #/// Copy the public data from the .new onto the peristent volume 
 cd /var/www/html
-cp -a pub.new/* pub/
-cp -a .htaccess pub/
 
-#/// run setup, deploy the static content and flush the cache :)
-php bin/magento setup:static-content:deploy -f
-php bin/magento setup:upgrade
-php bin/magento deploy:mode:set production
-php bin/magento cache:flush
+MODE=$(php bin/magento deploy:mode:show | sed -e 's/\.//g' | awk '{print $4}')
+if [ $MODE != 'production' ]
+then
+    cp -a pub.new/* pub/
+    cp -a .htaccess pub/
+
+    #/// deploy the static content, compile di and flush the cache :)
+    php bin/magento deploy:mode:set production
+fi
+
+
 
 
