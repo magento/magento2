fd99ace7518c59f973ca5ec20636c58eb6d94e7d
Serhii Kovalenko <ganster3012@gmail.com>
2020-06-16 18:27:01 +0300

    [17] Read API :: Integration tests :: Customer* #55

--- dev/tests/integration/testsuite/Magento/Wishlist/_files/two_wishlists_for_two_diff_customers.php
+++ dev/tests/integration/testsuite/Magento/Wishlist/_files/two_wishlists_for_two_diff_customers.php
@@ -31,3 +31,16 @@ $wishlistForSecondCustomer = $objectManager->create(
 $wishlistForSecondCustomer->loadByCustomerId($secondCustomerIdFromFixture, true);
 $item = $wishlistForSecondCustomer->addNewItem($product, new \Magento\Framework\DataObject([]));
 $wishlistForSecondCustomer->save();
+
+/**
+ * We need to remember that automatic reindexation is not working properly in integration tests
+ * Reindexation is sitting on top of afterCommit callbacks:
+ * \Magento\Catalog\Model\Product::priceReindexCallback
+ *
+ * However, callbacks are applied only when transaction_level = 0 (when transaction is commited), however
+ * integration tests are not committing transactions, so we need to reindex data manually in order to reuse it in tests
+ */
+/** @var \Magento\Indexer\Model\Indexer $indexer */
+$indexer = Bootstrap::getObjectManager()->create(\Magento\Indexer\Model\Indexer::class);
+$indexer->load('catalog_product_price');
+$indexer->reindexList([$product->getId()]);
