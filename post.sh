c61da541a1340873f88714c69f6cb02292377de8
Cris Pini <cris_pini@yahoo.co.uk>
2020-06-17 19:42:20 +0100

    test...

--- post.sh
+++ post.sh
@@ -12,9 +12,10 @@ cd /var/www/html
 SOUCRE="pub.new"
 TARGET="pub"
 if [ -d "$SOUCRE" ]; then
-  mv $SOUCRE/* $TARGET/
+  cp -a $SOUCRE/* $TARGET/
   rm -f $SOURCE
   php bin/magento cache:flush
+  touch been-here.txt
 fi
 php bin/magento maintenance:disable
 
