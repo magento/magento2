76c4198b9060adcc2a8495843dcf42105797de66
Valerii Naida <vnayda@adobe.com>
2020-06-16 23:46:46 -0500

    magento/magento2-login-as-customer#171: Force customer unlogin during admin loglout without additional MySQL queries

--- app/code/Magento/LoginAsCustomer/Plugin/AdminLogoutPlugin.php
+++ app/code/Magento/LoginAsCustomer/Plugin/AdminLogoutPlugin.php
@@ -46,7 +46,7 @@ class AdminLogoutPlugin
     public function beforeLogout(Auth $subject): void
     {
         $user = $subject->getUser();
-        if ($this->config->isEnabled() && $user && $user->getIsLoggedAsCustomer()) {
+        if ($this->config->isEnabled() && $user) {
             $userId = (int)$user->getId();
             $this->deleteAuthenticationDataForUser->execute($userId);
         }
