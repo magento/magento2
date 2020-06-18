c65f7ef0d72f0129c90ae4d2f8848c4dd61a7b8f
Cris Pini <cris_pini@yahoo.co.uk>
2020-06-18 16:29:46 +0100

    Tweaks to settings

--- post.sh
+++ post.sh
@@ -9,6 +9,8 @@
 #/// Copy the public data from the .new onto the peristent volume 
 cd /var/www/html
 
+chgrp -R 82 pub/ var/
+
 MAINTENANCE="var/.maintenance.flag"
 if [ -f "$MAINTENANCE" ]; then
     php bin/magento deploy:mode:set production --skip-compilation
