a2d51131606d59e3124919f120243cfd69d998f3
Nazar Klovanych <nazarn96@gmail.com>
2020-06-18 14:33:26 +0300

    Introduce observers to track deleted entities

---
+++ app/code/Magento/MediaContentCms/Observer/PageDelete.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\MediaContentCms\Observer;
+
+use Magento\Cms\Model\Page as CmsPage;
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+use Magento\MediaContentApi\Api\Data\ContentIdentityInterfaceFactory;
+use Magento\MediaContentApi\Api\Data\ContentAssetLinkInterfaceFactory;
+use Magento\MediaContentApi\Api\DeleteContentAssetLinksInterface;
+use Magento\MediaContentApi\Model\GetEntityContentsInterface;
+use Magento\MediaContentApi\Api\ExtractAssetsFromContentInterface;
+
+/**
+ * Observe the adminhtml_cmspage_on_delete event and deletes relation between page content and media asset.
+ */
+class PageDelete implements ObserverInterface
+{
+    private const CONTENT_TYPE = 'cms_page';
+    private const TYPE = 'entityType';
+    private const ENTITY_ID = 'entityId';
+    private const FIELD = 'field';
+    
+    /**
+     * @var ContentIdentityInterfaceFactory
+     */
+    private $contentIdentityFactory;
+
+    /**
+      * @var ContentAssetLinkInterfaceFactory
+      */
+    private $contentAssetLinkFactory;
+
+    /**
+     * @var DeleteContentAssetLinksInterface
+     */
+    private $deleteContentAssetLinks;
+
+    /**
+     * @var array
+     */
+    private $fields;
+
+    /**
+     * @var GetEntityContentsInterface
+     */
+    private $getContent;
+
+    /**
+     * @var ExtractAssetsFromContentInterface
+     */
+    private $extractAssetsFromContent;
+    
+    /**
+     * @param ExtractAssetsFromContentInterface $extractAssetsFromContent
+     * @param GetEntityContentsInterface $getContent
+     * @param DeleteContentAssetLinksInterface $deleteContentAssetLinks
+     * @param ContentIdentityInterfaceFactory $contentIdentityFactory
+     * @param ContentAssetLinkInterfaceFactory $contentAssetLinkFactory
+     */
+    public function __construct(
+        ExtractAssetsFromContentInterface $extractAssetsFromContent,
+        GetEntityContentsInterface $getContent,
+        DeleteContentAssetLinksInterface $deleteContentAssetLinks,
+        ContentIdentityInterfaceFactory $contentIdentityFactory,
+        ContentAssetLinkInterfaceFactory $contentAssetLinkFactory,
+        array $fields
+    ) {
+        $this->extractAssetsFromContent = $extractAssetsFromContent;
+        $this->getContent = $getContent;
+        $this->deleteContentAssetLinks = $deleteContentAssetLinks;
+        $this->contentAssetLinkFactory = $contentAssetLinkFactory;
+        $this->contentIdentityFactory = $contentIdentityFactory;
+        $this->fields = $fields;
+    }
+
+    /**
+     * Retrieve the deleted category and  remove relation betwen category and asset
+     *
+     * @param Observer $observer
+     * @throws \Exception
+     */
+    public function execute(Observer $observer): void
+    {
+        $model = $observer->getEvent()->getData('object');
+        $contentAssetsLinks = [];
+        
+        if ($model instanceof CmsPage) {
+            foreach ($this->fields as $field) {
+                $contentIdentity = $this->contentIdentityFactory->create(
+                    [
+                        self::TYPE => self::CONTENT_TYPE,
+                        self::FIELD => $field,
+                        self::ENTITY_ID => (string) $model->getId(),
+                    ]
+                );
+                
+                $assets = $this->extractAssetsFromContent->execute((string) $model->getData($field));
+                
+                foreach ($assets as $asset) {
+                    $contentAssetLinks[] = $this->contentAssetLinkFactory->create(
+                        [
+                            'assetId' => $asset->getId(),
+                            'contentIdentity' => $contentIdentity
+                        ]
+                    );
+                }
+            }
+            if (!empty($contentAssetLinks)) {
+                $this->deleteContentAssetLinks->execute($contentAssetLinks);
+            }
+        }
+    }
+}
