b80a898e5b1288b3e0c41dd3f746f4c70ecac40c
Cris Pini <cris_pini@yahoo.co.uk>
2020-06-18 14:08:00 +0100

    Making some amends to the status

--- start.sh
+++ start.sh
@@ -9,16 +9,16 @@
 #/// Copy the public data from the .new onto the peristent volume 
 cd /var/www/html
 
-MODE=$(php bin/magento deploy:mode:show | sed -e 's/\.//g' | awk '{print $4}')
-if [ $MODE != 'production' ]
-then
-    #/// set maintenance mode
-    php bin/magento maintenance:enable
-    cp -a pub.release/* pub/
-    touch var/pre1.txt
-fi
-touch var/pre2.txt
-#echo "Release complete ...."
+#/// set maintenance mode + copy some static files into pub
+php bin/magento maintenance:enable
+cp -a pub.release/* pub/
+touch var/pre1.txt
+
+#MODE=$(php bin/magento deploy:mode:show | sed -e 's/\.//g' | awk '{print $4}')
+#if [ $MODE != 'production' ]
+#then
+#    #/// set maintenance mode
+#fi
 
 
 
