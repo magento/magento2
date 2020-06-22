e100822f5858d5c218cfb72a76478ff3db194a26
Barny Shergold <barnyshergold@MacBook-Pro-9.local>
2020-06-22 15:21:22 +0100

    Reversed change

--- app/code/Magento/CatalogUrlRewrite/Model/CategoryUrlPathGenerator.php
+++ app/code/Magento/CatalogUrlRewrite/Model/CategoryUrlPathGenerator.php
@@ -73,7 +73,10 @@ class CategoryUrlPathGenerator
         if (in_array($category->getParentId(), [Category::ROOT_CATEGORY_ID, Category::TREE_ROOT_ID])) {
             return '';
         }
-
+        $path = $category->getUrlPath();
+        if ($path !== null && !$category->dataHasChangedFor('url_key') && !$category->dataHasChangedFor('parent_id')) {
+            return $path;
+        }
         $path = $category->getUrlKey();
         if ($path === false) {
             return $category->getUrlPath();
