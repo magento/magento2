f925a4d91dac4523c0095aea3f524727574c4713
Paweł Tylek <pawel.tylek@creativestyle.pl>
2020-06-22 14:35:23 +0200

    M2C-22090 Move logic to js

--- app/code/Magento/Catalog/view/frontend/web/js/view/compare-products.js
+++ app/code/Magento/Catalog/view/frontend/web/js/view/compare-products.js
@@ -7,9 +7,10 @@ define([
     'uiComponent',
     'Magento_Customer/js/customer-data',
     'jquery',
+    'mage/translate',
     'mage/mage',
     'mage/decorate'
-], function (Component, customerData, $) {
+], function (Component, customerData, $, $t) {
     'use strict';
 
     var sidebarInitialized = false;
@@ -33,6 +34,15 @@ define([
             this.compareProducts = customerData.get('compare-products');
 
             initSidebar();
-        }
+        },
+        getCounterLabel: function () {
+            var counter = this.compareProducts().count;
+
+            if (counter === 1) {
+                return counter + ' ' + $t('item');
+            } else {
+                return $t('%1 items').replace('%1', counter);
+            }
+        },
     });
 });
