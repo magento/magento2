de70ebfdc2f86f66f86c6861947ddd0ef924ecae
Cris Pini <cris_pini@yahoo.co.uk>
2020-06-22 16:09:30 +0100

    Added horizantal pod scaler

--- start.sh
+++ start.sh
@@ -12,7 +12,7 @@ cd /var/www/html
 #MODE=$(php bin/magento deploy:mode:show | sed -e 's/\.//g' | awk '{print $4}')
 #/// set maintenance mode + copy some static files into pub
 php bin/magento maintenance:enable
-cp -a pub.release/* pub/
+cp -r pub.release/. pub/
 
 
 
