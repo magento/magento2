934db87d56bad355d759925aff9fe837c61ef398
Oleh Usik <o.usik@atwix.com>
2020-06-22 23:20:17 +0300

    Add some checks

--- app/code/Magento/PaypalGraphQl/Observer/PayflowProSetCcData.php
+++ app/code/Magento/PaypalGraphQl/Observer/PayflowProSetCcData.php
@@ -8,32 +8,89 @@ declare(strict_types=1);
 
 namespace Magento\PaypalGraphQl\Observer;
 
+use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\Event\Observer;
+use Magento\Framework\GraphQl\Exception\GraphQlInputException;
 use Magento\Payment\Observer\AbstractDataAssignObserver;
 use Magento\Quote\Api\Data\PaymentInterface;
+use Magento\Customer\Model\Session as CustomerModelSession;
 
 /**
  * Class PayflowProSetCcData set CcData to quote payment
  */
 class PayflowProSetCcData extends AbstractDataAssignObserver
 {
+    const XML_PATH_PAYMENT_PAYFLOWPRO_CC_VAULT_ACTIVE = "payment/payflowpro_cc_vault/active";
+    const IS_ACTIVE_PAYMENT_TOKEN_ENABLER = "is_active_payment_token_enabler";
+
+    /**
+     * @var CustomerModelSession
+     */
+    private $customerSession;
+
+    /**
+     * Core store config
+     *
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
+     * @param CustomerModelSession $customerSession
+     * @param ScopeConfigInterface $scopeConfig
+     */
+    public function __construct(
+        CustomerModelSession $customerSession,
+        ScopeConfigInterface $scopeConfig
+    ) {
+        $this->customerSession = $customerSession;
+        $this->scopeConfig = $scopeConfig;
+    }
+
     /**
      * Set CcData
      *
      * @param Observer $observer
+     *
+     * @throws GraphQlInputException
      */
     public function execute(Observer $observer)
     {
         $dataObject = $this->readDataArgument($observer);
         $additionalData = $dataObject->getData(PaymentInterface::KEY_ADDITIONAL_DATA);
+        $paymentModel = $this->readPaymentModelArgument($observer);
 
         if (!isset($additionalData['cc_details'])) {
             return;
         }
 
-        $paymentModel = $this->readPaymentModelArgument($observer);
+        if($this->customerSession->isLoggedIn() && $this->isPayflowProVaultEnable()) {
+            if (!isset($additionalData[self::IS_ACTIVE_PAYMENT_TOKEN_ENABLER])) {
+                throw new GraphQlInputException(
+                    __('Required parameter "is_active_payment_token_enabler" is missing.')
+                );
+            }
+
+            $paymentModel->setData(
+                self::IS_ACTIVE_PAYMENT_TOKEN_ENABLER,
+                $additionalData[self::IS_ACTIVE_PAYMENT_TOKEN_ENABLER]
+            );
+        } else {
+            $paymentModel->setData(self::IS_ACTIVE_PAYMENT_TOKEN_ENABLER, false);
+        }
+
         foreach ($additionalData['cc_details'] as $ccKey => $ccValue) {
             $paymentModel->setData($ccKey, $ccValue);
         }
+    }
+
+    /**
+     * Check if payflowpro vault is enable
+     *
+     * @return bool
+     */
+    private function isPayflowProVaultEnable()
+    {
+        return (bool)$this->scopeConfig->getValue(self::XML_PATH_PAYMENT_PAYFLOWPRO_CC_VAULT_ACTIVE);
     }
 }
