6f918fa450580d623454f7be36d439def9cf6779
Alex Taranovsky <firster@atwix.com>
2020-06-24 15:47:52 +0300

    magento/magento2#28481: GraphQl. updateCustomer allows to set any INT value in gender argument

--- app/code/Magento/CustomerGraphQl/Model/Customer/ValidateCustomerData.php
+++ app/code/Magento/CustomerGraphQl/Model/Customer/ValidateCustomerData.php
@@ -7,8 +7,7 @@ declare(strict_types=1);
 
 namespace Magento\CustomerGraphQl\Model\Customer;
 
-use Magento\Customer\Api\CustomerMetadataInterface;
-use Magento\Customer\Model\Data\AttributeMetadata;
+use Magento\CustomerGraphQl\Api\ValidateCustomerDataInterface;
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\GraphQl\Exception\GraphQlInputException;
@@ -19,11 +18,6 @@ use Magento\Framework\Validator\EmailAddress as EmailAddressValidator;
  */
 class ValidateCustomerData
 {
-    /**
-     * @var CustomerMetadataInterface
-     */
-    private $customerMetadata;
-
     /**
      * Get allowed/required customer attributes
      *
@@ -37,20 +31,25 @@ class ValidateCustomerData
     private $emailAddressValidator;
 
     /**
+     * @var ValidateCustomerDataInterface[]
+     */
+    private $validators = [];
+
+    /**
      * ValidateCustomerData constructor.
      *
      * @param GetAllowedCustomerAttributes $getAllowedCustomerAttributes
      * @param EmailAddressValidator $emailAddressValidator
-     * @param CustomerMetadataInterface $customerMetadata
+     * @param array $validators
      */
     public function __construct(
         GetAllowedCustomerAttributes $getAllowedCustomerAttributes,
         EmailAddressValidator $emailAddressValidator,
-        CustomerMetadataInterface $customerMetadata
+        $validators = []
     ) {
         $this->getAllowedCustomerAttributes = $getAllowedCustomerAttributes;
         $this->emailAddressValidator = $emailAddressValidator;
-        $this->customerMetadata = $customerMetadata;
+        $this->validators = $validators;
     }
 
     /**
@@ -61,81 +60,11 @@ class ValidateCustomerData
      * @throws LocalizedException
      * @throws NoSuchEntityException
      */
-    public function execute(array $customerData): void
+    public function execute(array $customerData)
     {
-        $this->validateRequiredArguments($customerData);
-        $this->validateEmail($customerData);
-        $this->validateGender($customerData);
-    }
-
-    /**
-     * Validate required attributes
-     *
-     * @param array $customerData
-     * @throws GraphQlInputException
-     */
-    private function validateRequiredArguments(array $customerData): void
-    {
-        $attributes = $this->getAllowedCustomerAttributes->execute(array_keys($customerData));
-        $errorInput = [];
-
-        foreach ($attributes as $attributeInfo) {
-            if ($attributeInfo->getIsRequired()
-                && (!isset($customerData[$attributeInfo->getAttributeCode()])
-                    || $customerData[$attributeInfo->getAttributeCode()] == '')
-            ) {
-                $errorInput[] = $attributeInfo->getDefaultFrontendLabel();
-            }
-        }
-
-        if ($errorInput) {
-            throw new GraphQlInputException(
-                __('Required parameters are missing: %1', [implode(', ', $errorInput)])
-            );
-        }
-    }
-
-    /**
-     * Validate an email
-     *
-     * @param array $customerData
-     * @throws GraphQlInputException
-     */
-    private function validateEmail(array $customerData): void
-    {
-        if (isset($customerData['email']) && !$this->emailAddressValidator->isValid($customerData['email'])) {
-            throw new GraphQlInputException(
-                __('"%1" is not a valid email address.', $customerData['email'])
-            );
-        }
-    }
-
-    /**
-     * Validate gender value
-     *
-     * @param array $customerData
-     * @throws GraphQlInputException
-     * @throws LocalizedException
-     * @throws NoSuchEntityException
-     */
-    private function validateGender(array $customerData): void
-    {
-        if (isset($customerData['gender']) && $customerData['gender']) {
-            /** @var AttributeMetadata $genderData */
-            $options = $this->customerMetadata->getAttributeMetadata('gender')->getOptions();
-
-            $isValid = false;
-            foreach ($options as $optionData) {
-                if ($optionData->getValue() && $optionData->getValue() == $customerData['gender']) {
-                    $isValid = true;
-                }
-            }
-
-            if (!$isValid) {
-                throw new GraphQlInputException(
-                    __('"%1" is not a valid gender value.', $customerData['gender'])
-                );
-            }
+        /** @var ValidateCustomerDataInterface $validator */
+        foreach ($this->validators as $validator) {
+            $validator->execute($customerData);
         }
     }
 }
