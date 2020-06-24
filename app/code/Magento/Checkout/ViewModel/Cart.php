b96085979a5009112b82caf7ee028e8f307ef87c
John Carlo Octabio <johncarlo@abovethefray.io>
2020-06-24 16:30:58 +0800

    magento/magento2#108: Clear Shopping Cart - Added file header copyright

--- app/code/Magento/Checkout/ViewModel/Cart.php
+++ app/code/Magento/Checkout/ViewModel/Cart.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
 namespace Magento\Checkout\ViewModel;
 
 use Magento\Framework\App\Config\ScopeConfigInterface;
