c9373a93a144eb1ad08de4719e306fd8dabad093
Serhii Balko <serhii.balko@transoftgroup.com>
2020-07-07 16:21:49 +0300

    MC-34621: Uses Per Coupon and Uses Per Customer not honored with concurrent orders

--- app/code/Magento/SalesRule/view/frontend/requirejs-config.js
+++ app/code/Magento/SalesRule/view/frontend/requirejs-config.js
@@ -11,7 +11,10 @@ var config = {
             },
             'Magento_Checkout/js/model/shipping-save-processor': {
                 'Magento_SalesRule/js/model/shipping-save-processor-mixin': true
-            }
+            },
+            'Magento_Checkout/js/action/place-order': {
+                'Magento_SalesRule/js/model/place-order-mixin': true
+            },
         }
     }
 };
