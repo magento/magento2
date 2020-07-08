c689798c58e6be2232164be9035d08c79e1f15e3
Marjan Petkovski <petkovski.marjan@gmail.com>
2020-07-08 15:32:24 +0200

    magento/magento2#28563: GraphQL product search does not consider Category Permissions configuration - Pass the context as argument
    Reveert AddUsserInfoToContext

--- app/code/Magento/CustomerGraphQl/Model/Context/AddUserInfoToContext.php
+++ app/code/Magento/CustomerGraphQl/Model/Context/AddUserInfoToContext.php
@@ -10,8 +10,6 @@ namespace Magento\CustomerGraphQl\Model\Context;
 use Magento\Authorization\Model\UserContextInterface;
 use Magento\GraphQl\Model\Query\ContextParametersInterface;
 use Magento\GraphQl\Model\Query\ContextParametersProcessorInterface;
-use Magento\Customer\Api\CustomerRepositoryInterface;
-use Magento\Customer\Api\Data\GroupInterface;
 
 /**
  * @inheritdoc
@@ -24,20 +22,12 @@ class AddUserInfoToContext implements ContextParametersProcessorInterface
     private $userContext;
 
     /**
-     * @var CustomerRepositoryInterface
-     */
-    private $customerRepository;
-
-    /**
      * @param UserContextInterface $userContext
-     * @param CustomerRepositoryInterface $customerRepository
      */
     public function __construct(
-        UserContextInterface $userContext,
-        CustomerRepositoryInterface $customerRepository
+        UserContextInterface $userContext
     ) {
         $this->userContext = $userContext;
-        $this->customerRepository = $customerRepository;
     }
 
     /**
@@ -58,15 +48,6 @@ class AddUserInfoToContext implements ContextParametersProcessorInterface
         $contextParameters->setUserType($currentUserType);
 
         $contextParameters->addExtensionAttribute('is_customer', $this->isCustomer($currentUserId, $currentUserType));
-
-        try {
-            $customerGroupId = $this->customerRepository->getById($currentUserId)->getGroupId();
-        } catch (\Exception $e) {
-            $customerGroupId = GroupInterface::NOT_LOGGED_IN_ID;
-        }
-
-        $contextParameters->addExtensionAttribute('customer_group_id', $customerGroupId);
-
         return $contextParameters;
     }
 
