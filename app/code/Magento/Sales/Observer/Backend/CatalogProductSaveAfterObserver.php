d005417df344f3811d73e78a50769ccd126b7034
vadim.malesh <engcom-vendorworker-charlie@adobe.com>
2020-07-20 19:51:21 +0300

    test

--- app/code/Magento/Sales/Observer/Backend/CatalogProductSaveAfterObserver.php
+++ app/code/Magento/Sales/Observer/Backend/CatalogProductSaveAfterObserver.php
@@ -3,48 +3,58 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+
+declare(strict_types=1);
+
 namespace Magento\Sales\Observer\Backend;
 
+use Magento\Catalog\Api\Data\ProductInterface;
+use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
+use Magento\Quote\Model\ResourceModel\Quote;
+use Magento\Catalog\Model\Product\Attribute\Source\Status;
+use Magento\Quote\Model\Product\QuoteItemsCleanerInterface;
 
+/**
+ * Change status process and delete disabled product from quote
+ */
 class CatalogProductSaveAfterObserver implements ObserverInterface
 {
     /**
-     * @var \Magento\Quote\Model\ResourceModel\Quote
+     * @var Quote
      */
-    protected $_quote;
+    private $quote;
 
     /**
-     * @param \Magento\Quote\Model\ResourceModel\Quote $quote
+     * @var QuoteItemsCleanerInterface
      */
-    public function __construct(\Magento\Quote\Model\ResourceModel\Quote $quote)
+    private $quoteItemsCleaner;
+
+    /**
+     * @param Quote $quote
+     */
+    public function __construct(Quote $quote, QuoteItemsCleanerInterface $quoteItemsCleaner)
     {
-        $this->_quote = $quote;
+        $this->quote = $quote;
+        $this->quoteItemsCleaner = $quoteItemsCleaner;
     }
 
     /**
-     * Mark recollect contain product(s) quotes
+     * Catalog Product After Save (change status process) and delete disabled product from quote
      *
-     * @param int $productId
-     * @param int $status
+     * @param Observer $observer
      * @return void
      */
-    protected function _recollectQuotes($productId, $status)
-    {
-        if ($status != \Magento\Catalog\Model\Product\Attribute\Source\Status::STATUS_ENABLED) {
-            $this->_quote->markQuotesRecollect($productId);
-        }
-    }
-
-    /**
-     * Catalog Product After Save (change status process)
-     *
-     * @param \Magento\Framework\Event\Observer $observer
-     * @return void
-     */
-    public function execute(\Magento\Framework\Event\Observer $observer)
+    public function execute(Observer $observer): void
     {
         $product = $observer->getEvent()->getProduct();
-        $this->_recollectQuotes($product->getId(), $product->getStatus());
+
+        $isProductDisabled = (int) $product->getStatus() === Status::STATUS_DISABLED;
+        if ($product->dataHasChangedFor(ProductInterface::STATUS) && $isProductDisabled) {
+            $this->quote->subtractProductFromQuotes($product);
+            $this->quoteItemsCleaner->execute($product);
+
+            $this->quote->markQuotesRecollect($product->getId());
+        }
     }
 }
