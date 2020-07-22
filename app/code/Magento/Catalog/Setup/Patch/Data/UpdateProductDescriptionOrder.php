eb09069eb6e060343c47ba506b81ffa30485e649
Matt Walters <bluemwhitew@hotmail.co.uk>
2020-07-22 21:48:05 +0100

    magento/magento2-page-builder#543: Content Product Attributes Default Sorting Order - Adding New Patch

---
+++ app/code/Magento/Catalog/Setup/Patch/Data/UpdateProductDescriptionOrder.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Catalog\Setup\Patch\Data;
+
+use Magento\Catalog\Setup\CategorySetup;
+use Magento\Catalog\Setup\CategorySetupFactory;
+use Magento\Framework\App\ResourceConnection;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\Patch\PatchVersionInterface;
+
+/**
+ * Class UpdateProductDescriptionOrder
+ *
+ * @package Magento\Catalog\Setup\Patch
+ */
+class UpdateProductDescriptionOrder implements DataPatchInterface, PatchVersionInterface
+{
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+
+    /**
+     * @var CategorySetupFactory
+     */
+    private $categorySetupFactory;
+
+    /**
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param CategorySetupFactory $categorySetupFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        CategorySetupFactory $categorySetupFactory
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->categorySetupFactory = $categorySetupFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply()
+    {
+        /** @var CategorySetup $categorySetup */
+        $categorySetup = $this->categorySetupFactory->create(['setup' => $this->moduleDataSetup]);
+        $attributeSetId = $categorySetup->getDefaultAttributeSetId(\Magento\Catalog\Model\Product::ENTITY);
+
+        // Content
+        $categorySetup->addAttributeGroup(
+            \Magento\Catalog\Model\Product::ENTITY,
+            $attributeSetId,
+            'Content',
+            15
+        );
+        $categorySetup->updateAttributeGroup(
+            \Magento\Catalog\Model\Product::ENTITY,
+            $attributeSetId,
+            'Content',
+            'tab_group_code',
+            'basic'
+        );
+        $categorySetup->addAttributeToGroup(
+            \Magento\Catalog\Model\Product::ENTITY,
+            $attributeSetId,
+            'Content',
+            'short_description',
+            100
+        );
+        $categorySetup->addAttributeToGroup(
+            \Magento\Catalog\Model\Product::ENTITY,
+            $attributeSetId,
+            'Content',
+            'description',
+            110
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies()
+    {
+        return [
+            UpdateMediaAttributesBackendTypes::class,
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getVersion()
+    {
+        return '2.3.5';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+}
