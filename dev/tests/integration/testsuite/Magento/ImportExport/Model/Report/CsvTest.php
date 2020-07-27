2f496e293418708f751189b36e803fa1d02503ef
Vasya Tsviklinskyi <tsviklinskyi@gmail.com>
2020-07-27 14:28:37 +0300

    MC-35479: Unable to download import history file

--- dev/tests/integration/testsuite/Magento/ImportExport/Model/Report/CsvTest.php
+++ dev/tests/integration/testsuite/Magento/ImportExport/Model/Report/CsvTest.php
@@ -44,7 +44,7 @@ class CsvTest extends \PHPUnit\Framework\TestCase
     /**
      * @inheritDoc
      */
-    protected function setUp()
+    protected function setUp(): void
     {
         $filesystem = Bootstrap::getObjectManager()->create(Filesystem::class);
         $this->directory = $filesystem->getDirectoryWrite(DirectoryList::VAR_DIR);
@@ -54,7 +54,7 @@ class CsvTest extends \PHPUnit\Framework\TestCase
     /**
      * @inheritDoc
      */
-    protected function tearDown()
+    protected function tearDown(): void
     {
         foreach ([$this->importFilePath, $this->reportPath] as $path) {
             if ($path && $this->directory->isExist($path)) {
