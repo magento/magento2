18f2b08c105d6ed6a939b70f13b0a17ecfae2f07
Oleh Usik <o.usik@atwix.com>
2020-07-30 10:46:19 +0300

    added changes to test

--- dev/tests/api-functional/testsuite/Magento/GraphQl/Catalog/ProductWithMultiselectAttributeTest.php
+++ dev/tests/api-functional/testsuite/Magento/GraphQl/Catalog/ProductWithMultiselectAttributeTest.php
@@ -7,6 +7,9 @@ declare(strict_types=1);
 
 namespace Magento\GraphQl\Catalog;
 
+use Magento\Framework\Exception\LocalizedException;
+use Magento\TestFramework\Helper\Bootstrap;
+use Magento\TestFramework\Helper\CacheCleaner;
 use Magento\TestFramework\TestCase\GraphQlAbstract;
 
 /**
@@ -21,6 +24,7 @@ class ProductWithMultiselectAttributeTest extends GraphQlAbstract
      */
     public function testQueryProductWithMultiselectAttribute()
     {
+        $this->reIndexAndCleanCache();
         $query = <<<QUERY
 {
   products(filter: {sku: {eq: "simple_ms_2" }}) {
@@ -36,5 +40,20 @@ QUERY;
             'Option 2, Option 3, Option 4 "!@#$%^&*',
             $response['products']['items'][0]['multiselect_attribute']
         );
+    }
+
+    /**
+     * Reindex and clean cache
+     *
+     * @return void
+     * @throws LocalizedException
+     */
+    private function reIndexAndCleanCache() : void
+    {
+        $appDir = dirname(Bootstrap::getInstance()->getAppTempDir());
+        $out = '';
+        // phpcs:ignore Magento2.Security.InsecureFunction
+        exec("php -f {$appDir}/bin/magento indexer:reindex", $out);
+        CacheCleaner::cleanAll();
     }
 }
