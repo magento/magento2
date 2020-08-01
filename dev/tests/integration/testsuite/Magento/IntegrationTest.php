6e003ee113398ce0b3fee00193dc8700725b8ba7
Jonas Hünig <jonas@huenig.name>
2020-08-01 18:08:17 +0200

    Update IntegrationTest.php

--- dev/tests/integration/testsuite/Magento/IntegrationTest.php
+++ dev/tests/integration/testsuite/Magento/IntegrationTest.php
@@ -5,70 +5,121 @@
  */
 declare(strict_types=1);
 
-namespace Magento;
+namespace Magento\MediaStorage\Console\Command;
 
-use Magento\TestFramework\SkippableInterface;
-use Magento\TestFramework\Workaround\Override\Config;
-use Magento\TestFramework\Workaround\Override\WrapperGenerator;
-use PHPUnit\Framework\TestSuite;
-use PHPUnit\TextUI\Configuration\Registry;
-use PHPUnit\TextUI\Configuration\TestSuiteCollection;
-use PHPUnit\TextUI\Configuration\TestSuiteMapper;
+use Magento\Catalog\Model\Product\Gallery\Processor;
+use Magento\Catalog\Model\ProductRepository;
+use Magento\Framework\App\Filesystem\DirectoryList;
+use Magento\Framework\Filesystem;
+use Magento\Framework\Filesystem\Directory\WriteInterface;
+use Magento\Framework\ObjectManagerInterface;
+use Magento\TestFramework\Helper\Bootstrap;
+use Symfony\Component\Console\Tester\CommandTester;
 
 /**
- * Integration tests wrapper.
+ * Test for \Magento\MediaStorage\Console\Command\ImagesResizeCommand.
  */
-class IntegrationTest extends TestSuite
+class ImageResizeCommandTest extends \PHPUnit\Framework\TestCase
 {
     /**
-     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
-     * @param string $className
-     * @return TestSuite
+     * @var CommandTester
      */
-    public static function suite($className)
+    private $tester;
+
+    /**
+     * @var ImagesResizeCommand
+     */
+    private $command;
+
+    /**
+     * @var ObjectManagerInterface
+     */
+    private $objectManager;
+
+    /**
+     * @var WriteInterface
+     */
+    private $mediaDirectory;
+
+    /**
+     * @var Filesystem
+     */
+    private $filesystem;
+
+    /**
+     * @var string
+     */
+    private $fileName;
+
+    /**
+     * @inheritDoc
+     */
+    protected function setUp(): void
     {
-        $generator = new WrapperGenerator();
-        $overrideConfig = Config::getInstance();
-        $configuration = Registry::getInstance()->get(self::getConfigurationFile());
-        $suitesConfig = $configuration->testSuite();
-        $suite = new TestSuite();
-        /** @var \PHPUnit\TextUI\Configuration\TestSuite $suiteConfig */
-        foreach ($suitesConfig as $suiteConfig) {
-            if ($suiteConfig->name() === 'Magento Integration Tests') {
-                continue;
-            }
-            $suites = (new TestSuiteMapper())->map(TestSuiteCollection::fromArray([$suiteConfig]), '');
-            /** @var TestSuite $testSuite */
-            foreach ($suites as $testSuite) {
-                /** @var TestSuite $test */
-                foreach ($testSuite as $test) {
-                    $testName = $test->getName();
+        parent::setUp();
 
-                    if ($overrideConfig->hasSkippedTest($testName) && !$test instanceof SkippableInterface) {
-                        $reflectionClass = new \ReflectionClass($testName);
-                        $resultTest = $generator->generateTestWrapper($reflectionClass);
-                        $suite->addTest(new TestSuite($resultTest, $testName));
-                    } else {
-                        $suite->addTest($test);
-                    }
-                }
-            }
-        }
-
-        return $suite;
+        $this->objectManager = Bootstrap::getObjectManager();
+        $this->fileName = 'image.jpg';
+        $this->command = $this->objectManager->get(ImagesResizeCommand::class);
+        $this->tester = new CommandTester($this->command);
+        $this->filesystem = $this->objectManager->get(Filesystem::class);
+        $this->mediaDirectory = $this->filesystem->getDirectoryWrite(DirectoryList::MEDIA);
     }
 
     /**
-     * Returns config file name from command line params.
+     * Test that catalog:image:resize command executed successfully with missing image file
      *
-     * @return string
+     * @magentoDataFixture Magento/MediaStorage/_files/product_with_missed_image.php
      */
-    private static function getConfigurationFile(): string
+    public function testRunResizeWithMissingFile()
     {
-        $params = getopt('c:', ['configuration:']);
-        $longConfig = $params['configuration'] ?? '';
-        $shortConfig = $params['c'] ?? '';
+        $this->tester->execute([]);
+        $this->assertStringContainsString('original image not found', $this->tester->getDisplay());
+    }
 
-        return $shortConfig ? $shortConfig : $longConfig;
+    /**
+     * Test command with zero byte file
+     *
+     * @magentoDataFixture Magento/Catalog/_files/product_simple.php
+     * @magentoDataFixture Magento/Catalog/_files/product_image.php
+     *
+     * @return void
+     */
+    public function testExecuteWithZeroByteImage()
+    {
+        $this->mediaDirectory->writeFile($this->fileName, '');
+
+        /** @var ProductRepository $productRepository */
+        $productRepository = $this->objectManager->create(ProductRepository::class);
+        $product = $productRepository->getById(1);
+
+        /** @var Processor $mediaGalleryProcessor */
+        $mediaGalleryProcessor = $this->objectManager->get(Processor::class);
+        $mediaGalleryProcessor->addImage(
+            $product,
+            $this->mediaDirectory->getAbsolutePath($this->fileName),
+            ['image','thumbnail','small_image'],
+            false,
+            false
+        );
+
+        $product->save();
+
+        $this->tester->execute([]);
+        $this->assertStringContainsString('Wrong file', $this->tester->getDisplay());
+        $this->mediaDirectory->getDriver()->deleteFile($this->mediaDirectory->getAbsolutePath($this->fileName));
+    }
+
+    /**
+     * Test that catalog:image:resize command executes successfully in database storage mode
+     * with file missing from local folder
+     *
+     * @magentoDataFixture Magento/MediaStorage/_files/database_mode.php
+     * @magentoDataFixture Magento/MediaStorage/_files/product_with_missed_image.php
+     */
+    public function testDatabaseStorageMissingFile()
+    {
+        $this->tester->execute([]);
+        $this->assertStringContainsString('Product images resized successfully', $this->tester->getDisplay());
     }
 }
