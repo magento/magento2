7462443858f4524b5eb539e6cf875fa808e87b89
DmytroPaidych <dimonovp@gmail.com>
2020-08-06 20:50:37 +0300

    MC-30658: Storefront: Create reorder from customer profile page.

---
+++ dev/tests/integration/testsuite/Magento/Sales/_files/customer_order_with_simple_product_rollback.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+use Magento\Framework\Registry;
+use Magento\Sales\Api\Data\OrderInterfaceFactory;
+use Magento\Sales\Api\OrderRepositoryInterface;
+use Magento\TestFramework\Helper\Bootstrap;
+
+$objectManager = Bootstrap::getObjectManager();
+/** @var Registry $registry */
+$registry = $objectManager->get(Registry::class);
+/** @var OrderRepositoryInterface $orderRepository */
+$orderRepository = $objectManager->get(OrderRepositoryInterface::class);
+/** @var OrderInterfaceFactory $orderFactory */
+$orderFactory = $objectManager->get(OrderInterfaceFactory::class);
+
+$registry->unregister('isSecureArea');
+$registry->register('isSecureArea', true);
+
+$order = $orderFactory->create()->loadByIncrementId('55555555');
+if ($order->getId()) {
+    $orderRepository->delete($order);
+}
+
+$registry->unregister('isSecureArea');
+$registry->register('isSecureArea', false);
+
+require __DIR__ . '/../../Checkout/_files/customer_quote_ready_for_order_rollback.php';
