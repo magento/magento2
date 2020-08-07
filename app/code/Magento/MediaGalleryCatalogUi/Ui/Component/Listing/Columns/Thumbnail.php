4d5be024b12f21bc1a58132250e2b648505889c3
janmonteros <janraymonteros@gmail.com>
2020-08-07 23:33:10 +0800

    magento/adobe-stock-integration#1711: Use product model instead of data object for catalog image helper init - Revised approach based from pr suggestion

--- app/code/Magento/MediaGalleryCatalogUi/Ui/Component/Listing/Columns/Thumbnail.php
+++ app/code/Magento/MediaGalleryCatalogUi/Ui/Component/Listing/Columns/Thumbnail.php
@@ -5,8 +5,9 @@
  */
 namespace Magento\MediaGalleryCatalogUi\Ui\Component\Listing\Columns;
 
-use Magento\Catalog\Helper\Image;
-use Magento\Catalog\Model\ProductFactory;
+use Magento\Catalog\Model\Category\Image;
+use Magento\Catalog\Model\CategoryRepository;
+use Magento\Framework\View\Asset\Repository as AssetRepository;
 use Magento\Framework\View\Element\UiComponent\ContextInterface;
 use Magento\Framework\View\Element\UiComponentFactory;
 use Magento\Store\Model\Store;
@@ -27,19 +28,32 @@ class Thumbnail extends Column
     /**
      * @var Image
      */
-    private $imageHelper;
+    private $categoryImage;
 
     /**
-     * @var ProductFactory
+     * @var CategoryRepository
      */
-    private $productFactory;
+    private $categoryRepository;
 
     /**
+     * @var AssetRepository
+     */
+    private $assetRepository;
+
+    /**
+     * @var string[]
+     */
+    private $defaultPlaceholder;
+
+    /**
+     * Thumbnail constructor.
      * @param ContextInterface $context
      * @param UiComponentFactory $uiComponentFactory
      * @param StoreManagerInterface $storeManager
-     * @param Image $image
-     * @param ProductFactory $productFactory
+     * @param Image $categoryImage
+     * @param CategoryRepository $categoryRepository
+     * @param AssetRepository $assetRepository
+     * @param array $defaultPlaceholder
      * @param array $components
      * @param array $data
      */
@@ -47,15 +61,19 @@ class Thumbnail extends Column
         ContextInterface $context,
         UiComponentFactory $uiComponentFactory,
         StoreManagerInterface $storeManager,
-        Image $image,
-        ProductFactory $productFactory,
+        Image $categoryImage,
+        CategoryRepository $categoryRepository,
+        AssetRepository $assetRepository,
+        array $defaultPlaceholder = [],
         array $components = [],
         array $data = []
     ) {
         parent::__construct($context, $uiComponentFactory, $components, $data);
-        $this->imageHelper = $image;
         $this->storeManager = $storeManager;
-        $this->productFactory = $productFactory;
+        $this->categoryImage = $categoryImage;
+        $this->categoryRepository = $categoryRepository;
+        $this->assetRepository = $assetRepository;
+        $this->defaultPlaceholder = $defaultPlaceholder;
     }
 
     /**
@@ -63,20 +81,34 @@ class Thumbnail extends Column
      *
      * @param array $dataSource
      * @return array
+     * @throws \Magento\Framework\Exception\LocalizedException
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
      */
     public function prepareDataSource(array $dataSource)
     {
-        if (isset($dataSource['data']['items'])) {
-            $fieldName = $this->getData('name');
-            foreach ($dataSource['data']['items'] as & $item) {
-                if (isset($item[$fieldName])) {
-                    $item[$fieldName . '_src'] = $this->getUrl($item[$fieldName]);
-                } else {
-                    $category = $this->productFactory->create(['data' => $item]);
-                    $imageHelper = $this->imageHelper->init($category, 'product_listing_thumbnail');
-                    $item[$fieldName . '_src'] = $imageHelper->getUrl();
+        if (!isset($dataSource['data']['items'])) {
+            return $dataSource;
+        }
+
+        $fieldName = $this->getData('name');
+        foreach ($dataSource['data']['items'] as & $item) {
+            if (isset($item[$fieldName])) {
+                $item[$fieldName . '_src'] = $this->getUrl($item[$fieldName]);
+                continue;
+            }
+
+            if (isset($item['entity_id'])) {
+                $src = $this->categoryImage->getUrl(
+                    $this->categoryRepository->get($item['entity_id'])
+                );
+
+                if (!empty($src)) {
+                    $item[$fieldName . '_src'] = $src;
+                    continue;
                 }
             }
+
+            $item[$fieldName . '_src'] = $this->assetRepository->getUrl($this->defaultPlaceholder['image']);
         }
 
         return $dataSource;
