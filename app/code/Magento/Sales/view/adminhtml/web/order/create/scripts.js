magento/magento2

commit faa5f9a572ac1ea44750936bd9dc51c20513269d

MAGETWO-42996: Implement prompt, alert, confirm dialogs

app/code/Magento/Sales/view/adminhtml/web/order/create/scripts.js
@@ -4,10 +4,11 @@
  */
 define([
     "jquery",
+    'Magento_Ui/js/modal/confirm',
     "mage/translate",
     "prototype",
     "Magento_Catalog/catalog/product/composite/configure"
-], function(jQuery){
+], function(jQuery, confirm){
 
 window.AdminOrder = new Class.create();
 
@@ -704,11 +705,18 @@ AdminOrder.prototype = {
     },
 
     clearShoppingCart : function(confirmMessage){
-        if (confirm(confirmMessage)) {
-            this.collectElementsValue = false;
-            order.sidebarApplyChanges({'sidebar[empty_customer_cart]': 1});
-            this.collectElementsValue = true;
-        }
+        var self = this;
+
+        confirm({
+            content: confirmMessage,
+            actions: {
+                confirm: function() {
+                    self.collectElementsValue = false;
+                    order.sidebarApplyChanges({'sidebar[empty_customer_cart]': 1});
+                    self.collectElementsValue = true;
+                }
+            }
+        });
     },
 
     sidebarApplyChanges : function(auxiliaryParams) {

app/code/Magento/Backend/view/adminhtml/templates/dashboard/index.phtml
@@ -10,7 +10,10 @@
 
 <?php if (is_array($block->getChildBlock('diagrams')->getTabsIds())) : ?>
 <script>
-require(['prototype'], function(){
+require([
+    'Magento_Ui/js/modal/alert',
+    'prototype'
+], function(alert){
 
 window.changeDiagramsPeriod = function(periodObj) {
     periodParam = periodObj.value ? 'period/' + periodObj.value + '/' : '';
@@ -25,7 +28,9 @@ window.changeDiagramsPeriod = function(periodObj) {
                 if (transport.responseText.isJSON()) {
                     var response = transport.responseText.evalJSON()
                     if (response.error) {
-                        alert(response.message);
+                        alert({
+                            content: response.message
+                        });
                     }
                     if(response.ajaxExpired && response.ajaxRedirect) {
                         setLocation(response.ajaxRedirect);
@@ -47,9 +52,11 @@ window.changeDiagramsPeriod = function(periodObj) {
             tabContentElementId = 'dashboard_diagram_totals';
             try {
                 if (transport.responseText.isJSON()) {
-                    var response = transport.responseText.evalJSON()
+                    var response = transport.responseText.evalJSON();
                     if (response.error) {
-                        alert(response.message);
+                        alert({
+                            content: response.message
+                        });
                     }
                     if(response.ajaxExpired && response.ajaxRedirect) {
                         setLocation(response.ajaxRedirect);

app/code/Magento/Backend/view/adminhtml/templates/media/uploader.phtml
@@ -31,9 +31,10 @@
 require([
     "jquery",
     'mage/template',
+    'Magento_Ui/js/modal/alert',
     "mage/translate",
     "jquery/file-uploader"
-], function ($, mageTemplate) {
+], function ($, mageTemplate, alert) {
 
     $('#fileupload').fileupload({
         dataType: 'json',
@@ -75,7 +76,9 @@ require([
                 $('#' + data.fileId)
                     .delay(2000)
                     .hide('highlight');
-                alert($.mage.__('We don\'t recognize or support this file extension type.'));
+                alert({
+                   content: $.mage.__('We don\'t recognize or support this file extension type.')
+                });
             }
             $('#' + data.fileId).remove();
         },

app/code/Magento/Catalog/view/adminhtml/templates/catalog/category/edit.phtml
@@ -21,9 +21,10 @@ require([
     "jquery",
     "tinymce",
     'Magento_Ui/js/modal/confirm',
+    'Magento_Ui/js/modal/alert',
     "loadingPopup",
     "mage/backend/floating-header"
-], function(jQuery, tinyMCE, confirm){
+], function(jQuery, tinyMCE, confirm, alert){
 
 //<![CDATA[
     function categoryReset(url,useAjax){
@@ -87,7 +88,9 @@ require([
                         });
                         $categoryContainer.html('');
                     } catch (e) {
-                        alert(e.message);
+                        alert({
+                            content: e.message
+                        });
                     }
                     $categoryContainer.html(data.content).trigger('contentUpdated');
                     setTimeout(function() {
@@ -104,7 +107,9 @@ require([
                                 window.refreshTreeArea();
                             }
                         } catch (e) {
-                            alert(e.message);
+                            alert({
+                                content: e.message
+                            });
                         };
                     }, 25);
                 }

app/code/Magento/Catalog/view/adminhtml/templates/catalog/product/attribute/js.phtml
@@ -8,7 +8,12 @@
 
 ?>
 <script>
-require(["jquery", "collapsable", "prototype"], function(jQuery){
+require([
+    "jquery",
+    'Magento_Ui/js/modal/alert',
+    "collapsable",
+    "prototype"
+], function(jQuery, alert){
 
 function toggleApplyVisibility(select) {
     if ($(select).value == 1) {
@@ -269,7 +274,10 @@ function saveAttributeInNewSet(promptMessage)
     var rules = ['required-entry', 'validate-no-html-tags'];
     for (var i = 0; i < rules.length; i++) {
         if (!jQuery.validator.methods[rules[i]](newAttributeSetName)) {
-            alert(jQuery.validator.messages[rules[i]]);
+            alert({
+                content: jQuery.validator.messages[rules[i]]
+            });
+
             return;
         }
     }

app/code/Magento/Checkout/view/frontend/web/js/sidebar.js
@@ -9,9 +9,10 @@ define([
     'Magento_Customer/js/model/authentication-popup',
     'Magento_Customer/js/customer-data',
     'Magento_Ui/js/modal/alert',
+    'Magento_Ui/js/modal/confirm',
     "jquery/ui",
     "mage/decorate"
-], function($, authenticationPopup, customerData, alert){
+], function($, authenticationPopup, customerData, alert, confirm){
 
     $.widget('mage.sidebar', {
         options: {
@@ -47,9 +48,14 @@ define([
             }, this);
             events['click ' + this.options.button.remove] =  function(event) {
                 event.stopPropagation();
-                if (confirm(self.options.confirmMessage)) {
-                    self._removeItem($(event.target));
-                }
+                confirm({
+                    content: self.options.confirmMessage,
+                    actions: {
+                        confirm: function () {
+                            self._removeItem($(event.target))
+                        }
+                    }
+                });
             };
             events['keyup ' + this.options.item.qty] = function(event) {
                 self._showItemButton($(event.target));

app/code/Magento/ConfigurableProduct/view/adminhtml/templates/catalog/product/attribute/set/js.phtml
@@ -9,7 +9,10 @@
 
 ?>
 <script>
-require(["tree-panel"], function(){
+require([
+    "Magento_Ui/js/modal/alert",
+    "tree-panel"
+], function(alert){
 ConfigurableNodeExists = function(currentNode) {
     for (var i in currentNode.childNodes ) {
         if (currentNode.childNodes[i].id) {
@@ -25,7 +28,9 @@ ConfigurableNodeExists = function(currentNode) {
 editSet.submit = editSet.submit.wrap(function(original) {
     if (editSet.currentNode){
         if (ConfigurableNodeExists(editSet.currentNode)) {
-            alert('<?php /* @escapeNotVerified */ echo $block->escapeJsQuote(__('This group contains attributes used in configurable products. Please move these attributes to another group and try again.')) ?>');
+            alert({
+                content: '<?php /* @escapeNotVerified */ echo $block->escapeJsQuote(__('This group contains attributes used in configurable products. Please move these attributes to another group and try again.')) ?>'
+            });
             return;
         }
     }
@@ -34,7 +39,9 @@ editSet.submit = editSet.submit.wrap(function(original) {
 
 editSet.rightBeforeAppend = editSet.rightBeforeAppend.wrap(function(original, tree, nodeThis, node, newParent) {
     if (node.attributes.is_configurable == 1) {
-        alert('<?php /* @escapeNotVerified */ echo $block->escapeJsQuote(__('This attribute is used in configurable products. You cannot remove it from the product template.')) ?>');
+        alert({
+            content: '<?php /* @escapeNotVerified */ echo $block->escapeJsQuote(__('This attribute is used in configurable products. You cannot remove it from the product template.')) ?>'
+        });
         return false;
     }
     return original(tree, nodeThis, node, newParent);
@@ -42,7 +49,9 @@ editSet.rightBeforeAppend = editSet.rightBeforeAppend.wrap(function(original, tr
 
 editSet.rightBeforeInsert = editSet.rightBeforeInsert.wrap(function(original, tree, nodeThis, node, newParent) {
     if (node.attributes.is_configurable == 1) {
-        alert('<?php /* @escapeNotVerified */ echo $block->escapeJsQuote(__('This attribute is used in configurable products. You cannot remove it from the product template.')) ?>');
+        alert({
+            content: '<?php /* @escapeNotVerified */ echo $block->escapeJsQuote(__('This attribute is used in configurable products. You cannot remove it from the product template.')) ?>'
+        });
         return false;
     }
     return original(tree, nodeThis, node, newParent);

commit b5045c4abcbd70d82b712ba293c83ce171dace54

Fix customer group update confirm
Was broken with commit https://github.com/magento/magento2/commit/faa5f9a572ac1ea44750936bd9dc51c20513269d#diff-47459785dc7008bc9145a9609c9c7f6c and never fixed.

app/code/Magento/Sales/view/adminhtml/web/order/create/scripts.js
@@ -1493,14 +1493,20 @@ define([
             }
 
             if (action === 'change') {
-                var confirmText = message.replace(/%s/, customerGroupOption.text);
+                var self = this,
+                    confirmText = message.replace(/%s/, customerGroupOption.text);
                 confirmText = confirmText.replace(/%s/, currentCustomerGroupTitle);
-                if (confirm(confirmText)) {
-                    $$('#' + groupIdHtmlId + ' option').each(function (o) {
-                        o.selected = o.readAttribute('value') == groupId;
-                    });
-                    this.accountGroupChange();
-                }
+                confirm({
+                    content: confirmText,
+                    actions: {
+                        confirm: function() {
+                            $$('#' + groupIdHtmlId + ' option').each(function (o) {
+                                o.selected = o.readAttribute('value') == groupId;
+                            });
+                            self.accountGroupChange();
+                        }
+                    }
+                })
             } else if (action === 'inform') {
                 alert({
                     content: message + '\n' + groupMessage