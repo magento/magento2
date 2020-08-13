b6e5b4ddf7551ed380229cd543aa40e4470edd1b
ruslankostiv <rkostiv@adobe.com>
2020-08-13 12:28:46 -0500

    fixtures isolation fix.

--- dev/tests/integration/testsuite/Magento/Reports/_files/recently_compared_out_of_stock_product_rollback.php
+++ dev/tests/integration/testsuite/Magento/Reports/_files/recently_compared_out_of_stock_product_rollback.php
@@ -6,6 +6,21 @@
 declare(strict_types=1);
 
 use Magento\TestFramework\Workaround\Override\Fixture\Resolver;
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\TestFramework\Helper\Bootstrap;
 
 Resolver::getInstance()->requireDataFixture('Magento/Catalog/_files/out_of_stock_product_with_category.php');
 Resolver::getInstance()->requireDataFixture('Magento/Customer/_files/customer_rollback.php');
+
+/** @var \Magento\Framework\Registry $registry */
+$objectManager = Bootstrap::getObjectManager();
+$registry = $objectManager->get(\Magento\Framework\Registry::class);
+$registry->unregister('isSecureArea');
+$registry->register('isSecureArea', true);
+
+/** @var ProductRepositoryInterface $productRepository */
+$productRepository = $objectManager->get(ProductRepositoryInterface::class);
+$product = $productRepository->deleteById('out-of-stock-product');
+
+$registry->unregister('isSecureArea');
+$registry->register('isSecureArea', false);
