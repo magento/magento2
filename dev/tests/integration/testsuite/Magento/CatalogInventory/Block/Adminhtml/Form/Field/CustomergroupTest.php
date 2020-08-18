5436ecff043ea62a31a39e592168743736647fcf
vadim.malesh <engcom-vendorworker-charlie@adobe.com>
2020-08-18 11:24:41 +0300

    merge

--- dev/tests/integration/testsuite/Magento/CatalogInventory/Block/Adminhtml/Form/Field/CustomergroupTest.php
+++ dev/tests/integration/testsuite/Magento/CatalogInventory/Block/Adminhtml/Form/Field/CustomergroupTest.php
@@ -6,31 +6,33 @@
 
 namespace Magento\CatalogInventory\Block\Adminhtml\Form\Field;
 
-class CustomergroupTest extends \PHPUnit\Framework\TestCase
+use Magento\TestFramework\Helper\Bootstrap;
+use PHPUnit\Framework\TestCase;
+
+class CustomergroupTest extends TestCase
 {
     /**
-     * @var \Magento\CatalogInventory\Block\Adminhtml\Form\Field\Customergroup
+     * @var Customergroup
      */
     protected $_block;
 
     protected function setUp(): void
     {
-        $this->_block = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create(
-            \Magento\CatalogInventory\Block\Adminhtml\Form\Field\Customergroup::class
-        );
+        $this->_block = Bootstrap::getObjectManager()->create(Customergroup::class);
     }
 
-    public function testToHtml()
+    public function testToHtml(): void
     {
         $this->_block->setClass('customer_group_select admin__control-select');
         $this->_block->setId('123');
         $this->_block->setTitle('Customer Group');
-        $this->_block->setInputName('groups[item_options]');
+        $this->_block->setName('groups[item_options]');
         $expectedResult = '<select name="groups[item_options]" id="123" '
             . 'class="customer_group_select admin__control-select" '
             . 'title="Customer Group" ><option value="32000" >ALL GROUPS</option><option value="0" >NOT LOGGED IN'
             . '</option><option value="1" >General</option><option value="2" >Wholesale</option><option value="3" >'
             . 'Retailer</option></select>';
+
         $this->assertEquals($expectedResult, $this->_block->_toHtml());
     }
 }
