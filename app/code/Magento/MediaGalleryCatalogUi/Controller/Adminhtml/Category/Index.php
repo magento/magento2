afe499d5fbd09abd86e3c8d48797395fada01fa6
janmonteros <janraymonteros@gmail.com>
2020-08-18 20:01:07 +0800

    magento/adobe-stock-integration#1760: Media Gallery page and Category grid page opened successfully if "Enhanced Media Gallery" disabled - redirect to 404 if enhanced media gallery config is disabled.

--- app/code/Magento/MediaGalleryCatalogUi/Controller/Adminhtml/Category/Index.php
+++ app/code/Magento/MediaGalleryCatalogUi/Controller/Adminhtml/Category/Index.php
@@ -8,10 +8,13 @@ declare(strict_types=1);
 namespace Magento\MediaGalleryCatalogUi\Controller\Adminhtml\Category;
 
 use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
 use Magento\Backend\Model\View\Result\Page;
+use Magento\Backend\Model\View\Result\Forward;
 use Magento\Framework\App\Action\HttpGetActionInterface;
 use Magento\Framework\Controller\ResultFactory;
 use Magento\Framework\Controller\ResultInterface;
+use Magento\MediaContentApi\Model\Config;
 
 /**
  * Controller serving the media gallery content
@@ -21,12 +24,38 @@ class Index extends Action implements HttpGetActionInterface
     public const ADMIN_RESOURCE = 'Magento_Cms::media_gallery';
 
     /**
+     * @var Config
+     */
+    private $config;
+
+    /**
+     * Index constructor.
+     * @param Context $context
+     * @param Config $config
+     */
+    public function __construct(
+        Context $context,
+        Config $config
+    ) {
+        parent::__construct($context);
+        $this->config = $config;
+    }
+
+    /**
      * Get the media gallery layout
      *
      * @return ResultInterface
      */
     public function execute(): ResultInterface
     {
+        if (!$this->config->isEnabled()) {
+            /** @var Forward $resultForward */
+            $resultForward = $this->resultFactory->create(ResultFactory::TYPE_FORWARD);
+            $resultForward->forward('noroute');
+
+            return $resultForward;
+        }
+
         /** @var Page $resultPage */
         $resultPage = $this->resultFactory->create(ResultFactory::TYPE_PAGE);
         $resultPage->getConfig()->getTitle()->prepend(__('Categories'));
