16dc1fa89136018040542b03fde972180d445ebe
yolouiese <honeymay@abovethefray.io>
2020-08-21 19:47:03 +0800

    magento/adobe-stock-integration#1724: Support batches processing for synchronization queue messages - added method to sync and save media content to database

--- app/code/Magento/MediaContentSynchronization/Model/SynchronizeIdentities.php
+++ app/code/Magento/MediaContentSynchronization/Model/SynchronizeIdentities.php
@@ -7,6 +7,7 @@ declare(strict_types=1);
 
 namespace Magento\MediaContentSynchronization\Model;
 
+use Magento\Framework\App\ResourceConnection;
 use Magento\MediaContentApi\Api\Data\ContentIdentityInterfaceFactory;
 use Magento\MediaContentApi\Api\UpdateContentAssetLinksInterface;
 use Magento\MediaContentApi\Model\GetEntityContentsInterface;
@@ -17,11 +18,23 @@ class SynchronizeIdentities implements SynchronizeIdentitiesInterface
     private const ENTITY_TYPE = 'entityType';
     private const ENTITY_ID = 'entityId';
     private const FIELD = 'field';
+
     private const MEDIA_CONTENT_TYPE = 'entity_type';
     private const MEDIA_CONTENT_ENTITY_ID = 'entity_id';
     private const MEDIA_CONTENT_FIELD = 'field';
+
     private const FIELD_CMS_PAGE = 'cms_page';
     private const FIELD_CMS_BLOCK = 'cms_block';
+
+    private const ID_CMS_PAGE = 'page_id';
+    private const ID_CMS_BLOCK = 'block_id';
+
+    private const COLUMN_CMS_CONTENT = 'content';
+
+    /**
+     * @var ResourceConnection
+     */
+    private $resourceConnection;
 
     /**
      * @var ContentIdentityInterfaceFactory
@@ -39,26 +52,21 @@ class SynchronizeIdentities implements SynchronizeIdentitiesInterface
     private $getEntityContents;
 
     /**
-     * @var array
-     */
-    private $fields;
-
-    /**
+     * @param ResourceConnection $resourceConnection
      * @param ContentIdentityInterfaceFactory $contentIdentityFactory
      * @param UpdateContentAssetLinksInterface $updateContentAssetLinks
      * @param GetEntityContentsInterface $getEntityContents
-     * @param array $fields
      */
     public function __construct(
+        ResourceConnection $resourceConnection,
         ContentIdentityInterfaceFactory $contentIdentityFactory,
         UpdateContentAssetLinksInterface $updateContentAssetLinks,
-        GetEntityContentsInterface $getEntityContents,
-        array $fields = []
+        GetEntityContentsInterface $getEntityContents
     ) {
+        $this->resourceConnection = $resourceConnection;
         $this->contentIdentityFactory = $contentIdentityFactory;
         $this->updateContentAssetLinks = $updateContentAssetLinks;
         $this->getEntityContents = $getEntityContents;
-        $this->fields = $fields;
     }
 
     /**
@@ -78,7 +86,7 @@ class SynchronizeIdentities implements SynchronizeIdentitiesInterface
             if ($identity[self::MEDIA_CONTENT_TYPE] === self::FIELD_CMS_PAGE
                 || $identity[self::MEDIA_CONTENT_TYPE] === self::FIELD_CMS_BLOCK
             ) {
-                $content = (string) $identity[self::MEDIA_CONTENT_FIELD];
+                $content = $this->getCmsMediaContent($identity[self::MEDIA_CONTENT_TYPE], $identity[self::MEDIA_CONTENT_ENTITY_ID]);
             } else {
                 $content = implode(PHP_EOL, $this->getEntityContents->execute($contentIdentity));
             }
@@ -88,5 +96,26 @@ class SynchronizeIdentities implements SynchronizeIdentitiesInterface
                 $content
             );
         }
+    }
+
+    /**
+     * Get cms media content from database
+     *
+     * @param string $tableName
+     * @param string $cmsId
+     * @return string
+     */
+    private function getCmsMediaContent(string $tableName, string $cmsId): string
+    {
+        $connection = $this->resourceConnection->getConnection();
+        $tableName = $this->resourceConnection->getTableName($tableName);
+        $idField = $tableName == self::FIELD_CMS_BLOCK ? $idField = self::ID_CMS_BLOCK : self::ID_CMS_PAGE;
+
+        $select = $connection->select()
+            ->from($tableName, self::COLUMN_CMS_CONTENT)
+            ->where($idField . '= ?', $cmsId);
+        $data = $connection->fetchOne($select);
+
+        return (string)$data;
     }
 }
