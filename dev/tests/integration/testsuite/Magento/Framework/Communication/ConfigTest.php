11ccb291f24c23b6c2602bfcdc44c096ada7e14b
Matias Hidalgo <matias.hidalgo@mediotype.com>
2020-08-21 16:22:41 -0300

    Adding Integration test cases for communication.xml merging process

--- dev/tests/integration/testsuite/Magento/Framework/Communication/ConfigTest.php
+++ dev/tests/integration/testsuite/Magento/Framework/Communication/ConfigTest.php
@@ -17,7 +17,9 @@ class ConfigTest extends \PHPUnit\Framework\TestCase
      */
     public function testGetTopics()
     {
-        $topics = $this->getConfigInstance(__DIR__ . '/_files/valid_communication.xml')->getTopics();
+        $topics = $this->getConfigInstance(
+            [__DIR__ . '/_files/valid_communication.xml', __DIR__ . '/_files/valid_communication_extra.xml']
+        )->getTopics();
         $expectedParsedTopics = include __DIR__ . '/_files/valid_communication_expected.php';
         $this->assertEquals($expectedParsedTopics, $topics);
     }
@@ -31,7 +33,7 @@ class ConfigTest extends \PHPUnit\Framework\TestCase
         $this->expectException(\LogicException::class);
         $this->expectExceptionMessage('Service method specified in the definition of topic "customerDeletedNumbers" is not av');
 
-        $this->getConfigInstance(__DIR__ . '/_files/valid_communication_numeric.xml')->getTopics();
+        $this->getConfigInstance([__DIR__ . '/_files/valid_communication_numeric.xml'])->getTopics();
     }
 
     // @codingStandardsIgnoreStart
@@ -58,7 +60,7 @@ class ConfigTest extends \PHPUnit\Framework\TestCase
         $this->expectException(\Magento\Framework\Exception\LocalizedException::class);
         $this->expectExceptionMessage('The XML in file "0" is invalid:');
 
-        $this->getConfigInstance(__DIR__ . '/_files/invalid_communication_numeric.xml')->getTopics();
+        $this->getConfigInstance([__DIR__ . '/_files/invalid_communication_numeric.xml'])->getTopics();
     }
 
     /**
@@ -66,7 +68,9 @@ class ConfigTest extends \PHPUnit\Framework\TestCase
      */
     public function testGetTopic()
     {
-        $topics = $this->getConfigInstance(__DIR__ . '/_files/valid_communication.xml')->getTopic('customerCreated');
+        $topics = $this->getConfigInstance(
+            [__DIR__ . '/_files/valid_communication.xml', __DIR__ . '/_files/valid_communication_extra.xml']
+        )->getTopic('customerCreated');
         $expectedParsedTopics = include __DIR__ . '/_files/valid_communication_expected.php';
         $this->assertEquals($expectedParsedTopics['customerCreated'], $topics);
     }
@@ -80,7 +84,7 @@ class ConfigTest extends \PHPUnit\Framework\TestCase
         $this->expectException(\Magento\Framework\Exception\LocalizedException::class);
         $this->expectExceptionMessage('Topic "invalidTopic" is not configured.');
 
-        $this->getConfigInstance(__DIR__ . '/_files/valid_communication.xml')->getTopic('invalidTopic');
+        $this->getConfigInstance([__DIR__ . '/_files/valid_communication.xml'])->getTopic('invalidTopic');
     }
 
     /**
@@ -90,7 +94,7 @@ class ConfigTest extends \PHPUnit\Framework\TestCase
         $this->expectException(\LogicException::class);
         $this->expectExceptionMessage('Either "request" or "schema" attribute must be specified for topic "customerUpdated"');
 
-        $this->getConfigInstance(__DIR__ . '/_files/communication_missing_request.xml')->getTopics();
+        $this->getConfigInstance([__DIR__ . '/_files/communication_missing_request.xml'])->getTopics();
     }
 
     /**
@@ -100,7 +104,7 @@ class ConfigTest extends \PHPUnit\Framework\TestCase
         $this->expectException(\LogicException::class);
         $this->expectExceptionMessage('Service method specified in the definition of topic "customerRetrieved" is not');
 
-        $this->getConfigInstance(__DIR__ . '/_files/communication_not_existing_service_method.xml')->getTopics();
+        $this->getConfigInstance([__DIR__ . '/_files/communication_not_existing_service_method.xml'])->getTopics();
     }
 
     /**
@@ -110,7 +114,7 @@ class ConfigTest extends \PHPUnit\Framework\TestCase
         $this->expectException(\LogicException::class);
         $this->expectExceptionMessage('Service method specified in the definition of topic "customerRetrieved" is not');
 
-        $this->getConfigInstance(__DIR__ . '/_files/communication_not_existing_service.xml')->getTopics();
+        $this->getConfigInstance([__DIR__ . '/_files/communication_not_existing_service.xml'])->getTopics();
     }
 
     /**
@@ -120,7 +124,7 @@ class ConfigTest extends \PHPUnit\Framework\TestCase
         $this->expectException(\LogicException::class);
         $this->expectExceptionMessage('Either "request" or "schema" attribute must be specified for topic "customerRetrieved"');
 
-        $this->getConfigInstance(__DIR__ . '/_files/communication_no_attributes.xml')->getTopics();
+        $this->getConfigInstance([__DIR__ . '/_files/communication_no_attributes.xml'])->getTopics();
     }
 
     /**
@@ -130,7 +134,7 @@ class ConfigTest extends \PHPUnit\Framework\TestCase
         $this->expectException(\LogicException::class);
         $this->expectExceptionMessage('Response schema definition for topic "customerUpdated" should reference existing');
 
-        $this->getConfigInstance(__DIR__ . '/_files/communication_response_not_existing_service.xml')->getTopics();
+        $this->getConfigInstance([__DIR__ . '/_files/communication_response_not_existing_service.xml'])->getTopics();
     }
 
     /**
@@ -140,7 +144,7 @@ class ConfigTest extends \PHPUnit\Framework\TestCase
         $this->expectException(\LogicException::class);
         $this->expectExceptionMessage('Request schema definition for topic "customerUpdated" should reference existing');
 
-        $this->getConfigInstance(__DIR__ . '/_files/communication_request_not_existing_service.xml')->getTopics();
+        $this->getConfigInstance([__DIR__ . '/_files/communication_request_not_existing_service.xml'])->getTopics();
     }
 
     /**
@@ -150,7 +154,7 @@ class ConfigTest extends \PHPUnit\Framework\TestCase
         $this->expectException(\LogicException::class);
         $this->expectExceptionMessage('Topic "customerDeleted" is configured for synchronous requests, that is why it must');
 
-        $this->getConfigInstance(__DIR__ . '/_files/communication_multiple_handlers_synchronous_mode.xml')->getTopics();
+        $this->getConfigInstance([__DIR__ . '/_files/communication_multiple_handlers_synchronous_mode.xml'])->getTopics();
     }
 
     /**
@@ -160,7 +164,7 @@ class ConfigTest extends \PHPUnit\Framework\TestCase
         $this->expectException(\LogicException::class);
         $this->expectExceptionMessage('Service method specified in the definition of handler "customHandler" for topic "custo');
 
-        $this->getConfigInstance(__DIR__ . '/_files/communication_not_existing_handler_method.xml')->getTopics();
+        $this->getConfigInstance([__DIR__ . '/_files/communication_not_existing_handler_method.xml'])->getTopics();
     }
 
     /**
@@ -171,7 +175,7 @@ class ConfigTest extends \PHPUnit\Framework\TestCase
         $this->expectExceptionMessage('Topic name "customerAdded" and attribute "name" = "customerCreated" must be equal');
 
         $this->getConfigInstance(
-            __DIR__ . '/_files/valid_communication.xml',
+            [__DIR__ . '/_files/valid_communication.xml'],
             __DIR__ . '/_files/communication_invalid_topic_name.php'
         )->getTopics();
     }
@@ -184,7 +188,7 @@ class ConfigTest extends \PHPUnit\Framework\TestCase
         $this->expectExceptionMessage('Topic "customerCreated" must contain data');
 
         $this->getConfigInstance(
-            __DIR__ . '/_files/valid_communication.xml',
+            [__DIR__ . '/_files/valid_communication.xml'],
             __DIR__ . '/_files/communication_topic_without_data.php'
         )->getTopics();
     }
@@ -197,7 +201,7 @@ class ConfigTest extends \PHPUnit\Framework\TestCase
         $this->expectExceptionMessage('Topic "customerCreated" has missed keys: [response]');
 
         $this->getConfigInstance(
-            __DIR__ . '/_files/valid_communication.xml',
+            [__DIR__ . '/_files/valid_communication.xml'],
             __DIR__ . '/_files/communication_topic_with_missed_keys.php'
         )->getTopics();
     }
@@ -210,7 +214,7 @@ class ConfigTest extends \PHPUnit\Framework\TestCase
         $this->expectExceptionMessage('Topic "customerCreated" has excessive keys: [some_incorrect_key]');
 
         $this->getConfigInstance(
-            __DIR__ . '/_files/valid_communication.xml',
+            [__DIR__ . '/_files/valid_communication.xml'],
             __DIR__ . '/_files/communication_topic_with_excessive_keys.php'
         )->getTopics();
     }
@@ -223,7 +227,7 @@ class ConfigTest extends \PHPUnit\Framework\TestCase
         $this->expectExceptionMessage('Topic name "customerDeleted" and attribute "name" = "customerRemoved" must be equal');
 
         $this->getConfigInstance(
-            __DIR__ . '/_files/valid_communication.xml',
+            [__DIR__ . '/_files/valid_communication.xml'],
             __DIR__ . '/_files/communication_with_non_matched_name.php'
         )->getTopics();
     }
@@ -236,7 +240,7 @@ class ConfigTest extends \PHPUnit\Framework\TestCase
         $this->expectExceptionMessage('Topic "customerDeleted" is configured for synchronous requests, that is why it must');
 
         $this->getConfigInstance(
-            __DIR__ . '/_files/valid_communication.xml',
+            [__DIR__ . '/_files/valid_communication.xml'],
             __DIR__ . '/_files/communication_multiple_handlers_synchronous_mode.php'
         )->getTopics();
     }
@@ -249,7 +253,7 @@ class ConfigTest extends \PHPUnit\Framework\TestCase
         $this->expectExceptionMessage('Request schema definition for topic "customerCreated" should reference existing service');
 
         $this->getConfigInstance(
-            __DIR__ . '/_files/valid_communication.xml',
+            [__DIR__ . '/_files/valid_communication.xml'],
             __DIR__ . '/_files/communication_request_not_existing_service.php'
         )->getTopics();
     }
@@ -262,7 +266,7 @@ class ConfigTest extends \PHPUnit\Framework\TestCase
         $this->expectExceptionMessage('Response schema definition for topic "customerCreated" should reference existing type o');
 
         $this->getConfigInstance(
-            __DIR__ . '/_files/valid_communication.xml',
+            [__DIR__ . '/_files/valid_communication.xml'],
             __DIR__ . '/_files/communication_response_not_existing_service.php'
         )->getTopics();
     }
@@ -275,7 +279,7 @@ class ConfigTest extends \PHPUnit\Framework\TestCase
         $this->expectExceptionMessage('Service method specified in the definition of handler "customerCreatedFirst" for topic');
 
         $this->getConfigInstance(
-            __DIR__ . '/_files/valid_communication.xml',
+            [__DIR__ . '/_files/valid_communication.xml'],
             __DIR__ . '/_files/communication_not_existing_handler_method.php'
         )->getTopics();
     }
@@ -288,7 +292,7 @@ class ConfigTest extends \PHPUnit\Framework\TestCase
         $this->expectExceptionMessage('Disabled handler "default" for topic "customerCreated" cannot be added to the config fi');
 
         $this->getConfigInstance(
-            __DIR__ . '/_files/valid_communication.xml',
+            [__DIR__ . '/_files/valid_communication.xml'],
             __DIR__ . '/_files/communication_with_disabled_handler.php'
         )->getTopics();
     }
@@ -301,7 +305,7 @@ class ConfigTest extends \PHPUnit\Framework\TestCase
         $this->expectExceptionMessage('Request schema type for topic "customerCreated" must be "object_interface" or "service_');
 
         $this->getConfigInstance(
-            __DIR__ . '/_files/valid_communication.xml',
+            [__DIR__ . '/_files/valid_communication.xml'],
             __DIR__ . '/_files/communication_incorrect_request_schema_type.php'
         )->getTopics();
     }
@@ -314,7 +318,7 @@ class ConfigTest extends \PHPUnit\Framework\TestCase
         $this->expectExceptionMessage('The attribute "is_synchronous" for topic "customerCreated" should have the value of the');
 
         $this->getConfigInstance(
-            __DIR__ . '/_files/valid_communication.xml',
+            [__DIR__ . '/_files/valid_communication.xml'],
             __DIR__ . '/_files/communication_is_synchronous_is_not_boolean.php'
         )->getTopics();
     }
@@ -322,16 +326,20 @@ class ConfigTest extends \PHPUnit\Framework\TestCase
     /**
      * Create config instance initialized with configuration from $configFilePath
      *
-     * @param string $configFilePath
+     * @param array $configFilePaths
      * @param string|null $envConfigFilePath
      * @return \Magento\Framework\Communication\ConfigInterface
      */
-    protected function getConfigInstance($configFilePath, $envConfigFilePath = null)
+    protected function getConfigInstance($configFilePaths, $envConfigFilePath = null)
     {
         $fileResolver = $this->getMockForAbstractClass(\Magento\Framework\Config\FileResolverInterface::class);
+        $fileResolverResult = [];
+        foreach ($configFilePaths as $configFilePath) {
+            $fileResolverResult[] = file_get_contents($configFilePath);
+        }
         $fileResolver->expects($this->any())
             ->method('get')
-            ->willReturn([file_get_contents($configFilePath)]);
+            ->willReturn($fileResolverResult);
         $objectManager = \Magento\TestFramework\Helper\Bootstrap::getObjectManager();
         $xmlReader = $objectManager->create(
             \Magento\Framework\Communication\Config\Reader\XmlReader::class,
