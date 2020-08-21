4f978a44e6feb91d99530503df61a3ee16e8d211
Oleh Usik <o.usik@atwix.com>
2020-08-21 17:57:05 +0300

    add some changes to test

--- dev/tests/api-functional/testsuite/Magento/GraphQl/CatalogCustomer/PriceTiersTest.php
+++ dev/tests/api-functional/testsuite/Magento/GraphQl/CatalogCustomer/PriceTiersTest.php
@@ -64,10 +64,12 @@ class PriceTiersTest extends GraphQlAbstract
         );
 
         $itemTiers = $response['products']['items'][0]['price_tiers'];
-        $this->assertCount(3, $itemTiers);
+        $this->assertCount(5, $itemTiers);
         $this->assertEquals(9.25, $this->getValueForQuantity(2, $itemTiers));
         $this->assertEquals(8.25, $this->getValueForQuantity(3, $itemTiers));
         $this->assertEquals(7.25, $this->getValueForQuantity(5, $itemTiers));
+        $this->assertEquals(9.00, $this->getValueForQuantity(7, $itemTiers));
+        $this->assertEquals(7.25, $this->getValueForQuantity(8, $itemTiers));
     }
 
     /**
@@ -95,24 +97,24 @@ class PriceTiersTest extends GraphQlAbstract
         );
 
         $itemTiers = $response['products']['items'][0]['price_tiers'];
-        $this->assertCount(3, $itemTiers);
+        $this->assertCount(5, $itemTiers);
         $this->assertEquals(round(9.25 * $rate, 2), $this->getValueForQuantity(2, $itemTiers));
         $this->assertEquals(round(8.25 * $rate, 2), $this->getValueForQuantity(3, $itemTiers));
         $this->assertEquals(round(7.25 * $rate, 2), $this->getValueForQuantity(5, $itemTiers));
     }
 
     /**
-     * @magentoApiDataFixture Magento/Catalog/_files/simple_product_with_price_tiers_for_multiple_groups.php
+     * @magentoApiDataFixture Magento/Catalog/_files/simple_product_with_tier_prices_for_multiple_groups.php
      */
-    public function testGetLowersPrices()
+    public function testGetLowestPriceForGuest()
     {
         $productSku = 'simple';
         $query = $this->getProductSearchQuery($productSku);
         $response = $this->graphQlQuery($query);
         $itemTiers = $response['products']['items'][0]['price_tiers'];
         $this->assertCount(2, $itemTiers);
-        $this->assertEquals(round(8.25, 2), $this->getValueForQuantity(3, $itemTiers));
-        $this->assertEquals(round(7.25, 2), $this->getValueForQuantity(5, $itemTiers));
+        $this->assertEquals(round(8.25, 2), $this->getValueForQuantity(7, $itemTiers));
+        $this->assertEquals(round(7.25, 2), $this->getValueForQuantity(8, $itemTiers));
     }
 
     /**
