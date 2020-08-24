f794b771c3e1e028519a52222cf299f723fc22ff
Roman Lytvynenko <lytvynen@adobe.com>
2020-08-24 08:11:06 -0500

    MC-36647: Order can be placed as a customer after session cookie expiration with Persistent Cart enabled

--- app/code/Magento/Persistent/Model/Customer/Authorization.php
+++ app/code/Magento/Persistent/Model/Customer/Authorization.php
@@ -6,19 +6,12 @@
 
 namespace Magento\Persistent\Model\Customer;
 
-use Magento\Authorization\Model\UserContextInterface;
 use Magento\Customer\Model\Session as CustomerSession;
 use Magento\Framework\AuthorizationInterface;
-use Magento\Integration\Api\AuthorizationServiceInterface as AuthorizationService;
 use Magento\Persistent\Helper\Session as PersistentSession;
 
 class Authorization implements AuthorizationInterface
 {
-    /**
-     * @var UserContextInterface
-     */
-    private $userContext;
-
     /**
      * @var CustomerSession
      */
@@ -30,16 +23,13 @@ class Authorization implements AuthorizationInterface
     private $persistentSession;
 
     /**
-     * @param UserContextInterface $userContext
      * @param CustomerSession $customerSession
      * @param PersistentSession $persistentSession
      */
     public function __construct(
-        UserContextInterface $userContext,
         CustomerSession $customerSession,
         PersistentSession $persistentSession
     ) {
-        $this->userContext = $userContext;
         $this->customerSession = $customerSession;
         $this->persistentSession = $persistentSession;
     }
@@ -48,12 +38,7 @@ class Authorization implements AuthorizationInterface
         $resource,
         $privilege = null
     ) {
-        if ($this->persistentSession->isPersistent()
-            && $resource == AuthorizationService::PERMISSION_SELF
-            && $this->userContext->getUserId()
-            && $this->userContext->getUserType() === UserContextInterface::USER_TYPE_CUSTOMER
-            && !$this->customerSession->isLoggedIn()
-        ) {
+        if ($this->persistentSession->isPersistent() && !$this->customerSession->isLoggedIn()) {
             return false;
         }
 
