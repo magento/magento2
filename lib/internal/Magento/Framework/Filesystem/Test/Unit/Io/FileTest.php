ed111ae759aa7b924846f9ded51030a25be0082f
Bunyamin <inanbunyamin90@gmail.com>
2020-08-26 10:03:29 +0200

    Copyright added to the new file

--- lib/internal/Magento/Framework/Filesystem/Test/Unit/Io/FileTest.php
+++ lib/internal/Magento/Framework/Filesystem/Test/Unit/Io/FileTest.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
 namespace Magento\Framework\Filesystem\Test\Unit\Io;
 
 use Magento\Framework\Exception\LocalizedException;
