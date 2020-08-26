288455522a5cfd0f2313db4d9666c049b9cf99a1
Viktor Sevch <viktor.sevch@transoftgroup.com>
2020-08-26 12:56:59 +0300

    MC-36759: Widget layout update

--- dev/tests/integration/testsuite/Magento/Widget/_files/cms_page_link_widget_rollback.php
+++ dev/tests/integration/testsuite/Magento/Widget/_files/cms_page_link_widget_rollback.php
@@ -8,8 +8,14 @@ declare(strict_types=1);
 use Magento\TestFramework\Helper\Bootstrap;
 use Magento\Widget\Model\Widget\InstanceFactory;
 use Magento\Widget\Model\Widget\Instance;
+use Magento\Framework\Registry;
 
 $objectManager = Bootstrap::getObjectManager();
+
+/** @var Registry $registry */
+$registry = Bootstrap::getObjectManager()->get(Registry::class);
+$registry->unregister('isSecureArea');
+$registry->register('isSecureArea', true);
 
 /** @var InstanceFactory $widgetModelFactory */
 $widgetModelFactory = $objectManager->get(InstanceFactory::class);
@@ -20,3 +26,6 @@ $widgetModel->load('Test Widget', 'title');
 if ($widgetModel->getId()) {
     $widgetModel->delete();
 }
+
+$registry->unregister('isSecureArea');
+$registry->register('isSecureArea', false);
