79151d69ba9ba8bffc919861f6a7aa650b04079e
yolouiese <honeymay@abovethefray.io>
2020-09-01 02:25:06 +0800

    magento/adobe-stock-integration#1724: Support batches processing for synchronization queue messages - updated publisher integration test file

--- app/code/Magento/MediaContentSynchronization/Test/Integration/Model/PublisherTest.php
+++ app/code/Magento/MediaContentSynchronization/Test/Integration/Model/PublisherTest.php
@@ -9,14 +9,13 @@ namespace Magento\MediaContentSynchronization\Test\Integration\Model;
 
 use Magento\Framework\Exception\IntegrationException;
 use Magento\Framework\Exception\LocalizedException;
-use Magento\Framework\MessageQueue\ConsumerInterface;
+use Magento\Framework\MessageQueue\ConsumerFactory;
 use Magento\MediaContentApi\Api\Data\ContentIdentityInterfaceFactory;
 use Magento\MediaContentApi\Api\GetAssetIdsByContentIdentityInterface;
 use Magento\MediaContentApi\Api\GetContentByAssetIdsInterface;
 use Magento\MediaContentSynchronization\Model\Publish;
 use Magento\TestFramework\Helper\Bootstrap;
 use PHPUnit\Framework\TestCase;
-use Magento\Framework\MessageQueue\ConsumerFactory;
 
 /**
  * Test for media content Publisher
@@ -26,14 +25,14 @@ class PublisherTest extends TestCase
     private const TOPIC_MEDIA_CONTENT_SYNCHRONIZATION = 'media.content.synchronization';
 
     /**
+     * @var ConsumerFactory
+     */
+    private $consumerFactory;
+
+    /**
      * @var ContentIdentityInterfaceFactory
      */
     private $contentIdentityFactory;
-
-    /**
-     * @var GetAssetIdsByContentIdentityInterface
-     */
-    private $getAssetIds;
 
     /**
      * @var GetContentByAssetIdsInterface
@@ -41,28 +40,22 @@ class PublisherTest extends TestCase
     private $getContentIdentities;
 
     /**
+     * @var GetAssetIdsByContentIdentityInterface
+     */
+    private $getAssetIds;
+
+    /**
      * @var Publish
      */
     private $publish;
 
-    /**
-     * @var ConsumerInterface
-     */
-    private $consumer;
-
-    /**
-     * @var ConsumerFactory
-     */
-    private $consumerFactory;
-
     protected function setUp(): void
     {
-        $this->contentIdentityFactory = Bootstrap::getObjectManager()->get(ContentIdentityInterfaceFactory::class);
-        $this->publish = Bootstrap::getObjectManager()->get(Publish::class);
-        $this->getAssetIds = Bootstrap::getObjectManager()->get(GetAssetIdsByContentIdentityInterface::class);
-        $this->getContentIdentities = Bootstrap::getObjectManager()->get(GetContentByAssetIdsInterface::class);
         $this->consumerFactory = Bootstrap::getObjectManager()->get(ConsumerFactory::class);
-        $this->consumer = Bootstrap::getObjectManager()->get(ConsumerInterface::class);
+        $this->contentIdentityFactory = Bootstrap::getObjectManager()->get(ContentIdentityInterfaceFactory::class);
+        $this->getContentIdentities = Bootstrap::getObjectManager()->get(GetContentByAssetIdsInterface::class);
+        $this->getAssetIds = Bootstrap::getObjectManager()->get(GetAssetIdsByContentIdentityInterface::class);
+        $this->publish = Bootstrap::getObjectManager()->get(Publish::class);
     }
 
     /**
@@ -82,25 +75,26 @@ class PublisherTest extends TestCase
         $this->publish->execute($contentIdentities);
 
         // run and process message
-        $consumerName = self::TOPIC_MEDIA_CONTENT_SYNCHRONIZATION;
-        $this->consumerFactory->get(self::TOPIC_MEDIA_CONTENT_SYNCHRONIZATION);
-        $this->consumer->process();
+        $batchSize = 1;
+        $maxNumberOfMessages = 1;
+        $consumer = $this->consumerFactory->get(self::TOPIC_MEDIA_CONTENT_SYNCHRONIZATION, $batchSize);
+        $consumer->process($maxNumberOfMessages);
 
         // verify synchronized media content
         foreach ($contentIdentities as $contentIdentity) {
             $assetId = 2020;
             $contentIdentityObject = $this->contentIdentityFactory->create($contentIdentity);
-            var_dump($this->getAssetIds->execute($contentIdentityObject));
             $this->assertEquals([$assetId], $this->getAssetIds->execute($contentIdentityObject));
             $synchronizedContentIdentities = $this->getContentIdentities->execute([$assetId]);
-            var_dump(count($synchronizedContentIdentities));
             $this->assertEquals(4, count($synchronizedContentIdentities));
 
             $syncedIds = [];
             foreach ($synchronizedContentIdentities as $syncedContentIdentity) {
                 $syncedIds[] = $syncedContentIdentity->getEntityId();
             }
-            $this->assertContains($contentIdentity->getEntityId(), $syncedIds);
+            foreach ($contentIdentityObject as $identityObject) {
+                $this->assertContains($identityObject->getEntityId(), $syncedIds);
+            }
         }
     }
 
