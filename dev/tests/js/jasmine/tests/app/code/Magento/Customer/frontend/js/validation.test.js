269089f9b6be72fa146c86452b618efc37e4fcfc
Roman Lytvynenko <lytvynen@adobe.com>
2020-08-31 17:43:47 -0500

    MC-36978: Invalid Character Customer Account Create DOB

--- dev/tests/js/jasmine/tests/app/code/Magento/Customer/frontend/js/validation.test.js
+++ dev/tests/js/jasmine/tests/app/code/Magento/Customer/frontend/js/validation.test.js
@@ -10,140 +10,30 @@ define([
 ], function ($) {
     'use strict';
 
-    describe('Testing Customer/view/frontend/web/js/validation.js', function () {
+    describe('Testing customer DOB validation to tolerate zeroes in the single digit dates', function () {
         var params,
             dataProvider;
 
         dataProvider = [
             {
-                format: 'dd.MM.yy.',
-                date: '09.02.18.',
-                expects: true
-            },
-            {
-                format: 'd/MM/y',
-                date: '9/02/2018',
-                expects: true
-            },
-            {
-                format: 'MM/dd/yy',
-                date: '02/09/18',
-                expects: true
-            },
-            {
-                format: 'M/d/yy',
-                date: '2/9/18',
-                expects: true
-            },
-            {
-                format: 'yy-MM-dd',
-                date: '18-02-09',
-                expects: true
-            },
-            {
-                format: 'dd.MM.y.',
-                date: '09.02.2018.',
-                expects: true
-            },
-            {
-                format: 'y. MM. dd.',
-                date: '2018. 02. 09.',
-                expects: true
-            },
-            {
-                format: 'd/MM/yy',
-                date: '9/02/18',
-                expects: true
-            },
-            {
-                format: 'dd-MM-yy',
-                date: '09-02-18',
-                expects: true
-            },
-            {
-                format: 'dd/MM/yy',
-                date: '09/02/18',
-                expects: true
-            },
-            {
-                format: 'dd.MM.y',
-                date: '09.02.2018',
-                expects: true
-            },
-            {
-                format: 'd. MM. yy',
-                date: '9. 02. 18',
-                expects: true
-            },
-            {
-                format: 'dd/MM/y',
-                date: '09/02/2018',
-                expects: true
-            },
-            {
-                format: 'd.MM.y',
-                date: '9.02.2018',
-                expects: true
-            },
-            {
-                format: 'd.M.yy',
-                date: '9.2.18',
-                expects: true
-            },
-            {
-                format: 'd.MM.yy г.',
-                date: '9.02.18 г.',
-                expects: true
-            },
-            {
-                format: 'dd.M.yy',
-                date: '09.2.18',
-                expects: true
-            },
-            {
-                format: 'y-MM-dd',
-                date: '2018-02-09',
-                expects: true
-            },
-            {
-                format: 'd.M.yy.',
-                date: '9.2.18.',
-                expects: true
-            },
-            {
-                format: 'd.M.y',
-                date: '9.2.2018',
-                expects: true
-            },
-            {
-                format: 'd/M/y',
-                date: '9/2/2018',
-                expects: true
-            },
-            {
-                format: 'yy/M/d',
-                date: '19/2/9',
-                expects: true
-            },
-            {
-                format: 'd/M/yy',
+                format: 'M/d/Y',
                 date: '9/2/18',
                 expects: true
             },
             {
-                format: 'y/M/d',
-                date: '2018/2/9',
+                format: 'M/DD/Y',
+                date: '9/2/18',
+                expects: false
+            },
+            {
+                format: 'MM/DD/Y',
+                date: '09/02/18',
                 expects: true
             },
             {
-                format: 'y/MM/dd',
-                date: '2018/02/09',
-                expects: true
-            },
-            {
-                format: 'yy. M. d.',
-                date: '18. 2. 9.',
-                expects: true
+                format: 'MM/DD/YYYY',
+                date: '09/2/18',
+                expects: false
             }
         ];
 
