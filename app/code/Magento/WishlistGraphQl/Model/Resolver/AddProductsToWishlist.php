30aebc39fb59cc76456e48198ee285c1b86f4e96
Prabhu Ram <pganapat@adobe.com>
2020-09-02 10:52:16 -0500

    MC-37250: [GraphQl] Change exception message when wishlist is not enabled - updated exception messages

--- app/code/Magento/WishlistGraphQl/Model/Resolver/AddProductsToWishlist.php
+++ app/code/Magento/WishlistGraphQl/Model/Resolver/AddProductsToWishlist.php
@@ -97,7 +97,7 @@ class AddProductsToWishlist implements ResolverInterface
         $wishlist = $this->getWishlist($wishlistId, $customerId);
 
         if (null === $wishlist->getId() || $customerId !== (int) $wishlist->getCustomerId()) {
-            throw new GraphQlInputException(__('The wishlist configuration is currently disabled.'));
+            throw new GraphQlInputException(__('The wishlist was not found.'));
         }
 
         $wishlistItems = $this->getWishlistItems($args['wishlistItems']);
