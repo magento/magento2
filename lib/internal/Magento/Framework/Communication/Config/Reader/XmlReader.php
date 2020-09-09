e7cde72591b81895e8b02e1de3e8f88a4ede80b1
Pavel Bystritsky <engcom-vendorworker-foxtrot@adobe.com>
2020-09-09 13:14:54 +0300

    magento/magento2#29528: Fix for communication.xml Handlers merging processs.

--- lib/internal/Magento/Framework/Communication/Config/Reader/XmlReader.php
+++ lib/internal/Magento/Framework/Communication/Config/Reader/XmlReader.php
@@ -5,6 +5,10 @@
  */
 namespace Magento\Framework\Communication\Config\Reader;
 
+use Magento\Framework\Config\Dom;
+use Magento\Framework\Config\FileResolverInterface;
+use Magento\Framework\Config\ValidationStateInterface;
+
 /**
  * Communication configuration filesystem reader. Reads data from XML configs.
  */
@@ -19,4 +23,38 @@ class XmlReader extends \Magento\Framework\Config\Reader\Filesystem
         '/config/topic' => 'name',
         '/config/topic/handler' => 'name'
     ];
+
+    /**
+     * @param FileResolverInterface $fileResolver
+     * @param XmlReader\Converter $converter
+     * @param XmlReader\SchemaLocator $schemaLocator
+     * @param ValidationStateInterface $validationState
+     * @param string $fileName
+     * @param array $idAttributes
+     * @param string $domDocumentClass
+     * @param string $defaultScope
+     * phpcs:disable Generic.CodeAnalysis.UselessOverridingMethod
+     */
+    public function __construct(
+        FileResolverInterface $fileResolver,
+        XmlReader\Converter $converter,
+        XmlReader\SchemaLocator $schemaLocator,
+        ValidationStateInterface $validationState,
+        $fileName = 'communication.xml',
+        $idAttributes = [],
+        $domDocumentClass = Dom::class,
+        $defaultScope = 'global'
+    ) {
+        parent::__construct(
+            $fileResolver,
+            $converter,
+            $schemaLocator,
+            $validationState,
+            $fileName,
+            $idAttributes,
+            $domDocumentClass,
+            $defaultScope
+        );
+    }
+    //phpcs:enable Generic.CodeAnalysis.UselessOverridingMethod
 }
