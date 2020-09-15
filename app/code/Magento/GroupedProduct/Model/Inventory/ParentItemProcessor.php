91a7251bc50a597581b2533d763360bdfdc33d28
rostyslav.hymon <rostyslav.hymon@transoftgroup.com>
2020-09-15 11:41:16 +0300

    MC-37249: Grouped product remains In Stock on product edit page

--- app/code/Magento/GroupedProduct/Model/Inventory/ParentItemProcessor.php
+++ app/code/Magento/GroupedProduct/Model/Inventory/ParentItemProcessor.php
@@ -7,6 +7,8 @@ declare(strict_types=1);
 
 namespace Magento\GroupedProduct\Model\Inventory;
 
+use Magento\Catalog\Api\Data\ProductInterface;
+use Magento\Framework\EntityManager\MetadataPool;
 use Magento\GroupedProduct\Model\Product\Type\Grouped;
 use Magento\Catalog\Api\Data\ProductInterface as Product;
 use Magento\CatalogInventory\Api\StockItemCriteriaInterfaceFactory;
@@ -14,6 +16,8 @@ use Magento\CatalogInventory\Api\StockItemRepositoryInterface;
 use Magento\CatalogInventory\Api\StockConfigurationInterface;
 use Magento\CatalogInventory\Observer\ParentItemProcessorInterface;
 use Magento\CatalogInventory\Api\Data\StockItemInterface;
+use Magento\GroupedProduct\Model\ResourceModel\Product\Link;
+use Magento\Framework\App\ResourceConnection;
 
 /**
  * Process parent stock item for grouped product
@@ -41,21 +45,39 @@ class ParentItemProcessor implements ParentItemProcessorInterface
     private $criteriaInterfaceFactory;
 
     /**
+     * Product metadata pool
+     *
+     * @var MetadataPool
+     */
+    private $metadataPool;
+
+    /**
+     * @var ResourceConnection
+     */
+    private $resource;
+
+    /**
      * @param Grouped $groupedType
      * @param StockItemCriteriaInterfaceFactory $criteriaInterfaceFactory
      * @param StockItemRepositoryInterface $stockItemRepository
      * @param StockConfigurationInterface $stockConfiguration
+     * @param ResourceConnection $resource
+     * @param MetadataPool $metadataPool
      */
     public function __construct(
         Grouped $groupedType,
         StockItemCriteriaInterfaceFactory $criteriaInterfaceFactory,
         StockItemRepositoryInterface $stockItemRepository,
-        StockConfigurationInterface $stockConfiguration
+        StockConfigurationInterface $stockConfiguration,
+        ResourceConnection $resource,
+        MetadataPool $metadataPool
     ) {
         $this->groupedType = $groupedType;
         $this->criteriaInterfaceFactory = $criteriaInterfaceFactory;
         $this->stockConfiguration = $stockConfiguration;
         $this->stockItemRepository = $stockItemRepository;
+        $this->resource = $resource;
+        $this->metadataPool = $metadataPool;
     }
 
     /**
@@ -66,7 +88,7 @@ class ParentItemProcessor implements ParentItemProcessorInterface
      */
     public function process(Product $product)
     {
-        $parentIds = $this->groupedType->getParentIdsByChild($product->getId());
+        $parentIds = $this->getParentEntityIdsByChild($product->getId());
         foreach ($parentIds as $productId) {
             $this->processStockForParent((int)$productId);
         }
@@ -121,5 +143,31 @@ class ParentItemProcessor implements ParentItemProcessorInterface
     {
         return $parentStockItem->getIsInStock() !== $childrenIsInStock &&
             ($childrenIsInStock === false || $parentStockItem->getStockStatusChangedAuto());
+    }
+
+    /**
+     * Retrieve parent ids array by child id
+     *
+     * @param int $childId
+     * @return string[]
+     */
+    private function getParentEntityIdsByChild($childId)
+    {
+        $select = $this->resource->getConnection()
+            ->select()
+            ->from(['l' => $this->resource->getTableName('catalog_product_link')], [])
+            ->join(
+                ['e' => $this->resource->getTableName('catalog_product_entity')],
+                'e.' .
+                $this->metadataPool->getMetadata(ProductInterface::class)->getLinkField() . ' = l.product_id',
+                ['e.entity_id']
+            )
+            ->where('l.linked_product_id = ?', $childId)
+            ->where(
+                'link_type_id = ?',
+                Link::LINK_TYPE_GROUPED
+            );
+
+        return $this->resource->getConnection()->fetchCol($select);
     }
 }
