e7e156cb426737e0bc50a8032853407641cb0b7e
Dmytro Horytskyi <dhorytskyi@magento.com>
2020-09-15 20:22:24 -0500

    MC-36785: Unable to set YouTube API key by CLI

--- app/code/Magento/Config/App/Config/Source/ModularConfigSource.php
+++ app/code/Magento/Config/App/Config/Source/ModularConfigSource.php
@@ -5,9 +5,11 @@
  */
 namespace Magento\Config\App\Config\Source;
 
+use Magento\Config\Model\Config\Structure\Reader as ConfigStructureReader;
+use Magento\Framework\App\Area;
 use Magento\Framework\App\Config\ConfigSourceInterface;
 use Magento\Framework\DataObject;
-use Magento\Framework\App\Config\Initial\Reader;
+use Magento\Framework\App\Config\Initial\Reader as InitialConfigReader;
 
 /**
  * Class for retrieving initial configuration from modules
@@ -18,16 +20,25 @@ use Magento\Framework\App\Config\Initial\Reader;
 class ModularConfigSource implements ConfigSourceInterface
 {
     /**
-     * @var Reader
+     * @var InitialConfigReader
      */
-    private $reader;
+    private $initialConfigReader;
 
     /**
-     * @param Reader $reader
+     * @var ConfigStructureReader
      */
-    public function __construct(Reader $reader)
-    {
-        $this->reader = $reader;
+    private $configStructureReader;
+
+    /**
+     * @param InitialConfigReader $initialConfigReader
+     * @param ConfigStructureReader $configStructureReader
+     */
+    public function __construct(
+        InitialConfigReader $initialConfigReader,
+        ConfigStructureReader $configStructureReader
+    ) {
+        $this->initialConfigReader = $initialConfigReader;
+        $this->configStructureReader = $configStructureReader;
     }
 
     /**
@@ -39,10 +50,51 @@ class ModularConfigSource implements ConfigSourceInterface
      */
     public function get($path = '')
     {
-        $data = new DataObject($this->reader->read());
+        $initialConfig = $this->initialConfigReader->read();
+        $configStructure = $this->configStructureReader->read(Area::AREA_ADMINHTML);
+        $sections = $configStructure['config']['system']['sections'] ?? [];
+        $defaultConfig = $initialConfig['data']['default'] ?? [];
+        $initialConfig['data']['default'] = $this->merge($defaultConfig, $sections);
+
+        $data = new DataObject($initialConfig);
         if ($path !== '') {
             $path = '/' . $path;
         }
         return $data->getData('data' . $path) ?: [];
+    }
+
+    private function addPathKey(array $data, string $path): array
+    {
+        if (strpos($path, '/') !== false) {
+            list ($key, $subPath) = explode('/', $path, 2);
+            $data[$key] = $this->addPathKey($data[$key] ?? [], $subPath);
+        } else {
+            $data += [$path => null];
+        }
+
+        return $data;
+    }
+
+    /**
+     * Merge initial config with config structure
+     *
+     * @param array $config
+     * @param array $sections
+     * @return array
+     */
+    private function merge(array $config, array $sections): array
+    {
+        foreach ($sections as $key => $section) {
+            if (isset($section['children'])) {
+                $config[$section['id']] = $this->merge(
+                    $config[$section['id']] ?? [],
+                    $section['children']
+                );
+            } elseif ($section['_elementType'] === 'field') {
+                $config += [$section['id'] => null];
+            }
+        }
+
+        return $config;
     }
 }
