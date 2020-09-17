5e93afcfa594f60c969578b314c282d4ff891077
Serhii Kovalenko <ganster3012@gmail.com>
2020-09-17 18:31:16 +0300

    Mview patch update -- move framework logic to saas-export

--- lib/internal/Magento/Framework/Mview/Config/Converter.php
+++ lib/internal/Magento/Framework/Mview/Config/Converter.php
@@ -52,7 +52,6 @@ class Converter implements \Magento\Framework\Config\ConverterInterface
             $data['view_id'] = $viewId;
             $data['action_class'] = $this->getAttributeValue($viewNode, 'class');
             $data['group'] = $this->getAttributeValue($viewNode, 'group');
-            $data['store_scope'] = $this->getAttributeValue($viewNode, 'store_scope');
             $data['iterator'] = $this->getAttributeValue($viewNode, 'iterator') ?: $this->defaultIterator;
             $data['subscriptions'] = [];
 
