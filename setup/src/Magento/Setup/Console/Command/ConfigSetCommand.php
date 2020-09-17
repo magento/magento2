5c23f4d24d6ff9c95f50ba3378498676ba548eb4
Ihor Sviziev <ihor-sviziev@users.noreply.github.com>
2020-09-17 09:36:41 +0300

    Fix static test

--- setup/src/Magento/Setup/Console/Command/ConfigSetCommand.php
+++ setup/src/Magento/Setup/Console/Command/ConfigSetCommand.php
@@ -14,9 +14,6 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\Question;
 
-/**
- * Config Set Command
- */
 class ConfigSetCommand extends AbstractSetupCommand
 {
     /**
