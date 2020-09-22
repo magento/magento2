60d0fbd369314a7e54fdb3eefffed4bc1226e322
yolouiese <honeymay@abovethefray.io>
2020-09-22 17:19:49 +0800

    magento/adobe-stock-integration#1843: Media Gallery grid is scrolled down when text doesn't pass the validation on Edit Image page - override validation script to remove scroll feature

---
+++ app/code/Magento/MediaGalleryUi/view/adminhtml/requirejs-config.js
@@ -0,0 +1,14 @@
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+var config = {
+    config: {
+        mixins: {
+            'mage/validation': {
+                'Magento_MediaGalleryUi/js/lib/mage/validation-mixin': true
+            }
+        }
+    }
+};
