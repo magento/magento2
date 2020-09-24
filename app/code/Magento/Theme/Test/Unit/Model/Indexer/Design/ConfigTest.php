9327c28eb7b54db7b48fa2759484cfd049559e0d
Roman Flowers <flowers@adobe.com>
2020-09-24 13:42:03 -0500

    MC-37630: Updating Design Configuration runs full reindex which generates DDL statement DROP Table

--- app/code/Magento/Theme/Test/Unit/Model/Indexer/Design/ConfigTest.php
+++ app/code/Magento/Theme/Test/Unit/Model/Indexer/Design/ConfigTest.php
@@ -7,6 +7,8 @@ declare(strict_types=1);
 
 /**
  * Test design config indexer model
+ *
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
  */
 namespace Magento\Theme\Test\Unit\Model\Indexer\Design;
 
