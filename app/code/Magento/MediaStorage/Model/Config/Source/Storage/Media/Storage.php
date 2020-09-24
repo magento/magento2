2df958078201ae6098277fe4ad8f6dd38515c163
Breno Fábio <3138323+brenofabio@users.noreply.github.com>
2020-09-24 12:10:45 -0300

    Corrects coding standard
    Split option array into multiple lines.

--- app/code/Magento/MediaStorage/Model/Config/Source/Storage/Media/Storage.php
+++ app/code/Magento/MediaStorage/Model/Config/Source/Storage/Media/Storage.php
@@ -23,7 +23,10 @@ class Storage implements \Magento\Framework\Option\ArrayInterface
                 'value' => \Magento\MediaStorage\Model\File\Storage::STORAGE_MEDIA_FILE_SYSTEM,
                 'label' => __('File System'),
             ],
-            ['value' => \Magento\MediaStorage\Model\File\Storage::STORAGE_MEDIA_DATABASE, 'label' => __('Database')]
+            [
+                'value' => \Magento\MediaStorage\Model\File\Storage::STORAGE_MEDIA_DATABASE,
+                'label' => __('Database')
+            ]
         ];
     }
 }
