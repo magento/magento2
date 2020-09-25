ba50f2122e04475f60285963db64bd92011729d1
Joan He <johe@adobe.com>
2020-09-25 15:06:42 -0500

    MC-37483: Refactored Login model in LoginAsCustomer Extension

---
+++ app/code/Magento/LoginAsCustomerApi/Api/GenerateAuthenticationSecretInterface.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\LoginAsCustomerApi\Api;
+
+use Magento\LoginAsCustomerApi\Api\Data\AuthenticationDataInterface;
+
+/**
+ * Generate authentication secret
+ * @api
+ */
+interface GenerateAuthenticationSecretInterface
+{
+    /**
+     * Generate authentication secret
+     *
+     * @param AuthenticationDataInterface $authenticationData
+     * @return string authentication secret
+     */
+    public function execute(AuthenticationDataInterface $authenticationData): string;
+}
