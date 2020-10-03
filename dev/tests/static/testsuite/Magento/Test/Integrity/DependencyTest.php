2d1d04bc095fc0db4a41503b48f7d3462a55fc65
oleksandrkravchuk <oleksandr.kravchuk@vaimo.com>
2020-10-03 23:28:21 +0200

    Do not skip dependency test in vendor.

--- dev/tests/static/testsuite/Magento/Test/Integrity/DependencyTest.php
+++ dev/tests/static/testsuite/Magento/Test/Integrity/DependencyTest.php
@@ -188,15 +188,6 @@ class DependencyTest extends \PHPUnit\Framework\TestCase
      */
     public static function setUpBeforeClass(): void
     {
-        $root = BP;
-        $rootJson = json_decode(file_get_contents($root . '/composer.json'), true);
-        if (preg_match('/magento\/project-*/', $rootJson['name']) == 1) {
-            // The Dependency test is skipped for vendor/magento build
-            self::markTestSkipped(
-                'MAGETWO-43654: The build is running from vendor/magento. DependencyTest is skipped.'
-            );
-        }
-
         self::$routeMapper = new RouteMapper();
         self::$_namespaces = implode('|', Files::init()->getNamespaces());
 
