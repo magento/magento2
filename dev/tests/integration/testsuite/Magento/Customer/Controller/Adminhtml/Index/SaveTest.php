e15188899de53d1794b094eb37dad4995a16af0c
IvanPletnyov <ivan.pletnyov@transoftgroup.com>
2020-10-07 10:34:45 +0300

    MC-37100: Create automated test for "Create customer, when admin interface locale is en_GB"

--- dev/tests/integration/testsuite/Magento/Customer/Controller/Adminhtml/Index/SaveTest.php
+++ dev/tests/integration/testsuite/Magento/Customer/Controller/Adminhtml/Index/SaveTest.php
@@ -10,10 +10,12 @@ namespace Magento\Customer\Controller\Adminhtml\Index;
 use Magento\Backend\Model\Session;
 use Magento\Customer\Api\CustomerNameGenerationInterface;
 use Magento\Customer\Api\CustomerRepositoryInterface;
+use Magento\Customer\Api\Data\CustomerInterface;
 use Magento\Customer\Model\Data\Customer as CustomerData;
 use Magento\Customer\Model\EmailNotification;
 use Magento\Framework\App\Area;
 use Magento\Framework\App\Request\Http as HttpRequest;
+use Magento\Framework\Locale\ResolverInterface;
 use Magento\Framework\Mail\Template\TransportBuilder;
 use Magento\Framework\Mail\TransportInterface;
 use Magento\Framework\Message\MessageInterface;
@@ -54,6 +56,12 @@ class SaveTest extends AbstractBackendController
     /** @var StoreManagerInterface */
     private $storeManager;
 
+    /** @var ResolverInterface */
+    private $localeResolver;
+
+    /** @var CustomerInterface */
+    private $customer;
+
     /**
      * @inheritdoc
      */
@@ -65,6 +73,19 @@ class SaveTest extends AbstractBackendController
         $this->subscriberFactory = $this->_objectManager->get(SubscriberFactory::class);
         $this->session = $this->_objectManager->get(Session::class);
         $this->storeManager = $this->_objectManager->get(StoreManagerInterface::class);
+        $this->localeResolver = $this->_objectManager->get(ResolverInterface::class);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function tearDown(): void
+    {
+        if ($this->customer instanceof CustomerInterface) {
+            $this->customerRepository->delete($this->customer);
+        }
+
+        parent::tearDown();
     }
 
     /**
@@ -419,6 +440,43 @@ class SaveTest extends AbstractBackendController
     }
 
     /**
+     * @return void
+     */
+    public function testCreateCustomerByAdminWithLocaleGB(): void
+    {
+        $this->localeResolver->setLocale('en_GB');
+        $postData = array_replace_recursive(
+            $this->getDefaultCustomerData(),
+            [
+                'customer' => [
+                    CustomerData::DOB => '24/10/1990',
+                ],
+            ]
+        );
+        $expectedData = array_replace_recursive(
+            $postData,
+            [
+                'customer' => [
+                    CustomerData::DOB => '1990-10-24',
+                    CustomerData::GENDER => '0',
+                ],
+            ]
+        );
+        unset($expectedData['customer']['sendemail_store_id']);
+        $this->dispatchCustomerSave($postData);
+        $this->assertSessionMessages(
+            $this->equalTo([(string)__('You saved the customer.')]),
+            MessageInterface::TYPE_SUCCESS
+        );
+        $this->assertRedirect($this->stringContains($this->baseControllerUrl . 'index/key/'));
+        $this->assertCustomerData(
+            $postData['customer'][CustomerData::EMAIL],
+            (int)$postData['customer'][CustomerData::WEBSITE_ID],
+            $expectedData
+        );
+    }
+
+    /**
      * Default values for customer creation
      *
      * @return array
@@ -496,9 +554,8 @@ class SaveTest extends AbstractBackendController
         int $customerWebsiteId,
         array $expectedData
     ): void {
-        /** @var CustomerData $customerData */
-        $customerData = $this->customerRepository->get($customerEmail, $customerWebsiteId);
-        $actualCustomerArray = $customerData->__toArray();
+        $this->customer = $this->customerRepository->get($customerEmail, $customerWebsiteId);
+        $actualCustomerArray = $this->customer->__toArray();
         foreach ($expectedData['customer'] as $key => $expectedValue) {
             $this->assertEquals(
                 $expectedValue,
