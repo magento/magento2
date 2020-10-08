1c1e70e1f96e9e027b667a9c9fc4cb7aca78dc19
Sudheer S <sudheers@kensium.com>
2020-10-08 16:50:34 +0530

    30349: Product filter with category_id does not work as expected - fixed total_count and items issues

--- app/code/Magento/CatalogGraphQl/Model/Resolver/Products/SearchCriteria/CollectionProcessor/FilterProcessor/CategoryFilter.php
+++ app/code/Magento/CatalogGraphQl/Model/Resolver/Products/SearchCriteria/CollectionProcessor/FilterProcessor/CategoryFilter.php
@@ -61,7 +61,6 @@ class CategoryFilter implements CustomFilterInterface
             $category = $this->categoryFactory->create();
             $this->categoryResourceModel->load($category, $categoryId);
             $categoryProducts[$categoryId] = $category->getProductCollection()->getAllIds();
-            $collection->addCategoryFilter($category);
         }
 
         $categoryProductIds = array_unique(array_merge(...$categoryProducts));
