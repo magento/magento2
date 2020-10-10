440a4cf6c465fab862706b4faed460e9dd7f7a29
Yash Bhayani <72012678+Yash-Bhayani@users.noreply.github.com>
2020-10-10 17:05:50 +0530

    Update BackButton.php

--- app/code/Magento/Customer/Block/Adminhtml/Edit/BackButton.php
+++ app/code/Magento/Customer/Block/Adminhtml/Edit/BackButton.php
@@ -13,6 +13,26 @@ use Magento\Framework\View\Element\UiComponent\Control\ButtonProviderInterface;
 class BackButton extends GenericButton implements ButtonProviderInterface
 {
     /**
+     * @var Form
+     */
+    private $form;
+    /**
+     * @var \Magento\Framework\Url\DecoderInterface
+     */
+    private $decoder;
+
+    public function __construct(
+        \Magento\Framework\Url\DecoderInterface $decoder,
+        \Magento\Customer\Block\Adminhtml\Edit\Form $form,
+        \Magento\Backend\Block\Widget\Context $context,
+        \Magento\Framework\Registry $registry
+    ) {
+        parent::__construct($context, $registry);
+        $this->form = $form;
+        $this->decoder = $decoder;
+    }
+
+    /**
      * @return array
      */
     public function getButtonData()
@@ -32,6 +52,12 @@ class BackButton extends GenericButton implements ButtonProviderInterface
      */
     public function getBackUrl()
     {
+        if ($fromPath=$this->decoder->decode($this->form->getRequest()->getParam('fromPath'))) {
+            return $this->getUrl(
+                $fromPath,
+                ['id' => $this->form->getRequest()->getParam('review_id')]
+            );
+        }
         return $this->getUrl('*/*/');
     }
 }
