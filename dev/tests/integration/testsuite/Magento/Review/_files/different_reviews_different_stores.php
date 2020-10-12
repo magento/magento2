5030f722876663e06c4fd55f75f2684b1dbebbcb
huykon225 <huykon225@gmail.com>
2020-10-12 09:48:29 +0700

    load testsuite store code

--- dev/tests/integration/testsuite/Magento/Review/_files/different_reviews_different_stores.php
+++ dev/tests/integration/testsuite/Magento/Review/_files/different_reviews_different_stores.php
@@ -8,7 +8,7 @@ use Magento\TestFramework\Workaround\Override\Fixture\Resolver;
 Resolver::getInstance()->requireDataFixture('Magento/Catalog/_files/product_simple.php');
 $store = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()
                                                 ->create(\Magento\Store\Model\Store::class);
-$secondStoreId = $store->load('fixture_second_store')->getId();
+$secondStoreId = $store->load('fixture_second_store', 'code')->getId();
 
 $review = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create(
     \Magento\Review\Model\Review::class,
