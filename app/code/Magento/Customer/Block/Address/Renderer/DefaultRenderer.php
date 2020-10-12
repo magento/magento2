94f9919d179a4a83522f5cc3354b42e2d2120eec
Your Name <mmdhudasia@gmail.com>
2020-10-12 17:44:05 +0530

    added indentation

--- app/code/Magento/Customer/Block/Address/Renderer/DefaultRenderer.php
+++ app/code/Magento/Customer/Block/Address/Renderer/DefaultRenderer.php
@@ -189,9 +189,8 @@ class DefaultRenderer extends AbstractBlock implements RendererInterface
                         $data[$key] = $v;
                     }
                 }
-                if(in_array($attributeCode,['prefix','suffix'])) {
-                  $value = __($value);
-                }
+                if (in_array($attributeCode, ['prefix','suffix']))
+                    $value = __($value);
                 $data[$attributeCode] = $value;
             }
         }
