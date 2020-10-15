6b39ef29532e070857bae7172daf444199db8dc2
Serhiy Yelahin <serhiy.yelahin@transoftgroup.com>
2020-10-15 09:38:03 +0300

    MC-38269: [CLARIFICATION] [Magento Cloud] - Persistent Shopping cart Header Weelcome & Not you?

--- app/code/Magento/Persistent/view/frontend/web/js/view/additional-welcome.js
+++ app/code/Magento/Persistent/view/frontend/web/js/view/additional-welcome.js
@@ -40,7 +40,7 @@ define([
 
                         $(this).attr('data-bind', html);
                         $(this).html(html);
-                        $(this).after('<span><a ' + window.notYouLink + '>' + $t('Not you?') + '</a></span>');
+                        $(this).after(' <span><a ' + window.notYouLink + '>' + $t('Not you?') + '</a></span>');
                     });
                 }
             }
