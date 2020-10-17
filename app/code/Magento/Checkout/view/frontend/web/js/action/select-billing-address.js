a3dbffc761c5b394630bceef1d74988daa937c51
Dan Wallis <mrdanwallis@gmail.com>
2020-10-17 19:25:44 +0100

    Set address only if different

--- app/code/Magento/Checkout/view/frontend/web/js/action/select-billing-address.js
+++ app/code/Magento/Checkout/view/frontend/web/js/action/select-billing-address.js
@@ -23,6 +23,29 @@ define([
         } else {
             address = billingAddress;
         }
+
+        if (addressesAreEqual(address, quote.billingAddress())) {
+            return;
+        }
+
         quote.billingAddress(address);
     };
+
+    function addressesAreEqual (a, b) {
+        if (a === b) {
+            return true;
+        }
+        if (a == null || b == null) {
+            return false;
+        }
+
+        return JSON.stringify(sortAddress(a)) == JSON.stringify(sortAddress(b));
+    }
+
+    function sortAddress (address) {
+        return Object.keys(address).sort().reduce(function (result, key) {
+            result[key] = address[key];
+            return result;
+        }, {});
+    }
 });
