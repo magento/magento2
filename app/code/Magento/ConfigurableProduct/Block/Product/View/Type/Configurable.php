7c689ec37d0ca9d6d8d23d4f92d0164248b1ab10
Lukasz Bajsarowicz <lukasz.bajsarowicz@gmail.com>
2020-10-30 01:04:56 +0100

    Developer Experience: Make `getAdditionalConfig` extensible

--- app/code/Magento/ConfigurableProduct/Block/Product/View/Type/Configurable.php
+++ app/code/Magento/ConfigurableProduct/Block/Product/View/Type/Configurable.php
@@ -206,12 +206,24 @@ class Configurable extends \Magento\Catalog\Block\Product\View\AbstractView
 
     /**
      * Returns additional values for js config, con be overridden by descendants
+     * @deprecared Use composition approach to extend the Configuration (plugin for getAdditionalerConfig method)
+     * @see getAdditionalConfig to apply `after` plugin and extend an array
      *
      * @return array
      */
     protected function _getAdditionalConfig()
     {
         return [];
+    }
+
+    /**
+     * Returns additional values for js config, con be overridden by descendants
+     *
+     * @return array
+     */
+    public function getAdditionalConfig()
+    {
+        return $this->_getAdditionalConfig();
     }
 
     /**
@@ -244,7 +256,7 @@ class Configurable extends \Magento\Catalog\Block\Product\View\AbstractView
             $config['defaultValues'] = $attributesData['defaultValues'];
         }
 
-        $config = array_merge($config, $this->_getAdditionalConfig());
+        $config = array_merge($config, $this->getAdditionalConfig());
 
         return $this->jsonEncoder->encode($config);
     }
