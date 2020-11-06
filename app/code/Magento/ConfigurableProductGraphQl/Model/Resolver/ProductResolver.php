7ca4158ecac89a1c1a7f4829a10fed637897f3b7
Shikha Mishra <mshikha1103@gmail.com>
2020-11-06 18:44:07 +0530

    #28573:ConfigurableCartItem added to Cart returns wrong thumbnail
    Added configured_variant field to the ConfigurableCartItem

---
+++ app/code/Magento/ConfigurableProductGraphQl/Model/Resolver/ProductResolver.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\ConfigurableProductGraphQl\Model\Resolver;
+
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Magento\Catalog\Api\ProductRepositoryInterface;
+
+/**
+ * Fetches the Product data according to the GraphQL schema
+ */
+class ProductResolver implements ResolverInterface
+{
+    /**
+     * @var ProductRepositoryInterface
+     */
+    private $productRepository;
+
+    /**
+     * @param ProductRepositoryInterface $productRepository
+     */
+    public function __construct(ProductRepositoryInterface $productRepository)
+    {
+        $this->productRepository = $productRepository;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function resolve(
+        Field $field,
+        $context,
+        ResolveInfo $info,
+        array $value = null,
+        array $args = null
+    ) {
+        if (!isset($value['model'])) {
+            throw new LocalizedException(__('"model" value should be specified'));
+        }
+
+        $cartItem = $value['model'];
+        $product = $this->productRepository->get($cartItem->getSku());
+        $productData = $product->toArray();
+        $productData['model'] = $product;
+        return $productData;
+    }
+}
