379027f80007472cee9419684871ac157ff9c8db
vadim.malesh <engcom-vendorworker-charlie@adobe.com>
2020-11-18 11:54:10 +0200

    added test for filter review dataProvider

--- dev/tests/integration/testsuite/Magento/Review/Ui/DataProvider/Product/ReviewDataProviderTest.php
+++ dev/tests/integration/testsuite/Magento/Review/Ui/DataProvider/Product/ReviewDataProviderTest.php
@@ -12,6 +12,7 @@ use Magento\Framework\App\RequestInterface;
 use Magento\Framework\ObjectManagerInterface;
 use Magento\TestFramework\Helper\Bootstrap;
 use PHPUnit\Framework\TestCase;
+use Magento\Framework\Api\Filter;
 
 /**
  * Test for \Magento\Review\Ui\DataProvider\Product\ReviewDataProvider.
@@ -102,5 +103,36 @@ class ReviewDataProviderTest extends TestCase
                 ['Review Summary', '2 filter first review', '1 filter second review'],
             ],
         ];
+    }
+
+    /**
+     * Filter dataProvider test
+     *
+     * @magentoDataFixture Magento/Review/_files/different_reviews.php
+     *
+     * @return void
+     */
+    public function testFilter(): void
+    {
+        $searchTitle = '2 filter first review';
+
+        $request = $this->objectManager->create(RequestInterface::class);
+        $request->setParam('current_product_id', 1);
+
+        /** @var ReviewDataProvider $dataProvider */
+        $dataProvider = $this->objectManager->create(
+            ReviewDataProvider::class,
+            array_merge($this->modelParams, ['request' => $request])
+        );
+
+        /** @var Filter $filter */
+        $filter = $this->objectManager->create(Filter::class);
+        $filter->setField('title')
+            ->setValue($searchTitle);
+
+        $dataProvider->addFilter($filter);
+        $result = $dataProvider->getData();
+
+        $this->assertEquals($this->getItemsField($result, 'title'), [$searchTitle]);
     }
 }
