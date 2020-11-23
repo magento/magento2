4845457624e11d6d4c94d5b17713ed21e39378d9
Nihar Ranjan <nsahoo@ztech.io>
2020-11-23 20:24:10 +0530

    Code formatting

--- dev/tests/integration/testsuite/Magento/LoginAsCustomer/_files/admin.php
+++ dev/tests/integration/testsuite/Magento/LoginAsCustomer/_files/admin.php
@@ -12,6 +12,9 @@ use Magento\TestFramework\Helper\Bootstrap;
 use Magento\User\Model\User;
 use Magento\Authorization\Model\RulesFactory;
 use Magento\Authorization\Model\Rules;
+use Magento\Authorization\Model\ResourceModel\Role as RoleResource;
+use Magento\Authorization\Model\ResourceModel\Rules as RulesResource;
+use Magento\User\Model\ResourceModel\User as UserResource;
 
 //Creating a new admin user with a custom role to safely change role settings without affecting the main user's role.
 /** @var Role $role */
@@ -20,13 +23,20 @@ $role->setName('test_custom_role');
 $role->setData('role_name', $role->getName());
 $role->setRoleType(\Magento\Authorization\Model\Acl\Role\Group::ROLE_TYPE);
 $role->setUserType((string)\Magento\Authorization\Model\UserContextInterface::USER_TYPE_ADMIN);
-$role->save();
+
+/** @var RoleResource $roleResource */
+$roleResource = Bootstrap::getObjectManager()->get(RoleResource::class);
+$roleResource->save($role);
+
 /** @var Rules $rules */
 $rules = Bootstrap::getObjectManager()->get(RulesFactory::class)->create();
 $rules->setRoleId($role->getId());
 //Granted all permissions.
 $rules->setResources([Bootstrap::getObjectManager()->get(\Magento\Framework\Acl\RootResource::class)->getId()]);
-$rules->saveRel();
+
+/** @var RulesResource $rulesResource */
+$rulesResource = Bootstrap::getObjectManager()->get(RulesResource::class);
+$rulesResource->saveRel($rules);
 
 /** @var User $user */
 $user = Bootstrap::getObjectManager()->create(User::class);
@@ -37,4 +47,7 @@ $user->setFirstname("John")
     ->setEmail('testadmin1@gmail.com')
     ->setIsActive(1)
     ->setRoleId($role->getId());
-$user->save();
+
+/** @var UserResource $userResource */
+$userResource = Bootstrap::getObjectManager()->get(UserResource::class);
+$userResource->save($user);
