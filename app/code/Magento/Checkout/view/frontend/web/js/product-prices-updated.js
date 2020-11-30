16e1b8432e5e71190e3172e1cb996cd0437ee83a
Pavel Bystritsky <engcom-vendorworker-foxtrot@adobe.com>
2020-11-30 11:47:56 +0200

    magento/magento2#3625: Cart caching problems.

--- app/code/Magento/Checkout/view/frontend/web/js/product-prices-updated.js
+++ app/code/Magento/Checkout/view/frontend/web/js/product-prices-updated.js
@@ -8,22 +8,21 @@ define([
     'Magento_Customer/js/customer-data'], function ($, customerData) {
     'use strict';
 
-    return function () {
+    return function (data) {
         var cacheKey = 'cart-cache-updated',
             cartData = customerData.get('cart'),
-            localStorage = $.initNamespaceStorage('mage-cache-storage').localStorage,
-            cartCacheUpdated = localStorage.get(cacheKey);
+            cartCacheUpdated = window.localStorage.getItem(cacheKey);
 
         customerData.getInitCustomerData().done(function () {
             var cartUpdated = cartData().updated_at;
             if (!cartCacheUpdated || cartUpdated > cartCacheUpdated) {
                 cartCacheUpdated = cartUpdated;
             }
-            if (PRODUCTS_UPDATED_AT > cartCacheUpdated) {
+            if (data.productsUpdatedTime > cartCacheUpdated) {
                 customerData.invalidate(['cart']);
                 customerData.reload(['cart'], true);
-                cartCacheUpdated = PRODUCTS_UPDATED_AT;
-                localStorage.set(cacheKey, cartCacheUpdated);
+                cartCacheUpdated = data.productsUpdatedTime;
+                window.localStorage.setItem(cacheKey, cartCacheUpdated);
             }
         });
     };
