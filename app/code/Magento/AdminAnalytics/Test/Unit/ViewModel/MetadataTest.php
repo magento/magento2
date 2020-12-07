50af59c2deb2b2a88213436d583c76398395ae60
Khien Pham <khienpc2@smartosc.com>
2020-12-07 17:07:19 +0700

    Update unit tests for MetadataTest.php

--- app/code/Magento/AdminAnalytics/Test/Unit/ViewModel/MetadataTest.php
+++ app/code/Magento/AdminAnalytics/Test/Unit/ViewModel/MetadataTest.php
@@ -8,10 +8,14 @@ declare(strict_types=1);
 namespace Magento\AdminAnalytics\Test\Unit\ViewModel;
 
 use Magento\AdminAnalytics\ViewModel\Metadata;
+use Magento\Authorization\Model\Role;
 use Magento\Backend\Model\Auth\Session;
+use Magento\Config\Model\Config\Backend\Admin\Custom;
+use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\App\ProductMetadataInterface;
 use Magento\Framework\App\State;
 use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
+use Magento\Store\Model\Information;
 use Magento\User\Model\User;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
@@ -38,6 +42,11 @@ class MetadataTest extends TestCase
      */
     private $appState;
 
+    /**
+     * @var ScopeConfigInterface|MockObject
+     */
+    private $config;
+
     protected function setUp(): void
     {
         $this->productMetadata = $this->createMock(ProductMetadataInterface::class);
@@ -46,6 +55,7 @@ class MetadataTest extends TestCase
             ->setMethods(['getUser'])
             ->getMock();
         $this->appState = $this->createMock(State::class);
+        $this->config = $this->createMock(ScopeConfigInterface::class);
 
         $objectManager = new ObjectManager($this);
         $this->metadata = $objectManager->getObject(
@@ -54,6 +64,7 @@ class MetadataTest extends TestCase
                 'productMetadata' => $this->productMetadata,
                 'authSession' => $this->authSession,
                 'appState' => $this->appState,
+                'config' => $this->config,
             ]
         );
     }
@@ -67,6 +78,21 @@ class MetadataTest extends TestCase
             ->willReturn($version);
 
         $this->assertSame($version, $this->metadata->getMagentoVersion());
+    }
+
+    public function testGetProductEdition()
+    {
+        $productEdition = 'edition_fake';
+
+        $this->productMetadata
+            ->expects($this->once())
+            ->method('getEdition')
+            ->willReturn($productEdition);
+
+        $this->assertSame(
+            $productEdition,
+            $this->metadata->getProductEdition()
+        );
     }
 
     public function testGetCurrentUser()
@@ -83,8 +109,111 @@ class MetadataTest extends TestCase
             ->willReturn($email);
 
         $this->assertSame(
-            hash('sha512', 'ADMIN_USER' . $email),
+            hash('sha256', 'ADMIN_USER' . $email),
             $this->metadata->getCurrentUser()
+        );
+    }
+
+    public function testGetCurrentUserCreatedDate()
+    {
+        $user = $this->createMock(User::class);
+
+        $createdDate = '2020-12-19 13-03-45';
+        $this->authSession
+            ->expects($this->once())
+            ->method('getUser')
+            ->willReturn($user);
+        $user->expects($this->once())
+            ->method('getCreated')
+            ->willReturn($createdDate);
+
+        $this->assertSame(
+            $createdDate,
+            $this->metadata->getCurrentUserCreatedDate()
+        );
+    }
+
+    public function testGetCurrentUserLogDate()
+    {
+        $user = $this->getMockBuilder(User::class)
+            ->setMethods(['getLogdate'])
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $createdDate = '2020-12-01 13-01-01';
+        $this->authSession
+            ->expects($this->once())
+            ->method('getUser')
+            ->willReturn($user);
+        $user->expects($this->once())
+            ->method('getLogdate')
+            ->willReturn($createdDate);
+
+        $this->assertSame(
+            $createdDate,
+            $this->metadata->getCurrentUserLogDate()
+        );
+    }
+
+    public function testGetSecureBaseUrlForScope()
+    {
+        $scope = 'default';
+        $scopeCode = 'th';
+        $result = 'test';
+
+        $this->config
+            ->expects($this->once())
+            ->method('getValue')
+            ->with(Custom::XML_PATH_SECURE_BASE_URL, $scope, $scopeCode)
+            ->willReturn($result);
+
+        $this->assertSame(
+            $result,
+            $this->metadata->getSecureBaseUrlForScope($scope, $scopeCode)
+        );
+    }
+
+    public function testGetStoreNameForScope()
+    {
+        $scope = 'default';
+        $scopeCode = 'th';
+        $result = 'test_fake_GetStoreNameForScope';
+
+        $this->config
+            ->expects($this->once())
+            ->method('getValue')
+            ->with(Information::XML_PATH_STORE_INFO_NAME, $scope, $scopeCode)
+            ->willReturn($result);
+
+        $this->assertSame(
+            $result,
+            $this->metadata->getStoreNameForScope($scope, $scopeCode)
+        );
+    }
+
+    public function testGetCurrentUserRoleName()
+    {
+        $user = $this->createMock(User::class);
+        $role = $this->getMockBuilder(Role::class)
+            ->setMethods(['getRoleName'])
+            ->disableOriginalConstructor()
+            ->getMock();
+        $roleName = 'khien';
+
+        $this->authSession
+            ->expects($this->once())
+            ->method('getUser')
+            ->willReturn($user);
+        $user->expects($this->once())
+            ->method('getRole')
+            ->willReturn($role);
+        $role->expects($this->once())
+            ->method('getRoleName')
+            ->willReturn($roleName);
+
+        $this->assertSame(
+            $roleName,
+            $this->metadata->getCurrentUserRoleName()
         );
     }
 
