82f34b0759937e40a2ab7c62eb7a21799ac91ced
Dinesh V B <dbabu@ztech.io>
2020-12-07 14:59:50 +0530

    issue-29302-added a test in another function

--- dev/tests/api-functional/testsuite/Magento/GraphQl/Quote/EditQuoteItemWithCustomOptionsTest.php
+++ dev/tests/api-functional/testsuite/Magento/GraphQl/Quote/EditQuoteItemWithCustomOptionsTest.php
@@ -113,6 +113,7 @@ mutation {
         ... on SimpleCartItem {
           customizable_options {
             label
+            type
             values {
               value
             }
@@ -130,6 +131,8 @@ QUERY;
         self::assertCount(2, $cartItemResponse['customizable_options']);
         self::assertEquals('initial value', $cartItemResponse['customizable_options'][0]['values'][0]['value']);
         self::assertEquals('initial value', $cartItemResponse['customizable_options'][1]['values'][0]['value']);
+        self::assertEquals('field', $cartItemResponse['customizable_options'][0]['type']);
+        self::assertEquals('area', $cartItemResponse['customizable_options'][1]['type']);
     }
 
     /**
