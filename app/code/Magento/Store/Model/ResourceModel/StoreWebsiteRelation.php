bf74533e75a16f88ef86601a9ea58f6d7f31956d
Cristian Partica <cpartica@magento.com>
2020-12-08 23:11:47 -0600

    MC-39744: Expose group_id through storeConfig query
    - expose group id to schema
    - fix availableStores resolver & query

--- app/code/Magento/Store/Model/ResourceModel/StoreWebsiteRelation.php
+++ app/code/Magento/Store/Model/ResourceModel/StoreWebsiteRelation.php
@@ -55,24 +55,48 @@ class StoreWebsiteRelation
     public function getWebsiteStores(int $websiteId, bool $available = false, int $storeGroupId = null): array
     {
         $connection = $this->resource->getConnection();
-        $storeTable = $this->resource->getTableName('store');
-        $storeSelect = $connection->select()->from($storeTable)->where(
-            'website_id = ?',
-            $websiteId
-        );
+        $storeTable = $this->resource->getTableName('store_website');
+        $storeSelect = $connection->select()
+            ->from(['main_table' => $storeTable])
+            ->columns(
+                [
+                    'website_code' => 'code',
+                    'website_name' => 'name',
+                    'website_sort_order' => 'sort_order',
+                    'default_group_id'
+                ]
+            )
+            ->join(
+                ['group_table' => $this->resource->getTableName('store_group')],
+                'main_table.website_id = group_table.website_id',
+                [
+                    'store_group_code' => 'code',
+                    'store_group_name' => 'name',
+                    'default_store_id'
+                ]
+            )
+            ->join(
+                ['store_table' => $this->resource->getTableName('store')],
+                'group_table.group_id = store_table.group_id'
+            );
 
         if ($storeGroupId) {
             $storeSelect = $storeSelect->where(
-                'group_id = ?',
+                'store_table.group_id = ?',
                 $storeGroupId
             );
         }
 
         if ($available) {
             $storeSelect = $storeSelect->where(
-                'is_active = 1'
+                'store_table.is_active = 1'
             );
         }
+
+        $storeSelect = $storeSelect->where(
+            'main_table.website_id = ?',
+            $websiteId
+        );
 
         return $connection->fetchAll($storeSelect);
     }
