6f2772b394b67e58336344bac6d4128cafc73ebf
Pavel Bystritsky <engcom-vendorworker-foxtrot@adobe.com>
2020-12-24 16:08:32 +0200

    magento/magento2#3625: Cart caching problems.

--- app/code/Magento/Checkout/Observer/CatalogProductSaveAfterObserver.php
+++ app/code/Magento/Checkout/Observer/CatalogProductSaveAfterObserver.php
@@ -3,13 +3,14 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+
 namespace Magento\Checkout\Observer;
 
-use Magento\CacheInvalidate\Model\PurgeCache;
 use Magento\Catalog\Model\Product;
 use Magento\Checkout\Block\ProductPricesUpdated;
 use Magento\Framework\App\CacheInterface;
 use Magento\Framework\Cache\FrontendInterface;
+use Magento\Framework\EntityManager\EventManager;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
 use Magento\PageCache\Model\Config;
@@ -30,23 +31,31 @@ class CatalogProductSaveAfterObserver implements ObserverInterface
     private $cacheConfig;
 
     /**
-     * @var PurgeCache
+     * @var EventManager
      */
-    private $purgeCache;
+    private $eventManager;
+
+    /**
+     * @var ProductPricesUpdated
+     */
+    private $productPricesUpdated;
 
     /**
      * @param CacheInterface $cache
      * @param Config $cacheConfig
-     * @param PurgeCache $purgeCache
+     * @param EventManager $eventManager
+     * @param ProductPricesUpdated $productPricesUpdated
      */
     public function __construct(
         CacheInterface $cache,
         Config $cacheConfig,
-        PurgeCache $purgeCache
+        EventManager $eventManager,
+        ProductPricesUpdated $productPricesUpdated
     ) {
         $this->cache = $cache;
         $this->cacheConfig = $cacheConfig;
-        $this->purgeCache = $purgeCache;
+        $this->eventManager = $eventManager;
+        $this->productPricesUpdated = $productPricesUpdated;
     }
 
     /**
@@ -60,10 +69,13 @@ class CatalogProductSaveAfterObserver implements ObserverInterface
         /** @var Product $product */
         $product = $observer->getEvent()->getProduct();
         if ($product->dataHasChangedFor('price')) {
-            $this->cache->clean(ProductPricesUpdated::CACHE_TAG);
-            if ($this->cacheConfig->isEnabled() && $this->cacheConfig->getType() == Config::VARNISH) {
-                $this->purgeCache->sendPurgeRequest([ProductPricesUpdated::CACHE_TAG]);
-            }
+            $this->cache->clean($this->productPricesUpdated->getIdentities());
+            $this->eventManager->dispatch(
+                'catalog_product_price_changed',
+                [
+                    'object' => $this->productPricesUpdated,
+                ]
+            );
         }
     }
 }
