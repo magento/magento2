940d74adbfe02a5bc5c169285d70dc4f99a739e5
engcom-Echo <engcom-vendorworker-echo@adobe.com>
2021-01-08 16:58:13 +0200

    MC-23915: Wrong currency symbol in creditmemo_grid & sales_order_view > creditmemo grid for subtotal & Shipping and Handling fee

--- app/code/Magento/Sales/Test/Unit/Ui/Component/Listing/Column/PurchasedPriceTest.php
+++ app/code/Magento/Sales/Test/Unit/Ui/Component/Listing/Column/PurchasedPriceTest.php
@@ -8,6 +8,10 @@ declare(strict_types=1);
 namespace Magento\Sales\Test\Unit\Ui\Component\Listing\Column;
 
 use Magento\Directory\Model\Currency;
+use Magento\Sales\Api\Data\OrderInterface;
+use Magento\Sales\Api\Data\OrderSearchResultInterface;
+use Magento\Framework\Api\SearchCriteria;
+use Magento\Framework\Api\SearchCriteriaBuilder;
 use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
 use Magento\Framework\View\Element\UiComponent\ContextInterface;
 use Magento\Framework\View\Element\UiComponent\Processor;
@@ -17,6 +21,9 @@ use Magento\Sales\Ui\Component\Listing\Column\PurchasedPrice;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Test for \Magento\Sales\Ui\Component\Listing\Column\PurchasedPrice
+ */
 class PurchasedPriceTest extends TestCase
 {
     /**
@@ -34,6 +41,21 @@ class PurchasedPriceTest extends TestCase
      */
     private $orderRepository;
 
+    /**
+     * @var SearchCriteria|MockObject
+     */
+    private $searchCriteriaMock;
+
+    /**
+     * @var OrderSearchResultInterface|MockObject
+     */
+    private $orderSearchResultMock;
+
+    /**
+     * @var OrderInterface|MockObject
+     */
+    private $order;
+
     protected function setUp(): void
     {
         $objectManager = new ObjectManager($this);
@@ -47,56 +69,61 @@ class PurchasedPriceTest extends TestCase
             ->setMethods(['load', 'format'])
             ->disableOriginalConstructor()
             ->getMock();
-        $this->orderRepository = $this->getMockBuilder(OrderRepositoryInterface::class)
-            ->setMethods(['getList','get','delete','save','getOrderCurrencyCode'])
-            ->disableOriginalConstructor()
-            ->getMock();
+        $this->orderRepository = $this->getMockForAbstractClass(OrderRepositoryInterface::class);
+        $this->order = $this->getMockForAbstractClass(OrderInterface::class);
+        $this->orderSearchResultMock = $this->getMockForAbstractClass(OrderSearchResultInterface::class);
+        $this->searchCriteriaMock = $this->createMock(SearchCriteria::class);
+        $searchCriteriaBuilderMock = $this->createMock(SearchCriteriaBuilder::class);
+        $searchCriteriaBuilderMock->expects($this->once())
+            ->method('addFilter')
+            ->willReturnSelf();
+
+        $searchCriteriaBuilderMock->expects($this->once())
+            ->method('create')
+            ->willReturn($this->searchCriteriaMock);
+
         $this->model = $objectManager->getObject(
             PurchasedPrice::class,
             [
                 'currency' => $this->currencyMock,
                 'context' => $contextMock,
-                'order' => $this->orderRepository,
+                'orderRepository' => $this->orderRepository,
+                'order' => $this->order,
+                'searchCriteriaBuilder' => $searchCriteriaBuilderMock,
+                'searchCriteria' => $this->searchCriteriaMock,
+                'orderSearchResult' => $this->orderSearchResultMock,
             ]
         );
     }
 
     /**
-     * @param string $itemName
-     * @param string $oldItemValue
-     * @param string $newItemValue
-     * @param string|null $orderCurrencyCode
+     * @param array $orderData
+     * @param array $dataSource
      * @dataProvider prepareDataSourceDataProvider
      */
-    public function testPrepareDataSource(
-        $itemName,
-        $oldItemValue,
-        $newItemValue,
-        $orderCurrencyCode
-    ): void {
-        $dataSource = [
-            'data' => [
-                'items' => [
-                    [
-                        $itemName => $oldItemValue,
-                        'order_currency_code' => $orderCurrencyCode,
-                        'order_id' => 1,
-                    ]
-                ]
-            ]
-        ];
+    public function testPrepareDataSource(array $orderData,array $dataSource): void
+    {
+        $oldItemValue = 'oldItemValue';
+        $newItemValue = 'newItemValue';
 
-        if (isset($dataSource['data']['items'][0]['order_currency_code'])) {
-            $currencyCode = $dataSource['data']['items'][0]['order_currency_code'];
-        } else {
-            $currencyCode = 'FR';
-            $this->orderRepository->expects($this->once())
-                ->method('get')
-                ->willReturnSelf();
-            $this->orderRepository->expects($this->once())
-                ->method('getOrderCurrencyCode')
-                ->willReturn($currencyCode);
-        }
+        $this->orderRepository->expects($this->once())
+            ->method('getList')
+            ->with($this->searchCriteriaMock)
+            ->willReturn($this->orderSearchResultMock);
+
+        $this->orderSearchResultMock->expects($this->once())
+            ->method('getItems')
+            ->willReturn([$this->order]);
+
+        $this->order->expects($this->once())
+            ->method('getEntityId')
+            ->willReturn($orderData['entity_id']);
+
+        $this->order->expects($this->once())
+            ->method('getOrderCurrencyCode')
+            ->willReturn($orderData['order_currency_code']);
+
+        $currencyCode = $dataSource['data']['items'][0]['order_currency_code'] ?? $orderData['order_currency_code'];
 
         $this->currencyMock->expects($this->once())
             ->method('load')
@@ -108,9 +135,9 @@ class PurchasedPriceTest extends TestCase
             ->with($oldItemValue, [], false)
             ->willReturn($newItemValue);
 
-        $this->model->setData('name', $itemName);
+        $this->model->setData('name', 'item_name');
         $dataSource = $this->model->prepareDataSource($dataSource);
-        $this->assertEquals($newItemValue, $dataSource['data']['items'][0][$itemName]);
+        $this->assertEquals($newItemValue, $dataSource['data']['items'][0]['item_name']);
     }
 
     /**
@@ -120,17 +147,38 @@ class PurchasedPriceTest extends TestCase
     {
         return [
             [
-                'item_name' => 'itemName',
-                'old_item_value' => 'oldItemValue',
-                'new_item_value' => 'newItemValue',
-                'order_currency_code' => 'US',
+                'orderData' => [
+                    'entity_id' => 1,
+                    'order_currency_code' => 'US',
+                ],
+                'dataSource' => [
+                    'data' => [
+                        'items' => [
+                            [
+                                'item_name' => 'oldItemValue',
+                                'order_currency_code' => 'US',
+                                'order_id' => 1,
+                            ]
+                        ]
+                    ]
+                ]
             ],
             [
-                'item_name' => 'itemName',
-                'old_item_value' => 'oldItemValue',
-                'new_item_value' => 'newItemValue',
-                'order_currency_code' => null,
-            ],
+                'orderData' => [
+                    'entity_id' => 1,
+                    'order_currency_code' => 'FR',
+                ],
+                'dataSource' => [
+                    'data' => [
+                        'items' => [
+                            [
+                                'item_name' => 'oldItemValue',
+                                'order_id' => 1,
+                            ]
+                        ]
+                    ]
+                ]
+            ]
         ];
     }
 }
--- app/code/Magento/Sales/Ui/Component/Listing/Column/PurchasedPrice.php
+++ app/code/Magento/Sales/Ui/Component/Listing/Column/PurchasedPrice.php
@@ -7,6 +7,7 @@ declare(strict_types=1);
 
 namespace Magento\Sales\Ui\Component\Listing\Column;
 
+use Magento\Framework\Api\SearchCriteriaBuilder;
 use Magento\Framework\View\Element\UiComponent\ContextInterface;
 use Magento\Framework\View\Element\UiComponentFactory;
 use Magento\Sales\Api\OrderRepositoryInterface;
@@ -35,6 +36,11 @@ class PurchasedPrice extends Column
     private $orderRepository;
 
     /**
+     * @var SearchCriteriaBuilder
+     */
+    private $searchCriteriaBuilder;
+
+    /**
      * Constructor
      *
      * @param ContextInterface $context
@@ -44,6 +50,7 @@ class PurchasedPrice extends Column
      * @param array $data
      * @param Currency $currency
      * @param OrderRepositoryInterface $orderRepository
+     * @param SearchCriteriaBuilder $searchCriteriaBuilder
      */
     public function __construct(
         ContextInterface $context,
@@ -52,15 +59,19 @@ class PurchasedPrice extends Column
         array $components = [],
         array $data = [],
         Currency $currency = null,
-        OrderRepositoryInterface $orderRepository = null
+        OrderRepositoryInterface $orderRepository = null,
+        SearchCriteriaBuilder $searchCriteriaBuilder = null
     ) {
         $this->priceFormatter = $priceFormatter;
         $this->currency = $currency ?: \Magento\Framework\App\ObjectManager::getInstance()
             ->create(Currency::class);
         $this->orderRepository = $orderRepository ?: \Magento\Framework\App\ObjectManager::getInstance()
             ->create(OrderRepositoryInterface::class);
+        $this->searchCriteriaBuilder = $searchCriteriaBuilder ?: \Magento\Framework\App\ObjectManager::getInstance()
+            ->create(SearchCriteriaBuilder::class);
         parent::__construct($context, $uiComponentFactory, $components, $data);
     }
+
 
     /**
      * Prepare Data Source
@@ -71,9 +82,10 @@ class PurchasedPrice extends Column
     public function prepareDataSource(array $dataSource)
     {
         if (isset($dataSource['data']['items'])) {
+            $orderIds = array_column($dataSource['data']['items'],'order_id');
+            $orderCurrencyCodes = $this->getOrdersCurrency($orderIds);
             foreach ($dataSource['data']['items'] as & $item) {
-                $currencyCode = $item['order_currency_code']
-                    ?? $this->orderRepository->get($item['order_id'])->getOrderCurrencyCode();
+                $currencyCode = $item['order_currency_code'] ?? $orderCurrencyCodes[$item['order_id']];
                 $purchaseCurrency = $this->currency->load($currencyCode);
                 $item[$this->getData('name')] = $purchaseCurrency
                     ->format($item[$this->getData('name')], [], false);
@@ -81,5 +93,24 @@ class PurchasedPrice extends Column
         }
 
         return $dataSource;
+    }
+
+    /**
+     * @param array $orderIds
+     * @return array
+     */
+    private function getOrdersCurrency(array $orderIds): array
+    {
+        $orderCurrencyCodes = [];
+
+        $searchCriteria = $this->searchCriteriaBuilder
+            ->addFilter('entity_id', $orderIds ,'in')
+            ->create();
+
+        foreach ($this->orderRepository->getList($searchCriteria)->getItems() as $order) {
+            $orderCurrencyCodes[$order->getEntityId()] = $order->getOrderCurrencyCode();
+        }
+
+        return $orderCurrencyCodes;
     }
 }
