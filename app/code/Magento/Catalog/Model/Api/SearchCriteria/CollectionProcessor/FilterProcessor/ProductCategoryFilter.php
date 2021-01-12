b0d6f4300d26d7cc331139955a0e7226ceb6b9f2
engcom-Kilo <mikola.malevanec@transoftgroup.com>
2021-01-12 16:00:19 +0200

    MC-40344: Sorting by position on product search via REST API does not work.

--- app/code/Magento/Catalog/Model/Api/SearchCriteria/CollectionProcessor/FilterProcessor/ProductCategoryFilter.php
+++ app/code/Magento/Catalog/Model/Api/SearchCriteria/CollectionProcessor/FilterProcessor/ProductCategoryFilter.php
@@ -3,15 +3,31 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+
 namespace Magento\Catalog\Model\Api\SearchCriteria\CollectionProcessor\FilterProcessor;
 
 use Magento\Catalog\Model\ResourceModel\Product\Collection;
 use Magento\Framework\Api\Filter;
 use Magento\Framework\Api\SearchCriteria\CollectionProcessor\FilterProcessor\CustomFilterInterface;
 use Magento\Framework\Data\Collection\AbstractDb;
+use Magento\Framework\Exception\LocalizedException;
+use Psr\Log\LoggerInterface;
 
 class ProductCategoryFilter implements CustomFilterInterface
 {
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @param LoggerInterface $logger
+     */
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     /**
      * Apply category_id Filter to Product Collection
      *
@@ -23,14 +39,25 @@ class ProductCategoryFilter implements CustomFilterInterface
     {
         $value = $filter->getValue();
         $conditionType = $filter->getConditionType() ?: 'in';
-        $filterValue = [$value];
-        if (($conditionType === 'in' || $conditionType === 'nin') && is_string($value)) {
-            $filterValue = explode(',', $value);
-        }
-        $categoryFilter = [$conditionType => $filterValue];
+        $categoryIds = explode(',', $value);
+        $categoryFilter = [$conditionType => $categoryIds];
 
         /** @var Collection $collection */
         $collection->addCategoriesFilter($categoryFilter);
+        if (count($categoryIds) === 1) {
+            try {
+                $collection->joinField(
+                    'position',
+                    'catalog_category_product',
+                    'position',
+                    'product_id=entity_id',
+                    ['category_id' => $value],
+                    'left'
+                );
+            } catch (LocalizedException $e) {
+                $this->logger->warning($e->getMessage());
+            }
+        }
 
         return true;
     }
