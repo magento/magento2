86d30a5e909da11f37d98934fe52d726308f8eca
mvadim <vadim.malesh@transoftgroup.com>
2021-01-14 16:24:09 +0200

    magento/magento2#30492 Adding required custom options to simple product removes it from parent composite products without warning

--- app/code/Magento/Catalog/Model/Product/Option/SaveHandler.php
+++ app/code/Magento/Catalog/Model/Product/Option/SaveHandler.php
@@ -7,11 +7,22 @@ declare(strict_types=1);
 
 namespace Magento\Catalog\Model\Product\Option;
 
+use Magento\Catalog\Api\Data\ProductCustomOptionInterface;
+use Magento\Catalog\Api\Data\ProductInterface;
 use Magento\Catalog\Api\ProductCustomOptionRepositoryInterface as OptionRepository;
+use Magento\Catalog\Model\Product\Option;
+use Magento\ConfigurableProduct\Model\Product\Type\Configurable;
+use Magento\Framework\App\ObjectManager;
+use Magento\Catalog\Model\Product\Type;
 use Magento\Framework\EntityManager\Operation\ExtensionInterface;
+use Magento\Catalog\Model\ResourceModel\Product\Relation;
+use Magento\Framework\Exception\CouldNotSaveException;
+use Magento\GroupedProduct\Model\Product\Type\Grouped;
 
 /**
- * Class SaveHandler
+ * SaveHandler for product option
+ *
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
  */
 class SaveHandler implements ExtensionInterface
 {
@@ -21,12 +32,20 @@ class SaveHandler implements ExtensionInterface
     protected $optionRepository;
 
     /**
+     * @var Relation
+     */
+    private $relation;
+
+    /**
      * @param OptionRepository $optionRepository
+     * @param Relation|null $relation
      */
     public function __construct(
-        OptionRepository $optionRepository
+        OptionRepository $optionRepository,
+        ?Relation $relation = null
     ) {
         $this->optionRepository = $optionRepository;
+        $this->relation = $relation ?: ObjectManager::getInstance()->get(Relation::class);
     }
 
     /**
@@ -34,7 +53,7 @@ class SaveHandler implements ExtensionInterface
      *
      * @param object $entity
      * @param array $arguments
-     * @return \Magento\Catalog\Api\Data\ProductInterface|object
+     * @return ProductInterface|object
      * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
     public function execute($entity, $arguments = [])
@@ -47,23 +66,42 @@ class SaveHandler implements ExtensionInterface
         $optionIds = [];
 
         if ($options) {
-            $optionIds = array_map(function ($option) {
-                /** @var \Magento\Catalog\Model\Product\Option $option */
+            $optionIds = array_map(function (Option $option) {
                 return $option->getOptionId();
             }, $options);
         }
 
-        /** @var \Magento\Catalog\Api\Data\ProductInterface $entity */
+        /** @var ProductInterface $entity */
         foreach ($this->optionRepository->getProductOptions($entity) as $option) {
             if (!in_array($option->getOptionId(), $optionIds)) {
                 $this->optionRepository->delete($option);
             }
         }
         if ($options) {
-            $this->processOptionsSaving($options, (bool)$entity->dataHasChangedFor('sku'), (string)$entity->getSku());
+            $this->processOptionsSaving($options, (bool)$entity->dataHasChangedFor('sku'), $entity);
         }
 
         return $entity;
+    }
+
+    /**
+     * Check if product doesn't belong to composite product
+     *
+     * @param ProductInterface $product
+     * @return bool
+     */
+    private function isProductValid(ProductInterface $product): bool
+    {
+        $result = true;
+        if ($product->getTypeId() !== Type::TYPE_BUNDLE
+            && $product->getTypeId() !== Configurable::TYPE_CODE
+            && $product->getTypeId() !== Grouped::TYPE_CODE
+            && $this->relation->getRelationsByChildren([$product->getId()])
+        ) {
+            $result = false;
+        }
+
+        return $result;
     }
 
     /**
@@ -71,13 +109,23 @@ class SaveHandler implements ExtensionInterface
      *
      * @param array $options
      * @param bool $hasChangedSku
-     * @param string $newSku
+     * @param ProductInterface $product
+     * @return void
+     * @throws CouldNotSaveException
      */
-    private function processOptionsSaving(array $options, bool $hasChangedSku, string $newSku)
+    private function processOptionsSaving(array $options, bool $hasChangedSku, ProductInterface $product): void
     {
+        $isProductValid = $this->isProductValid($product);
+        /** @var ProductCustomOptionInterface $option */
         foreach ($options as $option) {
+            if (!$isProductValid && $option->getIsRequire()) {
+                $message = 'Required custom options cannot be added to a simple product'
+                    . ' that is a part of a composite product.';
+                throw new CouldNotSaveException(__($message));
+            }
+
             if ($hasChangedSku && $option->hasData('product_sku')) {
-                $option->setProductSku($newSku);
+                $option->setProductSku($product->getSku());
             }
             $this->optionRepository->save($option);
         }
