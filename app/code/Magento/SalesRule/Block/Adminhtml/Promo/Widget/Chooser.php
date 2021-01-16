09b3fe20c818f455af5af99a8699ab74be757e1c
sergey <sergey.nezbritskiy@gmail.com>
2021-01-16 20:48:49 +0200

    Magento_SalesRule: avoid using deprecated escape* methods from AbstractBlock

--- app/code/Magento/SalesRule/Block/Adminhtml/Promo/Widget/Chooser.php
+++ app/code/Magento/SalesRule/Block/Adminhtml/Promo/Widget/Chooser.php
@@ -91,7 +91,7 @@ class Chooser extends \Magento\Backend\Block\Widget\Grid\Extended
         if ($element->getValue()) {
             $rule = $this->ruleFactory->create()->load((int)$element->getValue());
             if ($rule->getId()) {
-                $chooser->setLabel($this->escapeHtml($rule->getName()));
+                $chooser->setLabel($this->_escaper->escapeHtml($rule->getName()));
             }
         }
 
