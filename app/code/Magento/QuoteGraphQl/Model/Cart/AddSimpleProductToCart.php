67c52ad4ae57af8e9d19101291e5a5a38e76a0ef
Shikha Mishra <shikhamishra@cedcoss.com>
2021-01-18 12:58:20 +0530

    Unable to add configurable product to cart on any non-default store view

--- app/code/Magento/QuoteGraphQl/Model/Cart/AddSimpleProductToCart.php
+++ app/code/Magento/QuoteGraphQl/Model/Cart/AddSimpleProductToCart.php
@@ -63,7 +63,7 @@ class AddSimpleProductToCart
 
         try {
             $result = $cart->addProduct($product, $this->buyRequestBuilder->build($cartItemData));
-        } catch (Exception $e) {
+        } catch (Exception $e) {die($e);
             throw new GraphQlInputException(
                 __(
                     'Could not add the product with SKU %sku to the shopping cart: %message',
