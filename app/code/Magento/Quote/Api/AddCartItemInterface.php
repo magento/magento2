831b6f149b686bcc9ace653c2b6ec7f69f04c901
engcom-Echo <engcom-vendorworker-echo@adobe.com>
2021-01-25 18:34:04 +0200

    MC-34298: Introduce separate POST and PUT API for carts/mine/items

--- app/code/Magento/Quote/Api/AddCartItemInterface.php
+++ app/code/Magento/Quote/Api/AddCartItemInterface.php
@@ -18,8 +18,8 @@ interface AddCartItemInterface
     /**
      * Add the specified cart item.
      *
-     * @param CartItemInterface $cartItem The item.
-     * @return CartItemInterface Item.
+     * @param \Magento\Quote\Api\Data\CartItemInterface $cartItem The item.
+     * @return \Magento\Quote\Api\Data\CartItemInterface Item.
      * @throws \Magento\Framework\Exception\NoSuchEntityException The specified cart does not exist.
      * @throws \Magento\Framework\Exception\CouldNotSaveException The specified item could not be saved to the cart.
      * @throws \Magento\Framework\Exception\InputException The specified item or cart is not valid.
