cec8bc2a7371ea96485dc0dc384c49059d4e3b03
Mark Berube <berube@adobe.com>
2021-01-26 11:56:28 -0600

    MC-40099: Changing groupId functionality on createAccount()

--- app/code/Magento/Customer/Model/AccountManagement.php
+++ app/code/Magento/Customer/Model/AccountManagement.php
@@ -850,7 +850,7 @@ class AccountManagement implements AccountManagementInterface
     public function createAccount(CustomerInterface $customer, $password = null, $redirectUrl = '')
     {
         $groupId = $customer->getGroupId();
-        if ($groupId && $groupId !== 0 && !$this->authorization->isAllowed(self::ADMIN_RESOURCE)) {
+        if ($groupId || $groupId === 0 && !$this->authorization->isAllowed(self::ADMIN_RESOURCE)) {
             $customer->setGroupId(null);
         }
 
