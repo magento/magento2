604deadd40f326b076d5fe0e92bdb608bef7a47d
Oleksandr Iegorov <oiegorov@magento.com>
2021-02-05 12:55:48 -0600

    MC-39619: Specify product option error for bundled product(single checkbox/dropdown)

--- app/code/Magento/Bundle/Plugin/Catalog/Model/Product/Type/AbstractType.php
+++ app/code/Magento/Bundle/Plugin/Catalog/Model/Product/Type/AbstractType.php
@@ -10,6 +10,7 @@ namespace Magento\Bundle\Plugin\Catalog\Model\Product\Type;
 use Magento\Catalog\Model\Product\Type\AbstractType as Subject;
 use Magento\Catalog\Model\Product;
 use Magento\Catalog\Model\Product\Type as BundleType;
+use Magento\Bundle\Model\Product\SingleChoiceProvider;
 
 /**
  * Plugin to add possibility to add bundle product with single option from list
@@ -17,31 +18,34 @@ use Magento\Catalog\Model\Product\Type as BundleType;
 class AbstractType
 {
     /**
+     * @var SingleChoiceProvider
+     */
+    private $singleChoiceProvider;
+
+    /**
+     * @param SingleChoiceProvider $singleChoiceProvider
+     */
+    public function __construct(
+        SingleChoiceProvider $singleChoiceProvider
+    ) {
+        $this->singleChoiceProvider = $singleChoiceProvider;
+    }
+
+    /**
      * Add possibility to add to cart from the list in case of one required option
      *
      * @param Subject $subject
      * @param bool $result
      * @param Product $product
      * @return bool
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
     public function afterIsPossibleBuyFromList(Subject $subject, $result, $product)
     {
         if ($product->getTypeId() === BundleType::TYPE_BUNDLE) {
-            $typeInstance = $product->getTypeInstance();
-            $typeInstance->setStoreFilter($product->getStoreId(), $product);
-
-            $optionsIds = $typeInstance->getOptionsIds($product);
-            if (count($optionsIds) === 1 && $typeInstance->hasRequiredOptions($product)) {
-                $selectionsCollection = $typeInstance->getSelectionsCollection(
-                    $optionsIds,
-                    $product
-                );
-                $selections = $selectionsCollection->exportToArray();
-                if (count($selections) === 1) {
-                    $selection = array_pop($selections);
-                    $result = (int) $selection['is_default'] === 1
-                        && (int) $selection['selection_can_change_qty'] === 0;
-                }
+            $isSingleChoice = $this->singleChoiceProvider->isSingleChoiceAvailable($product);
+            if ($isSingleChoice === true) {
+                $result = $isSingleChoice;
             }
         }
         return $result;
