0e043d67fe1194e8502f1f1a56e2ae4984d1e74b
Oleksandr Iegorov <oiegorov@magento.com>
2021-02-08 07:56:21 -0600

    MC-39619: Specify product option error for bundled product(single checkbox/dropdown)

---
+++ app/code/Magento/Bundle/Plugin/Catalog/ViewModel/Product/AddBundleOptionsData.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\Bundle\Plugin\Catalog\ViewModel\Product;
+
+use Magento\Catalog\Model\Product;
+use Magento\Catalog\ViewModel\Product\OptionsData as Subject;
+use Magento\Catalog\Model\Product\Type;
+use Magento\Bundle\Model\Product\SingleChoiceProvider;
+
+/**
+ * Plugin to add bundle options data
+ */
+class AddBundleOptionsData
+{
+    /**
+     * @var SingleChoiceProvider
+     */
+    private $singleChoiceProvider;
+
+    /**
+     * @param SingleChoiceProvider $singleChoiceProvider
+     */
+    public function __construct(
+        SingleChoiceProvider $singleChoiceProvider
+    ) {
+        $this->singleChoiceProvider = $singleChoiceProvider;
+    }
+
+    public function afterGetOptionsData(Subject $subject, array $result, Product $product) : array
+    {
+        if ($product->getTypeId() === Type::TYPE_BUNDLE) {
+            if ($this->singleChoiceProvider->isSingleChoiceAvailable($product) === true) {
+                $typeInstance = $product->getTypeInstance();
+                $typeInstance->setStoreFilter($product->getStoreId(), $product);
+                $options = $typeInstance->getOptions($product);
+                foreach ($options as $option) {
+                    $optionId = $option->getId();
+                    $selectionsCollection = $typeInstance->getSelectionsCollection(
+                        [$optionId],
+                        $product
+                    );
+                    $selections = $selectionsCollection->exportToArray();
+                    if (count($selections) === 1) {
+                        $selection = array_pop($selections);
+                        $result[] = [
+                            'name' => 'bundle_option[' . $optionId . ']',
+                            'value' => $selection['id']
+                        ];
+                    } else {
+                        foreach ($selections as $selection) {
+                            $result[] = [
+                                'name' => 'bundle_option[' . $optionId . '][]',
+                                'value' => $selection['id']
+                            ];
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
