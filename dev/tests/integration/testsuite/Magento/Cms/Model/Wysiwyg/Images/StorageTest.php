1cf3f59afe9f45cd3edcf0ac82d858decd5cf3de
Marc Ginesta <mginesta@adobe.com>
2021-02-10 12:01:38 +0100

    MC-39923: Stabilize Integration Tests (PageBuilder) - Split line exceeding 120 characters

--- dev/tests/integration/testsuite/Magento/Cms/Model/Wysiwyg/Images/StorageTest.php
+++ dev/tests/integration/testsuite/Magento/Cms/Model/Wysiwyg/Images/StorageTest.php
@@ -167,7 +167,9 @@ class StorageTest extends \PHPUnit\Framework\TestCase
      */
     public function testUploadFileWithExcludedDirPath(): void
     {
-        $this->expectExceptionMessage("We can't upload the file to current folder right now. Please try another folder.");
+        $this->expectExceptionMessage(
+            "We can't upload the file to current folder right now. Please try another folder."
+        );
         $this->expectException(\Magento\Framework\Exception\LocalizedException::class);
         $fileName = 'magento_small_image.jpg';
         $tmpDirectory = $this->filesystem->getDirectoryWrite(\Magento\Framework\App\Filesystem\DirectoryList::SYS_TMP);
