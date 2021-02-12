ed7263f8118e4886fde67251b6c65e3c81e91f40
Medvediev <v.medvediev@atwix.com>
2021-02-12 12:56:48 +0200

    lib/internal/Magento/Framework/Image/Adapter/UploadConfigInterface was removed

--- lib/internal/Magento/Framework/Image/Adapter/Config.php
+++ lib/internal/Magento/Framework/Image/Adapter/Config.php
@@ -10,23 +10,11 @@ namespace Magento\Framework\Image\Adapter;
 /**
  * Image config provider.
  */
-class Config implements ConfigInterface, UploadConfigInterface
+class Config implements ConfigInterface
 {
     const XML_PATH_IMAGE_ADAPTER = 'dev/image/default_adapter';
 
     const XML_PATH_IMAGE_ADAPTERS = 'dev/image/adapters';
-
-    /**
-     * Config path for the maximal image width value
-     * @deprecated Used in a method that is deprecated
-     */
-    const XML_PATH_MAX_WIDTH_IMAGE = 'system/upload_configuration/max_width';
-
-    /**
-     * Config path for the maximal image height value
-     * @deprecated Used in a method that is deprecated
-     */
-    const XML_PATH_MAX_HEIGHT_IMAGE = 'system/upload_configuration/max_height';
 
     /**
      * @var \Magento\Framework\App\Config\ScopeConfigInterface
@@ -59,29 +47,5 @@ class Config implements ConfigInterface, UploadConfigInterface
     public function getAdapters()
     {
         return $this->config->getValue(self::XML_PATH_IMAGE_ADAPTERS);
-    }
-
-    /**
-     * Get Maximum Image Width resolution in pixels. For image resizing on client side.
-     *
-     * @return int
-     * @deprecated 102.0.1
-     * @see \Magento\Backend\Model\Image\UploadResizeConfigInterface::getMaxHeight()
-     */
-    public function getMaxWidth(): int
-    {
-        return (int)$this->config->getValue(self::XML_PATH_MAX_WIDTH_IMAGE);
-    }
-
-    /**
-     * Get Maximum Image Height resolution in pixels. For image resizing on client side.
-     *
-     * @return int
-     * @deprecated 102.0.1
-     * @see \Magento\Backend\Model\Image\UploadResizeConfigInterface::getMaxHeight()
-     */
-    public function getMaxHeight(): int
-    {
-        return (int)$this->config->getValue(self::XML_PATH_MAX_HEIGHT_IMAGE);
     }
 }
