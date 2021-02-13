b5d740a5ccc4315a4ff3ca0af5885e9dc76562ed
Andrii Kasian <akasian@magento.com>
2021-02-12 18:19:34 -0600

    wip: fix xsd validation

---
+++ app/etc/.env.php
@@ -0,0 +1,109 @@
+<?php
+return [
+    'queue' => [
+        'default_connection' => 'redis',
+        'consumers_wait_for_messages' => 1,
+        'redis' => [
+            'host' => 'redis',
+            'database' => '0',
+            'port' => '6379'
+        ]
+    ],
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'db' => [
+        'connection' => [
+            'indexer' => [
+                'host' => 'db',
+                'dbname' => 'magento2',
+                'username' => 'root',
+                'password' => 'root',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'persistent' => null
+            ],
+            'default' => [
+                'host' => 'db',
+                'dbname' => 'magento2',
+                'username' => 'root',
+                'password' => 'root',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ],
+        'table_prefix' => ''
+    ],
+    'crypt' => [
+        'key' => 'bffb2261464b07d6e0d987dc24938274'
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => '770_',
+                'backend' => 'Magento\\Framework\\Cache\\Backend\\Redis',
+                'backend_options' => [
+                    'server' => 'redis',
+                    'database' => '0',
+                    'port' => '6379',
+                    'password' => '',
+                    'compress_data' => '1',
+                    'compression_lib' => ''
+                ]
+            ],
+            'page_cache' => [
+                'id_prefix' => '770_'
+            ]
+        ],
+        'allow_parallel_generation' => false
+    ],
+    'lock' => [
+        'provider' => 'db',
+        'config' => [
+            'prefix' => null
+        ]
+    ],
+    'directories' => [
+        'document_root_is_pub' => false
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'target_rule' => 1,
+        'config_webservice' => 1,
+        'translate' => 1
+    ],
+    'downloadable_domains' => [
+        'mage.perf'
+    ],
+    'install' => [
+        'date' => 'Tue, 03 Nov 2020 19:13:41 +0000'
+    ]
+];
