1c238c1a5d4bb153d35a974bacb6d9832aad5518
Oleh Usik <o.usik@atwix.com>
2021-02-15 18:23:22 +0200

    marked as api

--- lib/internal/Magento/Framework/Model/ResourceModel/Type/Db/ConnectionFactoryInterface.php
+++ lib/internal/Magento/Framework/Model/ResourceModel/Type/Db/ConnectionFactoryInterface.php
@@ -5,11 +5,14 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types=1);
+
 namespace Magento\Framework\Model\ResourceModel\Type\Db;
 
 /**
  * Interface \Magento\Framework\Model\ResourceModel\Type\Db\ConnectionFactoryInterface
  *
+ * @api
  */
 interface ConnectionFactoryInterface
 {
