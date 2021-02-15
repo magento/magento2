de600a6f2127dedcaf978d9e8ccf55ffd25a4a45
Oleh Usik <o.usik@atwix.com>
2021-02-15 17:24:48 +0200

    MArked as api

--- app/code/Magento/Analytics/Api/LinkProviderInterface.php
+++ app/code/Magento/Analytics/Api/LinkProviderInterface.php
@@ -3,10 +3,14 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types=1);
+
 namespace Magento\Analytics\Api;
 
 /**
  * Provides link to file with collected report data.
+ *
+ * @api
  */
 interface LinkProviderInterface
 {
