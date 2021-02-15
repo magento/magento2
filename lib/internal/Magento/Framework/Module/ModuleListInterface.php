a9e5c977c4e96c4af2b83b11f47733ba612871d9
Oleh Usik <o.usik@atwix.com>
2021-02-15 18:23:22 +0200

    marked as api

--- lib/internal/Magento/Framework/Module/ModuleListInterface.php
+++ lib/internal/Magento/Framework/Module/ModuleListInterface.php
@@ -5,11 +5,14 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types=1);
+
 namespace Magento\Framework\Module;
 
 /**
  * Interface \Magento\Framework\Module\ModuleListInterface
  *
+ * @api
  */
 interface ModuleListInterface
 {
