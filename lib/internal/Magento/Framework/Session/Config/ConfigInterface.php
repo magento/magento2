a1d765e198f8b248ee7a3bdddcf4ac7593146dd8
Oleh Usik <o.usik@atwix.com>
2021-02-15 18:12:18 +0200

    marked as api

--- lib/internal/Magento/Framework/Session/Config/ConfigInterface.php
+++ lib/internal/Magento/Framework/Session/Config/ConfigInterface.php
@@ -5,11 +5,14 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types=1);
+
 namespace Magento\Framework\Session\Config;
 
 /**
  * Interface \Magento\Framework\Session\Config\ConfigInterface
  *
+ * @api
  */
 interface ConfigInterface
 {
