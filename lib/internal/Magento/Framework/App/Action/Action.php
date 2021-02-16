dda55cfbf3545dcc74e249f6dedf5aa8dab3db14
Sergio Vera <svera@adobe.com>
2021-02-16 15:09:27 +0100

    MC-40558: Stabilize static tests

--- lib/internal/Magento/Framework/App/Action/Action.php
+++ lib/internal/Magento/Framework/App/Action/Action.php
@@ -5,7 +5,6 @@
  */
 namespace Magento\Framework\App\Action;
 
-use Magento\Backend\App\AbstractAction;
 use Magento\Framework\App\RequestInterface;
 use Magento\Framework\App\ResponseInterface;
 use Magento\Framework\Controller\ResultInterface;
