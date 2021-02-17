c98919be31d65f2ca6d00a54e0f3a7c877029b69
Gabriel Galvao da Gama <galvaoda@adobe.com>
2021-02-17 13:20:09 +0000

    Added unit test

--- dev/tests/js/jasmine/tests/app/code/Magento/Customer/frontend/js/customer-data.test.js
+++ dev/tests/js/jasmine/tests/app/code/Magento/Customer/frontend/js/customer-data.test.js
@@ -510,6 +510,23 @@ define([
             });
         });
 
+        describe('"onAjaxComplete" method', function () {
+            it('Should not trigger reload if sections is empty', function () {
+                var event, xhr, settings;
+
+                event = jasmine.createSpy().and.returnValue($.event);
+                xhr = jasmine.createSpy();
+                spyOn(sectionConfig, 'getAffectedSections').and.returnValue([]);
+                spyOn(obj, 'reload');
+                settings = {
+                    type: "POST",
+                    url: "http://test.local"
+                };
+                obj.onAjaxComplete(event, xhr, settings);
+                expect(obj.reload).not.toHaveBeenCalled();
+            });
+        });
+
         describe('"Magento_Customer/js/customer-data" method', function () {
             it('Should be defined', function () {
                 expect(obj.hasOwnProperty('Magento_Customer/js/customer-data')).toBeDefined();
