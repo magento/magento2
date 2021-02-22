2b196be20e00558ea3ad21781e7ca6df815f277c
Joan He <johe@adobe.com>
2021-02-22 14:21:17 -0600

    B2B-1610: Upgrade to Flysystem 2.0
    - Fix MFTF test failure

--- app/code/Magento/AwsS3/Test/Mftf/Helper/S3FileAssertions.php
+++ app/code/Magento/AwsS3/Test/Mftf/Helper/S3FileAssertions.php
@@ -9,7 +9,7 @@ namespace Magento\AwsS3\Test\Mftf\Helper;
 
 use Aws\S3\S3Client;
 use Codeception\Lib\ModuleContainer;
-use League\Flysystem\AwsS3v3\AwsS3Adapter;
+use League\Flysystem\AwsS3V3\AwsS3V3Adapter;
 use Magento\AwsS3\Driver\AwsS3;
 use Magento\FunctionalTestingFramework\Helper\Helper;
 use Magento\Framework\Filesystem\DriverInterface;
@@ -56,7 +56,7 @@ class S3FileAssertions extends Helper
         }
 
         $client = new S3Client($config);
-        $adapter = new AwsS3Adapter($client, $config['bucket'], $prefix);
+        $adapter = new AwsS3V3Adapter($client, $config['bucket'], $prefix);
         $objectUrl = $client->getObjectUrl($adapter->getBucket(), $adapter->applyPathPrefix('.'));
         $s3Driver = new AwsS3($adapter, new MockTestLogger(), $objectUrl);
 
