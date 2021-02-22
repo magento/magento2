8c0407b8219a3c89b1f97975489ba30f49c09869
taras.gamanov <engcom-vendorworker-hotel@adobe.com>
2021-02-22 15:43:39 +0200

    Test has been added

---
+++ dev/tests/integration/testsuite/Magento/Bundle/Model/Product/LinksListTest.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Bundle\Model\Product;
+
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Catalog\Model\Product;
+use Magento\Framework\ObjectManagerInterface;
+use Magento\TestFramework\Helper\Bootstrap;
+use Magento\TestModuleBundle\Block\Index as TestModuleBundleBlockIndex;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Integration test for Magento\Bundle\Model\OptionList
+ */
+class LinksListTest extends TestCase
+{
+    /**
+     * @var Product
+     */
+    protected $product;
+
+    /**
+     * @var ObjectManagerInterface
+     */
+    protected $objectManager;
+
+    /**
+     * Set up
+     */
+    protected function setUp(): void
+    {
+        $this->objectManager = Bootstrap::getObjectManager();
+    }
+
+    /**
+     * Assert that added extension attributes wont affect getItems method
+     *
+     * @magentoDataFixture Magento/Bundle/_files/product.php
+     * @magentoDbIsolation disabled
+     */
+    public function testGetBundleById(): void
+    {
+        /** @var ProductRepositoryInterface $productRepository */
+        $productRepository = $this->objectManager->create(ProductRepositoryInterface::class);
+        $this->product = $productRepository->get('bundle-product');
+
+        $indexBlock = $this->objectManager->create(TestModuleBundleBlockIndex::class);
+        $bundleById = $indexBlock->getBundleById($this->product->getEntityId());
+
+        $this->assertEquals($this->product, $bundleById);
+    }
+}
