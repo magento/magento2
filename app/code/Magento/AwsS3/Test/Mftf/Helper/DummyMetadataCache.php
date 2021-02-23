9361d291b67418a0d3d4c8e8ff4a30c04aabd4a9
Roman Hanin <rganin@ip-192-168-1-7.ec2.internal>
2021-02-22 20:53:39 -0600

    B2B-1610: Upgrade to Flysystem 2.0
    - Fix MFTF test assertions

---
+++ app/code/Magento/AwsS3/Test/Mftf/Helper/DummyMetadataCache.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\AwsS3\Test\Mftf\Helper;
+
+/**
+ * Cache mock for metadata provider.
+ */
+class DummyMetadataCache implements \Magento\RemoteStorage\Driver\Adapter\Cache\CacheInterface
+{
+    /**
+     * @inheirtDoc
+     */
+    public function exists(string $path): bool
+    {
+        return false;
+    }
+
+    /**
+     * @inheirtDoc
+     */
+    public function getFileContents(string $path): ?array
+    {
+        return null;
+    }
+
+    /**
+     * @inheirtDoc
+     */
+    public function getMetadata(string $path): ?array
+    {
+        return null;
+    }
+
+    /**
+     * @inheirtDoc
+     */
+    public function isDirListingComplete(string $dirname, bool $recursive): bool
+    {
+        return false;
+    }
+
+    /**
+     * @inheirtDoc
+     */
+    public function setDirListingComplete(string $dirname, bool $recursive): void
+    {
+    }
+
+    /**
+     * @inheirtDoc
+     */
+    public function flushCache(): void
+    {
+    }
+
+    /**
+     * @inheirtDoc
+     */
+    public function persist(): void
+    {
+    }
+
+    /**
+     * @inheirtDoc
+     */
+    public function load(): void
+    {
+    }
+
+    /**
+     * @inheirtDoc
+     */
+    public function moveFile(string $path, string $newpath): void
+    {
+    }
+
+    /**
+     * @inheirtDoc
+     */
+    public function copyFile(string $path, string $newpath): void
+    {
+    }
+
+    /**
+     * @inheirtDoc
+     */
+    public function deleteFile(string $path): void
+    {
+    }
+
+    /**
+     * @inheirtDoc
+     */
+    public function deleteDir(string $dirname): void
+    {
+    }
+
+    /**
+     * @inheirtDoc
+     */
+    public function updateMetadata(string $path, array $objectMetadata, bool $persist = false): void
+    {
+    }
+
+    /**
+     * @inheirtDoc
+     */
+    public function resetData(string $path): void
+    {
+    }
+}
--- app/code/Magento/AwsS3/Test/Mftf/Helper/S3FileAssertions.php
+++ app/code/Magento/AwsS3/Test/Mftf/Helper/S3FileAssertions.php
@@ -14,6 +14,7 @@ use League\Flysystem\PathPrefixer;
 use Magento\AwsS3\Driver\AwsS3;
 use Magento\FunctionalTestingFramework\Helper\Helper;
 use Magento\Framework\Filesystem\DriverInterface;
+use Magento\RemoteStorage\Driver\Adapter\MetadataProvider;
 
 /**
  * Class for MFTF helpers for doing file assertions using S3.
@@ -60,7 +61,8 @@ class S3FileAssertions extends Helper
         $adapter = new AwsS3V3Adapter($client, $config['bucket'], $prefix);
         $prefixer = new PathPrefixer($prefix);
         $objectUrl = $client->getObjectUrl($config['bucket'], ltrim($prefixer->prefixPath('.'), '/'));
-        $s3Driver = new AwsS3($adapter, new MockTestLogger(), $objectUrl);
+        $metadataProvider = new MetadataProvider($adapter, new DummyMetadataCache());
+        $s3Driver = new AwsS3($adapter, new MockTestLogger(), $objectUrl, $metadataProvider);
 
         $this->driver = $s3Driver;
     }
