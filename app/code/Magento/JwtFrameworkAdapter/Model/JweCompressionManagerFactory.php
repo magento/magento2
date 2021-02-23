4fb151c18e7a8dec9369fa326e562db4b53684a1
ogorkun <ogorkun@adobe.com>
2021-02-23 09:28:04 -0600

    MC-38539: Introduce JWT wrapper

--- app/code/Magento/JwtFrameworkAdapter/Model/JweCompressionManagerFactory.php
+++ app/code/Magento/JwtFrameworkAdapter/Model/JweCompressionManagerFactory.php
@@ -9,24 +9,12 @@ declare(strict_types=1);
 namespace Magento\JwtFrameworkAdapter\Model;
 
 use Jose\Component\Encryption\Compression\CompressionMethodManager;
+use Jose\Component\Encryption\Compression\Deflate;
 
 class JweCompressionManagerFactory
 {
-    /**
-     * @var \Jose\Component\Encryption\Compression\CompressionMethod[]
-     */
-    private $methods;
-
-    /**
-     * @param \Jose\Component\Encryption\Compression\CompressionMethod[] $methods
-     */
-    public function __construct(array $methods)
-    {
-        $this->methods = $methods;
-    }
-
     public function create(): CompressionMethodManager
     {
-        return new CompressionMethodManager($this->methods);
+        return new CompressionMethodManager([new Deflate()]);
     }
 }
