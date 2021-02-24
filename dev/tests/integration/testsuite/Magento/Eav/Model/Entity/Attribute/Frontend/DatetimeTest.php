6327c2c292ca92819df08620a4900591278ad30c
Yurii Sapiha <yurasapiga93@gmail.com>
2021-02-24 11:37:18 +0200

    MC-40865: Create automated test for: ""Date and Time" attribute correctly rendered regarding timezone settings"

--- dev/tests/integration/testsuite/Magento/Eav/Model/Entity/Attribute/Frontend/DatetimeTest.php
+++ dev/tests/integration/testsuite/Magento/Eav/Model/Entity/Attribute/Frontend/DatetimeTest.php
@@ -12,11 +12,13 @@ use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Framework\ObjectManagerInterface;
 use Magento\Framework\Stdlib\DateTime\DateTime;
 use Magento\TestFramework\Helper\Bootstrap;
-use Magento\Store\Model\StoreManagerInterface;
+use Magento\TestFramework\Store\ExecuteInStoreContext;
 use PHPUnit\Framework\TestCase;
 
 /**
  * Checks Datetime attribute's frontend model
+ *
+ * @magentoAppArea frontend
  *
  * @see \Magento\Eav\Model\Entity\Attribute\Frontend\Datetime
  */
@@ -43,25 +45,28 @@ class DatetimeTest extends TestCase
     private $productRepository;
 
     /**
-     * @var StoreManagerInterface
-     */
-    private $storeManager;
-
-    /**
      * @var DateTime
      */
     private $dateTime;
 
+    /**
+     * @var ExecuteInStoreContext
+     */
+    private $executeInStoreContext;
+
+    /**
+     * @inheritdoc
+     */
     protected function setUp(): void
     {
         parent::setUp();
 
         $this->objectManager = Bootstrap::getObjectManager();
-        $this->storeManager = $this->objectManager->get(StoreManagerInterface::class);
         $this->attributeRepository = $this->objectManager->get(ProductAttributeRepositoryInterface::class);
         $this->productRepository = $this->objectManager->get(ProductRepositoryInterface::class);
         $this->productRepository->cleanCache();
         $this->dateTime = $this->objectManager->create(DateTime::class);
+        $this->executeInStoreContext = $this->objectManager->get(ExecuteInStoreContext::class);
     }
 
     /**
@@ -80,14 +85,16 @@ class DatetimeTest extends TestCase
         $attribute = $this->attributeRepository->get('datetime_attribute');
         $product = $this->productRepository->get('simple-on-two-websites');
         $product->setDatetimeAttribute($this->dateTime->date('Y-m-d H:i:s'));
-        $valueOnWebsiteOne = $attribute->getFrontend()->getValue($product);
-        $secondStoreId = $this->storeManager->getStore('fixture_second_store')->getId();
-        $this->storeManager->setCurrentStore($secondStoreId);
-        $valueOnWebsiteTwo = $attribute->getFrontend()->getValue($product);
+        $firstWebsiteValue = $attribute->getFrontend()->getValue($product);
+        $secondWebsiteValue = $this->executeInStoreContext->execute(
+            'fixture_second_store',
+            [$attribute->getFrontend(), 'getValue'],
+            $product
+        );
         $this->assertEquals(
             self::ONE_HOUR_IN_MILLISECONDS,
-            $this->dateTime->gmtTimestamp($valueOnWebsiteOne) - $this->dateTime->gmtTimestamp($valueOnWebsiteTwo),
-            'The difference between the two time zones are incorrect'
+            $this->dateTime->gmtTimestamp($firstWebsiteValue) - $this->dateTime->gmtTimestamp($secondWebsiteValue),
+            'The difference between values per different timezones is incorrect'
         );
     }
 }
