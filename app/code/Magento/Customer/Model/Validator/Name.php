781e4b166f63da8d1bd4d63ecd4deaa5dcc99a5c
Viktor Sevch <viktor.sevch@transoftgroup.com>
2021-02-24 10:49:11 +0200

    MC-39885: Improve customer api

---
+++ app/code/Magento/Customer/Model/Validator/Name.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\Customer\Model\Validator;
+
+use Magento\Customer\Model\Customer;
+use Magento\Framework\Validator\AbstractValidator;
+
+/**
+ * Customer name fields validator.
+ */
+class Name extends AbstractValidator
+{
+    /**
+     * Validate name fields.
+     *
+     * @param Customer $customer
+     * @return bool
+     */
+    public function isValid($customer)
+    {
+        if (!$this->isValidName($customer->getFirstname())) {
+            parent::_addMessages([['firstname' => 'First Name is not valid!']]);
+        }
+
+        if (!$this->isValidName($customer->getLastname())) {
+            parent::_addMessages([['lastname' => 'Last Name is not valid!']]);
+        }
+
+        if (!$this->isValidName($customer->getMiddlename())) {
+            parent::_addMessages([['middlename' => 'Middle Name is not valid!']]);
+        }
+
+        return count($this->_messages) == 0;
+    }
+
+    /**
+     * Check if name field is valid.
+     *
+     * @param string|null $nameValue
+     * @return bool
+     */
+    private function isValidName($nameValue)
+    {
+        if ($nameValue != null) {
+            $pattern = '/(?:[\p{L}\p{M}\,\-\_\.\'\"\s\d]){1,255}+/u';
+            if (preg_match($pattern, $nameValue, $matches)) {
+                return $matches[0] == $nameValue;
+            }
+        }
+
+        return true;
+    }
+}
