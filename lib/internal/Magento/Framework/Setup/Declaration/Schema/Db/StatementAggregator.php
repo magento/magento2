aa30a6c3aa746196fad57e7049bdef3a57a4ca63
Oleh Usik <o.usik@atwix.com>
2021-02-25 18:09:18 +0200

    fixed static failed test

--- lib/internal/Magento/Framework/Setup/Declaration/Schema/Db/StatementAggregator.php
+++ lib/internal/Magento/Framework/Setup/Declaration/Schema/Db/StatementAggregator.php
@@ -10,6 +10,8 @@ namespace Magento\Framework\Setup\Declaration\Schema\Db;
  * Statement aggregator.
  *
  * Statements are concatenated conditionally, decides which statements go separately and which may be concatenated.
+ *
+ * @api
  */
 class StatementAggregator
 {
