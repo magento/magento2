df17fb62d54b552b686bdd0c18b9c9b35ea939c2
David Haecker <dhaecker@magento.com>
2021-02-26 08:03:33 -0600

    B2B-1704: Add MFTF test for MC-38621
     - Cleaning up tax helpers

--- app/code/Magento/Tax/Test/Mftf/Helper/TaxHelpers.php
+++ app/code/Magento/Tax/Test/Mftf/Helper/TaxHelpers.php
@@ -7,10 +7,8 @@ declare(strict_types=1);
 
 namespace Magento\Tax\Test\Mftf\Helper;
 
-use Facebook\WebDriver\Remote\RemoteWebDriver as FacebookWebDriver;
 use Facebook\WebDriver\WebDriverBy;
 use Magento\FunctionalTestingFramework\Helper\Helper;
-use Magento\FunctionalTestingFramework\Module\MagentoWebDriver;
 
 /**
  * Class for MFTF helpers for Tax module.
@@ -36,12 +34,11 @@ class TaxHelpers extends Helper
         string $successMessageContainer
     ): void {
         try {
-            /** @var MagentoWebDriver $webDriver */
             $magentoWebDriver = $this->getModule('\Magento\FunctionalTestingFramework\Module\MagentoWebDriver');
-            /** @var FacebookWebDriver $webDriver */
-            $webDriver = $magentoWebDriver->webDriver;
+            $facebookWebDriver = $magentoWebDriver->webDriver;
+
             $magentoWebDriver->waitForPageLoad(30);
-            $rows = $webDriver->findElements(WebDriverBy::xpath($rowsToDelete));
+            $rows = $facebookWebDriver->findElements(WebDriverBy::xpath($rowsToDelete));
             while (!empty($rows)) {
                 $rows[0]->click();
                 $magentoWebDriver->waitForPageLoad(30);
@@ -52,7 +49,7 @@ class TaxHelpers extends Helper
                 $magentoWebDriver->click($modalAcceptButton);
                 $magentoWebDriver->waitForPageLoad(60);
                 $magentoWebDriver->waitForText($successMessage, 10, $successMessageContainer);
-                $rows = $webDriver->findElements(WebDriverBy::xpath($rowsToDelete));
+                $rows = $facebookWebDriver->findElements(WebDriverBy::xpath($rowsToDelete));
             }
         } catch (\Exception $exception) {
             $this->fail($exception->getMessage());
