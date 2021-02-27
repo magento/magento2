f7903ab4de011ed44572d38f30ad66943dc05dd4
Cristian Partica <cpartica@magento.com>
2021-02-26 18:16:03 -0600

    PWA-1326: Implement the schema changes for Configurable Options Selection
    - renaming metadata back to original name for comparison purposes

--- app/code/Magento/ConfigurableProductGraphQl/Model/Resolver/OptionsSelectionMetadata.php
+++ app/code/Magento/ConfigurableProductGraphQl/Model/Resolver/OptionsSelectionMetadata.php
@@ -3,6 +3,7 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types=1);
 
 namespace Magento\ConfigurableProductGraphQl\Model\Resolver;
 
@@ -19,7 +20,7 @@ use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
 /**
  * Resolver for options selection
  */
-class OptionsSelection implements ResolverInterface
+class OptionsSelectionMetadata implements ResolverInterface
 {
     /**
      * @var Metadata
