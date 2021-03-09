141159891814dbb379c19548c89fe02fcde4e7ed
Vitaliy Prokopoov <v.prokopov@atwix.com>
2021-03-09 09:44:05 +0200

    Reverted changes for http get action

--- lib/internal/Magento/Framework/App/Action/HttpGetActionInterface.php
+++ lib/internal/Magento/Framework/App/Action/HttpGetActionInterface.php
@@ -8,14 +8,12 @@ declare(strict_types=1);
 
 namespace Magento\Framework\App\Action;
 
-use Magento\Framework\App\ActionInterface;
-
 /**
  * Marker for actions processing GET requests.
  *
  * @api
  */
-interface HttpGetActionInterface extends ActionInterface
+interface HttpGetActionInterface extends HttpHeadActionInterface
 {
 
 }
