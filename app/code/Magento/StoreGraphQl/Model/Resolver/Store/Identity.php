f4f290d072b41e14e7fd1f3a2d64b73f1ec276f5
Lewis Voncken <lewis@experius.nl>
2021-03-19 16:20:21 +0100

    [FEATURE][issue#32554] Added caching to the storeConfig GraphQl Endpoint

---
+++ app/code/Magento/StoreGraphQl/Model/Resolver/Store/Identity.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\StoreGraphQl\Model\Resolver\Store;
+
+use Magento\Framework\GraphQl\Query\Resolver\IdentityInterface;
+use Magento\Framework\App\Config;
+
+class Identity implements IdentityInterface
+{
+    /**
+     * @var string
+     */
+    private $cacheTag = Config::CACHE_TAG;
+
+    /**
+     * @inheritDoc
+     */
+    public function getIdentities(array $resolvedData): array
+    {
+        $ids =  empty($resolvedData) ?
+            [] : array_merge([$this->cacheTag], array_map(function ($key) {
+                return sprintf('%s_%s', $this->cacheTag, $key);
+            }, array_keys($resolvedData)));
+        return $ids;
+    }
+}
