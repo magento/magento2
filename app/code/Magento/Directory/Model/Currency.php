6449fc93ca1ca5d16a59c61060908a1ea09fa653
Dmytro Poperechnyy <dpoperechnyy@magento.com>
2021-04-01 14:43:12 -0500

    MCP-288: [Load Cart Section] Replace Zend_Currency component with Intl NumberFormatter
    - Get currency code by locale in Numberformatter;

--- app/code/Magento/Directory/Model/Currency.php
+++ app/code/Magento/Directory/Model/Currency.php
@@ -410,7 +410,7 @@ class Currency extends \Magento\Framework\Model\AbstractModel
         $this->setOptions($options);
 
         $formattedCurrency = $this->numberFormatter->formatCurrency(
-            $price, $this->getCode() ?? LocaleCurrency::DEFAULT_CURRENCY
+            $price, $this->getCode() ?? $this->numberFormatter->getTextAttribute(\NumberFormatter::CURRENCY_CODE)
         );
 
         if (array_key_exists(LocaleCurrency::CURRENCY_OPTION_DISPLAY, $options)
