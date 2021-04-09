710c277936aad8acfd59116a49ba336fe773a402
OlgaVasyltsun <olga.vasyltsun@transoftgroup.com>
2021-04-09 12:32:57 +0300

    MC-37820: [UX] Payment methods list is not refreshed on billing address change

--- app/code/Magento/Checkout/view/frontend/web/js/view/billing-address.js
+++ app/code/Magento/Checkout/view/frontend/web/js/view/billing-address.js
@@ -165,7 +165,6 @@ function (
                     checkoutData.setNewCustomerBillingAddress(addressData);
                 }
             }
-            setBillingAddressAction(globalMessageList);
             this.updateAddresses();
         },
 
