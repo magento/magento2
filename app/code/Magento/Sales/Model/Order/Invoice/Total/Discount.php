c789a531f0b0d6d3c693badc7865406ac7d021be
rostyslav-hymon <rostyslav.hymon@transoftgroup.com>
2021-04-13 11:36:29 +0300

    MC-41438: 100% Discount cart rule results in Tax invoiced

--- app/code/Magento/Sales/Model/Order/Invoice/Total/Discount.php
+++ app/code/Magento/Sales/Model/Order/Invoice/Total/Discount.php
@@ -71,11 +71,9 @@ class Discount extends AbstractTotal
         $invoice->setDiscountAmount(-$totalDiscountAmount);
         $invoice->setBaseDiscountAmount(-$baseTotalDiscountAmount);
 
-        $grandTotalWithoutDiscount = $invoice->getGrandTotal() - $totalDiscountAmount;
-        $grandTotal = $grandTotalWithoutDiscount > 0 && $grandTotalWithoutDiscount < 0.0001
+        $grandTotal = abs($invoice->getGrandTotal() - $totalDiscountAmount) < 0.0001
             ? 0 : $invoice->getGrandTotal() - $totalDiscountAmount;
-        $baseGrandTotalWithoutDiscount = $invoice->getBaseGrandTotal() - $baseTotalDiscountAmount;
-        $baseGrandTotal = $baseGrandTotalWithoutDiscount > 0 && $baseGrandTotalWithoutDiscount < 0.0001
+        $baseGrandTotal = abs($invoice->getBaseGrandTotal() - $baseTotalDiscountAmount) < 0.0001
             ? 0 : $invoice->getBaseGrandTotal() - $baseTotalDiscountAmount;
         $invoice->setGrandTotal($grandTotal);
         $invoice->setBaseGrandTotal($baseGrandTotal);
