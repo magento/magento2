c0c3c4e2eed5e68bfbe622641351ba5ea9a60242
Ben Tideswell <ben@fishpig.co.uk>
2021-05-03 22:14:56 +0000

    Update scripts.js
    Extended fix to cover 2 other instances of the same issue in the same file.

--- app/code/Magento/Sales/view/adminhtml/web/order/create/scripts.js
+++ app/code/Magento/Sales/view/adminhtml/web/order/create/scripts.js
@@ -1107,7 +1107,7 @@ define([
         },
 
         accountFieldChange: function () {
-            this.saveData(this.serializeData('order-form_account'));
+            this.saveData(this.serializeData('order-form_account').toObject());
         },
 
         commentFieldsBind: function (container) {
@@ -1119,7 +1119,7 @@ define([
         },
 
         commentFieldChange: function () {
-            this.saveData(this.serializeData('order-comment'));
+            this.saveData(this.serializeData('order-comment').toObject());
         },
 
         giftmessageFieldsBind: function (container) {
