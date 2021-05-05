d0c3fc2ee13770eb76a35b0644b0c1bd3da49ce1
Vasya Tsviklinskyi <tsviklinskyi@gmail.com>
2021-05-05 10:29:59 +0300

    MC-41810: Cannot add bundle product with more than 1 checkbox option to the cart via Graphql

--- app/code/Magento/Bundle/Model/Product/Type.php
+++ app/code/Magento/Bundle/Model/Product/Type.php
@@ -1231,8 +1231,12 @@ class Type extends \Magento\Catalog\Model\Product\Type\AbstractType
     protected function getQty($selection, $qtys, $selectionOptionId)
     {
         if ($selection->getSelectionCanChangeQty() && isset($qtys[$selectionOptionId])) {
-            $selectionQty = $qtys[$selectionOptionId][$selection->getSelectionId()];
-            $qty = (float)$selectionQty > 0 ? $selectionQty : 1;
+            if (is_array($qtys[$selectionOptionId]) && isset($qtys[$selectionOptionId][$selection->getSelectionId()])) {
+                $selectionQty = $qtys[$selectionOptionId][$selection->getSelectionId()];
+                $qty = (float)$selectionQty > 0 ? $selectionQty : 1;
+            } else {
+                $qty = (float)$qtys[$selectionOptionId] > 0 ? $qtys[$selectionOptionId] : 1;
+            }
         } else {
             $qty = (float)$selection->getSelectionQty() ? $selection->getSelectionQty() : 1;
         }
