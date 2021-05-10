965013da42a6b5e16202ad86e909f75832981930
Sergio Vera <svera@adobe.com>
2021-05-10 13:02:27 +0200

    MC-41899: Fix jQuery nodeName calls

--- app/code/Magento/PageCache/view/frontend/web/js/page-cache.js
+++ app/code/Magento/PageCache/view/frontend/web/js/page-cache.js
@@ -65,7 +65,7 @@ define([
             contents = function (elem) {
                 return $.map(elem, function (el) {
                     try {
-                        return $.nodeName(el, 'iframe') ?
+                        return elem.nodeName.toLowerCase() === 'iframe' ?
                             el.contentDocument || (el.contentWindow ? el.contentWindow.document : []) :
                             $.merge([], el.childNodes);
                     } catch (e) {
