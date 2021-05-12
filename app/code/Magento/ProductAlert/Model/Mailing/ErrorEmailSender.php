89024af204f451333b11f018a1f223da63f234ff
Serhii Balko <serhii.balko@transogtgroup.com>
2021-05-12 16:32:37 +0300

    MC-41917: Issue with 'catalog_product_alert' cron

---
+++ app/code/Magento/ProductAlert/Model/Mailing/ErrorEmailSender.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\ProductAlert\Model\Mailing;
+
+use Magento\Backend\App\Area\FrontNameResolver;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\Mail\Template\TransportBuilder;
+use Magento\Framework\Translate\Inline\StateInterface;
+use Magento\Store\Model\ScopeInterface;
+
+/**
+ * Class to send error emails to administrator
+ */
+class ErrorEmailSender
+{
+    /**
+     * Error email template configuration
+     */
+    private const XML_PATH_ERROR_TEMPLATE = 'catalog/productalert_cron/error_email_template';
+
+    /**
+     * Error email identity configuration
+     */
+    private const XML_PATH_ERROR_IDENTITY = 'catalog/productalert_cron/error_email_identity';
+
+    /**
+     * 'Send error emails to' configuration
+     */
+    private const XML_PATH_ERROR_RECIPIENT = 'catalog/productalert_cron/error_email';
+
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
+     * @var TransportBuilder
+     */
+    private $transportBuilder;
+
+    /**
+     * @var StateInterface
+     */
+    private $inlineTranslation;
+
+    /**
+     * @param ScopeConfigInterface $scopeConfig
+     * @param TransportBuilder $transportBuilder
+     * @param StateInterface $inlineTranslation
+     */
+    public function __construct(
+        ScopeConfigInterface $scopeConfig,
+        TransportBuilder $transportBuilder,
+        StateInterface $inlineTranslation
+    ) {
+        $this->scopeConfig = $scopeConfig;
+        $this->transportBuilder = $transportBuilder;
+        $this->inlineTranslation = $inlineTranslation;
+    }
+
+    /**
+     * Send error emails to administrator
+     *
+     * @param array $errors
+     * @param int $storeId
+     */
+    public function execute(array $errors, int $storeId)
+    {
+        if (!count($errors)) {
+            return;
+        }
+
+        if (!$this->scopeConfig->getValue(self::XML_PATH_ERROR_TEMPLATE, ScopeInterface::SCOPE_STORE, $storeId)) {
+            return;
+        }
+
+        $this->inlineTranslation->suspend();
+        $transport = $this->transportBuilder->setTemplateIdentifier(
+            $this->scopeConfig->getValue(
+                self::XML_PATH_ERROR_TEMPLATE,
+                ScopeInterface::SCOPE_STORE,
+                $storeId
+            )
+        )->setTemplateOptions(
+            [
+                'area' => FrontNameResolver::AREA_CODE,
+                'store' => $storeId,
+            ]
+        )->setTemplateVars(
+            ['warnings' => join("\n", $errors)]
+        )->setFromByScope(
+            $this->scopeConfig->getValue(
+                self::XML_PATH_ERROR_IDENTITY,
+                ScopeInterface::SCOPE_STORE,
+                $storeId
+            ),
+            $storeId
+        )->addTo(
+            $this->scopeConfig->getValue(
+                self::XML_PATH_ERROR_RECIPIENT,
+                ScopeInterface::SCOPE_STORE,
+                $storeId
+            )
+        )->getTransport();
+
+        $transport->sendMessage();
+
+        $this->inlineTranslation->resume();
+    }
+}
