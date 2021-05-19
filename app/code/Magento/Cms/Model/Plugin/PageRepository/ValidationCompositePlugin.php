f41f4d48891a01e07d948152ffdf793f822169e8
silinmykola <m.silin@atwix.com>
2021-05-19 11:52:48 +0300

    32913 set url key for page before validation

---
+++ app/code/Magento/Cms/Model/Plugin/PageRepository/ValidationCompositePlugin.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Cms\Model\Plugin\PageRepository;
+
+use Magento\Cms\Api\Data\PageInterface;
+use Magento\Cms\Model\PageRepository\ValidationComposite;
+use Magento\CmsUrlRewrite\Model\CmsPageUrlPathGenerator;
+
+/**
+ * Generate url_key if the merchant didn't fill this field
+ */
+class ValidationCompositePlugin
+{
+    /**
+     * @var CmsPageUrlPathGenerator
+     */
+    private $cmsPageUrlPathGenerator;
+
+    /**
+     * @param CmsPageUrlPathGenerator $cmsPageUrlPathGenerator
+     */
+    public function __construct(
+        CmsPageUrlPathGenerator $cmsPageUrlPathGenerator
+    ) {
+        $this->cmsPageUrlPathGenerator = $cmsPageUrlPathGenerator;
+    }
+
+    /**
+     * Before save handler
+     *
+     * @param ValidationComposite $subject
+     * @param PageInterface $page
+     */
+    public function beforeSave(
+        ValidationComposite $subject,
+        PageInterface $page
+    ) {
+        $urlKey = $page->getData('identifier');
+        if ($urlKey === '' || $urlKey === null) {
+            $page->setData('identifier', $this->cmsPageUrlPathGenerator->generateUrlKey($page));
+        }
+    }
+}
