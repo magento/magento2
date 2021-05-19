485ef768e6d76f44ba89a32aaf492f9fcee9f6ee
silinmykola <m.silin@atwix.com>
2021-05-19 19:19:19 +0300

    32913 changed plugin for url rewrite

--- app/code/Magento/CmsUrlRewrite/Plugin/Cms/Model/PageRepository/ValidationCompositePlugin.php
+++ app/code/Magento/CmsUrlRewrite/Plugin/Cms/Model/PageRepository/ValidationCompositePlugin.php
@@ -3,7 +3,9 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Magento\Cms\Model\Plugin\PageRepository;
+declare(strict_types=1);
+
+namespace Magento\CmsUrlRewrite\Plugin\Cms\Model\PageRepository;
 
 use Magento\Cms\Api\Data\PageInterface;
 use Magento\Cms\Model\PageRepository\ValidationComposite;
