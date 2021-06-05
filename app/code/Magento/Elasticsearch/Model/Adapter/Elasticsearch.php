4c6afc201436cb367ddaaced90193dee70ec79e0
Roman Hanin <rganin@ip-192-168-1-10.ec2.internal>
2021-06-04 22:39:25 -0500

    B2B-1832: Error when reindex Catalog Search index during the snapshot of elasticsearch

--- app/code/Magento/Elasticsearch/Model/Adapter/Elasticsearch.php
+++ app/code/Magento/Elasticsearch/Model/Adapter/Elasticsearch.php
@@ -239,12 +239,23 @@ class Elasticsearch
         // prepare new index name and increase version
         $indexPattern = $this->indexNameResolver->getIndexPattern($storeId, $mappedIndexerId);
         $version = (int)(str_replace($indexPattern, '', $indexName));
-        $newIndexName = $indexPattern . (++$version);
 
-        // remove index if already exists
-        if ($this->client->indexExists($newIndexName)) {
+        // compatibility with snapshotting collision
+        $deleteQueue = [];
+        do {
+            $newIndexName = $indexPattern . (++$version);
+            if ($this->client->indexExists($newIndexName)) {
+                $deleteQueue []= $newIndexName;
+                $indexExists = true;
+            } else {
+                $indexExists = false;
+            }
+        } while ($indexExists);
+
+        foreach ($deleteQueue as $indexToDelete) {
+            // remove index if already exists
             try {
-                $this->client->deleteIndex($newIndexName);
+                $this->client->deleteIndex($indexToDelete);
             } catch (\Exception $e) {
                 $this->logger->critical($e);
             }
