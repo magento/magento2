d4d19ba90a6a362e84857546654ca5b6151aa0bf
Matt Krupnik <mateusz.krupnik@gmail.com>
2021-06-09 10:52:56 +0200

    Set default cache life time value 86400

--- lib/internal/Magento/Framework/View/Element/AbstractBlock.php
+++ lib/internal/Magento/Framework/View/Element/AbstractBlock.php
@@ -1101,12 +1101,12 @@ abstract class AbstractBlock extends \Magento\Framework\DataObject implements Bl
     protected function getCacheLifetime()
     {
         if (!$this->hasData('cache_lifetime')) {
-            return null;
+            return 86400;
         }
 
         $cacheLifetime = $this->getData('cache_lifetime');
         if (false === $cacheLifetime || null === $cacheLifetime) {
-            return null;
+            return 86400;
         }
 
         return (int)$cacheLifetime;
