9f2af737c86147d0bba79b02ecc539c493a45404
Milan Osztromok <tufa.hu@gmail.com>
2021-07-06 09:52:41 +0200

    Apply suggestions from code review
    Remove return types due to backward compatibility
    
    Co-authored-by: Ihor Sviziev <ihor-sviziev@users.noreply.github.com>

--- lib/internal/Magento/Framework/Escaper.php
+++ lib/internal/Magento/Framework/Escaper.php
@@ -289,7 +289,7 @@ class Escaper
      * @return string
      * @since 101.0.0
      */
-    public function encodeUrlParam(string $string): string
+    public function encodeUrlParam(string $string)
     {
         return $this->getEscaper()->escapeUrl($string);
     }
@@ -328,7 +328,7 @@ class Escaper
      * @return string
      * @since 101.0.0
      */
-    public function escapeCss(string $string): string
+    public function escapeCss(string $string)
     {
         return $this->getEscaper()->escapeCss($string);
     }
