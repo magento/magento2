197063aa220e8af9907673c9f04ec5641ffa5fcf
eduard13 <e.chitoraga@atwix.com>
2021-07-21 08:50:23 +0300

    Removing the deprecated JS files

--- app/code/Magento/GiftMessage/view/frontend/requirejs-config.js
+++ app/code/Magento/GiftMessage/view/frontend/requirejs-config.js
@@ -7,9 +7,7 @@ var config = {
     map: {
         '*': {
             giftOptions:    'Magento_GiftMessage/js/gift-options',
-            extraOptions:   'Magento_GiftMessage/js/extra-options',
-            'Magento_GiftMessage/gift-options':    'Magento_GiftMessage/js/gift-options',
-            'Magento_GiftMessage/extra-options':   'Magento_GiftMessage/js/extra-options'
+            'Magento_GiftMessage/gift-options':    'Magento_GiftMessage/js/gift-options'
         }
     }
 };
