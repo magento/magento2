9576465e3de8227a27dd5a6a8700479cdd65b38a
Karyna <k.tsymbal@atwix.com>
2021-07-22 18:35:35 +0300

    Improve code style

--- dev/tests/api-functional/testsuite/Magento/GraphQl/Catalog/ProductHasOptionsTest.php
+++ dev/tests/api-functional/testsuite/Magento/GraphQl/Catalog/ProductHasOptionsTest.php
@@ -7,14 +7,21 @@ declare(strict_types=1);
 
 namespace Magento\GraphQl\Catalog;
 
+use Exception;
 use Magento\TestFramework\TestCase\GraphQlAbstract;
 
+/**
+ * Test for checking has_options & required_options attributes in Products query.
+ */
 class ProductHasOptionsTest extends GraphQlAbstract
 {
     /**
      * @magentoApiDataFixture Magento/Catalog/_files/product_simple_with_custom_options.php
+     *
+     * @return void
+     * @throws Exception
      */
-    public function testHasOptionsAndRequiredOptionsAttribute()
+    public function testHasOptionsAndRequiredOptionsAttribute(): void
     {
         $productSku = 'simple_with_custom_options';
         $query = <<<QUERY
@@ -22,7 +29,7 @@ class ProductHasOptionsTest extends GraphQlAbstract
   products(filter: {sku: {eq: "{$productSku}"}}) {
     items {
       sku
-	  has_options
+      has_options
       required_options
     }
   }
@@ -30,7 +37,7 @@ class ProductHasOptionsTest extends GraphQlAbstract
 QUERY;
         $response = $this->graphQlQuery($query);
 
-        self::assertArrayHasKey('has_options', $response['products']['items'][0]);
-        self::assertArrayHasKey('required_options', $response['products']['items'][0]);
+        $this->assertArrayHasKey('has_options', $response['products']['items'][0]);
+        $this->assertArrayHasKey('required_options', $response['products']['items'][0]);
     }
 }
