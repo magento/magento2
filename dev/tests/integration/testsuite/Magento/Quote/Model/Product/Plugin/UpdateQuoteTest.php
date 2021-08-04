584edb83869dcbb09c4a486bf12f47bd92fe8dd2
D Arularasan <glo02433@adobe.com>
2021-08-04 23:41:46 +0530

    MC-42443: Row Sub Total is not updated with new price when Price is updated through API - Fixed the CR comments.

--- dev/tests/integration/testsuite/Magento/Quote/Model/Product/Plugin/UpdateQuoteTest.php
+++ dev/tests/integration/testsuite/Magento/Quote/Model/Product/Plugin/UpdateQuoteTest.php
@@ -7,15 +7,14 @@ declare(strict_types=1);
 
 namespace Magento\Quote\Model\Product\Plugin;
 
-use Magento\Quote\Model\ResourceModel\Quote as QuoteResource;
 use Magento\TestFramework\Helper\Bootstrap;
 use Magento\TestFramework\Quote\Model\GetQuoteByReservedOrderId;
+use Magento\Catalog\Api\TierPriceStorageInterface;
+use Magento\Catalog\Api\Data\TierPriceInterface;
 use PHPUnit\Framework\TestCase;
 
 /**
  * Tests for update quote plugin.
- *
- * @magentoAppArea adminhtml
  */
 class UpdateQuoteTest extends TestCase
 {
@@ -23,6 +22,16 @@ class UpdateQuoteTest extends TestCase
      * @var GetQuoteByReservedOrderId
      */
     private $getQuoteByReservedOrderId;
+
+    /**
+     * @var TierPriceStorageInterface
+     */
+    private $tierPriceStorage;
+
+    /**
+     * @var TierPriceInterface
+     */
+    private $tierPrice;
 
     /**
      * @inheritdoc
@@ -33,6 +42,8 @@ class UpdateQuoteTest extends TestCase
 
         $objectManager = Bootstrap::getObjectManager();
         $this->getQuoteByReservedOrderId = $objectManager->get(GetQuoteByReservedOrderId::class);
+        $this->tierPriceStorage = $objectManager->get(TierPriceStorageInterface::class);
+        $this->tierPrice = $objectManager->get(TierPriceInterface::class);
     }
 
     /**
@@ -43,16 +54,24 @@ class UpdateQuoteTest extends TestCase
      */
     public function testUpdateQuoteRecollectAfterChangeProductPrice(): void
     {
-        $quote = $this->getQuoteByReservedOrderId->execute('test_order_with_simple_product_without_address');
+        $quoteId = 'test_order_with_simple_product_without_address';
+        $quote = $this->getQuoteByReservedOrderId->execute($quoteId);
         $this->assertNotNull($quote);
         $this->assertFalse((bool)$quote->getTriggerRecollect());
         $this->assertNotEmpty($quote->getItems());
         $quoteItem = current($quote->getItems());
         $product = $quoteItem->getProduct();
 
+        $this->tierPrice->setPrice($product->getPrice());
+        $this->tierPrice->setPriceType('fixed');
+        $this->tierPrice->setWebsiteId(0);
+        $this->tierPrice->setSku($product->getSku());
+        $this->tierPrice->setCustomerGroup('ALL GROUPS');
+        $this->tierPrice->setQuantity(1);
+        $this->tierPriceStorage->update([$this->tierPrice]);
+
         /** @var QuoteResource $quoteResource */
         $quoteResource = $quote->getResource();
-        $quoteResource->markQuotesRecollect($product->getId());
         /** @var AdapterInterface $connection */
         $connection = $quoteResource->getConnection();
         $select = $connection->select()
