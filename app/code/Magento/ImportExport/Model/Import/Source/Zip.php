a784ab4aea192e62701632f2e7d7c684ee815584
Joan He <johe@adobe.com>
2021-08-09 14:52:23 -0500

    B2B-1876: New directories in pub/media do not get synced back to local filesystem from S3 Remote Storage
    - fix import export validate issue

--- app/code/Magento/ImportExport/Model/Import/Source/Zip.php
+++ app/code/Magento/ImportExport/Model/Import/Source/Zip.php
@@ -6,6 +6,7 @@
 namespace Magento\ImportExport\Model\Import\Source;
 
 use Magento\Framework\Exception\ValidatorException;
+use Magento\Framework\Filesystem\Directory\WriteInterface;
 
 /**
  * Zip import adapter.
@@ -14,14 +15,14 @@ class Zip extends Csv
 {
     /**
      * @param string $file
-     * @param \Magento\Framework\Filesystem\Directory\Write $directory
+     * @param WriteInterface $directory
      * @param string $options
      * @throws \Magento\Framework\Exception\LocalizedException
      * @throws \Magento\Framework\Exception\ValidatorException
      */
     public function __construct(
         $file,
-        \Magento\Framework\Filesystem\Directory\Write $directory,
+        WriteInterface $directory,
         $options
     ) {
         $zip = new \Magento\Framework\Archive\Zip();
