1be618c15d7e76a60d983c9d5af92bd79bc011f6
Buba Suma <soumah@adobe.com>
2021-08-20 12:07:33 -0500

    MC-42799: [Magento Cloud] Premiere Support - Layered Navigation setting for price range is not working with our custom attributes
    - Add unit test coverage

--- app/code/Magento/CatalogSearch/Test/Unit/Model/Layer/Filter/DecimalTest.php
+++ app/code/Magento/CatalogSearch/Test/Unit/Model/Layer/Filter/DecimalTest.php
@@ -16,6 +16,7 @@ use Magento\CatalogSearch\Model\Layer\Filter\Decimal;
 use Magento\CatalogSearch\Model\ResourceModel\Fulltext\Collection;
 use Magento\Eav\Model\Entity\Attribute;
 use Magento\Framework\App\RequestInterface;
+use Magento\Framework\Pricing\PriceCurrencyInterface;
 use Magento\Framework\TestFramework\Unit\Helper\ObjectManager as ObjectManagerHelper;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
@@ -82,7 +83,15 @@ class DecimalTest extends TestCase
             ->method($this->anything())->willReturnSelf();
         $this->filterItemFactory->expects($this->any())
             ->method('create')
-            ->willReturn($this->filterItem);
+            ->willReturnCallback(
+                function (array $data) {
+                    return new Item(
+                        $this->createMock(\Magento\Framework\UrlInterface::class),
+                        $this->createMock(\Magento\Theme\Block\Html\Pager::class),
+                        $data
+                    );
+                }
+            );
 
         $this->fulltextCollection = $this->getMockBuilder(
             Collection::class
@@ -121,12 +130,20 @@ class DecimalTest extends TestCase
             ->willReturn($this->state);
 
         $objectManagerHelper = new ObjectManagerHelper($this);
+        $priceFormatter = $this->createMock(PriceCurrencyInterface::class);
+        $priceFormatter->method('format')
+            ->willReturnCallback(
+                function ($number) {
+                    return sprintf('$%01.2f', $number);
+                }
+            );
         $this->target = $objectManagerHelper->getObject(
             Decimal::class,
             [
                 'filterItemFactory' => $this->filterItemFactory,
                 'layer' => $this->layer,
                 'filterDecimalFactory' => $filterDecimalFactory,
+                'priceCurrency' => $priceFormatter,
             ]
         );
 
@@ -212,7 +229,12 @@ class DecimalTest extends TestCase
         $this->target->apply($this->request);
     }
 
-    public function testItemData()
+    /**
+     * @param array $facets
+     * @param array $expected
+     * @dataProvider itemDataDataProvider
+     */
+    public function testItemData(array $facets, array $expected): void
     {
         $this->fulltextCollection->expects($this->any())
             ->method('getSize')
@@ -220,15 +242,41 @@ class DecimalTest extends TestCase
 
         $this->fulltextCollection->expects($this->any())
             ->method('getFacetedData')
-            ->willReturn([
-                '2_10' => ['count' => 5],
-                '*_*' => ['count' => 2]
-            ]);
-        $this->assertEquals(
+            ->willReturn($facets);
+        $actual = [];
+        foreach ($this->target->getItems() as $item) {
+            $actual[] = ['label' => $item->getLabel(), 'value' => $item->getValue(), 'count' => $item->getCount()];
+        }
+        $this->assertEquals($expected, $actual);
+    }
+
+    /**
+     * @return array
+     */
+    public function itemDataDataProvider(): array
+    {
+        return [
             [
-                $this->filterItem
+                [
+                    '0_10' => ['count' => 5],
+                    '10_20' => ['count' => 2],
+                    '30_' => ['count' => 1]
+                ],
+                [
+                    ['label' => '$10.00 - $19.99', 'value' => '10-20', 'count' => '2'],
+                    ['label' => '$30.00 and above', 'value' => '30-', 'count' => '1'],
+                ]
             ],
-            $this->target->getItems()
-        );
+            [
+                [
+                    '*_100' => ['count' => 3],
+                    '200_*' => ['count' => 1],
+                ],
+                [
+                    ['label' => '$0.00 - $99.99', 'value' => '-100', 'count' => '3'],
+                    ['label' => '$200.00 and above', 'value' => '200-', 'count' => '1'],
+                ]
+            ]
+        ];
     }
 }
