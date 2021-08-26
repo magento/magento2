cda98b598210c3e31ae6f792488a089f300b1c0b
Ihor Sviziev <ihor-sviziev@users.noreply.github.com>
2021-08-26 13:34:14 +0300

    Replaced PHP8-only str_contains method
    Fix after code review

--- lib/internal/Magento/Framework/Filesystem/Directory/DenyListPathValidator.php
+++ lib/internal/Magento/Framework/Filesystem/Directory/DenyListPathValidator.php
@@ -71,7 +71,7 @@ class DenyListPathValidator implements PathValidatorInterface
 
         foreach ($this->fileDenyList as $file) {
             $baseName = pathinfo($actualPath, PATHINFO_BASENAME);
-            if ('' === $needle || false !== strpos($haystack, $needle) || preg_match('#' . "\." . $file . '#', $fullPath)) {
+            if ('' === $file || false !== strpos($baseName, $file) || preg_match('#' . "\." . $file . '#', $fullPath)) {
                 throw new ValidatorException(
                     new Phrase('"%1" is not a valid file path', [$path])
                 );
