46df6cd08b0239e0acaa02750e185373bd64f23d
Tu Nguyen <tuna9x.it@gmail.com>
2021-09-08 06:48:52 +0700

    Fix code style

--- lib/internal/Magento/Framework/Css/PreProcessor/Instruction/MagentoImport.php
+++ lib/internal/Magento/Framework/Css/PreProcessor/Instruction/MagentoImport.php
@@ -169,6 +169,7 @@ class MagentoImport implements PreProcessorInterface
                 $context->getAreaCode() . '/' . $context->getThemePath()
             );
         }
+
         return $this->design->getDesignTheme();
     }
 
