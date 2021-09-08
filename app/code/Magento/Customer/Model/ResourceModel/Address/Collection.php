bc38677454829c958dd03bdc3182fc1d1836c870
pradeep.rauthan <Prpro@2021>
2021-09-08 08:43:58 +0530

    MC-42623:Countries list is incorrect when using different countries per website and Global Customer Account settings - Only corresponding address should be populated in admin too

--- app/code/Magento/Customer/Model/ResourceModel/Address/Collection.php
+++ app/code/Magento/Customer/Model/ResourceModel/Address/Collection.php
@@ -5,6 +5,11 @@
  */
 namespace Magento\Customer\Model\ResourceModel\Address;
 
+use Magento\Customer\Model\Config\Share;
+use Magento\Store\Model\ScopeInterface;
+use Magento\Directory\Model\AllowedCountries;
+use Magento\Framework\App\ObjectManager;
+
 /**
  * Customers collection
  *
@@ -15,13 +20,30 @@ namespace Magento\Customer\Model\ResourceModel\Address;
 class Collection extends \Magento\Eav\Model\Entity\Collection\VersionControl\AbstractCollection
 {
     /**
+     * @var \Magento\Customer\Model\Config\Share
+     */
+    private $shareConfig;
+
+    /**
+     * @var AllowedCountries
+     */
+    private $allowedCountryReader;
+
+    /**
      * Resource initialization
      *
      * @return void
      */
-    protected function _construct()
+    protected function _construct(
+        ?Share $share = null,
+        ?AllowedCountries $allowedCountryReader = null
+    )
     {
         $this->_init(\Magento\Customer\Model\Address::class, \Magento\Customer\Model\ResourceModel\Address::class);
+        $this->shareConfig = $share ?: ObjectManager::getInstance()
+            ->get(Share::class);
+        $this->allowedCountryReader = $allowedCountryReader ?: ObjectManager::getInstance()
+            ->get(AllowedCountries::class);
     }
 
     /**
@@ -38,6 +60,22 @@ class Collection extends \Magento\Eav\Model\Entity\Collection\VersionControl\Abs
             $this->addAttributeToFilter('parent_id', $customer->getId());
         } else {
             $this->addAttributeToFilter('parent_id', '-1');
+        }
+        return $this;
+    }
+
+    /**
+     * Set store filter
+     *
+     * @param \Magento\Store\Model\Store|integer $storeId
+     * @return $this
+     */
+    public function setScopeFilter($storeId)
+    {
+        // Checks if a country present in the allowed countries list.
+        $allowedCountries = $this->allowedCountryReader->getAllowedCountries(ScopeInterface::SCOPE_STORE, $storeId);
+        if ($this->shareConfig->isGlobalScope()) {
+            $this->addAttributeToFilter('country_id', ['in' => $allowedCountries]);
         }
         return $this;
     }
