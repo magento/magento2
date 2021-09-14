48ddccc3c32f150147b435efe24baad4510c8afe
pradeep.rauthan <Prpro@2021>
2021-09-14 14:54:59 +0530

    MC-42623:Countries list is incorrect when using different countries per website and Global Customer Account settings

---
+++ app/code/Magento/Customer/Model/ResourceModel/Address/StoreAddressCollection.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Customer\Model\ResourceModel\Address;
+
+use Magento\Framework\App\ObjectManager;
+use Magento\Sales\Block\Adminhtml\Order\Create\Form\Address as AddressBlock;
+use Magento\Directory\Model\AllowedCountries;
+use Magento\Framework\App\RequestInterface;
+use Magento\Store\Model\ScopeInterface;
+
+class StoreAddressCollection extends \Magento\Customer\Model\ResourceModel\Address\Collection
+{
+    /**
+     * @var AddressBlock
+     */
+    private $addressBlock;
+
+    /**
+     * @var AllowedCountries
+     */
+    private $allowedCountryReader;
+
+    /**
+     * @var RequestInterface
+     */
+    private $request;
+
+    /**
+     * Resource initialization
+     *
+     * @return void
+     */
+    protected function _construct()
+    {
+        parent::_construct();
+        $this->addressBlock = ObjectManager::getInstance()->get(AddressBlock::class);
+        $this->allowedCountryReader = ObjectManager::getInstance()->get(AllowedCountries::class);
+        $this->request = ObjectManager::getInstance()->get(RequestInterface::class);
+    }
+
+    /**
+     * Set customer filter
+     *
+     * @param \Magento\Customer\Model\Customer|array $customer
+     * @return $this
+     */
+    public function setCustomerFilter($customer)
+    {
+        parent::setCustomerFilter($customer);
+
+        $storeId = $this->addressBlock->getStoreId() ?? null;
+        //echo $storeId;die;
+        if ($storeId) {
+            $allowedCountries = $this->allowedCountryReader->getAllowedCountries(
+                ScopeInterface::SCOPE_STORE,
+                $storeId
+            );
+            $this->addAttributeToFilter('country_id', ['in' => $allowedCountries]);
+        }
+        return $this;
+    }
+}
