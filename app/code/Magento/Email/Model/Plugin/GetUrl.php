1677e3dcdb5f8ac217c6a3f54a49a6ae1d503ee3
pradeep.rauthan <Prpro@2021>
2021-09-16 12:22:51 +0530

    ACP2E-127:Asynchronous order emails from non-default websites has default website logo URL - New Plugin introduce

--- app/code/Magento/Email/Model/Plugin/GetUrl.php
+++ app/code/Magento/Email/Model/Plugin/GetUrl.php
@@ -8,12 +8,13 @@ namespace Magento\Email\Model\Plugin;
 class GetUrl
 {
     /**
-     * A unique store parameter need to pass in \Magento\Email\Model\AbstractTemplate `getUrl` function
-     * for separating links in email content for sales order email confirmation.
+     * A unique store parameter need to pass in \Magento\Email\Model\AbstractTemplate `getUrl` function for separating
+     * links in email content for sales order email confirmation.
      *
      * @param \Magento\Email\Model\AbstractTemplate $subject
      * @param \Magento\Store\Model\Store $store
-     * @param string $scope
+     * @param string $route
+     * @param array $params
      * @return array
      */
     public function beforeGetUrl(
