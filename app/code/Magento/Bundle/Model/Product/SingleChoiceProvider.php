8d7f23712fc151f45d45d0469d48b3b5437ce58e
Pavel Dzedavets <dedovets.pavel4@gmail.com>
2021-09-20 22:06:52 +0300

    #33935 fixed issue with Call to undefined method getLinkField, refactoring

--- app/code/Magento/Bundle/Model/Product/SingleChoiceProvider.php
+++ app/code/Magento/Bundle/Model/Product/SingleChoiceProvider.php
@@ -21,44 +21,72 @@ class SingleChoiceProvider
      * @param Product $product
      * @return bool
      */
-    public function isSingleChoiceAvailable(Product $product) : bool
+    public function isSingleChoiceAvailable(Product $product): bool
     {
         $result = false;
-        if ($product->getTypeId() === BundleType::TYPE_BUNDLE) {
-            $typeInstance = $product->getTypeInstance();
-            $typeInstance->setStoreFilter($product->getStoreId(), $product);
+        if ($product->getTypeId() !== BundleType::TYPE_BUNDLE) {
+            return $result;
+        }
 
-            if ($typeInstance->hasRequiredOptions($product)) {
-                $options = $typeInstance->getOptions($product);
-                $isNoCustomizations = true;
-                foreach ($options as $option) {
-                    $optionId = $option->getId();
-                    $required = $option->getRequired();
-                    if ($isNoCustomizations && (int) $required === 1) {
-                        $selectionsCollection = $typeInstance->getSelectionsCollection(
-                            [$optionId],
-                            $product
-                        );
-                        $selections = $selectionsCollection->exportToArray();
-                        if (count($selections) > 1) {
-                            foreach ($selections as $selection) {
-                                if ($isNoCustomizations) {
-                                    $isNoCustomizations = (int)$selection['is_default'] === 1
-                                        && (int)$selection['selection_can_change_qty'] === 0;
-                                } else {
-                                    break;
-                                }
-                            }
+        if (!$this->hasRequiredOptions($product)) {
+            return $result;
+        }
+
+        return $this->hasCustomizations($product);
+    }
+
+    /**
+     * Options checker
+     *
+     * @param Product $product
+     * @return bool
+     */
+    private function hasRequiredOptions(Product $product): bool
+    {
+        $result = true;
+        $typeInstance = $product->getTypeInstance();
+        $typeInstance->setStoreFilter($product->getStoreId(), $product);
+
+        if (!$typeInstance->hasRequiredOptions($product)) {
+            $result = false;
+        }
+
+        return $result;
+    }
+
+    /**
+     * Customizations checker
+     *
+     * @param Product $product
+     * @return bool
+     */
+    private function hasCustomizations(Product $product): bool
+    {
+        $typeInstance = $product->getTypeInstance();
+        $isNoCustomizations = true;
+        foreach ($typeInstance->getOptions($product) as $option) {
+            if ($isNoCustomizations && (int)$option->getRequired() === 1) {
+                $selectionsCollection = $typeInstance->getSelectionsCollection(
+                    [$option->getId()],
+                    $product
+                );
+
+                if ($selectionsCollection->count() > 1) {
+                    foreach ($selectionsCollection as $selection) {
+                        if ($isNoCustomizations) {
+                            $isNoCustomizations = (int)$selection->getData('is_default') === 1
+                                && (int)$selection->getData('selection_can_change_qty') === 0;
+                        } else {
+                            break;
                         }
-                    } else {
-                        $isNoCustomizations = false;
-                        break;
                     }
                 }
-
-                $result = $isNoCustomizations;
+            } else {
+                $isNoCustomizations = false;
+                break;
             }
         }
-        return $result;
+
+        return $isNoCustomizations;
     }
 }
