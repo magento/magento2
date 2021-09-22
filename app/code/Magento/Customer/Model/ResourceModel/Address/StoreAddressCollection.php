dcc4f97e5c8c677a788d55a0c628876d0c530de3
pradeep.rauthan <Prpro@2021>
2021-09-22 15:16:37 +0530

    MC-42623:Countries list is incorrect when using different countries per website and Global Customer Account settings

--- app/code/Magento/Customer/Model/ResourceModel/Address/StoreAddressCollection.php
+++ app/code/Magento/Customer/Model/ResourceModel/Address/StoreAddressCollection.php
@@ -5,13 +5,19 @@
  */
 namespace Magento\Customer\Model\ResourceModel\Address;
 
-use Magento\Framework\App\ObjectManager;
-use Magento\Sales\Block\Adminhtml\Order\Create\Form\Address as AddressBlock;
 use Magento\Directory\Model\AllowedCountries;
+use Magento\Sales\Block\Adminhtml\Order\Create\Form\Address as AddressBlock;
 use Magento\Store\Model\ScopeInterface;
 
-class StoreAddressCollection extends \Magento\Customer\Model\ResourceModel\Address\Collection
+class StoreAddressCollection extends Collection
 {
+    /**
+     * Object Manager instance
+     *
+     * @var \Magento\Framework\ObjectManagerInterface
+     */
+    protected $_objectManager;
+
     /**
      * @var AddressBlock
      */
@@ -23,6 +29,51 @@ class StoreAddressCollection extends \Magento\Customer\Model\ResourceModel\Addre
     private $allowedCountryReader;
 
     /**
+     * @param \Magento\Framework\Data\Collection\EntityFactory $entityFactory
+     * @param \Psr\Log\LoggerInterface $logger
+     * @param \Magento\Framework\Data\Collection\Db\FetchStrategyInterface $fetchStrategy
+     * @param \Magento\Framework\Event\ManagerInterface $eventManager
+     * @param \Magento\Eav\Model\Config $eavConfig
+     * @param \Magento\Framework\App\ResourceConnection $resource
+     * @param \Magento\Eav\Model\EntityFactory $eavEntityFactory
+     * @param \Magento\Eav\Model\ResourceModel\Helper $resourceHelper
+     * @param \Magento\Framework\Validator\UniversalFactory $universalFactory
+     * @param \Magento\Framework\Model\ResourceModel\Db\VersionControl\Snapshot $entitySnapshot
+     * @param \Magento\Framework\ObjectManagerInterface $objectManager
+     * @param \Magento\Framework\DB\Adapter\AdapterInterface|null $connection
+     */
+    public function __construct(
+        \Magento\Framework\Data\Collection\EntityFactory $entityFactory,
+        \Psr\Log\LoggerInterface $logger,
+        \Magento\Framework\Data\Collection\Db\FetchStrategyInterface $fetchStrategy,
+        \Magento\Framework\Event\ManagerInterface $eventManager,
+        \Magento\Eav\Model\Config $eavConfig,
+        \Magento\Framework\App\ResourceConnection $resource,
+        \Magento\Eav\Model\EntityFactory $eavEntityFactory,
+        \Magento\Eav\Model\ResourceModel\Helper $resourceHelper,
+        \Magento\Framework\Validator\UniversalFactory $universalFactory,
+        \Magento\Framework\Model\ResourceModel\Db\VersionControl\Snapshot $entitySnapshot,
+        \Magento\Framework\ObjectManagerInterface $objectManager,
+        \Magento\Framework\DB\Adapter\AdapterInterface $connection = null
+    ) {
+        $this->_objectManager = $objectManager;
+
+        parent::__construct(
+            $entityFactory,
+            $logger,
+            $fetchStrategy,
+            $eventManager,
+            $eavConfig,
+            $resource,
+            $eavEntityFactory,
+            $resourceHelper,
+            $universalFactory,
+            $entitySnapshot,
+            $connection
+        );
+    }
+
+    /**
      * Resource initialization
      *
      * @return void
@@ -31,12 +82,12 @@ class StoreAddressCollection extends \Magento\Customer\Model\ResourceModel\Addre
     {
         parent::_construct();
 
-        $this->addressBlock = ObjectManager::getInstance()->get(AddressBlock::class);
-        $this->allowedCountryReader = ObjectManager::getInstance()->get(AllowedCountries::class);
+        $this->addressBlock = $this->_objectManager->create(AddressBlock::class);
+        $this->allowedCountryReader = $this->_objectManager->create(AllowedCountries::class);
     }
 
     /**
-     * Set customer filter
+     * Set allowed country filter for customer's addresses
      *
      * @param \Magento\Customer\Model\Customer|array $customer
      * @return $this
