38c8ac0bab15bff815f2ed8343909cb2b6fa1c1e
silinmykola <m.silin@atwix.com>
2021-09-29 17:43:21 +0200

    php8 compatibility/fix price currency in web-api tests

--- app/code/Magento/Directory/Model/PriceCurrency.php
+++ app/code/Magento/Directory/Model/PriceCurrency.php
@@ -152,7 +152,7 @@ class PriceCurrency implements \Magento\Framework\Pricing\PriceCurrencyInterface
      */
     public function round($price)
     {
-        return round($price, 2);
+        return round((float) $price, 2);
     }
 
     /**
