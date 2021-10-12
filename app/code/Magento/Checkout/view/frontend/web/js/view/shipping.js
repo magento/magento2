ca0fb69c95db35b30b35bc2d7c3210a44dcaa49f
Vladyslav Shcherbyna <shcherby@adobe.com>
2021-10-12 22:28:04 +0000

    ACP2E-227: Shipping street address is not cleared completely after removing from the input box

--- app/code/Magento/Checkout/view/frontend/web/js/view/shipping.js
+++ app/code/Magento/Checkout/view/frontend/web/js/view/shipping.js
@@ -121,7 +121,7 @@ define([
                     );
                 }
                 checkoutProvider.on('shippingAddress', function (shippingAddrsData) {
-                    if (shippingAddrsData.street && !_.isEmpty(shippingAddrsData.street[0])) {
+                    if (shippingAddrsData.street) {
                         checkoutData.setShippingAddressFromData(shippingAddrsData);
                     }
                 });
