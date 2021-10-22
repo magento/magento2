dbe45dda4888983ddf4e6ec1a4162f536b630490
Matt Krilanovich <Matt.Krilanovich@adswerve.com>
2021-10-21 21:03:45 -0700

    Update Head.php
    * strict type declaration for new class
    * removed comments from copyright block
    * unmarked this class as @api

--- app/code/Magento/GoogleAdwords/Block/Head.php
+++ app/code/Magento/GoogleAdwords/Block/Head.php
@@ -1,16 +1,11 @@
 <?php
 /**
- * Google AdWords Code block
- *
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types=1);
 namespace Magento\GoogleAdwords\Block;
 
-/**
- * @api
- * @since 100.0.2
- */
 class Head extends \Magento\Framework\View\Element\Template
 {
     /**
