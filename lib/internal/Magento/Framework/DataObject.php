db6ca5513e9c3ba2dc6ac1a9c08e191ef5d11988
Ihor Sviziev <ihor-sviziev@users.noreply.github.com>
2021-10-22 16:31:30 +0300

    Speedup addData when used on an empty object
    Fix failing static tests

--- lib/internal/Magento/Framework/DataObject.php
+++ lib/internal/Magento/Framework/DataObject.php
@@ -378,9 +378,9 @@ class DataObject implements \ArrayAccess
     /**
      * Set/Get attribute wrapper
      *
-     * @param   string $method
-     * @param   array $args
-     * @return  mixed
+     * @param string $method
+     * @param array $args
+     * @return mixed
      * @throws \Magento\Framework\Exception\LocalizedException
      */
     public function __call($method, $args)
@@ -469,7 +469,7 @@ class DataObject implements \ArrayAccess
      * Present object data as string in debug mode
      *
      * @param mixed $data
-     * @param array &$objects
+     * @param array $objects
      * @return array
      */
     public function debug($data = null, &$objects = [])
