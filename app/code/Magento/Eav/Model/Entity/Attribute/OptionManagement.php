02f7fa05e04c5874ea85a9b00cbb8e3b10d13fc9
Mike Bijnsdorp <m.bijnsdorp@youweagency.com>
2021-11-10 22:29:28 +0100

    Apply label change to add method

--- app/code/Magento/Eav/Model/Entity/Attribute/OptionManagement.php
+++ app/code/Magento/Eav/Model/Entity/Attribute/OptionManagement.php
@@ -60,8 +60,8 @@ class OptionManagement implements AttributeOptionManagementInterface, AttributeO
     {
         $attribute = $this->loadAttribute($entityType, (string)$attributeCode);
 
-        $label = trim($option->getLabel() ?: '');
-        if (empty($label)) {
+        $label = trim((string) $option->getLabel());
+        if ($label === '') {
             throw new InputException(__('The attribute option label is empty. Enter the value and try again.'));
         }
 
