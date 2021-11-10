1fe84ea899853444e2d9a7e69fe2d185d8e6d886
Vasiliev.A <avasiliev@comwrap.com>
2021-11-10 15:28:34 +0200

    add BulkManagementInterface to AsynchronousOperations\Api which will allow to use as service in webapi.xml

---
+++ app/code/Magento/AsynchronousOperations/Api/BulkManagementInterface.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace Magento\AsynchronousOperations\Api;
+
+use Magento\Framework\Bulk\BulkManagementInterface as FrameworkBulkManagementInterface;
+
+/**
+ * Interface for using in webapi.xml
+ */
+interface BulkManagementInterface extends FrameworkBulkManagementInterface
+{
+}
