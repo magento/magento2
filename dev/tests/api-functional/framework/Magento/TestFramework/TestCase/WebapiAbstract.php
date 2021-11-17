0040aa593cf4c3304c72711b931bb20a28d9f89e
Yevhen Miroshnychenko <miroshny@adobe.com>
2021-11-17 06:19:13 -0600

    AC-465: Allow to configure input limit for RESTful endpoints

--- dev/tests/api-functional/framework/Magento/TestFramework/TestCase/WebapiAbstract.php
+++ dev/tests/api-functional/framework/Magento/TestFramework/TestCase/WebapiAbstract.php
@@ -565,21 +565,25 @@ abstract class WebapiAbstract extends \PHPUnit\Framework\TestCase
      */
     public function processRestExceptionResult(\Exception $e)
     {
+        echo 'pickachu';
+        var_dump([
+            'message' => $e->getMessage(),
+            'code' => $e->getCode(),
+            'file' => $e->getFile(),
+//            'trace' => $e->getTrace(),
+            'traceAsString' => $e->getTraceAsString(),
+//            'previous' => $e->getPrevious(),
+        ]);
         $error = json_decode($e->getMessage(), true);
-        if(empty($error) && json_last_error() !== JSON_ERROR_NONE){
+        if (empty($error) && json_last_error() !== JSON_ERROR_NONE) {
             $error['message'] = $e->getMessage();
-            $error['code'] = $e->getCode();
-            $error['file'] = $e->getFile();
-            $error['trace'] = $e->getTrace();
-            $error['traceAsString'] = $e->getTraceAsString();
-            $error['previous'] = $e->getPrevious();
         }
 
         //Remove line breaks and replace with space
         $error['message'] = trim(preg_replace('/\s+/', ' ', $error['message']));
         // remove trace and type, will only be present if server is in dev mode
-//        unset($error['trace']);
-//        unset($error['type']);
+        unset($error['trace']);
+        unset($error['type']);
         return $error;
     }
 
