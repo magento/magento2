005ed60b2aee08c2c520f26bb73cbd19215e0410
Yevhen Miroshnychenko <miroshny@adobe.com>
2021-11-18 23:33:03 -0600

    AC-465: Allow to configure input limit for RESTful endpoints

--- lib/internal/Magento/Framework/App/Bootstrap.php
+++ lib/internal/Magento/Framework/App/Bootstrap.php
@@ -429,6 +429,8 @@ class Bootstrap
      */
     protected function terminate(\Throwable $e)
     {
+        echo "ramamba";
+        var_dump($e);
         /** @var Response $response */
         $response = $this->objectManager->get(Response::class);
         $response->clearHeaders();
