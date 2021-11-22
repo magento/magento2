a9acb73188f0dc717e8858ba5ccc89fe73191192
pradeep1819 <pradeep05.pro@gmail.com>
2021-11-22 12:41:03 +0530

    ACP2E-182: Error while validating Store VAT Number

---
+++ app/code/Magento/Customer/Model/EuCountryProvider.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Magento\Customer\Model;
+
+use Magento\Directory\Controller\Adminhtml\Json\EuCountryProviderInterface;
+use Magento\Framework\Serialize\SerializerInterface;
+
+class EuCountryProvider implements EuCountryProviderInterface
+{
+    /**
+     * @var SerializerInterface
+     */
+    private $serializer;
+
+    /**
+     * @var Vat
+     */
+    protected $customerVat;
+
+    /**
+     * @param SerializerInterface $serializer
+     * @param Vat                 $customerVat
+     */
+    public function __construct(
+        SerializerInterface $serializer,
+        Vat $customerVat
+    ) {
+        $this->serializer  = $serializer;
+        $this->customerVat = $customerVat;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function isEuCountry($countryCode): string
+    {
+        $isEuCountry = false;
+
+        if (!empty($countryCode)) {
+            if ($this->customerVat->isCountryInEU($countryCode)) {
+                $isEuCountry = true;
+            }
+        }
+
+        return $this->serializer->serialize($isEuCountry);
+    }
+}
---
+++ app/code/Magento/Directory/Controller/Adminhtml/Json/EuCountryProvider.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Directory\Controller\Adminhtml\Json;
+
+use Magento\Framework\Serialize\SerializerInterface;
+
+class EuCountryProvider implements EuCountryProviderInterface
+{
+    /**
+     * @var SerializerInterface
+     */
+    private $serializer;
+
+    /**
+     * @param SerializerInterface $serializer
+     */
+    public function __construct(
+        SerializerInterface $serializer
+    ) {
+        $this->serializer = $serializer;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function isEuCountry($countryCode): string
+    {
+        $isEuCountry = false;
+        return $this->serializer->serialize($isEuCountry);
+    }
+}
