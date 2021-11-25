caf0b4fba876ec2eefe849233e33d7f60622ced4
Rajesh Kumar <glo71317@adobe.com>
2021-11-25 15:52:03 +0530

    Resolved pr comments

--- dev/tests/integration/testsuite/Magento/Catalog/_files/product_virtual_rollback.php
+++ dev/tests/integration/testsuite/Magento/Catalog/_files/product_virtual_rollback.php
@@ -5,13 +5,13 @@
  */
 declare(strict_types=1);
 
-use Magento\Catalog\Api\ProductRepositoryInterface;
-use Magento\Framework\Exception\NoSuchEntityException;
-use Magento\Framework\Exception\StateException;
 use Magento\TestFramework\Helper\Bootstrap;
+use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\App\ObjectManager;
 use Magento\CatalogInventory\Model\Indexer\Stock\Processor;
+use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Framework\Registry;
+use Magento\Framework\Exception\StateException;
 
 $registry = Bootstrap::getObjectManager()->get(Registry::class);
 
@@ -29,9 +29,7 @@ try {
 } catch (StateException $exception) {
 }
 
-ObjectManager::getInstance()
-    ->create(Processor::class)
-    ->reindexAll();
+ObjectManager::getInstance()->create(Processor::class)->reindexAll();
 
 $registry->unregister('isSecureArea');
 $registry->register('isSecureArea', false);
