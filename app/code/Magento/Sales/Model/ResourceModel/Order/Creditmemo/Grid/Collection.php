032f409319dc743136b2087b46008f3523b1b47f
pradeep1819 <pradeep05.pro@gmail.com>
2021-12-13 14:19:24 +0530

    ACP2E-56 : [On Prem] Magento invoice order date filter not working

---
+++ app/code/Magento/Sales/Model/ResourceModel/Order/Creditmemo/Grid/Collection.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Sales\Model\ResourceModel\Order\Creditmemo\Grid;
+
+use Magento\Framework\Data\Collection\Db\FetchStrategyInterface as FetchStrategy;
+use Magento\Framework\Data\Collection\EntityFactoryInterface as EntityFactory;
+use Magento\Framework\Event\ManagerInterface as EventManager;
+use Magento\Framework\View\Element\UiComponent\DataProvider\SearchResult;
+use Magento\Sales\Model\ResourceModel\Order\Creditmemo;
+use Psr\Log\LoggerInterface as Logger;
+
+class Collection extends SearchResult
+{
+    /**
+     * Initialize dependencies.
+     *
+     * @param EntityFactory $entityFactory
+     * @param Logger $logger
+     * @param FetchStrategy $fetchStrategy
+     * @param EventManager $eventManager
+     * @param string $mainTable
+     * @param string $resourceModel
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    public function __construct(
+        EntityFactory $entityFactory,
+        Logger $logger,
+        FetchStrategy $fetchStrategy,
+        EventManager $eventManager,
+        $mainTable = 'sales_creditmemo_grid',
+        $resourceModel = Creditmemo::class
+    ) {
+        parent::__construct($entityFactory, $logger, $fetchStrategy, $eventManager, $mainTable, $resourceModel);
+    }
+}
---
+++ app/code/Magento/Sales/Plugin/Model/ResourceModel/Order/OrderGridCollectionFilter.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\Sales\Plugin\Model\ResourceModel\Order;
+
+use Magento\Framework\DB\Select;
+use Magento\Framework\Stdlib\DateTime\TimezoneInterface;
+use Magento\Framework\View\Element\UiComponent\DataProvider\SearchResult;
+
+class OrderGridCollectionFilter
+{
+    /**
+     * @var TimezoneInterface
+     */
+    private TimezoneInterface $timeZone;
+
+    /**
+     * @param TimezoneInterface $timeZone
+     */
+    public function __construct(
+        TimezoneInterface $timeZone
+    ) {
+        $this->timeZone = $timeZone;
+    }
+
+    private function _initSelect(SearchResult $subject)
+    {
+        $tableDescription = $subject->getConnection()->describeTable($subject->getMainTable());
+        if ($tableDescription) {
+            foreach ($tableDescription as $columnInfo) {
+                $subject->addFilterToMap($columnInfo['COLUMN_NAME'], 'main_table.' . $columnInfo['COLUMN_NAME']);
+            }
+        }
+
+        return $subject;
+    }
+
+    /**
+     * @param  SearchResult $subject
+     * @param  \Closure     $proceed
+     * @param  string       $field
+     * @param  string|null  $condition
+     * @return SearchResult|mixed
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    public function aroundAddFieldToFilter(
+        SearchResult $subject,
+        \Closure $proceed,
+        $field,
+        $condition = null
+    ) {
+        $this->_initSelect($subject);
+        $fieldMap  = $this->getFilterFieldsMap();
+        $fieldName = $fieldMap['fields'][$field] ?? null;
+        if (!$fieldName) {
+            return $proceed($field, $condition);
+        }
+
+        if ($field === 'created_at' || $field === 'order_created_at') {
+            if (is_array($condition)) {
+                foreach ($condition as $key => $value) {
+                    $condition[$key] = $this->timeZone->convertConfigTimeToUtc($value);
+                }
+            }
+        }
+
+        $fieldName = $subject->getConnection()->quoteIdentifier($fieldName);
+        $condition = $subject->getConnection()->prepareSqlCondition($fieldName, $condition);
+        $subject->getSelect()->where($condition, null, Select::TYPE_CONDITION);
+
+        return $subject;
+    }
+
+    /**
+     * @return \string[][]
+     */
+    private function getFilterFieldsMap(): array
+    {
+        return [
+            'fields' => [
+                'created_at'       => 'main_table.created_at',
+                'order_created_at' => 'main_table.order_created_at',
+            ],
+        ];
+    }
+}
