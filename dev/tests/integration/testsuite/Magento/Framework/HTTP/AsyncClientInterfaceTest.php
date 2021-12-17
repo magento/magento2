92ebef1626a44a379e6f70e3cba94a39966c105a
Yevhen Miroshnychenko <miroshny@adobe.com>
2021-12-17 16:51:05 -0600

    AC-1173: Change HTTPS setting to secure defaults

--- dev/tests/integration/testsuite/Magento/Framework/HTTP/AsyncClientInterfaceTest.php
+++ dev/tests/integration/testsuite/Magento/Framework/HTTP/AsyncClientInterfaceTest.php
@@ -35,19 +35,13 @@ class AsyncClientInterfaceTest extends TestCase
      */
     public function testRequest(): void
     {
-        $request = new Request('https://adobe.com', Request::METHOD_GET, [], null);
+        $request = new Request('https://magento.com', Request::METHOD_GET, [], null);
         $response1 = $this->client->request($request);
         $response2 = $this->client->request($request);
         $this->assertEquals(200, $response2->get()->getStatusCode());
         $this->assertEquals(200, $response1->get()->getStatusCode());
-        $this->assertStringContainsString(
-            'Adobe: Creative, marketing and document management solutions',
-            $response1->get()->getBody()
-        );
-        $this->assertStringContainsString(
-            'Adobe: Creative, marketing and document management solutions',
-            $response2->get()->getBody()
-        );
+        $this->assertStringContainsString('Magento Commerce', $response1->get()->getBody());
+        $this->assertStringContainsString('Magento Commerce', $response2->get()->getBody());
         $date1 = new \DateTime($response1->get()->getHeaders()['date']);
         $date2 = new \DateTime($response2->get()->getHeaders()['date']);
         $this->assertLessThanOrEqual(1, abs((int)$date1->format('U') - (int)$date2->format('U')));
@@ -62,7 +56,7 @@ class AsyncClientInterfaceTest extends TestCase
         $this->expectException(\Magento\Framework\Async\CancelingDeferredException::class);
         $this->expectExceptionMessage('Deferred is canceled');
 
-        $request = new Request('https://adobe.com/home-page', Request::METHOD_GET, [], null);
+        $request = new Request('https://magento.com/home-page', Request::METHOD_GET, [], null);
         $response = $this->client->request($request);
         $response->cancel(true);
         $this->assertTrue($response->isCancelled());
@@ -78,7 +72,7 @@ class AsyncClientInterfaceTest extends TestCase
         $this->expectException(\Magento\Framework\Async\CancelingDeferredException::class);
         $this->expectExceptionMessage('Failed to cancel HTTP request');
 
-        $request = new Request('https://adobe.com/home-page', Request::METHOD_GET, [], null);
+        $request = new Request('https://magento.com/home-page', Request::METHOD_GET, [], null);
         $response = $this->client->request($request);
         $response->cancel();
     }
