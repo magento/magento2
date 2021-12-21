b8660cc77940d390b940ed4f0012ec88503d97a9
Arularasan <glo02433@adobe.com>
2021-12-21 13:50:17 +0530

    ACP2E-55: Simple only global attributes make configurable attributes hidden when creating product configurations  - Fixed the solution and test coverage.

--- app/code/Magento/ConfigurableProduct/Plugin/Model/UpdateConfigurableProductAttributeCollection.php
+++ app/code/Magento/ConfigurableProduct/Plugin/Model/UpdateConfigurableProductAttributeCollection.php
@@ -7,6 +7,8 @@ declare(strict_types=1);
 
 namespace Magento\ConfigurableProduct\Plugin\Model;
 
+use Magento\Catalog\Model\Product\Type;
+use Magento\ConfigurableProduct\Model\Product\Type\Configurable;
 use Magento\ConfigurableProduct\Ui\DataProvider\Attributes;
 
 /**
@@ -18,21 +20,26 @@ class UpdateConfigurableProductAttributeCollection
      * Adding a field to filter in existing configurable product attribute collection.
      *
      * @param Attributes $subject
-     * @return object
+     * @return void
      */
-    public function beforeGetData(Attributes $subject): object
+    public function beforeGetData(Attributes $subject): void
     {
-        $types = [
-            \Magento\Catalog\Model\Product\Type::TYPE_SIMPLE,
-            \Magento\Catalog\Model\Product\Type::TYPE_VIRTUAL,
-            \Magento\ConfigurableProduct\Model\Product\Type\Configurable::TYPE_CODE,
-        ];
-        return $subject->getCollection()->addFieldToFilter(
-            ['apply_to', 'apply_to'],
-            [
-                ['null' => true],
-                ['like' => '%' . implode(',', $types) . '%']
-            ]
+        $subject->getCollection()->getSelect()->where(
+            '(`apply_to` IS NULL) OR
+                (
+                    FIND_IN_SET(' .
+            "'" . Type::TYPE_SIMPLE . "'" . ',
+                        `apply_to`
+                    ) AND
+                    FIND_IN_SET(' .
+            "'" . Type::TYPE_VIRTUAL . "'" . ',
+                        `apply_to`
+                    ) AND
+                    FIND_IN_SET(' .
+            "'" . Configurable::TYPE_CODE . "'" . ',
+                        `apply_to`
+                    )
+                 )'
         );
     }
 }
