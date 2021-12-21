c9f9d0fcfd7a680b2c6caae1ae385b8bd04518d1
Alexander Menk <a.menk@imi.de>
2021-12-21 16:49:21 +0100

    #34791 Add failing test based on the file from #34210

--- dev/tests/integration/testsuite/Magento/CatalogUrlRewrite/Model/Category/Plugin/StorageTest.php
+++ dev/tests/integration/testsuite/Magento/CatalogUrlRewrite/Model/Category/Plugin/StorageTest.php
@@ -8,56 +8,235 @@ declare(strict_types=1);
 namespace Magento\CatalogUrlRewrite\Model\Category\Plugin;
 
 use Magento\Catalog\Api\ProductRepositoryInterface;
-use Magento\CatalogUrlRewrite\Model\Map\UrlRewriteFinder;
-use Magento\CatalogUrlRewrite\Model\ObjectRegistryFactory;
+use Magento\Catalog\Model\Product as ProductEntity;
+use Magento\Catalog\Model\Product\Media\ConfigInterface;
+use Magento\Framework\App\Bootstrap as AppBootstrap;
+use Magento\Framework\App\Filesystem\DirectoryList;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\Filesystem;
+use Magento\Framework\Filesystem\Driver\File;
 use Magento\Framework\ObjectManagerInterface;
+use Magento\ImportExport\Model\Import;
+use Magento\CatalogImportExport\Model\Import\ProductFactory;
+use Magento\ImportExport\Model\Import\Source\Csv;
+use Magento\ImportExport\Model\Import\Source\CsvFactory;
+use Magento\ImportExport\Model\ResourceModel\Import\Data;
 use Magento\TestFramework\Helper\Bootstrap;
-use Magento\UrlRewrite\Service\V1\Data\UrlRewrite;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Checks that product import with same images can be successfully done
+ *
+ * @magentoAppArea adminhtml
+ *
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+ */
 class StorageTest extends TestCase
 {
-
-    /**
-     * @var ObjectManagerInterface
-     */
+    /** @var ObjectManagerInterface */
     private $objectManager;
 
-    /**
-     * @var ProductRepositoryInterface
-     */
+    /** @var Filesystem */
+    private $fileSystem;
+
+    /** @var ProductRepositoryInterface */
     private $productRepository;
 
+    /** @var File */
+    private $fileDriver;
+
+    /** @var Import */
+    private $import;
+
+    /** @var ConfigInterface */
+    private $mediaConfig;
+
+    /** @var array */
+    private $appParams;
+
+    /** @var array */
+    private $createdProductsSkus = [];
+
+    /** @var array */
+    private $filesToRemove = [];
+
+    /** @var CsvFactory */
+    private $csvFactory;
+
+    /** @var Data */
+    private $importDataResource;
+
     /**
-     * @inheritDoc
+     * @inheritdoc
      */
     protected function setUp(): void
     {
         parent::setUp();
+
         $this->objectManager = Bootstrap::getObjectManager();
+        $this->fileSystem = $this->objectManager->get(Filesystem::class);
+        $this->fileDriver = $this->objectManager->get(File::class);
+        $this->mediaConfig = $this->objectManager->get(ConfigInterface::class);
         $this->productRepository = $this->objectManager->get(ProductRepositoryInterface::class);
+        $this->productRepository->cleanCache();
+        $this->import = $this->objectManager->get(ProductFactory::class)->create();
+        $this->csvFactory = $this->objectManager->get(CsvFactory::class);
+        $this->importDataResource = $this->objectManager->get(Data::class);
+        $this->appParams = Bootstrap::getInstance()->getBootstrap()->getApplication()
+            ->getInitParams()[AppBootstrap::INIT_PARAM_FILESYSTEM_DIR_PATHS];
     }
 
     /**
-     * @magentoDataFixture Magento/CatalogUrlRewrite/_files/product_with_stores.php
-     * @magentoDbIsolation disabled
+     * @inheritdoc
      */
-    public function testAfterReplace(): void
+    protected function tearDown(): void
     {
-        $storeId = 1;
-        $categoryId = 4;
-        $productId = $this->productRepository->get('simple')->getId();
+        $this->removeFiles();
+        $this->removeProducts();
+        $this->importDataResource->cleanBunches();
 
-        $urlRewriteFinder = $this->objectManager->get(UrlRewriteFinder::class);
-        $rewrites = $urlRewriteFinder->findAllByData($productId, $storeId, UrlRewriteFinder::ENTITY_TYPE_PRODUCT);
+        parent::tearDown();
+    }
+
+    /**
+     * @return void
+     * @magentoDataFixture Magento/CatalogUrlRewrite/_files/issue34210_structure.php
+     */
+    public function testIssue34210(): void
+    {
+        $source = $this->prepareFile('issue34210.csv');
+        $this->updateUploader();
+        $errors = $this->import->setParameters([
+            'behavior' => Import::BEHAVIOR_ADD_UPDATE,
+            'entity' => ProductEntity::ENTITY,
+        ])
+            ->setSource($source)->validateData();
+        $this->assertEmpty($errors->getAllErrors());
+
         /**
-         * @var $rewrite UrlRewrite
+         * if the issue is fixed, we get
+         * Zend_Db_Statement_Exception : SQLSTATE[HY000]: General error: 1412 Table definition has changed, please retry transaction, query was: SELECT `catalog_category_product_index_store2`.`category_id`, `catalog_category_product_index_store2`.`product_id`, `catalog_category_product_index_store2`.`position`, `catalog_category_product_index_store2`.`store_id` FROM `catalog_category_product_index_store2` WHERE (store_id = '2') AND (product_id IN (4, 9))
+         * in the next line
          */
-        $rewrite = $rewrites[0];
-        $rewrite->setMetadata(['category_id' => $categoryId]);
+        $result = $this->import->importData();
+        $this->assertTrue($result);
+    }
 
-        $dbStorage = $this->objectManager->get(\Magento\UrlRewrite\Model\Storage\DbStorage::class);
-        $dbStorage->replace([$rewrite]);
-        $dbStorage->replace([$rewrite]);  // we assume this to fail with a unique key constraint violation
+    /**
+     * Check product images
+     *
+     * @param string $expectedImagePath
+     * @param array $productSkus
+     * @return void
+     */
+    private function checkProductsImages(string $expectedImagePath, array $productSkus): void
+    {
+        foreach ($productSkus as $productSku) {
+            $product = $this->productRepository->get($productSku);
+            $productImageItem = $product->getMediaGalleryImages()->getFirstItem();
+            $productImageFile = $productImageItem->getFile();
+            $productImagePath = $productImageItem->getPath();
+            $this->filesToRemove[] = $productImagePath;
+            $this->assertEquals($expectedImagePath, $productImageFile);
+            $this->assertNotEmpty($productImagePath);
+            $this->assertTrue($this->fileDriver->isExists($productImagePath));
+        }
+    }
+
+    /**
+     * Remove created files
+     *
+     * @return void
+     */
+    private function removeFiles(): void
+    {
+        foreach ($this->filesToRemove as $file) {
+            if ($this->fileDriver->isExists($file)) {
+                $this->fileDriver->deleteFile($file);
+            }
+        }
+    }
+
+    /**
+     * Remove created products
+     *
+     * @return void
+     */
+    private function removeProducts(): void
+    {
+        foreach ($this->createdProductsSkus as $sku) {
+            try {
+                $this->productRepository->deleteById($sku);
+            } catch (NoSuchEntityException $e) {
+                //already removed
+            }
+        }
+    }
+
+    /**
+     * Prepare file
+     *
+     * @param string $fileName
+     * @return Csv
+     */
+    private function prepareFile(string $fileName): Csv
+    {
+        $tmpDirectory = $this->fileSystem->getDirectoryWrite(DirectoryList::VAR_DIR);
+        $fixtureDir = realpath(__DIR__ . '/../../../_files');
+        $filePath = $tmpDirectory->getAbsolutePath($fileName);
+        $this->filesToRemove[] = $filePath;
+        $tmpDirectory->getDriver()->copy($fixtureDir . DIRECTORY_SEPARATOR . $fileName, $filePath);
+        $source = $this->csvFactory->create(
+            [
+                'file' => $fileName,
+                'directory' => $tmpDirectory
+            ]
+        );
+
+        return $source;
+    }
+
+    /**
+     * Update upload to use sandbox folders
+     *
+     * @return void
+     */
+    private function updateUploader(): void
+    {
+        $uploader = $this->import->getUploader();
+        $rootDirectory = $this->fileSystem->getDirectoryWrite(DirectoryList::ROOT);
+        $destDir = $rootDirectory->getRelativePath(
+            $this->appParams[DirectoryList::MEDIA][DirectoryList::PATH]
+            . DIRECTORY_SEPARATOR . $this->mediaConfig->getBaseMediaPath()
+        );
+        $tmpDir = $rootDirectory->getRelativePath(
+            $this->appParams[DirectoryList::MEDIA][DirectoryList::PATH]
+        );
+        $rootDirectory->create($destDir);
+        $rootDirectory->create($tmpDir);
+        $uploader->setDestDir($destDir);
+        $uploader->setTmpDir($tmpDir);
+    }
+
+    /**
+     * Move images to appropriate folder
+     *
+     * @param string $fileName
+     * @return void
+     */
+    private function moveImages(string $fileName): void
+    {
+        $rootDirectory = $this->fileSystem->getDirectoryWrite(DirectoryList::ROOT);
+        $tmpDir = $rootDirectory->getRelativePath(
+            $this->appParams[DirectoryList::MEDIA][DirectoryList::PATH]
+        );
+        $fixtureDir = realpath(__DIR__ . '/../../_files');
+        $tmpFilePath = $rootDirectory->getAbsolutePath($tmpDir . DIRECTORY_SEPARATOR . $fileName);
+        $this->fileDriver->createDirectory($tmpDir);
+        $rootDirectory->getDriver()->copy(
+            $fixtureDir . DIRECTORY_SEPARATOR . $fileName,
+            $tmpFilePath
+        );
+        $this->filesToRemove[] = $tmpFilePath;
     }
 }
