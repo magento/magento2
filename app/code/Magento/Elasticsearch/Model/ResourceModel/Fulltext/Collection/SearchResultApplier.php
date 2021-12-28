0fac468628eebf620a283457ad73f02c6d2e78df
pradeep1819 <pradeep05.pro@gmail.com>
2021-12-28 18:02:03 +0530

    ACP2E-322:'Move out of stock to the bottom' automating sorting is not updated when product's stock changes

--- app/code/Magento/Elasticsearch/Model/ResourceModel/Fulltext/Collection/SearchResultApplier.php
+++ app/code/Magento/Elasticsearch/Model/ResourceModel/Fulltext/Collection/SearchResultApplier.php
@@ -8,7 +8,9 @@ namespace Magento\Elasticsearch\Model\ResourceModel\Fulltext\Collection;
 
 use Magento\CatalogSearch\Model\ResourceModel\Fulltext\Collection\SearchResultApplierInterface;
 use Magento\Framework\Api\Search\SearchResultInterface;
+use Magento\Framework\App\ObjectManager;
 use Magento\Framework\Data\Collection;
+use Magento\Framework\App\Config\ScopeConfigInterface;
 
 /**
  * Resolve specific attributes for search criteria.
@@ -36,21 +38,29 @@ class SearchResultApplier implements SearchResultApplierInterface
     private $currentPage;
 
     /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
      * @param Collection $collection
      * @param SearchResultInterface $searchResult
      * @param int $size
      * @param int $currentPage
+     * @param ScopeConfigInterface|null $scopeConfig
      */
     public function __construct(
         Collection $collection,
         SearchResultInterface $searchResult,
         int $size,
-        int $currentPage
+        int $currentPage,
+        ?ScopeConfigInterface $scopeConfig = null
     ) {
         $this->collection = $collection;
         $this->searchResult = $searchResult;
         $this->size = $size;
         $this->currentPage = $currentPage;
+        $this->scopeConfig = $scopeConfig ?? ObjectManager::getInstance()->get(ScopeConfigInterface::class);
     }
 
     /**
@@ -129,63 +139,76 @@ class SearchResultApplier implements SearchResultApplierInterface
     {
         $ids = [];
 
-        if ($this->collection->getFlag('has_stock_status_filter')) {
-            $categoryId = null;
-            $searchCriteria = $this->searchResult->getSearchCriteria();
-            foreach ($searchCriteria->getFilterGroups() as $filterGroup) {
-                foreach ($filterGroup->getFilters() as $filter) {
-                    if ($filter->getField() === 'category_ids') {
-                        $categoryId = $filter->getValue();
-                        break 2;
-                    }
-                }
-            }
+        if (!$this->getShowOutOfStockStatus() || !$this->collection->getFlag('has_stock_status_filter')) {
+            return $ids;
+        }
 
-            if ($categoryId) {
-                $searchOrders = $searchCriteria->getSortOrders();
-                $searchOrders = array_merge(['is_salable' => \Magento\Framework\DB\Select::SQL_DESC], $searchOrders);
-                $storeId = $this->collection->getStoreId();
-
-                $connection = $this->collection->getConnection();
-                $query = clone $connection->select()
-                    ->reset(\Magento\Framework\DB\Select::ORDER)
-                    ->reset(\Magento\Framework\DB\Select::LIMIT_COUNT)
-                    ->reset(\Magento\Framework\DB\Select::LIMIT_OFFSET)
-                    ->reset(\Magento\Framework\DB\Select::COLUMNS)
-                    ->from(
-                        ['product' => $this->collection->getTable('catalog_product_entity')],
-                        [
-                            $this->collection->getResource()->getEntityIdField(),
-                            'cat_index.position AS cat_index_position',
-                            'stock_status_index.stock_status AS is_salable'
-                        ]
-                    )->join(
-                        ['stock_status_index' => $this->collection->getTable('cataloginventory_stock_status')],
-                        'stock_status_index.product_id = ' . $this->collection->getResource()->getEntityIdField(),
-                        []
-                    )->join(
-                        ['cat_index' => $this->collection->getTable('catalog_category_product_index_store' . $storeId)],
-                        'cat_index.product_id = ' . $this->collection->getResource()->getEntityIdField()
-                        . ' AND cat_index.category_id = ' . $categoryId
-                        . ' AND cat_index.store_id = ' . $storeId,
-                        []
-                    );
-
-                foreach ($searchOrders as $field => $dir) {
-                    $query->order(new \Zend_Db_Expr("{$field} {$dir}"));
-                }
-
-                $offset = ($searchCriteria->getCurrentPage() * $searchCriteria->getPageSize());
-                $offset = (int)$offset >= 0 ? (int)$offset : 0;
-                $query->limitPage($offset, $searchCriteria->getPageSize());
-                $resultSet = $this->collection->getConnection()->fetchAssoc($query);
-
-                foreach ($resultSet as $item) {
-                    $ids[] = (int)$item['entity_id'];
+        $categoryId = null;
+        $searchCriteria = $this->searchResult->getSearchCriteria();
+        foreach ($searchCriteria->getFilterGroups() as $filterGroup) {
+            foreach ($filterGroup->getFilters() as $filter) {
+                if ($filter->getField() === 'category_ids') {
+                    $categoryId = $filter->getValue();
+                    break 2;
                 }
             }
         }
 
+        if ($categoryId) {
+            $storeId = $this->collection->getStoreId();
+            $searchOrders = $searchCriteria->getSortOrders();
+
+            $connection = $this->collection->getConnection();
+            $query = clone $connection->select()
+                ->reset(\Magento\Framework\DB\Select::ORDER)
+                ->reset(\Magento\Framework\DB\Select::LIMIT_COUNT)
+                ->reset(\Magento\Framework\DB\Select::LIMIT_OFFSET)
+                ->reset(\Magento\Framework\DB\Select::COLUMNS)
+                ->from(
+                    ['product' => $this->collection->getTable('catalog_product_entity')],
+                    [
+                        'product.entity_id',
+                        'cat_index.position AS cat_index_position',
+                        'stock_status_index.stock_status AS is_salable'
+                    ]
+                )->join(
+                    ['stock_status_index' => $this->collection->getTable('cataloginventory_stock_status')],
+                    'stock_status_index.product_id = product.entity_id',
+                    []
+                )->join(
+                    ['cat_index' => $this->collection->getTable('catalog_category_product_index_store' . $storeId)],
+                    'cat_index.product_id = product.entity_id'
+                    . ' AND cat_index.category_id = ' . $categoryId
+                    . ' AND cat_index.store_id = ' . $storeId,
+                    []
+                );
+            $query->order(new \Zend_Db_Expr('is_salable DESC'));
+
+            if (array_key_exists('position', $searchOrders)) {
+                $query->order(new \Zend_Db_Expr('position ASC'));
+            }
+            $query->order(new \Zend_Db_Expr('entity_id DESC'));
+
+            $offset = ($searchCriteria->getCurrentPage() * $searchCriteria->getPageSize());
+            $offset = (int)$offset >= 0 ? (int)$offset : 0;
+            $query->limitPage($offset, $searchCriteria->getPageSize());
+            $resultSet = $this->collection->getConnection()->fetchAssoc($query);
+
+            foreach ($resultSet as $item) {
+                $ids[] = (int)$item['entity_id'];
+            }
+        }
+
         return $ids;
+    }
+
+    /**
+     * Returns if display out of stock status set or not in catalog inventory
+     *
+     * @return bool
+     */
+    private function getShowOutOfStockStatus(): bool
+    {
+        return (bool) $this->scopeConfig->getValue('cataloginventory/options/show_out_of_stock');
     }
 }
