7d6d0d6cefface94755c34a91b4c14f2b6ea6b2a
rizwankhan <glo52257@adobe.com>
2022-01-09 21:17:26 +0530

    AC-1821: Registered User & Customer API fixes 	* Static failure fixes

--- app/code/Magento/Customer/Model/Plugin/UpdateCustomer.php
+++ app/code/Magento/Customer/Model/Plugin/UpdateCustomer.php
@@ -53,7 +53,8 @@ class UpdateCustomer
         CustomerInterface $customer,
         ?string $passwordHash = null
     ): array {
-        $customerSessionId = $this->userContext->getUserType() == self::USER_TYPE_CUSTOMER ? $this->userContext->getUserId() : 0;
+        $customerSessionId = $this->userContext->getUserType() == self::USER_TYPE_CUSTOMER ?
+                             $this->userContext->getUserId() : 0;
         $customerId = $this->request->getParam('customerId');
         $bodyParams = $this->request->getBodyParams();
         if (!isset($bodyParams['customer']['Id']) && $customerId) {
