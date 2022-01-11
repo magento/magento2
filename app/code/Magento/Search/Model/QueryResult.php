e264e9bea2566b06d7e6e17f6ae75b105974b1f3
Arularasan <glo02433@adobe.com>
2022-01-11 14:42:42 +0530

    ACP2E-72: Search results suggestion give total count of 0 even though there are products  - Fixed the CR Comments.

--- app/code/Magento/Search/Model/QueryResult.php
+++ app/code/Magento/Search/Model/QueryResult.php
@@ -5,6 +5,10 @@
  */
 namespace Magento\Search\Model;
 
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Search\Model\ResourceModel\Query\CollectionFactory as QueryCollectionFactory;
+use Magento\Store\Model\StoreManagerInterface;
+
 /**
  * @api
  * @since 100.0.2
@@ -14,7 +18,7 @@ class QueryResult
     /**
      * @var string
      */
-    private $queryText;
+    private string $queryText;
 
     /**
      * @var int
@@ -22,28 +26,55 @@ class QueryResult
     private $resultsCount;
 
     /**
-     * @param string $queryText
-     * @param string $resultsCount
+     * @var QueryCollectionFactory
      */
-    public function __construct($queryText, $resultsCount)
-    {
+    private QueryCollectionFactory $queryCollectionFactory;
+
+    /**
+     * @var StoreManagerInterface
+     */
+    private StoreManagerInterface $storeManager;
+
+    /**
+     * @param $queryText
+     * @param $resultsCount
+     * @param QueryCollectionFactory $queryCollectionFactory
+     * @param StoreManagerInterface $storeManager
+     */
+    public function __construct(
+        $queryText,
+        $resultsCount,
+        QueryCollectionFactory $queryCollectionFactory,
+        StoreManagerInterface  $storeManager
+    ) {
         $this->queryText = $queryText;
         $this->resultsCount = $resultsCount;
+        $this->queryCollectionFactory = $queryCollectionFactory;
+        $this->storeManager = $storeManager;
     }
 
     /**
      * @return string
      */
-    public function getQueryText()
+    public function getQueryText(): string
     {
         return $this->queryText;
     }
 
     /**
      * @return int
+     * @throws NoSuchEntityException
      */
-    public function getResultsCount()
+    public function getResultsCount(): int
     {
+        $collection = $this->queryCollectionFactory->create()->setStoreId(
+            $this->storeManager->getStore()->getId()
+        )->setQueryFilter(
+            $this->getQueryText()
+        );
+        foreach ($collection as $item) {
+            $this->resultsCount = $item->getData('num_results');
+        }
         return $this->resultsCount;
     }
 }
