d22a1ea31aa67ba3548e94407b4d1a285ccc038c
Dmytro Poperechnyy <dpoperechnyy@magento.com>
2022-02-03 19:32:48 -0600

    MCP-903: Fix default connection = db setting after Magento installation

--- app/code/Magento/MessageQueue/Setup/ConfigOptionsList.php
+++ app/code/Magento/MessageQueue/Setup/ConfigOptionsList.php
@@ -62,8 +62,7 @@ class ConfigOptionsList implements ConfigOptionsListInterface
                 self::INPUT_KEY_QUEUE_DEFAULT_CONNECTION,
                 TextConfigOption::FRONTEND_WIZARD_TEXT,
                 self::CONFIG_PATH_QUEUE_DEFAULT_CONNECTION,
-                'Message queues default connection. Can be db, amqp or a custom one.',
-                self::DEFAULT_QUEUE_CONNECTION
+                'Message queues default connection. Can be \'db\', \'amqp\' or a custom queue system that is installed and configured. Otherwise messages won\'t be processed correctly.'
             ),
         ];
     }
