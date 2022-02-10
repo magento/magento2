c0448daa3efa95a4e89cd0d64f2a70f7abdd8094
silinmykola <m.silin@atwix.com>
2022-02-10 18:00:36 +0200

    34588 fix simple product position in Groped product after delete simple product

--- app/code/Magento/Ui/view/base/web/js/dynamic-rows/dynamic-rows.js
+++ app/code/Magento/Ui/view/base/web/js/dynamic-rows/dynamic-rows.js
@@ -952,7 +952,7 @@ define([
                     elems[idx].recordId = rec[this.identificationProperty];
                 }
 
-                if (!rec.position) {
+                if (!rec.position && rec.position !== 0) {
                     rec.position = this.maxPosition;
                     this.setMaxPosition();
                 }
