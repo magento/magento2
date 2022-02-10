d9235e9ecec2a4ce1bb157f831126cf98af4764a
Sebastian Kull <sebastianKull@web.de>
2022-02-10 10:39:26 +0100

    CABPI-171: cleanup code and remove not needed constructor arguments

--- app/code/Magento/AdminAdobeIms/Controller/Adminhtml/OAuth/Callback.php
+++ app/code/Magento/AdminAdobeIms/Controller/Adminhtml/OAuth/Callback.php
@@ -10,58 +10,40 @@ namespace Magento\AdminAdobeIms\Controller\Adminhtml\OAuth;
 
 use Magento\Backend\App\Action\Context;
 use Magento\AdminAdobeIms\Model\Connection;
-use Magento\Backend\Model\UrlInterface;
+use Magento\Backend\Controller\Adminhtml\Auth;
+use Magento\Backend\Model\View\Result\Redirect;
 use Magento\Framework\App\Action\HttpGetActionInterface;
-use Magento\Framework\App\Action\HttpPostActionInterface;
-use Magento\Framework\Controller\Result\JsonFactory;
-use Magento\Framework\Controller\ResultInterface;
 use Magento\Framework\Exception\AuthenticationException;
+use Magento\Framework\Exception\AuthorizationException;
 use Magento\Framework\Message\ManagerInterface;
 
-class Callback extends \Magento\Backend\Controller\Adminhtml\Auth implements HttpPostActionInterface, HttpGetActionInterface
+class Callback extends Auth implements HttpGetActionInterface
 {
-    /**
-     * @var JsonFactory
-     */
-    protected $resultJsonFactory;
-
     /**
      * @var Connection
      */
     private Connection $connection;
 
     /**
-     * @var UrlInterface
-     */
-    private UrlInterface $backendUrl;
-
-    /**
      * @param Context $context
-     * @param JsonFactory $resultJsonFactory
      * @param Connection $connection
-     * @param UrlInterface $backendUrl
      * @param ManagerInterface $messageManager
      */
     public function __construct(
         Context $context,
-        JsonFactory $resultJsonFactory,
         Connection $connection,
-        UrlInterface $backendUrl,
         ManagerInterface $messageManager
     ) {
         parent::__construct($context);
-        $this->context = $context;
-        $this->resultJsonFactory = $resultJsonFactory;
         $this->connection = $connection;
-        $this->backendUrl = $backendUrl;
         $this->messageManager = $messageManager;
     }
 
     /**
-     * @return \Magento\Framework\App\ResponseInterface|ResultInterface|void
-     * @throws \Magento\Framework\Exception\AuthorizationException
+     * @return Redirect
+     * @throws AuthorizationException
      */
-    public function execute()
+    public function execute(): Redirect
     {
         $code = $this->getRequest()->getParam('code');
 
@@ -69,8 +51,8 @@ class Callback extends \Magento\Backend\Controller\Adminhtml\Auth implements Htt
          * todo: add error-handling for empty code or invalid/empty accessToken
          */
 
-//        $accessToken = $this->connection->getAccessToken($code);
-//        $profile = $this->connection->getProfile($accessToken);
+        $accessToken = $this->connection->getAccessToken($code);
+        $profile = $this->connection->getProfile($accessToken);
 
         try {
             if (false) {
@@ -83,7 +65,7 @@ class Callback extends \Magento\Backend\Controller\Adminhtml\Auth implements Htt
             $this->messageManager->addErrorMessage($e->getMessage());
         }
 
-        /** @var \Magento\Backend\Model\View\Result\Redirect $resultRedirect */
+        /** @var Redirect $resultRedirect */
         $resultRedirect = $this->resultRedirectFactory->create();
         return $resultRedirect->setPath($this->_helper->getHomePageUrl());
     }
