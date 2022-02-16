bb604930b9b0a0f11de1f1d994d9f832049badcf
Mohan Ahuja <>
2022-02-16 11:33:49 +0530

    ACP2E-98: Customer with global account sharing has to login again in the secondary website when guest checkout is disabled - Updated jasmine test for customer-global-session-loader.js file

--- dev/tests/js/jasmine/tests/app/code/Magento/Customer/frontend/js/customer-global-session-loader.test.js
+++ dev/tests/js/jasmine/tests/app/code/Magento/Customer/frontend/js/customer-global-session-loader.test.js
@@ -11,22 +11,26 @@ define([
     'use strict';
 
     describe('Magento_Customer/js/customer-global-session-loader', function () {
-        var injector = new Squire(),
+        var injector,
             customer = ko.observable({}),
             mocks = {
                 'Magento_Customer/js/customer-data': {
                     get: jasmine.createSpy('get', function () {
                         return customer;
                     }).and.callThrough(),
-                    reload: jasmine.createSpy(),
+                    reload: jasmine.createSpy('reload'),
                     getInitCustomerData: function () {}
                 }
             },
             deferred,
             customerSessionLoader;
 
-        beforeEach(function (done) {
+        beforeAll(function () {
             $('body').append('<div id="customerMenu" class="customer-menu">Customer Menu</div>');
+        });
+
+        beforeEach(function (done) {
+            injector = new Squire();
             injector.mock(mocks);
             injector.require(['Magento_Customer/js/customer-global-session-loader'], function (instance) {
                 customerSessionLoader = instance;
@@ -38,62 +42,19 @@ define([
             try {
                 injector.clean();
                 injector.remove();
-            } catch (e) {}
+                injector = null;
+            } catch (e) {
+                console.log(e);
+            }
 
             customer({});
+        });
+
+        afterAll(function() {
             $('#customerMenu').remove();
         });
 
         describe('Check customer data preparation process', function () {
-            it('Tests that customer data is NOT checked before initialization', function () {
-                spyOn(mocks['Magento_Customer/js/customer-data'], 'getInitCustomerData').and.callFake(function () {
-                    deferred = $.Deferred();
-
-                    return deferred.promise();
-                });
-                expect(customerSessionLoader()).toBe(undefined);
-
-                expect(mocks['Magento_Customer/js/customer-data'].get).toHaveBeenCalledWith('customer');
-                expect(mocks['Magento_Customer/js/customer-data'].getInitCustomerData).toHaveBeenCalled();
-                expect(mocks['Magento_Customer/js/customer-data'].reload).not.toHaveBeenCalled();
-            });
-
-            it('Tests that customer data reloads if customer first name is not there', function () {
-                spyOn(mocks['Magento_Customer/js/customer-data'], 'getInitCustomerData').and.callFake(function () {
-                    deferred = $.Deferred();
-
-                    deferred.resolve();
-
-                    return deferred.promise();
-                });
-                customer({
-                    _data: null
-                });
-                customerSessionLoader();
-
-                expect(mocks['Magento_Customer/js/customer-data'].get).toHaveBeenCalledWith('customer');
-                expect(mocks['Magento_Customer/js/customer-data'].reload).toHaveBeenCalledWith([], false);
-            });
-
-            it('Tests that customer data is checked only after initialization', function () {
-                spyOn(mocks['Magento_Customer/js/customer-data'], 'getInitCustomerData').and.callFake(function () {
-                    deferred = $.Deferred();
-
-                    return deferred.promise();
-                });
-                customer({
-                    firstname: "First Name"
-                });
-                customerSessionLoader();
-
-                expect(mocks['Magento_Customer/js/customer-data'].get).toHaveBeenCalledWith('customer');
-                expect(mocks['Magento_Customer/js/customer-data'].reload).not.toHaveBeenCalled();
-
-                deferred.resolve();
-
-                expect(mocks['Magento_Customer/js/customer-data'].reload).toHaveBeenCalledWith([], false);
-            });
-
             it('Tests that customer data does not reloads if it has first name defined', function () {
                 spyOn(mocks['Magento_Customer/js/customer-data'], 'getInitCustomerData').and.callFake(function () {
                     deferred = $.Deferred();
@@ -109,6 +70,40 @@ define([
 
                 expect(mocks['Magento_Customer/js/customer-data'].get).toHaveBeenCalledWith('customer');
                 expect(mocks['Magento_Customer/js/customer-data'].reload).not.toHaveBeenCalled();
+            });
+
+            it('Tests that customer data reloads if customer first name data is not there but wait until promise resolved', function () {
+                spyOn(mocks['Magento_Customer/js/customer-data'], 'getInitCustomerData').and.callFake(function () {
+                    deferred = $.Deferred();
+
+                    return deferred.promise();
+                });
+                customer({
+                    _data: null
+                });
+                customerSessionLoader();
+
+                expect(mocks['Magento_Customer/js/customer-data'].get).toHaveBeenCalledWith('customer');
+                expect(mocks['Magento_Customer/js/customer-data'].reload).not.toHaveBeenCalled();
+
+                deferred.resolve();
+
+                expect(mocks['Magento_Customer/js/customer-data'].reload).toHaveBeenCalledWith([], false);
+            });
+
+            it('Tests that customer data reloads if customer data is not there', function () {
+                spyOn(mocks['Magento_Customer/js/customer-data'], 'getInitCustomerData').and.callFake(function () {
+                    deferred = $.Deferred();
+
+                     deferred.resolve();
+
+                    return deferred.promise();
+                });
+                expect(customerSessionLoader()).toBe(undefined);
+
+                expect(mocks['Magento_Customer/js/customer-data'].get).toHaveBeenCalledWith('customer');
+                expect(mocks['Magento_Customer/js/customer-data'].getInitCustomerData).toHaveBeenCalled();
+                expect(mocks['Magento_Customer/js/customer-data'].reload).toHaveBeenCalledWith([], false);
             });
         });
     });
