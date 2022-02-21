1ae0ffd911e7602c87a098fb9e6356c9f6a4c633
anastasiia <anastasiia.miednykh@tudock.de>
2022-02-21 16:57:48 +0100

    CABPI-197 - Extend Admin Logout to remove IMS related tokens/credentials on Magento side - adding event for admin logout - fixing captcha observer name for the plugin in di.xml

---
+++ app/code/Magento/AdminAdobeIms/Observer/AdminLogoutObserver.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Magento\AdminAdobeIms\Observer;
+
+use Magento\AdobeIms\Model\LogOut;
+use Magento\Framework\Event\ObserverInterface;
+
+class AdminLogoutObserver implements ObserverInterface
+{
+    private LogOut $logOut;
+
+    /**
+     * @param LogOut $logOut
+     */
+    public function __construct(
+        LogOut $logOut
+    ) {
+        $this->logOut = $logOut;
+    }
+    /**
+     *
+     *
+     * @param \Magento\Framework\Event\Observer $observer
+     * @return $this
+     */
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        $res = $this->logOut->execute();
+        return $this;
+    }
+}
