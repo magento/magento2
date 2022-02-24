fa6a944378024b96935a9943bab3f515ddb4ca3a
Nathan Smith <nathsmit@adobe.com>
2022-02-24 14:46:40 -0600

    AC-2461: Implement email and newsletter template compatibility CLI check

---
+++ dev/tests/integration/testsuite/Magento/Email/Model/Template/VariableCompatibilityCheckerTest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Magento\Email\Test\Unit\Model\Template;
+
+use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
+use PHPUnit\Framework\TestCase;
+
+class VariableCompatibilityCheckerTest extends TestCase
+{
+    public function testCompatibilityCheck()
+    {
+        $checker = new
+    }
+}
