a668273780eb2963585f1052a37a8c2f5d8a6ec8
Aakash Kumar <aakashk@adobe.com>
2022-03-28 21:25:44 -0500

    MCP-957: Parallel Indexation for Catalog Permission
    -Show command when indexer is disabled;

--- app/code/Magento/Indexer/Console/Command/IndexerShowDimensionsModeCommand.php
+++ app/code/Magento/Indexer/Console/Command/IndexerShowDimensionsModeCommand.php
@@ -12,6 +12,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Magento\Framework\App\ObjectManagerFactory;
 use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\Console\Cli;
+use Magento\SharedCatalog\Model\State as SharedCatalogState;
 use Symfony\Component\Console\Input\InputArgument;
 
 /**
@@ -36,26 +37,27 @@ class IndexerShowDimensionsModeCommand extends AbstractIndexerCommand
      * @var string[]
      */
     private $indexers;
+
     /**
-     * @var string[]
+     * @var SharedCatalogState
      */
-    private $optionalIndexers;
+    private $sharedCatalogState;
 
     /**
      * @param ObjectManagerFactory $objectManagerFactory
      * @param ScopeConfigInterface $configReader
+     * @param SharedCatalogState $sharedCatalogState
      * @param array $indexers
-     * @param array $optionalIndexers
      */
     public function __construct(
         ObjectManagerFactory $objectManagerFactory,
         ScopeConfigInterface $configReader,
-        array $indexers,
-        array $optionalIndexers
+        SharedCatalogState $sharedCatalogState,
+        array $indexers
     ) {
         $this->configReader = $configReader;
+        $this->sharedCatalogState = $sharedCatalogState;
         $this->indexers = $indexers;
-        $this->optionalIndexers = $optionalIndexers;
         parent::__construct($objectManagerFactory);
     }
 
@@ -99,7 +101,9 @@ class IndexerShowDimensionsModeCommand extends AbstractIndexerCommand
                 $output->writeln(sprintf('%-50s ', $indexer->getTitle() . ':') . $mode);
             }
         } catch (\Exception $e) {
-            if (!in_array($indexerId, $this->optionalIndexers)) {
+            if ($indexerId === 'catalogpermissions_category' && !$this->sharedCatalogState->isGlobal()) {
+                // do nothing
+            } else {
                 $output->writeln('"' . $indexer->getTitle() . '" indexer process unknown error:' . PHP_EOL);
                 $output->writeln($e->getMessage() . PHP_EOL);
                 // we must have an exit code higher than zero to indicate something was wrong
