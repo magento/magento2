617566d87806175cf43314cc5949d30bfa9aa98e
Valentina <valentina.zhuk@tudock.de>
2022-04-04 09:51:43 +0200

    CABPI-290: add copyright and declare strict types

--- app/code/Magento/AdminAdobeIms/Plugin/BackendAuthSessionPlugin.php
+++ app/code/Magento/AdminAdobeIms/Plugin/BackendAuthSessionPlugin.php
@@ -1,11 +1,16 @@
 <?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+declare(strict_types=1);
 
 namespace Magento\AdminAdobeIms\Plugin;
 
 use Magento\AdminAdobeIms\Model\ImsConnection;
 use Magento\AdminAdobeIms\Service\ImsConfig;
 use Magento\Backend\Model\Auth\Session;
-use Magento\Framework\Message\ManagerInterface;
 use Magento\Framework\Stdlib\DateTime\DateTime;
 
 class BackendAuthSessionPlugin
