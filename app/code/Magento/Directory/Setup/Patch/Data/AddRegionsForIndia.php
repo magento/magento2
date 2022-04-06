ef42968dc44b72b185b88893c65b3c3d72eb14a5
Manav Padhariya <52840038+manavluhar@users.noreply.github.com>
2022-04-06 13:36:05 +0530

    Added Ladakh Union Territory

---
+++ app/code/Magento/Directory/Setup/Patch/Data/AddRegionsForIndia.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\Directory\Setup\Patch\Data;
+
+use Magento\Directory\Setup\DataInstaller;
+use Magento\Framework\App\ResourceConnection;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\Patch\PatchVersionInterface;
+
+/**
+ * Add Regions/States for India.
+ */
+class AddRegionsForIndia implements DataPatchInterface, PatchVersionInterface
+{
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+
+    /**
+     * @var \Magento\Directory\Setup\DataInstallerFactory
+     */
+    private $dataInstallerFactory;
+
+    /**
+     * AddRegionsForIndia constructor.
+     *
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param \Magento\Directory\Setup\DataInstallerFactory $dataInstallerFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface                      $moduleDataSetup,
+        \Magento\Directory\Setup\DataInstallerFactory $dataInstallerFactory
+    )
+    {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->dataInstallerFactory = $dataInstallerFactory;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function apply()
+    {
+        /** @var DataInstaller $dataInstaller */
+        $dataInstaller = $this->dataInstallerFactory->create();
+        $dataInstaller->addCountryRegions(
+            $this->moduleDataSetup->getConnection(),
+            $this->getDataForIndia()
+        );
+    }
+
+    /**
+     * Indian states data.
+     *
+     * @return array
+     */
+    private function getDataForIndia()
+    {
+        return [
+            ['IN', 'LA', 'Ladakh']
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function getDependencies()
+    {
+        return [
+            InitializeDirectoryData::class,
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function getVersion()
+    {
+        return '1.0.0';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+}
