eb02b42eabd412f09c5bf2faba70579c80eff29e
rajan-adobe <glo26808@adobe.com>
2022-04-12 02:18:54 +0530

    ACP2E-209: "Table rate" shipping method displays as "Not yet calculated" on checkout Integration test failures fixed.

--- dev/tests/integration/testsuite/Magento/Quote/Model/ShippingMethodManagementTest.php
+++ dev/tests/integration/testsuite/Magento/Quote/Model/ShippingMethodManagementTest.php
@@ -129,59 +129,6 @@ class ShippingMethodManagementTest extends TestCase
     }
 
     /**
-     *
-     * Test table rate with zero amount is available for the cart when discount coupon cart price rule to all items
-     * and freeshipping cart price rule is applied when order subtotal is greater than specified amount.
-     *
-     * @magentoConfigFixture current_store carriers/tablerate/active 1
-     * @magentoConfigFixture current_store carriers/flatrate/active 0
-     * @magentoConfigFixture current_store carriers/freeshipping/active 0
-     * @magentoConfigFixture default_store carriers/tablerate/condition_name package_value_with_discount
-     * @magentoDataFixture Magento/Catalog/_files/categories.php
-     * @magentoDataFixture Magento/SalesRule/_files/cart_rule_free_shipping_subtotal_equals_more_than_30.php
-     * @magentoDataFixture Magento/SalesRule/_files/cart_rule_20_percent_off_with_coupon_no_condition.php
-     * @magentoDataFixture Magento/Sales/_files/quote_with_multiple_products.php
-     * @magentoDataFixture Magento/OfflineShipping/_files/tablerates_price.php
-     *
-     * @return void
-     */
-    public function testTableRateWithZeroPriceShownWhenDiscountCouponAndFreeShippingCartRuleApplied()
-    {
-        $objectManager = Bootstrap::getObjectManager();
-        $quote = $this->getQuote('tableRate');
-        $cartId = $quote->getId();
-        if (!$cartId) {
-            $this->fail('quote fixture failed');
-        }
-        /** @var QuoteIdMask $quoteIdMask */
-        $quoteIdMask = Bootstrap::getObjectManager()
-            ->create(QuoteIdMaskFactory::class)
-            ->create();
-        $quoteIdMask->load($cartId, 'quote_id');
-        //Use masked cart Id
-        $cartId = $quoteIdMask->getMaskedId();
-        $addressFactory = $this->objectManager->get(AddressInterfaceFactory::class);
-        /** @var \Magento\Quote\Api\Data\AddressInterface $address */
-        $address = $addressFactory->create();
-        $address->setCountryId('US');
-        /** @var CouponManagementInterface $couponManagement */
-        $couponManagement = Bootstrap::getObjectManager()->get(CouponManagementInterface::class);
-        $couponManagement->set($quote->getId(), '123');
-        /** @var  GuestShippingMethodManagementInterface $shippingEstimation */
-        $shippingEstimation = $objectManager->get(GuestShippingMethodManagementInterface::class);
-        $result = $shippingEstimation->estimateByExtendedAddress($cartId, $address);
-        $this->assertCount(1, $result);
-        $rate = reset($result);
-
-        $expectedResult = [
-            'method_code' => 'bestway',
-            'amount' => 0
-        ];
-        $this->assertEquals($expectedResult['method_code'], $rate->getMethodCode());
-        $this->assertEquals($expectedResult['amount'], $rate->getAmount());
-    }
-
-    /**
      * Test table rate amount for the cart that contains some items with free shipping applied.
      *
      * @magentoConfigFixture current_store carriers/tablerate/active 1
@@ -500,5 +447,56 @@ class ShippingMethodManagementTest extends TestCase
         foreach ($configData as $data) {
             $config->setValue($data['path'], $data['value'], $data['scope']);
         }
+    }
+
+    /**
+     *
+     * Test table rate with zero amount is available for the cart when discount coupon cart price rule to all items
+     * and freeshipping cart price rule is applied when order subtotal is greater than specified amount.
+     *
+     * @magentoConfigFixture default_store carriers/tablerate/active 1
+     * @magentoConfigFixture default_store carriers/flatrate/active 0
+     * @magentoConfigFixture default_store carriers/freeshipping/active 0
+     * @magentoConfigFixture default_store carriers/tablerate/condition_name package_value_with_discount
+     * @magentoDataFixture Magento/SalesRule/_files/cart_rule_free_shipping_subtotal_equals_more_than_30.php
+     * @magentoDataFixture Magento/SalesRule/_files/cart_rule_20_percent_off_with_coupon_no_condition.php
+     * @magentoDataFixture Magento/Sales/_files/quote_with_multiple_products.php
+     * @magentoDataFixture Magento/OfflineShipping/_files/tablerates_price.php
+     * @return void
+     */
+    public function testTableRateWithZeroPriceShownWhenDiscountCouponAndFreeShippingCartRuleApplied()
+    {
+        $objectManager = Bootstrap::getObjectManager();
+        $quote = $this->getQuote('tableRate');
+        $cartId = $quote->getId();
+        if (!$cartId) {
+            $this->fail('quote fixture failed');
+        }
+        /** @var QuoteIdMask $quoteIdMask */
+        $quoteIdMask = Bootstrap::getObjectManager()
+            ->create(QuoteIdMaskFactory::class)
+            ->create();
+        $quoteIdMask->load($cartId, 'quote_id');
+        //Use masked cart Id
+        $cartId = $quoteIdMask->getMaskedId();
+        $addressFactory = $this->objectManager->get(AddressInterfaceFactory::class);
+        /** @var \Magento\Quote\Api\Data\AddressInterface $address */
+        $address = $addressFactory->create();
+        $address->setCountryId('US');
+        /** @var CouponManagementInterface $couponManagement */
+        $couponManagement = Bootstrap::getObjectManager()->get(CouponManagementInterface::class);
+        $couponManagement->set($quote->getId(), '123');
+        /** @var  GuestShippingMethodManagementInterface $shippingEstimation */
+        $shippingEstimation = $objectManager->get(GuestShippingMethodManagementInterface::class);
+        $result = $shippingEstimation->estimateByExtendedAddress($cartId, $address);
+        $this->assertCount(1, $result);
+        $rate = reset($result);
+
+        $expectedResult = [
+            'method_code' => 'bestway',
+            'amount' => 0
+        ];
+        $this->assertEquals($expectedResult['method_code'], $rate->getMethodCode());
+        $this->assertEquals($expectedResult['amount'], $rate->getAmount());
     }
 }
