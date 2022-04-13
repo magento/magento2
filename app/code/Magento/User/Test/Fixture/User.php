54e42a6f5871a8bdd21c3edb5d6e6df9cae50292
alzota <97607802+alzota@users.noreply.github.com>
2022-04-13 16:32:29 +0300

    Update app/code/Magento/User/Test/Fixture/User.php
    Co-authored-by: Buba Soumah <bubasuma@users.noreply.github.com>

--- app/code/Magento/User/Test/Fixture/User.php
+++ app/code/Magento/User/Test/Fixture/User.php
@@ -23,7 +23,7 @@ class User implements RevertibleDataFixtureInterface
         'username' => 'adminuser%uniqid%',
         'firstname' => 'Firstname %uniqid%',
         'lastname' => 'Lastname %uniqid%',
-        'email' => 'admin_user_%uniqid%@email.com',
+        'email' => 'adminUser%uniqid%@example.com',
         'password' => Bootstrap::ADMIN_PASSWORD,
         'interface_locale' => 'en_US',
         'is_active' => 1
