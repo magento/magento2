b15c021c66c9217d333989cb7a696614981e12fa
Sebastian Kull <sebastianKull@web.de>
2022-04-28 14:15:39 +0200

    CABPI-333: Fix static-tests and improve code

--- app/code/Magento/AdminAdobeIms/Plugin/PerformIdentityCheckMessagePlugin.php
+++ app/code/Magento/AdminAdobeIms/Plugin/PerformIdentityCheckMessagePlugin.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+declare(strict_types=1);
 
 namespace Magento\AdminAdobeIms\Plugin;
 
@@ -30,10 +36,11 @@ class PerformIdentityCheckMessagePlugin
      * @param string $passwordString
      * @return mixed
      * @throws AuthenticationException
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
     public function aroundPerformIdentityCheck(User $subject, callable $proceed, string $passwordString)
     {
-        if ($this->imsConfig->enabled() === false)  {
+        if ($this->imsConfig->enabled() === false) {
             return $proceed($passwordString);
         }
 
