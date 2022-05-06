1e298b0e2aa8479a956cc40d3de4ed905dc30333
glo82145 <glo82145@adobe.com>
2022-05-06 19:04:47 +0530

    AC-2765::Modified Code for testcase in CollectionTest.php

--- dev/tests/integration/testsuite/Magento/Review/Model/ResourceModel/Review/Summary/CollectionTest.php
+++ dev/tests/integration/testsuite/Magento/Review/Model/ResourceModel/Review/Summary/CollectionTest.php
@@ -7,40 +7,120 @@ declare(strict_types=1);
 
 namespace Magento\Review\Model\ResourceModel\Review\Summary;
 
+use Magento\Framework\Data\Collection\Db\FetchStrategy\Query;
+use Magento\Framework\Data\Collection\EntityFactory;
+use Magento\Framework\DB\Adapter\AdapterInterface;
+use Magento\Framework\DB\Adapter\Pdo\Mysql;
 use Magento\Framework\DB\Select;
+use Magento\Framework\DB\Select\SelectRenderer;
+use Magento\Framework\Model\ResourceModel\Db\AbstractDb;
+use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
 use Magento\Review\Model\ResourceModel\Review\Summary\Collection;
 use PHPUnit\Framework\MockObject\MockObject;
+use Psr\Log\LoggerInterface;
+
 
 /**
- * Tests some functionality of the Product Review collection
+ * Tests some functionality of the Review Summary collection
  */
 class CollectionTest extends \PHPUnit\Framework\TestCase
 {
     /**
-     * @var Collection|MockObject
+     * @var Collection
      */
-    protected Collection|MockObject $_model;
+    protected Collection $collection;
+
+    /**
+     * @var Query|MockObject
+     */
+    protected Query|MockObject $fetchStrategyMock;
+
+    /**
+     * @var EntityFactory|MockObject
+     */
+    protected EntityFactory|MockObject $entityFactoryMock;
+
+    /**
+     * @var LoggerInterface|MockObject
+     */
+    protected LoggerInterface|MockObject $loggerMock;
+
+    /**
+     * @var AbstractDb|MockObject
+     */
+    protected MockObject|AbstractDb $resourceMock;
+
+    /**
+     * @var AdapterInterface|MockObject
+     */
+    protected MockObject|Mysql|AdapterInterface $connectionMock;
+
+    /**
+     * @var Select|MockObject
+     */
+    protected Select|MockObject $selectMock;
 
     protected function setUp(): void
     {
-        $this->_model = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create(Collection::class);
+        $this->fetchStrategyMock = $this->createPartialMock(
+            Query::class,
+            ['fetchAll']
+        );
+        $this->entityFactoryMock = $this->createPartialMock(
+            EntityFactory::class,
+            ['create']
+        );
+        $this->loggerMock = $this->getMockForAbstractClass(LoggerInterface::class);
+        $this->resourceMock = $this->getMockBuilder(AbstractDb::class)
+            ->setMethods(['getConnection', 'getMainTable', 'getTable'])
+            ->disableOriginalConstructor()
+            ->getMockForAbstractClass();
+        $this->connectionMock = $this->createPartialMock(
+            Mysql::class,
+            ['select', 'query']
+        );
+        $selectRenderer = $this->getMockBuilder(SelectRenderer::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->selectMock = $this->getMockBuilder(Select::class)
+            ->setMethods(['where'])
+            ->setConstructorArgs(['adapter' => $this->connectionMock, 'selectRenderer' => $selectRenderer])
+            ->getMock();
+        $this->connectionMock->expects($this->once())
+            ->method('select')
+            ->willReturn($this->selectMock);
+        $this->resourceMock->expects($this->once())
+            ->method('getConnection')
+            ->willReturn($this->connectionMock);
+        $this->resourceMock->expects($this->once())
+            ->method('getMainTable')
+            ->willReturn('main_table_name');
+
+        $this->resourceMock->expects($this->once())
+            ->method('getTable')
+            ->willReturnArgument(0);
+        $objectManager = new ObjectManager($this);
+        $this->collection = $objectManager->getObject(
+            Collection::class,
+            [
+                'entityFactory' => $this->entityFactoryMock,
+                'logger' => $this->loggerMock,
+                'fetchStrategy' => $this->fetchStrategyMock,
+                'resource' => $this->resourceMock
+            ]
+        );
     }
 
     /**
      * @param array|int $storeId
+     * @param string $expectedQuery
      * @dataProvider storeIdDataProvider
      */
-    public function testAddStoreFilter(array|int $storeId)
+    public function testAddStoreFilter(array|int $storeId, string $expectedQuery)
     {
-        $expectedWhere = is_numeric($storeId) ? 'store_id = ?' : 'store_id IN (?)';
+        $this->selectMock->expects($this->once())->method('where')->with($expectedQuery, $storeId);
+        $this->collection->addStoreFilter($storeId);
 
-        $select = $this->createPartialMock(Select::class, ['where']);
-        $select->expects( $this->any())
-            ->method('where')
-            ->with($this->equalTo($expectedWhere))
-            ->willReturnSelf();
-
-        $this->assertEquals($this->_model, $this->_model->addStoreFilter($storeId));
     }
 
     /**
@@ -49,8 +129,8 @@ class CollectionTest extends \PHPUnit\Framework\TestCase
     public function storeIdDataProvider(): array
     {
         return [
-            [1],
-           [1, [1,2]]
+            [1, 'store_id = ?'],
+            [[1,2], 'store_id IN (?)']
         ];
     }
 }
