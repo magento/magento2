f32d72e8a2edc4be1c52d76a0e01dbcb399672c0
Mohan Ahuja <>
2022-06-09 12:32:44 +0530

    ACP2E-726: Unable to query bulk operations by search criteria using REST Api - Moved the Test file in integration test and fixed static test failure

--- dev/tests/integration/testsuite/Magento/AsynchronousOperations/Model/ResourceModel/Operation/CollectionTest.php
+++ dev/tests/integration/testsuite/Magento/AsynchronousOperations/Model/ResourceModel/Operation/CollectionTest.php
@@ -1,11 +1,16 @@
 <?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
 
-namespace Magento\AsynchronousOperations\Test\Unit\Model\ResourceModel\Operation;
+namespace Magento\AsynchronousOperations\Model\ResourceModel\Operation;
 
 use Magento\AsynchronousOperations\Model\ResourceModel\Operation as OperationResourceModel;
+use Magento\AsynchronousOperations\Model\ResourceModel\Operation\CollectionFactory as OperationCollectionFactory;
 use Magento\TestFramework\Helper\Bootstrap;
 use PHPUnit\Framework\TestCase;
-use Magento\AsynchronousOperations\Model\ResourceModel\Operation\CollectionFactory as OperationCollectionFactory;
 
 class CollectionTest extends TestCase
 {
