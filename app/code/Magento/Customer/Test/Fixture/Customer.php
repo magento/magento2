2cf7b3a2d3f68f0dbf20f00d8e6e0f36afdcb674
Alexandra Zota <zota@adobe.com>
2022-07-26 13:08:15 +0300

    ACP2E-894: wip adding parametrized data fixtures

---
+++ app/code/Magento/Customer/Test/Fixture/Customer.php
@@ -0,0 +1,213 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Magento\Customer\Test\Fixture;
+
+use Magento\Customer\Api\AddressRepositoryInterface;
+use Magento\Customer\Api\CustomerRepositoryInterface;
+use Magento\Customer\Api\Data\AddressInterface;
+use Magento\Customer\Api\Data\AddressInterfaceFactory;
+use Magento\Customer\Api\Data\CustomerInterface;
+use Magento\Customer\Model\CustomerFactory;
+use Magento\Framework\DataObject;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\TestFramework\Fixture\Api\DataMerger;
+use Magento\TestFramework\Fixture\Api\ServiceFactory;
+use Magento\TestFramework\Fixture\Data\ProcessorInterface;
+use Magento\TestFramework\Fixture\RevertibleDataFixtureInterface;
+
+/**
+ * Data fixture for customer
+ */
+class Customer implements RevertibleDataFixtureInterface
+{
+    private const DEFAULT_DATA = [
+        'password' => null,
+        CustomerInterface::ID => null,
+        CustomerInterface::CONFIRMATION => null,
+        CustomerInterface::CREATED_AT => null,
+        CustomerInterface::UPDATED_AT => null,
+        CustomerInterface::CREATED_IN => null,
+        CustomerInterface::DOB => null,
+        CustomerInterface::EMAIL => 'customer%uniqid%@mail.com',
+        CustomerInterface::FIRSTNAME => 'Firstname %uniqid%',
+        CustomerInterface::GENDER => null,
+        CustomerInterface::GROUP_ID => null,
+        CustomerInterface::LASTNAME => 'Lastname %uniqid%',
+        CustomerInterface::MIDDLENAME => null,
+        CustomerInterface::PREFIX => null,
+        CustomerInterface::STORE_ID => null,
+        CustomerInterface::SUFFIX => null,
+        CustomerInterface::TAXVAT => null,
+        CustomerInterface::WEBSITE_ID => null,
+        CustomerInterface::DEFAULT_BILLING => null,
+        CustomerInterface::DEFAULT_SHIPPING => null,
+        CustomerInterface::KEY_ADDRESSES => [],
+        CustomerInterface::DISABLE_AUTO_GROUP_CHANGE => null
+    ];
+
+    private const DEFAULT_DATA_ADDRESS = [
+        AddressInterface::ID => null,
+        AddressInterface::CUSTOMER_ID => null,
+        AddressInterface::REGION => null,
+        AddressInterface::REGION_ID => null,
+        AddressInterface::COUNTRY_ID => null,
+        AddressInterface::STREET => null,
+        AddressInterface::COMPANY => null,
+        AddressInterface::TELEPHONE => null,
+        AddressInterface::FAX => null,
+        AddressInterface::POSTCODE => null,
+        AddressInterface::CITY => null,
+        AddressInterface::FIRSTNAME => 'Firstname %uniqid%',
+        AddressInterface::LASTNAME => 'Lastname %uniqid%',
+        AddressInterface::MIDDLENAME => null,
+        AddressInterface::PREFIX => null,
+        AddressInterface::SUFFIX => null,
+        AddressInterface::VAT_ID => null,
+        AddressInterface::DEFAULT_BILLING => null,
+        AddressInterface::DEFAULT_SHIPPING => null
+    ];
+
+    /**
+     * @var ServiceFactory
+     */
+    private $serviceFactory;
+
+    /**
+     * @var CustomerRepositoryInterface
+     */
+    private $customerRepository;
+
+    /**
+     * @var CustomerFactory
+     */
+    private $customerFactory;
+
+    /**
+     * @var AddressRepositoryInterface
+     */
+    private $addressRepository;
+
+    /**
+     * @var AddressInterfaceFactory
+     */
+    private $addressDataFactory;
+
+    /**
+     * @var ProcessorInterface
+     */
+    private $dataProcessor;
+
+    /**
+     * @var DataMerger
+     */
+    private $dataMerger;
+
+    /**
+     * @var null
+     */
+    private $customer;
+
+    /**
+     * @param ServiceFactory $serviceFactory
+     * @param CustomerRepositoryInterface $customerRepository
+     * @param CustomerFactory $customerFactory
+     * @param AddressRepositoryInterface $addressRepository
+     * @param AddressInterfaceFactory $addressDataFactory
+     * @param ProcessorInterface $dataProcessor
+     * @param DataMerger $dataMerger
+     */
+    public function __construct(
+        ServiceFactory $serviceFactory,
+        CustomerRepositoryInterface $customerRepository,
+        CustomerFactory $customerFactory,
+        AddressRepositoryInterface $addressRepository,
+        AddressInterfaceFactory $addressDataFactory,
+        ProcessorInterface $dataProcessor,
+        DataMerger $dataMerger,
+    ) {
+        $this->serviceFactory = $serviceFactory;
+        $this->customerRepository = $customerRepository;
+        $this->customerFactory = $customerFactory;
+        $this->addressRepository = $addressRepository;
+        $this->addressDataFactory = $addressDataFactory;
+        $this->dataProcessor = $dataProcessor;
+        $this->dataMerger = $dataMerger;
+        $this->customer = null;
+    }
+
+    /**
+     * {@inheritdoc}
+     * @param array $data Parameters. Same format as Customer::DEFAULT_DATA.
+     * @return DataObject|null
+     * @throws LocalizedException
+     * @throws NoSuchEntityException
+     */
+    public function apply(array $data = []): ?DataObject
+    {
+        $customerSaveService = $this->serviceFactory->create(CustomerRepositoryInterface::class, 'save');
+        $data = $this->prepareData($data);
+        if (count($data[CustomerInterface::KEY_ADDRESSES])) {
+            $addresses = $this->prepareCustomerAddress($data[CustomerInterface::KEY_ADDRESSES]);
+            $data[CustomerInterface::KEY_ADDRESSES] = $addresses;
+        }
+        return $customerSaveService->execute(
+            ['customer' => $data, 'passwordHash' => $this->customer->getPasswordHash()]
+        );
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function revert(DataObject $data): void
+    {
+        $service = $this->serviceFactory->create(CustomerRepositoryInterface::class, 'deleteById');
+        $service->execute(
+            [
+                'id' => $data->getId()
+            ]
+        );
+    }
+
+    /**
+     * Prepare customer data
+     *
+     * @param array $data
+     * @return array
+     */
+    private function prepareData(array $data): array
+    {
+        $data = $this->dataMerger->merge(self::DEFAULT_DATA, $data, false);
+
+        $this->customer = $this->customerFactory->create()->setData($data);
+        $this->customer->setPassword($data['password']);
+        if (isset($data['password'])) {
+            unset($data['password']);
+        }
+
+        return $this->dataProcessor->process($this, $data);
+    }
+
+    /**
+     * Prepare customer address
+     *
+     * @param array $data
+     * @return array
+     */
+    private function prepareCustomerAddress(array $data): array
+    {
+        $addresses = [];
+        foreach ($data as $dataAddress) {
+            $dataAddress = $this->dataMerger->merge(self::DEFAULT_DATA_ADDRESS, $dataAddress, false);
+            $addresses[] = $this->dataProcessor->process($this, $dataAddress);
+        }
+
+        return $addresses;
+    }
+}
---
+++ dev/tests/api-functional/testsuite/Magento/GraphQl/Sales/CustomerOrders/GetCustomerOrdersTest.php
@@ -0,0 +1,139 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\GraphQl\Sales;
+
+use Magento\Catalog\Helper\Data;
+use Magento\Catalog\Test\Fixture\Product as ProductFixture;
+use Magento\Customer\Api\CustomerRepositoryInterface;
+use Magento\Customer\Model\CustomerAuthUpdate;
+use Magento\Customer\Model\CustomerRegistry;
+use Magento\Customer\Test\Fixture\Customer;
+use Magento\Framework\ObjectManagerInterface;
+use Magento\Integration\Api\CustomerTokenServiceInterface;
+use Magento\Store\Test\Fixture\Group as StoreGroupFixture;
+use Magento\Store\Test\Fixture\Store as StoreFixture;
+use Magento\Store\Test\Fixture\Website as WebsiteFixture;
+use Magento\TestFramework\Fixture\Config;
+use Magento\TestFramework\Fixture\DataFixture;
+use Magento\TestFramework\Fixture\DataFixtureStorageManager;
+use Magento\TestFramework\Helper\Bootstrap;
+use Magento\TestFramework\TestCase\GraphQlAbstract;
+
+/**
+ * GraphQl tests for @see \Magento\SalesGraphQl\Model\Resolver\CustomerOrders.
+ */
+class GetCustomerOrdersTest extends GraphQlAbstract
+{
+    /**
+     * @var CustomerTokenServiceInterface
+     */
+    private $customerTokenService;
+
+    /**
+     * @var CustomerRegistry
+     */
+    private $customerRegistry;
+
+    /**
+     * @var CustomerAuthUpdate
+     */
+    private $customerAuthUpdate;
+
+    /**
+     * @var CustomerRepositoryInterface
+     */
+    private $customerRepository;
+
+    /**
+     * @var ObjectManagerInterface
+     */
+    private $objectManager;
+
+    /**
+     * @inheridoc
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->objectManager = Bootstrap::getObjectManager();
+        $this->fixtures = $this->objectManager->get(DataFixtureStorageManager::class)->getStorage();
+        $this->customerTokenService = $this->objectManager->get(CustomerTokenServiceInterface::class);
+        $this->customerRegistry = $this->objectManager->get(CustomerRegistry::class);
+        $this->customerAuthUpdate = $this->objectManager->get(CustomerAuthUpdate::class);
+        $this->customerRepository = $this->objectManager->get(CustomerRepositoryInterface::class);
+    }
+
+    #[
+        Config(Data::XML_PATH_PRICE_SCOPE, Data::PRICE_SCOPE_WEBSITE),
+        DataFixture(WebsiteFixture::class, as: 'website2'),
+        DataFixture(StoreGroupFixture::class, ['website_id' => '$website2.id$'], 'store_group2'),
+        DataFixture(StoreFixture::class, ['store_group_id' => '$store_group2.id$'], 'store2'),
+        DataFixture(StoreFixture::class, ['store_group_id' => '$store_group2.id$'], 'store3'),
+        DataFixture(ProductFixture::class, ['website_ids' => [1, '$website2.id$' ]], as: 'product'),
+        DataFixture(
+            Customer::class,
+            [
+                'email' => 'customer_graphql@mail.com',
+                'password' => 'password',
+                'store_id' => '$store2.id$',
+                'website_id' => '$website2.id$',
+                'addresses' => [
+                    [
+                        'country_id' => 'US',
+                        'region_id' => 32,
+                        'city' => 'Boston',
+                        'street' => ['10 Milk Street'],
+                        'postcode' => '02108',
+                        'telephone' => '1234567890',
+                        'default_billing' => true,
+                        'default_shipping' => true
+                    ]
+                ]
+            ],
+            as: 'customer'
+        )
+    ]
+    public function testGetCustomerOrders()
+    {
+        $currentEmail = 'customer_graphql@mail.com';
+        $currentPassword = 'password';
+
+        $query = <<<QUERY
+query {
+	customer {
+		orders(
+			pageSize: 20,
+            scope: STORE
+		) {
+			items {
+				id
+				order_number
+				order_date
+				total {
+				grand_total
+				{ value currency }}
+				status
+			}
+		}
+	}
+} 
+QUERY;
+        $response = $this->graphQlQuery(
+            $query,
+            [],
+            '',
+            $this->getCustomerAuthHeaders($currentEmail, $currentPassword)
+        );
+
+        $this->assertNull($response['customer']['id']);
+        $this->assertEquals('John', $response['customer']['firstname']);
+        $this->assertEquals('Smith', $response['customer']['lastname']);
+        $this->assertEquals($currentEmail, $response['customer']['email']);
+    }
+}
