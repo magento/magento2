4c6c0534c34336c28dc08bed731d1e75edcc21d1
Dmytro Poperechnyy <dpoperechnyy@magento.com>
2022-08-02 18:42:21 -0500

    ACPT-677: Catalog product grid improvement

--- app/code/Magento/Catalog/Ui/DataProvider/Product/ProductCollection.php
+++ app/code/Magento/Catalog/Ui/DataProvider/Product/ProductCollection.php
@@ -5,6 +5,8 @@
  */
 namespace Magento\Catalog\Ui\DataProvider\Product;
 
+use Magento\Framework\DB\Select;
+
 /**
  * Collection which is used for rendering product list in the backend.
  *
@@ -24,5 +26,57 @@ class ProductCollection extends \Magento\Catalog\Model\ResourceModel\Product\Col
     {
         $this->_productLimitationFilters->setUsePriceIndex(false);
         return $this->_productLimitationPrice(true);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getSize()
+    {
+        if ($this->_scopeConfig->getValue('admin/grid/show_approximate_total_number_of_products')) {
+            $sql = $this->getSelectCountSql();
+            return $this->analyzeCount($sql);
+        }
+
+        return parent::getSize();
+    }
+
+    /**
+     * Analyze amount of entities in DB.
+     *
+     * @param $sql
+     * @return int|mixed
+     * @throws \Zend_Db_Select_Exception
+     * @throws \Zend_Db_Statement_Exception
+     */
+    private function analyzeCount($sql)
+    {
+        $results = $this->getConnection()->query('EXPLAIN ' . $sql)->fetchAll();
+        $alias = $this->getMainTableAlias();
+
+        foreach ($results as $result) {
+            if ($result['table'] == $alias) {
+                return $result['rows'];
+            }
+        }
+
+        return 0;
+    }
+
+    /**
+     * Identify main table alias or its name if alias is not defined.
+     *
+     * @return string
+     * @throws \LogicException
+     * @throws \Zend_Db_Select_Exception
+     */
+    private function getMainTableAlias()
+    {
+        foreach ($this->getSelect()->getPart(Select::FROM) as $tableAlias => $tableMetadata) {
+            if ($tableMetadata['joinType'] == 'from') {
+                return $tableAlias;
+            }
+        }
+        throw new \LogicException("Main table cannot be identified.");
     }
 }
