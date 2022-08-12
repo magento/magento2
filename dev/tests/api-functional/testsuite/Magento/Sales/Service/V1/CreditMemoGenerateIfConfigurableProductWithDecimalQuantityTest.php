480ae1f90d8daebdb9f7bd9729d9e0b38c0ed2c2
Arularasan <glo02433@adobe.com>
2022-08-12 17:59:08 +0530

    ACP2E-1066: Credit memo not genrated if order configrable product with decimal quantity  - Fixed the static test failure.

--- dev/tests/api-functional/testsuite/Magento/Sales/Service/V1/CreditMemoGenerateIfConfigurableProductWithDecimalQuantityTest.php
+++ dev/tests/api-functional/testsuite/Magento/Sales/Service/V1/CreditMemoGenerateIfConfigurableProductWithDecimalQuantityTest.php
@@ -3,6 +3,8 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types=1);
+
 namespace Magento\Sales\Service\V1;
 
 use Magento\Checkout\Test\Fixture\PlaceOrder as PlaceOrderFixture;
