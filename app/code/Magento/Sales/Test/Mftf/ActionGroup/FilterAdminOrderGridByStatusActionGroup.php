0cfc99da9c64772a1983ad68ccf350239a441c00
Ashish.Kumar18 <ashish.kumar18@BLR1-LMC-N71378.local>
2022-08-29 16:06:27 +0530

    Unskip test EndToEndB2CAdminTest semantic fixes

--- app/code/Magento/Sales/Test/Mftf/ActionGroup/FilterAdminOrderGridByStatusActionGroup.php
+++ app/code/Magento/Sales/Test/Mftf/ActionGroup/FilterAdminOrderGridByStatusActionGroup.php
@@ -1,4 +1,4 @@
-<?xml version="1.0" encoding="UTF-8"?>
+dd<?xml version="1.0" encoding="UTF-8"?>
 <!--
  /**
   * Copyright © Magento, Inc. All rights reserved.
@@ -8,17 +8,17 @@
 
 <actionGroups xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
         xsi:noNamespaceSchemaLocation="urn:magento:mftf:Test/etc/actionGroupSchema.xsd">
-    <actionGroup name="FilterOrderGridByStatusActionGroup">
+    <actionGroup name="FilterAdminOrderGridByStatusActionGroup">
         <annotations>
             <description>Filters the Admin Orders grid based on the provided Order Status.</description>
         </annotations>
         <arguments>
-            <argument name="status"/>
+            <argument name="orderStatus"/>
         </arguments>
 
         <conditionalClick selector="{{AdminDataGridHeaderSection.clearFilters}}" dependentSelector="{{AdminDataGridHeaderSection.clearFilters}}" visible="true" stepKey="clearExistingOrderFilters"/>
         <click selector="{{AdminDataGridHeaderSection.filters}}" stepKey="openOrderGridFilters"/>
-        <selectOption selector="{{AdminDataGridHeaderSection.filterFieldSelect('status')}}" userInput="{{status}}" stepKey="fillOrderStatusFilter"/>
+        <selectOption selector="{{AdminDataGridHeaderSection.filterFieldSelect('status')}}" userInput="{{orderStatus}}" stepKey="fillOrderStatusFilter"/>
         <click selector="{{AdminDataGridHeaderSection.applyFilters}}" stepKey="clickApplyFilters"/>
     </actionGroup>
 </actionGroups>
