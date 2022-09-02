aa8c8ec7e3e19638bd861233a2ed2af89d8d8e42
Nishant Rana <glo04412@adobe.com>
2022-09-02 14:40:16 +0530

    CABPI-467: Changed exception error messages

--- app/code/Magento/AdminAdobeIms/Model/Authorization/AdobeImsAdminTokenUserContext.php
+++ app/code/Magento/AdminAdobeIms/Model/Authorization/AdobeImsAdminTokenUserContext.php
@@ -86,7 +86,7 @@ class AdobeImsAdminTokenUserContext implements UserContextInterface
             try {
                 $this->adminTokenUserService->processLoginRequest();
             } catch (\Exception $e) {
-                throw new AuthenticationException(__('Login request error. ' . $e->getMessage()), $e);
+                throw new AuthenticationException(__('Login request error  %1', $e->getMessage()), $e, 0);
             }
         }
 
