5367cbfd5381820b7f1edb05f22d1c6c0e9aeb3c
Roger <rogerdz88@gmail.com>
2022-09-07 09:47:13 +0000

    Issue 36101: Category selection tree in edit product is not in the same order as set in Catalog->Categories

--- app/code/Magento/Catalog/Ui/DataProvider/Product/Form/Modifier/Categories.php
+++ app/code/Magento/Catalog/Ui/DataProvider/Product/Form/Modifier/Categories.php
@@ -7,18 +7,18 @@ declare(strict_types=1);
 
 namespace Magento\Catalog\Ui\DataProvider\Product\Form\Modifier;
 
+use Magento\Backend\Model\Auth\Session;
+use Magento\Catalog\Model\Category as CategoryModel;
 use Magento\Catalog\Model\Locator\LocatorInterface;
 use Magento\Catalog\Model\ResourceModel\Category\CollectionFactory as CategoryCollectionFactory;
-use Magento\Framework\App\ObjectManager;
 use Magento\Framework\App\CacheInterface;
+use Magento\Framework\App\ObjectManager;
+use Magento\Framework\AuthorizationInterface;
 use Magento\Framework\DB\Helper as DbHelper;
-use Magento\Catalog\Model\Category as CategoryModel;
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Serialize\SerializerInterface;
-use Magento\Framework\UrlInterface;
 use Magento\Framework\Stdlib\ArrayManager;
-use Magento\Framework\AuthorizationInterface;
-use Magento\Backend\Model\Auth\Session;
+use Magento\Framework\UrlInterface;
 
 /**
  * Data provider for categories field of product page
@@ -52,6 +52,7 @@ class Categories extends AbstractModifier
      * @var array
      * @deprecated 101.0.0
      * @since 101.0.0
+     * @see Not use anymore
      */
     protected $categoriesTrees = [];
 
@@ -128,6 +129,7 @@ class Categories extends AbstractModifier
      *
      * @return CacheInterface
      * @deprecated 101.0.3
+     * @see getCategoriesTree
      */
     private function getCacheManager(): CacheInterface
     {
@@ -442,6 +444,8 @@ class Categories extends AbstractModifier
 
         $collection->addAttributeToFilter('entity_id', ['in' => array_keys($shownCategoriesIds)])
             ->addAttributeToSelect(['name', 'is_active', 'parent_id'])
+            ->addAttributeToSort('level', 'asc')
+            ->addAttributeToSort('position', 'asc')
             ->setStoreId($storeId);
 
         $categoryById = [
