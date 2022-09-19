ad60427f215d5a65464ecb32c809259fe146733a
Shradha Jain <shradhajain@BLR1-LHP-N81143.local>
2022-09-19 17:59:28 +0530

    Fix For Error message appears then hides then re-appears in checkout

--- app/code/Magento/Ui/view/frontend/web/js/view/messages.js
+++ app/code/Magento/Ui/view/frontend/web/js/view/messages.js
@@ -68,7 +68,10 @@ define([
             // Hide message block if needed
             if (isHidden) {
                 setTimeout(function () {
-                    $(this.selector).hide('blind', {}, this.hideSpeed);
+                    $(this.selector).hide('slow');
+
+                    //commented because effect-blind.js(1.13.1) is having show & hide issue
+                    // $(this.selector).hide('blind', {}, this.hideSpeed);
                 }.bind(this), this.hideTimeout);
             }
         }
