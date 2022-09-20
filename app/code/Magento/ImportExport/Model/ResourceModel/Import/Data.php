87d366044b624f5fe40a00514c8860b6a4038866
Aakash Kumar <aakashk@adobe.com>
2022-09-20 11:52:56 -0500

    ACPT-99: Import API Parallelization

--- app/code/Magento/ImportExport/Model/ResourceModel/Import/Data.php
+++ app/code/Magento/ImportExport/Model/ResourceModel/Import/Data.php
@@ -140,10 +140,11 @@ class Data extends \Magento\Framework\Model\ResourceModel\Db\AbstractDb implemen
      */
     public function markProcessedBunches(array $ids): void
     {
+        $where = $ids ? ['id IN (?)' => $ids] : '';
         $this->getConnection()->update(
             $this->getMainTable(),
             ['is_processed' => true],
-            ['id IN (?)' => $ids]
+            $where
         );
     }
 
