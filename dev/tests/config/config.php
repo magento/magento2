6c7c900d96a3963c01e5c6ce69469892ddc65bad
Rajesh Kumar <glo71317@adobe.com>
2022-09-21 17:07:44 +0530

    AC-2660::Fixed the issue and added old allure test results files

--- dev/tests/config/config.php
+++ dev/tests/config/config.php
@@ -1,6 +1,16 @@
 <?php
 
+$outputDirectory = 'var/allure-results';
 return [
     // Path to output directory (default is build/allure-results)
-    'outputDirectory' => 'var/allure-results'
+    'outputDirectory' => $outputDirectory,
+    'setupHook' => function () use ($outputDirectory): void {
+        $files = scandir($outputDirectory);
+        foreach ($files as $file) {
+            $filePath = $outputDirectory . DIRECTORY_SEPARATOR . $file;
+            if (is_file($filePath)) {
+                unlink($filePath);
+            }
+        }
+    }
 ];
