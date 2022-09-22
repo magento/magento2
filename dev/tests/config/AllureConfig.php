3a8e62e3de80f10dd8909dda6435d0e4da5eb86e
Rajesh Kumar <glo71317@adobe.com>
2022-09-22 22:32:08 +0530

    AC-2660::Fixed the static tests

--- dev/tests/config/AllureConfig.php
+++ dev/tests/config/AllureConfig.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace config;
 
 class AllureConfig
 {
--- dev/tests/static/allure/allure.config.php
+++ dev/tests/static/allure/allure.config.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
 
 require_once __DIR__ . "/../../config/AllureConfig.php";
 
