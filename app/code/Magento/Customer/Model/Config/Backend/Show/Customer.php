aae9d454a04ea66c39b1cf74e76a521a30c2b50a
Roger <rogerdz88@hotmail.com>
2022-10-04 16:00:50 +0000

    Issue 36196: update code

--- app/code/Magento/Customer/Model/Config/Backend/Show/Customer.php
+++ app/code/Magento/Customer/Model/Config/Backend/Show/Customer.php
@@ -3,8 +3,6 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
-declare(strict_types=1);
-
 namespace Magento\Customer\Model\Config\Backend\Show;
 
 use Magento\Config\App\Config\Source\ModularConfigSource;
