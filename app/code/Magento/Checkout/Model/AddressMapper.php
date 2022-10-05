24c8b6402cf2e753b045f8f7ce4a5b20936606f4
pradeep1819 <pradeep05.pro@gmail.com>
2022-10-05 09:00:03 +0530

    ACP2E-1117: CustomerAddressId not set for new billing address

--- app/code/Magento/Checkout/Model/AddressMapper.php
+++ app/code/Magento/Checkout/Model/AddressMapper.php
@@ -64,15 +64,19 @@ class AddressMapper implements AddressMapperInterface
         $quote = $this->cartRepository->getActive($cartId);
         $shippingAddress = $quote->getShippingAddress();
         $quoteShippingAddressData = $shippingAddress->getData();
-        $sameAsBillingFlag = (int)$shippingAddress->getSameAsBilling();
+        $quoteSameAsBilling = (int)$shippingAddress->getSameAsBilling();
         $customer = $quote->getCustomer();
         $customerId = $customer->getId();
         $hasDefaultBilling = $customer->getDefaultBilling();
         $hasDefaultShipping = $customer->getDefaultShipping();
 
-        if ($sameAsBillingFlag !== 1 && !empty($quoteShippingAddressData) && !empty($billingAddress)) {
+        if ($quoteSameAsBilling === 1) {
+            $sameAsBillingFlag = 1;
+        } elseif (!empty($quoteShippingAddressData) && !empty($billingAddress)) {
             $sameAsBillingFlag = $quote->getCustomerId() &&
-                $this->checkIfShippingAddressMatchesWithBillingAddress($shippingAddress, $billingAddress);
+                    $this->checkIfShippingAddressMatchesWithBillingAddress($shippingAddress, $billingAddress);
+        } else {
+            $sameAsBillingFlag = 0;
         }
 
         if ($sameAsBillingFlag) {
