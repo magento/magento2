e979297bc7cd92cadc1db5a27d124de1ecedd185
Roger <you@example.com>
2022-10-17 10:50:15 +0000

    Issue 36274: Duplicate orders with 2 parallel GraphQL requests

--- app/code/Magento/QuoteGraphQl/Model/Resolver/PlaceOrder.php
+++ app/code/Magento/QuoteGraphQl/Model/Resolver/PlaceOrder.php
@@ -7,11 +7,13 @@ declare(strict_types=1);
 
 namespace Magento\QuoteGraphQl\Model\Resolver;
 
+use Magento\Framework\App\ObjectManager;
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\GraphQl\Config\Element\Field;
 use Magento\Framework\GraphQl\Exception\GraphQlInputException;
 use Magento\Framework\GraphQl\Query\ResolverInterface;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Magento\Framework\Lock\LockManagerInterface;
 use Magento\GraphQl\Helper\Error\AggregateExceptionMessageFormatter;
 use Magento\QuoteGraphQl\Model\Cart\GetCartForUser;
 use Magento\QuoteGraphQl\Model\Cart\PlaceOrder as PlaceOrderModel;
@@ -22,6 +24,16 @@ use Magento\Sales\Api\OrderRepositoryInterface;
  */
 class PlaceOrder implements ResolverInterface
 {
+    /**
+     * Static lock prefix for place order locking
+     */
+    public const LOCK_PREFIX = 'PLACE_ORDER_';
+
+    /**
+     * How long to wait for place order to become unlocked
+     */
+    public const LOCK_TIMEOUT = 60;
+
     /**
      * @var GetCartForUser
      */
@@ -43,21 +55,29 @@ class PlaceOrder implements ResolverInterface
     private $errorMessageFormatter;
 
     /**
+     * @var LockManagerInterface
+     */
+    private $lockManager;
+
+    /**
      * @param GetCartForUser $getCartForUser
      * @param PlaceOrderModel $placeOrder
      * @param OrderRepositoryInterface $orderRepository
      * @param AggregateExceptionMessageFormatter $errorMessageFormatter
+     * @param LockManagerInterface $lockManager
      */
     public function __construct(
         GetCartForUser $getCartForUser,
         PlaceOrderModel $placeOrder,
         OrderRepositoryInterface $orderRepository,
-        AggregateExceptionMessageFormatter $errorMessageFormatter
+        AggregateExceptionMessageFormatter $errorMessageFormatter,
+        LockManagerInterface $lockManager = null
     ) {
         $this->getCartForUser = $getCartForUser;
         $this->placeOrder = $placeOrder;
         $this->orderRepository = $orderRepository;
         $this->errorMessageFormatter = $errorMessageFormatter;
+        $this->lockManager = $lockManager ?: ObjectManager::getInstance()->get(LockManagerInterface::class);
     }
 
     /**
@@ -72,7 +92,16 @@ class PlaceOrder implements ResolverInterface
         $userId = (int)$context->getUserId();
         $storeId = (int)$context->getExtensionAttributes()->getStore()->getId();
 
+        $lockedName = self::LOCK_PREFIX . $maskedCartId;
+        if ($this->lockManager->isLocked($lockedName)) {
+            throw new LocalizedException(__(
+                'Unable to place order: A server error stopped your order from being placed. '
+                . 'Please try to place your order again'
+            ));
+        }
+
         try {
+            $this->lockManager->lock($lockedName, self::LOCK_TIMEOUT);
             $cart = $this->getCartForUser->getCartForCheckout($maskedCartId, $userId, $storeId);
             $orderId = $this->placeOrder->execute($cart, $maskedCartId, $userId);
             $order = $this->orderRepository->get($orderId);
@@ -86,6 +115,8 @@ class PlaceOrder implements ResolverInterface
                 $context,
                 $info
             );
+        } finally {
+            $this->lockManager->unlock($lockedName);
         }
 
         return [
