8d74dcdc02ba8d03dbc9e5b1debb46d6a074b5d1
Igor Wulff <igorwulff@gmail.com>
2022-10-21 11:55:28 +0200

    Update DataObject.php

--- lib/internal/Magento/Framework/DataObject.php
+++ lib/internal/Magento/Framework/DataObject.php
@@ -57,7 +57,7 @@ class DataObject implements \ArrayAccess
         }
 
         foreach ($arr as $index => $value) {
-            $this->_data[$key] = $value;
+            $this->_data[$index] = $value;
         }
         return $this;
     }
