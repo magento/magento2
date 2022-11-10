27704135582c303553ed738d40f75d6e2b3ed4f5
Luke Rodgers <lr@amp.co>
2022-11-10 10:51:14 +0000

    Fix `Undefined array key`

--- app/code/Magento/GraphQl/Model/Query/Logger/NewRelic.php
+++ app/code/Magento/GraphQl/Model/Query/Logger/NewRelic.php
@@ -41,7 +41,7 @@ class NewRelic implements LoggerInterface
      */
     public function execute(array $queryDetails)
     {
-        $transactionName = $queryDetails[LoggerInterface::OPERATION_NAMES] ?: '';
+        $transactionName = $queryDetails[LoggerInterface::OPERATION_NAMES] ?? '';
         $this->newRelicWrapper->setTransactionName('GraphQL-' . $transactionName);
 
         if (!$this->config->isNewRelicEnabled()) {
