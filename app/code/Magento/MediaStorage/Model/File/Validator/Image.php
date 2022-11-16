edcbdab5807a3689b654c2fd02d0c65fe0aad406
Alexandra Zota <zota@adobe.com>
2022-11-16 18:32:29 +0200

    ACP2E-1353: allow image/x-icon favicon

--- app/code/Magento/MediaStorage/Model/File/Validator/Image.php
+++ app/code/Magento/MediaStorage/Model/File/Validator/Image.php
@@ -89,7 +89,7 @@ class Image extends AbstractValidator
         $fileMimeType = $this->fileMime->getMimeType($filePath);
         $isValid = false;
 
-        if (stripos(json_encode($this->imageMimeTypes), $fileMimeType) !== false) {
+        if (stripos(json_encode($this->imageMimeTypes), json_encode($fileMimeType)) !== false) {
             $defaultAdapter = $this->config->getAdapterAlias();
             try {
                 $image = $this->imageFactory->create($filePath, $defaultAdapter);
