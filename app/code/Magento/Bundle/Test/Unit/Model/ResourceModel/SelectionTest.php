8d66a78d6421e09644866ddd09be7ce4ac140bc5
aplapana <aplapana@adobe.com>
2022-11-17 11:08:44 +0200

    ACP2E-1345, CR feedback

--- app/code/Magento/Bundle/Test/Unit/Model/ResourceModel/SelectionTest.php
+++ app/code/Magento/Bundle/Test/Unit/Model/ResourceModel/SelectionTest.php
@@ -10,13 +10,34 @@ namespace Magento\Bundle\Test\Unit\Model\ResourceModel;
 use Codeception\PHPUnit\TestCase;
 use Magento\Bundle\Model\ResourceModel\Selection as ResourceSelection;
 use Magento\Bundle\Model\Selection;
+use Magento\Framework\App\ResourceConnection;
 use Magento\Framework\DB\Adapter\AdapterInterface;
+use Magento\Framework\EntityManager\MetadataPool;
+use Magento\Framework\Model\ResourceModel\Db\Context;
 
-/**
- * Class to test Selection Resource Model
- */
 class SelectionTest extends TestCase
 {
+    /**
+     * @var Context|Context&\PHPUnit\Framework\MockObject\MockObject|\PHPUnit\Framework\MockObject\MockObject
+     */
+    private Context $context;
+
+    /**
+     * @var MetadataPool|MetadataPool&\PHPUnit\Framework\MockObject\MockObject|\PHPUnit\Framework\MockObject\MockObject
+     */
+    private MetadataPool $metadataPool;
+
+    /**
+     * @inheritdoc
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->context = $this->createMock(Context::class);
+        $this->metadataPool = $this->createMock(MetadataPool::class);
+    }
+
     public function testSaveSelectionPrice()
     {
         $item = $this->getMockBuilder(Selection::class)
@@ -43,27 +64,29 @@ class SelectionTest extends TestCase
         $item->expects($this->once())->method('getSelectionPriceValue')->willReturn($values['selection_price_value']);
         $item->expects($this->once())->method('getParentProductId')->willReturn($values['parent_product_id']);
 
-        $selection = $this->getMockBuilder(ResourceSelection::class)
-            ->disableOriginalConstructor()
-            ->onlyMethods(['getConnection', 'getTable'])
-            ->getMock();
-        $selection->expects($this->any())
-            ->method('getTable')
-            ->with('catalog_product_bundle_selection_price')
-            ->willReturn('catalog_product_bundle_selection_price');
-
         $connection = $this->createMock(AdapterInterface::class);
         $connection->expects($this->once())
             ->method('insertOnDuplicate')
             ->with(
-                $selection->getTable('catalog_product_bundle_selection_price'),
+                'catalog_product_bundle_selection_price',
                 $this->callback(function ($insertValues) {
                     return $insertValues['selection_price_type'] === 0 && $insertValues['selection_price_value'] === 0;
                 }),
                 ['selection_price_type', 'selection_price_value']
             );
 
-        $selection->expects($this->once())->method('getConnection')->willReturn($connection);
+        $parentResources = $this->createMock(ResourceConnection::class);
+        $parentResources->expects($this->once())->method('getConnection')->willReturn($connection);
+        $parentResources->expects($this->once())->method('getTableName')
+            ->with('catalog_product_bundle_selection_price', 'test_connection_name')
+            ->willReturn('catalog_product_bundle_selection_price');
+
+        $selection = new ResourceSelection($this->context, $this->metadataPool, 'test_connection_name');
+        $reflect = new \ReflectionClass($selection);
+        $property = $reflect->getProperty('_resources');
+        $property->setAccessible(true);
+        $property->setValue($selection, $parentResources);
+
         $selection->saveSelectionPrice($item);
     }
 }
