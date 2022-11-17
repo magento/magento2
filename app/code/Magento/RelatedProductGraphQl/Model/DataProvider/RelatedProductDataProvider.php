b1cf6c4e45c286819693cccfa9dec5860742197d
Vasudev <vasudev.penu.sundaresan@gds.ey.com>
2022-11-17 11:28:56 +0530

    Related Product Position in Graphql response

--- app/code/Magento/RelatedProductGraphQl/Model/DataProvider/RelatedProductDataProvider.php
+++ app/code/Magento/RelatedProductGraphQl/Model/DataProvider/RelatedProductDataProvider.php
@@ -118,7 +118,10 @@ class RelatedProductDataProvider
         $collection = $link->getLinkCollection();
         $collection->addFieldToFilter('product_id', ['in' => array_keys($productsByActualIds)]);
         $collection->addLinkTypeIdFilter();
-
+        // Fix for this issue https://github.com/magento/magento2/issues/36461
+        $collection->addProductIdFilter();
+        $collection->joinAttributes();
+        $collection->setOrder('position', 'ASC');
         //Prepare map
         $map = [];
         /** @var Link $item */
