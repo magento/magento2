dbb760fac112b3ba91ff3ed49e8767b88fa7d46c
Rajesh Kumar <glo71317@adobe.com>
2022-11-22 17:37:17 +0530

    AC-3772::Fixed the Automation test failures

--- app/code/Magento/JwtFrameworkAdapter/Model/JweContentAlgorithmManagerFactory.php
+++ app/code/Magento/JwtFrameworkAdapter/Model/JweContentAlgorithmManagerFactory.php
@@ -9,29 +9,34 @@ declare(strict_types=1);
 namespace Magento\JwtFrameworkAdapter\Model;
 
 use Jose\Component\Core\AlgorithmManager;
+use Jose\Component\Encryption\Algorithm\ContentEncryption\A128CBCHS256;
+use Jose\Component\Encryption\Algorithm\ContentEncryption\A128GCM;
+use Jose\Component\Encryption\Algorithm\ContentEncryption\A192CBCHS384;
+use Jose\Component\Encryption\Algorithm\ContentEncryption\A192GCM;
+use Jose\Component\Encryption\Algorithm\ContentEncryption\A256CBCHS512;
+use Jose\Component\Encryption\Algorithm\ContentEncryption\A256GCM;
 
 class JweContentAlgorithmManagerFactory
 {
-    private const ALGOS = [
-        \Jose\Component\Encryption\Algorithm\ContentEncryption\A128CBCHS256::class,
-        \Jose\Component\Encryption\Algorithm\ContentEncryption\A192CBCHS384::class,
-        \Jose\Component\Encryption\Algorithm\ContentEncryption\A256CBCHS512::class,
-        \Jose\Component\Encryption\Algorithm\ContentEncryption\A128GCM::class,
-        \Jose\Component\Encryption\Algorithm\ContentEncryption\A192GCM::class,
-        \Jose\Component\Encryption\Algorithm\ContentEncryption\A256GCM::class,
-    ];
-
     /**
      * @var AlgorithmProviderFactory
      */
     private $algorithmProviderFactory;
 
-    public function __construct(AlgorithmProviderFactory $algorithmProviderFactory) {
+    public function __construct(AlgorithmProviderFactory $algorithmProviderFactory)
+    {
         $this->algorithmProviderFactory = $algorithmProviderFactory;
     }
 
     public function create(): AlgorithmManager
     {
-        return new AlgorithmManager($this->algorithmProviderFactory->create(self::ALGOS)->getAvailableAlgorithms());
+        return new AlgorithmManager([
+            new A128CBCHS256(),
+            new A192CBCHS384(),
+            new A256CBCHS512(),
+            new A128GCM(),
+            new A192GCM(),
+            new A256GCM(),
+        ]);
     }
 }
