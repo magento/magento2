0d0663605dd21daf4023d60f746890f7efebd09e
lakshmana <lakshmana.katragadda@globallogic.com>
2022-11-24 04:55:49 +0530

    ACP2E-1294 : Fixed static test issues

--- dev/tests/integration/testsuite/Magento/Backend/Block/Dashboard/TotalsTest.php
+++ dev/tests/integration/testsuite/Magento/Backend/Block/Dashboard/TotalsTest.php
@@ -17,7 +17,6 @@ use Magento\Checkout\Test\Fixture\SetDeliveryMethod as SetDeliveryMethodFixture;
 use Magento\Checkout\Test\Fixture\SetGuestEmail as SetGuestEmailFixture;
 use Magento\Checkout\Test\Fixture\SetPaymentMethod as SetPaymentMethodFixture;
 use Magento\Checkout\Test\Fixture\SetShippingAddress as SetShippingAddressFixture;
-use Magento\Framework\App\Area;
 use Magento\Framework\App\Request\Http as HttpRequest;
 use Magento\Framework\Stdlib\DateTime;
 use Magento\Framework\View\LayoutInterface;
@@ -29,7 +28,6 @@ use Magento\Sales\Test\Fixture\Shipment as ShipmentFixture;
 use Magento\TestFramework\Fixture\DataFixture;
 use Magento\TestFramework\Fixture\DataFixtureStorage;
 use Magento\TestFramework\Fixture\DataFixtureStorageManager;
-use Magento\TestFramework\Helper\Bootstrap;
 use Magento\TestFramework\TestCase\AbstractBackendController;
 
 class TotalsTest extends AbstractBackendController
@@ -46,8 +44,11 @@ class TotalsTest extends AbstractBackendController
     {
         parent::setUp();
         $this->fixtures = $this->_objectManager->get(DataFixtureStorageManager::class)->getStorage();
-        Bootstrap::getInstance()->loadArea(Area::AREA_ADMINHTML);
     }
+
+    /**
+     * @magentoAppArea adminhtml
+     */
 
     #[
         DataFixture(ProductFixture::class, ['price' => 100], 'product'),
@@ -75,23 +76,22 @@ class TotalsTest extends AbstractBackendController
     public function testTotals(): void
     {
         $orderData = $this->fixtures->get('order');
-        $order = $this->_objectManager->get(Order::class);
-        $order->loadByIncrementId($orderData->getIncrementId());
-        $dateTime = new \DateTime('now');
-        $order->setCreatedAt($dateTime->modify('-23 hours')->format(DateTime::DATETIME_PHP_FORMAT));
-        $order->save();
+        $this->updateOrderCreateDate($orderData->getIncrementId(), '-23 hours');
         $order2Data = $this->fixtures->get('order2');
-        $order2 = $this->_objectManager->get(Order::class);
-        $order2->loadByIncrementId($order2Data->getIncrementId());
-        $dateTime2 = new \DateTime('now');
-        $order2->setCreatedAt($dateTime2->modify('-1 hour')->format(DateTime::DATETIME_PHP_FORMAT));
-        $order2->save();
+        $this->updateOrderCreateDate($order2Data->getIncrementId(), '-1 hour');
         $layout = $this->_objectManager->get(LayoutInterface::class);
         $totalsDefaultBlock = $layout->createBlock(Totals::class);
         $totalsDefaults = $totalsDefaultBlock->getTotals();
+        $totals24Hours = $this->callAjaxBlock();
+        $this->assertEquals(100, $this->parseRevenue($totalsDefaults[0]['value']));
+        $this->assertEquals(200, $this->parseRevenue($totals24Hours));
+    }
+
+    private function callAjaxBlock()
+    {
         $this->getRequest()->setMethod(HttpRequest::METHOD_POST);
         $this->getRequest()->setParam('block', 'totals');
-        $this->getRequest()->setParam('period', Period::PERIOD_24_HOURS);
+        $this->getRequest()->setParam('period', Period::PERIOD_TODAY);
         $this->dispatch('backend/admin/dashboard/ajaxBlock/');
         $this->assertEquals(200, $this->getResponse()->getHttpResponseCode());
         $actualHtml = $this->getResponse()->getBody();
@@ -99,12 +99,20 @@ class TotalsTest extends AbstractBackendController
         $dom->loadHTML($actualHtml);
         $xPath = '//*[@id="dashboard_diagram_totals"]/ul/li[1]/strong/span[1]';
         $totals24Hours = (new DOMXPath($dom))->query($xPath)->item(0)->textContent;
-        $this->assertEquals(100, $this->parseRevenue($totalsDefaults[0]['value']));
-        $this->assertEquals(200, $this->parseRevenue($totals24Hours));
+        return $totals24Hours;
     }
 
     private function parseRevenue($txt): float
     {
         return (float)preg_replace("/[^0-9.]/", '', strip_tags($txt));
+    }
+
+    private function updateOrderCreateDate($orderIncrementId, $modify)
+    {
+        $order2 = $this->_objectManager->get(Order::class);
+        $order2->loadByIncrementId($orderIncrementId);
+        $dateTime2 = new \DateTime('now');
+        $order2->setCreatedAt($dateTime2->modify($modify)->format(DateTime::DATETIME_PHP_FORMAT));
+        $order2->save();
     }
 }
