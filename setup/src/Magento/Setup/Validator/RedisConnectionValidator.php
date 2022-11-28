bbbc4d4ae6650341e64a81b2c04d2f42d47fea68
Lavi Jain <87003041+glo05363@users.noreply.github.com>
2022-11-29 00:33:18 +0530

    Update setup/src/Magento/Setup/Validator/RedisConnectionValidator.php
    Co-authored-by: Rimple Saini <85893545+rimple-saini@users.noreply.github.com>

--- setup/src/Magento/Setup/Validator/RedisConnectionValidator.php
+++ setup/src/Magento/Setup/Validator/RedisConnectionValidator.php
@@ -40,7 +40,9 @@ class RedisConnectionValidator
                 $config['password']
             );
             $redisClient->setMaxConnectRetries(1);
-            $redisClient->auth($config['password']);
+            if (isset($config[‘password’]) && $config[‘password’] !== ‘’) {
+               $redisClient->auth($config[‘password’]);
+           }
             $redisClient->connect();
         } catch (\CredisException $e) {
             return false;
