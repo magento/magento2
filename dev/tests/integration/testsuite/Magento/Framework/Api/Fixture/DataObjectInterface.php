81c39f5a64a6d3e6da28a9ee3535599b0bd8c59c
Viktor Tymchynskyi <tymchyns@adobe.com>
2022-12-14 13:31:47 -0600

    ACP2E-1381: Different behaviors on snake_case formatting

---
+++ dev/tests/integration/testsuite/Magento/Framework/Api/Fixture/DataObjectInterface.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Magento\Framework\Api\Fixture;
+
+interface DataObjectInterface
+{
+    /**
+     * Set A
+     *
+     * @param string $value
+     *
+     * @return void
+     */
+    public function setFirstASecond(string $value): void;
+
+    /**
+     * Set At
+     *
+     * @param string $value
+     *
+     * @return void
+     */
+    public function setFirstAtSecond(string $value): void;
+
+    /**
+     * Set ATM
+     *
+     * @param string $value
+     *
+     * @return void
+     */
+    public function setFirstATMSecond(string $value): void;
+}
