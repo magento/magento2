84099e77dd11d59c314a3e52b1aa359ace92f4a4
Faizan Shaikh <glo88465@adobe.com>
2022-12-15 12:34:08 +0530

    AC-6921:Fixed Integraion and static tests related to maridb10.6 support

--- dev/tests/setup-integration/_files/Magento/TestSetupDeclarationModule1/fixture/dry_run_log.mariadb106.php
+++ dev/tests/setup-integration/_files/Magento/TestSetupDeclarationModule1/fixture/dry_run_log.mariadb106.php
@@ -3,6 +3,8 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types=1);
+
 // @codingStandardsIgnoreFile
 return ['CREATE TABLE `reference_table` (
 `tinyint_ref` tinyint  NOT NULL  AUTO_INCREMENT , 
