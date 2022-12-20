ac3a859349cff630391b00af4beae56e6c6db254
lakshmana <lakshmana.katragadda@globallogic.com>
2022-12-20 23:03:29 +0530

    ACP2E-1446 : Added test coverage for this ticket

---
+++ dev/tests/integration/testsuite/Magento/User/Controller/Adminhtml/UserResetPasswordEmailTest.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\User\Controller\Adminhtml;
+
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Mail\EmailMessage;
+use Magento\TestFramework\Fixture\Config as Config;
+use Magento\TestFramework\Fixture\DataFixture;
+use Magento\TestFramework\Fixture\DataFixtureStorage;
+use Magento\TestFramework\Fixture\DataFixtureStorageManager;
+use Magento\TestFramework\Mail\Template\TransportBuilderMock;
+use Magento\TestFramework\TestCase\AbstractBackendController;
+use Magento\User\Model\User as UserModel;
+use Magento\User\Test\Fixture\User as UserDataFixture;
+
+/**
+ * Test class for user reset password email
+ *
+ * @magentoAppArea adminhtml
+ */
+class UserResetPasswordEmailTest extends AbstractBackendController
+{
+    /**
+     * @var DataFixtureStorage
+     */
+    private $fixtures;
+
+    /**
+     * @var UserModel
+     */
+    protected $userModel;
+
+    /**
+     * @throws LocalizedException
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->fixtures = DataFixtureStorageManager::getStorage();
+        $this->userModel = $this->_objectManager->create(UserModel::class);
+    }
+
+    #[
+        Config('admin/emails/forgot_email_template', 'admin_emails_forgot_email_template'),
+        Config('admin/emails/forgot_email_identity', 'general'),
+        Config('web/url/use_store', 1),
+        DataFixture(UserDataFixture::class, ['role_id' => 1], 'user')
+    ]
+    public function testUserResetPasswordEmail()
+    {
+        $user = $this->fixtures->get('user');
+        $userEmail = $user->getDataByKey('email');
+        $transportMock = $this->_objectManager->get(TransportBuilderMock::class);
+        $this->getRequest()->setPostValue('email', $userEmail);
+        $this->dispatch('backend/admin/auth/forgotpassword');
+        $message = $transportMock->getSentMessage();
+        $this->assertNotEmpty($message);
+        $this->assertEquals('backend/admin/auth/resetpassword', $this->getResetPasswordUri($message));
+    }
+
+    private function getResetPasswordUri(EmailMessage $message): string
+    {
+        $emailParts = $message->getBody()->getParts();
+        $messageContent = current($emailParts)->getRawContent();
+        $pattern = '#\bhttps?://[^,\s()<>]+(?:\([\w\d]+\)|([^,[:punct:]\s]|/))#';
+        preg_match_all($pattern, $messageContent, $match);
+        $urlString = strstr($match[0][0], 'backend');
+        return substr($urlString, 0, strpos($urlString, "/key"));
+    }
+}
