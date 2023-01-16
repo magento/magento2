31a64cf9cda8938ea7fb3f52c5c77b969b0f91cf
aplapana <aplapana@adobe.com>
2023-01-16 09:18:33 +0200

    ACP2E-1512: Env variable config not working for store views with capital letters in the code fixed static

--- lib/internal/Magento/Framework/App/Config.php
+++ lib/internal/Magento/Framework/App/Config.php
@@ -1,7 +1,5 @@
 <?php
 /**
- * Application configuration object. Used to access configuration when application is initialized and installed.
- *
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
@@ -12,14 +10,14 @@ use Magento\Framework\App\Config\ScopeCodeResolver;
 use Magento\Framework\App\Config\ScopeConfigInterface;
 
 /**
- * Class Config
+ * Application configuration object. Used to access configuration when application is initialized and installed.
  */
 class Config implements ScopeConfigInterface
 {
     /**
      * Config cache tag
      */
-    const CACHE_TAG = 'CONFIG';
+    public const CACHE_TAG = 'CONFIG';
 
     /**
      * @var ScopeCodeResolver
