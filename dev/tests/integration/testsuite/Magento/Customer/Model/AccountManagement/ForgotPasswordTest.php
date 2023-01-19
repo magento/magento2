ad2e8767e54b3707b322564cd300077846ab225a
akaash <akaash@BLR1-LMC-N71907.local>
2023-01-19 12:51:25 +0530

    ACQE-4501 | Integration Testing - Reset password flow

--- dev/tests/integration/testsuite/Magento/Customer/Model/AccountManagement/ForgotPasswordTest.php
+++ dev/tests/integration/testsuite/Magento/Customer/Model/AccountManagement/ForgotPasswordTest.php
@@ -10,6 +10,7 @@ namespace Magento\Customer\Model\AccountManagement;
 use Magento\Customer\Api\AccountManagementInterface;
 use Magento\Customer\Model\AccountManagement;
 use Magento\Framework\ObjectManagerInterface;
+use Magento\Store\Model\StoreManagerInterface;
 use Magento\TestFramework\Helper\Bootstrap;
 use Magento\TestFramework\Helper\Xpath;
 use Magento\TestFramework\Mail\Template\TransportBuilderMock;
@@ -35,6 +36,9 @@ class ForgotPasswordTest extends TestCase
     private $newPasswordLinkPath = "//a[contains(@href, 'customer/account/createPassword') "
     . "and contains(text(), 'Set a New Password')]";
 
+    /** @var StoreManagerInterface */
+    private $storeManager;
+
     /**
      * @inheritdoc
      */
@@ -45,6 +49,7 @@ class ForgotPasswordTest extends TestCase
         $this->objectManager = Bootstrap::getObjectManager();
         $this->accountManagement = $this->objectManager->get(AccountManagementInterface::class);
         $this->transportBuilder = $this->objectManager->get(TransportBuilderMock::class);
+        $this->storeManager = $this->objectManager->get(StoreManagerInterface::class);
     }
 
     /**
@@ -60,5 +65,45 @@ class ForgotPasswordTest extends TestCase
         $messageContent = $message->getBody()->getParts()[0]->getRawContent();
         $this->assertTrue($result);
         $this->assertEquals(1, Xpath::getElementsCountForXpath($this->newPasswordLinkPath, $messageContent));
+    }
+
+    /**
+     * @magentoDataFixture Magento/Customer/_files/customer.php
+     *
+     * @return void
+     */
+    public function testResetPasswordFlowStorefront(): void
+    {
+        // Forgot password section
+        $email = 'customer@example.com';
+        $result = $this->accountManagement->initiatePasswordReset($email, AccountManagement::EMAIL_RESET);
+        $message = $this->transportBuilder->getSentMessage();
+        $messageContent = $message->getBody()->getParts()[0]->getRawContent();
+        $this->assertTrue($result);
+        $this->assertEquals(1, Xpath::getElementsCountForXpath($this->newPasswordLinkPath, $messageContent));
+
+        // Send reset password link
+        $websiteId = (int)$this->storeManager->getWebsite('base')->getId();
+        $this->accountManagement->initiatePasswordReset($email, AccountManagement::EMAIL_RESET, $websiteId);
+
+        // login with old credentials
+        $customer = $this->accountManagement->authenticate('customer@example.com', 'password');
+
+        $this->assertEquals(
+            $customer->getId(),
+            $this->accountManagement->authenticate('customer@example.com', 'password')->getId()
+        );
+
+        // Change password
+        $this->accountManagement->changePassword('customer@example.com', 'password', 'new_Password123');
+
+        // Login with new credentials
+        $this->accountManagement->authenticate('customer@example.com', 'new_Password123');
+
+        $this->assertEquals(
+            $customer->getId(),
+            $this->accountManagement->authenticate('customer@example.com', 'new_Password123')->getId()
+        );
+
     }
 }
