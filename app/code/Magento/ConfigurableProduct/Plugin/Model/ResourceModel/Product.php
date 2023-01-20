b8d0a54a55361d194b3f19af95632e41d4606e8d
pradeep1819 <pradeep05.pro@gmail.com>
2023-01-20 21:19:11 +0530

    ACP2E-1524: Page Cache is not cleared for the parent product on the child product save

--- app/code/Magento/ConfigurableProduct/Plugin/Model/ResourceModel/Product.php
+++ app/code/Magento/ConfigurableProduct/Plugin/Model/ResourceModel/Product.php
@@ -4,17 +4,25 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types=1);
 
 namespace Magento\ConfigurableProduct\Plugin\Model\ResourceModel;
 
 use Magento\Catalog\Api\Data\ProductAttributeInterface;
 use Magento\Catalog\Api\ProductAttributeRepositoryInterface;
+use Magento\Catalog\Model\Indexer\Product\Category;
+use Magento\Catalog\Model\Product as ProductModel;
+use Magento\Catalog\Model\ResourceModel\Product as ProductResource;
+use Magento\CatalogSearch\Model\Indexer\Fulltext as FulltextIndexer;
 use Magento\ConfigurableProduct\Api\Data\OptionInterface;
 use Magento\ConfigurableProduct\Model\Product\Type\Configurable;
 use Magento\Framework\Api\FilterBuilder;
 use Magento\Framework\Api\SearchCriteriaBuilder;
 use Magento\Framework\App\ObjectManager;
+use Magento\Framework\DataObject;
 use Magento\Framework\Indexer\ActionInterface;
+use Magento\Framework\Indexer\CacheContext;
+use Magento\Framework\Indexer\IndexerRegistry;
 
 /**
  * Plugin product resource model
@@ -47,20 +55,34 @@ class Product
     private $filterBuilder;
 
     /**
+     * @var CacheContext
+     */
+    private $cacheContext;
+
+    /**
+     * @var IndexerRegistry
+     */
+    private $indexerRegistry;
+
+    /**
      * Initialize Product dependencies.
      *
      * @param Configurable $configurable
      * @param ActionInterface $productIndexer
-     * @param ProductAttributeRepositoryInterface $productAttributeRepository
-     * @param SearchCriteriaBuilder $searchCriteriaBuilder
-     * @param FilterBuilder $filterBuilder
+     * @param ProductAttributeRepositoryInterface|null $productAttributeRepository
+     * @param SearchCriteriaBuilder|null $searchCriteriaBuilder
+     * @param FilterBuilder|null $filterBuilder
+     * @param CacheContext|null $cacheContext
+     * @param IndexerRegistry|null $indexerRegistry
      */
     public function __construct(
         Configurable $configurable,
         ActionInterface $productIndexer,
         ProductAttributeRepositoryInterface $productAttributeRepository = null,
-        SearchCriteriaBuilder $searchCriteriaBuilder = null,
-        FilterBuilder $filterBuilder = null
+        ?SearchCriteriaBuilder $searchCriteriaBuilder = null,
+        ?FilterBuilder $filterBuilder = null,
+        ?CacheContext $cacheContext = null,
+        ?IndexerRegistry $indexerRegistry = null
     ) {
         $this->configurable = $configurable;
         $this->productIndexer = $productIndexer;
@@ -70,23 +92,25 @@ class Product
             ->get(SearchCriteriaBuilder::class);
         $this->filterBuilder = $filterBuilder ?: ObjectManager::getInstance()
             ->get(FilterBuilder::class);
+        $this->cacheContext = $cacheContext ?: ObjectManager::getInstance()->get(CacheContext::class);
+        $this->indexerRegistry = $indexerRegistry ?: ObjectManager::getInstance()
+            ->get(IndexerRegistry::class);
     }
 
     /**
      * We need reset attribute set id to attribute after related simple product was saved
      *
-     * @param \Magento\Catalog\Model\ResourceModel\Product $subject
-     * @param \Magento\Framework\DataObject $object
+     * @param ProductResource $subject
+     * @param DataObject $object
      * @return void
-     * @throws \Magento\Framework\Exception\NoSuchEntityException
      *
      * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
     public function beforeSave(
-        \Magento\Catalog\Model\ResourceModel\Product $subject,
-        \Magento\Framework\DataObject $object
+        ProductResource $subject,
+        DataObject $object
     ) {
-        /** @var \Magento\Catalog\Model\Product $object */
+        /** @var ProductModel $object */
         if ($object->getTypeId() == Configurable::TYPE_CODE) {
             $object->getTypeInstance()->getSetAttributes($object);
             $this->resetConfigurableOptionsData($object);
@@ -94,11 +118,42 @@ class Product
     }
 
     /**
+     * Invalidate cache and perform reindexing for configurable associated product
+     *
+     * @param ProductResource $subject
+     * @param ProductResource $result
+     * @param DataObject $object
+     * @return ProductResource
+     *
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function afterSave(
+        ProductResource $subject,
+        ProductResource $result,
+        DataObject $object
+    ): ProductResource {
+        $productId = $object->getId();
+        $parentProductIds = $this->configurable->getParentIdsByChild($productId);
+        if (count($parentProductIds) > 0) {
+            $productCategoryIndexer = $this->indexerRegistry->get(Category::INDEXER_ID);
+            $productCategoryIndexer->reindexRow($productId);
+
+            $this->cacheContext->registerEntities(
+                ProductModel::CACHE_TAG,
+                array_unique(array_merge([$productId], $parentProductIds))
+            );
+            $indexer = $this->indexerRegistry->get(FulltextIndexer::INDEXER_ID);
+            $indexer->reindexRow($productId);
+        }
+
+        return $result;
+    }
+
+    /**
      * Set null for configurable options attribute of configurable product
      *
-     * @param \Magento\Catalog\Model\Product $object
+     * @param ProductModel $object
      * @return void
-     * @throws \Magento\Framework\Exception\NoSuchEntityException
      */
     private function resetConfigurableOptionsData($object)
     {
@@ -128,16 +183,16 @@ class Product
     /**
      * Gather configurable parent ids of product being deleted and reindex after delete is complete.
      *
-     * @param \Magento\Catalog\Model\ResourceModel\Product $subject
+     * @param ProductResource $subject
      * @param \Closure $proceed
-     * @param \Magento\Catalog\Model\Product $product
-     * @return \Magento\Catalog\Model\ResourceModel\Product
+     * @param ProductModel $product
+     * @return ProductResource
      * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
     public function aroundDelete(
-        \Magento\Catalog\Model\ResourceModel\Product $subject,
+        ProductResource $subject,
         \Closure $proceed,
-        \Magento\Catalog\Model\Product $product
+        ProductModel $product
     ) {
         $configurableProductIds = $this->configurable->getParentIdsByChild($product->getId());
         $result = $proceed($product);
