ef6c9b8f95ff3c66367bb15e7b6c725b3d22d4f4
lakshmana <lakshmana.katragadda@globallogic.com>
2023-01-30 21:58:34 +0530

    ACP2E-1526 : No base64 image code in REST API response

--- app/code/Magento/Catalog/Model/Product/Attribute/Backend/Media/ImageEntryConverter.php
+++ app/code/Magento/Catalog/Model/Product/Attribute/Backend/Media/ImageEntryConverter.php
@@ -7,8 +7,16 @@
 namespace Magento\Catalog\Model\Product\Attribute\Backend\Media;
 
 use Magento\Catalog\Api\Data\ProductAttributeMediaGalleryEntryInterface;
+use Magento\Catalog\Api\Data\ProductAttributeMediaGalleryEntryInterfaceFactory;
 use Magento\Catalog\Model\Product;
 use Magento\Framework\Api\Data\ImageContentInterface;
+use Magento\Framework\Api\Data\ImageContentInterfaceFactory;
+use Magento\Framework\Api\DataObjectHelper;
+use Magento\Framework\App\Filesystem\DirectoryList;
+use Magento\Framework\App\ObjectManager;
+use Magento\Framework\Exception\FileSystemException;
+use Magento\Framework\Filesystem;
+use Magento\Framework\Filesystem\Driver\File\Mime;
 
 /**
  * Converter for Image media gallery type
@@ -31,15 +39,42 @@ class ImageEntryConverter implements EntryConverterInterface
     protected $dataObjectHelper;
 
     /**
-     * @param \Magento\Catalog\Api\Data\ProductAttributeMediaGalleryEntryInterfaceFactory $mediaGalleryEntryFactory
-     * @param \Magento\Framework\Api\DataObjectHelper $dataObjectHelper
+     * @var ImageContentInterfaceFactory
+     */
+    protected $imageContentInterface;
+
+    /**
+     * Filesystem facade
+     *
+     * @var Filesystem
+     */
+    protected $filesystem;
+
+    /**
+     * @var Mime
+     */
+    protected $imageMime;
+
+    /**
+     * @param ProductAttributeMediaGalleryEntryInterfaceFactory $mediaGalleryEntryFactory
+     * @param DataObjectHelper $dataObjectHelper
+     * @param ImageContentInterfaceFactory|null $imageContentInterface
+     * @param Filesystem|null $filesystem
+     * @param Mime|null $imageMime
      */
     public function __construct(
         \Magento\Catalog\Api\Data\ProductAttributeMediaGalleryEntryInterfaceFactory $mediaGalleryEntryFactory,
-        \Magento\Framework\Api\DataObjectHelper $dataObjectHelper
+        \Magento\Framework\Api\DataObjectHelper $dataObjectHelper,
+        ImageContentInterfaceFactory $imageContentInterface = null,
+        Filesystem $filesystem = null,
+        Mime $imageMime = null
+
     ) {
         $this->mediaGalleryEntryFactory = $mediaGalleryEntryFactory;
         $this->dataObjectHelper = $dataObjectHelper;
+        $this->imageContentInterface = $imageContentInterface ?? ObjectManager::getInstance()->get(ImageContentInterfaceFactory::class);
+        $this->filesystem =  $filesystem ?? ObjectManager::getInstance()->get(Filesystem::class);
+        $this->imageMime = $imageMime ?? ObjectManager::getInstance()->get(Mime::class);
     }
 
     /**
@@ -71,7 +106,23 @@ class ImageEntryConverter implements EntryConverterInterface
         if (isset($image['value_id'])) {
             $entry->setId($image['value_id']);
         }
+        $imageFileContent = file_get_contents($product->getMediaConfig()->getMediaUrl(($entry->getFile())));
+        $entryContent = $this->imageContentInterface->create()
+            ->setName(basename($entry->getFile()))
+            ->setBase64EncodedData(base64_encode($imageFileContent))
+            ->setType($this->getImageMimeType($product,$entry));
+        $entry->setContent($entryContent);
         return $entry;
+    }
+
+    /**
+     * @throws FileSystemException
+     */
+    public function getImageMimeType($product, $entry)
+    {
+        $directory = $this->filesystem->getDirectoryRead(DirectoryList::MEDIA);
+        $path = $directory->getAbsolutePath($product->getMediaConfig()->getMediaPath($entry->getFile()));
+        return $this->imageMime->getMimeType($path);
     }
 
     /**
