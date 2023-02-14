50e3360fb1186f6b89acdedcb70db50cc28656ec
engcom-Charlie <engcom-vendorworker-charlie@adobe.com>
2023-02-14 12:20:08 +0530

    Fixed static test failures

--- app/code/Magento/Swatches/Helper/Data.php
+++ app/code/Magento/Swatches/Helper/Data.php
@@ -30,12 +30,12 @@ class Data
     /**
      * When we init media gallery empty image types contain this value.
      */
-    const EMPTY_IMAGE_VALUE = 'no_selection';
+    public const EMPTY_IMAGE_VALUE = 'no_selection';
 
     /**
      * The int value of the Default store ID
      */
-    const DEFAULT_STORE_ID = 0;
+    public const DEFAULT_STORE_ID = 0;
 
     /**
      * @var CollectionFactory
