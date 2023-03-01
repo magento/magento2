5877921109997b1a5f6c67efceaab8d1c5953120
Roman Hanin <rganin@adobe.com>
2023-03-01 10:42:43 -0600

    B2B-2259: customAttributeMetadata GraphQl query has no cache identity

---
+++ app/code/Magento/EavGraphQl/Model/Resolver/Cache/CustomAttributeMetadataIdentity.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Magento\EavGraphQl\Model\Resolver\Cache;
+
+use Magento\Eav\Model\Entity\Attribute as EavAttribute;
+use Magento\Framework\GraphQl\Query\Resolver\IdentityInterface;
+
+/**
+ * Cache identity provider for custom attribute metadata query results.
+ */
+class CustomAttributeMetadataIdentity implements IdentityInterface
+{
+    /**
+     * @inheirtdoc
+     */
+    public function getIdentities(array $resolvedData): array
+    {
+        $identities = [EavAttribute::CACHE_TAG];
+        if (isset($resolvedData['items']) && !empty($resolvedData['items'])) {
+            foreach ($resolvedData['items'] as $item) {
+                $identities[] = sprintf(
+                    "%s_%s",
+                    EavAttribute::CACHE_TAG,
+                    $item['attribute_code']
+                );
+            }
+        }
+        return $identities;
+    }
+}
