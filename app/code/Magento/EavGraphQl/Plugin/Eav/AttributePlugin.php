5b5720034ca64173f01fdad57b7c552ae7bcce90
Roman Hanin <rganin@adobe.com>
2023-03-02 10:58:14 -0600

    B2B-2259: customAttributeMetadata GraphQl query has no cache identity

---
+++ app/code/Magento/EavGraphQl/Plugin/Eav/AttributePlugin.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\EavGraphQl\Plugin\Eav;
+
+use Magento\Eav\Model\Entity\Attribute;
+use Magento\Eav\Model\Entity\Attribute as EavAttribute;
+use Magento\Framework\DataObject\IdentityInterface;
+use Magento\Framework\Event\ManagerInterface;
+
+/**
+ * Currency plugin triggers clean page cache and provides currency cache identities
+ */
+class AttributePlugin implements IdentityInterface
+{
+    /**
+     * Application Event Dispatcher
+     *
+     * @var ManagerInterface
+     */
+    private $eventManager;
+
+    /**
+     * @var array
+     */
+    private $identities = [];
+
+    /**
+     * @param ManagerInterface $eventManager
+     */
+    public function __construct(ManagerInterface $eventManager)
+    {
+        $this->eventManager = $eventManager;
+    }
+
+    /**
+     * Clean cache by relevant tags.
+     *
+     * @param Attribute $subject
+     * @param Attribute $result
+     * @return Attribute
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function afterSave(
+        Attribute $subject,
+        Attribute $result
+    ): Attribute
+    {
+        $this->identities[] = sprintf(
+            "%s_%s",
+            EavAttribute::CACHE_TAG,
+            $subject->getAttributeCode()
+        );
+        $this->eventManager->dispatch('clean_cache_by_tags', ['object' => $this]);
+        return $result;
+    }
+
+    public function getIdentities()
+    {
+        return $this->identities;
+    }
+}
