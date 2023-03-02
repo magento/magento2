c19acc9f3541e59d71284cd26a26afa62ad302c1
Henrique Venchiarutti <henrique.venchiarutti@webjump.com.br>
2023-03-02 19:15:35 -0300

    Adding created_at, status and grand_total filters to customer Orders query and fixing multiple filters failure

--- app/code/Magento/SalesGraphQl/Model/Resolver/CustomerOrders/Query/OrderFilter.php
+++ app/code/Magento/SalesGraphQl/Model/Resolver/CustomerOrders/Query/OrderFilter.php
@@ -25,6 +25,8 @@ class OrderFilter
      */
     private $fieldTranslatorArray = [
         'number' => 'increment_id',
+        'order_date' => 'created_at',
+        'grand_total' => 'base_grand_total',
     ];
 
     /**
@@ -85,7 +87,6 @@ class OrderFilter
         $filterGroups[] = $this->filterGroupBuilder->create();
 
         if (isset($args['filter'])) {
-            $filters = [];
             foreach ($args['filter'] as $field => $cond) {
                 if (isset($this->fieldTranslatorArray[$field])) {
                     $field = $this->fieldTranslatorArray[$field];
@@ -96,21 +97,21 @@ class OrderFilter
                             throw new InputException(__('Invalid match filter'));
                         }
                         $searchValue = $value !== null ? str_replace('%', '', $value) : '';
-                        $filters[] = $this->filterBuilder->setField($field)
+                        $filter = $this->filterBuilder->setField($field)
                             ->setValue("%{$searchValue}%")
                             ->setConditionType('like')
                             ->create();
                     } else {
-                        $filters[] = $this->filterBuilder->setField($field)
+                        $filter = $this->filterBuilder->setField($field)
                             ->setValue($value)
                             ->setConditionType($condType)
                             ->create();
                     }
                 }
-            }
 
-            $this->filterGroupBuilder->setFilters($filters);
-            $filterGroups[] = $this->filterGroupBuilder->create();
+                $this->filterGroupBuilder->setFilters([$filter]);
+                $filterGroups[] = $this->filterGroupBuilder->create();
+            }
         }
         return $filterGroups;
     }
