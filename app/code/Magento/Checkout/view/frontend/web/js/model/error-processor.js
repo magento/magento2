fb9bb8bbab5546d403e60486b5742668814f96ca
tuna <tuna9x.it@gmail.com>
2023-03-06 16:42:04 +0700

    improved error reporting when unexpected went wrong

--- app/code/Magento/Checkout/view/frontend/web/js/model/error-processor.js
+++ app/code/Magento/Checkout/view/frontend/web/js/model/error-processor.js
@@ -29,7 +29,7 @@ define([
                 try {
                     error = JSON.parse(response.responseText);
                 } catch (exception) {
-                    console.log(exception);
+                    console.error(exception);
                     error = {
                         message: $t('Something went wrong with your request. Please try again later.')
                     };
