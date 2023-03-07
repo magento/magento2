57755d207318cfd0c672423a51c6760abe32f47d
Roman Hanin <rganin@adobe.com>
2023-03-06 22:51:51 -0600

    B2B-2259: customAttributeMetadata GraphQl query has no cache identity

--- app/code/Magento/EavGraphQl/Plugin/Eav/AttributePlugin.php
+++ app/code/Magento/EavGraphQl/Plugin/Eav/AttributePlugin.php
@@ -8,85 +8,31 @@ declare(strict_types=1);
 namespace Magento\EavGraphQl\Plugin\Eav;
 
 use Magento\Eav\Model\Entity\Attribute;
-use Magento\Framework\DataObject\IdentityInterface;
-use Magento\Framework\Event\ManagerInterface;
 
 /**
  * EAV plugin runs page cache clean and provides proper EAV identities.
  */
-class AttributePlugin implements IdentityInterface
+class AttributePlugin
 {
-    /**
-     * Application Event Dispatcher
-     *
-     * @var ManagerInterface
-     */
-    private $eventManager;
-
-    /**
-     * @var array
-     */
-    private $identities = [];
-
-    /**
-     * @param ManagerInterface $eventManager
-     */
-    public function __construct(ManagerInterface $eventManager)
-    {
-        $this->eventManager = $eventManager;
-    }
-
     /**
      * Clean cache by relevant tags after entity save.
      *
      * @param Attribute $subject
-     * @param Attribute $result
+     * @param array $result
      *
      * @return Attribute
      */
-    public function afterSave(Attribute $subject, Attribute $result): Attribute
+    public function afterGetIdentities(Attribute $subject, array $result): array
     {
-        if (!$subject->isObjectNew()) {
-            $this->triggerCacheClean($subject);
-        }
-        return $result;
-    }
-
-    /**
-     * Clean cache by relevant tags after entity is deleted and afterDelete handler is executed.
-     *
-     * @param Attribute $subject
-     * @param Attribute $result
-     *
-     * @return Attribute
-     */
-    public function afterAfterDelete(Attribute $subject, Attribute $result) : Attribute
-    {
-        $this->triggerCacheClean($subject);
-        return $result;
-    }
-
-    /**
-     * Trigger cache clean event in event manager.
-     *
-     * @param Attribute $entity
-     * @return void
-     */
-    private function triggerCacheClean(Attribute $entity): void
-    {
-        $this->identities[] = sprintf(
-            "%s_%s",
-            Attribute::CACHE_TAG,
-            $entity->getAttributeCode()
+        return array_merge(
+            $result,
+            [
+                sprintf(
+                    "%s_%s",
+                    Attribute::CACHE_TAG,
+                    $subject->getAttributeCode()
+                )
+            ]
         );
-        $this->eventManager->dispatch('clean_cache_by_tags', ['object' => $this]);
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function getIdentities()
-    {
-        return $this->identities;
     }
 }
