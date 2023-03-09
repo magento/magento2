1af701ff5e668f325351b4234d4ffdaa524d951f
glo5363 <glo05363@adobe.com>
2023-03-09 17:21:02 +0530

    AC-1342:RestAPI add order comment without providing status sets status to NULL, preventing from showing in order grid- fixed status data when sending through API

--- app/code/Magento/Sales/Model/Order.php
+++ app/code/Magento/Sales/Model/Order.php
@@ -198,6 +198,7 @@ class Order extends AbstractModel implements EntityInterface, OrderInterface
     /**
      * @var \Magento\Catalog\Api\ProductRepositoryInterface
      * @deprecated 100.1.0 Remove unused dependency.
+     * @see no alternative
      */
     protected $productRepository;
 
@@ -1741,7 +1742,17 @@ class Order extends AbstractModel implements EntityInterface, OrderInterface
     public function addStatusHistory(\Magento\Sales\Model\Order\Status\History $history)
     {
         $history->setOrder($this);
-        $this->setStatus($history->getStatus());
+        if (!$history->getStatus()) {
+            $previousStatus = $this->getStatusHistoryCollection()->getFirstItem()->getData('status');
+            if (!$previousStatus) {
+                $defaultStatus = $this->getConfig()->getStateDefaultStatus($this->getState());
+                $history->setStatus($defaultStatus);
+            } else {
+                $history->setStatus($previousStatus);
+            }
+        } else {
+            $this->setStatus($history->getStatus());
+        }
         if (!$history->getId()) {
             $this->setStatusHistories(array_merge($this->getStatusHistories(), [$history]));
             $this->setDataChanges(true);
