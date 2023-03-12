1af531e67136be567f3d6cd11f304f641c88a46a
Tu Nguyen Anh <tuna9x.it@gmail.com>
2023-03-12 09:48:09 +0700

    Update options links

--- app/code/Magento/Theme/view/frontend/web/js/cookie-status.js
+++ app/code/Magento/Theme/view/frontend/web/js/cookie-status.js
@@ -4,6 +4,24 @@ define([
     'use strict';
 
     $.widget('mage.cookieStatus', {
+        options: {
+            type: 'popup',
+            responsive: true,
+            innerScroll: true,
+            autoOpen: true,
+            buttons: [{
+                text: $.mage.__('Close'),
+                class: 'cookie-status',
+
+                /**
+                 * Callback for click event
+                 */
+                click: function () {
+                    this.closeModal();
+                }
+            }]
+        },
+
         /**
          * Init object
          * @private
@@ -11,25 +29,7 @@ define([
         _init: function () {
             if (!navigator.cookieEnabled) {
                 require(['Magento_Ui/js/modal/modal', 'mage/translate'], function (modal) {
-                    var options = {
-                        type: 'popup',
-                        responsive: true,
-                        innerScroll: true,
-                        autoOpen: true,
-                        buttons: [{
-                            text: $.mage.__('Close'),
-                            class: 'cookie-status',
-
-                            /**
-                             * Callback for click event
-                             */
-                            click: function () {
-                                this.closeModal();
-                            }
-                        }]
-                    };
-
-                    modal(options, $('#cookie-status'));
+                    modal(this.options, $('#cookie-status'));
                 });
             }
         }
