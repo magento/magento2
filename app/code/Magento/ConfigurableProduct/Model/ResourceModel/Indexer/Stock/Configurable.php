7b3421310c7e79c520865e60e85675d3da14df8e
Dan Wallis <dan@wallis.nz>
2023-03-14 22:07:39 +0000

    Fix static test failure

--- app/code/Magento/ConfigurableProduct/Model/ResourceModel/Indexer/Stock/Configurable.php
+++ app/code/Magento/ConfigurableProduct/Model/ResourceModel/Indexer/Stock/Configurable.php
@@ -1,7 +1,5 @@
 <?php
 /**
- * CatalogInventory Configurable Products Stock Status Indexer Resource Model
- *
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
