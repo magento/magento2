775228092ef5bffe27364402f0f3f534c93cf9be
Rachana <rachana@BLR1-LMC-N84150.local>
2023-03-20 14:48:42 -0700

    BUG#AC-7014:elated, upsell and crosssell products query  position correctly saved in

--- app/code/Magento/RelatedProductGraphQl/Model/DataProvider/RelatedProductDataProvider.php
+++ app/code/Magento/RelatedProductGraphQl/Model/DataProvider/RelatedProductDataProvider.php
@@ -119,6 +119,9 @@ class RelatedProductDataProvider
         $collection->addFieldToFilter('product_id', ['in' => array_keys($productsByActualIds)]);
         $collection->addLinkTypeIdFilter();
 
+        $collection->addProductIdFilter();
+        $collection->joinAttributes();
+        $collection->setOrder('position', 'ASC');
         //Prepare map
         $map = [];
         /** @var Link $item */
