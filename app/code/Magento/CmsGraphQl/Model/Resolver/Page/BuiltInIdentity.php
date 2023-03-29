6c22c4141391f1605f13b98ccb7ccc262aee0f7d
Dan Mooney <dmooney@adobe.com>
2023-03-29 09:10:08 -0500

    B2B-2451: Implement GraphQL Resolver Cache for CMS Page Type

--- app/code/Magento/CmsGraphQl/Model/Resolver/Page/BuiltInIdentity.php
+++ app/code/Magento/CmsGraphQl/Model/Resolver/Page/BuiltInIdentity.php
@@ -12,6 +12,13 @@ use Magento\Framework\GraphQl\Query\Resolver\IdentityInterface;
 
 /**
  * Identity for resolved CMS page for built-in resolver cache type
+ *
+ * Magento\Cms\Model\Page::cleanModelCache overzealously purges all cache types for CMS page
+ * after modification of any CMS page by providing the global tag. Since CMS page GraphQL responses can only contain
+ * single entities, no two CMS page-specific tag assignments can coexist in a GraphQL resolver cache entry.
+ * Therefore, purging the entire CMS page-related GraphQL resolver cache on a single CMS page update is overkill for
+ * this cache type.  This class purposely does not provide global cms_p tag in order to avoid a global purge of the
+ * CMS page-related GraphQL resolver cache.
  */
 class BuiltInIdentity implements IdentityInterface
 {
