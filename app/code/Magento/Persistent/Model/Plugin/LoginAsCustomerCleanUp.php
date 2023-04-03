e1b12e95a9c189bdbe1ccf12cb30a3ab0a8c6c9e
Anna Bukatar <abukatar@magento.com>
2023-04-03 15:58:11 -0700

    ACP2E-1763: Issue with Cart when logged in as customer from admin

---
+++ app/code/Magento/Persistent/Model/Plugin/LoginAsCustomerCleanUp.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\Persistent\Model\Plugin;
+
+use Magento\LoginAsCustomerApi\Api\AuthenticateCustomerBySecretInterface;
+use Magento\Persistent\Helper\Session as PersistentSession;
+
+class LoginAsCustomerCleanUp
+{
+    /**
+     * @var PersistentSession
+     */
+    private $persistentSession;
+
+    /**
+     * @param PersistentSession $persistentSession
+     */
+    public function __construct(PersistentSession $persistentSession)
+    {
+        $this->persistentSession = $persistentSession;
+    }
+
+    /**
+     * @param AuthenticateCustomerBySecretInterface $subject
+     * @param string $secret
+     * @return string
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function beforeExecute(AuthenticateCustomerBySecretInterface $subject, string $secret)
+    {
+        if ($this->persistentSession->isPersistent()) {
+            $this->persistentSession->getSession()->removePersistentCookie();
+        }
+        return $secret;
+    }
+}
