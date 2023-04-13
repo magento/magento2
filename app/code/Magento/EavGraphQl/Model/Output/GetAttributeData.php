21d34c32576d2c801f9819260bc26f648af8f94e
Rafal Janicki <rjanicki@adobe.com>
2023-04-13 13:16:52 +0100

    LYNX-100: Return UNDEFINED for frontend_input if not set for attribute

--- app/code/Magento/EavGraphQl/Model/Output/GetAttributeData.php
+++ app/code/Magento/EavGraphQl/Model/Output/GetAttributeData.php
@@ -1,8 +1,10 @@
 <?php
+
 /**
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+
 declare(strict_types=1);
 
 namespace Magento\EavGraphQl\Model\Output;
@@ -69,16 +71,30 @@ class GetAttributeData implements GetAttributeDataInterface
                 'AttributeEntityTypeEnum',
                 $entityType
             ),
-            'frontend_input' => $this->enumLookup->getEnumValueFromField(
-                'AttributeFrontendInputEnum',
-                $attribute->getFrontendInput() ?? ""
-            ),
+            'frontend_input' => $this->getFrontendInput($attribute),
             'is_required' => $attribute->getIsRequired(),
             'default_value' => $attribute->getDefaultValue(),
             'is_unique' => $attribute->getIsUnique(),
             'options' => $this->getOptions($attribute),
             'attribute' => $attribute
         ];
+    }
+
+    /**
+     * Returns default frontend input for attribute if not set
+     * 
+     * @param AttributeInterface $attribute
+     * @return string
+     */
+    private function getFrontendInput(AttributeInterface $attribute): string
+    {
+        if ($attribute->getFrontendInput() == null) {
+            return "UNDEFINED";
+        }
+        return $this->enumLookup->getEnumValueFromField(
+            'AttributeFrontendInputEnum',
+            $attribute->getFrontendInput() ?? "undefined"
+        );
     }
 
     /**
