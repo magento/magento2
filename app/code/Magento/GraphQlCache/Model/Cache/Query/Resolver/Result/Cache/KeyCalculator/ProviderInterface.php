099e498be9847e41531239636f15204997b7d9f2
Roman Hanin <rganin@adobe.com>
2023-04-17 16:14:54 -0500

    B2B-2658: Implement GraphQL Resolver Cache for Customer query
    - CR-related fixes

--- app/code/Magento/GraphQlCache/Model/Cache/Query/Resolver/Result/Cache/KeyCalculator/ProviderInterface.php
+++ app/code/Magento/GraphQlCache/Model/Cache/Query/Resolver/Result/Cache/KeyCalculator/ProviderInterface.php
@@ -16,14 +16,6 @@ use Magento\GraphQlCache\Model\Cache\Query\Resolver\Result\Cache\KeyCalculator;
  */
 interface ProviderInterface
 {
-//    /**
-//     * Initialize strategy for the provided resolver.
-//     *
-//     * @param ResolverInterface $resolver
-//     * @return void
-//     */
-//    public function initForResolver(ResolverInterface $resolver): void;
-
     /**
      * Get cache id calculator for the given resolver.
      *
@@ -33,14 +25,4 @@ interface ProviderInterface
      * @return KeyCalculator
      */
     public function getForResolver(ResolverInterface $resolver): KeyCalculator;
-
-//    /**
-//     * Reinitialize state of the factor providers for the given resolver from the previous resolver data.
-//     *
-//     * @param ResolverInterface $resolver
-//     * @param array|null $result
-//     * @param ContextInterface $context
-//     * @return void
-//     */
-//    public function actualize(ResolverInterface $resolver, ?array $result, ContextInterface $context): void;
 }
