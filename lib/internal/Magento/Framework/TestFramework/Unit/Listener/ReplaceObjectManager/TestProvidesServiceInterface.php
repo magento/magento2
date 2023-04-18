dfc60a4d64245de6fad132efa09cc794e59001f8
Jacob Brown <jacob@gnu.org>
2023-04-18 16:31:22 -0500

    ACPT-987
    fixes for unit tests

---
+++ lib/internal/Magento/Framework/TestFramework/Unit/Listener/ReplaceObjectManager/TestProvidesServiceInterface.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Magento\Framework\TestFramework\Unit\Listener\ReplaceObjectManager;
+
+interface TestProvidesServiceInterface
+{
+    /**
+     * Gets a service object from a test to use by the mock object manager
+     *
+     * @param string $type
+     * @return object|null
+     */
+    public function getServiceForObjectManager(string $type) : ?object;
+}
