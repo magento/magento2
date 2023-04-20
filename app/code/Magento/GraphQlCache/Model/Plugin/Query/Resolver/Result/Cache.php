c6ff30517f063c8c33cadd8f0aaa207d6d8038e2
Roman Hanin <rganin@adobe.com>
2023-04-20 10:54:02 -0500

    B2B-2658: Implement GraphQL Resolver Cache
    - performance-related refactoring (avoid unnecessary instance creation)

--- app/code/Magento/GraphQlCache/Model/Plugin/Query/Resolver/Result/Cache.php
+++ app/code/Magento/GraphQlCache/Model/Plugin/Query/Resolver/Result/Cache.php
@@ -198,6 +198,6 @@ class Cache
             . '_'
             . $this->cacheKeyCalculatorProvider->getKeyCalculatorForResolver($resolver)->calculateCacheKey($value)
             . '_'
-            . sha1($queryPayloadHash);
+            . $queryPayloadHash;
     }
 }
