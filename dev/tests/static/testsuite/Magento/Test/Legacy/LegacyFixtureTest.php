51efbb31038a10972819ef5338336d4e0a970622
Andrii Dimov <andimov@gmail.com>
2023-04-25 20:47:20 -0500

    ACPT-987: Evaluate State check testing from PR
    - Fix Static Tests failures

--- dev/tests/static/testsuite/Magento/Test/Legacy/LegacyFixtureTest.php
+++ dev/tests/static/testsuite/Magento/Test/Legacy/LegacyFixtureTest.php
@@ -26,9 +26,10 @@ class LegacyFixtureTest extends TestCase
         $files = AddedFiles::getAddedFilesList(__DIR__ . '/..');
         $legacyFixtureFiles = [];
         //pattern to ignore skip and filter files
-        $skip_pattern = '/app\/code\/.*\/Tests?\/GraphQl\/_files\/(.*[filter|skip]-list(_ee|_b2b|).php)/';
+        $skip_pattern = '/(.*(filter|skip)-list(_ee|_b2b|).php)/';
         foreach ($files as $file) {
             if (pathinfo($file, PATHINFO_EXTENSION) === 'php'
+                && !preg_match($skip_pattern, $file)
                 && (
                     preg_match('/(integration\/testsuite|api-functional\/testsuite).*\/(_files|Fixtures)/', $file)
                     // Cover the case when tests are located in the module folder instead of dev/tests.
@@ -37,7 +38,6 @@ class LegacyFixtureTest extends TestCase
                         strpos($file, 'dev/tests/') === false
                         && preg_match('/app\/code\/.*\/Test.*\/(_files|Fixtures)/', $file)
                         && !preg_match('/app\/code\/.*\/Tests?\/Performance\/(_files|Fixtures)/', $file)
-                        && !preg_match($skip_pattern, $file)
                     )
                 )
             ) {
