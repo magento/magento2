11494edb33cf1c94238555243c474d8cb31470a7
glo82145 <glo82145@adobe.com>
2023-04-26 12:14:12 +0530

    Revert "AC-6507 : GraphQL Routes query with fragments has issue in the category page"
    This reverts commit 1cc7d68e0d5b1ba28c9b760b51f889a975edcc0b.

--- app/code/Magento/CatalogGraphQl/Model/Category/DepthCalculator.php
+++ app/code/Magento/CatalogGraphQl/Model/Category/DepthCalculator.php
@@ -80,7 +80,7 @@ class DepthCalculator
         foreach ($selections as $field) {
             if ($field->kind === NodeKind::INLINE_FRAGMENT) {
                 $depth[] = $this->addInlineFragmentDepth($resolveInfo, $field, $depth);
-            } elseif (isset($field->selectionSet) && $field->selectionSet->selections) {
+            } elseif ($field->selectionSet && $field->selectionSet->selections) {
                 $depth[] = $this->calculate($resolveInfo, $field);
             }
         }
