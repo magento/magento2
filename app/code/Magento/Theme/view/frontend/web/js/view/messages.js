60433922ce8458806da00d4d617d4933fdec85ff
glo24157 <glo24157@adobe.com>
2023-05-15 14:49:53 +0530

    Fix Error - Unexpected trailing comma. (comma-dangle)

--- app/code/Magento/Theme/view/frontend/web/js/view/messages.js
+++ app/code/Magento/Theme/view/frontend/web/js/view/messages.js
@@ -29,7 +29,7 @@ define([
         initialize: function () {
             this._super().observe(
                 [
-                    'cookieMessages',
+                    'cookieMessages'
                 ]
             );
 
