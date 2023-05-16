29519af0f6eb060abe0c568c0b9fe2cd3bf98d0c
Marc Ginesta <loginesta@gmail.com>
2023-05-16 15:05:08 +0200

    LYNX-163: Update CustomerAddress.custom_attributes deprecation message (#115)

--- dev/tests/api-functional/testsuite/Magento/GraphQl/Customer/CreateCustomerV2WithCustomAttributesTest.php
+++ dev/tests/api-functional/testsuite/Magento/GraphQl/Customer/CreateCustomerV2WithCustomAttributesTest.php
@@ -7,6 +7,14 @@ declare(strict_types=1);
 
 namespace Magento\GraphQl\Customer;
 
+use Exception;
+use Magento\Customer\Api\Data\AttributeMetadataInterface;
+use Magento\Customer\Api\Data\CustomerInterface;
+use Magento\Eav\Api\Data\AttributeOptionInterface;
+use Magento\Eav\Model\Entity\Attribute\Backend\ArrayBackend;
+use Magento\Eav\Model\Entity\Attribute\Source\Table;
+use Magento\Eav\Test\Fixture\AttributeOption as AttributeOptionFixture;
+use Magento\TestFramework\Fixture\DataFixtureStorageManager;
 use Magento\TestFramework\Helper\Bootstrap;
 use Magento\Customer\Api\CustomerMetadataInterface;
 use Magento\TestFramework\Fixture\DataFixture;
@@ -16,10 +24,112 @@ use Magento\Customer\Api\CustomerRepositoryInterface;
 use Magento\Framework\Registry;
 
 /**
- * Tests for create customer (V2)
+ * Tests for create customer V2
  */
+#[
+    DataFixture(
+        CustomerAttribute::class,
+        [
+            'entity_type_id' => CustomerMetadataInterface::ATTRIBUTE_SET_ID_CUSTOMER,
+            'attribute_set_id' => CustomerMetadataInterface::ATTRIBUTE_SET_ID_CUSTOMER,
+            'attribute_group_id' => 1,
+            'attribute_code' => 'random_attribute',
+            'sort_order' => 1
+        ],
+        'random_attribute',
+    ),
+    DataFixture(
+        CustomerAttribute::class,
+        [
+            'entity_type_id' => CustomerMetadataInterface::ATTRIBUTE_SET_ID_CUSTOMER,
+            'attribute_set_id' => CustomerMetadataInterface::ATTRIBUTE_SET_ID_CUSTOMER,
+            'attribute_group_id' => 1,
+            'source_model' => Table::class,
+            'backend_model' => ArrayBackend::class,
+            'attribute_code' => 'multiselect_attribute',
+            'frontend_input' => 'multiselect',
+            'sort_order' => 2
+        ],
+        'multiselect_attribute',
+    ),
+    DataFixture(
+        AttributeOptionFixture::class,
+        [
+            'entity_type' => CustomerMetadataInterface::ATTRIBUTE_SET_ID_CUSTOMER,
+            'attribute_code' => '$multiselect_attribute.attribute_code$',
+            'label' => 'line 1',
+            'sort_order' => 10
+        ],
+        'multiselect_attribute_option1'
+    ),
+    DataFixture(
+        AttributeOptionFixture::class,
+        [
+            'entity_type' => CustomerMetadataInterface::ATTRIBUTE_SET_ID_CUSTOMER,
+            'attribute_code' => '$multiselect_attribute.attribute_code$',
+            'label' => 'option 2',
+            'sort_order' => 20
+        ],
+        'multiselect_attribute_option2'
+    ),
+    DataFixture(
+        AttributeOptionFixture::class,
+        [
+            'entity_type' => CustomerMetadataInterface::ATTRIBUTE_SET_ID_CUSTOMER,
+            'attribute_code' => '$multiselect_attribute.attribute_code$',
+            'label' => 'option 3',
+            'sort_order' => 30
+        ],
+        'multiselect_attribute_option3'
+    )
+]
 class CreateCustomerV2WithCustomAttributesTest extends GraphQlAbstract
 {
+    /**
+     * @var string
+     */
+    private $query = <<<QUERY
+mutation {
+    createCustomerV2(
+        input: {
+            firstname: "Adam"
+            lastname: "Smith"
+            email: "adam@smith.com"
+            password: "test123#"
+            custom_attributes: [
+                {
+                    attribute_code: "%s",
+                    value: "%s"
+                }
+                {
+                    attribute_code: "%s"
+                    value: "%s"
+                    selected_options: []
+                }
+            ]
+        }
+    ) {
+        customer {
+            firstname
+            lastname
+            email
+            custom_attributes {
+                code
+                ... on AttributeValue {
+                    value
+                }
+                ... on AttributeSelectedOptions {
+                    selected_options {
+                        label
+                        value
+                    }
+                }
+            }
+        }
+    }
+}
+QUERY;
+
     /**
      * @var Registry
      */
@@ -30,79 +140,55 @@ class CreateCustomerV2WithCustomAttributesTest extends GraphQlAbstract
      */
     private $customerRepository;
 
+    /**
+     * @var AttributeMetadataInterface|null
+     */
+    private $random_attribute;
+
+    /**
+     * @var AttributeMetadataInterface|null
+     */
+    private $multiselect_attribute;
+
+    /**
+     * @var AttributeOptionInterface|null
+     */
+    private $option2;
+
+    /**
+     * @var AttributeOptionInterface|null
+     */
+    private $option3;
+
     protected function setUp(): void
     {
         parent::setUp();
 
         $this->registry = Bootstrap::getObjectManager()->get(Registry::class);
         $this->customerRepository = Bootstrap::getObjectManager()->get(CustomerRepositoryInterface::class);
+
+        $this->random_attribute = DataFixtureStorageManager::getStorage()->get('random_attribute');
+        $this->multiselect_attribute = DataFixtureStorageManager::getStorage()->get('multiselect_attribute');
+        $this->option2 = DataFixtureStorageManager::getStorage()->get('multiselect_attribute_option2');
+        $this->option3 = DataFixtureStorageManager::getStorage()->get('multiselect_attribute_option3');
     }
 
     /**
-     * @throws \Exception
+     * @return void
+     * @throws Exception
      */
-    #[
-        DataFixture(
-            CustomerAttribute::class,
-            [
-                'entity_type_id' => CustomerMetadataInterface::ATTRIBUTE_SET_ID_CUSTOMER,
-                'sort_order' => 1,
-                'attribute_code' => 'custom_attribute_one',
-                'attribute_set_id' => CustomerMetadataInterface::ATTRIBUTE_SET_ID_CUSTOMER,
-                'attribute_group_id' => 1
-            ],
-            'attribute1'
-        ),
-        DataFixture(
-            CustomerAttribute::class,
-            [
-                'entity_type_id' => CustomerMetadataInterface::ATTRIBUTE_SET_ID_CUSTOMER,
-                'sort_order' => 2,
-                'attribute_code' => 'custom_attribute_two',
-                'attribute_set_id' => CustomerMetadataInterface::ATTRIBUTE_SET_ID_CUSTOMER,
-                'attribute_group_id' => 1
-            ],
-            'attribute2'
-        )
-    ]
     public function testCreateCustomerAccountWithCustomAttributes()
     {
-        $query = <<<QUERY
-mutation {
-    createCustomerV2(
-        input: {
-            firstname: "Adam"
-            lastname: "Smith"
-            email: "adam@smith.com"
-            password: "test123#"
-            custom_attributes: [
-                {
-                    attribute_code: "custom_attribute_one",
-                    value: "value_one"
-                },
-                {
-                    attribute_code: "custom_attribute_two",
-                    value: "value_two"
-                }
-            ]
-        }
-    ) {
-        customer {
-            firstname
-            lastname
-            email
-            custom_attributes {
-                uid
-                code
-                ... on AttributeValue {
-                    value
-                }
-            }
-        }
-    }
-}
-QUERY;
-        $response = $this->graphQlMutation($query);
+        $response = $this->graphQlMutation(
+            sprintf(
+                $this->query,
+                $this->random_attribute->getAttributeCode(),
+                'new_value_for_attribute',
+                $this->multiselect_attribute->getAttributeCode(),
+                $this->option2->getValue() . "," . $this->option3->getValue()
+            )
+        );
+
         $this->assertEquals(
             [
                 'createCustomerV2' =>
@@ -115,17 +201,24 @@ QUERY;
                         'custom_attributes' =>
                         [
                             0 =>
-                            [
-                                'uid' => 'Y3VzdG9tZXIvY3VzdG9tX2F0dHJpYnV0ZV9vbmU=',
-                                'code' => 'custom_attribute_one',
-                                'value' => 'value_one',
-                            ],
+                                [
+                                    'code' => $this->random_attribute->getAttributeCode(),
+                                    'value' => 'new_value_for_attribute',
+                                ],
                             1 =>
-                            [
-                                'uid' => 'Y3VzdG9tZXIvY3VzdG9tX2F0dHJpYnV0ZV90d28=',
-                                'code' => 'custom_attribute_two',
-                                'value' => 'value_two',
-                            ],
+                                [
+                                    'code' => $this->multiselect_attribute->getAttributeCode(),
+                                    'selected_options' => [
+                                        [
+                                            'label' => $this->option2->getLabel(),
+                                            'value' => $this->option2->getValue()
+                                        ],
+                                        [
+                                            'label' => $this->option3->getLabel(),
+                                            'value' => $this->option3->getValue()
+                                        ]
+                                    ]
+                                ]
                         ],
                     ],
                 ],
@@ -192,7 +285,7 @@ QUERY;
         try {
             $customer1 = $this->customerRepository->get($email1);
             $customer2 = $this->customerRepository->get($email2);
-        } catch (\Exception $exception) {
+        } catch (Exception $exception) {
             return;
         }
 
