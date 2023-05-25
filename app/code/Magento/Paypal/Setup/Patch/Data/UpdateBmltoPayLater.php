dc29b1bbc0175215d543e6ca16f18d97183abf7e
RPrudon <r.prudon@youweagency.com>
2023-05-25 14:08:34 +0200

    Rewrite implementation with code standards in mind

--- app/code/Magento/Paypal/Setup/Patch/Data/UpdateBmltoPayLater.php
+++ app/code/Magento/Paypal/Setup/Patch/Data/UpdateBmltoPayLater.php
@@ -133,11 +133,8 @@ class UpdateBmltoPayLater implements DataPatchInterface
 
         foreach ($bmlSettings as $bmlPath => $bmlValue) {
             $setting = str_replace(self::BMLPATH, '', $bmlPath);
-            if(strpos($setting, '_') === false) {
-                continue; 
-            }
             $settingParts = explode('_', $setting);
-            if (count($settingParts) !== 2) {
+            if (!str_contains($setting, '_') || count($settingParts) !== 2) {
                 continue;
             }
             $page = $settingParts[0];
