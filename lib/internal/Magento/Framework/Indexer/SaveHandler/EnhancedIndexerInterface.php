9017c187c9654790944d1614eca4f7267fe55116
aplapana <aplapana@adobe.com>
2023-05-27 08:35:00 +0300

    ACP2E-1875: [Magento Cloud] Products incorrectly showed Out of Stock then all products show in stock - fixed static

--- lib/internal/Magento/Framework/Indexer/SaveHandler/EnhancedIndexerInterface.php
+++ lib/internal/Magento/Framework/Indexer/SaveHandler/EnhancedIndexerInterface.php
@@ -1,20 +1,30 @@
 <?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
 
 namespace Magento\Framework\Indexer\SaveHandler;
 
 interface EnhancedIndexerInterface extends IndexerInterface
 {
     /**
+     * Disable stacked queries mode
+     *
      * @return void
      */
     public function disableStackedActions(): void;
 
     /**
+     * Activates stacked actions mode
+     *
      * @return void
      */
     public function enableStackedActions(): void;
 
     /**
+     * Run stacked queries
+     *
      * @return void
      */
     public function triggerStackedActions(): void;
