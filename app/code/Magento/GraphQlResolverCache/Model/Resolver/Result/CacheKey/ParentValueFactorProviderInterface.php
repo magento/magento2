290ab3cb031c05c287c4f009062d75c4ac643646
Roman Hanin <rganin@adobe.com>
2023-06-15 11:50:10 -0500

    B2B-2658: Implement GraphQL Resolver Cache for Customer query

--- app/code/Magento/GraphQlResolverCache/Model/Resolver/Result/CacheKey/ParentValueFactorProviderInterface.php
+++ app/code/Magento/GraphQlResolverCache/Model/Resolver/Result/CacheKey/ParentValueFactorProviderInterface.php
@@ -37,13 +37,8 @@ interface ParentValueFactorProviderInterface
     /**
      * Returns the runtime value that should be used as factor.
      *
-     * Throws an Exception if factor value cannot be resolved.
-     *
      * @param ContextInterface $context
      * @param array $parentValue
-     *
-     * @throws \Exception
-     *
      * @return string
      * @throws \InvalidArgumentException
      */
