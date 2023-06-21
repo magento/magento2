4d27baf172f16e8ec9f0fabc439e36c1fbdfacbd
Anupam Bhatt <51681435+engcom-Echo@users.noreply.github.com>
2023-06-21 11:41:38 +0530

    Update CreditMemoGenerateIfConfigurableProductWithDecimalQuantityTest.php
    Revert DbIsolation flag

--- dev/tests/api-functional/testsuite/Magento/Sales/Service/V1/CreditMemoGenerateIfConfigurableProductWithDecimalQuantityTest.php
+++ dev/tests/api-functional/testsuite/Magento/Sales/Service/V1/CreditMemoGenerateIfConfigurableProductWithDecimalQuantityTest.php
@@ -21,7 +21,6 @@ use Magento\TestFramework\Fixture\DataFixture;
 use Magento\TestFramework\TestCase\WebapiAbstract;
 use Magento\Framework\ObjectManagerInterface;
 use Magento\TestFramework\Fixture\DataFixtureStorage;
-use Magento\TestFramework\Fixture\DbIsolation;
 use Magento\TestFramework\Fixture\DataFixtureStorageManager;
 use Magento\TestFramework\Helper\Bootstrap;
 use Magento\Catalog\Test\Fixture\Product as ProductFixture;
@@ -30,9 +29,6 @@ use Magento\ConfigurableProduct\Test\Fixture\Product as ConfigurableProductFixtu
 use Magento\Quote\Test\Fixture\GuestCart as GuestCartFixture;
 use Magento\ConfigurableProduct\Test\Fixture\AddProductToCart as AddConfigurableProductToCartFixture;
 
-#[
-    DbIsolation(true)
-]
 /**
  * API test for credit memo generation with configurable product decimal quantity.
  */
