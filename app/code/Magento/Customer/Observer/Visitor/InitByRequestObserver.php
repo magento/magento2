8bbaf5f70968a15ef8c9fab586cb495f73a6a9f3
Alexandra Zota <zota@adobe.com>
2023-06-24 15:55:41 +0300

    ACP2E-1972: reset visitor data before login, in case a previous post request generated a new visitor session

--- app/code/Magento/Customer/Observer/Visitor/InitByRequestObserver.php
+++ app/code/Magento/Customer/Observer/Visitor/InitByRequestObserver.php
@@ -6,13 +6,28 @@
 
 namespace Magento\Customer\Observer\Visitor;
 
+use Magento\Customer\Model\Visitor;
 use Magento\Framework\Event\Observer;
+use Magento\Framework\Session\SessionManagerInterface;
 
 /**
  * Visitor Observer
  */
 class InitByRequestObserver extends AbstractVisitorObserver
 {
+    /**
+     * @var SessionManagerInterface
+     */
+    private $sessionManager;
+
+    public function __construct(
+        Visitor $visitor,
+        SessionManagerInterface $sessionManager
+    ) {
+        parent::__construct($visitor);
+        $this->sessionManager = $sessionManager;
+    }
+
     /**
      * initByRequest
      *
@@ -21,6 +36,9 @@ class InitByRequestObserver extends AbstractVisitorObserver
      */
     public function execute(Observer $observer)
     {
+        if ($observer->getRequest()->getFullActionName() === 'customer_account_loginPost') {
+            $this->sessionManager->unsVisitorData();
+        }
         $this->visitor->initByRequest($observer);
     }
 }
