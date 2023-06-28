a46ae4e247ffa9bea4d06e6d744f9441b55e3516
Rizwan Khan <glo52257@adobe.com>
2023-06-28 10:49:20 +0530

    AC-8477: API page_cache/block/esi handles param enhancement

--- app/code/Magento/PageCache/Controller/Block.php
+++ app/code/Magento/PageCache/Controller/Block.php
@@ -4,6 +4,8 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types=1);
+
 namespace Magento\PageCache\Controller;
 
 use Magento\Framework\Serialize\Serializer\Base64Json;
