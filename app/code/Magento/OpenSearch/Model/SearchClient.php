ccf8a07fc47c1cb8f5edadd80fa77fd3506b88f7
Dmytro Horytskyi <dhorytskyi@magento.com>
2023-06-29 20:32:38 -0500

    ACP2E-948: Product listing GraphQL query limited to total_count 10,000 products only

--- app/code/Magento/OpenSearch/Model/SearchClient.php
+++ app/code/Magento/OpenSearch/Model/SearchClient.php
@@ -378,4 +378,26 @@ class SearchClient implements ClientInterface
         }
         return $properties;
     }
+
+    /**
+     * Open point in time
+     *
+     * @param array $params
+     * @return array
+     */
+    public function openPointInTime(array $params = []): array
+    {
+        return $this->getOpenSearchClient()->openPointInTime($params);
+    }
+
+    /**
+     * Close point in time
+     *
+     * @param array $params
+     * @return array
+     */
+    public function closePointInTime(array $params = []): array
+    {
+        return $this->getOpenSearchClient()->closePointInTime($params);
+    }
 }
