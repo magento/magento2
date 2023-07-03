845812006e986608ebce61d38c09f973f25aa951
akaash <akaash@BLR1-LMC-N71907.local>
2023-07-03 19:06:20 +0530

    ACQE-5180 | Place an order with other than the default currency and validate the email confirmation has the same currency

--- dev/tests/integration/testsuite/Magento/Bundle/Block/Sales/Order/Items/RendererTest.php
+++ dev/tests/integration/testsuite/Magento/Bundle/Block/Sales/Order/Items/RendererTest.php
@@ -11,6 +11,7 @@ use Magento\Bundle\Test\Fixture\Option as BundleOptionFixture;
 use Magento\Bundle\Test\Fixture\OrderItem as OrderItemFixture;
 use Magento\Bundle\Test\Fixture\Product as BundleProductFixture;
 use Magento\Catalog\Test\Fixture\Product as ProductFixture;
+use Magento\Customer\Api\AccountManagementInterface;
 use Magento\Customer\Model\Session;
 use Magento\Framework\ObjectManagerInterface;
 use Magento\Framework\View\LayoutInterface;
@@ -44,6 +45,11 @@ class RendererTest extends TestCase
     private $block;
 
     /**
+     * @var AccountManagementInterface
+     */
+    private $accountManagement;
+
+    /**
      * @defaultDoc
      */
     protected function setUp(): void
@@ -52,6 +58,7 @@ class RendererTest extends TestCase
         $layout = $this->objectManager->get(LayoutInterface::class);
         $this->block = $layout->createBlock(Renderer::class);
         $this->fixtures = Bootstrap::getObjectManager()->get(DataFixtureStorageManager::class)->getStorage();
+        $this->accountManagement = $this->objectManager->get(AccountManagementInterface::class);
     }
 
     #[
@@ -72,6 +79,64 @@ class RendererTest extends TestCase
         $incrementId =  $this->fixtures->get('order')->getIncrementId();
         $order->loadByIncrementId($incrementId);
 
+        $storeManager = $this->objectManager->get(StoreManagerInterface::class);
+        $currencyCode = $storeManager->getWebsite()->getDefaultStore()->getDefaultCurrency()->getCode();
+        $storeId = $this->objectManager->get(StoreManagerInterface::class)->getStore()->getId();
+        $order->setStoreId($storeId);
+        $order->setOrderCurrencyCode($currencyCode);
+        $order->save();
+
+        $priceBlockHtml = [];
+
+        $items = $order->getAllItems();
+        foreach ($items as $item) {
+            $item->setProductOptions([
+                'bundle_options' => [
+                    [
+                        'value' => [
+                            ['title' => '']
+                        ],
+                    ],
+                ],
+                'bundle_selection_attributes' => '{"qty":5 ,"price":99}'
+            ]);
+            $this->block->setItem($item);
+            $priceBlockHtml[] = $this->block->getValueHtml($item);
+        }
+
+        $this->assertStringContainsString("€99", $priceBlockHtml[0]);
+    }
+
+    /**
+     * @return void
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    #[
+        DbIsolation(false),
+        Config('default/currency/options/base', 'USD', 'store', 'default'),
+        Config('currency/options/default', 'EUR', 'store', 'default'),
+        Config('currency/options/allow', 'USD, EUR', 'store', 'default'),
+        DataFixture(ProductFixture::class, ['price' => 10], 'p1'),
+        DataFixture(ProductFixture::class, ['price' => 20], 'p2'),
+        DataFixture(ProductFixture::class, ['price' => 30], 'p3'),
+        DataFixture(BundleOptionFixture::class, ['product_links' => ['$p1$', '$p2$', '$p3$']], 'opt1'),
+        DataFixture(BundleProductFixture::class, ['_options' => ['$opt1$']], 'bundle1'),
+        DataFixture(OrderItemFixture::class, ['items' => [['sku' => '$bundle1.sku$']]], 'order'),
+        DataFixture(Customer::class, ['email' => 'customer@example.com'], as: 'customer'),
+    ]
+    public function testPlaceOrderWithOtherThanDefaultCurrencyValidateEmailHasSameCurrency(): void
+    {
+        // Load customer data
+        $customer = $this->fixtures->get('customer');
+        $customerEmail = $customer->getEmail();
+
+        // Login to customer
+        $this->accountManagement->authenticate($customerEmail, 'password');
+
+        // Place the order
+        $order = $this->objectManager->create(Order::class);
+        $incrementId = $this->fixtures->get('order')->getIncrementId();
+        $order->loadByIncrementId($incrementId);
         $storeManager = $this->objectManager->get(StoreManagerInterface::class);
         $currencyCode = $storeManager->getWebsite()->getDefaultStore()->getDefaultCurrency()->getCode();
         $storeId = $this->objectManager->get(StoreManagerInterface::class)->getStore()->getId();
