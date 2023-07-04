b2b5e64f5a3f43c2be7d380177bf2ea430134c9a
Dmytro Horytskyi <dhorytskyi@magento.com>
2023-07-04 10:34:21 -0500

    ACP2E-948: Product listing GraphQL query limited to total_count 10,000 products only

--- app/code/Magento/OpenSearch/SearchAdapter/Adapter.php
+++ app/code/Magento/OpenSearch/SearchAdapter/Adapter.php
@@ -141,7 +141,7 @@ class Adapter implements AdapterInterface
             $rawResponse = $client->query($query);
         } catch (\Exception $e) {
             $this->logger->critical($e);
-            // return empty search result in case an exception is thrown from Elasticsearch
+            // return empty search result in case an exception is thrown from OpenSearch
             $rawResponse = self::$emptyRawResponse;
         } finally {
             if (isset($pit)) {
