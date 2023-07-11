0ca356189ce3f39e90564d82b592d6ed13eeddf5
Francisco Fernandez <115582581+Franciscof-Serfe@users.noreply.github.com>
2023-07-10 23:00:46 -0300

    Update Data.php

--- app/code/Magento/Usps/Helper/Data.php
+++ app/code/Magento/Usps/Helper/Data.php
@@ -18,24 +18,24 @@ class Data extends AbstractHelper
      * @var array
      */
     protected $availableShippingMethods = [
-        'usps_0_FCLE', // First-Class Mail Large Envelope
-        'usps_1',      // Priority Mail
-        'usps_2',      // Priority Mail Express Hold For Pickup
-        'usps_3',      // Priority Mail Express
-        'usps_4',      // Retail Ground
-        'usps_6',      // Media Mail
-        'usps_INT_1',  // Priority Mail Express International
-        'usps_INT_2',  // Priority Mail International
-        'usps_INT_4',  // Global Express Guaranteed (GXG)
-        'usps_INT_7',  // Global Express Guaranteed Non-Document Non-Rectangular
-        'usps_INT_8',  // Priority Mail International Flat Rate Envelope
-        'usps_INT_9',  // Priority Mail International Medium Flat Rate Box
-        'usps_INT_10', // Priority Mail Express International Flat Rate Envelope
-        'usps_INT_11', // Priority Mail International Large Flat Rate Box
-        'usps_INT_12', // USPS GXG Envelopes
-        'usps_INT_14', // First-Class Mail International Large Envelope
-        'usps_INT_16', // Priority Mail International Small Flat Rate Box
-        'usps_INT_20', // Priority Mail International Small Flat Rate Envelope
+        'usps_0_FCLE',  // First-Class Mail Large Envelope
+        'usps_1',       // Priority Mail
+        'usps_2',       // Priority Mail Express Hold For Pickup
+        'usps_3',       // USPS Ground Advantage (up to 15.999 oz)
+        'usps_6',       // USPS Ground Advantage LIVES, Offshore
+        'usps_INT_1',   // Priority Mail Express International
+        'usps_INT_2',   // Priority Mail International
+        'usps_INT_4',   // Global Express Guaranteed (GXG)
+        'usps_INT_7',   // Global Express Guaranteed Non-Document Non-Rectangular
+        'usps_INT_8',   // Priority Mail International Flat Rate Envelope
+        'usps_INT_9',   // Priority Mail International Medium Flat Rate Box
+        'usps_INT_10',  // Priority Mail Express International Flat Rate Envelope
+        'usps_INT_11',  // Priority Mail International Large Flat Rate Box
+        'usps_INT_12',  // USPS GXG Envelopes
+        'usps_INT_14',  // First-Class Mail International Large Envelope
+        'usps_INT_16',  // Priority Mail International Small Flat Rate Box
+        'usps_INT_20',  // Priority Mail International Small Flat Rate Envelope
+        'usps_9'        // USPS Ground Advantage (1 to 70lbs)
     ];
 
     /**
