b1b28ae4da13aaf609bc531c2e5adb21447489c6
glo5363 <glo05363@adobe.com>
2023-07-12 14:56:05 +0530

    #AC-8918::UPS REST API integration , converting XML API to REST API

---
+++ app/code/Magento/Ups/Model/UpsOauth.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Magento\Ups\Model;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\Serialize\Serializer\Json;
+use Magento\Store\Model\StoreManagerInterface;
+
+/**
+ * UPS OAuth Token
+ */
+class UpsOauth
+{
+    /**
+     * @var Client
+     */
+    protected $httpClient;
+
+    /**
+     * @var StoreManagerInterface
+     */
+    protected $storeManager;
+
+    /**
+     * @var Json
+     */
+    protected $jsonSerializer;
+
+    public const TEST_AUTH_URL = 'https://onlinetools.ups.com/security/v1/oauth/token';
+
+    /**
+     *
+     * @param Client $httpClient
+     * @param StoreManagerInterface $storeManager
+     * @param Json $jsonSerializer
+     */
+    public function __construct(
+        Client                $httpClient,
+        StoreManagerInterface $storeManager,
+        Json                  $jsonSerializer
+    ) {
+        $this->httpClient = $httpClient;
+        $this->storeManager = $storeManager;
+        $this->jsonSerializer = $jsonSerializer;
+    }
+
+    /**
+     * Token Generation
+     *
+     * @param String $clientId
+     * @param String $clientSecret
+     * @return mixed
+     * @throws LocalizedException
+     * @throws NoSuchEntityException
+     */
+    public function getAccessToken($clientId, $clientSecret)
+    {
+        $storeId = $this->storeManager->getStore()->getId();
+        $scope = $this->storeManager->getStore($storeId)->getCode();
+
+        try {
+            $response = $this->httpClient->request('POST', self::TEST_AUTH_URL, [
+                'headers' => [
+                    'Content-Type' => 'application/x-www-form-urlencoded',
+                    'x-merchant-id' => 'string',
+                    'Authorization' => 'Basic ' . base64_encode("$clientId:$clientSecret")
+                ],
+                'form_params' => [
+                    'grant_type' => 'client_credentials',
+                    'client_id' => $clientId,
+                    'client_secret' => $clientSecret,
+                    'scope' => $scope,
+                ],
+            ]);
+
+            $body = $response->getBody()->getContents();
+            $responseData = $this->jsonSerializer->unserialize($body);
+
+            if (isset($responseData['access_token'])) {
+                return $responseData['access_token'];
+            } else {
+                throw new LocalizedException(__('Unable to retrieve access token.'));
+            }
+        } catch (GuzzleException $e) {
+            throw new LocalizedException(__('Error occurred: %1', $e->getMessage()));
+        }
+    }
+}
