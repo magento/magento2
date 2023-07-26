8a4fb7f414242cb677f09b40e784a1c7b4cfba04
Dan Mooney <dmooney@adobe.com>
2023-07-26 15:24:44 -0500

    B2B-2677: [MediaGallery]Implement data caching for GraphQL results on resolver level
    - Invalidate media gallery resolver cache on import

---
+++ app/code/Magento/CatalogGraphQl/Observer/AfterImportDataObserver.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\CatalogGraphQl\Observer;
+
+use Magento\Framework\Event\Observer;
+use Magento\CatalogGraphQl\Model\Resolver\Cache\Product\MediaGallery\ResolverCacheIdentity;
+use Magento\Framework\Event\ObserverInterface;
+use Magento\GraphQlResolverCache\Model\Resolver\Result\Type as GraphQlResolverCache;
+
+class AfterImportDataObserver implements ObserverInterface
+{
+    /**
+     * @var GraphQlResolverCache
+     */
+    private $graphQlResolverCache;
+
+    /**
+     * @param GraphQlResolverCache $graphQlResolverCache
+     */
+    public function __construct(GraphQlResolverCache $graphQlResolverCache)
+    {
+        $this->graphQlResolverCache = $graphQlResolverCache;
+    }
+
+    public function execute(Observer $observer)
+    {
+        $mediaGalleryEntriesChanged = $observer->getEvent()->getMediaGallery();
+
+        $productSkusToInvalidate = [];
+
+        foreach ($mediaGalleryEntriesChanged as $productSkus) {
+            $productSkusToInvalidate[] = array_keys($productSkus);
+        }
+
+        $productSkusToInvalidate = array_merge([], ...$productSkusToInvalidate);
+
+        $tags = array_map(function ($productSku) {
+            return sprintf('%s_%s', ResolverCacheIdentity::CACHE_TAG, $productSku);
+        }, $productSkusToInvalidate);
+
+        if (!empty($tags)) {
+            $this->graphQlResolverCache->clean(
+                \Zend_Cache::CLEANING_MODE_MATCHING_ANY_TAG,
+                $tags
+            );
+        }
+    }
+}
