bc0353ddcbd7e91f3de0606663bad642666c80d9
Dan Mooney <dmooney@adobe.com>
2023-07-26 09:56:44 -0500

    B2B-2677: [MediaGallery]Implement data caching for GraphQL results on resolver level
    - Load product from DB

--- app/code/Magento/CatalogGraphQl/Plugin/Product/UpdateIdentities.php
+++ app/code/Magento/CatalogGraphQl/Plugin/Product/UpdateIdentities.php
@@ -7,6 +7,7 @@ declare(strict_types=1);
 
 namespace Magento\CatalogGraphQl\Plugin\Product;
 
+use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Catalog\Model\Product;
 use Magento\CatalogGraphQl\Model\Resolver\Cache\Product\MediaGallery\ResolverCacheIdentity;
 use Magento\Framework\Serialize\SerializerInterface;
@@ -18,15 +19,24 @@ use Magento\Framework\Serialize\SerializerInterface;
 class UpdateIdentities
 {
     /**
+     * @var ProductRepositoryInterface
+     */
+    private $productRepository;
+
+    /**
      * @var SerializerInterface
      */
     private $serializer;
 
     /**
+     * @param ProductRepositoryInterface $productRepository
      * @param SerializerInterface $serializer
      */
-    public function __construct(SerializerInterface $serializer)
-    {
+    public function __construct(
+        ProductRepositoryInterface $productRepository,
+        SerializerInterface $serializer
+    ) {
+        $this->productRepository = $productRepository;
         $this->serializer = $serializer;
     }
 
@@ -57,7 +67,12 @@ class UpdateIdentities
             return false;
         }
 
-        $mediaGalleryImages = $product->getMediaGallery('images') ?? [];
+        $mediaGalleryImages = $this->productRepository->get(
+            $product->getSku(),
+            false,
+            null,
+            true
+        )->getMediaGallery('images') ?? [];
 
         $origMediaGalleryImages = $product->getOrigData('media_gallery')['images'] ?? [];
 
