e7978200d2695a1ee09b424dc4a9fb38f473ebfd
Marc Ginesta <loginesta@gmail.com>
2023-08-08 11:06:54 +0200

    LYNX-174: Add a config to enable / disable cancellation (#137)

--- app/code/Magento/OrderCancellation/registration.php
+++ app/code/Magento/OrderCancellation/registration.php
@@ -8,4 +8,4 @@ declare(strict_types=1);
 
 use Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Magento_OrderCancellationGraphQl', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Magento_OrderCancellation', __DIR__);
