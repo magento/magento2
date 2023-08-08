6b05b843dafb9caa69a0f4cb1c3cbbb9d5d13830
engcom-Charlie <engcom-vendorworker-charlie@adobe.com>
2023-08-08 13:17:07 +0530

    view model implementation

--- app/code/Magento/Catalog/ViewModel/Category/Image.php
+++ app/code/Magento/Catalog/ViewModel/Category/Image.php
@@ -7,10 +7,13 @@ declare(strict_types=1);
 
 namespace Magento\Catalog\ViewModel\Category;
 
+use Magento\Framework\App\ObjectManager;
 use Magento\Catalog\Model\Category;
 use Magento\Catalog\Model\Category\Image as CategoryImage;
+use Magento\Framework\ObjectManagerInterface;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
-
+//use Magento\Catalog\Model\Product\Image as ProductImage;
+use Magento\Catalog\Model\Product as ProductImage;
 /**
  * Category image view model
  */
@@ -22,14 +25,24 @@ class Image implements ArgumentInterface
      */
     private $image;
 
+
+    private object $product;
+
+    /**
+     * @var ObjectManagerInterface
+     */
+    private $objectManager;
+
     /**
      * Initialize dependencies.
      *
      * @param CategoryImage $image
+     *
      */
     public function __construct(CategoryImage $image)
     {
         $this->image = $image;
+        $this->product = $product ?? ObjectManager::getInstance()->get(ProductImage::class);
     }
 
     /**
@@ -42,5 +55,21 @@ class Image implements ArgumentInterface
     public function getUrl(Category $category, string $attributeCode = self::ATTRIBUTE_NAME): string
     {
         return $this->image->getUrl($category, $attributeCode);
+    }
+
+    /**
+     * Get unique container ID for image
+     * @param $product string
+     * @return string
+     */
+    public function getContainerIdnew( $product) : string
+    {
+        if (!$this->product->hasData('container_id')) {
+            $uniqId = uniqid($product);
+            //$uniqId = uniqid($this->product->getProductId());
+            $this->product->setData('container_id', $uniqId);
+        }
+
+        return $this->product->getData('container_id');
     }
 }
