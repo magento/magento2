13991e472984eb4729e00192d641a8bc8206c943
glo5363 <glo05363@adobe.com>
2023-09-01 12:06:28 +0530

    AC-8918::Migration from SOAP to RESTfulAPI-copyright changes for static failures fixes

--- dev/tests/integration/testsuite/Magento/Ups/Model/UpsAuthTest.php
+++ dev/tests/integration/testsuite/Magento/Ups/Model/UpsAuthTest.php
@@ -1,23 +1,8 @@
 <?php
-/************************************************************************
- *
- * ADOBE CONFIDENTIAL
- * ___________________
- *
- * Copyright 2023 Adobe
- * All Rights Reserved.
- *
- * NOTICE: All information contained herein is, and remains
- * the property of Adobe and its suppliers, if any. The intellectual
- * and technical concepts contained herein are proprietary to Adobe
- * and its suppliers and are protected by all applicable intellectual
- * property laws, including trade secret and copyright laws.
- * Dissemination of this information or reproduction of this material
- * is strictly forbidden unless prior written permission is obtained
- * from Adobe.
- * ************************************************************************
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
  */
-
 declare(strict_types=1);
 
 namespace Magento\Ups\Model;
