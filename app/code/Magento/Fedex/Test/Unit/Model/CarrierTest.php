f236db8b8addf843c3f4b06254f64a7e162c8503
aplapana <aplapana@adobe.com>
2023-09-22 11:30:09 +0300

    ACP2E-2258: Cannot create FedEx Shipping Label - adjusted copyright

--- app/code/Magento/Fedex/Test/Unit/Model/CarrierTest.php
+++ app/code/Magento/Fedex/Test/Unit/Model/CarrierTest.php
@@ -1,8 +1,4 @@
 <?php
-/**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
- */
 /************************************************************************
  *
  * ADOBE CONFIDENTIAL
