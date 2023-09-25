c1f19d18d862ff68c234f4016912647630d3e9bf
engcom-Echo <engcom-vendorworker-echo@adobe.com>
2023-09-25 13:07:08 +0530

    Fixed static test failure

--- app/code/Magento/Wishlist/Block/Customer/Wishlist/Item/Column/Actions.php
+++ app/code/Magento/Wishlist/Block/Customer/Wishlist/Item/Column/Actions.php
@@ -11,6 +11,7 @@ namespace Magento\Wishlist\Block\Customer\Wishlist\Item\Column;
  *
  * @api
  * @deprecated 101.1.2
+ * @see no longer used
  * @since 100.0.2
  */
 class Actions extends \Magento\Wishlist\Block\Customer\Wishlist\Item\Column
