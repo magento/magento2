f1975924bfddbc6c3098e181204ed8f8460de61c
glo17720 <glo17720@adobe.com>
2023-10-09 13:32:40 +0530

    AC-1477:: [Issue] Update AddDataForIndia.php

--- app/code/Magento/Directory/Setup/Patch/Data/AddDataForIndia.php
+++ app/code/Magento/Directory/Setup/Patch/Data/AddDataForIndia.php
@@ -82,7 +82,6 @@ class AddDataForIndia implements DataPatchInterface, PatchVersionInterface
             ['IN', 'JH', 'Jharkhand'],
             ['IN', 'KA', 'Karnataka'],
             ['IN', 'KL', 'Kerala'],
-            ['IN', 'LA', 'Ladakh'],
             ['IN', 'LD', 'Lakshadweep'],
             ['IN', 'MP', 'Madhya Pradesh'],
             ['IN', 'MH', 'Maharashtra'],
