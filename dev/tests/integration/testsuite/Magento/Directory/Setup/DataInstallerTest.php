e2d647b4b2fcef1c76573c56c0a2f60fd114d328
Yaroslav Kozar <kozar.yaroslav1@gmail.com>
2023-10-23 19:49:43 +0300

    magento#37796: Fixed static test

--- dev/tests/integration/testsuite/Magento/Directory/Setup/DataInstallerTest.php
+++ dev/tests/integration/testsuite/Magento/Directory/Setup/DataInstallerTest.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
 
 namespace Magento\Directory\Setup;
 
@@ -23,7 +28,7 @@ class DataInstallerTest extends TestCase
     private $resourceConnection;
 
     /**
-     * @return void
+     * @inheritDoc
      */
     protected function setUp(): void
     {
