a69b9a4b5e41390f8c98328b32ec6cef394cc057
Aasim Goriya <aasim110@gmail.com>
2023-10-26 10:59:31 +0530

    Update ProductPriceTest.php for Unit test

--- dev/tests/api-functional/testsuite/Magento/GraphQl/Catalog/ProductPriceTest.php
+++ dev/tests/api-functional/testsuite/Magento/GraphQl/Catalog/ProductPriceTest.php
@@ -1229,8 +1229,11 @@ QUERY;
                 self::EPSILON
             );
             $this->assertEqualsWithDelta(
-                $expected['discount']['percent_off'],
-                round(($actual['regular_price']['value'] - round($actual['final_price']['value'],2))*100/$actual['regular_price']['value'], 2),
+                $expected['discount']['percent_off'], 
+                round(
+                    ($actual['regular_price']['value'] - round($actual['final_price']['value'], 2)) * 100 / $actual['regular_price']['value'],
+                    2
+                ),
                 self::EPSILON
             );
         }
