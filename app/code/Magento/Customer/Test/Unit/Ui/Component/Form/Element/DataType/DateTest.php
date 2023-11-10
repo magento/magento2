894dccf527c67d7f4a5f75c6542b62c77993d857
Arularasan <glo02433@adobe.com>
2023-11-10 15:53:25 +0530

    ACP2E-2127: Date filter is not working in admin grid.  - Added the test coverage.

---
+++ app/code/Magento/Customer/Test/Unit/Ui/Component/Form/Element/DataType/DateTest.php
@@ -0,0 +1,157 @@
+<?php
+
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\Customer\Test\Unit\Ui\Component\Form\Element\DataType;
+
+use Exception;
+use Magento\Customer\Ui\Component\Form\Element\DataType\Date;
+use Magento\Framework\Stdlib\DateTime\Intl\DateFormatterFactory;
+use Magento\Ui\Component\Form\Element\DataType\Date as UiComponentDate;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test for class \Magento\Customer\Ui\Component\Form\Element\DataType\Date
+ */
+class DateTest extends TestCase
+{
+    /**
+     * @var Date
+     */
+    protected Date $model;
+
+    /**
+     * @var DateFormatterFactory|MockObject
+     */
+    private $dateFormatterFactoryMock;
+
+    /**
+     * @var UiComponentDate|MockObject
+     */
+    private $subjectMock;
+
+    /**
+     * Set up
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        $this->dateFormatterFactoryMock = $this->getMockForAbstractClass(DateFormatterFactory::class);
+        $this->subjectMock = $this->getMockBuilder(UiComponentDate::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->model = new Date(
+            $this->dateFormatterFactoryMock
+        );
+    }
+
+    /**
+     * Run test for beforeConvertDate() method
+     *
+     * @param string $date
+     * @param string $locale
+     * @param int $hour
+     * @param int $minute
+     * @param int $second
+     * @param bool $setUtcTimeZone
+     * @param array $expected
+     * @return void
+     * @dataProvider beforeConvertDateDataProvider
+     * @throws Exception
+     */
+    public function testBeforeConvertDate(
+        string $date,
+        string $locale,
+        int $hour,
+        int $minute,
+        int $second,
+        bool $setUtcTimeZone,
+        array $expected
+    ): void {
+        $this->subjectMock->expects($this->once())
+            ->method('getLocale')
+            ->willReturn($locale);
+        $this->assertEquals(
+            $expected,
+            $this->model->beforeConvertDate(
+                $this->subjectMock,
+                $date,
+                $hour,
+                $minute,
+                $second,
+                $setUtcTimeZone
+            )
+        );
+    }
+
+    /**
+     * DataProvider for testBeforeConvertDate()
+     *
+     * @return array
+     */
+    public function beforeConvertDateDataProvider(): array
+    {
+        return [
+            [
+                '2023-10-15',
+                'en_US',
+                0,
+                0,
+                0,
+                true,
+                ['10/15/2023', 0, 0, 0, true]
+            ],
+            [
+                '10/15/2023',
+                'en_US',
+                0,
+                0,
+                0,
+                true,
+                ['10/15/2023', 0, 0, 0, true]
+            ],
+            [
+                '2023-10-15',
+                'en_GB',
+                0,
+                0,
+                0,
+                true,
+                ['15/10/2023', 0, 0, 0, true]
+            ],
+            [
+                '15/10/2023',
+                'en_GB',
+                0,
+                0,
+                0,
+                true,
+                ['15/10/2023', 0, 0, 0, true]
+            ],
+            [
+                '2023-10-15',
+                'ja_JP',
+                0,
+                0,
+                0,
+                true,
+                ['2023/10/15', 0, 0, 0, true]
+            ],
+            [
+                '2023/10/15',
+                'ja_JP',
+                0,
+                0,
+                0,
+                true,
+                ['2023/10/15', 0, 0, 0, true]
+            ]
+        ];
+    }
+}
