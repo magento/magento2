2ff00cba29fa47c32db42c92f8b3425f9a4efe69
Rizwan Khan <glo52257@adobe.com>
2023-11-20 23:25:33 +0530

    AC-9831: Block template render enhancement

--- lib/internal/Magento/Framework/View/Element/AbstractBlock.php
+++ lib/internal/Magento/Framework/View/Element/AbstractBlock.php
@@ -1048,10 +1048,10 @@ abstract class AbstractBlock extends \Magento\Framework\DataObject implements Bl
     public function getCacheKey()
     {
         if ($this->hasData('cache_key')) {
-            if (!preg_match('/^[a-fA-F0-9]{64}$/', $this->getData('cache_key'))) {
+            if (preg_match('/[^a-z0-9\-\_]/', $this->getData('cache_key'))) {
                 throw new \Magento\Framework\Exception\LocalizedException(
                     __(
-                        'Please enter cache key with only alphanumeric characters.'
+                        'Please enter cache key with only alphanumeric or hash string.'
                     )
                 );
             }
