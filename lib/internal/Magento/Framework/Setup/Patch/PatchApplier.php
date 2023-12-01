f7f812312628918bfd17f58ae951e2d4db6c0cff
Thomas Klein <thomasklein876@gmail.com>
2023-12-01 15:15:46 +0100

    Add patch to history if alias exists

--- lib/internal/Magento/Framework/Setup/Patch/PatchApplier.php
+++ lib/internal/Magento/Framework/Setup/Patch/PatchApplier.php
@@ -3,6 +3,7 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
+declare(strict_types=1);
 
 namespace Magento\Framework\Setup\Patch;
 
@@ -309,9 +310,11 @@ class PatchApplier
      */
     private function isApplied(PatchInterface $patch): bool
     {
-        if (!$this->patchHistory->isApplied(get_class($patch))) {
+        $patchIdentity = get_class($patch);
+        if (!$this->patchHistory->isApplied($patchIdentity)) {
             foreach ($patch->getAliases() as $alias) {
                 if ($this->patchHistory->isApplied($alias)) {
+                    $this->patchHistory->fixPatch($patchIdentity);
                     return true;
                 }
             }
