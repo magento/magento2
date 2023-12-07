c808611368f7eac91a567058faa82d02df8645b7
Chhandak.Barua <chhandak.barua@BLR1-LMC-N73490.local>
2023-12-07 23:34:33 +0530

    ACP2E-2519: The coupon code count does not update in the Time Used column in the Manage Coupon Codes tab if an order is placed with multi-shipping.

---
+++ app/code/Magento/SalesRule/Observer/CouponUsagesIncrementObserver.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\SalesRule\Observer;
+
+use Magento\Framework\Event\Observer as EventObserver;
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\SalesRule\Model\Coupon\Quote\UpdateCouponUsages;
+
+/**
+ * Decrement number of coupon usages after error of placing order
+ */
+class CouponUsagesIncrementObserver implements ObserverInterface
+{
+    /**
+     * @var UpdateCouponUsages
+     */
+    private $updateCouponUsages;
+
+    /**
+     * @param UpdateCouponUsages $updateCouponUsages
+     */
+    public function __construct(UpdateCouponUsages $updateCouponUsages)
+    {
+        $this->updateCouponUsages = $updateCouponUsages;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function execute(EventObserver $observer)
+    {
+        /** @var CartInterface $quote */
+        $quote = $observer->getQuote();
+        /* if coupon code has been canceled then need to notify the customer */
+        if (!$quote->getCouponCode() && $quote->dataHasChangedFor('coupon_code')) {
+            throw new NoSuchEntityException(__("The coupon code isn't valid. Verify the code and try again."));
+        }
+        $this->updateCouponUsages->execute($quote, true);
+    }
+}
