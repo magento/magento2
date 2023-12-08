323ac833ed8218cc778ad58237ad4cb2f1019a52
Chhandak.Barua <chhandak.barua@BLR1-LMC-N73490.local>
2023-12-08 18:14:47 +0530

    ACP2E-2519: The coupon code count does not update in the Time Used column in the Manage Coupon Codes tab if an order is placed with multi-shipping.

--- app/code/Magento/SalesRule/Observer/CouponUsagesIncrementObserver.php
+++ app/code/Magento/SalesRule/Observer/CouponUsagesIncrementObserver.php
@@ -41,6 +41,7 @@ class CouponUsagesIncrementObserver implements ObserverInterface
         if (!$quote->getCouponCode() && $quote->dataHasChangedFor('coupon_code')) {
             throw new NoSuchEntityException(__("The coupon code isn't valid. Verify the code and try again."));
         }
-        $this->updateCouponUsages->execute($quote, true);
+        ($observer->getOrder()) ? $this->updateCouponUsages->execute($quote, true)
+            : $this->updateCouponUsages->execute($quote, false);
     }
 }
