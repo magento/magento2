b70a763706aa6570fb839f4808faff5bfe2d9b6d
Roger <you@example.com>
2023-12-12 05:48:31 +0000

    update code

--- app/code/Magento/PageCache/Model/App/Request/Http/IdentifierForSave.php
+++ app/code/Magento/PageCache/Model/App/Request/Http/IdentifierForSave.php
@@ -56,7 +56,7 @@ class IdentifierForSave implements IdentifierInterface
      */
     private function reconstructUrl($url)
     {
-        $baseUrl = strtok($url, '?');
+        $baseUrl = strtok((string)$url, '?');
         $query = $this->request->getQuery()->toArray();
         if (!empty($query)) {
             ksort($query);
