a129782f27a01b108edd016db521616b37a48447
Arularasan <glo02433@adobe.com>
2024-01-31 21:05:25 +0530

    ACP2E-2689: No Appropriate error message when website id is wrong in the request  - Checking functional B2B error.

--- app/code/Magento/Catalog/Model/Product/Price/Validation/TierPriceValidator.php
+++ app/code/Magento/Catalog/Model/Product/Price/Validation/TierPriceValidator.php
@@ -376,8 +376,6 @@ class TierPriceValidator implements ResetAfterRequestInterface
                 count($this->productsCacheBySku[$price->getSku()]->getTierPrices()) > 0 &&
                 (int) $this->allWebsitesValue !== $price->getWebsiteId()
             ) {
-                // phpstan:ignore "File has calls static method. (phpStaticMethodCalls)"
-                throw NoSuchEntityException::singleField('website_id', $price->getWebsiteId());
             }
             $this->websiteRepository->getById($price->getWebsiteId());
         } catch (\Magento\Framework\Exception\NoSuchEntityException $e) {
