86b187facc511ffeae4ba842b4263fb740cc8195
Sumesh P <sumesh.p@globallogic.com>
2024-02-06 22:42:28 +0530

    LYNX-308: Revert changes

--- app/code/Magento/SalesRule/Model/Quote/Discount.php
+++ app/code/Magento/SalesRule/Model/Quote/Discount.php
@@ -330,7 +330,7 @@ class Discount extends AbstractTotal
      * @param AddressInterface $address
      * @return void
      */
-    protected function aggregateDiscountPerRule(
+    private function aggregateDiscountPerRule(
         AbstractItem $item,
         AddressInterface $address
     ) {
