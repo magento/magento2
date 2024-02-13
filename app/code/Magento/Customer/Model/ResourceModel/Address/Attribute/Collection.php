3a4fbabd6c747253ba9a651633334f391fab3b40
Thomas Klein <thomasklein876@gmail.com>
2024-02-13 09:33:05 +0100

    Revert changes

--- app/code/Magento/Customer/Model/ResourceModel/Address/Attribute/Collection.php
+++ app/code/Magento/Customer/Model/ResourceModel/Address/Attribute/Collection.php
@@ -3,7 +3,6 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
-declare(strict_types=1);
 
 /**
  * Customer Address EAV additional attribute resource collection
@@ -12,17 +11,6 @@ declare(strict_types=1);
  */
 namespace Magento\Customer\Model\ResourceModel\Address\Attribute;
 
-use Magento\Customer\Model\Address\Config as AddressConfig;
-use Magento\Eav\Model\Config;
-use Magento\Framework\App\ObjectManager;
-use Magento\Framework\Data\Collection\Db\FetchStrategyInterface;
-use Magento\Framework\Data\Collection\EntityFactory;
-use Magento\Framework\DB\Adapter\AdapterInterface;
-use Magento\Framework\Event\ManagerInterface;
-use Magento\Framework\Model\ResourceModel\Db\AbstractDb;
-use Magento\Store\Model\StoreManagerInterface;
-use Psr\Log\LoggerInterface;
-
 class Collection extends \Magento\Customer\Model\ResourceModel\Attribute\Collection
 {
     /**
@@ -30,42 +18,5 @@ class Collection extends \Magento\Customer\Model\ResourceModel\Attribute\Collect
      *
      * @var string
      */
-    protected $_entityTypeCode = 'customer_address';
-
-    private AddressConfig $addressConfig;
-
-    public function __construct(
-        EntityFactory $entityFactory,
-        LoggerInterface $logger,
-        FetchStrategyInterface $fetchStrategy,
-        ManagerInterface $eventManager,
-        Config $eavConfig,
-        StoreManagerInterface $storeManager,
-        AdapterInterface $connection = null,
-        AbstractDb $resource = null,
-        AddressConfig $addressConfig = null
-    ) {
-        parent::__construct(
-            $entityFactory,
-            $logger,
-            $fetchStrategy,
-            $eventManager,
-            $eavConfig,
-            $storeManager,
-            $connection,
-            $resource
-        );
-        $this->addressConfig = $addressConfig ?? ObjectManager::getInstance()->get(AddressConfig::class);
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function getWebsite()
-    {
-        if ($this->_website === null) {
-            $this->_website = $this->addressConfig->getStore()->getWebsite();
-        }
-        return $this->_website;
-    }
+    protected $_entityTypeCode = 'customer_address'; 
 }
