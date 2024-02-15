d5f5201847759a81c9c3f149a8037d170279fe66
Bansi Ravaliya <bra@salecto.in>
2024-02-15 18:53:02 +0530

    remove escapeHtml

--- app/code/Magento/ConfigurableProduct/Ui/DataProvider/Product/Form/Modifier/Data/AssociatedProducts.php
+++ app/code/Magento/ConfigurableProduct/Ui/DataProvider/Product/Form/Modifier/Data/AssociatedProducts.php
@@ -299,7 +299,7 @@ class AssociatedProducts
                             ['id' => $product->getId()]
                         ) . '" target="_blank">' . $this->escaper->escapeHtml($product->getName()) . '</a>',
                         'sku' => $product->getSku(),
-                        'name' => $this->escaper->escapeHtml($product->getName()),
+                        'name' => $product->getName(),
                         'qty' => $this->getProductStockQty($product),
                         'price' => $price,
                         'price_string' => $currency->toCurrency(sprintf("%f", $price)),
@@ -461,6 +461,6 @@ class AssociatedProducts
      */
     protected function getAttributes()
     {
-        return (array)$this->configurableType->getConfigurableAttributesAsArray($this->locator->getProduct());
+        return (array) $this->configurableType->getConfigurableAttributesAsArray($this->locator->getProduct());
     }
 }
