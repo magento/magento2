0289925ab42597df266ef9c0804199df0cf2f11b
Dmytro Voskoboinikov <voskoboi@adobe.com>
2024-02-16 17:47:20 -0600

    AC-10686: [PCI] SRI enabled on payment pages

--- app/code/Magento/Csp/view/frontend/web/js/sri.js
+++ app/code/Magento/Csp/view/frontend/web/js/sri.js
@@ -2,17 +2,11 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
-require([
-    'jquery',
-    'mage/cookies',
-    'mage/storage',
-    'mage/url'
-], function ($, cookies, storage, urlBuilder) {
-    'use strict';
-
-    require.config({
-        onNodeCreated: function (node, config, moduleName, url) {
-
+require.config({
+    onNodeCreated: function (node, config, moduleName, url) {
+        if ('sriHashes' in window && url in window.sriHashes) {
+            node.setAttribute("integrity", window.sriHashes[url])
+            node.setAttribute("crossorigin", "anonymous")
         }
-    });
+    }
 });
