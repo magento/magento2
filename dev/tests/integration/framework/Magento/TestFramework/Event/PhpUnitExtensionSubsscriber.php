ad34f13df049855c389e510e6203af4e350e4eb1
Atul-glo35265 <glo35265@adobe.com>
2024-02-19 20:04:19 +0530

    integration test fixes

--- dev/tests/integration/framework/Magento/TestFramework/Event/PhpUnitExtensionSubsscriber.php
+++ dev/tests/integration/framework/Magento/TestFramework/Event/PhpUnitExtensionSubsscriber.php
@@ -15,23 +15,15 @@ use PHPUnit\Event\Test\PreparationStartedSubscriber;
 final class PhpUnitExtensionSubsscriber implements PreparationStartedSubscriber
 {
     public function notify(\PHPUnit\Event\Test\PreparationStarted $event): void{
-        $className = $event->test()->className();
-        $methodName = $event->test()->methodName();
-        $docComment = (new ReflectionMethod($className, $methodName))->getDocComment();
-
-        $docComment = str_replace("\r\n", "\n", $docComment);
-        $docComment = preg_replace('/\n\s*\*\s?/', "\n", $docComment);
-        $docComment = substr($docComment, 0, -1);
-        $docComment = rtrim($docComment, "\n");
-
-        if (!preg_match('/@magentoDataFixture\s+/', $docComment, $matches, PREG_OFFSET_CAPTURE)) {
-            return;
-        }
-
-        $offset            = strlen($matches[0][0]) + (int) $matches[0][1];
-        $annotationContent = substr($docComment, $offset);
-        $data              = [];
-
-        require $annotationContent;
+        $subscribers[] = new \Magento\TestFramework\Event\Transaction(
+            new \Magento\TestFramework\EventManager(
+                [
+                    new \Magento\TestFramework\Annotation\DbIsolation(),
+                    new \Magento\TestFramework\Annotation\DataFixture(),
+                ]
+            )
+        );
+        $eventManager = new \Magento\TestFramework\EventManager($subscribers);
+        $eventManager->fireEvent('startTest', [new \Magento\AdobeStockAsset\Test\Integration\Model\AssetRepositoryTest($methodName)]);
     }
 }
