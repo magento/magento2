296e9d14aaa9a5cdcbe2fb364f651999ad9de142
Anna Bukatar <abukatar@magento.com>
2024-02-23 15:55:19 -0800

    ACP2E-2841: Payflow creates new transaction each time we click on fetch button on the view transaction screen

--- app/code/Magento/Paypal/Test/Unit/Model/Payflow/TransparentTest.php
+++ app/code/Magento/Paypal/Test/Unit/Model/Payflow/TransparentTest.php
@@ -125,15 +125,21 @@ class TransparentTest extends TestCase
         $gatewayToken = 'gateway_token';
         $this->payment->expects($this->once())->method('getParentTransactionId')->willReturn($parentTransactionId);
         $this->payment->expects($this->exactly($setParentTransactionIdCalls))->method('setParentTransactionId');
-        $this->payment->expects($this->exactly($setAdditionalInformationCalls))->method('setAdditionalInformation')->with(Payflowpro::PNREF, $gatewayToken);
+        $this->payment->expects($this->exactly($setAdditionalInformationCalls))
+            ->method('setAdditionalInformation')
+            ->with(Payflowpro::PNREF, $gatewayToken);
         $this->payment->expects($this->exactly(4))->method('getAdditionalInformation')->withConsecutive(
             ['result_code'],
             [Payflowpro::PNREF],
             [Payflowpro::PNREF],
             [Payflowpro::PNREF],
         )->willReturn(0, '', Payflowpro::PNREF, Payflowpro::PNREF);
-        $this->paymentExtensionAttributes->expects($this->once())->method('getVaultPaymentToken')->willReturn($this->paymentToken);
-        $this->paymentToken->expects($this->exactly($getGatewayTokenCalls))->method('getGatewayToken')->willReturn($gatewayToken);
+        $this->paymentExtensionAttributes->expects($this->once())
+            ->method('getVaultPaymentToken')
+            ->willReturn($this->paymentToken);
+        $this->paymentToken->expects($this->exactly($getGatewayTokenCalls))
+            ->method('getGatewayToken')
+            ->willReturn($gatewayToken);
 
         $this->subject->capture($this->payment, 100);
     }
