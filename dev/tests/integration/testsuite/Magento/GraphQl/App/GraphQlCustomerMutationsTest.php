ab55f38897a17588f0793baced9e62832b2e5d6d
Rajesh Kumar <glo71317@adobe.com>
2024-02-27 16:43:14 +0530

    AC-11010: Integration test failure for userExpiration with PHP 8.3

--- dev/tests/integration/testsuite/Magento/GraphQl/App/GraphQlCustomerMutationsTest.php
+++ dev/tests/integration/testsuite/Magento/GraphQl/App/GraphQlCustomerMutationsTest.php
@@ -32,6 +32,10 @@ class GraphQlCustomerMutationsTest extends \PHPUnit\Framework\TestCase
      */
     protected function setUp(): void
     {
+        if (!class_exists(GraphQlStateDiff::class)) {
+            $this->markTestSkipped('GraphQlStateDiff class is not available on this version of Magento.');
+        }
+
         $this->graphQlStateDiff = new GraphQlStateDiff();
         parent::setUp();
     }
