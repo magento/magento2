8564b4279e56e21ac778832e37135bd560a20e9d
Rajesh Kumar <glo71317@adobe.com>
2024-03-15 17:00:02 +0530

    AC-11633::Unit test failure with PHP 8.3 in Jenkins Build

--- app/code/Magento/Catalog/Test/Unit/Controller/Adminhtml/Product/Initialization/HelperTest.php
+++ app/code/Magento/Catalog/Test/Unit/Controller/Adminhtml/Product/Initialization/HelperTest.php
@@ -151,7 +151,7 @@ class HelperTest extends TestCase
             ->disableOriginalConstructor()
             ->getMockForAbstractClass();
         $productExtensionAttributes = $this->getMockBuilder(ProductExtensionInterface::class)
-            ->addMethods(['getCategoryLinks', 'setCategoryLinks'])
+            ->onlyMethods(['getCategoryLinks', 'setCategoryLinks'])
             ->getMockForAbstractClass();
         $this->productMock->setExtensionAttributes($productExtensionAttributes);
 
@@ -788,6 +788,6 @@ class HelperTest extends TestCase
 
         $reflectionProperty = new \ReflectionProperty(\Magento\Framework\App\ObjectManager::class, '_instance');
         $reflectionProperty->setAccessible(true);
-        $reflectionProperty->setValue($objectManagerMock, null);
+        $reflectionProperty->setValue($objectManagerMock, $objectManagerMock);
     }
 }
