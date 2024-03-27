d822fc9c2f459437caa0ceab4e2c825cea153539
Thomas Klein <thomasklein876@gmail.com>
2024-03-27 22:19:54 +0100

    Remove scope resolver usage

--- app/code/Magento/Backend/App/Area/FrontNameResolver.php
+++ app/code/Magento/Backend/App/Area/FrontNameResolver.php
@@ -121,8 +121,8 @@ class FrontNameResolver implements FrontNameResolverInterface
      */
     public function isHostBackend()
     {
-        if ($this->scopeConfig->getValue(self::XML_PATH_USE_CUSTOM_ADMIN_URL, ScopeInterface::SCOPE_STORE)) {
-            $backendUrl = $this->scopeConfig->getValue(self::XML_PATH_CUSTOM_ADMIN_URL, ScopeInterface::SCOPE_STORE);
+        if ($this->scopeConfig->isSetFlag(self::XML_PATH_USE_CUSTOM_ADMIN_URL)) {
+            $backendUrl = $this->scopeConfig->getValue(self::XML_PATH_CUSTOM_ADMIN_URL);
         } else {
             $backendUrl = $this->config->getValue(Store::XML_PATH_UNSECURE_BASE_URL);
             if ($backendUrl === null) {
