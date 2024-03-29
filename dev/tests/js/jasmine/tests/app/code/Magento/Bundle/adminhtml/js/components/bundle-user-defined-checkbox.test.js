c1a13101f7f983ecff4be7b0869c7c77c0b16659
Arularasan <glo02433@adobe.com>
2024-03-29 15:08:25 +0530

    ACP2E-2909: dynamic-rows.js:658 Uncaught TypeError: dataRecord.slice while editing bundle products  - Added the copyright.

--- dev/tests/js/jasmine/tests/app/code/Magento/Bundle/adminhtml/js/components/bundle-user-defined-checkbox.test.js
+++ dev/tests/js/jasmine/tests/app/code/Magento/Bundle/adminhtml/js/components/bundle-user-defined-checkbox.test.js
@@ -1,6 +1,20 @@
-/**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
+/************************************************************************
+ *
+ *  ADOBE CONFIDENTIAL
+ *  ___________________
+ *
+ *  Copyright 2024 Adobe
+ *  All Rights Reserved.
+ *
+ *  NOTICE: All information contained herein is, and remains
+ *  the property of Adobe and its suppliers, if any. The intellectual
+ *  and technical concepts contained herein are proprietary to Adobe
+ *  and its suppliers and are protected by all applicable intellectual
+ *  property laws, including trade secret and copyright laws.
+ *  Dissemination of this information or reproduction of this material
+ *  is strictly forbidden unless prior written permission is obtained
+ *  from Adobe.
+ *  ************************************************************************
  */
 
 /*eslint max-nested-callbacks: 0*/
