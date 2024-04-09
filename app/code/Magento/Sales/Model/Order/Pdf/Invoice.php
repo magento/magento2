6ce9366e63d5f52414a70d48017f266f98bd9fb5
Dhruv Basan <dba@salecto.in>
2024-04-09 16:52:52 +0530

    Shipping price showing diffrent in printed pdf

--- app/code/Magento/Sales/Model/Order/Pdf/Invoice.php
+++ app/code/Magento/Sales/Model/Order/Pdf/Invoice.php
@@ -34,6 +34,7 @@ class Invoice extends AbstractPdf
      * @param \Magento\Framework\Stdlib\DateTime\TimezoneInterface $localeDate
      * @param \Magento\Framework\Translate\Inline\StateInterface $inlineTranslation
      * @param \Magento\Sales\Model\Order\Address\Renderer $addressRenderer
+     * @param \Magento\Tax\Helper\Data $taxHelper
      * @param \Magento\Store\Model\StoreManagerInterface $storeManager
      * @param \Magento\Store\Model\App\Emulation $appEmulation
      * @param array $data
@@ -51,6 +52,7 @@ class Invoice extends AbstractPdf
         \Magento\Framework\Stdlib\DateTime\TimezoneInterface $localeDate,
         \Magento\Framework\Translate\Inline\StateInterface $inlineTranslation,
         \Magento\Sales\Model\Order\Address\Renderer $addressRenderer,
+        \Magento\Tax\Helper\Data $taxHelper,
         \Magento\Store\Model\StoreManagerInterface $storeManager,
         \Magento\Store\Model\App\Emulation $appEmulation,
         array $data = []
@@ -68,6 +70,7 @@ class Invoice extends AbstractPdf
             $localeDate,
             $inlineTranslation,
             $addressRenderer,
+            $taxHelper,
             $data
         );
     }
