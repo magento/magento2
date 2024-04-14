d2787d349496cfa2b70b8a43641d4937cdccfbba
glo5363 <glo05363@adobe.com>
2024-04-14 19:59:33 +0530

    #AC-11388-Verify Deleting Folder Deletes S3 and Local File Storage Directories- updating league library made sure the s3 tests are passing

--- app/code/Magento/AwsS3/Test/Mftf/Helper/S3FileAssertions.php
+++ app/code/Magento/AwsS3/Test/Mftf/Helper/S3FileAssertions.php
@@ -65,6 +65,7 @@ class S3FileAssertions extends Helper
         $s3Driver = new AwsS3($adapter, new MockTestLogger(), $objectUrl, $metadataProvider);
 
         $this->driver = $s3Driver;
+        $this->adapter = $adapter;
     }
 
     /**
@@ -196,10 +197,11 @@ class S3FileAssertions extends Helper
      * @return void
      *
      * @throws \Magento\Framework\Exception\FileSystemException
+     * @throws \League\Flysystem\FilesystemException
      */
     public function assertDirectoryExists($path, $message = ''): void
     {
-        $this->assertTrue($this->driver->isDirectory($path), "Failed asserting $path exists. " . $message);
+        $this->assertTrue($this->adapter->directoryExists($path), "Failed asserting $path exists. " . $message);
     }
 
     /**
