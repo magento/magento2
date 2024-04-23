91995dc5c65a7d6f4fb90e5ffb530de20a0823bd
Sreeni A <sreeniavasulu@gmail.com>
2024-04-23 22:59:49 +0530

    AC-10815 - API Input validation

--- lib/internal/Magento/Framework/Webapi/ServiceInputProcessor.php
+++ lib/internal/Magento/Framework/Webapi/ServiceInputProcessor.php
@@ -280,7 +280,9 @@ class ServiceInputProcessor implements ServicePayloadConverterInterface, ResetAf
         $className = (string) $className;
         if (is_subclass_of($className, \SimpleXMLElement::class)
             || is_subclass_of($className, \DOMElement::class)) {
-            return null;
+            throw new SerializationException(
+                new Phrase('Invalid data type.')
+            );
         }
         $class = new ClassReflection($className);
         if (is_subclass_of($className, self::EXTENSION_ATTRIBUTES_TYPE)) {
