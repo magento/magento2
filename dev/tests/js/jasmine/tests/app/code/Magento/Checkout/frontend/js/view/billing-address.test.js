a9c4e93a944655034ea582ef025dddb19fa9f129
Chhandak.Barua <chhandak.barua@BLR1-LMC-N73490.local>
2024-06-18 19:46:08 +0530

    ACP2E-3037: Checkout page Validation message incorrect.

--- dev/tests/js/jasmine/tests/app/code/Magento/Checkout/frontend/js/view/billing-address.test.js
+++ dev/tests/js/jasmine/tests/app/code/Magento/Checkout/frontend/js/view/billing-address.test.js
@@ -104,5 +104,13 @@ define([
                 expect(checkoutData.setNewCustomerBillingAddress).toHaveBeenCalledWith(lastSelectedBillingAddress);
             });
         });
+
+        describe('"updateAddress" method', function () {
+            it('should call updateAddresses(true) when form is valid', function () {
+                spyOn(billingAddress, 'updateAddresses');
+                billingAddress.updateAddress();
+                expect(billingAddress.updateAddresses).toHaveBeenCalledWith(true);
+            });
+        });
     });
 });
