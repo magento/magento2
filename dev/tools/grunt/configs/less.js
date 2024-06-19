a53f8f111aad6cd77b63f96260d973df5186f074
Pieter Hoste <hoste.pieter@gmail.com>
2024-06-19 08:23:33 +0200

    Attempt to fix the static test failures.

--- dev/tools/grunt/configs/less.js
+++ dev/tools/grunt/configs/less.js
@@ -5,46 +5,44 @@
 
 'use strict';
 
-var combo  = require('./combo'),
-    themes = require('../tools/files-router').get('themes'),
-    _      = require('underscore');
+var combo        = require('./combo'),
+    themes       = require('../tools/files-router').get('themes'),
+    _            = require('underscore'),
+    themeOptions = {},
+    lessOptions  = {
+        options: {
+            sourceMap: true,
+            strictImports: false,
+            sourceMapRootpath: '/',
+            sourceMapBasepath: function () {
+                this.sourceMapURL = this.sourceMapFilename.substr(this.sourceMapFilename.lastIndexOf('/') + 1);
+                return 'pub/';
+            },
+            dumpLineNumbers: false, // use 'comments' instead false to output line comments for source
+            ieCompat: false
+        },
+        setup: {
+            files: {
+                '<%= path.css.setup %>/setup.css': '<%= path.less.setup %>/_setup.less'
+            }
+        },
+        updater: {
+            files: {
+                '<%= path.css.updater %>/updater.css': '<%= path.less.setup %>/_setup.less'
+            }
+        },
+        documentation: {
+            files: {
+                '<%= path.doc %>/docs.css': '<%= path.doc %>/source/docs.less'
+            }
+        }
+    };
 
-var themeOptions = {};
-
-_.each(themes, function(theme, name) {
+_.each(themes, function (theme, name) {
     themeOptions[name] = {
         files: combo.lessFiles(name)
     };
 });
-
-var lessOptions = {
-    options: {
-        sourceMap: true,
-        strictImports: false,
-        sourceMapRootpath: '/',
-        sourceMapBasepath: function (f) {
-            this.sourceMapURL = this.sourceMapFilename.substr(this.sourceMapFilename.lastIndexOf('/') + 1);
-            return 'pub/';
-        },
-        dumpLineNumbers: false, // use 'comments' instead false to output line comments for source
-        ieCompat: false
-    },
-    setup: {
-        files: {
-            '<%= path.css.setup %>/setup.css': '<%= path.less.setup %>/_setup.less'
-        }
-    },
-    updater: {
-        files: {
-            '<%= path.css.updater %>/updater.css': '<%= path.less.setup %>/_setup.less'
-        }
-    },
-    documentation: {
-        files: {
-            '<%= path.doc %>/docs.css': '<%= path.doc %>/source/docs.less'
-        }
-    }
-};
 
 /**
  * Compiles Less to CSS and generates necessary files if requested.
