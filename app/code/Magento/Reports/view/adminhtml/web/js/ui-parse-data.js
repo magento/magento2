1bdb37bafcea1935c627df86f987295386cff667
anujnehra <glo16746@adobe.com>
2024-06-20 10:01:16 +0530

    ACP2E-3080: Admin Ordered Products Report date range visibility issue.

--- app/code/Magento/Reports/view/adminhtml/web/js/ui-parse-data.js
+++ app/code/Magento/Reports/view/adminhtml/web/js/ui-parse-data.js
@@ -16,7 +16,7 @@
 
 define([
     'jquery',
-    'jquery-ui-modules/datepicker',
+    'jquery-ui-modules/datepicker'
 ], function ($) {
     'use strict';
 
@@ -31,24 +31,24 @@ define([
      * @private
      */
     $.datepicker.parseDate = function (format, value, settings) {
-        if ( format == null || value == null ) {
-            throw "Invalid arguments";
+        if (format == null || value == null) {
+            throw 'Invalid arguments';
         }
 
-        value = ( typeof value === "object" ? value.toString() : value + "" );
-        if ( value === "" ) {
+        value =  typeof value === 'object' ? value.toString() : value + '';
+        if (value === '') {
             return null;
         }
 
-        var iFormat, dim, extra,
+        let iFormat, dim, extra,
             iValue = 0,
-            shortYearCutoffTemp = ( settings ? settings.shortYearCutoff : null ) || this._defaults.shortYearCutoff,
-            shortYearCutoff = ( typeof shortYearCutoffTemp !== "string" ? shortYearCutoffTemp :
-                new Date().getFullYear() % 100 + parseInt( shortYearCutoffTemp, 10 ) ),
-            dayNamesShort = ( settings ? settings.dayNamesShort : null ) || this._defaults.dayNamesShort,
-            dayNames = ( settings ? settings.dayNames : null ) || this._defaults.dayNames,
-            monthNamesShort = ( settings ? settings.monthNamesShort : null ) || this._defaults.monthNamesShort,
-            monthNames = ( settings ? settings.monthNames : null ) || this._defaults.monthNames,
+            shortYearCutoffTemp = (settings ? settings.shortYearCutoff : null) || this._defaults.shortYearCutoff,
+            shortYearCutoff = typeof shortYearCutoffTemp !== 'string' ? shortYearCutoffTemp :
+                new Date().getFullYear() % 100 + parseInt(shortYearCutoffTemp, 10),
+            dayNamesShort = (settings ? settings.dayNamesShort : null) || this._defaults.dayNamesShort,
+            dayNames = (settings ? settings.dayNames : null) || this._defaults.dayNames,
+            monthNamesShort = (settings ? settings.monthNamesShort : null) || this._defaults.monthNamesShort,
+            monthNames = (settings ? settings.monthNames : null) || this._defaults.monthNames,
             year = -1,
             month = -1,
             day = -1,
@@ -57,143 +57,149 @@ define([
             date,
 
             // Check whether a format character is doubled
-            lookAhead = function ( match ) {
-                var matches = ( iFormat + 1 < format.length && format.charAt( iFormat + 1 ) === match );
-                if ( matches ) {
+            lookAhead = function (match) {
+                const matches = iFormat + 1 < format.length && format.charAt(iFormat + 1) === match;
+
+                if (matches) {
                     iFormat++;
                 }
                 return matches;
             },
 
             // Extract a number from the string value
-            getNumber = function ( match ) {
-                var isDoubled = lookAhead( match ),
-                    size = ( match === "@" ? 14 : ( match === "!" ? 20 :
-                        ( match === "y" && isDoubled ? 4 : ( match === "o" ? 3 : 2 ) ) ) ),
+            getNumber = function (match) {
+                const isDoubled = lookAhead(match),
+                    size = match === '@' ? 14 : match === '!' ? 20 :
+                        match === 'y' && isDoubled ? 4 : match === 'o' ? 3 : 2,
                     // v1.9.2 RegExp to fix selection of dates
                     digits = new RegExp('^\\d{1,' + size + '}'),
-                    num = value.substring( iValue ).match( digits );
-                if ( !num ) {
-                    throw "Missing number at position " + iValue;
+                    num = value.substring(iValue).match(digits);
+
+                if (!num) {
+                    throw 'Missing number at position ' + iValue;
                 }
-                iValue += num[ 0 ].length;
-                return parseInt( num[ 0 ], 10 );
+                iValue += num[0].length;
+                return parseInt(num[0], 10);
             },
 
             // Extract a name from the string value and convert to an index
-            getName = function ( match, shortNames, longNames ) {
+            getName = function (match, shortNames, longNames) {
                 var index = -1,
-                    names = $.map( lookAhead( match ) ? longNames : shortNames, function ( v, k ) {
-                        return [ [ k, v ] ];
-                    } ).sort( function ( a, b ) {
-                        return -( a[ 1 ].length - b[ 1 ].length );
-                    } );
+                    names = $.map(lookAhead(match) ? longNames : shortNames, function (v, k) {
+                        return [[k, v]];
+                    }).sort(function (a, b) {
+                        return -(a[1].length - b[1].length);
+                    });
 
-                $.each( names, function ( i, pair ) {
-                    var name = pair[ 1 ];
-                    if ( value.substr( iValue, name.length ).toLowerCase() === name.toLowerCase() ) {
-                        index = pair[ 0 ];
+                $.each(names, function (i, pair) {
+                    const name = pair[1];
+
+                    if (value.substr(iValue, name.length).toLowerCase() === name.toLowerCase()) {
+                        index = pair[0];
                         iValue += name.length;
                         return false;
                     }
-                } );
-                if ( index !== -1 ) {
+                });
+                if (index !== -1) {
                     return index + 1;
-                } else {
-                    throw "Unknown name at position " + iValue;
                 }
+                throw 'Unknown name at position ' + iValue;
             },
 
             // Confirm that a literal character matches the string value
             checkLiteral = function () {
-                if ( value.charAt( iValue ) !== format.charAt( iFormat ) ) {
-                    throw "Unexpected literal at position " + iValue;
+                if (value.charAt(iValue) !== format.charAt(iFormat)) {
+                    throw 'Unexpected literal at position ' + iValue;
                 }
                 iValue++;
             };
 
         for (iFormat = 0; iFormat < format.length; iFormat++) {
-            if ( literal ) {
-                if ( format.charAt( iFormat ) === "'" && !lookAhead( "'" ) ) {
+            if (literal) {
+                // eslint-disable-next-line max-depth
+                if (format.charAt(iFormat) === '\'' && !lookAhead('\'')) {
                     literal = false;
                 } else {
                     checkLiteral();
                 }
             } else {
-                switch ( format.charAt( iFormat ) ) {
-                    case "d":
-                        day = getNumber( "d" );
-                        break;
-                    case "D":
-                        getName( "D", dayNamesShort, dayNames );
-                        break;
-                    case "o":
-                        doy = getNumber( "o" );
-                        break;
-                    case "m":
-                        month = getNumber( "m" );
-                        break;
-                    case "M":
-                        month = getName( "M", monthNamesShort, monthNames );
-                        break;
-                    case "y":
-                        year = getNumber( "y" );
-                        break;
-                    case "@":
-                        date = new Date( getNumber( "@" ) );
-                        year = date.getFullYear();
-                        month = date.getMonth() + 1;
-                        day = date.getDate();
-                        break;
-                    case "!":
-                        date = new Date( ( getNumber( "!" ) - this._ticksTo1970 ) / 10000 );
-                        year = date.getFullYear();
-                        month = date.getMonth() + 1;
-                        day = date.getDate();
-                        break;
-                    case "'":
-                        if ( lookAhead( "'" ) ) {
-                            checkLiteral();
-                        } else {
-                            literal = true;
-                        }
-                        break;
-                    default:
+                // eslint-disable-next-line max-depth
+                switch (format.charAt(iFormat)) {
+                case 'd':
+                    day = getNumber('d');
+                    break;
+                case 'D':
+                    getName('D', dayNamesShort, dayNames);
+                    break;
+                case 'o':
+                    doy = getNumber('o');
+                    break;
+                case 'm':
+                    month = getNumber('m');
+                    break;
+                case 'M':
+                    month = getName('M', monthNamesShort, monthNames);
+                    break;
+                case 'y':
+                    year = getNumber('y');
+                    break;
+                case '@':
+                    date = new Date(getNumber('@'));
+                    year = date.getFullYear();
+                    month = date.getMonth() + 1;
+                    day = date.getDate();
+                    break;
+                case '!':
+                    date = new Date((getNumber('!') - this._ticksTo1970) / 10000);
+                    year = date.getFullYear();
+                    month = date.getMonth() + 1;
+                    day = date.getDate();
+                    break;
+                case '\'':
+                    // eslint-disable-next-line max-depth
+                    if (lookAhead('\'')) {
                         checkLiteral();
+                    } else {
+                        literal = true;
+                    }
+                    break;
+                default:
+                    checkLiteral();
                 }
             }
         }
 
-        if ( iValue < value.length ) {
-            extra = value.substr( iValue );
-            if ( !/^\s+/.test( extra ) ) {
-                throw "Extra/unparsed characters found in date: " + extra;
+        if (iValue < value.length) {
+            extra = value.substr(iValue);
+            if (!/^\s+/.test(extra)) {
+                throw 'Extra/unparsed characters found in date: ' + extra;
             }
         }
 
-        if ( year === -1 ) {
+        if (year === -1) {
             year = new Date().getFullYear();
-        } else if ( year < 100 ) {
+        } else if (year < 100) {
             year += new Date().getFullYear() - new Date().getFullYear() % 100 +
-                ( year <= shortYearCutoff ? 0 : -100 );
+                (year <= shortYearCutoff ? 0 : -100);
         }
 
-        if ( doy > -1 ) {
+        if (doy > -1) {
             month = 1;
             day = doy;
             do {
-                dim = this._getDaysInMonth( year, month - 1 );
-                if ( day <= dim ) {
+                dim = this._getDaysInMonth(year, month - 1);
+                if (day <= dim) {
                     break;
                 }
                 month++;
                 day -= dim;
-            } while ( true );
+                // eslint-disable-next-line no-constant-condition
+            } while (true);
         }
 
-        date = this._daylightSavingAdjust( new Date( year, month - 1, day ) );
-        if ( date.getFullYear() !== year || date.getMonth() + 1 !== month || date.getDate() !== day ) {
-            throw "Invalid date"; // E.g. 31/02/00
+        date = this._daylightSavingAdjust(new Date(year, month - 1, day));
+        if (date.getFullYear() !== year || date.getMonth() + 1 !== month || date.getDate() !== day) {
+            throw 'Invalid date'; // E.g. 31/02/00
         }
         return date;
     };
