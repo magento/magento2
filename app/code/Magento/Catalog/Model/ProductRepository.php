f62f1ee0cd1239253980a9f8a493e1962c68cc08
Alexandra Zota <zota@adobe.com>
2024-08-07 15:48:41 +0300

    ACP2E-3236: check if both product id and sku are missing before saving the product;  added translatable text; added unit test

--- app/code/Magento/Catalog/Model/ProductRepository.php
+++ app/code/Magento/Catalog/Model/ProductRepository.php
@@ -526,6 +526,13 @@ class ProductRepository implements \Magento\Catalog\Api\ProductRepositoryInterfa
         $assignToCategories = false;
         $tierPrices = $product->getData('tier_price');
         $productDataToChange = $product->getData();
+
+        if (!$product->getSku() && !$product->getId()) {
+            throw new \Magento\Framework\Exception\CouldNotSaveException(
+                __('Invalid product data: %1', 'The SKU or ID of the product must be set before saving the product.')
+            );
+        }
+
         try {
             $existingProduct = $product->getId() ?
                 $this->getById($product->getId()) :
