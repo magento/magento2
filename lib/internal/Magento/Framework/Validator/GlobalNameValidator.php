2d92ef27cbf463ad5c79f61a020b74a3eb59d357
in-session <info@in-session.de>
2024-08-09 11:21:54 +0200

    GlobalNameValidator.php

---
+++ lib/internal/Magento/Framework/Validator/GlobalNameValidator.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\Framework\Validator;
+
+class GlobalNameValidator
+{
+    /**
+     * Regular expression pattern for validating names.
+     */
+    public const PATTERN_NAME = '/(?:[\p{L}\p{M}\,\-\_\.\'’`&\s\d]){1,255}+/u';
+
+    /**
+     * Validate a name string.
+     *
+     * @param string|null $nameValue
+     * @return bool
+     */
+    public static function isValidName(?string $nameValue): bool
+    {
+        if ($nameValue !== null) {
+            if (preg_match(self::PATTERN_NAME, $nameValue, $matches)) {
+                return $matches[0] === $nameValue;
+            }
+        }
+        return false;
+    }
+}
