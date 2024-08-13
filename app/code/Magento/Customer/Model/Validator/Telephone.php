0e0977e7f86618dfc4cabae16e1d49679df8d272
in-session <info@in-session.de>
2024-08-13 07:58:42 +0200

    Update Telephone.php

--- app/code/Magento/Customer/Model/Validator/Telephone.php
+++ app/code/Magento/Customer/Model/Validator/Telephone.php
@@ -9,22 +9,13 @@ namespace Magento\Customer\Model\Validator;
 
 use Magento\Customer\Model\Customer;
 use Magento\Framework\Validator\AbstractValidator;
+use Magento\Framework\Validator\GlobalPhoneValidation;
 
 /**
  * Customer telephone fields validator.
  */
 class Telephone extends AbstractValidator
 {
-    /**
-     * Allowed char:
-     *
-     * \() :Matches open and close parentheses
-     * \+: Matches the plus sign.
-     * \-: Matches the hyphen.
-     * \d: Digits (0-9).
-     */
-    private const PATTERN_TELEPHONE = '/(?:[\d\s\+\-\()]{1,20})/u';
-    
     /**
      * Validate telephone fields.
      *
@@ -33,29 +24,12 @@ class Telephone extends AbstractValidator
      */
     public function isValid($customer)
     {
-        if (!$this->isValidTelephone($customer->getTelephone())) {
+        if (!GlobalPhoneValidation::isValidPhone($customer->getTelephone())) {
             parent::_addMessages([[
-                'telephone' => "Invalid Phone Number. Please use 0-9, +, -, (, ) and space."
+                'telephone' => __('Invalid Phone Number. Please use 0-9, +, -, (, ) and space.')
             ]]);
         }
 
         return count($this->_messages) == 0;
-    }
-
-    /**
-     * Check if telephone field is valid.
-     *
-     * @param string|null $telephoneValue
-     * @return bool
-     */
-    private function isValidTelephone($telephoneValue)
-    {
-        if ($telephoneValue != null) {
-            if (preg_match(self::PATTERN_TELEPHONE, (string) $telephoneValue, $matches)) {
-                return $matches[0] == $telephoneValue;
-            }
-        }
-
-        return true;
     }
 }
