33ea0665a128d47a474bc90de9e46e4190e3bf8a
Nishant Rana <glo04412@adobe.com>
2024-08-14 17:50:33 +0530

    AC-12481: JS code optimised

--- app/code/Magento/CatalogSearch/view/adminhtml/web/js/search-engine-comment.js
+++ app/code/Magento/CatalogSearch/view/adminhtml/web/js/search-engine-comment.js
@@ -11,18 +11,21 @@ require([
 
     $(function () {
         const engineField = $('#catalog_search_engine'),
-            commentContainer = $('#row_catalog_search_engine p');
+            commentContainer = $('#row_catalog_search_engine p'),
+            defaultText = $.mage.__('If not specified, Default Search Engine will be used.'),
+            unsupportedText = $.mage.__('This search engine option is no longer supported by Adobe. ' +
+                'It is recommended to use OpenSearch as a search engine instead.'),
+            updateCommentText = () => {
+                const engineValue = engineField.val(),
+                    newCommentText = ['elasticsearch7', 'elasticsearch8'].includes(engineValue) ?
+                        unsupportedText : defaultText;
 
-        engineField.change(() => {
-            const engineValue = engineField.val();
-            let commentText = $.mage.__('If not specified, Default Search Engine will be used.');
+                if (commentContainer.text() !== newCommentText) {
+                    commentContainer.text(newCommentText);
+                }
+            };
 
-            if (['elasticsearch7', 'elasticsearch8'].includes(engineValue)) {
-                commentText = $.mage.__('This search engine option is no longer supported by Adobe. ' +
-                    'It is recommended to use OpenSearch as a search engine instead.');
-            }
-
-            commentContainer.text(commentText);
-        });
+        engineField.change(updateCommentText);
+        updateCommentText();
     });
 });
