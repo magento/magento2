b35a8544152e9e49c3062744a71bb6d9c3029b0a
in-session <info@in-session.de>
2024-08-23 08:13:58 +0200

    Update NameValidationRule.php

--- app/code/Magento/Quote/Model/ValidationRules/NameValidationRule.php
+++ app/code/Magento/Quote/Model/ValidationRules/NameValidationRule.php
@@ -1,8 +1,4 @@
 <?php
-/**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
- */
 declare(strict_types=1);
 
 namespace Magento\Quote\Model\ValidationRules;
@@ -10,10 +6,12 @@ namespace Magento\Quote\Model\ValidationRules;
 use Magento\Framework\Validation\ValidationResultFactory;
 use Magento\Quote\Model\Quote;
 use Magento\Framework\Validator\GlobalNameValidator;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Model\ScopeInterface;
 
 /**
  * Class NameValidationRule
- * Validates the first name and last name fields in a quote.
+ * Validates the first name, middle name, last name, prefix, and suffix fields in a quote.
  */
 class NameValidationRule implements QuoteValidationRuleInterface
 {
@@ -23,17 +21,34 @@ class NameValidationRule implements QuoteValidationRuleInterface
     private $validationResultFactory;
 
     /**
+     * @var GlobalNameValidator
+     */
+    private $nameValidator;
+
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
      * Constructor.
      *
      * @param ValidationResultFactory $validationResultFactory
+     * @param GlobalNameValidator $nameValidator
+     * @param ScopeConfigInterface $scopeConfig
      */
-    public function __construct(ValidationResultFactory $validationResultFactory)
-    {
+    public function __construct(
+        ValidationResultFactory $validationResultFactory,
+        GlobalNameValidator $nameValidator,
+        ScopeConfigInterface $scopeConfig
+    ) {
         $this->validationResultFactory = $validationResultFactory;
+        $this->nameValidator = $nameValidator;
+        $this->scopeConfig = $scopeConfig;
     }
 
     /**
-     * Validate the first name, middle name, and last name in the quote.
+     * Validate the first name, middle name, last name, prefix, and suffix in the quote.
      *
      * @param Quote $quote
      * @return array
@@ -41,20 +56,35 @@ class NameValidationRule implements QuoteValidationRuleInterface
     public function validate(Quote $quote): array
     {
         $validationErrors = [];
-        $firstName = $quote->getCustomerFirstname();
-        $middleName = $quote->getCustomerMiddlename();
-        $lastName = $quote->getCustomerLastname();
 
-        if (!GlobalNameValidator::isValidName($firstName)) {
-            $validationErrors[] = __('First Name is not valid');
-        }
+        $isRegexEnabled = $this->scopeConfig->isSetFlag(
+            'system/security/security_regex_enabled',
+            ScopeInterface::SCOPE_STORE
+        );
 
-        if (!GlobalNameValidator::isValidName($middleName)) {
-            $validationErrors[] = __('Middle Name is not valid');
-        }
+        if ($isRegexEnabled) {
+            $firstName = $quote->getCustomerFirstname();
+            $middleName = $quote->getCustomerMiddlename();
+            $lastName = $quote->getCustomerLastname();
+            $customerPrefix = $quote->getCustomerPrefix();
+            $customerSuffix = $quote->getCustomerSuffix();
 
-        if (!GlobalNameValidator::isValidName($lastName)) {
-            $validationErrors[] = __('Last Name is not valid');
+            // Validate each name-related field
+            if (!GlobalNameValidator::isValidName($firstName)) {
+                $validationErrors[] = __('First Name is not valid');
+            }
+            if (!GlobalNameValidator::isValidName($middleName)) {
+                $validationErrors[] = __('Middle Name is not valid');
+            }
+            if (!GlobalNameValidator::isValidName($lastName)) {
+                $validationErrors[] = __('Last Name is not valid');
+            }
+            if (!GlobalNameValidator::isValidName($customerPrefix)) {
+                $validationErrors[] = __('Prefix is not valid');
+            }
+            if (!GlobalNameValidator::isValidName($customerSuffix)) {
+                $validationErrors[] = __('Suffix is not valid');
+            }
         }
 
         return [$this->validationResultFactory->create(['errors' => $validationErrors])];
