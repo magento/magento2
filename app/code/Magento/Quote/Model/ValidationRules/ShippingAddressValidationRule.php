92825bf5e404ea4e43e7de72a3819e454c6e13b2
in-session <info@in-session.de>
2024-08-23 08:16:52 +0200

    Update ShippingAddressValidationRule.php

--- app/code/Magento/Quote/Model/ValidationRules/ShippingAddressValidationRule.php
+++ app/code/Magento/Quote/Model/ValidationRules/ShippingAddressValidationRule.php
@@ -9,6 +9,9 @@ namespace Magento\Quote\Model\ValidationRules;
 
 use Magento\Framework\Validation\ValidationResultFactory;
 use Magento\Quote\Model\Quote;
+use Magento\Framework\Validator\GlobalForbiddenPatterns;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Model\ScopeInterface;
 
 /**
  * @inheritdoc
@@ -26,14 +29,30 @@ class ShippingAddressValidationRule implements QuoteValidationRuleInterface
     private $validationResultFactory;
 
     /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
+     * @var GlobalForbiddenPatterns
+     */
+    private $forbiddenPatternsValidator;
+
+    /**
      * @param ValidationResultFactory $validationResultFactory
+     * @param ScopeConfigInterface $scopeConfig
+     * @param GlobalForbiddenPatterns $forbiddenPatternsValidator
      * @param string $generalMessage
      */
     public function __construct(
         ValidationResultFactory $validationResultFactory,
+        ScopeConfigInterface $scopeConfig,
+        GlobalForbiddenPatterns $forbiddenPatternsValidator,
         string $generalMessage = ''
     ) {
         $this->validationResultFactory = $validationResultFactory;
+        $this->scopeConfig = $scopeConfig;
+        $this->forbiddenPatternsValidator = $forbiddenPatternsValidator;
         $this->generalMessage = $generalMessage;
     }
 
@@ -47,12 +66,29 @@ class ShippingAddressValidationRule implements QuoteValidationRuleInterface
         if (!$quote->isVirtual()) {
             $shippingAddress = $quote->getShippingAddress();
             $shippingAddress->setStoreId($quote->getStoreId());
+
+            // Validate the shipping address
             $validationResult = $shippingAddress->validate();
             if ($validationResult !== true) {
                 $validationErrors = [__($this->generalMessage)];
             }
             if (is_array($validationResult)) {
                 $validationErrors = array_merge($validationErrors, $validationResult);
+            }
+
+            // Check if regex validation is enabled
+            $isRegexEnabled = $this->scopeConfig->isSetFlag(
+                'system/security/security_regex_enabled',
+                ScopeInterface::SCOPE_STORE
+            );
+
+            if ($isRegexEnabled) {
+                // Validate shipping address fields against forbidden patterns
+                foreach ($shippingAddress->getData() as $key => $value) {
+                    if (is_string($value) && !$this->forbiddenPatternsValidator->isValid($value)) {
+                        $validationErrors[] = __("Field %1 contains invalid characters.", $key);
+                    }
+                }
             }
         }
 
