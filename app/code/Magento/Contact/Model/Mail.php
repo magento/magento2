7486983db034a28d90958289e860e302c7ef248b
in-session <info@in-session.de>
2024-08-24 13:19:14 +0200

    Update Mail.php

--- app/code/Magento/Contact/Model/Mail.php
+++ app/code/Magento/Contact/Model/Mail.php
@@ -10,6 +10,10 @@ use Magento\Framework\Translate\Inline\StateInterface;
 use Magento\Store\Model\StoreManagerInterface;
 use Magento\Framework\App\ObjectManager;
 use Magento\Framework\App\Area;
+use Magento\Framework\Validator\GlobalForbiddenPatterns;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Store\Model\ScopeInterface;
 
 class Mail implements MailInterface
 {
@@ -34,23 +38,39 @@ class Mail implements MailInterface
     private $storeManager;
 
     /**
+     * @var GlobalForbiddenPatterns
+     */
+    private $forbiddenPatternsValidator;
+
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
      * Initialize dependencies.
      *
      * @param ConfigInterface $contactsConfig
      * @param TransportBuilder $transportBuilder
      * @param StateInterface $inlineTranslation
      * @param StoreManagerInterface|null $storeManager
+     * @param GlobalForbiddenPatterns $forbiddenPatternsValidator
+     * @param ScopeConfigInterface $scopeConfig
      */
     public function __construct(
         ConfigInterface $contactsConfig,
         TransportBuilder $transportBuilder,
         StateInterface $inlineTranslation,
-        StoreManagerInterface $storeManager = null
+        StoreManagerInterface $storeManager = null,
+        GlobalForbiddenPatterns $forbiddenPatternsValidator,
+        ScopeConfigInterface $scopeConfig
     ) {
         $this->contactsConfig = $contactsConfig;
         $this->transportBuilder = $transportBuilder;
         $this->inlineTranslation = $inlineTranslation;
         $this->storeManager = $storeManager ?: ObjectManager::getInstance()->get(StoreManagerInterface::class);
+        $this->forbiddenPatternsValidator = $forbiddenPatternsValidator;
+        $this->scopeConfig = $scopeConfig;
     }
 
     /**
@@ -59,9 +79,27 @@ class Mail implements MailInterface
      * @param string $replyTo
      * @param array $variables
      * @return void
+     * @throws LocalizedException
      */
     public function send($replyTo, array $variables)
     {
+        // Check if regex validation is enabled
+        $isRegexEnabled = $this->scopeConfig->isSetFlag(
+            GlobalForbiddenPatterns::XML_PATH_SECURITY_REGEX_ENABLED,
+            ScopeInterface::SCOPE_STORE
+        );
+
+        // Perform regex validation only if it's enabled
+        if ($isRegexEnabled) {
+            foreach ($variables['data'] as $key => $value) {
+                if (is_string($value) && !$this->forbiddenPatternsValidator->isValid($value)) {
+                    throw new LocalizedException(
+                        __("Field %1 contains invalid characters.", $key)
+                    );
+                }
+            }
+        }
+
         /** @see \Magento\Contact\Controller\Index\Post::validatedParams() */
         $replyToName = !empty($variables['data']['name']) ? $variables['data']['name'] : null;
 
