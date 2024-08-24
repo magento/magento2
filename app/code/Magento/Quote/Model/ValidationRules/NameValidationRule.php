0290b52110bdee63880f4abb37d61dafb742187b
in-session <info@in-session.de>
2024-08-24 10:47:56 +0200

    Update NameValidationRule.php

--- app/code/Magento/Quote/Model/ValidationRules/NameValidationRule.php
+++ app/code/Magento/Quote/Model/ValidationRules/NameValidationRule.php
@@ -5,6 +5,7 @@ namespace Magento\Quote\Model\ValidationRules;
 
 use Magento\Framework\Validation\ValidationResultFactory;
 use Magento\Quote\Model\Quote;
+use Magento\Framework\Validator\GlobalForbiddenPatterns;
 use Magento\Framework\Validator\GlobalNameValidator;
 use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Store\Model\ScopeInterface;
@@ -26,6 +27,11 @@ class NameValidationRule implements QuoteValidationRuleInterface
     private $nameValidator;
 
     /**
+     * @var GlobalForbiddenPatterns
+     */
+    private $forbiddenPatternsValidator;
+
+    /**
      * @var ScopeConfigInterface
      */
     private $scopeConfig;
@@ -35,15 +41,18 @@ class NameValidationRule implements QuoteValidationRuleInterface
      *
      * @param ValidationResultFactory $validationResultFactory
      * @param GlobalNameValidator $nameValidator
+     * @param GlobalForbiddenPatterns $forbiddenPatternsValidator
      * @param ScopeConfigInterface $scopeConfig
      */
     public function __construct(
         ValidationResultFactory $validationResultFactory,
         GlobalNameValidator $nameValidator,
+        GlobalForbiddenPatterns $forbiddenPatternsValidator,
         ScopeConfigInterface $scopeConfig
     ) {
         $this->validationResultFactory = $validationResultFactory;
         $this->nameValidator = $nameValidator;
+        $this->forbiddenPatternsValidator = $forbiddenPatternsValidator;
         $this->scopeConfig = $scopeConfig;
     }
 
@@ -56,34 +65,35 @@ class NameValidationRule implements QuoteValidationRuleInterface
     public function validate(Quote $quote): array
     {
         $validationErrors = [];
+        
+        // Define the fields to validate with their respective validators
+        $fieldsToValidate = [
+            'First Name' => [$quote->getCustomerFirstname(), 'isValidName', GlobalNameValidator::class],
+            'Middle Name' => [$quote->getCustomerMiddlename(), 'isValidName', GlobalNameValidator::class],
+            'Last Name' => [$quote->getCustomerLastname(), 'isValidName', GlobalNameValidator::class],
+            'Prefix' => [$quote->getCustomerPrefix(), 'isValidName', GlobalNameValidator::class],
+            'Suffix' => [$quote->getCustomerSuffix(), 'isValidName', GlobalNameValidator::class],
+        ];
 
+        // Validate each field
+        foreach ($fieldsToValidate as $fieldName => [$fieldValue, $validationMethod, $validatorClass]) {
+            if (!$validatorClass::$validationMethod($fieldValue)) {
+                $validationErrors[] = __("$fieldName is not valid");
+            }
+        }
+
+        // Check if regex validation is enabled
         $isRegexEnabled = $this->scopeConfig->isSetFlag(
             'system/security/security_regex_enabled',
             ScopeInterface::SCOPE_STORE
         );
 
-        if ($isRegexEnabled) {
-            $firstName = $quote->getCustomerFirstname();
-            $middleName = $quote->getCustomerMiddlename();
-            $lastName = $quote->getCustomerLastname();
-            $customerPrefix = $quote->getCustomerPrefix();
-            $customerSuffix = $quote->getCustomerSuffix();
-
-            // Validate each name-related field
-            if (!GlobalNameValidator::isValidName($firstName)) {
-                $validationErrors[] = __('First Name is not valid');
-            }
-            if (!GlobalNameValidator::isValidName($middleName)) {
-                $validationErrors[] = __('Middle Name is not valid');
-            }
-            if (!GlobalNameValidator::isValidName($lastName)) {
-                $validationErrors[] = __('Last Name is not valid');
-            }
-            if (!GlobalNameValidator::isValidName($customerPrefix)) {
-                $validationErrors[] = __('Prefix is not valid');
-            }
-            if (!GlobalNameValidator::isValidName($customerSuffix)) {
-                $validationErrors[] = __('Suffix is not valid');
+        // Perform regex validation only if no other errors exist
+        if (empty($validationErrors) && $isRegexEnabled) {
+            foreach ($quote->getData() as $key => $value) {
+                if (is_string($value) && !$this->forbiddenPatternsValidator->isValid($value)) {
+                    $validationErrors[] = __("Field %1 contains invalid characters.", $key);
+                }
             }
         }
 
