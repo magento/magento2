568743e886377317444fca017d04fdd7258fd18d
in-session <info@in-session.de>
2024-08-28 08:07:25 +0200

    Update GlobalPhoneValidation.php

--- lib/internal/Magento/Framework/Validator/GlobalPhoneValidation.php
+++ lib/internal/Magento/Framework/Validator/GlobalPhoneValidation.php
@@ -10,7 +10,17 @@ namespace Magento\Framework\Validator;
 class GlobalPhoneValidation
 {
     /**
-     * Regular expression pattern for validating phone numbers.
+     * Allowed characters for validating phone numbers:
+     *
+     * \d: Digits (0-9), representing the numbers in a phone number.
+     * \s: Whitespace characters (spaces, tabs, newlines, etc.), allowing separation within the number.
+     * \+: Plus sign, often used to indicate the country code (e.g., +1 for the USA).
+     * \-: Hyphen, commonly used to separate different parts of a phone number (e.g., 555-1234).
+     * \(: Opening parenthesis, often used around area codes (e.g., (555) 123-4567).
+     * \): Closing parenthesis, used with the opening parenthesis around area codes.
+     * \/: Forward slash, sometimes used in extensions or other parts of the number.
+     *
+     * The pattern ensures that a phone number can be between 1 and 30 characters long.
      */
     public const PATTERN_TELEPHONE = '/(?:[\d\s\+\-\()\/]{1,30})/u';
 
