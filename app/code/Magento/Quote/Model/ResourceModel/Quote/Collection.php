7e4c8e00afc9579813a92801da6849da95a1bfd7
Oleksandr Iegorov <oiegorov@adobe.com>
2024-08-29 11:45:35 -0500

    ACP2E-3247: sales_clean_quotes cron deletes quotes from to yet approved purchase orders

--- app/code/Magento/Quote/Model/ResourceModel/Quote/Collection.php
+++ app/code/Magento/Quote/Model/ResourceModel/Quote/Collection.php
@@ -21,4 +21,29 @@ class Collection extends \Magento\Framework\Model\ResourceModel\Db\VersionContro
     {
         $this->_init(\Magento\Quote\Model\Quote::class, \Magento\Quote\Model\ResourceModel\Quote::class);
     }
+
+    /**
+     * Join table to collection select
+     *
+     * @param array|string $table
+     * @param string $cond
+     * @param array|string $cols
+     * @return $this
+     */
+    public function joinLeft(array|string $table, string $cond, array|string $cols = '*')
+    {
+        if (is_array($table)) {
+            $newTable = reset($table);
+            $alias = key($table);
+            $table = $newTable;
+        } else {
+            $alias = $table;
+        }
+
+        if (!isset($this->_joinedTables[$alias])) {
+            $this->getSelect()->joinLeft([$alias => $this->getTable($table)], $cond, $cols);
+            $this->_joinedTables[$alias] = true;
+        }
+        return $this;
+    }
 }
