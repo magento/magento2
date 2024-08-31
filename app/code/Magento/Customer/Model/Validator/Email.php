c7d5611569c995a0b46f4e6365dc290744b920b1
in-session <info@in-session.de>
2024-08-31 10:54:49 +0200

    Create Email.php

---
+++ app/code/Magento/Customer/Model/Validator/Email.php
@@ -0,0 +1,90 @@
+<?php
+declare(strict_types=1);
+
+namespace Magento\Customer\Model\Validator;
+
+use Magento\Customer\Model\Customer;
+use Magento\Framework\Validator\AbstractValidator;
+use Magento\Security\Model\Validator\Pattern\EmailAddressValidator;
+
+/**
+ * Customer email fields validator.
+ */
+class Email extends AbstractValidator
+{
+    /**
+     * @var EmailAddressValidator
+     */
+    private EmailAddressValidator $emailValidator;
+
+    /**
+     * Constructor.
+     *
+     * @param EmailAddressValidator $emailValidator
+     */
+    public function __construct(EmailAddressValidator $emailValidator)
+    {
+        $this->emailValidator = $emailValidator;
+    }
+
+    /**
+     * Validate email fields.
+     *
+     * @param Customer $customer
+     * @return bool
+     */
+    public function isValid($customer): bool
+    {
+        if (!$this->emailValidator->isValidationEnabled()) {
+            return true;
+        }
+
+        $email = $customer->getEmail();
+        if (empty($email)) {
+            return true;
+        }
+
+        if (!$this->validateEmailField('Email', $email)) {
+            return false;
+        }
+
+        return count($this->_messages) == 0;
+    }
+
+    /**
+     * Validate the email field.
+     *
+     * @param string $fieldName
+     * @param string|null $emailValue
+     * @return bool
+     */
+    private function validateEmailField(string $fieldName, ?string $emailValue): bool
+    {
+        if (!$this->emailValidator->isValid($emailValue)) {
+            parent::_addMessages([
+                "Email address is not valid! Allowed characters: {$this->emailValidator->allowedCharsDescription}"
+            ]);
+            return false;
+        }
+
+        if ($this->isBlacklistEmail($emailValue)) {
+            parent::_addMessages([
+                __('The email address or domain is blacklisted.')
+            ]);
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Check if email field is blacklisted using the EmailAddressValidator.
+     *
+     * @param string|null $emailValue
+     * @return bool
+     */
+    private function isBlacklistEmail(?string $emailValue): bool
+    {
+        return $this->emailValidator->isBlacklist($emailValue);
+    }
+}
