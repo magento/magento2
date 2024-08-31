90e86ecddf344638345073a470716ca420998eba
in-session <info@in-session.de>
2024-08-31 14:22:44 +0200

    Update TelephoneTest.php

--- app/code/Magento/Customer/Test/Unit/Model/Validator/TelephoneTest.php
+++ app/code/Magento/Customer/Test/Unit/Model/Validator/TelephoneTest.php
@@ -7,20 +7,20 @@ declare(strict_types=1);
 
 namespace Magento\Customer\Test\Unit\Model\Validator;
 
-use Magento\Customer\Model\Validator\Street;
+use Magento\Customer\Model\Validator\Telephone;
 use Magento\Customer\Model\Customer;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
 /**
- * Customer street validator tests
+ * Customer telephone validator tests
  */
-class StreetTest extends TestCase
+class TelephoneTest extends TestCase
 {
     /**
-     * @var Street
+     * @var Telephone
      */
-    private Street $streetValidator;
+    private Telephone $telephoneValidator;
 
     /**
      * @var Customer|MockObject
@@ -32,112 +32,55 @@ class StreetTest extends TestCase
      */
     protected function setUp(): void
     {
-        // PatternStreetValidator is not directly mocked anymore.
-        $this->streetValidator = $this->createMock(Street::class);
+        $this->telephoneValidator = new Telephone();
         $this->customerMock = $this
             ->getMockBuilder(Customer::class)
             ->disableOriginalConstructor()
-            ->addMethods(['getStreet'])
+            ->addMethods(['getTelephone', 'getFax'])
             ->getMock();
     }
 
     /**
-     * Test for allowed characters in street addresses
+     * Test for allowed punctuation characters in customer telephone numbers
      *
-     * @param array $street
+     * @param string $telephone
+     * @param string $fax
      * @param string $message
      * @return void
-     * @dataProvider expectedPunctuationInStreetDataProvider
+     * @dataProvider expectedPunctuationInTelephoneDataProvider
      */
-    public function testValidateCorrectPunctuationInStreet(
-        array $street,
+    public function testValidateCorrectPunctuationInTelephone(
+        string $telephone,
+        string $fax,
         string $message
     ): void {
-        $this->customerMock->expects($this->once())->method('getStreet')->willReturn($street);
+        $this->customerMock->expects($this->once())->method('getTelephone')->willReturn($telephone);
+        $this->customerMock->expects($this->once())->method('getFax')->willReturn($fax);
 
-        $isValid = $this->streetValidator->isValid($this->customerMock);
+        $isValid = $this->telephoneValidator->isValid($this->customerMock);
         $this->assertTrue($isValid, $message);
     }
 
     /**
-     * Data provider for valid street names
-     *
      * @return array
      */
-    public function expectedPunctuationInStreetDataProvider(): array
+    public function expectedPunctuationInTelephoneDataProvider(): array
     {
         return [
             [
-                'street' => [
-                    "123 Rue de l'Étoile",
-                    "Ville d'Ölives, Çôte d'Azur",
-                    "Çôte d'Azur"
-                ],
-                'message' => 'Unicode marks and Unicode letters must be allowed in street'
+                'telephone' => '(1)99887766',
+                'fax' => '123456789',
+                'message' => 'Parentheses must be allowed in telephone, and digits must be allowed in fax.'
             ],
             [
-                'street' => [
-                    '876 Elm Way, Redwood Lodge',
-                    '456 Pine Street, Serenity Cottage',
-                    '321 Birch Boulevard, Willow Retreat'
-                ],
-                'message' => 'Comma must be allowed in street'
+                'telephone' => '+6255554444',
+                'fax' => '123 456 789',
+                'message' => 'Plus sign must be allowed in telephone, and spaces must be allowed in fax.'
             ],
             [
-                'street' => [
-                    '321 Birch Boulevard-Retreat',
-                    '234 Spruce Place-Residence',
-                    '456 Pine Street-Haven'
-                ],
-                'message' => 'Hyphen must be allowed in street'
-            ],
-            [
-                'street' => [
-                    '1234 Elm St.',
-                    'Main. Street',
-                    '1234 Elm St'
-                ],
-                'message' => 'Period must be allowed in street'
-            ],
-            [
-                'street' => [
-                    'O\'Connell Street',
-                    'O`Connell Street',
-                    '321 Birch Boulevard ’Willow Retreat’'
-                ],
-                'message' => 'Quotes must be allowed in street'
-            ],
-            [
-                'street' => [
-                    '123 Main Street & Elm Avenue',
-                    '456 Pine Street & Maple Avenue',
-                    '789 Oak Lane & Cedar Road'
-                ],
-                'message' => 'Ampersand must be allowed in street'
-            ],
-            [
-                'street' => [
-                    'Oak Lane Space',
-                    'Birch Boulevard Space',
-                    'Spruce Place'
-                ],
-                'message' => 'Whitespace must be allowed in street'
-            ],
-            [
-                'street' => [
-                    '234 Spruce Place',
-                    '321 Birch Boulevard',
-                    '876 Elm Way'
-                ],
-                'message' => 'Digits must be allowed in street'
-            ],
-            [
-                'street' => [
-                    '1234 Elm St. [Apartment 5]',
-                    'Main St. (Suite 200)',
-                    '456 Pine St. [Unit 10]'
-                ],
-                'message' => 'Square brackets and parentheses must be allowed in street'
+                'telephone' => '555-555-555',
+                'fax' => '123/456/789',
+                'message' => 'Hyphen must be allowed in telephone, and forward slashes must be allowed in fax.'
             ]
         ];
     }
