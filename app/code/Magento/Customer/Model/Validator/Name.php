da11da8a046cf8430999306d5ff2a2d36ba04ae9
in-session <info@in-session.de>
2024-09-03 07:48:00 +0200

    Update Name.php

--- app/code/Magento/Customer/Model/Validator/Name.php
+++ app/code/Magento/Customer/Model/Validator/Name.php
@@ -9,7 +9,7 @@ namespace Magento\Customer\Model\Validator;
 
 use Magento\Customer\Model\Customer;
 use Magento\Framework\Validator\AbstractValidator;
-use Magento\Security\Model\Validator\Pattern\NameValidator;
+use Magento\Customer\Model\Validator\Pattern\NameValidator;
 
 /**
  * Customer name fields validator.
