add1b956bc79d2bbd38bca0277abf22eebea6eb4
in-session <info@in-session.de>
2024-09-03 07:52:32 +0200

    Update NameTest.php

--- app/code/Magento/Customer/Test/Unit/Model/Validator/NameTest.php
+++ app/code/Magento/Customer/Test/Unit/Model/Validator/NameTest.php
@@ -9,7 +9,7 @@ namespace Magento\Customer\Test\Unit\Model\Validator;
 
 use Magento\Customer\Model\Validator\Name;
 use Magento\Customer\Model\Customer;
-use Magento\Security\Model\Validator\Pattern\NameValidator;
+use Magento\Customer\Model\Validator\Pattern\NameValidator;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
