85683bdf583a9f7f731dac1a278242cb8f3da45b
engcom-Dash <grp-engcom-vendorworker-Dash@adobe.com>
2024-09-09 17:59:07 +0530

    38326: Add unit test and update return type of _columnBrake function

--- app/code/Magento/Theme/Block/Html/Topmenu.php
+++ app/code/Magento/Theme/Block/Html/Topmenu.php
@@ -133,7 +133,7 @@ class Topmenu extends Template implements IdentityInterface
      *
      * @param Menu $items
      * @param int $limit
-     * @return array|void
+     * @return array
      *
      * @todo: Add Depth Level limit, and better logic for columns
      */
