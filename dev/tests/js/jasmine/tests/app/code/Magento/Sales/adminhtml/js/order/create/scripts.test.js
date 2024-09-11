b2799f187ae72b00c1093a6cc4e7c4ce50bbd70a
Alexandru Plapana <aplapana@adobe.com>
2024-09-11 17:07:45 +0300

    ACP2E-3294: The shipping address state is not auto updating

--- dev/tests/js/jasmine/tests/app/code/Magento/Sales/adminhtml/js/order/create/scripts.test.js
+++ dev/tests/js/jasmine/tests/app/code/Magento/Sales/adminhtml/js/order/create/scripts.test.js
@@ -162,20 +162,22 @@ define([
 
         describe('Testing syncAddressField method', function () {
             it('Synchronize region and region_id fields display when called with field named "country"', function () {
-                let form, billingCountryId, billingRegionId, billingRegion;
-                let shippingCountryId, shippingRegionId, shippingRegion;
+                let form, billingCountryId, billingRegionId, billingRegion, billingCountryIdOption1,
+                    billingCountryIdOption2, shippingCountryId, shippingRegionId, shippingRegion, billingRegionOption1,
+                    billingRegionOption2, shippingCountryIdOption1, shippingOption2, shippingRegionOption1,
+                    shippingRegionOption2;
 
                 form = document.createElement('form');
 
                 //create billing country id field
                 billingCountryId = document.createElement('select');
                 billingCountryId.name = 'order[billing_address][country_id]';
-                let billingCountryIdOption1 = document.createElement("option");
-                billingCountryIdOption1.value = "USA";
-                billingCountryIdOption1.innerText = "United States of America";
-                let billingCountryIdOption2 = document.createElement("option");
-                billingCountryIdOption2.value = "RO";
-                billingCountryIdOption2.innerText = "Romania";
+                billingCountryIdOption1 = document.createElement('option');
+                billingCountryIdOption1.value = 'USA';
+                billingCountryIdOption1.innerText = 'United States of America';
+                billingCountryIdOption2 = document.createElement('option');
+                billingCountryIdOption2.value = 'RO';
+                billingCountryIdOption2.innerText = 'Romania';
                 billingCountryId.appendChild(billingCountryIdOption1);
                 billingCountryId.appendChild(billingCountryIdOption2);
                 form.appendChild(billingCountryId);
@@ -184,12 +186,12 @@ define([
                 billingRegionId = document.createElement('select');
                 billingRegionId.name = 'order[billing_address][region_id]';
                 billingRegionId.id = 'order-billing_address_region_id';
-                let billingRegionOption1 = document.createElement("option");
-                billingRegionOption1.value = "NY";
-                billingRegionOption1.innerText = "New York";
-                let billingRegionOption2 = document.createElement("option");
-                billingRegionOption2.value = "TX";
-                billingRegionOption2.innerText = "Texas";
+                billingRegionOption1 = document.createElement('option');
+                billingRegionOption1.value = 'NY';
+                billingRegionOption1.innerText = 'New York';
+                billingRegionOption2 = document.createElement('option');
+                billingRegionOption2.value = 'TX';
+                billingRegionOption2.innerText = 'Texas';
                 billingRegionId.appendChild(billingRegionOption1);
                 billingRegionId.appendChild(billingRegionOption2);
                 form.appendChild(billingRegionId);
@@ -204,27 +206,27 @@ define([
                 //create shipping country id field
                 shippingCountryId = document.createElement('select');
                 shippingCountryId.name = 'order[shipping_address][country_id]';
-                let shippingCountryIdOption1 = document.createElement("option");
-                shippingCountryIdOption1.value = "USA";
-                shippingCountryIdOption1.innerText = "United States of America";
-                let shippingOption2 = document.createElement("option");
-                shippingOption2.value = "RO";
-                shippingOption2.innerText = "Romania";
+                shippingCountryIdOption1 = document.createElement('option');
+                shippingCountryIdOption1.value = 'USA';
+                shippingCountryIdOption1.innerText = 'United States of America';
+                shippingOption2 = document.createElement('option');
+                shippingOption2.value = 'RO';
+                shippingOption2.innerText = 'Romania';
                 shippingCountryId.appendChild(shippingCountryIdOption1);
                 shippingCountryId.appendChild(shippingOption2);
-                shippingCountryId.value = "RO";
+                shippingCountryId.value = 'RO';
                 form.appendChild(shippingCountryId);
 
                 //create shipping region id field
                 shippingRegionId = document.createElement('select');
                 shippingRegionId.name = 'order[shipping_address][region_id]';
                 shippingRegionId.id = 'order-shipping_address_region_id';
-                let shippingRegionOption1 = document.createElement("option");
-                shippingRegionOption1.value = "B";
-                shippingRegionOption1.innerText = "Bucuresti";
-                let shippingRegionOption2 = document.createElement("option");
-                shippingRegionOption2.value = "CT";
-                shippingRegionOption2.innerText = "Constanta";
+                shippingRegionOption1 = document.createElement('option');
+                shippingRegionOption1.value = 'B';
+                shippingRegionOption1.innerText = 'Bucuresti';
+                shippingRegionOption2 = document.createElement('option');
+                shippingRegionOption2.value = 'CT';
+                shippingRegionOption2.innerText = 'Constanta';
                 shippingRegionId.appendChild(shippingRegionOption1);
                 shippingRegionId.appendChild(shippingRegionOption2);
                 form.appendChild(shippingRegionId);
@@ -239,8 +241,8 @@ define([
                 order = new window.AdminOrder({});
                 order.syncAddressField(form, 'order[billing_address][country_id]', billingCountryId);
 
-                expect(shippingCountryId.value).toEqual("USA");
-                expect(shippingRegion.style.display).toEqual("none");
+                expect(shippingCountryId.value).toEqual('USA');
+                expect(shippingRegion.style.display).toEqual('none');
             });
         });
 
