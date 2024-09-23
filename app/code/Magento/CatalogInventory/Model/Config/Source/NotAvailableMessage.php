a782abab73d46e5ffe005c8e208fd73e3ff7b493
Sumesh P <142383015+sumesh-GL@users.noreply.github.com>
2024-09-23 17:10:35 +0530

    LYNX-540: Add quantity to ProductInterface

--- app/code/Magento/CatalogInventory/Model/Config/Source/NotAvailableMessage.php
+++ app/code/Magento/CatalogInventory/Model/Config/Source/NotAvailableMessage.php
@@ -15,6 +15,12 @@ use Magento\Framework\Data\OptionSourceInterface;
 class NotAvailableMessage implements OptionSourceInterface
 {
     /**
+     * Message config values
+     */
+    public const VALUE_ONLY_X_OF_Y = 1;
+    public const VALUE_NOT_ENOUGH_ITEMS = 2;
+
+    /**
      * Options getter
      *
      * @return array
@@ -23,11 +29,11 @@ class NotAvailableMessage implements OptionSourceInterface
     {
         $options = [];
         $options[] = [
-            'value' => 1,
+            'value' => self::VALUE_ONLY_X_OF_Y,
             'label' => __('Only X available for sale. Please adjust the quantity to continue'),
         ];
         $options[] = [
-            'value' => 2,
+            'value' => self::VALUE_NOT_ENOUGH_ITEMS,
             'label' => __('Not enough items for sale. Please adjust the quantity to continue'),
         ];
         return $options;
@@ -41,8 +47,8 @@ class NotAvailableMessage implements OptionSourceInterface
     public function toArray(): array
     {
         return [
-            1 => __('Only X available for sale. Please adjust the quantity to continue'),
-            2 => __('Not enough items for sale. Please adjust the quantity to continue')
+            self::VALUE_ONLY_X_OF_Y => __('Only X available for sale. Please adjust the quantity to continue'),
+            self::VALUE_NOT_ENOUGH_ITEMS => __('Not enough items for sale. Please adjust the quantity to continue')
         ];
     }
 }
