117010160710ec5e1074a8b9bc2a6e3fe07c615c
Chhandak.Barua <chhandak.barua@BLR1-LMC-N73490.local>
2024-10-04 15:29:07 +0530

    ACP2E-3233: Admin can still place order even without payment method

--- app/code/Magento/Sales/Controller/Adminhtml/Order/Create/LoadBlock.php
+++ app/code/Magento/Sales/Controller/Adminhtml/Order/Create/LoadBlock.php
@@ -90,11 +90,6 @@ class LoadBlock extends CreateAction implements HttpPostActionInterface, HttpGet
             $this->storeManager->setCurrentStore($request->getParam('store_id'));
         }
         try {
-//            if (!$request->getParam('payment')) {
-//                $paymentData = array('payment' => array('method' => ''));
-//                //$this->getRequest()->setPost($paymentData);
-//                //$request->setParams($paymentData);
-//            }
             $this->_initSession()->_processData();
         } catch (\Magento\Framework\Exception\LocalizedException $e) {
             $this->_reloadQuote();
