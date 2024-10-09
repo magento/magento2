402ab7b2b2fa3d9945edea5a7525a533e0475e4f
lakshmana49 <glo28218@adobe.com>
2024-10-09 13:02:22 +0530

    ACP2E-3276: Order reports showing the wrong currency symbol

--- app/code/Magento/Directory/Test/Fixture/CurrencyRate.php
+++ app/code/Magento/Directory/Test/Fixture/CurrencyRate.php
@@ -1,18 +1,7 @@
 <?php
-/************************************************************************
- *
+/**
  * Copyright 2024 Adobe
  * All Rights Reserved.
- *
- * NOTICE: All information contained herein is, and remains
- * the property of Adobe and its suppliers, if any. The intellectual
- * and technical concepts contained herein are proprietary to Adobe
- * and its suppliers and are protected by all applicable intellectual
- * property laws, including trade secret and copyright laws.
- * Dissemination of this information or reproduction of this material
- * is strictly forbidden unless prior written permission is obtained
- * from Adobe.
- * ************************************************************************
  */
 declare(strict_types=1);
 
@@ -56,15 +45,14 @@ class CurrencyRate implements RevertibleDataFixtureInterface
 
     public function apply(array $data = []): ?DataObject
     {
-        if (is_array($data)) {
-            foreach ($data as $currencyCode => $rate) {
-                foreach ($rate as $currencyTo => $value) {
-                    $value = abs((float) $this->format->getNumber($value));
-                    $data[$currencyCode][$currencyTo] = $value;
-                }
+        foreach ($data as $currencyCode => $rate) {
+            foreach ($rate as $currencyTo => $value) {
+                $value = abs((float) $this->format->getNumber($value));
+                $data[$currencyCode][$currencyTo] = $value;
             }
-            return $this->currency->saveRates($data);
         }
+
+        return $this->currency->saveRates($data);
     }
 
     public function revert(DataObject $data): void
