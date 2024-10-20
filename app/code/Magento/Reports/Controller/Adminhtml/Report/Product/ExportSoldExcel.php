0d8f9034684b653a164b2296f71c85d1eb63e2de
Mohamed Azarudeen <mdasar2595@gmail.com>
2024-10-20 19:28:30 +0530

    Incorrect http method issue fix

--- app/code/Magento/Reports/Controller/Adminhtml/Report/Product/ExportSoldExcel.php
+++ app/code/Magento/Reports/Controller/Adminhtml/Report/Product/ExportSoldExcel.php
@@ -9,12 +9,12 @@ declare(strict_types=1);
 namespace Magento\Reports\Controller\Adminhtml\Report\Product;
 
 use Magento\Backend\Block\Widget\Grid\ExportInterface;
-use Magento\Framework\App\Action\HttpPostActionInterface;
+use Magento\Framework\App\Action\HttpGetActionInterface;
 use Magento\Framework\App\ResponseInterface;
 use Magento\Framework\App\Filesystem\DirectoryList;
 use Magento\Reports\Controller\Adminhtml\Report\Product;
 
-class ExportSoldExcel extends Product implements HttpPostActionInterface
+class ExportSoldExcel extends Product implements HttpGetActionInterface
 {
     /**
      * Authorization level of a basic admin session
