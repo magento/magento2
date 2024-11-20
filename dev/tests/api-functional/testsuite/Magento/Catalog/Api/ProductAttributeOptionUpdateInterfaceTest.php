1afef0de073c320e0b03389c6267394cfb438ced
Indrani Sonawane <indrani.sonawane@BLR1-LMC-N71679.local>
2024-11-20 15:03:27 +0530

    Fixed Web API and static test failures

--- dev/tests/api-functional/testsuite/Magento/Catalog/Api/ProductAttributeOptionUpdateInterfaceTest.php
+++ dev/tests/api-functional/testsuite/Magento/Catalog/Api/ProductAttributeOptionUpdateInterfaceTest.php
@@ -76,7 +76,7 @@ class ProductAttributeOptionUpdateInterfaceTest extends WebapiAbstract
      */
     public function testUpdateWithAlreadyExistsException()
     {
-        $this->expectExceptionMessage("Admin store attribute option label '%1' is already exists.");
+        $this->expectExceptionMessage("Admin store attribute option label '%1' for attribute '%2' already exists.");
         $testAttributeCode = 'select_attribute';
 
         $newOptionData = [
