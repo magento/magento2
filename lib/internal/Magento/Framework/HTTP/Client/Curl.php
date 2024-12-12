71b178df1124a19fbc4aa03ff65b3981b15a9133
Lukasz Bajsarowicz <lukasz@swiftotter.com>
2024-12-12 12:19:01 +0100

    SwiftOtter-SOP-348 Match CURL Client to RFC-9110 and RFC-5789

--- lib/internal/Magento/Framework/HTTP/Client/Curl.php
+++ lib/internal/Magento/Framework/HTTP/Client/Curl.php
@@ -3,7 +3,6 @@
  * Copyright 2011 Adobe
  * All Rights Reserved.
  */
-
 declare(strict_types=1);
 
 namespace Magento\Framework\HTTP\Client;
@@ -222,6 +221,8 @@ class Curl implements \Magento\Framework\HTTP\ClientInterface
      *
      * @param string $uri uri relative to host, ex. "/index.php"
      * @return void
+     *
+     * @url https://www.rfc-editor.org/rfc/rfc9110.html#section-9.3.1
      */
     public function get($uri)
     {
@@ -238,11 +239,107 @@ class Curl implements \Magento\Framework\HTTP\ClientInterface
      * @param array|string $params
      * @return void
      *
-     * @see \Magento\Framework\HTTP\Client#post($uri, $params)
+     * @see \Magento\Framework\HTTP\Client::post($uri, $params)
+     * @url https://www.rfc-editor.org/rfc/rfc9110.html#section-9.3.3
      */
     public function post($uri, $params)
     {
         $this->makeRequest("POST", $uri, $params);
+    }
+
+    /**
+     * Make PUT request
+     *
+     * @param string $uri
+     * @param array|string $params
+     * @return void
+     *
+     * @url https://www.rfc-editor.org/rfc/rfc9110.html#section-9.3.4
+     */
+    public function put(string $uri, string|array $params): void
+    {
+        $this->makeRequest("PUT", $uri, $params);
+    }
+
+    /**
+     * Make DELETE request
+     *
+     * @param string $uri
+     * @param array|string $params
+     * @return void
+     *
+     * @url https://www.rfc-editor.org/rfc/rfc9110.html#section-9.3.5
+     */
+    public function delete(string $uri, array|string $params = []): void
+    {
+        $this->makeRequest("DELETE", $uri, $params);
+    }
+
+    /**
+     * Make PATCH request
+     *
+     * @param string $uri
+     * @param array|string $params
+     * @return void
+     *
+     * @url https://www.rfc-editor.org/info/rfc5789
+     */
+    public function patch(string $uri, array|string $params): void
+    {
+        $this->makeRequest("PATCH", $uri, $params);
+    }
+
+    /**
+     * Make OPTIONS request
+     *
+     * @param string $uri
+     * @param array|string $params
+     * @return void
+     *
+     * @url https://www.rfc-editor.org/rfc/rfc9110.html#section-9.3.7
+     */
+    public function options(string $uri, array|string $params = []): void
+    {
+        $this->makeRequest("OPTIONS", $uri, $params);
+    }
+
+    /**
+     * Make HEAD request
+     *
+     * @param string $uri
+     * @return void
+     *
+     * @url https://www.rfc-editor.org/rfc/rfc9110.html#section-9.3.2
+     */
+    public function head(string $uri): void
+    {
+        $this->makeRequest("HEAD", $uri);
+    }
+
+    /**
+     * Make TRACE request
+     *
+     * @param string $uri
+     * @return void
+     *
+     * @url https://www.rfc-editor.org/rfc/rfc9110.html#section-9.3.8
+     */
+    public function trace(string $uri): void
+    {
+        $this->makeRequest("TRACE", $uri);
+    }
+
+    /**
+     * Make CONNECT request
+     *
+     * @param string $uri
+     * @return void
+     *
+     * @url https://www.rfc-editor.org/rfc/rfc9110.html#section-9.3.6
+     */
+    public function connect(string $uri): void
+    {
+        $this->makeRequest("CONNECT", $uri);
     }
 
     /**
