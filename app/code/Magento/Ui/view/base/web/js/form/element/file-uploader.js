768edf89ccf66c30d94dcbc63e6943e90060633a
Manan Patel <map@salecto.in>
2025-02-14 15:55:44 +0530

    Fixed static tests

--- app/code/Magento/Ui/view/base/web/js/form/element/file-uploader.js
+++ app/code/Magento/Ui/view/base/web/js/form/element/file-uploader.js
@@ -182,9 +182,12 @@ define([
             $('#' + fileId).closest('.file-uploader-area').find('.file-uploader-button:first').on('click', function () {
                 $('#' + fileId).closest('.file-uploader-area').find('.uppy-Dashboard-browse').trigger('click');
             });
-            $('#' + fileId).closest('.pagebuilder-image-uploader-container').find('.pagebuilder-options .file-uploader-button:first').on('click', function () {
-                $('#' + fileId).closest('.file-uploader-area').find('.uppy-Dashboard-browse').trigger('click');
-            });   
+            $('#' + fileId).closest('.pagebuilder-image-uploader-container')
+                .find('.pagebuilder-options .file-uploader-button:first')
+                .on('click', function () {
+                    $('#' + fileId).closest('.file-uploader-area').find('.uppy-Dashboard-browse').trigger('click');
+                }
+            );
         },
 
         /**
