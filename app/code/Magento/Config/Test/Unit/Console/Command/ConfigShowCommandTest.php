94001b75214669e1a8d44b3c25790e93f45c23d4
rogerdz <rogerdz88@hotmail.com>
2025-03-06 06:18:59 +0000

    fix unit test

--- app/code/Magento/Config/Test/Unit/Console/Command/ConfigShowCommandTest.php
+++ app/code/Magento/Config/Test/Unit/Console/Command/ConfigShowCommandTest.php
@@ -1,9 +1,8 @@
 <?php
 /**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
+ * Copyright 2017 Adobe
+ * All Rights Reserved.
  */
-
 declare(strict_types=1);
 
 namespace Magento\Config\Test\Unit\Console\Command;
