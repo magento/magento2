a446fbaa19406ca406c1daef26c807dd72a62bee
Tran Ngoc Duc <caothu91@gmail.com>
2025-04-01 22:47:23 +0700

    add CategorySimbaAttribute data patch and UI component for new category attribute; update module version to 1.0.1

---
+++ app/code/Diepxuan/Magento/Setup/Patch/Data/CategorySimbaAttribute.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * @copyright  © 2019 Dxvn, Inc.
+ *
+ * @author     Tran Ngoc Duc <ductn@diepxuan.com>
+ * @author     Tran Ngoc Duc <caothu91@gmail.com>
+ *
+ * @lastupdate 2025-04-01 22:40:01
+ */
+
+namespace Diepxuan\Magento\Setup\Patch\Data;
+
+use Magento\Catalog\Model\Category;
+use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\Patch\PatchVersionInterface;
+
+class CategorySimbaAttribute implements DataPatchInterface, PatchVersionInterface
+{
+    private $eavSetupFactory;
+    private $moduleDataSetup;
+
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        EavSetupFactory $eavSetupFactory
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    public function apply()
+    {
+        // $this->moduleDataSetup->getConnection()->startSetup();
+
+        /** @var EavSetup $eavSetup */
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+        $eavSetup->addAttribute(
+            Category::ENTITY,
+            'simba_category',
+            [
+                'type'       => 'varchar',
+                'label'      => 'Category Simba',
+                'input'      => 'text',
+                'required'   => false,
+                'sort_order' => 20,
+                'global'     => ScopedAttributeInterface::SCOPE_STORE,
+                'group'      => 'General Information',
+            ]
+        );
+
+        return $this;
+        // $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    public static function getVersion()
+    {
+        return '2.4.6';
+    }
+
+    public function getAliases()
+    {
+        return [];
+    }
+}
