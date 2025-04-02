b81eaca0120eb5305e57832b4e8d0f1c64eeb183
Tran Ngoc Duc <caothu91@gmail.com>
2025-04-02 13:24:59 +0700

    fix(CategorySync): correct category activation and menu inclusion logic

--- app/code/Diepxuan/SyncCRM/Sync/CategorySync.php
+++ app/code/Diepxuan/SyncCRM/Sync/CategorySync.php
@@ -8,7 +8,7 @@ declare(strict_types=1);
  * @author     Tran Ngoc Duc <ductn@diepxuan.com>
  * @author     Tran Ngoc Duc <caothu91@gmail.com>
  *
- * @lastupdate 2025-04-02 09:35:22
+ * @lastupdate 2025-04-02 13:17:30
  */
 
 namespace Diepxuan\SyncCRM\Sync;
@@ -75,7 +75,8 @@ class CategorySync extends CrmSync
                 $categorySlug     = strtolower(vn_convert_encoding($categoryData['ma_nhvt']));
                 $categoryParentId = $this->getCategoryParentId($categoryData, $categories);
                 $category->setName($categoryData['ten_nhvt']);
-                $category->setIsActive(true);
+                $category->setIsActive($category->setIncludeInMenu());
+                $category->setIncludeInMenu($category->setIncludeInMenu());
                 $category->setParentId($categoryParentId);
                 $category->setPath($this->getCategoryPath($categoryData, $categories));
                 // $category->setPath("1/2/{$categoryId}");
