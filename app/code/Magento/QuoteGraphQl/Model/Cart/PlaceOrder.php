ff67a1db6adb8cbb6fd194243bc705eb44e2949e
KrasnoshchokBohdan <krasnoshchok.bohdan@gmail.com>
2025-06-03 12:04:47 +0300

    magento/magento2#37983: Place order with disabled Payment method working
    - copyright and static test errors fix

--- app/code/Magento/QuoteGraphQl/Model/Cart/PlaceOrder.php
+++ app/code/Magento/QuoteGraphQl/Model/Cart/PlaceOrder.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
+ * Copyright 2015 Adobe
+ * All Rights Reserved.
  */
 declare(strict_types=1);
 
@@ -64,7 +64,7 @@ class PlaceOrder
         $isPaymentMethodAvailable = false;
 
         // Check if the selected payment method is in the available methods list
-        if($paymentMethodCode && $availablePaymentMethods){
+        if ($paymentMethodCode && $availablePaymentMethods) {
             foreach ($availablePaymentMethods as $availableMethod) {
                 if ($availableMethod->getCode() === $paymentMethodCode) {
                     $isPaymentMethodAvailable = true;
