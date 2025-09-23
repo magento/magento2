4f2accee714506d7d363750e450bed0482c75a4c
engcom-Charlie <engcom-vendorworker-charlie@adobe.com>
2025-09-23 13:16:48 +0530

    Updated the copyright

--- app/code/Magento/Store/Model/Website.php
+++ app/code/Magento/Store/Model/Website.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
+ * Copyright 2011 Adobe
+ * All Rights Reserved.
  */
 namespace Magento\Store\Model;
 
