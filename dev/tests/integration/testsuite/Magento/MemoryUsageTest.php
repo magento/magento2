dc06f6002e0e5098839fae297655e41c35d9e807
engcom-Charlie <engcom-vendorworker-charlie@adobe.com>
2025-10-30 20:39:18 +0530

    Fixing static test failures

--- dev/tests/integration/testsuite/Magento/MemoryUsageTest.php
+++ dev/tests/integration/testsuite/Magento/MemoryUsageTest.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
+ * Copyright 2013 Adobe
+ * All Rights Reserved.
  */
 namespace Magento;
 
@@ -10,7 +10,7 @@ class MemoryUsageTest extends \PHPUnit\Framework\TestCase
     /**
      * Number of application reinitialization iterations to be conducted by tests
      */
-    const APP_REINITIALIZATION_LOOPS = 20;
+    private const APP_REINITIALIZATION_LOOPS = 20;
 
     /**
      * @var \Magento\TestFramework\Helper\Memory
