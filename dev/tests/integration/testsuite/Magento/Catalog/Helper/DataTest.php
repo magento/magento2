b5e525988d6e8c898212c92482f11a0daf1d4423
engcom-Charlie <engcom-vendorworker-charlie@adobe.com>
2025-11-26 22:04:13 +0530

    Fixed static test failures

--- dev/tests/integration/testsuite/Magento/Catalog/Helper/DataTest.php
+++ dev/tests/integration/testsuite/Magento/Catalog/Helper/DataTest.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
+ * Copyright 2011 Adobe
+ * All Rights Reserved.
  */
 namespace Magento\Catalog\Helper;
 
@@ -23,7 +23,7 @@ class DataTest extends \PHPUnit\Framework\TestCase
     private $helper;
 
     /**
-     * Object Manager
+     * Object Manager variable
      *
      * @var \Magento\Framework\ObjectManagerInterface
      */
