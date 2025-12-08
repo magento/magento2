a4062ec64a74e389cb1e705eea68b3880b28ef02
indykoning <15870933+indykoning@users.noreply.github.com>
2025-12-08 09:47:58 +0100

    Updated copyright

--- lib/internal/Magento/Framework/Exception/ErrorHandlerException.php
+++ lib/internal/Magento/Framework/Exception/ErrorHandlerException.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
+ * Copyright 2025 Adobe
+ * All Rights Reserved.
  */
 namespace Magento\Framework\Exception;
 
