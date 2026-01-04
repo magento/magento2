ba7d6298e87ab54b5a28ae839c99776d9eced8f3
Koushik CH <35527481+koushikch7@users.noreply.github.com>
2026-01-04 17:15:28 +0000

    feat(http): add missing HTTP methods (PUT, DELETE, PATCH, OPTIONS, HEAD, TRACE, CONNECT); update test helper; add unit test for method presence
    (cherry picked from commit 5defeed2c04312a864e8640afa5d3dd1c9532ed4)

---
+++ dev/tests/unit/testsuite/Magento/Framework/HTTP/Client/CurlMethodsTest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Magento\Framework\HTTP\Client\Tests;
+
+use Magento\Framework\HTTP\Client\Curl;
+use PHPUnit\Framework\TestCase;
+
+class CurlMethodsTest extends TestCase
+{
+    public function testPublicMethodsExist()
+    {
+        $methods = [
+            'put',
+            'delete',
+            'patch',
+            'options',
+            'head',
+            'trace',
+            'connect'
+        ];
+
+        foreach ($methods as $method) {
+            $this->assertTrue(method_exists(Curl::class, $method), "Method $method should exist on Curl class");
+        }
+    }
+}
