8891988ec27ab561b00b91b95209f6f353baec3a
Jisse Reitsma <jisse@yireo.com>
2026-01-08 14:27:20 +0100

    Fix file check

--- dev/tests/integration/framework/Magento/TestFramework/Db/Mysql.php
+++ dev/tests/integration/framework/Magento/TestFramework/Db/Mysql.php
@@ -234,7 +234,7 @@ class Mysql extends \Magento\TestFramework\Db\AbstractDb
         ];
 
         $additionalFile = dirname(dirname($this->_varPath)).'/etc/mysql-client-config.php';
-        if (file_exists($this->_varPath)) {
+        if (file_exists($additionalFile)) {
             $config = array_merge($config, (array)include $additionalFile);
         }
 
