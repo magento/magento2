2e9c2a040e6ae4fa7eac7371e83545cd8cbedc96
Łukasz Bajsarowicz <lukasz@lbajsarowicz.me>
2026-04-09 19:00:35 +0200

    fix: replace deprecated fixture files with parameterized DataFixture attributes
    Remove standalone fixture PHP files and rewrite integration tests
    to use modern #[DataFixture] attributes with WebsiteFixture,
    StoreGroupFixture, StoreFixture, and ConfigFixture.
    
    Adds missing declare(strict_types=1).

--- dev/tests/integration/testsuite/Magento/Store/Model/StoreBaseCurrencyTest.php
+++ dev/tests/integration/testsuite/Magento/Store/Model/StoreBaseCurrencyTest.php
@@ -7,6 +7,18 @@ declare(strict_types=1);
 
 namespace Magento\Store\Model;
 
+use Magento\Catalog\Helper\Data as CatalogHelper;
+use Magento\Config\Model\ResourceModel\Config as ConfigResource;
+use Magento\Directory\Model\Currency;
+use Magento\Store\Model\ScopeInterface;
+use Magento\Store\Test\Fixture\Group as StoreGroupFixture;
+use Magento\Store\Test\Fixture\Store as StoreFixture;
+use Magento\Store\Test\Fixture\Website as WebsiteFixture;
+use Magento\TestFramework\Fixture\AppIsolation;
+use Magento\TestFramework\Fixture\Config as ConfigFixture;
+use Magento\TestFramework\Fixture\DataFixture;
+use Magento\TestFramework\Fixture\DataFixtureStorageManager;
+use Magento\TestFramework\Fixture\DbIsolation;
 use Magento\TestFramework\Helper\Bootstrap;
 use PHPUnit\Framework\TestCase;
 
@@ -14,23 +26,28 @@ use PHPUnit\Framework\TestCase;
  * Integration test for Store::getBaseCurrencyCode() with multi-website currency configuration.
  *
  * @see https://github.com/magento/magento2/issues/40632
- * @magentoDbIsolation disabled
  */
 class StoreBaseCurrencyTest extends TestCase
 {
     /**
      * Verify that getBaseCurrencyCode() returns the website-scoped base currency
      * when catalog price scope is set to WEBSITE.
-     *
-     * @magentoDataFixture Magento/Store/_files/second_website_with_second_base_currency.php
-     * @magentoAppIsolation enabled
      */
+    #[
+        DbIsolation(false),
+        AppIsolation(true),
+        DataFixture(WebsiteFixture::class, ['code' => 'test_eur_website'], 'w1'),
+        DataFixture(StoreGroupFixture::class, ['website_id' => '$w1.id$'], 'g1'),
+        DataFixture(StoreFixture::class, ['store_group_id' => '$g1.id$', 'code' => 'test_eur_store'], 's1'),
+        ConfigFixture('catalog/price/scope', Store::PRICE_SCOPE_WEBSITE),
+        ConfigFixture(Currency::XML_PATH_CURRENCY_BASE, 'EUR', ScopeInterface::SCOPE_WEBSITE, 'test_eur_website'),
+    ]
     public function testGetBaseCurrencyCodeReturnsWebsiteCurrencyWhenPriceScopeIsWebsite(): void
     {
         $objectManager = Bootstrap::getObjectManager();
         /** @var StoreManagerInterface $storeManager */
         $storeManager = $objectManager->get(StoreManagerInterface::class);
-        $store = $storeManager->getStore('fixture_second_store');
+        $store = $storeManager->getStore('test_eur_store');
 
         $this->assertSame(
             'EUR',
@@ -42,17 +59,22 @@ class StoreBaseCurrencyTest extends TestCase
     /**
      * Verify that getBaseCurrencyCode() returns the default base currency
      * when catalog price scope is set to GLOBAL, even if website has a different base currency.
-     *
-     * @magentoDataFixture Magento/Store/_files/second_website_with_second_base_currency.php
-     * @magentoConfigFixture current_store catalog/price/scope 0
-     * @magentoAppIsolation enabled
      */
+    #[
+        DbIsolation(false),
+        AppIsolation(true),
+        DataFixture(WebsiteFixture::class, ['code' => 'test_eur_website'], 'w1'),
+        DataFixture(StoreGroupFixture::class, ['website_id' => '$w1.id$'], 'g1'),
+        DataFixture(StoreFixture::class, ['store_group_id' => '$g1.id$', 'code' => 'test_eur_store'], 's1'),
+        ConfigFixture('catalog/price/scope', Store::PRICE_SCOPE_GLOBAL),
+        ConfigFixture(Currency::XML_PATH_CURRENCY_BASE, 'EUR', ScopeInterface::SCOPE_WEBSITE, 'test_eur_website'),
+    ]
     public function testGetBaseCurrencyCodeReturnsDefaultCurrencyWhenPriceScopeIsGlobal(): void
     {
         $objectManager = Bootstrap::getObjectManager();
         /** @var StoreManagerInterface $storeManager */
         $storeManager = $objectManager->get(StoreManagerInterface::class);
-        $store = $storeManager->getStore('fixture_second_store');
+        $store = $storeManager->getStore('test_eur_store');
 
         $this->assertSame(
             'USD',
@@ -64,10 +86,16 @@ class StoreBaseCurrencyTest extends TestCase
     /**
      * Verify that default store still returns the correct base currency
      * when price scope is WEBSITE (default website uses USD).
-     *
-     * @magentoDataFixture Magento/Store/_files/second_website_with_second_base_currency.php
-     * @magentoAppIsolation enabled
      */
+    #[
+        DbIsolation(false),
+        AppIsolation(true),
+        DataFixture(WebsiteFixture::class, ['code' => 'test_eur_website'], 'w1'),
+        DataFixture(StoreGroupFixture::class, ['website_id' => '$w1.id$'], 'g1'),
+        DataFixture(StoreFixture::class, ['store_group_id' => '$g1.id$', 'code' => 'test_eur_store'], 's1'),
+        ConfigFixture('catalog/price/scope', Store::PRICE_SCOPE_WEBSITE),
+        ConfigFixture(Currency::XML_PATH_CURRENCY_BASE, 'EUR', ScopeInterface::SCOPE_WEBSITE, 'test_eur_website'),
+    ]
     public function testDefaultStoreRetainsBaseCurrencyWhenPriceScopeIsWebsite(): void
     {
         $objectManager = Bootstrap::getObjectManager();
