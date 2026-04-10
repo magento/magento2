4d2443bc7f7161d053453ffa75866a1a79b1b935
Łukasz Bajsarowicz <lukasz@lbajsarowicz.me>
2026-04-10 11:39:47 +0200

    perf: add compiled config loading for catalog_attributes.xml
    Extends the opcache-friendly compiled config pattern to catalog attribute
    configuration. This static XML config is loaded on product/category pages
    and benefits from opcache-friendly PHP file loading.

--- app/code/Magento/Catalog/Model/Attribute/Config/Data.php
+++ app/code/Magento/Catalog/Model/Attribute/Config/Data.php
@@ -5,6 +5,7 @@
  */
 namespace Magento\Catalog\Model\Attribute\Config;
 
+use Magento\Framework\App\ObjectManager\ConfigWriterInterface;
 use Magento\Framework\Serialize\SerializerInterface;
 
 /**
@@ -19,14 +20,16 @@ class Data extends \Magento\Framework\Config\Data
      * @param \Magento\Framework\Config\CacheInterface $cache
      * @param string|null $cacheId
      * @param SerializerInterface|null $serializer
+     * @param ConfigWriterInterface|null $configWriter
      * phpcs:disable Generic.CodeAnalysis.UselessOverridingMethod.Found
      */
     public function __construct(
         \Magento\Catalog\Model\Attribute\Config\Reader $reader,
         \Magento\Framework\Config\CacheInterface $cache,
         $cacheId = 'catalog_attributes',
-        ?SerializerInterface $serializer = null
+        ?SerializerInterface $serializer = null,
+        ?ConfigWriterInterface $configWriter = null
     ) {
-        parent::__construct($reader, $cache, $cacheId, $serializer);
+        parent::__construct($reader, $cache, $cacheId, $serializer, null, $configWriter);
     }
 }
