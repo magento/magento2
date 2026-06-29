975f410676bf161c945be47451ed1abe4a6255c1
Abhishek Pathak <wip44850@adobe.com>
2026-06-29 11:38:40 +0530

    AC-6883: Fix Web-Api test

--- dev/tests/api-functional/testsuite/Magento/GraphQl/CatalogGraphQl/ProductPriceFilterWithTaxTest.php
+++ dev/tests/api-functional/testsuite/Magento/GraphQl/CatalogGraphQl/ProductPriceFilterWithTaxTest.php
@@ -7,13 +7,68 @@ declare(strict_types=1);
 
 namespace Magento\GraphQl\CatalogGraphQl;
 
+use Magento\Catalog\Test\Fixture\Category as CategoryFixture;
+use Magento\Catalog\Test\Fixture\Product as ProductFixture;
+use Magento\Indexer\Test\Fixture\Indexer;
+use Magento\Tax\Test\Fixture\TaxRate as TaxRateFixture;
+use Magento\Tax\Test\Fixture\TaxRule as TaxRuleFixture;
+use Magento\TestFramework\Fixture\Config;
+use Magento\TestFramework\Fixture\DataFixture;
+use Magento\TestFramework\Fixture\DataFixtureStorageManager;
 use Magento\TestFramework\TestCase\GraphQlAbstract;
 
 /**
  * Verify GraphQL price filter uses displayed (incl. tax) values when catalog prices exclude tax.
- *
- * @magentoApiDataFixture Magento/CatalogGraphQl/_files/products_price_filter_tax.php
  */
+#[
+    Config('tax/calculation/price_includes_tax', 0),
+    Config('tax/display/type', 2),
+    Config('tax/defaults/country', 'US'),
+    Config('tax/defaults/region', '12'),
+    Config('tax/defaults/postcode', '*'),
+    Config('shipping/origin/country_id', 'US'),
+    Config('shipping/origin/region_id', '12'),
+    Config('shipping/origin/postcode', '90001'),
+    DataFixture(
+        TaxRateFixture::class,
+        [
+            'tax_country_id' => 'US',
+            'tax_region_id' => '12',
+            'tax_postcode' => '*',
+            'rate' => '7.5',
+        ],
+        'tax_rate'
+    ),
+    DataFixture(
+        TaxRuleFixture::class,
+        [
+            'customer_tax_class_ids' => [3],
+            'product_tax_class_ids' => [2],
+            'tax_rate_ids' => ['$tax_rate.id$'],
+        ],
+        'tax_rule'
+    ),
+    DataFixture(CategoryFixture::class, as: 'category'),
+    DataFixture(
+        ProductFixture::class,
+        [
+            'sku' => 'graphql_price_filter_a',
+            'price' => 9.30,
+            'category_ids' => ['$category.id$'],
+        ],
+        'product_a'
+    ),
+    DataFixture(
+        ProductFixture::class,
+        [
+            'sku' => 'graphql_price_filter_b',
+            'price' => 10.00,
+            'category_ids' => ['$category.id$'],
+        ],
+        'product_b'
+    ),
+    DataFixture(Indexer::class),
+]
 class ProductPriceFilterWithTaxTest extends GraphQlAbstract
 {
     private const QUERY = <<<'QUERY'
@@ -42,7 +97,7 @@ QUERY;
     public function testPriceFilterIncludesProductWithDisplayedPriceAtLowerBound(): void
     {
         $response = $this->graphQlQuery(
-            sprintf(self::QUERY, '10', '15', $this->getDefaultCategoryUid())
+            sprintf(self::QUERY, '10', '15', $this->getCategoryUid())
         );
 
         self::assertCount(2, $response['products']['items']);
@@ -55,7 +110,7 @@ QUERY;
     public function testPriceFilterExcludesProductBelowDisplayedFromValue(): void
     {
         $response = $this->graphQlQuery(
-            sprintf(self::QUERY, '10', '10.74', $this->getDefaultCategoryUid())
+            sprintf(self::QUERY, '10', '10.74', $this->getCategoryUid())
         );
 
         self::assertCount(1, $response['products']['items']);
@@ -66,15 +121,17 @@ QUERY;
     public function testPriceFilterExcludesProductAboveDisplayedToValue(): void
     {
         $response = $this->graphQlQuery(
-            sprintf(self::QUERY, '9', '9.99', $this->getDefaultCategoryUid())
+            sprintf(self::QUERY, '9', '9.99', $this->getCategoryUid())
         );
 
         self::assertCount(1, $response['products']['items']);
         self::assertSame('graphql_price_filter_a', $response['products']['items'][0]['sku']);
     }
 
-    private function getDefaultCategoryUid(): string
+    private function getCategoryUid(): string
     {
-        return base64_encode('2');
+        $category = DataFixtureStorageManager::getStorage()->get('category');
+
+        return base64_encode((string)$category->getId());
     }
 }
