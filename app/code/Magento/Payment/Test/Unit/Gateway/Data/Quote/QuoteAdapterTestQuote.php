6a4ba400d525a20f08cc1558d6caee94d8f05778
Łukasz Bajsarowicz <lukasz@lbajsarowicz.me>
2026-07-11 01:39:06 +0200

    Fix copyright year in new test files

--- app/code/Magento/Payment/Test/Unit/Gateway/Data/Quote/QuoteAdapterTestQuote.php
+++ app/code/Magento/Payment/Test/Unit/Gateway/Data/Quote/QuoteAdapterTestQuote.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright 2015 Adobe
+ * Copyright 2026 Adobe
  * All Rights Reserved.
  */
 declare(strict_types=1);
@@ -9,8 +9,16 @@ namespace Magento\Payment\Test\Unit\Gateway\Data\Quote;
 
 use Magento\Quote\Model\Quote;
 
+/**
+ * Quote double declaring the magic getter mocked by QuoteAdapterTest.
+ */
 class QuoteAdapterTestQuote extends Quote
 {
+    /**
+     * Return the base grand total; overridden in test mocks.
+     *
+     * @return float|null
+     */
     public function getBaseGrandTotal()
     {
         return null;
