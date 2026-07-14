40ac964d52b0d69ec8bdf8a605913d8982502b83
Dmytro Yushkin <dyushkin@adobe.com>
2026-07-14 12:52:55 -0500

    ACP2E-4743: Improve Product Export UI to clearly indicate mandatory (non-excludable) fields - Dropdown dependant notice rendering

---
+++ app/code/Magento/CatalogImportExport/view/adminhtml/web/js/mandatory-notice-toggle.js
@@ -0,0 +1,26 @@
+/**
+ * Copyright 2026 Adobe
+ * All Rights Reserved.
+ */
+define(['jquery'], function ($) {
+    'use strict';
+
+    return function (config, element) {
+        var $notice = $(element),
+            $entityField = $(config.entitySelector);
+
+        var toggleMandatoryAttributesNotice = function (entityValue) {
+            if (entityValue === 'catalog_product') {
+                $notice.show();
+            } else {
+                $notice.hide();
+            }
+        };
+
+        toggleMandatoryAttributesNotice($entityField.val());
+
+        $entityField.on('change', function () {
+            toggleMandatoryAttributesNotice($(this).val());
+        });
+    };
+});
