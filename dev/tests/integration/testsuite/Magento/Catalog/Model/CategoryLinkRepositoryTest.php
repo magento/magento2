48518c90c1b931b848a1b01d87492c0a291ee7c1
Stanislav Ilnytskyi <stailx1@gmail.com>
2026-08-05 14:15:07 +0200

    static tests fixes

--- dev/tests/integration/testsuite/Magento/Catalog/Model/CategoryLinkRepositoryTest.php
+++ dev/tests/integration/testsuite/Magento/Catalog/Model/CategoryLinkRepositoryTest.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Copyright © Magento, Inc. All rights reserved.
- * See COPYING.txt for license details.
+ * Copyright 2021 Adobe
+ * All Rights Reserved.
  */
 declare(strict_types=1);
 
@@ -10,14 +10,23 @@ namespace Magento\Catalog\Model;
 use Magento\Catalog\Api\CategoryLinkRepositoryInterface;
 use Magento\Catalog\Api\CategoryRepositoryInterface;
 use Magento\Catalog\Api\Data\CategoryProductLinkInterface;
+use Magento\Catalog\Test\Fixture\Category as CategoryFixture;
+use Magento\Catalog\Test\Fixture\Product as ProductFixture;
 use Magento\Framework\App\Area;
-use Magento\Store\Api\StoreRepositoryInterface;
 use Magento\Store\Model\App\Emulation;
 use Magento\Store\Model\Store;
+use Magento\Store\Test\Fixture\Store as StoreFixture;
+use Magento\TestFramework\Fixture\AppArea;
+use Magento\TestFramework\Fixture\DataFixture;
+use Magento\TestFramework\Fixture\DataFixtureStorageManager;
+use Magento\TestFramework\Fixture\DbIsolation;
+use Magento\TestFramework\Fixture\ScopeFixture;
 use Magento\TestFramework\Helper\Bootstrap;
 use Magento\TestFramework\ObjectManager;
 use PHPUnit\Framework\TestCase;
 
+#[AppArea('crontab')]
+#[DbIsolation(false)]
 class CategoryLinkRepositoryTest extends TestCase
 {
     /**
@@ -36,9 +45,9 @@ class CategoryLinkRepositoryTest extends TestCase
     private $categoryRepository;
 
     /**
-     * @var StoreRepositoryInterface
+     * @var DataFixtureStorageManager
      */
-    private $storeRepository;
+    private $fixtures;
 
     /**
      * @var Emulation
@@ -49,9 +58,9 @@ class CategoryLinkRepositoryTest extends TestCase
     {
         $this->objectManager = Bootstrap::getObjectManager();
         $this->categoryLinkRepository = $this->objectManager->get(CategoryLinkRepositoryInterface::class);
-        $this->storeRepository = $this->objectManager->get(StoreRepositoryInterface::class);
         $this->categoryRepository = $this->objectManager->get(CategoryRepositoryInterface::class);
         $this->appEmulation = $this->objectManager->get(Emulation::class);
+        $this->fixtures = DataFixtureStorageManager::getStorage();
         parent::setUp();
     }
 
@@ -61,30 +70,72 @@ class CategoryLinkRepositoryTest extends TestCase
      * The default store is used in
      * @see \Magento\Catalog\Model\CategoryLinkRepository::save
      *
-     * @magentoAppArea crontab
-     * @magentoDbIsolation disabled
-     * @magentoDataFixture Magento/Catalog/_files/saving_product_category_link_under_different_stores.php
      */
+    #[
+        DataFixture(ScopeFixture::class, ['code' => 'default'], as: 'default_store'),
+        DataFixture(
+            StoreFixture::class,
+            ['code' => 'fixture_second_store', 'name' => 'Fixture Store', 'sort_order' => 10],
+            as: 'second_store'
+        ),
+        DataFixture(
+            CategoryFixture::class,
+            [
+                'name' => 'Test Category In Default Store',
+                'description' => 'Test description in default store',
+                'is_anchor' => 1,
+                'parent_id' => 2,
+                'available_sort_by' => ['name'],
+                'default_sort_by' => 'name',
+                'is_active' => true,
+            ],
+            as: 'category',
+            scope: 'default_store'
+        ),
+        DataFixture(
+            CategoryFixture::class,
+            [
+                'id' => '$category.id$',
+                'name' => 'Test Category In Second Store',
+                'description' => 'Test description in second store',
+            ],
+            scope: 'second_store'
+        ),
+        DataFixture(
+            ProductFixture::class,
+            [
+                'name' => 'Simple Product116',
+                'sku' => 'simple116',
+                'price' => 10,
+                'website_ids' => ['$second_store.website_id$'],
+            ],
+            as: 'product',
+            scope: 'default_store'
+        ),
+    ]
     public function testCategoryDataNotChanged()
     {
-        $secondStore = $this->storeRepository->get('fixture_second_store');
+        $secondStore = $this->fixtures->get('second_store');
+        $category = $this->fixtures->get('category');
+        $product = $this->fixtures->get('product');
         $secondStoreId = (int)$secondStore->getId();
+        $categoryId = (int)$category->getId();
         /** @var CategoryProductLinkInterface $categoryProductLink */
         $categoryProductLink = $this->objectManager->create(CategoryProductLinkInterface::class);
 
         $this->appEmulation->startEnvironmentEmulation($secondStoreId, Area::AREA_FRONTEND, true);
 
         $categoryProductLink
-            ->setCategoryId(113)
-            ->setSku('simple116')
+            ->setCategoryId($categoryId)
+            ->setSku($product->getSku())
             ->setPosition(2);
 
         $this->categoryLinkRepository->save($categoryProductLink);
 
         $this->appEmulation->stopEnvironmentEmulation();
 
-        $categoryFromDefaultStore = $this->categoryRepository->get(113);
-        $categoryFromSecondStore = $this->categoryRepository->get(113, $secondStoreId);
+        $categoryFromDefaultStore = $this->categoryRepository->get($categoryId, Store::DEFAULT_STORE_ID);
+        $categoryFromSecondStore = $this->categoryRepository->get($categoryId, $secondStoreId);
 
         $categoryNameFromDefaultStore = $categoryFromDefaultStore->getName();
         $categoryDescriptionFromDefaultStore = $categoryFromDefaultStore->getDescription();
