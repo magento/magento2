149d84c28e25367c09158548f128757a038ef6a8
Alexander Khokhlov <dyxterrorist7+github@gmail.com>
2026-09-02 14:44:24 +0300

    Revert "magento/magento2#40127: remove Whitespace Characters in Translatable Phrases"
    This reverts commit 3d05bdeeac2da08b95993d44481f4a0142d4a7cf.

--- app/code/Magento/Quote/Model/Quote/Address/Total/Collector.php
+++ app/code/Magento/Quote/Model/Quote/Address/Total/Collector.php
@@ -126,7 +126,8 @@ class Collector extends \Magento\Sales\Model\Config\Ordered
         if (!$model instanceof \Magento\Quote\Model\Quote\Address\Total\AbstractTotal) {
             throw new \Magento\Framework\Exception\LocalizedException(
                 __(
-                    'The address total model should be extended from \Magento\Quote\Model\Quote\Address\Total\AbstractTotal.'
+                    'The address total model should be extended from
+                    \Magento\Quote\Model\Quote\Address\Total\AbstractTotal.'
                 )
             );
         }
