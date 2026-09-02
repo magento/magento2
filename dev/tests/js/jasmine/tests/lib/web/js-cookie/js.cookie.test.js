c22709a26b40d3671a2416d5ee0af9b88a3de731
Bhavin Parmar <cod62627@adobe.com>
2026-09-02 13:54:14 +0530

    AC-18124: Upgrade js-cookie from 3.0.5 to 3.0.8 to resolve CVE-2026-46625

---
+++ dev/tests/js/jasmine/tests/lib/web/js-cookie/js.cookie.test.js
@@ -0,0 +1,51 @@
+/**
+ * Copyright 2026 Adobe
+ * All Rights Reserved.
+ */
+
+/* eslint-disable max-nested-callbacks */
+define(['js-cookie/js.cookie'], (Cookies) => {
+    'use strict';
+
+    describe('js-cookie/js.cookie', () => {
+        let lastSetCookie;
+
+        beforeEach(() => {
+            lastSetCookie = '';
+
+            Object.defineProperty(document, 'cookie', {
+                configurable: true,
+                get: () => lastSetCookie,
+                set: (value) => {
+                    lastSetCookie = value;
+                }
+            });
+        });
+
+        describe('CVE-2026-46625', () => {
+            it('should not inject cookie attributes via __proto__ pollution', () => {
+                const attackerAttrs = JSON.parse(
+                    '{"__proto__":{"secure":"false","domain":"evil.com","samesite":"None","expires":-1}}'
+                );
+
+                Cookies.set('session', 'TOKEN', attackerAttrs);
+
+                expect(lastSetCookie).toContain('session=TOKEN');
+                expect(lastSetCookie).not.toContain('domain=evil.com');
+                expect(lastSetCookie).not.toContain('secure=false');
+                expect(lastSetCookie).not.toContain('samesite=None');
+            });
+
+            it('should still apply legitimate cookie attributes', () => {
+                Cookies.set('session', 'TOKEN', {
+                    path: '/checkout',
+                    secure: true
+                });
+
+                expect(lastSetCookie).toContain('session=TOKEN');
+                expect(lastSetCookie).toContain('path=/checkout');
+                expect(lastSetCookie).toContain('secure');
+            });
+        });
+    });
+});
